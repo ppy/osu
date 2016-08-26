24e8defcd252bb0ec812f92fa70e79da0e6793f8
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2016-08-26 17:27:49 +0900

    License -> Licence.

--- osu.Desktop/Program.cs
+++ osu.Desktop/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
-//Licensed under the MIT License - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
 using osu.Framework.Desktop;
