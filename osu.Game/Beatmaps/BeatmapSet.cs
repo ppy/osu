3098204dda1839aeb0e848848dc48bca2b441b38
Dean Herbert <pe@ppy.sh>
2016-08-31 12:33:01 +0900

    Add basic class structure for Beatmap/HitObject/SampleInfo/User.

---
+++ osu.Game/Beatmaps/BeatmapSet.cs
@@ -0,0 +1,21 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+using osu.Game.Users;
+
+namespace osu.Game.Beatmaps
+{
+    /// <summary>
+    /// A beatmap set contains multiple beatmap (difficulties).
+    /// </summary>
+    public class BeatmapSet
+    {
+        public List<Beatmap> Beatmaps { get; protected set; }
+
+        public string Artist;
+        public string Title;
+
+        public User Creator;
+    }
+}
---
+++ osu.Game/Beatmaps/Objects/HitObject.cs
@@ -0,0 +1,18 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Beatmaps.Samples;
+
+namespace osu.Game.Beatmaps.Objects
+{
+    /// <summary>
+    /// A hitobject describes a point in a beatmap 
+    /// </summary>
+    public class HitObject
+    {
+        public double StartTime;
+        public double? EndTime;
+
+        public SampleInfo Sample;
+    }
+}
