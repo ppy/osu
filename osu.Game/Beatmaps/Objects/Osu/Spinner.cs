f316017455ad95b908c76eab31085037bfe61833
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2016-09-02 18:35:49 +0900

    Add basic structure for osu! mode objects.

--- osu.Game/Beatmaps/Objects/Osu/Spinner.cs
+++ osu.Game/Beatmaps/Objects/Osu/Spinner.cs
@@ -1,12 +1,10 @@
 ﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 //Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK;
 
 namespace osu.Game.Beatmaps.Objects.Osu
 {
-    public abstract class OsuBaseHit : BaseHit
+    public class Spinner
     {
-        public Vector2 Position;
     }
 }
