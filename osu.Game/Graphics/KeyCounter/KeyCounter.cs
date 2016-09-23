d2aa885bc333dfa3e833e160de0f435b13c3eafb
abraker95 <abraker95@gmail.com>
2016-09-23 03:59:31 -0400

    Add license info

--- osu.Game/Graphics/KeyCounter/KeyCounter.cs
+++ osu.Game/Graphics/KeyCounter/KeyCounter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 
--- osu.Game/Graphics/KeyCounter/MouseCount.cs
+++ osu.Game/Graphics/KeyCounter/MouseCount.cs
@@ -1,5 +1,8 @@
 ﻿using OpenTK;
 using OpenTK.Input;
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
 using osu.Framework.Graphics;
 using osu.Framework.Input;
 
