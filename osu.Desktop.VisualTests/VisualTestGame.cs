19eda06f33d019d2dae3ee49b2582c30b6c62b13
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2016-09-24 00:39:20 +0900

    Add basic implementation of VisualTest framework for osu! project.
    Comes with one complimentary test.

---
+++ osu.Desktop.VisualTests/VisualTestGame.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.GameModes.Testing;
+using osu.Framework.Graphics.Cursor;
+using osu.Game;
+
+namespace osu.Framework.VisualTests
+{
+    class VisualTestGame : OsuGameBase
+    {
+        public override void Load()
+        {
+            base.Load();
+
+            Add(new TestBrowser());
+
+            ShowPerformanceOverlay = true;
+        }
+    }
+}
