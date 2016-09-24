9fa81090fb50c961f2d27862a39cad282f76a508
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2016-09-24 08:57:13 +0800

    Visual test for KeyCounter.

---
+++ osu.Desktop.VisualTests/Tests/TestCaseKeyCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenTK.Input;
+using osu.Framework.GameModes.Testing;
+using osu.Framework.Graphics;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Desktop.Tests
+{
+    class TestCaseKeyCounter : TestCase
+    {
+        public override string Name => @"KeyCounter";
+
+        public override string Description => @"Tests key counter";
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            KeyCounter kc = new KeyCounter
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                IsCounting = true
+            };
+            Add(kc);
+            kc.AddKey(new KeyBoardCount(@"Z", Key.Z));
+            kc.AddKey(new KeyBoardCount(@"X", Key.X));
+            kc.AddKey(new MouseCount(@"M1", MouseButton.Left));
+            kc.AddKey(new MouseCount(@"M2", MouseButton.Right));
+        }
+    }
+}
