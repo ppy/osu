d93718d953bd0cddc37304b2cebf595550b6aa08
Dean Herbert <pe@ppy.sh>
2016-09-30 18:45:27 +0900

    Add template toolbar.

---
+++ osu.Game/Overlays/Toolbar.cs
@@ -0,0 +1,39 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Drawables;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace osu.Game.Overlays
+{
+    public class Toolbar : Container
+    {
+        const float height = 50;
+
+        public override void Load()
+        {
+            base.Load();
+
+            Depth = float.MaxValue;
+            SizeMode = InheritMode.X;
+            Size = new Vector2(1, height);
+
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    SizeMode = InheritMode.XY,
+                    Colour = new Color4(0.1f, 0.1f, 0.1f, 0.9f)
+                }
+            };
+        }
+    }
+}
