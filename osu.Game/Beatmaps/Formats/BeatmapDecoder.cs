5dd57e4be23713e305ed4670141c1004573f221e
Drew DeVault <sir@cmpwn.com>
2016-10-04 16:29:08 -0400

    Initial support code for beatmap loading

---
+++ osu.Game/Beatmaps/Formats/BeatmapDecoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace osu.Game.Beatmaps.Formats
+{
+    public abstract class BeatmapDecoder
+    {
+        private static Dictionary<string, Type> Decoders { get; set; } = new Dictionary<string, Type>();
+    
+        public static BeatmapDecoder GetDecoder(TextReader stream)
+        {
+            var line = stream.ReadLine().Trim();
+            if (!Decoders.ContainsKey(line))
+                throw new IOException("Unknown file format");
+            return (BeatmapDecoder)Activator.CreateInstance(Decoders[line]);
+        }
+        protected static void AddDecoder<T>(string magic) where T : BeatmapDecoder
+        {
+            Decoders[magic] = typeof(T);
+        }
+    
+        public abstract Beatmap Decode(TextReader stream);
+    }
+}
