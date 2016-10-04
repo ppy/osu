d14bf6b78b049088b647b8f7a52ee369b514dfb3
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-10-04 11:31:10 -0400

    Add initial DB schema and support code

---
+++ osu.Game/Database/BeatmapSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SQLite;
+
+namespace osu.Game.Database
+{
+    public class BeatmapSet
+    {
+        [PrimaryKey]
+        public int BeatmapSetID { get; set; }
+        [NotNull, Indexed]
+        public int MetadataID { get; set; }
+    }
+}
+
