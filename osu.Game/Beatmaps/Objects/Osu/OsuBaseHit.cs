0f0662bbec6dc81d45f5601c6b6aadd526a09d5c
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-10-07 16:35:14 -0400

    Add basic osu! hit object parsing

--- osu.Game/Beatmaps/Objects/Osu/OsuBaseHit.cs
+++ osu.Game/Beatmaps/Objects/Osu/OsuBaseHit.cs
@@ -1,17 +1,53 @@
 ﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 //Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
 using OpenTK;
+using osu.Game.Beatmaps.Samples;
 
 namespace osu.Game.Beatmaps.Objects.Osu
 {
     public abstract class OsuBaseHit : HitObject
     {
-        public Vector2 Position;
+        public Vector2 Position { get; set; }
+        public bool NewCombo { get; set; }
+
+        [Flags]
+        private enum HitObjectType
+        {
+            Circle = 1,
+            Slider = 2,
+            NewCombo = 4,
+            Spinner = 8,
+        }
 
         public static OsuBaseHit Parse(string val)
         {
-            return null;
+            string[] split = val.Split(',');
+            var type = (HitObjectType)int.Parse(split[3]);
+            bool combo = type.HasFlag(HitObjectType.NewCombo);
+            type &= ~HitObjectType.NewCombo;
+            OsuBaseHit result;
+            switch (type)
+            {
+                case HitObjectType.Circle:
+                    result = new Circle();
+                    break;
+                case HitObjectType.Slider:
+                    result = new Slider();
+                    break;
+                case HitObjectType.Spinner:
+                    result = new Spinner();
+                    break;
+                default:
+                    throw new InvalidOperationException($@"Unknown hit object type {type}");
+            }
+            result.Position = new Vector2(int.Parse(split[0]), int.Parse(split[1]));
+            result.StartTime = double.Parse(split[2]);
+            result.Sample = new HitSampleInfo { Type = (SampleType)int.Parse(split[3]) };
+            result.NewCombo = combo;
+            // TODO: "addition" field
+            return result;
         }
     }
 }
