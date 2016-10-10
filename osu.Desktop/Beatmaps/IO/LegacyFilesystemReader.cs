984215b554e8ad1b58191e065691f22bbe3989fb
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-10-10 14:00:33 -0400

    Fix casing on private properties

--- osu.Desktop/Beatmaps/IO/LegacyFilesystemReader.cs
+++ osu.Desktop/Beatmaps/IO/LegacyFilesystemReader.cs
@@ -18,36 +18,36 @@ namespace osu.Desktop.Beatmaps.IO
             AddReader<LegacyFilesystemReader>((storage, path) => Directory.Exists(path));
         }
     
-        private string BasePath { get; set; }
-        private string[] Beatmaps { get; set; }
-        private Beatmap FirstMap { get; set; }
+        private string basePath { get; set; }
+        private string[] beatmaps { get; set; }
+        private Beatmap firstMap { get; set; }
     
         public LegacyFilesystemReader(string path)
         {
-            BasePath = path;
-            Beatmaps = Directory.GetFiles(BasePath, "*.osu").Select(f => Path.GetFileName(f)).ToArray();
-            if (Beatmaps.Length == 0)
+            basePath = path;
+            beatmaps = Directory.GetFiles(basePath, "*.osu").Select(f => Path.GetFileName(f)).ToArray();
+            if (beatmaps.Length == 0)
                 throw new FileNotFoundException("This directory contains no beatmaps");
-            using (var stream = new StreamReader(ReadFile(Beatmaps[0])))
+            using (var stream = new StreamReader(ReadFile(beatmaps[0])))
             {
                 var decoder = BeatmapDecoder.GetDecoder(stream);
-                FirstMap = decoder.Decode(stream);
+                firstMap = decoder.Decode(stream);
             }
         }
 
         public override string[] ReadBeatmaps()
         {
-            return Beatmaps;
+            return beatmaps;
         }
 
         public override Stream ReadFile(string name)
         {
-            return File.OpenRead(Path.Combine(BasePath, name));
+            return File.OpenRead(Path.Combine(basePath, name));
         }
 
         public override BeatmapMetadata ReadMetadata()
         {
-            return FirstMap.Metadata;
+            return firstMap.Metadata;
         }
         
         public override void Dispose()
