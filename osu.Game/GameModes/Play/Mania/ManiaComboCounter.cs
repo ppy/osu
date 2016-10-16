9d71e1ba4213a2bf814b2f91280a4d143bfc6ab3
Adonais Romero González <87902114+bubb13gummy@users.noreply.github.com>
2016-10-16 17:45:37 -0500

    Modified ManiaComboCounter behavior
    This to match more with the current behavior in osu!mania.

--- osu.Game/GameModes/Play/Mania/ManiaComboCounter.cs
+++ osu.Game/GameModes/Play/Mania/ManiaComboCounter.cs
@@ -4,6 +4,7 @@
 using OpenTK.Graphics;
 using osu.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Transformations;
 using osu.Game.GameModes.Play.Taiko;
 using System;
 using System.Collections.Generic;
@@ -18,9 +19,14 @@ namespace osu.Game.GameModes.Play.Mania
     /// </summary>
     public class ManiaComboCounter : TaikoComboCounter
     {
+        protected ushort KeysHeld = 0;
+
         protected Color4 OriginalColour;
 
-        protected Color4 TintColour => Color4.OrangeRed;
+        protected Color4 TintColour => Color4.Orange;
+        protected EasingTypes TintEasing => EasingTypes.None;
+        protected int TintDuration => 500;
+
         protected Color4 PopOutColor => Color4.Red;
         protected override float PopOutInitialAlpha => 1.0f;
         protected override ulong PopOutDuration => 300;
@@ -32,7 +38,7 @@ namespace osu.Game.GameModes.Play.Mania
             PopOutSpriteText.Anchor = Anchor.BottomCentre;
             PopOutSpriteText.Origin = Anchor.Centre;
             PopOutSpriteText.FadeColour(PopOutColor, 0);
-            OriginalColour = Colour;
+            OriginalColour = DisplayedCountSpriteText.Colour;
         }
 
         protected override void OnCountRolling(ulong currentValue, ulong newValue)
@@ -51,11 +57,28 @@ namespace osu.Game.GameModes.Play.Mania
             base.OnCountRolling(currentValue, newValue);
         }
 
-        protected override void transformAnimate(ulong newValue)
+        /// <summary>
+        /// Tints text while holding a key.
+        /// </summary>
+        /// <remarks>
+        /// Does not alter combo. This has to be done depending of the scoring system.
+        /// (i.e. v1 = each period of time; v2 = when starting and ending a key hold)
+        /// </remarks>
+        public void HoldStart()
         {
-            base.transformAnimate(newValue);
-            DisplayedCountSpriteText.FadeColour(TintColour, 0);
-            DisplayedCountSpriteText.FadeColour(OriginalColour, AnimationDuration, AnimationEasing);
+            if (KeysHeld == 0)
+                DisplayedCountSpriteText.FadeColour(TintColour, TintDuration, TintEasing);
+            KeysHeld++;
+        }
+
+        /// <summary>
+        /// Ends tinting.
+        /// </summary>
+        public void HoldEnd()
+        {
+            KeysHeld--;
+            if (KeysHeld == 0)
+                DisplayedCountSpriteText.FadeColour(OriginalColour, TintDuration, TintEasing);
         }
     }
 }
