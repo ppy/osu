e254070dfba68cf39565717887b6dd3450c4f425
Lunar Dust <awoonar.dust@gmail.com>
2016-10-17 15:52:54 -0500

    Avatar implementation

---
+++ osu.Game/Users/Avatar.cs
@@ -0,0 +1,40 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
+using OpenTK;
+
+namespace osu.Framework.Graphics.Sprites
+{
+    public class Avatar : Sprite
+    {
+        public int userId = 0, rounding = 0;
+        public int avatarSize = 50;
+
+
+        public Avatar(int uId, int aSize, int round)
+        {
+            this.userId = uId;
+            this.avatarSize = aSize;
+            this.rounding = round;
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+        }
+
+        public override async void Load(BaseGame game)
+        {
+            base.Load(game);
+            
+            Texture = await game.Textures.GetAsync($@"https://a.ppy.sh/{userId}");
+            Size = new Vector2(avatarSize);
+            CornerRadius = rounding;
+        }
+    }
+}
