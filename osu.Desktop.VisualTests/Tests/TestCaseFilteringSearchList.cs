428f4c3204dedf77a95cb9b9bdf126ae03bf3f39
TocoToucan <TocoToucanMS@gmail.com>
2016-10-23 14:06:49 +0300

    Add FilteringSearchList + TestCase

---
+++ osu.Desktop.VisualTests/Tests/TestCaseFilteringSearchList.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using osu.Framework.GameModes.Testing;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Game.Graphics.UserInterface;
+using OpenTK;
+
+namespace osu.Desktop.Tests
+{
+    public class TestCaseFilteringSearchList : TestCase
+    {
+        public override string Name => @"FilteringSearchList";
+
+        public override string Description => @"Tests filtering search list";
+
+        public TextBox Tb { get; set; }
+        public FilteringSearchList<SpriteText> FilteringSearchList { get; set; }
+
+        public override void Reset()
+        {
+            base.Reset();
+            var items = new List<SpriteText>
+            {
+                new SpriteText
+                {
+                    Text = "A search textbox"
+                },
+                new SpriteText
+                {
+                    Text = "A filtering search list"
+                },
+                new SpriteText
+                {
+                    Text = "A dropdown menu"
+                },
+                new SpriteText
+                {
+                    Text = "Possibly more I've missed"
+                }
+            };
+            var property = typeof(SpriteText).GetProperty("Text");
+            Children = new Drawable[]
+            {
+                new FlowContainer
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Direction = FlowDirection.VerticalOnly,
+                    RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        Tb = new TextBox
+                        {
+                            Size = new Vector2(300, 30)
+                        },
+                        FilteringSearchList = new FilteringSearchList<SpriteText>(items, property)
+                    }
+                }
+            };
+            Tb.OnChange += Tb_OnChange;
+        }
+
+        private void Tb_OnChange(TextBox sender, bool newText)
+        {
+            if (newText)
+                FilteringSearchList.Filter(Tb.Text);
+        }
+    }
+}
---
+++ osu.Game/Graphics/UserInterface/FilteringSearchList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using osu.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class FilteringSearchList<T> : Container where T : Drawable
+    {
+        public List<T> Items { get; }
+
+        public PropertyInfo FilterProperty
+        {
+            get { return filterProperty; }
+            private set
+            {
+                if (typeof(T).GetProperty(value.Name) != null)
+                    filterProperty = value;
+                else
+                    throw new ArgumentException($"\"{value.Name}\" property was not found in \"{typeof(T).Name}\" class");
+            }
+        }
+
+        private FlowContainer contentFlowContainer;
+        private PropertyInfo filterProperty;
+
+        public FilteringSearchList(List<T> items, PropertyInfo filterProperty)
+        {
+            Items = items;
+            FilterProperty = filterProperty;
+        }
+
+        public override void Load(BaseGame game)
+        {
+            base.Load(game);
+
+            RelativeSizeAxes = Axes.Both;
+            Children = new Drawable[]
+            {
+                new ScrollContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        contentFlowContainer = new FlowContainer
+                        {
+                            Direction = FlowDirection.VerticalOnly,
+                            RelativeSizeAxes = Axes.Both,
+                            Children = Items
+                        }
+                    }
+                }
+            };
+        }
+
+        public void Filter(string input = null)
+        {
+            var filteringResult =
+                FilterProperty == null || string.IsNullOrEmpty(input)
+                    ? Items
+                    : Items.Where(i => FilterProperty.GetValue(i).ToString().IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0);
+            foreach (var listItem in contentFlowContainer.Children)
+            {
+                if (filteringResult.Contains(listItem)) listItem.Show();
+                else listItem.Hide();
+            }
+            contentFlowContainer.Invalidate();
+        }
+    }
+}
