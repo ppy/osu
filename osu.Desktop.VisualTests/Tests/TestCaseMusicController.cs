56ae346225329fc6154eb9d59fc0f3d54fedd4fe
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2016-10-24 10:22:38 +0800

    Create MusicController UI class and visual test.

---
+++ osu.Desktop.VisualTests/Tests/TestCaseMusicController.cs
@@ -0,0 +1,32 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using osu.Framework.GameModes.Testing;
+using osu.Game.Overlays;
+
+namespace osu.Desktop.Tests
+{
+    class TestCaseMusicController : TestCase
+    {
+        public override string Name => @"Music Controller";
+        public override string Description => @"Tests music controller ui.";
+
+        public override void Reset()
+        {
+            base.Reset();
+            MusicController mc = new MusicController
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre
+            };
+            Add(mc);
+            AddToggle(@"Show", mc.ToggleVisibility);
+        }
+    }
+}
---
+++ osu.Game/Overlays/MusicController.cs
@@ -0,0 +1,25 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework;
+using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Overlays
+{
+    public class MusicController : OverlayContainer
+    {
+        public override void Load(BaseGame game)
+        {
+            base.Load(game);
+            Width = 400;
+            Height = 130;
+            CornerRadius = 5;
+            Masking = true;
+        }
+
+        //placeholder for toggling
+        protected override void PopIn() => FadeIn(500);
+
+        protected override void PopOut() => FadeOut(500);
+    }
+}
