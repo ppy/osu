55e5ec6fae4779eefba41c62990e09cb5eae47a9
Drew DeVault <sir@cmpwn.com>
2016-10-26 10:52:04 -0400

    General cleanup per PR feedback

--- osu.Game/GameModes/Play/BeatmapPanel.cs
+++ osu.Game/GameModes/Play/BeatmapPanel.cs
@@ -2,29 +2,22 @@
 //Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
-using System.Collections.Generic;
-using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input;
-using osu.Game.Beatmaps;
-using osu.Game.GameModes.Backgrounds;
-using osu.Framework;
 using osu.Game.Database;
 using osu.Framework.Graphics.Primitives;
 using OpenTK.Graphics;
 using OpenTK;
 using osu.Game.Graphics;
-using osu.Framework.Graphics.Transformations;
 using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.GameModes.Play
 {
-    class BeatmapButton : Container
+    class BeatmapPanel : Container
     {
-        private BeatmapSetInfo beatmapSet;
-        private BeatmapInfo beatmap;
+        public BeatmapInfo Beatmap;
 
         public Action<BeatmapInfo> MapSelected;
         
@@ -47,10 +40,9 @@ namespace osu.Game.GameModes.Play
             }
         }
 
-        public BeatmapButton(BeatmapSetInfo set, BeatmapInfo beatmap)
+        public BeatmapPanel(BeatmapSetInfo set, BeatmapInfo beatmap)
         {
-            this.beatmapSet = set;
-            this.beatmap = beatmap;
+            Beatmap = beatmap;
             AutoSizeAxes = Axes.Y;
             Masking = true;
             CornerRadius = 5;
@@ -109,7 +101,7 @@ namespace osu.Game.GameModes.Play
         
         protected override bool OnClick(InputState state)
         {
-            MapSelected?.Invoke(beatmap);
+            MapSelected?.Invoke(Beatmap);
             return true;
         }
     }
