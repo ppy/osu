b40ccccbe4fb2cc6074170dd1ef58be98c0b53c0
Dean Herbert <pe@ppy.sh>
2016-11-02 18:08:08 +0900

    Add HitObject colours back in the mix.

---
+++ osu.Game/Beatmaps/Formats/ConstructableBeatmapDecoder.cs
@@ -0,0 +1,20 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Beatmaps.Formats
+{
+    public class ConstructableBeatmapDecoder : BeatmapDecoder
+    {
+        protected override Beatmap ParseFile(TextReader stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
