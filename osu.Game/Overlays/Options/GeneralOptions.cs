2aa85a4b18e7bd738932c8c4fcca49072254d410
Drew DeVault <sir@cmpwn.com>
2016-11-02 22:22:34 -0400

    Split up options into subclasses

---
+++ osu.Game/Overlays/Options/GeneralOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Platform;
+using osu.Game.Online.API;
+
+namespace osu.Game.Overlays.Options
+{
+    public class GeneralOptions : OptionsSection
+    {
+        public GeneralOptions(BasicStorage storage, APIAccess api)
+        {
+            Header = "General";
+            Children = new Drawable[]
+            {
+                new LoginOptions(api),
+                new LanguageOptions(),
+                new UpdateOptions(storage),
+            };
+        }
+    }
+}
+
---
+++ osu.Game/Overlays/Options/LanguageOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace osu.Game.Overlays.Options
+{
+    public class LanguageOptions : OptionsSubsection
+    {
+        public LanguageOptions()
+        {
+            Header = "Language";
+            Children = new Drawable[]
+            {
+                new SpriteText { Text = "TODO: Dropdown" },
+                new BasicCheckBox
+                {
+                    Children = new[] { new SpriteText { Text = "Prefer metadata in original language" } }
+                },
+                new BasicCheckBox
+                {
+                    Children = new[] { new SpriteText { Text = "Use alternative font for chat display" } }
+                },
+            };
+        }
+    }
+}
