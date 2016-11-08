bdd6559e59f4cc003c43677c2a5404e56cc4c19d
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-11-07 22:15:48 -0500

    Rearrange some options a bit, update framework

---
+++ osu.Game/Overlays/Options/Online/NotificationsOptions.cs
@@ -0,0 +1,23 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+
+namespace osu.Game.Overlays.Options.Online
+{
+    public class NotificationsOptions : OptionsSubsection
+    {
+        protected override string Header => "Notifications";
+    
+        public NotificationsOptions()
+        {
+            Children = new Drawable[]
+            {
+                new BasicCheckBox { LabelText = "Enable chat ticker" },
+                new BasicCheckBox { LabelText = "Show a notification popup when someone says your name" },
+                new BasicCheckBox { LabelText = "Show chat message notifications" },
+                new BasicCheckBox { LabelText = "Play a sound when someone says your name" },
+                new BasicCheckBox { LabelText = "Show notification popups instantly during gameplay" },
+                new BasicCheckBox { LabelText = "Show notification popups when friends change status" },
+            };
+        }
+    }
+}
