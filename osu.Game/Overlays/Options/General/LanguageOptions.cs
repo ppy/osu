98658af8915f391744386dbdded50e6a50f97c12
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-11-07 19:04:31 -0500

    Wire up one of the settings to the config
    One step at a time, right

--- osu.Game/Overlays/Options/General/LanguageOptions.cs
+++ osu.Game/Overlays/Options/General/LanguageOptions.cs
@@ -1,4 +1,6 @@
-﻿using osu.Framework.Graphics;
+﻿using System;
+using osu.Framework;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 
@@ -7,15 +9,26 @@ namespace osu.Game.Overlays.Options.General
     public class LanguageOptions : OptionsSubsection
     {
         protected override string Header => "Language";
+        private CheckBoxOption showUnicode;
     
         public LanguageOptions()
         {
             Children = new Drawable[]
             {
                 new SpriteText { Text = "TODO: Dropdown" },
-                new BasicCheckBox { LabelText = "Prefer metadata in original language" },
+                showUnicode = new CheckBoxOption { LabelText = "Prefer metadata in original language" },
                 new BasicCheckBox { LabelText = "Use alternative font for chat display" },
             };
+        }
+        
+        protected override void Load(BaseGame game)
+        {
+            base.Load(game);
+            var osuGame = game as OsuGameBase;
+            if (osuGame != null)
+            {
+                showUnicode.Bindable = osuGame.Config.GetBindable<bool>(Configuration.OsuConfig.ShowUnicode);
+            }
         }
     }
 }
