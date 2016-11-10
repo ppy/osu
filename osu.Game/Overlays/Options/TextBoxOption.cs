9ff962cf600972e4413824c675832e11ca2e6fbf
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-11-10 18:15:30 -0500

    Add TextBoxOption and wire it up

---
+++ osu.Game/Overlays/Options/TextBoxOption.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics.UserInterface;
+
+namespace osu.Game.Overlays.Options
+{
+    public class TextBoxOption : TextBox
+    {
+        private Bindable<string> bindable;
+
+        public Bindable<string> Bindable
+        {
+            set
+            {
+                if (bindable != null)
+                    bindable.ValueChanged -= bindableValueChanged;
+                bindable = value;
+                if (bindable != null)
+                {
+                    base.Text = bindable.Value;
+                    bindable.ValueChanged += bindableValueChanged;
+                }
+            }
+        }
+        
+        protected override string internalText
+        {
+            get { return base.internalText; }
+            set
+            {
+                base.internalText = value;
+                if (bindable != null)
+                    bindable.Value = value;
+            }
+        }
+
+        private void bindableValueChanged(object sender, EventArgs e)
+        {
+            Text = bindable.Value;
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (bindable != null)
+                bindable.ValueChanged -= bindableValueChanged;
+            base.Dispose(isDisposing);
+        }
+    }
+}
