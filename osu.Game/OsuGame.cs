b34c7280fd196c638aba6b6331f1dfe2ccbd18a5
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2016-11-15 20:43:22 +0900

    Render musicController above toolbar.

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -121,8 +121,8 @@ namespace osu.Game
 
             //overlay elements
             (chat = new ChatConsole(API) { Depth = 0 }).Preload(this, overlayContent.Add);
-            (musicController = new MusicController()).Preload(this, overlayContent.Add);
             (Options = new OptionsOverlay { Depth = 1 }).Preload(this, overlayContent.Add);
+            (musicController = new MusicController() { Depth = 3 }).Preload(this, overlayContent.Add);
             (Toolbar = new Toolbar
             {
                 Depth = 2,
