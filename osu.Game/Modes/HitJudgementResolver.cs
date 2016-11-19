aa559571d22e5e5a9b85efd3cdaf8be424af3a6a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2016-11-19 16:19:26 +0900

    Add the concept of a HitJudgementResolver.

---
+++ osu.Game/Modes/HitJudgementResolver.cs
@@ -0,0 +1,48 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Game.Modes.Objects;
+
+namespace osu.Game.Modes
+{
+    public class HitJudgementResolver
+    {
+        public JudgementResult CheckJudgement(HitObject h) => new JudgementResult { Combo = ComboJudgement.None, Judgement = Judgement.Hit300 };
+    }
+
+    public struct JudgementResult
+    {
+        public ComboJudgement Combo;
+        public Judgement Judgement;
+    }
+
+    public enum ComboJudgement
+    {
+        [Description(@"")]
+        None,
+        [Description(@"Good")]
+        Good,
+        [Description(@"Amazing")]
+        Perfect
+    }
+
+    public enum Judgement
+    {
+        [Description(@"Miss")]
+        Miss,
+        [Description(@"50")]
+        Hit50,
+        [Description(@"100")]
+        Hit100,
+        [Description(@"300")]
+        Hit300,
+        [Description(@"500")]
+        Hit500
+    }
+}
