5cd1ca30cc781ef47cfd615d873dcabd3abb6da1
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-11-30 09:08:11 -0500

    Add OptionsSlider and wire up volume sliders

---
+++ osu.Game/Overlays/Options/OptionsSlider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenTK.Graphics;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace osu.Game.Overlays.Options
+{
+    public class OptionsSlider : FlowContainer
+    {
+        private SliderBar slider;
+        private SpriteText text;
+    
+        public string Label
+        {
+            get { return text.Text; }
+            set { text.Text = value; }
+        }
+        
+        public BindableDouble Bindable
+        {
+            get { return slider.Bindable; }
+            set { slider.Bindable = value; }
+        }
+
+        public OptionsSlider()
+        {
+            Direction = FlowDirection.VerticalOnly;
+            RelativeSizeAxes = Axes.X;
+            AutoSizeAxes = Axes.Y;
+            Children = new Drawable[]
+            {
+                text = new SpriteText(),
+                slider = new SliderBar
+                {
+                    Margin = new MarginPadding { Top = 5 },
+                    Height = 10,
+                    RelativeSizeAxes = Axes.X,
+                    Color = Color4.White,
+                    SelectionColor = new Color4(255, 102, 170, 255),
+                }
+            };
+        }
+    }
+}
