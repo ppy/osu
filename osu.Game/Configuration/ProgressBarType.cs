0378de83466810fce0c867514e7a7e02da949d38
Drew DeVault <sir@cmpwn.com>
2016-12-01 16:45:43 -0500

    Add DisplayName

--- osu.Game/Configuration/ProgressBarType.cs
+++ osu.Game/Configuration/ProgressBarType.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+
 namespace osu.Game.Configuration
 {
     public enum ProgressBarType
     {
         Off,
         Pie,
+        [DisplayName("Top Right")]
         TopRight,
+        [DisplayName("Bottom Right")]
         BottomRight,
         Bottom
     }
