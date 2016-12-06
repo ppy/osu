f356640cb744f7beae66d9dbe78253c896ddbb1d
Dean Herbert <pe@ppy.sh>
2016-12-06 18:56:20 +0900

    Add missing licence headers.

--- osu.Game/Overlays/Options/Graphics/DetailOptions.cs
+++ osu.Game/Overlays/Options/Graphics/DetailOptions.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
