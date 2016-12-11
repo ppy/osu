283a19d9aa03e15898beb12c7eaf1fc39c6d7150
Thomas Müller <87902114+bubb13gummy@users.noreply.github.com>
2016-12-11 10:11:22 +0100

    Wire up CircleSize to hitobjects.
    Note, that circle sizes still are wrong compared to stable osu. In order to fix this, the base radius of hitcircles needs to become 64, but it currently is 72.

--- osu.Game.Modes.Osu/Objects/Drawables/DrawableOsuHitObject.cs
+++ osu.Game.Modes.Osu/Objects/Drawables/DrawableOsuHitObject.cs
@@ -1,12 +1,7 @@
 ﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 //Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using osu.Game.Modes.Objects;
 using osu.Game.Modes.Objects.Drawables;
 
