e46656edf8550f2a4dd3ef16a4e49c8e84220e1b
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2016-12-14 14:59:23 -0500

    Add license header

--- osu.Game/Overlays/Options/SliderOption.cs
+++ osu.Game/Overlays/Options/SliderOption.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
 using OpenTK;
 using OpenTK.Input;
 using OpenTK.Graphics;
