a377fea432bf030fc9bf975b179541bc4c689111
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-01-26 22:15:49 +0900

    Fix incorrect configuration retrievals.

--- osu.Game/Overlays/Options/Graphics/RendererOptions.cs
+++ osu.Game/Overlays/Options/Graphics/RendererOptions.cs
@@ -16,7 +16,7 @@ namespace osu.Game.Overlays.Options.Graphics
         protected override string Header => "Renderer";
 
         [BackgroundDependencyLoader]
-        private void load(OsuConfigManager config)
+        private void load(OsuConfigManager osuConfig, FrameworkConfigManager config)
         {
             // NOTE: Compatability mode omitted
             Children = new Drawable[]
@@ -25,22 +25,22 @@ namespace osu.Game.Overlays.Options.Graphics
                 new DropdownOption<FrameSync>
                 {
                     LabelText = "Frame limiter",
-                    Bindable = config.GetBindable<FrameSync>(OsuConfig.FrameSync)
+                    Bindable = config.GetBindable<FrameSync>(FrameworkConfig.FrameSync)
                 },
                 new CheckBoxOption
                 {
                     LabelText = "Show FPS counter",
-                    Bindable = config.GetBindable<bool>(OsuConfig.FpsCounter),
+                    Bindable = osuConfig.GetBindable<bool>(OsuConfig.FpsCounter),
                 },
                 new CheckBoxOption
                 {
                     LabelText = "Reduce dropped frames",
-                    Bindable = config.GetBindable<bool>(OsuConfig.ForceFrameFlush),
+                    Bindable = osuConfig.GetBindable<bool>(OsuConfig.ForceFrameFlush),
                 },
                 new CheckBoxOption
                 {
                     LabelText = "Detect performance issues",
-                    Bindable = config.GetBindable<bool>(OsuConfig.DetectPerformanceIssues),
+                    Bindable = osuConfig.GetBindable<bool>(OsuConfig.DetectPerformanceIssues),
                 },
             };
         }
