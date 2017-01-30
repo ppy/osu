0272c4b55940d48452f8e51f1b426d3fb73b5a1f
Dean Herbert <pe@ppy.sh>
2017-01-30 15:26:28 +0900

    Add StarDifficulty property and correct colouring of difficulty icons.

--- osu.Game/Beatmaps/Drawables/DifficultyIcon.cs
+++ osu.Game/Beatmaps/Drawables/DifficultyIcon.cs
@@ -1,7 +1,10 @@
 //Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 //Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Database;
 using osu.Game.Graphics;
@@ -13,20 +16,76 @@ namespace osu.Game.Beatmaps.Drawables
 {
     class DifficultyIcon : Container
     {
+        private readonly BeatmapInfo beatmap;
+        private OsuColour palette;
+
         public DifficultyIcon(BeatmapInfo beatmap)
         {
+            this.beatmap = beatmap;
             const float size = 20;
             Size = new Vector2(size);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour palette)
+        {
+            this.palette = palette;
+
             Children = new[]
             {
                 new TextAwesome
                 {
                     Anchor = Anchor.Centre,
-                    TextSize = size,
-                    Colour = new Color4(159, 198, 0, 255),
+                    TextSize = Size.X,
+                    Colour = getColour(beatmap),
+                    Icon = FontAwesome.fa_circle
+                },
+                new TextAwesome
+                {
+                    Anchor = Anchor.Centre,
+                    TextSize = Size.X,
+                    Colour = Color4.White,
                     Icon = Ruleset.GetRuleset(beatmap.Mode).Icon
                 }
             };
+        }
+
+        enum DifficultyRating
+        {
+            Easy,
+            Normal,
+            Hard,
+            Insane,
+            Expert
+        }
+
+        private DifficultyRating getDifficultyRating(BeatmapInfo beatmap)
+        {
+            var rating = beatmap.StarDifficulty;
+
+            if (rating < 1.5) return DifficultyRating.Easy;
+            if (rating < 2.25) return DifficultyRating.Normal;
+            if (rating < 3.75) return DifficultyRating.Hard;
+            if (rating < 5.25) return DifficultyRating.Insane;
+            return DifficultyRating.Expert;
+        }
+
+        private Color4 getColour(BeatmapInfo beatmap)
+        {
+            switch (getDifficultyRating(beatmap))
+            {
+                case DifficultyRating.Easy:
+                    return palette.Green;
+                default:
+                case DifficultyRating.Normal:
+                    return palette.Yellow;
+                case DifficultyRating.Hard:
+                    return palette.Pink;
+                case DifficultyRating.Insane:
+                    return palette.Purple;
+                case DifficultyRating.Expert:
+                    return palette.Gray0;
+            }
         }
     }
 }
