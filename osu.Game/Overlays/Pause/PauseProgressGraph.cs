dcb02eff65372036cffdf3efac1487b771051993
DrabWeb <sethunity@gmail.com>
2017-01-30 07:19:14 -0400

    Cleared out PauseProgressGraph

--- osu.Game/Overlays/Pause/PauseProgressGraph.cs
+++ osu.Game/Overlays/Pause/PauseProgressGraph.cs
@@ -1,25 +1,10 @@
-﻿using OpenTK;
-using OpenTK.Graphics;
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps;
-using osu.Framework.Graphics.Primitives;
-using osu.Framework.Configuration;
+﻿using osu.Framework.Graphics.Containers;
 
 namespace osu.Game.Overlays.Pause
 {
-    public class PauseProgressGraph : FlowContainer
+    public class PauseProgressGraph : Container
     {
-        private WorkingBeatmap current;
-
-        [BackgroundDependencyLoader]
-        private void load(OsuGameBase osuGame)
-        {
-            current = osuGame.Beatmap.Value;
-        }
-
+        
         public PauseProgressGraph()
         {
             // TODO: Implement the pause progress graph
