c09dcd0c954c659ca6e22c14334aa1d7853e737d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-01-30 20:29:04 +0900

    Add custom implementation of TextBox.

---
+++ osu.Game/Graphics/UserInterface/OsuTextBox.cs
@@ -0,0 +1,48 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
+using OpenTK.Graphics;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class OsuTextBox : TextBox
+    {
+        public OsuTextBox()
+        {
+            Height = 40;
+            TextContainer.Height = 0.6f;
+            CornerRadius = 5;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colour)
+        {
+            BorderColour = colour.Yellow;
+        }
+
+        protected override bool OnFocus(InputState state)
+        {
+            BorderThickness = 3;
+
+            return base.OnFocus(state);
+        }
+
+        protected override void OnFocusLost(InputState state)
+        {
+            BorderThickness = 0;
+
+            base.OnFocusLost(state);
+        }
+    }
+
+    public class OsuPasswordTextBox : OsuTextBox
+    {
+        protected virtual char MaskCharacter => '*';
+
+        protected override Drawable AddCharacterToFlow(char c) => base.AddCharacterToFlow(MaskCharacter);
+    }
+}
