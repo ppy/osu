3bc0e96ebbebaa0e16bdde34ac8b736af9ce5df2
Drew DeVault <87902114+bubb13gummy@users.noreply.github.com>
2017-02-02 12:21:34 -0500

    @Tom94 pointed out this problem

--- osu.Game/Screens/Select/PlaySongSelect.cs
+++ osu.Game/Screens/Select/PlaySongSelect.cs
@@ -91,7 +91,7 @@ namespace osu.Game.Screens.Select
 
         private void start()
         {
-            if (player != null)
+            if (player != null || Beatmap == null)
                 return;
 
             //in the future we may want to move this logic to a PlayerLoader gamemode or similar, so we can rely on the SongSelect transition
