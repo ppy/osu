65e5b4e827fc102bb931247391549367e8766ccd
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2017-02-03 15:22:02 -0400

    Added back current work

---
+++ osu.Desktop.VisualTests/Tests/TestCaseSongProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using osu.Framework.Graphics;
+using osu.Framework.GameModes.Testing;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Desktop.VisualTests
+{
+    public class TestCaseSongProgressBar : TestCase
+    {
+        public override string Name => @"SongProgressBar";
+
+        public override string Description => @"Tests the song progress bar";
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            Add(new SongProgressBar
+            {
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                RelativeSizeAxes = Axes.X
+            });
+        }
+    }
+}
---
+++ osu.Game/Graphics/UserInterface/SongProgressGraph.cs
@@ -0,0 +1,9 @@
+﻿using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class SongProgressGraph : Container
+    {
+        // TODO: Implement the song progress graph
+    }
+}
