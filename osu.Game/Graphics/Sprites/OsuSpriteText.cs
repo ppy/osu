e720bed9e5bf755787bc985c1d6d20f45682fc6f
Dean Herbert <pe@ppy.sh>
2017-02-08 11:19:17 +0900

    Add custom representation of unrenderable unicode characters.

--- osu.Game/Graphics/Sprites/OsuSpriteText.cs
+++ osu.Game/Graphics/Sprites/OsuSpriteText.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.MathUtils;
+using OpenTK;
+using OpenTK.Graphics;
 
 namespace osu.Game.Graphics.Sprites
 {
@@ -13,6 +17,27 @@ namespace osu.Game.Graphics.Sprites
         {
             Shadow = true;
             TextSize = FONT_SIZE;
+        }
+
+        protected override Drawable GetUndrawableCharacter()
+        {
+            var tex = GetTextureForCharacter('?');
+
+            if (tex != null)
+            {
+                float adjust = (RNG.NextSingle() - 0.5f) * 2;
+                return new Sprite
+                {
+                    Texture = tex,
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                    Scale = new Vector2(1 + adjust * 0.2f),
+                    Rotation = adjust * 15,
+                    Colour = Color4.White,
+                };
+            }
+
+            return base.GetUndrawableCharacter();
         }
     }
 }
