84096b146e45614c2cd988db5f6f439a22de64d5
Dean Herbert <pe@ppy.sh>
2017-02-08 12:41:23 +0900

    Make password mask characters look better.

---
+++ osu.Game/Graphics/UserInterface/OsuPasswordTextBox.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class OsuPasswordTextBox : OsuTextBox
+    {
+        protected override Drawable GetDrawableCharacter(char c) =>
+            new Container
+            {
+                Size = new Vector2(CalculatedTextSize / 2, CalculatedTextSize),
+                Children = new[]
+                {
+                    new CircularContainer
+                    {
+                        Anchor = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Size = new Vector2(0.8f),
+                        Children = new[]
+                        {
+                            new Box
+                            {
+                                Colour = Color4.White,
+                                RelativeSizeAxes = Axes.Both,
+                            }
+                        },
+                    }
+                }
+            };
+    }
+}
