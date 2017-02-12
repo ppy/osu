d7be9539d1a18cec2ec45b10871e4853461cb80c
Damnae <damnaee@gmail.com>
2017-02-12 07:29:36 +0100

    Create connections from HitObjects instead of DrawableHitObjects.

---
+++ osu.Game.Modes.Osu/Objects/Drawables/Connections/ConnectionRenderer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics.Containers;
+using osu.Game.Modes.Objects;
+using System.Collections.Generic;
+
+namespace osu.Game.Modes.Osu.Objects.Drawables.Connections
+{
+    public abstract class ConnectionRenderer<T> : Container
+        where T : HitObject
+    {
+        /// <summary>
+        /// Create drawables inside this container, connecting hit objects visually, for example with follow points.
+        /// </summary>
+        /// <param name="hitObjects">Hit objects to create connections for</param>
+        public abstract void AddConnections(IEnumerable<T> hitObjects);
+    }
+}
