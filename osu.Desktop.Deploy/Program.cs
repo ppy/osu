a073b1db0d89dd0d2a2518f731b6677f82aa60a9
Dean Herbert <pe@ppy.sh>
2017-02-13 01:02:44 +0900

    very simple build automation.

---
+++ osu.Desktop.Deploy/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace osu.Desktop.Deploy
+{
+    class Program
+    {
+        static string NUGET_PATH = @"packages\NuGet.CommandLine.3.5.0\tools\NuGet.exe";
+        static string SQUIRREL_PATH = @"packages\squirrel.windows.1.5.2\tools\Squirrel.exe";
+        static string MSBUILD_PATH = @"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
+
+        static string STAGING_FOLDER = "Staging";
+        static string PROJECT_NAME = "osu.Desktop";
+
+        static string CODE_SIGNING_CMD => $"/a /f {CODE_SIGNING_CERT} /p {codeSigningPassword} /t http://timestamp.comodoca.com/authenticode";
+        static string IconPath => Path.Combine(SolutionPath, PROJECT_NAME, "lazer.ico");
+
+        static internal string HomeDir { get { return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); } }
+        static internal string CODE_SIGNING_CERT => Path.Combine(HomeDir, "deanherbert.pfx");
+
+        static string SolutionPath => Environment.CurrentDirectory;
+        static string StagingPath => Path.Combine(SolutionPath, STAGING_FOLDER);
+
+        static string codeSigningPassword;
+
+        static void Main(string[] args)
+        {
+            FindSolutionPath();
+
+            if (Directory.Exists(STAGING_FOLDER))
+                Directory.Delete(STAGING_FOLDER, true);
+            Directory.CreateDirectory(STAGING_FOLDER);
+
+            string verBase = DateTime.Now.ToString("yyyy.Md.");
+
+            int increment = 0;
+
+            while (Directory.GetFiles("Releases", $"*{verBase}{increment}*").Count() > 0)
+                increment++;
+
+            string ver = $"{verBase}{increment}";
+
+            Console.Write(ver);
+
+            ver += Console.ReadLine();
+
+            Console.WriteLine("Enter code signing password:");
+
+            codeSigningPassword = Console.ReadLine();
+
+            Console.WriteLine("Restoring NuGet packages...");
+            RunCommand(NUGET_PATH, "restore " + SolutionPath);
+
+            Console.WriteLine("Running build process...");
+            RunCommand(MSBUILD_PATH, $"/v:quiet /m /t:Client\\{PROJECT_NAME.Replace('.', '_')} /p:OutputPath={StagingPath};Configuration=Release osu.sln");
+
+            Console.WriteLine("Creating NuGet deployment package...");
+            RunCommand(NUGET_PATH, $"pack osu.Desktop\\osu.nuspec -Version {ver} -Properties Configuration=Deploy -OutputDirectory {StagingPath} -BasePath {StagingPath}");
+
+            Console.WriteLine("Releasifying package...");
+            RunCommand(SQUIRREL_PATH, $"--releasify {StagingPath}\\osulazer.{ver}.nupkg --setupIcon {IconPath} --icon {IconPath} -n \"{CODE_SIGNING_CMD}\" --no-msi");
+
+            File.Copy("Releases\\Setup.exe", "Releases\\install.exe", true);
+            File.Delete("Releases\\Setup.exe");
+
+            Console.WriteLine("Done!");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Find the base path of the osu! solution (git checkout location)
+        /// </summary>
+        private static void FindSolutionPath()
+        {
+            string path = Path.GetDirectoryName(Environment.CommandLine.Replace("\"", "").Trim());
+
+            if (string.IsNullOrEmpty(path))
+                path = Environment.CurrentDirectory;
+
+            while (!File.Exists(path + "\\osu.sln"))
+                path = path.Remove(path.LastIndexOf('\\'));
+            path += "\\";
+
+            Environment.CurrentDirectory = path;
+        }
+
+        private static bool RunCommand(string command, string args)
+        {
+            var psi = new ProcessStartInfo(command, args);
+            if (psi != null)
+            {
+                psi.WorkingDirectory = SolutionPath;
+                psi.CreateNoWindow = true;
+                psi.RedirectStandardOutput = true;
+                psi.UseShellExecute = false;
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                Process p = Process.Start(psi);
+                string output = p.StandardOutput.ReadToEnd();
+                if (p.ExitCode != 0)
+                {
+                    Console.WriteLine(output);
+                    Error($"Command {command} {args} failed!");
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void Error(string p)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR: " + p);
+
+            Console.ReadLine();
+            Environment.Exit(-1);
+        }
+    }
+}
--- osu.Desktop.Deploy/Properties/AssemblyInfo.cs
+++ osu.Desktop.Deploy/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("osu!lazer")]
-[assembly: AssemblyDescription("click the circles. to the beat.")]
+[assembly: AssemblyTitle("osu.Desktop.Deploy")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ppy Pty Ltd")]
-[assembly: AssemblyProduct("osu!lazer")]
-[assembly: AssemblyCopyright("ppy Pty Ltd 2007-2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("osu.Desktop.Deploy")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55e28cb2-7b6c-4595-8dcc-9871d8aad7e9")]
+[assembly: Guid("baea2f74-0315-4667-84e0-acac0b4bf785")]
 
-[assembly: AssemblyVersion("2017.212.0")]
-[assembly: AssemblyFileVersion("2017.212.0")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
