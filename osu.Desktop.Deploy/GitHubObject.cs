e192ecbc510f1191b5711c94aeb28f2c1490d7dc
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-02-13 12:31:55 +0900

    Add ability to bootstrap the releases directory using github release API.

---
+++ osu.Desktop.Deploy/GitHubObject.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace osu.Desktop.Deploy
+{
+    internal class GitHubObject
+    {
+        [JsonProperty(@"id")]
+        public int Id;
+
+        [JsonProperty(@"name")]
+        public string Name;
+    }
+}
