4c0a4852d22f6a107b918366c70768c2f55b3881
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-02-19 18:39:54 +0900

    Remove unused using statements.

--- osu.Game/Overlays/ChatOverlay.cs
+++ osu.Game/Overlays/ChatOverlay.cs
@@ -1,12 +1,10 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using OpenTK;
-using OpenTK.Graphics;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -20,7 +18,6 @@ using osu.Game.Online.API.Requests;
 using osu.Game.Online.Chat;
 using osu.Game.Online.Chat.Drawables;
 using osu.Game.Graphics.UserInterface;
-using osu.Game.Screens.Select;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.UserInterface;
 
