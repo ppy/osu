91194b2f9d112d2adca3f89febd7b15af5dfb0da
Andrey Zavadskiy <megaman9919@gmail.com>
2017-02-23 15:37:07 +0300

    Now FailScreen using PauseOverlay

--- osu.Game/Screens/Play/FailDialog.cs
+++ osu.Game/Screens/Play/FailDialog.cs
@@ -15,6 +15,8 @@ using osu.Framework.Allocation;
 using osu.Game.Graphics;
 using osu.Game.Overlays.Pause;
 using System;
+using osu.Framework.Input;
+using OpenTK.Input;
 
 namespace osu.Game.Screens.Play
 {
@@ -24,101 +26,24 @@ namespace osu.Game.Screens.Play
 
         private static readonly Vector2 background_blur = new Vector2(20);
 
-        private const int button_height = 70;
-        private const float background_alpha = 0.75f;
+        private PauseOverlay failOverlay;
 
         [BackgroundDependencyLoader]
-        private void load(OsuColour colours)
+        private void load()
         {
+            failOverlay = new PauseOverlay()
+            {
+                Type = false,
+                MainText = @"failed",
+                AdditionalText = @"retry?",
+                OnRetry = retry,
+                OnQuit = Exit
+            };
             Children = new Drawable[]
             {
-                new Box
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Colour = Color4.Black,
-                    Alpha = background_alpha,
-                },
-                new FlowContainer
-                {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Direction = FlowDirections.Vertical,
-                    Spacing = new Vector2(0f, 50f),
-                    Origin = Anchor.Centre,
-                    Anchor = Anchor.Centre,
-                    Children = new Drawable[]
-                    {
-                        new FlowContainer
-                        {
-                            AutoSizeAxes = Axes.Both,
-                            Direction = FlowDirections.Vertical,
-                            Spacing = new Vector2(0f, 20f),
-                            Origin = Anchor.TopCentre,
-                            Anchor = Anchor.TopCentre,
-                            Children = new Drawable[]
-                            {
-                                new OsuSpriteText
-                                {
-                                    Text = @"failed",
-                                    Font = @"Exo2.0-Medium",
-                                    Spacing = new Vector2(5, 0),
-                                    Origin = Anchor.TopCentre,
-                                    Anchor = Anchor.TopCentre,
-                                    TextSize = 30,
-                                    Colour = colours.Yellow,
-                                    Shadow = true,
-                                    ShadowColour = new Color4(0, 0, 0, 0.25f)
-                                },
-                                new OsuSpriteText
-                                {
-                                    Text = @"try again?",
-                                    Origin = Anchor.TopCentre,
-                                    Anchor = Anchor.TopCentre,
-                                    Shadow = true,
-                                    ShadowColour = new Color4(0, 0, 0, 0.25f)
-                                }
-                            }
-                        },
-                        new FlowContainer
-                        {
-                            RelativeSizeAxes = Axes.X,
-                            AutoSizeAxes = Axes.Y,
-                            Masking = true,
-                            EdgeEffect = new EdgeEffect
-                            {
-                                Type = EdgeEffectType.Shadow,
-                                Colour = Color4.Black.Opacity(0.6f),
-                                Radius = 50
-                            },
-                            Children = new Drawable[]
-                            {
-                                new RetryButton
-                                {
-                                    RelativeSizeAxes = Axes.X,
-                                    Origin = Anchor.TopCentre,
-                                    Anchor = Anchor.TopCentre,
-                                    Height = button_height,
-                                    Action = retry
-                                },
-                                new QuitButton
-                                {
-                                    RelativeSizeAxes = Axes.X,
-                                    Origin = Anchor.TopCentre,
-                                    Anchor = Anchor.TopCentre,
-                                    Height = button_height,
-                                    Action = Exit
-                                }
-                            }
-                        }
-                    }
-                },
-                new PauseProgressBar
-                {
-                    Origin = Anchor.BottomCentre,
-                    Anchor = Anchor.BottomCentre,
-                    Width = 1f
-                }
+                failOverlay
             };
+            failOverlay.ToggleVisibility();
         }
 
         protected override void OnEntering(Screen last)
