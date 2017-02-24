f7d985fe1884f39812f4ae2c51249aa5e737bf4f
Thomas Müller <thomas94@gmx.net>
2017-02-24 19:36:17 +0100

    Fix and refactor star difficulty calculation boilerplate
    Moves star difficulty calculation entry-point to Beatmap, and sets
    star difficulty at the correct place for song select to display.

--- osu.Game/Screens/Select/PlaySongSelect.cs
+++ osu.Game/Screens/Select/PlaySongSelect.cs
@@ -325,11 +325,7 @@ namespace osu.Game.Screens.Select
                 if (b.Metadata == null) b.Metadata = beatmapSet.Metadata;
             });
 
-            foreach (var b in beatmapSet.Beatmaps)
-                b.ComputeDifficulty(database);
-            beatmapSet.Beatmaps = beatmapSet.Beatmaps.OrderBy(b => b.StarDifficulty).ToList();
-
-            var group = new BeatmapGroup(database.GetWorkingBeatmap(beatmapSet.Beatmaps.FirstOrDefault()))
+            var group = new BeatmapGroup(beatmapSet, database)
             {
                 SelectionChanged = selectionChanged,
                 StartRequested = b => footer.StartButton.TriggerClick()
