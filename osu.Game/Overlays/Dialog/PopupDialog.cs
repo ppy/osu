64d036fe8149fe302766646298ab9f1b9a422bd6
DrabWeb <sethunity@gmail.com>
2017-02-27 16:41:45 -0400

    Update to work with latest framework

--- osu.Game/Overlays/Dialog/PopupDialog.cs
+++ osu.Game/Overlays/Dialog/PopupDialog.cs
@@ -10,7 +10,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Transformations;
+using osu.Framework.Graphics.Transforms;
 using osu.Framework.MathUtils;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Backgrounds;
