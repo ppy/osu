afdb95f4d52c16299b90439744ee9321745fcb38
Dean Herbert <pe@ppy.sh>
2017-02-27 20:39:04 +0900

    Fix even more test regressions.

--- osu.Game/Beatmaps/Beatmap.cs
+++ osu.Game/Beatmaps/Beatmap.cs
@@ -18,8 +18,8 @@ namespace osu.Game.Beatmaps
         public List<HitObject> HitObjects { get; set; }
         public List<ControlPoint> ControlPoints { get; set; }
         public List<Color4> ComboColors { get; set; }
-        public double BPMMaximum => 60000 / ControlPoints.Where(c => c.BeatLength != 0).OrderBy(c => c.BeatLength).First().BeatLength;
-        public double BPMMinimum => 60000 / ControlPoints.Where(c => c.BeatLength != 0).OrderByDescending(c => c.BeatLength).First().BeatLength;
+        public double BPMMaximum => 60000 / (ControlPoints?.Where(c => c.BeatLength != 0).OrderBy(c => c.BeatLength).FirstOrDefault() ?? ControlPoint.Default).BeatLength;
+        public double BPMMinimum => 60000 / (ControlPoints?.Where(c => c.BeatLength != 0).OrderByDescending(c => c.BeatLength).FirstOrDefault() ?? ControlPoint.Default).BeatLength;
         public double BPMMode => BPMAt(ControlPoints.Where(c => c.BeatLength != 0).GroupBy(c => c.BeatLength).OrderByDescending(grp => grp.Count()).First().First().Time);
 
         public double BPMAt(double time)
