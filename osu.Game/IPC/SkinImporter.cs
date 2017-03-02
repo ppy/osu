6090c9284be54afe8e2aba33d997a7bac70d5174
Alex Amadori <alexamadori592@gmail.com>
2017-03-02 12:59:26 +0100

    Started SkinImporter

---
+++ osu.Game/IPC/SkinImporter.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using osu.Framework.Platform;
+
+namespace osu.Game.IPC
+{
+    public class SkinImporter
+    {
+        private IpcChannel<SkinImportMessage> channel;
+        // SkinDatabase database; ?
+
+        public SkinImporter()
+        {
+        }
+    }
+
+    public class SkinImportMessage {
+        string Path;
+    }
+}
