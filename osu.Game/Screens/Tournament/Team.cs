00956083103027819366d99c696c90baabdfec6a
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-03 13:17:24 +0900

    Add license headers.

--- osu.Game/Screens/Tournament/Team.cs
+++ osu.Game/Screens/Tournament/Team.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
