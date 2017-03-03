a1677bb88e05809aa92e7309eb8491c75b778968
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-03 16:33:40 +0900

    Make test case add test drawings file.

--- osu.Desktop.VisualTests/Tests/TestCaseDrawings.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseDrawings.cs
@@ -1,7 +1,10 @@
-﻿using osu.Framework.Screens.Testing;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Platform;
+using osu.Framework.Screens.Testing;
 using osu.Game.Screens.Tournament;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,29 @@ namespace osu.Desktop.VisualTests.Tests
     {
         public override string Name => @"Drawings";
         public override string Description => "Tournament drawings";
+
+        [BackgroundDependencyLoader]
+        private void load(Storage storage)
+        {
+            string[] testTeams =
+            {
+                "GB:United Kingdom:UK",
+                "FR:France:FRA",
+                "CN:China:CHN",
+                "AU:Australia:AUS",
+                "JP:Japan:JPN",
+                "RO:Romania",
+                "IT:Italy",
+                "VE:Venezuela:VNZ"
+            };
+
+            using (Stream stream = storage.GetStream(Drawings.TEAMS_FILENAME, FileAccess.Write, FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(stream))
+            {
+                foreach (string line in testTeams)
+                    sw.WriteLine(line);
+            }
+        }
 
         public override void Reset()
         {
