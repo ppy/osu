3e0334a7781111e65b743de4df4bd01d913d7f46
Jorolf <jorolf@gmx.de>
2017-03-05 00:14:11 +0100

    Initial vitaru commit

---
+++ osu.Game.Modes.Vitaru/UI/VitaruJudgementInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using osu.Game.Modes.Objects.Drawables;
+
+namespace osu.Game.Modes.Vitaru.UI
+{
+    internal class VitaruJudgementInfo
+    {
+        public static explicit operator VitaruJudgementInfo(JudgementInfo v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
