09a1f55d43914c5186babe6261afbf4e7428d3e0
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-03-06 10:05:42 +0900

    Remove unnecessary usings.

--- osu.Desktop.VisualTests/Tests/TestCaseReplay.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseReplay.cs
@@ -2,34 +2,14 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using osu.Framework.Allocation;
-using osu.Framework.Screens.Testing;
-using osu.Game.Beatmaps;
-using osu.Game.Beatmaps.Formats;
-using OpenTK;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Input;
 using osu.Framework.Input.Handlers;
-using osu.Framework.MathUtils;
 using osu.Framework.Platform;
-using osu.Game.Beatmaps.IO;
+using osu.Game.Beatmaps;
 using osu.Game.Database;
-using osu.Game.Input.Handlers;
-using osu.Game.IO.Legacy;
 using osu.Game.Modes;
-using osu.Game.Modes.Objects;
-using osu.Game.Modes.Osu.Objects;
 using osu.Game.Screens.Play;
-using OpenTK.Graphics;
-using OpenTK.Input;
-using SharpCompress.Archives.SevenZip;
-using SharpCompress.Compressors.LZMA;
-using SharpCompress.Readers;
-using KeyboardState = osu.Framework.Input.KeyboardState;
-using MouseState = osu.Framework.Input.MouseState;
 
 namespace osu.Desktop.VisualTests.Tests
 {
