5e68dad94c1c516cc614214e1a4351a6d8499071
Jorolf <jorolf@gmx.de>
2017-03-06 13:19:28 +0100

    forgot an import

--- osu.Game/Modes/Ruleset.cs
+++ osu.Game/Modes/Ruleset.cs
@@ -1,13 +1,14 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.Collections.Generic;
-using osu.Game.Modes.Objects;
-using osu.Game.Modes.UI;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
+using osu.Game.Modes.Objects;
+using osu.Game.Modes.UI;
 
 namespace osu.Game.Modes
 {
