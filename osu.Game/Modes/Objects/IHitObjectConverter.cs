bbd81dbc47a0e3a030b40470a0938b0d4d8ef7f7
smoogipooo <smoogipooo@gmail.com>
2017-03-14 13:02:42 +0900

    Add license headers.

--- osu.Game/Modes/Objects/IHitObjectConverter.cs
+++ osu.Game/Modes/Objects/IHitObjectConverter.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Game.Modes.Objects
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+namespace osu.Game.Modes.Objects
 {
     /// <summary>
     /// Converts HitObjects to another mode.
