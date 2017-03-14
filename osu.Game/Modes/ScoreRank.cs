650fc7563d1c2a09c331798f47ab18d3aef94118
DrabWeb <sethunity@gmail.com>
2017-03-14 10:58:28 -0300

    ScoreRank, Avatar to own files, +DrawableRank

---
+++ osu.Game/Modes/ScoreRank.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.ComponentModel;
+
+namespace osu.Game.Modes
+{
+    public enum ScoreRank
+    {
+        [Description(@"F")]
+        F,
+
+        [Description(@"F")]
+        D,
+
+        [Description(@"C")]
+        C,
+
+        [Description(@"B")]
+        B,
+
+        [Description(@"A")]
+        A,
+
+        [Description(@"S")]
+        S,
+
+        [Description(@"SPlus")]
+        SPlus,
+
+        [Description(@"SS")]
+        SS,
+
+        [Description(@"SSPlus")]
+        SSPlus,
+    }
+}
