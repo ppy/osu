481c910ec8e68e3763aafafbeb4ccfd54b51bb15
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-14 13:02:42 +0900

    Add license headers.

--- osu.Game/Modes/Objects/Types/IHasCurve.cs
+++ osu.Game/Modes/Objects/Types/IHasCurve.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
 using OpenTK;
 
 namespace osu.Game.Modes.Objects.Types
