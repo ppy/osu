c4e5eac35ba1ed6454c1755b2ea82b30015c7b53
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-03-14 18:38:06 +0800

    Move footer stuff to derived.

--- osu.Game/Screens/Select/PlaySongSelect.cs
+++ osu.Game/Screens/Select/PlaySongSelect.cs
@@ -1,8 +1,14 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using OpenTK.Input;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Primitives;
 using osu.Framework.Screens;
 using osu.Game.Beatmaps;
+using osu.Game.Graphics;
+using osu.Game.Overlays.Mods;
 using osu.Game.Screens.Play;
 
 namespace osu.Game.Screens.Select
@@ -10,6 +16,29 @@ namespace osu.Game.Screens.Select
     public class PlaySongSelect : SongSelect
     {
         private OsuScreen player;
+        private ModSelectOverlay modSelect;
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            Add(modSelect = new ModSelectOverlay
+            {
+                RelativeSizeAxes = Axes.X,
+                Origin = Anchor.BottomCentre,
+                Anchor = Anchor.BottomCentre,
+                Margin = new MarginPadding { Bottom = 50 }
+            });
+
+            Footer.AddButton(@"mods", colours.Yellow, modSelect.ToggleVisibility, Key.F1);
+            Footer.AddButton(@"random", colours.Green, SelectRandom, Key.F2);
+            Footer.AddButton(@"options", colours.Blue, BeatmapOptions.ToggleVisibility, Key.F3);
+        }
+
+        protected override void OnBeatmapChanged(WorkingBeatmap beatmap)
+        {
+            beatmap?.Mods.BindTo(modSelect.SelectedMods);
+            base.OnBeatmapChanged(beatmap);
+        }
 
         protected override void OnResuming(Screen last)
         {
