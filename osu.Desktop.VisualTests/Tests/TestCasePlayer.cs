e52d5181f7e478405d242162aeedd4298c5c037e
smoogipooo <smoogipooo@gmail.com>
2017-03-14 17:01:46 +0900

    Fix a few visual tests failing.

--- osu.Desktop.VisualTests/Tests/TestCasePlayer.cs
+++ osu.Desktop.VisualTests/Tests/TestCasePlayer.cs
@@ -68,6 +68,7 @@ namespace osu.Desktop.VisualTests.Tests
                     HitObjects = objects,
                     BeatmapInfo = new BeatmapInfo
                     {
+                        BaseDifficulty = new BaseDifficulty(),
                         Metadata = new BeatmapMetadata
                         {
                             Artist = @"Unknown",
