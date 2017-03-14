670c467610ec4a3356ea2de3f681214475108423
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-14 13:17:11 +0900

    Remove a few more things that aren't required yet.

--- osu.Game.Modes.Mania/Objects/ManiaBaseHit.cs
+++ osu.Game.Modes.Mania/Objects/ManiaBaseHit.cs
@@ -2,16 +2,11 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using osu.Game.Modes.Objects;
-using osu.Game.Modes.Objects.Types;
 
 namespace osu.Game.Modes.Mania.Objects
 {
-    public abstract class ManiaBaseHit : HitObject, IHasEndTime
+    public abstract class ManiaBaseHit : HitObject
     {
         public int Column;
-
-        public double EndTime { get; set; }
-
-        public double Duration { get; set; }
     }
 }
