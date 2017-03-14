0e8c85906d48da6fbd4825ecdf474a1127e5f57f
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-03-14 22:39:57 +0900

    Update framework.

--- osu.Game/Graphics/UserInterface/OsuDropDownMenu.cs
+++ osu.Game/Graphics/UserInterface/OsuDropDownMenu.cs
@@ -1,8 +1,6 @@
 // Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Graphics.Transforms;
 using osu.Framework.Graphics.UserInterface;
 using OpenTK;
@@ -14,9 +12,6 @@ namespace osu.Game.Graphics.UserInterface
     public class OsuDropDownMenu<U> : DropDownMenu<U>
     {
         protected override DropDownHeader CreateHeader() => new OsuDropDownHeader();
-
-        protected override IEnumerable<DropDownMenuItem<U>> GetDropDownItems(IEnumerable<KeyValuePair<string, U>> values)
-            => values.Select(v => new OsuDropDownMenuItem<U>(v.Key, v.Value));
 
         public OsuDropDownMenu()
         {
@@ -38,5 +33,7 @@ namespace osu.Game.Graphics.UserInterface
         {
             ContentContainer.ResizeTo(State == DropDownMenuState.Opened ? new Vector2(1, ContentHeight) : new Vector2(1, 0), 300, EasingTypes.OutQuint);
         }
+
+        protected override DropDownMenuItem<U> CreateDropDownItem(string key, U value) => new OsuDropDownMenuItem<U>(key, value);
     }
 }
