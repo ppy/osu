be37d4bb0b3fb2884a36c0078ce1599d75149ca9
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-14 19:24:47 +0900

    Fix incorrect hit object type.

--- osu.Game/Modes/Objects/Types/LegacyHitObjectType.cs
+++ osu.Game/Modes/Objects/Types/LegacyHitObjectType.cs
@@ -12,8 +12,7 @@ namespace osu.Game.Modes.Objects.Types
         Slider = 1 << 1,
         NewCombo = 1 << 2,
         Spinner = 1 << 3,
-        ColourHax = 122,
-        Hold = 1 << 7,
-        SliderTick = 1 << 8,
+        ColourHax = 112,
+        Hold = 1 << 7
     }
 }
