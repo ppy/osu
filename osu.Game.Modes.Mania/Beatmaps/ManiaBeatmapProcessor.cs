2b3b3b72556f9213ac9bd036773994631f8ca7aa
smoogipooo <smoogipooo@gmail.com>
2017-03-14 17:07:38 +0900

    Fix license headers (sorry I have to do this as post-processing for now cos VS 2017...).

--- osu.Game.Modes.Mania/Beatmaps/ManiaBeatmapProcessor.cs
+++ osu.Game.Modes.Mania/Beatmaps/ManiaBeatmapProcessor.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Beatmaps;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Beatmaps;
 using osu.Game.Modes.Mania.Objects;
 
 namespace osu.Game.Modes.Mania.Beatmaps
