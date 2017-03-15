41f912836a7eb1c219222dfd2b550210679e7d87
smoogipooo <smoogipooo@gmail.com>
2017-03-15 12:57:17 +0900

    Refactorings.

--- osu.Game/Modes/Objects/CurvedHitObject.cs
+++ osu.Game/Modes/Objects/CurvedHitObject.cs
@@ -1,6 +1,9 @@
-﻿using osu.Game.Modes.Objects.Types;
-using System.Collections.Generic;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
 using OpenTK;
+using osu.Game.Modes.Objects.Types;
+using System.Collections.Generic;
 
 namespace osu.Game.Modes.Objects
 {
