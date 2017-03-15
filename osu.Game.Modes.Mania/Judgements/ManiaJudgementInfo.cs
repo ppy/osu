3a8d1d3619e017e10bf4c5c7589fea3b3501df05
smoogipooo <smoogipooo@gmail.com>
2017-03-15 21:32:47 +0900

    Add license headers.

--- osu.Game.Modes.Mania/Judgements/ManiaJudgementInfo.cs
+++ osu.Game.Modes.Mania/Judgements/ManiaJudgementInfo.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Modes.Judgements;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Modes.Judgements;
 
 namespace osu.Game.Modes.Mania.Judgements
 {
