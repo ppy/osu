6bab6b66a3270b556aa34b299e29ba93a51dce60
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-15 21:32:47 +0900

    Add license headers.

--- osu.Game/Modes/Judgements/JudgementInfo.cs
+++ osu.Game/Modes/Judgements/JudgementInfo.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Modes.Objects.Drawables;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Modes.Objects.Drawables;
 
 namespace osu.Game.Modes.Judgements
 {
