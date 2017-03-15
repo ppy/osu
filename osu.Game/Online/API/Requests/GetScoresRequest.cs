08e93275fa8cc8a3bd620ade9f3073523490cdd1
Dean Herbert <pe@ppy.sh>
2017-03-15 17:50:52 +0900

    Styling fixes.

--- osu.Game/Online/API/Requests/GetScoresRequest.cs
+++ osu.Game/Online/API/Requests/GetScoresRequest.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using osu.Framework.IO.Network;
 using osu.Game.Database;
