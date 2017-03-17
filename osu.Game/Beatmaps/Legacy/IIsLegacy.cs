717710ee8a672cfb7133dee3c634380f1f2744a4
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-17 19:54:36 +0900

    Fix license headers.

--- osu.Game/Beatmaps/Legacy/IIsLegacy.cs
+++ osu.Game/Beatmaps/Legacy/IIsLegacy.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Game.Beatmaps.Legacy
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+namespace osu.Game.Beatmaps.Legacy
 {
     /// <summary>
     /// A Beatmap that was loaded from a legacy .osu beatmap file (version &lt;=15).
