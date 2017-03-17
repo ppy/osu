3c8fe7c5cbc842cc16c42ec49002ae22ac271aed
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-17 16:05:16 +0900

    Add AcentColour and xmldoc.

--- osu.Game.Modes.Taiko/Objects/Drawable/DrawableTaikoHitObject.cs
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableTaikoHitObject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using OpenTK.Graphics;
 using osu.Game.Modes.Objects.Drawables;
 using osu.Game.Modes.Taiko.Judgements;
 
@@ -8,6 +9,11 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
 {
     public class DrawableTaikoHitObject : DrawableHitObject<TaikoHitObject, TaikoJudgementInfo>
     {
+        /// <summary>
+        /// The colour used for various elements of this DrawableHitObject.
+        /// </summary>
+        public virtual Color4 AccentColour { get; }
+
         public DrawableTaikoHitObject(TaikoHitObject hitObject)
             : base(hitObject)
         {
@@ -21,6 +27,11 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
         {
         }
 
+        /// <summary>
+        /// Sets the scroll position of the DrawableHitObject relative to the offset between
+        /// a time value and the HitObject's StartTime.
+        /// </summary>
+        /// <param name="time"></param>
         protected void UpdateScrollPosition(double time)
         {
             MoveToX((float)((HitObject.StartTime - time) / HitObject.PreEmpt));
