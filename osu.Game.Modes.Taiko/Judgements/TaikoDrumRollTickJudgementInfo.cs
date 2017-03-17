95f51aa0d4c562c464f0d148dae1b9d407e32d2f
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-17 19:08:50 +0900

    Add basic DrawableDrumRollTick (no graphics yet, just input).

---
+++ osu.Game.Modes.Taiko/Judgements/TaikoDrumRollTickJudgementInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace osu.Game.Modes.Taiko.Judgements
+{
+    public class TaikoDrumRollTickJudgementInfo : TaikoJudgementInfo
+    {
+        protected override int ScoreToInt(TaikoScoreResult result)
+        {
+            switch (result)
+            {
+                default:
+                    return 0;
+                case TaikoScoreResult.Great:
+                    return 200;
+            }
+        }
+
+        protected override int AccuracyScoreToInt(TaikoScoreResult result)
+        {
+            return 0;
+        }
+    }
+}
