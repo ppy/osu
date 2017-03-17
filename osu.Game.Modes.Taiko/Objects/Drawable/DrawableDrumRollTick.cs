1ede12d847468d9b5e2826df79b17dc8d906c2db
smoogipooo <smoogipooo@gmail.com>
2017-03-17 19:08:50 +0900

    Add basic DrawableDrumRollTick (no graphics yet, just input).

---
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableDrumRollTick.cs
@@ -0,0 +1,60 @@
+﻿using OpenTK.Input;
+using System.Collections.Generic;
+using osu.Game.Modes.Taiko.Judgements;
+using System;
+using osu.Game.Modes.Objects.Drawables;
+using osu.Framework.Input;
+
+namespace osu.Game.Modes.Taiko.Objects.Drawable
+{
+    public class DrawableDrumRollTick : DrawableTaikoHitObject<DrumRollTick>
+    {
+        /// <summary>
+        /// A list of keys which this HitObject will accept. These are the standard Taiko keys for now.
+        /// These should be moved to bindings later.
+        /// </summary>
+        private List<Key> validKeys = new List<Key>(new[] { Key.D, Key.F, Key.J, Key.K });
+
+        public DrawableDrumRollTick(DrumRollTick tick)
+            : base(tick)
+        {
+        }
+
+        protected override TaikoJudgementInfo CreateJudgementInfo() => new TaikoDrumRollTickJudgementInfo();
+
+        protected override void CheckJudgement(bool userTriggered)
+        {
+            if (!userTriggered)
+            {
+                if (Judgement.TimeOffset > HitObject.TickTimeDistance / 2)
+                    Judgement.Result = Modes.Objects.Drawables.HitResult.Miss;
+                return;
+            }
+
+            if (Math.Abs(Judgement.TimeOffset) < HitObject.TickTimeDistance / 2)
+            {
+                Judgement.Result = HitResult.Hit;
+                Judgement.Score = TaikoScoreResult.Great;
+            }
+        }
+
+        protected override void Update()
+        {
+            // Drum roll ticks shouldn't move
+        }
+
+        protected override bool OnKeyDown(InputState state, KeyDownEventArgs args)
+        {
+            if (args.Repeat)
+                return false;
+
+            if (Judgement.Result.HasValue)
+                return false;
+
+            if (!validKeys.Contains(args.Key))
+                return false;
+
+            return UpdateJudgement(true);
+        }
+    }
+}
