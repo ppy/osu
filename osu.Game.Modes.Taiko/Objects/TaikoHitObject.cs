73ffbf81423970358f210231bbd7e3ad66f92163
Dean Herbert <pe@ppy.sh>
2017-03-19 11:52:16 +0900

    Remove unnecessary using.

--- osu.Game.Modes.Taiko/Objects/TaikoHitObject.cs
+++ osu.Game.Modes.Taiko/Objects/TaikoHitObject.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Game.Beatmaps.Samples;
 using osu.Game.Beatmaps.Timing;
 using osu.Game.Database;
 using osu.Game.Modes.Objects;
