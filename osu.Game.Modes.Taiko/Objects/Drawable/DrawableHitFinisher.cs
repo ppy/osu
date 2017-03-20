17b684e738fae8e1256dff67519c0cd046826834
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-03-20 18:25:12 +0900

    Add license headers.

--- osu.Game.Modes.Taiko/Objects/Drawable/DrawableHitFinisher.cs
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableHitFinisher.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Input;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK.Input;
 using System;
 using System.Linq;
 using osu.Framework.Input;
