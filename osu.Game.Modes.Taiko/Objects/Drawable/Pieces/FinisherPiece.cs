edd9a22177b5c4032fdf7f7c6a8ebe13a291081c
smoogipooo <smoogipooo@gmail.com>
2017-03-20 17:02:06 +0900

    License headers.

--- osu.Game.Modes.Taiko/Objects/Drawable/Pieces/FinisherPiece.cs
+++ osu.Game.Modes.Taiko/Objects/Drawable/Pieces/FinisherPiece.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK;
 
 namespace osu.Game.Modes.Taiko.Objects.Drawable.Pieces
 {
