16d2a8ea7937a288cf2fcf0309d40535389ee4c1
smoogipooo <smoogipooo@gmail.com>
2017-03-20 19:26:28 +0900

    Add centre/rim hit drawing.

---
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableCentreHit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using OpenTK.Input;
+using osu.Game.Modes.Taiko.Objects.Drawable.Pieces;
+
+namespace osu.Game.Modes.Taiko.Objects.Drawable
+{
+    public class DrawableCentreHit : DrawableHit
+    {
+        protected override List<Key> HitKeys { get; } = new List<Key>(new[] { Key.F, Key.J });
+
+        public DrawableCentreHit(TaikoHitObject hitObject)
+            : base(hitObject)
+        {
+        }
+
+        protected override ScrollingCirclePiece CreateCircle() => new CentreHitCirclePiece
+        {
+            KiaiMode = HitObject.Kiai
+        };
+    }
+}
