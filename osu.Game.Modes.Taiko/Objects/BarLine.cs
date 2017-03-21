b05a12372a2ef916ad81a0103563f84bad3c1681
smoogipooo <smoogipooo@gmail.com>
2017-03-21 18:44:07 +0900

    Add base barline.

---
+++ osu.Game.Modes.Taiko/Objects/BarLine.cs
@@ -0,0 +1,28 @@
+﻿using osu.Game.Beatmaps.Timing;
+using osu.Game.Database;
+
+namespace osu.Game.Modes.Taiko.Objects
+{
+    public class BarLine
+    {
+        /// <summary>
+        /// The start time of the control point this bar line represents.
+        /// </summary>
+        public double StartTime;
+
+        /// <summary>
+        /// The time to scroll in the bar line.
+        /// </summary>
+        public double PreEmpt;
+
+        /// <summary>
+        /// Whether this is a major bar line (affects display).
+        /// </summary>
+        public bool IsMajor;
+
+        public void ApplyDefaults(TimingInfo timing, BeatmapDifficulty difficulty)
+        {
+            PreEmpt = 600 / (timing.SliderVelocityAt(StartTime) * difficulty.SliderMultiplier) * 1000;
+        }
+    }
+}
