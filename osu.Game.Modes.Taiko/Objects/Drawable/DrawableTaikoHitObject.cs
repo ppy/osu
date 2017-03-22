aa8780797ebd3e6df63a6a9d1e2a96204348829a
smoogipooo <smoogipooo@gmail.com>
2017-03-22 15:22:02 +0900

    Abstract class + move AccentColour to base.

--- osu.Game.Modes.Taiko/Objects/Drawable/DrawableTaikoHitObject.cs
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableTaikoHitObject.cs
@@ -1,21 +1,15 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK.Graphics;
 using osu.Framework.Graphics;
 using osu.Game.Modes.Objects.Drawables;
 using osu.Game.Modes.Taiko.Judgements;
 
 namespace osu.Game.Modes.Taiko.Objects.Drawable
 {
-    public class DrawableTaikoHitObject : DrawableHitObject<TaikoHitObject, TaikoJudgementInfo>
+    public abstract class DrawableTaikoHitObject : DrawableHitObject<TaikoHitObject, TaikoJudgementInfo>
     {
-        /// <summary>
-        /// The colour used for various elements of this DrawableHitObject.
-        /// </summary>
-        public virtual Color4 AccentColour { get; }
-
-        public DrawableTaikoHitObject(TaikoHitObject hitObject)
+        protected DrawableTaikoHitObject(TaikoHitObject hitObject)
             : base(hitObject)
         {
             Anchor = Anchor.CentreLeft;
@@ -25,16 +19,9 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
 
             LifetimeStart = HitObject.StartTime - HitObject.PreEmpt * 2;
             LifetimeEnd = HitObject.StartTime + HitObject.PreEmpt;
-
-            // Todo: Remove (suppresses Resharper)
-            AccentColour = Color4.White;
         }
 
         protected override TaikoJudgementInfo CreateJudgementInfo() => new TaikoJudgementInfo();
-
-        protected override void UpdateState(ArmedState state)
-        {
-        }
 
         /// <summary>
         /// Sets the scroll position of the DrawableHitObject relative to the offset between
