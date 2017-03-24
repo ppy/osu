df46d0f446c574074a645098d4833f1c11f64ae6
smoogipooo <smoogipooo@gmail.com>
2017-03-24 18:49:10 +0900

    Cleanup.

--- osu.Desktop.VisualTests/Tests/TestCaseTaikoHitObjects.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseTaikoHitObjects.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using OpenTK;
-using OpenTK.Graphics;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Screens.Testing;
