5281842965316b488cfb7f68d083db9a77a67a31
smoogipooo <smoogipooo@gmail.com>
2017-03-24 18:37:56 +0900

    Rewrite CirclePiece to be referenced to 128x128px and handle accented hits more sanely.

---
+++ osu.Game.Modes.Taiko/Objects/Drawable/Pieces/AccentedCirclePiece.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+
+namespace osu.Game.Modes.Taiko.Objects.Drawable.Pieces
+{
+    public class AccentedCirclePiece : CirclePiece
+    {
+        public AccentedCirclePiece(string symbolName)
+            : base(symbolName)
+        {
+        }
+
+        public override Vector2 Size => new Vector2(base.Size.X, base.Size.Y * 1.5f);
+    }
+}
