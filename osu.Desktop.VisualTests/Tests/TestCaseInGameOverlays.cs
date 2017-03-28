bc0f0087e3eb63cde9457952f79057044785ac01
Andrey Zavadskiy <87902114+bubb13gummy@users.noreply.github.com>
2017-03-28 15:03:12 +0300

    Edited pause visual test

--- osu.Desktop.VisualTests/Tests/TestCaseInGameOverlays.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseInGameOverlays.cs
@@ -1,17 +1,19 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Logging;
 using osu.Framework.Screens.Testing;
 using osu.Game.Screens.Play;
 
 namespace osu.Desktop.VisualTests.Tests
 {
-    internal class TestCasePauseOverlay : TestCase
+    internal class TestCaseInGameOverlays : TestCase
     {
-        public override string Description => @"Tests the pause overlay";
+        public override string Description => @"Tests the pause and fail overlays";
 
         private PauseOverlay pauseOverlay;
+        private FailOverlay failOverlay;
         private int retryCount;
 
         public override void Reset()
@@ -27,12 +29,34 @@ namespace osu.Desktop.VisualTests.Tests
                 Title = @"paused",
                 Description = @"you're not going to do what i think you're going to do, are ya?",
             });
+            Add(failOverlay = new FailOverlay
+            {
+                Depth = -1,
+                OnRetry = () => Logger.Log(@"Retry"),
+                OnQuit = () => Logger.Log(@"Quit"),
+                Title = @"failed",
+                Description = @"you're dead, try again?",
+            });
 
-            AddButton("Pause", pauseOverlay.Show);
+            AddButton("Pause", delegate {
+                if(failOverlay.State == Visibility.Visible)
+                {
+                    failOverlay.Hide();
+                }
+                pauseOverlay.Show();
+            });
+            AddButton("Fail", delegate {
+                if (pauseOverlay.State == Visibility.Visible)
+                {
+                    pauseOverlay.Hide();
+                }
+                failOverlay.Show();
+            });
             AddButton("Add Retry", delegate
             {
                 retryCount++;
                 pauseOverlay.Retries = retryCount;
+                failOverlay.Retries = retryCount;
             });
 
             retryCount = 0;
