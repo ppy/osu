2967000839b83f5e045c143ba307a0fda8978184
smoogipooo <smoogipooo@gmail.com>
2017-03-29 16:22:08 +0900

    Taiko autoplay username :D.

--- osu.Game.Modes.Taiko/Mods/TaikoMod.cs
+++ osu.Game.Modes.Taiko/Mods/TaikoMod.cs
@@ -5,6 +5,7 @@ using osu.Game.Beatmaps;
 using osu.Game.Modes.Mods;
 using osu.Game.Modes.Scoring;
 using osu.Game.Modes.Taiko.Objects;
+using osu.Game.Users;
 
 namespace osu.Game.Modes.Taiko.Mods
 {
@@ -69,6 +70,7 @@ namespace osu.Game.Modes.Taiko.Mods
     {
         protected override Score CreateReplayScore(Beatmap<TaikoHitObject> beatmap) => new Score
         {
+            User = new User { Username = "mekkadosu!" },
             Replay = new TaikoAutoReplay(beatmap)
         };
     }
