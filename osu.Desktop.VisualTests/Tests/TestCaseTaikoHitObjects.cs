7ba7bc18f8468aeba9c6875f15eded270954f363
smoogipooo <smoogipooo@gmail.com>
2017-03-29 15:35:22 +0900

    Don't use a List for HitKeys.

--- osu.Desktop.VisualTests/Tests/TestCaseTaikoHitObjects.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseTaikoHitObjects.cs
@@ -6,7 +6,6 @@ using OpenTK.Graphics;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Testing;
 using osu.Game.Graphics;
 using osu.Game.Modes.Taiko.Objects.Drawable.Pieces;
