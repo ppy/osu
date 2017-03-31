2b847aa2f46488e4933c869fdabd6552a93f7b88
Dean Herbert <pe@ppy.sh>
2017-03-31 17:55:07 +0900

    Bring VisualTests up-to-date.

---
+++ osu.Desktop.VisualTests/AutomatedVisualTestGame.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Testing;
+using osu.Game;
+
+namespace osu.Desktop.VisualTests
+{
+    public class AutomatedVisualTestGame : OsuGameBase
+    {
+        public AutomatedVisualTestGame()
+        {
+            Add(new TestRunner(new TestBrowser()));
+        }
+    }
+}
