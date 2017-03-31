9add5e974d09e0534fb424fe6cf3ce52b1779ab0
Jorolf <87902114+bubb13gummy@users.noreply.github.com>
2017-03-31 21:13:20 +0200

    add license header

--- osu.Game/Screens/Select/Details/BeatmapDetailsGraph.cs
+++ osu.Game/Screens/Select/Details/BeatmapDetailsGraph.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Graphics;
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK.Graphics;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using System.Collections.Generic;
