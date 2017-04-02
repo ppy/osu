668252377bc841cb7849c32a94e39922bf493cee
Jorolf <87902114+bubb13gummy@users.noreply.github.com>
2017-04-02 18:19:59 +0200

    add Tooltips

---
+++ osu.Desktop.VisualTests/Tests/TestCaseTooltip.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Desktop.VisualTests.Tests
+{
+    internal class TestCaseTooltip : TestCase
+    {
+        public override string Description => "tests tooltips on various elements";
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            Children = new Drawable[]
+            {
+                new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Children = new[]
+                    {
+                        new TooltipSpriteText
+                        {
+                            Text = "Text with some tooltip",
+                        },
+                        new TooltipSpriteText
+                        {
+                           Text = "and another one",
+                        },
+
+                    },
+                },
+            };
+        }
+
+        private class TooltipSpriteText : OsuSpriteText, IHasTooltip
+        {
+            public string Tooltip => Text;
+        }
+    }
+}
