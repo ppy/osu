6a3601efdca8614a274b3089ceecc106bbfe5317
smoogipooo <smoogipooo@gmail.com>
2017-04-03 14:22:22 +0900

    Fix post-merge errors.

--- osu.Game.Modes.Taiko/UI/TaikoHitRenderer.cs
+++ osu.Game.Modes.Taiko/UI/TaikoHitRenderer.cs
@@ -4,15 +4,15 @@
 using osu.Game.Beatmaps;
 using osu.Framework.Graphics;
 using osu.Game.Modes.Objects.Drawables;
-using osu.Game.Modes.Replays;
 using osu.Game.Modes.Scoring;
 using osu.Game.Modes.Taiko.Beatmaps;
 using osu.Game.Modes.Taiko.Judgements;
 using osu.Game.Modes.Taiko.Objects;
 using osu.Game.Modes.Taiko.Objects.Drawable;
-using osu.Game.Modes.Taiko.Replays;
 using osu.Game.Modes.Taiko.Scoring;
 using osu.Game.Modes.UI;
+using osu.Game.Modes.Replays;
+using osu.Game.Modes.Taiko.Replays;
 
 namespace osu.Game.Modes.Taiko.UI
 {
@@ -37,20 +37,20 @@ namespace osu.Game.Modes.Taiko.UI
 
         protected override DrawableHitObject<TaikoHitObject, TaikoJudgement> GetVisualRepresentation(TaikoHitObject h)
         {
-            var hit = h as Hit;
-            if (hit != null)
+            var centreHit = h as CentreHit;
+            if (centreHit != null)
             {
-                switch (hit.Type)
-                {
-                    case HitType.Centre:
-                        if (h.IsStrong)
-                            return new DrawableStrongCentreHit(hit);
-                        return new DrawableCentreHit(hit);
-                    case HitType.Rim:
-                        if (h.IsStrong)
-                            return new DrawableStrongRimHit(hit);
-                        return new DrawableRimHit(hit);
-                }
+                if (h.IsStrong)
+                    return new DrawableStrongCentreHit(centreHit);
+                return new DrawableCentreHit(centreHit);
+            }
+
+            var rimHit = h as RimHit;
+            if (centreHit != null)
+            {
+                if (h.IsStrong)
+                    return new DrawableStrongRimHit(rimHit);
+                return new DrawableRimHit(rimHit);
             }
 
             var drumRoll = h as DrumRoll;
@@ -67,5 +67,7 @@ namespace osu.Game.Modes.Taiko.UI
 
             return null;
         }
+
+        protected override FramedReplayInputHandler CreateReplayInputHandler(Replay replay) => new TaikoFramedReplayInputHandler(replay);
     }
 }
