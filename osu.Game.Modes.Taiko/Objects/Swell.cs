774e15b89dd94efd1e7fbfd001db55e8aa611adc
smoogipooo <smoogipooo@gmail.com>
2017-04-03 15:02:21 +0900

    Set RequiredHits at conversion time to remove HitMultiplier.

--- osu.Game.Modes.Taiko/Objects/Swell.cs
+++ osu.Game.Modes.Taiko/Objects/Swell.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System;
-using osu.Game.Beatmaps.Timing;
-using osu.Game.Database;
 using osu.Game.Modes.Objects.Types;
 
 namespace osu.Game.Modes.Taiko.Objects
@@ -15,22 +12,8 @@ namespace osu.Game.Modes.Taiko.Objects
         public double Duration => EndTime - StartTime;
 
         /// <summary>
-        /// The multiplier for cases in which the number of required hits by a swell is not
-        /// dependent on solely the overall difficulty and the duration of the swell.
-        /// </summary>
-        public double HitMultiplier { get; set; } = 1;
-
-        /// <summary>
         /// The number of hits required to complete the swell successfully.
         /// </summary>
-        public int RequiredHits { get; protected set; } = 10;
-
-        public override void ApplyDefaults(TimingInfo timing, BeatmapDifficulty difficulty)
-        {
-            base.ApplyDefaults(timing, difficulty);
-
-            double baseHitMultiplier = BeatmapDifficulty.DifficultyRange(difficulty.OverallDifficulty, 3, 5, 7.5);
-            RequiredHits = (int)Math.Max(1, Duration / 1000f * baseHitMultiplier * HitMultiplier);
-        }
+        public int RequiredHits = 10;
     }
 }
