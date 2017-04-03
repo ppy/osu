6e612bbefc029cb2fc788469feeb910e56b22d95
smoogipooo <smoogipooo@gmail.com>
2017-04-03 14:39:56 +0900

    Reduce complexity of instantiating circle pieces in hits.

--- osu.Game.Modes.Taiko/Objects/Drawable/DrawableHit.cs
+++ osu.Game.Modes.Taiko/Objects/Drawable/DrawableHit.cs
@@ -6,6 +6,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Modes.Objects.Drawables;
 using osu.Game.Modes.Taiko.Judgements;
+using osu.Game.Modes.Taiko.Objects.Drawable.Pieces;
 using System;
 using System.Linq;
 
@@ -20,14 +21,16 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
 
         protected override Container<Framework.Graphics.Drawable> Content => bodyContainer;
 
+        protected readonly CirclePiece Circle;
+
         private readonly Hit hit;
+
+        private readonly Container bodyContainer;
 
         /// <summary>
         /// Whether the last key pressed is a valid hit key.
         /// </summary>
         private bool validKeyPressed;
-
-        private readonly Container bodyContainer;
 
         protected DrawableHit(Hit hit)
             : base(hit)
@@ -38,6 +41,10 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
+                Children = new[]
+                {
+                    Circle = CreateCirclePiece()
+                }
             });
         }
 
@@ -100,5 +107,7 @@ namespace osu.Game.Modes.Taiko.Objects.Drawable
 
             Expire();
         }
+
+        protected abstract CirclePiece CreateCirclePiece();
     }
 }
