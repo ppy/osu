05ac73edcb18241c7822a941deba83388d875aae
smoogipooo <smoogipooo@gmail.com>
2017-04-10 10:33:52 +0900

    Apply width modification to hit object container instead of hit objects, to preserve original size.

--- osu.Game.Modes.Taiko/Objects/Drawables/DrawableTaikoHitObject.cs
+++ osu.Game.Modes.Taiko/Objects/Drawables/DrawableTaikoHitObject.cs
@@ -22,7 +22,7 @@ namespace osu.Game.Modes.Taiko.Objects.Drawables
         /// </summary>
         private readonly List<Key> validKeys = new List<Key>(new[] { Key.D, Key.F, Key.J, Key.K });
 
-        public override Vector2 OriginPosition => new Vector2(bodyContainer.DrawHeight / 2f, DrawHeight / 2f);
+        public override Vector2 OriginPosition => new Vector2(DrawHeight / 2f);
 
         protected override Container<Drawable> Content => bodyContainer;
 
@@ -46,8 +46,6 @@ namespace osu.Game.Modes.Taiko.Objects.Drawables
 
             AddInternal(bodyContainer = new Container
             {
-                Anchor = Anchor.CentreLeft,
-                Origin = Anchor.CentreLeft,
                 AutoSizeAxes = Axes.Both,
                 Children = new[]
                 {
