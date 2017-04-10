ba03a989925285ab0f979238de7711657fa5d04c
Dean Herbert <pe@ppy.sh>
2017-04-10 16:39:10 +0900

    Add missing config set.

--- osu.Game/Configuration/OsuConfigManager.cs
+++ osu.Game/Configuration/OsuConfigManager.cs
@@ -46,6 +46,7 @@ namespace osu.Game.Configuration
             Set(OsuConfig.AutomaticDownload, true).Disabled = true;
             Set(OsuConfig.AutomaticDownloadNoVideo, false).Disabled = true;
             Set(OsuConfig.BlockNonFriendPM, false).Disabled = true;
+            Set(OsuConfig.Bloom, false).Disabled = true;
             Set(OsuConfig.BloomSoftening, false).Disabled = true;
             Set(OsuConfig.BossKeyFirstActivation, true).Disabled = true;
             Set(OsuConfig.ChatAudibleHighlight, true).Disabled = true;
