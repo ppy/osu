9c4319fbe480c16d92a6ba8b29f8562c957e6da2
ocboogie <87902114+bubb13gummy@users.noreply.github.com>
2017-04-16 20:18:41 -0700

    Added basic loading screen and test case

---
+++ osu.Game/Screens/Play/LoadingScreen.cs
@@ -0,0 +1,37 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Screens;
+using osu.Game.Beatmaps;
+using osu.Game.Database;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Screens.Backgrounds;
+using osu.Game.Screens.Menu;
+using OpenTK;
+
+namespace osu.Game.Screens.Play
+{
+    public class LoadingScreen : OsuScreen
+    {
+
+        private string loadingText = "loading...";
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Add(
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Text = loadingText,
+                    TextSize = 48,
+                    Font = @"Exo2.0-MediumItalic"
+                }
+            );
+        }
+    }
+}
