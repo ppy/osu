773e6a29119e02df9d4cf3434df113f30fcb0385
Jorolf <jorolf@gmx.de>
2017-04-17 17:04:48 +0200

    moved tooltip stuff to Tooltip instead of MenuCursor

--- osu.Game/Graphics/Cursor/Tooltip.cs
+++ osu.Game/Graphics/Cursor/Tooltip.cs
@@ -8,7 +8,10 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input;
+using osu.Framework.Threading;
 using osu.Game.Graphics.Sprites;
+using System.Linq;
 
 namespace osu.Game.Graphics.Cursor
 {
@@ -16,6 +19,10 @@ namespace osu.Game.Graphics.Cursor
     {
         private readonly Box tooltipBackground;
         private readonly OsuSpriteText text;
+
+        private ScheduledDelegate show;
+        private UserInputManager input;
+        private IHasOverhangingTooltip overhang;
 
         public string TooltipText {
             get
@@ -65,9 +72,34 @@ namespace osu.Game.Graphics.Cursor
         }
 
         [BackgroundDependencyLoader]
-        private void load(OsuColour colour)
+        private void load(OsuColour colour, UserInputManager input)
         {
+            this.input = input;
             tooltipBackground.Colour = colour.Gray3;
+        }
+
+        public void UpdateTooltip(InputState state)
+        {
+            Scheduler.Update();
+            if (overhang?.Overhanging ?? false)
+                TooltipText = overhang.Tooltip;
+            else if (state.Mouse.Position != state.Mouse.LastPosition)
+            {
+                show?.Cancel();
+                TooltipText = string.Empty;
+                IHasTooltip hasTooltip = input.HoveredDrawables.OfType<IHasTooltip>().FirstOrDefault();
+                if (hasTooltip != null)
+                {
+                    IHasDelayedTooltip delayedTooltip = hasTooltip as IHasDelayedTooltip;
+                    overhang = hasTooltip as IHasOverhangingTooltip;
+                    show = Scheduler.AddDelayed(() => TooltipText = hasTooltip.Tooltip, delayedTooltip?.Delay ?? 250);
+                }
+            }
+            else if (overhang != null)
+            {
+                overhang = null;
+                TooltipText = string.Empty;
+            }
         }
     }
 }
