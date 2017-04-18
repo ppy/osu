2f53f2e248a6a39feb2d91298b1131293b41f07c
Dean Herbert <pe@ppy.sh>
2017-04-18 22:35:21 +0900

    Add license headers.

--- osu.Game/Screens/Ranking/ResultModeButton.cs
+++ osu.Game/Screens/Ranking/ResultModeButton.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
