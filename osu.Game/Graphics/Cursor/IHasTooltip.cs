885d1ca062c8ff44aaec5b315cf14a8cb20491c2
Jorolf <87902114+bubb13gummy@users.noreply.github.com>
2017-04-19 13:47:00 +0200

    renaming and some changes in TestCaseTooltip

--- osu.Game/Graphics/Cursor/IHasTooltip.cs
+++ osu.Game/Graphics/Cursor/IHasTooltip.cs
@@ -1,35 +1,37 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Graphics;
+
 namespace osu.Game.Graphics.Cursor
 {
-    public interface IHasTooltip
+    public interface IHasTooltip : IDrawable
     {
         /// <summary>
-        /// Tooltip that shows when hovering the object
+        /// Tooltip that shows when hovering the drawable
         /// </summary>
-        string Tooltip { get; }
+        string TooltipText { get; }
     }
 
     /// <summary>
     /// Tooltip with custom appear time
     /// </summary>
-    public interface IHasDelayedTooltip : IHasTooltip
+    public interface IHasTooltipWithCustomDelay : IHasTooltip
     {
         /// <summary>
         /// Time until the tooltip appears (in milliseconds)
         /// </summary>
-        int Delay { get; }
+        int TooltipDelay { get; }
     }
 
     /// <summary>
-    /// Tooltip which can show after hovering over the object
+    /// Tooltip which can decide when to disappear
     /// </summary>
-    public interface IHasOverhangingTooltip : IHasTooltip
+    public interface IHasDisappearingTooltip : IHasTooltip
     {
         /// <summary>
-        /// Should the tooltip still show?
+        /// Should the tooltip disappear?
         /// </summary>
-        bool Overhanging { get; }
+        bool Disappear { get; }
     }
 }
