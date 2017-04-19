afcd7d7fa08330c1d870df7e56ec0faebe1bf93f
Dean Herbert <pe@ppy.sh>
2017-04-19 16:03:00 +0900

    Format accuracy better in LeaderboardScores.

--- osu.Game/Screens/Select/Leaderboards/LeaderboardScore.cs
+++ osu.Game/Screens/Select/Leaderboards/LeaderboardScore.cs
@@ -204,7 +204,7 @@ namespace osu.Game.Screens.Select.Leaderboards
                                                     Children = new Drawable[]
                                                     {
                                                         maxCombo = new ScoreComponentLabel(FontAwesome.fa_link, Score.MaxCombo.ToString()),
-                                                        accuracy = new ScoreComponentLabel(FontAwesome.fa_crosshairs, string.Format(Score.Accuracy % 1 == 0 ? @"{0:0}" : @"{0:0.00}", Score.Accuracy)),
+                                                        accuracy = new ScoreComponentLabel(FontAwesome.fa_crosshairs, string.Format(Score.Accuracy % 1 == 0 ? @"{0:P0}" : @"{0:P2}", Score.Accuracy)),
                                                     },
                                                 },
                                             },
