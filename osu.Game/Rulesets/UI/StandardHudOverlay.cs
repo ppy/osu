d1ca5a9d15c5b75974b9473d1fb811fe1b07ff33
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-04-20 18:02:09 +0900

    CI fixes.

--- osu.Game/Rulesets/UI/StandardHudOverlay.cs
+++ osu.Game/Rulesets/UI/StandardHudOverlay.cs
@@ -3,7 +3,6 @@
 
 using OpenTK;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Primitives;
 using osu.Game.Graphics;
