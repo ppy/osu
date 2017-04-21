ef7bc0f92ef50cc3458753f95b8edb21acdc726c
Dean Herbert <pe@ppy.sh>
2017-04-21 13:53:11 +0900

    Update UI controls to understand DisabledChanged.

--- osu.Game/Overlays/Options/OptionDropdown.cs
+++ osu.Game/Overlays/Options/OptionDropdown.cs
@@ -34,8 +34,6 @@ namespace osu.Game.Overlays.Options
             {
                 bindable = value;
                 dropdown.Current.BindTo(bindable);
-                if (value?.Disabled ?? true)
-                    Alpha = 0.3f;
             }
         }
 
@@ -74,6 +72,11 @@ namespace osu.Game.Overlays.Options
                     RelativeSizeAxes = Axes.X,
                     Items = Items,
                 }
+            };
+
+            dropdown.Current.DisabledChanged += disabled =>
+            {
+                Alpha = disabled ? 0.3f : 1;
             };
         }
     }
