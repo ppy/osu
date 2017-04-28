2b95157bce6bddaa332f35205e6ceb0066655ffe
Thomas Tan <87902114+bubb13gummy@users.noreply.github.com>
2017-04-29 02:13:54 +0800

    Add license header

--- osu.Game/Rulesets/Replays/IAutoGenerator.cs
+++ osu.Game/Rulesets/Replays/IAutoGenerator.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Rulesets.Replays;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Rulesets.Replays;
 
 namespace osu.Game
 {
