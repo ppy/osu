8ac99c4a71c66b44b848f5e3f44d437794c4a07a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-05-01 19:56:01 +0900

    Use less usings.

--- osu.Game/Overlays/Music/PlaylistItem.cs
+++ osu.Game/Overlays/Music/PlaylistItem.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
-using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
