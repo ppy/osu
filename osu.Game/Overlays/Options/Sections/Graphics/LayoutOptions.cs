52b4c3d6e63363bf88782896fa30c00b970c259b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-05-04 23:32:27 +0900

    Apply CI fixes.

--- osu.Game/Overlays/Options/Sections/Graphics/LayoutOptions.cs
+++ osu.Game/Overlays/Options/Sections/Graphics/LayoutOptions.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
-using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Overlays.Options.Sections.Graphics
 {
--- osu.Game/Overlays/Options/Sections/Graphics/MainMenuOptions.cs
+++ osu.Game/Overlays/Options/Sections/Graphics/MainMenuOptions.cs
@@ -3,7 +3,6 @@
 
 using osu.Framework.Allocation;
 using osu.Game.Configuration;
-using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Overlays.Options.Sections.Graphics
 {
