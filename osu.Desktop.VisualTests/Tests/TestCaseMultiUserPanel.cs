a541c1e0c5056cd44de8f765ed6a1292edf74a6c
Naeferith <toto2020@hotmail.fr>
2017-05-06 20:40:55 +0200

    Update TestCaseMultiUserPanel.cs due to MultiUserPanel.cs modifications

--- osu.Desktop.VisualTests/Tests/TestCaseMultiUserPanel.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseMultiUserPanel.cs
@@ -3,7 +3,7 @@
 
 using OpenTK;
 using OpenTK.Graphics;
-using osu.Framework.Screens.Testing;
+using osu.Framework.Testing;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Screens.Select;
 using osu.Game.Screens.Multiplayer;
@@ -13,14 +13,24 @@ using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input;
 using osu.Game.Graphics.Sprites;
+using osu.Game.Users;
 
 namespace osu.Desktop.VisualTests.Tests
 {
     class TestCaseMultiUserPanel : TestCase
     {
         private MultiUserPanel test;
-        private FlowContainer panelContainer;
-        public override string Name => @"MultiUserPanel";
+        private FillFlowContainer panelContainer;
+        public User User = new User
+        {
+            Id = 9492835,
+            Username = @"Naeferith",
+            Country = new Country
+            {
+                FullName = @"France",
+                FlagName = @"FR",
+            },
+        };
         public override string Description => @"User pannel in a multiplayer room";
 
         private void action(int action)
@@ -37,19 +47,18 @@ namespace osu.Desktop.VisualTests.Tests
         {
             base.Reset();
 
-            AddButton(@"Switch status", () => action(0));
+            AddStep(@"Switch status", () => action(0));
 
-            Add(panelContainer = new FlowContainer //Positionning container
+            Add(panelContainer = new FillFlowContainer
             {
                 RelativeSizeAxes = Axes.Both,
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                Direction = FlowDirections.Horizontal,
+                Direction = FillDirection.Full,
                 Size = new Vector2(0.9f, 0.5f),
                 Children = new Drawable[]
                 {
-                    test = new MultiUserPanel("Naeferith"),
-                    new MultiUserPanel(),
+                    test = new MultiUserPanel(User),
                 }
             });
         }
