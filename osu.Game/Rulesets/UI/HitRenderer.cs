1e9e109efe6dacd7ad2ebc9750b581985ce01f1d
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-05-10 20:39:13 +0900

    Internalize HitObjects to playfields, since some playfields may not use that container.

--- osu.Game/Rulesets/UI/HitRenderer.cs
+++ osu.Game/Rulesets/UI/HitRenderer.cs
@@ -187,17 +187,19 @@ namespace osu.Game.Rulesets.UI
 
         public sealed override bool ProvidingUserCursor => !HasReplayLoaded && Playfield.ProvidingUserCursor;
 
-        protected override Container<Drawable> Content => content;
-        protected override bool AllObjectsJudged => Playfield.HitObjects.Children.All(h => h.Judged);
+        public override IEnumerable<HitObject> Objects => Beatmap.HitObjects;
+
+        protected override bool AllObjectsJudged => drawableObjects.All(o => o.Judged);
 
         /// <summary>
         /// The playfield.
         /// </summary>
         protected Playfield<TObject, TJudgement> Playfield;
 
+        protected override Container<Drawable> Content => content;
         private readonly Container content;
 
-        public override IEnumerable<HitObject> Objects => Beatmap.HitObjects;
+        private List<DrawableHitObject<TObject, TJudgement>> drawableObjects = new List<DrawableHitObject<TObject, TJudgement>>();
 
         protected HitRenderer(WorkingBeatmap beatmap)
             : base(beatmap)
@@ -224,6 +226,8 @@ namespace osu.Game.Rulesets.UI
 
         private void loadObjects()
         {
+            drawableObjects.Clear();
+
             foreach (TObject h in Beatmap.HitObjects)
             {
                 var drawableObject = GetVisualRepresentation(h);
@@ -233,6 +237,7 @@ namespace osu.Game.Rulesets.UI
 
                 drawableObject.OnJudgement += onJudgement;
 
+                drawableObjects.Add(drawableObject);
                 Playfield.Add(drawableObject);
             }
 
