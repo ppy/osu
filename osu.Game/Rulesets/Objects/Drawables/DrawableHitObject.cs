09d6cd7e7647e24c6e839c2fc6d01a84045e5f28
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-05-11 17:16:01 +0900

    Fix post-merge breakage

--- osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
+++ osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
@@ -34,11 +34,6 @@ namespace osu.Game.Rulesets.Objects.Drawables
 
         public TJudgement Judgement;
 
-        /// <summary>
-        /// Whether this hit object has been judged.
-        /// </summary>
-        public virtual bool Judged => (Judgement?.Result ?? HitResult.None) != HitResult.None;
-
         protected abstract TJudgement CreateJudgement();
 
         protected abstract void UpdateState(ArmedState state);
@@ -82,7 +77,7 @@ namespace osu.Game.Rulesets.Objects.Drawables
         /// <summary>
         /// Whether this hit object and all of its nested hit objects have been judged.
         /// </summary>
-        public sealed override bool Judged => base.Judged && (NestedHitObjects?.All(h => h.Judged) ?? true);
+        public bool Judged => (Judgement?.Result ?? HitResult.None) != HitResult.None && (NestedHitObjects?.All(h => h.Judged) ?? true);
 
         protected DrawableHitObject(TObject hitObject)
         {
