52ef072944e4d142555cbea3128d54148c979bb9
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-05-15 10:55:29 +0900

    Change all instances of options to settings where applicable

---
+++ osu.Game/Overlays/Settings/Sections/OnlineSection.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+
+namespace osu.Game.Overlays.Settings.Sections
+{
+    public class OnlineSection : SettingsSection
+    {
+        public override string Header => "Online";
+        public override FontAwesome Icon => FontAwesome.fa_globe;
+
+        public OnlineSection()
+        {
+            Children = new Drawable[]
+            {
+            };
+        }
+    }
+}
