5cc588c547c7133b1e63556c12419517413855ab
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-05-17 18:42:48 +0900

    Add base for event parsing code.

---
+++ osu.Game/Beatmaps/Events/Event.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+namespace osu.Game.Beatmaps.Events
+{
+    public abstract class Event
+    {
+        /// <summary>
+        /// The event start time.
+        /// </summary>
+        public double StartTime;
+    }
+}
