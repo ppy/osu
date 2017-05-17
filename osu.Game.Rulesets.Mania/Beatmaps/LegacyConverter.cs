5f253fb77d6a2d9003d06448fab2495846ef6bee
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-05-17 13:07:56 +0900

    Add basic mania-specific beatmap conversion.
    # Conflicts:
    #	osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs

---
+++ osu.Game.Rulesets.Mania/Beatmaps/LegacyConverter.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
+using SharpNeat.Utility;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Rulesets.Mania.Beatmaps
+{
+    /// <summary>
+    /// Special converter used for converting from osu!stable beatmaps.
+    /// </summary>
+    internal class LegacyConverter
+    {
+        private readonly FastRandom random;
+
+        private readonly int availableColumns;
+        private readonly float localXDivisor; 
+
+        private readonly Beatmap beatmap;
+
+        public LegacyConverter(Beatmap beatmap)
+        {
+            this.beatmap = beatmap;
+
+            int seed = (int)Math.Round(beatmap.BeatmapInfo.Difficulty.DrainRate + beatmap.BeatmapInfo.Difficulty.CircleSize)
+                * 20 + (int)(beatmap.BeatmapInfo.Difficulty.OverallDifficulty * 41.2) + (int)Math.Round(beatmap.BeatmapInfo.Difficulty.ApproachRate);
+
+            availableColumns = (int)Math.Round(beatmap.BeatmapInfo.Difficulty.CircleSize);
+            localXDivisor = 512.0f / availableColumns;
+        }
+
+        public IEnumerable<ManiaHitObject> Convert(HitObject original)
+        {
+            if (beatmap.BeatmapInfo.RulesetID == 3)
+                yield return generateSpecific(original);
+        }
+
+        private ManiaHitObject generateSpecific(HitObject original)
+        {
+            var endTimeData = original as IHasEndTime;
+            var positionData = original as IHasPosition;
+
+            int column = getColumn(positionData?.X ?? 0);
+
+            if (endTimeData != null)
+            {
+                return new HoldNote
+                {
+                    StartTime = original.StartTime,
+                    Samples = original.Samples,
+                    Duration = endTimeData.Duration,
+                    Column = column,
+                };
+            }
+
+            return new Note
+            {
+                StartTime = original.StartTime,
+                Samples = original.Samples,
+                Column = column
+            };
+        }
+
+        private int getColumn(float position) => MathHelper.Clamp((int)Math.Floor(position / localXDivisor), 0, availableColumns - 1);
+    }
+}
--- osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
+++ osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
@@ -1,35 +1,38 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Game.Beatmaps;
-using osu.Game.Rulesets.Mania.Objects;
-using System.Collections.Generic;
-using System;
-using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Beatmaps;
+using osu.Game.Rulesets.Mania.Objects;
+using System;
+using System.Collections.Generic;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Objects;
-using OpenTK;
+using osu.Game.Rulesets.Objects.Types;
+using System.Linq;
 
 namespace osu.Game.Rulesets.Mania.Beatmaps
 {
-    internal class ManiaBeatmapConverter : BeatmapConverter<ManiaHitObject>
+    public class ManiaBeatmapConverter : BeatmapConverter<ManiaHitObject>
     {
         protected override IEnumerable<Type> ValidConversionTypes { get; } = new[] { typeof(IHasXPosition) };
 
+        protected override Beatmap<ManiaHitObject> ConvertBeatmap(Beatmap original)
+        {
+            // Todo: This should be cased when we get better conversion methods
+            var converter = new LegacyConverter(original);
+
+            return new Beatmap<ManiaHitObject>
+            {
+                BeatmapInfo = original.BeatmapInfo,
+                TimingInfo = original.TimingInfo,
+                HitObjects = original.HitObjects.SelectMany(converter.Convert).ToList()
+            };
+        }
+
         protected override IEnumerable<ManiaHitObject> ConvertHitObject(HitObject original, Beatmap beatmap)
         {
-            int availableColumns = (int)Math.Round(beatmap.BeatmapInfo.Difficulty.CircleSize);
-
-            var positionData = original as IHasXPosition;
-
-            float localWDivisor = 512.0f / availableColumns;
-            int column = MathHelper.Clamp((int)Math.Floor((positionData?.X ?? 1) / localWDivisor), 0, availableColumns - 1);
-
-            yield return new Note
-            {
-                StartTime = original.StartTime,
-                Column = column,
-            };
+            // Handled by the LegacyConvereter
+            yield return null;
         }
     }
 }
