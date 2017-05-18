9e3930b56e8c9afbc506e605593f0f67525feabe
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-05-18 17:43:59 +0900

    This function can't be used for all pattern generations. So let's not use it.

--- osu.Game.Rulesets.Mania/Beatmaps/Patterns/PatternGenerator.cs
+++ osu.Game.Rulesets.Mania/Beatmaps/Patterns/PatternGenerator.cs
@@ -47,43 +47,5 @@ namespace osu.Game.Rulesets.Mania.Beatmaps.Patterns
         /// </summary>
         /// <returns>The <see cref="Pattern"/> containing the hit objects.</returns>
         public abstract Pattern Generate();
-
-        /// <summary>
-        /// Constructs and adds a note to a pattern.
-        /// </summary>
-        /// <param name="pattern">The pattern to add to.</param>
-        /// <param name="originalObject">The original hit object (used for samples).</param>
-        /// <param name="column">The column to add the note to.</param>
-        /// <param name="startTime">The start time of the note.</param>
-        /// <param name="endTime">The end time of the note (set to <paramref name="startTime"/> for a non-hold note).</param>
-        /// <param name="siblings">The number of children alongside this note (these will not be generated, but are used for volume calculations).</param>
-        protected void AddToPattern(Pattern pattern, HitObject originalObject, int column, double startTime, double endTime, int siblings = 1)
-        {
-            ManiaHitObject newObject;
-
-            if (startTime == endTime)
-            {
-                newObject = new Note
-                {
-                    StartTime = startTime,
-                    Samples = originalObject.Samples,
-                    Column = column
-                };
-            }
-            else
-            {
-                newObject = new HoldNote
-                {
-                    StartTime = startTime,
-                    Samples = originalObject.Samples,
-                    Column = column,
-                    Duration = endTime - startTime
-                };
-            }
-
-            // Todo: Consider siblings and write sample volumes (probably at ManiaHitObject level)
-
-            pattern.Add(newObject);
-        }
     }
 }
