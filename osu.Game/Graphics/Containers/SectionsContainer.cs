b5e82d41f095871822f2784155cb7defff9bde12
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2017-05-20 04:41:06 +0800

    Basic children management of SectionsContainer.

---
+++ osu.Game/Graphics/Containers/SectionsContainer.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Graphics.Containers
+{
+    /// <summary>
+    /// A container that can scroll to each section inside it.
+    /// </summary>
+    public class SectionsContainer : Container
+    {
+        private Drawable expandableHeader, fixedHeader;
+        private ScrollContainer scrollContainer;
+
+        public Drawable ExpandableHeader
+        {
+            get { return expandableHeader; }
+            set
+            {
+                if (value == expandableHeader) return;
+
+                scrollContainer.Remove(expandableHeader);
+                expandableHeader = value;
+                expandableHeader.Depth = float.MinValue;
+                scrollContainer.Add(expandableHeader);
+            }
+        }
+
+        public Drawable FixedHeader
+        {
+            get { return fixedHeader; }
+            set
+            {
+                if (value == fixedHeader) return;
+
+                scrollContainer.Remove(fixedHeader);
+                fixedHeader = value;
+                fixedHeader.Depth = float.MinValue / 2;
+                scrollContainer.Add(fixedHeader);
+            }
+        }
+
+        private List<Drawable> sections = new List<Drawable>();
+        public IEnumerable<Drawable> Sections
+        {
+            get { return sections; }
+            set
+            {
+                if (value == sections) return;
+
+                foreach (var section in sections)
+                    scrollContainer.Remove(section);
+
+                sections = value.ToList();
+                scrollContainer.Add(value);
+            }
+        }
+
+        public SectionsContainer()
+        {
+            Add(scrollContainer = new ScrollContainer
+            {
+                RelativeSizeAxes = Axes.Both
+            });
+        }
+    }
+}
