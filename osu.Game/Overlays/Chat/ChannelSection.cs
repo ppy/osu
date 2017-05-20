2e1d01a2681973567744727e7174b2cbb306f063
DrabWeb <sethunity@gmail.com>
2017-05-20 20:22:55 -0300

    Added searching

--- osu.Game/Overlays/Chat/ChannelSection.cs
+++ osu.Game/Overlays/Chat/ChannelSection.cs
@@ -11,25 +11,30 @@ using osu.Game.Online.Chat;
 
 namespace osu.Game.Overlays.Chat
 {
-    public class ChannelSection : Container
+    public class ChannelSection : Container, IHasFilterableChildren
     {
         private readonly FillFlowContainer<ChannelListItem> items;
         private readonly OsuSpriteText header;
 
+        public IEnumerable<IFilterable> FilterableChildren => items.Children.OfType<IFilterable>();
+        public string[] FilterTerms => new[] { Header };
+        public bool MatchingCurrentFilter
+        {
+        	set
+            {
+                FadeTo(value ? 1f : 0f, 100);
+            }
+        }
+
         public string Header
         {
-            set
-            {
-                header.Text = value;
-            }
+            get { return header.Text; }
+            set { header.Text = value; }
         }
 
         public IEnumerable<Channel> Channels
         {
-            set
-            {
-                items.Children = value.Select(c => new ChannelListItem { Channel = c });
-            }
+            set { items.Children = value.Select(c => new ChannelListItem { Channel = c }); }
         }
 
         public ChannelSection()
