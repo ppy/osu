d447c72b9d602fb05697346dc1bce91b76daf6bc
MrTheMake <87902114+bubb13gummy@users.noreply.github.com>
2017-05-21 20:27:41 +0200

    Fixed an error with looping

--- osu.Game/Screens/Select/PlaySongSelect.cs
+++ osu.Game/Screens/Select/PlaySongSelect.cs
@@ -71,7 +71,10 @@ namespace osu.Game.Screens.Select
         protected override void OnResuming(Screen last)
         {
             player = null;
-            Beatmap.Track.Looping = false;
+
+            Beatmap.Track.Reset();
+            Beatmap.Track.Start();
+            Beatmap.Track.Looping = true;
 
             base.OnResuming(last);
         }
