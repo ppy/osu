bed36dc5f8f21813708d8ee5b2e3d3842679fffc
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2017-05-22 01:29:39 -0300

    CI fixes

--- osu.Desktop.VisualTests/Tests/TestCaseDrawableMultiplayerRoom.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseDrawableMultiplayerRoom.cs
@@ -1,25 +1,17 @@
 // Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK;
-using OpenTK.Graphics;
-using osu.Game.Graphics.UserInterface;
-using osu.Game.Screens.Select;
-using osu.Game.Screens.Multiplayer;
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Primitives;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Input;
-using osu.Game.Graphics.Sprites;
+using osu.Framework.Graphics;
 using osu.Framework.Testing;
+using osu.Game.Screens.Multiplayer;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Users;
 using osu.Game.Database;
 
 namespace osu.Desktop.VisualTests.Tests
 {
-    class TestCaseDrawableMultiplayerRoom : TestCase
+    internal class TestCaseDrawableMultiplayerRoom : TestCase
     {
         public override string Description => @"Select your favourite room";
 
