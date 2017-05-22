92d0d3b4b674eac5f7ae7a559923f3516d13bd4b
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2017-05-22 13:14:56 -0300

    Unused using

--- osu.Game/Screens/Multiplayer/DrawableRoom.cs
+++ osu.Game/Screens/Multiplayer/DrawableRoom.cs
@@ -4,7 +4,6 @@
 using OpenTK;
 using OpenTK.Graphics;
 using osu.Framework.Allocation;
-using osu.Framework.Configuration;
 using osu.Framework.Extensions;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
