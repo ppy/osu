7f5c72f4fd1dec56071e646571c66e6e667de365
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-05-24 02:09:31 +0900

    Remove unused parameter

--- osu.Game/Screens/Menu/OsuLogo.cs
+++ osu.Game/Screens/Menu/OsuLogo.cs
@@ -213,7 +213,7 @@ namespace osu.Game.Screens.Menu
         }
 
         [BackgroundDependencyLoader]
-        private void load(TextureStore textures, AudioManager audio, OsuGameBase game)
+        private void load(TextureStore textures, AudioManager audio)
         {
             sampleClick = audio.Sample.Get(@"Menu/menuhit");
             logo.Texture = textures.Get(@"Menu/logo");
