e89f433f6857262f3bbb19202820f68837122ee9
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2017-05-25 02:11:07 +0800

    Basic section management of userpage.

---
+++ osu.Game/Overlays/UserPageOverlay.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Overlays.UserPage;
+using osu.Game.Users;
+
+namespace osu.Game.Overlays
+{
+    public class UserPageOverlay : FocusedOverlayContainer
+    {
+        private readonly User user;
+        private UserPageSection lastSection;
+        public UserPageOverlay(User user)
+        {
+            this.user = user;
+            var tab = new OsuTabControl<UserPageSection>();
+            var sections = new UserPageSection[] { };
+            var sectionsContainer = new SectionsContainer
+            {
+                ExpandableHeader = new UserPageHeader(),
+                FixedHeader = tab,
+                Sections = sections
+            };
+
+            Add(sectionsContainer);
+
+            sectionsContainer.SelectedSection.ValueChanged += s =>
+            {
+                if (lastSection != s)
+                {
+                    lastSection = s as UserPageSection;
+                    tab.Current.Value = lastSection;
+                }
+            };
+
+            tab.Current.ValueChanged += s =>
+            {
+                if (lastSection != s)
+                {
+                    lastSection = s;
+                    sectionsContainer.ScrollContainer.ScrollIntoView(lastSection);
+                }
+            };
+        }
+    }
+}
