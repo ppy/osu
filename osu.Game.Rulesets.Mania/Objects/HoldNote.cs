7d7f885059d39b01c2ffa6851a066421310ec995
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2017-05-29 15:35:50 +0900

    Update HoldNote.cs

--- osu.Game.Rulesets.Mania/Objects/HoldNote.cs
+++ osu.Game.Rulesets.Mania/Objects/HoldNote.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System.Collections.Generic;
-using System.Linq;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Database;
 using osu.Game.Rulesets.Objects.Types;
