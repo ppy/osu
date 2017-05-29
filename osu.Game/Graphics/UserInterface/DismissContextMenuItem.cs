5b2185ca50c4fe925b9ec8cf986e4ad513ab6145
EVAST9919 <megaman9919@gmail.com>
2017-05-29 17:22:21 +0300

    Added dismiss item

---
+++ osu.Game/Graphics/UserInterface/DismissContextMenuItem.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class DismissContextMenuItem : ContextMenuItem
+    {
+        public DismissContextMenuItem(string title) : base(title)
+        {
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            TextColour = colours.Red;
+        }
+    }
+}
