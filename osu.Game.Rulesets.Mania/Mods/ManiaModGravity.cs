186fecca82bc35c38a0dae151bce6045dc4bf48e
smoogipooo <smoogipooo@gmail.com>
2017-06-02 18:20:14 +0900

    Add missing license headers.

--- osu.Game.Rulesets.Mania/Mods/ManiaModGravity.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModGravity.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
 using System.Collections.Generic;
 using osu.Game.Rulesets.Mania.Objects;
 using osu.Game.Rulesets.Mania.UI;
