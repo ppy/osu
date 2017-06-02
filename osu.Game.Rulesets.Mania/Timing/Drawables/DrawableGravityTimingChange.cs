1da5d508fac44ba69eebc87f53a4be27e3e27335
smoogipooo <smoogipooo@gmail.com>
2017-06-02 11:35:51 +0900

    aaaaa

--- osu.Game.Rulesets.Mania/Timing/Drawables/DrawableGravityTimingChange.cs
+++ osu.Game.Rulesets.Mania/Timing/Drawables/DrawableGravityTimingChange.cs
@@ -1,25 +1,39 @@
+using System;
+using osu.Framework.Graphics;
+using osu.Framework.MathUtils;
 using osu.Framework.Physics;
+using osu.Game.Rulesets.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Mania.Timing.Drawables
 {
     public class DrawableGravityTimingChange : DrawableTimingChange
     {
-        private const float acceleration = 9.8f;
-        private const float terminal_velocity = 50f;
+        private const double acceleration = 9.8f;
+        private const double terminal_velocity = 50f;
 
-        private RigidBodySimulation sim;
+        private const double duration = 2000;
+
 
         public DrawableGravityTimingChange(TimingChange timingChange)
             : base(timingChange)
         {
-            sim = new RigidBodySimulation(Content);
         }
 
         protected override void Update()
         {
             base.Update();
 
-            // Todo: Gravity calculations here
+            var parent = (TimingChangeContainer)Parent;
+
+            double elapsed = Math.Max(0, Time.Current - TimingChange.Time);
+
+            // @ Current == TimingChange.Time - duration -> Y = TimingChange.Time
+            // @ Current == TimingChange.Time -> Y = 0
+            // @ Current == TimingChange.Time + x -> Y = -f(x)
+
+            double acceleration = elapsed / 2 / 1000 / 1000;
+
+            Content.Y = (float)(TimingChange.Time - 1 / 2f * acceleration * elapsed * elapsed);
         }
     }
 }
