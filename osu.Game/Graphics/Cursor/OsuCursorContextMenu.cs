2d996b23608109d5194c80cce9fbd26508b0844b
EVAST9919 <megaman9919@gmail.com>
2017-06-04 15:02:51 +0300

    Context menu implementation

---
+++ osu.Game/Graphics/Cursor/OsuCursorContextMenu.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.UserInterface;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Game.Graphics.Cursor
+{
+    public class OsuCursorContextMenu : CursorContextMenu
+    {
+        protected override ContextMenuContainer CreateContextMenuContainer() => new OsuContextMenuContainer();
+
+        public OsuCursorContextMenu(CursorContainer cursor) : base(cursor)
+        {
+        }
+    }
+}
---
+++ osu.Game/Graphics/UserInterface/OsuContextMenuItem.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK.Graphics;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
+using osu.Game.Graphics.Sprites;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class OsuContextMenuItem : ContextMenuItem
+    {
+        private const int transition_length = 200;
+        private const int margin_horizontal = 15;
+        public const int MARGIN_VERTICAL = 5;
+
+        private OsuSpriteText text;
+        private OsuSpriteText textBold;
+
+        private SampleChannel sampleClick;
+        private SampleChannel sampleHover;
+
+        private ContextMenuType type;
+
+        protected override Container CreateTextContainer(string title) => new Container
+        {
+            AutoSizeAxes = Axes.Both,
+            Anchor = Anchor.CentreLeft,
+            Origin = Anchor.CentreLeft,
+            Children = new Drawable[]
+            {
+                text = new OsuSpriteText
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    TextSize = 17,
+                    Text = title,
+                    Margin = new MarginPadding { Horizontal = margin_horizontal, Vertical = MARGIN_VERTICAL },
+                },
+                textBold = new OsuSpriteText
+                {
+                    AlwaysPresent = true,
+                    Alpha = 0,
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    TextSize = 17,
+                    Text = title,
+                    Font = @"Exo2.0-Bold",
+                    Margin = new MarginPadding { Horizontal = margin_horizontal, Vertical = MARGIN_VERTICAL },
+                }
+            }
+        };
+
+        public OsuContextMenuItem(string title, ContextMenuType type = ContextMenuType.Standard) : base(title)
+        {
+            this.type = type;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(AudioManager audio)
+        {
+            sampleHover = audio.Sample.Get(@"Menu/menuclick");
+            sampleClick = audio.Sample.Get(@"Menu/menuback");
+
+            BackgroundColour = Color4.Transparent;
+            BackgroundColourHover = OsuColour.FromHex(@"172023");
+
+            updateTextColour();
+        }
+
+        private void updateTextColour()
+        {
+            switch (type)
+            {
+                case ContextMenuType.Standard:
+                    textBold.Colour = text.Colour = Color4.White;
+                    break;
+                case ContextMenuType.Destructive:
+                    textBold.Colour = text.Colour = Color4.Red;
+                    break;
+                case ContextMenuType.Highlighted:
+                    textBold.Colour = text.Colour = OsuColour.FromHex(@"ffcc22");
+                    break;
+            }
+        }
+
+        protected override bool OnHover(InputState state)
+        {
+            sampleHover.Play();
+            textBold.FadeIn(transition_length, EasingTypes.OutQuint);
+            text.FadeOut(transition_length, EasingTypes.OutQuint);
+            return base.OnHover(state);
+        }
+
+        protected override void OnHoverLost(InputState state)
+        {
+            textBold.FadeOut(transition_length, EasingTypes.OutQuint);
+            text.FadeIn(transition_length, EasingTypes.OutQuint);
+            base.OnHoverLost(state);
+        }
+
+        protected override bool OnClick(InputState state)
+        {
+            sampleClick.Play();
+            return base.OnClick(state);
+        }
+    }
+}
