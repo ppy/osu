face50700748b31ceeb1b258e4fee0dbb0e0b7c5
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2017-06-09 15:22:07 +0800

    Improve section behaviour.

--- osu.Game/Users/Profile/ProfileSection.cs
+++ osu.Game/Users/Profile/ProfileSection.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using OpenTK;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
@@ -13,27 +14,44 @@ namespace osu.Game.Users.Profile
     {
         public abstract string Title { get; }
 
+        private readonly FillFlowContainer content;
+        protected override Container<Drawable> Content => content;
+
         protected ProfileSection()
         {
-            Margin = new MarginPadding { Horizontal = UserProfile.CONTENT_X_MARGIN };
             Direction = FillDirection.Vertical;
             AutoSizeAxes = Axes.Y;
             RelativeSizeAxes = Axes.X;
-            Children = new Drawable[]
+            InternalChildren = new Drawable[]
             {
                 new OsuSpriteText
                 {
                     Text = Title,
                     TextSize = 16,
                     Font = @"Exo2.0-RegularItalic",
-                    Margin = new MarginPadding { Vertical = 20 }
+                    Margin = new MarginPadding
+                    {
+                        Horizontal = UserProfile.CONTENT_X_MARGIN,
+                        Vertical = 20
+                    }
+                },
+                content = new FillFlowContainer
+                {
+                    Direction = FillDirection.Vertical,
+                    AutoSizeAxes = Axes.Y,
+                    RelativeSizeAxes = Axes.X,
+                    Margin = new MarginPadding
+                    {
+                        Horizontal = UserProfile.CONTENT_X_MARGIN,
+                        Bottom = 20
+                    }
                 },
                 new Box
                 {
                     RelativeSizeAxes = Axes.X,
                     Height = 1,
                     Colour = OsuColour.Gray(34),
-                    Depth = float.MinValue
+                    EdgeSmoothness = new Vector2(1)
                 }
             };
         }
