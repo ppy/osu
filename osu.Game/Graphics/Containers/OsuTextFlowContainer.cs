185dd4c547a46c797e5f1d6f3708ee85097ec56e
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2017-06-13 00:58:32 +0800

    Expose AddTextAwesome for OsuTextFlowContainer.

--- osu.Game/Graphics/Containers/OsuTextFlowContainer.cs
+++ osu.Game/Graphics/Containers/OsuTextFlowContainer.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
+using System.Collections.Generic;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Graphics.Sprites;
@@ -10,5 +12,7 @@ namespace osu.Game.Graphics.Containers
     public class OsuTextFlowContainer: TextFlowContainer
     {
         protected override SpriteText CreateSpriteText() => new OsuSpriteText();
+
+        public IEnumerable<SpriteText> AddTextAwesome(FontAwesome icon, Action<SpriteText> creationParameters = null) => AddText(((char)icon).ToString(), creationParameters);
     }
 }
