3b8b01be8f2df85cf6540a8ea01423bccad1d585
Huo Yaoyuan <87902114+bubb13gummy@users.noreply.github.com>
2017-06-13 00:51:06 +0800

    Add OsuTextFlowContainer.

---
+++ osu.Game/Graphics/Containers/OsuTextFlowContainer.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics.Sprites;
+
+namespace osu.Game.Graphics.Containers
+{
+    public class OsuTextFlowContainer: TextFlowContainer
+    {
+        protected override SpriteText CreateSpriteText() => new OsuSpriteText();
+    }
+}
