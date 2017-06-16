874db28478f8cc339153ce026c1d705ef8f44049
smoogipooo <smoogipooo@gmail.com>
2017-06-16 13:09:06 +0900

    CI fixes.

--- osu.Game.Rulesets.Mania/Timing/ManiaSpeedAdjustmentContainer.cs
+++ osu.Game.Rulesets.Mania/Timing/ManiaSpeedAdjustmentContainer.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Framework.Graphics;
 using osu.Game.Rulesets.Timing;
 
 namespace osu.Game.Rulesets.Mania.Timing
