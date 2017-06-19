1b95991e40a0ad60e2b824905690e91dfa50ed5d
MrTheMake <marc.stephan96@hotmail.de>
2017-06-19 19:05:09 +0200

    Extended condition

--- osu.Game/Overlays/MusicController.cs
+++ osu.Game/Overlays/MusicController.cs
@@ -238,7 +238,7 @@ namespace osu.Game.Overlays
                 progressBar.UpdatePosition(track.Length == 0 ? 0 : (float)(track.CurrentTime / track.Length));
                 playButton.Icon = track.IsRunning ? FontAwesome.fa_pause_circle_o : FontAwesome.fa_play_circle_o;
 
-                if (track.HasCompleted && !track.Looping) next();
+                if (track.HasCompleted && !track.Looping && AllowBeatmapChange) next();
             }
             else
                 playButton.Icon = FontAwesome.fa_play_circle_o;
