c77b5033456c17ddac7ef5ce68838c6a5083d9df
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-06-28 10:18:12 -0700

    Add OsuFocusedOverlayContainer with predefined sounds.

--- osu.Game/Overlays/Dialog/PopupDialog.cs
+++ osu.Game/Overlays/Dialog/PopupDialog.cs
@@ -15,10 +15,11 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Input;
 using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.Containers;
 
 namespace osu.Game.Overlays.Dialog
 {
-    public class PopupDialog : FocusedOverlayContainer
+    public class PopupDialog : OsuFocusedOverlayContainer
     {
         public static readonly float ENTER_DURATION = 500;
         public static readonly float EXIT_DURATION = 200;
