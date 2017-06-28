40225238d96b78e1544f1f18721211d0174fb422
Thomas Müller <thomas94@gmx.net>
2017-06-28 12:24:23 +0300

    Set RelativeSizeAxes for updated FillMode behavior

--- osu.Game/Users/UserPanel.cs
+++ osu.Game/Users/UserPanel.cs
@@ -46,6 +46,7 @@ namespace osu.Game.Users
             {
                 new AsyncLoadWrapper(new CoverBackgroundSprite(user)
                 {
+                    RelativeSizeAxes = Axes.Both,
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     FillMode = FillMode.Fill,
