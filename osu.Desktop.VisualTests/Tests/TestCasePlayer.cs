3c425dd07f2d2e55fe4588900e93119b7f2cd623
Dean Herbert <pe@ppy.sh>
2017-07-08 18:04:55 +0900

    Fix CI issue

--- osu.Desktop.VisualTests/Tests/TestCasePlayer.cs
+++ osu.Desktop.VisualTests/Tests/TestCasePlayer.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Testing;
 using osu.Game.Beatmaps;
