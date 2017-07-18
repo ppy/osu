007b343854b7cb4119dee337c6c87c5272f68c5e
Dean Herbert <pe@ppy.sh>
2017-07-18 17:45:27 +0900

    Fix incorrect equality check when TimingPointAt returns a default T

--- osu.Game/Beatmaps/ControlPoints/ControlPoint.cs
+++ osu.Game/Beatmaps/ControlPoints/ControlPoint.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace osu.Game.Beatmaps.ControlPoints
 {
-    public class ControlPoint : IComparable<ControlPoint>
+    public class ControlPoint : IComparable<ControlPoint>, IEquatable<ControlPoint>
     {
         /// <summary>
         /// The time at which the control point takes effect.
@@ -13,5 +13,12 @@ namespace osu.Game.Beatmaps.ControlPoints
         public double Time;
 
         public int CompareTo(ControlPoint other) => Time.CompareTo(other.Time);
+
+        public bool Equals(ControlPoint other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Time.Equals(other.Time);
+        }
     }
 }
