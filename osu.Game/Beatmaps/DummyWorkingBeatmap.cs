89bdc7c9ce2b5946dadec046c511e21b0826cfc3
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-07-18 16:51:34 +0900

    Add a dummy beatmap provider for use in BeatmapInfoWedge

---
+++ osu.Game/Beatmaps/DummyWorkingBeatmap.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+using osu.Framework.Audio.Track;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Database;
+using osu.Game.Rulesets.Objects;
+
+namespace osu.Game.Beatmaps
+{
+    internal class DummyWorkingBeatmap : WorkingBeatmap
+    {
+        public DummyWorkingBeatmap()
+            : base(new BeatmapInfo
+            {
+                Metadata = new BeatmapMetadata
+                {
+                    Artist = "please load a beatmap!",
+                    Title = "no beatmaps available!",
+                    Author = "no one",
+                },
+                Difficulty = new BeatmapDifficulty(),
+            })
+        {
+        }
+
+        protected override Beatmap GetBeatmap() => new Beatmap
+        {
+            HitObjects = new List<HitObject>(),
+        };
+
+        protected override Texture GetBackground() => null;
+
+        protected override Track GetTrack() => null;
+    }
+}
