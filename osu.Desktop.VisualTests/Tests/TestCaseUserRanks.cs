59c48b59e35de2539a3892f407061d14c77dc085
Jorolf <87902114+bubb13gummy@users.noreply.github.com>
2017-07-26 19:42:34 +0200

    CI stuff

--- osu.Desktop.VisualTests/Tests/TestCaseUserRanks.cs
+++ osu.Desktop.VisualTests/Tests/TestCaseUserRanks.cs
@@ -15,8 +15,6 @@ using osu.Game.Rulesets.Scoring;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace osu.Desktop.VisualTests.Tests
 {
