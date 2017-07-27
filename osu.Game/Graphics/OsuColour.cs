8bd552cfe3fb41ddee514a3266529b40e006c924
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-07-27 18:03:24 +0900

    Allow OsuColour.FromHex to support prefix #.
    Coincides with https://github.com/ppy/osu-web/pull/1373

--- osu.Game/Graphics/OsuColour.cs
+++ osu.Game/Graphics/OsuColour.cs
@@ -13,6 +13,9 @@ namespace osu.Game.Graphics
 
         public static Color4 FromHex(string hex)
         {
+            if (hex[0] == '#')
+                hex = hex.Substring(1);
+
             switch (hex.Length)
             {
                 default:
