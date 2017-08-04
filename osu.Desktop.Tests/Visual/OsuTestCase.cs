ff833e173751686bec364922a418a9b8565854f7
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-08-04 15:37:31 +0900

    Update to support new framework VisualTests structure

---
+++ osu.Desktop.Tests/Visual/OsuTestCase.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using osu.Framework.Desktop.Platform;
+using osu.Framework.Testing;
+using osu.Game;
+
+namespace osu.Desktop.Tests.Visual
+{
+    [TestFixture]
+    public abstract class OsuTestCase : TestCase
+    {
+        [Test]
+        public override void RunTest()
+        {
+            using (var host = new HeadlessGameHost())
+                host.Run(new OsuTestCaseTestRunner(this));
+        }
+
+        public class OsuTestCaseTestRunner : OsuGameBase
+        {
+            private readonly OsuTestCase testCase;
+
+            public OsuTestCaseTestRunner(OsuTestCase testCase)
+            {
+                this.testCase = testCase;
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+                Add(new TestCaseTestRunner.TestRunner(testCase));
+            }
+        }
+    }
+}
