f8448b8b199cd779ba01349a70ff5be81c4b75f7
Dean Herbert <pe@ppy.sh>
2017-08-04 15:37:31 +0900

    Update to support new framework VisualTests structure

---
+++ osu.Desktop.Tests/Visual/TestCaseKeyCounter.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.MathUtils;
+using osu.Game.Screens.Play;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace osu.Desktop.Tests.Visual
+{
+    internal class TestCaseKeyCounter : OsuTestCase
+    {
+        public override string Description => @"Tests key counter";
+
+        public TestCaseKeyCounter()
+        {
+            KeyCounterCollection kc = new KeyCounterCollection
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                IsCounting = true,
+                Children = new KeyCounter[]
+                {
+                    new KeyCounterKeyboard(Key.Z),
+                    new KeyCounterKeyboard(Key.X),
+                    new KeyCounterMouse(MouseButton.Left),
+                    new KeyCounterMouse(MouseButton.Right),
+                },
+            };
+            BindableInt bindable = new BindableInt { MinValue = 0, MaxValue = 200, Default = 50 };
+            bindable.ValueChanged += delegate { kc.FadeTime = bindable.Value; };
+            AddStep("Add Random", () =>
+            {
+                Key key = (Key)((int)Key.A + RNG.Next(26));
+                kc.Add(new KeyCounterKeyboard(key));
+            });
+
+            TestSliderBar<int> sliderBar;
+
+            Add(new Container
+            {
+                Anchor = Anchor.TopRight,
+                Origin = Anchor.TopRight,
+                AutoSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    new SpriteText { Text = "FadeTime" },
+                    sliderBar = new TestSliderBar<int>
+                    {
+                        Width = 150,
+                        Height = 10,
+                        SelectionColor = Color4.Orange,
+                    }
+                }
+            });
+
+            sliderBar.Current.BindTo(bindable);
+
+            Add(kc);
+        }
+        private class TestSliderBar<T> : SliderBar<T> where T : struct
+        {
+            public Color4 Color
+            {
+                get { return Box.Colour; }
+                set { Box.Colour = value; }
+            }
+
+            public Color4 SelectionColor
+            {
+                get { return SelectionBox.Colour; }
+                set { SelectionBox.Colour = value; }
+            }
+
+            protected readonly Box SelectionBox;
+            protected readonly Box Box;
+
+            public TestSliderBar()
+            {
+                Children = new Drawable[]
+                {
+                    Box = new Box { RelativeSizeAxes = Axes.Both },
+                    SelectionBox = new Box { RelativeSizeAxes = Axes.Both }
+                };
+            }
+
+            protected override void UpdateValue(float value)
+            {
+                SelectionBox.ScaleTo(
+                    new Vector2(value, 1),
+                    300, Easing.OutQuint);
+            }
+        }
+    }
+}
--- osu.Desktop/OsuTestBrowser.cs
+++ osu.Desktop/OsuTestBrowser.cs
@@ -1,14 +1,11 @@
-﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
-
-using osu.Framework.Platform;
-using osu.Framework.VisualTests;
+﻿using osu.Framework.Platform;
+using osu.Framework.Testing;
 using osu.Game;
 using osu.Game.Screens.Backgrounds;
 
-namespace osu.Desktop.VisualTests
+namespace osu.Desktop
 {
-    internal class VisualTestGame : OsuGameBase
+    internal class OsuTestBrowser : OsuGameBase
     {
         protected override void LoadComplete()
         {
