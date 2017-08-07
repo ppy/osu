b71caca131c42c6b34cc0717483f754e111b2ec0
Dean Herbert <pe@ppy.sh>
2017-08-07 15:09:31 +0900

    Expand abilities of catcher

---
+++ osu.Desktop.Tests/Visual/TestCaseCatcher.cs
@@ -0,0 +1,26 @@
+﻿using osu.Framework.Graphics;
+using osu.Game.Rulesets.Catch.UI;
+using OpenTK;
+
+namespace osu.Desktop.Tests.Visual
+{
+    internal class TestCaseCatcher : OsuTestCase
+    {
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            Children = new Drawable[]
+            {
+                new CatcherArea
+                {
+                    RelativePositionAxes = Axes.Both,
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.BottomLeft,
+                    Origin = Anchor.BottomLeft,
+                    Size = new Vector2(1, 0.2f),
+                }
+            };
+        }
+    }
+}
