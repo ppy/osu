10c45c3cbd70f14fce80bef59051b53083644a11
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-08-09 15:15:41 +0900

    Fix CI issues

--- osu.Game/Input/Binding.cs
+++ osu.Game/Input/Binding.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
 using osu.Game.Rulesets;
 using OpenTK.Input;
 using SQLite.Net.Attributes;
