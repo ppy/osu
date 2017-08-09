627894467b4447e53045a6d59aa58c46673112e3
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-08-09 12:37:47 +0900

    Store bindings to database

--- osu.Game/Input/ActionMappingInputManager.cs
+++ osu.Game/Input/ActionMappingInputManager.cs
@@ -1,9 +1,16 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
 using System.Collections.Generic;
+using osu.Framework.Allocation;
 using osu.Framework.Input;
+using osu.Framework.Platform;
+using osu.Game.Database;
+using osu.Game.Rulesets;
 using OpenTK.Input;
+using SQLite.Net;
+using SQLiteNetExtensions.Attributes;
 
 namespace osu.Game.Input
 {
@@ -11,6 +18,13 @@ namespace osu.Game.Input
         where T : struct
     {
         protected IDictionary<Key, T> Mappings { get; set; }
+
+        [BackgroundDependencyLoader]
+        private void load(BindingStore bindings)
+        {
+            foreach (var b in bindings.Query<Binding>())
+                Mappings[b.Key] = (T)(object)b.Action;
+        }
 
         protected override bool OnKeyDown(InputState state, KeyDownEventArgs args)
         {
@@ -30,5 +44,44 @@ namespace osu.Game.Input
             if (Mappings.TryGetValue(key, out mappedData))
                 state.Data = mappedData;
         }
+
+        private T parseStringRepresentation(string str)
+        {
+            T res;
+
+            if (Enum.TryParse(str, out res))
+                return res;
+
+            return default(T);
+        }
+    }
+
+    public class Binding
+    {
+        [ForeignKey(typeof(RulesetInfo))]
+        public int? RulesetID { get; set; }
+
+        public Key Key { get; set; }
+
+        public int Action { get; set; }
+    }
+
+    public class BindingStore : DatabaseBackedStore
+    {
+        public BindingStore(SQLiteConnection connection, Storage storage = null)
+            : base(connection, storage)
+        {
+        }
+
+        protected override void Prepare(bool reset = false)
+        {
+            Connection.CreateTable<Binding>();
+        }
+
+        protected override Type[] ValidTypes => new[]
+        {
+            typeof(Binding)
+        };
+
     }
 }
