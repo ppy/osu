55b159b976208e72c422d5353639ae8f7fb3628a
Thomas Müller <thomas94@gmx.net>
2017-08-16 13:02:11 +0900

    Remove unnecessary usings

--- osu.Desktop.Tests/Visual/TestCaseKeyCounter.cs
+++ osu.Desktop.Tests/Visual/TestCaseKeyCounter.cs
@@ -1,11 +1,8 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Framework.Configuration;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.MathUtils;
 using osu.Game.Screens.Play;
