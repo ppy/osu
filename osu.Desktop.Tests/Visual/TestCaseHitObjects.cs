4b465f0e69fd6ed6e9ecb505ef1d32284a0c2bbc
Thomas Müller <87902114+bubb13gummy@users.noreply.github.com>
2017-08-16 13:02:11 +0900

    Remove unnecessary usings

--- osu.Desktop.Tests/Visual/TestCaseHitObjects.cs
+++ osu.Desktop.Tests/Visual/TestCaseHitObjects.cs
@@ -2,18 +2,14 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System.Collections.Generic;
-using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Timing;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Judgements;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
 using OpenTK;
-using OpenTK.Graphics;
 
 namespace osu.Desktop.Tests.Visual
 {
