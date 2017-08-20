1f22942d6fd288dfbfe77a6c594fbd8f2c95818a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-08-20 21:56:37 +0900

    Add taiko default keys

--- osu.Game.Rulesets.Taiko/TaikoRuleset.cs
+++ osu.Game.Rulesets.Taiko/TaikoRuleset.cs
@@ -13,12 +13,22 @@ using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.Taiko.Scoring;
+using osu.Framework.Input.Bindings;
+using osu.Game.Rulesets.Taiko.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Taiko
 {
     public class TaikoRuleset : Ruleset
     {
         public override RulesetContainer CreateRulesetContainerWith(WorkingBeatmap beatmap, bool isForCurrentRuleset) => new TaikoRulesetContainer(this, beatmap, isForCurrentRuleset);
+
+        public override IEnumerable<KeyBinding> GetDefaultKeyBindings(int variant = 0) => new[]
+        {
+            new KeyBinding(InputKey.D, TaikoAction.LeftRim),
+            new KeyBinding(InputKey.F, TaikoAction.LeftCentre),
+            new KeyBinding(InputKey.J, TaikoAction.RightCentre),
+            new KeyBinding(InputKey.K, TaikoAction.RightRim),
+        };
 
         public override IEnumerable<Mod> GetModsFor(ModType type)
         {
