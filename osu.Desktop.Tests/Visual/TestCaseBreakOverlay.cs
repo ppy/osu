f6168bca118d1adfaec02d7b693c9897a80840db
EVAST9919 <megaman9919@gmail.com>
2017-08-23 06:52:30 +0300

    Added testcase

---
+++ osu.Desktop.Tests/Visual/TestCaseBreakOverlay.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Screens.Play;
+
+namespace osu.Desktop.Tests.Visual
+{
+    internal class TestCaseBreakOverlay : OsuTestCase
+    {
+        public override string Description => @"Tests breaks behavior";
+
+        public TestCaseBreakOverlay()
+        {
+            BreakOverlay breakOverlay = new BreakOverlay();
+
+            AddStep("Add 5s break", () => breakOverlay.Show(5000));
+            AddStep("Add 10s break", () => breakOverlay.Show(10000));
+            AddStep("Add 15s break", () => breakOverlay.Show(15000));
+        }
+    }
+}
