32a23c7fe41fd2c4f978385fdb6f32f576d9e0dc
Dean Herbert <pe@ppy.sh>
2017-08-24 20:16:03 +0900

    Add initial osu!direct beatmap download and import process

--- osu.Game/Overlays/Direct/DirectPanel.cs
+++ osu.Game/Overlays/Direct/DirectPanel.cs
@@ -4,6 +4,8 @@
 using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
+using System.IO;
+using System.Threading.Tasks;
 using OpenTK;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -19,6 +21,9 @@ using osu.Framework.Input;
 using osu.Framework.MathUtils;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.API;
+using osu.Framework.Logging;
+using osu.Game.Beatmaps.IO;
+using osu.Game.Overlays.Notifications;
 
 namespace osu.Game.Overlays.Direct
 {
@@ -34,6 +39,8 @@ namespace osu.Game.Overlays.Direct
 
         private APIAccess api;
         private ProgressBar progressBar;
+        private BeatmapManager beatmaps;
+        private NotificationOverlay notifications;
 
         protected override Container<Drawable> Content => content;
 
@@ -43,9 +50,11 @@ namespace osu.Game.Overlays.Direct
         }
 
         [BackgroundDependencyLoader]
-        private void load(APIAccess api, OsuColour colours)
+        private void load(APIAccess api, BeatmapManager beatmaps, OsuColour colours, NotificationOverlay notifications)
         {
             this.api = api;
+            this.beatmaps = beatmaps;
+            this.notifications = notifications;
 
             AddInternal(content = new Container
             {
@@ -101,12 +110,79 @@ namespace osu.Game.Overlays.Direct
 
         protected void StartDownload()
         {
+            if (!api.LocalUser.Value.IsSupporter)
+            {
+                notifications.Post(new SimpleNotification
+                {
+                    Icon = FontAwesome.fa_superpowers,
+                    Text = "You gotta be a supporter to download for now 'yo"
+                });
+                return;
+            }
+
             progressBar.FadeIn(400, Easing.OutQuint);
             progressBar.ResizeHeightTo(4, 400, Easing.OutQuint);
 
-            progressBar.Current.Value = 0.5f;
+            progressBar.Current.Value = 0;
 
-            api.Queue(new APIRequest());
+            ProgressNotification downloadNotification = new ProgressNotification
+            {
+                Text = $"Downloading {SetInfo.Metadata.Artist} - {SetInfo.Metadata.Title}",
+            };
+
+            var request = new DownloadBeatmapSetRequest(SetInfo);
+            request.Failure += e =>
+            {
+                progressBar.Current.Value = 0;
+                progressBar.FadeOut(500);
+                downloadNotification.State = ProgressNotificationState.Completed;
+                Logger.Error(e, "Failed to get beatmap download information");
+            };
+
+            request.Progress += (current, total) =>
+            {
+                float progress = (float)current / total;
+
+                progressBar.Current.Value = progress;
+
+                downloadNotification.State = ProgressNotificationState.Active;
+                downloadNotification.Progress = progress;
+            };
+
+            request.Success += data =>
+            {
+                progressBar.Current.Value = 1;
+                progressBar.FadeOut(500);
+
+                downloadNotification.State = ProgressNotificationState.Completed;
+
+                using (var stream = new MemoryStream(data))
+                using (var archive = new OszArchiveReader(stream))
+                    beatmaps.Import(archive);
+            };
+
+            downloadNotification.CancelRequested += () =>
+            {
+                request.Cancel();
+                return true;
+            };
+
+            notifications.Post(downloadNotification);
+
+            // don't run in the main api queue as this is a long-running task.
+            Task.Run(() => request.Perform(api));
+        }
+
+        public class DownloadBeatmapSetRequest : APIDownloadRequest
+        {
+            private readonly BeatmapSetInfo beatmapSet;
+
+            public DownloadBeatmapSetRequest(BeatmapSetInfo beatmapSet)
+            {
+                this.beatmapSet = beatmapSet;
+            }
+
+            protected override string Target => $@"beatmapsets/{beatmapSet.OnlineBeatmapSetID}/download";
         }
 
         protected override void LoadComplete()
