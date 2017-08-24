a2ad73559b490fedd8821182e2de2e68d6589dab
smoogipooo <smoogipooo@gmail.com>
2017-08-24 19:26:51 +0900

    a

---
+++ osu.Desktop.Tests/Visual/TestCaseEditorBeatSnapDivisor.cs
@@ -0,0 +1,23 @@
+﻿using osu.Framework.Testing;
+
+namespace osu.Desktop.Tests.Visual
+{
+    internal class TestCaseEditorBeatSnapDivisor : TestCase
+    {
+        public TestCaseEditorBeatSnapDivisor()
+        {
+
+        }
+
+        private enum SnapDivisor
+        {
+            1,
+            2,
+            3,
+            4,
+            6,
+            8,
+            1
+        }
+    }
+}
