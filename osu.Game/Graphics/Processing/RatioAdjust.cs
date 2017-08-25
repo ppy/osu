c9accc2774a80ccd75a3ee8670be81054c69c4dc
Dean Herbert <pe@ppy.sh>
2017-08-25 14:35:19 +0900

    Fix black screens on startup
    RatioAdjust was calculating in its update method without a valid parent size.

--- osu.Game/Graphics/Processing/RatioAdjust.cs
+++ osu.Game/Graphics/Processing/RatioAdjust.cs
@@ -18,7 +18,12 @@ namespace osu.Game.Graphics.Processing
         protected override void Update()
         {
             base.Update();
+
             Vector2 parent = Parent.DrawSize;
+
+            // return if parent doesn't have a size yet.
+            // this was happening at the top-level and causing nothing to be displayed as a result.
+            if (parent.X == 0 || parent.Y == 0) return;
 
             Scale = new Vector2(Math.Min(parent.Y / 768f, parent.X / 1024f));
             Size = new Vector2(1 / Scale.X);
