2aa9975fe65ac3ac44155cb71c9f82ff54f0c89b
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2017-09-08 19:17:39 -0300

    Added favourite/download buttons and author info.

--- osu.Game/Overlays/OnlineBeatmapSet/Header.cs
+++ osu.Game/Overlays/OnlineBeatmapSet/Header.cs
@@ -3,6 +3,8 @@
 
 using OpenTK;
 using OpenTK.Graphics;
+using osu.Framework.Allocation;
+using osu.Framework.Configuration;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
@@ -10,11 +12,19 @@ using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Drawables;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Backgrounds;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 
 namespace osu.Game.Overlays.OnlineBeatmapSet
 {
     public class Header : Container
     {
+        private const float tabs_height = 50;
+
+        private readonly Box tabsBg;
+
         public Header(BeatmapSetInfo set)
         {
             RelativeSizeAxes = Axes.X;
@@ -32,81 +42,298 @@ namespace osu.Game.Overlays.OnlineBeatmapSet
             {
                 new Container
                 {
-                    RelativeSizeAxes = Axes.Both,
-                    Children = new Drawable[]
+                    RelativeSizeAxes = Axes.X,
+                    Height = tabs_height,
+                    Children = new[]
                     {
-                        new Box
+                        tabsBg = new Box
                         {
                             RelativeSizeAxes = Axes.Both,
-                            Colour = Color4.Black,
-                        },
-                        new DelayedLoadWrapper(new BeatmapSetCover(set)
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            RelativeSizeAxes = Axes.Both,
-                            FillMode = FillMode.Fill,
-                            OnLoadComplete = d =>
-                            {
-                                d.FadeInFromZero(400, Easing.Out);
-                            },
-                        })
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            TimeBeforeLoad = 300
-                        },
-                        new Box
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Colour = ColourInfo.GradientVertical(Color4.Black.Opacity(0.3f), Color4.Black.Opacity(0.8f)),
                         },
                     },
                 },
-                new FillFlowContainer
+                new Container
                 {
-                    Anchor = Anchor.BottomRight,
-                    Origin = Anchor.BottomRight,
-                    Width = OnlineBeatmapSetOverlay.RIGHT_WIDTH,
-                    AutoSizeAxes = Axes.Y,
-                    Margin = new MarginPadding { Right = OnlineBeatmapSetOverlay.X_PADDING },
-                    Spacing = new Vector2(1f),
+                    RelativeSizeAxes = Axes.Both,
+                    Padding = new MarginPadding { Top = tabs_height },
                     Children = new Drawable[]
                     {
-                        new DetailBox
+                        new Container
                         {
-                            Child = new Container
+                            RelativeSizeAxes = Axes.Both,
+                            Children = new Drawable[]
                             {
-                                RelativeSizeAxes = Axes.X,
-                                Height = 42,
+                                new Box
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Colour = Color4.Black,
+                                },
+                                new DelayedLoadWrapper(new BeatmapSetCover(set)
+                                {
+                                    Anchor = Anchor.Centre,
+                                    Origin = Anchor.Centre,
+                                    RelativeSizeAxes = Axes.Both,
+                                    FillMode = FillMode.Fill,
+                                    OnLoadComplete = d =>
+                                    {
+                                        d.FadeInFromZero(400, Easing.Out);
+                                    },
+                                })
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    TimeBeforeLoad = 300
+                                },
+                                new Box
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Colour = ColourInfo.GradientVertical(Color4.Black.Opacity(0.3f), Color4.Black.Opacity(0.8f)),
+                                },
                             },
                         },
-                        new DetailBox
+                        new Container
                         {
-                            Child = new Container
+                            RelativeSizeAxes = Axes.Both,
+                            Padding = new MarginPadding { Top = 20, Bottom = 30, Horizontal = OnlineBeatmapSetOverlay.X_PADDING },
+                            Child = new FillFlowContainer
                             {
-                                RelativeSizeAxes = Axes.X,
-                                Height = 35,
+                                RelativeSizeAxes = Axes.Both,
+                                Direction = FillDirection.Vertical,
+                                Children = new Drawable[]
+                                {
+                                    new Container
+                                    {
+                                        Name = @"beatmap picker",
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 113,
+                                    },
+                                    new OsuSpriteText
+                                    {
+                                        Text = set.Metadata.Title,
+                                        Font = @"Exo2.0-BoldItalic",
+                                        TextSize = 37,
+                                    },
+                                    new OsuSpriteText
+                                    {
+                                        Text = set.Metadata.Artist,
+                                        Font = @"Exo2.0-SemiBoldItalic",
+                                        TextSize = 25,
+                                    },
+                                    new Container
+                                    {
+                                        Name = "mapper",
+                                        RelativeSizeAxes = Axes.X,
+                                        AutoSizeAxes = Axes.Y,
+                                        Margin = new MarginPadding { Top = 20 },
+                                        Child = new AuthorInfo(set.OnlineInfo),
+                                    },
+                                    new FillFlowContainer
+                                    {
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 45,
+                                        Spacing = new Vector2(5f),
+                                        Margin = new MarginPadding { Top = 10 },
+                                        Children = new Button[]
+                                        {
+                                            new FavouriteButton(),
+                                            new DownloadButton("Download", ""),
+                                            new DownloadButton("osu!direct", ""),
+                                        },
+                                    },
+                                },
                             },
                         },
-                        new DetailBox
+                        new FillFlowContainer
                         {
-                            Child = new Container
+                            Anchor = Anchor.BottomRight,
+                            Origin = Anchor.BottomRight,
+                            Width = OnlineBeatmapSetOverlay.RIGHT_WIDTH,
+                            AutoSizeAxes = Axes.Y,
+                            Margin = new MarginPadding { Right = OnlineBeatmapSetOverlay.X_PADDING },
+                            Spacing = new Vector2(1f),
+                            Children = new Drawable[]
                             {
-                                RelativeSizeAxes = Axes.X,
-                                Height = 110,
-                            },
-                        },
-                        new DetailBox
-                        {
-                            Child = new Container
-                            {
-                                RelativeSizeAxes = Axes.X,
-                                Height = 115,
+                                new DetailBox
+                                {
+                                    Child = new Container
+                                    {
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 42,
+                                    },
+                                },
+                                new DetailBox
+                                {
+                                    Child = new Container
+                                    {
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 35,
+                                    },
+                                },
+                                new DetailBox
+                                {
+                                    Child = new Container
+                                    {
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 110,
+                                    },
+                                },
+                                new DetailBox
+                                {
+                                    Child = new Container
+                                    {
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 115,
+                                    },
+                                },
                             },
                         },
                     },
                 },
             };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            tabsBg.Colour = colours.Gray3;
+        }
+
+        private class Button : OsuClickableContainer
+        {
+            private readonly Container content;
+
+            protected override Container<Drawable> Content => content;
+
+            public Button()
+            {
+                CornerRadius = 3;
+                Masking = true;
+
+                InternalChildren = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = OsuColour.FromHex(@"094c5f"),
+                    },
+                    new Triangles
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        ColourLight = OsuColour.FromHex(@"0f7c9b"),
+                        ColourDark = OsuColour.FromHex(@"094c5f"),
+                        TriangleScale = 1.5f,
+                    },
+                    content = new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                };
+            }
+        }
+
+        private class DownloadButton : Button
+        {
+            public DownloadButton(string title, string subtitle)
+            {
+                Width = 120;
+                RelativeSizeAxes = Axes.Y;
+
+                Child = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Padding = new MarginPadding { Horizontal = 10 },
+                    Children = new Drawable[]
+                    {
+                        new FillFlowContainer
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            AutoSizeAxes = Axes.Both,
+                            Direction = FillDirection.Vertical,
+                            Children = new[]
+                            {
+                                new OsuSpriteText
+                                {
+                                    Text = title,
+                                    TextSize = 13,
+                                    Font = @"Exo2.0-Bold",
+                                },
+                                new OsuSpriteText
+                                {
+                                    Text = subtitle,
+                                    TextSize = 11,
+                                    Font = @"Exo2.0-Bold",
+                                },
+                            },
+                        },
+                        new SpriteIcon
+                        {
+                            Anchor = Anchor.CentreRight,
+                            Origin = Anchor.CentreRight,
+                            Icon = FontAwesome.fa_download,
+                            Size = new Vector2(16),
+                            Margin = new MarginPadding { Right = 5 },
+                        },
+                    },
+                };
+            }
+        }
+
+        private class FavouriteButton : Button
+        {
+            public readonly Bindable<bool> Favourited = new Bindable<bool>();
+
+            public FavouriteButton()
+            {
+                RelativeSizeAxes = Axes.Y;
+
+                Container pink;
+                SpriteIcon icon;
+                Children = new Drawable[]
+                {
+                    pink = new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Alpha = 0f,
+                        Children = new Drawable[]
+                        {
+                            new Box
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = OsuColour.FromHex(@"9f015f"),
+                            },
+                            new Triangles
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                ColourLight = OsuColour.FromHex(@"cb2187"),
+                                ColourDark = OsuColour.FromHex(@"9f015f"),
+                                TriangleScale = 1.5f,
+                            },
+                        },
+                    },
+                    icon = new SpriteIcon
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Icon = FontAwesome.fa_heart_o,
+                        Size = new Vector2(18),
+                        Shadow = false,
+                    },
+                };
+
+                Favourited.ValueChanged += value =>
+                {
+                    pink.FadeTo(value ? 1 : 0, 200);
+                    icon.Icon = value ? FontAwesome.fa_heart : FontAwesome.fa_heart_o;
+                };
+                Action = () => Favourited.Value = !Favourited.Value;
+            }
+
+            protected override void UpdateAfterChildren()
+            {
+                base.UpdateAfterChildren();
+
+                Width = DrawHeight;
+            }
         }
 
         private class DetailBox : Container
