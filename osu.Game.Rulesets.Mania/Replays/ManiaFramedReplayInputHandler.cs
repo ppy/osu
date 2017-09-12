50ba555ad9d19ca800c28a60335bfa6365d59993
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-09-12 16:21:32 +0900

    Fix ordering of license header.

--- osu.Game.Rulesets.Mania/Replays/ManiaFramedReplayInputHandler.cs
+++ osu.Game.Rulesets.Mania/Replays/ManiaFramedReplayInputHandler.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using osu.Framework.Input;
-// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.Collections.Generic;
+using osu.Framework.Input;
 using osu.Game.Rulesets.Replays;
 
 namespace osu.Game.Rulesets.Mania.Replays
