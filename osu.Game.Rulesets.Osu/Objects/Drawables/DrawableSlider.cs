bfb3f3773dfb4f01071a6e6eaeb7fab44d8ab944
smoogipooo <87902114+bubb13gummy@users.noreply.github.com>
2017-09-13 00:43:30 +0900

    Fix osu! slider initial circles being placed at the wrong depth.

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSlider.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSlider.cs
@@ -65,7 +65,7 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
                     Scale = s.Scale,
                     ComboColour = s.ComboColour,
                     Samples = s.Samples,
-                }),
+                }) { Depth = 0 },
             };
 
             components.Add(body);
