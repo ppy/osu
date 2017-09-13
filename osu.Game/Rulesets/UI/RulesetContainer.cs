f1130eb899dda8d1d8e15f680aed9773485501dd
smoogipooo <smoogipooo@gmail.com>
2017-09-13 16:35:13 +0900

    Add default implementation for ScoreProcessor using Exponential scoring

--- osu.Game/Rulesets/UI/RulesetContainer.cs
+++ osu.Game/Rulesets/UI/RulesetContainer.cs
@@ -133,6 +133,8 @@ namespace osu.Game.Rulesets.UI
 
         public sealed override bool ProvidingUserCursor => !HasReplayLoaded && Playfield.ProvidingUserCursor;
 
+        public override ScoreProcessor CreateScoreProcessor() => new ScoreProcessor<TObject>(this);
+
         /// <summary>
         /// The playfield.
         /// </summary>
