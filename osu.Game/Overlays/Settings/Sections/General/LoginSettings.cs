aef1c7c43a5d7dd0760eedb0090ddc210e8221b2
EVAST9919 <megaman9919@gmail.com>
2017-09-14 03:00:04 +0300

    Make SocialOverlay non handle right click

--- osu.Game/Overlays/Settings/Sections/General/LoginSettings.cs
+++ osu.Game/Overlays/Settings/Sections/General/LoginSettings.cs
@@ -378,10 +378,6 @@ namespace osu.Game.Overlays.Settings.Sections.General
                     BackgroundColour = colours.Gray3;
                 }
             }
-
-
-
-
         }
 
         private enum UserAction
