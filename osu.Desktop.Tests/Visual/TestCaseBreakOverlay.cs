740e766201ccedccfa191a80a51ebc2c01a62352
EVAST9919 <megaman9919@gmail.com>
2017-09-15 19:39:55 +0300

    BreakOverlay and LetterboxOverlay implementation

---
+++ osu.Desktop.Tests/Visual/TestCaseBreakOverlay.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Timing;
+using osu.Game.Beatmaps.Timing;
+using osu.Game.Screens.Play;
+using System.Collections.Generic;
+
+namespace osu.Desktop.Tests.Visual
+{
+    internal class TestCaseBreakOverlay : OsuTestCase
+    {
+        public override string Description => @"Tests breaks behavior";
+
+        private readonly BreakOverlay breakOverlay;
+
+        public TestCaseBreakOverlay()
+        {
+            Clock = new FramedClock();
+
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.White,
+                },
+                breakOverlay = new BreakOverlay(true)
+            };
+
+            AddStep("Add 2s break", () => startBreak(2000));
+            AddStep("Add 5s break", () => startBreak(5000));
+            AddStep("Add 2 breaks (2s each)", startMultipleBreaks);
+        }
+
+        private void startBreak(double duration)
+        {
+            breakOverlay.Breaks = new List<BreakPeriod>
+            {
+                new BreakPeriod
+                {
+                    StartTime = Clock.CurrentTime,
+                    EndTime = Clock.CurrentTime + duration,
+                }
+            };
+
+            breakOverlay.InitializeBreaks();
+        }
+
+        private void startMultipleBreaks()
+        {
+            double currentTime = Clock.CurrentTime;
+
+            breakOverlay.Breaks = new List<BreakPeriod>
+            {
+                new BreakPeriod
+                {
+                    StartTime = currentTime,
+                    EndTime = currentTime + 2000,
+                },
+                new BreakPeriod
+                {
+                    StartTime = currentTime + 4000,
+                    EndTime = currentTime + 6000,
+                }
+            };
+
+            breakOverlay.InitializeBreaks();
+        }
+    }
+}
---
+++ osu.Game/Screens/Play/BreakOverlay.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using System.Collections.Generic;
+using osu.Game.Beatmaps.Timing;
+
+namespace osu.Game.Screens.Play
+{
+    public class BreakOverlay : VisibilityContainer
+    {
+        private const double fade_duration = BreakPeriod.MIN_BREAK_DURATION / 2;
+
+        public List<BreakPeriod> Breaks;
+
+        private readonly bool letterboxing;
+        private readonly LetterboxOverlay letterboxOverlay;
+
+        public BreakOverlay(bool letterboxing)
+        {
+            this.letterboxing = letterboxing;
+
+            RelativeSizeAxes = Axes.Both;
+            Child = letterboxOverlay = new LetterboxOverlay();
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            InitializeBreaks();
+        }
+
+        public void InitializeBreaks()
+        {
+            if (Breaks != null)
+            {
+                foreach (var b in Breaks)
+                {
+                    if (b.HasEffect)
+                    {
+                        using (BeginAbsoluteSequence(b.StartTime, true))
+                        {
+                            Show();
+
+                            using (BeginDelayedSequence(b.Duration, true))
+                                Hide();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected override void PopIn()
+        {
+            if (letterboxing) letterboxOverlay.FadeIn(fade_duration);
+        }
+
+        protected override void PopOut()
+        {
+            if (letterboxing) letterboxOverlay.FadeOut(fade_duration);
+        }
+    }
+}
