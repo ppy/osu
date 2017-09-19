611f4df484972eb8236c2b3fd7ed12faf67d9d14
Dean Herbert <pe@ppy.sh>
2017-09-19 21:41:51 +0900

    Add licence header

--- osu.Game.Rulesets.Catch/Tests/TestCaseCatchPlayer.cs
+++ osu.Game.Rulesets.Catch/Tests/TestCaseCatchPlayer.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Catch.Objects;
 
