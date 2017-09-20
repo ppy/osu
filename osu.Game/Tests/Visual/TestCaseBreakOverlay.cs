53b80dc8c1543f30b6011fbc914a9bcf63e3071c
EVAST9919 <87902114+bubb13gummy@users.noreply.github.com>
2017-09-20 16:03:31 +0300

    Remove useless usings

--- osu.Game/Tests/Visual/TestCaseBreakOverlay.cs
+++ osu.Game/Tests/Visual/TestCaseBreakOverlay.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK.Graphics;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Timing;
 using osu.Game.Beatmaps.Timing;
 using osu.Game.Screens.Play;
