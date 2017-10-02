635edd0e8744de16d71afcc6b12c1b364cfd714f
EVAST9919 <87902114+bubb13gummy@users.noreply.github.com>
2017-10-02 19:33:58 +0300

    Hide/Show Replay settings on pressing Ctrl+H

--- osu.Game/Screens/Play/HUDOverlay.cs
+++ osu.Game/Screens/Play/HUDOverlay.cs
@@ -180,12 +180,7 @@ namespace osu.Game.Screens.Play
             Margin = new MarginPadding { Top = 20, Right = 10 },
         };
 
-        protected virtual ReplaySettingsOverlay CreateReplaySettingsOverlay() => new ReplaySettingsOverlay
-        {
-            Anchor = Anchor.TopRight,
-            Origin = Anchor.TopRight,
-            Margin = new MarginPadding { Top = 100, Right = 10 },
-        };
+        protected virtual ReplaySettingsOverlay CreateReplaySettingsOverlay() => new ReplaySettingsOverlay();
 
         public virtual void BindProcessor(ScoreProcessor processor)
         {
