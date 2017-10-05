0a9d23b4bad1da2081f720a3227319d10bbd11df
smoogipoo <smoogipoo@smgi.me>
2017-10-05 14:33:39 +0900

    Update with framework changes (removal of WaveformDisplay)

--- osu.Game/Tests/Visual/TestCaseWaveform.cs
+++ osu.Game/Tests/Visual/TestCaseWaveform.cs
@@ -41,7 +41,7 @@ namespace osu.Game.Tests.Visual
 
             for (int i = 1; i <= 16; i *= 2)
             {
-                var newDisplay = new WaveformDisplay
+                var newDisplay = new BeatmapWaveformGraph
                 {
                     RelativeSizeAxes = Axes.Both,
                     Resolution = 1f / i
