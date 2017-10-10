b5a8f504e3c47bb94a718eeeeb55317aefd9d303
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2017-10-10 18:35:30 +0900

    Remove unused using

--- osu.Game/Tests/Visual/TestCaseIconButton.cs
+++ osu.Game/Tests/Visual/TestCaseIconButton.cs
@@ -6,7 +6,6 @@ using OpenTK.Graphics;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Testing;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
