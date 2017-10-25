2ba7088d69779e9f139e23c72d8f23a47e4bcc52
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-10-25 15:51:39 +0900

    Add missing database indices
    Also add missing unique constraints where applicable.

--- osu.Game/Migrations/20171025071459_AddMissingIndexRules.Designer.cs
+++ osu.Game/Migrations/20171025071459_AddMissingIndexRules.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace osu.Game.Migrations
 {
     [DbContext(typeof(OsuDbContext))]
-    partial class OsuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171025071459_AddMissingIndexRules")]
+    partial class AddMissingIndexRules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
