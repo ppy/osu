e2b6003f9864ff412df4d3ebe842f1b13ecc9f17
smoogipoo <smoogipoo@smgi.me>
2017-11-02 21:55:50 +0900

    Make taiko use the new "Final" field
    Ensures that the first hit on HitStrongs is _always_ non-final unless it was a miss. The second hit is always final.

--- osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableHit.cs
+++ osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableHit.cs
@@ -18,6 +18,11 @@ namespace osu.Game.Rulesets.Taiko.Objects.Drawables
         protected abstract TaikoAction[] HitActions { get; }
 
         /// <summary>
+        /// Whether a second hit is allowed to be processed.
+        /// </summary>
+        protected bool SecondHitAllowed { get; private set; }
+
+        /// <summary>
         /// Whether the last key pressed is a valid hit key.
         /// </summary>
         private bool validKeyPressed;
@@ -45,7 +50,15 @@ namespace osu.Game.Rulesets.Taiko.Objects.Drawables
             if (!validKeyPressed)
                 AddJudgement(new TaikoJudgement { Result = HitResult.Miss });
             else if (hitOffset < HitObject.HitWindowGood)
-                AddJudgement(new TaikoJudgement { Result = hitOffset < HitObject.HitWindowGreat ? HitResult.Great : HitResult.Good });
+            {
+                AddJudgement(new TaikoJudgement
+                {
+                    Result = hitOffset < HitObject.HitWindowGreat ? HitResult.Great : HitResult.Good,
+                    Final = !HitObject.IsStrong
+                });
+
+                SecondHitAllowed = true;
+            }
             else
                 AddJudgement(new TaikoJudgement { Result = HitResult.Miss });
         }
