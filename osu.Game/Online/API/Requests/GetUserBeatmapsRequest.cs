761d885167d1ea94db18c5798fe7746a954d0b55
EVAST9919 <megaman9919@gmail.com>
2017-11-03 20:25:21 +0300

    Add Favourite Beatmaps section in UserProfileOverlay

---
+++ osu.Game/Online/API/Requests/GetUserBeatmapsRequest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class GetUserBeatmapsRequest : APIRequest<List<GetBeatmapSetsResponse>>
+    {
+        private readonly long userId;
+        private readonly BeatmapSetType type;
+        private readonly int offset;
+
+        public GetUserBeatmapsRequest(long userId, BeatmapSetType type, int offset = 0)
+        {
+            this.userId = userId;
+            this.type = type;
+            this.offset = offset;
+        }
+
+        protected override string Target => $@"users/{userId}/beatmapsets/{type.ToString().ToLower()}?offset={offset}";
+    }
+
+    public enum BeatmapSetType
+    {
+        Most_Played,
+        Favourite,
+        Ranked_And_Approved
+    }
+}
