bd2de899183ebc28262a4466e35c3f2dc09991f9
Dean Herbert <pe@ppy.sh>
2017-11-10 19:26:25 +0900

    Why weren't these fixed previously

--- osu.Game.Rulesets.Taiko/Judgements/TaikoStrongHitJudgement.cs
+++ osu.Game.Rulesets.Taiko/Judgements/TaikoStrongHitJudgement.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Game.Rulesets.Objects.Drawables;
-
 namespace osu.Game.Rulesets.Taiko.Judgements
 {
     public class TaikoStrongHitJudgement : TaikoJudgement
