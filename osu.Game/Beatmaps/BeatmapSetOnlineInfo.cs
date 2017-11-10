0a56c2518d0cc53abac126e16d1f534a12333aee
EVAST9919 <87902114+bubb13gummy@users.noreply.github.com>
2017-11-10 03:01:11 +0300

    Extend Beatmap Set Response

--- osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs
+++ osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
-using Newtonsoft.Json;
 
 namespace osu.Game.Beatmaps
 {
@@ -25,17 +24,20 @@ namespace osu.Game.Beatmaps
         /// The date this beatmap set was last updated.
         /// </summary>
         public DateTimeOffset? LastUpdated { get; set; }
+        
+        /// <summary>
+        /// Whether or not this beatmap set has a background video.
+        /// </summary>
+        public bool HasVideo { get; set; }
 
         /// <summary>
         /// The different sizes of cover art for this beatmap set.
         /// </summary>
-        [JsonProperty(@"covers")]
         public BeatmapSetOnlineCovers Covers { get; set; }
 
         /// <summary>
         /// A small sample clip of this beatmap set's song.
         /// </summary>
-        [JsonProperty(@"previewUrl")]
         public string Preview { get; set; }
 
         /// <summary>
@@ -46,13 +48,11 @@ namespace osu.Game.Beatmaps
         /// <summary>
         /// The amount of plays this beatmap set has.
         /// </summary>
-        [JsonProperty(@"play_count")]
         public int PlayCount { get; set; }
 
         /// <summary>
         /// The amount of people who have favourited this beatmap set.
         /// </summary>
-        [JsonProperty(@"favourite_count")]
         public int FavouriteCount { get; set; }
     }
 
@@ -60,17 +60,14 @@ namespace osu.Game.Beatmaps
     {
         public string CoverLowRes { get; set; }
 
-        [JsonProperty(@"cover@2x")]
         public string Cover { get; set; }
 
         public string CardLowRes { get; set; }
 
-        [JsonProperty(@"card@2x")]
         public string Card { get; set; }
 
         public string ListLowRes { get; set; }
 
-        [JsonProperty(@"list@2x")]
         public string List { get; set; }
     }
 }
