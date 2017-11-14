fe1d6f928f52e30e6dfd9bbf87b6e6624a0ccbf4
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-11-14 19:03:50 +0900

    Move common width definition to base class

--- osu.Game/Overlays/Direct/DirectGridPanel.cs
+++ osu.Game/Overlays/Direct/DirectGridPanel.cs
@@ -29,6 +29,7 @@ namespace osu.Game.Overlays.Direct
 
         public DirectGridPanel(BeatmapSetInfo beatmap) : base(beatmap)
         {
+            Width = 400;
             Height = 140 + vertical_padding; //full height of all the elements plus vertical padding (autosize uses the image)
         }
 
