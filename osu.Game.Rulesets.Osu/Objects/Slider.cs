a341eed428b5f04daf3e2db5e43805f60db5b10b
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2017-11-17 21:28:59 +0900

    Consider slider lengths as part of Distance

--- osu.Game.Rulesets.Osu/Objects/Slider.cs
+++ osu.Game.Rulesets.Osu/Objects/Slider.cs
@@ -46,10 +46,16 @@ namespace osu.Game.Rulesets.Osu.Objects
         }
 
         /// <summary>
-        /// The position of the cursor at the point of completion of this <see cref="OsuHitObject"/>.
-        /// This is set and used by difficulty calculation.
+        /// The position of the cursor at the point of completion of this <see cref="Slider"/> if it was hit
+        /// with as few movements as possible. This is set and used by difficulty calculation.
         /// </summary>
-        internal Vector2? CursorPosition;
+        internal Vector2? LazyEndPosition;
+
+        /// <summary>
+        /// The distance travelled by the cursor upon completion of this <see cref="Slider"/> if it was hit
+        /// with as few movements as possible. This is set and used by difficulty calculation.
+        /// </summary>
+        internal float LazyTravelDistance;
 
         public List<SampleInfoList> RepeatSamples { get; set; } = new List<SampleInfoList>();
         public int RepeatCount { get; set; } = 1;
