fe7f9cccaa0520791ff08064fd5a851b0eaf423d
jorolf <jorolf@gmx.de>
2017-11-19 14:16:00 +0100

    BeatmapSetCover can display other types of covers now

--- osu.Game/Beatmaps/Drawables/BeatmapSetCover.cs
+++ osu.Game/Beatmaps/Drawables/BeatmapSetCover.cs
@@ -11,21 +11,44 @@ namespace osu.Game.Beatmaps.Drawables
     public class BeatmapSetCover : Sprite
     {
         private readonly BeatmapSetInfo set;
-        public BeatmapSetCover(BeatmapSetInfo set)
+        private readonly BeatmapSetCoverType type;
+
+        public BeatmapSetCover(BeatmapSetInfo set, BeatmapSetCoverType type = BeatmapSetCoverType.Cover)
         {
             if (set == null)
                 throw new ArgumentNullException(nameof(set));
 
             this.set = set;
+            this.type = type;
         }
 
         [BackgroundDependencyLoader]
         private void load(TextureStore textures)
         {
-            string resource = set.OnlineInfo.Covers.Cover;
+            string resource = null;
+
+            switch (type)
+            {
+                case BeatmapSetCoverType.Cover:
+                    resource = set.OnlineInfo.Covers.Cover;
+                    break;
+                case BeatmapSetCoverType.Card:
+                    resource = set.OnlineInfo.Covers.Card;
+                    break;
+                case BeatmapSetCoverType.List:
+                    resource = set.OnlineInfo.Covers.List;
+                    break;
+            }
 
             if (resource != null)
                 Texture = textures.Get(resource);
         }
+    }
+
+    public enum BeatmapSetCoverType
+    {
+        Cover,
+        Card,
+        List,
     }
 }
