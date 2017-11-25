a626913e999245d532dc772533938a2e166b6a53
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-11-26 02:41:18 +0900

    Add hover/click sound effects to more stuff everywhere

--- osu.Game/Graphics/UserInterface/OsuButton.cs
+++ osu.Game/Graphics/UserInterface/OsuButton.cs
@@ -1,13 +1,7 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.ComponentModel;
-using osu.Framework.Allocation;
-using osu.Framework.Audio;
-using osu.Framework.Audio.Sample;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Input;
 
 namespace osu.Game.Graphics.UserInterface
 {
@@ -16,38 +10,9 @@ namespace osu.Game.Graphics.UserInterface
     /// </summary>
     public class OsuButton : Button
     {
-        private SampleChannel sampleClick;
-        private SampleChannel sampleHover;
-
-        protected HoverSampleSet SampleSet = HoverSampleSet.Normal;
-
-        protected override bool OnClick(InputState state)
+        public OsuButton()
         {
-            sampleClick?.Play();
-            return base.OnClick(state);
-        }
-
-        protected override bool OnHover(InputState state)
-        {
-            sampleHover?.Play();
-            return base.OnHover(state);
-        }
-
-        [BackgroundDependencyLoader]
-        private void load(AudioManager audio)
-        {
-            sampleClick = audio.Sample.Get($@"UI/generic-select{SampleSet.GetDescription()}");
-            sampleHover = audio.Sample.Get($@"UI/generic-hover{SampleSet.GetDescription()}");
-        }
-
-        public enum HoverSampleSet
-        {
-            [Description("")]
-            Normal,
-            [Description("-soft")]
-            Soft,
-            [Description("-softer")]
-            Softer
+            Add(new HoverClickSounds(HoverSampleSet.Loud));
         }
     }
 }
