dfb5f116a1bc3e81c6d8d4fddb6317fefe812738
Miterosan <miterosan@gmail.com>
2017-11-25 23:03:03 +0100

    adjust VersionManager to notificationIcon

--- osu.Desktop/Overlays/VersionManager.cs
+++ osu.Desktop/Overlays/VersionManager.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using osu.Framework.Allocation;
 using osu.Framework.Development;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
@@ -28,6 +29,7 @@ namespace osu.Desktop.Overlays
         private NotificationOverlay notificationOverlay;
         private OsuConfigManager config;
         private OsuGameBase game;
+        private OsuColour osuColours;
 
         public override bool HandleInput => false;
 
@@ -37,6 +39,7 @@ namespace osu.Desktop.Overlays
             notificationOverlay = notification;
             this.config = config;
             this.game = game;
+            osuColours = colours;
 
             AutoSizeAxes = Axes.Both;
             Anchor = Anchor.BottomCentre;
@@ -102,25 +105,24 @@ namespace osu.Desktop.Overlays
 
             var version = game.Version;
             var lastVersion = config.Get<string>(OsuSetting.Version);
-            if (game.IsDeployedBuild && version != lastVersion)
-            {
-                config.Set(OsuSetting.Version, version);
+            if (!game.IsDeployedBuild || version == lastVersion) return;
+            config.Set(OsuSetting.Version, version);
 
-
-
-                // only show a notificationDrawable if we've previously saved a version to the config file (ie. not the first run).
-                if (!string.IsNullOrEmpty(lastVersion))
-                    Scheduler.AddDelayed(() => notificationOverlay.Post(
-                        new Notification(
+            // only show a notificationDrawable if we've previously saved a version to the config file (ie. not the first run).
+            if (!string.IsNullOrEmpty(lastVersion))
+                Scheduler.AddDelayed(() =>
+                    notificationOverlay.Post(
+                            new Notification(
                             $"You are now running osu!lazer {version}.\nClick to see what's new!",
-                            FontAwesome.fa_check_square,
-                            () =>
+                            onActivate: () =>
                             {
                                 Process.Start($"https://github.com/ppy/osu/releases/tag/v{version}");
                             })
-
-                    ), 5000);
-            }
+                        {
+                            NotificationIcon = new NotificationIcon(FontAwesome.fa_check_square, osuColours.BlueDark)
+                        }),
+                    5000
+                );
         }
 
         protected override void Dispose(bool isDisposing)
@@ -145,40 +147,42 @@ namespace osu.Desktop.Overlays
 
                 if (notificationContainer == null)
                 {
-                    notificationContainer = new ProgressNotification("", FontAwesome.fa_upload);
-                    notificationContainer.ProgressCompleted += () =>
+                    notificationContainer = new ProgressNotification("", FontAwesome.fa_upload)
                     {
-                        var readyNotification = new Notification(@"Update ready to install. Click to restart!");
-                        readyNotification.OnActivate += () =>
+                        FollowUpNotifications =
                         {
-                            // Squirrel returns execution to us after the update process is started, so it's safe to use Wait() here
-                            UpdateManager.RestartAppWhenExited().Wait();
-                            game.Exit();
-                        };
-
-                        Schedule(() => notificationOverlay.Post(readyNotification));
+                            new Notification(@"Update ready to install. Click to restart!", onActivate: () =>
+                            {
+                                // Squirrel returns execution to us after the update process is started, so it's safe to use Wait() here
+                                UpdateManager.RestartAppWhenExited().Wait();
+                                game.Exit();
+                            })
+                            {
+                                NotificationIcon = new NotificationIcon(backgroundColour: osuColours.Green)
+                            }
+                        },
+                        NotificationIcon = new NotificationIcon(
+                            FontAwesome.fa_upload,
+                            ColourInfo.GradientVertical(osuColours.YellowDark, osuColours.Yellow)
+                        )
                     };
 
-                    Schedule(() => notificationOverlay.Post(notificationContainer));
+                    notificationOverlay.Post(notificationContainer);
                 }
 
-                Schedule(() =>
-                {
-                    notificationContainer.Progress = 0;
-                    notificationContainer.Text = @"Downloading update...";
-                });
+
+                notificationContainer.Progress = 0;
+                notificationContainer.Text = @"Downloading update...";
 
                 try
                 {
-                    await updateManager.DownloadReleases(info.ReleasesToApply, p => Schedule(() => notificationContainer.Progress = p / 100f));
+                    await updateManager.DownloadReleases(info.ReleasesToApply, p => notificationContainer.Progress = p / 100f);
 
-                    Schedule(() =>
-                    {
-                        notificationContainer.Progress = 0;
-                        notificationContainer.Text = @"Installing update...";
-                    });
+                    notificationContainer.Progress = 0;
+                    notificationContainer.State = ProgressNotificationState.Active;
+                    notificationContainer.Text = @"Installing update...";
 
-                    await updateManager.ApplyReleases(info, p => Schedule(() => notificationContainer.Progress = p / 100f));
+                    await updateManager.ApplyReleases(info, p => notificationContainer.Progress = p / 100f);
 
                     Schedule(() => notificationContainer.State = ProgressNotificationState.Completed);
                 }
