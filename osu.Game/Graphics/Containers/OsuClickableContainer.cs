671b3d01ff83d9550fcf1b65cfcccdf5d352181d
Dean Herbert <pe@ppy.sh>
2017-11-26 04:59:55 +0900

    Fix OsuClickableContainer's local content geting overwritten

--- osu.Game/Graphics/Containers/OsuClickableContainer.cs
+++ osu.Game/Graphics/Containers/OsuClickableContainer.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
 
@@ -11,6 +12,10 @@ namespace osu.Game.Graphics.Containers
     {
         private readonly HoverSampleSet sampleSet;
 
+        private readonly Container content = new Container { RelativeSizeAxes = Axes.Both };
+
+        protected override Container<Drawable> Content => content;
+
         public OsuClickableContainer(HoverSampleSet sampleSet = HoverSampleSet.Normal)
         {
             this.sampleSet = sampleSet;
@@ -19,7 +24,17 @@ namespace osu.Game.Graphics.Containers
         [BackgroundDependencyLoader]
         private void load()
         {
-            AddInternal(new HoverClickSounds(sampleSet));
+            if (AutoSizeAxes != Axes.None)
+            {
+                content.RelativeSizeAxes = RelativeSizeAxes;
+                content.AutoSizeAxes = AutoSizeAxes;
+            }
+
+            InternalChildren = new Drawable[]
+            {
+                content,
+                new HoverClickSounds(sampleSet)
+            };
         }
     }
 }
