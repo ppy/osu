f6a33b3ea2c4a78c899303ef51911bf507937090
Unknown <aergwyn@t-online.de>
2017-11-25 14:45:57 +0100

    fix osu!direct download reloading all panels
    This fixes the recreation of all panels when a download completes.
    Also fixes NullReference when you download without ever opening the details of one Set.

--- osu.Game/Overlays/DirectOverlay.cs
+++ osu.Game/Overlays/DirectOverlay.cs
@@ -65,8 +65,6 @@ namespace osu.Game.Overlays
                 }
 
                 ResultAmounts = new ResultCounts(distinctCount(artists), distinctCount(songs), distinctCount(tags));
-
-                recreatePanels(Filter.DisplayStyleControl.DisplayStyle.Value);
             }
         }
 
@@ -282,7 +280,11 @@ namespace osu.Game.Overlays
                     var sets = response.Select(r => r.ToBeatmapSet(rulesets)).Where(b => !presentOnlineIds.Contains(b.OnlineBeatmapSetID)).ToList();
 
                     // may not need scheduling; loads async internally.
-                    Schedule(() => BeatmapSets = sets);
+                    Schedule(() =>
+                    {
+                        BeatmapSets = sets;
+                        recreatePanels(Filter.DisplayStyleControl.DisplayStyle.Value);
+                    });
                 });
             };
 
