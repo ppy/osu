7ceedced98908ea7b0bda77b77f8c257d05e5eb3
jorolf <87902114+bubb13gummy@users.noreply.github.com>
2017-11-26 21:52:35 +0100

    move shared stuff between MostPlayedBeatmapDrawable and DrawableScore to DrawableBeatmapRow

--- osu.Game/Overlays/Profile/Sections/Ranks/DrawableScore.cs
+++ osu.Game/Overlays/Profile/Sections/Ranks/DrawableScore.cs
@@ -11,24 +11,14 @@ using osu.Game.Rulesets.Mods;
 using osu.Game.Screens.Select.Leaderboards;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.UI;
-using OpenTK.Graphics;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Input;
-using osu.Framework.Extensions.Color4Extensions;
 
 namespace osu.Game.Overlays.Profile.Sections.Ranks
 {
-    public abstract class DrawableScore : Container
+    public abstract class DrawableScore : DrawableBeatmapRow
     {
-        private const int fade_duration = 200;
-
-        protected readonly FillFlowContainer<OsuSpriteText> Stats;
         private readonly FillFlowContainer metadata;
         private readonly ScoreModsContainer modsContainer;
         protected readonly Score Score;
-        private readonly Box underscoreLine;
-        private readonly Box coloredBackground;
-        private readonly Container background;
 
         protected DrawableScore(Score score)
         {
@@ -38,85 +28,21 @@ namespace osu.Game.Overlays.Profile.Sections.Ranks
             Height = 60;
             Children = new Drawable[]
             {
-                background = new Container
+                modsContainer = new ScoreModsContainer
                 {
-                    RelativeSizeAxes = Axes.Both,
-                    Masking = true,
-                    CornerRadius = 3,
-                    Alpha = 0,
-                    EdgeEffect = new EdgeEffectParameters
-                    {
-                        Type = EdgeEffectType.Shadow,
-                        Offset = new Vector2(0f, 1f),
-                        Radius = 1f,
-                        Colour = Color4.Black.Opacity(0.2f),
-                    },
-                    Child = coloredBackground = new Box { RelativeSizeAxes = Axes.Both }
-                },
-                new Container
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.TopCentre,
-                    Width = 0.97f,
-                    Children = new Drawable[]
-                    {
-                        underscoreLine = new Box
-                        {
-                            Anchor = Anchor.BottomCentre,
-                            Origin = Anchor.BottomCentre,
-                            RelativeSizeAxes = Axes.X,
-                            Height = 1,
-                        },
-                        new DrawableRank(score.Rank)
-                        {
-                            RelativeSizeAxes = Axes.Y,
-                            Width = 60,
-                            FillMode = FillMode.Fit,
-                        },
-                        Stats = new FillFlowContainer<OsuSpriteText>
-                        {
-                            RelativeSizeAxes = Axes.X,
-                            AutoSizeAxes = Axes.Y,
-                            Anchor = Anchor.CentreRight,
-                            Origin = Anchor.CentreRight,
-                            Direction = FillDirection.Vertical,
-                        },
-                        metadata = new FillFlowContainer
-                        {
-                            RelativeSizeAxes = Axes.X,
-                            AutoSizeAxes = Axes.Y,
-                            Anchor = Anchor.CentreLeft,
-                            Origin = Anchor.CentreLeft,
-                            Margin = new MarginPadding { Left = 70 },
-                            Direction = FillDirection.Vertical,
-                            Child = new OsuSpriteText
-                            {
-                                Text = score.Date.LocalDateTime.ToShortDateString(),
-                                TextSize = 11,
-                                Colour = OsuColour.Gray(0xAA),
-                                Depth = -1,
-                            },
-                        },
-                        modsContainer = new ScoreModsContainer
-                        {
-                            AutoSizeAxes = Axes.Y,
-                            Anchor = Anchor.CentreRight,
-                            Origin = Anchor.CentreRight,
-                            Width = 60,
-                            Margin = new MarginPadding { Right = 160 }
-                        }
-                    }
-                },
+                    AutoSizeAxes = Axes.Y,
+                    Anchor = Anchor.CentreRight,
+                    Origin = Anchor.CentreRight,
+                    Width = 60,
+                    Margin = new MarginPadding { Right = 160 }
+                }
             };
         }
 
         [BackgroundDependencyLoader(true)]
         private void load(OsuColour colour)
         {
-            coloredBackground.Colour = underscoreLine.Colour = colour.Gray4;
-
-            Stats.Add(new OsuSpriteText
+            RightFlowContainer.Add(new OsuSpriteText
             {
                 Text = $"accuracy: {Score.Accuracy:P2}",
                 Anchor = Anchor.TopRight,
@@ -127,26 +53,23 @@ namespace osu.Game.Overlays.Profile.Sections.Ranks
                 Depth = -1,
             });
 
-            metadata.Add(new BeatmapMetadataContainer(Score.Beatmap));
+            LeftFlowContainer.Add(new BeatmapMetadataContainer(Score.Beatmap));
+            LeftFlowContainer.Add(new OsuSpriteText
+            {
+                Text = Score.Date.LocalDateTime.ToShortDateString(),
+                TextSize = 11,
+                Colour = OsuColour.Gray(0xAA),
+            });
 
             foreach (Mod mod in Score.Mods)
                 modsContainer.Add(new ModIcon(mod) { Scale = new Vector2(0.5f) });
         }
 
-        protected override bool OnClick(InputState state) => true;
-
-        protected override bool OnHover(InputState state)
+        protected override Drawable CreatePicture() => new DrawableRank(Score.Rank)
         {
-            background.FadeIn(fade_duration, Easing.OutQuint);
-            underscoreLine.FadeOut(fade_duration, Easing.OutQuint);
-            return true;
-        }
-
-        protected override void OnHoverLost(InputState state)
-        {
-            background.FadeOut(fade_duration, Easing.OutQuint);
-            underscoreLine.FadeIn(fade_duration, Easing.OutQuint);
-            base.OnHoverLost(state);
-        }
+            RelativeSizeAxes = Axes.Y,
+            Width = 60,
+            FillMode = FillMode.Fit,
+        };
     }
 }
