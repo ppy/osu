c058065a3ab78258ec24ef90a33738a4a8b1af69
Aergwyn <aergwyn@t-online.de>
2017-11-27 20:24:01 +0100

    remove unnecessary using

--- osu.Game/Beatmaps/BeatmapStore.cs
+++ osu.Game/Beatmaps/BeatmapStore.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using osu.Game.Database;
