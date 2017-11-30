fe5742b8ac55d1c13df2a3885fa171ede058d6ca
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2017-11-30 21:56:12 +0900

    Add overlay/underlay

---
+++ osu.Game/Rulesets/Edit/PlayfieldUnderlay.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.Edit
+{
+    public class PlayfieldUnderlay : CompositeDrawable
+    {
+        private readonly Playfield playfield;
+
+        public PlayfieldUnderlay(Playfield playfield)
+        {
+            this.playfield = playfield;
+
+            RelativeSizeAxes = Axes.Both;
+        }
+    }
+}
