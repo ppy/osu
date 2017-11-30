456bbe25f38e34253cf77b9057e20022add7e9e5
smoogipoo <smoogipoo@smgi.me>
2017-11-30 16:58:14 +0900

    Implement toolbox into HitObjectComposer

--- osu.Game/Rulesets/Edit/HitObjectComposer.cs
+++ osu.Game/Rulesets/Edit/HitObjectComposer.cs
@@ -1,9 +1,20 @@
 // Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Logging;
+using osu.Framework.Timing;
+using osu.Game.Rulesets.Edit.Tools;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.UI;
+using osu.Game.Screens.Edit.Screens.Compose.RadioButtons;
+using osu.Game.Screens.Play.ReplaySettings;
 
 namespace osu.Game.Rulesets.Edit
 {
@@ -21,11 +32,63 @@ namespace osu.Game.Rulesets.Edit
         [BackgroundDependencyLoader]
         private void load(OsuGameBase osuGame)
         {
+            RulesetContainer rulesetContainer;
             try
             {
-                InternalChild = ruleset.CreateRulesetContainerWith(osuGame.Beatmap.Value, true);
+                rulesetContainer = ruleset.CreateRulesetContainerWith(osuGame.Beatmap.Value, true);
             }
-            catch { }
+            catch (Exception e)
+            {
+                Logger.Log($"Could not load this beatmap sucessfully ({e})!", LoggingTarget.Runtime, LogLevel.Error);
+                return;
+            }
+
+            RadioButtonCollection toolboxCollection;
+            InternalChild = new GridContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Content = new[]
+                {
+                    new Drawable[]
+                    {
+                        new FillFlowContainer
+                        {
+                            Name = "Sidebar",
+                            RelativeSizeAxes = Axes.Both,
+                            Padding = new MarginPadding { Right = 10 },
+                            Children = new Drawable[]
+                            {
+                                new ToolboxGroup { Child = toolboxCollection = new RadioButtonCollection { RelativeSizeAxes = Axes.X } }
+                            }
+                        },
+                        new Container
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Masking = true,
+                            Child = rulesetContainer
+                        }
+                    },
+                },
+                ColumnDimensions = new[]
+                {
+                    new Dimension(GridSizeMode.Absolute, 200),
+                }
+            };
+
+            rulesetContainer.Clock = new InterpolatingFramedClock((IAdjustableClock)osuGame.Beatmap.Value.Track ?? new StopwatchClock());
+
+            toolboxCollection.Items =
+                new[] { new RadioButton("Select", () => setCompositionTool(new SelectionTool())) }
+                .Concat(
+                    CompositionTools.Select(t => new RadioButton(t.Name, () => setCompositionTool(t)))
+                )
+                .ToList();
         }
+
+        private void setCompositionTool(ICompositionTool tool)
+        {
+        }
+
+        protected abstract IReadOnlyList<ICompositionTool> CompositionTools { get; }
     }
 }
