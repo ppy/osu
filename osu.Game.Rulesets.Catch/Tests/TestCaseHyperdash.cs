5ee21f9f4bf4fe290b9a566b7e4e13aba3fc3d5d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-12-01 19:33:20 +0900

    Make hyperdash testcase easier to win

--- osu.Game.Rulesets.Catch/Tests/TestCaseHyperdash.cs
+++ osu.Game.Rulesets.Catch/Tests/TestCaseHyperdash.cs
@@ -18,7 +18,7 @@ namespace osu.Game.Rulesets.Catch.Tests
             var beatmap = new Beatmap();
 
             for (int i = 0; i < 512; i++)
-                beatmap.HitObjects.Add(new Fruit { X = 0.5f + (i % 8 < 4 ? -0.4f : 0.4f), StartTime = i * 100, NewCombo = i % 8 == 0 });
+                beatmap.HitObjects.Add(new Fruit { X = i % 8 < 4 ? 0.02f : 0.98f, StartTime = i * 100, NewCombo = i % 8 == 0 });
 
             return beatmap;
         }
