a80059032cdc94a37a83b78fd86b4466be095c61
smoogipoo <smoogipoo@smgi.me>
2017-12-02 20:24:10 +0900

    Remove PlayfieldOverlay

--- osu.Game/Rulesets/Edit/HitObjectComposer.cs
+++ osu.Game/Rulesets/Edit/HitObjectComposer.cs
@@ -77,8 +77,7 @@ namespace osu.Game.Rulesets.Edit
                                     Alpha = 0,
                                     AlwaysPresent = true,
                                 },
-                                rulesetContainer,
-                                CreateOverlay(rulesetContainer.Playfield)
+                                rulesetContainer
                             }
                         }
                     },
@@ -104,8 +103,6 @@ namespace osu.Game.Rulesets.Edit
         private void setCompositionTool(ICompositionTool tool) => CurrentTool = tool;
 
         protected virtual RulesetContainer CreateRulesetContainer(Ruleset ruleset, WorkingBeatmap beatmap) => ruleset.CreateRulesetContainerWith(beatmap, true);
-
-        protected virtual PlayfieldOverlay CreateOverlay(Playfield playfield) => new PlayfieldOverlay();
 
         protected abstract IReadOnlyList<ICompositionTool> CompositionTools { get; }
     }
