b04ddba2a0b6062b90e5bc45dc7eb52a075d2c20
FreezyLemon <freezestyleman@gmx.de>
2017-12-06 10:27:30 +0100

    Added basic "on click" actions to the in-chat links

--- osu.Game/Online/API/Requests/GetUserRequest.cs
+++ osu.Game/Online/API/Requests/GetUserRequest.cs
@@ -8,12 +8,21 @@ namespace osu.Game.Online.API.Requests
     public class GetUserRequest : APIRequest<User>
     {
         private long? userId;
+        private string userName;
 
+        /// <param name="userId">The user's ID.</param>
         public GetUserRequest(long? userId = null)
         {
             this.userId = userId;
         }
 
-        protected override string Target => userId.HasValue ? $@"users/{userId}" : @"me";
+        /// <param name="userName">The user's username.</param>
+        public GetUserRequest(string userName)
+        {
+            this.userName = userName;
+        }
+
+        // Prefer ID over name
+        protected override string Target => userId.HasValue ? $@"users/{userId}" : ((!string.IsNullOrEmpty(userName)) ? $@"users/{userName}" : @"me");
     }
 }
