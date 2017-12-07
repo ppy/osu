9f4ca93fad5fe5b4053210c0ee200118a6471513
FreezyLemon <87902114+bubb13gummy@users.noreply.github.com>
2017-12-07 23:09:51 +0100

    Added basic SampleInfo.FromSoundPoint and GetChannel methods

--- osu.Game/Audio/SampleInfo.cs
+++ osu.Game/Audio/SampleInfo.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
+using osu.Framework.Configuration;
+using osu.Game.Beatmaps.ControlPoints;
+
 namespace osu.Game.Audio
 {
     public class SampleInfo
@@ -9,6 +14,24 @@ namespace osu.Game.Audio
         public const string HIT_FINISH = @"hitfinish";
         public const string HIT_NORMAL = @"hitnormal";
         public const string HIT_CLAP = @"hitclap";
+
+        public static SampleInfo FromSoundPoint(SoundControlPoint soundPoint, string sampleName = SampleInfo.HIT_NORMAL)
+        {
+            return new SampleInfo()
+            {
+                Bank = soundPoint.SampleBank,
+                Name = sampleName,
+                Volume = soundPoint.SampleVolume,
+            };
+        }
+
+        public SampleChannel GetChannel(SampleManager manager)
+        {
+            var channel = manager.Get($"{Bank}-{Name}");
+
+            channel.AddAdjustment(AdjustableProperty.Volume, new BindableDouble(Volume / 100.0));
+            return channel;
+        }
 
         /// <summary>
         /// The bank to load the sample from.
