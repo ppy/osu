e1c04a1f445933c1deb21b26a4bc39e190eb130c
FreezyLemon <freezestyleman@gmx.de>
2017-12-08 12:50:04 +0100

    Added check for "menu music beatmap hash" before undeleting so circles.osu doesn't get imported on Undelete. Also moved the const property to BeatmapManager.

--- osu.Game/Beatmaps/BeatmapManager.cs
+++ osu.Game/Beatmaps/BeatmapManager.cs
@@ -35,6 +35,11 @@ namespace osu.Game.Beatmaps
     public class BeatmapManager
     {
         /// <summary>
+        /// The hash of the supplied menu music's beatmap set.
+        /// </summary>
+        public const string MENU_MUSIC_BEATMAP_HASH = "3c8b1fcc9434dbb29e2fb613d3b9eada9d7bb6c125ceb32396c3b53437280c83";
+
+        /// <summary>
         /// Fired when a new <see cref="BeatmapSetInfo"/> becomes available in the database.
         /// </summary>
         public event Action<BeatmapSetInfo> BeatmapSetAdded;
@@ -341,6 +346,10 @@ namespace osu.Game.Beatmaps
 
         public void Undelete(BeatmapSetInfo beatmapSet)
         {
+            // So circles.osz doesn't get added as a map
+            if (beatmapSet.Hash == MENU_MUSIC_BEATMAP_HASH)
+                return;
+
             lock (importContext)
             {
                 var context = importContext.Value;
