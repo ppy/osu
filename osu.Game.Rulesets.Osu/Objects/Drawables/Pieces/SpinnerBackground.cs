ed81ed7c5431776f84dced2e97e89aaf8c05e7e4
smoogipoo <smoogipoo@smgi.me>
2017-12-11 15:19:00 +0900

    Add back HandleInput disable overrides

--- osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/SpinnerBackground.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/SpinnerBackground.cs
@@ -11,6 +11,8 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables.Pieces
 {
     public class SpinnerBackground : CircularContainer, IHasAccentColour
     {
+        public override bool HandleInput => false;
+
         protected Box Disc;
 
         public Color4 AccentColour
--- osu.Game/Screens/Menu/MenuSideFlashes.cs
+++ osu.Game/Screens/Menu/MenuSideFlashes.cs
@@ -19,6 +19,8 @@ namespace osu.Game.Screens.Menu
 {
     public class MenuSideFlashes : BeatSyncedContainer
     {
+        public override bool HandleInput => false;
+
         private readonly Bindable<WorkingBeatmap> beatmap = new Bindable<WorkingBeatmap>();
 
         private readonly Box leftBox;
