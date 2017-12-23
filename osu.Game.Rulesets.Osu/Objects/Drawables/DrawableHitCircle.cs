4fa44c258fdc93b6abbe24aa47cda6fa465029b1
Aergwyn <aergwyn@t-online.de>
2017-12-23 10:43:32 +0100

    fix weirding out ApproachCircles
    they don't like going fast

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableHitCircle.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableHitCircle.cs
@@ -89,8 +89,8 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
 
             if (!Hidden)
             {
-                ApproachCircle.FadeIn(Math.Min(TIME_FADEIN * 2, TIME_PREEMPT) * FadeInSpeedMultiplier);
-                ApproachCircle.ScaleTo(1.1f, TIME_PREEMPT * FadeInSpeedMultiplier);
+                ApproachCircle.FadeIn(Math.Min(TIME_FADEIN * 2, TIME_PREEMPT));
+                ApproachCircle.ScaleTo(1.1f, TIME_PREEMPT);
             }
         }
 
