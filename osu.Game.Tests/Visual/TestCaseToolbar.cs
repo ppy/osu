a46437a7b227714549e88234915c4f385296a9df
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2017-12-26 01:12:46 +0900

    Make toolbar testable and add the most basic of visual tests

---
+++ osu.Game.Tests/Visual/TestCaseToolbar.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Overlays.Toolbar;
+
+namespace osu.Game.Tests.Visual
+{
+    public class TestCaseToolbar : OsuTestCase
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(ToolbarButton),
+            typeof(ToolbarModeSelector),
+            typeof(ToolbarModeButton),
+        };
+
+        public TestCaseToolbar()
+        {
+            Add(new Toolbar { State = Visibility.Visible });
+        }
+    }
+}
