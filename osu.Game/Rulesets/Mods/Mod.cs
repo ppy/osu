d9a80dae5de1f8e13c7677c6015b50fb01297852
Dean Herbert <pe@ppy.sh>
2017-12-31 04:10:25 +0900

    Male NoFail use an applicable interface

--- osu.Game/Rulesets/Mods/Mod.cs
+++ osu.Game/Rulesets/Mods/Mod.cs
@@ -55,10 +55,5 @@ namespace osu.Game.Rulesets.Mods
         /// The mods this mod cannot be enabled with.
         /// </summary>
         public virtual Type[] IncompatibleMods => new Type[] { };
-
-        /// <summary>
-        /// Whether we should allow failing at the current point in time.
-        /// </summary>
-        public virtual bool AllowFail => true;
     }
 }
