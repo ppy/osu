481a16b491ce9b451fccfe7b58578c562b1b35d2
Aergwyn <aergwyn@t-online.de>
2018-01-01 11:55:24 +0100

    extended hitobject tests

---
+++ osu.Game.Rulesets.Osu/Tests/TestCaseHitCircleHidden.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    public class TestCaseHitCircleHidden : TestCaseHitCircle
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(HitCircle),
+            typeof(OsuModHidden),
+            typeof(DrawableHitCircle)
+        };
+
+        public TestCaseHitCircleHidden()
+        {
+            Mods.Add(new OsuModHidden());
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Osu/Tests/TestCaseSliderHidden.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    public class TestCaseSliderHidden : TestCaseSlider
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(Slider),
+            typeof(HitCircle),
+            typeof(SliderTick),
+            typeof(RepeatPoint),
+            typeof(OsuModHidden),
+            typeof(DrawableSlider),
+            typeof(DrawableHitCircle),
+            typeof(DrawableSliderTick),
+            typeof(DrawableRepeatPoint)
+        };
+
+        public TestCaseSliderHidden()
+        {
+            Mods.Add(new OsuModHidden());
+        }
+    }
+}
