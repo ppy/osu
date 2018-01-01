f764e071131f449e589aaf9e47bec661fec9e135
Aergwyn <87902114+bubb13gummy@users.noreply.github.com>
2018-01-01 11:55:24 +0100

    extended hitobject tests

---
+++ osu.Game.Rulesets.Osu/Tests/TestCaseSpinnerHidden.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    public class TestCaseSpinnerHidden : TestCaseSpinner
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(Spinner),
+            typeof(OsuModHidden),
+            typeof(DrawableSpinner)
+        };
+
+        public TestCaseSpinnerHidden()
+        {
+            Mods.Add(new OsuModHidden());
+        }
+    }
+}
