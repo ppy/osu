f419518887b2d840cb06f80f04bc2393985fc903
Dean Herbert <pe@ppy.sh>
2018-01-02 15:33:00 +0900

    Make comment xmldoc

--- osu.Game/Rulesets/Mods/IApplicableMod.cs
+++ osu.Game/Rulesets/Mods/IApplicableMod.cs
@@ -3,7 +3,10 @@
 
 namespace osu.Game.Rulesets.Mods
 {
-    // The base interface for a mod which can be applied in some way.
+    /// <summary>
+    /// The base interface for a mod which can be applied in some way.
+    /// If this is not implemented by a mod, it will not be available for use in-game.
+    /// </summary>
     public interface IApplicableMod
     {
     }
