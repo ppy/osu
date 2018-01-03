46a9f9115c7c7502dbf2c981babfe92df1dcec53
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-01-03 21:26:36 +0900

    Implement interface on ManiaKeyMod for now

--- osu.Game.Rulesets.Mania/Mods/ManiaMod.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaMod.cs
@@ -103,7 +103,7 @@ namespace osu.Game.Rulesets.Mania.Mods
         }
     }
 
-    public abstract class ManiaKeyMod : Mod
+    public abstract class ManiaKeyMod : Mod, IApplicableToBeatmapConverter<ManiaHitObject>
     {
         // TODO: implement using the IApplicable interface. Haven't done so yet because KeyCount isn't even hooked up at the moment.
 
@@ -111,6 +111,11 @@ namespace osu.Game.Rulesets.Mania.Mods
         public abstract int KeyCount { get; }
         public override double ScoreMultiplier => 1; // TODO: Implement the mania key mod score multiplier
         public override bool Ranked => true;
+
+        public void ApplyToBeatmapConverter(BeatmapConverter<ManiaHitObject> beatmapConverter)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class ManiaModKey1 : ManiaKeyMod
