597e9f20c69ea0a41ad01d835c4facbd927c853f
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-03 20:52:01 +0900

    Move combo colouring to test case

--- osu.Game.Rulesets.Catch/Tests/TestCaseFruitObjects.cs
+++ osu.Game.Rulesets.Catch/Tests/TestCaseFruitObjects.cs
@@ -5,11 +5,13 @@ using System;
 using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.MathUtils;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.Objects.Drawable;
 using osu.Game.Rulesets.Catch.Objects.Drawable.Pieces;
 using osu.Game.Tests.Visual;
 using OpenTK;
+using OpenTK.Graphics;
 
 namespace osu.Game.Rulesets.Catch.Tests
 {
@@ -49,19 +51,52 @@ namespace osu.Game.Rulesets.Catch.Tests
             });
         }
 
-        private DrawableFruit createDrawable(int index) => new DrawableFruit(new Fruit
+        private DrawableFruit createDrawable(int index)
         {
-            StartTime = 1000000000000,
-            IndexInBeatmap = index,
-            Scale = 1.5f,
-        })
+            var fruit = new Fruit
+            {
+                StartTime = 1000000000000,
+                IndexInBeatmap = index,
+                Scale = 1.5f,
+            };
+
+            fruit.ComboColour = colourForRrepesentation(fruit.VisualRepresentation);
+
+            return new DrawableFruit(fruit)
+            {
+                Anchor = Anchor.Centre,
+                RelativePositionAxes = Axes.Both,
+                Position = Vector2.Zero,
+                Alpha = 1,
+                LifetimeStart = double.NegativeInfinity,
+                LifetimeEnd = double.PositiveInfinity,
+            };
+        }
+
+        private Color4 colourForRrepesentation(FruitVisualRepresentation representation)
         {
-            Anchor = Anchor.Centre,
-            RelativePositionAxes = Axes.Both,
-            Position = Vector2.Zero,
-            Alpha = 1,
-            LifetimeStart = double.NegativeInfinity,
-            LifetimeEnd = double.PositiveInfinity,
-        };
+            switch (representation)
+            {
+                default:
+                case FruitVisualRepresentation.Triforce:
+                    return new Color4(17, 136, 170, 255);
+                case FruitVisualRepresentation.Grape:
+                    return new Color4(204, 102, 0, 255);
+                case FruitVisualRepresentation.DPad:
+                    return new Color4(121, 9, 13, 255);
+                case FruitVisualRepresentation.Pineapple:
+                    return new Color4(102, 136, 0, 255);
+                case FruitVisualRepresentation.Banana:
+                    switch (RNG.Next(0, 3))
+                    {
+                        default:
+                            return new Color4(255, 240, 0, 255);
+                        case 1:
+                            return new Color4(255, 192, 0, 255);
+                        case 2:
+                            return new Color4(214, 221, 28, 255);
+                    }
+            }
+        }
     }
 }
