2b79ad879f7df53567eec7845b89aaeea46ce076
smoogipoo <smoogipoo@smgi.me>
2018-01-04 16:37:48 +0900

    Add a way to access alive hitobjects

--- osu.Game/Rulesets/UI/Playfield.cs
+++ osu.Game/Rulesets/UI/Playfield.cs
@@ -97,6 +97,8 @@ namespace osu.Game.Rulesets.UI
         public class HitObjectContainer : CompositeDrawable
         {
             public virtual IEnumerable<DrawableHitObject> Objects => InternalChildren.Cast<DrawableHitObject>();
+            public virtual IEnumerable<DrawableHitObject> AliveObjects => AliveInternalChildren.Cast<DrawableHitObject>();
+
             public virtual void Add(DrawableHitObject hitObject) => AddInternal(hitObject);
             public virtual bool Remove(DrawableHitObject hitObject) => RemoveInternal(hitObject);
         }
