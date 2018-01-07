0e708cd657fc8c54c62717384b2b567235e7bac8
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-01-07 12:47:09 +0900

    Split out scrolling algorithm

---
+++ osu.Game/Rulesets/UI/Scrolling/Algorithms/IScrollingAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Objects.Drawables;
+using OpenTK;
+
+namespace osu.Game.Rulesets.UI.Scrolling.Algorithms
+{
+    public interface IScrollingAlgorithm
+    {
+        void ComputeInitialStates(IEnumerable<DrawableHitObject> hitObjects, ScrollingDirection direction, double timeRange, Vector2 length);
+        void ComputePositions(IEnumerable<DrawableHitObject> hitObjects, ScrollingDirection direction, double currentTime, double timeRange, Vector2 length);
+    }
+}
