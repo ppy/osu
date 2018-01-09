60f2f38ade99f6bb462099b3f9b0fd7a4f6f465e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-09 12:44:52 +0900

    Improve variable names

--- osu.Game.Tests/Visual/TestCaseBeatmapCarousel.cs
+++ osu.Game.Tests/Visual/TestCaseBeatmapCarousel.cs
@@ -237,7 +237,7 @@ namespace osu.Game.Tests.Visual
             nextRandom();
             AddAssert("ensure repeat", () => selectedSets.Contains(carousel.SelectedBeatmapSet));
 
-            AddStep("Add set with 100 difficulties", () => carousel.UpdateBeatmapSet(createTestBeatmapSetWith100Difficulties(set_count + 1)));
+            AddStep("Add set with 100 difficulties", () => carousel.UpdateBeatmapSet(createTestBeatmapSetWithManyDifficulties(set_count + 1)));
             AddStep("Filter Extra", () => carousel.Filter(new FilterCriteria { SearchText = "Extra 10" }, false));
             checkInvisibleDifficultiesUnselectable();
             checkInvisibleDifficultiesUnselectable();
@@ -353,26 +353,26 @@ namespace osu.Game.Tests.Visual
             }
         }
 
-        private BeatmapSetInfo createTestBeatmapSet(int i)
+        private BeatmapSetInfo createTestBeatmapSet(int id)
         {
             return new BeatmapSetInfo
             {
-                ID = i,
-                OnlineBeatmapSetID = i,
+                ID = id,
+                OnlineBeatmapSetID = id,
                 Hash = new MemoryStream(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())).ComputeMD5Hash(),
                 Metadata = new BeatmapMetadata
                 {
-                    OnlineBeatmapSetID = i,
+                    OnlineBeatmapSetID = id,
                     // Create random metadata, then we can check if sorting works based on these
-                    Artist = $"peppy{i.ToString().PadLeft(6, '0')}",
-                    Title = $"test set #{i}!",
-                    AuthorString = string.Concat(Enumerable.Repeat((char)('z' - Math.Min(25, i - 1)), 5))
+                    Artist = $"peppy{id.ToString().PadLeft(6, '0')}",
+                    Title = $"test set #{id}!",
+                    AuthorString = string.Concat(Enumerable.Repeat((char)('z' - Math.Min(25, id - 1)), 5))
                 },
                 Beatmaps = new List<BeatmapInfo>(new[]
                 {
                     new BeatmapInfo
                     {
-                        OnlineBeatmapID = i * 10,
+                        OnlineBeatmapID = id * 10,
                         Path = "normal.osu",
                         Version = "Normal",
                         StarDifficulty = 2,
@@ -383,7 +383,7 @@ namespace osu.Game.Tests.Visual
                     },
                     new BeatmapInfo
                     {
-                        OnlineBeatmapID = i * 10 + 1,
+                        OnlineBeatmapID = id * 10 + 1,
                         Path = "hard.osu",
                         Version = "Hard",
                         StarDifficulty = 5,
@@ -394,7 +394,7 @@ namespace osu.Game.Tests.Visual
                     },
                     new BeatmapInfo
                     {
-                        OnlineBeatmapID = i * 10 + 2,
+                        OnlineBeatmapID = id * 10 + 2,
                         Path = "insane.osu",
                         Version = "Insane",
                         StarDifficulty = 6,
@@ -407,20 +407,20 @@ namespace osu.Game.Tests.Visual
             };
         }
 
-        private BeatmapSetInfo createTestBeatmapSetWith100Difficulties(int i)
+        private BeatmapSetInfo createTestBeatmapSetWithManyDifficulties(int id)
         {
             var toReturn = new BeatmapSetInfo
             {
-                ID = i,
-                OnlineBeatmapSetID = i,
+                ID = id,
+                OnlineBeatmapSetID = id,
                 Hash = new MemoryStream(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())).ComputeMD5Hash(),
                 Metadata = new BeatmapMetadata
                 {
-                    OnlineBeatmapSetID = i,
+                    OnlineBeatmapSetID = id,
                     // Create random metadata, then we can check if sorting works based on these
-                    Artist = $"peppy{i.ToString().PadLeft(6, '0')}",
-                    Title = $"test set #{i}!",
-                    AuthorString = string.Concat(Enumerable.Repeat((char)('z' - Math.Min(25, i - 1)), 5))
+                    Artist = $"peppy{id.ToString().PadLeft(6, '0')}",
+                    Title = $"test set #{id}!",
+                    AuthorString = string.Concat(Enumerable.Repeat((char)('z' - Math.Min(25, id - 1)), 5))
                 },
                 Beatmaps = new List<BeatmapInfo>(),
             };
