164bd940adefe19404274e12270cde4ab173cb69
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-09 12:43:10 +0900

    Avoid unnecessary use of linq

--- osu.Game/Screens/Select/BeatmapCarousel.cs
+++ osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -267,7 +267,7 @@ namespace osu.Game.Screens.Select
                 set = visibleSets.ElementAt(RNG.Next(visibleSets.Count));
 
             var visibleBeatmaps = set.Beatmaps.Where(s => !s.Filtered).ToList();
-            select(visibleBeatmaps.Skip(RNG.Next(visibleBeatmaps.Count)).FirstOrDefault());
+            select(visibleBeatmaps[RNG.Next(visibleBeatmaps.Count)]);
             return true;
         }
 
