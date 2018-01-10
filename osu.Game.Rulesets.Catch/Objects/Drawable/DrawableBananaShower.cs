a36cfd426532ed148cbcd0088f8babf89d6ea49e
Dean Herbert <pe@ppy.sh>
2018-01-10 14:52:22 +0900

    Add BananaShower models and representations
    (cherry picked from commit e12e095)

---
+++ osu.Game.Rulesets.Catch/Objects/Drawable/DrawableBananaShower.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Linq;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using OpenTK;
+using osu.Game.Rulesets.Objects.Drawables;
+
+namespace osu.Game.Rulesets.Catch.Objects.Drawable
+{
+    public class DrawableBananaShower : DrawableCatchHitObject<BananaShower>
+    {
+        private readonly Container dropletContainer;
+
+        public DrawableBananaShower(BananaShower s) : base(s)
+        {
+            RelativeSizeAxes = Axes.Both;
+            Height = (float)HitObject.Duration;
+            X = 0;
+
+            Child = dropletContainer = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                RelativeChildOffset = new Vector2(0, (float)HitObject.StartTime),
+                RelativeChildSize = new Vector2(1, (float)HitObject.Duration)
+            };
+
+            foreach (var b in s.NestedHitObjects.OfType<BananaShower.Banana>())
+                    AddNested(new DrawableFruit(b));
+        }
+
+        protected override void AddNested(DrawableHitObject<CatchHitObject> h)
+        {
+            ((DrawableCatchHitObject)h).CheckPosition = o => CheckPosition?.Invoke(o) ?? false;
+            dropletContainer.Add(h);
+            base.AddNested(h);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Catch/Tests/TestCaseBananaShower.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Catch.Objects;
+
+namespace osu.Game.Rulesets.Catch.Tests
+{
+    [TestFixture]
+    [Ignore("getting CI working")]
+    public class TestCaseBananaShower : Game.Tests.Visual.TestCasePlayer
+    {
+        public TestCaseBananaShower()
+            : base(typeof(CatchRuleset))
+        {
+        }
+
+        protected override Beatmap CreateBeatmap()
+        {
+            var beatmap = new Beatmap
+            {
+                BeatmapInfo = new BeatmapInfo
+                {
+                    BaseDifficulty = new BeatmapDifficulty
+                    {
+                        CircleSize = 6,
+                    }
+                }
+            };
+
+            for (int i = 0; i < 10; i++)
+                beatmap.HitObjects.Add(new BananaShower { StartTime = i * 1200, Duration = 1000, NewCombo = i % 2 == 0 });
+
+            return beatmap;
+        }
+    }
+}
