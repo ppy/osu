4b2d971b005bf6e82d84d4969b248bbf55ef32b8
smoogipoo <smoogipoo@smgi.me>
2018-01-12 13:03:47 +0900

    Add some comments

--- osu.Game/Rulesets/UI/Scrolling/Algorithms/IScrollingAlgorithm.cs
+++ osu.Game/Rulesets/UI/Scrolling/Algorithms/IScrollingAlgorithm.cs
@@ -9,7 +9,25 @@ namespace osu.Game.Rulesets.UI.Scrolling.Algorithms
 {
     public interface IScrollingAlgorithm
     {
+        /// <summary>
+        /// Computes the states of <see cref="DrawableHitObject"/>s that are constant, such as lifetime and spatial length.
+        /// This is invoked once whenever <paramref name="timeRange"/> or <paramref name="length"/> changes.
+        /// </summary>
+        /// <param name="hitObjects">The <see cref="DrawableHitObject"/>s whose states should be computed.</param>
+        /// <param name="direction">The scrolling direction.</param>
+        /// <param name="timeRange">The duration required to scroll through one length of the screen before any control point adjustments.</param>
+        /// <param name="length">The length of the screen that is scrolled through.</param>
         void ComputeInitialStates(IEnumerable<DrawableHitObject> hitObjects, ScrollingDirection direction, double timeRange, Vector2 length);
+
+        /// <summary>
+        /// Computes the states of <see cref="DrawableHitObject"/>s that change depending on <paramref name="currentTime"/>, such as position.
+        /// This is invoked once per frame.
+        /// </summary>
+        /// <param name="hitObjects">The <see cref="DrawableHitObject"/>s whose states should be computed.</param>
+        /// <param name="direction">The scrolling direction.</param>
+        /// <param name="currentTime">The current time.</param>
+        /// <param name="timeRange">The duration required to scroll through one length of the screen before any control point adjustments.</param>
+        /// <param name="length">The length of the screen that is scrolled through.</param>
         void ComputePositions(IEnumerable<DrawableHitObject> hitObjects, ScrollingDirection direction, double currentTime, double timeRange, Vector2 length);
     }
 }
