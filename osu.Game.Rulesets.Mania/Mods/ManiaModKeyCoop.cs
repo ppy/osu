e7143cf8dac1bf088a1668bc55c64e09e7aeb66b
ANDY840119-PC\andy840119 <87902114+bubb13gummy@users.noreply.github.com>
2018-01-14 21:35:35 +0900

    implement co-op mod

--- osu.Game.Rulesets.Mania/Mods/ManiaModKeyCoop.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModKeyCoop.cs
@@ -1,16 +1,40 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.Collections.Generic;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Mania.Beatmaps;
+using osu.Game.Rulesets.Mania.Objects;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Mania.Mods
 {
-    public class ManiaModKeyCoop : Mod
+    public class ManiaModKeyCoop : Mod, IApplicableToBeatmapConverter<ManiaHitObject>
     {
         public override string Name => "KeyCoop";
         public override string ShortenedName => "2P";
         public override string Description => @"Double the key amount, double the fun!";
         public override double ScoreMultiplier => 1;
         public override bool Ranked => true;
+
+        public void ApplyToBeatmapConverter(BeatmapConverter<ManiaHitObject> beatmapConverter)
+        {
+            var mbc = (ManiaBeatmapConverter)beatmapConverter;
+
+            // Although this can work, for now let's not allow keymods for mania-specific beatmaps
+            if (mbc.IsForCurrentRuleset)
+                return;
+
+            int originTargetColumns = mbc.TargetColumns;
+
+            var newStages = new List<StageDefinition>()
+            {
+                new StageDefinition() { Columns = originTargetColumns },
+                new StageDefinition() { Columns = originTargetColumns },
+            };
+
+            mbc.StageDefinitions = newStages;
+            mbc.TargetColumns = originTargetColumns * 2;
+        }
     }
 }
