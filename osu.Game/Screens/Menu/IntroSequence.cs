13a897b88f651a9eb88ec7768ed447406b113fe5
TocoToucan <87902114+bubb13gummy@users.noreply.github.com>
2018-01-15 21:42:17 +0300

    Use local functions

--- osu.Game/Screens/Menu/IntroSequence.cs
+++ osu.Game/Screens/Menu/IntroSequence.cs
@@ -188,7 +188,7 @@ namespace osu.Game.Screens.Menu
             mediumRing.ResizeTo(130, 340, Easing.OutQuad);
             mediumRing.Foreground.ResizeTo(1, 880, Easing.Out);
 
-            Func<double> remainingTime = () => length - TransformDelay;
+            double remainingTime() => length - TransformDelay;
 
             using (BeginDelayedSequence(250, true))
             {
