9f4ebad6e3c05f25aced5dc2d20538e92466903f
smoogipoo <smoogipoo@smgi.me>
2018-01-17 20:10:35 +0900

    Add mania config manager

---
+++ osu.Game.Rulesets.Mania/Configuration/ManiaConfigManager.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Platform;
+using osu.Game.Rulesets.Configuration;
+
+namespace osu.Game.Rulesets.Mania.Configuration
+{
+    public class ManiaConfigManager : RulesetConfigManager<ManiaSetting>
+    {
+        public ManiaConfigManager(Ruleset ruleset, Storage storage)
+            : base(ruleset, storage)
+        {
+        }
+    }
+
+    public enum ManiaSetting
+    {
+        ScrollSpeed
+    }
+}
