6c7730d7edbab7fffa5aed14c9519b42aa2281bf
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-01-18 16:57:32 +0900

    Remove generics from OSD  registration methods

--- osu.Game/Rulesets/Configuration/IRulesetConfigManager.cs
+++ osu.Game/Rulesets/Configuration/IRulesetConfigManager.cs
@@ -1,9 +1,12 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Configuration;
+using osu.Framework.Configuration.Tracking;
+
 namespace osu.Game.Rulesets.Configuration
 {
-    public interface IRulesetConfigManager
+    public interface IRulesetConfigManager : ITrackableConfigManager, IConfigManager
     {
     }
 }
