89f4bfa7b540d8bf24909bc56498c476de446ddf
smoogipoo <smoogipoo@smgi.me>
2018-01-18 17:00:41 +0900

    Track mania scroll speed

--- osu.Game.Rulesets.Mania/Configuration/ManiaConfigManager.cs
+++ osu.Game.Rulesets.Mania/Configuration/ManiaConfigManager.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Configuration.Tracking;
 using osu.Framework.Platform;
 using osu.Game.Rulesets.Configuration;
 
@@ -19,6 +20,11 @@ namespace osu.Game.Rulesets.Mania.Configuration
 
             Set(ManiaSetting.ScrollTime, 1500.0, 50.0, 10000.0, 50.0);
         }
+
+        public override TrackedSettings CreateTrackedSettings() => new TrackedSettings
+        {
+            new TrackedSetting<double>(ManiaSetting.ScrollTime, v => new SettingDescription(v, "Scroll Time", $"{v}ms"))
+        };
     }
 
     public enum ManiaSetting
