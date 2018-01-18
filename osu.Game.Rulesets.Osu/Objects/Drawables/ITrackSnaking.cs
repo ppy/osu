4be0a0d9e4541ba7ff75ab95e14204c1c2f68507
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-18 21:06:47 +0900

    Fix repeat points not following slider snaking correctly

---
+++ osu.Game.Rulesets.Osu/Objects/Drawables/ITrackSnaking.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK;
+
+namespace osu.Game.Rulesets.Osu.Objects.Drawables
+{
+    /// <summary>
+    /// A component which tracks the current end snaking position of a slider.
+    /// </summary>
+    public interface ITrackSnaking
+    {
+        void UpdateSnakingPosition(Vector2 start, Vector2 end);
+    }
+}
