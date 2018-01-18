56619ae926e180bb408a01cabcfe28551236e4de
Aergwyn <aergwyn@t-online.de>
2018-01-18 15:53:53 +0100

    use ternary expression

--- osu.Game/Overlays/BeatmapSet/PreviewButton.cs
+++ osu.Game/Overlays/BeatmapSet/PreviewButton.cs
@@ -15,7 +15,6 @@ using OpenTK;
 using OpenTK.Graphics;
 using osu.Game.Overlays.Direct;
 using osu.Framework.Configuration;
-using System;
 
 namespace osu.Game.Overlays.BeatmapSet
 {
@@ -84,7 +83,7 @@ namespace osu.Game.Overlays.BeatmapSet
             if (Playing.Value && preview != null)
             {
                 // prevent negative (potential infinite) width if a track without length was loaded
-                progress.Width = (float)Math.Max(preview.CurrentTime / preview.Length, 0);
+                progress.Width = preview.Length > 0 ? (float)(preview.CurrentTime / preview.Length) : 0f;
             }
         }
 
