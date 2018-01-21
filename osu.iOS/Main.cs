bf28c9b559997ea83b8e6ad43f126991ec794970
smoogipoo <smoogipoo@smgi.me>
2018-01-21 14:51:35 +0900

    osu.iOS changes in-line with framework changes

--- osu.iOS/Main.cs
+++ osu.iOS/Main.cs
@@ -9,8 +9,6 @@ namespace osu.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            SQLitePCL.Batteries_V2.Init();
-
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
---
+++ osu.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+extern alias IOS;
+
+using IOS::Foundation;
+
+[assembly: Preserve]
