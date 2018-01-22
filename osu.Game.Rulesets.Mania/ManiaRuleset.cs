71596f35bc62c008ae432c1241fda4f8748b2919
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-01-22 15:03:05 +0900

    Privatise VariantMappingGenerator to ManiaRuleset

--- osu.Game.Rulesets.Mania/ManiaRuleset.cs
+++ osu.Game.Rulesets.Mania/ManiaRuleset.cs
@@ -242,58 +242,58 @@ namespace osu.Game.Rulesets.Mania
         {
             return (PlayfieldType)Enum.GetValues(typeof(PlayfieldType)).Cast<int>().OrderByDescending(i => i).First(v => variant >= v);
         }
-    }
 
-    public class VariantMappingGenerator
-    {
-        /// <summary>
-        /// All the <see cref="InputKey"/>s available to the left hand.
-        /// </summary>
-        public InputKey[] LeftKeys;
-
-        /// <summary>
-        /// All the <see cref="InputKey"/>s available to the right hand.
-        /// </summary>
-        public InputKey[] RightKeys;
-
-        /// <summary>
-        /// The <see cref="InputKey"/> for the special key.
-        /// </summary>
-        public InputKey SpecialKey;
-
-        /// <summary>
-        /// The <see cref="ManiaAction"/> at which the normal columns should begin.
-        /// </summary>
-        public ManiaAction NormalActionStart;
-
-        /// <summary>
-        /// The <see cref="ManiaAction"/> for the special column.
-        /// </summary>
-        public ManiaAction SpecialAction;
-
-        /// <summary>
-        /// Generates a list of <see cref="KeyBinding"/>s for a specific number of columns.
-        /// </summary>
-        /// <param name="columns">The number of columns that need to be bound.</param>
-        /// <param name="nextNormalAction">The next <see cref="ManiaAction"/> to use for normal columns.</param>
-        /// <returns>The keybindings.</returns>
-        public IEnumerable<KeyBinding> GenerateKeyBindingsFor(int columns, out ManiaAction nextNormalAction)
+        private class VariantMappingGenerator
         {
-            ManiaAction currentNormalAction = NormalActionStart;
+            /// <summary>
+            /// All the <see cref="InputKey"/>s available to the left hand.
+            /// </summary>
+            public InputKey[] LeftKeys;
 
-            var bindings = new List<KeyBinding>();
+            /// <summary>
+            /// All the <see cref="InputKey"/>s available to the right hand.
+            /// </summary>
+            public InputKey[] RightKeys;
 
-            for (int i = LeftKeys.Length - columns / 2; i < LeftKeys.Length; i++)
-                bindings.Add(new KeyBinding(LeftKeys[i], currentNormalAction++));
+            /// <summary>
+            /// The <see cref="InputKey"/> for the special key.
+            /// </summary>
+            public InputKey SpecialKey;
 
-            for (int i = 0; i < columns / 2; i++)
-                bindings.Add(new KeyBinding(RightKeys[i], currentNormalAction++));
+            /// <summary>
+            /// The <see cref="ManiaAction"/> at which the normal columns should begin.
+            /// </summary>
+            public ManiaAction NormalActionStart;
 
-            if (columns % 2 == 1)
-                bindings.Add(new KeyBinding(SpecialKey, SpecialAction));
+            /// <summary>
+            /// The <see cref="ManiaAction"/> for the special column.
+            /// </summary>
+            public ManiaAction SpecialAction;
 
-            nextNormalAction = currentNormalAction;
-            return bindings;
+            /// <summary>
+            /// Generates a list of <see cref="KeyBinding"/>s for a specific number of columns.
+            /// </summary>
+            /// <param name="columns">The number of columns that need to be bound.</param>
+            /// <param name="nextNormalAction">The next <see cref="ManiaAction"/> to use for normal columns.</param>
+            /// <returns>The keybindings.</returns>
+            public IEnumerable<KeyBinding> GenerateKeyBindingsFor(int columns, out ManiaAction nextNormalAction)
+            {
+                ManiaAction currentNormalAction = NormalActionStart;
+
+                var bindings = new List<KeyBinding>();
+
+                for (int i = LeftKeys.Length - columns / 2; i < LeftKeys.Length; i++)
+                    bindings.Add(new KeyBinding(LeftKeys[i], currentNormalAction++));
+
+                for (int i = 0; i < columns / 2; i++)
+                    bindings.Add(new KeyBinding(RightKeys[i], currentNormalAction++));
+
+                if (columns % 2 == 1)
+                    bindings.Add(new KeyBinding(SpecialKey, SpecialAction));
+
+                nextNormalAction = currentNormalAction;
+                return bindings;
+            }
         }
     }
 
