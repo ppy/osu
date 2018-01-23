fa1f4304f6e7d9bdd1b3a84be3a4fc102ccc8a01
Dean Herbert <pe@ppy.sh>
2018-01-23 13:24:04 +0900

    Remove usings

--- osu.Game/Screens/Play/HotkeyRetryOverlay.cs
+++ osu.Game/Screens/Play/HotkeyRetryOverlay.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Framework.Input;
-using OpenTK.Input;
 using osu.Framework.Allocation;
 using System;
 using osu.Framework.Graphics.Containers;
