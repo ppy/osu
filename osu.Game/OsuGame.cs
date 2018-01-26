c65c0cf401bd493aaafaa638d5a1ff970bda3d06
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-26 19:30:29 +0900

    Remember mod selection when re-entering song select
    Removes mod application when exiting back to main menu.
    
    Alternative to #1968.
    Closes #1961.

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
+using System.Collections.Generic;
 using osu.Framework.Configuration;
 using osu.Framework.Screens;
 using osu.Game.Configuration;
@@ -27,6 +28,7 @@ using osu.Game.Overlays.Notifications;
 using osu.Game.Rulesets;
 using osu.Game.Screens.Play;
 using osu.Game.Input.Bindings;
+using osu.Game.Rulesets.Mods;
 using OpenTK.Graphics;
 
 namespace osu.Game
@@ -79,6 +81,9 @@ namespace osu.Game
         private readonly string[] args;
 
         private SettingsOverlay settings;
+
+        // todo: move this to SongSelect once Screen has the ability to unsuspend.
+        public readonly Bindable<IEnumerable<Mod>> SelectedMods = new Bindable<IEnumerable<Mod>>(new List<Mod>());
 
         public OsuGame(string[] args = null)
         {
