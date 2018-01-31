fa4883ecbd275466e9a45bf0f220da5022c2c5f7
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-01-31 18:11:38 +0900

    Add the ability to duck volume when the game is inactive

--- osu.Game/Overlays/Settings/Sections/Audio/VolumeSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Audio/VolumeSettings.cs
@@ -4,6 +4,7 @@
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Graphics;
+using osu.Game.Configuration;
 
 namespace osu.Game.Overlays.Settings.Sections.Audio
 {
@@ -12,11 +13,12 @@ namespace osu.Game.Overlays.Settings.Sections.Audio
         protected override string Header => "Volume";
 
         [BackgroundDependencyLoader]
-        private void load(AudioManager audio)
+        private void load(AudioManager audio, OsuConfigManager config)
         {
             Children = new Drawable[]
             {
                 new SettingsSlider<double> { LabelText = "Master", Bindable = audio.Volume, KeyboardStep = 0.1f },
+                new SettingsSlider<double> { LabelText = "Master (Window Inactive)", Bindable = config.GetBindable<double>(OsuSetting.VolumeInactive), KeyboardStep = 0.1f },
                 new SettingsSlider<double> { LabelText = "Effect", Bindable = audio.VolumeSample, KeyboardStep = 0.1f },
                 new SettingsSlider<double> { LabelText = "Music", Bindable = audio.VolumeTrack, KeyboardStep = 0.1f },
             };
