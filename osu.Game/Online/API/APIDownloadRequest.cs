47c346067e2bd0aae11eccd2e90b5eee4ac55e19
Dean Herbert <pe@ppy.sh>
2018-02-14 20:26:49 +0900

    Move import logic to shared implementation

---
+++ osu.Game/Online/API/APIDownloadRequest.cs
@@ -0,0 +1,30 @@
+﻿using osu.Framework.IO.Network;
+
+namespace osu.Game.Online.API
+{
+    public abstract class APIDownloadRequest : APIRequest
+    {
+        protected override WebRequest CreateWebRequest()
+        {
+            var request = new WebRequest(Uri);
+            request.DownloadProgress += request_Progress;
+            return request;
+        }
+
+        private void request_Progress(long current, long total) => API.Scheduler.Add(delegate { Progress?.Invoke(current, total); });
+
+        protected APIDownloadRequest()
+        {
+            base.Success += onSuccess;
+        }
+
+        private void onSuccess()
+        {
+            Success?.Invoke(WebRequest.ResponseData);
+        }
+
+        public event APIProgressHandler Progress;
+
+        public new event APISuccessHandler<byte[]> Success;
+    }
+}
