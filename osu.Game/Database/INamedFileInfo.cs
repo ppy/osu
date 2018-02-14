e0d28564d0d69b4132f6dea94f1e2a162d18c2e4
Dean Herbert <pe@ppy.sh>
2018-02-14 20:26:49 +0900

    Move import logic to shared implementation

---
+++ osu.Game/Database/INamedFileInfo.cs
@@ -0,0 +1,13 @@
+﻿using osu.Game.IO;
+
+namespace osu.Game.Database
+{
+    /// <summary>
+    /// Represent a join model which gives a filename and scope to a <see cref="FileInfo"/>.
+    /// </summary>
+    public interface INamedFileInfo
+    {
+        FileInfo FileInfo { get; set; }
+        string Filename { get; set; }
+    }
+}
