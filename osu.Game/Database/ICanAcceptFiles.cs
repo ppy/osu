fe5df663be1c648eb4c0c41398d447141e8eaa04
Dean Herbert <pe@ppy.sh>
2018-02-15 15:14:46 +0900

    Add more xmldoc

--- osu.Game/Database/ICanAcceptFiles.cs
+++ osu.Game/Database/ICanAcceptFiles.cs
@@ -1,9 +1,19 @@
 ﻿namespace osu.Game.Database
 {
+    /// <summary>
+    /// A class which can accept files for importing.
+    /// </summary>
     public interface ICanAcceptFiles
     {
+        /// <summary>
+        /// Import the specified paths.
+        /// </summary>
+        /// <param name="paths">The files which should be imported.</param>
         void Import(params string[] paths);
 
+        /// <summary>
+        /// An array of accepted file extensions (in the standard format of ".abc").
+        /// </summary>
         string[] HandledExtensions { get; }
     }
 }
