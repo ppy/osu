8e46be557e0b0149eaf704af9bcb3a571b326983
Dean Herbert <pe@ppy.sh>
2018-02-15 13:46:37 +0900

    Add default skin and basic component structure

---
+++ osu.Game/Skinning/LegacySkin.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Linq;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
+
+namespace osu.Game.Skinning
+{
+    public class LegacySkin : Skin
+    {
+        private readonly TextureStore textures;
+
+        public LegacySkin(string name, Storage storage)
+            : base(name)
+        {
+            textures = new TextureStore(new RawTextureLoaderStore(new StorageBackedResourceStore(storage.GetStorageForDirectory($"skins/{name}"))));
+        }
+
+        public override Drawable GetComponent(string componentName)
+        {
+            var legacyComponentName = componentName.Split('/').Last();
+
+            var texture = textures.Get(legacyComponentName);
+            if (texture == null) return null;
+
+            return new Sprite
+            {
+                RelativeSizeAxes = Axes.Both,
+                FillMode = FillMode.Fit,
+                Texture = textures.Get(legacyComponentName),
+            };
+        }
+    }
+}
