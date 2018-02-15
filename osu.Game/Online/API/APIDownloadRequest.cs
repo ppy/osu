aec5a432c21300725cc3d30db106d779bcf3c014
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-02-15 16:33:33 +0900

    Add missing licence headers

--- osu.Game/Online/API/APIDownloadRequest.cs
+++ osu.Game/Online/API/APIDownloadRequest.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.IO.Network;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.IO.Network;
 
 namespace osu.Game.Online.API
 {
