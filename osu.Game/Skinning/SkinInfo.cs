ba311a5304a2e5af26e845b0453bdacd447cbdea
Dean Herbert <pe@ppy.sh>
2018-02-15 13:45:39 +0900

    Add basic skin database model layout and importing

---
+++ osu.Game/Skinning/SkinInfo.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using osu.Game.Database;
+
+namespace osu.Game.Skinning
+{
+    public class SkinInfo : IHasFiles<SkinFileInfo>, IHasPrimaryKey, ISoftDelete
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Creator { get; set; }
+
+        public List<SkinFileInfo> Files { get; set; }
+
+        public bool DeletePending { get; set; }
+    }
+}
