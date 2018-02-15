38efa659518518c9c27db463a738f793cf004ca7
Dean Herbert <pe@ppy.sh>
2018-02-15 13:46:37 +0900

    Add default skin and basic component structure

---
+++ osu.Game/Skinning/Skin.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+
+namespace osu.Game.Skinning
+{
+    public abstract class Skin
+    {
+        public string Name { get; }
+
+        public abstract Drawable GetComponent(string componentName);
+
+        protected Skin(string name)
+        {
+            Name = name;
+        }
+    }
+}
