e4b1ea614660c3262640b4796c1c746a4bf0e483
Dean Herbert <pe@ppy.sh>
2018-02-19 15:36:36 +0900

    Add method to get all usable skins

--- osu.Game/Skinning/SkinManager.cs
+++ osu.Game/Skinning/SkinManager.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Configuration;
 using osu.Framework.Platform;
 using osu.Game.Database;
@@ -13,6 +15,12 @@ namespace osu.Game.Skinning
         public Bindable<Skin> CurrentSkin = new Bindable<Skin>(new DefaultSkin());
 
         public override string[] HandledExtensions => new[] { ".osk" };
+
+        /// <summary>
+        /// Returns a list of all usable <see cref="SkinInfo"/>s.
+        /// </summary>
+        /// <returns>A list of available <see cref="SkinInfo"/>.</returns>
+        public List<SkinInfo> GetAllUsableSkins() => ModelStore.ConsumableItems.Where(s => !s.DeletePending).ToList();
 
         protected override SkinInfo CreateModel(ArchiveReader archive) => new SkinInfo
         {
