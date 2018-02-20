9a9f53ddfd76731d76462bdcc287190749667a8e
smoogipoo <smoogipoo@smgi.me>
2018-02-20 14:13:52 +0900

    Remove 0.75 scale from osu! playfield in the editor

--- osu.Game.Rulesets.Osu/Edit/OsuEditRulesetContainer.cs
+++ osu.Game.Rulesets.Osu/Edit/OsuEditRulesetContainer.cs
@@ -5,6 +5,7 @@ using osu.Framework.Graphics.Cursor;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.UI;
+using OpenTK;
 
 namespace osu.Game.Rulesets.Osu.Edit
 {
@@ -16,6 +17,12 @@ namespace osu.Game.Rulesets.Osu.Edit
         }
 
         protected override Playfield CreatePlayfield() => new OsuEditPlayfield();
+
+        protected override Vector2 GetAspectAdjustedSize()
+        {
+            var aspectSize = DrawSize.X * 0.75f < DrawSize.Y ? new Vector2(DrawSize.X, DrawSize.X * 0.75f) : new Vector2(DrawSize.Y * 4f / 3f, DrawSize.Y);
+            return new Vector2(aspectSize.X / DrawSize.X, aspectSize.Y / DrawSize.Y);
+        }
 
         protected override CursorContainer CreateCursor() => null;
     }
