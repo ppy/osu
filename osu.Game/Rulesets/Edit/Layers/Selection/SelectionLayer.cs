995ecb7d94fcfee4bc916e6d491137e1999ade61
smoogipoo <smoogipoo@smgi.me>
2018-02-20 14:48:35 +0900

    Make selectedHitObjects into a HashSet for quick Contains checks

--- osu.Game/Rulesets/Edit/Layers/Selection/SelectionLayer.cs
+++ osu.Game/Rulesets/Edit/Layers/Selection/SelectionLayer.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
@@ -27,7 +28,7 @@ namespace osu.Game.Rulesets.Edit.Layers.Selection
         private SelectionBox selectionBox;
         private CaptureBox captureBox;
 
-        private readonly List<DrawableHitObject> selectedHitObjects = new List<DrawableHitObject>();
+        private readonly HashSet<DrawableHitObject> selectedHitObjects = new HashSet<DrawableHitObject>();
 
         protected override bool OnMouseDown(InputState state, MouseDownEventArgs args)
         {
