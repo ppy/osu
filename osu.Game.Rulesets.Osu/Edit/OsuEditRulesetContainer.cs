bbee35ae248a60c68b6d5b385cf44ca11cf1d40c
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-02-21 19:52:36 +0900

    Add a separate property to control playfield area

--- osu.Game.Rulesets.Osu/Edit/OsuEditRulesetContainer.cs
+++ osu.Game.Rulesets.Osu/Edit/OsuEditRulesetContainer.cs
@@ -18,11 +18,7 @@ namespace osu.Game.Rulesets.Osu.Edit
 
         protected override Playfield CreatePlayfield() => new OsuEditPlayfield();
 
-        protected override Vector2 GetAspectAdjustedSize()
-        {
-            var aspectSize = DrawSize.X * 0.75f < DrawSize.Y ? new Vector2(DrawSize.X, DrawSize.X * 0.75f) : new Vector2(DrawSize.Y * 4f / 3f, DrawSize.Y);
-            return new Vector2(aspectSize.X / DrawSize.X, aspectSize.Y / DrawSize.Y);
-        }
+        protected override Vector2 PlayfieldArea => Vector2.One;
 
         protected override CursorContainer CreateCursor() => null;
     }
