f9cb68b0bb5906706f6de938e3de89151fb9f4e4
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-02-22 17:16:48 +0900

    Allow instantiation of skins

---
+++ osu.Game/Skinning/DefaultSkin.cs
@@ -0,0 +1,17 @@
+﻿using osu.Framework.Audio.Sample;
+using osu.Framework.Graphics;
+
+namespace osu.Game.Skinning
+{
+    public class DefaultSkin : Skin
+    {
+        public DefaultSkin()
+            : base(SkinInfo.Default)
+        {
+        }
+
+        public override Drawable GetDrawableComponent(string componentName) => null;
+
+        public override SampleChannel GetSample(string sampleName) => null;
+    }
+}
