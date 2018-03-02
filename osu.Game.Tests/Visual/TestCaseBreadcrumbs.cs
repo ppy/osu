3d5fefae8c192b33bfba3e79aae68579de76ea43
smoogipoo <smoogipoo@smgi.me>
2018-03-02 15:34:31 +0900

    Add TestFixtureAttribute to all tests

--- osu.Game.Tests/Visual/TestCaseBreadcrumbs.cs
+++ osu.Game.Tests/Visual/TestCaseBreadcrumbs.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Tests.Visual
 {
+    [TestFixture]
     public class TestCaseBreadcrumbs : OsuTestCase
     {
         public TestCaseBreadcrumbs()
--- osu.Game.Tests/Visual/TestCaseRoomInspector.cs
+++ osu.Game.Tests/Visual/TestCaseRoomInspector.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
@@ -11,6 +12,7 @@ using osu.Game.Users;
 
 namespace osu.Game.Tests.Visual
 {
+    [TestFixture]
     public class TestCaseRoomInspector : OsuTestCase
     {
         private RulesetStore rulesets;
