c6c55c40edf51afef87b5b1531e7da02ebccdfaa
Dean Herbert <pe@ppy.sh>
2018-03-04 00:50:41 +0900

    Rewrite BreakOverlay
    No longer relies on Schedule calls (could not be rewound).
    Also no longer sucks.

--- osu.Game/Screens/Play/Break/RemainingTimeCounter.cs
+++ osu.Game/Screens/Play/Break/RemainingTimeCounter.cs
@@ -1,18 +1,15 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Game.Graphics.Sprites;
-using osu.Framework.Graphics;
 using System;
-using osu.Game.Beatmaps.Timing;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.UserInterface;
+using osu.Game.Graphics.Sprites;
 
-namespace osu.Game.Screens.Play.BreaksOverlay
+namespace osu.Game.Screens.Play.Break
 {
     public class RemainingTimeCounter : Counter
     {
-        private const double fade_duration = BreakPeriod.MIN_BREAK_DURATION / 2;
-
         private readonly OsuSpriteText counter;
 
         public RemainingTimeCounter()
@@ -25,13 +22,8 @@ namespace osu.Game.Screens.Play.BreaksOverlay
                 TextSize = 33,
                 Font = "Venera",
             };
-
-            Alpha = 0;
         }
 
         protected override void OnCountChanged(double count) => counter.Text = ((int)Math.Ceiling(count / 1000)).ToString();
-
-        public override void Show() => this.FadeIn(fade_duration);
-        public override void Hide() => this.FadeOut(fade_duration);
     }
 }
