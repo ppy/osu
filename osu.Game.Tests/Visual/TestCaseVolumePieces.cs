d1f2393f069580243f3d0b9a77c4fc3f7a1816e1
jorolf <87902114+bubb13gummy@users.noreply.github.com>
2018-03-04 14:03:53 +0100

    change TestCase to OsuTestCase

--- osu.Game.Tests/Visual/TestCaseVolumePieces.cs
+++ osu.Game.Tests/Visual/TestCaseVolumePieces.cs
@@ -4,13 +4,12 @@
 using System;
 using System.Collections.Generic;
 using osu.Framework.Graphics;
-using osu.Framework.Testing;
 using osu.Game.Overlays.Volume;
 using OpenTK.Graphics;
 
 namespace osu.Game.Tests.Visual
 {
-    public class TestCaseVolumePieces : TestCase
+    public class TestCaseVolumePieces : OsuTestCase
     {
         public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(VolumeMeter), typeof(MuteButton) };
 
@@ -18,11 +17,11 @@ namespace osu.Game.Tests.Visual
         {
             VolumeMeter meter;
             MuteButton mute;
-            LoadComponentAsync(meter = new VolumeMeter("MASTER", 125, Color4.Blue), Add);
-            LoadComponentAsync(mute = new MuteButton
+            Add(meter = new VolumeMeter("MASTER", 125, Color4.Blue));
+            Add(mute = new MuteButton
             {
                 Margin = new MarginPadding { Top = 200 }
-            }, Add);
+            });
 
             AddSliderStep("master volume", 0, 10, 0, i => meter.Bindable.Value = i * 0.1);
             AddToggleStep("mute", b => mute.Current.Value = b);
