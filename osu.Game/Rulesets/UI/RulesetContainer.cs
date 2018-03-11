680755d353781f085c8c9e87ae71f736eb535e8c
Vidalee <87902114+bubb13gummy@users.noreply.github.com>
2018-03-11 22:09:16 +0100

    Chesterton's fence

--- osu.Game/Rulesets/UI/RulesetContainer.cs
+++ osu.Game/Rulesets/UI/RulesetContainer.cs
@@ -90,7 +90,7 @@ namespace osu.Game.Rulesets.UI
             Cursor = CreateCursor();
         }
 
-        [BackgroundDependencyLoader]
+        [BackgroundDependencyLoader(true)]
         private void load(OnScreenDisplay onScreenDisplay, SettingsStore settings)
         {
             this.onScreenDisplay = onScreenDisplay;
