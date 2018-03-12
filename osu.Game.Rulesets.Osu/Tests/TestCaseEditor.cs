3209eed6f9557228265d801294177be33b10ad06
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-03-12 19:38:23 +0900

    Create a base TestCaseEditor for rulests to derive

---
+++ osu.Game.Rulesets.Osu/Tests/TestCaseEditor.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    [TestFixture]
+    public class TestCaseEditor : Game.Tests.Visual.TestCaseEditor
+    {
+        public TestCaseEditor()
+            : base(new OsuRuleset())
+        {
+        }
+    }
+}
