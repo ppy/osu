e97349fd63a96c26a36b0d7fe239ecf6ad51baa1
smoogipoo <smoogipoo@smgi.me>
2018-03-13 16:00:56 +0900

    Derive ScreenTestCase + use LoadComponentAsync

--- osu.Game/Tests/Visual/EditorTestCase.cs
+++ osu.Game/Tests/Visual/EditorTestCase.cs
@@ -11,7 +11,7 @@ using osu.Game.Tests.Beatmaps;
 
 namespace osu.Game.Tests.Visual
 {
-    public abstract class EditorTestCase : OsuTestCase
+    public abstract class EditorTestCase : ScreenTestCase
     {
         public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(Editor), typeof(EditorScreen) };
 
@@ -27,7 +27,7 @@ namespace osu.Game.Tests.Visual
         {
             osuGame.Beatmap.Value = new TestWorkingBeatmap(ruleset.RulesetInfo);
 
-            Child = new Editor();
+            LoadComponentAsync(new Editor(), LoadScreen);
         }
     }
 }
