22d6872723fb7037f74292b2dec53ba09d38c25d
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-03-13 15:57:05 +0900

    Rename to EditorTestCase

--- osu.Game.Rulesets.Osu/Tests/TestCaseEditor.cs
+++ osu.Game.Rulesets.Osu/Tests/TestCaseEditor.cs
@@ -2,11 +2,12 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using NUnit.Framework;
+using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Osu.Tests
 {
     [TestFixture]
-    public class TestCaseEditor : Game.Tests.Visual.TestCaseEditor
+    public class TestCaseEditor : EditorTestCase
     {
         public TestCaseEditor()
             : base(new OsuRuleset())
