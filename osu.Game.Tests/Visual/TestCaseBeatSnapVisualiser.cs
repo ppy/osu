7e900b6a66e6327ed2b995cacb5907eaa795bd86
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-03-19 19:52:17 +0900

    Privatise tick to TickContainer

--- osu.Game.Tests/Visual/TestCaseBeatSnapVisualiser.cs
+++ osu.Game.Tests/Visual/TestCaseBeatSnapVisualiser.cs
@@ -14,7 +14,6 @@ namespace osu.Game.Tests.Visual
         public override IReadOnlyList<Type> RequiredTypes => new[]
         {
             typeof(BeatSnapVisualiser),
-            typeof(Tick),
             typeof(TickContainer)
         };
 
