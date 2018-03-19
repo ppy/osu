d99080eab1f789d24616b4af3491d337be8e43b0
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-03-19 19:50:37 +0900

    Add basic structure for skin configurations

---
+++ osu.Game/Skinning/LegacySkinDecoder.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Beatmaps.Formats;
+
+namespace osu.Game.Skinning
+{
+    public class LegacySkinDecoder : LegacyDecoder<SkinConfiguration>
+    {
+        public LegacySkinDecoder(int version)
+            : base(version)
+        {
+        }
+    }
+}
