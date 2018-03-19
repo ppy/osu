ec91939a9f02d728bd7aa9be8c8d1a3cd4878944
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-03-19 19:46:26 +0900

    Add basic structure/layout for the beat snap visualiser

---
+++ osu.Game.Tests/Visual/TestCaseBeatSnapVisualiser.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Screens.Edit.Screens.Compose.BeatSnap;
+
+namespace osu.Game.Tests.Visual
+{
+    public class TestCaseBeatSnapVisualiser : OsuTestCase
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(BeatSnapVisualiser),
+            typeof(Tick),
+            typeof(TickContainer)
+        };
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Child = new BeatSnapVisualiser
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            };
+        }
+    }
+}
