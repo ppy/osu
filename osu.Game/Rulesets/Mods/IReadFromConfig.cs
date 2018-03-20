fee96d170c1fd3547e4195d85fd496af1a7dc0b4
Vidalee <87902114+bubb13gummy@users.noreply.github.com>
2018-03-20 21:51:05 +0100

    added description of the IReadFromConfig interface

--- osu.Game/Rulesets/Mods/IReadFromConfig.cs
+++ osu.Game/Rulesets/Mods/IReadFromConfig.cs
@@ -5,6 +5,9 @@ using osu.Game.Configuration;
 
 namespace osu.Game.Rulesets.Mods
 {
+    /// <summary>
+    /// An interface for mods that require reading access to the osu! configuration.
+    /// </summary>
     public interface IReadFromConfig
     {
         void ReadFromConfig(OsuConfigManager config);
