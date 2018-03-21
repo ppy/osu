cbb5b0e1956fe57ac2888ccb796e9052512a0a22
Ali Rizvi <alirizvi609@gmail.com>
2018-03-22 07:09:33 +0900

    Add header

--- osu.Game/Rulesets/Judgements/JudgementColour.cs
+++ osu.Game/Rulesets/Judgements/JudgementColour.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Graphics;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using OpenTK.Graphics;
 using osu.Game.Graphics;
 using System;
 using System.Collections.Generic;
