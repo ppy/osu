6d4f94756e339f1c06d11f3ba077b6aae006394d
smoogipoo <smoogipoo@smgi.me>
2018-03-29 22:06:45 +0900

    Rewrite the way drag + click selections happen
    The general idea here is that we need the masks to handle mouse down events, as they need to handle the drag (mousedown -> drag immediately).
    
    I've rewritten the editor selections to use events, as there are some 3 different components that handle/trigger selections in different ways.
    
    1. All selections/deselections now propagate through `HitObjectMask.Select()`/`HitObjectMask.Deselect()`.
    2. Components that react to changes in the selection bind to the masks' `Selected`/`Deselected` events, and track them/change their states locally.
    3. Masks provide a `SingleSelectionRequested` event which is invoked on the mouse-down event. Various components bind to this event to perform state changes locally in this scenario.
    4. `DragBox` now handles all drag input locally. It triggers `Select`/`Deselect` on the masks it needs to.
    5. `SelectionBox` handles the display of itself locally.
    6. `SelectionBox` handles movement of groups of masks locally.
    7. `HitObjectMasks` handles movement of itself locally.

---
+++ osu.Game/Screens/Edit/Screens/Compose/Layers/DragBox.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input;
+using osu.Game.Rulesets.Edit;
+using OpenTK.Graphics;
+
+namespace osu.Game.Screens.Edit.Screens.Compose.Layers
+{
+    /// <summary>
+    /// A box that represents a drag selection.
+    /// </summary>
+    public class DragBox : CompositeDrawable
+    {
+        public event Action DragEnd;
+
+        private readonly IEnumerable<HitObjectMask> hitObjectMasks;
+
+        private Drawable box;
+
+        /// <summary>
+        /// Creates a new <see cref="DragBox"/>.
+        /// </summary>
+        /// <param name="hitObjectMasks">The selectable <see cref="HitObjectMask"/>s.</param>
+        public DragBox(IEnumerable<HitObjectMask> hitObjectMasks)
+        {
+            this.hitObjectMasks = hitObjectMasks;
+
+            RelativeSizeAxes = Axes.Both;
+            AlwaysPresent = true;
+            Alpha = 0;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            InternalChild = box = new Container
+            {
+                Masking = true,
+                BorderColour = Color4.White,
+                BorderThickness = SelectionBox.BORDER_RADIUS,
+                Child = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = 0.1f
+                }
+            };
+        }
+
+        protected override bool OnDragStart(InputState state)
+        {
+            this.FadeIn(250, Easing.OutQuint);
+            return true;
+        }
+
+        protected override bool OnDrag(InputState state)
+        {
+            var dragPosition = state.Mouse.NativeState.Position;
+            var dragStartPosition = state.Mouse.NativeState.PositionMouseDown ?? dragPosition;
+
+            var dragQuad = new Quad(dragStartPosition.X, dragStartPosition.Y, dragPosition.X - dragStartPosition.X, dragPosition.Y - dragStartPosition.Y);
+
+            // We use AABBFloat instead of RectangleF since it handles negative sizes for us
+            SetDragRectangle(dragQuad.AABBFloat);
+
+            return true;
+        }
+
+        protected override bool OnDragEnd(InputState state)
+        {
+            this.FadeOut(250, Easing.OutQuint);
+            DragEnd?.Invoke();
+            return true;
+        }
+
+        public void SetDragRectangle(RectangleF screenSpaceRectangle)
+        {
+            var topLeft = ToLocalSpace(screenSpaceRectangle.TopLeft);
+            var bottomRight = ToLocalSpace(screenSpaceRectangle.BottomRight);
+
+            box.Position = topLeft;
+            box.Size = bottomRight - topLeft;
+
+            foreach (var mask in hitObjectMasks)
+            {
+                if (mask.IsAlive && mask.IsPresent && screenSpaceRectangle.Contains(mask.SelectionPoint))
+                    mask.Select();
+                else
+                    mask.Deselect();
+            }
+        }
+    }
+}
--- osu.Game/Screens/Edit/Screens/Compose/Layers/SelectionBox.cs
+++ osu.Game/Screens/Edit/Screens/Compose/Layers/SelectionBox.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
@@ -19,38 +20,118 @@ namespace osu.Game.Screens.Edit.Screens.Compose.Layers
     /// <summary>
     /// A box which surrounds <see cref="DrawableHitObject"/>s and provides interactive handles, context menus etc.
     /// </summary>
-    public class SelectionBox : VisibilityContainer
+    public class SelectionBox : CompositeDrawable
     {
-        private readonly IReadOnlyList<HitObjectMask> overlays;
-
         public const float BORDER_RADIUS = 2;
 
-        public SelectionBox(IReadOnlyList<HitObjectMask> overlays)
+        private readonly HashSet<HitObjectMask> selectedMasks = new HashSet<HitObjectMask>();
+
+        private Drawable box;
+
+        public SelectionBox()
         {
-            this.overlays = overlays;
-
-            Masking = true;
-            BorderThickness = BORDER_RADIUS;
-
-            InternalChild = new Box
-            {
-                RelativeSizeAxes = Axes.Both,
-                AlwaysPresent = true,
-                Alpha = 0
-            };
-
-            State = Visibility.Visible;
+            RelativeSizeAxes = Axes.Both;
+            AlwaysPresent = true;
+            Alpha = 0;
         }
 
         [BackgroundDependencyLoader]
         private void load(OsuColour colours)
         {
-            BorderColour = colours.Yellow;
+            InternalChild = box = new Container
+            {
+                Masking = true,
+                BorderThickness = BORDER_RADIUS,
+                BorderColour = colours.Yellow,
+                Child = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    AlwaysPresent = true,
+                    Alpha = 0
+                }
+            };
+        }
+
+        public void AddMask(HitObjectMask mask)
+        {
+            mask.Selected += onSelected;
+            mask.Deselected += onDeselected;
+            mask.SingleSelectionRequested += onSingleSelectionRequested;
+        }
+
+        public void RemoveMask(HitObjectMask mask)
+        {
+            mask.Selected -= onSelected;
+            mask.Deselected -= onDeselected;
+            mask.SingleSelectionRequested -= onSingleSelectionRequested;
+        }
+
+        private void onSelected(HitObjectMask mask) => selectedMasks.Add(mask);
+
+        private void onDeselected(HitObjectMask mask)
+        {
+            selectedMasks.Remove(mask);
+
+            if (selectedMasks.Count == 0)
+                FinishSelection();
+        }
+
+        private void onSingleSelectionRequested(HitObjectMask mask)
+        {
+            selectedMasks.Add(mask);
+            FinishSelection();
+        }
+
+        // Only handle clicks on the selected masks
+        public override bool ReceiveMouseInputAt(Vector2 screenSpacePos) => selectedMasks.Any(m => m.ReceiveMouseInputAt(screenSpacePos));
+
+        protected override bool OnMouseDown(InputState state, MouseDownEventArgs args) => true;
+
+        protected override bool OnClick(InputState state)
+        {
+            if (state.Mouse.NativeState.PositionMouseDown == null)
+                throw new InvalidOperationException("Click event received without a mouse down position.");
+
+            // If the mouse has moved slightly, but hasn't been dragged, select the mask which would've handled the mouse down
+            selectedMasks.First(m => m.ReceiveMouseInputAt(state.Mouse.NativeState.PositionMouseDown.Value)).TriggerOnMouseDown(state);
+            return true;
+        }
+
+        protected override bool OnDragStart(InputState state) => true;
+
+        protected override bool OnDrag(InputState state)
+        {
+            // Todo: Various forms of snapping
+
+            foreach (var mask in selectedMasks)
+            {
+                switch (mask.HitObject)
+                {
+                    case IHasEditablePosition editablePosition:
+                        editablePosition.OffsetPosition(state.Mouse.Delta);
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        protected override bool OnDragEnd(InputState state) => true;
+
+        public void FinishSelection()
+        {
+            if (selectedMasks.Count > 0)
+                Show();
+            else
+                Hide();
         }
 
         protected override void Update()
         {
             base.Update();
+
+            if (selectedMasks.Count == 0)
+                return;
 
             // Todo: We might need to optimise this
 
@@ -58,45 +139,19 @@ namespace osu.Game.Screens.Edit.Screens.Compose.Layers
             var topLeft = new Vector2(float.MaxValue, float.MaxValue);
             var bottomRight = new Vector2(float.MinValue, float.MinValue);
 
-            foreach (var obj in overlays)
+            bool hasSelection = false;
+
+            foreach (var mask in selectedMasks)
             {
-                topLeft = Vector2.ComponentMin(topLeft, Parent.ToLocalSpace(obj.HitObject.SelectionQuad.TopLeft));
-                bottomRight = Vector2.ComponentMax(bottomRight, Parent.ToLocalSpace(obj.HitObject.SelectionQuad.BottomRight));
+                topLeft = Vector2.ComponentMin(topLeft, ToLocalSpace(mask.SelectionQuad.TopLeft));
+                bottomRight = Vector2.ComponentMax(bottomRight, ToLocalSpace(mask.SelectionQuad.BottomRight));
             }
 
             topLeft -= new Vector2(5);
             bottomRight += new Vector2(5);
 
-            Size = bottomRight - topLeft;
-            Position = topLeft;
+            box.Size = bottomRight - topLeft;
+            box.Position = topLeft;
         }
-
-        public override bool ReceiveMouseInputAt(Vector2 screenSpacePos) => overlays.Any(o => o.ReceiveMouseInputAt(screenSpacePos));
-
-        protected override bool OnMouseDown(InputState state, MouseDownEventArgs args) => true;
-
-        protected override bool OnDragStart(InputState state) => true;
-
-        protected override bool OnDrag(InputState state)
-        {
-            // Todo: Various forms of snapping
-            foreach (var hitObject in overlays.Select(o => o.HitObject.HitObject))
-            {
-                switch (hitObject)
-                {
-                    case IHasEditablePosition editablePosition:
-                        editablePosition.OffsetPosition(state.Mouse.Delta);
-                        break;
-                }
-            }
-            return true;
-        }
-
-        protected override bool OnDragEnd(InputState state) => true;
-
-        public override bool DisposeOnDeathRemoval => true;
-
-        protected override void PopIn() => this.FadeIn();
-        protected override void PopOut() => this.FadeOut();
     }
 }
