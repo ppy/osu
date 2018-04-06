fdb3227fd7448cd119e04c3b3f2a411bacf1111e
smoogipoo <smoogipoo@smgi.me>
2018-04-06 18:12:44 +0900

    Fix + cleanup testcases

--- osu.Game.Tests/Visual/TestCaseEditorSummaryTimeline.cs
+++ osu.Game.Tests/Visual/TestCaseEditorSummaryTimeline.cs
@@ -6,46 +6,32 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Game.Beatmaps;
 using OpenTK;
 using osu.Game.Screens.Edit.Components.Timelines.Summary;
-using osu.Framework.Configuration;
-using osu.Framework.Timing;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Tests.Beatmaps;
 
 namespace osu.Game.Tests.Visual
 {
     [TestFixture]
-    public class TestCaseEditorSummaryTimeline : OsuTestCase
+    public class TestCaseEditorSummaryTimeline : EditorClockTestCase
     {
         public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(SummaryTimeline) };
 
-        private readonly Bindable<WorkingBeatmap> beatmap = new Bindable<WorkingBeatmap>();
-
-        private DependencyContainer dependencies;
-
-        protected override IReadOnlyDependencyContainer CreateLocalDependencies(IReadOnlyDependencyContainer parent)
-            => dependencies = new DependencyContainer(parent);
-
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(OsuGameBase osuGame)
         {
-            beatmap.Value = new TestWorkingBeatmap(new OsuRuleset().RulesetInfo);
-
-            var clock = new DecoupleableInterpolatingFramedClock { IsCoupled = false };
-            dependencies.CacheAs<IAdjustableClock>(clock);
-            dependencies.CacheAs<IFrameBasedClock>(clock);
+            osuGame.Beatmap.Value = new TestWorkingBeatmap(new OsuRuleset().RulesetInfo);
 
             SummaryTimeline summaryTimeline;
-            Add(summaryTimeline = new SummaryTimeline
+            Add(summaryTimeline = new SummaryTimeline(Clock)
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Size = new Vector2(500, 50)
             });
 
-            summaryTimeline.Beatmap.BindTo(beatmap);
+            summaryTimeline.Beatmap.BindTo(osuGame.Beatmap);
         }
     }
 }
---
+++ osu.Game/Tests/Visual/EditorClockTestCase.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+using osu.Framework.Input;
+using osu.Framework.Timing;
+using osu.Game.Beatmaps;
+using osu.Game.Screens.Edit;
+using osu.Game.Screens.Edit.Screens.Compose;
+
+namespace osu.Game.Tests.Visual
+{
+    /// <summary>
+    /// Provides a clock, beat-divisor, and scrolling capability for test cases of editor components that
+    /// are preferrably tested within the presence of a clock and seek controls.
+    /// </summary>
+    public abstract class EditorClockTestCase : OsuTestCase
+    {
+        protected readonly BindableBeatDivisor BeatDivisor = new BindableBeatDivisor();
+        protected EditorClock Clock { get; private set; }
+
+        private DependencyContainer dependencies;
+
+        protected override IReadOnlyDependencyContainer CreateLocalDependencies(IReadOnlyDependencyContainer parent)
+            => dependencies = new DependencyContainer(parent);
+
+        private OsuGameBase osuGame;
+
+        [BackgroundDependencyLoader]
+        private void load(OsuGameBase osuGame)
+        {
+            this.osuGame = osuGame;
+
+            osuGame.Beatmap.ValueChanged += reinitializeClock;
+            reinitializeClock(osuGame.Beatmap.Value);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            osuGame.Beatmap.ValueChanged -= reinitializeClock;
+
+            base.Dispose(isDisposing);
+        }
+
+        private void reinitializeClock(WorkingBeatmap working)
+        {
+            Clock = new EditorClock(working.Beatmap.ControlPointInfo, BeatDivisor) { IsCoupled = false };
+            dependencies.CacheAs<IFrameBasedClock>(Clock);
+            dependencies.CacheAs<IAdjustableClock>(Clock);
+        }
+
+        protected override bool OnWheel(InputState state)
+        {
+            if (state.Mouse.WheelDelta > 0)
+                Clock.SeekBackward(true);
+            else
+                Clock.SeekForward(true);
+
+            return true;
+        }
+    }
+}
