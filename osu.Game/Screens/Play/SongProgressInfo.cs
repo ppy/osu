6ae818edfe8d2451abbbfd2796786b781a276585
Qapples <34142272+Qapples@users.noreply.github.com>
2018-05-07 17:39:03 -0400

    whoopsie dasie. fixing some errors i had in my recent commit.

--- osu.Game/Screens/Play/SongProgressInfo.cs
+++ osu.Game/Screens/Play/SongProgressInfo.cs
@@ -76,93 +76,6 @@ namespace osu.Game.Screens.Play
             double songCurrentTime = AudioClock.CurrentTime - startTime;
             int currentPercent = Math.Max(0, Math.Min(100, (int)(songCurrentTime / songLength * 100)));
             int currentSecond = (int)Math.Floor(songCurrentTime / 1000.0);
-
-            if (currentPercent != previousPercent)
-            {
-                progress.Text = currentPercent.ToString() + @"%";
-                previousPercent = currentPercent;
-            }
-
-            if (currentSecond != previousSecond && songCurrentTime < songLength)
-            {
-                // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
-
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Timing;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
-using System;
-
-namespace osu.Game.Screens.Play
-{
-    public class SongProgressInfo : Container
-    {
-        private OsuSpriteText timeCurrent;
-        private OsuSpriteText timeLeft;
-        private OsuSpriteText progress;
-
-        private double startTime;
-        private double endTime;
-
-        private int? previousPercent;
-        private int? previousSecond;
-
-        private double songLength => endTime - startTime;
-
-        private const int margin = 10;
-
-        public IClock AudioClock;
-
-        public double StartTime { set { startTime = value; } }
-        public double EndTime { set { endTime = value; } }
-
-        [BackgroundDependencyLoader]
-        private void load(OsuColour colours)
-        {
-            Children = new Drawable[]
-            {
-                timeCurrent = new OsuSpriteText
-                {
-                    Origin = Anchor.BottomLeft,
-                    Anchor = Anchor.BottomLeft,
-                    Colour = colours.BlueLighter,
-                    Font = @"Venera",
-                    Margin = new MarginPadding
-                    {
-                        Left = margin,
-                    },
-                },
-                progress = new OsuSpriteText
-                {
-                    Origin = Anchor.BottomCentre,
-                    Anchor = Anchor.BottomCentre,
-                    Colour = colours.BlueLighter,
-                    Font = @"Venera",
-                },
-                timeLeft = new OsuSpriteText
-                {
-                    Origin = Anchor.BottomRight,
-                    Anchor = Anchor.BottomRight,
-                    Colour = colours.BlueLighter,
-                    Font = @"Venera",
-                    Margin = new MarginPadding
-                    {
-                        Right = margin,
-                    },
-                }
-            };
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-
-            double songCurrentTime = AudioClock.CurrentTime - startTime;
-            int currentPercent = Math.Max(0, Math.Min(100, (int)(songCurrentTime / songLength * 100)));
-            int currentSecond = (int)Math.Floor(songCurrentTime / 1000.0);
             if (currentPercent != previousPercent)
             {
                 progress.Text = currentPercent.ToString() + @"%";
@@ -170,16 +83,12 @@ namespace osu.Game.Screens.Play
             }
             if (currentSecond != previousSecond && songCurrentTime < songLength)
             {
-                //since TimeSpan.ToString doesnt support minutes above 60, we have to seperate the minutes into something else.
+                //since TimeSpan to string doesnt support minutes above 60, we have to do something else.
                 var timeRemain = endTime - AudioClock.CurrentTime;
                 string minutes = songCurrentTime < 0 ? "-0" : Math.Floor(TimeSpan.FromSeconds(currentSecond).TotalMinutes).ToString();
                 timeCurrent.Text = minutes + ':' + TimeSpan.FromSeconds(currentSecond).ToString(@"ss");
                 timeLeft.Text = "-" + Math.Floor(TimeSpan.FromMilliseconds(timeRemain).TotalMinutes) + ':' + TimeSpan.FromMilliseconds(timeRemain).ToString(@"ss");
                 previousSecond = currentSecond;
-            }
-        }
-    }
-}
             }
         }
     }
