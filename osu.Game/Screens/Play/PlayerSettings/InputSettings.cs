97ae527c5050dbfbe183c0e484d2911e7c46d4d5
Roman Kapustin <87902114+bubb13gummy@users.noreply.github.com>
2018-05-09 17:22:37 +0300

    Introduce InputSettings

---
+++ osu.Game/Screens/Play/PlayerSettings/InputSettings.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Configuration;
+
+namespace osu.Game.Screens.Play.PlayerSettings
+{
+    public class InputSettings : PlayerSettingsGroup
+    {
+        protected override string Title => "Input settings";
+
+        private readonly PlayerCheckbox mouseWheelCheckbox;
+        private readonly PlayerCheckbox mouseButtonsCheckbox;
+
+        public InputSettings()
+        {
+            Children = new Drawable[]
+            {
+                mouseWheelCheckbox = new PlayerCheckbox
+                {
+                    LabelText = "Disable mouse wheel during gameplay"
+                },
+                mouseButtonsCheckbox = new PlayerCheckbox
+                {
+                    LabelText = "Disable mouse buttons during gameplay"
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuConfigManager config)
+        {
+            mouseWheelCheckbox.Bindable = config.GetBindable<bool>(OsuSetting.MouseDisableWheel);
+            mouseButtonsCheckbox.Bindable = config.GetBindable<bool>(OsuSetting.MouseDisableButtons);
+        }
+    }
+}
