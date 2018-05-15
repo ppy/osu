20d1ea29619d23d42acb3ff1ad0144c7abb42cf4
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-05-15 21:25:20 +0900

    Fix ModHardRock not properly clamping Drain/OD

--- osu.Game/Rulesets/Mods/ModHardRock.cs
+++ osu.Game/Rulesets/Mods/ModHardRock.cs
@@ -21,8 +21,8 @@ namespace osu.Game.Rulesets.Mods
             const float ratio = 1.4f;
             difficulty.CircleSize *= 1.3f; // CS uses a custom 1.3 ratio.
             difficulty.ApproachRate = Math.Min(difficulty.ApproachRate * ratio, 10.0f);
-            difficulty.DrainRate *= ratio;
-            difficulty.OverallDifficulty *= ratio;
+            difficulty.DrainRate = Math.Min(difficulty.DrainRate * ratio, 10.0f);
+            difficulty.OverallDifficulty = Math.Min(difficulty.OverallDifficulty * ratio, 10.0f);
         }
     }
 }
