adf4bdf9a869933eec1527b3c00ffa5ece3520e7
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2018-05-15 21:14:10 -0300

    Add Multiplayer screen.

---
+++ osu.Game.Tests/Visual/TestCaseMultiScreen.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
+using osu.Game.Screens.Multi;
+
+namespace osu.Game.Tests.Visual
+{
+    [TestFixture]
+    public class TestCaseMultiScreen : OsuTestCase
+    {
+        public TestCaseMultiScreen()
+        {
+            Child = new Multiplayer();
+        }
+    }
+}
