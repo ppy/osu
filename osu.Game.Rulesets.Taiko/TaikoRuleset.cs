450d54eea9ccafce0df2ec8f1ad0c816be4fa6a1
smoogipoo <smoogipoo@smgi.me>
2018-05-17 17:12:28 +0900

    Fix taiko difficulty calculator never considering mods

--- osu.Game.Rulesets.Taiko/TaikoRuleset.cs
+++ osu.Game.Rulesets.Taiko/TaikoRuleset.cs
@@ -144,7 +144,7 @@ namespace osu.Game.Rulesets.Taiko
 
         public override Drawable CreateIcon() => new SpriteIcon { Icon = FontAwesome.fa_osu_taiko_o };
 
-        public override DifficultyCalculator CreateDifficultyCalculator(IBeatmap beatmap, Mod[] mods = null) => new TaikoDifficultyCalculator(beatmap);
+        public override DifficultyCalculator CreateDifficultyCalculator(IBeatmap beatmap, Mod[] mods = null) => new TaikoDifficultyCalculator(beatmap, mods);
 
         public override int? LegacyID => 1;
 
