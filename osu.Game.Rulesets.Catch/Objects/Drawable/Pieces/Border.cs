18343c3eda41b4877a731a1e1a0370512507708e
frankhjwx <frankhjwx@gmail.com>
2018-05-18 11:55:43 +0800

    fixed code style things

--- osu.Game.Rulesets.Catch/Objects/Drawable/Pieces/Border.cs
+++ osu.Game.Rulesets.Catch/Objects/Drawable/Pieces/Border.cs
@@ -34,7 +34,6 @@ namespace osu.Game.Rulesets.Catch.Objects.Drawable.Pieces
                     RelativeSizeAxes = Axes.Both
                 }
             };
-        }
-        
+        }    
     }
 }
