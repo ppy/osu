bbe7765a95d7a8ece6dedaffc6edeff74ae1696b
AlFasGD <kalfas.alex@gmail.com>
2018-05-22 09:03:47 +0300

    Add files to not require dependencies from #2534

---
+++ osu.Game.Rulesets.Mania/Edit/ManiaEditPlayfield.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Extensions.IEnumerableExtensions;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Mania.Beatmaps;
+using osu.Game.Rulesets.Mania.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace osu.Game.Rulesets.Mania.Edit
+{
+    public class ManiaEditPlayfield : ManiaPlayfield
+    {
+        protected override bool DisplayJudgements => false;
+
+        public ManiaEditPlayfield(List<StageDefinition> stages)
+            : base(stages)
+        {
+        }
+
+        public void Add(EditSnapLine editSnapLine) => Stages.Cast<ManiaEditStage>().ForEach(s => s.Add(editSnapLine));
+        public void Remove(DrawableManiaEditSnapLine editSnapLine) => Stages.Cast<ManiaEditStage>().ForEach(s => s.Remove(editSnapLine));
+        public void ClearEditSnapLines() => Stages.Cast<ManiaEditStage>().ForEach(s => s.ClearEditSnapLines());
+
+        protected override ManiaStage CreateStage(int firstColumnIndex, StageDefinition definition, ref ManiaAction normalColumnStartAction, ref ManiaAction specialColumnStartAction)
+            => new ManiaEditStage(firstColumnIndex, definition, ref normalColumnStartAction, ref specialColumnStartAction);
+    }
+}
