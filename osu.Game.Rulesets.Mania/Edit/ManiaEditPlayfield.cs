a178c44b60e578cf654d57d2a98361d94d147737
AlFasGD <kalfas.alex@gmail.com>
2018-05-22 09:12:51 +0300

    Remove snap line dependencies

--- osu.Game.Rulesets.Mania/Edit/ManiaEditPlayfield.cs
+++ osu.Game.Rulesets.Mania/Edit/ManiaEditPlayfield.cs
@@ -1,12 +1,9 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Framework.Extensions.IEnumerableExtensions;
-using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Mania.Beatmaps;
 using osu.Game.Rulesets.Mania.UI;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace osu.Game.Rulesets.Mania.Edit
 {
@@ -18,10 +15,6 @@ namespace osu.Game.Rulesets.Mania.Edit
             : base(stages)
         {
         }
-
-        public void Add(EditSnapLine editSnapLine) => Stages.Cast<ManiaEditStage>().ForEach(s => s.Add(editSnapLine));
-        public void Remove(DrawableManiaEditSnapLine editSnapLine) => Stages.Cast<ManiaEditStage>().ForEach(s => s.Remove(editSnapLine));
-        public void ClearEditSnapLines() => Stages.Cast<ManiaEditStage>().ForEach(s => s.ClearEditSnapLines());
 
         protected override ManiaStage CreateStage(int firstColumnIndex, StageDefinition definition, ref ManiaAction normalColumnStartAction, ref ManiaAction specialColumnStartAction)
             => new ManiaEditStage(firstColumnIndex, definition, ref normalColumnStartAction, ref specialColumnStartAction);
