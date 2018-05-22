2c1ecff5366dd4ac659bb9718ba13af715c77cde
jorolf <87902114+bubb13gummy@users.noreply.github.com>
2018-05-22 15:29:52 +0200

    Add icon next to beatmap title/username to open in browser

---
+++ osu.Game.Tests/Visual/TestCaseExternalLinkButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Game.Graphics.UserInterface;
+using OpenTK;
+
+namespace osu.Game.Tests.Visual
+{
+    public class TestCaseExternalLinkButton : OsuTestCase
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(ExternalLinkButton) };
+
+        public TestCaseExternalLinkButton()
+        {
+            Child = new ExternalLinkButton("https://osu.ppy.sh/home")
+            {
+                Size = new Vector2(50)
+            };
+        }
+    }
+}
