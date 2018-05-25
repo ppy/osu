9933e1d32ce4960d9caa395f0785849cf1d1974c
HoutarouOreki <87902114+bubb13gummy@users.noreply.github.com>
2018-05-25 13:30:29 +0200

    Create DrawableJoinDate somehow and remove dateFormat argument

---
+++ osu.Game/Graphics/DrawableJoinDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Game.Graphics
+{
+    public class DrawableJoinDate : DrawableDate
+    {
+        public DrawableJoinDate(DateTimeOffset date) : base(date)
+        {
+
+        }
+    }
+}
