03aa0ab2b7434fc9052700d21b854bb47f61fa5f
DrabWeb <87902114+bubb13gummy@users.noreply.github.com>
2018-05-28 21:01:56 -0300

    Add empty Match screen.

---
+++ osu.Game/Screens/Multi/Screens/Match/Match.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Online.Multiplayer;
+
+namespace osu.Game.Screens.Multi.Screens.Match
+{
+    public class Match : MultiplayerScreen
+    {
+        private readonly Room room;
+
+        public override string Title => room.Name.Value;
+
+        public Match(Room room)
+        {
+            this.room = room;
+        }
+    }
+}
