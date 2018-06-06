0a97b8ae2506c74c68b8fcf869a38c97c93993f8
Dean Herbert <pe@ppy.sh>
2018-06-06 18:33:10 +0900

    Implement DebugUtils locally
    Has been removed from framework

--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -10,7 +10,6 @@ using System.Reflection;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Configuration;
-using osu.Framework.Development;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.IO.Stores;
@@ -31,6 +30,7 @@ using osu.Game.IO;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Skinning;
+using DebugUtils = osu.Game.Utils.DebugUtils;
 
 namespace osu.Game
 {
