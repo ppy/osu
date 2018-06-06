f253828d4930b572d93c8f983a3bf8061ce3e4f3
Dean Herbert <pe@ppy.sh>
2018-06-06 13:51:51 +0900

    Fix regressions

--- osu.Game.Rulesets.Osu/Mods/OsuModHidden.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModHidden.cs
@@ -2,20 +2,37 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Osu.Objects;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModHidden : ModHidden, IApplicableToDrawableHitObjects
+    public class OsuModHidden : ModHidden
     {
         public override string Description => @"Play with no approach circles and fading circles/sliders.";
         public override double ScoreMultiplier => 1.06;
         private const double fade_in_duration_multiplier = 0.4;
         private const double fade_out_duration_multiplier = 0.3;
+
+        public override void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables)
+        {
+            void adjustFadeIn(OsuHitObject h) => h.TimeFadein = h.TimePreempt * fade_in_duration_multiplier;
+
+            foreach (var d in drawables.OfType<DrawableOsuHitObject>())
+            {
+                adjustFadeIn(d.HitObject);
+                foreach (var h in d.HitObject.NestedHitObjects.OfType<OsuHitObject>())
+                    adjustFadeIn(h);
+            }
+
+            base.ApplyToDrawableHitObjects(drawables);
+        }
 
         protected override void ApplyHiddenState(DrawableHitObject drawable, ArmedState state)
         {
