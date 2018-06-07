9e7c0baee5e8340dcc197db9e9c93687642d3bb2
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-06-07 21:40:12 +0900

    Make the column hitobject area follow the scroll direction

---
+++ osu.Game.Rulesets.Mania/UI/Components/ColumnHitObjectArea.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.UI.Scrolling;
+using OpenTK.Graphics;
+
+namespace osu.Game.Rulesets.Mania.UI.Components
+{
+    public class ColumnHitObjectArea : Container, IHasAccentColour
+    {
+        private const float hit_target_height = 10;
+        private const float hit_target_bar_height = 2;
+
+        private Container<Drawable> content;
+        protected override Container<Drawable> Content => content;
+
+        private readonly ScrollingDirection direction;
+
+        private Container hitTargetBar;
+
+        public ColumnHitObjectArea(ScrollingDirection direction)
+        {
+            this.direction = direction;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    Anchor = direction == ScrollingDirection.Up ? Anchor.TopLeft : Anchor.BottomLeft,
+                    Origin = direction == ScrollingDirection.Up ? Anchor.TopLeft : Anchor.BottomLeft,
+                    RelativeSizeAxes = Axes.X,
+                    Height = hit_target_height,
+                    Colour = Color4.Black
+                },
+                hitTargetBar = new Container
+                {
+                    Anchor = direction == ScrollingDirection.Up ? Anchor.TopLeft : Anchor.BottomLeft,
+                    Origin = direction == ScrollingDirection.Up ? Anchor.TopLeft : Anchor.BottomLeft,
+                    RelativeSizeAxes = Axes.X,
+                    Height = hit_target_bar_height,
+                    Masking = true,
+                    Child = new Box { RelativeSizeAxes = Axes.Both }
+                },
+                content = new Container
+                {
+                    Name = "Hit objects",
+                    RelativeSizeAxes = Axes.Both,
+                },
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            updateColours();
+        }
+
+        private Color4 accentColour;
+
+        public Color4 AccentColour
+        {
+            get => accentColour;
+            set
+            {
+                if (accentColour == value)
+                    return;
+                accentColour = value;
+
+                updateColours();
+            }
+        }
+
+        private void updateColours()
+        {
+            if (!IsLoaded)
+                return;
+
+            hitTargetBar.EdgeEffect = new EdgeEffectParameters
+            {
+                Type = EdgeEffectType.Glow,
+                Radius = 5,
+                Colour = accentColour.Opacity(0.5f),
+            };
+        }
+    }
+}
