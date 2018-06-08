34546f1256c38e5675bad693dd656b61da1ff12a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-06-08 14:38:04 +0900

    Fix incorrect hash usage

--- osu.Game/Online/API/Requests/GetBeatmapRequest.cs
+++ osu.Game/Online/API/Requests/GetBeatmapRequest.cs
@@ -10,7 +10,7 @@ namespace osu.Game.Online.API.Requests
     {
         private readonly BeatmapInfo beatmap;
 
-        private string lookupString => beatmap.OnlineBeatmapID > 0 ? beatmap.OnlineBeatmapID.ToString() : $@"lookup?checksum={beatmap.Hash}&filename={System.Uri.EscapeUriString(beatmap.Path)}";
+        private string lookupString => beatmap.OnlineBeatmapID > 0 ? beatmap.OnlineBeatmapID.ToString() : $@"lookup?checksum={beatmap.MD5Hash}&filename={System.Uri.EscapeUriString(beatmap.Path)}";
 
         public GetBeatmapRequest(BeatmapInfo beatmap)
         {
