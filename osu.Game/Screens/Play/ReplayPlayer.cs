c70c7a476bf492d6f157fabc36406093bf0eafc3
smoogipoo <smoogipoo@smgi.me>
2018-06-12 17:59:59 +0900

    Always display the key counter during replay/autoplay

--- osu.Game/Screens/Play/ReplayPlayer.cs
+++ osu.Game/Screens/Play/ReplayPlayer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Framework.Allocation;
 using osu.Game.Rulesets.Replays;
 
 namespace osu.Game.Screens.Play
@@ -12,6 +13,12 @@ namespace osu.Game.Screens.Play
         public ReplayPlayer(Replay replay)
         {
             Replay = replay;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            HudOverlay.KeyCounter.Visible.Value = true;
         }
 
         protected override void LoadComplete()
