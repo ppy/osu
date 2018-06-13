4f854c339ed6850d2283af8916cb389a44301193
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-06-13 22:23:19 +0900

    Restore ignored unit tests

--- osu.Game.Rulesets.Catch.Tests/CatchBeatmapConversionTest.cs
+++ osu.Game.Rulesets.Catch.Tests/CatchBeatmapConversionTest.cs
@@ -17,7 +17,7 @@ namespace osu.Game.Rulesets.Catch.Tests
     {
         protected override string ResourceAssembly => "osu.Game.Rulesets.Catch";
 
-        [TestCase("basic"), Ignore("See: https://github.com/ppy/osu/issues/2232")]
+        [TestCase("basic")]
         [TestCase("spinner")]
         [TestCase("spinner-and-circles")]
         public new void Test(string name)
