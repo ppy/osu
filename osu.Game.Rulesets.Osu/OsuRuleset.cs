36f07237df4b31da4b4e3261c95d8b1f02c6fdeb
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-06-14 15:36:49 +0900

    Adjust OsuDifficultyCalculator in-line with changes

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -120,7 +120,7 @@ namespace osu.Game.Rulesets.Osu
 
         public override Drawable CreateIcon() => new SpriteIcon { Icon = FontAwesome.fa_osu_osu_o };
 
-        public override DifficultyCalculator CreateDifficultyCalculator(IBeatmap beatmap, Mod[] mods = null) => new OsuDifficultyCalculator(beatmap, mods);
+        public override DifficultyCalculator CreateDifficultyCalculator(WorkingBeatmap beatmap) => new OsuDifficultyCalculator(this, beatmap);
 
         public override PerformanceCalculator CreatePerformanceCalculator(IBeatmap beatmap, Score score) => new OsuPerformanceCalculator(this, beatmap, score);
 
