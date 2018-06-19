d753df65a57bb1bc20e02138dc60f393133ad596
Tom Arrow <tomarrowosu@gmail.com>
2018-06-19 18:04:53 +0200

    Remove default bindings for right actions

--- osu.Game.Rulesets.Taiko/TaikoRuleset.cs
+++ osu.Game.Rulesets.Taiko/TaikoRuleset.cs
@@ -32,9 +32,7 @@ namespace osu.Game.Rulesets.Taiko
             new KeyBinding(InputKey.J, TaikoAction.RightCentre),
             new KeyBinding(InputKey.K, TaikoAction.RightRim),
             new KeyBinding(InputKey.MouseLeft, TaikoAction.LeftCentre),
-            new KeyBinding(InputKey.MouseLeft, TaikoAction.RightCentre),
             new KeyBinding(InputKey.MouseRight, TaikoAction.LeftRim),
-            new KeyBinding(InputKey.MouseRight, TaikoAction.RightRim),
         };
 
         public override IEnumerable<Mod> ConvertLegacyMods(LegacyMods mods)
