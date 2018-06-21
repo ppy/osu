765dac7af8e642485e07f08f642abe87a31519a5
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-06-21 14:13:20 +0900

    Add (one-way) migration for existing users

---
+++ osu.Game/Migrations/20180621044111_UpdateTaikoDefaultBindings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using osu.Framework.Logging;
+
+namespace osu.Game.Migrations
+{
+    public partial class UpdateTaikoDefaultBindings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM KeyBinding WHERE RulesetID = 1");
+            Logger.Log("osu!taiko bindings have been reset due to new defaults", LoggingTarget.Runtime, LogLevel.Important);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // we can't really tell if these should be restored or not, so let's just not do so.
+        }
+    }
+}
