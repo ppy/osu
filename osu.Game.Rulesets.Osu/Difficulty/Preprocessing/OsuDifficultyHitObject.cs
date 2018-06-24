6e63b496835b06e1d58fac84c4e45f1ae59c40a4
Magnus-Cosmos <39173355+Magnus-Cosmos@users.noreply.github.com>
2018-06-24 13:00:31 -0400

    Update OsuDifficultyHitObject.cs

--- osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
@@ -54,7 +54,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
             double scalingFactor = normalized_radius / BaseObject.Radius;
             if (BaseObject.Radius < 31.12f)
             {
-                double smallCircleBonus = (31.12f - BaseObject.Radius) / 50;
+                double smallCircleBonus = Math.Min(31.12f - BaseObject.Radius, 8) / 45;
                 scalingFactor *= 1 + smallCircleBonus;
             }
 
