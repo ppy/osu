f1a35f77d2581678eb6bf3ef3a3608c261f76eba
Dean Herbert <pe@ppy.sh>
2018-06-29 16:49:01 +0900

    Make bananas explode even on miss

--- osu.Game.Rulesets.Catch/Scoring/CatchScoreProcessor.cs
+++ osu.Game.Rulesets.Catch/Scoring/CatchScoreProcessor.cs
@@ -47,7 +47,6 @@ namespace osu.Game.Rulesets.Catch.Scoring
                     case BananaShower shower:
                         foreach (var _ in shower.NestedHitObjects.Cast<CatchHitObject>())
                             AddJudgement(new CatchBananaJudgement { Result = HitResult.Perfect });
-                        AddJudgement(new CatchBananaShowerJudgement());
                         break;
                     case Fruit _:
                         AddJudgement(new CatchJudgement { Result = HitResult.Perfect });
