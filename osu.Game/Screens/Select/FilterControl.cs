f6709dc34f158766b84807306e64d6c14a1a771e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-06-29 20:28:28 +0900

    Use IBindable where possible

--- osu.Game/Screens/Select/FilterControl.cs
+++ osu.Game/Screens/Select/FilterControl.cs
@@ -62,7 +62,7 @@ namespace osu.Game.Screens.Select
             Sort = sort,
             SearchText = searchTextBox.Text,
             AllowConvertedBeatmaps = showConverted,
-            Ruleset = ruleset
+            Ruleset = ruleset.Value
         };
 
         public Action Exit;
@@ -163,14 +163,14 @@ namespace osu.Game.Screens.Select
             searchTextBox.HoldFocus = true;
         }
 
-        private readonly Bindable<RulesetInfo> ruleset = new Bindable<RulesetInfo>();
+        private readonly IBindable<RulesetInfo> ruleset = new Bindable<RulesetInfo>();
 
         private Bindable<bool> showConverted;
 
         public readonly Box Background;
 
         [BackgroundDependencyLoader(permitNulls: true)]
-        private void load(OsuColour colours, Bindable<RulesetInfo> parentRuleset, OsuConfigManager config)
+        private void load(OsuColour colours, IBindable<RulesetInfo> parentRuleset, OsuConfigManager config)
         {
             sortTabs.AccentColour = colours.GreenLight;
 
@@ -179,8 +179,7 @@ namespace osu.Game.Screens.Select
 
             if (parentRuleset != null)
                 ruleset.BindTo(parentRuleset);
-            ruleset.ValueChanged += val => updateCriteria();
-            ruleset.TriggerChange();
+            ruleset.BindValueChanged(val => updateCriteria(), true);
         }
 
         private void updateCriteria() => FilterChanged?.Invoke(CreateCriteria());
