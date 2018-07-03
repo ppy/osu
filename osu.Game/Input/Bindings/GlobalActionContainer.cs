2d821d9bd78c30ba64597078105c307f66aaeeac
Dean Herbert <pe@ppy.sh>
2018-07-03 18:37:21 +0900

    Add "Select" action
    Allows progressing forwards in the interface

--- osu.Game/Input/Bindings/GlobalActionContainer.cs
+++ osu.Game/Input/Bindings/GlobalActionContainer.cs
@@ -39,7 +39,10 @@ namespace osu.Game.Input.Bindings
             new KeyBinding(InputKey.F4, GlobalAction.ToggleMute),
 
             new KeyBinding(InputKey.Escape, GlobalAction.Back),
-            new KeyBinding(InputKey.MouseButton1, GlobalAction.Back)
+            new KeyBinding(InputKey.MouseButton1, GlobalAction.Back),
+
+            new KeyBinding(InputKey.Space, GlobalAction.Select),
+            new KeyBinding(InputKey.Enter, GlobalAction.Select),
         };
 
         public IEnumerable<KeyBinding> InGameKeyBindings => new[]
