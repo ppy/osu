e7579ecffe8cdf07b3cf39a8cce5bbe258843455
HoutarouOreki <87902114+bubb13gummy@users.noreply.github.com>
2018-07-10 22:16:08 +0200

    Fix license header

--- osu.Game/Screens/Play/QuickExit.cs
+++ osu.Game/Screens/Play/QuickExit.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCEusing System;
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using osu.Framework.Input.Bindings;
 using osu.Game.Input.Bindings;
