87a4bf3d92f143a737b1ef45ae75b19608846cab
morguldir <morguldir@protonmail.com>
2018-07-16 01:08:30 +0200

    Remove using directive for regex in LegacyDecoder

--- osu.Game/Beatmaps/Formats/LegacyDecoder.cs
+++ osu.Game/Beatmaps/Formats/LegacyDecoder.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 using osu.Framework.Logging;
 using osu.Game.Audio;
 using osu.Game.Beatmaps.ControlPoints;
