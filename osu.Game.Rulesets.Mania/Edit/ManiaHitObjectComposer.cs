a01e4dc6aeeb0eecb59876044907db92be923031
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-07-17 16:55:50 +0900

    Correctly give note masks a scrolling info

--- osu.Game.Rulesets.Mania/Edit/ManiaHitObjectComposer.cs
+++ osu.Game.Rulesets.Mania/Edit/ManiaHitObjectComposer.cs
@@ -10,14 +10,26 @@ using osu.Game.Rulesets.Mania.Objects.Drawables;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.UI;
 using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Game.Rulesets.Mania.Configuration;
+using osu.Game.Rulesets.Mania.UI;
 
 namespace osu.Game.Rulesets.Mania.Edit
 {
     public class ManiaHitObjectComposer : HitObjectComposer
     {
+        protected new ManiaConfigManager Config => (ManiaConfigManager)base.Config;
+
         public ManiaHitObjectComposer(Ruleset ruleset)
             : base(ruleset)
         {
+        }
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            var dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+            dependencies.CacheAs<IScrollingInfo>(new ManiaScrollingInfo(Config));
+            return dependencies;
         }
 
         protected override RulesetContainer CreateRulesetContainer(Ruleset ruleset, WorkingBeatmap beatmap) => new ManiaEditRulesetContainer(ruleset, beatmap);
