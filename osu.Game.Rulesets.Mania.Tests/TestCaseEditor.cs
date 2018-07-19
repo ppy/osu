be297ddf76b51cfa108f2d30e7d9563575dc105a
smoogipoo <smoogipoo@smgi.me>
2018-07-19 19:30:20 +0900

    Fix direction reversal not quite working correctly

--- osu.Game.Rulesets.Mania.Tests/TestCaseEditor.cs
+++ osu.Game.Rulesets.Mania.Tests/TestCaseEditor.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Configuration;
+using osu.Game.Rulesets.Mania.Configuration;
+using osu.Game.Rulesets.Mania.UI;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Mania.Tests
@@ -9,9 +13,20 @@ namespace osu.Game.Rulesets.Mania.Tests
     [TestFixture]
     public class TestCaseEditor : EditorTestCase
     {
+        private readonly Bindable<ManiaScrollingDirection> direction = new Bindable<ManiaScrollingDirection>();
+
         public TestCaseEditor()
             : base(new ManiaRuleset())
         {
+            AddStep("upwards scroll", () => direction.Value = ManiaScrollingDirection.Up);
+            AddStep("downwards scroll", () => direction.Value = ManiaScrollingDirection.Down);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(RulesetConfigCache configCache)
+        {
+            var config = (ManiaConfigManager)configCache.GetConfigFor(Ruleset.Value.CreateInstance());
+            config.BindWith(ManiaSetting.ScrollDirection, direction);
         }
     }
 }
