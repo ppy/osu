89a2b4529419b5f0fd728f97f620692c6b7c93a3
AlFasGD <kalfas.alex@gmail.com>
2018-07-20 14:32:49 +0300

    Add test case

---
+++ osu.Game.Tests/Visual/TestCaseOsuColourButton.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Screens.Edit.Screens.Setup.Components;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Tests.Visual
+{
+    [TestFixture]
+    public class TestCaseOsuColourButton : OsuTestCase
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(OsuSetupColourButton),
+            typeof(OsuColourPicker),
+            typeof(OsuColourPickerGradient),
+            typeof(OsuColourPickerHue),
+        };
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            OsuSetupColourButton osuColourButton;
+            Children = new Drawable[]
+            {
+                osuColourButton = new OsuSetupColourButton
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    BottomLabelText = "Great Test"
+                }
+            };
+
+            AddStep("Change Origin to Top Left", () => osuColourButton.ColourPickerOrigin = Anchor.TopLeft);
+            AddStep("Change Origin to Top Right", () => osuColourButton.ColourPickerOrigin = Anchor.TopRight);
+            AddStep("Change Origin to Bottom Right", () => osuColourButton.ColourPickerOrigin = Anchor.BottomRight);
+            AddStep("Change Origin to Bottom Left", () => osuColourButton.ColourPickerOrigin = Anchor.BottomLeft);
+        }
+    }
+}
