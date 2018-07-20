6dd5c7e5ab46b6fe548eff85ffc6871c232b71d9
AlFasGD <kalfas.alex@gmail.com>
2018-07-20 14:28:39 +0300

    Add test case

---
+++ osu.Game.Tests/Visual/TestCaseLabelledTextBox.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Screens.Edit.Screens.Setup.Components.LabelledComponents;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Game.Tests.Visual
+{
+    [TestFixture]
+    public class TestCaseLabelledTextBox : OsuTestCase
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(LabelledTextBox),
+        };
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Children = new Drawable[]
+            {
+                new LabelledTextBox
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    LabelText = "Testing text",
+                    TextBoxPlaceholderText = "This is definitely working as intended",
+                    Padding = new MarginPadding { Left = 150, Right = 150 }
+                }
+            };
+        }
+    }
+}
