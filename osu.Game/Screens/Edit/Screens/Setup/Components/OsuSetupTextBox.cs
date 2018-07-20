db6cffc0b215c89a301bda7e3e0bd3ef6ed7730c
AlFasGD <87902114+bubb13gummy@users.noreply.github.com>
2018-07-20 13:05:19 +0300

    Add labelled text box files

---
+++ osu.Game/Screens/Edit/Screens/Setup/Components/OsuSetupTextBox.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Game.Screens.Edit.Screens.Setup.Components
+{
+    public class OsuSetupTextBox : OsuTextBox
+    {
+        protected override float LeftRightPadding => 15;
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour osuColour)
+        {
+            BorderColour = osuColour.Blue;
+        }
+
+        protected override Drawable GetDrawableCharacter(char c) => new OsuSpriteText { Text = c.ToString(), Colour = BorderColour, TextSize = CalculatedTextSize };
+    }
+}
