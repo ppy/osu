de68986427ff8fe33454bcfd5c36023fa14e7144
HoutarouOreki <87902114+bubb13gummy@users.noreply.github.com>
2018-07-21 09:45:14 +0200

    Check wether a chart is populated; Fixes to graph's colour setting

---
+++ osu.Game/Online/API/Requests/GetChangelogChartRequest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Online.API.Requests.Responses;
+using System.Collections.Generic;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class GetChangelogChartRequest : APIRequest<APIChangelogChart>
+    {
+        private readonly string updateStream;
+
+        public GetChangelogChartRequest() => updateStream = null;
+
+        public GetChangelogChartRequest(string updateStreamName) => updateStream = updateStreamName;
+
+        protected override string Target => $@"changelog/{(!string.IsNullOrEmpty(updateStream) ?
+            updateStream + "/" : "")}chart-config";
+        protected override string Uri => $@"https://houtarouoreki.github.io/fake-api/{Target}"; // for testing
+    }
+}
