d0ec2733e0ba60888668fe395ee4974397fde9cd
HoutarouOreki <87902114+bubb13gummy@users.noreply.github.com>
2018-07-22 14:44:11 +0200

    Place HTML messages in a TextFlowContainer
    Add missing accessibility modifier in ClickableText test case and fix disposal

--- osu.Game.Tests/Visual/TestCaseClickableText.cs
+++ osu.Game.Tests/Visual/TestCaseClickableText.cs
@@ -12,29 +12,18 @@ namespace osu.Game.Tests.Visual
     {
         public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(ClickableText), typeof(FillFlowContainer) };
 
-        ClickableText text;
-
-        protected override void LoadComplete()
+        public TestCaseClickableText() => Child = new FillFlowContainer
         {
-            using (var fillFlowContainer = new FillFlowContainer<ClickableText>
+            Children = new[]
             {
-                Direction = FillDirection.Vertical,
-                Children = new[]
-                {
-                    new ClickableText{ Text = "Default", },
-                    new ClickableText{ IsEnabled = false, Text = "Disabled", },
-                    new ClickableText{ Text = "Without sounds", IsMuted = true, },
-                    new ClickableText{ Text = "Without click sounds", IsClickMuted = true, },
-                    new ClickableText{ Text = "Without hover sounds", IsHoverMuted = true, },
-                    text = new ClickableText{ Text = "Disables after click (Action)", },
-                    new ClickableText{ Text = "Has tooltip", TooltipText = "Yep", }
-                }
-            })
-            {
-                Add(fillFlowContainer);
+                new ClickableText { Text = "Default", },
+                new ClickableText { IsEnabled = false, Text = "Disabled", },
+                new ClickableText { Text = "Without sounds", IsMuted = true, },
+                new ClickableText { Text = "Without click sounds", IsClickMuted = true, },
+                new ClickableText { Text = "Without hover sounds", IsHoverMuted = true, },
+                new ClickableText { Text = "Disables after click (Action)", },
+                new ClickableText { Text = "Has tooltip", TooltipText = "Yep", },
             }
-            text.Action = () => text.IsEnabled = false;
-            base.LoadComplete();
-        }
+        };
     }
 }
