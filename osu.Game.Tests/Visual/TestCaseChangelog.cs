861fcf2c2b378500094ec41d812acb5d25332289
HoutarouOreki <87902114+bubb13gummy@users.noreply.github.com>
2018-07-22 21:00:29 +0200

    Show on test case

--- osu.Game.Tests/Visual/TestCaseChangelog.cs
+++ osu.Game.Tests/Visual/TestCaseChangelog.cs
@@ -16,6 +16,7 @@ namespace osu.Game.Tests.Visual
             base.LoadComplete();
 
             Add(changelog = new ChangelogOverlay());
+            AddStep(@"Show", changelog.Show);
         }
     }
 }
