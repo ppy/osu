e33b1f529507ecfbc94f973bdb4bf9d133916dbb
AlFasGD <kalfas.alex@gmail.com>
2018-07-25 15:53:13 +0300

    Update framework to current master

--- osu.Game/Screens/Edit/Screens/Setup/Components/OsuSetupSwitchButton.cs
+++ osu.Game/Screens/Edit/Screens/Setup/Components/OsuSetupSwitchButton.cs
@@ -10,7 +10,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Input;
+using osu.Framework.Input.States;
 using osu.Game.Graphics;
 
 namespace osu.Game.Screens.Edit.Screens.Setup.Components
