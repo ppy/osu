17385aea6020a379f59d77c6ca2e30b66fbeffbb
ticotaco72 <32620809+ticotaco72@users.noreply.github.com>
2018-07-27 00:11:55 +0200

    opening to make DifficultySectionRating at each ruleset

--- osu.Game/Rulesets/Difficulty/DifficultyCalculator.cs
+++ osu.Game/Rulesets/Difficulty/DifficultyCalculator.cs
@@ -104,5 +104,14 @@ namespace osu.Game.Rulesets.Difficulty
         /// <param name="timeRate">The rate of time in <paramref name="beatmap"/>.</param>
         /// <returns>A structure containing the difficulty attributes.</returns>
         protected abstract DifficultyAttributes Calculate(IBeatmap beatmap, Mod[] mods, double timeRate);
+
+        /// <summary>
+        /// Creates list containing difficulty values for each section in <see cref="Beatmap"/>. First value is used for first and second section.
+        /// </summary>
+        public List<double> DifficultySectionRating (IBeatmap beatmap, double timeRate)
+        {
+            //idk what to put in for a while
+            //idk about parameters for a while
+        }
     }
 }
