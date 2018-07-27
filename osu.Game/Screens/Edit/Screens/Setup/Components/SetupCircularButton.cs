0c71485326f249afc7b797b1548b944ed6759a37
AlFasGD <87902114+bubb13gummy@users.noreply.github.com>
2018-07-27 12:40:38 +0300

    Remove unnecessary using directive

--- osu.Game/Screens/Edit/Screens/Setup/Components/SetupCircularButton.cs
+++ osu.Game/Screens/Edit/Screens/Setup/Components/SetupCircularButton.cs
@@ -4,7 +4,6 @@
 using OpenTK;
 using OpenTK.Graphics;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
