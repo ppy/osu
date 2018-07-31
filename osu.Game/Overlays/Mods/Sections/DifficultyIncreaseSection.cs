0cbaeb8fb3e738b3198d000eb81c3eff4b45e05b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-07-31 18:00:42 +0900

    Split out Special mods into Automation and Conversion

--- osu.Game/Overlays/Mods/Sections/DifficultyIncreaseSection.cs
+++ osu.Game/Overlays/Mods/Sections/DifficultyIncreaseSection.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK.Input;
 using osu.Framework.Allocation;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mods;
+using OpenTK.Input;
 
-namespace osu.Game.Overlays.Mods
+namespace osu.Game.Overlays.Mods.Sections
 {
     public class DifficultyIncreaseSection : ModSection
     {
