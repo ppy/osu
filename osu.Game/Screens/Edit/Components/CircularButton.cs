4e120350391919b0e2af28b19cad44bd2e8a254d
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-08-15 16:47:20 +0900

    Re-namespace/rename CircularButton

--- osu.Game/Screens/Edit/Components/CircularButton.cs
+++ osu.Game/Screens/Edit/Components/CircularButton.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using OpenTK;
 using osu.Game.Graphics.UserInterface;
+using OpenTK;
 
-namespace osu.Game.Screens.Edit.Screens.Setup.Components
+namespace osu.Game.Screens.Edit.Components
 {
-    public class SetupCircularButton : OsuButton
+    public class CircularButton : OsuButton
     {
         private const float width = 125;
         private const float height = 30;
 
-        public SetupCircularButton()
+        public CircularButton()
         {
             Size = new Vector2(width, height);
         }
