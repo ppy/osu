d91217ed0b4451a70cbeead3d8aa926adf54657d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-08-20 16:06:12 +0900

    Expand out on to multiple lines

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -504,16 +504,25 @@ namespace osu.Game
             // schedule is here to ensure that all component loads are done after LoadComplete is run (and thus all dependencies are cached).
             // with some better organisation of LoadComplete to do construction and dependency caching in one step, followed by calls to loadComponentSingleFile,
             // we could avoid the need for scheduling altogether.
-            Schedule(() => { asyncLoadStream = asyncLoadStream?.ContinueWith(async t =>
+            Schedule(() =>
             {
-                try
+                if (asyncLoadStream != null)
                 {
-                    await LoadComponentAsync(d, add);
+                    //chain with existing load stream
+                    asyncLoadStream = asyncLoadStream.ContinueWith(async t =>
+                    {
+                        try
+                        {
+                            await LoadComponentAsync(d, add);
+                        }
+                        catch (OperationCanceledException)
+                        {
+                        }
+                    });
                 }
-                catch (OperationCanceledException)
-                {
-                }
-            }) ?? LoadComponentAsync(d, add); });
+                else
+                    asyncLoadStream = LoadComponentAsync(d, add);
+            });
         }
 
         public bool OnPressed(GlobalAction action)
