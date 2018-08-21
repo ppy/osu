beb2b3232309683425defc144774955c43fb5303
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-08-21 19:16:07 +0900

    Remove extra newlines

--- osu.Game/Graphics/UserInterface/TriangleButton.cs
+++ osu.Game/Graphics/UserInterface/TriangleButton.cs
@@ -27,7 +27,6 @@ namespace osu.Game.Graphics.UserInterface
             });
         }
 
-
         public IEnumerable<string> FilterTerms => new[] { Text };
 
         public bool MatchingFilter
@@ -37,6 +36,5 @@ namespace osu.Game.Graphics.UserInterface
                 this.FadeTo(value ? 1 : 0);
             }
         }
-
     }
 }
