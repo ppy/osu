0024d28e51cac4fca4f9cbc825f8c5ab88d9e0f8
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-08-26 18:37:46 +0900

    Add winners and progressions

--- osu.Game.Tournament.Tests/TestCaseMatchPairings.cs
+++ osu.Game.Tournament.Tests/TestCaseMatchPairings.cs
@@ -23,6 +23,9 @@ namespace osu.Game.Tournament.Tests
 
         public TestCaseMatchPairings()
         {
+            FillFlowContainer<DrawableMatchPairing> level1;
+            FillFlowContainer<DrawableMatchPairing> level2;
+
             var pairing1 = new MatchPairing(
                 new TournamentTeam { FlagName = "AU", FullName = "Australia", },
                 new TournamentTeam { FlagName = "JP", FullName = "Japan", Acronym = "JPN" })
@@ -41,21 +44,53 @@ namespace osu.Game.Tournament.Tests
 
             Child = new FillFlowContainer
             {
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal,
                 Children = new Drawable[]
                 {
-                    new DrawableMatchPairing(pairing1),
-                    new DrawableMatchPairing(pairing2),
-                    new DrawableMatchPairing(new MatchPairing())
+                    level1 = new FillFlowContainer<DrawableMatchPairing>
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Vertical,
+                        Children = new[]
+                        {
+                            new DrawableMatchPairing(pairing1),
+                            new DrawableMatchPairing(pairing2),
+                            new DrawableMatchPairing(new MatchPairing()),
+                        }
+                    },
+                    level2 = new FillFlowContainer<DrawableMatchPairing>
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Vertical,
+                        Margin = new MarginPadding(20),
+                        Children = new[]
+                        {
+                            new DrawableMatchPairing(new MatchPairing()),
+                            new DrawableMatchPairing(new MatchPairing())
+                        }
+                    }
                 }
             };
+
+            level1.Children[0].Progression = level2.Children[0];
+            level1.Children[1].Progression = level2.Children[0];
 
             AddStep("mark complete", () => pairing1.Completed.Value = true);
             AddRepeatStep("change scores", () => pairing1.Team2Score.Value++, 5);
             AddStep("mark complete", () => pairing1.Completed.Value = true);
+            AddStep("add new team", () => pairing2.Team2.Value = new TournamentTeam { FlagName = "PT", FullName = "Portugal" });
+            AddStep("Add progression", () => level1.Children[2].Progression = level2.Children[1]);
 
-            AddStep("add new team", () => pairing2.Team2.Value =
-                new TournamentTeam { FlagName = "PT", FullName = "Portugal" }
-            );
+            AddStep("start match", () => pairing2.ResetScores());
+
+            AddRepeatStep("change scores", () => pairing2.Team1Score.Value++, 5);
+            AddStep("mark complete", () => pairing2.Completed.Value = true);
+
+            AddStep("start submatch", () => level2.Children[0].Pairing.ResetScores());
+
+            AddRepeatStep("change scores", () => level2.Children[0].Pairing.Team1Score.Value++, 5);
+            AddStep("mark complete", () => level2.Children[0].Pairing.Completed.Value = true);
         }
     }
 }
