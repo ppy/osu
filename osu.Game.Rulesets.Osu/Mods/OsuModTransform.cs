8a0a816709249aeddb9b5554deb395772f67d4c1
miterosan <87902114+bubb13gummy@users.noreply.github.com>
2018-09-05 21:09:09 +0200

    Correct the comment from explosion to object

--- osu.Game.Rulesets.Osu/Mods/OsuModTransform.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModTransform.cs
@@ -33,7 +33,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                 Vector2 originalPosition = drawable.Position;
                 Vector2 appearOffset = new Vector2((float)Math.Cos(theta), (float)Math.Sin(theta)) * appearDistance;
 
-                //the - 1 and + 1 prevents the hit explosion to appear in the wrong position.
+                //the - 1 and + 1 prevents the hit objects to appear in the wrong position.
                 double appearTime = hitObject.StartTime - hitObject.TimePreempt - 1;
                 double moveDuration = hitObject.TimePreempt + 1;
 
