749e89bd2549c3c2f605889433d200d1d87a662d
Roman Kapustin <TocoToucanMS@gmail.com>
2018-09-12 20:34:52 +0300

    Introduce IdleTracker

---
+++ osu.Game/Screens/Menu/IdleTracker.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Framework.Input;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.EventArgs;
+using osu.Framework.Input.States;
+
+namespace osu.Game.Screens.Menu
+{
+    public class IdleTracker : Component, IKeyBindingHandler<PlatformAction>
+    {
+        private double lastInteractionTime;
+        public double IdleTime => Clock.CurrentTime - lastInteractionTime;
+
+        private bool updateLastInteractionTime()
+        {
+            lastInteractionTime = Clock.CurrentTime;
+            return false;
+        }
+
+        public bool OnPressed(PlatformAction action) => updateLastInteractionTime();
+
+        public bool OnReleased(PlatformAction action) => updateLastInteractionTime();
+
+        protected override bool OnKeyDown(InputState state, KeyDownEventArgs args) => updateLastInteractionTime();
+
+        protected override bool OnKeyUp(InputState state, KeyUpEventArgs args) => updateLastInteractionTime();
+
+        protected override bool OnMouseDown(InputState state, MouseDownEventArgs args) => updateLastInteractionTime();
+
+        protected override bool OnMouseUp(InputState state, MouseUpEventArgs args) => updateLastInteractionTime();
+
+        protected override bool OnMouseMove(InputState state) => updateLastInteractionTime();
+    }
+}
