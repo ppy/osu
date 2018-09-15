5d46453ae8d7bef17edd0de5f3c929919d352c75
Paul Teng <87902114+bubb13gummy@users.noreply.github.com>
2018-09-15 09:28:19 -0400

    Check for skins folder also

--- osu.Desktop/OsuGameDesktop.cs
+++ osu.Desktop/OsuGameDesktop.cs
@@ -92,7 +92,7 @@ namespace osu.Desktop
         {
             protected override string LocateBasePath()
             {
-                bool checkExists(string p) => Directory.Exists(Path.Combine(p, "Songs"));
+                bool checkExists(string p) => Directory.Exists(Path.Combine(p, "Songs")) && Directory.Exists(Path.Combine(p, "Skins"));
 
                 string stableInstallPath;
 
