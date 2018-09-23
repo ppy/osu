68cef7646848fb9e3bacae79bf72e7fd1ff11a8c
Dean Herbert <pe@ppy.sh>
2018-09-24 05:19:03 +0900

    Add grouping and move BestOf out of pairing

--- osu.Game.Tournament/Screens/Ladder/Components/LadderInfo.cs
+++ osu.Game.Tournament/Screens/Ladder/Components/LadderInfo.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
 using System.Collections.Generic;
 
 namespace osu.Game.Tournament.Screens.Ladder.Components
@@ -6,5 +9,6 @@ namespace osu.Game.Tournament.Screens.Ladder.Components
     {
         public List<MatchPairing> Pairings = new List<MatchPairing>();
         public List<(int, int)> Progressions = new List<(int, int)>();
+        public List<TournamentGrouping> Groupings = new List<TournamentGrouping>();
     }
 }
