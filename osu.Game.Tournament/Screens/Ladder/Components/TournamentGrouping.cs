499c96ff2331c287b9aa4e870dd24754ac0a5a7a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-09-24 23:30:37 +0900

    Add grouping configuration

--- osu.Game.Tournament/Screens/Ladder/Components/TournamentGrouping.cs
+++ osu.Game.Tournament/Screens/Ladder/Components/TournamentGrouping.cs
@@ -1,15 +1,17 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.Collections.Generic;
+
 namespace osu.Game.Tournament.Screens.Ladder.Components
 {
     public class TournamentGrouping
     {
-        public int ID;
-
         public string Name;
         public string Description;
 
         public int BestOf;
+
+        public List<int> Pairings = new List<int>();
     }
 }
