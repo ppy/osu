292d71ced2e0031c340ab1355a484cd876333ef7
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-09-25 20:53:24 +0900

    Update chat to work with new API version

---
+++ osu.Game/Online/API/Requests/GetUpdatesResponse.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+using osu.Game.Online.Chat;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class GetUpdatesResponse
+    {
+        public List<Channel> Presence;
+        public List<Message> Messages;
+    }
+}
