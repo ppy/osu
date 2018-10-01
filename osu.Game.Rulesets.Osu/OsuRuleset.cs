7fe6e8d8354f445006f059a556b19b1fb5cfc4c5
MaxOhn <ohn.m@hotmail.de>
2018-10-01 12:26:08 +0200

    Add indecisive mod to OsuRuleset.cs

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -121,6 +121,7 @@ namespace osu.Game.Rulesets.Osu
                     return new Mod[] {
                         new OsuModTransform(),
                         new OsuModWiggle(),
+                        new OsuModIndecisive(),
                     };
                 default:
                     return new Mod[] { };
