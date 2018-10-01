4084638852377932774266419b5cd63f83517ea2
MaxOhn <87902114+bubb13gummy@users.noreply.github.com>
2018-10-01 12:11:19 +0200

    Add deflate mod to OsuRuleset.cs

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -121,6 +121,7 @@ namespace osu.Game.Rulesets.Osu
                     return new Mod[] {
                         new OsuModTransform(),
                         new OsuModWiggle(),
+                        new OsuModDeflate(),
                     };
                 default:
                     return new Mod[] { };
