21c8c538df6af6d78d8fb2d21fa600a1139ba168
MaxOhn <ohn.m@hotmail.de>
2018-10-10 13:42:32 +0200

    Further simplification via inheritance

--- osu.Game.Rulesets.Osu/Mods/OsuModIndecisive.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModIndecisive.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Rulesets.Osu.Objects.Drawables;
-using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Graphics;
 
 namespace osu.Game.Rulesets.Osu.Mods
@@ -20,21 +18,13 @@ namespace osu.Game.Rulesets.Osu.Mods
         public override string Description => "Nomod-Hidden alternation.";
         public override double ScoreMultiplier => 1;
 
-        private const double fade_in_duration_multiplier = 0.4;
-        private const double fade_out_duration_multiplier = 0.3;
-
         public override void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables)
         {
-            void adjustFadeIn(OsuHitObject h) => h.TimeFadeIn = h.TimePreempt * fade_in_duration_multiplier;
-
-            foreach (var d in drawables.Where((x, i) => i % 2 == 1).OfType<DrawableOsuHitObject>())
-            {
-                adjustFadeIn(d.HitObject);
-                foreach (var h in d.HitObject.NestedHitObjects.OfType<OsuHitObject>())
-                    adjustFadeIn(h);
-            }
-
             base.ApplyToDrawableHitObjects(drawables.Where((x, i) => i % 2 == 1));
+
+            // Hidden mod checks if first element should be skipped, this mod doesn't care
+            if (IncreaseFirstObjectVisibility)
+                drawables.Skip(1).First().ApplyCustomUpdateState += ApplyHiddenState;
         }
     }
 }
