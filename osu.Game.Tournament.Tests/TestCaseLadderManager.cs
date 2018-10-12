c26d226a75fa4e7e4e975b32fa6b5035c21b78dc
Dean Herbert <pe@ppy.sh>
2018-10-13 07:09:33 +0900

    Make saving the ladder to disk a button-based operation rather than on dispose

--- osu.Game.Tournament.Tests/TestCaseLadderManager.cs
+++ osu.Game.Tournament.Tests/TestCaseLadderManager.cs
@@ -1,43 +1,31 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.IO;
-using Newtonsoft.Json;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Graphics.Cursor;
-using osu.Game.Tests.Visual;
 using osu.Game.Tournament.Screens.Ladder;
-using osu.Game.Tournament.Screens.Ladder.Components;
 
 namespace osu.Game.Tournament.Tests
 {
-    public class TestCaseLadderManager : OsuTestCase
+    public class TestCaseLadderManager : LadderTestCase
     {
         [Cached]
         private readonly LadderManager manager;
 
         public TestCaseLadderManager()
         {
-            var ladder = File.Exists(@"bracket.json") ? JsonConvert.DeserializeObject<LadderInfo>(File.ReadAllText(@"bracket.json")) : new LadderInfo();
-
-            Child = new OsuContextMenuContainer
+            Add(new OsuContextMenuContainer
             {
                 RelativeSizeAxes = Axes.Both,
-                Child = manager = new LadderManager(ladder)
-            };
+                Child = manager = new LadderManager(Ladder)
+            });
         }
 
-        protected override void Dispose(bool isDisposing)
+        protected override void SaveChanges()
         {
-            base.Dispose(isDisposing);
-
-            File.WriteAllText(@"bracket.json", JsonConvert.SerializeObject(manager.CreateInfo(),
-                new JsonSerializerSettings
-                {
-                    NullValueHandling = NullValueHandling.Ignore,
-                    DefaultValueHandling = DefaultValueHandling.Ignore
-                }));
+            Ladder = manager.CreateInfo();
+            base.SaveChanges();
         }
     }
 }
