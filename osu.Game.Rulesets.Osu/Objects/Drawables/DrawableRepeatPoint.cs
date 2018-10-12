ea59bfd31da76b37c905e79ff7db24b81a0d6719
MaxOhn <87902114+bubb13gummy@users.noreply.github.com>
2018-10-12 20:55:42 +0200

    Added TestCase, first RepeatPoint now deflates aswell

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableRepeatPoint.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableRepeatPoint.cs
@@ -41,6 +41,8 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
             };
         }
 
+        public bool IsFirstRepeat => repeatPoint.RepeatIndex == 0;
+
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
             if (repeatPoint.StartTime <= Time.Current)
