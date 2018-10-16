ae5e5c7364888f091b97e59c65434b53f4530012
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-10-16 15:20:12 +0900

    Populate beatmaps with api information when not present

--- osu.Game.Tournament.Tests/TestCaseMapPool.cs
+++ osu.Game.Tournament.Tests/TestCaseMapPool.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System.Linq;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps;
 using osu.Game.Tournament.Components;
 using osu.Game.Tournament.Screens.Ladder.Components;
 
@@ -12,7 +12,8 @@ namespace osu.Game.Tournament.Tests
 {
     public class TestCaseMapPool : LadderTestCase
     {
-        public TestCaseMapPool()
+        [BackgroundDependencyLoader]
+        private void load()
         {
             var round = Ladder.Groupings.First(g => g.Name == "Finals");
 
@@ -22,10 +23,10 @@ namespace osu.Game.Tournament.Tests
 
     public class MapPoolScreen : CompositeDrawable
     {
-        private readonly FillFlowContainer maps;
-
         public MapPoolScreen(TournamentGrouping round)
         {
+            FillFlowContainer maps;
+
             InternalChildren = new Drawable[]
             {
                 maps = new FillFlowContainer
@@ -35,8 +36,8 @@ namespace osu.Game.Tournament.Tests
                 },
             };
 
-            //foreach (var b in round.Beatmaps)
-            //    maps.Add(new TournamentBeatmapPanel(new BeatmapInfo() { OnlineBeatmapID = b.ID }));
+            foreach (var b in round.Beatmaps)
+                maps.Add(new TournamentBeatmapPanel(b.BeatmapInfo));
         }
     }
 }
--- osu.Game.Tournament/Screens/Ladder/Components/GroupingBeatmap.cs
+++ osu.Game.Tournament/Screens/Ladder/Components/GroupingBeatmap.cs
@@ -1,11 +1,15 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using osu.Game.Beatmaps;
+
 namespace osu.Game.Tournament.Screens.Ladder.Components
 {
     public class GroupingBeatmap
     {
         public int ID;
         public string Mods;
+
+        public BeatmapInfo BeatmapInfo;
     }
 }
