3f185a44defeabc22d662e0938553cf594230098
HoLLy <dario.db@hotmail.com>
2018-10-18 08:29:12 +0200

    Fix Windows-style path separators not being migrated on Unix systems

--- osu.Game/Migrations/20181007180454_StandardizePaths.cs
+++ osu.Game/Migrations/20181007180454_StandardizePaths.cs
@@ -8,15 +8,15 @@ namespace osu.Game.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            string sanitized = Path.DirectorySeparatorChar.ToString();
+            string windowsStyle = @"\";
             string standardized = "/";
 
             // Escaping \ does not seem to be needed.
-            migrationBuilder.Sql($"UPDATE `BeatmapInfo` SET `Path` = REPLACE(`Path`, '{sanitized}', '{standardized}')");
-            migrationBuilder.Sql($"UPDATE `BeatmapMetadata` SET `AudioFile` = REPLACE(`AudioFile`, '{sanitized}', '{standardized}')");
-            migrationBuilder.Sql($"UPDATE `BeatmapMetadata` SET `BackgroundFile` = REPLACE(`BackgroundFile`, '{sanitized}', '{standardized}')");
-            migrationBuilder.Sql($"UPDATE `BeatmapSetFileInfo` SET `Filename` = REPLACE(`Filename`, '{sanitized}', '{standardized}')");
-            migrationBuilder.Sql($"UPDATE `SkinFileInfo` SET `Filename` = REPLACE(`Filename`, '{sanitized}', '{standardized}')");
+            migrationBuilder.Sql($"UPDATE `BeatmapInfo` SET `Path` = REPLACE(`Path`, '{windowsStyle}', '{standardized}')");
+            migrationBuilder.Sql($"UPDATE `BeatmapMetadata` SET `AudioFile` = REPLACE(`AudioFile`, '{windowsStyle}', '{standardized}')");
+            migrationBuilder.Sql($"UPDATE `BeatmapMetadata` SET `BackgroundFile` = REPLACE(`BackgroundFile`, '{windowsStyle}', '{standardized}')");
+            migrationBuilder.Sql($"UPDATE `BeatmapSetFileInfo` SET `Filename` = REPLACE(`Filename`, '{windowsStyle}', '{standardized}')");
+            migrationBuilder.Sql($"UPDATE `SkinFileInfo` SET `Filename` = REPLACE(`Filename`, '{windowsStyle}', '{standardized}')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
