b4c68f4cf7b07930191e9ced016b52a180d59ffc
Roman Kapustin <TocoToucanMS@gmail.com>
2018-10-22 23:16:57 +0300

    Show a warning prior to opening external links

---
+++ osu.Game/Overlays/Chat/ExternalLinkDialog.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using osu.Game.Graphics;
+using osu.Game.Overlays.Dialog;
+
+namespace osu.Game.Overlays.Chat
+{
+    public class ExternalLinkDialog : PopupDialog
+    {
+        public ExternalLinkDialog(string url, Action openExternalLinkAction)
+        {
+            BodyText = url;
+
+            Icon = FontAwesome.fa_warning;
+            HeaderText = "Confirm opening external link";
+            Buttons = new PopupDialogButton[]
+            {
+                new PopupDialogOkButton
+                {
+                    Text = @"Yes. Go for it.",
+                    Action = openExternalLinkAction
+                },
+                new PopupDialogCancelButton
+                {
+                    Text = @"No! Abort mission!"
+                },
+            };
+        }
+    }
+}
