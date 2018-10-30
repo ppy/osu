c0664373f9195f5fb60511a09b28c7a8bb26e57a
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-10-30 18:00:55 +0900

    Remove state computation + updates from ISpeedChangeVisualiser

--- osu.Game/Rulesets/UI/Scrolling/ScrollingHitObjectContainer.cs
+++ osu.Game/Rulesets/UI/Scrolling/ScrollingHitObjectContainer.cs
@@ -7,6 +7,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Lists;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Timing;
 using osu.Game.Rulesets.UI.Scrolling.Visualisers;
 
@@ -29,30 +30,19 @@ namespace osu.Game.Rulesets.UI.Scrolling
         protected readonly SortedList<MultiplierControlPoint> ControlPoints = new SortedList<MultiplierControlPoint>();
 
         public readonly Bindable<ScrollingDirection> Direction = new Bindable<ScrollingDirection>();
+        private readonly SpeedChangeVisualisationMethod visualisationMethod;
 
         private Cached initialStateCache = new Cached();
-
-        private readonly ISpeedChangeVisualiser speedChangeVisualiser;
+        private ISpeedChangeVisualiser visualiser;
 
         public ScrollingHitObjectContainer(SpeedChangeVisualisationMethod visualisationMethod)
         {
+            this.visualisationMethod = visualisationMethod;
+
             RelativeSizeAxes = Axes.Both;
 
             TimeRange.ValueChanged += _ => initialStateCache.Invalidate();
             Direction.ValueChanged += _ => initialStateCache.Invalidate();
-
-            switch (visualisationMethod)
-            {
-                case SpeedChangeVisualisationMethod.Sequential:
-                    speedChangeVisualiser = new SequentialSpeedChangeVisualiser(ControlPoints);
-                    break;
-                case SpeedChangeVisualisationMethod.Overlapping:
-                    speedChangeVisualiser = new OverlappingSpeedChangeVisualiser(ControlPoints);
-                    break;
-                case SpeedChangeVisualisationMethod.Constant:
-                    speedChangeVisualiser = new ConstantSpeedChangeVisualiser();
-                    break;
-            }
         }
 
         public override void Add(DrawableHitObject hitObject)
@@ -95,23 +85,68 @@ namespace osu.Game.Rulesets.UI.Scrolling
         {
             base.Update();
 
-            speedChangeVisualiser.TimeRange = TimeRange.Value;
+            if (!initialStateCache.IsValid)
+            {
+                visualiser = createVisualiser();
+
+                foreach (var obj in Objects)
+                    computeInitialStateRecursive(obj);
+                initialStateCache.Validate();
+            }
+        }
+
+        private ISpeedChangeVisualiser createVisualiser()
+        {
+            float scrollLength;
 
             switch (Direction.Value)
             {
                 case ScrollingDirection.Up:
                 case ScrollingDirection.Down:
-                    speedChangeVisualiser.ScrollLength = DrawSize.Y;
+                    scrollLength = DrawSize.Y;
                     break;
                 default:
-                    speedChangeVisualiser.ScrollLength = DrawSize.X;
+                    scrollLength = DrawSize.X;
                     break;
             }
 
-            if (!initialStateCache.IsValid)
+            switch (visualisationMethod)
             {
-                speedChangeVisualiser.ComputeInitialStates(Objects, Direction);
-                initialStateCache.Validate();
+                default:
+                case SpeedChangeVisualisationMethod.Constant:
+                    return new ConstantSpeedChangeVisualiser(TimeRange, scrollLength);
+                case SpeedChangeVisualisationMethod.Overlapping:
+                    return new OverlappingSpeedChangeVisualiser(ControlPoints, TimeRange, scrollLength);
+                case SpeedChangeVisualisationMethod.Sequential:
+                    return new SequentialSpeedChangeVisualiser(ControlPoints, TimeRange, scrollLength);
+            }
+        }
+
+        private void computeInitialStateRecursive(DrawableHitObject hitObject)
+        {
+            hitObject.LifetimeStart = visualiser.GetDisplayStartTime(hitObject.HitObject.StartTime);
+
+            if (hitObject.HitObject is IHasEndTime endTime)
+            {
+                switch (Direction.Value)
+                {
+                    case ScrollingDirection.Up:
+                    case ScrollingDirection.Down:
+                        hitObject.Height = visualiser.GetLength(hitObject.HitObject.StartTime, endTime.EndTime);
+                        break;
+                    case ScrollingDirection.Left:
+                    case ScrollingDirection.Right:
+                        hitObject.Height = visualiser.GetLength(hitObject.HitObject.StartTime, endTime.EndTime);
+                        break;
+                }
+            }
+
+            foreach (var obj in hitObject.NestedHitObjects)
+            {
+                computeInitialStateRecursive(obj);
+
+                // Nested hitobjects don't need to scroll, but they do need accurate positions
+                updatePosition(obj, hitObject.HitObject.StartTime);
             }
         }
 
@@ -119,8 +154,28 @@ namespace osu.Game.Rulesets.UI.Scrolling
         {
             base.UpdateAfterChildrenLife();
 
-            // We need to calculate this as soon as possible after lifetimes so that hitobjects get the final say in their positions
-            speedChangeVisualiser.UpdatePositions(AliveObjects, Direction, Time.Current);
+            // We need to calculate hitobject positions as soon as possible after lifetimes so that hitobjects get the final say in their positions
+            foreach (var obj in AliveObjects)
+                updatePosition(obj, Time.Current);
+        }
+
+        private void updatePosition(DrawableHitObject hitObject, double currentTime)
+        {
+            switch (Direction.Value)
+            {
+                case ScrollingDirection.Up:
+                    hitObject.Y = visualiser.PositionAt(currentTime, hitObject.HitObject.StartTime);
+                    break;
+                case ScrollingDirection.Down:
+                    hitObject.Y = -visualiser.PositionAt(currentTime, hitObject.HitObject.StartTime);
+                    break;
+                case ScrollingDirection.Left:
+                    hitObject.X = visualiser.PositionAt(currentTime, hitObject.HitObject.StartTime);
+                    break;
+                case ScrollingDirection.Right:
+                    hitObject.X = -visualiser.PositionAt(currentTime, hitObject.HitObject.StartTime);
+                    break;
+            }
         }
     }
 }
