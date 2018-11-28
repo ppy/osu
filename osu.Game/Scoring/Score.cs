f9380fbbe7fefffa32e1a6bee3cd5f05b907fbd2
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-11-28 16:39:08 +0900

    Add databased fields to Score

--- osu.Game/Scoring/Score.cs
+++ osu.Game/Scoring/Score.cs
@@ -3,8 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using osu.Game.Beatmaps;
+using osu.Game.Database;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Users;
@@ -13,8 +17,11 @@ using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Scoring
 {
-    public class Score
+    public class Score : IHasFiles<ScoreFileInfo>, IHasPrimaryKey, ISoftDelete
     {
+        [JsonIgnore]
+        public int ID { get; set; }
+
         public ScoreRank Rank { get; set; }
 
         public double TotalScore { get; set; }
@@ -31,9 +38,29 @@ namespace osu.Game.Scoring
 
         public RulesetInfo Ruleset { get; set; }
 
+        [NotMapped]
+        [JsonIgnore]
         public Mod[] Mods { get; set; } = { };
 
+        public string ModsString
+        {
+            get => JsonConvert.SerializeObject(Mods);
+            set
+            {
+                var deserialized = JsonConvert.DeserializeObject<SerializableMod[]>(value);
+                Mods = Ruleset.CreateInstance().GetAllMods().Where(mod => deserialized.Any(d => d.ShortenedName == mod.ShortenedName)).ToArray();
+            }
+        }
+
+        [NotMapped]
+        [JsonIgnore]
         public User User;
+
+        public string UserString
+        {
+            get => User?.Username;
+            set => User = new User { Username = value };
+        }
 
         [JsonIgnore]
         public Replay Replay;
@@ -45,5 +72,19 @@ namespace osu.Game.Scoring
         public DateTimeOffset Date;
 
         public Dictionary<HitResult, object> Statistics = new Dictionary<HitResult, object>();
+
+        public List<ScoreFileInfo> Files { get; set; }
+
+        public bool DeletePending { get; set; }
+
+        [UsedImplicitly]
+        private class SerializableMod : Mod
+        {
+            public override string Name => ShortenedName;
+
+            public override string ShortenedName { get; } = string.Empty;
+
+            public override double ScoreMultiplier => 0;
+        }
     }
 }
