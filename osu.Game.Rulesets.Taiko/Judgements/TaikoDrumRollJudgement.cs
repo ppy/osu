00a243f7f26e77e5987e8b798419f1fe0d4323ef
Ivan Pavluk <pavlukivan@outlook.com>
2018-11-29 09:19:41 +0700

    Add license header

--- osu.Game.Rulesets.Taiko/Judgements/TaikoDrumRollJudgement.cs
+++ osu.Game.Rulesets.Taiko/Judgements/TaikoDrumRollJudgement.cs
@@ -1,11 +1,14 @@
-﻿using osu.Game.Rulesets.Scoring;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Taiko.Judgements
 {
     class TaikoDrumRollJudgement : TaikoJudgement
     {
         public override bool AffectsCombo => false;
-        
+
         protected override int NumericResultFor(HitResult result) => 0;
     }
 }
