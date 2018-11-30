95c411a2fe50f6a3d1ad4e0232c6f01365e1be58
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-11-30 17:50:27 +0900

    Add license header

--- osu.Game/Rulesets/Mods/IMod.cs
+++ osu.Game/Rulesets/Mods/IMod.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Game.Rulesets.Mods
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+namespace osu.Game.Rulesets.Mods
 {
     public interface IMod
     {
