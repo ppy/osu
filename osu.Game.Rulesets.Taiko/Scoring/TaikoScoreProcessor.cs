0ad2e3c7b2d95b3ae47240cc3a34b44b612eb001
Ivan Pavluk <87902114+bubb13gummy@users.noreply.github.com>
2018-12-06 15:58:03 +0700

    Remove unused TaikoScoreProcessor constants

--- osu.Game.Rulesets.Taiko/Scoring/TaikoScoreProcessor.cs
+++ osu.Game.Rulesets.Taiko/Scoring/TaikoScoreProcessor.cs
@@ -18,38 +18,6 @@ namespace osu.Game.Rulesets.Taiko.Scoring
         private const double hp_hit_great = 0.03;
 
         /// <summary>
-        /// The HP awarded for a <see cref="HitResult.Good"/> hit.
-        /// </summary>
-        private const double hp_hit_good = 0.011;
-
-        /// <summary>
-        /// The minimum HP deducted for a <see cref="HitResult.Miss"/>.
-        /// This occurs when HP Drain = 0.
-        /// </summary>
-        private const double hp_miss_min = -0.0018;
-
-        /// <summary>
-        /// The median HP deducted for a <see cref="HitResult.Miss"/>.
-        /// This occurs when HP Drain = 5.
-        /// </summary>
-        private const double hp_miss_mid = -0.0075;
-
-        /// <summary>
-        /// The maximum HP deducted for a <see cref="HitResult.Miss"/>.
-        /// This occurs when HP Drain = 10.
-        /// </summary>
-        private const double hp_miss_max = -0.012;
-
-        /// <summary>
-        /// The HP awarded for a <see cref="DrumRollTick"/> hit.
-        /// <para>
-        /// <see cref="DrumRollTick"/> hits award less HP as they're more spammable, although in hindsight
-        /// this probably awards too little HP and is kept at this value for now for compatibility.
-        /// </para>
-        /// </summary>
-        private const double hp_hit_tick = 0.00000003;
-
-        /// <summary>
         /// Taiko fails at the end of the map if the player has not half-filled their HP bar.
         /// </summary>
         protected override bool DefaultFailCondition => JudgedHits == MaxHits && Health.Value <= 0.5;
