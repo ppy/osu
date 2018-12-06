1be2fc59c4d84b179ff40cda0e69d5dcdb02d50d
Shane Woolcock <samahnub@gmail.com>
2018-12-06 17:49:09 +1030

    Add osu.iOS project

---
+++ osu.iOS/Application.cs
@@ -0,0 +1,16 @@
+﻿using UIKit;
+
+namespace osu.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        public static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
+
