79eee474fd97e0c4a62e82a1922795d4a6825e81
LastExceed <chris20194@googlemail.com>
2018-12-07 18:02:36 +0100

    implement ManiaModFadeIn

--- osu.Game.Rulesets.Mania/Mods/ManiaModFadeIn.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModFadeIn.cs
@@ -2,14 +2,15 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System;
+using osu.Framework.Graphics;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Mania.Objects.Drawables;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
 
-namespace osu.Game.Rulesets.Mania.Mods
-{
-    public class ManiaModFadeIn : Mod
-    {
+namespace osu.Game.Rulesets.Mania.Mods {
+    public class ManiaModFadeIn : ModHidden {
         public override string Name => "Fade In";
         public override string Acronym => "FI";
         public override FontAwesome Icon => FontAwesome.fa_osu_mod_hidden;
@@ -18,5 +19,27 @@ namespace osu.Game.Rulesets.Mania.Mods
         public override double ScoreMultiplier => 1;
         public override bool Ranked => true;
         public override Type[] IncompatibleMods => new[] { typeof(ModFlashlight<ManiaHitObject>) };
+        private const double fading_duration = 150;
+        private const double fading_offset_time = 300;
+
+        protected override void ApplyHiddenState(DrawableHitObject drawableHitObject, ArmedState state) {
+            var fadingStartTime = drawableHitObject.HitObject.StartTime - fading_duration - fading_offset_time;
+
+            switch (drawableHitObject) {
+                case DrawableHoldNoteTick holdNoteTick:
+                case DrawableNote note:
+                    drawableHitObject.Hide();
+                    using (drawableHitObject.BeginAbsoluteSequence(fadingStartTime, true))
+                        drawableHitObject.FadeIn(fading_duration);
+
+                    break;
+                case DrawableHoldNote holdNote:
+                    //TODO
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
