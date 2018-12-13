b6a8eff458432dc6b998ba5cce9de2368c6bf9a9
tangalbert919 <87902114+bubb13gummy@users.noreply.github.com>
2018-12-13 00:10:15 -0600

    Add Android project

---
+++ osu.Android/GameView.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using Android.Content;
+using Android.Util;
+using osu.Framework;
+using osu.Framework.Android;
+using osu.Game;
+
+namespace osu.Android
+{
+    public class GameView : AndroidGameView
+    {
+        public GameView(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            CreateGame();
+        }
+
+        public GameView(Context context) : base(context)
+        {
+            CreateGame();
+        }
+        public override Framework.Game CreateGame() => new OsuGame();
+    }
+}
---
+++ osu.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content.PM;
+
+namespace osu.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, SupportsPictureInPicture = false)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnBackPressed()
+        {
+            
+        }
+    }
+}
