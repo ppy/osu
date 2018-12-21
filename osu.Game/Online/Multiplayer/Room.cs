e27b7b0c0d1f594e8fd9d7933356c7cc9eb152aa
smoogipoo <smoogipoo@smgi.me>
2018-12-21 13:06:11 +0900

    Cleanup

--- osu.Game/Online/Multiplayer/Room.cs
+++ osu.Game/Online/Multiplayer/Room.cs
@@ -22,28 +22,28 @@ namespace osu.Game.Online.Multiplayer
         public Bindable<User> Host { get; private set; } = new Bindable<User>();
 
         [JsonProperty("playlist")]
-        public readonly BindableCollection<PlaylistItem> Playlist = new BindableCollection<PlaylistItem>();
+        public BindableCollection<PlaylistItem> Playlist { get; set; } = new BindableCollection<PlaylistItem>();
 
         [JsonIgnore]
-        public readonly Bindable<TimeSpan> Duration = new Bindable<TimeSpan>(TimeSpan.FromMinutes(30));
+        public Bindable<TimeSpan> Duration { get; private set; } = new Bindable<TimeSpan>(TimeSpan.FromMinutes(30));
 
         [JsonIgnore]
-        public readonly Bindable<int?> MaxAttempts = new Bindable<int?>();
+        public Bindable<int?> MaxAttempts { get; private set; } = new Bindable<int?>();
 
         [JsonIgnore]
-        public Bindable<RoomStatus> Status = new Bindable<RoomStatus>(new RoomStatusOpen());
+        public Bindable<RoomStatus> Status { get; private set; } = new Bindable<RoomStatus>(new RoomStatusOpen());
 
         [JsonIgnore]
-        public Bindable<RoomAvailability> Availability = new Bindable<RoomAvailability>();
+        public Bindable<RoomAvailability> Availability { get; private set; } = new Bindable<RoomAvailability>();
 
         [JsonIgnore]
-        public Bindable<GameType> Type = new Bindable<GameType>(new GameTypeTimeshift());
+        public Bindable<GameType> Type { get; private set; } = new Bindable<GameType>(new GameTypeTimeshift());
 
         [JsonIgnore]
-        public Bindable<int?> MaxParticipants = new Bindable<int?>();
+        public Bindable<int?> MaxParticipants { get; private set; } = new Bindable<int?>();
 
         [JsonIgnore]
-        public Bindable<IEnumerable<User>> Participants = new Bindable<IEnumerable<User>>(Enumerable.Empty<User>());
+        public Bindable<IEnumerable<User>> Participants { get; private set; } = new Bindable<IEnumerable<User>>(Enumerable.Empty<User>());
 
         [JsonProperty("duration")]
         private int duration
@@ -54,7 +54,7 @@ namespace osu.Game.Online.Multiplayer
 
         // Only supports retrieval for now
         [JsonProperty("ends_at")]
-        public Bindable<DateTimeOffset> EndDate = new Bindable<DateTimeOffset>();
+        public Bindable<DateTimeOffset> EndDate { get; private set; } = new Bindable<DateTimeOffset>();
 
         // Todo: Find a better way to do this (https://github.com/ppy/osu-framework/issues/1930)
         [JsonProperty("max_attempts", DefaultValueHandling = DefaultValueHandling.Ignore)]
