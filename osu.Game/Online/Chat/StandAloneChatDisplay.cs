1fdd56f32fea47a23f44a85629ea7841c6affaec
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-12-21 17:54:12 +0900

    Make standalone chat reuse more code from main chat overlay

--- osu.Game/Online/Chat/StandAloneChatDisplay.cs
+++ osu.Game/Online/Chat/StandAloneChatDisplay.cs
@@ -1,19 +1,15 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Containers;
-using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
-using osuTK;
+using osu.Game.Overlays.Chat;
 using osuTK.Graphics;
 
 namespace osu.Game.Online.Chat
@@ -23,15 +19,19 @@ namespace osu.Game.Online.Chat
     /// </summary>
     public class StandAloneChatDisplay : CompositeDrawable
     {
+        private readonly bool postingTextbox;
+
         public readonly Bindable<Channel> Channel = new Bindable<Channel>();
-
-        private readonly FillFlowContainer messagesFlow;
-
-        private Channel lastChannel;
 
         private readonly FocusedTextBox textbox;
 
         protected ChannelManager ChannelManager;
+
+        private ScrollContainer scroll;
+
+        private DrawableChannel drawableChannel;
+
+        private const float textbox_height = 30;
 
         /// <summary>
         /// Construct a new instance.
@@ -39,6 +39,7 @@ namespace osu.Game.Online.Chat
         /// <param name="postingTextbox">Whether a textbox for posting new messages should be displayed.</param>
         public StandAloneChatDisplay(bool postingTextbox = false)
         {
+            this.postingTextbox = postingTextbox;
             CornerRadius = 10;
             Masking = true;
 
@@ -50,23 +51,11 @@ namespace osu.Game.Online.Chat
                     Alpha = 0.8f,
                     RelativeSizeAxes = Axes.Both
                 },
-                messagesFlow = new FillFlowContainer
-                {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    LayoutEasing = Easing.Out,
-                    LayoutDuration = 500,
-                    Anchor = Anchor.BottomLeft,
-                    Origin = Anchor.BottomLeft,
-                    Direction = FillDirection.Vertical
-                }
             };
 
-            const float textbox_height = 30;
 
             if (postingTextbox)
             {
-                messagesFlow.Y -= textbox_height;
                 AddInternal(textbox = new FocusedTextBox
                 {
                     RelativeSizeAxes = Axes.X,
@@ -117,112 +106,43 @@ namespace osu.Game.Online.Chat
             this.MoveToY(100, 500, Easing.In);
         }
 
-        protected virtual Drawable CreateMessage(Message message)
-        {
-            return new StandAloneMessage(message);
-        }
+        protected virtual ChatLine CreateMessage(Message message) => new StandAloneMessage(message);
 
         private void channelChanged(Channel channel)
         {
-            if (lastChannel != null)
-                lastChannel.NewMessagesArrived -= newMessages;
-
-            lastChannel = channel;
-            messagesFlow.Clear();
+            drawableChannel?.Expire();
 
             if (channel == null) return;
 
-            channel.NewMessagesArrived += newMessages;
-
-            newMessages(channel.Messages);
+            AddInternal(drawableChannel = new StandAloneDrawableChannel(channel)
+            {
+                CreateChatLineAction = CreateMessage,
+                Padding = new MarginPadding { Bottom = postingTextbox ? textbox_height : 0 }
+            });
         }
 
-        private void newMessages(IEnumerable<Message> messages)
+        protected class StandAloneDrawableChannel : DrawableChannel
         {
-            var excessChildren = messagesFlow.Children.Count - 10;
-            if (excessChildren > 0)
-                foreach (var c in messagesFlow.Children.Take(excessChildren))
-                    c.Expire();
+            public Func<Message,ChatLine> CreateChatLineAction;
 
-            foreach (var message in messages)
+            protected override ChatLine CreateChatLine(Message m) => CreateChatLineAction(m);
+
+            public StandAloneDrawableChannel(Channel channel)
+                : base(channel)
             {
-                var formatted = MessageFormatter.FormatMessage(message);
-                var drawable = CreateMessage(formatted);
-                drawable.Y = messagesFlow.Height;
-                messagesFlow.Add(drawable);
+                ChatLineFlow.Padding = new MarginPadding { Horizontal = 0 };
             }
         }
 
-        protected class StandAloneMessage : CompositeDrawable
+        protected class StandAloneMessage : ChatLine
         {
-            protected readonly Message Message;
-            protected OsuSpriteText SenderText;
-            protected Circle ColourBox;
+            protected override float TextSize => 15;
 
-            public StandAloneMessage(Message message)
+            protected override float HorizontalPadding => 10;
+            protected override float MessagePadding => 120;
+
+            public StandAloneMessage(Message message) : base(message)
             {
-                Message = message;
-            }
-
-            [BackgroundDependencyLoader]
-            private void load()
-            {
-                Margin = new MarginPadding(3);
-
-                RelativeSizeAxes = Axes.X;
-                AutoSizeAxes = Axes.Y;
-
-                InternalChildren = new Drawable[]
-                {
-                    new FillFlowContainer
-                    {
-                        RelativeSizeAxes = Axes.X,
-                        AutoSizeAxes = Axes.Y,
-                        Direction = FillDirection.Horizontal,
-                        Children = new Drawable[]
-                        {
-                            new Container
-                            {
-                                RelativeSizeAxes = Axes.X,
-                                Width = 0.2f,
-                                Children = new Drawable[]
-                                {
-                                    SenderText = new OsuSpriteText
-                                    {
-                                        Font = @"Exo2.0-Bold",
-                                        Anchor = Anchor.TopRight,
-                                        Origin = Anchor.TopRight,
-                                        Text = Message.Sender.ToString()
-                                    }
-                                }
-                            },
-                            new Container
-                            {
-                                Size = new Vector2(8, OsuSpriteText.FONT_SIZE),
-                                Margin = new MarginPadding { Horizontal = 3 },
-                                Children = new Drawable[]
-                                {
-                                    ColourBox = new Circle
-                                    {
-                                        Anchor = Anchor.Centre,
-                                        Origin = Anchor.Centre,
-                                        Size = new Vector2(8)
-                                    }
-                                }
-                            },
-                            new OsuTextFlowContainer
-                            {
-                                RelativeSizeAxes = Axes.X,
-                                AutoSizeAxes = Axes.Y,
-                                Width = 0.5f,
-                                Text = Message.DisplayContent
-                            }
-                        }
-                    }
-                };
-
-                if (!string.IsNullOrEmpty(Message.Sender.Colour))
-                    SenderText.Colour = ColourBox.Colour = OsuColour.FromHex(Message.Sender.Colour);
             }
         }
     }
