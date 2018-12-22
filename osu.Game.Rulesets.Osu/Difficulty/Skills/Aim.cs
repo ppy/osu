c848c83d0daa5e26cf82bb8c245613e98802d15c
smoogipoo <smoogipoo@smgi.me>
2018-12-22 09:56:33 +0900

    Refactor for readability + performance

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -14,8 +14,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private const double angle_bonus_begin = 5 * Math.PI / 12;
         private const double timing_threshold = 107;
         private const double min_distance_for_bonus = 90;
-
         private const double angle_threshold = Math.PI / 4;
+
+        private static readonly double sin_angle_threshold = Math.Sin(angle_threshold);
 
         protected override double SkillMultiplier => 26.25;
         protected override double StrainDecayBase => 0.15;
@@ -28,23 +29,33 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             {
                 if (current.Angle != null && current.Angle.Value > angle_bonus_begin)
                 {
-                    var angleBonus = Math.Sqrt(
+                    var sinDiffAngle = Math.Sin(current.Angle.Value - angle_bonus_begin);
+
+                    var angleBonus = Math.Sqrt
+                    (
                         Math.Max(0, Previous[0].JumpDistance + Previous[0].TravelDistance - min_distance_for_bonus)
-                        * Math.Min(
-                            Math.Sin(current.Angle.Value - angle_bonus_begin),
-                            Math.Sin(angle_threshold))
-                        * (Math.Max(0, current.JumpDistance - min_distance_for_bonus)
-                           * Math.Min(
-                               Math.Sin(current.Angle.Value - angle_bonus_begin),
-                               Math.Sin(angle_threshold))));
+                        * Math.Min
+                        (
+                            sinDiffAngle,
+                            sin_angle_threshold
+                        )
+                        * Math.Max(0, current.JumpDistance - min_distance_for_bonus)
+                        * Math.Min
+                        (
+                            sinDiffAngle,
+                            sin_angle_threshold
+                        )
+                    );
 
                     result = 2 * Math.Pow(Math.Max(0, angleBonus), 0.99) / Math.Max(Previous[0].StrainTime, timing_threshold);
                 }
             }
 
-            return Math.Max(
+            return Math.Max
+            (
                 result + (Math.Pow(current.TravelDistance, 0.99) + Math.Pow(current.JumpDistance, 0.99)) / Math.Max(current.StrainTime, timing_threshold),
-                (Math.Pow(current.TravelDistance, 0.99) + Math.Pow(current.JumpDistance, 0.99)) / current.StrainTime);
+                (Math.Pow(current.TravelDistance, 0.99) + Math.Pow(current.JumpDistance, 0.99)) / current.StrainTime
+            );
         }
     }
 }
