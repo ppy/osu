1ac97f2c60b978b6c9b3ba9774e6d7116c7bb5e1
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2018-12-22 09:56:33 +0900

    Refactor for readability + performance

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -13,6 +13,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     {
         private const double angle_bonus_begin = 3 * Math.PI / 4;
         private const double pi_over_4 = Math.PI / 4;
+        private const double pi_over_2 = Math.PI / 2;
+        private const double max_distance_for_bonus = 90;
+
+        private static readonly double sin_pi_over_4 = Math.Sin(pi_over_4);
 
         protected override double SkillMultiplier => 1400;
         protected override double StrainDecayBase => 0.3;
@@ -33,11 +37,24 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             double angleBonus = 1.0;
             if (current.Angle != null && current.Angle.Value < angle_bonus_begin)
             {
-                angleBonus = 1 + Math.Min(Math.Sin(angle_bonus_begin - current.Angle.Value), Math.Sin(Math.PI / 4)) / 2.5;
-                if (distance < 90 && current.Angle.Value < Math.PI / 4)
-                    angleBonus += (1 - angleBonus) * Math.Min((90 - distance) / 10, 1);
-                else if (distance < 90 && current.Angle.Value < Math.PI / 2)
-                    angleBonus += (1 - angleBonus) * Math.Min((90 - distance) / 10, 1) * Math.Sin((Math.PI / 2 - current.Angle.Value) / pi_over_4);
+                angleBonus = 1 + Math.Min(Math.Sin(angle_bonus_begin - current.Angle.Value), sin_pi_over_4) / 2.5;
+
+                if (distance < max_distance_for_bonus)
+                {
+                    if (current.Angle.Value < pi_over_4)
+                    {
+                        angleBonus +=
+                            (1 - angleBonus)
+                            * Math.Min((max_distance_for_bonus - distance) / 10, 1);
+                    }
+                    else if (current.Angle.Value < pi_over_2)
+                    {
+                        angleBonus +=
+                            (1 - angleBonus)
+                            * Math.Min((max_distance_for_bonus - distance) / 10, 1)
+                            * Math.Sin((pi_over_2 - current.Angle.Value) / pi_over_4);
+                    }
+                }
             }
 
             return speedBonus * angleBonus * (0.95 + Math.Pow(distance / SINGLE_SPACING_THRESHOLD, 4)) / current.StrainTime;
