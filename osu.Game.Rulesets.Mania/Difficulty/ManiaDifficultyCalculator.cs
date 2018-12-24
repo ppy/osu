ec74bbb6ba4f870c8da44cf5ef98203873c0c340
Poyo <poyoarya@gmail.com>
2018-12-24 00:32:34 -0800

    Remove extra newline
    useful commit, yes, thabks

--- osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyCalculator.cs
+++ osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyCalculator.cs
@@ -52,8 +52,7 @@ namespace osu.Game.Rulesets.Mania.Difficulty
 
             if (!calculateStrainValues(difficultyHitObjects, timeRate))
                 return new DifficultyAttributes(mods, 0);
-
-
+            
             double starRating = calculateDifficulty(difficultyHitObjects, timeRate) * star_scaling_factor;
 
             return new ManiaDifficultyAttributes(mods, starRating)
