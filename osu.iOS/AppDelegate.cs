49527df5659f598ead7d6a7ffba885b762d7931a
Shivam Ramkisoen <s.r.v.ramkisoen@umail.leidenuniv.nl>
2018-12-25 16:37:33 +0100

    Non-working importer code

--- osu.iOS/AppDelegate.cs
+++ osu.iOS/AppDelegate.cs
@@ -1,15 +1,39 @@
-﻿using Foundation;
+﻿using System;
+using System.IO;
+using Foundation;
+using osu.Framework;
 using osu.Framework.iOS;
 using osu.Game;
+using osu.Game.Beatmaps;
+using osu.Game.Database;
+using osu.Game.IO.Archives;
 
 namespace osu.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+
     [Register("AppDelegate")]
     public class AppDelegate : GameAppDelegate
     {
         protected override Framework.Game CreateGame() => new OsuGame();
+        private BeatmapManager beatmap = null;
+
+        [Export("application:openURL:options:")]
+
+        override public bool OpenUrl(UIKit.UIApplication app, NSUrl url, NSDictionary options)
+        {
+
+            Console.WriteLine(url.Path);
+            string path = url.Path;
+            //Stream beatmapToLoad = new FileStream(path, FileMode.Open, FileAccess.Read);
+            beatmap.Import(path);
+            return true;
+        }
+
+
     }
+
+
 }
 
