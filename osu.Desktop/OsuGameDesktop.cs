0210b6eaa9805b4f7e8b902970fbe64da9a0f8cf
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2018-12-27 19:23:58 +0900

    And some more

--- osu.Desktop/OsuGameDesktop.cs
+++ osu.Desktop/OsuGameDesktop.cs
@@ -15,12 +15,16 @@ using Microsoft.Win32;
 using osu.Desktop.Updater;
 using osu.Framework;
 using osu.Framework.Platform.Windows;
+using osu.Framework.Screens;
+using osu.Game.Screens;
+using osu.Game.Screens.Menu;
 
 namespace osu.Desktop
 {
     internal class OsuGameDesktop : OsuGame
     {
         private readonly bool noVersionOverlay;
+        private VersionManager versionManager;
 
         public OsuGameDesktop(string[] args = null)
             : base(args)
@@ -46,7 +50,7 @@ namespace osu.Desktop
 
             if (!noVersionOverlay)
             {
-                LoadComponentAsync(new VersionManager { Depth = int.MinValue }, v =>
+                LoadComponentAsync(versionManager = new VersionManager { Depth = int.MinValue }, v =>
                 {
                     Add(v);
                     v.State = Visibility.Visible;
@@ -56,6 +60,21 @@ namespace osu.Desktop
                     Add(new SquirrelUpdateManager());
                 else
                     Add(new SimpleUpdateManager());
+            }
+        }
+
+        protected override void ScreenChanged(OsuScreen current, Screen newScreen)
+        {
+            base.ScreenChanged(current, newScreen);
+            switch (newScreen)
+            {
+                case Intro _:
+                case MainMenu _:
+                    versionManager.State = Visibility.Visible;
+                    break;
+                default:
+                    versionManager.State = Visibility.Hidden;
+                    break;
             }
         }
 
