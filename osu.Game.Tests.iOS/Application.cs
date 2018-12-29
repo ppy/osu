9f22125860befc92ca3d58ec3c9d871fe0bdc5a1
Shane Woolcock <87902114+bubb13gummy@users.noreply.github.com>
2018-12-29 18:47:35 +1030

    Add basic visual tests

---
+++ osu.Game.Tests.iOS/Application.cs
@@ -0,0 +1,16 @@
+﻿using UIKit;
+
+namespace osu.Game.Tests.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
+
