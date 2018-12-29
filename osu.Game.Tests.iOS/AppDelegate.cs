ff4b41b35febd8fcfa8ca979eaaccbab35f9a41c
Shane Woolcock <samahnub@gmail.com>
2018-12-29 18:47:35 +1030

    Add basic visual tests

---
+++ osu.Game.Tests.iOS/AppDelegate.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using osu.Framework.iOS;
+
+namespace osu.Game.Tests.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : GameAppDelegate
+    {
+        protected override Framework.Game CreateGame() => new OsuTestBrowser();
+    }
+}
+
