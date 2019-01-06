ddce608f97977d5dd6b5dd7ebf8eaff4ac9cda6e
tangalbert919 <tangalbert919@gmail.com>
2019-01-05 22:15:09 -0600

    Setup osu.Android solution and its ruleset tests

---
+++ osu.Game.Rulesets.Osu.Tests.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace osu.Game.Rulesets.Osu.Tests.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
