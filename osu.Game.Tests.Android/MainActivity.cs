5fdf4ee67b3c4e3c8c97625cdbf858c3e92faeec
tangalbert919 <tangalbert919@gmail.com>
2019-01-05 23:18:12 -0600

    Add Android's VisualTests project

---
+++ osu.Game.Tests.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using Android.App;
+using Android.Content.PM;
+using osu.Framework.Android;
+
+namespace osu.Game.Tests.Android
+{
+    [Activity(Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.SensorLandscape, SupportsPictureInPicture = false, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override Framework.Game CreateGame()
+            => new OsuTestBrowser();
+    }
+}
