e5dd95198a283282936bb76aa463869c84cd22a2
tangalbert919 <tangalbert919@gmail.com>
2019-01-05 23:17:55 -0600

    Cleanup project files with common props

--- osu.Game.Rulesets.Mania.Tests.Android/MainActivity.cs
+++ osu.Game.Rulesets.Mania.Tests.Android/MainActivity.cs
@@ -1,19 +1,17 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using Android.App;
+using Android.Content.PM;
+using osu.Framework.Android;
+using osu.Game.Tests;
 
 namespace osu.Game.Rulesets.Mania.Tests.Android
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.SensorLandscape, SupportsPictureInPicture = false, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AndroidGameActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-        }
+        protected override Framework.Game CreateGame()
+            => new OsuTestBrowser();
     }
 }
