c16f5b7e019f7ac6548f488a0c535ec60c3b2c9e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-01-08 14:56:42 +0900

    Use better logic for setting slider bar settings

--- osu.Game/Overlays/Settings/SettingsItem.cs
+++ osu.Game/Overlays/Settings/SettingsItem.cs
@@ -23,7 +23,7 @@ namespace osu.Game.Overlays.Settings
     {
         protected abstract Drawable CreateControl();
 
-        public Drawable Control { get; }
+        protected Drawable Control { get; }
 
         private IHasCurrentValue<T> controlWithCurrent => Control as IHasCurrentValue<T>;
 
