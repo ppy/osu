e6f26a73610651a7d83b7bc7ac273a4b0acf4372
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-01-08 19:26:24 +0900

    Remove unused using

--- osu.Game/Screens/Multi/Lounge/Components/RoomInspector.cs
+++ osu.Game/Screens/Multi/Lounge/Components/RoomInspector.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System.Threading;
 using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using osu.Framework.Extensions.Color4Extensions;
