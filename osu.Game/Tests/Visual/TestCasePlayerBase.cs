fa49ccdf4410025dcef0cf0f6173aaca7a00a896
David Zhao <87902114+bubb13gummy@users.noreply.github.com>
2019-01-24 14:46:36 +0900

    Fix auto-generated lisence header

--- osu.Game/Tests/Visual/TestCasePlayerBase.cs
+++ osu.Game/Tests/Visual/TestCasePlayerBase.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-2019 ppy Pty Ltd <contact@ppy.sh>.
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 namespace osu.Game.Tests.Visual
