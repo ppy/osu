03e57d0423fa25fce086af404a8304b40ec6347a
LeNitrous <n20gaming2000@gmail.com>
2019-01-26 20:15:19 +0800

    fix codefactor warnings and derive ModWindUp and ModWindDown to ModTimeRamp

--- osu.Game/Rulesets/Mods/ModDoubleTime.cs
+++ osu.Game/Rulesets/Mods/ModDoubleTime.cs
@@ -15,7 +15,7 @@ namespace osu.Game.Rulesets.Mods
         public override ModType Type => ModType.DifficultyIncrease;
         public override string Description => "Zoooooooooom...";
         public override bool Ranked => true;
-        public override Type[] IncompatibleMods => new[] { typeof(ModHalfTime), typeof(ModWindUp) };
+        public override Type[] IncompatibleMods => new[] { typeof(ModHalfTime), typeof(ModTimeRamp) };
 
         public virtual void ApplyToClock(IAdjustableClock clock)
         {
