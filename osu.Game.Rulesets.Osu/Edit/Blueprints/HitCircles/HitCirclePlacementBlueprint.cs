2fb4bc0fd5b2d4472a74618e70a22d44f2266bc7
ProgrammaticNajel <87902114+bubb13gummy@users.noreply.github.com>
2019-01-26 22:55:33 +0800

    Add null check

--- osu.Game.Rulesets.Osu/Edit/Blueprints/HitCircles/HitCirclePlacementBlueprint.cs
+++ osu.Game.Rulesets.Osu/Edit/Blueprints/HitCircles/HitCirclePlacementBlueprint.cs
@@ -5,6 +5,7 @@ using osu.Framework.Input.Events;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Osu.Edit.Blueprints.HitCircles.Components;
 using osu.Game.Rulesets.Osu.Objects;
+using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Edit.Blueprints.HitCircles
 {
@@ -23,7 +24,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.HitCircles
             base.LoadComplete();
 
             // Fixes a 1-frame position discrepancy due to the first mouse move event happening in the next frame
-            HitObject.Position = Parent.ToLocalSpace(GetContainingInputManager().CurrentState.Mouse.Position);
+            HitObject.Position = Parent?.ToLocalSpace(GetContainingInputManager().CurrentState.Mouse.Position)?? Vector2.Zero;
         }
 
         protected override bool OnClick(ClickEvent e)
