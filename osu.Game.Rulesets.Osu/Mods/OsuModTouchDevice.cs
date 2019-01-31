ebf2e6e1243f73d66e28c385feca4293276adadd
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-01-31 11:51:40 +0900

    Add touchdevice mod to performance calculation

---
+++ osu.Game.Rulesets.Osu/Mods/OsuModTouchDevice.cs
@@ -0,0 +1,14 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Osu.Mods
+{
+    public class OsuModTouchDevice : Mod
+    {
+        public override string Name => "Touch Device";
+        public override string Acronym => "TD";
+        public override double ScoreMultiplier => 1;
+    }
+}
