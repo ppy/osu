f6318d36706e0ea69a7d97b106bdc7246ba7c3d3
HoLLy <dario.db@hotmail.com>
2019-02-01 13:06:36 +0100

    Add comment explaining *= 0.8f

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -41,6 +41,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty
             var catcher = new CatcherArea.Catcher(beatmap.BeatmapInfo.BaseDifficulty);
             float halfCatchWidth = catcher.CatchWidth * 0.5f;
 
+            // We're only using 80% of the catcher's width to simulate imperfect gameplay.
             halfCatchWidth *= 0.8f;
 
             var difficultyHitObjects = new List<CatchDifficultyHitObject>();
