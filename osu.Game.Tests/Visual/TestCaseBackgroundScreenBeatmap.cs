ad9dae975dc3e98fef2084f997791383a3c128a9
David Zhao <nyquill@ppy.sh>
2019-02-15 16:38:27 +0900

    Add visual test for background dimming

--- osu.Game.Tests/Visual/TestCaseBackgroundScreenBeatmap.cs
+++ osu.Game.Tests/Visual/TestCaseBackgroundScreenBeatmap.cs
@@ -2,7 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using NUnit.Framework.Internal;
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Game.Configuration;
 using osu.Game.Graphics;
@@ -13,20 +13,23 @@ using osu.Game.Screens.Play;
 
 namespace osu.Game.Tests.Visual
 {
+    [TestFixture]
     public class TestCaseBackgroundScreenBeatmap : TestCasePlayer
     {
-
-        [BackgroundDependencyLoader]
-        private void load(OsuConfigManager manager)
-        {
-            LoadScreen(new DimAccessiblePlayer());
-        }
-
         [Test]
         public void EnableUserDimTest()
         {
             AddStep("Test User Dimming", () => ((DimAccessiblePlayer)Player).EnableScreenDim());
+            AddWaitStep(5, "Wait for dim");
             AddAssert("Check screen dim", () => ((DimAccessiblePlayer)Player).AssertDimState());
+        }
+
+        [Test]
+        public void DisableUserDimTest()
+        {
+            AddStep("Test User Dimming", () => ((DimAccessiblePlayer)Player).DisableScreenDim());
+            AddWaitStep(5, "Wait for dim");
+            AddAssert("Check screen dim", () => ((DimAccessiblePlayer)Player).AssertUndimmed());
         }
 
         protected override Player CreatePlayer(Ruleset ruleset) => new DimAccessiblePlayer();
@@ -49,17 +52,23 @@ namespace osu.Game.Tests.Visual
                 return ((FadeAccessibleBackground)Background).AssertDimState();
             }
 
+            public bool AssertUndimmed()
+            {
+                return ((FadeAccessibleBackground)Background).AssertUndimmed();
+            }
+
             private class FadeAccessibleBackground : BackgroundScreenBeatmap
             {
                 public bool AssertDimState()
                 {
                     return FadeContainer.Colour == OsuColour.Gray(BackgroundOpacity);
                 }
+
+                public bool AssertUndimmed()
+                {
+                    return FadeContainer.Colour == OsuColour.Gray(1.0f);
+                }
             }
         }
-    }
-
-    internal class SetupAttribute : Attribute
-    {
     }
 }
