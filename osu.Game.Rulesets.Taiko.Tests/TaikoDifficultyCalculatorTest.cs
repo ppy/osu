17368c6ad1409b4b4124144733a14af6b9d72c10
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-02-15 14:44:26 +0900

    Add taiko difficulty calculator tests

---
+++ osu.Game.Rulesets.Taiko.Tests/TaikoDifficultyCalculatorTest.cs
@@ -0,0 +1,27 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Difficulty;
+using osu.Game.Rulesets.Taiko.Difficulty;
+using osu.Game.Tests.Beatmaps;
+
+namespace osu.Game.Rulesets.Taiko.Tests
+{
+    public class TaikoDifficultyCalculatorTest : DifficultyCalculatorTest
+    {
+        protected override string ResourceAssembly => "osu.Game.Rulesets.Taiko";
+
+        [TestCase(2.9811336589467095, "diffcalc-test")]
+        [TestCase(2.9811336589467095, "diffcalc-test-strong")]
+        public void Test(double expected, string name)
+        {
+            base.Test(expected, name);
+        }
+
+        protected override DifficultyCalculator CreateDifficultyCalculator(WorkingBeatmap beatmap) => new TaikoDifficultyCalculator(new TaikoRuleset(), beatmap);
+
+        protected override Ruleset CreateRuleset() => new TaikoRuleset();
+    }
+}
