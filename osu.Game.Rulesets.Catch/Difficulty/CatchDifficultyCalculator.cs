241d93143e7082add334bdab3307f2ee32d9cb84
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-02-18 14:48:19 +0900

    Fix incorrect hitobject being used as the last hitobject

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -60,14 +60,16 @@ namespace osu.Game.Rulesets.Catch.Difficulty
                     // We want to only consider fruits that contribute to the combo. Droplets are addressed as accuracy and spinners are not relevant for "skill" calculations.
                     case Fruit fruit:
                         yield return new CatchDifficultyHitObject(fruit, lastObject, timeRate, halfCatchWidth);
+                        lastObject = hitObject;
                         break;
                     case JuiceStream _:
                         foreach (var nested in hitObject.NestedHitObjects.OfType<CatchHitObject>().Where(o => !(o is TinyDroplet)))
+                        {
                             yield return new CatchDifficultyHitObject(nested, lastObject, timeRate, halfCatchWidth);
+                            lastObject = nested;
+                        }
                         break;
                 }
-
-                lastObject = hitObject;
             }
         }
 
