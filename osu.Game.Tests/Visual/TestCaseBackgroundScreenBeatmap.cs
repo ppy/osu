fc3f97072643a1c16d88f350e2721ff55d311e51
David Zhao <87902114+bubb13gummy@users.noreply.github.com>
2019-02-18 19:53:55 +0900

    Add test cases for transitioning into pause overlay and into results

--- osu.Game.Tests/Visual/TestCaseBackgroundScreenBeatmap.cs
+++ osu.Game.Tests/Visual/TestCaseBackgroundScreenBeatmap.cs
@@ -10,6 +10,9 @@ using osu.Game.Scoring;
 using osu.Game.Screens;
 using osu.Game.Screens.Backgrounds;
 using osu.Game.Screens.Play;
+using osu.Game.Screens.Select;
+using osu.Game.Users;
+using osuTK.Graphics;
 
 namespace osu.Game.Tests.Visual
 {
@@ -27,17 +30,6 @@ namespace osu.Game.Tests.Visual
         }
 
         /// <summary>
-        /// Check if the fade container is properly being faded when screen dim is enabled.
-        /// </summary>
-        [Test]
-        public void EnableUserDimTest()
-        {
-            AddStep("Test User Dimming", () => ((DimAccessiblePlayer)Player).DimEnabled.Value = true);
-            AddWaitStep(5, "Wait for dim");
-            AddAssert("Check screen dim", () => ((DimAccessiblePlayer)Player).AssertDimState());
-        }
-
-        /// <summary>
         /// Check if the fade container is properly being reset when screen dim is disabled.
         /// </summary>
         [Test]
@@ -45,10 +37,52 @@ namespace osu.Game.Tests.Visual
         {
             AddStep("Test User Undimming", () => ((DimAccessiblePlayer)Player).DimEnabled.Value = false);
             AddWaitStep(5, "Wait for dim");
-            AddAssert("Check screen dim", () => ((DimAccessiblePlayer)Player).AssertUndimmed());
+            AddAssert("Screen is undimmed", () => ((DimAccessiblePlayer)Player).AssertUndimmed());
+        }
+
+        /// <summary>
+        /// Check if the fade container is properly being faded when screen dim is enabled.
+        /// </summary>
+        [Test]
+        public void EnableUserDimTest()
+        {
+            AddStep("Test User Dimming", () => ((DimAccessiblePlayer)Player).DimEnabled.Value = true);
+            AddWaitStep(5, "Wait for dim");
+            AddAssert("Screen is dimmed", () => ((DimAccessiblePlayer)Player).AssertDimState());
+        }
+
+        /// <summary>
+        /// Check if the fade container retains dim when pausing
+        /// </summary>
+        [Test]
+        public void PauseTest()
+        {
+            AddStep("Transition to Results", () => ((DimAccessiblePlayer)Player).TriggerExit());
+            AddWaitStep(5, "Wait for dim");
+            AddAssert("Screen is dimmed", () => ((DimAccessiblePlayer)Player).AssertDimState());
+        }
+
+        /// <summary>
+        /// Check if the fade container removes user dim when leaving the player
+        /// </summary>
+        [Test]
+        public void TransitionTest()
+        {
+            AddStep("Transition to Results", () => LoadScreen(new FadeAccesibleResults(new ScoreInfo { User = new User { Username = "osu!" }})));
+            AddWaitStep(5, "Wait for dim");
+            AddAssert("Screen is undimmed", () => ((DimAccessiblePlayer)Player).AssertUndimmed());
         }
 
         protected override Player CreatePlayer(Ruleset ruleset) => new DimAccessiblePlayer();
+
+        private class FadeAccesibleResults : SoloResults
+        {
+            public FadeAccesibleResults(ScoreInfo score) : base(score)
+            {
+            }
+
+            protected override BackgroundScreen CreateBackground() => new FadeAccessibleBackground();
+        }
 
         private class DimAccessiblePlayer : Player
         {
@@ -71,17 +105,22 @@ namespace osu.Game.Tests.Visual
                 return ((FadeAccessibleBackground)Background).AssertUndimmed();
             }
 
-            private class FadeAccessibleBackground : BackgroundScreenBeatmap
+            public void TriggerExit()
             {
-                public bool AssertDimState()
-                {
-                    return FadeContainer.Colour == OsuColour.Gray(1 - (float)DimLevel);
-                }
+                OnExiting(new PlaySongSelect());
+            }
+        }
 
-                public bool AssertUndimmed()
-                {
-                    return FadeContainer.Colour == OsuColour.Gray(1.0f);
-                }
+        private class FadeAccessibleBackground : BackgroundScreenBeatmap
+        {
+            public bool AssertDimState()
+            {
+                return FadeContainer.Colour == OsuColour.Gray(1 - (float)DimLevel);
+            }
+
+            public bool AssertUndimmed()
+            {
+                return FadeContainer.Colour == Color4.White;
             }
         }
     }
