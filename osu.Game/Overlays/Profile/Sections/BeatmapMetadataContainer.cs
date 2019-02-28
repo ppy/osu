99e2e6cf1c6e23b12b8736b263d6580653aa49d2
Dean Herbert <pe@ppy.sh>
2019-02-28 14:46:52 +0900

    Align text

--- osu.Game/Overlays/Profile/Sections/BeatmapMetadataContainer.cs
+++ osu.Game/Overlays/Profile/Sections/BeatmapMetadataContainer.cs
@@ -47,7 +47,8 @@ namespace osu.Game.Overlays.Profile.Sections
                 {
                     new OsuSpriteText
                     {
-                        Text = new LocalisedString(($"{beatmap.Metadata.TitleUnicode ?? beatmap.Metadata.Title} [{beatmap.Version}] ",
+                        Text = new LocalisedString((
+                            $"{beatmap.Metadata.TitleUnicode ?? beatmap.Metadata.Title} [{beatmap.Version}] ",
                             $"{beatmap.Metadata.Title ?? beatmap.Metadata.TitleUnicode} [{beatmap.Version}] ")),
                         Font = OsuFont.GetFont(size: 15, weight: FontWeight.SemiBold, italics: true)
                     },
