f463cd1861d9aca20c98fde4b2e11a5defb0c1e5
LeNitrous <87902114+bubb13gummy@users.noreply.github.com>
2019-03-03 16:30:08 +0800

    trim whitespace

--- osu.Game/Rulesets/Mods/ModTimeRamp.cs
+++ osu.Game/Rulesets/Mods/ModTimeRamp.cs
@@ -39,7 +39,6 @@ namespace osu.Game.Rulesets.Mods
         public virtual void Update(Playfield playfield)
         {
             double newRate;
-            
             if (1 + AppendRate < 1)
                 newRate = Math.Max(1 + AppendRate, 1 + (AppendRate * (clock.CurrentTime / (lastObjectEndTime * 0.75))));
             else
