68b658c52005851705a271930c51be47e901f9f9
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-03-06 14:36:30 +0900

    Implement sorcerer's diffcalc changes

--- osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
@@ -14,7 +14,8 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
     {
         private const float absolute_player_positioning_error = 16f;
         private const float normalized_hitobject_radius = 41.0f;
-        private const double direction_change_bonus = 12.5;
+        private const double direction_change_bonus = 9.5;
+        private const double antiflow_bonus = 25.0;
 
         protected override double SkillMultiplier => 850;
         protected override double StrainDecayBase => 0.2;
@@ -23,6 +24,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
         private float? lastPlayerPosition;
         private float lastDistanceMoved;
+        private double lastStrainTime;
 
         protected override double StrainValueOf(DifficultyHitObject current)
         {
@@ -39,8 +41,11 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
             float distanceMoved = playerPosition - lastPlayerPosition.Value;
 
-            double distanceAddition = Math.Pow(Math.Abs(distanceMoved), 1.3) / 500;
-            double sqrtStrain = Math.Sqrt(catchCurrent.StrainTime);
+            // Reduce speed scaling
+            double weightedStrainTime = catchCurrent.StrainTime + 20;
+
+            double distanceAddition = Math.Pow(Math.Abs(distanceMoved), 1.3) / 600;
+            double sqrtStrain = Math.Sqrt(weightedStrainTime);
 
             double bonus = 0;
 
@@ -53,33 +58,50 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
                     distanceAddition += direction_change_bonus / sqrtStrain * bonusFactor;
 
-                    // Bonus for tougher direction switches and "almost" hyperdashes at this point
-                    if (catchCurrent.LastObject.DistanceToHyperDash <= 10 / CatchPlayfield.BASE_WIDTH)
-                        bonus = 0.3 * bonusFactor;
+                    // Direction changes after jumps (antiflow) are harder
+                    double antiflowBonusFactor = Math.Min(Math.Sqrt(Math.Abs(distanceMoved)) / 10, 1);
+
+                    distanceAddition += (antiflow_bonus / sqrtStrain) * (Math.Sqrt(Math.Abs(lastDistanceMoved)) / (lastStrainTime / 40 + 10.0)) * antiflowBonusFactor;
                 }
 
                 // Base bonus for every movement, giving some weight to streams.
-                distanceAddition += 7.5 * Math.Min(Math.Abs(distanceMoved), normalized_hitobject_radius * 2) / (normalized_hitobject_radius * 6) / sqrtStrain;
+                distanceAddition += 10.0 * Math.Min(Math.Abs(distanceMoved), normalized_hitobject_radius * 2) / (normalized_hitobject_radius * 6) / sqrtStrain;
             }
 
-            // Bonus for "almost" hyperdashes at corner points
-            if (catchCurrent.LastObject.DistanceToHyperDash <= 10.0f / CatchPlayfield.BASE_WIDTH)
+            // Big bonus for edge hyperdashes
+            if (catchCurrent.LastObject.DistanceToHyperDash <= 14.0f / CatchPlayfield.BASE_WIDTH)
             {
                 if (!catchCurrent.LastObject.HyperDash)
-                    bonus += 1.0;
+                    bonus += 5.0;
                 else
                 {
                     // After a hyperdash we ARE in the correct position. Always!
                     playerPosition = catchCurrent.NormalizedPosition;
                 }
 
-                distanceAddition *= 1.0 + bonus * ((10 - catchCurrent.LastObject.DistanceToHyperDash * CatchPlayfield.BASE_WIDTH) / 10);
+                distanceAddition *= 1.0 + bonus * (14 - catchCurrent.LastObject.DistanceToHyperDash * CatchPlayfield.BASE_WIDTH) / 14 * (Math.Min(catchCurrent.StrainTime, 180) / 180); // Edge dashes are easier at lower ms values
+            }
+
+            // Prevent wide, dense stacks of notes which fit on the catcher from greatly increasing SR
+            if (Math.Abs(distanceMoved) > 0.1)
+            {
+                if (Math.Abs(lastDistanceMoved) > 0.1 && Math.Sign(distanceMoved) != Math.Sign(lastDistanceMoved))
+                {
+                    if (Math.Abs(distanceMoved) <= (CatcherArea.CATCHER_SIZE) && Math.Abs(lastDistanceMoved) == Math.Abs(distanceMoved))
+                    {
+                        if (catchCurrent.StrainTime <= 80 && lastStrainTime == catchCurrent.StrainTime)
+                        {
+                            distanceAddition *= Math.Max(((catchCurrent.StrainTime / 80) - 0.75) * 4, 0);
+                        }
+                    }
+                }
             }
 
             lastPlayerPosition = playerPosition;
             lastDistanceMoved = distanceMoved;
+            lastStrainTime = catchCurrent.StrainTime;
 
-            return distanceAddition / catchCurrent.StrainTime;
+            return distanceAddition / weightedStrainTime;
         }
     }
 }
