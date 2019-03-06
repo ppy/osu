1bb6e3e28f450e339610f76d493b2aa7bda6d97f
David Zhao <nyquill@ppy.sh>
2019-03-06 11:04:23 +0900

    xmldoc for OsuScreenStack

--- osu.Game/Screens/OsuScreenStack.cs
+++ osu.Game/Screens/OsuScreenStack.cs
@@ -21,6 +21,13 @@ namespace osu.Game.Screens
             ScreenExited += unbindAllDependencies;
         }
 
+
+        /// <summary> Return all child dependency bindables created by the exiting screen </summary>
+        /// <remarks>
+        /// While all bindables will eventually be returned by disposal logic,
+        /// Bindables that are created by every OsuScreen, namely ones created in <see cref="OsuScreenDependencies"/>, will not be returned in time.
+        /// We need to return them manually after OnExiting runs to ensure a new instance of the same screen can use these bindables immediately.
+        /// </remarks>>
         private void unbindAllDependencies(IScreen prev, IScreen next)
         {
             var beatmap = (prev as OsuScreen)?.Beatmap;
