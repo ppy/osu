b2973fe90697dfbac10c536cb040bfca7f0ff245
Jamie Taylor <87902114+bubb13gummy@users.noreply.github.com>
2019-03-06 17:13:28 +0900

    Fix some nullref crashes when escaping out of new room creation

--- osu.Game/Screens/Select/MatchSongSelect.cs
+++ osu.Game/Screens/Select/MatchSongSelect.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Humanizer;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -61,7 +62,7 @@ namespace osu.Game.Screens.Select
                 return true;
 
             Beatmap.Value = beatmaps.GetWorkingBeatmap(CurrentItem.Value?.Beatmap);
-            Beatmap.Value.Mods.Value = selectedMods.Value = CurrentItem.Value?.RequiredMods;
+            Beatmap.Value.Mods.Value = selectedMods.Value = CurrentItem.Value?.RequiredMods ?? Enumerable.Empty<Mod>();
             Ruleset.Value = CurrentItem.Value?.Ruleset;
 
             Beatmap.Disabled = true;
