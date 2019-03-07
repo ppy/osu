e13516c2bace6447459569353f5ee11076e70395
Jamie Taylor <me@nekodex.net>
2019-03-07 14:09:28 +0900

    Only attempt to restore CurrentItem state if present

--- osu.Game/Screens/Select/MatchSongSelect.cs
+++ osu.Game/Screens/Select/MatchSongSelect.cs
@@ -60,9 +60,12 @@ namespace osu.Game.Screens.Select
             if (base.OnExiting(next))
                 return true;
 
-            Beatmap.Value = beatmaps.GetWorkingBeatmap(CurrentItem.Value?.Beatmap);
-            Beatmap.Value.Mods.Value = selectedMods.Value = CurrentItem.Value?.RequiredMods;
-            Ruleset.Value = CurrentItem.Value?.Ruleset;
+            if (CurrentItem.Value != null)
+            {
+                Ruleset.Value = CurrentItem.Value.Ruleset;
+                Beatmap.Value = beatmaps.GetWorkingBeatmap(CurrentItem.Value.Beatmap);
+                Beatmap.Value.Mods.Value = selectedMods.Value = CurrentItem.Value.RequiredMods;
+            }
 
             Beatmap.Disabled = true;
             Ruleset.Disabled = true;
