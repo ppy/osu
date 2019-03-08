5344bf440f0c68a2732d5a71a49c352d3a43f7fe
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-03-08 20:13:55 +0900

    Update catch dfificulty test
    Likely the result of fixing https://github.com/ppy/osu/issues/4426.

--- osu.Game.Rulesets.Catch.Tests/CatchDifficultyCalculatorTest.cs
+++ osu.Game.Rulesets.Catch.Tests/CatchDifficultyCalculatorTest.cs
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Catch.Tests
     {
         protected override string ResourceAssembly => "osu.Game.Rulesets.Catch";
 
-        [TestCase(4.2038001515546597d, "diffcalc-test")]
+        [TestCase(4.2058561036909863d, "diffcalc-test")]
         public void Test(double expected, string name)
             => base.Test(expected, name);
 
