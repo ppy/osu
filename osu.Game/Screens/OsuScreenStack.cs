6a1e60009999f23cfc0f19338d0c7d38bf97a8bd
David Zhao <nyquill@ppy.sh>
2019-03-11 19:48:07 +0900

    Create new OsuScreenStack for use in Multiplayer

---
+++ osu.Game/Screens/OsuScreenStack.cs
@@ -0,0 +1,28 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Screens;
+using osu.Game.Graphics.Containers;
+using osu.Game.Screens.Multi.Lounge;
+
+namespace osu.Game.Screens
+{
+    public class OsuScreenStack : ScreenStack
+    {
+        [Cached]
+        private BackgroundScreenStack backgroundScreenStack;
+
+        public OsuScreenStack(IScreen baseScreen)
+            : base(baseScreen)
+        {
+            backgroundScreenStack = new BackgroundScreenStack { RelativeSizeAxes = Axes.Both };
+            InternalChild = new ParallaxContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Child = backgroundScreenStack,
+            };
+        }
+    }
+}
