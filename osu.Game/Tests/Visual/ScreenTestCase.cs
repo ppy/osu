e6a55cd6744e289addd6b86943e6c83ef6080bce
David Zhao <nyquill@ppy.sh>
2019-03-12 17:45:16 +0900

    Fix TestCaseEditor as well

--- osu.Game/Tests/Visual/ScreenTestCase.cs
+++ osu.Game/Tests/Visual/ScreenTestCase.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Screens;
 using osu.Game.Screens;
 
 namespace osu.Game.Tests.Visual
