2685c3b470cb6e6348b1f7f5f9733082bbe367a0
Santeri Nogelainen <87902114+bubb13gummy@users.noreply.github.com>
2019-03-16 12:41:03 +0200

    Border size to float, add min and max size, other small changes

--- osu.Game/Skinning/SkinConfiguration.cs
+++ osu.Game/Skinning/SkinConfiguration.cs
@@ -25,7 +25,7 @@ namespace osu.Game.Skinning
 
         public int HitCircleOverlap { get; set; }
 
-        public int? SliderBorderSize { get; set; }
+        public float? SliderBorderSize { get; set; }
 
         public bool? CursorExpand { get; set; } = true;
     }
