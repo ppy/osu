0024a0bdb2974864a4c5c892c497ef5185787790
David Zhao <nyquill@ppy.sh>
2019-03-18 14:35:03 +0900

    Remove unused using

--- osu.Game/Graphics/Containers/VisualSettingsContainer.cs
+++ osu.Game/Graphics/Containers/VisualSettingsContainer.cs
@@ -5,7 +5,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Logging;
 using osu.Game.Configuration;
 using osu.Game.Graphics.Backgrounds;
 using osuTK;
