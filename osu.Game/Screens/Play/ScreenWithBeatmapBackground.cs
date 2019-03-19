dc26e90a8dad211abce21888944145f9b8add92a
David Zhao <nyquill@ppy.sh>
2019-03-19 13:16:06 +0900

    Remove unused usings

--- osu.Game/Screens/Play/ScreenWithBeatmapBackground.cs
+++ osu.Game/Screens/Play/ScreenWithBeatmapBackground.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Bindables;
-using osu.Game.Configuration;
 using osu.Game.Screens.Backgrounds;
 
 namespace osu.Game.Screens.Play
