e4538feaacb52554bbe9aa6c39fb4882b7e75073
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-03-26 17:04:14 +0900

    Fix cursor adding

--- osu.Game/Rulesets/UI/Playfield.cs
+++ osu.Game/Rulesets/UI/Playfield.cs
@@ -67,7 +67,7 @@ namespace osu.Game.Rulesets.UI
 
             Cursor = CreateCursor();
             if (Cursor != null)
-                CursorTargetContainer.Add(Cursor);
+                AddInternal(Cursor);
         }
 
         /// <summary>
