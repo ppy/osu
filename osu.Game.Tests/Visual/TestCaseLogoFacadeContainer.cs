34a33b335d1e5046cd1cc5f9712f5994753d8fc9
David Zhao <nyquill@ppy.sh>
2019-03-27 17:29:38 +0900

    oops

--- osu.Game.Tests/Visual/TestCaseLogoFacadeContainer.cs
+++ osu.Game.Tests/Visual/TestCaseLogoFacadeContainer.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -13,8 +11,6 @@ using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 using osu.Game.Configuration;
 using osu.Game.Graphics.Containers;
-using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Osu.Mods;
 using osu.Game.Screens;
 using osu.Game.Screens.Menu;
 using osu.Game.Screens.Play;
