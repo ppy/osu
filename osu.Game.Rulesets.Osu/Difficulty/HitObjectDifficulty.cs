2890c809cdf13e4bfbda63c9b9c4f16bf3177d83
Joseph Ireland <joseph.ireland@outlook.com>
2019-04-06 22:41:09 +0100

    added missing file

---
+++ osu.Game.Rulesets.Osu/Difficulty/HitObjectDifficulty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Game.Rulesets.Osu.Difficulty
+{
+    public class OsuHitObjectDifficulty
+    {
+        public double Time;
+        public double AimStars;
+        public double AimCumulativeStars;
+        public double SpeedStars;
+        public double SpeedCumulativeStars;
+    }
+}
