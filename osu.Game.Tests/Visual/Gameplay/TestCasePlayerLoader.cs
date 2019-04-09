aa2c97b859eec9cc6778520a45b13bb5d1c21341
smoogipoo <smoogipoo@smgi.me>
2019-04-09 15:05:03 +0900

    Add mod reinstantiation testcase

--- osu.Game.Tests/Visual/Gameplay/TestCasePlayerLoader.cs
+++ osu.Game.Tests/Visual/Gameplay/TestCasePlayerLoader.cs
@@ -1,11 +1,16 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Screens;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens;
 using osu.Game.Screens.Play;
 
@@ -40,6 +45,81 @@ namespace osu.Game.Tests.Visual.Gameplay
             });
 
             AddUntilStep("wait for no longer current", () => !loader.IsCurrentScreen());
+        }
+
+        [Test]
+        public void TestModReinstantiation()
+        {
+            TestPlayer player = null;
+            TestMod gameMod = null;
+            TestMod playerMod1 = null;
+            TestMod playerMod2 = null;
+
+            AddStep("load player", () =>
+            {
+                SelectedMods.Value = new[] { gameMod = new TestMod() };
+                InputManager.MoveMouseTo(loader.ScreenSpaceDrawQuad.Centre);
+                stack.Push(new PlayerLoader(() => player = new TestPlayer()));
+            });
+
+            AddUntilStep("wait for player to become current", () =>
+            {
+                if (player.IsCurrentScreen())
+                {
+                    playerMod1 = (TestMod)player.SelectedMods.Value.Single();
+                    return true;
+                }
+
+                return false;
+            });
+
+            AddAssert("game mods not applied", () => gameMod.Applied == false);
+            AddAssert("player mods applied", () => playerMod1.Applied);
+
+            AddStep("restart player", () =>
+            {
+                player = null;
+                player.Restart();
+            });
+
+            AddUntilStep("wait for player to become current", () =>
+            {
+                if (player.IsCurrentScreen())
+                {
+                    playerMod2 = (TestMod)player.SelectedMods.Value.Single();
+                    return true;
+                }
+
+                return false;
+            });
+
+            AddAssert("game mods not applied", () => gameMod.Applied == false);
+            AddAssert("player has different mods", () => playerMod1 != playerMod2);
+            AddAssert("player mods applied", () => playerMod2.Applied);
+        }
+
+        private class TestMod : Mod, IApplicableToScoreProcessor
+        {
+            public override string Name => string.Empty;
+            public override string Acronym => string.Empty;
+            public override double ScoreMultiplier => 1;
+
+            public bool Applied { get; private set; }
+
+            public void ApplyToScoreProcessor(ScoreProcessor scoreProcessor)
+            {
+                Applied = true;
+            }
+        }
+
+        private class TestPlayer : Player
+        {
+            public new Bindable<IEnumerable<Mod>> SelectedMods => base.SelectedMods;
+
+            public TestPlayer()
+                : base(false, false)
+            {
+            }
         }
 
         protected class SlowLoadPlayer : Player
