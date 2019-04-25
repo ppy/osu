7e9709db07583d3defe71303c2fd566328b21fb6
iiSaLMaN <slmanarendo1950@gmail.com>
2019-04-25 21:28:06 +0300

    Fix exiting from fail scene bug
    A bug that causes the song to be stopped if you hold escape or exit while in a fail scene

--- osu.Game/Screens/Play/FailTransform.cs
+++ osu.Game/Screens/Play/FailTransform.cs
@@ -1,25 +1,63 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Transforms;
 using osu.Framework.MathUtils;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace osu.Game.Screens.Play
 {
     public class FailTransform : Transform<float, HitObjectContainer>
     {
-        public override string TargetMember => "Alpha";
+        public override string TargetMember => "Alpha, GameplayClockContainer.UserPlaybackRate, AudioManager.Track.Frequency";
 
-        private readonly GameplayClockContainer clock;
-        private readonly AudioManager audio;
+        private BindableDouble clockRate = new BindableDouble();
+        private BindableDouble trackFreq = new BindableDouble();
+
+        private SampleChannel sampleFail;
+
+        private int seed;
 
         public FailTransform(GameplayClockContainer clockContainer, AudioManager audioManager)
         {
-            audio = audioManager;
-            clock = clockContainer;
+            clockRate.BindTo(clockContainer.UserPlaybackRate);
+            trackFreq.BindTo(audioManager.Track.Frequency);
+
+            // Save the rates to be used if completed or aborted
+            clockRate.Default = clockRate.Value;
+            trackFreq.Default = trackFreq.Value;
+
+            sampleFail = audioManager.Sample.Get(@"Gameplay/failsound");
+            sampleFail?.Play();
+
+            base.OnComplete = onCompletion;
+            base.OnAbort = onAbort;
+            seed = RNG.Next();
+        }
+
+        private void onCompletion()
+        {
+            sampleFail?.Stop();
+
+            clockRate.SetDefault();
+            trackFreq.SetDefault();
+            OnComplete?.Invoke();
+        }
+
+        private void onAbort()
+        {
+            sampleFail?.Stop();
+
+            clockRate.SetDefault();
+            trackFreq.SetDefault();
+            OnAbort?.Invoke();
         }
 
         private float valueAt(double time)
@@ -29,21 +67,20 @@ namespace osu.Game.Screens.Play
         {
             var currentValue = valueAt(time);
 
-            clock.UserPlaybackRate.Value = currentValue;
-            audio.Track.Frequency.Value = currentValue;
+            clockRate.Value = currentValue;
+            trackFreq.Value = currentValue;
             d.Alpha = currentValue;
 
-            bool posOrNeg = false; // Object falling direction
+            // TODO: Use RNG with 'seed' if it was implemented.
+            Random randObj = new Random(seed);
             foreach (DrawableHitObject obj in d.Objects)
             {
                 obj.Rotation += RNG.NextSingle(0.2f);
                 obj.Y += RNG.NextSingle(0.4f, 0.8f);
-                obj.X += posOrNeg ? RNG.NextSingle(0.0f, 0.2f) : RNG.NextSingle(-0.2f, -0.0f);
-
-                posOrNeg = !posOrNeg;
+                obj.X += (randObj.NextDouble() > 0.5) ? RNG.NextSingle(0.0f, 0.4f) : RNG.NextSingle(-0.4f, -0.0f);
             }
         }
 
-        protected override void ReadIntoStartValue(HitObjectContainer d) => StartValue = 1;
+        protected override void ReadIntoStartValue(HitObjectContainer d) => StartValue = (float)(clockRate.Default + trackFreq.Default) / 2;
     }
 }
