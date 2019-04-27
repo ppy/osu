27c1c368ac715470e22b8dccfc5b42eed2745464
Dean Herbert <pe@ppy.sh>
2019-04-27 20:31:00 +0900

    Remove unused using

--- osu.Game.Rulesets.Osu/UI/Cursor/OsuCursorContainer.cs
+++ osu.Game.Rulesets.Osu/UI/Cursor/OsuCursorContainer.cs
@@ -5,7 +5,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Effects;
 using osu.Framework.Input.Bindings;
 using osu.Game.Rulesets.Osu.Configuration;
 using osu.Game.Rulesets.UI;
