bd5cb86b1546e9c9c7fe6d64f373f5b9766e0500
unknown <chris20194@googlemail.com>
2019-05-12 21:01:51 +0200

    add license header

--- osu.Game.Rulesets.Catch/UI/CatchCursorContainer.cs
+++ osu.Game.Rulesets.Catch/UI/CatchCursorContainer.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Graphics;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
 using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Catch.UI {
