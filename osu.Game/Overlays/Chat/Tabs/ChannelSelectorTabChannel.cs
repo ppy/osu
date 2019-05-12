496a9dd41dc74460463ad3341541874f97f938a7
Paul Teng <plankp@outlook.com>
2019-05-12 06:02:21 -0400

    Create separate type for Join-Channel button

---
+++ osu.Game/Overlays/Chat/Tabs/ChannelSelectorTabChannel.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.Chat;
+
+namespace osu.Game.Overlays.Chat.Tabs
+{
+    public class ChannelSelectorTabChannel : Channel
+    {
+        public ChannelSelectorTabChannel()
+        {
+            Name = "+";
+        }
+    }
+}
