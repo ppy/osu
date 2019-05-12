883d5ed58edfb1922df3f00e42f5821efdeff15b
unknown <chris20194@googlemail.com>
2019-05-12 21:14:27 +0200

    formatting

--- osu.Game.Rulesets.Catch/UI/CatchCursorContainer.cs
+++ osu.Game.Rulesets.Catch/UI/CatchCursorContainer.cs
@@ -4,7 +4,8 @@
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.UI;
 
-namespace osu.Game.Rulesets.Catch.UI {
+namespace osu.Game.Rulesets.Catch.UI
+{
     class CatchCursorContainer : GameplayCursorContainer
     {
         protected override Drawable CreateCursor() => new InvisibleCursor();
