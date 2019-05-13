9d9563bb97aab15a8c504419a6421f79a7e4c29e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-05-13 17:26:50 +0900

    Add JsonProperty hinting

--- osu.Game/Online/API/Requests/Responses/APIChangelogIndex.cs
+++ osu.Game/Online/API/Requests/Responses/APIChangelogIndex.cs
@@ -2,13 +2,16 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace osu.Game.Online.API.Requests.Responses
 {
     public class APIChangelogIndex
     {
+        [JsonProperty]
         public List<APIChangelogBuild> Builds;
 
+        [JsonProperty]
         public List<APIUpdateStream> Streams;
     }
 }
