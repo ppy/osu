4913ae1cf84c11c119d1dbb939e4ea95aa059bb4
Alten <87902114+bubb13gummy@users.noreply.github.com>
2019-05-14 16:18:46 -0400

    Shorten code

--- osu.Game.Rulesets.Osu/UI/DrawableOsuRuleset.cs
+++ osu.Game.Rulesets.Osu/UI/DrawableOsuRuleset.cs
@@ -63,13 +63,10 @@ namespace osu.Game.Rulesets.Osu.UI
         {
             get
             {
-                if (Objects.Any())
-                {
-                    var first = (OsuHitObject)Objects.First();
+                if (Objects.FirstOrDefault() is OsuHitObject first)
                     return first.StartTime - Math.Max(2000, first.TimePreempt);
-                }
-
-                return 0;
+                else
+                    return 0;
             }
         }
     }
