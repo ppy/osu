e89de9f7b7a5cea4045cfd441139af4cbb868804
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-05-17 12:40:15 +0900

    fixup! Convert BadgeDisplay to use bindable

--- osu.Game/Online/API/Requests/Responses/APIUpdateStream.cs
+++ osu.Game/Online/API/Requests/Responses/APIUpdateStream.cs
@@ -1,11 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using Newtonsoft.Json;
 
 namespace osu.Game.Online.API.Requests.Responses
 {
-    public class APIUpdateStream
+    public class APIUpdateStream : IEquatable<APIUpdateStream>
     {
         [JsonProperty("id")]
         public long Id { get; set; }
@@ -21,5 +22,13 @@ namespace osu.Game.Online.API.Requests.Responses
 
         [JsonProperty("latest_build")]
         public APIChangelogBuild LatestBuild { get; set; }
+
+        public bool Equals(APIUpdateStream other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return Id == other.Id;
+        }
     }
 }
