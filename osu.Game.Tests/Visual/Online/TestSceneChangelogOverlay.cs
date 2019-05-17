a7e62c172edfb89e386dd3692c75cc9c8b1ab76a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-05-17 10:29:43 +0900

    Fix dynamic recompilation in TestSceneChangelogOverlay

--- osu.Game.Tests/Visual/Online/TestSceneChangelogOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneChangelogOverlay.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Overlays;
+using osu.Game.Overlays.Changelog;
+using osu.Game.Overlays.Changelog.Header;
 
 namespace osu.Game.Tests.Visual.Online
 {
@@ -11,6 +15,18 @@ namespace osu.Game.Tests.Visual.Online
     public class TestSceneChangelogOverlay : OsuTestScene
     {
         private ChangelogOverlay changelog;
+
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(BadgeDisplay),
+            typeof(StreamBadge),
+            typeof(ChangelogHeader),
+            typeof(ChangelogContent),
+            typeof(ChangelogContentGroup),
+            typeof(Breadcrumb),
+            typeof(BreadcrumbListing),
+            typeof(BreadcrumbRelease),
+        };
 
         protected override void LoadComplete()
         {
