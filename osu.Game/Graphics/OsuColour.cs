c3c0901f92e8ed433573a0e34b2d9b3377c0ca30
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-05-20 15:58:40 +0900

    Use OsuColour for profile overlay

--- osu.Game/Graphics/OsuColour.cs
+++ osu.Game/Graphics/OsuColour.cs
@@ -77,7 +77,7 @@ namespace osu.Game.Graphics
 
         public readonly Color4 Seafoam = FromHex(@"05ffa2");
         public readonly Color4 GreySeafoamLighter = FromHex(@"9ebab1");
-        public readonly Color4 GreySeafoamLight = FromHex(@"4d7365");
+        public readonly Color4 GreySeafoamLight = FromHex(@"4e7466");
         public readonly Color4 GreySeafoam = FromHex(@"33413c");
         public readonly Color4 GreySeafoamDark = FromHex(@"2c3532");
         public readonly Color4 GreySeafoamDarker = FromHex(@"1e2422");
@@ -136,15 +136,5 @@ namespace osu.Game.Graphics
         public readonly Color4 ChatBlue = FromHex(@"17292e");
 
         public readonly Color4 ContextMenuGray = FromHex(@"223034");
-
-        public readonly Color4 CommunityUserGreenLight = FromHex(@"deff87");
-        public readonly Color4 CommunityUserGreen = FromHex(@"05ffa2");
-        public readonly Color4 CommunityUserGreenDark = FromHex(@"a6cc00");
-        public readonly Color4 CommunityUserGrayGreenLighter = FromHex(@"9ebab1");
-        public readonly Color4 CommunityUserGrayGreenLight = FromHex(@"77998e");
-        public readonly Color4 CommunityUserGrayGreen = FromHex(@"4e7466");
-        public readonly Color4 CommunityUserGrayGreenDark = FromHex(@"33413c");
-        public readonly Color4 CommunityUserGrayGreenDarker = FromHex(@"2c3532");
-        public readonly Color4 CommunityUserGrayGreenDarkest = FromHex(@"1e2422");
     }
 }
