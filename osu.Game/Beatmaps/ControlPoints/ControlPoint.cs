7a56fe84f20433063d811290c5afedb381cd7d5f
smoogipoo <smoogipoo@smgi.me>
2019-05-21 14:27:57 +0900

    Remove ControlPoint.EquivalentTo

--- osu.Game/Beatmaps/ControlPoints/ControlPoint.cs
+++ osu.Game/Beatmaps/ControlPoints/ControlPoint.cs
@@ -19,15 +19,7 @@ namespace osu.Game.Beatmaps.ControlPoints
 
         public int CompareTo(ControlPoint other) => Time.CompareTo(other.Time);
 
-        /// <summary>
-        /// Whether this <see cref="ControlPoint"/> provides the same parametric changes as another <see cref="ControlPoint"/>.
-        /// Basically an equality check without considering the <see cref="Time"/>.
-        /// </summary>
-        /// <param name="other">The <see cref="ControlPoint"/> to compare to.</param>
-        /// <returns>Whether this <see cref="ControlPoint"/> is equivalent to <paramref name="other"/>.</returns>
-        public virtual bool EquivalentTo(ControlPoint other) => true;
-
         public bool Equals(ControlPoint other)
-            => EquivalentTo(other) && Time.Equals(other?.Time);
+            => Time.Equals(other?.Time);
     }
 }
