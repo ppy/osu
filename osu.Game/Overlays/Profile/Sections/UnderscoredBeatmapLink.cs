41d39c945f47ee92478461f181c43005abc233e3
EVAST9919 <87902114+bubb13gummy@users.noreply.github.com>
2019-05-29 20:24:01 +0300

    Split UnderscoredLinkContainer in different classes

---
+++ osu.Game/Overlays/Profile/Sections/UnderscoredBeatmapLink.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Game.Beatmaps;
+
+namespace osu.Game.Overlays.Profile.Sections
+{
+    public class UnderscoredBeatmapLink : UnderscoredLinkContainer
+    {
+        private readonly BeatmapInfo beatmap;
+
+        public UnderscoredBeatmapLink(BeatmapInfo beatmap)
+        {
+            this.beatmap = beatmap;
+        }
+
+        [BackgroundDependencyLoader(true)]
+        private void load(BeatmapSetOverlay beatmapSetOverlay)
+        {
+            ClickAction = () =>
+            {
+                if (beatmap.OnlineBeatmapID != null)
+                    beatmapSetOverlay?.FetchAndShowBeatmap(beatmap.OnlineBeatmapID.Value);
+                else if (beatmap.BeatmapSet?.OnlineBeatmapSetID != null)
+                    beatmapSetOverlay?.FetchAndShowBeatmapSet(beatmap.BeatmapSet.OnlineBeatmapSetID.Value);
+            };
+        }
+    }
+}
