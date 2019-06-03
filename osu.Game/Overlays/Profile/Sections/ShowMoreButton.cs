633c3b74ec5b746de3565a2dfe234641cfae1bea
Dean Herbert <pe@ppy.sh>
2019-06-03 17:10:18 +0900

    Don't handle clicks when in a loading state

--- osu.Game/Overlays/Profile/Sections/ShowMoreButton.cs
+++ osu.Game/Overlays/Profile/Sections/ShowMoreButton.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
@@ -37,6 +37,8 @@ namespace osu.Game.Overlays.Profile.Sections
                     return;
 
                 isLoading = value;
+
+                Enabled.Value = !isLoading;
 
                 if (value)
                 {
