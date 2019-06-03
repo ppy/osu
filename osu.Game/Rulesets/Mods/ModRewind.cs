2dfec1746526935fe806d5c35599208e9e604623
LeNitrous <n20gaming2000@gmail.com>
2019-06-03 21:14:41 +0800

    create Rewind Mod

---
+++ osu.Game/Rulesets/Mods/ModRewind.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Audio;
+using osu.Framework.Timing;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.Mods
+{
+    public class ModRewind : Mod, IApplicableToClock
+    {
+        public override string Name => "Rewind";
+
+        public override string Acronym => "RW";
+
+        public override double ScoreMultiplier => 1.0;
+
+        public void ApplyToClock(IAdjustableClock clock)
+        {
+            switch (clock)
+            {
+                case IHasPitchAdjust pitch:
+                    pitch.PitchAdjust = -1;
+                    break;
+                case IHasTempoAdjust tempo:
+                    tempo.TempoAdjust = -1;
+                    break;
+                default:
+                    clock.Rate = -1;
+                    break;
+            }
+        }
+    }
+}
