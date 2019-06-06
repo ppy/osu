cc99a8ba810e6d31e4532d99497cb15ca5dfa6c0
Dean Herbert <pe@ppy.sh>
2019-06-06 17:37:38 +0900

    Fix nullreff on attempting to lookup files in a skin with no files backing

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -119,6 +119,9 @@ namespace osu.Game.Skinning
 
             private string getPathForFile(string filename)
             {
+                if (source.Files == null)
+                    return null;
+
                 bool hasExtension = filename.Contains('.');
 
                 string lastPiece = filename.Split('/').Last();
