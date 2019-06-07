f326264a85abacef478d45cf71fc28867b3554e2
Ganendra Afrasya <gsculerlor@hotmail.com>
2019-06-08 00:42:57 +0700

    Adding increase first object grow mod visibility setting

--- osu.Game/Overlays/Settings/Sections/Gameplay/ModsSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Gameplay/ModsSettings.cs
@@ -20,6 +20,11 @@ namespace osu.Game.Overlays.Settings.Sections.Gameplay
                     LabelText = "Increase visibility of first object with \"Hidden\" mod",
                     Bindable = config.GetBindable<bool>(OsuSetting.IncreaseFirstObjectVisibility)
                 },
+                new SettingsCheckbox
+                {
+                    LabelText = "Increase visibility of first object with \"Grow\" mod",
+                    Bindable = config.GetBindable<bool>(OsuSetting.IncreaseFirstObjectGrowVisibility)
+                },
             };
         }
     }
