6f5f86912dab2d277446ae572fddae9251135b86
naoey <87902114+bubb13gummy@users.noreply.github.com>
2019-06-12 00:41:17 +0530

    Post merge and inverted condition fix

--- osu.Game/Overlays/Direct/BeatmapDownloadTrackingComposite.cs
+++ osu.Game/Overlays/Direct/BeatmapDownloadTrackingComposite.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Bindables;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
 using osu.Game.Beatmaps;
 using osu.Game.Online;
 
