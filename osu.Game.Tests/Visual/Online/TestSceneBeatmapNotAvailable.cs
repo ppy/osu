35cc45c9d9dff91795a90ef8785aa3bf99fe7228
KingLuigi4932 <slmanarendo1950@gmail.com>
2019-06-11 23:11:03 +0300

    Small update

--- osu.Game.Tests/Visual/Online/TestSceneBeatmapNotAvailable.cs
+++ osu.Game.Tests/Visual/Online/TestSceneBeatmapNotAvailable.cs
@@ -1,13 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using NUnit.Framework;
 using osu.Game.Beatmaps;
 using osu.Game.Overlays.BeatmapSet;
 
 namespace osu.Game.Tests.Visual.Online
 {
-    [TestFixture]
     public class TestSceneBeatmapNotAvailable : OsuTestScene
     {
         public TestSceneBeatmapNotAvailable()
