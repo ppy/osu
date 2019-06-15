4beb269abee295769e46034f7760b7b482cd3bc3
Ganendra Afrasya <87902114+bubb13gummy@users.noreply.github.com>
2019-06-16 00:02:26 +0700

    Adding custom tooltip to DifficultyIcon

--- osu.Game/Beatmaps/Drawables/DifficultyIcon.cs
+++ osu.Game/Beatmaps/Drawables/DifficultyIcon.cs
@@ -1,4 +1,4 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -6,17 +6,20 @@ using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Rulesets;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Beatmaps.Drawables
 {
-    public class DifficultyIcon : DifficultyColouredContainer
+    public class DifficultyIcon : DifficultyColouredContainer, IHasCustomTooltip
     {
         private readonly RulesetInfo ruleset;
 
@@ -27,8 +30,95 @@ namespace osu.Game.Beatmaps.Drawables
                 throw new ArgumentNullException(nameof(beatmap));
 
             this.ruleset = ruleset ?? beatmap.Ruleset;
+            TooltipText = $"{beatmap.Version}${beatmap.StarDifficulty.ToString("0.##")}";
 
             Size = new Vector2(20);
+        }
+
+        public string TooltipText { get; set; }
+
+        public ITooltip GetCustomTooltip() => new DifficultyIconTooltip();
+
+        public class DifficultyIconTooltip : VisibilityContainer, ITooltip
+        {
+            private readonly OsuSpriteText difficultyName, starRating;
+            private readonly Box background;
+
+            public string TooltipText { get; set; }
+
+            public DifficultyIconTooltip()
+            {
+                AutoSizeAxes = Axes.Both;
+                Masking = true;
+                CornerRadius = 5;
+
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both
+                    },
+                    new FillFlowContainer
+                    {
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Vertical,
+                        Padding = new MarginPadding(10),
+                        Children = new Drawable[]
+                        {
+                            difficultyName = new OsuSpriteText
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Font = OsuFont.GetFont(size: 16, weight: FontWeight.Bold),
+                            },
+                            new FillFlowContainer
+                            {
+                                AutoSizeAxes = Axes.Both,
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Direction = FillDirection.Horizontal,
+                                Children = new Drawable[]
+                                {
+                                    starRating = new OsuSpriteText
+                                    {
+                                        Anchor = Anchor.Centre,
+                                        Origin = Anchor.Centre,
+                                        Font = OsuFont.GetFont(size: 16, weight: FontWeight.Regular),
+                                    },
+                                    new SpriteIcon
+                                    {
+                                        Anchor = Anchor.Centre,
+                                        Origin = Anchor.Centre,
+                                        Margin = new MarginPadding { Left = 4 },
+                                        Icon = FontAwesome.Solid.Star,
+                                        Size = new Vector2(12),
+                                        Colour = Color4.White,
+                                    },
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                background.Colour = colours.GreyCarmineDark;
+            }
+
+            public void Refresh()
+            {
+                var info = TooltipText.Split('$');
+                difficultyName.Text = info[0];
+                starRating.Text = info[1];
+            }
+
+            public void Move(Vector2 pos) => this.Position = pos;
+
+            protected override void PopIn() => this.FadeIn(200, Easing.OutQuint);
+
+            protected override void PopOut() => this.FadeOut(200, Easing.OutQuint);
         }
 
         [BackgroundDependencyLoader]
