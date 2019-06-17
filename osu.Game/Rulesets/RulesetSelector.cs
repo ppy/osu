82ebaa99dc0f17b95d5907e07f83cbbda38ac5ad
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2019-06-18 00:23:00 +0300

    Implement a BindableRulesetSelector

--- osu.Game/Rulesets/RulesetSelector.cs
+++ osu.Game/Rulesets/RulesetSelector.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Bindables;
 using osu.Framework.Allocation;
 
 namespace osu.Game.Rulesets
@@ -10,53 +9,17 @@ namespace osu.Game.Rulesets
     public abstract class RulesetSelector : TabControl<RulesetInfo>
     {
         protected RulesetStore AvaliableRulesets;
-        protected readonly Bindable<RulesetInfo> GlobalRuleset = new Bindable<RulesetInfo>();
 
         protected override Dropdown<RulesetInfo> CreateDropdown() => null;
 
-        /// <summary>
-        /// Whether we want to change a global ruleset when local one is changed.
-        /// </summary>
-        protected virtual bool AllowGlobalRulesetChange => true;
-
-        /// <summary>
-        /// Whether we want to change a local ruleset when global one is changed.
-        /// /// </summary>
-        protected virtual bool AllowLocalRulesetChange => true;
-
         [BackgroundDependencyLoader]
-        private void load(RulesetStore rulesets, Bindable<RulesetInfo> parentRuleset)
+        private void load(RulesetStore rulesets)
         {
             AvaliableRulesets = rulesets;
-            GlobalRuleset.BindTo(parentRuleset);
 
             foreach (var r in rulesets.AvailableRulesets)
             {
                 AddItem(r);
-            }
-
-            GlobalRuleset.BindValueChanged(globalRulesetChanged);
-            Current.BindValueChanged(OnLocalRulesetChanged);
-        }
-
-        private void globalRulesetChanged(ValueChangedEvent<RulesetInfo> e)
-        {
-            if (AllowLocalRulesetChange)
-            {
-                OnGlobalRulesetChanged(e);
-            }
-        }
-
-        protected virtual void OnGlobalRulesetChanged(ValueChangedEvent<RulesetInfo> e)
-        {
-            Current.Value = e.NewValue;
-        }
-
-        protected virtual void OnLocalRulesetChanged(ValueChangedEvent<RulesetInfo> e)
-        {
-            if (!GlobalRuleset.Disabled && AllowGlobalRulesetChange)
-            {
-                GlobalRuleset.Value = e.NewValue;
             }
         }
     }
