9fa60e6e8cefd4edbe8fe547a6c15216052128ff
KingLuigi4932 <87902114+bubb13gummy@users.noreply.github.com>
2019-06-19 01:43:28 +0300

    Revert some changes + simplify BeatmapNotAvailable

--- osu.Game/Overlays/BeatmapSet/BeatmapNotAvailable.cs
+++ osu.Game/Overlays/BeatmapSet/BeatmapNotAvailable.cs
@@ -8,6 +8,7 @@ using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 using osuTK.Graphics;
 
 namespace osu.Game.Overlays.BeatmapSet
@@ -25,7 +26,7 @@ namespace osu.Game.Overlays.BeatmapSet
 
                 beatmapSet = value;
 
-                removeLinks();
+                link?.Clear();
 
                 if (beatmapSet?.OnlineInfo.Availability != null)
                     Show();
@@ -34,8 +35,10 @@ namespace osu.Game.Overlays.BeatmapSet
             }
         }
 
-        private readonly OsuTextFlowContainer text;
+        private readonly OsuSpriteText text;
         private readonly LinkFlowContainer link;
+
+        private BeatmapSetOnlineAvailability availability => BeatmapSet?.OnlineInfo.Availability;
 
         public BeatmapNotAvailable()
         {
@@ -56,14 +59,14 @@ namespace osu.Game.Overlays.BeatmapSet
                     AutoSizeAxes = Axes.Y,
                     Direction = FillDirection.Vertical,
                     Margin = new MarginPadding { Top = 10, Left = 5, Right = 20 },
-
                     Children = new Drawable[]
                     {
-                        text = new OsuTextFlowContainer(t => t.Font = OsuFont.GetFont(size: 20, weight: FontWeight.Medium))
+                        text = new OsuSpriteText
                         {
                             RelativeSizeAxes = Axes.X,
-                            AutoSizeAxes = Axes.Y,
+                            AllowMultiline = true,
                             Margin = new MarginPadding { Bottom = 10, Horizontal = 5 },
+                            Font = OsuFont.GetFont(size: 20, weight: FontWeight.Medium),
                             Colour = Color4.Orange,
                         },
                         link = new LinkFlowContainer(t => t.Font = OsuFont.GetFont(size: 14))
@@ -76,21 +79,18 @@ namespace osu.Game.Overlays.BeatmapSet
                     },
                 },
             };
-
-            Hide();
         }
 
         public override void Show()
         {
-            text.Text = BeatmapSet.OnlineInfo.Availability.DownloadDisabled
+            text.Text = availability.DownloadDisabled
                 ? "This beatmap is currently not available for download."
                 : "Portions of this beatmap have been removed at the request of the creator or a third-party rights holder.";
 
-            link.AddLink("Check here for more information.", BeatmapSet.OnlineInfo.Availability.ExternalLink);
+            if (!string.IsNullOrEmpty(availability.ExternalLink))
+                link.AddLink("Check here for more information.", availability.ExternalLink);
 
             base.Show();
         }
-
-        private void removeLinks() => link?.RemoveAll(x => true);
     }
 }
