5000a82efcfa9baeec45f9377b92159166584d69
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-06-18 15:00:33 +0900

    Add xmldoc and serialisable attributes

--- osu.Game.Tournament/Models/BeatmapChoice.cs
+++ osu.Game.Tournament/Models/BeatmapChoice.cs
@@ -1,11 +1,16 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace osu.Game.Tournament.Models
 {
+    /// <summary>
+    /// A beatmap choice by a team from a tournament's map pool.
+    /// </summary>
+    [Serializable]
     public class BeatmapChoice
     {
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
