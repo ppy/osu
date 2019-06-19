211d43bbba076a50b8a053f075edcaa96761da2f
KingLuigi4932 <87902114+bubb13gummy@users.noreply.github.com>
2019-06-19 03:50:16 +0300

    Seperate updateable from drawable

--- osu.Game/Users/Drawables/DrawableFlag.cs
+++ osu.Game/Users/Drawables/DrawableFlag.cs
@@ -3,33 +3,12 @@
 
 using System;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 
 namespace osu.Game.Users.Drawables
 {
-    public class UpdateableFlag : ModelBackedDrawable<Country>
-    {
-        public Country Country
-        {
-            get => Model;
-            set => Model = value;
-        }
-
-        public UpdateableFlag(Country country = null)
-        {
-            Country = country;
-        }
-
-        protected override Drawable CreateDrawable(Country country) => new DrawableFlag(country)
-        {
-            RelativeSizeAxes = Axes.Both,
-        };
-    }
-
     public class DrawableFlag : Sprite, IHasTooltip
     {
         private readonly Country country;
