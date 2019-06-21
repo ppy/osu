db767ecfa78ed14000c30a21281be55903dbd36d
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2019-06-22 02:40:33 +0300

    Add triangles

--- osu.Game/Overlays/Profile/ProfileSection.cs
+++ osu.Game/Overlays/Profile/ProfileSection.cs
@@ -4,11 +4,14 @@
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
+using osu.Game.Graphics.Backgrounds;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Users;
+using osuTK.Graphics;
 
 namespace osu.Game.Overlays.Profile
 {
@@ -35,6 +38,11 @@ namespace osu.Game.Overlays.Profile
                 background = new Box
                 {
                     RelativeSizeAxes = Axes.Both,
+                },
+                new SectionTriangles
+                {
+                    Anchor = Anchor.BottomCentre,
+                    Origin = Anchor.BottomCentre,
                 },
                 new FillFlowContainer
                 {
@@ -73,6 +81,43 @@ namespace osu.Game.Overlays.Profile
         private void load(OsuColour colours)
         {
             background.Colour = colours.GreySeafoamDarker;
+        }
+
+        private class SectionTriangles : Container
+        {
+            private readonly Triangles triangles;
+            private readonly Box foreground;
+
+            public SectionTriangles()
+            {
+                RelativeSizeAxes = Axes.X;
+                Height = 100;
+                Masking = true;
+                Children = new Drawable[]
+                {
+                    triangles = new Triangles
+                    {
+                        Anchor = Anchor.BottomCentre,
+                        Origin = Anchor.BottomCentre,
+                        RelativeSizeAxes = Axes.Both,
+                        Height = 0.95f,
+                        TriangleScale = 4,
+                        Velocity = 0.5f,
+                    },
+                    foreground = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    }
+                };
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                triangles.ColourLight = colours.GreySeafoamDark;
+                triangles.ColourDark = colours.GreySeafoamDarker;
+                foreground.Colour = ColourInfo.GradientVertical(colours.GreySeafoamDarker, new Color4(0,0,0,0));
+            }
         }
     }
 }
