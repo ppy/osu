7a9ca671946306189099502560bd1668edd0e0a2
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2019-06-22 18:23:20 +0200

    Fix CI inspections

--- osu.Game.Rulesets.Catch/UI/CatcherArea.cs
+++ osu.Game.Rulesets.Catch/UI/CatcherArea.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Bindings;
 using osu.Framework.MathUtils;
 using osu.Game.Beatmaps;
