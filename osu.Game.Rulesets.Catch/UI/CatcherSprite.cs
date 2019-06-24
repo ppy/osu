9baf8160d1e07256d36fe4ec7daee20f0be3259c
Dean Herbert <pe@ppy.sh>
2019-06-24 14:40:08 +0900

    Remove pointless SkinReloadableDrawable specification

--- osu.Game.Rulesets.Catch/UI/CatcherSprite.cs
+++ osu.Game.Rulesets.Catch/UI/CatcherSprite.cs
@@ -1,4 +1,4 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
@@ -9,7 +9,7 @@ using osuTK;
 
 namespace osu.Game.Rulesets.Catch.UI
 {
-    public class CatcherSprite : SkinReloadableDrawable
+    public class CatcherSprite : CompositeDrawable
     {
         public CatcherSprite()
         {
