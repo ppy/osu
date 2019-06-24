1caf97a713ffea82a3a5ed5540163bdd1245d3d9
jorolf <87902114+bubb13gummy@users.noreply.github.com>
2019-06-24 23:17:07 +0200

    update code to work with https://github.com/ppy/osu-framework/pull/2568

--- osu.Game/Graphics/UserInterface/OsuDropdown.cs
+++ osu.Game/Graphics/UserInterface/OsuDropdown.cs
@@ -9,6 +9,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
 using osuTK;
 
@@ -95,7 +96,11 @@ namespace osu.Game.Graphics.UserInterface
                 }
             }
 
-            protected override DrawableMenuItem CreateDrawableMenuItem(MenuItem item) => new DrawableOsuDropdownMenuItem(item) { AccentColour = accentColour };
+            protected override Menu CreateSubMenu() => new OsuMenu(Direction.Vertical);
+
+            protected override ScrollContainer<Drawable> CreateScrollContainer(Direction direction) => new OsuScrollContainer(direction);
+
+            protected override DrawableDropdownMenuItem CreateDrawableDropdownMenuItem(MenuItem item) => new DrawableOsuDropdownMenuItem(item) { AccentColour = accentColour };
 
             #region DrawableOsuDropdownMenuItem
 
