e87e4077e82ad92b1fc195df26ff87ee6d13099c
Dean Herbert <pe@ppy.sh>
2019-06-25 20:23:43 +0900

    Add testability of dismissal

--- osu.Game.Tests/Visual/UserInterface/TestSceneBackButton.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneBackButton.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -12,10 +14,15 @@ namespace osu.Game.Tests.Visual.UserInterface
 {
     public class TestSceneBackButton : OsuTestScene
     {
+        private readonly BackButton button;
+
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(TwoLayerButton)
+        };
+
         public TestSceneBackButton()
         {
-            BackButton button;
-
             Child = new Container
             {
                 Anchor = Anchor.Centre,
@@ -32,7 +39,8 @@ namespace osu.Game.Tests.Visual.UserInterface
                     button = new BackButton
                     {
                         Anchor = Anchor.BottomLeft,
-                        Origin = Anchor.BottomLeft
+                        Origin = Anchor.BottomLeft,
+                        Action = () => button.Hide(),
                     }
                 }
             };
