49980b1116166b9860f0d74a4c1d0bd74f04e699
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-06-25 18:30:43 +0900

    Adjust backbutton animation

---
+++ osu.Game.Tests/Visual/UserInterface/TestSceneBackButton.cs
@@ -0,0 +1,44 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.UserInterface;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneBackButton : OsuTestScene
+    {
+        public TestSceneBackButton()
+        {
+            BackButton button;
+
+            Child = new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(300),
+                Masking = true,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4.SlateGray
+                    },
+                    button = new BackButton
+                    {
+                        Anchor = Anchor.BottomLeft,
+                        Origin = Anchor.BottomLeft
+                    }
+                }
+            };
+
+            AddStep("show button", () => button.Show());
+            AddStep("hide button", () => button.Hide());
+        }
+    }
+}
