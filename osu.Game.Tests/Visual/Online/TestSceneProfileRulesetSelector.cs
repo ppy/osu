803198ff209b1591868dc4c94cb129d6486fdeec
smoogipoo <smoogipoo@smgi.me>
2019-06-27 14:53:18 +0900

    Gamemode -> Ruleset

--- osu.Game.Tests/Visual/Online/TestSceneProfileRulesetSelector.cs
+++ osu.Game.Tests/Visual/Online/TestSceneProfileRulesetSelector.cs
@@ -7,6 +7,10 @@ using osu.Game.Overlays.Profile.Header.Components;
 using osuTK.Graphics;
 using System;
 using System.Collections.Generic;
+using osu.Game.Rulesets.Catch;
+using osu.Game.Rulesets.Mania;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Taiko;
 
 namespace osu.Game.Tests.Visual.Online
 {
@@ -28,11 +32,11 @@ namespace osu.Game.Tests.Visual.Online
                 Origin = Anchor.Centre,
             };
 
-            AddStep("set osu! as default", () => selector.SetDefaultGamemode("osu"));
-            AddStep("set mania as default", () => selector.SetDefaultGamemode("mania"));
-            AddStep("set taiko as default", () => selector.SetDefaultGamemode("taiko"));
-            AddStep("set catch as default", () => selector.SetDefaultGamemode("fruits"));
-            AddStep("select default gamemode", selector.SelectDefaultGamemode);
+            AddStep("set osu! as default", () => selector.SetDefaultRuleset(new OsuRuleset().RulesetInfo));
+            AddStep("set mania as default", () => selector.SetDefaultRuleset(new ManiaRuleset().RulesetInfo));
+            AddStep("set taiko as default", () => selector.SetDefaultRuleset(new TaikoRuleset().RulesetInfo));
+            AddStep("set catch as default", () => selector.SetDefaultRuleset(new CatchRuleset().RulesetInfo));
+            AddStep("select default ruleset", selector.SelectDefaultRuleset);
 
             AddStep("set random colour", () => selector.AccentColour = new Color4(RNG.NextSingle(), RNG.NextSingle(), RNG.NextSingle(), 1));
         }
