c88889a694093703a7f32d84423ee7606bd9f8d3
iiSaLMaN <slmanarendo1950@gmail.com>
2019-06-28 12:50:07 +0300

    Remove empty overrides

--- osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableSwellTick.cs
+++ osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableSwellTick.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Taiko.Objects.Drawables
@@ -18,10 +17,6 @@ namespace osu.Game.Rulesets.Taiko.Objects.Drawables
         public void TriggerResult(HitResult type) => ApplyResult(r => r.Type = type);
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
-        {
-        }
-
-        protected override void UpdateState(ArmedState state)
         {
         }
 
