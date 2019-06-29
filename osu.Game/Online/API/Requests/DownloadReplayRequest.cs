21502d148e63ccb6c102085a49e8cf12daf8a2a2
naoey <87902114+bubb13gummy@users.noreply.github.com>
2019-06-29 12:29:12 +0530

    Code quality fixes

--- osu.Game/Online/API/Requests/DownloadReplayRequest.cs
+++ osu.Game/Online/API/Requests/DownloadReplayRequest.cs
@@ -1,4 +1,8 @@
-﻿using osu.Game.Scoring;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Scoring;
+
 namespace osu.Game.Online.API.Requests
 {
     public class DownloadReplayRequest : ArchiveDownloadRequest<ScoreInfo>
