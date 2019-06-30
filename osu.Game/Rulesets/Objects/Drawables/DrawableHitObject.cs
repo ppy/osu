b334fb0f0033b5b3df96560322117d55a6051381
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2019-06-30 18:27:47 +0300

    Move to DrawableOsuHitObject

--- osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
+++ osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
@@ -81,8 +81,6 @@ namespace osu.Game.Rulesets.Objects.Drawables
         public override bool RemoveCompletedTransforms => false;
         protected override bool RequiresChildrenUpdate => true;
 
-        public override bool HandlePositionalInput => true;
-
         public override bool IsPresent => base.IsPresent || (State.Value == ArmedState.Idle && Clock?.CurrentTime >= LifetimeStart);
 
         public readonly Bindable<ArmedState> State = new Bindable<ArmedState>();
