4f31bd7ed8098ca03d65579428f368b3780691d7
MaxOhnh <ohn.m@hotmail.de>
2019-07-01 14:32:48 +0200

    Updated imports, license header and Icon of indecisive mod

--- osu.Game.Rulesets.Osu/Mods/OsuModIndecisive.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModIndecisive.cs
@@ -1,20 +1,20 @@
-﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Mods;
+using osu.Framework.Graphics.Sprites;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Graphics;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
     public class OsuModIndecisive : OsuModHidden
     {
         public override string Name => "Indecisive";
-        public override string ShortenedName => "ID";
-        public override FontAwesome Icon => FontAwesome.fa_question_circle;
-        public override ModType Type => ModType.Fun;
+        public override string Acronym => "ID";
+        public override IconUsage Icon => FontAwesome.Regular.QuestionCircle;
+        public override ModType Type => ModType.DifficultyIncrease;
         public override string Description => "Nomod-Hidden alternation.";
         public override double ScoreMultiplier => 1;
 
@@ -23,7 +23,7 @@ namespace osu.Game.Rulesets.Osu.Mods
             base.ApplyToDrawableHitObjects(drawables.Where((x, i) => i % 2 == 1));
 
             // Hidden mod checks if first element should be skipped, this mod doesn't care
-            if (IncreaseFirstObjectVisibility)
+            if (IncreaseFirstObjectVisibility.Value)
                 drawables.Skip(1).First().ApplyCustomUpdateState += ApplyHiddenState;
         }
     }
