feb81f4b7b78d711bd911682249cdc9af4949fe4
miterosan <87902114+bubb13gummy@users.noreply.github.com>
2019-07-03 10:21:18 +0200

    Only log that the rulesets could not be loaded from a directory.

--- osu.Game/Rulesets/RulesetStore.cs
+++ osu.Game/Rulesets/RulesetStore.cs
@@ -31,9 +31,9 @@ namespace osu.Game.Rulesets
                 foreach (string file in files.Where(f => !Path.GetFileName(f).Contains("Tests")))
                     loadRulesetFromFile(file);
             }
-            catch (Exception e)
+            catch
             {
-                Logger.Error(e, $"Could not load rulesets from directory {Environment.CurrentDirectory}");
+                Logger.Log($"Could not load rulesets from directory {Environment.CurrentDirectory}");
             }
         }
 
