35ee117195e9250cc82bcb19b2ea0509678793ef
Andrei Zavatski <megaman9919@gmail.com>
2019-07-04 03:40:41 +0300

    Extend GetUserRequest

--- osu.Game/Online/API/Requests/GetUserRequest.cs
+++ osu.Game/Online/API/Requests/GetUserRequest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets;
 using osu.Game.Users;
 
 namespace osu.Game.Online.API.Requests
@@ -8,12 +9,14 @@ namespace osu.Game.Online.API.Requests
     public class GetUserRequest : APIRequest<User>
     {
         private readonly long? userId;
+        private readonly RulesetInfo ruleset;
 
-        public GetUserRequest(long? userId = null)
+        public GetUserRequest(long? userId = null, RulesetInfo ruleset = null)
         {
             this.userId = userId;
+            this.ruleset = ruleset;
         }
 
-        protected override string Target => userId.HasValue ? $@"users/{userId}" : @"me";
+        protected override string Target => userId.HasValue ? (ruleset != null ? $@"users/{userId}/{ruleset.ShortName}" : $@"users/{userId}") : @"me";
     }
 }
