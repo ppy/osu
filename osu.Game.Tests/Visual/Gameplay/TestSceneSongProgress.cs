41847de5a7091cc489a8489526a20bc5ed4c961a
Unknown <87902114+bubb13gummy@users.noreply.github.com>
2019-07-05 08:05:23 +0200

    reorganise tests

--- osu.Game.Tests/Visual/Gameplay/TestSceneSongProgress.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSongProgress.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.MathUtils;
+using osu.Framework.Testing;
 using osu.Framework.Timing;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Screens.Play;
@@ -21,65 +22,104 @@ namespace osu.Game.Tests.Visual.Gameplay
             typeof(SongProgressBar),
         };
 
-        private readonly SongProgress progress;
-        private readonly TestSongProgressGraph graph;
+        private SongProgress progress;
+        private TestSongProgressGraph graph;
 
         private readonly StopwatchClock clock;
+        private readonly FramedClock framedClock;
 
         [Cached]
         private readonly GameplayClock gameplayClock;
 
-        private readonly FramedClock framedClock;
-
         public TestSceneSongProgress()
         {
-            clock = new StopwatchClock(true);
-
+            clock = new StopwatchClock();
             gameplayClock = new GameplayClock(framedClock = new FramedClock(clock));
-
-            Add(progress = new SongProgress
-            {
-                RelativeSizeAxes = Axes.X,
-                Anchor = Anchor.BottomLeft,
-                Origin = Anchor.BottomLeft,
-            });
-
-            Add(graph = new TestSongProgressGraph
-            {
-                RelativeSizeAxes = Axes.X,
-                Height = 200,
-                Anchor = Anchor.TopLeft,
-                Origin = Anchor.TopLeft,
-            });
-
-            AddAssert("ensure not created", () => graph.CreationCount == 0);
-
-            AddStep("display values", displayNewValues);
-            AddUntilStep("wait for creation count", () => graph.CreationCount == 1);
-
-            AddStep("Toggle Bar", () => progress.AllowSeeking = !progress.AllowSeeking);
-            AddUntilStep("wait for creation count", () => graph.CreationCount == 1);
-
-            AddStep("Toggle Bar", () => progress.AllowSeeking = !progress.AllowSeeking);
-            AddUntilStep("wait for creation count", () => graph.CreationCount == 1);
-            AddRepeatStep("New Values", displayNewValues, 5);
-
-            AddWaitStep("wait some", 5);
-            AddAssert("ensure debounced", () => graph.CreationCount == 2);
-
-            AddStep("hide graph", () => progress.CollapseGraph.Value = true);
-            AddStep("show graph", () => progress.CollapseGraph.Value = false);
-
-            AddStep("start", clock.Start);
-            AddStep("pause", clock.Stop);
         }
 
-        private void displayNewValues()
+        [SetUpSteps]
+        public void SetupSteps()
         {
-            List<HitObject> objects = new List<HitObject>();
+            AddStep("add new song progress", () =>
+            {
+                if (progress != null)
+                {
+                    progress.Expire();
+                    progress = null;
+                }
+
+                Add(progress = new SongProgress
+                {
+                    RelativeSizeAxes = Axes.X,
+                    Anchor = Anchor.BottomLeft,
+                    Origin = Anchor.BottomLeft,
+                });
+            });
+
+            AddStep("add new big graph", () =>
+            {
+                if (graph != null)
+                {
+                    graph.Expire();
+                    graph = null;
+                }
+
+                Add(graph = new TestSongProgressGraph
+                {
+                    RelativeSizeAxes = Axes.X,
+                    Height = 200,
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                });
+            });
+
+            AddStep("reset clock", clock.Reset);
+        }
+
+        [Test]
+        public void TestGraphRecreation()
+        {
+            AddAssert("ensure not created", () => graph.CreationCount == 0);
+            AddStep("display values", displayRandomValues);
+            AddUntilStep("wait for creation count", () => graph.CreationCount == 1);
+            AddRepeatStep("new values", displayRandomValues, 5);
+            AddWaitStep("wait some", 5);
+            AddAssert("ensure recreation debounced", () => graph.CreationCount == 2);
+        }
+
+        [Test]
+        public void TestDisplay()
+        {
+            AddStep("display max values", displayMaxValues);
+            AddStep("start", clock.Start);
+            AddStep("show bar", () => progress.AllowSeeking = true);
+            AddStep("hide graph", () => progress.CollapseGraph.Value = true);
+            AddStep("hide Bar", () => progress.AllowSeeking = false);
+            AddStep("show bar", () => progress.AllowSeeking = true);
+            AddStep("show graph", () => progress.CollapseGraph.Value = false);
+            AddStep("stop", clock.Stop);
+        }
+
+        private void displayRandomValues()
+        {
+            var objects = new List<HitObject>();
             for (double i = 0; i < 5000; i += RNG.NextDouble() * 10 + i / 1000)
                 objects.Add(new HitObject { StartTime = i });
 
+            replaceObjects(objects);
+        }
+
+        private void displayMaxValues()
+        {
+            var objects = new List<HitObject>();
+            for (double i = 0; i < 5000; i++)
+                objects.Add(new HitObject { StartTime = i });
+
+            replaceObjects(objects);
+        }
+
+        private void replaceObjects(List<HitObject> objects)
+        {
             progress.Objects = objects;
             graph.Objects = objects;
 
