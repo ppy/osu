2d5fd7f1c4fc789daabbebd0dad891ed3033a8f2
Unknown <aergwyn@t-online.de>
2019-07-05 08:49:56 +0200

    remove unnecessarily setting value of bindable that was already bound to

--- osu.Game/Screens/Play/HUDOverlay.cs
+++ osu.Game/Screens/Play/HUDOverlay.cs
@@ -106,7 +106,6 @@ namespace osu.Game.Screens.Play
             BindDrawableRuleset(drawableRuleset);
 
             Progress.Objects = drawableRuleset.Objects;
-            Progress.AllowSeeking = drawableRuleset.HasReplayLoaded.Value;
             Progress.RequestSeek = time => RequestSeek(time);
             Progress.ReferenceClock = drawableRuleset.FrameStableClock;
 
