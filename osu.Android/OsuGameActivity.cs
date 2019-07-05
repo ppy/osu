f7b50e5010e1379738418dbfb8ee3daa92d69c41
EdrowsLuo <793971297@qq.com>
2019-07-05 20:38:20 +0800

    Import local osz in android system

--- osu.Android/OsuGameActivity.cs
+++ osu.Android/OsuGameActivity.cs
@@ -5,21 +5,32 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Android.Util;
+using Android.Content;
 using osu.Framework.Android;
 
 namespace osu.Android
 {
-    [Activity(Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.FullSensor, SupportsPictureInPicture = false, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize, HardwareAccelerated = true)]
     public class OsuGameActivity : AndroidGameActivity
     {
-        protected override Framework.Game CreateGame() => new OsuGameAndroid();
+        protected override Framework.Game CreateGame() => new OsuGameAndroid(getImportFilePath());
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            
             Window.AddFlags(WindowManagerFlags.Fullscreen);
             Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+        }
+
+        private string getImportFilePath()
+        {
+            if (Intent.Action != null
+                && Intent.Action.Equals("android.intent.action.VIEW"))
+            {
+                return Intent.Data.Path;
+            }
+            return null;
         }
     }
 }
--- osu.Android/OsuGameAndroid.cs
+++ osu.Android/OsuGameAndroid.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Threading.Tasks;
 using Android.App;
 using osu.Game;
 
@@ -10,5 +11,23 @@ namespace osu.Android
     public class OsuGameAndroid : OsuGame
     {
         public override Version AssemblyVersion => new Version(Application.Context.ApplicationContext.PackageManager.GetPackageInfo(Application.Context.ApplicationContext.PackageName, 0).VersionName);
+
+        private string fileToLoad;
+
+        public OsuGameAndroid(params string[] args)
+        {
+            fileToLoad = args?.Length > 0 ? args[0] : null;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            if(fileToLoad != null)
+            {
+                Task.Factory.StartNew(() => Import(fileToLoad), TaskCreationOptions.LongRunning);
+            }
+        }
+
     }
 }
