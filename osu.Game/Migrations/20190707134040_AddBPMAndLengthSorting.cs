28cc797fb61ee60fa82193982e84f2dc86c2d48f
iiSaLMaN <slmanarendo1950@gmail.com>
2019-07-07 18:29:25 +0300

    Add migrations

---
+++ osu.Game/Migrations/20190707134040_AddBPMAndLengthSorting.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace osu.Game.Migrations
+{
+    public partial class AddBPMAndLengthSorting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
