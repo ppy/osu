fbd300e6643a582b9fab5299265633e907e16d6b
David Zhao <nyquill@ppy.sh>
2019-07-08 17:37:20 +0900

    Move ruleset into ReplayPlayerLoader as well

--- osu.Game/Screens/Play/ReplayPlayerLoader.cs
+++ osu.Game/Screens/Play/ReplayPlayerLoader.cs
@@ -1,21 +1,19 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using osu.Framework.Allocation;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Scoring;
 
 namespace osu.Game.Screens.Play
 {
     public class ReplayPlayerLoader : PlayerLoader
     {
-        private readonly IReadOnlyList<Mod> mods;
+        private readonly ScoreInfo scoreInfo;
 
         public ReplayPlayerLoader(Score score)
             : base(() => new ReplayPlayer(score))
         {
-            mods = score.ScoreInfo.Mods;
+            scoreInfo = score.ScoreInfo;
         }
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
@@ -23,7 +21,8 @@ namespace osu.Game.Screens.Play
             var dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
 
             // Overwrite the global mods here for use in the mod hud.
-            Mods.Value = mods;
+            Mods.Value = scoreInfo.Mods;
+            Ruleset.Value = scoreInfo.Ruleset;
 
             return dependencies;
         }
