58bae2a4417f637239b5d0bd816dfc8fbbd0a8d2
Cory Lynch <cjl2625@gmail.com>
2019-07-10 02:27:36 -0400

    Change Taiko pp to be more consistent with stable

--- osu.Game.Rulesets.Taiko/Difficulty/TaikoDifficultyCalculator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/TaikoDifficultyCalculator.cs
@@ -34,7 +34,7 @@ namespace osu.Game.Rulesets.Taiko.Difficulty
                 StarRating = skills.Single().DifficultyValue() * star_scaling_factor,
                 Mods = mods,
                 // Todo: This int cast is temporary to achieve 1:1 results with osu!stable, and should be removed in the future
-                GreatHitWindow = (int)(beatmap.HitObjects.First().HitWindows.Great / 2) / clockRate,
+                GreatHitWindow = ((int)(beatmap.HitObjects.First().HitWindows.Great / 2) - 0.5) / clockRate,
                 MaxCombo = beatmap.HitObjects.Count(h => h is Hit),
                 Skills = skills
             };
