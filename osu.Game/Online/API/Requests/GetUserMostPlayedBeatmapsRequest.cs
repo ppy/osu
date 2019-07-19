99ab77b926a5fb3e569e2955eb577e1a2211adfd
smoogipoo <smoogipoo@smgi.me>
2019-07-19 15:33:09 +0900

    Add PaginatedWebRequest to handle request pagination

--- osu.Game/Online/API/Requests/GetUserMostPlayedBeatmapsRequest.cs
+++ osu.Game/Online/API/Requests/GetUserMostPlayedBeatmapsRequest.cs
@@ -6,19 +6,16 @@ using osu.Game.Online.API.Requests.Responses;
 
 namespace osu.Game.Online.API.Requests
 {
-    public class GetUserMostPlayedBeatmapsRequest : APIRequest<List<APIUserMostPlayedBeatmap>>
+    public class GetUserMostPlayedBeatmapsRequest : PaginatedAPIRequest<List<APIUserMostPlayedBeatmap>>
     {
         private readonly long userId;
-        private readonly int offset;
-        private readonly int limit;
 
         public GetUserMostPlayedBeatmapsRequest(long userId, int offset = 0, int limit = 5)
+            : base(offset, limit)
         {
             this.userId = userId;
-            this.offset = offset;
-            this.limit = limit;
         }
 
-        protected override string Target => $@"users/{userId}/beatmapsets/most_played?offset={offset}&limit={limit}";
+        protected override string Target => $@"users/{userId}/beatmapsets/most_played";
     }
 }
