2926932a1acf5adc8da8125ba83b05a7960ebb60
Lucas A <game4allyt@gmail.com>
2019-07-20 21:10:17 +0200

    Add MusicControllerToast used to display current music playback status on OSD

---
+++ osu.Game/Overlays/OSD/MusicControllerToast.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Overlays.OSD
+{
+    public class MusicControllerToast : Toast
+    {
+        public MusicControllerToast(string value)
+            : base("Music Playback", value, "")
+        {
+        }
+    }
+}
