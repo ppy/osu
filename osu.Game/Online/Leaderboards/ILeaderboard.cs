4c04304a05fe95a63a4523c7a103bf98c71f8d69
iiSaLMaN <slmanarendo1950@gmail.com>
2019-07-21 03:48:55 +0300

    Fix CI issues + add header license

--- osu.Game/Online/Leaderboards/ILeaderboard.cs
+++ osu.Game/Online/Leaderboards/ILeaderboard.cs
@@ -1,7 +1,8 @@
-﻿using osu.Game.Scoring;
-using System;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Scoring;
 using System.Collections.Generic;
-using System.Text;
 
 namespace osu.Game.Online.Leaderboards
 {
