53e207fa5ebecf2c398b3ec608886ed2f6690b69
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-07-22 15:43:27 +0900

    Revert unintended change

--- osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/RingPiece.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/RingPiece.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics;
@@ -35,7 +35,7 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables.Pieces
                         RelativeSizeAxes = Axes.Both
                     }
                 }
-            }, confineMode: ConfineMode.NoScaling);
+            });
         }
     }
 }
