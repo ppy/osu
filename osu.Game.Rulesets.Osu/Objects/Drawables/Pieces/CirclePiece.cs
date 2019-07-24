859233526d4ad1f3173e0b6d4a5c33425cbfc45b
Dean Herbert <pe@ppy.sh>
2019-07-24 18:50:57 +0900

    Move circle visual implementation to new class
    Allows for more precise skin control over state animations.

--- osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/CirclePiece.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/CirclePiece.cs
@@ -1,24 +1,15 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Input.Bindings;
 using osu.Game.Skinning;
 using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Objects.Drawables.Pieces
 {
-    public class CirclePiece : Container, IKeyBindingHandler<OsuAction>
+    public class CirclePiece : CompositeDrawable
     {
-        // IsHovered is used
-        public override bool HandlePositionalInput => true;
-
-        public Func<bool> Hit;
-
-        public OsuAction? HitAction;
-
         public CirclePiece()
         {
             Size = new Vector2(OsuHitObject.OBJECT_RADIUS * 2);
@@ -30,25 +21,5 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables.Pieces
 
             InternalChild = new SkinnableDrawable("Play/osu/hitcircle", _ => new DefaultCirclePiece());
         }
-
-        public bool OnPressed(OsuAction action)
-        {
-            switch (action)
-            {
-                case OsuAction.LeftButton:
-                case OsuAction.RightButton:
-                    if (IsHovered && (Hit?.Invoke() ?? false))
-                    {
-                        HitAction = action;
-                        return true;
-                    }
-
-                    break;
-            }
-
-            return false;
-        }
-
-        public bool OnReleased(OsuAction action) => false;
     }
 }
