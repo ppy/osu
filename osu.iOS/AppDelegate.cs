117a51f1aef12bdc23840a0188ed3430cee7a0a4
DTSDAO <87902114+bubb13gummy@users.noreply.github.com>
2019-07-24 20:49:35 +0800

    Add iOS import osu! files

--- osu.iOS/AppDelegate.cs
+++ osu.iOS/AppDelegate.cs
@@ -1,15 +1,32 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Threading.Tasks;
 using Foundation;
 using osu.Framework.iOS;
 using osu.Game;
+using UIKit;
 
 namespace osu.iOS
 {
     [Register("AppDelegate")]
     public class AppDelegate : GameAppDelegate
     {
-        protected override Framework.Game CreateGame() => new OsuGameIOS();
+        private OsuGameIOS IOSGame;
+
+        protected override Framework.Game CreateGame()
+        {
+            //Save OsuGameIOS for Import
+            IOSGame = new OsuGameIOS();
+            return IOSGame;
+        }
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+        {
+            //Open in Application
+            Task.Run(() => IOSGame.Import(url.Path));
+
+            return true;
+        }
     }
 }
