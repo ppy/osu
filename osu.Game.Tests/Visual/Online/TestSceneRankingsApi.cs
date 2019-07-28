69416172838282d6c11e43769a79c72ea4d2620b
Andrei Zavatski <megaman9919@gmail.com>
2019-07-28 21:40:30 +0300

    Implement GetRankingsPerformanceRequest

---
+++ osu.Game.Tests/Visual/Online/TestSceneRankingsApi.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.API.Requests;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Online.API;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Online.API.Requests.Responses;
+using System.Collections.Generic;
+using osu.Game.Rulesets.Mania;
+using osu.Game.Rulesets.Taiko;
+using osu.Game.Rulesets.Catch;
+using osuTK;
+
+namespace osu.Game.Tests.Visual.Online
+{
+    public class TestSceneRankingsApi : OsuTestScene
+    {
+        [Resolved]
+        private IAPIProvider api { get; set; }
+
+        private readonly FillFlowContainer usersContainer;
+        private GetRankingsPerformanceRequest request;
+
+        public TestSceneRankingsApi()
+        {
+            Add(usersContainer = new FillFlowContainer
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                Direction = FillDirection.Full,
+                Spacing = new Vector2(10, 5),
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            AddStep("Get osu performance", () =>
+            {
+                request?.Cancel();
+                request = new GetRankingsPerformanceRequest(new OsuRuleset().RulesetInfo);
+                request.Success += updateRankings;
+                api.Queue(request);
+            });
+
+            AddStep("Get mania performance", () =>
+            {
+                request?.Cancel();
+                request = new GetRankingsPerformanceRequest(new ManiaRuleset().RulesetInfo);
+                request.Success += updateRankings;
+                api.Queue(request);
+            });
+
+            AddStep("Get taiko performance", () =>
+            {
+                request?.Cancel();
+                request = new GetRankingsPerformanceRequest(new TaikoRuleset().RulesetInfo);
+                request.Success += updateRankings;
+                api.Queue(request);
+            });
+
+            AddStep("Get catch performance", () =>
+            {
+                request?.Cancel();
+                request = new GetRankingsPerformanceRequest(new CatchRuleset().RulesetInfo);
+                request.Success += updateRankings;
+                api.Queue(request);
+            });
+        }
+
+        private void updateRankings(List<APIPerformanceRankings> rankings)
+        {
+            usersContainer.Clear();
+            rankings.ForEach(r => usersContainer.Add(new OsuSpriteText
+            {
+                Text = r.User.Username
+            }));
+        }
+    }
+}
---
+++ osu.Game/Online/API/Requests/GetRankingsPerformanceRequest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets;
+using System.Collections.Generic;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class GetRankingsPerformanceRequest : PaginatedAPIRequest<List<APIPerformanceRankings>>
+    {
+        private readonly RulesetInfo ruleset;
+
+        public GetRankingsPerformanceRequest(RulesetInfo ruleset, int page = 0)
+            : base(page, 50)
+        {
+            this.ruleset = ruleset;
+        }
+
+        protected override string Target => $"rankings/{ruleset.ShortName}/performance";
+    }
+}
