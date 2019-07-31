2d243f27ae5053324bb6124d6697a04f87b7e9e6
jorolf <87902114+bubb13gummy@users.noreply.github.com>
2019-07-31 21:55:56 +0200

    fix crash and add some tests
    - still missing special skin textures

---
+++ osu.Game.Rulesets.Osu.Tests/TestSceneDrawableJudgement.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Extensions;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    public class TestSceneDrawableJudgement : SkinnableTestScene
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(DrawableJudgement),
+            typeof(DrawableOsuJudgement)
+        };
+
+        public TestSceneDrawableJudgement()
+        {
+            foreach (HitResult result in Enum.GetValues(typeof(HitResult)))
+            {
+                JudgementResult judgement = new JudgementResult(null)
+                {
+                    Type = result,
+                };
+
+                AddStep("Show " + result.GetDescription(), () => SetContents(() => new DrawableOsuJudgement(judgement, null)
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                }));
+            }
+        }
+    }
+}
