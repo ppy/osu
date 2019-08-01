342438b4d9e6532e4a9e710159ec4076ecd48b71
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-08-01 13:37:40 +0900

    Fix applying mods to the wrong beatmap

--- osu.Game/Beatmaps/WorkingBeatmap.cs
+++ osu.Game/Beatmaps/WorkingBeatmap.cs
@@ -142,7 +142,7 @@ namespace osu.Game.Beatmaps
             processor?.PostProcess();
 
             foreach (var mod in mods.OfType<IApplicableToBeatmap>())
-                mod.ApplyToBeatmap(Beatmap);
+                mod.ApplyToBeatmap(converted);
 
             return converted;
         }
