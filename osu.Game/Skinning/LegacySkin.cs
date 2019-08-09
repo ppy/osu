52ce2045ac5094cd9a32ef965f69a3d454fbfd53
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-08-09 15:25:42 +0900

    Remove explicit autosize

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -140,7 +140,7 @@ namespace osu.Game.Skinning
 
             if (texture != null && animatable)
             {
-                var animation = new TextureAnimation { DefaultFrameLength = frametime, AutoSizeAxes = Axes.None };
+                var animation = new TextureAnimation { DefaultFrameLength = frametime };
 
                 for (int i = 1; texture != null; i++)
                 {
