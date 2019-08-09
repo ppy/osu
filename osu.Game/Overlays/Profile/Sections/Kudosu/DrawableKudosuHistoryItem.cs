c6df540159d3f2d1e1df3f27e71586cd537cb320
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2019-08-09 11:14:38 +0300

    Implement basic layout for kudosu history

---
+++ osu.Game/Overlays/Profile/Sections/Kudosu/DrawableKudosuHistoryItem.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Online.Chat;
+
+namespace osu.Game.Overlays.Profile.Sections.Kudosu
+{
+    public class DrawableKudosuHistoryItem : DrawableProfileRow
+    {
+        private readonly APIKudosuHistory historyItem;
+        private LinkFlowContainer content;
+
+        public DrawableKudosuHistoryItem(APIKudosuHistory historyItem)
+        {
+            this.historyItem = historyItem;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            LeftFlowContainer.Padding = new MarginPadding { Left = 10 };
+
+            LeftFlowContainer.Add(content = new LinkFlowContainer
+            {
+                AutoSizeAxes = Axes.Y,
+                RelativeSizeAxes = Axes.X,
+            });
+
+            RightFlowContainer.Add(new DrawableDate(historyItem.CreatedAt)
+            {
+                Font = OsuFont.GetFont(size: 13),
+                Colour = OsuColour.Gray(0xAA),
+                Anchor = Anchor.TopRight,
+                Origin = Anchor.TopRight,
+            });
+
+            var formatted = createMessage();
+
+            content.AddLinks(formatted.Text, formatted.Links);
+        }
+
+        protected override Drawable CreateLeftVisual() => new Container
+        {
+            RelativeSizeAxes = Axes.Y,
+            AutoSizeAxes = Axes.X,
+        };
+
+        private MessageFormatter.MessageFormatterResult createMessage() => MessageFormatter.FormatText($@"{historyItem.Amount}");
+    }
+}
---
+++ osu.Game/Overlays/Profile/Sections/Kudosu/PaginatedKudosuHistoryContainer.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Online.API.Requests;
+using osu.Game.Users;
+using System.Linq;
+using osu.Framework.Bindables;
+
+namespace osu.Game.Overlays.Profile.Sections.Kudosu
+{
+    public class PaginatedKudosuHistoryContainer : PaginatedContainer
+    {
+        private GetUserKudosuHistoryRequest request;
+
+        public PaginatedKudosuHistoryContainer(Bindable<User> user, string header, string missing)
+            : base(user, header, missing)
+        {
+            ItemsPerPage = 5;
+        }
+
+        protected override void ShowMore()
+        {
+            request = new GetUserKudosuHistoryRequest(User.Value.Id, VisiblePages++, ItemsPerPage);
+            request.Success += items => Schedule(() =>
+            {
+                MoreButton.FadeTo(items.Count == ItemsPerPage ? 1 : 0);
+                MoreButton.IsLoading = false;
+
+                if (!items.Any() && VisiblePages == 1)
+                {
+                    MissingText.Show();
+                    return;
+                }
+
+                MissingText.Hide();
+
+                foreach (var item in items)
+                    ItemsContainer.Add(new DrawableKudosuHistoryItem(item));
+            });
+
+            Api.Queue(request);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            request?.Cancel();
+        }
+    }
+}
