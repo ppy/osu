89c2b029d8db2c030a1dfab02d5abc7cda155b48
Andrei Zavatski <megaman9919@gmail.com>
2019-08-09 09:33:50 +0300

    TestScene improvements

--- osu.Game.Tests/Visual/Online/TestSceneRankingsApi.cs
+++ osu.Game.Tests/Visual/Online/TestSceneRankingsApi.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using osu.Game.Rulesets.Mania;
 using osu.Game.Rulesets.Taiko;
 using osu.Game.Rulesets.Catch;
+using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Tests.Visual.Online
 {
@@ -22,16 +23,28 @@ namespace osu.Game.Tests.Visual.Online
         private IAPIProvider api { get; set; }
 
         private readonly FillFlowContainer usersContainer;
+        private readonly DimmedLoadingLayer loading;
         private GetUserRankingsRequest request;
 
         public TestSceneRankingsApi()
         {
-            Add(usersContainer = new FillFlowContainer
+            AddRange(new Drawable[]
             {
-                Anchor = Anchor.TopLeft,
-                Origin = Anchor.TopLeft,
-                AutoSizeAxes = Axes.Both,
-                Direction = FillDirection.Vertical,
+                new BasicScrollContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Child = usersContainer = new FillFlowContainer
+                    {
+                        Anchor = Anchor.TopLeft,
+                        Origin = Anchor.TopLeft,
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Vertical,
+                    }
+                },
+                loading = new DimmedLoadingLayer
+                {
+                    Alpha = 0,
+                }
             });
         }
 
@@ -41,6 +54,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get osu performance", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new OsuRuleset().RulesetInfo);
                 request.Success += updatePerformanceRankings;
@@ -49,6 +63,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get USA osu performance", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new OsuRuleset().RulesetInfo, country: "US");
                 request.Success += updatePerformanceRankings;
@@ -57,6 +72,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get osu performance page 100", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new OsuRuleset().RulesetInfo, page: 100);
                 request.Success += updatePerformanceRankings;
@@ -65,6 +81,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get mania performance", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new ManiaRuleset().RulesetInfo);
                 request.Success += updatePerformanceRankings;
@@ -73,6 +90,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get taiko performance", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new TaikoRuleset().RulesetInfo);
                 request.Success += updatePerformanceRankings;
@@ -81,6 +99,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get catch performance", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new CatchRuleset().RulesetInfo);
                 request.Success += updatePerformanceRankings;
@@ -89,6 +108,7 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Get mania scores for BY", () =>
             {
+                loading.Show();
                 request?.Cancel();
                 request = new GetUserRankingsRequest(new ManiaRuleset().RulesetInfo, UserRankingsType.Score, country: "BY");
                 request.Success += updateScoreRankings;
@@ -99,19 +119,25 @@ namespace osu.Game.Tests.Visual.Online
         private void updatePerformanceRankings(List<APIUserRankings> rankings)
         {
             usersContainer.Clear();
+
             rankings.ForEach(r => usersContainer.Add(new OsuSpriteText
             {
                 Text = $"{r.User.Username}, Accuracy: {r.Accuracy}, Play Count: {r.PlayCount}, Performance: {r.PP}, SS: {r.GradesCount.SS}, S: {r.GradesCount.S}, A: {r.GradesCount.A}"
             }));
+
+            loading.Hide();
         }
 
         private void updateScoreRankings(List<APIUserRankings> rankings)
         {
             usersContainer.Clear();
+
             rankings.ForEach(r => usersContainer.Add(new OsuSpriteText
             {
                 Text = $"{r.User.Username}, Accuracy: {r.Accuracy}, Play Count: {r.PlayCount}, Total Score: {r.TotalScore}, Ranked Score: {r.RankedScore}, SS: {r.GradesCount.SS}, S: {r.GradesCount.S}, A: {r.GradesCount.A}"
             }));
+
+            loading.Hide();
         }
     }
 }
