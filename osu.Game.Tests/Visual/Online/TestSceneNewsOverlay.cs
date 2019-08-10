0903698b2e0483b7d8b6eb30c11fcb8b8d126b14
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2019-08-10 12:26:54 +0200

    Add visual tests.

---
+++ osu.Game.Tests/Visual/Online/TestSceneNewsOverlay.cs
@@ -0,0 +1,17 @@
+﻿using osu.Game.Overlays;
+
+namespace osu.Game.Tests.Visual.Online
+{
+    public class TestSceneNewsOverlay : OsuTestScene
+    {
+        private NewsOverlay news;
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            Add(news = new NewsOverlay());
+            AddStep(@"Show", news.Show);
+            AddStep(@"Hide", news.Hide);
+        }
+    }
+}
