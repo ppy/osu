0c6486d0e05251ade080af490c04ae373fb11457
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2019-08-10 12:22:45 +0200

    Add NewsOverlay class

---
+++ osu.Game/Overlays/NewsOverlay.cs
@@ -0,0 +1,23 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+
+namespace osu.Game.Overlays
+{
+    public class NewsOverlay : FullscreenOverlay
+    {
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colours.PurpleLightAlternative
+                }
+            };
+        }
+    }
+}
