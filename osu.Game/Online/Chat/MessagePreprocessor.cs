270ff7364cd82e2739012ab059df717986783523
laur <laur.h_97@yahoo.ro>
2019-08-12 00:17:52 +0300

    added license header

--- osu.Game/Online/Chat/MessagePreprocessor.cs
+++ osu.Game/Online/Chat/MessagePreprocessor.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Text.RegularExpressions;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
