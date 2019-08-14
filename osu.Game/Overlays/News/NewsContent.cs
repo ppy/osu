840d4741daf285b0c2541997f39b6fda309a492b
Lucas A <game4allyt@gmail.com>
2019-08-14 20:24:36 +0200

    Add NewsContent class and fix broken reference.

---
+++ osu.Game/Overlays/News/NewsContent.cs
@@ -0,0 +1,16 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Overlays.News
+{
+    public abstract class NewsContent : FillFlowContainer
+    {
+        public NewsContent()
+        {
+            RelativeSizeAxes = Axes.X;
+            AutoSizeAxes = Axes.Y;
+            Direction = FillDirection.Vertical;
+            Padding = new MarginPadding { Bottom = 100 };
+        }
+    }
+}
