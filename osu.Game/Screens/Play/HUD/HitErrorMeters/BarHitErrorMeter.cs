8fc177b743d8a1b3c5300379b597c5487650cb31
Dean Herbert <pe@ppy.sh>
2019-08-30 18:46:42 +0900

    Fix namespacing and hitwindow source

--- osu.Game/Screens/Play/HUD/HitErrorMeters/BarHitErrorMeter.cs
+++ osu.Game/Screens/Play/HUD/HitErrorMeters/BarHitErrorMeter.cs
@@ -2,21 +2,21 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Linq;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Game.Rulesets.Judgements;
-using osuTK.Graphics;
-using osuTK;
-using osu.Framework.Graphics.Sprites;
-using osu.Game.Rulesets.Objects;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Game.Graphics;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Scoring;
+using osuTK;
+using osuTK.Graphics;
 
-namespace osu.Game.Screens.Play.HitErrorDisplay
+namespace osu.Game.Screens.Play.HUD.HitErrorMeters
 {
     public class BarHitErrorMeter : HitErrorMeter
     {
