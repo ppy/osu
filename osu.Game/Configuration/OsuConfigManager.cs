51da7fdb14e7a4c7e9d57c2a7b1f124d713ebbbe
iiSaLMaN <slmanarendo1950@gmail.com>
2019-09-02 00:44:13 +0300

    Add precision to scaling settings

--- osu.Game/Configuration/OsuConfigManager.cs
+++ osu.Game/Configuration/OsuConfigManager.cs
@@ -103,11 +103,11 @@ namespace osu.Game.Configuration
 
             Set(OsuSetting.Scaling, ScalingMode.Off);
 
-            Set(OsuSetting.ScalingSizeX, 0.8f, 0.2f, 1f);
-            Set(OsuSetting.ScalingSizeY, 0.8f, 0.2f, 1f);
+            Set(OsuSetting.ScalingSizeX, 0.8f, 0.2f, 1f, 0.01f);
+            Set(OsuSetting.ScalingSizeY, 0.8f, 0.2f, 1f, 0.01f);
 
-            Set(OsuSetting.ScalingPositionX, 0.5f, 0f, 1f);
-            Set(OsuSetting.ScalingPositionY, 0.5f, 0f, 1f);
+            Set(OsuSetting.ScalingPositionX, 0.5f, 0f, 1f, 0.01f);
+            Set(OsuSetting.ScalingPositionY, 0.5f, 0f, 1f, 0.01f);
 
             Set(OsuSetting.UIScale, 1f, 0.8f, 1.6f, 0.01f);
 
