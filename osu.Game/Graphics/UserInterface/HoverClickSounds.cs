e98059267d4348a92e4d16fe6b75d9ebe45e3912
Dean Herbert <pe@ppy.sh>
2019-09-03 19:21:24 +0900

    Improve xmldoc

--- osu.Game/Graphics/UserInterface/HoverClickSounds.cs
+++ osu.Game/Graphics/UserInterface/HoverClickSounds.cs
@@ -21,12 +21,12 @@ namespace osu.Game.Graphics.UserInterface
         private readonly MouseButton[] buttons;
 
         /// <summary>
-        /// Creates an instance that adds sounds on hover and on click for any of the buttons specified.
+        /// a container which plays sounds on hover and click for any specified <see cref="MouseButton"/>s.
         /// </summary>
         /// <param name="sampleSet">Set of click samples to play.</param>
         /// <param name="buttons">
         /// Array of button codes which should trigger the click sound.
-        /// If this optional parameter is omitted or set to <code>null</code>, the click sound will only be added on left click.
+        /// If this optional parameter is omitted or set to <code>null</code>, the click sound will only be played on left click.
         /// </param>
         public HoverClickSounds(HoverSampleSet sampleSet = HoverSampleSet.Normal, MouseButton[] buttons = null)
             : base(sampleSet)
