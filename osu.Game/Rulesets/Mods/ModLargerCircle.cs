83e7746137efdcfa81ee3aa1411693fd7cda0921
Junho <51792037+ojh050118@users.noreply.github.com>
2019-09-06 00:21:00 +0900

    Create ModLargerCircle.cs

---
+++ osu.Game/Rulesets/Mods/ModLargerCircle.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Beatmaps;
+
+namespace osu.Game.Rulesets.Mods
+{
+    public abstract class ModLargerCircle : Mod, IApplicableToDifficulty
+    {
+        public override string Name => "Larger Circle";
+        public override string Acronym => "LC";
+        public override IconUsage Icon => FontAwesome.Solid.ChevronCircleUp;
+        public override ModType Type => ModType.DifficultyReduction;
+        public override double ScoreMultiplier => 0.75;
+        public override bool Ranked => true;
+        public override Type[] IncompatibleMods => new[] { typeof(ModHardRock) };
+
+        public void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            const float ratio = 0.25f;
+            difficulty.CircleSize *= ratio;
+        }
+    }
+}
