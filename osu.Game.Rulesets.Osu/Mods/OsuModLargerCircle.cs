9c03c72a22f95235cecc5418bb81411e8451f84f
Junho <51792037+ojh050118@users.noreply.github.com>
2019-09-06 22:51:08 +0900

    Fix CS0246

--- osu.Game.Rulesets.Osu/Mods/OsuModLargerCircle.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModLargerCircle.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Mods;
 using System;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
