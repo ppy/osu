3450eb82094f3d21c37f61e119c9d7fb0eed2689
Junho <51792037+ojh050118@users.noreply.github.com>
2019-09-06 17:22:32 +0900

    Add "Larger Circle" mod
    Mod for people who are difficult to osu!

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -106,6 +106,7 @@ namespace osu.Game.Rulesets.Osu
                         new OsuModNoFail(),
                         new MultiMod(new OsuModHalfTime(), new OsuModDaycore()),
                         new OsuModSpunOut(),
+                        new OsuModLargerCircle(),
                     };
 
                 case ModType.DifficultyIncrease:
