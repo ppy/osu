e6a2ecdef68d04f06922c7769b7d1d83183638b3
Andrei Zavatski <megaman9919@gmail.com>
2019-09-07 06:42:46 +0300

    Remove unused using directive

--- osu.Game/Overlays/Profile/Header/DetailHeaderContainer.cs
+++ osu.Game/Overlays/Profile/Header/DetailHeaderContainer.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
