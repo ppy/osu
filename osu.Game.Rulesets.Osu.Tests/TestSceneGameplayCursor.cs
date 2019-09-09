af09ed1b7fe648930f4bc084e2dd00b7307cf1e1
smoogipoo <smoogipoo@smgi.me>
2019-09-09 12:48:34 +0900

    Make cursor test scene more automated

--- osu.Game.Rulesets.Osu.Tests/TestSceneGameplayCursor.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneGameplayCursor.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Testing.Input;
 using osu.Game.Rulesets.Osu.UI.Cursor;
+using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Tests
 {
@@ -18,11 +20,35 @@ namespace osu.Game.Rulesets.Osu.Tests
         [BackgroundDependencyLoader]
         private void load()
         {
-            SetContents(() => new OsuCursorContainer
+            SetContents(() => new MovingCursorInputManager
             {
-                RelativeSizeAxes = Axes.Both,
-                Masking = true,
+                Child = new OsuCursorContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Masking = true,
+                }
             });
+        }
+
+        private class MovingCursorInputManager : ManualInputManager
+        {
+            public MovingCursorInputManager()
+            {
+                UseParentInput = false;
+            }
+
+            protected override void Update()
+            {
+                base.Update();
+
+                const double spin_duration = 5000;
+                double currentTime = Time.Current;
+
+                double angle = (currentTime % spin_duration) / spin_duration * 2 * Math.PI;
+                Vector2 rPos = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+
+                MoveMouseTo(ToScreenSpace(DrawSize / 2 + DrawSize / 3 * rPos));
+            }
         }
     }
 }
