a200485fbd0db1b2a4b429bb15620c8174cc4a9f
smoogipoo <smoogipoo@smgi.me>
2019-09-09 19:23:02 +0900

    Implement disjoint (old style) cursor trails

--- osu.Game.Rulesets.Osu/Skinning/LegacyCursorTrail.cs
+++ osu.Game.Rulesets.Osu/Skinning/LegacyCursorTrail.cs
@@ -3,6 +3,7 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
 using osu.Game.Rulesets.Osu.UI.Cursor;
 using osu.Game.Skinning;
 
@@ -10,6 +11,11 @@ namespace osu.Game.Rulesets.Osu.Skinning
 {
     public class LegacyCursorTrail : CursorTrail
     {
+        private const double disjoint_trail_time_separation = 1000 / 60.0;
+
+        private bool disjointTrail;
+        private double lastTrailTime;
+
         public LegacyCursorTrail()
         {
             Blending = BlendingParameters.Additive;
@@ -19,6 +25,31 @@ namespace osu.Game.Rulesets.Osu.Skinning
         private void load(ISkinSource skin)
         {
             Texture = skin.GetTexture("cursortrail");
+            disjointTrail = skin.GetTexture("cursormiddle") == null;
+
+            if (disjointTrail && Texture != null)
+            {
+                // stable "magic ratio". see OsuPlayfieldAdjustmentContainer for full explanation.
+                Texture.ScaleAdjust *= 1.6f;
+            }
+        }
+
+        protected override double FadeDuration => disjointTrail ? 150 : 500;
+
+        protected override bool InterpolateMovements => !disjointTrail;
+
+        protected override bool OnMouseMove(MouseMoveEvent e)
+        {
+            if (!disjointTrail)
+                return base.OnMouseMove(e);
+
+            if (Time.Current - lastTrailTime >= disjoint_trail_time_separation)
+            {
+                lastTrailTime = Time.Current;
+                return base.OnMouseMove(e);
+            }
+
+            return false;
         }
     }
 }
