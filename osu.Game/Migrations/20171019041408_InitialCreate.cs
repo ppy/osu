c13950fbbf801d92a8c62aa4075ef51e2dadc323
Andrei Zavatski <megaman9919@gmail.com>
2019-09-13 13:43:21 +0300

    Remove custom db additions

--- osu.Game/Migrations/20171019041408_InitialCreate.cs
+++ osu.Game/Migrations/20171019041408_InitialCreate.cs
@@ -35,7 +35,6 @@ namespace osu.Game.Migrations
                     AudioFile = table.Column<string>(type: "TEXT", nullable: true),
                     Author = table.Column<string>(type: "TEXT", nullable: true),
                     BackgroundFile = table.Column<string>(type: "TEXT", nullable: true),
-                    VideoFile = table.Column<string>(type: "TEXT", nullable: true),
                     PreviewTime = table.Column<int>(type: "INTEGER", nullable: false),
                     Source = table.Column<string>(type: "TEXT", nullable: true),
                     Tags = table.Column<string>(type: "TEXT", nullable: true),
