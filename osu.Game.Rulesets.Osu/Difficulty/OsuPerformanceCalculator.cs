64edd8ccd79072cf015599d0c7dc63c2315ef369
hebuwei <hebuweitom@gmail.com>
2019-09-13 14:36:44 +0800

    nerf jumps with high correction value

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -257,7 +257,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             return modifiedAcc;
         }
 
-        private double tpToPP(double tp) => Math.Pow(tp, skillToPPExponent) * 0.117;
+        private double tpToPP(double tp) => Math.Pow(tp, skillToPPExponent) * 0.126;
 
         private double tapSkillToPP(double tapSkill) => Math.Pow(tapSkill, skillToPPExponent) * 0.204f;
 
