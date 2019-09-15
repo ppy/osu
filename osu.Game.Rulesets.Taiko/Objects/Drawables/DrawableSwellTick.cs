bebc550a70e2af57b4dc3aaaec129e4b14834519
Dean Herbert <pe@ppy.sh>
2019-09-15 22:40:19 +0900

    Fix CI issues

--- osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableSwellTick.cs
+++ osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableSwellTick.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Taiko.Objects.Drawables
@@ -25,7 +26,7 @@ namespace osu.Game.Rulesets.Taiko.Objects.Drawables
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
         }
-        
+
         public override bool OnPressed(TaikoAction action) => false;
     }
 }
