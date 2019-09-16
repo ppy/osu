b84a83ef9aa7d6e70fe84fbdf64390f07fa3f9f8
Andrei Zavatski <megaman9919@gmail.com>
2019-09-16 07:51:43 +0300

    Implement HumanizedNumber component for rankings overlay

---
+++ osu.Game.Tests/Visual/Online/TestSceneHumanizedNumber.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Overlays.Rankings;
+using osuTK;
+
+namespace osu.Game.Tests.Visual.Online
+{
+    public class TestSceneHumanizedNumber : OsuTestScene
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(HumanizedNumber),
+        };
+
+        public TestSceneHumanizedNumber()
+        {
+            Add(new FillFlowContainer<HumanizedNumber>
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Direction = FillDirection.Vertical,
+                Spacing = new Vector2(0, 5),
+                AutoSizeAxes = Axes.Both,
+                Children = new[]
+                {
+                    new HumanizedNumber(0)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new HumanizedNumber(999)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new HumanizedNumber(546895)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new HumanizedNumber(87546895)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new HumanizedNumber(456545546895)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new HumanizedNumber(76476545546895)
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                }
+            });
+        }
+    }
+}
---
+++ osu.Game/Overlays/Rankings/HumanizedNumber.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Game.Graphics.Sprites;
+using osu.Framework.Graphics.Cursor;
+
+namespace osu.Game.Overlays.Rankings
+{
+    public class HumanizedNumber : OsuSpriteText, IHasTooltip
+    {
+        private const int k = 1000;
+
+        public string TooltipText => value < k ? "" : $"{value:N0}";
+
+        private readonly long value;
+
+        public HumanizedNumber(long value)
+        {
+            this.value = value;
+
+            Text = numberToText(value);
+        }
+
+        private string numberToText(long value)
+        {
+            var suffixes = new[]
+            {
+                "",
+                "k",
+                "million",
+                "billion",
+                "trillion",
+            };
+
+            if (value < k)
+                return value.ToString();
+
+            int i = (int)Math.Floor(Math.Log(value) / Math.Log(k));
+            return $"{value / Math.Pow(k, i):F2} {suffixes[i]}";
+        }
+    }
+}
