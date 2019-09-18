5d38c21ad1142338ce0046c3e2b86683226f4b63
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-09-18 17:09:43 +0900

    Remove redundant length check

--- osu.Game.Rulesets.Mania/Replays/ManiaReplayFrame.cs
+++ osu.Game.Rulesets.Mania/Replays/ManiaReplayFrame.cs
@@ -21,8 +21,7 @@ namespace osu.Game.Rulesets.Mania.Replays
         public ManiaReplayFrame(double time, params ManiaAction[] actions)
             : base(time)
         {
-            if (actions.Length > 0)
-                Actions.AddRange(actions);
+            Actions.AddRange(actions);
         }
 
         public void ConvertFrom(LegacyReplayFrame legacyFrame, IBeatmap beatmap, ReplayFrame lastFrame = null)
