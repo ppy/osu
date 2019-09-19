88fc74816d4bbff60c52e1b2942fc6452bd32b8f
iiSaLMaN <slmanarendo1950@gmail.com>
2019-09-19 07:31:05 +0300

    Add generic class for virtual input handler

--- osu.Game/Input/Handlers/VirtualInputHandler.cs
+++ osu.Game/Input/Handlers/VirtualInputHandler.cs
@@ -2,14 +2,27 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Input.StateChanges;
 
 namespace osu.Game.Input.Handlers
 {
+    public class VirtualInputHandler<T> : VirtualInputHandler
+        where T : struct
+    {
+        public new List<T> Actions => base.Actions.Cast<T>().ToList();
+
+        public override List<IInput> GetPendingInputs() => new List<IInput>()
+        {
+            new GameplayInputState<T>
+            {
+                PressedActions = Actions,
+            }
+        };
+    }
+
     public class VirtualInputHandler : GameplayInputHandler
     {
         public readonly List<object> Actions = new List<object>();
-
-        public override List<IInput> GetPendingInputs() => new List<IInput>();
     }
 }
