509cd77a436f13d0dbbc29a203ab546c56473da3
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-09-20 18:50:50 +0900

    Update LabelledTextBox to use LabelledComponent

--- osu.Game.Tests/Visual/UserInterface/TestSceneLabelledTextBox.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneLabelledTextBox.cs
@@ -19,6 +19,36 @@ namespace osu.Game.Tests.Visual.UserInterface
             typeof(LabelledTextBox),
         };
 
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestTextBox(bool hasDescription) => createTextBox(hasDescription);
+
+        private void createTextBox(bool hasDescription = false)
+        {
+            AddStep("create component", () =>
+            {
+                LabelledComponent component;
+
+                Child = new Container
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Width = 500,
+                    AutoSizeAxes = Axes.Y,
+                    Child = component = new LabelledTextBox
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Label = "Testing text",
+                        PlaceholderText = "This is definitely working as intended",
+                    }
+                };
+
+                component.Label = "a sample component";
+                component.Description = hasDescription ? "this text describes the component" : string.Empty;
+            });
+        }
+
         [BackgroundDependencyLoader]
         private void load()
         {
@@ -32,7 +62,7 @@ namespace osu.Game.Tests.Visual.UserInterface
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
-                    LabelText = "Testing text",
+                    Label = "Testing text",
                     PlaceholderText = "This is definitely working as intended",
                 }
             };
