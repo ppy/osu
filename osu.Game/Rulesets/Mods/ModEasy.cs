c99b48f9342189ba0290702893fbdcc1a7b72f2f
Dean Herbert <pe@ppy.sh>
2019-09-21 23:30:54 +0900

    Bring up-to-date and use IApplicableFailOverride

--- osu.Game/Rulesets/Mods/ModEasy.cs
+++ osu.Game/Rulesets/Mods/ModEasy.cs
@@ -2,16 +2,17 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Scoring;
+using osu.Game.Scoring;
 
 namespace osu.Game.Rulesets.Mods
 {
-    public abstract class ModEasy : Mod, IApplicableToDifficulty, IApplicableToScoreProcessor
+    public abstract class ModEasy : Mod, IApplicableToDifficulty, IApplicableFailOverride, IApplicableToScoreProcessor
     {
-        private int lives;
         public override string Name => "Easy";
         public override string Acronym => "EZ";
         public override IconUsage Icon => OsuIcon.ModEasy;
@@ -19,6 +20,10 @@ namespace osu.Game.Rulesets.Mods
         public override double ScoreMultiplier => 0.5;
         public override bool Ranked => true;
         public override Type[] IncompatibleMods => new[] { typeof(ModHardRock) };
+
+        private int retries = 2;
+
+        private BindableNumber<double> health;
 
         public void ApplyToDifficulty(BeatmapDifficulty difficulty)
         {
@@ -29,21 +34,26 @@ namespace osu.Game.Rulesets.Mods
             difficulty.OverallDifficulty *= ratio;
         }
 
+        public bool AllowFail
+        {
+            get
+            {
+                if (retries == 0) return true;
+
+                health.Value = health.MaxValue;
+                retries--;
+
+                return false;
+            }
+        }
+
+        public bool RestartOnFail => false;
+
         public void ApplyToScoreProcessor(ScoreProcessor scoreProcessor)
         {
-            //Note : The lives has to be instaciated here in order to prevent the values from different plays to interfear
-            //with each other / not reseting after a restart , as this method is called once a play starts (to my knowlegde).
-            //This will be better implemented with a List<double> once I know how to reliably get the game time and update it.
-            //If you know any information about that, please contact me because I didn't find a sollution to that.
-            lives = 2;
-            scoreProcessor.Health.ValueChanged += valueChanged =>
-            {
-                if (scoreProcessor.Health.Value == scoreProcessor.Health.MinValue && lives > 0)
-                {
-                    lives--;
-                    scoreProcessor.Health.Value = scoreProcessor.Health.MaxValue;
-                }
-            };
+            health = scoreProcessor.Health.GetBoundCopy();
         }
+
+        public ScoreRank AdjustRank(ScoreRank rank, double accuracy) => rank;
     }
 }
