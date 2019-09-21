c2ed4432054f7e665c9b1fafee5d440aa2100962
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-09-22 04:15:02 +0900

    Read from (and allow reloading) IPC source

--- osu.Game.Tournament/Screens/SetupScreen.cs
+++ osu.Game.Tournament/Screens/SetupScreen.cs
@@ -1,9 +1,24 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Tournament.IPC;
+
 namespace osu.Game.Tournament.Screens
 {
     public class SetupScreen : TournamentScreen
     {
+        [Resolved]
+        private MatchIPCInfo ipc { get; set; }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AddInternal(new SpriteText
+            {
+                Text = (ipc as FileBasedIPC)?.Storage.GetFullPath(string.Empty)
+            });
+        }
     }
 }
