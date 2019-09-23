0fbcced33753b38d21ce0b8beeff59a4a6d2b7d2
iiSaLMaN <87902114+bubb13gummy@users.noreply.github.com>
2019-09-23 08:15:27 +0300

    Introduce SessionStatics

---
+++ osu.Game/Configuration/SessionStatics.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Configuration;
+
+namespace osu.Game.Configuration
+{
+    public class SessionStatics : ConfigManager<Statics>
+    {
+        // This is an in-memory store.
+        protected override void PerformLoad()
+        {
+        }
+
+        protected override bool PerformSave() => true;
+    }
+
+    public enum Statics
+    {
+    }
+}
