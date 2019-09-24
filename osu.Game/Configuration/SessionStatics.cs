9e75112fcd947783ff6d69336dcc8f4417875924
iiSaLMaN <87902114+bubb13gummy@users.noreply.github.com>
2019-09-25 01:25:05 +0300

    Move in-memory logic to a base class

--- osu.Game/Configuration/SessionStatics.cs
+++ osu.Game/Configuration/SessionStatics.cs
@@ -1,18 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Configuration;
-
 namespace osu.Game.Configuration
 {
-    public class SessionStatics : ConfigManager<Statics>
+    public class SessionStatics : InMemoryConfigManager<Statics>
     {
-        // This is an in-memory store.
-        protected override void PerformLoad()
+        protected override void InitialiseDefaults()
         {
+            Set(Statics.LoginOverlayDisplayed, false);
         }
-
-        protected override bool PerformSave() => true;
     }
 
     public enum Statics
