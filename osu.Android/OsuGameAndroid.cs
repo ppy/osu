a64887e47dbd633032921316b3382c110d51b34b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-09-24 17:45:45 +0900

    Read build from VersionCode

--- osu.Android/OsuGameAndroid.cs
+++ osu.Android/OsuGameAndroid.cs
@@ -9,6 +9,23 @@ namespace osu.Android
 {
     public class OsuGameAndroid : OsuGame
     {
-        public override Version AssemblyVersion => new Version(Application.Context.ApplicationContext.PackageManager.GetPackageInfo(Application.Context.ApplicationContext.PackageName, 0).VersionName);
+        public override Version AssemblyVersion
+        {
+            get
+            {
+                string versionName = Application.Context.ApplicationContext.PackageManager.GetPackageInfo(Application.Context.ApplicationContext.PackageName, 0).VersionCode.ToString();
+
+                try
+                {
+                    // undo play store version garbling
+                    return new Version(int.Parse(versionName.Substring(0, 4)), int.Parse(versionName.Substring(4, 4)), int.Parse(versionName.Substring(8, 1)));
+                }
+                catch
+                {
+                }
+
+                return new Version(versionName);
+            }
+        }
     }
 }
