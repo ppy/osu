3678c08ecce1efc46e13e9f9995c0e3203055f25
Andrei Zavatski <megaman9919@gmail.com>
2019-09-26 11:16:44 +0300

    Merge dependencies

---
+++ osu.Game/Online/API/Requests/GetCountryRankingsRequest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class GetCountryRankingsRequest : GetRankingsRequest<APICountryRankings>
+    {
+        public GetCountryRankingsRequest(RulesetInfo ruleset, int page = 1)
+            : base(ruleset, page)
+        {
+        }
+
+        protected override string TargetPostfix() => "country";
+    }
+}
