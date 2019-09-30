7324de0ea5dbb9f673f65870ac6cbd796020c0bc
iiSaLMaN <slmanarendo1950@gmail.com>
2019-09-30 18:52:04 +0300

    Add migration

---
+++ osu.Game/Migrations/20190930135517_AddSkinInfoVersion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace osu.Game.Migrations
+{
+    public partial class AddSkinInfoVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Version",
+                table: "SkinInfo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "SkinInfo");
+        }
+    }
+}
