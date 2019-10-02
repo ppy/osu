fcc343f13e0b7e900b75e8db3a986084c7387a4a
Joehu <87902114+bubb13gummy@users.noreply.github.com>
2019-10-02 16:08:24 -0700

    Revert "Fix alt-f4 being blocked in interface"
    This reverts commit d6e95e52e11cc977dc0ec355e28e2275183a4f78.

--- osu.Game/Screens/Menu/MainMenu.cs
+++ osu.Game/Screens/Menu/MainMenu.cs
@@ -81,8 +81,8 @@ namespace osu.Game.Screens.Menu
                     {
                         if (holdDelay.Value > 0)
                             confirmAndExit();
-                        else if (!exitConfirmed && dialogOverlay != null && !(dialogOverlay.CurrentDialog is ConfirmExitDialog))
-                            dialogOverlay.Push(new ConfirmExitDialog(confirmAndExit, () => exitConfirmOverlay.Abort()));
+                        else
+                            this.Exit();
                     }
                 });
             }
@@ -253,6 +253,12 @@ namespace osu.Game.Screens.Menu
 
         public override bool OnExiting(IScreen next)
         {
+            if (!exitConfirmed && dialogOverlay != null && !(dialogOverlay.CurrentDialog is ConfirmExitDialog))
+            {
+                dialogOverlay.Push(new ConfirmExitDialog(confirmAndExit, () => exitConfirmOverlay.Abort()));
+                return true;
+            }
+
             buttons.State = ButtonSystemState.Exit;
             this.FadeOut(3000);
             return base.OnExiting(next);
