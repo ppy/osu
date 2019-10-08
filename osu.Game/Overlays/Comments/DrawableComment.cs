f1df6faa1c608b7e9c3e5dbda04f78569764234c
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2019-10-08 14:51:12 +0300

    Move all the logic to it's own namespace

---
+++ osu.Game/Overlays/Comments/DrawableComment.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osuTK;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Users.Drawables;
+using osu.Game.Graphics.Containers;
+using osu.Game.Utils;
+
+namespace osu.Game.Overlays.Comments
+{
+    public class DrawableComment : CompositeDrawable
+    {
+        private const int avatar_size = 40;
+        private const int margin = 10;
+
+        public DrawableComment(Comment comment)
+        {
+            LinkFlowContainer username;
+
+            RelativeSizeAxes = Axes.X;
+            AutoSizeAxes = Axes.Y;
+            InternalChild = new GridContainer
+            {
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                Margin = new MarginPadding(margin),
+                ColumnDimensions = new[]
+                {
+                    new Dimension(GridSizeMode.AutoSize),
+                    new Dimension(),
+                },
+                RowDimensions = new[]
+                {
+                    new Dimension(GridSizeMode.AutoSize),
+                    new Dimension(GridSizeMode.AutoSize)
+                },
+                Content = new[]
+                {
+                    new Drawable[]
+                    {
+                        new UpdateableAvatar(comment.User)
+                        {
+                            Size = new Vector2(avatar_size),
+                            Margin = new MarginPadding { Horizontal = margin },
+                            Masking = true,
+                            CornerRadius = avatar_size / 2,
+                        },
+                        new FillFlowContainer
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y,
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Spacing = new Vector2(0, 2),
+                            Children = new Drawable[]
+                            {
+                                username = new LinkFlowContainer(s => s.Font = OsuFont.GetFont(size: 14, weight: FontWeight.Bold, italics: true))
+                                {
+                                    Anchor = Anchor.CentreLeft,
+                                    Origin = Anchor.CentreLeft,
+                                    AutoSizeAxes = Axes.Both,
+                                },
+                                new TextFlowContainer(s => s.Font = OsuFont.GetFont(size: 18))
+                                {
+                                    Anchor = Anchor.CentreLeft,
+                                    Origin = Anchor.CentreLeft,
+                                    RelativeSizeAxes = Axes.X,
+                                    AutoSizeAxes = Axes.Y,
+                                    Text = comment.GetMessage()
+                                }
+                            }
+                        }
+                    },
+                    new Drawable[]
+                    {
+                        new Container
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                        },
+                        new SpriteText
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Font = OsuFont.GetFont(size: 12),
+                            Text = HumanizerUtils.Humanize(comment.CreatedAt)
+                        }
+                    }
+                }
+            };
+
+            username.AddUserLink(comment.User);
+        }
+    }
+}
