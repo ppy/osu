4462d454e87fba91a25b92b3bc4fef0a9ba6007c
Andrei Zavatski <megaman9919@gmail.com>
2019-10-09 10:34:17 +0300

    Message padding improvements

--- osu.Game/Overlays/Comments/CommentsContainer.cs
+++ osu.Game/Overlays/Comments/CommentsContainer.cs
@@ -16,6 +16,7 @@ namespace osu.Game.Overlays.Comments
     public class CommentsContainer : CompositeDrawable
     {
         private const float separator_height = 1.5f;
+        private const int padding = 40;
 
         private readonly CommentableType type;
         private readonly long id;
@@ -92,7 +93,13 @@ namespace osu.Game.Overlays.Comments
                 if (!c.IsDeleted && c.IsTopLevel)
                     content.AddRange(new Drawable[]
                     {
-                        new DrawableComment(c),
+                        new Container
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y,
+                            Padding = new MarginPadding { Right = padding },
+                            Child = new DrawableComment(c)
+                        },
                         new Container
                         {
                             RelativeSizeAxes = Axes.X,
