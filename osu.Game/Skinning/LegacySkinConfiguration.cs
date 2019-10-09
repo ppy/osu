003af19e3f039985ec73565cee3d3ad7345e0e9b
iiSaLMaN <slmanarendo1950@gmail.com>
2019-10-09 23:04:34 +0300

    Introduce legacy skin configuration

---
+++ osu.Game/Skinning/LegacySkinConfiguration.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Skinning
+{
+    public class LegacySkinConfiguration : DefaultSkinConfiguration
+    {
+        public const double LATEST_VERSION = 2.5;
+
+        /// <summary>
+        /// Legacy version of this skin.
+        /// </summary>
+        public double? LegacyVersion { get; internal set; }
+    }
+
+    public enum LegacySkinConfigurations
+    {
+        Version,
+    }
+}
