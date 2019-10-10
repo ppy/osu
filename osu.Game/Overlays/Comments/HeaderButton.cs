64f62bd2bfbccd702bb39039bc77166eb16a1b0b
Andrei Zavatski <megaman9919@gmail.com>
2019-10-10 12:06:25 +0300

    Implement CommentsHeader component

---
+++ osu.Game/Overlays/Comments/HeaderButton.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Framework.Input.Events;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Game.Overlays.Comments
+{
+    public class HeaderButton : Container
+    {
+        private const int height = 20;
+        private const int corner_radius = 3;
+        private const int margin = 10;
+        private const int duration = 200;
+
+        protected override Container<Drawable> Content => content;
+
+        private readonly Box background;
+        private readonly Container content;
+
+        public HeaderButton()
+        {
+            AutoSizeAxes = Axes.X;
+            Height = height;
+            Masking = true;
+            CornerRadius = corner_radius;
+            AddRangeInternal(new Drawable[]
+            {
+                background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = 0,
+                },
+                content = new Container
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Margin = new MarginPadding { Horizontal = margin }
+                },
+                new HoverClickSounds(),
+            });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            background.Colour = colours.Gray4;
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            FadeInBackground();
+            return base.OnHover(e);
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            base.OnHoverLost(e);
+            FadeOutBackground();
+        }
+
+        protected void FadeInBackground() => background.FadeIn(duration, Easing.OutQuint);
+
+        protected void FadeOutBackground() => background.FadeOut(duration, Easing.OutQuint);
+    }
+}
---
+++ osu.Game/Overlays/Comments/SortSelector.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osuTK;
+using osu.Game.Graphics.UserInterface;
+using osu.Framework.Input.Events;
+using osu.Framework.Bindables;
+using osu.Framework.Allocation;
+using osuTK.Graphics;
+
+namespace osu.Game.Overlays.Comments
+{
+    public class SortSelector : OsuTabControl<SortCommentsBy>
+    {
+        private const int spacing = 5;
+
+        protected override Dropdown<SortCommentsBy> CreateDropdown() => null;
+
+        protected override TabItem<SortCommentsBy> CreateTabItem(SortCommentsBy value) => new SortTabItem(value);
+
+        protected override TabFillFlowContainer CreateTabFlow() => new TabFillFlowContainer
+        {
+            AutoSizeAxes = Axes.Both,
+            Direction = FillDirection.Horizontal,
+            Spacing = new Vector2(spacing, 0),
+        };
+
+        public SortSelector()
+        {
+            AutoSizeAxes = Axes.Both;
+        }
+
+        private class SortTabItem : TabItem<SortCommentsBy>
+        {
+            private readonly TabContent content;
+
+            public SortTabItem(SortCommentsBy value)
+                : base(value)
+            {
+                AutoSizeAxes = Axes.Both;
+                Child = content = new TabContent(value)
+                {
+                    Active = { BindTarget = Active }
+                };
+            }
+
+            protected override void OnActivated() => content.Activate();
+
+            protected override void OnDeactivated() => content.Deactivate();
+
+            private class TabContent : HeaderButton
+            {
+                private const int text_size = 14;
+
+                public readonly BindableBool Active = new BindableBool();
+
+                [Resolved]
+                private OsuColour colours { get; set; }
+
+                private readonly SpriteText text;
+
+                public TabContent(SortCommentsBy value)
+                {
+                    Add(text = new SpriteText
+                    {
+                        Font = OsuFont.GetFont(size: text_size),
+                        Text = value.ToString()
+                    });
+                }
+
+                public void Activate()
+                {
+                    FadeInBackground();
+                    text.Font = text.Font.With(weight: FontWeight.Bold);
+                    text.Colour = colours.BlueLighter;
+                }
+
+                public void Deactivate()
+                {
+                    if (!IsHovered)
+                        FadeOutBackground();
+
+                    text.Font = text.Font.With(weight: FontWeight.Medium);
+                    text.Colour = Color4.White;
+                }
+
+                protected override void OnHoverLost(HoverLostEvent e)
+                {
+                    if (!Active.Value) base.OnHoverLost(e);
+                }
+            }
+        }
+    }
+
+    public enum SortCommentsBy
+    {
+        New,
+        Old,
+        Top
+    }
+}
