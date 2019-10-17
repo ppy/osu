e5b50b5e1fda974c7e47ec5be852ff0ec98bcf63
Joehu <madamba.joehu@outlook.com>
2019-10-17 13:54:36 -0700

    Fix slider bar regression when using arrows

--- osu.Game/Overlays/SettingsPanel.cs
+++ osu.Game/Overlays/SettingsPanel.cs
@@ -37,7 +37,7 @@ namespace osu.Game.Overlays
 
         protected SettingsSectionsContainer SectionsContainer;
 
-        private SearchTextBox searchTextBox;
+        private FixedSearchTextBox searchTextBox;
 
         /// <summary>
         /// Provide a source for the toolbar height.
@@ -80,7 +80,7 @@ namespace osu.Game.Overlays
                         Masking = true,
                         RelativeSizeAxes = Axes.Both,
                         ExpandableHeader = CreateHeader(),
-                        FixedHeader = searchTextBox = new SearchTextBox
+                        FixedHeader = searchTextBox = new FixedSearchTextBox
                         {
                             RelativeSizeAxes = Axes.X,
                             Origin = Anchor.TopCentre,
