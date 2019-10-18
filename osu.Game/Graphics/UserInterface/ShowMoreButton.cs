00f7d9b3ee6680a6a1a5b32637ccaace3b591f64
Andrei Zavatski <megaman9919@gmail.com>
2019-10-18 19:29:02 +0300

    Merge dependencies

--- osu.Game/Graphics/UserInterface/ShowMoreButton.cs
+++ osu.Game/Graphics/UserInterface/ShowMoreButton.cs
@@ -5,8 +5,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Input.Events;
-using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
 using osuTK;
 using osuTK.Graphics;
@@ -14,10 +12,8 @@ using System.Collections.Generic;
 
 namespace osu.Game.Graphics.UserInterface
 {
-    public class ShowMoreButton : OsuHoverContainer
+    public class ShowMoreButton : LoadingButton
     {
-        private const float fade_duration = 200;
-
         private Color4 chevronIconColour;
 
         protected Color4 ChevronIconColour
@@ -32,100 +28,48 @@ namespace osu.Game.Graphics.UserInterface
             set => text.Text = value;
         }
 
-        private bool isLoading;
-
-        public bool IsLoading
-        {
-            get => isLoading;
-            set
-            {
-                isLoading = value;
-
-                Enabled.Value = !isLoading;
-
-                if (value)
-                {
-                    loading.Show();
-                    content.FadeOut(fade_duration, Easing.OutQuint);
-                }
-                else
-                {
-                    loading.Hide();
-                    content.FadeIn(fade_duration, Easing.OutQuint);
-                }
-            }
-        }
-
-        private readonly Box background;
-        private readonly LoadingAnimation loading;
-        private readonly FillFlowContainer content;
-        private readonly ChevronIcon leftChevron;
-        private readonly ChevronIcon rightChevron;
-        private readonly SpriteText text;
-
         protected override IEnumerable<Drawable> EffectTargets => new[] { background };
+
+        private ChevronIcon leftChevron;
+        private ChevronIcon rightChevron;
+        private SpriteText text;
+        private Box background;
 
         public ShowMoreButton()
         {
             AutoSizeAxes = Axes.Both;
+        }
+
+        protected override Container CreateBackground() => new CircularContainer
+        {
+            Masking = true,
+            Size = new Vector2(140, 30),
+            Child = background = new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+            }
+        };
+
+        protected override Drawable CreateContent() => new FillFlowContainer
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            AutoSizeAxes = Axes.Both,
+            Direction = FillDirection.Horizontal,
+            Spacing = new Vector2(7),
             Children = new Drawable[]
             {
-                new CircularContainer
+                leftChevron = new ChevronIcon(),
+                text = new OsuSpriteText
                 {
-                    Masking = true,
-                    Size = new Vector2(140, 30),
-                    Children = new Drawable[]
-                    {
-                        background = new Box
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                        },
-                        content = new FillFlowContainer
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            AutoSizeAxes = Axes.Both,
-                            Direction = FillDirection.Horizontal,
-                            Spacing = new Vector2(7),
-                            Children = new Drawable[]
-                            {
-                                leftChevron = new ChevronIcon(),
-                                text = new OsuSpriteText
-                                {
-                                    Anchor = Anchor.Centre,
-                                    Origin = Anchor.Centre,
-                                    Font = OsuFont.GetFont(size: 12, weight: FontWeight.Bold),
-                                    Text = "show more".ToUpper(),
-                                },
-                                rightChevron = new ChevronIcon(),
-                            }
-                        },
-                        loading = new LoadingAnimation
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            Size = new Vector2(12)
-                        },
-                    }
-                }
-            };
-        }
-
-        protected override bool OnClick(ClickEvent e)
-        {
-            if (!Enabled.Value)
-                return false;
-
-            try
-            {
-                return base.OnClick(e);
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Font = OsuFont.GetFont(size: 12, weight: FontWeight.Bold),
+                    Text = "show more".ToUpper(),
+                },
+                rightChevron = new ChevronIcon(),
             }
-            finally
-            {
-                // run afterwards as this will disable this button.
-                IsLoading = true;
-            }
-        }
+        };
 
         private class ChevronIcon : SpriteIcon
         {
