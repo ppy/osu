d1fdb8fc2c716f9eb50c15e56f728b7e8e7c9989
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-10-20 23:32:49 +0900

    Add missing required types

--- osu.Game.Tests/Visual/Editor/TestSceneTimingScreen.cs
+++ osu.Game.Tests/Visual/Editor/TestSceneTimingScreen.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Game.Rulesets.Osu;
@@ -11,6 +13,12 @@ namespace osu.Game.Tests.Visual.Editor
     [TestFixture]
     public class TestSceneTimingScreen : EditorClockTestScene
     {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(ControlPointTable),
+            typeof(RowAttribute)
+        };
+
         [BackgroundDependencyLoader]
         private void load()
         {
