f573313e7f84ea1ba4f645b77c07923336afe1de
hebuwei <hebuweitom@gmail.com>
2019-10-28 10:14:16 +0800

    nerf small jumps by rebalancing everything

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -259,7 +259,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             return modifiedAcc;
         }
 
-        private double tpToPP(double tp) => Math.Pow(tp, skillToPPExponent) * 0.126;
+        private double tpToPP(double tp) => Math.Pow(tp, skillToPPExponent) * 0.120f;
 
         private double tapSkillToPP(double tapSkill) => Math.Pow(tapSkill, skillToPPExponent) * 0.115f;
 
