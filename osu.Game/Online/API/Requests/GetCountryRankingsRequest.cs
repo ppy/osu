d99b3cb46bd323b5863476b028975beb9be8234e
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2019-11-01 21:49:50 +0300

    Update API with latest web changes

--- osu.Game/Online/API/Requests/GetCountryRankingsRequest.cs
+++ osu.Game/Online/API/Requests/GetCountryRankingsRequest.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Rulesets;
 
 namespace osu.Game.Online.API.Requests
 {
-    public class GetCountryRankingsRequest : GetRankingsRequest<APICountryRankings>
+    public class GetCountryRankingsRequest : GetRankingsRequest<GetCountriesResponse>
     {
         public GetCountryRankingsRequest(RulesetInfo ruleset, int page = 1)
             : base(ruleset, page)
