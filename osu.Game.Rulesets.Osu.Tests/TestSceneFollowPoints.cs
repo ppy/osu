74a2f91433be0651e9fddd72956d7c42ea1c4dbf
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-11-01 15:13:50 +0900

    Remove unnecessary class

--- osu.Game.Rulesets.Osu.Tests/TestSceneFollowPoints.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneFollowPoints.cs
@@ -6,21 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using NUnit.Framework;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Effects;
-using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
-using osu.Game.Skinning;
+using osu.Game.Rulesets.Osu.Objects.Drawables.Connections;
 using osu.Game.Tests.Visual;
 using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Osu.Tests
 {
@@ -357,39 +353,6 @@ namespace osu.Game.Rulesets.Osu.Tests
 
                     fp.Expire(true);
                 }
-            }
-        }
-
-        private class FollowPoint : CompositeDrawable
-        {
-            private const float width = 8;
-
-            public override bool RemoveWhenNotAlive => false;
-
-            public FollowPoint()
-            {
-                Origin = Anchor.Centre;
-
-                InternalChild = new SkinnableDrawable(new OsuSkinComponent(OsuSkinComponents.FollowPoint), _ => new Container
-                {
-                    Masking = true,
-                    AutoSizeAxes = Axes.Both,
-                    CornerRadius = width / 2,
-                    EdgeEffect = new EdgeEffectParameters
-                    {
-                        Type = EdgeEffectType.Glow,
-                        Colour = Color4.White.Opacity(0.2f),
-                        Radius = 4,
-                    },
-                    Child = new Box
-                    {
-                        Size = new Vector2(width),
-                        Blending = BlendingParameters.Additive,
-                        Origin = Anchor.Centre,
-                        Anchor = Anchor.Centre,
-                        Alpha = 0.5f,
-                    }
-                }, confineMode: ConfineMode.NoScaling);
             }
         }
     }
