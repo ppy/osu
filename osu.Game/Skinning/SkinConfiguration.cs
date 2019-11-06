cef6e2a26b49cdecd9663aa4af586292c8e8b4e6
iiSaLMaN <slmanarendo1950@gmail.com>
2019-11-06 23:20:36 +0300

    Move colours fallback logic to SkinConfiguration.ComboColours getter

--- osu.Game/Skinning/SkinConfiguration.cs
+++ osu.Game/Skinning/SkinConfiguration.cs
@@ -14,7 +14,32 @@ namespace osu.Game.Skinning
     {
         public readonly SkinInfo SkinInfo = new SkinInfo();
 
-        public List<Color4> ComboColours { get; set; } = new List<Color4>();
+        internal bool AllowDefaultColoursFallback;
+
+        public static List<Color4> DefaultComboColours = new List<Color4>
+        {
+            new Color4(255, 192, 0, 255),
+            new Color4(0, 202, 0, 255),
+            new Color4(18, 124, 255, 255),
+            new Color4(242, 24, 57, 255),
+        };
+
+        private List<Color4> comboColours = new List<Color4>();
+
+        public List<Color4> ComboColours
+        {
+            get
+            {
+                if (comboColours.Count > 0)
+                    return comboColours;
+
+                if (AllowDefaultColoursFallback)
+                    return DefaultComboColours;
+
+                return null;
+            }
+            set => comboColours = value;
+        }
 
         public Dictionary<string, Color4> CustomColours { get; set; } = new Dictionary<string, Color4>();
 
