3b13ad480af5afa7f0fe15c300a5e02bcf0fb4d7
smoogipoo <smoogipoo@smgi.me>
2019-11-11 13:06:41 +0900

    Increase fade-out time of hitobjects in the editor

--- osu.Game.Rulesets.Osu/Edit/DrawableOsuEditRuleset.cs
+++ osu.Game.Rulesets.Osu/Edit/DrawableOsuEditRuleset.cs
@@ -2,8 +2,12 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.UI;
 using osuTK;
@@ -15,6 +19,25 @@ namespace osu.Game.Rulesets.Osu.Edit
         public DrawableOsuEditRuleset(Ruleset ruleset, IWorkingBeatmap beatmap, IReadOnlyList<Mod> mods)
             : base(ruleset, beatmap, mods)
         {
+        }
+
+        public override DrawableHitObject<OsuHitObject> CreateDrawableRepresentation(OsuHitObject h)
+            => base.CreateDrawableRepresentation(h)?.With(d => d.ApplyCustomUpdateState += updateState);
+
+        private void updateState(DrawableHitObject hitObject, ArmedState state)
+        {
+            switch (state)
+            {
+                case ArmedState.Miss:
+                    // Get the existing fade out transform
+                    var existing = hitObject.Transforms.LastOrDefault(t => t.TargetMember == nameof(Alpha));
+                    if (existing == null)
+                        return;
+
+                    using (hitObject.BeginAbsoluteSequence(existing.StartTime))
+                        hitObject.FadeOut(500).Expire();
+                    break;
+            }
         }
 
         protected override Playfield CreatePlayfield() => new OsuPlayfieldNoCursor();
