46681e2f329eb3d52c9a9af0accbb2bc35962b81
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-11-15 13:47:14 +0900

    Allow screen to specify whether to apply audio rate adjustments from mods

--- osu.Game/Screens/StartupScreen.cs
+++ osu.Game/Screens/StartupScreen.cs
@@ -16,6 +16,8 @@ namespace osu.Game.Screens
 
         public override bool CursorVisible => false;
 
+        public override bool AllowRateAdjustments => false;
+
         public override OverlayActivation InitialOverlayActivationMode => OverlayActivation.Disabled;
     }
 }
