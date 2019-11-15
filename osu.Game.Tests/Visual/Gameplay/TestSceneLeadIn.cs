d0e0badf4e6378d3a91bf7f2566c1c8747317cfc
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-11-15 14:47:28 +0900

    Improve test quality

--- osu.Game.Tests/Visual/Gameplay/TestSceneLeadIn.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneLeadIn.cs
@@ -1,61 +1,55 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using NUnit.Framework;
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.Sprites;
-using osu.Game.Rulesets;
+using osu.Game.Rulesets.Osu;
 using osu.Game.Screens.Play;
 using osu.Game.Tests.Beatmaps;
-using osuTK.Graphics;
 
 namespace osu.Game.Tests.Visual.Gameplay
 {
     public class TestSceneLeadIn : RateAdjustedBeatmapTestScene
     {
-        private Ruleset ruleset;
-
         private LeadInPlayer player;
-
-        [BackgroundDependencyLoader]
-        private void load(RulesetStore rulesets)
-        {
-            Add(new Box
-            {
-                RelativeSizeAxes = Axes.Both,
-                Colour = Color4.Black,
-                Depth = int.MaxValue
-            });
-
-            ruleset = rulesets.AvailableRulesets.First().CreateInstance();
-        }
 
         [Test]
         public void TestShortLeadIn()
         {
-            AddStep("create player", () => loadPlayerWithBeatmap(new TestBeatmap(ruleset.RulesetInfo) { BeatmapInfo = { AudioLeadIn = 1000 } }));
-            AddUntilStep("correct lead-in", () => player.FirstFrameClockTime == 0);
+            loadPlayerWithBeatmap(new TestBeatmap(new OsuRuleset().RulesetInfo)
+            {
+                BeatmapInfo = { AudioLeadIn = 1000 }
+            });
+
+            AddUntilStep("player loaded", () => player.IsLoaded && player.Alpha == 1);
+
+            AddAssert("correct lead-in", () => player.FirstFrameClockTime == 0);
         }
 
         [Test]
         public void TestLongLeadIn()
         {
-            AddStep("create player", () => loadPlayerWithBeatmap(new TestBeatmap(ruleset.RulesetInfo) { BeatmapInfo = { AudioLeadIn = 10000 } }));
-            AddUntilStep("correct lead-in", () => player.FirstFrameClockTime == player.GameplayStartTime - 10000);
+            loadPlayerWithBeatmap(new TestBeatmap(new OsuRuleset().RulesetInfo)
+            {
+                BeatmapInfo = { AudioLeadIn = 10000 }
+            });
+
+            AddAssert("correct lead-in", () => player.FirstFrameClockTime == player.GameplayStartTime - 10000);
         }
 
         private void loadPlayerWithBeatmap(IBeatmap beatmap)
         {
-            Beatmap.Value = new TestWorkingBeatmap(beatmap);
+            AddStep("create player", () =>
+            {
+                Beatmap.Value = CreateWorkingBeatmap(beatmap);
+                LoadScreen(player = new LeadInPlayer());
+            });
 
-            LoadScreen(player = new LeadInPlayer());
+            AddUntilStep("player loaded", () => player.IsLoaded && player.Alpha == 1);
         }
 
-        private class LeadInPlayer : Player
+        private class LeadInPlayer : TestPlayer
         {
             public LeadInPlayer()
                 : base(false, false)
