800bda7e815caa2ff0739642444e431acf49c1f3
Andrei Zavatski <megaman9919@gmail.com>
2019-11-27 23:35:02 +0300

    Refactor tables to avoid code duplication

--- osu.Game/Overlays/Rankings/Tables/ScoresTable.cs
+++ osu.Game/Overlays/Rankings/Tables/ScoresTable.cs
@@ -3,97 +3,34 @@
 
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
-using osu.Game.Users.Drawables;
-using osuTK;
-using System.Collections.Generic;
-using osu.Game.Graphics.Containers;
 using osu.Game.Users;
 
 namespace osu.Game.Overlays.Rankings.Tables
 {
-    public class ScoresTable : RankingsTable<UserStatistics>
+    public class ScoresTable : UserBasedTable
     {
         public ScoresTable(int page = 1)
             : base(page)
         {
         }
 
-        protected override TableColumn[] CreateAdditionalHeaders() => new[]
+        protected override TableColumn[] CreateUniqueHeaders() => new[]
         {
-            new TableColumn("Accuracy", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
-            new TableColumn("Play Count", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
             new TableColumn("Total Score", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
-            new TableColumn("Ranked Score", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
-            new TableColumn("SS", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
-            new TableColumn("S", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
-            new TableColumn("A", Anchor.Centre, new Dimension(GridSizeMode.AutoSize)),
+            new TableColumn("Ranked Score", Anchor.Centre, new Dimension(GridSizeMode.AutoSize))
         };
 
-        protected override Drawable[] CreateContent(int index, UserStatistics item)
+        protected override Drawable[] CreateUniqueContent(UserStatistics item) => new[]
         {
-            var content = new List<Drawable>
+            new ColoredRowText
             {
-                new OsuSpriteText
-                {
-                    Text = $"#{index + 1}",
-                    Font = OsuFont.GetFont(size: TEXT_SIZE, weight: FontWeight.Bold)
-                },
-            };
-
-            var username = new LinkFlowContainer(t => t.Font = OsuFont.GetFont(size: TEXT_SIZE)) { AutoSizeAxes = Axes.Both };
-            username.AddUserLink(item.User);
-
-            content.AddRange(new Drawable[]
+                Text = $@"{item.TotalScore:N0}",
+            },
+            new RowText
             {
-                new FillFlowContainer
-                {
-                    AutoSizeAxes = Axes.Both,
-                    Direction = FillDirection.Horizontal,
-                    Spacing = new Vector2(7, 0),
-                    Children = new Drawable[]
-                    {
-                        new UpdateableFlag(item.User.Country)
-                        {
-                            Size = new Vector2(20, 13),
-                            ShowPlaceholderOnNull = false,
-                        },
-                        username
-                    }
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.Accuracy:F2}%",
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.PlayCount:N0}",
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.TotalScore:N0}",
-                },
-                new RowText
-                {
-                    Text = $@"{item.RankedScore:N0}",
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.GradesCount.SS + item.GradesCount.SSPlus:N0}",
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.GradesCount.S + item.GradesCount.SPlus:N0}",
-                },
-                new ColoredRowText
-                {
-                    Text = $@"{item.GradesCount.A:N0}",
-                },
-            });
-
-            return content.ToArray();
-        }
+                Text = $@"{item.RankedScore:N0}",
+            }
+        };
 
         protected override string HighlightedColumn() => @"Ranked Score";
     }
