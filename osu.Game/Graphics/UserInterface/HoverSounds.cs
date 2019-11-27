3a1ead86b6309d5f0041537eaf67de9eb7b717b8
phosphene47 <87902114+bubb13gummy@users.noreply.github.com>
2019-11-27 22:47:00 +1100

    Debounce hover sounds

--- osu.Game/Graphics/UserInterface/HoverSounds.cs
+++ osu.Game/Graphics/UserInterface/HoverSounds.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.ComponentModel;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
@@ -9,6 +10,7 @@ using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Input.Events;
+using osu.Framework.Threading;
 
 namespace osu.Game.Graphics.UserInterface
 {
@@ -20,6 +22,12 @@ namespace osu.Game.Graphics.UserInterface
     {
         private SampleChannel sampleHover;
 
+        /// <summary>
+        /// Length of debounce for sound playback.
+        /// Set this to <see cref="TimeSpan.Zero"/> to disable debouncing.
+        /// </summary>
+        public TimeSpan DebounceTime { get; set; } = TimeSpan.FromMilliseconds(50);
+
         protected readonly HoverSampleSet SampleSet;
 
         public HoverSounds(HoverSampleSet sampleSet = HoverSampleSet.Normal)
@@ -28,9 +36,19 @@ namespace osu.Game.Graphics.UserInterface
             RelativeSizeAxes = Axes.Both;
         }
 
+        private ScheduledDelegate playDelegate;
+
         protected override bool OnHover(HoverEvent e)
         {
-            sampleHover?.Play();
+            playDelegate?.Cancel();
+
+            var debounceMs = (int)DebounceTime.TotalMilliseconds;
+
+            if (debounceMs == 0)
+                sampleHover?.Play();
+            else
+                playDelegate = Scheduler.AddDelayed(() => sampleHover?.Play(), debounceMs);
+
             return base.OnHover(e);
         }
 
