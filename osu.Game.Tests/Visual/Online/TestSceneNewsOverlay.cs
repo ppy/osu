f5e44c73845545a6ac4efff3fa0aab6fec72399e
Lucas A <game4allyt@gmail.com>
2019-11-29 19:02:19 +0100

    Update visual tests for NewsOverlay

--- osu.Game.Tests/Visual/Online/TestSceneNewsOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneNewsOverlay.cs
@@ -2,22 +2,32 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Overlays;
+using osu.Game.Overlays.News;
 
 namespace osu.Game.Tests.Visual.Online
 {
     public class TestSceneNewsOverlay : OsuTestScene
     {
-        private NewsOverlay news;
+        private TestNewsOverlay news;
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
-            Add(news = new NewsOverlay());
+            Add(news = new TestNewsOverlay());
             AddStep(@"Show", news.Show);
             AddStep(@"Hide", news.Hide);
 
             AddStep(@"Show front page", () => news.ShowFrontPage());
             AddStep(@"Custom article", () => news.Current.Value = "Test Article 101");
+        }
+
+        private class TestNewsOverlay : NewsOverlay
+        {
+            public TestNewsOverlay()
+            {
+            }
+
+            public void ShowContent(NewsContent content) => LoadChildContent(content);
         }
     }
 }
