262aec59ffbaf2d2b3df8471e2ead1810d604e53
Lucas A <game4allyt@gmail.com>
2019-11-29 19:16:45 +0100

    Add NewsArticleCover

--- osu.Game.Tests/Visual/Online/TestSceneNewsOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneNewsOverlay.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
 using osu.Game.Overlays;
 using osu.Game.Overlays.News;
 
@@ -19,6 +24,8 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep(@"Show front page", () => news.ShowFrontPage());
             AddStep(@"Custom article", () => news.Current.Value = "Test Article 101");
+
+            AddStep(@"News article covers", () => news.ShowContent(new TestNewsContent()));
         }
 
         private class TestNewsOverlay : NewsOverlay
@@ -28,6 +35,40 @@ namespace osu.Game.Tests.Visual.Online
             }
 
             public void ShowContent(NewsContent content) => LoadChildContent(content);
+        }
+
+        private class TestNewsContent : NewsContent
+        {
+            public TestNewsContent()
+            {
+                Spacing = new osuTK.Vector2(0, 10);
+                Add(new TestCard("https://osu.ppy.sh/help/wiki/shared/news/banners/CWC_2019_banner.jpg"));
+                Add(new TestCard("https://osu.ppy.sh/help/wiki/shared/news/banners/CWC_2019_banner.jpg"));
+                Add(new TestCard("https://osu.ppy.sh/help/wiki/shared/news/banners/CWC_2019_banner.jpg"));
+            }
+
+            private class TestCard : Container
+            {
+                public TestCard(string url)
+                {
+                    RelativeSizeAxes = Axes.X;
+                    Height = 250;
+                    Masking = true;
+                    CornerRadius = 4;
+                    Children = new Drawable[]
+                    {
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = ColourInfo.GradientVertical(OsuColour.Gray(0.2f), OsuColour.Gray(0.1f))
+                        },
+                        new NewsArticleCover(url)
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                        }
+                    };
+                }
+            }
         }
     }
 }
---
+++ osu.Game/Overlays/News/NewsArticleCover.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+
+namespace osu.Game.Overlays.News
+{
+    public class NewsArticleCover : CompositeDrawable
+    {
+        public NewsArticleCover(string url)
+        {
+            NewsArticleCoverBackground background;
+            InternalChild = new DelayedLoadWrapper(background = new NewsArticleCoverBackground(url));
+
+            background.OnLoadComplete += bg => bg.FadeInFromZero(400, Easing.Out);
+        }
+
+        [LongRunningLoad]
+        private class NewsArticleCoverBackground : Sprite
+        {
+            private string url;
+
+            public NewsArticleCoverBackground(string url)
+            {
+                this.url = url;
+
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+                RelativeSizeAxes = Axes.Both;
+                FillMode = FillMode.Fill;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(LargeTextureStore store)
+            {
+                Texture = store.Get(url ?? "Headers/changelog");
+            }
+        }
+    }
+}
