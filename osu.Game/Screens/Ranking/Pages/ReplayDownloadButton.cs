0df8ce39d1a842c565f7ca358f2ff4fe23bec058
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-12-03 15:28:10 +0900

    Decouple APILegacyScoreInfo from ScoreInfo

--- osu.Game/Screens/Ranking/Pages/ReplayDownloadButton.cs
+++ osu.Game/Screens/Ranking/Pages/ReplayDownloadButton.cs
@@ -6,7 +6,6 @@ using osu.Framework.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Online;
-using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Scoring;
 using osuTK;
 
@@ -24,7 +23,7 @@ namespace osu.Game.Screens.Ranking.Pages
                 if (State.Value == DownloadState.LocallyAvailable)
                     return ReplayAvailability.Local;
 
-                if (Model.Value is APILegacyScoreInfo apiScore && apiScore.Replay)
+                if (!string.IsNullOrEmpty(Model.Value.Hash))
                     return ReplayAvailability.Online;
 
                 return ReplayAvailability.NotAvailable;
