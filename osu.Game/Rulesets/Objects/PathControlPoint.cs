f2385666ad58f866f17b65564dd6e2b07c318d6e
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-12-05 14:38:21 +0900

    Add xmldocs

--- osu.Game/Rulesets/Objects/PathControlPoint.cs
+++ osu.Game/Rulesets/Objects/PathControlPoint.cs
@@ -7,8 +7,15 @@ namespace osu.Game.Rulesets.Objects
 {
     public class PathControlPoint : IEquatable<PathControlPoint>
     {
+        /// <summary>
+        /// The position of this <see cref="PathControlPoint"/>.
+        /// </summary>
         public readonly Bindable<Vector2> Position = new Bindable<Vector2>();
 
+        /// <summary>
+        /// The type of path segment starting at this <see cref="PathControlPoint"/>.
+        /// If null, this <see cref="PathControlPoint"/> will be a part of the previous path segment.
+        /// </summary>
         public readonly Bindable<PathType?> Type = new Bindable<PathType?>();
 
         public bool Equals(PathControlPoint other) => Position.Value == other.Position.Value && Type.Value == other.Type.Value;
