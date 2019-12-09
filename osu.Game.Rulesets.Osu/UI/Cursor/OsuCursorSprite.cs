1e49078c526f2050a137f087cf0000f678f3484e
mcendu <nathandu@outlook.com>
2019-12-09 17:51:44 +0800

    Add OsuCursorSprite

---
+++ osu.Game.Rulesets.Osu/UI/Cursor/OsuCursorSprite.cs
@@ -0,0 +1,13 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Rulesets.Osu.UI.Cursor
+{
+    public class OsuCursorSprite : CompositeDrawable
+    {
+        public Drawable ExpandTarget { get; protected set; }
+    }
+}
