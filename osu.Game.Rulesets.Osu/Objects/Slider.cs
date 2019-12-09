bf80591abee3be2021448502764514fffcd4efad
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-12-09 17:48:27 +0900

    Refactor hitobjects to remove default control point

--- osu.Game.Rulesets.Osu/Objects/Slider.cs
+++ osu.Game.Rulesets.Osu/Objects/Slider.cs
@@ -27,7 +27,7 @@ namespace osu.Game.Rulesets.Osu.Objects
 
         public Vector2 StackedPositionAt(double t) => StackedPosition + this.CurvePositionAt(t);
 
-        private readonly SliderPath path = new SliderPath(new[] { new PathControlPoint { Type = { Value = PathType.Linear } } });
+        private readonly SliderPath path = new SliderPath();
 
         public SliderPath Path
         {
