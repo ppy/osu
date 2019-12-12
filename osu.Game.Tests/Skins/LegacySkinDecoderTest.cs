aaab059ad2932a1acad0f300232c97b88d481148
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2019-12-12 12:16:19 +0300

    Split combo colour decoding to small test cases

--- osu.Game.Tests/Skins/LegacySkinDecoderTest.cs
+++ osu.Game.Tests/Skins/LegacySkinDecoderTest.cs
@@ -13,47 +13,54 @@ namespace osu.Game.Tests.Skins
     [TestFixture]
     public class LegacySkinDecoderTest
     {
-        [TestCase(true)]
-        [TestCase(false)]
-        [TestCase(false, false)]
-        public void TestDecodeSkinColours(bool hasColours, bool canFallback = true)
+        public void TestDecodeSkinColours()
         {
             var decoder = new LegacySkinDecoder();
 
-            using (var resStream = TestResources.OpenResource(hasColours ? "skin.ini" : "skin-empty.ini"))
+            using (var resStream = TestResources.OpenResource("skin.ini"))
             using (var stream = new LineBufferedReader(resStream))
             {
-                var skinConfiguration = decoder.Decode(stream);
-                skinConfiguration.AllowDefaultComboColoursFallback = canFallback;
-
-                var comboColors = skinConfiguration.ComboColours;
-
-                if (!canFallback && !hasColours)
+                var comboColors = decoder.Decode(stream).ComboColours;
+                var expectedColors = new List<Color4>
                 {
-                    Assert.IsNull(comboColors);
-                    return;
-                }
-
-                List<Color4> expectedColors;
-
-                if (hasColours)
-                {
-                    expectedColors = new List<Color4>
-                    {
-                        new Color4(142, 199, 255, 255),
-                        new Color4(255, 128, 128, 255),
-                        new Color4(128, 255, 255, 255),
-                        new Color4(100, 100, 100, 100),
-                    };
-                }
-                else
-                {
-                    expectedColors = SkinConfiguration.DefaultComboColours;
-                }
+                    new Color4(142, 199, 255, 255),
+                    new Color4(255, 128, 128, 255),
+                    new Color4(128, 255, 255, 255),
+                    new Color4(100, 100, 100, 100),
+                };
 
                 Assert.AreEqual(expectedColors.Count, comboColors.Count);
                 for (int i = 0; i < expectedColors.Count; i++)
                     Assert.AreEqual(expectedColors[i], comboColors[i]);
+            }
+        }
+
+        public void TestDecodeEmptySkinColours()
+        {
+            var decoder = new LegacySkinDecoder();
+
+            using (var resStream = TestResources.OpenResource("skin-empty.ini"))
+            using (var stream = new LineBufferedReader(resStream))
+            {
+                var comboColors = decoder.Decode(stream).ComboColours;
+                var expectedColors = SkinConfiguration.DefaultComboColours;
+
+                Assert.AreEqual(expectedColors.Count, comboColors.Count);
+                for (int i = 0; i < expectedColors.Count; i++)
+                    Assert.AreEqual(expectedColors[i], comboColors[i]);
+            }
+        }
+
+        public void TestDecodeEmptySkinColoursNoFallback()
+        {
+            var decoder = new LegacySkinDecoder();
+
+            using (var resStream = TestResources.OpenResource("skin-empty.ini"))
+            using (var stream = new LineBufferedReader(resStream))
+            {
+                var skinConfiguration = decoder.Decode(stream);
+                skinConfiguration.AllowDefaultComboColoursFallback = false;
+                Assert.IsNull(skinConfiguration.ComboColours);
             }
         }
 
