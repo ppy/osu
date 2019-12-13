4e2f1bbf73f034889fd7c6b0d04cbd88ecd91698
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-12-13 18:13:24 +0900

    Fix incorrect asserts

--- osu.Game.Tests/NonVisual/ControlPointInfoTest.cs
+++ osu.Game.Tests/NonVisual/ControlPointInfoTest.cs
@@ -45,7 +45,7 @@ namespace osu.Game.Tests.NonVisual
             cpi.Add(1000, new DifficultyControlPoint()); // is redundant
 
             Assert.That(cpi.Groups.Count, Is.EqualTo(0));
-            Assert.That(cpi.TimingPoints.Count, Is.EqualTo(0));
+            Assert.That(cpi.DifficultyPoints.Count, Is.EqualTo(0));
             Assert.That(cpi.AllControlPoints.Count(), Is.EqualTo(0));
 
             cpi.Add(1000, new DifficultyControlPoint { SpeedMultiplier = 2 }); // is not redundant
@@ -64,7 +64,7 @@ namespace osu.Game.Tests.NonVisual
             cpi.Add(1000, new SampleControlPoint()); // is redundant
 
             Assert.That(cpi.Groups.Count, Is.EqualTo(1));
-            Assert.That(cpi.TimingPoints.Count, Is.EqualTo(0));
+            Assert.That(cpi.SamplePoints.Count, Is.EqualTo(1));
             Assert.That(cpi.AllControlPoints.Count(), Is.EqualTo(1));
 
             cpi.Add(1000, new SampleControlPoint { SampleVolume = 50 }); // is not redundant
@@ -83,7 +83,7 @@ namespace osu.Game.Tests.NonVisual
             cpi.Add(1000, new EffectControlPoint()); // is redundant
 
             Assert.That(cpi.Groups.Count, Is.EqualTo(0));
-            Assert.That(cpi.TimingPoints.Count, Is.EqualTo(0));
+            Assert.That(cpi.EffectPoints.Count, Is.EqualTo(0));
             Assert.That(cpi.AllControlPoints.Count(), Is.EqualTo(0));
 
             cpi.Add(1000, new EffectControlPoint { KiaiMode = true }); // is not redundant
