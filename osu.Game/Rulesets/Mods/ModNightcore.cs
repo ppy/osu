1411730c1a2888825c0fb25ff07bc47c344ba7df
unknown <87902114+bubb13gummy@users.noreply.github.com>
2019-12-15 16:56:02 +0800

    implement backing beats for nightcore mods

--- osu.Game/Rulesets/Mods/ModNightcore.cs
+++ osu.Game/Rulesets/Mods/ModNightcore.cs
@@ -1,15 +1,28 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
 using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
+using osu.Game.Audio;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.UI;
+using osu.Game.Skinning;
 
 namespace osu.Game.Rulesets.Mods
 {
-    public abstract class ModNightcore : ModDoubleTime
+    public abstract class ModNightcore<TObject> : ModDoubleTime, IApplicableToDrawableRuleset<TObject>
+        where TObject : HitObject
     {
         public override string Name => "Nightcore";
         public override string Acronym => "NC";
@@ -33,6 +46,48 @@ namespace osu.Game.Rulesets.Mods
             // base.ApplyToTrack() intentionally not called (different tempo adjustment is applied)
             track.AddAdjustment(AdjustableProperty.Frequency, freqAdjust);
             track.AddAdjustment(AdjustableProperty.Tempo, tempoAdjust);
+        }
+
+        public void ApplyToDrawableRuleset(DrawableRuleset<TObject> drawableRuleset)
+        {
+            drawableRuleset.Overlays.Add(new NightcoreBeatContainer());
+        }
+
+        public class NightcoreBeatContainer : BeatSyncedContainer
+        {
+            private SkinnableSound hatSample;
+            private SkinnableSound clapSample;
+            private SkinnableSound kickSample;
+            private SkinnableSound finishSample;
+
+            [BackgroundDependencyLoader]
+            private void load(AudioManager audio)
+            {
+                InternalChildren = new Drawable[]
+                {
+                    hatSample = new SkinnableSound(new SampleInfo("nightcore-hat")),
+                    clapSample = new SkinnableSound(new SampleInfo("nightcore-clap")),
+                    kickSample = new SkinnableSound(new SampleInfo("nightcore-kick")),
+                    finishSample = new SkinnableSound(new SampleInfo("nightcore-finish")),
+                };
+            }
+
+            protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, TrackAmplitudes amplitudes)
+            {
+                base.OnNewBeat(beatIndex, timingPoint, effectPoint, amplitudes);
+
+                if (beatIndex > -1)
+                {
+                    if (beatIndex % 16 == 0)
+                        finishSample?.Play();
+                    else if (beatIndex % 2 == 0)
+                        kickSample?.Play();
+                    else if (beatIndex % 2 == 1)
+                        clapSample?.Play();
+                    else
+                        hatSample?.Play();
+                }
+            }
         }
     }
 }
