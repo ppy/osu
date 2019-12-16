cfd811112061663c00edf15096273da32a1f5102
Dean Herbert <pe@ppy.sh>
2019-12-16 19:40:43 +0900

    Better initial beat handling

--- osu.Game/Rulesets/Mods/ModNightcore.cs
+++ osu.Game/Rulesets/Mods/ModNightcore.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Track;
@@ -93,7 +92,7 @@ namespace osu.Game.Rulesets.Mods
                 }
 
                 if (!firstBeat.HasValue || beatIndex < firstBeat)
-                    firstBeat = Math.Max(0, (beatIndex / segmentLength + 1) * segmentLength);
+                    firstBeat = beatIndex < 0 ? 0 : (beatIndex / segmentLength + 1) * segmentLength;
 
                 if (beatIndex >= firstBeat)
                 {
