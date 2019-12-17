cae797d6fb4727ea9dd0cd9a3c7fcb8ddc20250d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-12-17 12:06:09 +0900

    Move first beat offset to BeatSyncedContainer

--- osu.Game/Graphics/Containers/BeatSyncedContainer.cs
+++ osu.Game/Graphics/Containers/BeatSyncedContainer.cs
@@ -89,7 +89,7 @@ namespace osu.Game.Graphics.Containers
 
             double beatLength = timingPoint.BeatLength / Divisor;
 
-            int beatIndex = (int)((currentTrackTime - timingPoint.Time) / beatLength);
+            int beatIndex = (int)((currentTrackTime - timingPoint.Time) / beatLength) - (effectPoint.OmitFirstBarLine ? 1 : 0);
 
             // The beats before the start of the first control point are off by 1, this should do the trick
             if (currentTrackTime < timingPoint.Time)
