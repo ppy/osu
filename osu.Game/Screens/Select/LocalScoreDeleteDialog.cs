27163a51338a37cb2eee249d7102aca7d5c49547
Willy Tu <tu.willy.97@gmail.com>
2019-12-18 21:04:10 -0800

    Removed unnecessary code for deleting individual local score.

--- osu.Game/Screens/Select/LocalScoreDeleteDialog.cs
+++ osu.Game/Screens/Select/LocalScoreDeleteDialog.cs
@@ -5,7 +5,7 @@ using osu.Framework.Allocation;
 using osu.Game.Overlays.Dialog;
 using osu.Game.Scoring;
 using System;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Logging;
 
@@ -17,40 +17,25 @@ namespace osu.Game.Screens.Select
 
         public LocalScoreDeleteDialog(ScoreInfo score)
         {
-            try
-            {
-                string accuracy = string.Format(score?.Accuracy % 1 == 0 ? @"{0:P0}" : @"{0:P2}", score?.Accuracy);
+            Debug.Assert(score != null);
 
-                BodyText = $@"{score} {Environment.NewLine} Rank: {score.Rank} - Max Combo: {score.MaxCombo} - {accuracy}";
-                Icon = FontAwesome.Solid.Eraser;
-                HeaderText = @"Clearing this local score. Are you sure?";
-                Buttons = new PopupDialogButton[]
-                {
-                    new PopupDialogOkButton
-                    {
-                        Text = @"Yes. Please.",
-                        Action = () => scoreManager.Delete(score)
-                    },
-                    new PopupDialogCancelButton
-                    {
-                        Text = @"No, I'm still attached.",
-                    },
-                };
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e, "ScoreInfo cannot be null!");
+            string accuracy = string.Format(score.Accuracy % 1 == 0 ? @"{0:P0}" : @"{0:P2}", score.Accuracy);
 
-                HeaderText = $@"ScoreInfo cannot be null!";
-                Icon = FontAwesome.Solid.Ambulance;
-                Buttons = new PopupDialogButton[]
+            BodyText = $@"{score} {Environment.NewLine} Rank: {score.Rank} - Max Combo: {score.MaxCombo} - {accuracy}";
+            Icon = FontAwesome.Solid.Eraser;
+            HeaderText = @"Deleting this local score. Are you sure?";
+            Buttons = new PopupDialogButton[]
+            {
+                new PopupDialogOkButton
                 {
-                    new PopupDialogCancelButton
-                    {
-                        Text = @"OK, thanks.",
-                    },
-                };
-            }
+                    Text = @"Yes. Please.",
+                    Action = () => scoreManager.Delete(score)
+                },
+                new PopupDialogCancelButton
+                {
+                    Text = @"No, I'm still attached.",
+                },
+            };
         }
 
         [BackgroundDependencyLoader]
