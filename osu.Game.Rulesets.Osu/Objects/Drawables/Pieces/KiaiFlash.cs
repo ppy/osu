45ccc6f68400ea24a47dd491b841d3651db4b16e
mcendu <87902114+bubb13gummy@users.noreply.github.com>
2019-12-21 22:44:52 +0800

    improve code quality

--- osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/KiaiFlash.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/Pieces/KiaiFlash.cs
@@ -10,29 +10,19 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables.Pieces
 {
     public class KiaiFlash : BeatSyncedContainer
     {
-        public Drawable FlashComponent { get; set; }
-
         public float Intensity { get; set; }
 
         public KiaiFlash()
         {
+            EarlyActivationMilliseconds = 80;
             Blending = BlendingParameters.Additive;
-
-            Child = FlashComponent = new Box
-            {
-                RelativeSizeAxes = Axes.Both,
-                Colour = OsuColour.Gray(1f),
-                Alpha = 0f,
-            };
         }
-
-        protected new double EarlyActivationMilliseconds = 80;
 
         protected override void OnNewBeat(int beatIndex, Game.Beatmaps.ControlPoints.TimingControlPoint timingPoint, Game.Beatmaps.ControlPoints.EffectControlPoint effectPoint, Framework.Audio.Track.TrackAmplitudes amplitudes)
         {
             if (effectPoint.KiaiMode)
             {
-                FlashComponent
+                Child
                     .FadeTo(Intensity, EarlyActivationMilliseconds, Easing.OutQuint)
                     .Then()
                     .FadeOut(timingPoint.BeatLength - 80, Easing.OutSine);
