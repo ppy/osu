5b115d8d8a94fff0f265d8a5aba113c0c1b1ad53
Andrei Zavatski <megaman9919@gmail.com>
2019-12-21 13:41:50 +0300

    Implement basic logic

---
+++ osu.Game/Screens/Play/HUD/HitErrorMeters/ColourHitErrorMeter.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Scoring;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Game.Screens.Play.HUD.HitErrorMeters
+{
+    public class ColourHitErrorMeter : HitErrorMeter
+    {
+        public ColourHitErrorMeter(HitWindows hitWindows)
+            : base(hitWindows)
+        {
+
+        }
+
+        public override void OnNewJudgement(JudgementResult judgement)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
