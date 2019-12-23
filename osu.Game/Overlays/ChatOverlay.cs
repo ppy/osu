b038c192d12df09206f072214c2e20959dd4fe09
Joehu <madamba.joehu@outlook.com>
2019-12-22 19:47:47 -0800

    Rename property to IsSwitchable

--- osu.Game/Overlays/ChatOverlay.cs
+++ osu.Game/Overlays/ChatOverlay.cs
@@ -171,7 +171,7 @@ namespace osu.Game.Overlays
                                     d.Origin = Anchor.BottomLeft;
                                     d.RelativeSizeAxes = Axes.Both;
                                     d.OnRequestLeave = channelManager.LeaveChannel;
-                                    d.Tabbable = true;
+                                    d.IsSwitchable = true;
                                 }),
                             }
                         },
