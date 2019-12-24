d3413d5ee425f226b9c602f7d4bcd1f961bbd43e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2019-12-24 16:16:55 +0900

    Update RulesetStore to read from populated defaults

--- osu.Game/Rulesets/RulesetStore.cs
+++ osu.Game/Rulesets/RulesetStore.cs
@@ -58,16 +58,11 @@ namespace osu.Game.Rulesets
 
                 var instances = loadedAssemblies.Values.Select(r => (Ruleset)Activator.CreateInstance(r)).ToList();
 
-                //add all legacy modes in correct order
+                //add all legacy rulesets first to ensure they have exclusive choice of primary key.
                 foreach (var r in instances.Where(r => r is ILegacyRuleset))
                 {
-                    int legacyId = ((ILegacyRuleset)r).LegacyID;
-
-                    if (context.RulesetInfo.SingleOrDefault(rsi => rsi.ID == legacyId) == null)
-                    {
-                        r.RulesetInfo.ID = legacyId;
+                    if (context.RulesetInfo.SingleOrDefault(dbRuleset => dbRuleset.ID == r.RulesetInfo.ID) == null)
                         context.RulesetInfo.Add(r.RulesetInfo);
-                    }
                 }
 
                 context.SaveChanges();
