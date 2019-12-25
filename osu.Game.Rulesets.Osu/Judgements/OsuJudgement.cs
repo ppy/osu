07d988083b4bb46b395a37a97056ce1acc45726f
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2019-12-25 19:58:23 +0900

    Add perfect and ok values, move to base Judgement

--- osu.Game.Rulesets.Osu/Judgements/OsuJudgement.cs
+++ osu.Game.Rulesets.Osu/Judgements/OsuJudgement.cs
@@ -8,11 +8,6 @@ namespace osu.Game.Rulesets.Osu.Judgements
 {
     public class OsuJudgement : Judgement
     {
-        /// <summary>
-        /// The health increase for a maximum judgement result.
-        /// </summary>
-        protected const double MAX_HEALTH_INCREASE = 0.05;
-
         public override HitResult MaxResult => HitResult.Great;
 
         protected override int NumericResultFor(HitResult result)
@@ -30,27 +25,6 @@ namespace osu.Game.Rulesets.Osu.Judgements
 
                 case HitResult.Great:
                     return 300;
-            }
-        }
-
-        protected override double HealthIncreaseFor(HitResult result)
-        {
-            switch (result)
-            {
-                case HitResult.Miss:
-                    return -MAX_HEALTH_INCREASE;
-
-                case HitResult.Meh:
-                    return -MAX_HEALTH_INCREASE * 0.05;
-
-                case HitResult.Good:
-                    return MAX_HEALTH_INCREASE * 0.3;
-
-                case HitResult.Great:
-                    return MAX_HEALTH_INCREASE;
-
-                default:
-                    return 0;
             }
         }
     }
