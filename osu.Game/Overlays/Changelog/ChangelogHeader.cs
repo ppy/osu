647c83e6c858c3a11e74717f8c5af754b248482a
Andrei Zavatski <megaman9919@gmail.com>
2019-12-26 21:21:15 +0300

    Add ability to not create content for headers
    Rather than creating an empty container

--- osu.Game/Overlays/Changelog/ChangelogHeader.cs
+++ osu.Game/Overlays/Changelog/ChangelogHeader.cs
@@ -75,7 +75,7 @@ namespace osu.Game.Overlays.Changelog
 
         protected override Drawable CreateBackground() => new HeaderBackground();
 
-        protected override Drawable CreateContent() => new Container
+        protected override Drawable CreateContent => new Container
         {
             RelativeSizeAxes = Axes.X,
             AutoSizeAxes = Axes.Y,
