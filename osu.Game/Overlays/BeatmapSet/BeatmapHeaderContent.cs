96bf30a94fa6441719cf32c5e961015d264f0222
Andrei Zavatski <megaman9919@gmail.com>
2019-12-28 22:47:36 +0300

    Remove unused usings

--- osu.Game/Overlays/BeatmapSet/BeatmapHeaderContent.cs
+++ osu.Game/Overlays/BeatmapSet/BeatmapHeaderContent.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
