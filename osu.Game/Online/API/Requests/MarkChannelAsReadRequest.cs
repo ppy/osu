7b71e568177f1d827316dd3930ac64fd2921f5c0
Craftplacer <craftplacer@outlook.de>
2020-01-02 17:07:28 +0100

    Initial commit

---
+++ osu.Game/Online/API/Requests/MarkChannelAsReadRequest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.Chat;
+
+namespace osu.Game.Online.API.Requests
+{
+    public class MarkChannelAsReadRequest : APIRequest
+    {
+        private readonly Channel channel;
+        private readonly Message message;
+
+        public MarkChannelAsReadRequest(Channel channel, Message message)
+        {
+            this.channel = channel;
+            this.message = message;
+        }
+
+        protected override string Target => @"/chat/channels/{channel}/mark-as-read/{message}";
+    }
+}
