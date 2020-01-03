fcacfc9d0834baa7a2b75346440293bc189ab2df
Craftplacer <87902114+bubb13gummy@users.noreply.github.com>
2020-01-04 00:45:51 +0100

    Add HTTP method

--- osu.Game/Online/API/Requests/MarkChannelAsReadRequest.cs
+++ osu.Game/Online/API/Requests/MarkChannelAsReadRequest.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Net.Http;
+using osu.Framework.IO.Network;
 using osu.Game.Online.Chat;
 
 namespace osu.Game.Online.API.Requests
@@ -17,5 +19,12 @@ namespace osu.Game.Online.API.Requests
         }
 
         protected override string Target => $"chat/channels/{channel.Id}/mark-as-read/{message.Id}";
+
+        protected override WebRequest CreateWebRequest()
+        {
+            var req = base.CreateWebRequest();
+            req.Method = HttpMethod.Put;
+            return req;
+        }
     }
 }
