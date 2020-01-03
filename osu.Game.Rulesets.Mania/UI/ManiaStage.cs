974fc362ea0c0d01e2592f5205d4622873f02bd4
mcendu <nathandu@outlook.com>
2020-01-03 22:22:06 +0800

    Fix doc

--- osu.Game.Rulesets.Mania/UI/ManiaStage.cs
+++ osu.Game.Rulesets.Mania/UI/ManiaStage.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -21,7 +19,7 @@ using osuTK.Graphics;
 namespace osu.Game.Rulesets.Mania.UI
 {
     /// <summary>
-    /// A collection of <see cref="DefaultColumn"/>s.
+    /// A collection of <see cref="Column"/>s.
     /// </summary>
     public class ManiaStage : ScrollingPlayfield
     {
