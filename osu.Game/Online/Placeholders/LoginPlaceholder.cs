7f5eb11670636e41b9a10836e189845140e312c4
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2020-01-04 21:09:40 +0100

    Fix CI issues

--- osu.Game/Online/Placeholders/LoginPlaceholder.cs
+++ osu.Game/Online/Placeholders/LoginPlaceholder.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Allocation;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
@@ -39,6 +42,5 @@ namespace osu.Game.Online.Placeholders
             login?.Show();
             return base.OnClick(e);
         }
-
     }
 }
