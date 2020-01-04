3bd3ebad49466e55ade2c3a682a71d27c9e77dda
Lucas A <game4allyt@gmail.com>
2020-01-04 18:26:31 +0100

    Move placeholders to a dedicated namespace

--- osu.Game/Online/Leaderboards/RetrievalFailurePlaceholder.cs
+++ osu.Game/Online/Leaderboards/RetrievalFailurePlaceholder.cs
@@ -6,6 +6,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
 using osu.Game.Graphics.Containers;
+using osu.Game.Online.Placeholders;
 using osuTK;
 
 namespace osu.Game.Online.Leaderboards
