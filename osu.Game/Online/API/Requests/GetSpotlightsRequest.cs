08fb68ddfeddb891b2ddc9972544a0bf6f2bff68
Andrei Zavatski <megaman9919@gmail.com>
2020-01-10 16:28:52 +0300

    Fix incorrect return type for spotlight request

--- osu.Game/Online/API/Requests/GetSpotlightsRequest.cs
+++ osu.Game/Online/API/Requests/GetSpotlightsRequest.cs
@@ -2,12 +2,19 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using osu.Game.Online.API.Requests.Responses;
 
 namespace osu.Game.Online.API.Requests
 {
-    public class GetSpotlightsRequest : APIRequest<List<APISpotlight>>
+    public class GetSpotlightsRequest : APIRequest<SpotlightsArray>
     {
         protected override string Target => "spotlights";
+    }
+
+    public class SpotlightsArray
+    {
+        [JsonProperty("spotlights")]
+        public List<APISpotlight> Spotlights;
     }
 }
