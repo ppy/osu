11d4b1f984e804b1b4abe75ca66827b98d572dc6
TheWildTree <thewildtree@outlook.com>
2020-01-13 16:35:11 +0100

    Remove SliderHead movement

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSliderHead.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSliderHead.cs
@@ -32,17 +32,6 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
             pathVersion.BindValueChanged(_ => updatePosition(), true);
         }
 
-        protected override void Update()
-        {
-            base.Update();
-
-            double completionProgress = Math.Clamp((Time.Current - slider.StartTime) / slider.Duration, 0, 1);
-
-            //todo: we probably want to reconsider this before adding scoring, but it looks and feels nice.
-            if (!IsHit)
-                Position = slider.CurvePositionAt(completionProgress);
-        }
-
         public Action<double> OnShake;
 
         protected override void Shake(double maximumLength) => OnShake?.Invoke(maximumLength);
