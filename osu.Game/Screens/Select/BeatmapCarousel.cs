951dfe6c888cc805d2eee24dcca3d1372cbd6b9d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-01-15 15:11:36 +0900

    Expand on comment

--- osu.Game/Screens/Select/BeatmapCarousel.cs
+++ osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -413,7 +413,8 @@ namespace osu.Game.Screens.Select
         protected override bool OnKeyDown(KeyDownEvent e)
         {
             // allow for controlling volume when alt is held.
-            // mostly for compatibility with osu-stable.
+            // this is required as the VolumeControlReceptor uses OnPressed, which is
+            // executed after all OnKeyDown events.
             if (e.AltPressed)
                 return base.OnKeyDown(e);
 
