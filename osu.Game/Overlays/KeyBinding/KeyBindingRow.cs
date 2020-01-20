b62e3addf378ad05bbb3733d22ad347c594f3e82
smoogipoo <smoogipoo@smgi.me>
2020-01-20 20:30:25 +0900

    Apply input method signature refactorings

--- osu.Game/Overlays/KeyBinding/KeyBindingRow.cs
+++ osu.Game/Overlays/KeyBinding/KeyBindingRow.cs
@@ -235,13 +235,15 @@ namespace osu.Game.Overlays.KeyBinding
             return true;
         }
 
-        protected override bool OnJoystickRelease(JoystickReleaseEvent e)
+        protected override void OnJoystickRelease(JoystickReleaseEvent e)
         {
             if (!HasFocus)
-                return base.OnJoystickRelease(e);
+            {
+                base.OnJoystickRelease(e);
+                return;
+            }
 
             finalise();
-            return true;
         }
 
         private void clear()
