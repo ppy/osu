16c500d0b0dd29191466608bcd8f7fa970e3419c
Craftplacer <craftplacer@outlook.de>
2020-01-25 16:41:45 +0100

    Add mention tests

---
+++ osu.Game.Tests/Chat/MessageNotifierTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using osu.Game.Online.Chat;
+
+namespace osu.Game.Tests.Chat
+{
+    [TestFixture]
+    public class MessageNotifierTests
+    {
+        private readonly MessageNotifier messageNotifier = new MessageNotifier();
+
+        [Test]
+        public void TestMentions()
+        {
+            // Message (with mention, different casing)
+            Assert.IsTrue(messageNotifier.IsMentioning("Hey, Somebody Playing OSU!", "Somebody playing osu!"));
+
+            // Message (with mention, underscores)
+            Assert.IsTrue(messageNotifier.IsMentioning("Hey, Somebody_playing_osu!", "Somebody playing osu!"));
+
+            // Message (with mention, different casing, underscores)
+            Assert.IsTrue(messageNotifier.IsMentioning("Hey, Somebody_Playing_OSU!", "Somebody playing osu!"));
+
+            // Message (without mention)
+            Assert.IsTrue(!messageNotifier.IsMentioning("peppy, can you please fix this?", "Cookiezi"));
+        }
+    }
+}
