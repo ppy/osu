e8cdf59e021c6b04e88ddb0268ca2d9eec3d4679
Craftplacer <87902114+bubb13gummy@users.noreply.github.com>
2020-01-25 16:50:12 +0100

    Add license header

--- osu.Game.Tests/Chat/MessageNotifierTests.cs
+++ osu.Game.Tests/Chat/MessageNotifierTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
 using osu.Game.Online.Chat;
 
 namespace osu.Game.Tests.Chat
