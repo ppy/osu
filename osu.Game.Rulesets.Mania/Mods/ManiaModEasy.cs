41e011e33e9645b6d05c00dfd91af14e9e223867
TheWildTree <thewildtree@outlook.com>
2020-01-26 17:21:58 +0100

    Prevent mods from changing key amount in mania

--- osu.Game.Rulesets.Mania/Mods/ManiaModEasy.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModEasy.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Mania.Mods
@@ -8,5 +9,12 @@ namespace osu.Game.Rulesets.Mania.Mods
     public class ManiaModEasy : ModEasy
     {
         public override string Description => @"More forgiving HP drain, less accuracy required, and three lives!";
+
+        public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            var baseKeyAmount = difficulty.CircleSize;
+            base.ApplyToDifficulty(difficulty);
+            difficulty.CircleSize = baseKeyAmount;
+        }
     }
 }
--- osu.Game.Rulesets.Mania/Mods/ManiaModHardRock.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModHardRock.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Mania.Mods
@@ -8,5 +9,12 @@ namespace osu.Game.Rulesets.Mania.Mods
     public class ManiaModHardRock : ModHardRock
     {
         public override double ScoreMultiplier => 1;
+
+        public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            var baseKeyAmount = difficulty.CircleSize;
+            base.ApplyToDifficulty(difficulty);
+            difficulty.CircleSize = baseKeyAmount;
+        }
     }
 }
