292a82ac273f2f2df092aa7b4368e5e7ff9ef5c4
Dean Herbert <pe@ppy.sh>
2020-01-27 14:52:21 +0900

    Fix incorrect propagation of stream/build on initial display with build

--- osu.Game/Overlays/ChangelogOverlay.cs
+++ osu.Game/Overlays/ChangelogOverlay.cs
@@ -1,4 +1,4 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -181,7 +181,7 @@ namespace osu.Game.Overlays
                     builds = res.Builds;
                     Streams = res.Streams;
 
-                    Header.Streams.Populate(res.Streams);
+                    Header.Populate(res.Streams);
 
                     tcs.SetResult(true);
                 });
