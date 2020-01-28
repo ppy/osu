7a60f436b5bc6514937f7ed385faab4bcd297203
mcendu <nathandu@outlook.com>
2020-01-28 15:52:45 +0800

    add tests for slider snaking

---
+++ osu.Game.Rulesets.Osu.Tests/TestSceneSliderSnaking.cs
@@ -0,0 +1,25 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects.Drawables.Pieces;
+
+namespace osu.Game.Rulesets.Osu.Tests
+{
+    public class TestSceneSliderSnaking : TestSceneSlider
+    {
+        protected override DrawableSlider CreateDrawableSlider(Slider slider)
+        {
+            var snakingSlider = base.CreateDrawableSlider(slider);
+            snakingSlider.OnLoadComplete += d =>
+            {
+                var body = (d as DrawableSlider).Body.Drawable as PlaySliderBody;
+                body.SnakingIn.Value = true;
+                body.SnakingOut.Value = true;
+            };
+
+            return snakingSlider;
+        }
+    }
+}
