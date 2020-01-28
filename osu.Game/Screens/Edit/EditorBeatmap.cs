58654f28b67e4ce29d50125237e075007f0ec7ff
Dean Herbert <pe@ppy.sh>
2020-01-28 12:48:24 +0900

    Fix beat snap implementation being incorrect

--- osu.Game/Screens/Edit/EditorBeatmap.cs
+++ osu.Game/Screens/Edit/EditorBeatmap.cs
@@ -128,12 +128,14 @@ namespace osu.Game.Screens.Edit
             return list.Count - 1;
         }
 
-        public double SnapTime(double referenceTime, double duration)
+        public double SnapTime(double time, double? referenceTime)
         {
-            double beatLength = GetBeatLengthAtTime(referenceTime);
+            var timingPoint = ControlPointInfo.TimingPointAt(referenceTime ?? time);
+
+            var beatLength = timingPoint.BeatLength / BeatDivisor;
 
             // A 1ms offset prevents rounding errors due to minute variations in duration
-            return (int)((duration + 1) / beatLength) * beatLength;
+            return timingPoint.Time + (int)Math.Round(((time - timingPoint.Time) + 1) / beatLength) * beatLength;
         }
 
         public double GetBeatLengthAtTime(double referenceTime) => ControlPointInfo.TimingPointAt(referenceTime).BeatLength / BeatDivisor;
