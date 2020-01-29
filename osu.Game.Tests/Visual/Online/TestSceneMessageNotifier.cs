4f109c02d31e90dc525f92ffa4291a1a15529592
Craftplacer <craftplacer@outlook.de>
2020-01-29 02:18:46 +0100

    Add license header

--- osu.Game.Tests/Visual/Online/TestSceneMessageNotifier.cs
+++ osu.Game.Tests/Visual/Online/TestSceneMessageNotifier.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
