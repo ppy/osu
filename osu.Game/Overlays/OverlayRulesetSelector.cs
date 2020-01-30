ea2f66da1d9d10a9608a87cfed312b46a95afc4d
Andrei Zavatski <megaman9919@gmail.com>
2020-01-30 10:34:22 +0300

    Simplify OverlayRulesetTabItem.AccentColour

--- osu.Game/Overlays/OverlayRulesetSelector.cs
+++ osu.Game/Overlays/OverlayRulesetSelector.cs
@@ -1,42 +1,19 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.UserInterface;
-using osu.Game.Graphics;
 using osu.Game.Rulesets;
 using osuTK;
-using osuTK.Graphics;
-using System.Linq;
 
 namespace osu.Game.Overlays
 {
     public class OverlayRulesetSelector : RulesetSelector
     {
-        private Color4 accentColour;
-
-        public Color4 AccentColour
-        {
-            get => accentColour;
-            set
-            {
-                accentColour = value;
-                foreach (var i in TabContainer.Children.OfType<IHasAccentColour>())
-                    i.AccentColour = value;
-            }
-        }
-
         public OverlayRulesetSelector()
         {
             AutoSizeAxes = Axes.Both;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load(OverlayColourProvider colourProvider)
-        {
-            AccentColour = colourProvider.Highlight1;
         }
 
         protected override TabItem<RulesetInfo> CreateTabItem(RulesetInfo value) => new OverlayRulesetTabItem(value);
