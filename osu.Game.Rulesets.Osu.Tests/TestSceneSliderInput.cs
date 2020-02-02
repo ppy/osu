ad5161b06a2e269f1387e7c79cfd97e2f312ff6c
TheWildTree <87902114+bubb13gummy@users.noreply.github.com>
2020-02-02 21:25:35 +0100

    Modify tests to avoid fails in CLI testing

--- osu.Game.Rulesets.Osu.Tests/TestSceneSliderInput.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneSliderInput.cs
@@ -300,8 +300,8 @@ namespace osu.Game.Rulesets.Osu.Tests
             performTest(new List<ReplayFrame>
             {
                 new OsuReplayFrame { Position = new Vector2(0, 0), Actions = { OsuAction.LeftButton }, Time = time_slider_start },
-                new OsuReplayFrame { Position = new Vector2(0, OsuHitObject.OBJECT_RADIUS * 1.199f), Actions = { OsuAction.LeftButton }, Time = time_slider_start + 100 },
-                new OsuReplayFrame { Position = new Vector2(25, OsuHitObject.OBJECT_RADIUS * 1.199f), Actions = { OsuAction.LeftButton }, Time = time_slider_end },
+                new OsuReplayFrame { Position = new Vector2(0, OsuHitObject.OBJECT_RADIUS * 1.19f), Actions = { OsuAction.LeftButton }, Time = time_slider_start + 100 },
+                new OsuReplayFrame { Position = new Vector2(25, OsuHitObject.OBJECT_RADIUS * 1.19f), Actions = { OsuAction.LeftButton }, Time = time_slider_end },
             });
             AddAssert("Tracking kept", assertGreatJudge);
         }
@@ -320,8 +320,8 @@ namespace osu.Game.Rulesets.Osu.Tests
             performTest(new List<ReplayFrame>
             {
                 new OsuReplayFrame { Position = new Vector2(0, 0), Actions = { OsuAction.LeftButton }, Time = time_slider_start },
-                new OsuReplayFrame { Position = new Vector2(0, OsuHitObject.OBJECT_RADIUS * 1.2f), Actions = { OsuAction.LeftButton }, Time = time_slider_start + 100 },
-                new OsuReplayFrame { Position = new Vector2(25, OsuHitObject.OBJECT_RADIUS * 1.2f), Actions = { OsuAction.LeftButton }, Time = time_slider_end },
+                new OsuReplayFrame { Position = new Vector2(0, OsuHitObject.OBJECT_RADIUS * 1.21f), Actions = { OsuAction.LeftButton }, Time = time_slider_start + 100 },
+                new OsuReplayFrame { Position = new Vector2(25, OsuHitObject.OBJECT_RADIUS * 1.21f), Actions = { OsuAction.LeftButton }, Time = time_slider_end },
             });
             AddAssert("Tracking dropped", assertMidSliderJudgementFail);
         }
