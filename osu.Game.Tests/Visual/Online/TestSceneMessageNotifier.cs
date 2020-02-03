835d4f25ffbaefa3a79c04b778ef2cff5648eb82
Craftplacer <craftplacer@outlook.de>
2020-02-03 23:02:58 +0100

    Add notification testing to tests and show notification overlay while testing

--- osu.Game.Tests/Visual/Online/TestSceneMessageNotifier.cs
+++ osu.Game.Tests/Visual/Online/TestSceneMessageNotifier.cs
@@ -1,18 +1,22 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
 using osu.Game.Online.Chat;
 using osu.Game.Overlays;
+using osu.Game.Overlays.Notifications;
 using osu.Game.Users;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.Online
 {
-    public class TestSceneMessageNotifier : OsuTestScene
+    public class TestSceneMessageNotifier : ManualInputManagerTestScene
     {
         private User friend;
         private Channel publicChannel;
@@ -49,13 +53,35 @@ namespace osu.Game.Tests.Visual.Online
 
             AddStep("Send message containing mention", () => publicChannel.AddNewMessages(new Message(messageIdCounter++) { Content = $"Hello {API.LocalUser.Value.Username.ToLowerInvariant()}!", Sender = friend, ChannelId = publicChannel.Id }));
             AddAssert("Expect 1 notification", () => testContainer.NotificationOverlay.UnreadCount.Value == 1);
+
+            AddStep("Open notification overlay", () => testContainer.NotificationOverlay.Show());
+            AddStep("Click notification", clickNotification<MessageNotifier.MentionNotification>);
+
+            AddAssert("Expect ChatOverlay is open", () => testContainer.ChatOverlay.State.Value == Visibility.Visible);
+            AddAssert("Expect the public channel to be selected", () => testContainer.ChannelManager.CurrentChannel.Value == publicChannel);
         }
 
         [Test]
         public void TestPrivateMessageNotification()
         {
+            AddStep("Switch to public channel", () => testContainer.ChannelManager.CurrentChannel.Value = publicChannel);
+
             AddStep("Send PM", () => privateMessageChannel.AddNewMessages(new Message(messageIdCounter++) { Content = $"Hello {API.LocalUser.Value.Username}!", Sender = friend, ChannelId = privateMessageChannel.Id }));
             AddAssert("Expect 1 notification", () => testContainer.NotificationOverlay.UnreadCount.Value == 1);
+
+            AddStep("Open notification overlay", () => testContainer.NotificationOverlay.Show());
+            AddStep("Click notification", clickNotification<MessageNotifier.PrivateMessageNotification>);
+
+            AddAssert("Expect ChatOverlay is open", () => testContainer.ChatOverlay.State.Value == Visibility.Visible);
+            AddAssert("Expect the PM channel to be selected", () => testContainer.ChannelManager.CurrentChannel.Value == privateMessageChannel);
+        }
+
+        private void clickNotification<T>() where T : Notification
+        {
+            var notification = testContainer.NotificationOverlay.ChildrenOfType<T>().Single();
+
+            InputManager.MoveMouseTo(notification);
+            InputManager.Click(MouseButton.Left);
         }
 
         private class TestContainer : Container
