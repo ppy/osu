4dfdd98e8bda9e5c1f2c86351b1c0c2f23696ed8
recapitalverb <41869184+recapitalverb@users.noreply.github.com>
2020-02-03 22:12:34 +0700

    Use UserStatistics.DisplayAccuracy everywhere

--- osu.Game/Overlays/Profile/Header/TopHeaderContainer.cs
+++ osu.Game/Overlays/Profile/Header/TopHeaderContainer.cs
@@ -178,7 +178,7 @@ namespace osu.Game.Overlays.Profile.Header
             if (user?.Statistics != null)
             {
                 userStats.Add(new UserStatsLine("Ranked Score", user.Statistics.RankedScore.ToString("#,##0")));
-                userStats.Add(new UserStatsLine("Hit Accuracy", Math.Round(user.Statistics.Accuracy, 2).ToString("#0.00'%'")));
+                userStats.Add(new UserStatsLine("Hit Accuracy", user.Statistics.DisplayAccuracy));
                 userStats.Add(new UserStatsLine("Play Count", user.Statistics.PlayCount.ToString("#,##0")));
                 userStats.Add(new UserStatsLine("Total Score", user.Statistics.TotalScore.ToString("#,##0")));
                 userStats.Add(new UserStatsLine("Total Hits", user.Statistics.TotalHits.ToString("#,##0")));
