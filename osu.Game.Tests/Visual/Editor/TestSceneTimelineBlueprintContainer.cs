22697a4f8107e2a42d026dccbf49d0f321d19bf1
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-02-04 12:23:02 +0900

    Add blueprint type to timeline test

--- osu.Game.Tests/Visual/Editor/TestSceneTimelineBlueprintContainer.cs
+++ osu.Game.Tests/Visual/Editor/TestSceneTimelineBlueprintContainer.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Game.Screens.Edit.Compose.Components.Timeline;
@@ -10,6 +12,17 @@ namespace osu.Game.Tests.Visual.Editor
     [TestFixture]
     public class TestSceneTimelineBlueprintContainer : TimelineTestScene
     {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(TimelineHitObjectBlueprint),
+        };
+
         public override Drawable CreateTestComponent() => new TimelineBlueprintContainer();
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            Clock.Seek(10000);
+        }
     }
 }
