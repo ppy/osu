85155f29b69a19111357a81f1331a1b6c0d31e86
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2020-02-05 15:04:10 +0100

    Wrap Content into a container for animating visibility.

--- osu.Game/Online/OnlineViewContainer.cs
+++ osu.Game/Online/OnlineViewContainer.cs
@@ -11,36 +11,35 @@ using osu.Game.Online.Placeholders;
 namespace osu.Game.Online
 {
     /// <summary>
-    /// A <see cref="Container"/> for dislaying online content who require a local user to be logged in.
+    /// A <see cref="Container"/> for dislaying online content which require a local user to be logged in.
     /// Shows its children only when the local user is logged in and supports displaying a placeholder if not.
     /// </summary>
     public abstract class OnlineViewContainer : Container, IOnlineComponent
     {
-        private readonly Container placeholderContainer;
         private readonly Placeholder placeholder;
         protected readonly LoadingAnimation LoadingAnimation;
 
-        private const int transform_time = 300;
+        protected const double TRANSFORM_TIME = 300.0;
 
+        internal readonly Container TransformationTarget;
         protected override Container<Drawable> Content { get; }
 
         [Resolved]
         protected IAPIProvider API { get; private set; }
 
-        public OnlineViewContainer(string placeholderMessage)
+        protected OnlineViewContainer(string placeholderMessage)
         {
             InternalChildren = new Drawable[]
             {
-                Content = new Container
+                TransformationTarget = new Container
                 {
                     RelativeSizeAxes = Axes.Both,
+                    Child = Content = new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    }
                 },
-                placeholderContainer = new Container
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Alpha = 0,
-                    Child = placeholder = new LoginPlaceholder(placeholderMessage)
-                },
+                placeholder = new LoginPlaceholder(placeholderMessage),
                 LoadingAnimation = new LoadingAnimation
                 {
                     Alpha = 0,
@@ -53,26 +52,30 @@ namespace osu.Game.Online
             switch (state)
             {
                 case APIState.Offline:
-                    Content.FadeOut(150, Easing.OutQuint);
-                    placeholder.ScaleTo(0.8f).Then().ScaleTo(1, 3 * transform_time, Easing.OutQuint);
-                    placeholderContainer.FadeInFromZero(2 * transform_time, Easing.OutQuint);
+                    FadeContentOut(TransformationTarget);
+                    placeholder.ScaleTo(0.8f).Then().ScaleTo(1, 3 * TRANSFORM_TIME, Easing.OutQuint);
+                    placeholder.FadeInFromZero(2 * TRANSFORM_TIME, Easing.OutQuint);
                     LoadingAnimation.Hide();
                     break;
 
                 case APIState.Online:
-                    placeholderContainer.FadeOut(150, Easing.OutQuint);
-                    Content.FadeIn(transform_time, Easing.OutQuint);
+                    FadeContentIn(TransformationTarget);
+                    placeholder.FadeOut(TRANSFORM_TIME / 2, Easing.OutQuint);
                     LoadingAnimation.Hide();
                     break;
 
                 case APIState.Failing:
                 case APIState.Connecting:
+                    FadeContentOut(TransformationTarget);
                     LoadingAnimation.Show();
-                    placeholderContainer.FadeOut(150, Easing.OutQuint);
-                    Content.FadeOut(150, Easing.OutQuint);
+                    placeholder.FadeOut(TRANSFORM_TIME / 2, Easing.OutQuint);
                     break;
             }
         }
+
+        protected abstract void FadeContentOut(Drawable content);
+
+        protected abstract void FadeContentIn(Drawable content);
 
         protected override void LoadComplete()
         {
