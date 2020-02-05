4a0f86b86b7910217fd505e7e0b2b987b5491f52
TheWildTree <87902114+bubb13gummy@users.noreply.github.com>
2020-02-05 17:41:57 +0100

    Add alwaysShowDecimals param to FormatAccuracy
    This allows us to specify whether we want it to show decimal places if accuracy is 100%.

--- osu.Game/Utils/FormatUtils.cs
+++ osu.Game/Utils/FormatUtils.cs
@@ -7,18 +7,18 @@ namespace osu.Game.Utils
     {
         /// <summary>
         /// Turns the provided accuracy into a percentage with 2 decimal places.
-        /// Omits all decimal places when <paramref name="accuracy"/> equals 1d.
         /// </summary>
         /// <param name="accuracy">The accuracy to be formatted</param>
+        /// <param name="alwaysShowDecimals">Whether to show decimal places if <paramref name="accuracy"/> equals 1d</param>
         /// <returns>formatted accuracy in percentage</returns>
-        public static string FormatAccuracy(this double accuracy) => accuracy == 1 ? "100%" : $"{accuracy:0.00%}";
+        public static string FormatAccuracy(this double accuracy, bool alwaysShowDecimals = false) => accuracy == 1 && !alwaysShowDecimals ? "100%" : $"{accuracy:0.00%}";
 
         /// <summary>
         /// Turns the provided accuracy into a percentage with 2 decimal places.
-        /// Omits all decimal places when <paramref name="accuracy"/> equals 100m.
         /// </summary>
         /// <param name="accuracy">The accuracy to be formatted</param>
+        /// <param name="alwaysShowDecimals">Whether to show decimal places if <paramref name="accuracy"/> equals 100m</param>
         /// <returns>formatted accuracy in percentage</returns>
-        public static string FormatAccuracy(this decimal accuracy) => accuracy == 100 ? "100%" : $"{accuracy:0.00}%";
+        public static string FormatAccuracy(this decimal accuracy, bool alwaysShowDecimals = false) => accuracy == 100 && !alwaysShowDecimals ? "100%" : $"{accuracy:0.00}%";
     }
 }
