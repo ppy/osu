10a1948720b15e8de61149ff7abfcad89d5eca8d
mcendu <nathandu@outlook.com>
2020-02-09 16:19:21 +0800

    remove using directive

--- osu.Game.Rulesets.Osu/Mods/OsuModSpunOut.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModSpunOut.cs
@@ -9,7 +9,6 @@ using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects.Drawables.Pieces;
-using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
