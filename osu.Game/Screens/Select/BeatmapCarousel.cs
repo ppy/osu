66fb72cd8a476f33f07f408d0c0220d23233d950
Dean Herbert <pe@ppy.sh>
2020-02-10 16:31:52 +0900

    Fix song select not showing active beatmap if it is filtered by local criteria

--- osu.Game/Screens/Select/BeatmapCarousel.cs
+++ osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -17,6 +17,7 @@ using osu.Framework.Caching;
 using osu.Framework.Threading;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Input.Events;
+using osu.Framework.Logging;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Cursor;
@@ -229,6 +230,17 @@ namespace osu.Game.Screens.Select
                 if (item != null)
                 {
                     select(item);
+
+                    // if we got here and the set is filtered, it means we were bypassing filters.
+                    // in this case, reapplying the filter is necessary to ensure the panel is in the correct place
+                    // (since it is forcefully being included in the carousel).
+                    if (set.Filtered.Value)
+                    {
+                        Debug.Assert(bypassFilters);
+
+                        applyActiveCriteria(false, true);
+                    }
+
                     return true;
                 }
             }
