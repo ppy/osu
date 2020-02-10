403c03841d1d3497dfda7ffc3af8f2fb72d2daba
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-02-10 21:39:45 +0100

    Decouple test scene & add assertions

--- osu.Game.Rulesets.Osu.Tests/TestSceneSpinnerSpunOut.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneSpinnerSpunOut.cs
@@ -5,19 +5,66 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
+using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects.Drawables.Pieces;
+using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Osu.Tests
 {
     [TestFixture]
-    public class TestSceneSpinnerSpunOut : TestSceneSpinner
+    public class TestSceneSpinnerSpunOut : OsuTestScene
     {
-        public override IReadOnlyList<Type> RequiredTypes => base.RequiredTypes.Concat(new[] { typeof(OsuModSpunOut) }).ToList();
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(SpinnerDisc),
+            typeof(DrawableSpinner),
+            typeof(DrawableOsuHitObject),
+            typeof(OsuModSpunOut)
+        };
 
         [SetUp]
         public void SetUp() => Schedule(() =>
         {
             SelectedMods.Value = new[] { new OsuModSpunOut() };
         });
+
+        [Test]
+        public void TestSpunOut()
+        {
+            DrawableSpinner spinner = null;
+
+            AddStep("create spinner", () => spinner = createSpinner());
+
+            AddUntilStep("wait for end", () => Time.Current > spinner.LifetimeEnd);
+
+            AddAssert("spinner is completed", () => spinner.Progress >= 1);
+        }
+
+        private DrawableSpinner createSpinner()
+        {
+            var spinner = new Spinner
+            {
+                StartTime = Time.Current + 500,
+                EndTime = Time.Current + 2500
+            };
+            spinner.ApplyDefaults(new ControlPointInfo(), new BeatmapDifficulty());
+
+            var drawableSpinner = new DrawableSpinner(spinner)
+            {
+                Anchor = Anchor.Centre
+            };
+
+            foreach (var mod in SelectedMods.Value.OfType<IApplicableToDrawableHitObjects>())
+                mod.ApplyToDrawableHitObjects(new[] { drawableSpinner });
+
+            Add(drawableSpinner);
+            return drawableSpinner;
+        }
     }
 }
