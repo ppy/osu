f6238da7c5f637c65b5e26b40a6fb2e55187f602
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-02-11 18:46:49 +0300

    CommentEditor basic implementation

---
+++ osu.Game.Tests/Visual/UserInterface/TestSceneCommentEditor.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Overlays;
+using osu.Game.Overlays.Comments;
+
+namespace osu.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneCommentEditor : OsuTestScene
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(CommentEditor),
+        };
+
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Blue);
+
+        public TestSceneCommentEditor()
+        {
+            Add(new Container
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                AutoSizeAxes = Axes.Y,
+                Width = 800,
+                Child = new TestCommentEditor()
+            });
+        }
+
+        private class TestCommentEditor : CommentEditor
+        {
+            protected override string EmptyTextboxText() => @"This textbox is empty";
+
+            protected override string FooterText() => @"Footer text. And it is pretty long. Cool.";
+        }
+    }
+}
---
+++ osu.Game/Overlays/Comments/CommentEditor.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+
+namespace osu.Game.Overlays.Comments
+{
+    public abstract class CommentEditor : CompositeDrawable
+    {
+        private const int footer_height = 40;
+
+        [BackgroundDependencyLoader]
+        private void load(OverlayColourProvider colourProvider)
+        {
+            RelativeSizeAxes = Axes.X;
+            Height = footer_height * 2;
+            Masking = true;
+            CornerRadius = 6;
+
+            AddRangeInternal(new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.Background3
+                },
+                new Container
+                {
+                    Name = "Footer",
+                    Anchor = Anchor.BottomCentre,
+                    Origin = Anchor.BottomCentre,
+                    RelativeSizeAxes = Axes.X,
+                    Height = footer_height,
+                    Padding = new MarginPadding { Horizontal = 8 },
+                    Children = new Drawable[]
+                    {
+                        new OsuSpriteText
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Font = OsuFont.GetFont(size: 12),
+                            Shadow = false,
+                            Text = FooterText()
+                        }
+                    }
+                }
+            });
+        }
+
+        protected abstract string FooterText();
+
+        protected abstract string EmptyTextboxText();
+    }
+}
