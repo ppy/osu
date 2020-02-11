68563e9d708b9498d29a6c1b086b802ceb5cad57
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-02-12 00:57:06 +0300

    Implement CommitButton

--- osu.Game/Overlays/Comments/CommentEditor.cs
+++ osu.Game/Overlays/Comments/CommentEditor.cs
@@ -10,12 +10,18 @@ using osu.Game.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Graphics.Sprites;
 using osuTK.Graphics;
+using osu.Game.Graphics.UserInterface;
+using System.Collections.Generic;
+using System;
+using osuTK;
 
 namespace osu.Game.Overlays.Comments
 {
     public abstract class CommentEditor : CompositeDrawable
     {
         private const int side_padding = 8;
+
+        public Action<string> OnCommit;
 
         protected abstract string FooterText { get; }
 
@@ -28,6 +34,9 @@ namespace osu.Game.Overlays.Comments
         [BackgroundDependencyLoader]
         private void load(OverlayColourProvider colourProvider)
         {
+            EditorTextbox textbox;
+            CommitButton commitButton;
+
             RelativeSizeAxes = Axes.X;
             AutoSizeAxes = Axes.Y;
             Masking = true;
@@ -49,7 +58,7 @@ namespace osu.Game.Overlays.Comments
                     Direction = FillDirection.Vertical,
                     Children = new Drawable[]
                     {
-                        new EditorTextbox
+                        textbox = new EditorTextbox
                         {
                             Height = 40,
                             RelativeSizeAxes = Axes.X,
@@ -77,12 +86,21 @@ namespace osu.Game.Overlays.Comments
                                     Origin = Anchor.CentreRight,
                                     AutoSizeAxes = Axes.Both,
                                     Direction = FillDirection.Horizontal,
+                                    Spacing = new Vector2(5, 0),
+                                    Child = commitButton = new CommitButton(CommitButtonText)
+                                    {
+                                        Anchor = Anchor.CentreRight,
+                                        Origin = Anchor.CentreRight,
+                                        Action = () => OnCommit?.Invoke(textbox.Text)
+                                    }
                                 }
                             }
                         }
                     }
                 }
             });
+
+            textbox.OnCommit += (u, v) => commitButton.Click();
         }
 
         private class EditorTextbox : BasicTextBox
@@ -107,13 +125,63 @@ namespace osu.Game.Overlays.Comments
                 BackgroundCommit = Color4.LightSkyBlue;
             }
 
-
             protected override SpriteText CreatePlaceholder() => placeholder = new OsuSpriteText
             {
                 Font = OsuFont.GetFont(weight: FontWeight.Regular),
             };
 
             protected override Drawable GetDrawableCharacter(char c) => new OsuSpriteText { Text = c.ToString(), Font = OsuFont.GetFont(size: CalculatedTextSize) };
+        }
+
+        private class CommitButton : LoadingButton
+        {
+            private const int duration = 200;
+
+            protected override IEnumerable<Drawable> EffectTargets => new[] { background };
+
+            private OsuSpriteText drawableText;
+            private Box background;
+
+            public CommitButton(string text)
+            {
+                AutoSizeAxes = Axes.Both;
+                LoadingAnimationSize = new Vector2(10);
+
+                drawableText.Text = text;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OverlayColourProvider colourProvider)
+            {
+                IdleColour = colourProvider.GetColour(0.5f, 0.45f);
+                HoverColour = colourProvider.GetColour(0.5f, 0.6f);
+            }
+
+            protected override Drawable CreateContent() => new CircularContainer
+            {
+                Masking = true,
+                Height = 25,
+                AutoSizeAxes = Axes.X,
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both
+                    },
+                    drawableText = new OsuSpriteText
+                    {
+                        AlwaysPresent = true,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Font = OsuFont.GetFont(size: 12, weight: FontWeight.Bold),
+                        Margin = new MarginPadding { Horizontal = 20 }
+                    }
+                }
+            };
+
+            protected override void OnLoadStarted() => drawableText.FadeOut(duration, Easing.OutQuint);
+
+            protected override void OnLoadFinished() => drawableText.FadeIn(duration, Easing.OutQuint);
         }
     }
 }
