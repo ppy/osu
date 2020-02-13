e181a95c855d1ba6562219a7df9776955f1a7a4d
Dean Herbert <pe@ppy.sh>
2020-02-13 22:28:37 +0900

    Remove unused using

--- osu.Game/Online/Multiplayer/Room.cs
+++ osu.Game/Online/Multiplayer/Room.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using osu.Framework.Allocation;
