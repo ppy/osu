e20aacc052c1f180e7cf0fdee1e024ba432b3c47
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-02-14 20:42:14 +0900

    Implement overlined components

---
+++ osu.Game/Screens/Multi/Match/Components/OverlinedParticipants.cs
@@ -0,0 +1,29 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Screens.Multi.Components;
+
+namespace osu.Game.Screens.Multi.Match.Components
+{
+    public class OverlinedParticipants : OverlinedDisplay
+    {
+        public OverlinedParticipants()
+            : base("Participants")
+        {
+            Content.Add(new ParticipantsList { RelativeSizeAxes = Axes.Both });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            ParticipantCount.BindValueChanged(_ => setParticipantCount());
+            MaxParticipants.BindValueChanged(_ => setParticipantCount());
+
+            setParticipantCount();
+        }
+
+        private void setParticipantCount() => Details = MaxParticipants.Value != null ? $"{ParticipantCount.Value}/{MaxParticipants.Value}" : ParticipantCount.Value.ToString();
+    }
+}
