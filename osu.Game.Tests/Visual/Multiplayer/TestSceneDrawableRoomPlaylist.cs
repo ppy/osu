eb14dbcd77ffc141ff717f4307df40028027d451
smoogipoo <smoogipoo@smgi.me>
2020-02-14 15:01:45 +0900

    Initial implementation of rearrangeable playlist

---
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoomPlaylist.cs
@@ -0,0 +1,86 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Utils;
+using osu.Game.Beatmaps;
+using osu.Game.Online.Multiplayer;
+using osu.Game.Rulesets;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Screens.Multi;
+using osuTK;
+
+namespace osu.Game.Tests.Visual.Multiplayer
+{
+    public class TestSceneDrawableRoomPlaylist : OsuTestScene
+    {
+        [Resolved]
+        private BeatmapManager beatmapManager { get; set; }
+
+        [Resolved]
+        private RulesetStore rulesetStore { get; set; }
+
+        private DrawableRoomPlaylist playlist;
+
+        [Test]
+        public void TestItemsCanNotBeRemovedOrSelectedFromNonEditableAndNonSelectablePlaylist()
+        {
+            createPlaylist(false, false);
+        }
+
+        [Test]
+        public void TestItemsCanBeRemovedFromEditablePlaylist()
+        {
+            createPlaylist(true, false);
+        }
+
+        [Test]
+        public void TestItemsCanBeSelectedInSelectablePlaylist()
+        {
+            createPlaylist(false, true);
+        }
+
+        [Test]
+        public void TestItemsCanBeSelectedAndRemovedFromEditableAndSelectablePlaylist()
+        {
+            createPlaylist(true, true);
+        }
+
+        private void createPlaylist(bool allowEdit, bool allowSelection) => AddStep("create playlist", () =>
+        {
+            Child = playlist = new DrawableRoomPlaylist(allowEdit, allowSelection)
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(500, 300)
+            };
+
+            var beatmapSets = beatmapManager.GetAllUsableBeatmapSets();
+            var rulesets = rulesetStore.AvailableRulesets.ToList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var set = beatmapSets[RNG.Next(0, beatmapSets.Count)];
+                var beatmap = set.Beatmaps[RNG.Next(0, set.Beatmaps.Count)];
+
+                beatmap.BeatmapSet = set;
+                beatmap.Metadata = set.Metadata;
+
+                playlist.Items.Add(new PlaylistItem
+                {
+                    Beatmap = { Value = beatmap },
+                    Ruleset = { Value = rulesets[RNG.Next(0, rulesets.Count)] },
+                    RequiredMods =
+                    {
+                        new OsuModHardRock(),
+                        new OsuModDoubleTime(),
+                        new OsuModAutoplay()
+                    }
+                });
+            }
+        });
+    }
+}
