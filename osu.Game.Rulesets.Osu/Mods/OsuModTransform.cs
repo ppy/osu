aebbd9d96c4cf9c490477014f235d8da68423c7a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-02-16 15:35:12 +0900

    Don't transform repeat points

--- osu.Game.Rulesets.Osu/Mods/OsuModTransform.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModTransform.cs
@@ -38,6 +38,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                 case DrawableSliderHead _:
                 case DrawableSliderTail _:
                 case DrawableSliderTick _:
+                case DrawableRepeatPoint _:
                     return;
 
                 default:
