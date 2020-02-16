2ba0bd872bd41428a9da62a020be1d231f73a321
Andrei Zavatski <megaman9919@gmail.com>
2020-02-17 02:35:46 +0300

    Implement basic BeatmapListingSortTabControl

---
+++ osu.Game.Tests/Visual/UserInterface/TestSceneBeatmapListingSort.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Overlays;
+using osu.Game.Overlays.BeatmapListing;
+
+namespace osu.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneBeatmapListingSort : OsuTestScene
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(BeatmapListingSortTabControl),
+            typeof(OverlaySortTabControl<>),
+        };
+
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Blue);
+
+        public TestSceneBeatmapListingSort()
+        {
+            Add(new BeatmapListingSortTabControl
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+        }
+    }
+}
