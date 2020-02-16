d83208995e58bd7362556e55154f0e61e8ca8a16
TheWildTree <87902114+bubb13gummy@users.noreply.github.com>
2020-02-16 21:52:08 +0100

    Remove redundant using directives

--- osu.Game/Beatmaps/Drawables/UpdateableBeatmapSetCover.cs
+++ osu.Game/Beatmaps/Drawables/UpdateableBeatmapSetCover.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
