ad8fc097ab1abc495f8e645c52450cda5b8c4cc9
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-02-17 02:35:46 +0300

    Implement basic BeatmapListingSortTabControl

---
+++ osu.Game/Overlays/BeatmapListing/BeatmapListingSortTabControl.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Overlays.BeatmapListing
+{
+    public class BeatmapListingSortTabControl : OverlaySortTabControl<BeatmapSortCriteria>
+    {
+        public BeatmapListingSortTabControl()
+        {
+            Current.Value = BeatmapSortCriteria.Ranked;
+        }
+    }
+
+    public enum BeatmapSortCriteria
+    {
+        Title,
+        Artist,
+        Difficulty,
+        Ranked,
+        Rating,
+        Plays,
+        Favourites,
+    }
+}
