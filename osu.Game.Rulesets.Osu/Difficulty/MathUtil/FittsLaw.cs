9fc1cd769266eabd800b6c947a210cde1bca7dd8
Joseph Ireland <joseph.ireland@outlook.com>
2020-02-18 22:30:22 +0000

    fix typo in pow2, add comments

--- osu.Game.Rulesets.Osu/Difficulty/MathUtil/FittsLaw.cs
+++ osu.Game.Rulesets.Osu/Difficulty/MathUtil/FittsLaw.cs
@@ -29,6 +29,33 @@ namespace osu.Game.Rulesets.Osu.Difficulty.MathUtil
             return SpecialFunctions.Erf(2.066 / d * (Power2(mt * tp) - 1) / Math.Sqrt(2));
         }
 
+        /// <summary>
+        /// Fast approximation of 2^x. Accurate to around 9-10 significant figures, around 6x faster than Math.Pow or Math.Exp
+        /// Calculates the integer part using a bit shift and fraction part using a polynomial approximation
+        /// </summary>
+        public static double Power2(double x)
+        {
+            if (x<0)
+            {
+                return 1 / Power2(-x);
+            }
+            if (x>60)
+            {
+                return double.PositiveInfinity;
+            }
+
+            int floor = (int)x;
+            double frac = x - floor;
+            double frac2 = frac * frac;
+            double frac3 = frac * frac2;
+            double frac4 = frac * frac3;
+            double frac5 = frac * frac4;
+            double frac6 = frac * frac5;
+
+            return (1L << floor) * (coeffs[0] + coeffs[1] * frac + coeffs[2] * frac2
+                + coeffs[3] * frac3 + coeffs[4] * frac4 + coeffs[5] * frac5 + coeffs[6] * frac6);
+        }
+
         readonly static double[] coeffs = {
             1.0000000060371126,
             0.693146840098149,
@@ -38,40 +65,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.MathUtil
             0.0012382531241478965,
             0.00021871427263121524,
         };
-        public static double Power2(double x)
-        {
-            int floor = (int)x;
-            double frac = x - floor;
-            double frac2 = frac * frac;
-            double frac3 = frac * frac2;
-            double frac4 = frac * frac3;
-            double frac5 = frac * frac4;
-            double frac6 = frac * frac4;
-
-            return (1L << floor) * (coeffs[0] + coeffs[1] * frac + coeffs[2] * frac2
-                + coeffs[3] * frac3 + coeffs[4] * frac4 + coeffs[5] * frac5 + coeffs[6] * frac6);
-        }
-
-        /*
-        readonly static double[] coeffs = {
-            1.0000072153251425,
-            0.6929318839596742,
-            0.24170895290576638,
-            0.051667593566359506,
-            0.01367667229501924,
-        };
-        public static double Power2(double x)
-        {
-            int floor = (int)x;
-            double frac = x - floor;
-            double frac2 = frac * frac;
-            double frac3 = frac * frac2;
-            double frac4 = frac * frac3;
-
-            return (1L << floor) * (coeffs[0] + coeffs[1] * frac + coeffs[2] * frac2
-                + coeffs[3] * frac3 + coeffs[4] * frac4);
-        }
-        */
     }
 
 
