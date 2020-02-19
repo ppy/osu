01202f09bec882209c314a34cf05543c3acaa0a0
Andrei Zavatski <megaman9919@gmail.com>
2020-02-19 17:32:43 +0300

    Expand SearchBeatmapSetsResponse

--- osu.Game/Online/API/Requests/SearchBeatmapSetsResponse.cs
+++ osu.Game/Online/API/Requests/SearchBeatmapSetsResponse.cs
@@ -2,12 +2,20 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using osu.Game.Online.API.Requests.Responses;
 
 namespace osu.Game.Online.API.Requests
 {
     public class SearchBeatmapSetsResponse : ResponseWithCursor
     {
+        [JsonProperty("beatmapsets")]
         public IEnumerable<APIBeatmapSet> BeatmapSets;
+
+        [JsonProperty("error")]
+        public string Error;
+
+        [JsonProperty("total")]
+        public int Total;
     }
 }
