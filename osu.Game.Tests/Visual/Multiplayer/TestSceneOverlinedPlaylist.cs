8d6f6c2bb1116d9d4a53c278042efb303719d163
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-02-20 14:45:35 +0900

    Remove unused using

--- osu.Game.Tests/Visual/Multiplayer/TestSceneOverlinedPlaylist.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneOverlinedPlaylist.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Screens.Multi.Components;
