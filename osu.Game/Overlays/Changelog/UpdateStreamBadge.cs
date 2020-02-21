e99f53d9cef49672056ee829059d8a1675789880
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-02-21 18:12:23 +0300

    Adjust StreamBadgeArea

--- osu.Game/Overlays/Changelog/UpdateStreamBadge.cs
+++ osu.Game/Overlays/Changelog/UpdateStreamBadge.cs
@@ -3,8 +3,6 @@
 
 using Humanizer;
 using osu.Framework.Allocation;
-using osu.Framework.Audio;
-using osu.Framework.Audio.Sample;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -16,99 +14,90 @@ using osu.Framework.Graphics.UserInterface;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Overlays.Changelog
 {
     public class UpdateStreamBadge : TabItem<APIUpdateStream>
     {
-        private const float badge_height = 66.5f;
         private const float badge_width = 100;
         private const float transition_duration = 100;
 
-        private readonly ExpandingBar expandingBar;
-        private SampleChannel sampleClick;
-        private SampleChannel sampleHover;
-
-        private readonly FillFlowContainer<SpriteText> text;
-
         public readonly Bindable<APIUpdateStream> SelectedTab = new Bindable<APIUpdateStream>();
 
-        private readonly Container fadeContainer;
+        private readonly APIUpdateStream stream;
+
+        private Container fadeContainer;
+        private FillFlowContainer<SpriteText> text;
+        private ExpandingBar expandingBar;
 
         public UpdateStreamBadge(APIUpdateStream stream)
             : base(stream)
         {
-            Size = new Vector2(stream.IsFeatured ? badge_width * 2 : badge_width, badge_height);
-            Padding = new MarginPadding(5);
-
-            Child = fadeContainer = new Container
-            {
-                RelativeSizeAxes = Axes.Both,
-                Children = new Drawable[]
-                {
-                    text = new FillFlowContainer<SpriteText>
-                    {
-                        AutoSizeAxes = Axes.X,
-                        RelativeSizeAxes = Axes.Y,
-                        Direction = FillDirection.Vertical,
-                        Children = new[]
-                        {
-                            new OsuSpriteText
-                            {
-                                Text = stream.DisplayName,
-                                Font = OsuFont.GetFont(weight: FontWeight.Bold, size: 12),
-                                Margin = new MarginPadding { Top = 6 },
-                            },
-                            new OsuSpriteText
-                            {
-                                Text = stream.LatestBuild.DisplayVersion,
-                                Font = OsuFont.GetFont(weight: FontWeight.Light, size: 16),
-                            },
-                            new OsuSpriteText
-                            {
-                                Text = stream.LatestBuild.Users > 0 ? $"{stream.LatestBuild.Users:N0} {"user".Pluralize(stream.LatestBuild.Users == 1)} online" : null,
-                                Font = OsuFont.GetFont(weight: FontWeight.Regular, size: 10),
-                                Colour = new Color4(203, 164, 218, 255),
-                            },
-                        }
-                    },
-                    expandingBar = new ExpandingBar
-                    {
-                        Anchor = Anchor.TopCentre,
-                        Colour = stream.Colour,
-                        ExpandedSize = 4,
-                        CollapsedSize = 2,
-                        IsCollapsed = true
-                    },
-                }
-            };
-
-            SelectedTab.BindValueChanged(_ => updateState(), true);
+            this.stream = stream;
         }
 
         [BackgroundDependencyLoader]
-        private void load(AudioManager audio)
+        private void load(OverlayColourProvider colourProvider)
         {
-            sampleClick = audio.Samples.Get(@"UI/generic-select-soft");
-            sampleHover = audio.Samples.Get(@"UI/generic-hover-soft");
+            Size = new Vector2(stream.IsFeatured ? badge_width * 2 : badge_width, 60);
+            Padding = new MarginPadding(5);
+
+            AddRange(new Drawable[]
+            {
+                fadeContainer = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        text = new FillFlowContainer<SpriteText>
+                        {
+                            AutoSizeAxes = Axes.X,
+                            RelativeSizeAxes = Axes.Y,
+                            Direction = FillDirection.Vertical,
+                            Margin = new MarginPadding { Top = 6 },
+                            Children = new[]
+                            {
+                                new OsuSpriteText
+                                {
+                                    Text = stream.DisplayName,
+                                    Font = OsuFont.GetFont(size: 12, weight: FontWeight.Black),
+                                },
+                                new OsuSpriteText
+                                {
+                                    Text = stream.LatestBuild.DisplayVersion,
+                                    Font = OsuFont.GetFont(size: 16, weight: FontWeight.Regular),
+                                },
+                                new OsuSpriteText
+                                {
+                                    Text = stream.LatestBuild.Users > 0 ? $"{stream.LatestBuild.Users:N0} {"user".Pluralize(stream.LatestBuild.Users == 1)} online" : null,
+                                    Font = OsuFont.GetFont(size: 10),
+                                    Colour = colourProvider.Foreground1
+                                },
+                            }
+                        },
+                        expandingBar = new ExpandingBar
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Colour = stream.Colour,
+                            ExpandedSize = 4,
+                            CollapsedSize = 2,
+                            IsCollapsed = true
+                        },
+                    }
+                },
+                new HoverClickSounds()
+            });
+
+            SelectedTab.BindValueChanged(_ => updateState(), true);
         }
 
         protected override void OnActivated() => updateState();
 
         protected override void OnDeactivated() => updateState();
 
-        protected override bool OnClick(ClickEvent e)
-        {
-            sampleClick?.Play();
-            return base.OnClick(e);
-        }
-
         protected override bool OnHover(HoverEvent e)
         {
-            sampleHover?.Play();
             updateState();
-
             return base.OnHover(e);
         }
 
