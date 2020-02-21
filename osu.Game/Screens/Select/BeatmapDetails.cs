36f65cf343f7e0f6b03daf65869ea38fc1d1db17
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-02-21 15:31:40 +0900

    Combine all loading animation implementations

--- osu.Game/Screens/Select/BeatmapDetails.cs
+++ osu.Game/Screens/Select/BeatmapDetails.cs
@@ -7,7 +7,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
-using osu.Game.Graphics.UserInterface;
 using System.Linq;
 using osu.Game.Online.API;
 using osu.Framework.Threading;
@@ -17,6 +16,7 @@ using osu.Game.Screens.Select.Details;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.API.Requests;
 using osu.Game.Rulesets;
 
@@ -35,7 +35,7 @@ namespace osu.Game.Screens.Select
         private readonly MetadataSection description, source, tags;
         private readonly Container failRetryContainer;
         private readonly FailRetryGraph failRetryGraph;
-        private readonly DimmedLoadingLayer loading;
+        private readonly LoadingLayer loading;
 
         [Resolved]
         private IAPIProvider api { get; set; }
@@ -63,6 +63,8 @@ namespace osu.Game.Screens.Select
 
         public BeatmapDetails()
         {
+            Container content;
+
             Children = new Drawable[]
             {
                 new Box
@@ -70,7 +72,7 @@ namespace osu.Game.Screens.Select
                     RelativeSizeAxes = Axes.Both,
                     Colour = Color4.Black.Opacity(0.5f),
                 },
-                new Container
+                content = new Container
                 {
                     RelativeSizeAxes = Axes.Both,
                     Padding = new MarginPadding { Horizontal = spacing },
@@ -157,7 +159,7 @@ namespace osu.Game.Screens.Select
                         },
                     },
                 },
-                loading = new DimmedLoadingLayer(),
+                loading = new LoadingLayer(content),
             };
         }
 
