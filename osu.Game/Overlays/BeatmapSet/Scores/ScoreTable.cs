3b697fdefae85761c0866f51a6a9aa7d649eef58
TheWildTree <87902114+bubb13gummy@users.noreply.github.com>
2020-02-29 15:43:48 +0100

    Remove using directive

--- osu.Game/Overlays/BeatmapSet/Scores/ScoreTable.cs
+++ osu.Game/Overlays/BeatmapSet/Scores/ScoreTable.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions;
-using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
