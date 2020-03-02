239cfddcbb92503b662d89750953950b6a11a12d
smoogipoo <smoogipoo@smgi.me>
2020-03-02 10:50:41 +0900

    Improve test scenes/cases

--- osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModDifficultyAdjust.cs
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModDifficultyAdjust.cs
@@ -5,16 +5,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Scoring;
+using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Osu.Tests.Mods
 {
-    public class TestSceneOsuModDifficultyAdjust : TestSceneOsuModSandbox
+    public class TestSceneOsuModDifficultyAdjust : ModSandboxTestScene
     {
         public override IReadOnlyList<Type> RequiredTypes => base.RequiredTypes.Append(typeof(OsuModDifficultyAdjust)).ToList();
 
         public TestSceneOsuModDifficultyAdjust()
-            : base(new OsuModDifficultyAdjust())
+            : base(new OsuRuleset())
         {
+        }
+
+        protected override ModTestCaseData[] CreateTestCases() => new[]
+        {
+            new ModTestCaseData("no adjustment", new OsuModDifficultyAdjust())
+            {
+                Autoplay = true,
+                PassCondition = () => ((ScoreAccessibleTestPlayer)Player).ScoreProcessor.JudgedHits >= 2
+            },
+            new ModTestCaseData("cs = 10", new OsuModDifficultyAdjust { CircleSize = { Value = 10 } })
+            {
+                Autoplay = true,
+                PassCondition = () => ((ScoreAccessibleTestPlayer)Player).ScoreProcessor.JudgedHits >= 2
+            },
+            new ModTestCaseData("ar = 10", new OsuModDifficultyAdjust { ApproachRate = { Value = 10 } })
+            {
+                Autoplay = true,
+                PassCondition = () => ((ScoreAccessibleTestPlayer)Player).ScoreProcessor.JudgedHits >= 2
+            },
+        };
+
+        protected override TestPlayer CreateReplayPlayer(Score score) => new ScoreAccessibleTestPlayer(score);
+
+        private class ScoreAccessibleTestPlayer : TestPlayer
+        {
+            public new ScoreProcessor ScoreProcessor => base.ScoreProcessor;
+
+            public ScoreAccessibleTestPlayer(Score score)
+                : base(score)
+            {
+            }
         }
     }
 }
