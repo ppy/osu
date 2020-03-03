8429737538b2cc7066bb5e5aa297fb45f2d977be
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-03-04 00:28:47 +0300

    Move colour selection to the FriendsOnlineStatusItem

--- osu.Game/Overlays/Home/Friends/FriendsBundle.cs
+++ osu.Game/Overlays/Home/Friends/FriendsBundle.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using osuTK.Graphics;
-
 namespace osu.Game.Overlays.Home.Friends
 {
     public class FriendsBundle
@@ -12,30 +9,10 @@ namespace osu.Game.Overlays.Home.Friends
 
         public int Amount { get; }
 
-        public Color4 Colour => getColour();
-
         public FriendsBundle(FriendsOnlineStatus status, int amount)
         {
             Status = status;
             Amount = amount;
-        }
-
-        private Color4 getColour()
-        {
-            switch (Status)
-            {
-                default:
-                    throw new ArgumentException($@"{Status} status does not provide a colour in {nameof(getColour)}.");
-
-                case FriendsOnlineStatus.All:
-                    return Color4.White;
-
-                case FriendsOnlineStatus.Online:
-                    return Color4.Lime;
-
-                case FriendsOnlineStatus.Offline:
-                    return Color4.Black;
-            }
         }
     }
 
