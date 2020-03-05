1318f242c1cdd710b52ace6de9ce881ec24cb1fb
Andrei Zavatski <megaman9919@gmail.com>
2020-03-06 02:12:30 +0300

    Revert changes to basic implementation and remove redundant stuff

--- osu.Game.Tests/Visual/Online/TestSceneBeatmapListingOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneBeatmapListingOverlay.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using osu.Game.Overlays;
 using NUnit.Framework;
-using osu.Game.Online.API.Requests;
 
 namespace osu.Game.Tests.Visual.Online
 {
@@ -23,24 +22,6 @@ namespace osu.Game.Tests.Visual.Online
         public TestSceneBeatmapListingOverlay()
         {
             Add(overlay = new BeatmapListingOverlay());
-        }
-
-        [Test]
-        public void TestShowTag()
-        {
-            AddStep("Show Rem tag", () => overlay.ShowTag("Rem"));
-        }
-
-        [Test]
-        public void TestShowGenre()
-        {
-            AddStep("Show Anime genre", () => overlay.ShowGenre(BeatmapSearchGenre.Anime));
-        }
-
-        [Test]
-        public void TestShowLanguage()
-        {
-            AddStep("Show Japanese language", () => overlay.ShowLanguage(BeatmapSearchLanguage.Japanese));
         }
 
         [Test]
