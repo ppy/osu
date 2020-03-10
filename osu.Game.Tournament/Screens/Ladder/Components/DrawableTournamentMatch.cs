55839fcbd8452e7383c56fbf5bbdc23a4b63abc1
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-03-10 09:51:30 +0900

    Remove unused using

--- osu.Game.Tournament/Screens/Ladder/Components/DrawableTournamentMatch.cs
+++ osu.Game.Tournament/Screens/Ladder/Components/DrawableTournamentMatch.cs
@@ -9,7 +9,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
-using osu.Game.Graphics;
 using osu.Game.Tournament.Models;
 using osuTK;
 using osuTK.Graphics;
