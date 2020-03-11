d3115fd13822b3c95f616a0e8372fafa0f84eaf2
LanTenDragon <lantendragon@protonmail.com>
2020-03-11 23:51:14 +0800

    Add non asserting visual test

---
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModHidden.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Tests.Visual;
+using osuTK;
+
+namespace osu.Game.Rulesets.Osu.Tests.Mods
+{
+    public class TestSceneOsuModHidden : ModTestScene
+    {
+        public TestSceneOsuModHidden()
+            : base(new OsuRuleset())
+        {
+        }
+
+        [Test]
+        public void TestDefaultBeatmapTest() => CreateModTest(new ModTestData
+        {
+            Mod = new OsuModHidden(),
+            Autoplay = true,
+        });
+
+        [Test]
+        public void FirstCircleAfterTwoSpinners() => CreateModTest(new ModTestData
+        {
+            Mod = new OsuModHidden(),
+            Autoplay = true,
+            Beatmap = new Beatmap
+            {
+                HitObjects = new List<HitObject>
+                {
+                    new Spinner
+                    {
+                        Position = new Vector2(256, 192),
+                        EndTime = 1000,
+                    },
+                    new Spinner
+                    {
+                        Position = new Vector2(256, 192),
+                        StartTime = 1200,
+                        EndTime = 2200,
+                    },
+                    new HitCircle
+                    {
+                        Position = new Vector2(300, 192),
+                        StartTime = 3200,
+                    },
+                    new HitCircle
+                    {
+                        Position = new Vector2(384, 192),
+                        StartTime = 4200,
+                    }
+                }
+            }
+        });
+
+        [Test]
+        public void FirstSliderAfterTwoSpinners() => CreateModTest(new ModTestData
+        {
+            Mod = new OsuModHidden(),
+            Autoplay = true,
+            Beatmap = new Beatmap
+            {
+                HitObjects = new List<HitObject>
+                {
+                    new Spinner
+                    {
+                        Position = new Vector2(256, 192),
+                        EndTime = 1000,
+                    },
+                    new Spinner
+                    {
+                        Position = new Vector2(256, 192),
+                        StartTime = 1200,
+                        EndTime = 2200,
+                    },
+                    new Slider
+                    {
+                        StartTime = 3200,
+                        Path = new SliderPath(PathType.Linear, new[] { Vector2.Zero, new Vector2(100, 0), })
+                    },
+                    new Slider
+                    {
+                        StartTime = 5200,
+                        Path = new SliderPath(PathType.Linear, new[] { Vector2.Zero, new Vector2(100, 0), })
+                    }
+                }
+            }
+        });
+    }
+}
