fb1e924d2f0453dc2b55eaaf9094bb2f1bf62137
Andrei Zavatski <megaman9919@gmail.com>
2020-03-11 13:21:43 +0300

    Add basic online logic

--- osu.Game.Tests/Visual/Online/TestSceneHomeOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneHomeOverlay.cs
@@ -19,6 +19,8 @@ namespace osu.Game.Tests.Visual.Online
             typeof(FriendsLayout),
         };
 
+        protected override bool UseOnlineAPI => true;
+
         private readonly HomeOverlay overlay;
 
         public TestSceneHomeOverlay()
--- osu.Game/Overlays/Home/Friends/FriendsLayout.cs
+++ osu.Game/Overlays/Home/Friends/FriendsLayout.cs
@@ -1,15 +1,32 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Game.Online.API;
+using osu.Game.Online.API.Requests;
+using osu.Game.Overlays.Social;
+using osu.Game.Users;
+using osuTK;
 
 namespace osu.Game.Overlays.Home.Friends
 {
     public class FriendsLayout : CompositeDrawable
     {
+        [Resolved]
+        private IAPIProvider api { get; set; }
+
+        private readonly Bindable<User> localUser = new Bindable<User>();
+
+        private GetFriendsRequest request;
+        private CancellationTokenSource cancellationToken;
+
         private readonly Box background;
         private readonly Box controlBackground;
         private readonly FriendsOnlineStatusControl onlineStatusControl;
@@ -41,7 +58,7 @@ namespace osu.Game.Overlays.Home.Friends
                                 AutoSizeAxes = Axes.Y,
                                 Padding = new MarginPadding
                                 {
-                                    Vertical = 20,
+                                    Top = 20,
                                     Horizontal = 45
                                 },
                                 Child = onlineStatusControl = new FriendsOnlineStatusControl(),
@@ -85,7 +102,8 @@ namespace osu.Game.Overlays.Home.Friends
                                     itemsPlaceholder = new Container
                                     {
                                         RelativeSizeAxes = Axes.X,
-                                        AutoSizeAxes = Axes.Y
+                                        AutoSizeAxes = Axes.Y,
+                                        Padding = new MarginPadding { Horizontal = 50 }
                                     }
                                 }
                             }
@@ -100,6 +118,53 @@ namespace osu.Game.Overlays.Home.Friends
         {
             background.Colour = colourProvider.Background4;
             controlBackground.Colour = colourProvider.Background5;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            localUser.BindTo(api.LocalUser);
+            localUser.BindValueChanged(_ => refetch(), true);
+        }
+
+        private void refetch()
+        {
+            request?.Cancel();
+            request = new GetFriendsRequest();
+            request.Success += onSuccess;
+            api.Queue(request);
+        }
+
+        private void onSuccess(List<User> users)
+        {
+            onlineStatusControl.Clear();
+            onlineStatusControl.Populate(users);
+
+            recreatePanels(users);
+        }
+
+        private void recreatePanels(List<User> users)
+        {
+            cancellationToken?.Cancel();
+
+            var table = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                Spacing = new Vector2(10),
+                Children = users.Select(u => new SocialGridPanel(u).With(panel =>
+                {
+                    panel.Anchor = Anchor.TopCentre;
+                    panel.Origin = Anchor.TopCentre;
+                })).ToList()
+            };
+
+            LoadComponentAsync(table, loaded =>
+            {
+                itemsPlaceholder.Clear();
+                itemsPlaceholder.Add(loaded);
+            }, (cancellationToken = new CancellationTokenSource()).Token);
         }
     }
 }
