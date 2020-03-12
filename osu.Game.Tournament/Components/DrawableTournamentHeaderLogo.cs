b6b802e8212f44a669675f6705fd16db667fd758
Dean Herbert <pe@ppy.sh>
2020-03-12 14:06:40 +0900

    Add back customisable header logo/text
    Also adds test scene for MatchHeader component.

--- osu.Game.Tournament/Components/DrawableTournamentHeaderLogo.cs
+++ osu.Game.Tournament/Components/DrawableTournamentHeaderLogo.cs
@@ -2,17 +2,36 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 
 namespace osu.Game.Tournament.Components
 {
-    public class DrawableTournamentHeaderLogo : Sprite
+    public class DrawableTournamentHeaderLogo : CompositeDrawable
     {
-        [BackgroundDependencyLoader]
-        private void load(TextureStore textures)
+        public DrawableTournamentHeaderLogo()
         {
-            Texture = textures.Get("header-text");
+            InternalChild = new LogoSprite();
+
+            Height = 50;
+            RelativeSizeAxes = Axes.X;
+        }
+
+        private class LogoSprite : Sprite
+        {
+            [BackgroundDependencyLoader]
+            private void load(TextureStore textures)
+            {
+                RelativeSizeAxes = Axes.Both;
+                FillMode = FillMode.Fit;
+
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+
+                Texture = textures.Get("header-logo");
+            }
         }
     }
 }
--- osu.Game.Tournament/Components/DrawableTournamentHeaderText.cs
+++ osu.Game.Tournament/Components/DrawableTournamentHeaderText.cs
@@ -2,17 +2,36 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 
 namespace osu.Game.Tournament.Components
 {
-    public class DrawableTournamentHeaderText : Sprite
+    public class DrawableTournamentHeaderText : CompositeDrawable
     {
-        [BackgroundDependencyLoader]
-        private void load(TextureStore textures)
+        public DrawableTournamentHeaderText()
         {
-            Texture = textures.Get("header-text");
+            InternalChild = new TextSprite();
+
+            Height = 25;
+            RelativeSizeAxes = Axes.X;
+        }
+
+        private class TextSprite : Sprite
+        {
+            [BackgroundDependencyLoader]
+            private void load(TextureStore textures)
+            {
+                RelativeSizeAxes = Axes.Both;
+                FillMode = FillMode.Fit;
+
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+
+                Texture = textures.Get("header-text");
+            }
         }
     }
 }
