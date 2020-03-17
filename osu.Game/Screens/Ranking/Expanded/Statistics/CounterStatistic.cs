32230ef4b2e1ee5f8afb161520aba547423bf0f0
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-03-17 17:25:24 +0900

    Implement middle panel contents

---
+++ osu.Game/Screens/Ranking/Expanded/Statistics/CounterStatistic.cs
@@ -0,0 +1,48 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Screens.Ranking.Expanded.Accuracy;
+using osuTK;
+
+namespace osu.Game.Screens.Ranking.Expanded.Statistics
+{
+    public class CounterStatistic : StatisticDisplay
+    {
+        private readonly int count;
+
+        private RollingCounter<int> counter;
+
+        public CounterStatistic(string header, int count)
+            : base(header)
+        {
+            this.count = count;
+        }
+
+        public override void Appear()
+        {
+            base.Appear();
+            counter.Current.Value = count;
+        }
+
+        protected override Drawable CreateContent() => counter = new Counter();
+
+        private class Counter : RollingCounter<int>
+        {
+            protected override double RollingDuration => AccuracyCircle.ACCURACY_TRANSFORM_DURATION;
+
+            protected override Easing RollingEasing => AccuracyCircle.ACCURACY_TRANSFORM_EASING;
+
+            public Counter()
+            {
+                DisplayedCountSpriteText.Font = OsuFont.Torus.With(size: 20, fixedWidth: true);
+                DisplayedCountSpriteText.Spacing = new Vector2(-2, 0);
+            }
+
+            public override void Increment(int amount)
+                => Current.Value += amount;
+        }
+    }
+}
