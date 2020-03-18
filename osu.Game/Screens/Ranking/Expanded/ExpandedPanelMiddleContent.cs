66558ca8c527e44a5739389cf7dd0a716fe65b1b
smoogipoo <smoogipoo@smgi.me>
2020-03-18 17:21:57 +0900

    Colourise hit result statistics

--- osu.Game/Screens/Ranking/Expanded/ExpandedPanelMiddleContent.cs
+++ osu.Game/Screens/Ranking/Expanded/ExpandedPanelMiddleContent.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Localisation;
@@ -62,7 +61,7 @@ namespace osu.Game.Screens.Ranking.Expanded
 
             var bottomStatistics = new List<StatisticDisplay>();
             foreach (var stat in score.SortedStatistics)
-                bottomStatistics.Add(new CounterStatistic(stat.Key.GetDescription(), stat.Value));
+                bottomStatistics.Add(new HitResultStatistic(stat.Key, stat.Value));
 
             statisticDisplays.AddRange(topStatistics);
             statisticDisplays.AddRange(bottomStatistics);
