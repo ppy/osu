675079814fd38dd80c85a226b1e51408f49bf85a
voidedWarranties <87902114+bubb13gummy@users.noreply.github.com>
2020-03-21 12:15:20 -0700

    Test stopping behavior

---
+++ osu.Game.Tests/Visual/Editor/TestSceneEditorClock.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Screens.Edit.Components;
+using osuTK;
+
+namespace osu.Game.Tests.Visual.Editor
+{
+    [TestFixture]
+    public class TestSceneEditorClock : EditorClockTestScene
+    {
+        public TestSceneEditorClock()
+        {
+            Add(new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    new TimeInfoContainer
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(200, 100)
+                    },
+                    new PlaybackControl
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(200, 100)
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void TestStopAtTrackEnd()
+        {
+            AddStep("Reset clock", () => Clock.Seek(0));
+            AddStep("Start clock", Clock.Start);
+            AddAssert("Clock running", () => Clock.IsRunning);
+            AddStep("Seek near end", () => Clock.Seek(Clock.TrackLength - 250));
+            AddUntilStep("Clock stops", () => !Clock.IsRunning);
+            AddAssert("Clock stopped at end", () => Clock.CurrentTime == Clock.TrackLength);
+            AddStep("Start clock again", Clock.Start);
+            AddAssert("Clock looped", () => Clock.IsRunning && Clock.CurrentTime < Clock.TrackLength);
+        }
+    }
+}
