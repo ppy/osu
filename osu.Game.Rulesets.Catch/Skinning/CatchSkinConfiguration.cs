e6b2e3b0ed1f4059a9fef74053b7ed5d6ec39d9d
Salman Ahmed <email@iisalman.me>
2020-03-26 05:18:12 +0300

    Add osu!catch skin configurations

---
+++ osu.Game.Rulesets.Catch/Skinning/CatchSkinConfiguration.cs
@@ -0,0 +1,23 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Rulesets.Catch.Skinning
+{
+    public enum CatchSkinConfiguration
+    {
+        /// <summary>
+        /// The colour to be used for the catcher while on hyper-dashing state.
+        /// </summary>
+        HyperDash,
+
+        /// <summary>
+        /// The colour to be used for hyper-dash fruits.
+        /// </summary>
+        HyperDashFruit,
+
+        /// <summary>
+        /// The colour to be used for the "exploding" catcher sprite on beginning of hyper-dashing.
+        /// </summary>
+        HyperDashAfterImage,
+    }
+}
