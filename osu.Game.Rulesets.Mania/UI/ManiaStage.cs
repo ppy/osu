3fb044c3b659d4040454c9a345b18f1a9c0d9ee9
mcendu <nathandu@outlook.com>
2020-03-31 12:09:04 +0800

    rm unnecessary usings

--- osu.Game.Rulesets.Mania/UI/ManiaStage.cs
+++ osu.Game.Rulesets.Mania/UI/ManiaStage.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
