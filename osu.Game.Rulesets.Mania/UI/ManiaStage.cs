44fcd2613f99a9844c6a87205225710a29c9292a
mcendu <nathandu@outlook.com>
2020-03-31 22:58:04 +0800

    Add support for special column

--- osu.Game.Rulesets.Mania/UI/ManiaStage.cs
+++ osu.Game.Rulesets.Mania/UI/ManiaStage.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
