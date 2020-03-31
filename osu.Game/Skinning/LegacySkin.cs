3debb59ffb1cb0acad41cd3615f64a944541f088
mcendu <87902114+bubb13gummy@users.noreply.github.com>
2020-03-31 17:22:46 +0800

    rename variable used for mania lookup key storage

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -120,14 +120,14 @@ namespace osu.Game.Skinning
                 case SkinCustomColourLookup customColour:
                     return SkinUtils.As<TValue>(getCustomColour(customColour.Lookup.ToString()));
 
-                case LegacyManiaSkinConfigurationLookup legacy:
+                case LegacyManiaSkinConfigurationLookup maniaLookup:
                     if (!AllowManiaSkin)
                         return null;
 
-                    if (!maniaConfigurations.TryGetValue(legacy.Keys, out var existing))
-                        maniaConfigurations[legacy.Keys] = existing = new LegacyManiaSkinConfiguration(legacy.Keys);
+                    if (!maniaConfigurations.TryGetValue(maniaLookup.Keys, out var existing))
+                        maniaConfigurations[maniaLookup.Keys] = existing = new LegacyManiaSkinConfiguration(maniaLookup.Keys);
 
-                    switch (legacy.Lookup)
+                    switch (maniaLookup.Lookup)
                     {
                         case LegacyManiaSkinConfigurationLookups.HitPosition:
                             return SkinUtils.As<TValue>(new Bindable<float>(existing.HitPosition));
