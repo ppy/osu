94031b57eaceb94f9b0a575dd1ea3b319d522d52
smoogipoo <smoogipoo@smgi.me>
2020-04-01 19:19:32 +0900

    Split hit explosion positioning from column

--- osu.Game.Rulesets.Mania/UI/Column.cs
+++ osu.Game.Rulesets.Mania/UI/Column.cs
@@ -105,10 +105,10 @@ namespace osu.Game.Rulesets.Mania.UI
             if (!result.IsHit || !judgedObject.DisplayResult || !DisplayJudgements.Value)
                 return;
 
-            hitObjectArea.Explosions.Add(new HitExplosion(judgedObject.AccentColour.Value, judgedObject is DrawableHoldNoteTick)
+            hitObjectArea.Explosions.Add(new SkinnableDrawable(new ManiaSkinComponent(ManiaSkinComponents.HitExplosion), _ =>
+                new DefaultHitExplosion(judgedObject.AccentColour.Value, judgedObject is DrawableHoldNoteTick))
             {
-                Anchor = Direction.Value == ScrollingDirection.Up ? Anchor.TopCentre : Anchor.BottomCentre,
-                Origin = Anchor.Centre
+                RelativeSizeAxes = Axes.Both
             });
         }
 
