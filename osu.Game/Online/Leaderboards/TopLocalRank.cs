1e8badb14a9ebdb4733af916d847fc10e0505f7c
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2020-04-04 22:28:36 +0300

    Move all logic to TopLocalRank and remove CarouselBeatmapRank

--- osu.Game/Online/Leaderboards/TopLocalRank.cs
+++ osu.Game/Online/Leaderboards/TopLocalRank.cs
@@ -1,12 +1,9 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Game.Beatmaps;
 using osu.Game.Online.API;
 using osu.Game.Rulesets;
@@ -14,43 +11,44 @@ using osu.Game.Scoring;
 
 namespace osu.Game.Online.Leaderboards
 {
-    public class TopLocalRank : Container
+    public class TopLocalRank : UpdateableRank
     {
         private readonly BeatmapInfo beatmap;
-        private readonly UpdateableRank rank;
 
         private ScoreManager scores;
         private IBindable<RulesetInfo> ruleset;
         private IAPIProvider api;
 
-        /// <summary>
-        /// Raised when the top score is loaded
-        /// </summary>
-        public Action<ScoreInfo> ScoreLoaded;
+        protected override double LoadDelay => 250;
 
-        public TopLocalRank(BeatmapInfo beatmap)
+        public TopLocalRank(BeatmapInfo beatmap) : base(null)
         {
             this.beatmap = beatmap;
-
-            RelativeSizeAxes = Axes.Both;
-
-            InternalChild = rank = new UpdateableRank(null)
-            {
-                RelativeSizeAxes = Axes.Both
-            };
         }
 
         [BackgroundDependencyLoader]
         private void load(ScoreManager scores, IBindable<RulesetInfo> ruleset, IAPIProvider api)
         {
+            scores.ItemAdded += scoreChanged;
+            scores.ItemRemoved += scoreChanged;
+            ruleset.ValueChanged += _ => fetchAndLoadTopScore();
+
+            this.ruleset = ruleset.GetBoundCopy();
             this.scores = scores;
-            this.ruleset = ruleset;
             this.api = api;
 
-            FetchAndLoadTopScore();
+            fetchAndLoadTopScore();
         }
 
-        public void FetchAndLoadTopScore()
+        private void scoreChanged(ScoreInfo score)
+        {
+            if (score.BeatmapInfoID == beatmap.ID)
+            {
+                fetchAndLoadTopScore();
+            }
+        }
+
+        private void fetchAndLoadTopScore()
         {
             var score = fetchTopScore();
 
@@ -59,9 +57,16 @@ namespace osu.Game.Online.Leaderboards
 
         private void loadTopScore(ScoreInfo score)
         {
-            Schedule(() => rank.Rank = score?.Rank);
+            var rank = score?.Rank;
 
-            ScoreLoaded?.Invoke(score);
+            // toggle the display of this drawable
+            // we do not want empty space if there is no rank to be displayed
+            if (rank.HasValue)
+                Show();
+            else
+                Hide();
+
+            Schedule(() => Rank = rank);
         }
 
         private ScoreInfo fetchTopScore()
@@ -69,8 +74,7 @@ namespace osu.Game.Online.Leaderboards
             if (scores == null || beatmap == null || ruleset?.Value == null || api?.LocalUser.Value == null)
                 return null;
 
-            return scores.GetAllUsableScores()
-                         .Where(s => s.UserID == api.LocalUser.Value.Id && s.BeatmapInfoID == beatmap.ID && s.RulesetID == ruleset.Value.ID)
+            return scores.QueryScores(s => s.UserID == api.LocalUser.Value.Id && s.BeatmapInfoID == beatmap.ID && s.RulesetID == ruleset.Value.ID && !s.DeletePending)
                          .OrderByDescending(s => s.TotalScore)
                          .FirstOrDefault();
         }
