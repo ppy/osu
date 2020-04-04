c27a13fcad7b6b10004a8aa9fe6a4a2818ec6c18
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-04-05 03:17:11 +0900

    Fix sliderball accent colour not being set correctly

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSlider.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSlider.cs
@@ -186,7 +186,7 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
             base.ApplySkin(skin, allowFallback);
 
             bool allowBallTint = skin.GetConfig<OsuSkinConfiguration, bool>(OsuSkinConfiguration.AllowSliderBallTint)?.Value ?? false;
-            Ball.Colour = allowBallTint ? AccentColour.Value : Color4.White;
+            Ball.AccentColour = allowBallTint ? AccentColour.Value : Color4.White;
         }
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
