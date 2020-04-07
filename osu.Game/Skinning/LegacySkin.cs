3ecb99462fce3ac20e5e3aefd5bc909de6c99a81
smoogipoo <smoogipoo@smgi.me>
2020-04-07 16:07:18 +0900

    Make note height scale by minimum column width

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -207,6 +207,9 @@ namespace osu.Game.Skinning
 
                 case LegacyManiaSkinConfigurationLookups.ColumnLineColour:
                     return SkinUtils.As<TValue>(getCustomColour(existing, "ColourColumnLine"));
+
+                case LegacyManiaSkinConfigurationLookups.MinimumColumnWidth:
+                    return SkinUtils.As<TValue>(new Bindable<float>(existing.MinimumColumnWidth));
             }
 
             return null;
