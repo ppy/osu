0eaff00787293a93f945bdf0f5866b5285e2626f
Andrei Zavatski <megaman9919@gmail.com>
2020-04-13 09:45:49 +0300

    Fix typo in test

--- osu.Game.Tests/Visual/UserInterface/TestSceneOverlayScrollContainer.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneOverlayScrollContainer.cs
@@ -63,7 +63,7 @@ namespace osu.Game.Tests.Visual.UserInterface
             AddStep("scroll to start", () => scroll.ScrollToStart(false));
             AddAssert("button is hidden", () => scroll.Button.State == Visibility.Hidden);
 
-            AddStep("scroll to 250", () => scroll.ScrollTo(500));
+            AddStep("scroll to 500", () => scroll.ScrollTo(500));
             AddUntilStep("scrolled back to start", () => Precision.AlmostEquals(scroll.Current, 500, 0.1f));
             AddAssert("button is visible", () => scroll.Button.State == Visibility.Visible);
         }
