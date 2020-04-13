e526e99eaeecf5d0c4dd1278afcc3e9e44ae7996
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-04-13 12:34:51 +0300

    Remove unused usings

--- osu.Game/Overlays/NewsOverlay.cs
+++ osu.Game/Overlays/NewsOverlay.cs
@@ -8,7 +8,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
-using osu.Game.Graphics.Containers;
 using osu.Game.Overlays.News;
 
 namespace osu.Game.Overlays
