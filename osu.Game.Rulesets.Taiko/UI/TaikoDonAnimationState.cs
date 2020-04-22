8cc4c6678951cce25b1e48430ec801bfccb4e93b
Craftplacer <87902114+bubb13gummy@users.noreply.github.com>
2020-04-22 15:19:29 +0200

    Restart branch

--- osu.Game.Rulesets.Taiko/UI/TaikoDonAnimationState.cs
+++ osu.Game.Rulesets.Taiko/UI/TaikoDonAnimationState.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-namespace osu.Game.Rulesets.Taiko
+namespace osu.Game.Rulesets.Taiko.UI
 {
-    public enum TaikoSkinComponents
+    public enum TaikoDonAnimationState
     {
-        InputDrum,
-        TaikoDon
+        Idle,
+        Clear,
+        Kiai,
+        Fail
     }
 }
