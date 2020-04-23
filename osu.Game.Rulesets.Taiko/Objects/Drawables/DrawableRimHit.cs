7de9cd55422e412704d68fc1aebf152dfdc007f2
Tim Oliver <87902114+bubb13gummy@users.noreply.github.com>
2020-04-23 18:16:05 +0800

    Removed un-needed using directives

--- osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableRimHit.cs
+++ osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableRimHit.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Game.Beatmaps;
-using osu.Game.Beatmaps.ControlPoints;
-using osu.Game.Graphics;
-using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.Taiko.Objects.Drawables.Pieces;
 using osu.Game.Skinning;
 
