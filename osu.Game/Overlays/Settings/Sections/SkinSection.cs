c0e3d378658a05cb10fc1ab79321d50f85dfa248
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2020-04-26 21:17:40 +0200

    Remove un-needed RelativeSizeAxes specifications.

--- osu.Game/Overlays/Settings/Sections/SkinSection.cs
+++ osu.Game/Overlays/Settings/Sections/SkinSection.cs
@@ -21,7 +21,6 @@ namespace osu.Game.Overlays.Settings.Sections
 
         public override Drawable CreateIcon() => new SpriteIcon
         {
-            RelativeSizeAxes = Axes.Both,
             Icon = FontAwesome.Solid.PaintBrush
         };
 
