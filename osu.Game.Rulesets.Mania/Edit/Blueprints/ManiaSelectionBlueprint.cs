f93291e25b4967edd658d3e309d33905684d5343
smoogipoo <smoogipoo@smgi.me>
2020-04-28 18:35:22 +0900

    Remove unused override

--- osu.Game.Rulesets.Mania/Edit/Blueprints/ManiaSelectionBlueprint.cs
+++ osu.Game.Rulesets.Mania/Edit/Blueprints/ManiaSelectionBlueprint.cs
@@ -45,25 +45,5 @@ namespace osu.Game.Rulesets.Mania.Edit.Blueprints
             DrawableObject.AlwaysAlive = false;
             base.Hide();
         }
-
-        public override Vector2 GetInstantDelta(Vector2 screenSpacePosition)
-        {
-            var baseDelta = base.GetInstantDelta(screenSpacePosition);
-
-            if (scrollingInfo.Direction.Value == ScrollingDirection.Down)
-            {
-                // The parent of DrawableObject is the scrolling hitobject container (SHOC).
-                // In the coordinate-space of the SHOC, the screen-space position at the hit target is equal to the height of the SHOC,
-                // but this is not what we want as it means a slight movement downwards results in a delta greater than the height of the SHOC.
-                // To get around this issue, the height of the SHOC is subtracted from the delta.
-                //
-                // Ideally this should be a _negative_ value in the case described above, however this code gives a _positive_ delta.
-                // This is intentional as the delta is added to the hitobject's position (see: ManiaSelectionHandler) and a negative delta would move them towards the top of the screen instead,
-                // which would cause the delta to get increasingly larger as additional movements are performed.
-                return new Vector2(baseDelta.X, baseDelta.Y - DrawableObject.Parent.DrawHeight);
-            }
-
-            return baseDelta;
-        }
     }
 }
