544afb60265d1a656733d0066c8293723f95b716
Lucas A <game4allyt@gmail.com>
2020-04-30 12:18:41 +0200

    Remove unused using.

--- osu.Game.Tests/Visual/Online/TestSceneOverlayView.cs
+++ osu.Game.Tests/Visual/Online/TestSceneOverlayView.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
