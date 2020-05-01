ff1d63060dba823c0ea149f13f9b6b83e795944e
Dean Herbert <pe@ppy.sh>
2020-05-01 20:05:56 +0900

    Add and consume passing state in GameplayBeatmap

--- osu.Game/Screens/Play/GameplayBeatmap.cs
+++ osu.Game/Screens/Play/GameplayBeatmap.cs
@@ -2,11 +2,14 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Beatmaps.Timing;
+using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Screens.Play
 {
@@ -38,5 +41,14 @@ namespace osu.Game.Screens.Play
         public IEnumerable<BeatmapStatistic> GetStatistics() => PlayableBeatmap.GetStatistics();
 
         public IBeatmap Clone() => PlayableBeatmap.Clone();
+
+        public IBindable<bool> Passing => passing;
+
+        private readonly BindableBool passing = new BindableBool(true);
+
+        public void OnNewResult(JudgementResult result)
+        {
+            passing.Value = result.Type > HitResult.Miss;
+        }
     }
 }
--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -193,6 +193,7 @@ namespace osu.Game.Screens.Play
             {
                 HealthProcessor.ApplyResult(r);
                 ScoreProcessor.ApplyResult(r);
+                gameplayBeatmap.OnNewResult(r);
             };
 
             DrawableRuleset.OnRevertResult += r =>
