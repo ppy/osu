94d1665a182bd1c4338941c6ea7be4ea200cd0bf
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-05-12 11:08:30 +0900

    Fix beatmap skins providing fallback version lookup, preceding user skins

--- osu.Game/Skinning/LegacyBeatmapSkin.cs
+++ osu.Game/Skinning/LegacyBeatmapSkin.cs
@@ -27,9 +27,11 @@ namespace osu.Game.Skinning
             switch (lookup)
             {
                 case LegacySkinConfiguration.LegacySetting s when s == LegacySkinConfiguration.LegacySetting.Version:
-                    if (Configuration.LegacyVersion is decimal version)
-                        return SkinUtils.As<TValue>(new Bindable<decimal>(version));
+                    // For lookup simplicity, ignore beatmap-level versioning completely.
 
+                    // If it is decided that we need this due to beatmaps somehow using it, the default (1.0 specified in LegacySkinDecoder.CreateTemplateObject)
+                    // needs to be removed else it will cause incorrect skin behaviours. This is due to the config lookup having no context of which skin
+                    // it should be returning the version for.
                     return null;
             }
 
