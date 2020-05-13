1ac9c7c15a88fe24132084b93c2d619cfb46b2da
「空白」 <>
2020-05-14 00:04:31 +0900

    Add license header to WebRequestExtensions

--- osu.Game/Extensions/WebRequestExtensions.cs
+++ osu.Game/Extensions/WebRequestExtensions.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.IO.Network;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.IO.Network;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using System.Collections.Generic;
 using Newtonsoft.Json;
