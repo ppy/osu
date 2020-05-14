ff6642190f0d6e85cdff7eb3b2998fef0f9fb974
Salman Ahmed <email@iisalman.me>
2020-05-14 07:26:47 +0300

    Update colour retrieval logic

--- osu.Game.Rulesets.Catch.Tests/CatchSkinColourDecodingTest.cs
+++ osu.Game.Rulesets.Catch.Tests/CatchSkinColourDecodingTest.cs
@@ -19,9 +19,9 @@ namespace osu.Game.Rulesets.Catch.Tests
             var rawSkin = new TestLegacySkin(new SkinInfo { Name = "special-skin" }, store);
             var skin = new CatchLegacySkinTransformer(rawSkin);
 
-            Assert.AreEqual(new Color4(232, 185, 35, 255), skin.GetHyperDashCatcherColour()?.Value);
-            Assert.AreEqual(new Color4(232, 74, 35, 255), skin.GetHyperDashCatcherAfterImageColour()?.Value);
-            Assert.AreEqual(new Color4(0, 255, 255, 255), skin.GetHyperDashFruitColour()?.Value);
+            Assert.AreEqual(new Color4(232, 185, 35, 255), skin.GetConfig<CatchSkinColour, Color4>(CatchSkinColour.HyperDash)?.Value);
+            Assert.AreEqual(new Color4(232, 74, 35, 255), skin.GetConfig<CatchSkinColour, Color4>(CatchSkinColour.HyperDashAfterImage)?.Value);
+            Assert.AreEqual(new Color4(0, 255, 255, 255), skin.GetConfig<CatchSkinColour, Color4>(CatchSkinColour.HyperDashFruit)?.Value);
         }
 
         private class TestLegacySkin : LegacySkin
