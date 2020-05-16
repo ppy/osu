47ec98d31379d3fb0a86671a3f62b88e8c8dcd15
Shivam <87902114+bubb13gummy@users.noreply.github.com>
2020-05-16 02:57:58 +0200

    Introduce model to store path of stable osu!

---
+++ osu.Game.Tournament/Models/StableInfo.cs
@@ -0,0 +1,17 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Bindables;
+
+namespace osu.Game.Tournament.Models
+{
+    /// <summary>
+    /// Holds the complete data required to operate the tournament system.
+    /// </summary>
+    [Serializable]
+    public class StableInfo
+    {
+        public Bindable<string> StablePath = new Bindable<string>(string.Empty);
+    }
+}
