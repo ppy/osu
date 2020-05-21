a11873608bb8c5630a5be48f9acd1bbe133e1b4d
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-05-21 20:48:25 +0900

    Refactor initial state

--- osu.Game.Tests/Visual/Ranking/TestSceneScorePanelList.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneScorePanelList.cs
@@ -1,38 +1,51 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Screens.Ranking;
+using osuTK.Graphics;
 
 namespace osu.Game.Tests.Visual.Ranking
 {
     public class TestSceneScorePanelList : OsuTestScene
     {
-        public TestSceneScorePanelList()
+        private ScorePanelList list;
+
+        [SetUp]
+        public void Setup() => Schedule(() =>
         {
-            var list = new ScorePanelList
+            Child = list = new ScorePanelList(new TestScoreInfo(new OsuRuleset().RulesetInfo))
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
             };
 
-            Add(list);
+            Add(new Box
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                RelativeSizeAxes = Axes.Y,
+                Width = 1,
+                Colour = Color4.Red
+            });
+        });
 
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
-            list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
+        [Test]
+        public void TestSingleScore()
+        {
+        }
+
+        [Test]
+        public void TestManyScores()
+        {
+            AddStep("add many scores", () =>
+            {
+                for (int i = 0; i < 20; i++)
+                    list.AddScore(new TestScoreInfo(new OsuRuleset().RulesetInfo));
+            });
         }
     }
 }
