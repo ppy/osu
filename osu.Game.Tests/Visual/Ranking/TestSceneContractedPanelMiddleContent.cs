aaf5596f9c0f619fc9eb63ab046f21a03885bb45
smoogipoo <smoogipoo@smgi.me>
2020-05-26 15:54:07 +0900

    Cleanup test

--- osu.Game.Tests/Visual/Ranking/TestSceneContractedPanelMiddleContent.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneContractedPanelMiddleContent.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -15,7 +14,6 @@ using osu.Game.Rulesets.Osu;
 using osu.Game.Scoring;
 using osu.Game.Screens.Ranking;
 using osu.Game.Screens.Ranking.Contracted;
-using osu.Game.Tests.Beatmaps;
 using osuTK;
 
 namespace osu.Game.Tests.Visual.Ranking
@@ -28,24 +26,13 @@ namespace osu.Game.Tests.Visual.Ranking
         [Test]
         public void TestShowPanel()
         {
-            AddStep("show example score", () => showPanel(createTestBeatmap(), new TestScoreInfo(new OsuRuleset().RulesetInfo)));
+            AddStep("show example score", () => showPanel(CreateWorkingBeatmap(CreateBeatmap(new OsuRuleset().RulesetInfo)), new TestScoreInfo(new OsuRuleset().RulesetInfo)));
         }
 
         private void showPanel(WorkingBeatmap workingBeatmap, ScoreInfo score)
         {
             Child = new ContractedPanelMiddleContentContainer(workingBeatmap, score);
         }
-
-        private WorkingBeatmap createTestBeatmap()
-        {
-            var beatmap = new TestBeatmap(rulesetStore.GetRuleset(0));
-            beatmap.Metadata.Title = "Verrrrrrrrrrrrrrrrrrry looooooooooooooooooooooooong beatmap title";
-            beatmap.Metadata.Artist = "Verrrrrrrrrrrrrrrrrrry looooooooooooooooooooooooong beatmap artist";
-
-            return new TestWorkingBeatmap(beatmap);
-        }
-
-        private bool containsAny(string text, params string[] stringsToMatch) => stringsToMatch.Any(text.Contains);
 
         private class ContractedPanelMiddleContentContainer : Container
         {
