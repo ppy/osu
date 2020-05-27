dd7dbfd5488efddef6d56f897876ce6cba4eb32d
Dean Herbert <pe@ppy.sh>
2020-05-27 12:38:39 +0900

    Rename to IHasDuration

--- osu.Game/Rulesets/Objects/Types/IHasEndTime.cs
+++ osu.Game/Rulesets/Objects/Types/IHasEndTime.cs
@@ -1,24 +1,12 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using Newtonsoft.Json;
+using System;
 
 namespace osu.Game.Rulesets.Objects.Types
 {
-    /// <summary>
-    /// A HitObject that ends at a different time than its start time.
-    /// </summary>
-    public interface IHasEndTime
+    [Obsolete("Use IHasDuration instead.")] // can be removed 20201126
+    public interface IHasEndTime : IHasDuration
     {
-        /// <summary>
-        /// The time at which the HitObject ends.
-        /// </summary>
-        double EndTime { get; }
-
-        /// <summary>
-        /// The duration of the HitObject.
-        /// </summary>
-        [JsonIgnore]
-        double Duration { get; set; }
     }
 }
