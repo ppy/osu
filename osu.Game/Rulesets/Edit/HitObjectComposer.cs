9a060cfb3acfac28816eb68f245e365153a9af46
Dean Herbert <pe@ppy.sh>
2020-05-27 20:44:15 +0900

    Allow drag selections to occur from outside the playfield

--- osu.Game/Rulesets/Edit/HitObjectComposer.cs
+++ osu.Game/Rulesets/Edit/HitObjectComposer.cs
@@ -102,6 +102,7 @@ namespace osu.Game.Rulesets.Edit
                         {
                             Name = "Content",
                             RelativeSizeAxes = Axes.Both,
+                            Masking = true,
                             Children = new Drawable[]
                             {
                                 // layers below playfield
