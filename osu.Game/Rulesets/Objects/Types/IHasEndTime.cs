d62d06615037598e9e73c81c2ec677568c4b473a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-05-29 16:11:26 +0900

    Flip direction to avoid breaking other usages

--- osu.Game/Rulesets/Objects/Types/IHasEndTime.cs
+++ osu.Game/Rulesets/Objects/Types/IHasEndTime.cs
@@ -2,11 +2,25 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using Newtonsoft.Json;
 
 namespace osu.Game.Rulesets.Objects.Types
 {
+    /// <summary>
+    /// A HitObject that ends at a different time than its start time.
+    /// </summary>
     [Obsolete("Use IHasDuration instead.")] // can be removed 20201126
-    public interface IHasEndTime : IHasDuration
+    public interface IHasEndTime
     {
+        /// <summary>
+        /// The time at which the HitObject ends.
+        /// </summary>
+        [JsonIgnore]
+        double EndTime { get; set; }
+
+        /// <summary>
+        /// The duration of the HitObject.
+        /// </summary>
+        double Duration { get; }
     }
 }
