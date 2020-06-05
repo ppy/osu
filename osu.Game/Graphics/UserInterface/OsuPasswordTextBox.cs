daaa4875817c8c2f3b334f304faa35f74129703e
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2020-06-05 06:44:41 +0300

    Fix password text boxes having distinguishable key sounds
    Closes https://github.com/ppy/osu-framework/issues/3280

--- osu.Game/Graphics/UserInterface/OsuPasswordTextBox.cs
+++ osu.Game/Graphics/UserInterface/OsuPasswordTextBox.cs
@@ -24,6 +24,8 @@ namespace osu.Game.Graphics.UserInterface
             Child = new PasswordMaskChar(CalculatedTextSize),
         };
 
+        protected override bool AllowUpperCaseSamples => false;
+
         protected override bool AllowClipboardExport => false;
 
         private readonly CapsWarning warning;
