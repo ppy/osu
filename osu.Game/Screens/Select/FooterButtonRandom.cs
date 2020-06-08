17b6ed73f0970619d35790288df4b09a0e36af1e
Jack Boswell (boswelja) <87902114+bubb13gummy@users.noreply.github.com>
2020-06-08 17:31:31 +1200

    Clean up FooterButtonRandom Removed unnecessary using statements Removed unnecessary constructor

--- osu.Game/Screens/Select/FooterButtonRandom.cs
+++ osu.Game/Screens/Select/FooterButtonRandom.cs
@@ -4,10 +4,7 @@
 using System;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Sprites;
 using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
 using osu.Game.Input.Bindings;
 
 namespace osu.Game.Screens.Select
@@ -18,8 +15,6 @@ namespace osu.Game.Screens.Select
         public Action PreviousRandom { get; set; }
 
         private bool rewindSearch;
-
-        public FooterButtonRandom() { }
 
         [BackgroundDependencyLoader]
         private void load(OsuColour colours)
