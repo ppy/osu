d47cd63a91258289608f9033ab3fb1226985eec3
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-06-09 18:53:55 +0900

    Add loading spinner

--- osu.Game/Screens/Multi/Ranking/TimeshiftResultsScreen.cs
+++ osu.Game/Screens/Multi/Ranking/TimeshiftResultsScreen.cs
@@ -4,6 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests;
 using osu.Game.Online.Multiplayer;
@@ -17,6 +21,8 @@ namespace osu.Game.Screens.Multi.Ranking
         private readonly int roomId;
         private readonly PlaylistItem playlistItem;
 
+        private LoadingSpinner loadingLayer;
+
         public TimeshiftResultsScreen(ScoreInfo score, int roomId, PlaylistItem playlistItem, bool allowRetry = true)
             : base(score, allowRetry)
         {
@@ -24,10 +30,31 @@ namespace osu.Game.Screens.Multi.Ranking
             this.playlistItem = playlistItem;
         }
 
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AddInternal(loadingLayer = new LoadingLayer
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                X = -10,
+                State = { Value = Score == null ? Visibility.Visible : Visibility.Hidden },
+                Padding = new MarginPadding { Bottom = TwoLayerButton.SIZE_EXTENDED.Y }
+            });
+        }
+
         protected override APIRequest FetchScores(Action<IEnumerable<ScoreInfo>> scoresCallback)
         {
             var req = new GetRoomPlaylistScoresRequest(roomId, playlistItem.ID);
-            req.Success += r => scoresCallback?.Invoke(r.Scores.Where(s => s.ID != Score?.OnlineScoreID).Select(s => s.CreateScoreInfo(playlistItem)));
+
+            req.Success += r =>
+            {
+                scoresCallback?.Invoke(r.Scores.Where(s => s.ID != Score?.OnlineScoreID).Select(s => s.CreateScoreInfo(playlistItem)));
+                loadingLayer.Hide();
+            };
+
+            req.Failure += _ => loadingLayer.Hide();
+
             return req;
         }
     }
