347be74407193203416305cdd459a679b4788006
Shivam <87902114+bubb13gummy@users.noreply.github.com>
2020-06-12 15:57:23 +0200

    Change hash to be lowercase and change sample directories

--- osu.Game/Screens/Menu/IntroCircles.cs
+++ osu.Game/Screens/Menu/IntroCircles.cs
@@ -24,7 +24,7 @@ namespace osu.Game.Screens.Menu
         private void load(AudioManager audio)
         {
             if (MenuVoice.Value)
-                welcome = audio.Samples.Get(@"Intro/lazer/welcome");
+                welcome = audio.Samples.Get(@"Intro/welcome");
         }
 
         protected override void LogoArriving(OsuLogo logo, bool resuming)
