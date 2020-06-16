1992ce3bce889ef0c1c202268daeb86ce690fd22
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-06-16 17:49:37 +0900

    Improve test scene

--- osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
@@ -1,8 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Osu.Scoring;
 using osu.Game.Scoring;
 using osu.Game.Screens.Ranking.Statistics;
 
@@ -13,7 +15,16 @@ namespace osu.Game.Tests.Visual.Ranking
         [Test]
         public void TestScore()
         {
-            loadPanel(new TestScoreInfo(new OsuRuleset().RulesetInfo));
+            var score = new TestScoreInfo(new OsuRuleset().RulesetInfo)
+            {
+                ExtraStatistics =
+                {
+                    ["timing_distribution"] = TestSceneTimingDistributionGraph.CreateNormalDistribution(),
+                    ["hit_offsets"] = new List<HitOffset>()
+                }
+            };
+
+            loadPanel(score);
         }
 
         private void loadPanel(ScoreInfo score) => AddStep("load panel", () =>
