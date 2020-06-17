bed5e857df7d2af44ae5f4bbfa304f04be741da1
Ronnie Moir <7267697+H2n9@users.noreply.github.com>
2020-06-17 14:49:55 +0100

    Add missing license header and remove unused usings

--- osu.Game/Rulesets/Mods/IApplicableToAudio.cs
+++ osu.Game/Rulesets/Mods/IApplicableToAudio.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
 
 namespace osu.Game.Rulesets.Mods
 {
