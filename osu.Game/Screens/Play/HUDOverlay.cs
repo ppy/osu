798e8e7a8deea5d1ac665bc9491604c0f082e5ed
Power Maker <power_maker@o2.pl>
2020-06-26 15:12:01 +0200

    Fix CI fail

--- osu.Game/Screens/Play/HUDOverlay.cs
+++ osu.Game/Screens/Play/HUDOverlay.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.Diagnostics.Runtime.Interop;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.IEnumerableExtensions;
