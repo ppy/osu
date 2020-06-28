0ffecb5b797bc726141e804408f44d30b413dffc
Shivam <87902114+bubb13gummy@users.noreply.github.com>
2020-06-29 00:39:49 +0200

    Add the option to loop the intro in the main menu

--- osu.Game/Configuration/OsuConfigManager.cs
+++ osu.Game/Configuration/OsuConfigManager.cs
@@ -55,6 +55,7 @@ namespace osu.Game.Configuration
             Set(OsuSetting.VolumeInactive, 0.25, 0, 1, 0.01);
 
             Set(OsuSetting.MenuVoice, true);
+            Set(OsuSetting.MenuMusicLoop, true);
             Set(OsuSetting.MenuMusic, true);
 
             Set(OsuSetting.AudioOffset, 0, -500.0, 500.0, 1);
@@ -191,6 +192,7 @@ namespace osu.Game.Configuration
         AudioOffset,
         VolumeInactive,
         MenuMusic,
+        MenuMusicLoop,
         MenuVoice,
         CursorRotation,
         MenuParallax,
