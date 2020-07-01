fc1eb42a650fef5497bec37e20b5e2a29f773c07
Lucas A <game4allyt@gmail.com>
2020-07-01 17:15:41 +0200

    Disable windows key while in gameplay.

---
+++ osu.Desktop/Windows/GameplayWinKeyHandler.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Platform;
+using osu.Game.Configuration;
+
+namespace osu.Desktop.Windows
+{
+    public class GameplayWinKeyHandler : Component
+    {
+        private Bindable<bool> winKeyEnabled;
+        private Bindable<bool> disableWinKey;
+
+        private GameHost host;
+
+        [BackgroundDependencyLoader]
+        private void load(GameHost host, OsuConfigManager config)
+        {
+            this.host = host;
+
+            winKeyEnabled = host.AllowScreenSuspension.GetBoundCopy();
+            winKeyEnabled.ValueChanged += toggleWinKey;
+
+            disableWinKey = config.GetBindable<bool>(OsuSetting.GameplayDisableWinKey);
+            disableWinKey.BindValueChanged(t => winKeyEnabled.TriggerChange());
+        }
+
+        private void toggleWinKey(ValueChangedEvent<bool> e)
+        {
+            if (!e.NewValue && disableWinKey.Value)
+                host.InputThread.Scheduler.Add(WindowsKey.Disable);
+            else
+                host.InputThread.Scheduler.Add(WindowsKey.Enable);
+        }
+    }
+}
