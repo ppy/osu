c18ca19c9d60c1e5715d7b2ea05e00566f827fbd
Andrei Zavatski <megaman9919@gmail.com>
2020-07-06 05:31:34 +0300

    Add NewsPost api response

---
+++ osu.Game/Online/API/Requests/Responses/NewsPost.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Newtonsoft.Json;
+using System;
+
+namespace osu.Game.Online.API.Requests.Responses
+{
+    public class NewsPost
+    {
+        [JsonProperty(@"id")]
+        public long Id { get; set; }
+
+        [JsonProperty(@"author")]
+        public string Author { get; set; }
+
+        [JsonProperty(@"edit_url")]
+        public string EditUrl { get; set; }
+
+        [JsonProperty(@"first_image")]
+        public string FirstImage { get; set; }
+
+        [JsonProperty(@"published_at")]
+        public DateTimeOffset PublishedAt { get; set; }
+
+        [JsonProperty(@"updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonProperty(@"slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty(@"title")]
+        public string Title { get; set; }
+
+        [JsonProperty(@"preview")]
+        public string Preview { get; set; }
+    }
+}
