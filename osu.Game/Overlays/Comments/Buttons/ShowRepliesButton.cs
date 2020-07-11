0861ee0c8e985bb413681037ab54ea00f1910d82
Andrei Zavatski <megaman9919@gmail.com>
2020-07-11 07:54:37 +0300

    Make Icon rotate when clicking ShowRepliesButton

--- osu.Game/Overlays/Comments/Buttons/ShowRepliesButton.cs
+++ osu.Game/Overlays/Comments/Buttons/ShowRepliesButton.cs
@@ -2,16 +2,39 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using Humanizer;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
+using osuTK;
 
 namespace osu.Game.Overlays.Comments.Buttons
 {
     public class ShowRepliesButton : CommentRepliesButton
     {
+        public readonly BindableBool Expanded = new BindableBool(true);
+
         private readonly int count;
 
         public ShowRepliesButton(int count)
         {
             this.count = count;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            Expanded.BindValueChanged(onExpandedChanged, true);
+        }
+
+        private void onExpandedChanged(ValueChangedEvent<bool> expanded)
+        {
+            Icon.ScaleTo(new Vector2(1, expanded.NewValue ? -1 : 1));
+        }
+
+        protected override bool OnClick(ClickEvent e)
+        {
+            Expanded.Toggle();
+            return base.OnClick(e);
         }
 
         protected override string GetText() => "reply".ToQuantity(count);
