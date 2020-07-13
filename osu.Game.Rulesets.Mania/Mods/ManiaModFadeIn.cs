31782172165372dec692b8d875efd0ac58e06b24
LastExceed <chris20194@googlemail.com>
2020-07-13 13:14:47 +0200

    remove unnecessary import

--- osu.Game.Rulesets.Mania/Mods/ManiaModFadeIn.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModFadeIn.cs
@@ -9,7 +9,6 @@ using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Testing;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mania.Objects;
 using osu.Game.Rulesets.Mania.UI;
