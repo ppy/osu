5896c697c48082a4aeced817cdc106047a6118bc
MATRIX-feather <midnightcarnival@outlook.com>
2020-07-21 00:48:30 +0800

    (希望)修复故事版音频错位问题

--- osu.Game/Screens/Mvis/Storyboard/StoryboardClock.cs
+++ osu.Game/Screens/Mvis/Storyboard/StoryboardClock.cs
@@ -22,33 +22,23 @@ namespace osu.Game.Screens.Mvis.Storyboard
         private DecoupleableInterpolatingFramedClock adjustableClock;
         private WorkingBeatmap beatmap;
         private BindableBool IsPaused = new BindableBool();
-        private Bindable<double> userAudioOffset;
-        private double baseOffset => (RuntimeInfo.OS == RuntimeInfo.Platform.Windows ? 15 : 0);
 
         public ClockContainer(WorkingBeatmap b)
         {
             this.beatmap = b;
 
-            baseOffsetClock = new HardwareCorrectionOffsetClock(adjustableClock) { Offset = 0 };
-
             adjustableClock = new DecoupleableInterpolatingFramedClock { IsCoupled = false };
 
-            gameplayClock = new GameplayClock(adjustableClock);
-        }
+            baseOffsetClock = new HardwareCorrectionOffsetClock(adjustableClock) { Offset = 0 };
 
-
-        [BackgroundDependencyLoader]
-        private void load(OsuConfigManager config)
-        {
-            userAudioOffset = config.GetBindable<double>(OsuSetting.AudioOffset);
-            userAudioOffset.BindValueChanged(offset => baseOffsetClock.Offset = (baseOffset + offset.NewValue), true);
+            gameplayClock = new GameplayClock(baseOffsetClock);
         }
 
         public void Seek(double time)
         {
             adjustableClock.Seek(time);
 
-            adjustableClock.ProcessFrame();
+            baseOffsetClock.ProcessFrame();
         }
 
         public void Start()
@@ -68,7 +58,7 @@ namespace osu.Game.Screens.Mvis.Storyboard
         protected override void Update()
         {
             if ( !IsPaused.Value )
-                adjustableClock.ProcessFrame();
+                baseOffsetClock.ProcessFrame();
         }
     }
 }
