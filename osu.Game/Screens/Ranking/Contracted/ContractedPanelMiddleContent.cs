2fd2654d69c21b810a13e46d719ed6dd5600c25a
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-07-28 20:34:09 +0900

    Delay loading of cover backgrounds in score panels

--- osu.Game/Screens/Ranking/Contracted/ContractedPanelMiddleContent.cs
+++ osu.Game/Screens/Ranking/Contracted/ContractedPanelMiddleContent.cs
@@ -70,11 +70,14 @@ namespace osu.Game.Screens.Ranking.Contracted
                                     RelativeSizeAxes = Axes.Both,
                                     Colour = Color4Extensions.FromHex("444")
                                 },
-                                new UserCoverBackground
+                                new DelayedLoadUnloadWrapper(() => new UserCoverBackground
                                 {
                                     RelativeSizeAxes = Axes.Both,
                                     User = score.User,
                                     Colour = ColourInfo.GradientVertical(Color4.White.Opacity(0.5f), Color4Extensions.FromHex("#444").Opacity(0))
+                                }, 300, 5000)
+                                {
+                                    RelativeSizeAxes = Axes.Both
                                 },
                                 new FillFlowContainer
                                 {
