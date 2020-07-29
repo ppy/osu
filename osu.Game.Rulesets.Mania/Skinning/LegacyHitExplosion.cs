a048aad84f9aed445bcf1f92791e40c6541309c2
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-07-29 15:36:42 +0900

    Add pooling for mania hit explosions

--- osu.Game.Rulesets.Mania/Skinning/LegacyHitExplosion.cs
+++ osu.Game.Rulesets.Mania/Skinning/LegacyHitExplosion.cs
@@ -6,13 +6,14 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Animations;
+using osu.Game.Rulesets.Mania.UI;
 using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Skinning;
 using osuTK;
 
 namespace osu.Game.Rulesets.Mania.Skinning
 {
-    public class LegacyHitExplosion : LegacyManiaColumnElement
+    public class LegacyHitExplosion : LegacyManiaColumnElement, IHitExplosion
     {
         private readonly IBindable<ScrollingDirection> direction = new Bindable<ScrollingDirection>();
 
@@ -62,10 +63,8 @@ namespace osu.Game.Rulesets.Mania.Skinning
                 explosion.Anchor = direction.NewValue == ScrollingDirection.Up ? Anchor.TopCentre : Anchor.BottomCentre;
         }
 
-        protected override void LoadComplete()
+        public void Animate()
         {
-            base.LoadComplete();
-
             explosion?.FadeInFromZero(80)
                      .Then().FadeOut(120);
         }
