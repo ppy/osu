ce47a34991ce574265aeaf37134c3d9f6de02cee
Andrei Zavatski <megaman9919@gmail.com>
2020-08-01 06:14:24 +0300

    Implement DashboardNewBeatmapPanel component

---
+++ osu.Game/Overlays/Dashboard/Dashboard/DashboardNewBeatmapPanel.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Beatmaps;
+using osu.Game.Graphics;
+
+namespace osu.Game.Overlays.Dashboard.Dashboard
+{
+    public class DashboardNewBeatmapPanel : DashboardBeatmapPanel
+    {
+        public DashboardNewBeatmapPanel(BeatmapSetInfo setInfo)
+            : base(setInfo)
+        {
+        }
+
+        protected override Drawable CreateInfo() => new DrawableDate(SetInfo.OnlineInfo.Ranked.Value, 10, false)
+        {
+            Colour = ColourProvider.Foreground1
+        };
+    }
+}
