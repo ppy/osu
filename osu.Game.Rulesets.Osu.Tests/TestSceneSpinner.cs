bb73489ae5947c4be39ecaca7a3634f4b49c6db1
Dean Herbert <pe@ppy.sh>
2020-08-05 18:44:34 +0900

    Fix very short spinners being impossible to complete

--- osu.Game.Rulesets.Osu.Tests/TestSceneSpinner.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneSpinner.cs
@@ -17,32 +17,54 @@ namespace osu.Game.Rulesets.Osu.Tests
     {
         private int depthIndex;
 
-        public TestSceneSpinner()
+        private TestDrawableSpinner drawableSpinner;
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestVariousSpinners(bool autoplay)
         {
             AddStep("Miss Big", () => SetContents(() => testSingle(2)));
             AddStep("Miss Medium", () => SetContents(() => testSingle(5)));
             AddStep("Miss Small", () => SetContents(() => testSingle(7)));
-            AddStep("Hit Big", () => SetContents(() => testSingle(2, true)));
-            AddStep("Hit Medium", () => SetContents(() => testSingle(5, true)));
-            AddStep("Hit Small", () => SetContents(() => testSingle(7, true)));
+            AddStep("Hit Big", () => SetContents(() => testSingle(2, autoplay)));
+            AddStep("Hit Medium", () => SetContents(() => testSingle(5, autoplay)));
+            AddStep("Hit Small", () => SetContents(() => testSingle(7, autoplay)));
         }
 
-        private Drawable testSingle(float circleSize, bool auto = false)
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestLongSpinner(bool autoplay)
         {
-            var spinner = new Spinner { StartTime = Time.Current + 2000, EndTime = Time.Current + 5000 };
+            AddStep("Very short spinner", () => SetContents(() => testSingle(5, autoplay, 2000)));
+            AddUntilStep("Wait for completion", () => drawableSpinner.Result.HasResult);
+            AddUntilStep("Check correct progress", () => drawableSpinner.Progress == (autoplay ? 1 : 0));
+        }
+
+        [TestCase(false)]
+        [TestCase(true)]
+        public void TestSuperShortSpinner(bool autoplay)
+        {
+            AddStep("Very short spinner", () => SetContents(() => testSingle(5, autoplay, 200)));
+            AddUntilStep("Wait for completion", () => drawableSpinner.Result.HasResult);
+            AddUntilStep("Short spinner implicitly completes", () => drawableSpinner.Progress == 1);
+        }
+
+        private Drawable testSingle(float circleSize, bool auto = false, double length = 3000)
+        {
+            var spinner = new Spinner { StartTime = Time.Current + 2000, EndTime = Time.Current + +2000 + length };
 
             spinner.ApplyDefaults(new ControlPointInfo(), new BeatmapDifficulty { CircleSize = circleSize });
 
-            var drawable = new TestDrawableSpinner(spinner, auto)
+            drawableSpinner = new TestDrawableSpinner(spinner, auto)
             {
                 Anchor = Anchor.Centre,
                 Depth = depthIndex++
             };
 
             foreach (var mod in SelectedMods.Value.OfType<IApplicableToDrawableHitObjects>())
-                mod.ApplyToDrawableHitObjects(new[] { drawable });
+                mod.ApplyToDrawableHitObjects(new[] { drawableSpinner });
 
-            return drawable;
+            return drawableSpinner;
         }
 
         private class TestDrawableSpinner : DrawableSpinner
