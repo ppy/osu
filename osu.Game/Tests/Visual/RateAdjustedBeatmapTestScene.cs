4cfca71d080822734e7f29de27b5273f3304460a
smoogipoo <smoogipoo@smgi.me>
2020-08-06 19:05:15 +0900

    Fix a few test scenes

--- osu.Game/Tests/Visual/RateAdjustedBeatmapTestScene.cs
+++ osu.Game/Tests/Visual/RateAdjustedBeatmapTestScene.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
 using osu.Framework.Extensions.ObjectExtensions;
 
 namespace osu.Game.Tests.Visual
@@ -15,7 +16,11 @@ namespace osu.Game.Tests.Visual
             base.Update();
 
             // note that this will override any mod rate application
-            MusicController.CurrentTrack.AsNonNull().Tempo.Value = Clock.Rate;
+            if (MusicController.TrackLoaded)
+            {
+                Debug.Assert(MusicController.CurrentTrack != null);
+                MusicController.CurrentTrack.Tempo.Value = Clock.Rate;
+            }
         }
     }
 }
