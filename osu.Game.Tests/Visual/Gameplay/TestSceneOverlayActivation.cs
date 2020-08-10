08b3f418662a52491e2734fdbe1b85faafcac594
Lucas A <87902114+bubb13gummy@users.noreply.github.com>
2020-08-10 20:13:50 +0200

    Add ability to open overlays during gameplay breaks.

--- osu.Game.Tests/Visual/Gameplay/TestSceneOverlayActivation.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneOverlayActivation.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Game.Configuration;
 using osu.Game.Overlays;
@@ -45,6 +46,16 @@ namespace osu.Game.Tests.Visual.Gameplay
             AddAssert("activation mode is disabled", () => testPlayer.OverlayActivationMode == OverlayActivation.Disabled);
             AddStep("load a replay", () => testPlayer.DrawableRuleset.HasReplayLoaded.Value = true);
             AddAssert("activation mode is user triggered", () => testPlayer.OverlayActivationMode == OverlayActivation.UserTriggered);
+        }
+
+        [Test]
+        public void TestGameplayOverlayActivationBreaks()
+        {
+            AddAssert("activation mode is disabled", () => testPlayer.OverlayActivationMode == OverlayActivation.Disabled);
+            AddStep("seek to break", () => testPlayer.GameplayClockContainer.Seek(Beatmap.Value.Beatmap.Breaks.First().StartTime));
+            AddUntilStep("activation mode is user triggered", () => testPlayer.OverlayActivationMode == OverlayActivation.UserTriggered);
+            AddStep("seek to break end", () => testPlayer.GameplayClockContainer.Seek(Beatmap.Value.Beatmap.Breaks.First().EndTime));
+            AddUntilStep("activation mode is disabled", () => testPlayer.OverlayActivationMode == OverlayActivation.Disabled);
         }
 
         protected override TestPlayer CreatePlayer(Ruleset ruleset) => testPlayer = new OverlayTestPlayer();
