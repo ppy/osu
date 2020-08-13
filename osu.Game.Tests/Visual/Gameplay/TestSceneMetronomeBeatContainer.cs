cfd708e354534c4d116f19a432a131945dca4b91
BananeVolante <bananevolante67@gmail.com>
2020-08-13 23:03:40 +0200

    added tests

---
+++ osu.Game.Tests/Visual/Gameplay/TestSceneMetronomeBeatContainer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.IEnumerableExtensions;
+using osu.Game.Beatmaps.Timing;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Tests.Visual.UserInterface;
+
+namespace osu.Game.Tests.Visual.Gameplay
+{
+    public class TestSceneMetronomeBeatContainer : TestSceneBeatSyncedContainer
+    {
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            Beatmap.Value = CreateWorkingBeatmap(new OsuRuleset().RulesetInfo);
+
+            Beatmap.Value.Track.Start();
+            Beatmap.Value.Track.Seek(Beatmap.Value.Beatmap.HitObjects.First().StartTime - 1000);
+
+            var TickFrequency = new Bindable<ModMetronome.TickFrequency>(ModMetronome.TickFrequency.One);
+            Add(new ModMetronome<HitObject>.MetronomeBeatContainer(TickFrequency)); ;
+
+            AddStep("change tick frequency to One", () => TickFrequency.Value = ModMetronome.TickFrequency.One);
+            AddStep("change tick frequency to Two", () => TickFrequency.Value = ModMetronome.TickFrequency.Two);
+            AddStep("change tick frequency to Four", () =>  TickFrequency.Value = ModMetronome.TickFrequency.Four);
+        }
+    }
+}
