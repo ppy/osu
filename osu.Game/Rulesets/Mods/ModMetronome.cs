4f5c9ffbdb8c295d522e09052332bc5e8f8d8712
BananeVolante <bananevolante67@gmail.com>
2020-08-17 13:24:34 +0200

    changed metronome icon, fixed some things, added a different sample for the first beat of a bar

--- osu.Game/Rulesets/Mods/ModMetronome.cs
+++ osu.Game/Rulesets/Mods/ModMetronome.cs
@@ -13,6 +13,12 @@ using osu.Game.Skinning;
 using osu.Game.Configuration;
 using osu.Framework.Bindables;
 using osu.Game.Audio;
+using osu.Game.Beatmaps.Timing;
+using osu.Framework.Graphics;
+using osu.Framework.Audio;
+using osu.Framework.Logging;
+using System.Linq;
+using System;
 
 namespace osu.Game.Rulesets.Mods
 {
@@ -20,7 +26,7 @@ namespace osu.Game.Rulesets.Mods
     {
         public override string Name => "Metronome";
         public override string Acronym => "MT";
-        public override IconUsage? Icon => OsuIcon.ModAuto; // temporary icon
+        public override IconUsage? Icon => FontAwesome.Solid.TachometerAlt;
         public override ModType Type => ModType.DifficultyReduction;
         public override string Description => "Add a metronome";
         public override bool Ranked => false;
@@ -35,6 +41,9 @@ namespace osu.Game.Rulesets.Mods
 
         [SettingSource("Tick frequency", "Number of metronome ticks per beat")]
         public Bindable<TickFrequency> Frequency { get; } = new Bindable<TickFrequency>(TickFrequency.One);
+
+        [SettingSource("Different Sound on the first beat of a bar")]
+        public Bindable<bool> SpecialSampleForFirstBeatOfBar { get; } = new Bindable<bool>(true);
     }
 
     public abstract class ModMetronome<TObject> : ModMetronome, IApplicableToDrawableRuleset<TObject>
@@ -42,18 +51,26 @@ namespace osu.Game.Rulesets.Mods
     {
         public void ApplyToDrawableRuleset(DrawableRuleset<TObject> drawableRuleset)
         {
-            drawableRuleset.Overlays.Add(new MetronomeBeatContainer(Frequency.GetBoundCopy()));
+            drawableRuleset.Overlays.Add(new MetronomeBeatContainer(Frequency, SpecialSampleForFirstBeatOfBar));
         }
 
         public class MetronomeBeatContainer : BeatSyncedContainer
         {
-            private SkinnableSound metronomeSample;
+            private SkinnableSound firstBeatOfBarSample;
+            private SkinnableSound otherBeatOfBarSample;
 
-            public MetronomeBeatContainer(Bindable<TickFrequency> tickFrequency)
+            private bool specialSampleForFirstBeatOfBar;
+
+            public MetronomeBeatContainer(Bindable<TickFrequency> tickFrequency, Bindable<bool> specialSampleForFirstBeatOfBar)
             {
                 tickFrequency.BindValueChanged(val =>
                 {
-                    Divisor = (int)tickFrequency.Value;
+                    Divisor = (int) tickFrequency.Value;
+                }, true);
+
+                specialSampleForFirstBeatOfBar.BindValueChanged(val =>
+                {
+                    this.specialSampleForFirstBeatOfBar = specialSampleForFirstBeatOfBar.Value;
                 }, true);
             }
 
@@ -62,27 +79,42 @@ namespace osu.Game.Rulesets.Mods
             [BackgroundDependencyLoader]
             private void load()
             {
-                InternalChild = metronomeSample = new SkinnableSound(new SampleInfo("nightcore-hat")); // temporary sample
-            }
+                InternalChildren = new Drawable[]
+                {
+                    // temporary samples
+                    firstBeatOfBarSample = new SkinnableSound(new SampleInfo("nightcore-hat"))
+                    {
+                        Frequency = { Value = 2.0f }
+                    },
+                    otherBeatOfBarSample = new SkinnableSound(new SampleInfo("nightcore-hat")),
+                };
+        }
 
             protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
             {
                 base.OnNewBeat(beatIndex, timingPoint, effectPoint, amplitudes);
                 int beatsPerBar = (int)timingPoint.TimeSignature;
                 int segmentLength = beatsPerBar * Divisor * 4;
-
                 if (!IsBeatSyncedWithTrack)
                 {
                     firstBeat = null;
                     return;
                 }
-
                 if (!firstBeat.HasValue || beatIndex < firstBeat)
                     // decide on a good starting beat index if once has not yet been decided.
                     firstBeat = beatIndex < 0 ? 0 : (beatIndex / segmentLength + 1) * segmentLength;
 
                 if (beatIndex >= firstBeat)
-                    metronomeSample.Play();
+                    playBeatFor(beatIndex % segmentLength, timingPoint.TimeSignature);
+            }
+            
+            private void playBeatFor(int beatIndex, TimeSignatures signature)
+            {
+                if ( ( beatIndex % ( (int)signature * Divisor )  ) == 0 && specialSampleForFirstBeatOfBar )
+                    firstBeatOfBarSample.Play();
+                else
+                    otherBeatOfBarSample.Play();
+
             }
         }
     }
