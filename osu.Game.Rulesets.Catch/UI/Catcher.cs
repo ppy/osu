fb0ef59b9a08dd4605b4e7cbf596b783b22ff8fc
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-08-21 13:13:08 +0900

    Simplify condition

--- osu.Game.Rulesets.Catch/UI/Catcher.cs
+++ osu.Game.Rulesets.Catch/UI/Catcher.cs
@@ -229,8 +229,8 @@ namespace osu.Game.Rulesets.Catch.UI
                 catchObjectPosition >= catcherPosition - halfCatchWidth &&
                 catchObjectPosition <= catcherPosition + halfCatchWidth;
 
-            // only update hyperdash state if we are catching a fruit or a droplet (and not a tiny droplet).
-            if (!(fruit is Fruit || fruit is Droplet) || fruit is TinyDroplet) return validCatch;
+            // only update hyperdash state if we are catching not catching a tiny droplet.
+            if (fruit is TinyDroplet) return validCatch;
 
             if (validCatch && fruit.HyperDash)
             {
