7e838c80420d35a9c633cfc038fd3afd7ecf728a
Salman Ahmed <email@iisalman.me>
2020-08-22 13:07:15 +0300

    Add comment explaining why direct string lookups are used

--- osu.Game/Tests/Visual/SkinnableTestScene.cs
+++ osu.Game/Tests/Visual/SkinnableTestScene.cs
@@ -160,6 +160,7 @@ namespace osu.Game.Tests.Visual
             {
                 this.extrapolateAnimations = extrapolateAnimations;
 
+                // Use a direct string lookup for simplicity, as they're legacy settings and not worth creating enums for them.
                 legacyFontPrefixes.Add(GetConfig<string, string>("HitCirclePrefix")?.Value ?? "default");
                 legacyFontPrefixes.Add(GetConfig<string, string>("ScorePrefix")?.Value ?? "score");
                 legacyFontPrefixes.Add(GetConfig<string, string>("ComboPrefix")?.Value ?? "score");
