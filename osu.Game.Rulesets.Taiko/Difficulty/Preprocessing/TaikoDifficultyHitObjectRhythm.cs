e20892c2fb8e0410fff35f5b45a68c968f2fd389
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-08-22 21:27:08 +0200

    Fix invalid cref

--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/TaikoDifficultyHitObjectRhythm.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/TaikoDifficultyHitObjectRhythm.cs
@@ -14,7 +14,8 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing
         public readonly double Difficulty;
 
         /// <summary>
-        /// The ratio of current <see cref="TaikoDifficultyHitObject.DeltaTime"/> to previous <see cref="TaikoDifficultyHitObject.DeltaTime"/> for the rhythm change.
+        /// The ratio of current <see cref="osu.Game.Rulesets.Difficulty.Preprocessing.DifficultyHitObject.DeltaTime"/>
+        /// to previous <see cref="osu.Game.Rulesets.Difficulty.Preprocessing.DifficultyHitObject.DeltaTime"/> for the rhythm change.
         /// A <see cref="Ratio"/> above 1 indicates a slow-down; a <see cref="Ratio"/> below 1 indicates a speed-up.
         /// </summary>
         public readonly double Ratio;
