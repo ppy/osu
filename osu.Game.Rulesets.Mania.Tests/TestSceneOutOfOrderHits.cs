8151aa6ed8b761f4b51ceb7345c0c9bb855d7ad1
smoogipoo <smoogipoo@smgi.me>
2020-08-31 13:31:55 +0900

    Remove unused method

--- osu.Game.Rulesets.Mania.Tests/TestSceneOutOfOrderHits.cs
+++ osu.Game.Rulesets.Mania.Tests/TestSceneOutOfOrderHits.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
@@ -58,12 +57,6 @@ namespace osu.Game.Rulesets.Mania.Tests
         {
             AddAssert($"({hitObject.GetType().ReadableName()} @ {hitObject.StartTime}) judgement is {result}",
                 () => judgementResults.Single(r => r.HitObject == hitObject).Type == result);
-        }
-
-        private void addJudgementAssert(string name, Func<ManiaHitObject> hitObject, HitResult result)
-        {
-            AddAssert($"{name} judgement is {result}",
-                () => judgementResults.Single(r => r.HitObject == hitObject()).Type == result);
         }
 
         private void addJudgementOffsetAssert(ManiaHitObject hitObject, double offset)
