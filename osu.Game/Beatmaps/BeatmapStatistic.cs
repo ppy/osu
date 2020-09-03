72cb65c22f55c89677a4bf3a466e082788831a99
Dean Herbert <pe@ppy.sh>
2020-09-03 17:51:54 +0900

    Update and add missing beatmap statistic icons to info wedge

--- osu.Game/Beatmaps/BeatmapStatistic.cs
+++ osu.Game/Beatmaps/BeatmapStatistic.cs
@@ -1,14 +1,30 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 
 namespace osu.Game.Beatmaps
 {
     public class BeatmapStatistic
     {
-        public IconUsage Icon;
+        [Obsolete("Use CreateIcon instead")] // can be removed 20210203
+        public IconUsage Icon = FontAwesome.Regular.QuestionCircle;
+
+        /// <summary>
+        /// A function to create the icon for display purposes.
+        /// </summary>
+        public Func<Drawable> CreateIcon;
+
         public string Content;
         public string Name;
+
+        public BeatmapStatistic()
+        {
+#pragma warning disable 618
+            CreateIcon = () => new SpriteIcon { Icon = Icon };
+#pragma warning restore 618
+        }
     }
 }
