d14cd29a69ec109c9a45be7c1a15bce6243ba19c
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2020-09-03 16:29:25 +0300

    Move adjustment to LegacySkinConfiguration as a default value

--- osu.Game/Skinning/LegacySkinConfiguration.cs
+++ osu.Game/Skinning/LegacySkinConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Globalization;
+
 namespace osu.Game.Skinning
 {
     public class LegacySkinConfiguration : SkinConfiguration
@@ -11,6 +13,13 @@ namespace osu.Game.Skinning
         /// Legacy version of this skin.
         /// </summary>
         public decimal? LegacyVersion { get; internal set; }
+
+        public LegacySkinConfiguration()
+        {
+            // Roughly matches osu!stable's slider border portions.
+            // Can't use nameof(SliderBorderSize) as the lookup enum is declared in the osu! ruleset.
+            ConfigDictionary["SliderBorderSize"] = 0.77f.ToString(CultureInfo.InvariantCulture);
+        }
 
         public enum LegacySetting
         {
