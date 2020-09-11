ee6651fcf6314b42eeab3d906ed5f69a4d7a28bb
smoogipoo <smoogipoo@smgi.me>
2020-09-11 20:17:51 +0900

    Remove unnecessary bonus tick changes

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSpinnerBonusTick.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSpinnerBonusTick.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Game.Rulesets.Scoring;
-
 namespace osu.Game.Rulesets.Osu.Objects.Drawables
 {
     public class DrawableSpinnerBonusTick : DrawableSpinnerTick
@@ -11,7 +9,5 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
             : base(spinnerTick)
         {
         }
-
-        internal override void TriggerResult(bool hit) => ApplyResult(r => r.Type = hit ? r.Judgement.MaxResult : HitResult.Miss);
     }
 }
