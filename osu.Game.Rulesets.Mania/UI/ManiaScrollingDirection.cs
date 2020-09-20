181bd416635016888bcaf8acacf36f240c64782f
MATRIX-feather <midnightcarnival@outlook.com>
2020-09-20 22:39:44 +0800

    一些更改; 翻译补全

--- osu.Game.Rulesets.Mania/UI/ManiaScrollingDirection.cs
+++ osu.Game.Rulesets.Mania/UI/ManiaScrollingDirection.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.ComponentModel;
 using osu.Game.Rulesets.UI.Scrolling;
 
 namespace osu.Game.Rulesets.Mania.UI
 {
     public enum ManiaScrollingDirection
     {
+        [Description("从下往上")]
         Up = ScrollingDirection.Up,
+        [Description("从上往下")]
         Down = ScrollingDirection.Down
     }
 }
