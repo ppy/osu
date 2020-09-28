cbc9e09b4f03f440dc14ab45ecc3958ae9b3cdb0
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-09-28 18:16:19 +0900

    Fix hit samples playing while paused / seeking in the editor

---
+++ osu.Game/Screens/Play/ISeekableClock.cs
@@ -0,0 +1,13 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Screens.Play
+{
+    public interface ISeekableClock
+    {
+        /// <summary>
+        /// Whether an ongoing seek operation is active.
+        /// </summary>
+        bool IsSeeking { get; }
+    }
+}
