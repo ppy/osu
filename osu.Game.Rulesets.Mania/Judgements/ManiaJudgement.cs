8b85514dea818e93f1da0246d9020630d1d5e3f0
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-09-29 14:29:43 +0900

    Use new hit results in mania

--- osu.Game.Rulesets.Mania/Judgements/ManiaJudgement.cs
+++ osu.Game.Rulesets.Mania/Judgements/ManiaJudgement.cs
@@ -2,34 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Rulesets.Judgements;
-using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Mania.Judgements
 {
     public class ManiaJudgement : Judgement
     {
-        protected override int NumericResultFor(HitResult result)
-        {
-            switch (result)
-            {
-                default:
-                    return 0;
-
-                case HitResult.Meh:
-                    return 50;
-
-                case HitResult.Ok:
-                    return 100;
-
-                case HitResult.Good:
-                    return 200;
-
-                case HitResult.Great:
-                    return 300;
-
-                case HitResult.Perfect:
-                    return 350;
-            }
-        }
     }
 }
