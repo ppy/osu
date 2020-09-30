ae9e884a483fd5940a429a4b924c5d1cb059653e
Dean Herbert <pe@ppy.sh>
2020-09-30 15:35:25 +0900

    Fix header casing

--- osu.Game/Screens/Edit/Compose/Components/SelectionHandler.cs
+++ osu.Game/Screens/Edit/Compose/Components/SelectionHandler.cs
@@ -1,4 +1,4 @@
-﻿// copyright (c) ppy pty ltd <contact@ppy.sh>. licensed under the mit licence.
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
