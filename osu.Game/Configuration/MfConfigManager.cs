ac08b1179dc3c549ea98a2aa8b7da0e771204f5c
MATRIX-feather <midnightcarnival@outlook.com>
2020-10-03 15:01:28 +0800

    将mvis故事版功能默认设置为开启

--- osu.Game/Configuration/MfConfigManager.cs
+++ osu.Game/Configuration/MfConfigManager.cs
@@ -38,7 +38,7 @@ namespace osu.Game.Configuration
             Set(MfSetting.MvisParticleAmount, 350, 0, 350);
             Set(MfSetting.MvisContentAlpha, 1f, 0f, 1f);
             Set(MfSetting.MvisBgBlur, 0.2f, 0f, 1f);
-            Set(MfSetting.MvisEnableStoryboard, false);
+            Set(MfSetting.MvisEnableStoryboard, true);
             Set(MfSetting.MvisUseOsuLogoVisualisation, false);
             Set(MfSetting.MvisIdleBgDim, 0.6f, 0f, 1f);
             Set(MfSetting.MvisEnableBgTriangles, true);
