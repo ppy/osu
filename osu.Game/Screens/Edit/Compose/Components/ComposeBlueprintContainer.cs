4dfd69c4fd35368632241f0df2f1920fe8a0ee97
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-10-09 03:31:01 +0900

    Reset new combo button state after successful placement

--- osu.Game/Screens/Edit/Compose/Components/ComposeBlueprintContainer.cs
+++ osu.Game/Screens/Edit/Compose/Components/ComposeBlueprintContainer.cs
@@ -201,7 +201,12 @@ namespace osu.Game.Screens.Edit.Compose.Components
         protected override void AddBlueprintFor(HitObject hitObject)
         {
             refreshTool();
+
             base.AddBlueprintFor(hitObject);
+
+            // on successful placement, the new combo button should be reset as this is the most common user interaction.
+            if (Beatmap.SelectedHitObjects.Count == 0)
+                NewCombo.Value = TernaryState.False;
         }
 
         private void createPlacement()
