505d98603cd44e9f0764589484d37961ed7b7bce
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-10-09 13:11:44 +0900

    Set beatmap to sane default on exiting editor

--- osu.Game/Screens/Edit/Editor.cs
+++ osu.Game/Screens/Edit/Editor.cs
@@ -469,10 +469,17 @@ namespace osu.Game.Screens.Edit
 
         private void confirmExit()
         {
+            // stop the track if playing to allow the parent screen to choose a suitable playback mode.
+            Beatmap.Value.Track.Stop();
+
             if (isNewBeatmap)
             {
                 // confirming exit without save means we should delete the new beatmap completely.
                 beatmapManager.Delete(playableBeatmap.BeatmapInfo.BeatmapSet);
+
+                // in theory this shouldn't be required but due to EF core not sharing instance states 100%
+                // MusicController is unaware of the changed DeletePending state.
+                Beatmap.SetDefault();
             }
 
             exitConfirmed = true;
