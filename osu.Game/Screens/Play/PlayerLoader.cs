6e4b28ed1e69cdb3aaa574b035ce0dd040d82933
Salman Ahmed <email@iisalman.me>
2020-10-20 00:32:44 +0300

    Different version of epilepsy warning display

--- osu.Game/Screens/Play/PlayerLoader.cs
+++ osu.Game/Screens/Play/PlayerLoader.cs
@@ -6,16 +6,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input;
 using osu.Framework.Screens;
 using osu.Framework.Threading;
+using osu.Game.Beatmaps;
 using osu.Game.Configuration;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Input;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Notifications;
@@ -83,6 +88,8 @@ namespace osu.Game.Screens.Play
         private LogoTrackingContainer content;
 
         private bool hideOverlays;
+
+        private bool epilepsyShown;
 
         private InputManager inputManager;
 
@@ -308,7 +315,27 @@ namespace osu.Game.Screens.Play
                 {
                     contentOut();
 
-                    this.Delay(250).Schedule(() =>
+                    if (true && !epilepsyShown)
+                    {
+                        EpilepsyWarning warning;
+
+                        AddInternal(warning = new EpilepsyWarning
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            State = { Value = Visibility.Visible }
+                        });
+
+                        epilepsyShown = true;
+
+                        this.Delay(2000).Schedule(() =>
+                        {
+                            warning.Hide();
+                            warning.Expire();
+                        });
+                    }
+
+                    this.Delay(epilepsyShown ? 2500 : 250).Schedule(() =>
                     {
                         if (!this.IsCurrentScreen()) return;
 
@@ -398,5 +425,90 @@ namespace osu.Game.Screens.Play
         }
 
         #endregion
+
+        private class EpilepsyWarning : VisibilityContainer
+        {
+            private readonly BindableDouble trackVolumeOnEpilepsyWarning = new BindableDouble(1f);
+
+            private Track track;
+            private FillFlowContainer warningContent;
+
+            public EpilepsyWarning()
+            {
+                RelativeSizeAxes = Axes.Both;
+                Alpha = 0f;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours, IBindable<WorkingBeatmap> beatmap)
+            {
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4.Black.Opacity(0.5f),
+                    },
+                    warningContent = new FillFlowContainer
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+                        Direction = FillDirection.Vertical,
+                        Children = new Drawable[]
+                        {
+                            new SpriteIcon
+                            {
+                                Colour = colours.Yellow,
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Icon = FontAwesome.Solid.ExclamationTriangle,
+                                Size = new Vector2(50),
+                            },
+                            new OsuTextFlowContainer(s => s.Font = OsuFont.GetFont(size: 25))
+                            {
+                                RelativeSizeAxes = Axes.X,
+                                AutoSizeAxes = Axes.Y,
+                                TextAnchor = Anchor.Centre,
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                            }.With(tfc =>
+                            {
+                                tfc.AddText("This beatmap contains scenes with ");
+                                tfc.AddText("rapidly flashing colours", s =>
+                                {
+                                    s.Font = s.Font.With(weight: FontWeight.Bold);
+                                    s.Colour = colours.Yellow;
+                                });
+                                tfc.AddText(".");
+
+                                tfc.NewParagraph();
+                                tfc.AddText("Please take caution if you are affected by epilepsy.");
+                            }),
+                        }
+                    }
+                };
+
+                track = beatmap.Value.Track;
+                track.AddAdjustment(AdjustableProperty.Volume, trackVolumeOnEpilepsyWarning);
+            }
+
+            protected override void PopIn()
+            {
+                this.FadeIn(500, Easing.InQuint)
+                    .TransformBindableTo(trackVolumeOnEpilepsyWarning, 0.25, 500, Easing.InQuint);
+
+                warningContent.FadeIn(500, Easing.InQuint);
+            }
+
+            protected override void PopOut() => this.FadeOut(500, Easing.InQuint);
+
+            protected override void Dispose(bool isDisposing)
+            {
+                base.Dispose(isDisposing);
+                track?.RemoveAdjustment(AdjustableProperty.Volume, trackVolumeOnEpilepsyWarning);
+            }
+        }
     }
 }
