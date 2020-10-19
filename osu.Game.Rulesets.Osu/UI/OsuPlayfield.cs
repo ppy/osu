4267d23d598f2635df363b5b42019c91ddc72ff0
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-10-19 20:56:34 +0200

    Move border to more appropriate namespace

--- osu.Game.Rulesets.Osu/UI/OsuPlayfield.cs
+++ osu.Game.Rulesets.Osu/UI/OsuPlayfield.cs
@@ -20,7 +20,6 @@ using osu.Game.Skinning;
 using osu.Framework.Allocation;
 using osu.Game.Configuration;
 using osu.Framework.Bindables;
-using osu.Game.Screens;
 using osuTK;
 
 namespace osu.Game.Rulesets.Osu.UI
