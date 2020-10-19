bffb64b457be35f5a46a370c9694f53e58bd5216
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-10-20 01:06:20 +0200

    Fade volume back up on pop out

--- osu.Game/Screens/Play/EpilepsyWarning.cs
+++ osu.Game/Screens/Play/EpilepsyWarning.cs
@@ -89,12 +89,14 @@ namespace osu.Game.Screens.Play
         protected override void PopIn()
         {
             this.FadeIn(500, Easing.InQuint)
-                .TransformBindableTo(trackVolumeOnEpilepsyWarning, 0.25, 500, Easing.InSine);
+                .TransformBindableTo(trackVolumeOnEpilepsyWarning, 0.25, 500, Easing.InQuint);
 
             warningContent.FadeIn(500, Easing.InQuint);
         }
 
-        protected override void PopOut() => this.FadeOut(500, Easing.OutQuint);
+        protected override void PopOut()
+            => this.FadeOut(500, Easing.OutQuint)
+                   .TransformBindableTo(trackVolumeOnEpilepsyWarning, 1, 500, Easing.OutQuint);
 
         protected override void Dispose(bool isDisposing)
         {
