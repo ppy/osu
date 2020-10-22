c752b86b8a6b1b99b9b5dc11fc32e139392647a2
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-10-22 13:12:58 +0900

    Add xmldoc to spectator interfaces

--- osu.Game/Online/Spectator/ISpectatorClient.cs
+++ osu.Game/Online/Spectator/ISpectatorClient.cs
@@ -1,14 +1,31 @@
 using System.Threading.Tasks;
-using osu.Game.Online.Spectator;
 
-namespace osu.Server.Spectator.Hubs
+namespace osu.Game.Online.Spectator
 {
+    /// <summary>
+    /// An interface defining a spectator client instance.
+    /// </summary>
     public interface ISpectatorClient
     {
+        /// <summary>
+        /// Signals that a user has begun a new play session.
+        /// </summary>
+        /// <param name="userId">The user.</param>
+        /// <param name="beatmapId">The beatmap the user is playing.</param>
         Task UserBeganPlaying(string userId, int beatmapId);
 
+        /// <summary>
+        /// Signals that a user has finished a play session.
+        /// </summary>
+        /// <param name="userId">The user.</param>
+        /// <param name="beatmapId">The beatmap the user has finished playing.</param>
         Task UserFinishedPlaying(string userId, int beatmapId);
 
+        /// <summary>
+        /// Called when new frames are available for a subscribed user's play session.
+        /// </summary>
+        /// <param name="userId">The user.</param>
+        /// <param name="data">The frame data.</param>
         Task UserSentFrames(string userId, FrameDataBundle data);
     }
 }
