82a27c73a0077412ee1519c63c0b6d9323810e35
Dean Herbert <pe@ppy.sh>
2020-10-26 21:17:12 +0900

    Create basic testing setup

--- osu.Game.Tests/Visual/Gameplay/TestSceneSpectator.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSpectator.cs
@@ -2,6 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Game.Online.Spectator;
+using osu.Game.Replays.Legacy;
 using osu.Game.Screens.Play;
 using osu.Game.Users;
 
@@ -9,12 +12,34 @@ namespace osu.Game.Tests.Visual.Gameplay
 {
     public class TestSceneSpectator : ScreenTestScene
     {
-        private readonly User user = new User { Id = 1234, Username = "Test user" };
+        [Cached(typeof(SpectatorStreamingClient))]
+        private TestSpectatorStreamingClient testSpectatorStreamingClient = new TestSpectatorStreamingClient();
 
         [Test]
         public void TestSpectating()
         {
-            AddStep("load screen", () => LoadScreen(new Spectator(user)));
+            AddStep("load screen", () => LoadScreen(new Spectator(testSpectatorStreamingClient.StreamingUser)));
+            AddStep("start play", () => testSpectatorStreamingClient.StartPlay());
+            AddStep("send frames", () => testSpectatorStreamingClient.SendFrames());
+        }
+
+        internal class TestSpectatorStreamingClient : SpectatorStreamingClient
+        {
+            public readonly User StreamingUser = new User { Id = 1234, Username = "Test user" };
+
+            public void StartPlay()
+            {
+                ((ISpectatorClient)this).UserBeganPlaying((int)StreamingUser.Id, new SpectatorState());
+            }
+
+            public void SendFrames()
+            {
+                ((ISpectatorClient)this).UserSentFrames((int)StreamingUser.Id, new FrameDataBundle(new[]
+                {
+                    // todo: populate more frames
+                    new LegacyReplayFrame(0, 0, 0, ReplayButtonState.Left1)
+                }));
+            }
         }
     }
 }
