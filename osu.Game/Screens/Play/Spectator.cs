fbf1baedc1962da7105d6af267c5071da23d22e9
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-10-27 19:23:35 +0900

    Add basic beatmap download and play flow

--- osu.Game/Screens/Play/Spectator.cs
+++ osu.Game/Screens/Play/Spectator.cs
@@ -8,10 +8,15 @@ using JetBrains.Annotations;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Screens;
 using osu.Game.Beatmaps;
+using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
+using osu.Game.Online.API;
+using osu.Game.Online.API.Requests;
 using osu.Game.Online.Spectator;
+using osu.Game.Overlays.BeatmapListing.Panels;
 using osu.Game.Replays;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Mods;
@@ -19,6 +24,7 @@ using osu.Game.Rulesets.Replays;
 using osu.Game.Rulesets.Replays.Types;
 using osu.Game.Scoring;
 using osu.Game.Users;
+using osuTK;
 
 namespace osu.Game.Screens.Play
 {
@@ -36,6 +42,9 @@ namespace osu.Game.Screens.Play
         private Bindable<IReadOnlyList<Mod>> mods { get; set; }
 
         [Resolved]
+        private IAPIProvider api { get; set; }
+
+        [Resolved]
         private SpectatorStreamingClient spectatorStreaming { get; set; }
 
         [Resolved]
@@ -45,6 +54,12 @@ namespace osu.Game.Screens.Play
         private RulesetStore rulesets { get; set; }
 
         private Replay replay;
+
+        private Container beatmapPanelContainer;
+
+        private SpectatorState state;
+
+        private IBindable<WeakReference<BeatmapSetInfo>> managerUpdated;
 
         public Spectator([NotNull] User targetUser)
         {
@@ -56,11 +71,42 @@ namespace osu.Game.Screens.Play
         {
             InternalChildren = new Drawable[]
             {
-                new OsuSpriteText
+                new FillFlowContainer
                 {
-                    Text = $"Watching {targetUser}",
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
+                    Spacing = new Vector2(15),
+                    Children = new Drawable[]
+                    {
+                        new OsuSpriteText
+                        {
+                            Text = "Currently spectating",
+                            Font = OsuFont.Default.With(size: 30),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        },
+                        new UserGridPanel(targetUser)
+                        {
+                            Width = 290,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        },
+                        new OsuSpriteText
+                        {
+                            Text = "playing",
+                            Font = OsuFont.Default.With(size: 30),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        },
+                        beatmapPanelContainer = new Container
+                        {
+                            AutoSizeAxes = Axes.Both,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        },
+                    }
                 },
             };
         }
@@ -74,6 +120,15 @@ namespace osu.Game.Screens.Play
             spectatorStreaming.OnNewFrames += userSentFrames;
 
             spectatorStreaming.WatchUser((int)targetUser.Id);
+
+            managerUpdated = beatmaps.ItemUpdated.GetBoundCopy();
+            managerUpdated.BindValueChanged(beatmapUpdated);
+        }
+
+        private void beatmapUpdated(ValueChangedEvent<WeakReference<BeatmapSetInfo>> beatmap)
+        {
+            if (beatmap.NewValue.TryGetTarget(out var beatmapSet) && beatmapSet.Beatmaps.Any(b => b.OnlineBeatmapID == state.BeatmapID))
+                attemptStart();
         }
 
         private void userSentFrames(int userId, FrameDataBundle data)
@@ -107,16 +162,31 @@ namespace osu.Game.Screens.Play
 
             replay ??= new Replay { HasReceivedAllFrames = false };
 
+            this.state = state;
+
+            attemptStart();
+        }
+
+        private void attemptStart()
+        {
             var resolvedRuleset = rulesets.AvailableRulesets.FirstOrDefault(r => r.ID == state.RulesetID)?.CreateInstance();
 
             // ruleset not available
             if (resolvedRuleset == null)
                 return;
 
+            if (state.BeatmapID == null)
+                return;
+
+            this.MakeCurrent();
+
             var resolvedBeatmap = beatmaps.QueryBeatmap(b => b.OnlineBeatmapID == state.BeatmapID);
 
             if (resolvedBeatmap == null)
+            {
+                showBeatmapPanel(state.BeatmapID.Value);
                 return;
+            }
 
             var scoreInfo = new ScoreInfo
             {
@@ -124,8 +194,6 @@ namespace osu.Game.Screens.Play
                 Mods = state.Mods.Select(m => m.ToMod(resolvedRuleset)).ToArray(),
                 Ruleset = resolvedRuleset.RulesetInfo,
             };
-
-            this.MakeCurrent();
 
             ruleset.Value = resolvedRuleset.RulesetInfo;
             beatmap.Value = beatmaps.GetWorkingBeatmap(resolvedBeatmap);
@@ -135,6 +203,17 @@ namespace osu.Game.Screens.Play
                 ScoreInfo = scoreInfo,
                 Replay = replay,
             }));
+        }
+
+        private void showBeatmapPanel(int beatmapId)
+        {
+            var req = new GetBeatmapSetRequest(beatmapId, BeatmapSetLookupType.BeatmapId);
+            req.Success += res => Schedule(() =>
+            {
+                beatmapPanelContainer.Child = new GridBeatmapPanel(res.ToBeatmapSet(rulesets));
+            });
+
+            api.Queue(req);
         }
 
         private void userFinishedPlaying(int userId, SpectatorState state)
@@ -156,6 +235,8 @@ namespace osu.Game.Screens.Play
 
                 spectatorStreaming.StopWatchingUser((int)targetUser.Id);
             }
+
+            managerUpdated.UnbindAll();
         }
     }
 }
