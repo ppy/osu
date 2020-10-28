0a652e69df34ed59edaccfcacff8a385043d0870
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-10-28 18:05:15 +0900

    Allow continuing to automatically spectate user from results screen

--- osu.Game/Screens/Play/SpectatorResultsScreen.cs
+++ osu.Game/Screens/Play/SpectatorResultsScreen.cs
@@ -1,26 +1,19 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Screens;
-using osu.Game.Beatmaps;
 using osu.Game.Online.Spectator;
 using osu.Game.Scoring;
 using osu.Game.Screens.Ranking;
 
 namespace osu.Game.Screens.Play
 {
-    public class SpectatorPlayer : ReplayPlayer
+    public class SpectatorResultsScreen : SoloResultsScreen
     {
-        public SpectatorPlayer(Score score)
+        public SpectatorResultsScreen(ScoreInfo score)
             : base(score)
         {
-        }
-
-        protected override ResultsScreen CreateResults(ScoreInfo score)
-        {
-            return new SpectatorResultsScreen(score);
         }
 
         [Resolved]
@@ -34,7 +27,7 @@ namespace osu.Game.Screens.Play
 
         private void userBeganPlaying(int userId, SpectatorState state)
         {
-            if (userId == Score.ScoreInfo.UserID)
+            if (userId == Score.UserID)
                 Schedule(this.Exit);
         }
 
@@ -44,17 +37,6 @@ namespace osu.Game.Screens.Play
 
             if (spectatorStreaming != null)
                 spectatorStreaming.OnUserBeganPlaying -= userBeganPlaying;
-        }
-
-        protected override GameplayClockContainer CreateGameplayClockContainer(WorkingBeatmap beatmap, double gameplayStart)
-        {
-            // if we already have frames, start gameplay at the point in time they exist, should they be too far into the beatmap.
-            double? firstFrameTime = Score.Replay.Frames.FirstOrDefault()?.Time;
-
-            if (firstFrameTime == null || firstFrameTime <= gameplayStart + 5000)
-                return base.CreateGameplayClockContainer(beatmap, gameplayStart);
-
-            return new GameplayClockContainer(beatmap, firstFrameTime.Value, true);
         }
     }
 }
