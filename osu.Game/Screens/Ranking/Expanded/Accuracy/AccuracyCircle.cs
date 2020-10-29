f1b8a8f7f56337edd8693ea2cb8bf57c6c6bd5ff
Dean Herbert <pe@ppy.sh>
2020-10-29 18:16:04 +0900

    Remove unused using

--- osu.Game/Screens/Ranking/Expanded/Accuracy/AccuracyCircle.cs
+++ osu.Game/Screens/Ranking/Expanded/Accuracy/AccuracyCircle.cs
@@ -5,7 +5,6 @@ using System;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
-using osu.Framework.Audio.Sample;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
