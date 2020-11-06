893979b3deab4b95cc62814c18ba2db2fa3a9d87
Dean Herbert <pe@ppy.sh>
2020-11-06 13:10:18 +0900

    Add exception if attempting to exceed the maximum supported lookup size for one request

--- osu.Game/Online/API/Requests/GetUsersRequest.cs
+++ osu.Game/Online/API/Requests/GetUsersRequest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
 
 namespace osu.Game.Online.API.Requests
@@ -9,8 +10,13 @@ namespace osu.Game.Online.API.Requests
     {
         private readonly int[] userIds;
 
+        private const int max_ids_per_request = 50;
+
         public GetUsersRequest(int[] userIds)
         {
+            if (userIds.Length > max_ids_per_request)
+                throw new ArgumentException($"{nameof(GetUsersRequest)} calls only support up to {max_ids_per_request} IDs at once");
+
             this.userIds = userIds;
         }
 
