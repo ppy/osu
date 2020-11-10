c4562e6d7d6e10356e5b639ea6ff6f081c49f909
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-11-10 13:35:49 +0100

    Reintroduce KiaiHitExplosion as skinnable

---
+++ osu.Game.Rulesets.Taiko/UI/KiaiHitExplosion.cs
@@ -0,0 +1,47 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Taiko.Objects;
+using osu.Game.Skinning;
+using osuTK;
+
+namespace osu.Game.Rulesets.Taiko.UI
+{
+    public class KiaiHitExplosion : Container
+    {
+        public override bool RemoveWhenNotAlive => true;
+
+        [Cached(typeof(DrawableHitObject))]
+        public readonly DrawableHitObject JudgedObject;
+
+        private readonly HitType hitType;
+
+        private SkinnableDrawable skinnable;
+
+        public override double LifetimeStart => skinnable.Drawable.LifetimeStart;
+
+        public override double LifetimeEnd => skinnable.Drawable.LifetimeEnd;
+
+        public KiaiHitExplosion(DrawableHitObject judgedObject, HitType hitType)
+        {
+            JudgedObject = judgedObject;
+            this.hitType = hitType;
+
+            Anchor = Anchor.CentreLeft;
+            Origin = Anchor.Centre;
+
+            RelativeSizeAxes = Axes.Both;
+            Size = new Vector2(TaikoHitObject.DEFAULT_SIZE, 1);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Child = skinnable = new SkinnableDrawable(new TaikoSkinComponent(TaikoSkinComponents.TaikoExplosionKiai), _ => new DefaultKiaiHitExplosion(hitType));
+        }
+    }
+}
