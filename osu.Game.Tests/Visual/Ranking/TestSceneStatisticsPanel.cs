85017a009428ecf9a3ee881a28b209ea1e07b207
kamp <nbvdkamp@gmail.com>
2020-11-11 20:20:29 +0100

    Add test for accuracy heatmap to TestCaseStatisticsPanel

--- osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
@@ -1,23 +1,39 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Scoring;
 using osu.Game.Screens.Ranking.Statistics;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Scoring;
+using osuTK;
 
 namespace osu.Game.Tests.Visual.Ranking
 {
     public class TestSceneStatisticsPanel : OsuTestScene
     {
         [Test]
-        public void TestScoreWithStatistics()
+        public void TestScoreWithTimeStatistics()
         {
             var score = new TestScoreInfo(new OsuRuleset().RulesetInfo)
             {
                 HitEvents = TestSceneHitEventTimingDistributionGraph.CreateDistributedHitEvents()
+            };
+
+            loadPanel(score);
+        }
+
+        [Test]
+        public void TestScoreWithPositionStatistics()
+        {
+            var score = new TestScoreInfo(new OsuRuleset().RulesetInfo)
+            {
+                HitEvents = CreatePositionDistributedHitEvents()
             };
 
             loadPanel(score);
@@ -44,5 +60,24 @@ namespace osu.Game.Tests.Visual.Ranking
                 Score = { Value = score }
             };
         });
+
+        public static List<HitEvent> CreatePositionDistributedHitEvents()
+        {
+            var hitEvents = new List<HitEvent>();
+            // Use constant seed for reproducibility
+            var random = new Random(0);
+
+            for (int i = 0; i < 500; i++)
+            {
+                float angle = (float) random.NextDouble() * 2 * (float) Math.PI;
+                float radius = (float) random.NextDouble() * 0.5f * HitCircle.OBJECT_RADIUS;
+
+                Vector2 position = new Vector2(radius * (float) Math.Cos(angle), radius * (float) Math.Sin(angle));
+
+                hitEvents.Add(new HitEvent(0, HitResult.Perfect, new HitCircle(), new HitCircle(), position));
+            }
+
+            return hitEvents;
+        }
     }
 }
