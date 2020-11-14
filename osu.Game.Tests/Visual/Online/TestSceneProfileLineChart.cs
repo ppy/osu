9a8cb030376f919723ba9dc852287c19eeb695cf
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2020-11-14 04:46:26 +0300

    ProfileLineChart layout implementation

---
+++ osu.Game.Tests/Visual/Online/TestSceneProfileLineChart.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Overlays.Profile.Sections.Historical;
+using osu.Framework.Graphics;
+using static osu.Game.Users.User;
+using System;
+using osu.Game.Overlays;
+using osu.Framework.Allocation;
+
+namespace osu.Game.Tests.Visual.Online
+{
+    public class TestSceneProfileLineChart : OsuTestScene
+    {
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Pink);
+
+        public TestSceneProfileLineChart()
+        {
+            var values = new[]
+            {
+                new UserHistoryCount { Date = new DateTime(2010, 5, 1), Count = 1000 },
+                new UserHistoryCount { Date = new DateTime(2010, 6, 1), Count = 20 },
+                new UserHistoryCount { Date = new DateTime(2010, 7, 1), Count = 20000 },
+                new UserHistoryCount { Date = new DateTime(2010, 8, 1), Count = 30 },
+                new UserHistoryCount { Date = new DateTime(2010, 9, 1), Count = 50 },
+                new UserHistoryCount { Date = new DateTime(2010, 10, 1), Count = 2000 },
+                new UserHistoryCount { Date = new DateTime(2010, 11, 1), Count = 2100 }
+            };
+
+            Add(new ProfileLineChart
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Values = values
+            });
+        }
+    }
+}
