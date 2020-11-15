cf7ac6d5e38aa036cbea9b14fc618f607dd29de1
PercyDan54 <50285552+PercyDan54@users.noreply.github.com>
2020-11-15 13:38:21 +0800

    Remove import

--- osu.Game.Rulesets.Taiko/Mods/TaikoModEasy.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModEasy.cs
@@ -2,11 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using Humanizer;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
-using osu.Game.Configuration;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mods;
 
