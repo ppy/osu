7e93b586937679183a3fb2e3d210e57deb06e753
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-11-18 15:51:09 +0900

    Handle DrawableJudgement lifetime more flexibly

--- osu.Game/Rulesets/Judgements/IAnimatableJudgement.cs
+++ osu.Game/Rulesets/Judgements/IAnimatableJudgement.cs
@@ -1,12 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+
 namespace osu.Game.Rulesets.Judgements
 {
     /// <summary>
     /// A skinnable judgement element which supports playing an animation from the current point in time.
     /// </summary>
-    public interface IAnimatableJudgement
+    public interface IAnimatableJudgement : IDrawable
     {
         void PlayAnimation();
     }
