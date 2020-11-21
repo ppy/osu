99a95790c3ac901b83d34c67f3dc485920437b5d
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-11-21 14:36:59 +0100

    Resolve test failure

--- osu.Game.Tests/Visual/Ranking/TestSceneResultsScreen.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneResultsScreen.cs
@@ -326,7 +326,7 @@ namespace osu.Game.Tests.Visual.Ranking
             public HotkeyRetryOverlay RetryOverlay;
 
             public UnrankedSoloResultsScreen(ScoreInfo score)
-                : base(score, false)
+                : base(score, true)
             {
                 Score.Beatmap.OnlineBeatmapID = 0;
                 Score.Beatmap.Status = BeatmapSetOnlineStatus.Pending;
