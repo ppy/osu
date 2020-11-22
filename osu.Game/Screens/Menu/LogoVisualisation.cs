51591172aaaf5a324cc783a745d996ea5cdf3539
Derrick Timmermans <87902114+bubb13gummy@users.noreply.github.com>
2020-11-22 18:59:16 +0100

    Use 0.2f instead of .2f

--- osu.Game/Screens/Menu/LogoVisualisation.cs
+++ osu.Game/Screens/Menu/LogoVisualisation.cs
@@ -174,7 +174,7 @@ namespace osu.Game.Screens.Menu
             // Assuming the logo is a circle, we don't need a second dimension.
             private float size;
 
-            private readonly Color4 colour = Color4.White.Opacity(.2f);
+            private readonly Color4 colour = Color4.White.Opacity(0.2f);
 
             private float[] audioData;
 
