5af3beb35e79a254cce25bdff308e7e021c589ef
ekrctb <87902114+bubb13gummy@users.noreply.github.com>
2020-11-27 11:56:57 +0900

    Fix tiny droplet scale factor

--- osu.Game.Rulesets.Catch/Objects/Drawables/DrawableTinyDroplet.cs
+++ osu.Game.Rulesets.Catch/Objects/Drawables/DrawableTinyDroplet.cs
@@ -1,21 +1,15 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-
 namespace osu.Game.Rulesets.Catch.Objects.Drawables
 {
     public class DrawableTinyDroplet : DrawableDroplet
     {
+        protected override float ScaleFactor => base.ScaleFactor / 2;
+
         public DrawableTinyDroplet(TinyDroplet h)
             : base(h)
         {
-        }
-
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            ScaleContainer.Scale /= 2;
         }
     }
 }
