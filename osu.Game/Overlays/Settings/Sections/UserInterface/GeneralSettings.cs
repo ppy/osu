717ad47ba4bab52a80d4c33963151aa4e3e5106f
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-11-29 22:03:56 +0100

    Move hold-to-confirm setting back to gameplay section

--- osu.Game/Overlays/Settings/Sections/UserInterface/GeneralSettings.cs
+++ osu.Game/Overlays/Settings/Sections/UserInterface/GeneralSettings.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Configuration;
-using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Overlays.Settings.Sections.UserInterface
 {
@@ -27,18 +26,7 @@ namespace osu.Game.Overlays.Settings.Sections.UserInterface
                     LabelText = "Parallax",
                     Current = config.GetBindable<bool>(OsuSetting.MenuParallax)
                 },
-                new SettingsSlider<float, TimeSlider>
-                {
-                    LabelText = "Hold-to-confirm activation time",
-                    Current = config.GetBindable<float>(OsuSetting.UIHoldActivationDelay),
-                    KeyboardStep = 50
-                },
             };
-        }
-
-        private class TimeSlider : OsuSliderBar<float>
-        {
-            public override string TooltipText => Current.Value.ToString("N0") + "ms";
         }
     }
 }
