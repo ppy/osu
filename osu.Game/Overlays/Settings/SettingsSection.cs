b3fcccb19ce1f0823316592a241a3b82b4361e67
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-11-30 17:57:25 +0900

    Remove unnecessary usings #2

--- osu.Game/Overlays/Settings/SettingsSection.cs
+++ osu.Game/Overlays/Settings/SettingsSection.cs
@@ -1,16 +1,15 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osuTK;
-using osuTK.Graphics;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
-using System.Collections.Generic;
-using System.Linq;
+using osuTK.Graphics;
 
 namespace osu.Game.Overlays.Settings
 {
