3ad2eeaff551b279e5db2b33ac0d0a0087544d1f
Dean Herbert <pe@ppy.sh>
2020-11-30 18:35:30 +0900

    Fix outdated xmldoc

--- osu.Game/Rulesets/Edit/PlacementBlueprint.cs
+++ osu.Game/Rulesets/Edit/PlacementBlueprint.cs
@@ -85,7 +85,7 @@ namespace osu.Game.Rulesets.Edit
         }
 
         /// <summary>
-        /// Updates the position of this <see cref="PlacementBlueprint"/> to a new screen-space position.
+        /// Updates the time and position of this <see cref="PlacementBlueprint"/> based on the provided snap information.
         /// </summary>
         /// <param name="result">The snap result information.</param>
         public virtual void UpdateTimeAndPosition(SnapResult result)
