b237da14d6635b133325eeb6d8b5ade62aace990
Ryan Zmuda <87902114+bubb13gummy@users.noreply.github.com>
2020-11-30 21:38:16 -0500

    Moved enum to bottom, change defualt bind to Shift-Tab, Fixed Notification

--- osu.Game/Input/Bindings/GlobalActionContainer.cs
+++ osu.Game/Input/Bindings/GlobalActionContainer.cs
@@ -68,7 +68,7 @@ namespace osu.Game.Input.Bindings
             new KeyBinding(new[] { InputKey.Control, InputKey.Tilde }, GlobalAction.QuickExit),
             new KeyBinding(new[] { InputKey.Control, InputKey.Plus }, GlobalAction.IncreaseScrollSpeed),
             new KeyBinding(new[] { InputKey.Control, InputKey.Minus }, GlobalAction.DecreaseScrollSpeed),
-            new KeyBinding(InputKey.I, GlobalAction.ToggleInGameInterface),
+            new KeyBinding(new[] { InputKey.Shift, InputKey.Tab }, GlobalAction.ToggleInGameInterface),
             new KeyBinding(InputKey.MouseMiddle, GlobalAction.PauseGameplay),
             new KeyBinding(InputKey.Space, GlobalAction.TogglePauseReplay),
             new KeyBinding(InputKey.Control, GlobalAction.HoldForHUD),
@@ -146,9 +146,6 @@ namespace osu.Game.Input.Bindings
         [Description("Decrease scroll speed")]
         DecreaseScrollSpeed,
 
-        [Description("Toggle in-game interface")]
-        ToggleInGameInterface,
-
         [Description("Select")]
         Select,
 
@@ -204,5 +201,8 @@ namespace osu.Game.Input.Bindings
 
         [Description("Pause / resume replay")]
         TogglePauseReplay,
+
+        [Description("Toggle in-game interface")]
+        ToggleInGameInterface,
     }
 }
