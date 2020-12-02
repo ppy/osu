a4208639af058476d2e85aa558ac4a986d73416a
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-12-02 20:01:56 +0100

    Remove unused using directive

--- osu.Game/Overlays/Settings/Sections/Input/MouseSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Input/MouseSettings.cs
@@ -6,7 +6,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
-using osu.Framework.Input.Handlers.Mouse;
 using osu.Game.Configuration;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Input;
