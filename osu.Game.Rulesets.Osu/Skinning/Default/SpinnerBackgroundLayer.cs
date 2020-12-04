0ed9989a8203bde71733773bb43e79399b28cd03
ekrctb <ekrctb@gmail.com>
2020-12-04 20:25:49 +0900

    Adjust namespace

--- osu.Game.Rulesets.Osu/Skinning/Default/SpinnerBackgroundLayer.cs
+++ osu.Game.Rulesets.Osu/Skinning/Default/SpinnerBackgroundLayer.cs
@@ -5,9 +5,8 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Rulesets.Osu.Skinning.Default;
 
-namespace osu.Game.Rulesets.Osu.Objects.Drawables
+namespace osu.Game.Rulesets.Osu.Skinning.Default
 {
     public class SpinnerBackgroundLayer : SpinnerFill
     {
--- osu.Game.Rulesets.Osu/Skinning/Default/SpinnerCentreLayer.cs
+++ osu.Game.Rulesets.Osu/Skinning/Default/SpinnerCentreLayer.cs
@@ -9,11 +9,11 @@ using osu.Framework.Graphics.Sprites;
 using osu.Framework.Utils;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Rulesets.Osu.Skinning.Default;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osuTK;
 using osuTK.Graphics;
 
-namespace osu.Game.Rulesets.Osu.Objects.Drawables
+namespace osu.Game.Rulesets.Osu.Skinning.Default
 {
     public class SpinnerCentreLayer : CompositeDrawable, IHasAccentColour
     {
