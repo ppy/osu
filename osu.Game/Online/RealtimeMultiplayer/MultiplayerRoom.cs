3d39e3417bdf2e223c1d4f9287a10dbf581885e1
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-12-08 15:54:52 +0900

    Add xmldoc coverage of MultiplayerRoom

--- osu.Game/Online/RealtimeMultiplayer/MultiplayerRoom.cs
+++ osu.Game/Online/RealtimeMultiplayer/MultiplayerRoom.cs
@@ -9,14 +9,28 @@ namespace osu.Game.Online.RealtimeMultiplayer
     [Serializable]
     public class MultiplayerRoom
     {
+        /// <summary>
+        /// The ID of the room, used for database persistence.
+        /// </summary>
         public long RoomID { get; set; }
 
+        /// <summary>
+        /// The current state of the room (ie. whether it is in progress or otherwise).
+        /// </summary>
         public MultiplayerRoomState State { get; set; }
 
+        /// <summary>
+        /// All currently enforced game settings for this room.
+        /// </summary>
         public MultiplayerRoomSettings Settings { get; set; }
 
         private List<MultiplayerRoomUser> users = new List<MultiplayerRoomUser>();
 
+        private object writeLock = new object();
+
+        /// <summary>
+        /// All users which are currently in this room, in any state.
+        /// </summary>
         public IReadOnlyList<MultiplayerRoomUser> Users
         {
             get
@@ -26,6 +40,9 @@ namespace osu.Game.Online.RealtimeMultiplayer
             }
         }
 
+        /// <summary>
+        /// Join a new user to this room.
+        /// </summary>
         public MultiplayerRoomUser Join(int userId)
         {
             var user = new MultiplayerRoomUser(userId);
@@ -33,6 +50,9 @@ namespace osu.Game.Online.RealtimeMultiplayer
             return user;
         }
 
+        /// <summary>
+        /// Remove a user from this room.
+        /// </summary>
         public MultiplayerRoomUser Leave(int userId)
         {
             MultiplayerRoomUser user = null;
@@ -47,8 +67,6 @@ namespace osu.Game.Online.RealtimeMultiplayer
 
             return user;
         }
-
-        private object writeLock = new object();
 
         /// <summary>
         /// Perform an update on this room in a thread-safe manner.
