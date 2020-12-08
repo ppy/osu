71de7ce0a3abc0af75d210d54ceafca9c1f0f5b2
Dean Herbert <pe@ppy.sh>
2020-12-08 16:32:45 +0900

    Add missing methods to server interface

--- osu.Game/Online/RealtimeMultiplayer/IMultiplayerServer.cs
+++ osu.Game/Online/RealtimeMultiplayer/IMultiplayerServer.cs
@@ -18,5 +18,17 @@ namespace osu.Game.Online.RealtimeMultiplayer
         /// Request to leave the currently joined room.
         /// </summary>
         Task LeaveRoom();
+
+        /// <summary>
+        /// Transfer the host of the currently joined room to another user in the room.
+        /// </summary>
+        /// <param name="userId">The new user which is to become host.</param>
+        Task TransferHost(long userId);
+
+        /// <summary>
+        /// As the host, update the settings of the currently joined room.
+        /// </summary>
+        /// <param name="settings">The new settings to apply.</param>
+        Task ChangeSettings(MultiplayerRoomSettings settings);
     }
 }
