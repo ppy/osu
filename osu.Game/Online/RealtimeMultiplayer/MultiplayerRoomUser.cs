5ff6e58708783fadab8ee81aa29a1c13c7d35183
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-12-08 16:15:51 +0900

    Make remaining model classes nullable and serializable

--- osu.Game/Online/RealtimeMultiplayer/MultiplayerRoomUser.cs
+++ osu.Game/Online/RealtimeMultiplayer/MultiplayerRoomUser.cs
@@ -1,27 +1,29 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable enable
+
 using System;
 using osu.Game.Users;
 
 namespace osu.Game.Online.RealtimeMultiplayer
 {
+    [Serializable]
     public class MultiplayerRoomUser : IEquatable<MultiplayerRoomUser>
     {
+        public readonly long UserID;
+
+        public MultiplayerUserState State { get; set; } = MultiplayerUserState.Idle;
+
+        public User? User { get; set; }
+
         public MultiplayerRoomUser(in int userId)
         {
             UserID = userId;
         }
 
-        public long UserID { get; }
-
-        public MultiplayerUserState State { get; set; }
-
-        public User User { get; set; }
-
         public bool Equals(MultiplayerRoomUser other)
         {
-            if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
 
             return UserID == other.UserID;
@@ -29,9 +31,8 @@ namespace osu.Game.Online.RealtimeMultiplayer
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
 
             return Equals((MultiplayerRoomUser)obj);
         }
