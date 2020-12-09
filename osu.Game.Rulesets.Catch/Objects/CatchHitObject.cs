c1f3f1c231c259ff4b494ce3bdcb3d11d3632469
ekrctb <ekrctb@gmail.com>
2020-12-09 17:47:17 +0900

    Obsolete `X` getter

--- osu.Game.Rulesets.Catch/Objects/CatchHitObject.cs
+++ osu.Game.Rulesets.Catch/Objects/CatchHitObject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Bindables;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
@@ -29,6 +30,7 @@ namespace osu.Game.Rulesets.Catch.Objects
         /// </summary>
         public float X
         {
+            [Obsolete("Use EffectiveX instead")]
             get => EffectiveX;
             set => OriginalXBindable.Value = value;
         }
