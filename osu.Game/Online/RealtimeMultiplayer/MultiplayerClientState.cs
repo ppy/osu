48129c52d676d8e33d63852031e530fe4cef6b32
Dean Herbert <pe@ppy.sh>
2020-12-09 12:38:24 +0900

    Change get-only property for now

--- osu.Game/Online/RealtimeMultiplayer/MultiplayerClientState.cs
+++ osu.Game/Online/RealtimeMultiplayer/MultiplayerClientState.cs
@@ -1,13 +1,16 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 #nullable enable
 
 namespace osu.Game.Online.RealtimeMultiplayer
 {
+    [Serializable]
     public class MultiplayerClientState
     {
-        public long CurrentRoomID { get; set; }
+        public readonly long CurrentRoomID;
 
         public MultiplayerClientState(in long roomId)
         {
