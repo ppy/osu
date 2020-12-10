8be6ed434a6d682f5d1af06721191b7c9ab93968
MATRIX-feather <midnightcarnival@outlook.com>
2020-12-10 23:45:56 +0800

    bug修复

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -169,7 +169,7 @@ namespace osu.Game.Rulesets.Osu
                 case ModType.Automation:
                     return new Mod[]
                     {
-                        new MultiMod(new OsuModDance(), new OsuModAutoplay(), new OsuModCinema()),
+                        new MultiMod(new OsuModAutoplay(), new OsuModCinema(), new OsuModDance()),
                         new OsuModRelax(),
                         new OsuModAutopilot(),
                         new OsuModSpunOut(),
