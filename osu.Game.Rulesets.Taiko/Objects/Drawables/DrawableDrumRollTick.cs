4d444df6b3979852403e46e37c5a1c29237cfb21
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-12-13 12:53:42 +0100

    Rename DHO CreateStrong{-> Nested}Hit

--- osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableDrumRollTick.cs
+++ osu.Game.Rulesets.Taiko/Objects/Drawables/DrawableDrumRollTick.cs
@@ -61,7 +61,7 @@ namespace osu.Game.Rulesets.Taiko.Objects.Drawables
             return UpdateResult(true);
         }
 
-        protected override DrawableStrongNestedHit CreateStrongHit(StrongNestedHitObject hitObject) => new StrongNestedHit(hitObject, this);
+        protected override DrawableStrongNestedHit CreateStrongNestedHit(StrongNestedHitObject hitObject) => new StrongNestedHit(hitObject, this);
 
         private class StrongNestedHit : DrawableStrongNestedHit
         {
