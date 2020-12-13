60379b09dbc22a1b8229c45a5a74a8f384a2bb39
Firmatorenio <smeshmunya@gmail.com>
2020-12-13 18:14:41 +0600

    added a container for the judgements to move up or down

---
+++ osu.Game.Rulesets.Mania/UI/DrawableManiaJudgementAdjustmentContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Game.Rulesets.UI;
+using osu.Framework.Graphics;
+
+namespace osu.Game.Rulesets.Mania.UI
+{
+    public class DrawableManiaJudgementAdjustmentContainer : JudgementContainer<DrawableManiaJudgement>
+    {
+        private float scorePosition => 0;
+        public DrawableManiaJudgementAdjustmentContainer(float hitTargetPosition)
+        {
+            Anchor = Anchor.TopCentre;
+            Origin = Anchor.Centre;
+            RelativeSizeAxes = Axes.Both;
+            Y = hitTargetPosition + 150;
+        }
+
+        public DrawableManiaJudgementAdjustmentContainer()
+            : this(110) { }
+    }
+}
