084e4ce50bb4265c5fd020c47e9a41a08fc9dc0f
Angela Zhang <43687061+angelaz1@users.noreply.github.com>
2020-12-12 20:11:57 -0600

    Removing whitespace

--- osu.Game/Overlays/ChatOverlay.cs
+++ osu.Game/Overlays/ChatOverlay.cs
@@ -332,7 +332,7 @@ namespace osu.Game.Overlays
             if (channel != null)
                 ChannelTabControl.Current.Value = channel;
         }
-        
+
         protected override bool OnKeyDown(KeyDownEvent e)
         {
             if (e.AltPressed)
