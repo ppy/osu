8214bc5ce28dbacd8a514543c0558242e16097c1
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-12-18 15:18:06 +0900

    Fix room status test scene not working

--- osu.Game.Tests/Visual/Multiplayer/TestSceneRoomStatus.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneRoomStatus.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Online.Multiplayer;
@@ -22,18 +23,21 @@ namespace osu.Game.Tests.Visual.Multiplayer
                     new DrawableRoom(new Room
                     {
                         Name = { Value = "Room 1" },
-                        Status = { Value = new RoomStatusOpen() }
-                    }),
+                        Status = { Value = new RoomStatusOpen() },
+                        EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
+                    }) { MatchingFilter = true },
                     new DrawableRoom(new Room
                     {
                         Name = { Value = "Room 2" },
-                        Status = { Value = new RoomStatusPlaying() }
-                    }),
+                        Status = { Value = new RoomStatusPlaying() },
+                        EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
+                    }) { MatchingFilter = true },
                     new DrawableRoom(new Room
                     {
                         Name = { Value = "Room 3" },
-                        Status = { Value = new RoomStatusEnded() }
-                    }),
+                        Status = { Value = new RoomStatusEnded() },
+                        EndDate = { Value = DateTimeOffset.Now }
+                    }) { MatchingFilter = true },
                 }
             };
         }
