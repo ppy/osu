d4738ed4ec1b923a8b1d8c8885ad4a83a14c5e9a
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2020-12-21 00:37:13 +0900

    Implement realtime match song select

--- osu.Game/Screens/Multi/RealtimeMultiplayer/RealtimeMatchSongSelect.cs
+++ osu.Game/Screens/Multi/RealtimeMultiplayer/RealtimeMatchSongSelect.cs
@@ -1,20 +1,53 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using Humanizer;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Screens;
+using osu.Game.Online.Multiplayer;
+using osu.Game.Online.RealtimeMultiplayer;
 using osu.Game.Screens.Select;
 
 namespace osu.Game.Screens.Multi.RealtimeMultiplayer
 {
-    public class RealtimeMatchSongSelect : SongSelect
+    public class RealtimeMatchSongSelect : SongSelect, IMultiplayerSubScreen
     {
-        protected override BeatmapDetailArea CreateBeatmapDetailArea()
-        {
-            throw new System.NotImplementedException();
-        }
+        public string ShortTitle => "song selection";
+
+        public override string Title => ShortTitle.Humanize();
+
+        [Resolved(typeof(Room), nameof(Room.Playlist))]
+        private BindableList<PlaylistItem> playlist { get; set; }
+
+        [Resolved]
+        private StatefulMultiplayerClient client { get; set; }
 
         protected override bool OnStart()
         {
-            throw new System.NotImplementedException();
+            var item = new PlaylistItem();
+
+            item.Beatmap.Value = Beatmap.Value.BeatmapInfo;
+            item.Ruleset.Value = Ruleset.Value;
+
+            item.RequiredMods.Clear();
+            item.RequiredMods.AddRange(Mods.Value.Select(m => m.CreateCopy()));
+
+            // If the client is already in a room, update via the client.
+            // Otherwise, update the playlist directly in preparation for it to be submitted to the API on match creation.
+            if (client.Room != null)
+                client.ChangeSettings(item: item);
+            else
+            {
+                playlist.Clear();
+                playlist.Add(item);
+            }
+
+            this.Exit();
+            return true;
         }
+
+        protected override BeatmapDetailArea CreateBeatmapDetailArea() => new PlayBeatmapDetailArea();
     }
 }
