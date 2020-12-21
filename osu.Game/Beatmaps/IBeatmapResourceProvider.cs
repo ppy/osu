00533b5b94dba1ed442da17582a2d0a0a51bc603
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2020-12-21 15:14:32 +0900

    Expose resources to skin via interface (and share common pieces with beatmap)

--- osu.Game/Beatmaps/IBeatmapResourceProvider.cs
+++ osu.Game/Beatmaps/IBeatmapResourceProvider.cs
@@ -1,14 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Audio;
 using osu.Framework.Audio.Track;
 using osu.Framework.Graphics.Textures;
-using osu.Framework.IO.Stores;
+using osu.Game.IO;
 
 namespace osu.Game.Beatmaps
 {
-    public interface IBeatmapResourceProvider
+    public interface IBeatmapResourceProvider : IStorageResourceProvider
     {
         /// <summary>
         /// Retrieve a global large texture store, used for loading beatmap backgrounds.
@@ -19,22 +18,5 @@ namespace osu.Game.Beatmaps
         /// Access a global track store for retrieving beatmap tracks from.
         /// </summary>
         ITrackStore Tracks { get; }
-
-        /// <summary>
-        /// Retrieve the game-wide audio manager.
-        /// </summary>
-        AudioManager AudioManager { get; }
-
-        /// <summary>
-        /// Access game-wide user files.
-        /// </summary>
-        IResourceStore<byte[]> Files { get; }
-
-        /// <summary>
-        /// Create a texture loader store based on an underlying data store.
-        /// </summary>
-        /// <param name="underlyingStore">The underlying provider of texture data (in arbitrary image formats).</param>
-        /// <returns>A texture loader store.</returns>
-        IResourceStore<TextureUpload> CreateTextureLoaderStore(IResourceStore<byte[]> underlyingStore);
     }
 }
