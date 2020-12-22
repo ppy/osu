1d25f239de25d338a5ea85fd1efc6f62b78b23ee
Endrik Tombak <87902114+bubb13gummy@users.noreply.github.com>
2020-12-22 17:36:44 +0200

    Refactor code into UserTrackingScrollContainer

--- osu.Game/Screens/Select/BeatmapCarousel.cs
+++ osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -901,14 +901,9 @@ namespace osu.Game.Screens.Select
             }
         }
 
-        protected class CarouselScrollContainer : OsuScrollContainer<DrawableCarouselItem>
+        protected class CarouselScrollContainer : UserTrackingScrollContainer<DrawableCarouselItem>
         {
             private bool rightMouseScrollBlocked;
-
-            /// <summary>
-            /// Whether the last scroll event was user triggered, directly on the scroll container.
-            /// </summary>
-            public bool UserScrolling { get; private set; }
 
             public CarouselScrollContainer()
             {
@@ -917,19 +912,6 @@ namespace osu.Game.Screens.Select
 
                 // the scroll container may get pushed off-screen by global screen changes, but we still want panels to display outside of the bounds.
                 Masking = false;
-            }
-
-            // ReSharper disable once OptionalParameterHierarchyMismatch 2020.3 EAP4 bug. (https://youtrack.jetbrains.com/issue/RSRP-481535?p=RIDER-51910)
-            protected override void OnUserScroll(float value, bool animated = true, double? distanceDecay = default)
-            {
-                UserScrolling = true;
-                base.OnUserScroll(value, animated, distanceDecay);
-            }
-
-            public new void ScrollTo(float value, bool animated = true, double? distanceDecay = null)
-            {
-                UserScrolling = false;
-                base.ScrollTo(value, animated, distanceDecay);
             }
 
             protected override bool OnMouseDown(MouseDownEvent e)
