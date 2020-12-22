df5e1d83bd24fb0311868aa93034f790e7b47758
Dean Herbert <pe@ppy.sh>
2020-12-22 14:36:52 +0900

    Allow recommender to potentially be null

--- osu.Game/Screens/Select/SongSelect.cs
+++ osu.Game/Screens/Select/SongSelect.cs
@@ -118,7 +118,7 @@ namespace osu.Game.Screens.Select
                 BleedBottom = Footer.HEIGHT,
                 SelectionChanged = updateSelectedBeatmap,
                 BeatmapSetsChanged = carouselBeatmapsLoaded,
-                GetRecommendedBeatmap = recommender.GetRecommendedBeatmap,
+                GetRecommendedBeatmap = s => recommender?.GetRecommendedBeatmap(s),
             }, c => carouselContainer.Child = c);
 
             AddRangeInternal(new Drawable[]
