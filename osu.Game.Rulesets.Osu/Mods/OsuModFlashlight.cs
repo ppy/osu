dec997c0f49687800b9c90641299d38b4c9e060d
Dean Herbert <pe@ppy.sh>
2020-12-23 14:44:20 +0900

    Fix flashlight not updating its position during replay rewinding
    Closes #11260

--- osu.Game.Rulesets.Osu/Mods/OsuModFlashlight.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModFlashlight.cs
@@ -56,7 +56,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                 var destination = e.MousePosition;
 
                 FlashlightPosition = Interpolation.ValueAt(
-                    Math.Clamp(Clock.ElapsedFrameTime, 0, follow_delay), position, destination, 0, follow_delay, Easing.Out);
+                    Math.Clamp(Math.Abs(Clock.ElapsedFrameTime), 0, follow_delay), position, destination, 0, follow_delay, Easing.Out);
 
                 return base.OnMouseMove(e);
             }
