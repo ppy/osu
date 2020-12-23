8bef0b9ca502824b15965b6b8ced30587a9926c8
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2020-12-23 21:50:29 +0100

    Move out sizing logic to multiplayer screen

--- osu.Game/Screens/Multi/CreateRoomButton.cs
+++ osu.Game/Screens/Multi/CreateRoomButton.cs
@@ -2,24 +2,12 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Game.Screens.Multi.Match.Components;
-using osuTK;
 
 namespace osu.Game.Screens.Multi
 {
     public class CreateRoomButton : PurpleTriangleButton
     {
-        public CreateRoomButton()
-        {
-            Size = new Vector2(150, Header.HEIGHT - 20);
-            Margin = new MarginPadding
-            {
-                Top = 10,
-                Right = 10 + OsuScreen.HORIZONTAL_OVERFLOW_PADDING,
-            };
-        }
-
         [BackgroundDependencyLoader]
         private void load()
         {
