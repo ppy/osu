d6dadd12faa7c277befcdcefd0eecdc25196cf0c
Dean Herbert <pe@ppy.sh>
2020-12-24 10:38:53 +0900

    Send multiplayer user IDs via ctor for better thread safety

--- osu.Game/Screens/Multi/RealtimeMultiplayer/RealtimePlayer.cs
+++ osu.Game/Screens/Multi/RealtimeMultiplayer/RealtimePlayer.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
@@ -37,9 +36,17 @@ namespace osu.Game.Screens.Multi.RealtimeMultiplayer
         [CanBeNull]
         private MultiplayerGameplayLeaderboard leaderboard;
 
-        public RealtimePlayer(PlaylistItem playlistItem)
+        private readonly int[] userIds;
+
+        /// <summary>
+        /// Construct a multiplayer player.
+        /// </summary>
+        /// <param name="playlistItem">The playlist item to be played.</param>
+        /// <param name="userIds">The users which are participating in this game.</param>
+        public RealtimePlayer(PlaylistItem playlistItem, int[] userIds)
             : base(playlistItem, false)
         {
+            this.userIds = userIds;
         }
 
         [BackgroundDependencyLoader]
@@ -64,8 +71,6 @@ namespace osu.Game.Screens.Multi.RealtimeMultiplayer
             }
 
             Debug.Assert(client.Room != null);
-
-            int[] userIds = client.Room.Users.Where(u => u.State >= MultiplayerUserState.WaitingForLoad).Select(u => u.UserID).ToArray();
 
             // todo: this should be implemented via a custom HUD implementation, and correctly masked to the main content area.
             LoadComponentAsync(leaderboard = new MultiplayerGameplayLeaderboard(ScoreProcessor, userIds), HUDOverlay.Add);
