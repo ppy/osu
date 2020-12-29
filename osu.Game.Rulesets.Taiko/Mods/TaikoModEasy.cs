013b9b62a1ff7dfd22db139d0571259bcd89de43
Firmatorenio <smeshmunya@gmail.com>
2020-12-29 20:22:56 +0600

    add SV multipliers to taiko difficulty mods

--- osu.Game.Rulesets.Taiko/Mods/TaikoModEasy.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModEasy.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Taiko.Mods
@@ -8,5 +9,13 @@ namespace osu.Game.Rulesets.Taiko.Mods
     public class TaikoModEasy : ModEasy
     {
         public override string Description => @"Beats move slower, and less accuracy required!";
+
+        private const double slider_multiplier = 0.8;
+
+        public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            base.ApplyToDifficulty(difficulty);
+            difficulty.SliderMultiplier *= slider_multiplier;
+        }
     }
 }
--- osu.Game.Rulesets.Taiko/Mods/TaikoModHardRock.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModHardRock.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Taiko.Mods
@@ -9,5 +10,13 @@ namespace osu.Game.Rulesets.Taiko.Mods
     {
         public override double ScoreMultiplier => 1.06;
         public override bool Ranked => true;
+
+        private const double slider_multiplier = 1.87;
+
+        public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
+        {
+            base.ApplyToDifficulty(difficulty);
+            difficulty.SliderMultiplier *= slider_multiplier;
+        }
     }
 }
