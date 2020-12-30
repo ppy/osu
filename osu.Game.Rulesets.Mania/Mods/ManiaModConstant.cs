84f7750f0d4d5f78b413de214eab8279c7c036d8
Chev <11602755+chev2@users.noreply.github.com>
2020-12-29 16:25:50 -0800

    Change mod icon to a better-suited one, add timing point counteraction

--- osu.Game.Rulesets.Mania/Mods/ManiaModConstant.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModConstant.cs
@@ -3,6 +3,7 @@
 
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Mania.Mods
@@ -13,7 +14,7 @@ namespace osu.Game.Rulesets.Mania.Mods
         public override string Acronym => "CT";
         public override double ScoreMultiplier => 1;
         public override string Description => "No more tricky note speed changes!";
-        public override IconUsage? Icon => FontAwesome.Solid.ArrowDown;
+        public override IconUsage? Icon => FontAwesome.Solid.BalanceScale;
         public override ModType Type => ModType.Conversion;
         public override bool Ranked => false;
 
@@ -21,11 +22,28 @@ namespace osu.Game.Rulesets.Mania.Mods
         {
             // Get all difficulty points
             var difficultyPoints = beatmap.ControlPointInfo.DifficultyPoints;
+            // Get all timing points
+            var timingPoints = beatmap.ControlPointInfo.TimingPoints;
+            // Get the initial BPM
+            var initialBPM = timingPoints[0].BPM;
 
             foreach (var difficultyPoint in difficultyPoints)
             {
                 // Set this difficulty point's speed multiplier to 1
                 difficultyPoint.SpeedMultiplier = 1;
+            }
+
+            // Correct changes in speed caused by timing points by adding an associated difficulty point
+            foreach (var timingPoint in timingPoints)
+            {
+                // Create a new difficulty point to counteract timing point
+                DifficultyControlPoint diffControlPoint = new DifficultyControlPoint
+                {
+                    SpeedMultiplier = initialBPM / timingPoint.BPM
+                };
+
+                // Add the new control point to the beatmap
+                beatmap.ControlPointInfo.Add(timingPoint.Time, diffControlPoint);
             }
         }
     }
