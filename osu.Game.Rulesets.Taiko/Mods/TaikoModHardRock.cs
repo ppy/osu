669c42a38d7f3ac2d015754166b7af7aacbd13c9
Firmatorenio <smeshmunya@gmail.com>
2020-12-30 20:57:41 +0600

    add remarks explaining HR SV multiplier

--- osu.Game.Rulesets.Taiko/Mods/TaikoModHardRock.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModHardRock.cs
@@ -11,7 +11,15 @@ namespace osu.Game.Rulesets.Taiko.Mods
         public override double ScoreMultiplier => 1.06;
         public override bool Ranked => true;
 
-        private const double slider_multiplier = 1.87;
+        /// <summary>
+        /// Multiplier factor added to the scrolling speed.
+        /// </summary>
+        /// <remarks>
+        /// This factor is made up of two parts: the base part (1.4) and the aspect ratio adjustment (4/3).
+        /// Stable applies the latter by dividing the width of the user's display by the width of a display with the same height, but 4:3 aspect ratio.
+        /// TODO: Revisit if taiko playfield ever changes away from a hard-coded 16:9 (see https://github.com/ppy/osu/issues/5685).
+        /// </remarks>
+        private const double slider_multiplier = 1.4 * 4 / 3;
 
         public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
         {
