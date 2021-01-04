9e0c490141e4e85ecde7c96d7a6b3f37f0b3b4fa
Dean Herbert <pe@ppy.sh>
2021-01-04 15:40:22 +0900

    Remove unused using

--- osu.Game/Storyboards/Storyboard.cs
+++ osu.Game/Storyboards/Storyboard.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore.Internal;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
