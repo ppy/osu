6f3fbd6dbf20622a72fd8639d80981b875aac02e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-01-07 14:35:15 +0900

    Add realm store / key binding implementations

---
+++ osu.Game/Input/Bindings/RealmKeyBinding.cs
@@ -0,0 +1,33 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Input.Bindings;
+using osu.Game.Database;
+using Realms;
+
+namespace osu.Game.Input.Bindings
+{
+    [MapTo("KeyBinding")]
+    public class RealmKeyBinding : RealmObject, IHasGuidPrimaryKey
+    {
+        public Guid ID { get; set; }
+
+        public int? RulesetID { get; set; }
+
+        public int? Variant { get; set; }
+
+        [Ignored]
+        public KeyBinding KeyBinding
+        {
+            get
+            {
+                var split = KeyBindingString.Split(':');
+                return new KeyBinding(split[0], int.Parse(split[1]));
+            }
+            set => KeyBindingString = $"{value.KeyCombination}:{(int)value.Action}";
+        }
+
+        public string KeyBindingString { get; set; }
+    }
+}
