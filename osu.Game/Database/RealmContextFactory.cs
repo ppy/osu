864294c5c802e9d2a3b72e90a1aeb89170fa960b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-01-11 18:58:56 +0900

    Move extensions methods into own class

--- osu.Game/Database/RealmContextFactory.cs
+++ osu.Game/Database/RealmContextFactory.cs
@@ -2,16 +2,8 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Threading;
-using AutoMapper;
 using osu.Framework.Platform;
 using osu.Framework.Statistics;
-using osu.Game.Beatmaps;
-using osu.Game.Configuration;
-using osu.Game.Input.Bindings;
-using osu.Game.IO;
-using osu.Game.Rulesets;
-using osu.Game.Scoring;
-using osu.Game.Skinning;
 using Realms;
 
 namespace osu.Game.Database
@@ -169,50 +161,5 @@ namespace osu.Game.Database
                 storage.DeleteDatabase(database_name);
             }
         }
-    }
-
-    public static class RealmExtensions
-    {
-        private static readonly IMapper mapper = new MapperConfiguration(c =>
-        {
-            c.ShouldMapField = fi => false;
-            c.ShouldMapProperty = pi => pi.SetMethod != null && pi.SetMethod.IsPublic;
-
-            c.CreateMap<BeatmapDifficulty, BeatmapDifficulty>();
-            c.CreateMap<BeatmapInfo, BeatmapInfo>();
-            c.CreateMap<BeatmapMetadata, BeatmapMetadata>();
-            c.CreateMap<BeatmapSetFileInfo, BeatmapSetFileInfo>();
-
-            c.CreateMap<BeatmapSetInfo, BeatmapSetInfo>()
-             .ForMember(s => s.Beatmaps, d => d.MapFrom(s => s.Beatmaps))
-             .ForMember(s => s.Files, d => d.MapFrom(s => s.Files))
-             .MaxDepth(2);
-
-            c.CreateMap<DatabasedKeyBinding, DatabasedKeyBinding>();
-            c.CreateMap<RealmKeyBinding, RealmKeyBinding>();
-            c.CreateMap<DatabasedSetting, DatabasedSetting>();
-            c.CreateMap<FileInfo, FileInfo>();
-            c.CreateMap<ScoreFileInfo, ScoreFileInfo>();
-            c.CreateMap<SkinInfo, SkinInfo>();
-            c.CreateMap<RulesetInfo, RulesetInfo>();
-        }).CreateMapper();
-
-        public static T Detach<T>(this T obj) where T : RealmObject
-        {
-            if (!obj.IsManaged)
-                return obj;
-
-            var detached = mapper.Map<T>(obj);
-
-            //typeof(RealmObject).GetField("_realm", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(detached, null);
-
-            return detached;
-        }
-
-        public static Live<T> Wrap<T>(this T obj, IRealmFactory contextFactory)
-            where T : RealmObject, IHasGuidPrimaryKey => new Live<T>(obj, contextFactory);
-
-        public static Live<T> WrapAsUnmanaged<T>(this T obj)
-            where T : RealmObject, IHasGuidPrimaryKey => new Live<T>(obj, null);
     }
 }
