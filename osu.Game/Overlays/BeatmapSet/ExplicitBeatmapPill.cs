abf718242b11519825228a2f6098dff745231608
Salman Ahmed <frenzibyte@gmail.com>
2021-01-14 05:40:43 +0300

    Make explicit marker font semi-bold

--- osu.Game/Overlays/BeatmapSet/ExplicitBeatmapPill.cs
+++ osu.Game/Overlays/BeatmapSet/ExplicitBeatmapPill.cs
@@ -35,7 +35,7 @@ namespace osu.Game.Overlays.BeatmapSet
                     {
                         Margin = new MarginPadding { Horizontal = 10f, Vertical = 2f },
                         Text = "EXPLICIT",
-                        Font = OsuFont.GetFont(size: 10, weight: FontWeight.Bold),
+                        Font = OsuFont.GetFont(size: 10, weight: FontWeight.SemiBold),
                         Colour = OverlayColourProvider.Orange.Colour2,
                     }
                 }
