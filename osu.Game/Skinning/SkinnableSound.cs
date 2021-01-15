f4d240b9bc130530168dfa45afdf1b0b02a72cbf
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-01-15 17:16:12 +0900

    Add support for playing a SkinnableSample without restarting it

--- osu.Game/Skinning/SkinnableSound.cs
+++ osu.Game/Skinning/SkinnableSound.cs
@@ -119,12 +119,13 @@ namespace osu.Game.Skinning
         /// <summary>
         /// Plays the samples.
         /// </summary>
-        public virtual void Play()
+        /// <param name="restart">Whether to play the sample from the beginning.</param>
+        public virtual void Play(bool restart = true)
         {
             samplesContainer.ForEach(c =>
             {
                 if (PlayWhenZeroVolume || c.AggregateVolume.Value > 0)
-                    c.Play();
+                    c.Play(restart);
             });
         }
 
