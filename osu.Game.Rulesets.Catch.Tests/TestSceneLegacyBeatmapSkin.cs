846286bd0b2aecd9978767ce5c83999468d4a926
Mysfit <87902114+bubb13gummy@users.noreply.github.com>
2021-01-15 23:46:46 -0500

    Created base class for testing beatmap colours.

--- osu.Game.Rulesets.Catch.Tests/TestSceneLegacyBeatmapSkin.cs
+++ osu.Game.Rulesets.Catch.Tests/TestSceneLegacyBeatmapSkin.cs
@@ -2,13 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
-using osu.Framework.Bindables;
-using osu.Framework.IO.Stores;
 using osu.Framework.Testing;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
@@ -17,178 +14,113 @@ using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.Skinning;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Objects;
-using osu.Game.Screens.Play;
 using osu.Game.Skinning;
-using osu.Game.Tests.Visual;
+using osu.Game.Tests.Beatmaps;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Catch.Tests
 {
-    public class TestSceneLegacyBeatmapSkin : ScreenTestScene
+    public class TestSceneLegacyBeatmapSkin : LegacyBeatmapSkinColourTest
     {
         [Resolved]
         private AudioManager audio { get; set; }
 
-        private readonly Bindable<bool> beatmapSkins = new Bindable<bool>();
-        private readonly Bindable<bool> beatmapColours = new Bindable<bool>();
-
         [BackgroundDependencyLoader]
         private void load(OsuConfigManager config)
         {
-            config.BindWith(OsuSetting.BeatmapSkins, beatmapSkins);
-            config.BindWith(OsuSetting.BeatmapColours, beatmapColours);
+            config.BindWith(OsuSetting.BeatmapSkins, BeatmapSkins);
+            config.BindWith(OsuSetting.BeatmapColours, BeatmapColours);
         }
 
         [TestCase(true, true)]
         [TestCase(true, false)]
         [TestCase(false, true)]
         [TestCase(false, false)]
-        public void TestBeatmapComboColours(bool userHasCustomColours, bool useBeatmapSkin)
+        public override void TestBeatmapComboColours(bool userHasCustomColours, bool useBeatmapSkin)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, true);
-            AddStep("load coloured beatmap", () => player = loadBeatmap(userHasCustomColours, true));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is beatmap skin colours", () => player.UsableComboColours.SequenceEqual(TestBeatmapSkin.Colours));
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, true);
+            base.TestBeatmapComboColours(userHasCustomColours, useBeatmapSkin);
+            AddAssert("is beatmap skin colours", () => TestPlayer.UsableComboColours.SequenceEqual(TestBeatmapSkin.Colours));
         }
 
         [TestCase(true)]
         [TestCase(false)]
-        public void TestBeatmapComboColoursOverride(bool useBeatmapSkin)
+        public override void TestBeatmapComboColoursOverride(bool useBeatmapSkin)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, false);
-            AddStep("load coloured beatmap", () => player = loadBeatmap(true, true));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is user custom skin colours", () => player.UsableComboColours.SequenceEqual(TestSkin.Colours));
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, true);
+            base.TestBeatmapComboColoursOverride(useBeatmapSkin);
+            AddAssert("is user custom skin colours", () => TestPlayer.UsableComboColours.SequenceEqual(TestSkin.Colours));
         }
 
         [TestCase(true)]
         [TestCase(false)]
-        public void TestBeatmapComboColoursOverrideWithDefaultColours(bool useBeatmapSkin)
+        public override void TestBeatmapComboColoursOverrideWithDefaultColours(bool useBeatmapSkin)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, false);
-            AddStep("load coloured beatmap", () => player = loadBeatmap(false, true));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is default user skin colours", () => player.UsableComboColours.SequenceEqual(SkinConfiguration.DefaultComboColours));
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, true);
+            base.TestBeatmapComboColoursOverrideWithDefaultColours(useBeatmapSkin);
+            AddAssert("is default user skin colours", () => TestPlayer.UsableComboColours.SequenceEqual(SkinConfiguration.DefaultComboColours));
         }
 
         [TestCase(true, true)]
         [TestCase(false, true)]
         [TestCase(true, false)]
         [TestCase(false, false)]
-        public void TestBeatmapNoComboColours(bool useBeatmapSkin, bool useBeatmapColour)
+        public override void TestBeatmapNoComboColours(bool useBeatmapSkin, bool useBeatmapColour)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, useBeatmapColour);
-            AddStep("load no-colour beatmap", () => player = loadBeatmap(false, false));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is default user skin colours", () => player.UsableComboColours.SequenceEqual(SkinConfiguration.DefaultComboColours));
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, false);
+            base.TestBeatmapNoComboColours(useBeatmapSkin, useBeatmapColour);
+            AddAssert("is default user skin colours", () => TestPlayer.UsableComboColours.SequenceEqual(SkinConfiguration.DefaultComboColours));
         }
 
         [TestCase(true, true)]
         [TestCase(false, true)]
         [TestCase(true, false)]
         [TestCase(false, false)]
-        public void TestBeatmapNoComboColoursSkinOverride(bool useBeatmapSkin, bool useBeatmapColour)
+        public override void TestBeatmapNoComboColoursSkinOverride(bool useBeatmapSkin, bool useBeatmapColour)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, useBeatmapColour);
-            AddStep("load custom-skin colour", () => player = loadBeatmap(true, false));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is custom user skin colours", () => player.UsableComboColours.SequenceEqual(TestSkin.Colours));
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, false);
+            base.TestBeatmapNoComboColoursSkinOverride(useBeatmapSkin, useBeatmapColour);
+            AddAssert("is custom user skin colours", () => TestPlayer.UsableComboColours.SequenceEqual(TestSkin.Colours));
         }
 
         [TestCase(true)]
         [TestCase(false)]
         public void TestBeatmapHyperDashColours(bool useBeatmapSkin)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, true);
-            AddStep("load custom-skin colour", () => player = loadBeatmap(true, true));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is custom hyper dash colours", () => player.UsableHyperDashColour == TestBeatmapSkin.HYPER_DASH_COLOUR);
-            AddAssert("is custom hyper dash after image colours", () => player.UsableHyperDashAfterImageColour == TestBeatmapSkin.HYPER_DASH_AFTER_IMAGE_COLOUR);
-            AddAssert("is custom hyper dash fruit colours", () => player.UsableHyperDashFruitColour == TestBeatmapSkin.HYPER_DASH_FRUIT_COLOUR);
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, true);
+            ConfigureTest(useBeatmapSkin, true, true);
+            AddAssert("is custom hyper dash colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashColour == CatchTestBeatmapSkin.HYPER_DASH_COLOUR);
+            AddAssert("is custom hyper dash after image colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashAfterImageColour == CatchTestBeatmapSkin.HYPER_DASH_AFTER_IMAGE_COLOUR);
+            AddAssert("is custom hyper dash fruit colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashFruitColour == CatchTestBeatmapSkin.HYPER_DASH_FRUIT_COLOUR);
         }
 
         [TestCase(true)]
         [TestCase(false)]
         public void TestBeatmapHyperDashColoursOverride(bool useBeatmapSkin)
         {
-            ExposedPlayer player = null;
-
-            configureSettings(useBeatmapSkin, false);
-            AddStep("load custom-skin colour", () => player = loadBeatmap(true, true));
-            AddUntilStep("wait for player", () => player.IsLoaded);
-
-            AddAssert("is custom hyper dash colours", () => player.UsableHyperDashColour == TestSkin.HYPER_DASH_COLOUR);
-            AddAssert("is custom hyper dash after image colours", () => player.UsableHyperDashAfterImageColour == TestSkin.HYPER_DASH_AFTER_IMAGE_COLOUR);
-            AddAssert("is custom hyper dash fruit colours", () => player.UsableHyperDashFruitColour == TestSkin.HYPER_DASH_FRUIT_COLOUR);
+            TestBeatmap = new CatchCustomSkinWorkingBeatmap(audio, true);
+            ConfigureTest(useBeatmapSkin, false, true);
+            AddAssert("is custom hyper dash colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashColour == CatchTestSkin.HYPER_DASH_COLOUR);
+            AddAssert("is custom hyper dash after image colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashAfterImageColour == CatchTestSkin.HYPER_DASH_AFTER_IMAGE_COLOUR);
+            AddAssert("is custom hyper dash fruit colours", () => ((CatchExposedPlayer)TestPlayer).UsableHyperDashFruitColour == CatchTestSkin.HYPER_DASH_FRUIT_COLOUR);
         }
 
-        private ExposedPlayer loadBeatmap(bool userHasCustomColours, bool beatmapHasColours)
+        protected override ExposedPlayer CreateTestPlayer(bool userHasCustomColours) => new CatchExposedPlayer(userHasCustomColours);
+
+        private class CatchExposedPlayer : ExposedPlayer
         {
-            ExposedPlayer player;
-
-            Beatmap.Value = new CustomSkinWorkingBeatmap(audio, beatmapHasColours);
-
-            LoadScreen(player = new ExposedPlayer(userHasCustomColours));
-
-            return player;
-        }
-
-        private void configureSettings(bool beatmapSkins, bool beatmapColours)
-        {
-            AddStep($"{(beatmapSkins ? "enable" : "disable")} beatmap skins", () =>
+            public CatchExposedPlayer(bool userHasCustomColours)
+                : base(userHasCustomColours)
             {
-                this.beatmapSkins.Value = beatmapSkins;
-            });
-            AddStep($"{(beatmapColours ? "enable" : "disable")} beatmap colours", () =>
-            {
-                this.beatmapColours.Value = beatmapColours;
-            });
-        }
-
-        private class ExposedPlayer : Player
-        {
-            private readonly bool userHasCustomColours;
-
-            public ExposedPlayer(bool userHasCustomColours)
-                : base(new PlayerConfiguration
-                {
-                    AllowPause = false,
-                    ShowResults = false,
-                })
-            {
-                this.userHasCustomColours = userHasCustomColours;
             }
 
             protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
             {
                 var dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
-                dependencies.CacheAs<ISkinSource>(new TestSkin(userHasCustomColours));
+                dependencies.CacheAs<ISkinSource>(new CatchTestSkin(UserHasCustomColours));
                 return dependencies;
             }
-
-            public IReadOnlyList<Color4> UsableComboColours =>
-                GameplayClockContainer.ChildrenOfType<BeatmapSkinProvidingContainer>()
-                                      .First()
-                                      .GetConfig<GlobalSkinColours, IReadOnlyList<Color4>>(GlobalSkinColours.ComboColours)?.Value;
 
             public Color4 UsableHyperDashColour =>
                 GameplayClockContainer.ChildrenOfType<BeatmapSkinProvidingContainer>()
@@ -223,17 +155,14 @@ namespace osu.Game.Rulesets.Catch.Tests
             }
         }
 
-        private class CustomSkinWorkingBeatmap : ClockBackedTestWorkingBeatmap
+        private class CatchCustomSkinWorkingBeatmap : CustomSkinWorkingBeatmap
         {
-            private readonly bool hasColours;
-
-            public CustomSkinWorkingBeatmap(AudioManager audio, bool hasColours)
-                : base(createBeatmap(new CatchRuleset().RulesetInfo), null, null, audio)
+            public CatchCustomSkinWorkingBeatmap(AudioManager audio, bool hasColours)
+                : base(createBeatmap(new CatchRuleset().RulesetInfo), audio, hasColours)
             {
-                this.hasColours = hasColours;
             }
 
-            protected override ISkin GetSkin() => new TestBeatmapSkin(BeatmapInfo, hasColours);
+            protected override ISkin GetSkin() => new CatchTestBeatmapSkin(BeatmapInfo, HasColours);
 
             private static IBeatmap createBeatmap(RulesetInfo ruleset)
             {
@@ -297,26 +226,19 @@ namespace osu.Game.Rulesets.Catch.Tests
             }
         }
 
-        private class TestBeatmapSkin : LegacyBeatmapSkin
+        private class CatchTestBeatmapSkin : TestBeatmapSkin
         {
-            public static Color4[] Colours { get; } =
-            {
-                new Color4(50, 100, 150, 255),
-                new Color4(40, 80, 120, 255),
-            };
-
             public static readonly Color4 HYPER_DASH_COLOUR = Color4.DarkBlue;
 
             public static readonly Color4 HYPER_DASH_AFTER_IMAGE_COLOUR = Color4.DarkCyan;
 
             public static readonly Color4 HYPER_DASH_FRUIT_COLOUR = Color4.DarkGoldenrod;
 
-            public TestBeatmapSkin(BeatmapInfo beatmap, bool hasColours)
-                : base(beatmap, new ResourceStore<byte[]>(), null)
+            public CatchTestBeatmapSkin(BeatmapInfo beatmap, bool hasColours)
+                : base(beatmap, hasColours)
             {
                 if (hasColours)
                 {
-                    Configuration.AddComboColours(Colours);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDash.ToString(), HYPER_DASH_COLOUR);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDashAfterImage.ToString(), HYPER_DASH_AFTER_IMAGE_COLOUR);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDashFruit.ToString(), HYPER_DASH_FRUIT_COLOUR);
@@ -324,36 +246,23 @@ namespace osu.Game.Rulesets.Catch.Tests
             }
         }
 
-        private class TestSkin : LegacySkin, ISkinSource
+        private class CatchTestSkin : TestSkin
         {
-            public static Color4[] Colours { get; } =
-            {
-                new Color4(150, 100, 50, 255),
-                new Color4(20, 20, 20, 255),
-            };
-
             public static readonly Color4 HYPER_DASH_COLOUR = Color4.LightBlue;
 
             public static readonly Color4 HYPER_DASH_AFTER_IMAGE_COLOUR = Color4.LightCoral;
 
             public static readonly Color4 HYPER_DASH_FRUIT_COLOUR = Color4.LightCyan;
 
-            public TestSkin(bool hasCustomColours)
-                : base(new SkinInfo(), new ResourceStore<byte[]>(), null, string.Empty)
+            public CatchTestSkin(bool hasCustomColours)
+                : base(hasCustomColours)
             {
                 if (hasCustomColours)
                 {
-                    Configuration.AddComboColours(Colours);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDash.ToString(), HYPER_DASH_COLOUR);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDashAfterImage.ToString(), HYPER_DASH_AFTER_IMAGE_COLOUR);
                     Configuration.CustomColours.Add(CatchSkinColour.HyperDashFruit.ToString(), HYPER_DASH_FRUIT_COLOUR);
                 }
-            }
-
-            public event Action SourceChanged
-            {
-                add { }
-                remove { }
             }
         }
     }
