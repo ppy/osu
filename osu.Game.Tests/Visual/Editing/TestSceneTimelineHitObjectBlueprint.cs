75908bea3f974bb4a2602f44b1ce21fe4b3a91db
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-01-21 22:09:42 +0100

    Add missing license header

--- osu.Game.Tests/Visual/Editing/TestSceneTimelineHitObjectBlueprint.cs
+++ osu.Game.Tests/Visual/Editing/TestSceneTimelineHitObjectBlueprint.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Testing;
