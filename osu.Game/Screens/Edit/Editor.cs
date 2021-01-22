b44bd8c4eea20cb3338c3f85e639a7472b6f5262
Dean Herbert <pe@ppy.sh>
2021-01-22 18:03:33 +0900

    Remove unused using statement

--- osu.Game/Screens/Edit/Editor.cs
+++ osu.Game/Screens/Edit/Editor.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
