c39d1579a220dc2a345a690a707d23170e257ad9
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-01-22 19:48:33 +0100

    Update outdated comment

--- osu.Game/Graphics/Containers/SectionsContainer.cs
+++ osu.Game/Graphics/Containers/SectionsContainer.cs
@@ -217,7 +217,7 @@ namespace osu.Game.Graphics.Containers
 
                 var smallestSectionHeight = Children.Count > 0 ? Children.Min(d => d.Height) : 0;
 
-                // scroll offset is our fixed header height if we have it plus 20% of content height
+                // scroll offset is our fixed header height if we have it plus 10% of content height
                 // plus 5% to fix floating point errors and to not have a section instantly unselect when scrolling upwards
                 // but the 5% can't be bigger than our smallest section height, otherwise it won't get selected correctly
                 float selectionLenienceAboveSection = Math.Min(smallestSectionHeight / 2.0f, scrollContainer.DisplayableContent * 0.05f);
