faa221027444b36fd3089888119c39964bbdbe4a
Salman Ahmed <frenzibyte@gmail.com>
2021-01-24 00:55:45 +0300

    Use old-style Enum.GetValues method for older than .NET 5

--- osu.Game.Tests/Visual/UserInterface/TestSceneUpdateableBeatmapSetCover.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneUpdateableBeatmapSetCover.cs
@@ -57,11 +57,15 @@ namespace osu.Game.Tests.Visual.UserInterface
                     }
                 };
 
+#if NET5_0
                 var coverTypes = Enum.GetValues<BeatmapSetCoverType>();
+#else
+                var coverTypes = Enum.GetValues(typeof(BeatmapSetCoverType)).Cast<BeatmapSetCoverType>();
+#endif
 
                 for (int i = 0; i < 25; i++)
                 {
-                    var coverType = coverTypes[i % coverTypes.Length];
+                    var coverType = coverTypes.ElementAt(i);
 
                     var cover = new UpdateableBeatmapSetCover(coverType)
                     {
