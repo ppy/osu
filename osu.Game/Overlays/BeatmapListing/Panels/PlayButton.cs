f7004ea238d35130982c5dacd5fc5a6208789996
Salman Ahmed <frenzibyte@gmail.com>
2021-01-24 01:51:18 +0300

    Add support for disabling beatmap listing play buttons

--- osu.Game/Overlays/BeatmapListing/Panels/PlayButton.cs
+++ osu.Game/Overlays/BeatmapListing/Panels/PlayButton.cs
@@ -10,14 +10,19 @@ using osu.Framework.Input.Events;
 using osu.Game.Audio;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Overlays.BeatmapListing.Panels
 {
-    public class PlayButton : Container
+    public class PlayButton : CompositeDrawable
     {
+        private const float transition_duration = 500;
+
+        private readonly Button button;
+
         public IBindable<bool> Playing => playing;
 
         private readonly BindableBool playing = new BindableBool();
@@ -43,84 +48,30 @@ namespace osu.Game.Overlays.BeatmapListing.Panels
             }
         }
 
-        private Color4 hoverColour;
-        private readonly SpriteIcon icon;
-        private readonly LoadingSpinner loadingSpinner;
-
-        private const float transition_duration = 500;
-
-        private bool loading
-        {
-            set
-            {
-                if (value)
-                {
-                    icon.FadeTo(0.5f, transition_duration, Easing.OutQuint);
-                    loadingSpinner.Show();
-                }
-                else
-                {
-                    icon.FadeTo(1, transition_duration, Easing.OutQuint);
-                    loadingSpinner.Hide();
-                }
-            }
-        }
+        [Resolved]
+        private PreviewTrackManager previewTrackManager { get; set; }
 
         public PlayButton(BeatmapSetInfo setInfo = null)
         {
             BeatmapSet = setInfo;
-            AddRange(new Drawable[]
+
+            InternalChild = button = new Button(this)
             {
-                icon = new SpriteIcon
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    FillMode = FillMode.Fit,
-                    RelativeSizeAxes = Axes.Both,
-                    Icon = FontAwesome.Solid.Play,
-                },
-                loadingSpinner = new LoadingSpinner
-                {
-                    Size = new Vector2(15),
-                },
-            });
+                RelativeSizeAxes = Axes.Both,
+                Action = ToggleButton,
+            };
 
             playing.ValueChanged += playingStateChanged;
         }
 
-        [Resolved]
-        private PreviewTrackManager previewTrackManager { get; set; }
-
-        [BackgroundDependencyLoader]
-        private void load(OsuColour colour)
+        public void ToggleButton()
         {
-            hoverColour = colour.Yellow;
-        }
-
-        protected override bool OnClick(ClickEvent e)
-        {
-            playing.Toggle();
-            return true;
-        }
-
-        protected override bool OnHover(HoverEvent e)
-        {
-            icon.FadeColour(hoverColour, 120, Easing.InOutQuint);
-            return base.OnHover(e);
-        }
-
-        protected override void OnHoverLost(HoverLostEvent e)
-        {
-            if (!playing.Value)
-                icon.FadeColour(Color4.White, 120, Easing.InOutQuint);
-            base.OnHoverLost(e);
+            if (button.Enabled.Value)
+                playing.Toggle();
         }
 
         private void playingStateChanged(ValueChangedEvent<bool> e)
         {
-            icon.Icon = e.NewValue ? FontAwesome.Solid.Stop : FontAwesome.Solid.Play;
-            icon.FadeColour(e.NewValue || IsHovered ? hoverColour : Color4.White, 120, Easing.InOutQuint);
-
             if (e.NewValue)
             {
                 if (BeatmapSet == null)
@@ -135,7 +86,7 @@ namespace osu.Game.Overlays.BeatmapListing.Panels
                     return;
                 }
 
-                loading = true;
+                button.Loading = true;
 
                 LoadComponentAsync(Preview = previewTrackManager.Get(beatmapSet), preview =>
                 {
@@ -144,7 +95,7 @@ namespace osu.Game.Overlays.BeatmapListing.Panels
                         return;
 
                     AddInternal(preview);
-                    loading = false;
+                    button.Loading = false;
                     // make sure that the update of value of Playing (and the ensuing value change callbacks)
                     // are marshaled back to the update thread.
                     preview.Stopped += () => Schedule(() => playing.Value = false);
@@ -157,7 +108,7 @@ namespace osu.Game.Overlays.BeatmapListing.Panels
             else
             {
                 Preview?.Stop();
-                loading = false;
+                button.Loading = false;
             }
         }
 
@@ -165,6 +116,89 @@ namespace osu.Game.Overlays.BeatmapListing.Panels
         {
             if (Preview?.Start() != true)
                 playing.Value = false;
+        }
+
+        private class Button : OsuClickableContainer
+        {
+            private readonly SpriteIcon icon;
+            private readonly LoadingSpinner loadingSpinner;
+
+            private readonly IBindable<bool> playing;
+
+            public bool Loading
+            {
+                set
+                {
+                    if (value)
+                    {
+                        icon.FadeTo(0.5f, transition_duration, Easing.OutQuint);
+                        loadingSpinner.Show();
+                    }
+                    else
+                    {
+                        icon.FadeTo(1, transition_duration, Easing.OutQuint);
+                        loadingSpinner.Hide();
+                    }
+                }
+            }
+
+            public Button(PlayButton player)
+            {
+                playing = player.Playing.GetBoundCopy();
+
+                Children = new Drawable[]
+                {
+                    icon = new SpriteIcon
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        FillMode = FillMode.Fit,
+                        RelativeSizeAxes = Axes.Both,
+                        Icon = FontAwesome.Solid.Play,
+                    },
+                    loadingSpinner = new LoadingSpinner
+                    {
+                        Size = new Vector2(15),
+                    },
+                };
+            }
+
+            private Color4 hoverColour;
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                hoverColour = colours.Yellow;
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                Enabled.BindValueChanged(e => this.FadeTo(e.NewValue ? 1 : 0), true);
+
+                playing.BindValueChanged(e =>
+                {
+                    icon.Icon = e.NewValue ? FontAwesome.Solid.Stop : FontAwesome.Solid.Play;
+                    icon.FadeColour(e.NewValue || IsHovered ? hoverColour : Color4.White, 120, Easing.InOutQuint);
+                }, true);
+            }
+
+            protected override bool OnHover(HoverEvent e)
+            {
+                if (!Enabled.Value)
+                    return false;
+
+                icon.FadeColour(hoverColour, 120, Easing.InOutQuint);
+                return base.OnHover(e);
+            }
+
+            protected override void OnHoverLost(HoverLostEvent e)
+            {
+                if (!playing.Value)
+                    icon.FadeColour(Color4.White, 120, Easing.InOutQuint);
+                base.OnHoverLost(e);
+            }
         }
     }
 }
