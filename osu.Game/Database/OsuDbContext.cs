d7f6d81354a9c9be9559ad033648d246cbde1a6e
Steven He <hez2010@outlook.com>
2021-01-28 03:09:30 +0800

    Dependencies upgrade

--- osu.Game/Database/OsuDbContext.cs
+++ osu.Game/Database/OsuDbContext.cs
@@ -111,9 +111,6 @@ namespace osu.Game.Database
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder
-                // this is required for the time being due to the way we are querying in places like BeatmapStore.
-                // if we ever move to having consumers file their own .Includes, or get eager loading support, this could be re-enabled.
-                .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning))
                 .UseSqlite(connectionString, sqliteOptions => sqliteOptions.CommandTimeout(10))
                 .UseLoggerFactory(logger.Value);
         }
