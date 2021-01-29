7586032f3d5f611c4435043067e1c5ee556f0385
Firmatorenio <87902114+bubb13gummy@users.noreply.github.com>
2021-01-29 11:48:51 +0600

    add SV multiplier adjustment to TaikoModDifficultyAdjust

--- osu.Game.Rulesets.Taiko/Mods/TaikoModDifficultyAdjust.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModDifficultyAdjust.cs
@@ -1,11 +1,45 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Taiko.Mods
 {
     public class TaikoModDifficultyAdjust : ModDifficultyAdjust
     {
+        [SettingSource("Slider Velocity", "Adjust a beatmap's set SV", LAST_SETTING_ORDER + 1)]
+        public BindableNumber<float> SliderVelocity { get; } = new BindableFloat
+        {
+            Precision = 0.05f,
+            MinValue = 0.25f,
+            MaxValue = 4,
+            Default = 1,
+            Value = 1,
+        };
+
+        public override string SettingDescription
+        {
+            get
+            {
+                string sliderVelocity = SliderVelocity.IsDefault ? string.Empty : $"SV {SliderVelocity.Value:N1}";
+
+                return string.Join(", ", new[]
+                {
+                    base.SettingDescription,
+                    sliderVelocity
+                }.Where(s => !string.IsNullOrEmpty(s)));
+            }
+        }
+
+        protected override void ApplySettings(BeatmapDifficulty difficulty)
+        {
+            base.ApplySettings(difficulty);
+
+            ApplySetting(SliderVelocity, sv => difficulty.SliderMultiplier *= sv);
+        }
     }
 }
