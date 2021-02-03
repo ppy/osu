03b7817887ea059719094f001a81c5b4f1c9ee36
smoogipoo <smoogipoo@smgi.me>
2021-02-03 22:12:20 +0900

    Add flags to return to classic slider scoring

--- osu.Game.Rulesets.Osu/Objects/SliderHeadCircle.cs
+++ osu.Game.Rulesets.Osu/Objects/SliderHeadCircle.cs
@@ -1,13 +1,25 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Osu.Judgements;
+
 namespace osu.Game.Rulesets.Osu.Objects
 {
     public class SliderHeadCircle : HitCircle
     {
         /// <summary>
-        /// Makes the head circle track the follow circle when the start time is reached.
+        /// Makes this <see cref="SliderHeadCircle"/> track the follow circle when the start time is reached.
+        /// If <c>false</c>, this <see cref="SliderHeadCircle"/> will be pinned to its initial position in the slider.
         /// </summary>
         public bool TrackFollowCircle = true;
+
+        /// <summary>
+        /// Whether to treat this <see cref="SliderHeadCircle"/> as a normal <see cref="HitCircle"/> for judgement purposes.
+        /// If <c>false</c>, judgement will be ignored.
+        /// </summary>
+        public bool JudgeAsNormalHitCircle = true;
+
+        public override Judgement CreateJudgement() => JudgeAsNormalHitCircle ? base.CreateJudgement() : new OsuIgnoreJudgement();
     }
 }
