bd92bbf3715bf39d36c4a80c734c9b84ef2c5892
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-02-05 16:59:13 +0900

    Add classic slider ball tracking

--- osu.Game.Rulesets.Osu/Mods/OsuModClassic.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModClassic.cs
@@ -1,19 +1,22 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModClassic : Mod, IApplicableToHitObject, IApplicableToDrawableRuleset<OsuHitObject>
+    public class OsuModClassic : Mod, IApplicableToHitObject, IApplicableToDrawableHitObjects, IApplicableToDrawableRuleset<OsuHitObject>
     {
         public override string Name => "Classic";
 
@@ -35,6 +38,9 @@ namespace osu.Game.Rulesets.Osu.Mods
 
         [SettingSource("Disable note lock lenience", "Applies note lock to the full hit window.")]
         public Bindable<bool> DisableLenientNoteLock { get; } = new BindableBool(true);
+
+        [SettingSource("Disable exact slider follow circle tracking", "Makes the slider follow circle track its final size at all times.")]
+        public Bindable<bool> DisableExactFollowCircleTracking { get; } = new BindableBool(true);
 
         public void ApplyToHitObject(HitObject hitObject)
         {
@@ -59,6 +65,15 @@ namespace osu.Game.Rulesets.Osu.Mods
 
             if (!DisableLenientNoteLock.Value)
                 osuRuleset.Playfield.HitPolicy = new ObjectOrderedHitPolicy();
+        }
+
+        public void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables)
+        {
+            foreach (var obj in drawables)
+            {
+                if (obj is DrawableSlider slider)
+                    slider.Ball.TrackVisualSize = !DisableExactFollowCircleTracking.Value;
+            }
         }
     }
 }
