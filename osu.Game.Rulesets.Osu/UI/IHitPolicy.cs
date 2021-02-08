f4a31287bfca31bf088f077e014f7af4d68b6232
smoogipoo <smoogipoo@smgi.me>
2021-02-08 20:11:06 +0900

    Add/use IHitObjectContainer interface instead of IEnumerables

--- osu.Game.Rulesets.Osu/UI/IHitPolicy.cs
+++ osu.Game.Rulesets.Osu/UI/IHitPolicy.cs
@@ -1,19 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Osu.UI
 {
     public interface IHitPolicy
     {
         /// <summary>
-        /// Sets the <see cref="DrawableHitObject"/>s which this <see cref="IHitPolicy"/> controls.
+        /// The <see cref="IHitObjectContainer"/> containing the <see cref="DrawableHitObject"/>s which this <see cref="IHitPolicy"/> applies to.
         /// </summary>
-        /// <param name="hitObjects">An enumeration of the <see cref="DrawableHitObject"/>s.</param>
-        void SetHitObjects(IEnumerable<DrawableHitObject> hitObjects);
+        IHitObjectContainer HitObjectContainer { set; }
 
         /// <summary>
         /// Determines whether a <see cref="DrawableHitObject"/> can be hit at a point in time.
