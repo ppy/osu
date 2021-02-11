dddd776802ad1f77f085c9862c317f93a3d1b191
Dean Herbert <pe@ppy.sh>
2021-02-11 16:38:17 +0900

    Add the ability for settings items to have tooltips

--- osu.Game/Overlays/Settings/SettingsItem.cs
+++ osu.Game/Overlays/Settings/SettingsItem.cs
@@ -21,7 +21,7 @@ using osuTK;
 
 namespace osu.Game.Overlays.Settings
 {
-    public abstract class SettingsItem<T> : Container, IFilterable, ISettingsItem, IHasCurrentValue<T>
+    public abstract class SettingsItem<T> : Container, IFilterable, ISettingsItem, IHasCurrentValue<T>, IHasTooltip
     {
         protected abstract Drawable CreateControl();
 
@@ -214,5 +214,7 @@ namespace osu.Game.Overlays.Settings
                 this.FadeColour(bindable.Disabled ? Color4.Gray : buttonColour, 200, Easing.OutQuint);
             }
         }
+
+        public string TooltipText { get; set; }
     }
 }
