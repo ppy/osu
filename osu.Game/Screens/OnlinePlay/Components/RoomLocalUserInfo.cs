e969ca8974d621eb4a089231f65b684f1a3d4f60
Dean Herbert <pe@ppy.sh>
2021-02-16 13:52:42 +0900

    Remove unused using statement that rider could not identify

--- osu.Game/Screens/OnlinePlay/Components/RoomLocalUserInfo.cs
+++ osu.Game/Screens/OnlinePlay/Components/RoomLocalUserInfo.cs
@@ -3,7 +3,6 @@
 
 using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics;
