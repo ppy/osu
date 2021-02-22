3356724c849d90ff771097b9b963ed31571e6fb7
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-02-22 22:45:55 +0100

    Remove unused using directive

--- osu.Game.Tests/Visual/Playlists/TestScenePlaylistsResultsScreen.cs
+++ osu.Game.Tests/Visual/Playlists/TestScenePlaylistsResultsScreen.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
