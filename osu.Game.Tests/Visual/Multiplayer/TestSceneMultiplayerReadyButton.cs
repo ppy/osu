b337499a44c7584c6d1038bd6917f56f86d6da3e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-02-22 14:53:32 +0900

    Update other tests which can benefit from using a shorter beatmap

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerReadyButton.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerReadyButton.cs
@@ -42,7 +42,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
         {
             Dependencies.Cache(rulesets = new RulesetStore(ContextFactory));
             Dependencies.Cache(beatmaps = new BeatmapManager(LocalStorage, ContextFactory, rulesets, null, audio, host, Beatmap.Default));
-            beatmaps.Import(TestResources.GetTestBeatmapForImport(true)).Wait();
+            beatmaps.Import(TestResources.GetQuickTestBeatmapForImport()).Wait();
 
             Add(beatmapTracker = new OnlinePlayBeatmapAvailablilityTracker
             {
