d985b8ab2aafd86c9f4d24fdcd39634de8a0b10c
smoogipoo <smoogipoo@smgi.me>
2021-02-22 17:14:39 +0900

    Increase beatmapset download timeout

--- osu.Game/Online/API/Requests/DownloadBeatmapSetRequest.cs
+++ osu.Game/Online/API/Requests/DownloadBeatmapSetRequest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.IO.Network;
 using osu.Game.Beatmaps;
 
 namespace osu.Game.Online.API.Requests
@@ -13,6 +14,13 @@ namespace osu.Game.Online.API.Requests
             : base(set)
         {
             this.noVideo = noVideo;
+        }
+
+        protected override WebRequest CreateWebRequest()
+        {
+            var req = base.CreateWebRequest();
+            req.Timeout = 60000;
+            return req;
         }
 
         protected override string Target => $@"beatmapsets/{Model.OnlineBeatmapSetID}/download{(noVideo ? "?noVideo=1" : "")}";
