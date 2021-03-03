1ecb1d122a55500204eaea01d2321a1a9c71c707
smoogipoo <smoogipoo@smgi.me>
2021-03-03 21:54:34 +0900

    Fix up TestSceneMultiplayer

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayer.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayer.cs
@@ -1,13 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Game.Online.Multiplayer;
 using osu.Game.Screens.OnlinePlay.Components;
-using osu.Game.Users;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
-    public class TestSceneMultiplayer : MultiplayerTestScene
+    public class TestSceneMultiplayer : ScreenTestScene
     {
         public TestSceneMultiplayer()
         {
@@ -17,30 +17,16 @@ namespace osu.Game.Tests.Visual.Multiplayer
             AddUntilStep("wait for loaded", () => multi.IsLoaded);
         }
 
-        [Test]
-        public void TestOneUserJoinedMultipleTimes()
-        {
-            var user = new User { Id = 33 };
-
-            AddRepeatStep("add user multiple times", () => Client.AddUser(user), 3);
-
-            AddAssert("room has 2 users", () => Client.Room?.Users.Count == 2);
-        }
-
-        [Test]
-        public void TestOneUserLeftMultipleTimes()
-        {
-            var user = new User { Id = 44 };
-
-            AddStep("add user", () => Client.AddUser(user));
-            AddAssert("room has 2 users", () => Client.Room?.Users.Count == 2);
-
-            AddRepeatStep("remove user multiple times", () => Client.RemoveUser(user), 3);
-            AddAssert("room has 1 user", () => Client.Room?.Users.Count == 1);
-        }
-
         private class TestMultiplayer : Screens.OnlinePlay.Multiplayer.Multiplayer
         {
+            [Cached(typeof(StatefulMultiplayerClient))]
+            public readonly TestMultiplayerClient Client;
+
+            public TestMultiplayer()
+            {
+                AddInternal(Client = new TestMultiplayerClient((TestMultiplayerRoomManager)RoomManager));
+            }
+
             protected override RoomManager CreateRoomManager() => new TestMultiplayerRoomManager();
         }
     }
