4b3104be8fed4571e15ab7ce4f4e8da880b47c5a
Joehu <87902114+bubb13gummy@users.noreply.github.com>
2021-03-17 13:13:13 -0700

    Remove available kudosu section from user profile overlay in line with web

--- osu.Game/Overlays/Profile/Sections/Kudosu/KudosuInfo.cs
+++ osu.Game/Overlays/Profile/Sections/Kudosu/KudosuInfo.cs
@@ -23,43 +23,16 @@ namespace osu.Game.Overlays.Profile.Sections.Kudosu
         {
             this.user.BindTo(user);
             CountSection total;
-            CountSection avaliable;
             RelativeSizeAxes = Axes.X;
             AutoSizeAxes = Axes.Y;
             Masking = true;
             CornerRadius = 3;
-            Children = new Drawable[]
-            {
-                new FillFlowContainer
-                {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Direction = FillDirection.Horizontal,
-                    Spacing = new Vector2(5, 0),
-                    Children = new[]
-                    {
-                        total = new CountTotal(),
-                        avaliable = new CountAvailable()
-                    }
-                }
-            };
-            this.user.ValueChanged += u =>
-            {
-                total.Count = u.NewValue?.Kudosu.Total ?? 0;
-                avaliable.Count = u.NewValue?.Kudosu.Available ?? 0;
-            };
+            Child = total = new CountTotal();
+
+            this.user.ValueChanged += u => total.Count = u.NewValue?.Kudosu.Total ?? 0;
         }
 
         protected override bool OnClick(ClickEvent e) => true;
-
-        private class CountAvailable : CountSection
-        {
-            public CountAvailable()
-                : base("Kudosu Avaliable")
-            {
-                DescriptionText.Text = "Kudosu can be traded for kudosu stars, which will help your beatmap get more attention. This is the number of kudosu you haven't traded in yet.";
-            }
-        }
 
         private class CountTotal : CountSection
         {
@@ -86,7 +59,6 @@ namespace osu.Game.Overlays.Profile.Sections.Kudosu
             public CountSection(string header)
             {
                 RelativeSizeAxes = Axes.X;
-                Width = 0.5f;
                 AutoSizeAxes = Axes.Y;
                 Padding = new MarginPadding { Top = 10, Bottom = 20 };
                 Child = new FillFlowContainer
