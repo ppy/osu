10a8db18f6e4e82a3bbba60cee7e78122ddb97c3
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-03-23 16:41:52 +0900

    Implement SoloPlayer's request construction

--- osu.Game/Screens/Play/SoloPlayer.cs
+++ osu.Game/Screens/Play/SoloPlayer.cs
@@ -1,22 +1,34 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Diagnostics;
 using osu.Game.Online.API;
 using osu.Game.Online.Rooms;
+using osu.Game.Online.Solo;
 using osu.Game.Scoring;
 
 namespace osu.Game.Screens.Play
 {
     public class SoloPlayer : SubmittingPlayer
     {
-        public override APIRequest<MultiplayerScore> CreateSubmissionRequest(Score score, int token)
+        protected override APIRequest<MultiplayerScore> CreateSubmissionRequest(Score score, long token)
         {
-            throw new System.NotImplementedException();
+            Debug.Assert(Beatmap.Value.BeatmapInfo.OnlineBeatmapID != null);
+
+            int beatmapId = Beatmap.Value.BeatmapInfo.OnlineBeatmapID.Value;
+
+            return new SubmitSoloScoreRequest(beatmapId, token, score.ScoreInfo);
         }
 
         protected override APIRequest<APIScoreToken> CreateTokenRequestRequest()
         {
-            throw new System.NotImplementedException();
+            if (!(Beatmap.Value.BeatmapInfo.OnlineBeatmapID is int beatmapId))
+                return null;
+
+            return new CreateSoloScoreRequest(beatmapId, Game.VersionHash);
         }
+
+        protected override bool HandleTokenRetrievalFailure(Exception exception) => false;
     }
 }
