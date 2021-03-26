6a4157d1939f513d0ed1167192b9b7007e8db1d6
Dean Herbert <pe@ppy.sh>
2021-03-26 16:13:43 +0900

    Remove unused using statement

--- osu.Game/Screens/OnlinePlay/Lounge/LoungeSubScreen.cs
+++ osu.Game/Screens/OnlinePlay/Lounge/LoungeSubScreen.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Diagnostics;
 using System.Linq;
 using JetBrains.Annotations;
 using osu.Framework.Allocation;
