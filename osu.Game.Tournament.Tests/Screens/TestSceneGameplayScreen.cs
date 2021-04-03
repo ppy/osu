eb26f6f4273c638fac4be82a7a8535bcdf42a820
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-04-03 21:45:11 +0200

    Add failing test case

--- osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreen.cs
+++ osu.Game.Tournament.Tests/Screens/TestSceneGameplayScreen.cs
@@ -1,9 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using NUnit.Framework;
 using osu.Framework.Allocation;
+using osu.Framework.Testing;
 using osu.Game.Tournament.Components;
 using osu.Game.Tournament.Screens.Gameplay;
+using osu.Game.Tournament.Screens.Gameplay.Components;
 
 namespace osu.Game.Tournament.Tests.Screens
 {
@@ -18,5 +22,24 @@ namespace osu.Game.Tournament.Tests.Screens
             Add(new GameplayScreen());
             Add(chat);
         }
+
+        [Test]
+        public void TestWarmup()
+        {
+            checkScoreVisibility(false);
+
+            toggleWarmup();
+            checkScoreVisibility(true);
+
+            toggleWarmup();
+            checkScoreVisibility(false);
+        }
+
+        private void checkScoreVisibility(bool visible)
+            => AddUntilStep($"scores {(visible ? "shown" : "hidden")}",
+                () => this.ChildrenOfType<TeamScore>().All(score => score.Alpha == (visible ? 1 : 0)));
+
+        private void toggleWarmup()
+            => AddStep("toggle warmup", () => this.ChildrenOfType<TourneyButton>().First().Click());
     }
 }
