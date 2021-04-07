1f57b6884d0e944907cf4ce6f2cc12c157fc32c6
smoogipoo <smoogipoo@smgi.me>
2021-04-07 16:19:10 +0900

    Add ready button to test scene

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerSpectateButton.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerSpectateButton.cs
@@ -2,10 +2,22 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Linq;
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+using osu.Game.Beatmaps;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.Multiplayer;
+using osu.Game.Online.Rooms;
+using osu.Game.Rulesets;
 using osu.Game.Screens.OnlinePlay.Multiplayer.Match;
+using osu.Game.Tests.Resources;
 using osuTK;
 using osuTK.Input;
 
@@ -13,22 +25,96 @@ namespace osu.Game.Tests.Visual.Multiplayer
 {
     public class TestSceneMultiplayerSpectateButton : MultiplayerTestScene
     {
-        private MultiplayerSpectateButton button;
+        private MultiplayerSpectateButton spectateButton;
+        private MultiplayerReadyButton readyButton;
+
+        private readonly Bindable<PlaylistItem> selectedItem = new Bindable<PlaylistItem>();
+
+        private BeatmapSetInfo importedSet;
+        private BeatmapManager beatmaps;
+        private RulesetStore rulesets;
+
         private IDisposable readyClickOperation;
+
+        protected override Container<Drawable> Content => content;
+        private readonly Container content;
+
+        public TestSceneMultiplayerSpectateButton()
+        {
+            base.Content.Add(content = new Container
+            {
+                RelativeSizeAxes = Axes.Both
+            });
+        }
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            var dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
+            return dependencies;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(GameHost host, AudioManager audio)
+        {
+            Dependencies.Cache(rulesets = new RulesetStore(ContextFactory));
+            Dependencies.Cache(beatmaps = new BeatmapManager(LocalStorage, ContextFactory, rulesets, null, audio, host, Beatmap.Default));
+
+            var beatmapTracker = new OnlinePlayBeatmapAvailablilityTracker { SelectedItem = { BindTarget = selectedItem } };
+            base.Content.Add(beatmapTracker);
+            Dependencies.Cache(beatmapTracker);
+
+            beatmaps.Import(TestResources.GetQuickTestBeatmapForImport()).Wait();
+        }
 
         [SetUp]
         public new void Setup() => Schedule(() =>
         {
-            Child = button = new MultiplayerSpectateButton
+            importedSet = beatmaps.GetAllUsableBeatmapSetsEnumerable(IncludedDetails.All).First();
+            Beatmap.Value = beatmaps.GetWorkingBeatmap(importedSet.Beatmaps.First());
+            selectedItem.Value = new PlaylistItem
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(200, 50),
-                OnSpectateClick = async () =>
+                Beatmap = { Value = Beatmap.Value.BeatmapInfo },
+                Ruleset = { Value = Beatmap.Value.BeatmapInfo.Ruleset },
+            };
+
+            Child = new FillFlowContainer
+            {
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Vertical,
+                Children = new Drawable[]
                 {
-                    readyClickOperation = OngoingOperationTracker.BeginOperation();
-                    await Client.ToggleSpectate();
-                    readyClickOperation.Dispose();
+                    spectateButton = new MultiplayerSpectateButton
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(200, 50),
+                        OnSpectateClick = async () =>
+                        {
+                            readyClickOperation = OngoingOperationTracker.BeginOperation();
+                            await Client.ToggleSpectate();
+                            readyClickOperation.Dispose();
+                        }
+                    },
+                    readyButton = new MultiplayerReadyButton
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(200, 50),
+                        OnReadyClick = async () =>
+                        {
+                            readyClickOperation = OngoingOperationTracker.BeginOperation();
+
+                            if (Client.IsHost && Client.LocalUser?.State == MultiplayerUserState.Ready)
+                            {
+                                await Client.StartMatch();
+                                return;
+                            }
+
+                            await Client.ToggleReady();
+                            readyClickOperation.Dispose();
+                        }
+                    }
                 }
             };
         });
@@ -37,17 +123,26 @@ namespace osu.Game.Tests.Visual.Multiplayer
         [TestCase(MultiplayerUserState.Ready)]
         public void TestToggleWhenIdle(MultiplayerUserState initialState)
         {
-            addClickButtonStep();
+            addClickSpectateButtonStep();
             AddAssert("user is spectating", () => Client.Room?.Users[0].State == MultiplayerUserState.Spectating);
 
-            addClickButtonStep();
+            addClickSpectateButtonStep();
             AddAssert("user is idle", () => Client.Room?.Users[0].State == MultiplayerUserState.Idle);
         }
 
-        private void addClickButtonStep() => AddStep("click button", () =>
+        private void addClickSpectateButtonStep() => AddStep("click spectate button", () =>
         {
-            InputManager.MoveMouseTo(button);
+            InputManager.MoveMouseTo(spectateButton);
             InputManager.Click(MouseButton.Left);
         });
+
+        private void addClickReadyButtonStep() => AddStep("click ready button", () =>
+        {
+            InputManager.MoveMouseTo(readyButton);
+            InputManager.Click(MouseButton.Left);
+        });
+
+        private void assertReadyButtonEnablement(bool shouldBeEnabled)
+            => AddAssert($"ready button {(shouldBeEnabled ? "is" : "is not")} enabled", () => readyButton.ChildrenOfType<OsuButton>().Single().Enabled.Value == shouldBeEnabled);
     }
 }
