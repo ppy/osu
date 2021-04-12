8f84abf34807bae9679b2f70cdfd8c082eb25e03
Salman Ahmed <frenzibyte@gmail.com>
2021-04-12 21:51:04 +0300

    Display "replays watched" tooltip for replays subsection

--- osu.Game/Overlays/Profile/Sections/Historical/UserHistoryGraph.cs
+++ osu.Game/Overlays/Profile/Sections/Historical/UserHistoryGraph.cs
@@ -11,20 +11,22 @@ namespace osu.Game.Overlays.Profile.Sections.Historical
 {
     public class UserHistoryGraph : UserGraph<DateTime, long>
     {
+        private readonly string tooltipCounterName;
+
         [CanBeNull]
         public UserHistoryCount[] Values
         {
             set => Data = value?.Select(v => new KeyValuePair<DateTime, long>(v.Date, v.Count)).ToArray();
         }
 
-        /// <summary>
-        /// Text describing the value being plotted on the graph, which will be displayed as a prefix to the value in the <see cref="HistoryGraphTooltip"/>.
-        /// </summary>
-        public string TooltipCounterName { get; set; } = "Plays";
+        public UserHistoryGraph(string tooltipCounterName)
+        {
+            this.tooltipCounterName = tooltipCounterName;
+        }
 
         protected override float GetDataPointHeight(long playCount) => playCount;
 
-        protected override UserGraphTooltip GetTooltip() => new HistoryGraphTooltip(TooltipCounterName);
+        protected override UserGraphTooltip GetTooltip() => new HistoryGraphTooltip(tooltipCounterName);
 
         protected override object GetTooltipContent(DateTime date, long playCount)
         {
