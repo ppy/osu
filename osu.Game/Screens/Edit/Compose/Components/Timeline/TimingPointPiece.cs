be08b9d1eff63dd7ec47c9e26323b90c3e63d208
Dean Herbert <pe@ppy.sh>
2021-04-14 20:54:29 +0900

    Combine logic of Difficulty and Timing pieces where feasible

--- osu.Game/Screens/Edit/Compose/Components/Timeline/TimingPointPiece.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/TimingPointPiece.cs
@@ -3,58 +3,27 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
 
 namespace osu.Game.Screens.Edit.Compose.Components.Timeline
 {
-    public class TimingPointPiece : CompositeDrawable
+    public class TimingPointPiece : TopPointPiece
     {
-        private readonly TimingControlPoint point;
-
         private readonly BindableNumber<double> beatLength;
-        private OsuSpriteText bpmText;
 
         public TimingPointPiece(TimingControlPoint point)
+            : base(point)
         {
-            this.point = point;
             beatLength = point.BeatLengthBindable.GetBoundCopy();
         }
 
         [BackgroundDependencyLoader]
         private void load(OsuColour colours)
         {
-            Margin = new MarginPadding { Vertical = 10 };
-
-            const float corner_radius = 5;
-
-            AutoSizeAxes = Axes.Both;
-            Masking = true;
-            CornerRadius = corner_radius;
-
-            InternalChildren = new Drawable[]
-            {
-                new Box
-                {
-                    Colour = point.GetRepresentingColour(colours),
-                    RelativeSizeAxes = Axes.Both,
-                },
-                bpmText = new OsuSpriteText
-                {
-                    Alpha = 0.9f,
-                    Padding = new MarginPadding { Vertical = 3, Horizontal = 6 },
-                    Font = OsuFont.Default.With(size: 20, weight: FontWeight.SemiBold),
-                    Colour = colours.B5,
-                }
-            };
-
             beatLength.BindValueChanged(beatLength =>
             {
-                bpmText.Text = $"{60000 / beatLength.NewValue:n1} BPM";
+                Label.Text = $"{60000 / beatLength.NewValue:n1} BPM";
             }, true);
         }
     }
