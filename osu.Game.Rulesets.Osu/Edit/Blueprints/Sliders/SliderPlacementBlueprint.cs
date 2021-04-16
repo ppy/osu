d38e294d96e8a887ab188578925195f159c52ed6
Dean Herbert <pe@ppy.sh>
2021-04-16 15:22:32 +0900

    Centralise length validation function

--- osu.Game.Rulesets.Osu/Edit/Blueprints/Sliders/SliderPlacementBlueprint.cs
+++ osu.Game.Rulesets.Osu/Edit/Blueprints/Sliders/SliderPlacementBlueprint.cs
@@ -135,7 +135,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.Sliders
         private void endCurve()
         {
             updateSlider();
-            EndPlacement(HitObject.Path.ExpectedDistance?.Value > 0);
+            EndPlacement(HitObject.Path.HasValidLength);
         }
 
         protected override void Update()
