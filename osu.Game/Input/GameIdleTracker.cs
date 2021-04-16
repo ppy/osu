4c92678468109f2839892def4ff004cde5af4986
jvyden <87902114+bubb13gummy@users.noreply.github.com>
2021-04-16 05:01:58 -0400

    Reset SessionStatics on activity
    Closes #12424

--- osu.Game/Input/GameIdleTracker.cs
+++ osu.Game/Input/GameIdleTracker.cs
@@ -1,7 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Framework.Input;
+using osu.Game.Configuration;
 
 namespace osu.Game.Input
 {
@@ -9,9 +11,16 @@ namespace osu.Game.Input
     {
         private InputManager inputManager;
 
-        public GameIdleTracker(int time)
+        public GameIdleTracker(int time, SessionStatics statics)
             : base(time)
         {
+            IsIdle.ValueChanged += _ => UpdateStatics(_, statics);
+        }
+
+        protected static void UpdateStatics(ValueChangedEvent<bool> e, SessionStatics statics)
+        {
+            if (e.OldValue != e.NewValue && e.NewValue)
+                statics.ResetValues();
         }
 
         protected override void LoadComplete()
