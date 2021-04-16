84bc81a6deda614ce407730d62750c7a52195671
ekrctb <ekrctb@gmail.com>
2021-04-16 12:31:32 +0900

    Make FramedReplayInputHandler.CurrentTime non-null

--- osu.Game.Rulesets.Catch/Replays/CatchFramedReplayInputHandler.cs
+++ osu.Game.Rulesets.Catch/Replays/CatchFramedReplayInputHandler.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using osu.Framework.Input.StateChanges;
 using osu.Framework.Utils;
@@ -29,9 +28,7 @@ namespace osu.Game.Rulesets.Catch.Replays
                 if (frame == null)
                     return null;
 
-                Debug.Assert(CurrentTime != null);
-
-                return NextFrame != null ? Interpolation.ValueAt(CurrentTime.Value, frame.Position, NextFrame.Position, frame.Time, NextFrame.Time) : frame.Position;
+                return NextFrame != null ? Interpolation.ValueAt(CurrentTime, frame.Position, NextFrame.Position, frame.Time, NextFrame.Time) : frame.Position;
             }
         }
 
