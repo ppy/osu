5913922f2fabe0f0d2ab1597e80d744167d2b135
Fabian <87902114+bubb13gummy@users.noreply.github.com>
2021-04-17 16:01:23 +0200

    Add floating fruits mod

---
+++ osu.Game.Rulesets.Catch/Mods/CatchModFloatingFruits.cs
@@ -0,0 +1,23 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Screens.Play;
+
+namespace osu.Game.Rulesets.Catch.Mods
+{
+    public class CatchModFloatingFruits : Mod, IApplicableToPlayer
+    {
+        public override string Name => "Floating Fruits";
+        public override string Acronym => "FF";
+        public override string Description => "The fruits are... floating?";
+        public override double ScoreMultiplier => 1;
+        public override IconUsage? Icon => FontAwesome.Brands.Fly;
+
+        public void ApplyToPlayer(Player player)
+        {
+            player.DrawableRuleset.Anchor = Anchor.Centre;
+            player.DrawableRuleset.Origin = Anchor.Centre;
+            player.DrawableRuleset.Scale = new osuTK.Vector2(1, -1);
+        }
+    }
+}
