38a7c590c4d07ac17c9746c8feac2aa4a59c63af
PercyDan54 <50285552+PercyDan54@users.noreply.github.com>
2021-04-18 20:57:25 +0800

    Make versionString private

--- osu.Game/Beatmaps/BeatmapInfo.cs
+++ osu.Game/Beatmaps/BeatmapInfo.cs
@@ -128,7 +128,7 @@ namespace osu.Game.Beatmaps
         // Metadata
         public string Version { get; set; }
 
-        public string VersionString => string.IsNullOrEmpty(Version) ? string.Empty : $"[{Version}]";
+        private string versionString => string.IsNullOrEmpty(Version) ? string.Empty : $"[{Version}]";
 
         [JsonProperty("difficulty_rating")]
         public double StarDifficulty { get; set; }
@@ -146,12 +146,12 @@ namespace osu.Game.Beatmaps
             Version
         }.Concat(Metadata?.SearchableTerms ?? Enumerable.Empty<string>()).Where(s => !string.IsNullOrEmpty(s)).ToArray();
 
-        public override string ToString() => $"{Metadata ?? BeatmapSet?.Metadata} {VersionString}".Trim();
+        public override string ToString() => $"{Metadata ?? BeatmapSet?.Metadata} {versionString}".Trim();
 
         public RomanisableString ToRomanisableString()
         {
             var metadata = (Metadata ?? BeatmapSet?.Metadata)?.ToRomanisableString() ?? new RomanisableString(null, null);
-            return new RomanisableString($"{metadata.GetPreferred(true)} {VersionString}".Trim(), $"{metadata.GetPreferred(false)} {VersionString}".Trim());
+            return new RomanisableString($"{metadata.GetPreferred(true)} {versionString}".Trim(), $"{metadata.GetPreferred(false)} {versionString}".Trim());
         }
 
         public bool Equals(BeatmapInfo other)
