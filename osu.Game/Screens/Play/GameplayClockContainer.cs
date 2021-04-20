9fb88dbb28c72485afbd033b2d736453d35254fa
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-04-20 14:09:54 +0900

    Remove unused usings

--- osu.Game/Screens/Play/GameplayClockContainer.cs
+++ osu.Game/Screens/Play/GameplayClockContainer.cs
@@ -1,12 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Globalization;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Logging;
 using osu.Framework.Timing;
 
 namespace osu.Game.Screens.Play
