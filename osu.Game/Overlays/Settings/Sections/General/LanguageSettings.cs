1ddcef997dc73396a390808ff1cd0ff29ba2c0fb
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-04-20 17:06:01 +0900

    Add basic language switching ability

--- osu.Game/Overlays/Settings/Sections/General/LanguageSettings.cs
+++ osu.Game/Overlays/Settings/Sections/General/LanguageSettings.cs
@@ -1,27 +1,45 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
+using osu.Game.Localisation;
 
 namespace osu.Game.Overlays.Settings.Sections.General
 {
     public class LanguageSettings : SettingsSubsection
     {
+        private SettingsDropdown<Language> languageSelection;
+        private Bindable<string> frameworkLocale;
+
         protected override string Header => "Language";
 
         [BackgroundDependencyLoader]
         private void load(FrameworkConfigManager frameworkConfig)
         {
+            frameworkLocale = frameworkConfig.GetBindable<string>(FrameworkSetting.Locale);
+
             Children = new Drawable[]
             {
+                languageSelection = new SettingsEnumDropdown<Language>
+                {
+                    LabelText = "Language",
+                },
                 new SettingsCheckbox
                 {
                     LabelText = "Prefer metadata in original language",
                     Current = frameworkConfig.GetBindable<bool>(FrameworkSetting.ShowUnicode)
                 },
             };
+
+            if (!Enum.TryParse<Language>(frameworkLocale.Value, out var locale))
+                locale = Language.en;
+            languageSelection.Current.Value = locale;
+
+            languageSelection.Current.BindValueChanged(val => frameworkLocale.Value = val.NewValue.ToString());
         }
     }
 }
