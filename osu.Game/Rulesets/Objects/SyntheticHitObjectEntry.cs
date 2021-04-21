5301fff7c5c719a2db133193853a52306e9d23a6
ekrctb <87902114+bubb13gummy@users.noreply.github.com>
2021-04-21 10:02:50 +0900

    Revert "Fix wrong InitialLifetimeOffset is used for a non-pooled DHO."
    This reverts commit 6e462bde

--- osu.Game/Rulesets/Objects/SyntheticHitObjectEntry.cs
+++ osu.Game/Rulesets/Objects/SyntheticHitObjectEntry.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable enable
-
 using osu.Game.Rulesets.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Objects
@@ -13,8 +11,8 @@ namespace osu.Game.Rulesets.Objects
     /// </summary>
     internal class SyntheticHitObjectEntry : HitObjectLifetimeEntry
     {
-        public SyntheticHitObjectEntry(HitObject hitObject, double initialLifetimeStart)
-            : base(hitObject, initialLifetimeStart)
+        public SyntheticHitObjectEntry(HitObject hitObject)
+            : base(hitObject)
         {
         }
     }
