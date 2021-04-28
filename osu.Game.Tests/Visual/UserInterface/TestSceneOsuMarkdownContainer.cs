f4d1fc49ccf6191bc85f42ac31eec2813a7c3f0b
Gagah Pangeran Rosfatiputra <87902114+bubb13gummy@users.noreply.github.com>
2021-04-28 10:11:29 +0700

    add OsuMarkdownQuoteBlock
    Reference:
    https://github.com/ppy/osu-web/blob/d56352aeefc412507c3dab7c16e3e3118421b436/resources/assets/less/base.less#L7-L10

--- osu.Game.Tests/Visual/UserInterface/TestSceneOsuMarkdownContainer.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneOsuMarkdownContainer.cs
@@ -75,5 +75,15 @@ This is markdown code block.
 Line below";
             });
         }
+
+        [Test]
+        public void TestQuote()
+        {
+            AddStep("Add quote", () =>
+            {
+                markdownContainer.Text =
+                    @"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+            });
+        }
     }
 }
