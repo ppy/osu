40a26156e1ca3a0bcab4c9ec7ca6437791138a5d
Gagah Pangeran Rosfatiputra <87902114+bubb13gummy@users.noreply.github.com>
2021-04-28 10:53:12 +0700

    add OsuMarkdownTable

--- osu.Game.Tests/Visual/UserInterface/TestSceneOsuMarkdownContainer.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneOsuMarkdownContainer.cs
@@ -85,5 +85,19 @@ Line below";
                     @"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
             });
         }
+
+        [Test]
+        public void TestTable()
+        {
+            AddStep("Add Table", () =>
+            {
+                markdownContainer.Text =
+                    @"| Left Aligned  | Center Aligned | Right Aligned |
+| :------------------- | :--------------------: | ---------------------:|
+| Long Align Left Text | Long Align Center Text | Long Align Right Text |
+| Align Left           |      Align Center      |           Align Right |
+| Left                 |         Center         |                 Right |";
+            });
+        }
     }
 }
