126056c43626cfbe5fcb9906ceb8d8e8425029a9
PercyDan54 <50285552+PercyDan54@users.noreply.github.com>
2021-04-28 19:27:18 +0800

    Fix precision loss on exporting legacy replays

--- osu.Game/Scoring/Legacy/LegacyScoreEncoder.cs
+++ osu.Game/Scoring/Legacy/LegacyScoreEncoder.cs
@@ -95,8 +95,9 @@ namespace osu.Game.Scoring.Legacy
 
                     foreach (var f in score.Replay.Frames.OfType<IConvertibleReplayFrame>().Select(f => f.ToLegacy(beatmap)))
                     {
-                        replayData.Append(FormattableString.Invariant($"{(int)Math.Round(f.Time - lastF.Time)}|{f.MouseX ?? 0}|{f.MouseY ?? 0}|{(int)f.ButtonState},"));
+                        replayData.Append(FormattableString.Invariant($"{Math.Round(f.Time - lastF.Time)}|{f.MouseX ?? 0}|{f.MouseY ?? 0}|{(int)f.ButtonState},"));
                         lastF = f;
+                        lastF.Time = Math.Round(f.Time);
                     }
                 }
 
