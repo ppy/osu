8381a463dc5a6a287c3f047c7c54e86e673f1eef
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-04-28 19:00:16 +0900

    Add `DrawableRuleset` to the skin editor test to get a hit error meter to display

--- osu.Game.Tests/Visual/Gameplay/TestSceneSkinEditor.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSkinEditor.cs
@@ -2,10 +2,15 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 using osu.Game.Rulesets;
+using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens.Play;
 using osu.Game.Skinning.Editor;
 using osuTK.Input;
@@ -21,13 +26,32 @@ namespace osu.Game.Tests.Visual.Gameplay
             {
                 SetContents(() =>
                 {
-                    var hudOverlay = new HUDOverlay(null, null, null, Array.Empty<Mod>());
+                    var ruleset = new OsuRuleset();
+                    var working = CreateWorkingBeatmap(ruleset.RulesetInfo);
+                    var beatmap = working.GetPlayableBeatmap(ruleset.RulesetInfo);
+
+                    ScoreProcessor scoreProcessor = new ScoreProcessor();
+
+                    var drawableRuleset = ruleset.CreateDrawableRulesetWith(beatmap);
+
+                    var hudOverlay = new HUDOverlay(scoreProcessor, null, drawableRuleset, Array.Empty<Mod>());
 
                     // Add any key just to display the key counter visually.
                     hudOverlay.KeyCounter.Add(new KeyCounterKeyboard(Key.Space));
                     hudOverlay.ComboCounter.Current.Value = 1;
 
-                    return new SkinEditor(hudOverlay);
+                    // Apply a miss judgement
+                    scoreProcessor.ApplyResult(new JudgementResult(new HitObject(), new Judgement()) { Type = HitResult.Good });
+
+                    return new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Children = new Drawable[]
+                        {
+                            drawableRuleset,
+                            new SkinEditor(hudOverlay),
+                        }
+                    };
                 });
             });
         }
