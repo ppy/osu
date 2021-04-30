de11c308f0b021c140259e0c62fdc24a07f6d132
Gagah Pangeran Rosfatiputra <87902114+bubb13gummy@users.noreply.github.com>
2021-04-30 09:43:05 +0700

    add OsuMarkdownListItem

---
+++ osu.Game/Graphics/Containers/Markdown/OsuMarkdownListItem.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osuTK;
+
+namespace osu.Game.Graphics.Containers.Markdown
+{
+    public class OsuMarkdownListItem : FillFlowContainer
+    {
+        public OsuMarkdownListItem()
+        {
+            AutoSizeAxes = Axes.Y;
+            RelativeSizeAxes = Axes.X;
+
+            Direction = FillDirection.Vertical;
+            Spacing = new Vector2(10, 10);
+        }
+    }
+}
