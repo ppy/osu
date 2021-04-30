e3cc4561abcc0b8a7ec9b0c53e36c3339cb8c6a9
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-04-30 10:35:40 +0700

    add bullet marker in OsuMarkdownListItem

--- osu.Game/Graphics/Containers/Markdown/OsuMarkdownListItem.cs
+++ osu.Game/Graphics/Containers/Markdown/OsuMarkdownListItem.cs
@@ -1,8 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Containers.Markdown;
 using osuTK;
 
 namespace osu.Game.Graphics.Containers.Markdown
@@ -10,6 +12,9 @@ namespace osu.Game.Graphics.Containers.Markdown
     public class OsuMarkdownListItem : CompositeDrawable
     {
         private const float default_left_padding = 20;
+
+        [Resolved]
+        private IMarkdownTextComponent parentTextComponent { get; set; }
 
         public FillFlowContainer Content { get; }
 
@@ -29,6 +34,19 @@ namespace osu.Game.Graphics.Containers.Markdown
                     Spacing = new Vector2(10, 10),
                 }
             };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            var marker = parentTextComponent.CreateSpriteText();
+            marker.Text = "●";
+            marker.Font = OsuFont.GetFont(size: marker.Font.Size / 2);
+            marker.Origin = Anchor.Centre;
+            marker.X = -default_left_padding / 2;
+            marker.Y = marker.Font.Size;
+
+            AddInternal(marker);
         }
     }
 }
