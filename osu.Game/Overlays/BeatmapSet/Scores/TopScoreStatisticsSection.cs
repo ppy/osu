bc15edb85b6b56da887d2c9f374aa6abd16947db
Joseph Madamba <87902114+bubb13gummy@users.noreply.github.com>
2021-05-01 14:14:07 -0700

    Use equality operator instead of null coalescing
    Co-Authored-By: Salman Ahmed <frenzibyte@gmail.com>

--- osu.Game/Overlays/BeatmapSet/Scores/TopScoreStatisticsSection.cs
+++ osu.Game/Overlays/BeatmapSet/Scores/TopScoreStatisticsSection.cs
@@ -112,7 +112,7 @@ namespace osu.Game.Overlays.BeatmapSet.Scores
                 accuracyColumn.Text = value.DisplayAccuracy;
                 maxComboColumn.Text = $@"{value.MaxCombo:N0}x";
 
-                ppColumn.Alpha = value.Beatmap?.Status.GrantsPerformancePoints() ?? false ? 1 : 0;
+                ppColumn.Alpha = value.Beatmap?.Status.GrantsPerformancePoints() == true ? 1 : 0;
                 ppColumn.Text = $@"{value.PP:N0}";
 
                 statisticsColumns.ChildrenEnumerable = value.GetStatisticsForDisplay().Select(createStatisticsColumn);
