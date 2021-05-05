377af38d94541c48cab0d7083cae089b28916f20
Dean Herbert <pe@ppy.sh>
2021-05-05 17:59:49 +0900

    Remove unnecessary pixelSnapping parameter
    This was only required because there was text being rendered to the
    `BufferedContainer` content until now. Removing this should allow for
    better resolution in the background display (due to using a better
    minify scale mode).

--- osu.Game/Screens/Select/BeatmapInfoWedgeBackground.cs
+++ osu.Game/Screens/Select/BeatmapInfoWedgeBackground.cs
@@ -27,7 +27,7 @@ namespace osu.Game.Screens.Select
         {
             RelativeSizeAxes = Axes.Both;
 
-            InternalChild = new BufferedContainer(pixelSnapping: true)
+            InternalChild = new BufferedContainer()
             {
                 CacheDrawnFrameBuffer = true,
                 RelativeSizeAxes = Axes.Both,
