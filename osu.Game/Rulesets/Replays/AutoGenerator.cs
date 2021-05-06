d547c3668ddb96ed351df23fd1b430a76b900ab5
ekrctb <87902114+bubb13gummy@users.noreply.github.com>
2021-05-06 21:43:30 +0900

    Introduce AutoGenerator subclass for frame based replay generation

--- osu.Game/Rulesets/Replays/AutoGenerator.cs
+++ osu.Game/Rulesets/Replays/AutoGenerator.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Beatmaps;
@@ -7,34 +7,27 @@ using osu.Game.Rulesets.Objects;
 
 namespace osu.Game.Rulesets.Replays
 {
-    public abstract class AutoGenerator : IAutoGenerator
+    public abstract class AutoGenerator
     {
         /// <summary>
-        /// Creates the auto replay and returns it.
-        /// Every subclass of OsuAutoGeneratorBase should implement this!
+        /// The default duration of a key press in milliseconds.
         /// </summary>
-        public abstract Replay Generate();
-
-        #region Parameters
+        public const double KEY_UP_DELAY = 50;
 
         /// <summary>
-        /// The beatmap we're making.
+        /// The beatmap the autoplay is generated for.
         /// </summary>
-        protected IBeatmap Beatmap;
-
-        #endregion
+        protected IBeatmap Beatmap { get; }
 
         protected AutoGenerator(IBeatmap beatmap)
         {
             Beatmap = beatmap;
         }
 
-        #region Constants
-
-        // Shared amongst all modes
-        public const double KEY_UP_DELAY = 50;
-
-        #endregion
+        /// <summary>
+        /// Generate the replay of the autoplay.
+        /// </summary>
+        public abstract Replay Generate();
 
         protected virtual HitObject GetNextObject(int currentIndex)
         {
