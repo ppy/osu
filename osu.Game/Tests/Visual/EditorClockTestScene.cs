bed7d84bd1c69be599de4fa8b02609df8927f63e
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2021-05-07 08:27:56 +0300

    Revert wrong not-null track changes
    This reverts commit e8adb41ad106c566b77a2d50b871ab7d026d2994.
    This reverts commit fe9329e3ebbf918fae21547f60f43265a8a3345d.

--- osu.Game/Tests/Visual/EditorClockTestScene.cs
+++ osu.Game/Tests/Visual/EditorClockTestScene.cs
@@ -4,6 +4,7 @@
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Input.Events;
+using osu.Framework.Timing;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Screens.Edit;
@@ -45,7 +46,7 @@ namespace osu.Game.Tests.Visual
         private void beatmapChanged(ValueChangedEvent<WorkingBeatmap> e)
         {
             Clock.ControlPointInfo = e.NewValue.Beatmap.ControlPointInfo;
-            Clock.ChangeSource(e.NewValue.Track);
+            Clock.ChangeSource((IAdjustableClock)e.NewValue.Track ?? new StopwatchClock());
             Clock.ProcessFrame();
         }
 
