b248b2e5e3bf8b62ea94b9e1d5eb85e3809d448e
Dean Herbert <pe@ppy.sh>
2021-05-10 22:43:48 +0900

    Hook up full save/load flow

--- osu.Game/Skinning/Editor/SkinBlueprintContainer.cs
+++ osu.Game/Skinning/Editor/SkinBlueprintContainer.cs
@@ -1,12 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.IO;
 using System.Linq;
-using Newtonsoft.Json;
 using osu.Framework.Graphics;
 using osu.Framework.Testing;
-using osu.Game.Extensions;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Screens.Edit.Compose.Components;
 
@@ -31,11 +28,6 @@ namespace osu.Game.Skinning.Editor
         private void checkForComponents()
         {
             ISkinnableComponent[] skinnableComponents = target.ChildrenOfType<ISkinnableComponent>().ToArray();
-
-            // todo: the OfType() call can be removed with better IDrawable support.
-            string json = JsonConvert.SerializeObject(skinnableComponents.OfType<Drawable>().Select(d => d.CreateSerialisedInformation()), new JsonSerializerSettings { Formatting = Formatting.Indented });
-
-            File.WriteAllText("/Users/Dean/json-out.json", json);
 
             foreach (var c in skinnableComponents)
                 AddBlueprintFor(c);
