7ca3e13712b974ce4cee0b431602ddf2fa654eed
Andrei Zavatski <megaman9919@gmail.com>
2021-05-10 07:43:01 +0300

    Implement basic years panel

---
+++ osu.Game.Tests/Visual/Online/TestSceneNewsYearsPanel.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Overlays.News.Sidebar;
+using osu.Framework.Allocation;
+using osu.Game.Overlays;
+
+namespace osu.Game.Tests.Visual.Online
+{
+    public class TestSceneNewsYearsPanel : OsuTestScene
+    {
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Purple);
+
+        private readonly YearsPanel panel;
+
+        public TestSceneNewsYearsPanel()
+        {
+            Add(panel = new YearsPanel()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            AddStep("Load years", () => panel.Years = new[] { 1000, 2000, 3000, 4000 });
+            AddStep("Load different years", () => panel.Years = new[] { 1001, 2001, 3001, 4001, 5001, 6001, 7001, 8001 });
+        }
+    }
+}
