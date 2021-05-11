7fe4a998170b331c589df7584b4e7090f3be4b2c
Naxess <30292137+Naxesss@users.noreply.github.com>
2021-05-11 04:07:16 +0200

    Show difficulty interpretation directly
    ...rather than through a section, as it only contains one element anyway.

--- osu.Game/Screens/Edit/Verify/InterpretationSection.cs
+++ osu.Game/Screens/Edit/Verify/InterpretationSection.cs
@@ -3,33 +3,39 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Beatmaps;
 using osu.Game.Overlays.Settings;
 
 namespace osu.Game.Screens.Edit.Verify
 {
-    internal class InterpretationSection : IssueSection
+    internal class InterpretationSection : CompositeDrawable
     {
-        protected override string SectionName => "Interpretation";
+        private readonly IssueList issueList;
 
         public InterpretationSection(IssueList issueList)
-            : base(issueList)
         {
+            this.issueList = issueList;
         }
 
         [BackgroundDependencyLoader]
         private void load()
         {
+            RelativeSizeAxes = Axes.X;
+            AutoSizeAxes = Axes.Y;
+
+            Padding = new MarginPadding(10);
+
             var dropdown = new SettingsEnumDropdown<DifficultyRating>
             {
                 Anchor = Anchor.CentreLeft,
                 Origin = Anchor.CentreLeft,
-                LabelText = "Difficulty",
+                LabelText = "Difficulty Interpretation",
                 TooltipText = "Affects checks that depend on difficulty level"
             };
-            dropdown.Current.BindTo(IssueList.InterpretedDifficulty);
 
-            Flow.Add(dropdown);
+            dropdown.Current.BindTo(issueList.InterpretedDifficulty);
+            InternalChildren = new Drawable[] { dropdown };
         }
     }
 }
