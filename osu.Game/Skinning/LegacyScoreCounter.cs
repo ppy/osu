d5fe4f0f72e404f9527e2ccfe444dcd6645de0fc
Dean Herbert <pe@ppy.sh>
2021-05-11 14:34:49 +0900

    Remove unused skin resolution in `LegacyScoreCounter`

--- osu.Game/Skinning/LegacyScoreCounter.cs
+++ osu.Game/Skinning/LegacyScoreCounter.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Screens.Play.HUD;
@@ -13,9 +12,6 @@ namespace osu.Game.Skinning
     {
         protected override double RollingDuration => 1000;
         protected override Easing RollingEasing => Easing.Out;
-
-        [Resolved]
-        private ISkinSource skin { get; set; }
 
         public LegacyScoreCounter()
             : base(6)
