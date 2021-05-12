5692cecaa47e9e071fb6079d9f3847b8b1de0972
smoogipoo <smoogipoo@smgi.me>
2021-05-12 16:35:05 +0900

    Initial implementation of DHO pooling

--- osu.Game.Rulesets.Mania/Objects/Drawables/DrawableManiaHitObject.cs
+++ osu.Game.Rulesets.Mania/Objects/Drawables/DrawableManiaHitObject.cs
@@ -50,6 +50,7 @@ namespace osu.Game.Rulesets.Mania.Objects.Drawables
         protected DrawableManiaHitObject(ManiaHitObject hitObject)
             : base(hitObject)
         {
+            RelativeSizeAxes = Axes.X;
         }
 
         [BackgroundDependencyLoader(true)]
@@ -60,6 +61,22 @@ namespace osu.Game.Rulesets.Mania.Objects.Drawables
 
             Direction.BindTo(scrollingInfo.Direction);
             Direction.BindValueChanged(OnDirectionChanged, true);
+        }
+
+        protected override void OnApply()
+        {
+            base.OnApply();
+
+            if (ParentHitObject != null)
+                AccentColour.BindTo(ParentHitObject.AccentColour);
+        }
+
+        protected override void OnFree()
+        {
+            base.OnFree();
+
+            if (ParentHitObject != null)
+                AccentColour.UnbindFrom(ParentHitObject.AccentColour);
         }
 
         private double computedLifetimeStart;
@@ -147,12 +164,11 @@ namespace osu.Game.Rulesets.Mania.Objects.Drawables
     public abstract class DrawableManiaHitObject<TObject> : DrawableManiaHitObject
         where TObject : ManiaHitObject
     {
-        public new readonly TObject HitObject;
+        public new TObject HitObject => (TObject)base.HitObject;
 
         protected DrawableManiaHitObject(TObject hitObject)
             : base(hitObject)
         {
-            HitObject = hitObject;
         }
     }
 }
