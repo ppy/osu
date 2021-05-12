0af7f656021630215d3a3ffbdd9279793d5654ce
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-05-12 14:11:40 +0900

    Place new skin components at the centre of the screen by default

--- osu.Game/Skinning/ISkinnableTarget.cs
+++ osu.Game/Skinning/ISkinnableTarget.cs
@@ -1,12 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+
 namespace osu.Game.Skinning
 {
     /// <summary>
     /// Denotes a container which can house <see cref="ISkinnableComponent"/>s.
     /// </summary>
-    public interface ISkinnableTarget
+    public interface ISkinnableTarget : IDrawable
     {
         public SkinnableTarget Target { get; }
 
