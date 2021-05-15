3e7b7a6d03a92af73d60e6eb5ba932d3319f03ae
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2021-05-15 18:36:46 +0300

    Remove unused using directive
    ...damn it

--- osu.Game.Rulesets.Catch.Tests/TestSceneCatchPlayerLegacySkin.cs
+++ osu.Game.Rulesets.Catch.Tests/TestSceneCatchPlayerLegacySkin.cs
@@ -3,7 +3,6 @@
 
 using System.Linq;
 using NUnit.Framework;
-using osu.Framework.Allocation;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Screens;
 using osu.Framework.Testing;
