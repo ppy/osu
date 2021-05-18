e7d2f42149955a0fb623c39809f279b34f4a35a0
Salman Ahmed <frenzibyte@gmail.com>
2021-05-18 17:46:15 +0300

    Revert "Merge branch 'fix-legacy-skin-test' into catch-hide-combo-workaround"
    This reverts commit 380d004683ad658d9ecbc408f7844eaa4c2b43c8, reversing
    changes made to ff419af5128a3e0f2d188e10fcb05aed712ac128.

--- osu.Game/Tests/Visual/LegacySkinPlayerTestScene.cs
+++ osu.Game/Tests/Visual/LegacySkinPlayerTestScene.cs
@@ -1,17 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
-using osu.Framework.Audio;
 using osu.Framework.IO.Stores;
-using osu.Framework.Testing;
-using osu.Framework.Timing;
-using osu.Game.Beatmaps;
 using osu.Game.Rulesets;
 using osu.Game.Skinning;
-using osu.Game.Storyboards;
 
 namespace osu.Game.Tests.Visual
 {
@@ -24,22 +18,11 @@ namespace osu.Game.Tests.Visual
 
         protected override TestPlayer CreatePlayer(Ruleset ruleset) => new SkinProvidingPlayer(legacySkinSource);
 
-        protected override WorkingBeatmap CreateWorkingBeatmap(IBeatmap beatmap, Storyboard storyboard = null)
-            => new LegacySkinWorkingBeatmap(beatmap, storyboard, Clock, Audio);
-
         [BackgroundDependencyLoader]
         private void load(OsuGameBase game, SkinManager skins)
         {
             LegacySkin = new DefaultLegacySkin(new NamespacedResourceStore<byte[]>(game.Resources, "Skins/Legacy"), skins);
             legacySkinSource = new SkinProvidingContainer(LegacySkin);
-        }
-
-        public override void SetUpSteps()
-        {
-            base.SetUpSteps();
-
-            // check presence of a random legacy HUD component to ensure this is using legacy skin.
-            AddAssert("using legacy skin", () => this.ChildrenOfType<LegacyScoreCounter>().Any());
         }
 
         public class SkinProvidingPlayer : TestPlayer
@@ -51,16 +34,6 @@ namespace osu.Game.Tests.Visual
             {
                 this.skinSource = skinSource;
             }
-        }
-
-        private class LegacySkinWorkingBeatmap : ClockBackedTestWorkingBeatmap
-        {
-            public LegacySkinWorkingBeatmap(IBeatmap beatmap, Storyboard storyboard, IFrameBasedClock frameBasedClock, AudioManager audio)
-                : base(beatmap, storyboard, frameBasedClock, audio)
-            {
-            }
-
-            protected override ISkin GetSkin() => new LegacyBeatmapSkin(BeatmapInfo, null, null);
         }
     }
 }
