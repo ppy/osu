21ca51cc5ae568dabaeaf98e605aaf2022c3f8a2
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2021-05-19 15:28:12 +0300

    Implement sticky container for sidebar in NewsOverlay

--- osu.Game/Overlays/NewsOverlay.cs
+++ osu.Game/Overlays/NewsOverlay.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Threading;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Overlays.News;
 using osu.Game.Overlays.News.Displays;
+using osu.Game.Overlays.News.Sidebar;
 
 namespace osu.Game.Overlays
 {
@@ -13,9 +16,44 @@ namespace osu.Game.Overlays
     {
         private readonly Bindable<string> article = new Bindable<string>(null);
 
+        protected override Container<Drawable> Content => content;
+
+        private readonly Container content;
+        private readonly Container sidebarContainer;
+
         public NewsOverlay()
             : base(OverlayColourScheme.Purple, false)
         {
+            base.Content.Add(new GridContainer
+            {
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                RowDimensions = new[]
+                {
+                    new Dimension(GridSizeMode.AutoSize)
+                },
+                ColumnDimensions = new[]
+                {
+                    new Dimension(GridSizeMode.AutoSize),
+                    new Dimension()
+                },
+                Content = new[]
+                {
+                    new Drawable[]
+                    {
+                        sidebarContainer = new Container
+                        {
+                            AutoSizeAxes = Axes.X,
+                            Child = new NewsSidebar()
+                        },
+                        content = new Container
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y
+                        }
+                    }
+                }
+            });
         }
 
         protected override void LoadComplete()
@@ -88,6 +126,14 @@ namespace osu.Game.Overlays
                 Child = loaded;
                 Loading.Hide();
             }, (cancellationToken = new CancellationTokenSource()).Token);
+        }
+
+        protected override void UpdateAfterChildren()
+        {
+            base.UpdateAfterChildren();
+
+            sidebarContainer.Height = DrawHeight;
+            sidebarContainer.Y = Math.Clamp(ScrollFlow.Current - Header.DrawHeight, 0, Math.Max(ScrollFlow.ScrollContent.DrawHeight - DrawHeight - Header.DrawHeight, 0));
         }
 
         protected override void Dispose(bool isDisposing)
