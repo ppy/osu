ae1c94abe1bc37db1c682eb2fe55b7f453f1063b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-05-20 19:34:53 +0900

    Create base implementations of the two most common `TernaryStateMenuItem`s

---
+++ osu.Game/Graphics/UserInterface/TernaryStateRadioMenuItem.cs
@@ -0,0 +1,23 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class TernaryStateRadioMenuItem : TernaryStateMenuItem
+    {
+        /// <summary>
+        /// Creates a new <see cref="TernaryStateMenuItem"/>.
+        /// </summary>
+        /// <param name="text">The text to display.</param>
+        /// <param name="type">The type of action which this <see cref="TernaryStateMenuItem"/> performs.</param>
+        /// <param name="action">A delegate to be invoked when this <see cref="TernaryStateMenuItem"/> is pressed.</param>
+        public TernaryStateRadioMenuItem(string text, MenuItemType type = MenuItemType.Standard, Action<TernaryState> action = null)
+            : base(text, getNextState, type, action)
+        {
+        }
+
+        private static TernaryState getNextState(TernaryState state) => TernaryState.True;
+    }
+}
