292f314feeba625149aa30b639503ca794b2a67f
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-05-21 16:04:08 +0700

    initial wiki notice container

--- osu.Game/Overlays/Wiki/Markdown/WikiMarkdownContainer.cs
+++ osu.Game/Overlays/Wiki/Markdown/WikiMarkdownContainer.cs
@@ -1,6 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using Markdig.Extensions.Yaml;
+using Markdig.Syntax;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.Containers.Markdown;
 
 namespace osu.Game.Overlays.Wiki.Markdown
@@ -11,5 +14,21 @@ namespace osu.Game.Overlays.Wiki.Markdown
         {
             set => Schedule(() => DocumentUrl += $"wiki/{value}");
         }
+
+        protected override void AddMarkdownComponent(IMarkdownObject markdownObject, FillFlowContainer container, int level)
+        {
+            switch (markdownObject)
+            {
+                case YamlFrontMatterBlock yamlFrontMatterBlock:
+                    container.Add(CreateNotice(yamlFrontMatterBlock));
+                    break;
+
+                default:
+                    base.AddMarkdownComponent(markdownObject, container, level);
+                    break;
+            }
+        }
+
+        protected virtual FillFlowContainer CreateNotice(YamlFrontMatterBlock yamlFrontMatterBlock) => new WikiNoticeContainer(yamlFrontMatterBlock);
     }
 }
