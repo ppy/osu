412020e442ce05dbfc983d5450d9cc272f220585
Swords <87902114+bubb13gummy@users.noreply.github.com>
2021-05-24 22:49:40 +1000

    Renaming files

--- osu.Game/Overlays/KeyBinding/KeyBindingsSubsection.cs
+++ osu.Game/Overlays/KeyBinding/KeyBindingsSubsection.cs
@@ -38,12 +38,12 @@ namespace osu.Game.Overlays.KeyBinding
             foreach (var defaultGroup in Defaults.GroupBy(d => d.Action))
             {
                 // one row per valid action.
-                Add(new RestorableKeyBindingRow(defaultGroup.Key, bindings, Ruleset, defaultGroup.Select(d => d.KeyCombination)));
+                Add(new KeyBindingRow(defaultGroup.Key, bindings, Ruleset, defaultGroup.Select(d => d.KeyCombination)));
             }
 
             Add(new ResetButton
             {
-                Action = () => Children.OfType<RestorableKeyBindingRow>().ForEach(k => k.KeyBindingRow.RestoreDefaults())
+                Action = () => Children.OfType<KeyBindingRow>().ForEach(k => k.BasicKeyBindingRow.RestoreDefaults())
             });
         }
     }
