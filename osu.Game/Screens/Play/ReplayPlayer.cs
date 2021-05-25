7c89dbcd3502e68639476f9d99ab92e63205c870
Dean Herbert <pe@ppy.sh>
2021-05-25 18:37:04 +0900

    Externalise autoplay generation

--- osu.Game/Screens/Play/ReplayPlayer.cs
+++ osu.Game/Screens/Play/ReplayPlayer.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using osu.Framework.Allocation;
 using osu.Framework.Input.Bindings;
 using osu.Game.Input.Bindings;
+using osu.Game.Rulesets.Mods;
 using osu.Game.Scoring;
 using osu.Game.Screens.Ranking;
 
@@ -11,15 +15,28 @@ namespace osu.Game.Screens.Play
 {
     public class ReplayPlayer : Player, IKeyBindingHandler<GlobalAction>
     {
-        protected readonly Score Score;
+        protected Score Score { get; private set; }
+
+        private readonly Func<GameplayBeatmap, IReadOnlyList<Mod>, Score> createScore;
 
         // Disallow replays from failing. (see https://github.com/ppy/osu/issues/6108)
         protected override bool CheckModsAllowFailure() => false;
 
         public ReplayPlayer(Score score, PlayerConfiguration configuration = null)
+            : this((_, __) => score, configuration)
+        {
+        }
+
+        public ReplayPlayer(Func<GameplayBeatmap, IReadOnlyList<Mod>, Score> createScore, PlayerConfiguration configuration = null)
             : base(configuration)
         {
-            Score = score;
+            this.createScore = createScore;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Score = createScore(GameplayBeatmap, Mods.Value);
         }
 
         protected override void PrepareReplay()
