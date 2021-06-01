e47364ed620222f3997ba3acc48957e0d8cc7c9f
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-06-01 17:13:13 +0900

    Revert "Fix weird taiko logic failing for weird reasons that probably should not have been a thing"
    This reverts commit e9aa2b89b8161ecc0b8d2a33fc2c4487e214f227.

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -563,11 +563,7 @@ namespace osu.Game.Skinning
 
             // Fall back to using the last piece for components coming from lazer (e.g. "Gameplay/osu/approachcircle" -> "approachcircle").
             string lastPiece = componentName.Split('/').Last();
-
-            if (componentName.StartsWith("Gameplay/taiko/", StringComparison.Ordinal))
-                yield return "taiko-" + lastPiece;
-
-            yield return lastPiece;
+            yield return componentName.StartsWith("Gameplay/taiko/", StringComparison.Ordinal) ? "taiko-" + lastPiece : lastPiece;
         }
 
         protected override void Dispose(bool isDisposing)
