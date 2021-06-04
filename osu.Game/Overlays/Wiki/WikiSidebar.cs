f374215a7c6f83a8a71ee4cab2eaf2c0422d3ffb
Gagah Pangeran Rosfatiputra <87902114+bubb13gummy@users.noreply.github.com>
2021-06-04 10:16:49 +0700

    add basic content wiki sidebar

--- osu.Game/Overlays/Wiki/WikiSidebar.cs
+++ osu.Game/Overlays/Wiki/WikiSidebar.cs
@@ -1,9 +1,36 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+
 namespace osu.Game.Overlays.Wiki
 {
     public class WikiSidebar : OverlaySidebar
     {
+        private FillFlowContainer tableOfContents;
+
+        protected override Drawable CreateContent() => new FillFlowContainer
+        {
+            Direction = FillDirection.Vertical,
+            RelativeSizeAxes = Axes.X,
+            AutoSizeAxes = Axes.Y,
+            Children = new Drawable[]
+            {
+                new OsuSpriteText
+                {
+                    Text = "CONTENTS",
+                    Font = OsuFont.GetFont(size: 12, weight: FontWeight.Bold),
+                },
+                tableOfContents = new FillFlowContainer
+                {
+                    Direction = FillDirection.Vertical,
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                }
+            },
+        };
     }
 }
