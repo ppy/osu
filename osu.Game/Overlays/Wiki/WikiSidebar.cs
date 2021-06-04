f4550389ed6d2806f917fddc24138a35288ff0a6
Gagah Pangeran Rosfatiputra <87902114+bubb13gummy@users.noreply.github.com>
2021-06-04 11:09:20 +0700

    create TocTitle in WikiSidebar

--- osu.Game/Overlays/Wiki/WikiSidebar.cs
+++ osu.Game/Overlays/Wiki/WikiSidebar.cs
@@ -1,10 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Containers.Markdown;
 using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
 
 namespace osu.Game.Overlays.Wiki
@@ -39,12 +42,33 @@ namespace osu.Game.Overlays.Wiki
             switch (level)
             {
                 case 2:
-                    tableOfContents.Add(new OsuSpriteText
-                    {
-                        Text = title,
-                        Font = OsuFont.GetFont(size: 15),
-                    });
+                    tableOfContents.Add(new TocTitle(title));
                     break;
+            }
+        }
+
+        private class TocTitle : OsuHoverContainer
+        {
+            private readonly OsuSpriteText spriteText;
+
+            public TocTitle(string text)
+            {
+                RelativeSizeAxes = Axes.X;
+                AutoSizeAxes = Axes.Y;
+                Child = spriteText = new OsuSpriteText
+                {
+                    Text = text,
+                    Font = OsuFont.GetFont(size: 15),
+                };
+            }
+
+            protected override IEnumerable<Drawable> EffectTargets => new Drawable[] { spriteText };
+
+            [BackgroundDependencyLoader]
+            private void load(OverlayColourProvider colourProvider)
+            {
+                IdleColour = colourProvider.Light2;
+                HoverColour = colourProvider.Light1;
             }
         }
     }
