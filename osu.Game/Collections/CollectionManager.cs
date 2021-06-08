7341e474f1405dde31bd1082cdf7d725d99c61ac
smoogipoo <smoogipoo@smgi.me>
2021-06-08 14:25:39 +0900

    Attempt to safeguard against collections database corruptions

--- osu.Game/Collections/CollectionManager.cs
+++ osu.Game/Collections/CollectionManager.cs
@@ -264,14 +264,18 @@ namespace osu.Game.Collections
                     using (var sw = new SerializationWriter(storage.GetStream(database_name, FileAccess.Write)))
                     {
                         sw.Write(database_version);
-                        sw.Write(Collections.Count);
 
-                        foreach (var c in Collections)
+                        var collectionsCopy = Collections.ToArray();
+                        sw.Write(collectionsCopy.Length);
+
+                        foreach (var c in collectionsCopy)
                         {
                             sw.Write(c.Name.Value);
-                            sw.Write(c.Beatmaps.Count);
 
-                            foreach (var b in c.Beatmaps)
+                            var beatmapsCopy = c.Beatmaps.ToArray();
+                            sw.Write(beatmapsCopy.Length);
+
+                            foreach (var b in beatmapsCopy)
                                 sw.Write(b.MD5Hash);
                         }
                     }
