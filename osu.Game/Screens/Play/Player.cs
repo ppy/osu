1aaad7bfd442d74cfc28903fb4f4b35eb178edb7
Salman Ahmed <frenzibyte@gmail.com>
2021-06-09 22:24:53 +0300

    Apply few adjustments to skinning overlays comment

--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -247,9 +247,9 @@ namespace osu.Game.Screens.Play
                 createGameplayComponents(Beatmap.Value, playableBeatmap)
             });
 
-            // also give the HUD a ruleset container to allow rulesets to potentially override HUD elements (used to disable combo counters etc.)
-            // we may want to limit this in the future to disallow rulesets from outright replacing elements the user expects to be there.
             // add the overlay components as a separate step as they proxy some elements from the above underlay/gameplay components.
+            // also give the overlays the ruleset skin provider to allow rulesets to potentially override HUD elements (used to disable combo counters etc.)
+            // we may want to limit this in the future to disallow rulesets from outright replacing elements the user expects to be there.
             rulesetSkinProvider.Add(createOverlayComponents(Beatmap.Value));
 
             if (!DrawableRuleset.AllowGameplayOverlays)
