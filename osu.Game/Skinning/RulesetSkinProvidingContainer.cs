33a9cac398f446bb01b5e9e9bd9463f81edd62ae
Salman Ahmed <frenzibyte@gmail.com>
2021-06-09 20:41:16 +0300

    Add special `RulesetSkinProvidingContainer` managing ruleset-compatible skin setup

---
+++ osu.Game/Skinning/RulesetSkinProvidingContainer.cs
@@ -0,0 +1,59 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets;
+
+namespace osu.Game.Skinning
+{
+    /// <summary>
+    /// A type of <see cref="SkinProvidingContainer"/> that provides access to the beatmap skin and user skin,
+    /// each transformed with the ruleset's own skin transformer individually.
+    /// </summary>
+    public class RulesetSkinProvidingContainer : SkinProvidingContainer
+    {
+        private readonly Ruleset ruleset;
+        private readonly IBeatmap beatmap;
+
+        protected override Container<Drawable> Content { get; }
+
+        public RulesetSkinProvidingContainer(Ruleset ruleset, IBeatmap beatmap, ISkin beatmapSkin)
+        {
+            this.ruleset = ruleset;
+            this.beatmap = beatmap;
+
+            InternalChild = new BeatmapSkinProvidingContainer(ruleset.CreateLegacySkinProvider(beatmapSkin, beatmap))
+            {
+                Child = Content = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                }
+            };
+        }
+
+        [Resolved]
+        private SkinManager skinManager { get; set; }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            updateSkins();
+        }
+
+        protected override void OnSourceChanged()
+        {
+            updateSkins();
+            base.OnSourceChanged();
+        }
+
+        private void updateSkins()
+        {
+            SkinLayers.Clear();
+            SkinLayers.AddRange(skinManager.CurrentSkinLayers.Select(s => ruleset.CreateLegacySkinProvider(s, beatmap)));
+        }
+    }
+}
