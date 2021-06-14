ca061c4b939fdc8b8782ba0e80885f17da3965d5
ekrctb <ekrctb@gmail.com>
2021-06-14 19:41:51 +0900

    Factor out `SkinnableDrawable` component of the catcher to `SkinnableCatcher`

---
+++ osu.Game.Rulesets.Catch/UI/SkinnableCatcher.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Catch.Skinning.Default;
+using osu.Game.Skinning;
+using osuTK;
+
+namespace osu.Game.Rulesets.Catch.UI
+{
+    public class SkinnableCatcher : SkinnableDrawable
+    {
+        [Cached]
+        public readonly Bindable<CatcherAnimationState> AnimationState = new Bindable<CatcherAnimationState>();
+
+        public SkinnableCatcher()
+            : base(new CatchSkinComponent(CatchSkinComponents.Catcher), _ => new DefaultCatcher())
+        {
+            Anchor = Anchor.TopCentre;
+            // Sets the origin roughly to the centre of the catcher's plate to allow for correct scaling.
+            OriginPosition = new Vector2(0.5f, 0.06f) * CatcherArea.CATCHER_SIZE;
+        }
+    }
+}
