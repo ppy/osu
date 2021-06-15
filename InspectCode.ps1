a4c4867d6a6b49db710b467ead9b2ec5afef0f0e
smoogipoo <smoogipoo@smgi.me>
2021-06-15 22:40:31 +0900

    Add scripts for running inspections locally

---
+++ InspectCode.ps1
@@ -0,0 +1,11 @@
+dotnet tool restore
+
+# Temporarily disabled until the tool is upgraded to 5.0.
+  # The version specified in .config/dotnet-tools.json (3.1.37601) won't run on .NET hosts >=5.0.7.
+  # - cmd: dotnet format --dry-run --check
+
+dotnet CodeFileSanity
+dotnet jb inspectcode "osu.Desktop.slnf" --output="inspectcodereport.xml" --caches-home="inspectcode" --verbosity=WARN
+dotnet nvika parsereport "inspectcodereport.xml" --treatwarningsaserrors
+
+exit $LASTEXITCODE
