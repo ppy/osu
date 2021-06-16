1d552640c7d14c5e5fa3719c3982863bd15968a4
ekrctb <87902114+bubb13gummy@users.noreply.github.com>
2021-06-16 18:52:16 +0900

    Obsolete plural `IApplicableToDrawableHitObjects`

--- osu.Game/Rulesets/Mods/IApplicableToDrawableHitObject.cs
+++ osu.Game/Rulesets/Mods/IApplicableToDrawableHitObject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Game.Rulesets.Objects.Drawables;
@@ -19,6 +20,7 @@ namespace osu.Game.Rulesets.Mods
         void ApplyToDrawableHitObject(DrawableHitObject drawable);
     }
 
+    [Obsolete(@"Use the singular version IApplicableToDrawableHitObject instead.")] // Can be removed 20211216
     public interface IApplicableToDrawableHitObjects : IApplicableToDrawableHitObject
     {
         void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables);
