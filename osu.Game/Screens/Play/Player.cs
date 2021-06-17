86460cb84c84206b98ce93ff3c005f24db7711de
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-06-17 15:26:50 +0200

    Use xmldoc-specific syntax for nicer formatting

--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -515,12 +515,14 @@ namespace osu.Game.Screens.Play
         /// Attempts to complete a user request to exit gameplay.
         /// </summary>
         /// <remarks>
-        /// - This should only be called in response to a user interaction. Exiting is not guaranteed.
-        /// - This will interrupt any pending progression to the results screen, even if the transition has begun.
+        /// <list type="bullet">
+        /// <item>This should only be called in response to a user interaction. Exiting is not guaranteed.</item>
+        /// <item>This will interrupt any pending progression to the results screen, even if the transition has begun.</item>
+        /// </list>
         /// </remarks>
         /// <param name="showDialogFirst">
         /// Whether the pause or fail dialog should be shown before performing an exit.
-        /// If <c>true</c> and a dialog is not yet displayed, the exit will be blocked the relevant dialog will display instead.
+        /// If <see langword="true"/> and a dialog is not yet displayed, the exit will be blocked the relevant dialog will display instead.
         /// </param>
         protected void PerformExit(bool showDialogFirst)
         {
