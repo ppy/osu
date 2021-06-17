246ab41cc68e361ea9ba71430dbfd8b776fcad54
Dean Herbert <pe@ppy.sh>
2021-06-17 18:11:15 +0900

    Remove special casing for user exit during storyboard outro

--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -559,12 +559,6 @@ namespace osu.Game.Screens.Play
                     Pause();
                     return;
                 }
-
-                // if the score is ready for display but results screen has not been pushed yet (e.g. storyboard is still playing beyond gameplay), then transition to results screen instead of exiting.
-                if (prepareScoreForDisplayTask != null && completionProgressDelegate == null)
-                {
-                    updateCompletionState(true);
-                }
             }
 
             // The actual exit is performed if
