97cd1217a47985b1dd9052171905e5106a96a5fd
smoogipoo <smoogipoo@smgi.me>
2021-06-18 13:06:13 +0900

    Move IApplicableToDrawableHitObjects to its own file

--- osu.Game/Rulesets/Mods/IApplicableToDrawableHitObject.cs
+++ osu.Game/Rulesets/Mods/IApplicableToDrawableHitObject.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Collections.Generic;
-using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Game.Rulesets.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Mods
@@ -18,13 +15,5 @@ namespace osu.Game.Rulesets.Mods
         /// This will only be invoked with top-level <see cref="DrawableHitObject"/>s. Access <see cref="DrawableHitObject.NestedHitObjects"/> if adjusting nested objects is necessary.
         /// </summary>
         void ApplyToDrawableHitObject(DrawableHitObject drawable);
-    }
-
-    [Obsolete(@"Use the singular version IApplicableToDrawableHitObject instead.")] // Can be removed 20211216
-    public interface IApplicableToDrawableHitObjects : IApplicableToDrawableHitObject
-    {
-        void ApplyToDrawableHitObjects(IEnumerable<DrawableHitObject> drawables);
-
-        void IApplicableToDrawableHitObject.ApplyToDrawableHitObject(DrawableHitObject drawable) => ApplyToDrawableHitObjects(drawable.Yield());
     }
 }
