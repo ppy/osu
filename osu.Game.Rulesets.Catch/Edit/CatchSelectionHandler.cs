c28cd5dd75bfa68ff12762bed9985793f81b6e8c
ekrctb <ekrctb@gmail.com>
2021-06-22 11:39:04 +0900

    Add basic juice stream selection blueprint

--- osu.Game.Rulesets.Catch/Edit/CatchSelectionHandler.cs
+++ osu.Game.Rulesets.Catch/Edit/CatchSelectionHandler.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using osu.Framework.Allocation;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Objects;
@@ -33,6 +34,9 @@ namespace osu.Game.Rulesets.Catch.Edit
 
                 // TODO: confine in bounds
                 hitObject.OriginalXBindable.Value += deltaX;
+
+                foreach (var nested in hitObject.NestedHitObjects.OfType<CatchHitObject>())
+                    nested.OriginalXBindable.Value += deltaX;
             });
 
             return true;
