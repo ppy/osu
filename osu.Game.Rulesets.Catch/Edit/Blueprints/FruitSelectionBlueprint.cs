b8ccfe6ea7d4dfde1c743ae814090060148cf9cd
ekrctb <ekrctb@gmail.com>
2021-06-22 10:39:32 +0900

    Add basic selection blueprint movement logic

---
+++ osu.Game.Rulesets.Catch/Edit/Blueprints/FruitSelectionBlueprint.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Catch.Objects;
+
+namespace osu.Game.Rulesets.Catch.Edit.Blueprints
+{
+    public class FruitSelectionBlueprint : CatchSelectionBlueprint<Fruit>
+    {
+        public FruitSelectionBlueprint(Fruit hitObject)
+            : base(hitObject)
+        {
+        }
+    }
+}
