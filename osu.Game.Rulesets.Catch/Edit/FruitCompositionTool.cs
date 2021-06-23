77cd82a7123321d3c7abe167ff69bc30e1ad0fd2
ekrctb <87902114+bubb13gummy@users.noreply.github.com>
2021-06-23 09:34:11 +0900

    Add icon for composition tools

--- osu.Game.Rulesets.Catch/Edit/FruitCompositionTool.cs
+++ osu.Game.Rulesets.Catch/Edit/FruitCompositionTool.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Catch.Edit.Blueprints;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Edit;
@@ -14,6 +16,8 @@ namespace osu.Game.Rulesets.Catch.Edit
             : base(nameof(Fruit))
         {
         }
+
+        public override Drawable CreateIcon() => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles);
 
         public override PlacementBlueprint CreatePlacementBlueprint() => new FruitPlacementBlueprint();
     }
