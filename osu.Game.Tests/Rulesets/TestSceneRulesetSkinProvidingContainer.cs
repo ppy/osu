13ed52a990cc4f1b53b3a1f1b37694859b5427cf
Salman Ahmed <frenzibyte@gmail.com>
2021-06-25 11:16:26 +0300

    Fix weird license misindent
    No idea how the hell that happened... R# silent about it, of course.

--- osu.Game.Tests/Rulesets/TestSceneRulesetSkinProvidingContainer.cs
+++ osu.Game.Tests/Rulesets/TestSceneRulesetSkinProvidingContainer.cs
@@ -1,5 +1,5 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
- // See the LICENCE file in the repository root for full licence text.
+// See the LICENCE file in the repository root for full licence text.
 
 using System;
 using NUnit.Framework;
