99dc90ff354bfd62e6d4c8b60bb3b354a14eca5b
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2021-06-26 12:23:05 +0300

    Dispose `ResourcesSkin` before clearing skin sources

--- osu.Game/Skinning/RulesetSkinProvidingContainer.cs
+++ osu.Game/Skinning/RulesetSkinProvidingContainer.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using JetBrains.Annotations;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
@@ -72,6 +73,9 @@ namespace osu.Game.Skinning
 
         protected virtual void UpdateSkins()
         {
+            foreach (var resourcesSkin in SkinSources.OfType<ResourcesSkin>())
+                resourcesSkin.Dispose();
+
             SkinSources.Clear();
 
             foreach (var skin in skinSource.AllSources)
