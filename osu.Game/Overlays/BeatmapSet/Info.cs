7dae93ad669eff61cd5fb51173dd3cfc6ca09405
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-07-03 15:23:26 +0200

    Remove unused using directives

--- osu.Game/Overlays/BeatmapSet/Info.cs
+++ osu.Game/Overlays/BeatmapSet/Info.cs
@@ -9,7 +9,6 @@ using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
-using osu.Game.Screens.Select;
 
 namespace osu.Game.Overlays.BeatmapSet
 {
