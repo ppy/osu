7e2a635abd599fbaebcae615e3afb8bcf1b3e29b
PercyDan54 <87902114+bubb13gummy@users.noreply.github.com>
2021-07-04 22:09:23 +0800

    Add test

---
+++ osu.Game.Tests/Localisation/BeatmapMetadataRomanisationTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+
+namespace osu.Game.Tests.Localisation
+{
+    [TestFixture]
+    public class BeatmapMetadataRomanisationTest
+    {
+        [Test]
+        public void TestNoUnicode()
+        {
+            var beatmap = new Beatmap
+            {
+                BeatmapInfo = new BeatmapInfo
+                {
+                    Metadata = new BeatmapMetadata
+                    {
+                        Artist = "Artist",
+                        Title = "Romanised title"
+                    }
+                }
+            };
+            var romanisableString = beatmap.Metadata.ToRomanisableString();
+
+            Assert.AreEqual(romanisableString.Romanised, romanisableString.Original);
+        }
+    }
+}
