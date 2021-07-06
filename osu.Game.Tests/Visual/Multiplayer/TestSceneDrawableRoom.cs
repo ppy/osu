3c1b6f6ce0393ab279b4d74bd276aa1cd0416a0b
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-07-06 18:23:37 +0900

    Add clickability to test

--- osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoom.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoom.cs
@@ -4,6 +4,7 @@
 using System;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.Rooms;
 using osu.Game.Online.Rooms.RoomStatuses;
 using osu.Game.Screens.OnlinePlay.Lounge.Components;
@@ -20,32 +21,44 @@ namespace osu.Game.Tests.Visual.Multiplayer
                 Width = 0.5f,
                 Children = new Drawable[]
                 {
-                    new DrawableRoom(new Room
+                    createDrawableRoom(new Room
                     {
                         Name = { Value = "Open - ending in 1 day" },
                         Status = { Value = new RoomStatusOpen() },
                         EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
-                    }) { MatchingFilter = true },
-                    new DrawableRoom(new Room
+                    }),
+                    createDrawableRoom(new Room
                     {
                         Name = { Value = "Playing - ending in 1 day" },
                         Status = { Value = new RoomStatusPlaying() },
                         EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
-                    }) { MatchingFilter = true },
-                    new DrawableRoom(new Room
+                    }),
+                    createDrawableRoom(new Room
                     {
                         Name = { Value = "Ended" },
                         Status = { Value = new RoomStatusEnded() },
                         EndDate = { Value = DateTimeOffset.Now }
-                    }) { MatchingFilter = true },
-                    new DrawableRoom(new Room
+                    }),
+                    createDrawableRoom(new Room
                     {
                         Name = { Value = "Open" },
                         Status = { Value = new RoomStatusOpen() },
                         Category = { Value = RoomCategory.Realtime }
-                    }) { MatchingFilter = true },
+                    }),
                 }
             };
+        }
+
+        private DrawableRoom createDrawableRoom(Room room)
+        {
+            var drawableRoom = new DrawableRoom(room)
+            {
+                MatchingFilter = true
+            };
+
+            drawableRoom.Action = () => drawableRoom.State = drawableRoom.State == SelectionState.Selected ? SelectionState.NotSelected : SelectionState.Selected;
+
+            return drawableRoom;
         }
     }
 }
