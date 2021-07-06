4bdb6bdec66e26dca06544db4e5fea2fb5494aba
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-07-06 20:04:32 +0900

    Initial redesign of DrawableRoom

--- osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoom.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoom.cs
@@ -8,6 +8,7 @@ using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.Rooms;
 using osu.Game.Online.Rooms.RoomStatuses;
 using osu.Game.Screens.OnlinePlay.Lounge.Components;
+using osuTK;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
@@ -17,31 +18,34 @@ namespace osu.Game.Tests.Visual.Multiplayer
         {
             Child = new FillFlowContainer
             {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
                 RelativeSizeAxes = Axes.Both,
-                Width = 0.5f,
+                Size = new Vector2(0.9f),
+                Spacing = new Vector2(10),
                 Children = new Drawable[]
                 {
                     createDrawableRoom(new Room
                     {
-                        Name = { Value = "Open - ending in 1 day" },
+                        Name = { Value = "Room name: Open - ending in 1 day" },
                         Status = { Value = new RoomStatusOpen() },
                         EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
                     }),
                     createDrawableRoom(new Room
                     {
-                        Name = { Value = "Playing - ending in 1 day" },
+                        Name = { Value = "Room name: Playing - ending in 1 day" },
                         Status = { Value = new RoomStatusPlaying() },
                         EndDate = { Value = DateTimeOffset.Now.AddDays(1) }
                     }),
                     createDrawableRoom(new Room
                     {
-                        Name = { Value = "Ended" },
+                        Name = { Value = "Room name: Ended" },
                         Status = { Value = new RoomStatusEnded() },
                         EndDate = { Value = DateTimeOffset.Now }
                     }),
                     createDrawableRoom(new Room
                     {
-                        Name = { Value = "Open" },
+                        Name = { Value = "Room name: Open" },
                         Status = { Value = new RoomStatusOpen() },
                         Category = { Value = RoomCategory.Realtime }
                     }),
