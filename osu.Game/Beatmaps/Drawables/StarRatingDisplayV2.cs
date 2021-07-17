092c139020ded09ba2a23a840b7156da8fd996d0
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2021-07-17 05:41:34 +0300

    Use `OsuColour.Orange1` instead of pure yellow

--- osu.Game/Beatmaps/Drawables/StarRatingDisplayV2.cs
+++ osu.Game/Beatmaps/Drawables/StarRatingDisplayV2.cs
@@ -86,8 +86,8 @@ namespace osu.Game.Beatmaps.Drawables
 
                 background.Colour = colours.ForStarDifficulty(c.NewValue.Stars);
 
-                starIcon.Colour = c.NewValue.Stars >= 6.5 ? Color4.Yellow : colourProvider?.Background5 ?? Color4Extensions.FromHex("303d47");
-                starsText.Colour = c.NewValue.Stars >= 6.5 ? Color4.Yellow : colourProvider?.Background5 ?? Color4.Black.Opacity(0.75f);
+                starIcon.Colour = c.NewValue.Stars >= 6.5 ? colours.Orange1 : colourProvider?.Background5 ?? Color4Extensions.FromHex("303d47");
+                starsText.Colour = c.NewValue.Stars >= 6.5 ? colours.Orange1 : colourProvider?.Background5 ?? Color4.Black.Opacity(0.75f);
             }, true);
         }
     }
