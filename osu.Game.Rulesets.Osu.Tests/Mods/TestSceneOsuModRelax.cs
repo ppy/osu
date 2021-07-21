cb0493f0730e9c16b891d7ca148e45ef88fa3e6e
PercyDan <50285552+PercyDan54@users.noreply.github.com>
2021-07-21 13:31:28 +0800

    Check time >= lastHitTime - relax_leniency

--- osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModRelax.cs
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModRelax.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Game.Beatmaps;
@@ -13,6 +14,7 @@ using osu.Game.Rulesets.Osu.Replays;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Scoring;
 using osu.Game.Tests.Visual;
+using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Tests.Mods
 {
@@ -21,11 +23,8 @@ namespace osu.Game.Rulesets.Osu.Tests.Mods
         [Test]
         public void TestKeyUp()
         {
-            var lastObject = new HitCircle
-            {
-                Position = OsuPlayfield.BASE_SIZE / 2,
-                StartTime = 2550
-            };
+            var hitObjects = CreateHitObjects();
+
             CreateModTest(new ModTestData
             {
                 Autoplay = false,
@@ -37,26 +36,48 @@ namespace osu.Game.Rulesets.Osu.Tests.Mods
                     },
                 Beatmap = new Beatmap
                 {
-                    HitObjects =
-                    {
-                        new HitCircle
-                        {
-                            Position = OsuPlayfield.BASE_SIZE / 2,
-                            StartTime = 2500
-                        },
-                        lastObject
-                    }
+                    HitObjects = hitObjects
                 },
-                PassCondition = () => Player.ScoreProcessor.JudgedHits == 2
+                PassCondition = () => Player.ScoreProcessor.Accuracy.Value == 1
             });
             Replay replay = null;
+            InputManager.UseParentInput = false;
 
-            AddUntilStep("wait for beatmap end", () => Player.GameplayClockContainer.GameplayClock.CurrentTime >= lastObject.GetEndTime());
+            AddStep("show key overlay", () => Player.HUDOverlay.KeyCounter.AlwaysVisible.Value = true);
+            AddUntilStep("wait for beatmap end", () => Player.GameplayClockContainer.GameplayClock.CurrentTime >= hitObjects.Last().StartTime);
             AddStep("get replay", () => replay = ((ScoreAccessiblePlayer)Player).Score.Replay);
             AddAssert("no key was held", () => replay.Frames.Cast<OsuReplayFrame>().Last().Actions.Count == 0);
         }
 
         protected override TestPlayer CreateModPlayer(Ruleset ruleset) => new ScoreAccessiblePlayer(AllowFail);
+
+        protected List<HitObject> CreateHitObjects()
+        {
+            const int start = 500;
+            var list = new List<HitObject>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new HitCircle
+                {
+                    StartTime = start + (i - 1) * 50,
+                    Position = new Vector2((i + 1) * 32, OsuPlayfield.BASE_SIZE.Y / 2)
+                });
+            }
+
+            var lastPos = ((HitCircle)list.Last()).Position + new Vector2(32, 0);
+
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(new HitCircle
+                {
+                    StartTime = list.Last().StartTime,
+                    Position = lastPos
+                });
+            }
+
+            return list;
+        }
 
         protected class ScoreAccessiblePlayer : ModTestPlayer
         {
