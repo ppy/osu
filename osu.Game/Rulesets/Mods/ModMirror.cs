2e1cd4a389e8dce86a3f865b99dbed5c0b4b438d
Gabe Livengood <47010459+ggliv@users.noreply.github.com>
2021-07-25 21:26:21 -0400

    remove accidental tab characters

--- osu.Game/Rulesets/Mods/ModMirror.cs
+++ osu.Game/Rulesets/Mods/ModMirror.cs
@@ -9,6 +9,6 @@ namespace osu.Game.Rulesets.Mods
         public override string Acronym => "MR";
         public override ModType Type => ModType.Conversion;
         public override string Description => "Flips the beatmap horizontally.";
-		public override double ScoreMultiplier => 1;
+        public override double ScoreMultiplier => 1;
     }
 }
