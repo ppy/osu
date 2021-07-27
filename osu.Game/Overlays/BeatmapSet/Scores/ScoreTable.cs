94877117b9868e00ec8c95c9b482d92a25f7bc35
Lucas A <game4allyt@gmail.com>
2021-07-27 18:22:47 +0200

    Apply changes in-line with framework changes.

--- osu.Game/Overlays/BeatmapSet/Scores/ScoreTable.cs
+++ osu.Game/Overlays/BeatmapSet/Scores/ScoreTable.cs
@@ -18,6 +18,8 @@ using osu.Game.Scoring;
 using osu.Game.Users.Drawables;
 using osuTK;
 using osuTK.Graphics;
+using osu.Framework.Localisation;
+using osu.Framework.Extensions.LocalisationExtensions;
 
 namespace osu.Game.Overlays.BeatmapSet.Scores
 {
@@ -215,7 +217,7 @@ namespace osu.Game.Overlays.BeatmapSet.Scores
 
         private class HeaderText : OsuSpriteText
         {
-            public HeaderText(string text)
+            public HeaderText(LocalisableString text)
             {
                 Text = text.ToUpper();
                 Font = OsuFont.GetFont(size: 10, weight: FontWeight.Bold);
