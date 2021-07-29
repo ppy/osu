0620cd130e6427b3c468c03b8a4a0a19a1854b42
Henry Lin <henry.ys.lin@gmail.com>
2021-07-29 14:41:47 +0800

    Change mod description

--- osu.Game/Rulesets/Mods/ModMuted.cs
+++ osu.Game/Rulesets/Mods/ModMuted.cs
@@ -23,7 +23,7 @@ namespace osu.Game.Rulesets.Mods
         public override string Name => "Muted";
         public override string Acronym => "MU";
         public override IconUsage? Icon => FontAwesome.Solid.VolumeMute;
-        public override string Description => "Who needs music in a rhythm game?";
+        public override string Description => "Can you still feel the rhythm without music?";
         public override ModType Type => ModType.Fun;
         public override double ScoreMultiplier => 1;
     }
