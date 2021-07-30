a2f3edbfc0cfda19ce03fa388ab0436c96d43904
Dean Herbert <pe@ppy.sh>
2021-07-30 15:49:11 +0900

    Fade track volume out as combo increases

--- osu.Game/Rulesets/Mods/ModMuted.cs
+++ osu.Game/Rulesets/Mods/ModMuted.cs
@@ -1,14 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.UI;
+using osu.Game.Scoring;
 
 namespace osu.Game.Rulesets.Mods
 {
@@ -22,10 +26,15 @@ namespace osu.Game.Rulesets.Mods
         public override double ScoreMultiplier => 1;
     }
 
-    public abstract class ModMuted<TObject> : ModMuted, IApplicableToDrawableRuleset<TObject>, IApplicableToTrack
+    public abstract class ModMuted<TObject> : ModMuted, IApplicableToDrawableRuleset<TObject>, IApplicableToTrack, IApplicableToScoreProcessor
         where TObject : HitObject
     {
-        private readonly BindableNumber<double> volumeAdjust = new BindableDouble();
+        private readonly BindableNumber<double> trackVolumeAdjust = new BindableDouble(0.5);
+        private readonly BindableNumber<double> metronomeVolumeAdjust = new BindableDouble(0.5);
+
+        private BindableNumber<int> currentCombo;
+
+        private AudioContainer metronomeContainer;
 
         [SettingSource("Enable metronome", "Add a metronome to help you keep track of the rhythm.")]
         public BindableBool EnableMetronome { get; } = new BindableBool
@@ -34,15 +43,44 @@ namespace osu.Game.Rulesets.Mods
             Value = true
         };
 
+        [SettingSource("Muted at combo", "The combo count at which point the music is completely muted.")]
+        public BindableInt MuteComboCount { get; } = new BindableInt
+        {
+            Default = 100,
+            Value = 100,
+            MinValue = 0,
+            MaxValue = 500,
+        };
+
         public void ApplyToTrack(ITrack track)
         {
-            track.AddAdjustment(AdjustableProperty.Volume, volumeAdjust);
+            track.AddAdjustment(AdjustableProperty.Volume, trackVolumeAdjust);
         }
 
         public void ApplyToDrawableRuleset(DrawableRuleset<TObject> drawableRuleset)
         {
-            if (EnableMetronome.Value)
-                drawableRuleset.Overlays.Add(new Metronome(drawableRuleset.Beatmap.HitObjects.First().StartTime));
+            if (!EnableMetronome.Value) return;
+
+            drawableRuleset.Overlays.Add(metronomeContainer = new AudioContainer
+            {
+                Child = new Metronome(drawableRuleset.Beatmap.HitObjects.First().StartTime)
+            });
+
+            metronomeContainer.AddAdjustment(AdjustableProperty.Volume, metronomeVolumeAdjust);
         }
+
+        public void ApplyToScoreProcessor(ScoreProcessor scoreProcessor)
+        {
+            currentCombo = scoreProcessor.Combo.GetBoundCopy();
+            currentCombo.BindValueChanged(combo =>
+            {
+                double dimFactor = Math.Min(1, (double)combo.NewValue / MuteComboCount.Value);
+
+                metronomeVolumeAdjust.Value = dimFactor;
+                trackVolumeAdjust.Value = 1 - dimFactor;
+            }, true);
+        }
+
+        public ScoreRank AdjustRank(ScoreRank rank, double accuracy) => rank;
     }
 }
