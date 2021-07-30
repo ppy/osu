27504c05b13126f00805b651fa59bd1c7af889c5
PercyDan <50285552+PercyDan54@users.noreply.github.com>
2021-07-30 16:03:52 +0800

    Rewrite GetBeatmapRequest

--- osu.Game/Online/API/Requests/GetBeatmapRequest.cs
+++ osu.Game/Online/API/Requests/GetBeatmapRequest.cs
@@ -1,37 +1,47 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.IO.Network;
 using osu.Game.Beatmaps;
 using osu.Game.Online.API.Requests.Responses;
+
+#nullable enable
 
 namespace osu.Game.Online.API.Requests
 {
     public class GetBeatmapRequest : APIRequest<APIBeatmap>
     {
-        private readonly BeatmapInfo beatmap;
-        private readonly string beatmapHash;
+        private readonly int? id;
+        private readonly string checksum;
+        private readonly string? filename;
 
         public GetBeatmapRequest(BeatmapInfo beatmap)
         {
-            this.beatmap = beatmap;
+            id = beatmap.OnlineBeatmapID;
+            checksum = beatmap.Hash;
+            filename = beatmap.Path ?? string.Empty;
         }
 
         public GetBeatmapRequest(string beatmapHash)
         {
-            this.beatmapHash = beatmapHash;
+            checksum = beatmapHash;
         }
 
-        protected override string Target
+        protected override WebRequest CreateWebRequest()
         {
-            get
-            {
-                if (beatmap == null)
-                {
-                    return $@"beatmaps/lookup?checksum={beatmapHash}";
-                }
+            var request = base.CreateWebRequest();
 
-                return $@"beatmaps/lookup?id={beatmap.OnlineBeatmapID}&checksum={beatmap.MD5Hash}&filename={System.Uri.EscapeUriString(beatmap.Path ?? string.Empty)}";
-            }
+            request.AddParameter(@"checksum", checksum);
+
+            if (id != null)
+                request.AddParameter(@"id", id.Value.ToString());
+
+            if (filename != null)
+                request.AddParameter(@"filename", filename);
+
+            return request;
         }
+
+        protected override string Target => @"beatmaps/lookup";
     }
 }
