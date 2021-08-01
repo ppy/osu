c371e67d705df820ded3c66344a27cf3d1b07b20
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-08-01 15:32:10 +0200

    Update diffcalc test to match ground-truth formula

--- osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
+++ osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
@@ -15,13 +15,13 @@ namespace osu.Game.Rulesets.Osu.Tests
     {
         protected override string ResourceAssembly => "osu.Game.Rulesets.Osu";
 
-        [TestCase(6.7568168283591499d, "diffcalc-test")]
-        [TestCase(1.0348244046058293d, "zero-length-sliders")]
+        [TestCase(6.4164199087433484d, "diffcalc-test")]
+        [TestCase(1.0028132594779837d, "zero-length-sliders")]
         public void Test(double expected, string name)
             => base.Test(expected, name);
 
-        [TestCase(8.4783236764532557d, "diffcalc-test")]
-        [TestCase(1.2708532136987165d, "zero-length-sliders")]
+        [TestCase(8.0749334911818469d, "diffcalc-test")]
+        [TestCase(1.2251606765323657d, "zero-length-sliders")]
         public void TestClockRateAdjusted(double expected, string name)
             => Test(expected, name, new OsuModDoubleTime());
 
