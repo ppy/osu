fc48696718796a1cb3597e9132e362c8ec187ff5
Lucas A <game4allyt@gmail.com>
2021-08-07 18:27:55 +0200

    Localise detail buttons.

---
+++ osu.Game/Localisation/DownloadButtonStrings.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Localisation
+{
+    public static class DownloadButtonStrings
+    {
+        private const string prefix = @"osu.Game.Resources.Localisation.DownloadButton";
+
+        /// <summary>
+        /// "Downloading..."
+        /// </summary>
+        public static LocalisableString Downloading => new TranslatableString(getKey(@"downloading"), @"Downloading...");
+
+        /// <summary>
+        /// "Importing..."
+        /// </summary>
+        public static LocalisableString Importing => new TranslatableString(getKey(@"importing"), @"Importing...");
+
+        private static string getKey(string key) => $@"{prefix}:{key}";
+    }
+}
