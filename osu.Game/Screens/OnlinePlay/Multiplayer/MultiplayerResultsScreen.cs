e7acb05e9cb673a3fbd0849ab227fc33665cb64b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-08-11 14:43:16 +0900

    Convey and show team scores at the multiplayer results screen

--- osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerResultsScreen.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerResultsScreen.cs
@@ -1,17 +1,38 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Game.Online.Rooms;
 using osu.Game.Scoring;
 using osu.Game.Screens.OnlinePlay.Playlists;
+using osu.Game.Screens.Play.HUD;
 
 namespace osu.Game.Screens.OnlinePlay.Multiplayer
 {
     public class MultiplayerResultsScreen : PlaylistsResultsScreen
     {
-        public MultiplayerResultsScreen(ScoreInfo score, long roomId, PlaylistItem playlistItem)
+        private readonly SortedDictionary<int, BindableInt> teamScores;
+
+        public MultiplayerResultsScreen(ScoreInfo score, long roomId, PlaylistItem playlistItem, SortedDictionary<int, BindableInt> teamScores)
             : base(score, roomId, playlistItem, false, false)
         {
+            this.teamScores = teamScores;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            if (teamScores.Count == 2)
+            {
+                LoadComponentAsync(new MatchScoreDisplay
+                {
+                    Team1Score = { BindTarget = teamScores.First().Value },
+                    Team2Score = { BindTarget = teamScores.Last().Value },
+                }, AddInternal);
+            }
         }
     }
 }
