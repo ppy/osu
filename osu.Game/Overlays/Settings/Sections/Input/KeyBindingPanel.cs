ccc6b8ca1e8fbb505559271baa5859a61eed47a8
kj415j45 <87902114+bubb13gummy@users.noreply.github.com>
2021-08-12 09:53:31 +0800

    Add InputSettingsStrings
    Existed strings files keep no change

--- osu.Game/Overlays/Settings/Sections/Input/KeyBindingPanel.cs
+++ osu.Game/Overlays/Settings/Sections/Input/KeyBindingPanel.cs
@@ -4,13 +4,14 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Input.Bindings;
+using osu.Game.Localisation;
 using osu.Game.Rulesets;
 
 namespace osu.Game.Overlays.Settings.Sections.Input
 {
     public class KeyBindingPanel : SettingsSubPanel
     {
-        protected override Drawable CreateHeader() => new SettingsHeader("key configuration", "Customise your keys!");
+        protected override Drawable CreateHeader() => new SettingsHeader(InputSettingsStrings.KeyBindingPanelHeader, InputSettingsStrings.KeyBindingPanelDescription);
 
         [BackgroundDependencyLoader(permitNulls: true)]
         private void load(RulesetStore rulesets, GlobalActionContainer global)
