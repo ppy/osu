392fe72e2c688934f890c9d4e7a654ba981be438
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2021-08-13 11:12:26 +0300

    Add shared class for notice text in settings

---
+++ osu.Game/Overlays/Settings/SettingsNoticeText.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+
+namespace osu.Game.Overlays.Settings
+{
+    public class SettingsNoticeText : LinkFlowContainer
+    {
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            Colour = colours.Yellow;
+            RelativeSizeAxes = Axes.X;
+            AutoSizeAxes = Axes.Y;
+        }
+    }
+}
