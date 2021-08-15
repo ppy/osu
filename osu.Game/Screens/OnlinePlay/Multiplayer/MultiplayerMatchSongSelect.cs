2cc096101efabbe8c2e1fa9b16cdc06b06e1e0c9
Nathan Alo <nathan.alo2000@gmail.com>
2021-08-15 18:56:24 +0800

    trim whitespace

--- osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerMatchSongSelect.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerMatchSongSelect.cs
@@ -17,7 +17,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
     public class MultiplayerMatchSongSelect : OnlinePlaySongSelect
     {
         [Resolved]
-        private MultiplayerClient client { get; set; }   
+        private MultiplayerClient client { get; set; }
 
         private LoadingLayer loadingLayer;
 
