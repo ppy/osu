9b21016eed1aadc232c0023b23624d61775659c3
Xexxar <xexxar.xexxar@gmail.com>
2021-08-16 22:46:56 +0000

    accidently renamed osuStrainSkill, fixed

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -12,7 +12,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// <summary>
     /// Represents the skill required to press keys with regards to keeping up with the speed at which objects need to be hit.
     /// </summary>
-    public class Speed : OsuStrainDecaySkill
+    public class Speed : OsuStrainSkill
     {
         private const double single_spacing_threshold = 125;
 
