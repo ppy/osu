d52ad8e8f9b733061124d876374ebbe07721c627
Salman Ahmed <frenzibyte@gmail.com>
2021-08-17 12:41:11 +0300

    Add test coverage and remove stale

--- osu.Game.Tests/Gameplay/TestSceneMasterGameplayClockContainer.cs
+++ osu.Game.Tests/Gameplay/TestSceneMasterGameplayClockContainer.cs
@@ -1,8 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using NUnit.Framework;
+using osu.Framework.Audio;
+using osu.Framework.Bindables;
 using osu.Framework.Testing;
+using osu.Framework.Timing;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Screens.Play;
 using osu.Game.Tests.Visual;
@@ -53,6 +59,140 @@ namespace osu.Game.Tests.Gameplay
             });
 
             AddAssert("current time < time at reset", () => gcc.GameplayClock.CurrentTime < timeAtReset);
+        }
+
+        [Test]
+        public void TestTrueGameplayRate()
+        {
+            WorkingBeatmap working = null;
+            MasterGameplayClockContainer gcc = null;
+
+            AddStep("create container", () =>
+            {
+                working = CreateWorkingBeatmap(new OsuRuleset().RulesetInfo);
+                working.LoadTrack();
+
+                Add(gcc = new MasterGameplayClockContainer(working, 0));
+
+                gcc.Start();
+            });
+
+            AddUntilStep("gameplay clock rate = 1", () => gcc.GameplayClock.Rate == 1f);
+            AddAssert("true gameplay rate = 1", () => gcc.TrueGameplayRate == 1f);
+
+            AddStep("stop gcc", () => gcc.Stop());
+            AddUntilStep("gameplay clock rate = 0", () => gcc.GameplayClock.Rate == 0f);
+            AddAssert("true gameplay rate = 1", () => gcc.TrueGameplayRate == 1f);
+
+            AddStep("change user playback rate to 2", () => gcc.UserPlaybackRate.Value = 2f);
+            AddAssert("gameplay clock rate = 0", () => gcc.GameplayClock.Rate == 0f);
+            AddAssert("true gameplay rate = 1", () => gcc.TrueGameplayRate == 1f);
+
+            AddStep("add 1.5 gameplay freq adjustment", () => gcc.AddAdjustment(AdjustableProperty.Frequency, new BindableDouble(1.5f)));
+            AddAssert("gameplay clock rate = 0", () => gcc.GameplayClock.Rate == 0f);
+            AddAssert("true gameplay rate = 1.5", () => gcc.TrueGameplayRate == 1.5f);
+        }
+
+        [Test]
+        public void TestHardwareCorrectionOffsetClock()
+        {
+            OsuConfigManager config = null;
+            WorkingBeatmap working = null;
+            TestMasterGameplayClockContainer gcc = null;
+
+            AddStep("create container", () =>
+            {
+                config = new OsuConfigManager(LocalStorage);
+
+                working = CreateWorkingBeatmap(new OsuRuleset().RulesetInfo);
+                working.LoadTrack();
+
+                Add(new DependencyProvidingContainer
+                {
+                    CachedDependencies = new (Type, object)[] { (typeof(OsuConfigManager), config) },
+                    Child = gcc = new TestMasterGameplayClockContainer(working, 0),
+                });
+
+                gcc.Stop();
+                gcc.Reset();
+            });
+
+            AddStep("set audio offset to 100ms", () => config.SetValue(OsuSetting.AudioOffset, 100.0));
+
+            checkAppliedOffset(100);
+
+            AddStep("start gcc", () => gcc.Start());
+            checkAppliedOffset(100);
+
+            AddStep("stop gcc", () => gcc.Stop());
+            checkAppliedOffset(100);
+
+            AddStep("change user playback rate to 2", () => gcc.UserPlaybackRate.Value = 2f);
+            checkAppliedOffset(200);
+
+            AddStep("change user playback rate to 0.5", () => gcc.UserPlaybackRate.Value = 0.5f);
+            checkAppliedOffset(50);
+
+            AddStep("add 1.5 gameplay freq adjustment", () => gcc.AddAdjustment(AdjustableProperty.Frequency, new BindableDouble(0.5f)));
+            checkAppliedOffset(25);
+
+            void checkAppliedOffset(double expectedOffset)
+            {
+                AddAssert($"current time has {expectedOffset}% offset", () =>
+                    gcc.GameplayClock.CurrentTime == gcc.AdjustableSource.CurrentTime + expectedOffset);
+            }
+        }
+
+        [Test]
+        public void TestSeekPerformsInGameplayTime(
+            [Values(1.0, 0.5, 2.0, 0.0)] double clockRate,
+            [Values(0.0, 500.0, -500.0)] double userOffset)
+        {
+            OsuConfigManager config = null;
+            WorkingBeatmap working = null;
+            GameplayClockContainer gcc = null;
+
+            AddStep("create container", () =>
+            {
+                config = new OsuConfigManager(LocalStorage);
+
+                working = CreateWorkingBeatmap(new OsuRuleset().RulesetInfo);
+                working.LoadTrack();
+
+                Add(new DependencyProvidingContainer
+                {
+                    CachedDependencies = new (Type, object)[] { (typeof(OsuConfigManager), config) },
+                    Child = gcc = new MasterGameplayClockContainer(working, 0),
+                });
+
+                gcc.Stop();
+                gcc.Reset();
+            });
+
+            if (clockRate != 1.0)
+                AddStep($"set clock rate to {clockRate}", () => working.Track.AddAdjustment(AdjustableProperty.Frequency, new BindableDouble(clockRate)));
+
+            if (userOffset != 0f)
+                AddStep($"set audio offset to {userOffset}", () => config.SetValue(OsuSetting.AudioOffset, userOffset));
+
+            AddStep("seek to 0", () => gcc.Seek(0));
+            AddAssert("gameplay clock time = 0", () => gcc.CurrentTime == 0);
+
+            AddStep("seek to 2500", () => gcc.Seek(2500));
+            AddAssert("gameplay clock time = 2500", () => gcc.CurrentTime == 2500);
+
+            AddStep("seek to -2500", () => gcc.Seek(-2500));
+            AddAssert("gameplay clock time = -2500", () => gcc.GameplayClock.CurrentTime == -2500);
+        }
+
+        private class TestMasterGameplayClockContainer : MasterGameplayClockContainer
+        {
+            public new DecoupleableInterpolatingFramedClock AdjustableSource => base.AdjustableSource;
+
+            public TestMasterGameplayClockContainer(WorkingBeatmap beatmap, double gameplayStartTime, bool startAtGameplayStart = false)
+                : base(beatmap, gameplayStartTime, startAtGameplayStart)
+            {
+            }
         }
     }
 }
