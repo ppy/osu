072560ba3e583d5660f3df7d76999bca1264e390
Salman Ahmed <frenzibyte@gmail.com>
2021-08-19 07:17:42 +0300

    Remove leftover unused using

--- osu.Game/Rulesets/Mods/Mod.cs
+++ osu.Game/Rulesets/Mods/Mod.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using JetBrains.Annotations;
 using Newtonsoft.Json;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.TypeExtensions;
