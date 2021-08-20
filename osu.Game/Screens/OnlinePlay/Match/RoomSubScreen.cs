5c8ca32ea4b94387d082b8a994d261cb2a62c717
smoogipoo <smoogipoo@smgi.me>
2021-08-20 21:33:21 +0900

    Simplify lounge implementation

--- osu.Game/Screens/OnlinePlay/Match/RoomSubScreen.cs
+++ osu.Game/Screens/OnlinePlay/Match/RoomSubScreen.cs
@@ -29,10 +29,7 @@ namespace osu.Game.Screens.OnlinePlay.Match
         [Cached(typeof(IBindable<PlaylistItem>))]
         protected readonly Bindable<PlaylistItem> SelectedItem = new Bindable<PlaylistItem>();
 
-        protected override BackgroundScreen CreateBackground() => new RoomBackgroundScreen
-        {
-            SelectedItem = { BindTarget = SelectedItem }
-        };
+        protected override BackgroundScreen CreateBackground() => new RoomBackgroundScreen { SelectedItem = { BindTarget = SelectedItem } };
 
         public override bool DisallowExternalBeatmapRulesetChanges => true;
 
