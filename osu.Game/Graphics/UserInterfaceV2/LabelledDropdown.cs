acf38c723a5fd681ba62c83cf1629f4d26783629
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-08-22 14:11:57 +0200

    Move labelled dropdown from tournament to main game

---
+++ osu.Game/Graphics/UserInterfaceV2/LabelledDropdown.cs
@@ -0,0 +1,31 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Game.Graphics.UserInterface;
+
+namespace osu.Game.Graphics.UserInterfaceV2
+{
+    public class LabelledDropdown<TItem> : LabelledComponent<OsuDropdown<TItem>, TItem>
+    {
+        public LabelledDropdown()
+            : base(true)
+        {
+        }
+
+        public IEnumerable<TItem> Items
+        {
+            get => Component.Items;
+            set => Component.Items = value;
+        }
+
+        protected sealed override OsuDropdown<TItem> CreateComponent() => CreateDropdown().With(d =>
+        {
+            d.RelativeSizeAxes = Axes.X;
+            d.Width = 0.5f;
+        });
+
+        protected virtual OsuDropdown<TItem> CreateDropdown() => new OsuDropdown<TItem>();
+    }
+}
