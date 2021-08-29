e374ef163de240c91bff458fd59e10b62e2d91e7
Salman Ahmed <frenzibyte@gmail.com>
2021-08-29 15:00:28 +0300

    Update localisable formattable extensions usages inline with framework change

--- osu.Game/Overlays/Profile/Sections/Ranks/DrawableProfileWeightedScore.cs
+++ osu.Game/Overlays/Profile/Sections/Ranks/DrawableProfileWeightedScore.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Resources.Localisation.Web;
