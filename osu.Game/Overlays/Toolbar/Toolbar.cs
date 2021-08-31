da3fa9304aef4533246bb4b35f358cd536e3bf08
Joseph Madamba <madamba.joehu@outlook.com>
2021-08-31 12:39:18 -0700

    Make toolbar inherit overlay container

--- osu.Game/Overlays/Toolbar/Toolbar.cs
+++ osu.Game/Overlays/Toolbar/Toolbar.cs
@@ -18,7 +18,7 @@ using osu.Game.Input.Bindings;
 
 namespace osu.Game.Overlays.Toolbar
 {
-    public class Toolbar : VisibilityContainer, IKeyBindingHandler<GlobalAction>
+    public class Toolbar : OverlayContainer, IKeyBindingHandler<GlobalAction>
     {
         public const float HEIGHT = 40;
         public const float TOOLTIP_HEIGHT = 30;
@@ -40,8 +40,6 @@ namespace osu.Game.Overlays.Toolbar
 
         // Toolbar and its components need keyboard input even when hidden.
         public override bool PropagateNonPositionalInputSubTree => true;
-
-        protected override bool Handle(UIEvent e) => e is MouseEvent;
 
         public Toolbar()
         {
