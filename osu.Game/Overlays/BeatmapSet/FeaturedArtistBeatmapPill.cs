29f947fa07ac5a550f8470a0b178117021144d9a
ekrctb <ekrctb@gmail.com>
2021-09-09 19:25:30 +0900

    Revert `ExplicitContentBeatmapPill`, don't try to reuse common code

--- osu.Game/Overlays/BeatmapSet/FeaturedArtistBeatmapPill.cs
+++ osu.Game/Overlays/BeatmapSet/FeaturedArtistBeatmapPill.cs
@@ -1,9 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Resources.Localisation.Web;
@@ -15,13 +17,29 @@ namespace osu.Game.Overlays.BeatmapSet
         public FeaturedArtistBeatmapPill()
         {
             AutoSizeAxes = Axes.Both;
-            InternalChild = new BeatmapSetBadgePillContainer
+        }
+
+        [BackgroundDependencyLoader(true)]
+        private void load(OsuColour colours, OverlayColourProvider colourProvider)
+        {
+            InternalChild = new CircularContainer
             {
-                Child = new OsuSpriteText
+                Masking = true,
+                AutoSizeAxes = Axes.Both,
+                Children = new Drawable[]
                 {
-                    Text = BeatmapsetsStrings.FeaturedArtistBadgeLabel.ToUpper(),
-                    Font = OsuFont.GetFont(size: 10, weight: FontWeight.SemiBold),
-                    Colour = OverlayColourProvider.Blue.Colour1
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = colourProvider?.Background5 ?? colours.Gray2,
+                    },
+                    new OsuSpriteText
+                    {
+                        Margin = new MarginPadding { Horizontal = 10f, Vertical = 2f },
+                        Text = BeatmapsetsStrings.FeaturedArtistBadgeLabel.ToUpper(),
+                        Font = OsuFont.GetFont(size: 10, weight: FontWeight.SemiBold),
+                        Colour = OverlayColourProvider.Blue.Colour1,
+                    }
                 }
             };
         }
