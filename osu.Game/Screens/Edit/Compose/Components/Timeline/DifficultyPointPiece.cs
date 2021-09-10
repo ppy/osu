d2c194d79caa309ff662588cbaeb06a611ca3312
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-09-10 13:44:39 +0900

    Display difficulty and sample control points associated with hitobjects

--- osu.Game/Screens/Edit/Compose/Components/Timeline/DifficultyPointPiece.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/DifficultyPointPiece.cs
@@ -2,11 +2,12 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Bindables;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Beatmaps.ControlPoints;
 
 namespace osu.Game.Screens.Edit.Compose.Components.Timeline
 {
-    public class DifficultyPointPiece : TopPointPiece
+    public class DifficultyPointPiece : HitObjectPointPiece
     {
         private readonly BindableNumber<double> speedMultiplier;
 
@@ -14,13 +15,12 @@ namespace osu.Game.Screens.Edit.Compose.Components.Timeline
             : base(point)
         {
             speedMultiplier = point.SliderVelocityBindable.GetBoundCopy();
-
-            Y = Height;
         }
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
+
             speedMultiplier.BindValueChanged(multiplier => Label.Text = $"{multiplier.NewValue:n2}x", true);
         }
     }
