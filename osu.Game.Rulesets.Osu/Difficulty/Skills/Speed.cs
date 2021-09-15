0d041a093d94cf817346d4550dfbddb6ab28899c
apollo-dw <87902114+bubb13gummy@users.noreply.github.com>
2021-09-15 12:03:47 +0100

    elaborate comment

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -59,7 +59,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             }
 
             // Cap deltatime to the OD 300 hitwindow.
-            // 0.93 is derived from making sure 260bpm OD8 streams aren't nerfed harshly, 
+            // 0.93 is derived from making sure 260bpm OD8 streams aren't nerfed harshly, whilst 0.92 limits the effect of the cap.
             strainTime /= Math.Clamp(speedWindowRatio / 0.93, 0.92, 1);
 
             double speedBonus = 1.0;
