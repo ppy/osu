a18fed0da49ba399974bf62322a52da9db1a3300
kj415j45 <kj415j45@gmail.com>
2021-09-16 17:38:33 +0800

    Remove unused using

--- osu.Game/Input/Bindings/GlobalActionContainer.cs
+++ osu.Game/Input/Bindings/GlobalActionContainer.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using osu.Framework.Graphics;
 using osu.Framework.Input;
