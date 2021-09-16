32902aa7796936a26c55031bdfbd840abfc33624
MATRIX-feather <midnightcarnival@outlook.com>
2021-09-17 04:28:26 +0800

    enchance: 优化PluginPiece的外观、调整PluginPiece的操作逻辑 misc: 调整PluginsSections中FillFlow的属性

--- osu.Game/Screens/Mvis/SideBar/PluginsPage/PluginPiece.cs
+++ osu.Game/Screens/Mvis/SideBar/PluginsPage/PluginPiece.cs
@@ -1,4 +1,3 @@
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
@@ -6,6 +5,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Input.Events;
@@ -15,17 +15,20 @@ using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays;
 using osu.Game.Screens.Mvis.Plugins;
-using osu.Game.Screens.Mvis.SideBar.Settings.Items;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Screens.Mvis.SideBar.PluginsPage
 {
-    public class PluginPiece : SettingsPieceBasePanel, IHasTooltip
+    public class PluginPiece : CompositeDrawable, IHasTooltip
     {
         public readonly MvisPlugin Plugin;
-        private SpriteIcon unloadIcon;
-        private OsuAnimatedButton unloadButton;
+        private TriangleButton unloadButton;
+        private TriangleButton disableButton;
+        private TriangleButton enableButton;
+
+        [Resolved]
+        private CustomColourProvider colourProvider { get; set; }
 
         public PluginPiece(MvisPlugin pl)
         {
@@ -36,59 +39,31 @@ namespace osu.Game.Screens.Mvis.SideBar.PluginsPage
         private DialogOverlay dialog { get; set; }
 
         private readonly BindableBool disabled = new BindableBool();
-
-        private readonly OsuSpriteText pluginNameText = new OsuSpriteText
-        {
-            Anchor = Anchor.CentreLeft,
-            Origin = Anchor.CentreLeft,
-        };
-
-        protected override Drawable CreateSideDrawable() => pluginNameText;
-
-        private Action action;
+        private GridContainer buttonsGrid;
+        private Circle statusCircle;
 
         [BackgroundDependencyLoader]
         private void load(MvisPluginManager manager)
         {
-            Width = 270;
-            pluginNameText.Text = string.IsNullOrEmpty(Plugin.Name) ? " " : Plugin.Name;
+            Width = 610;
+            AutoSizeAxes = Axes.Y;
+            AutoSizeDuration = 200;
+            AutoSizeEasing = Easing.OutQuint;
 
-            FillFlow.AddRange(new Drawable[]
+            Masking = true;
+            CornerRadius = 10;
+
+            BorderColour = Color4.White;
+
+            Anchor = Origin = Anchor.TopCentre;
+
+            InternalChildren = new Drawable[]
             {
-                new OsuSpriteText
+                new Box
                 {
-                    Text = string.IsNullOrEmpty(Plugin.Author) ? " " : Plugin.Author,
-                    RelativeSizeAxes = Axes.X,
-                    Truncate = true
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.InActiveColor
                 },
-                new OsuSpriteText
-                {
-                    Text = string.IsNullOrEmpty(Plugin.Description) ? " " : Plugin.Description,
-                    RelativeSizeAxes = Axes.X,
-                    Truncate = true
-                },
-                unloadButton = new OsuAnimatedButton
-                {
-                    Margin = new MarginPadding { Right = 15 },
-                    Size = new Vector2(30),
-                    Child = unloadIcon = new SpriteIcon
-                    {
-                        Anchor = Anchor.CentreRight,
-                        Origin = Anchor.CentreRight,
-                        RelativeSizeAxes = Axes.Both
-                    }
-                },
-                new OsuSpriteText
-                {
-                    Colour = Color4.Gold,
-                    Text = Plugin.Version != manager.PluginVersion
-                        ? Plugin.Version < manager.PluginVersion ? "为历史版本打造" : "为未来版本打造"
-                        : " ",
-                    Font = OsuFont.GetFont(weight: FontWeight.Bold)
-                }
-            });
-
-            AddInternal(
                 new DelayedLoadUnloadWrapper(() =>
                 {
                     var coverName = Plugin.GetType().Namespace?.Replace(".", "") ?? "Plugin";
@@ -105,14 +80,125 @@ namespace osu.Game.Screens.Mvis.SideBar.PluginsPage
                 }, 0)
                 {
                     Colour = ColourInfo.GradientHorizontal(
-                        Color4.White.Opacity(0),
+                        Color4.White.Opacity(0.25f),
                         Color4.White),
+
                     RelativeSizeAxes = Axes.Both,
                     Anchor = Anchor.TopRight,
-                    Origin = Anchor.TopRight,
-                    Width = 0.8f,
-                    Depth = 1
-                });
+                    Origin = Anchor.TopRight
+                },
+                statusCircle = new Circle
+                {
+                    Margin = new MarginPadding(13),
+                    Size = new Vector2(7),
+                    Colour = colourProvider.Background5
+                },
+                new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Direction = FillDirection.Vertical,
+                    Spacing = new Vector2(10),
+                    Padding = new MarginPadding(10),
+                    Children = new Drawable[]
+                    {
+                        new FillFlowContainer
+                        {
+                            Name = "信息FillFlow",
+                            AutoSizeAxes = Axes.Y,
+                            RelativeSizeAxes = Axes.X,
+                            Children = new Drawable[]
+                            {
+                                new OsuSpriteText
+                                {
+                                    Anchor = Anchor.TopRight,
+                                    Origin = Anchor.TopRight,
+                                    Text = string.IsNullOrEmpty(Plugin.Name) ? Plugin.GetType().Name : Plugin.Name,
+                                    Font = OsuFont.GetFont(size: 19)
+                                },
+                                new OsuSpriteText
+                                {
+                                    Text = string.IsNullOrEmpty(Plugin.Author) ? " " : Plugin.Author,
+                                    RelativeSizeAxes = Axes.X,
+                                    Truncate = true,
+                                    Anchor = Anchor.TopRight,
+                                    Origin = Anchor.TopRight,
+                                    Font = OsuFont.GetFont(size: 19)
+                                },
+                                new OsuSpriteText
+                                {
+                                    Text = string.IsNullOrEmpty(Plugin.Description) ? " " : Plugin.Description,
+                                    RelativeSizeAxes = Axes.X,
+                                    Truncate = true,
+                                    Anchor = Anchor.TopRight,
+                                    Origin = Anchor.TopRight,
+                                    Font = OsuFont.GetFont(size: 19)
+                                },
+                                new OsuSpriteText
+                                {
+                                    Colour = Color4.Gold,
+                                    Text = Plugin.Version != manager.PluginVersion
+                                        ? Plugin.Version < manager.PluginVersion ? "为历史版本打造" : "为未来版本打造"
+                                        : " ",
+                                    Alpha = Plugin.Version != manager.PluginVersion ? 1 : 0,
+                                    Font = OsuFont.GetFont(weight: FontWeight.Bold, size: 19),
+                                    Anchor = Anchor.TopRight,
+                                    Origin = Anchor.TopRight
+                                }
+                            }
+                        },
+                        buttonsGrid = new GridContainer
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Height = 30,
+                            Alpha = 0,
+                            Content = new[]
+                            {
+                                new Drawable[]
+                                {
+                                    unloadButton = new DangerousTriangleButton
+                                    {
+                                        Height = 30,
+                                        RelativeSizeAxes = Axes.X,
+                                        Width = 0.95f,
+                                        Text = "卸载此插件",
+                                        Action = () =>
+                                        {
+                                            dialog.Push(new PluginRemoveConfirmDialog($"你确定要卸载{Plugin.Name}吗?", blockPlugin => manager.UnLoadPlugin(Plugin, blockPlugin)));
+                                        },
+                                        Enabled = { Value = false },
+                                        Anchor = Anchor.BottomCentre,
+                                        Origin = Anchor.BottomCentre,
+                                    },
+                                    disableButton = new TriangleButton
+                                    {
+                                        Height = 30,
+                                        RelativeSizeAxes = Axes.X,
+                                        Width = 0.95f,
+                                        Text = "禁用此插件",
+                                        Action = () => manager.DisablePlugin(Plugin),
+                                        Enabled = { Value = false },
+                                        Anchor = Anchor.BottomCentre,
+                                        Origin = Anchor.BottomCentre,
+                                    },
+                                    enableButton = new TriangleButton
+                                    {
+                                        Height = 30,
+                                        RelativeSizeAxes = Axes.X,
+                                        Width = 0.95f,
+                                        Text = "启用此插件",
+                                        Action = () => manager.ActivePlugin(Plugin),
+                                        Enabled = { Value = false },
+                                        Anchor = Anchor.BottomCentre,
+                                        Origin = Anchor.BottomCentre,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                new HoverClickSounds()
+            };
 
             if (Plugin.Flags.Contains(MvisPlugin.PluginFlags.CanDisable))
             {
@@ -121,85 +207,109 @@ namespace osu.Game.Screens.Mvis.SideBar.PluginsPage
                 disabled.BindValueChanged(v =>
                 {
                     var activeListContainsPlugin = manager.GetActivePlugins().Contains(Plugin);
+                    enableButton.Enabled.Value = v.NewValue;
+                    disableButton.Enabled.Value = !v.NewValue;
+                    statusCircle.FadeColour(v.NewValue
+                        ? colourProvider.Background5
+                        : colourProvider.Light2, 200, Easing.OutQuint);
+                    TooltipText = string.Empty;
 
                     switch (v.NewValue)
                     {
                         case true:
+                            if (activeListContainsPlugin)
+                            {
+                                TooltipText = "该插件报告它已被禁用, 但我们在已启用的插件中找到了它。";
 
-                            BgBox.FadeColour(ColourProvider.InActiveColor, 300, Easing.OutQuint);
-                            FillFlow.FadeColour(Color4.White, 300, Easing.OutQuint);
-                            pluginNameText.FadeColour(Color4.White, 300, Easing.OutQuint);
-
-                            TooltipText = (!activeListContainsPlugin
-                                              ? "启用该插件"
-                                              : "该插件报告它已被禁用, 但我们在已启用的插件中找到了它。")
-                                          + $"\n({Plugin.Description})";
+                                statusCircle.FadeColour(Color4.Gold, 200, Easing.OutQuint);
+                            }
 
                             break;
 
                         case false:
+                            if (!activeListContainsPlugin)
+                            {
+                                TooltipText = "该插件报告它已被启用, 但我们没有在已启用的插件中找到它。";
 
-                            BgBox.FadeColour(ColourProvider.ActiveColor, 300, Easing.OutQuint);
-                            FillFlow.FadeColour(Color4.Black, 300, Easing.OutQuint);
-                            pluginNameText.FadeColour(Color4.Black, 300, Easing.OutQuint);
-
-                            TooltipText = (activeListContainsPlugin
-                                              ? "禁用该插件"
-                                              : "该插件报告它已被启用, 但我们没有在已启用的插件中找到它。")
-                                          + $"\n({Plugin.Description})";
+                                statusCircle.FadeColour(Color4.Gold, 200, Easing.OutQuint);
+                            }
 
                             break;
                     }
                 }, true);
-
-                action = () =>
-                {
-                    if (Plugin.Disabled.Value)
-                        manager.ActivePlugin(Plugin);
-                    else
-                        manager.DisablePlugin(Plugin);
-                };
             }
             else
             {
-                TooltipText = "目前不能通过此面板禁用该插件"
-                              + $"\n({Plugin.Description})";
+                TooltipText = "目前不能通过此面板禁用该插件";
             }
 
             if (Plugin.Flags.Contains(MvisPlugin.PluginFlags.CanUnload))
             {
-                unloadButton.Action = () =>
-                {
-                    dialog.Push(new PluginRemoveConfirmDialog($"你确定要卸载{Plugin.Name}吗?", blockPlugin => manager.UnLoadPlugin(Plugin, blockPlugin)));
-                };
-                unloadButton.TooltipText = "卸载此插件";
-                unloadIcon.Icon = FontAwesome.Solid.TrashAlt;
+                unloadButton.Enabled.Value = true;
             }
             else
             {
-                unloadButton.TooltipText = "目前不能通过此面板卸载该插件";
-                unloadIcon.Icon = FontAwesome.Solid.Ban;
+                unloadButton.Text = "目前不能通过此面板卸载该插件";
             }
-        }
-
-        protected override void OnColorChanged()
-        {
-            disabled.TriggerChange();
-            base.OnColorChanged();
         }
 
         public override void Hide()
         {
-            this.FadeOut(200, Easing.OutExpo);
+            this.FadeOut(200, Easing.OutExpo)
+                .MoveToX(-15, 200, Easing.OutExpo)
+                .ScaleTo(0.8f, 200, Easing.OutExpo);
+
             this.Delay(200).Expire();
         }
 
         public LocalisableString TooltipText { get; set; }
 
+        public override bool AcceptsFocus => true;
+
+        protected override void OnFocus(FocusEvent e)
+        {
+            BorderColour = colourProvider.Light2;
+            buttonsGrid.FadeIn(200, Easing.OutQuint);
+            base.OnFocus(e);
+        }
+
+        protected override void OnFocusLost(FocusLostEvent e)
+        {
+            BorderColour = Color4.White;
+            BorderThickness = 0f;
+            buttonsGrid.FadeOut(200, Easing.OutQuint);
+            base.OnFocusLost(e);
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            if (!HasFocus)
+                BorderThickness = 1.5f;
+
+            return base.OnHover(e);
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            if (!HasFocus)
+                BorderThickness = 0f;
+
+            base.OnHoverLost(e);
+        }
+
         protected override bool OnClick(ClickEvent e)
         {
-            action?.Invoke();
-            return base.OnClick(e);
+            BorderThickness = 3f;
+
+            if (HasFocus)
+            {
+                if (Plugin.Disabled.Value)
+                    enableButton.TriggerClick();
+                else
+                    disableButton.TriggerClick();
+            }
+
+            return true;
         }
 
         private class PluginBackgroundSprite : Sprite
