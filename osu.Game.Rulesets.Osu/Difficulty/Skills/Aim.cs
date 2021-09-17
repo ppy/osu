37523f303f3ea4ef4714d23f50f084ad6b99ab2b
Dean Herbert <pe@ppy.sh>
2021-09-17 16:53:08 +0900

    Restructure for readability

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -33,14 +33,88 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private const double slider_jump_multiplier = 0.875;
         private const double vel_change_multiplier = 6.5;
 
+        protected override double StrainValueOf(DifficultyHitObject current)
+        {
+            if (current.BaseObject is Spinner || Previous.Count <= 1)
+                return 0;
+
+            var osuCurrObj = (OsuDifficultyHitObject)current;
+            var osuPrevObj = (OsuDifficultyHitObject)Previous[0];
+            var osuLastObj = (OsuDifficultyHitObject)Previous[1];
+
+            var currVector = Vector2.Divide(osuCurrObj.JumpVector, (float)osuCurrObj.StrainTime);
+            var prevVector = Vector2.Divide(osuPrevObj.JumpVector, (float)osuPrevObj.StrainTime);
+
+            // Start with regular velocity.
+            double aimStrain = currVector.Length;
+
+            if (Precision.AlmostEquals(osuCurrObj.StrainTime, osuPrevObj.StrainTime, 10)) // Rhythms are the same.
+            {
+                if (osuCurrObj.Angle != null)
+                {
+                    double angle = osuCurrObj.Angle.Value;
+
+                    // Rewarding angles, take the smaller velocity as base.
+                    double angleBonus = Math.Min(currVector.Length, prevVector.Length);
+
+                    double wideAngleBonus = calcWideAngleBonus(angle);
+                    double acuteAngleBonus = calcAcuteAngleBonus(angle);
+
+                    if (osuCurrObj.StrainTime > 100)
+                        acuteAngleBonus = 0;
+                    else
+                    {
+                        acuteAngleBonus *= Math.Min(2, Math.Pow((100 - osuCurrObj.StrainTime) / 15, 1.5));
+                        wideAngleBonus *= Math.Pow(osuCurrObj.StrainTime / 100, 6);
+                    }
+
+                    if (acuteAngleBonus > wideAngleBonus)
+                        angleBonus = Math.Min(angleBonus, 150 / osuCurrObj.StrainTime) * Math.Min(1, Math.Pow(Math.Min(osuCurrObj.JumpDistance, osuPrevObj.JumpDistance) / 150, 2));
+
+                    angleBonus *= Math.Max(acuteAngleBonus * acute_angle_multiplier, wideAngleBonus * wide_angle_multiplier);
+
+                    // add in angle velocity.
+                    aimStrain += angleBonus;
+                }
+
+                if (prevVector.Length > currVector.Length)
+                {
+                    double velChangeBonus = Math.Max(0, Math.Sqrt((prevVector.Length - currVector.Length) * currVector.Length) - currVector.Length) * Math.Min(1, osuCurrObj.JumpDistance / 100);
+
+                    aimStrain += velChangeBonus * vel_change_multiplier * Math.Sqrt(100 / osuCurrObj.StrainTime);
+                }
+            }
+            else // There is a rhythm change
+            {
+                // Rewarding rhythm, take the smaller velocity as base.
+                double rhythmBonus = Math.Min(currVector.Length, prevVector.Length);
+
+                if (osuCurrObj.StrainTime + 10 < osuPrevObj.StrainTime && osuPrevObj.StrainTime > osuLastObj.StrainTime + 10)
+                    // Don't want to reward for a rhythm change back to back (unless its a double, which is why this only checks for fast -> slow -> fast).
+                    rhythmBonus = 0;
+
+                aimStrain += rhythmBonus * rhythm_variance_multiplier; // add in rhythm velocity.
+            }
+
+            if (osuCurrObj.TravelDistance != 0)
+            {
+                double sliderBonus = Math.Max(osuCurrObj.TravelDistance, slider_jump_multiplier * Math.Sqrt(osuCurrObj.TravelDistance * osuCurrObj.JumpDistance)) / osuCurrObj.StrainTime;
+
+                // Add in slider velocity.
+                aimStrain += sliderBonus * slider_multiplier;
+            }
+
+            return aimStrain;
+        }
+
         private double calcWideAngleBonus(double angle)
         {
             if (angle < Math.PI / 3)
                 return 0;
             if (angle < 2 * Math.PI / 3)
                 return Math.Pow(Math.Sin(1.5 * (angle - Math.PI / 3)), 2);
-            else
-                return 0.25 + 0.75 * Math.Pow(Math.Sin(1.5 * (Math.PI / 3 - (angle - 2 * Math.PI / 3))), 2);
+
+            return 0.25 + 0.75 * Math.Pow(Math.Sin(1.5 * (Math.PI / 3 - (angle - 2 * Math.PI / 3))), 2);
         }
 
         private double calcAcuteAngleBonus(double angle)
@@ -49,81 +123,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
                 return 0.5 + 0.5 * Math.Pow(Math.Sin(1.5 * angle), 2);
             if (angle < 2 * Math.PI / 3)
                 return Math.Pow(Math.Sin(1.5 * (Math.PI / 3 - (angle - Math.PI / 3))), 2);
-            else
-                return 0;
-        }
 
-        protected override double StrainValueOf(DifficultyHitObject current)
-        {
-            if (current.BaseObject is Spinner)
-                return 0;
-
-            double aimStrain = 0;
-
-            if (Previous.Count > 1)
-            {
-                var osuCurrObj = (OsuDifficultyHitObject)current;
-                var osuPrevObj = (OsuDifficultyHitObject)Previous[0];
-                var osuLastObj = (OsuDifficultyHitObject)Previous[1];
-
-                var currVector = Vector2.Divide(osuCurrObj.JumpVector, (float)osuCurrObj.StrainTime);
-                var prevVector = Vector2.Divide(osuPrevObj.JumpVector, (float)osuPrevObj.StrainTime);
-                // var lastVector = Vector2.Divide(osuLastObj.JumpVector, (float)osuLastObj.StrainTime); // not needed right now.
-
-                if (Precision.AlmostEquals(osuCurrObj.StrainTime, osuPrevObj.StrainTime, 10)) // Rhythms are the same.
-                {
-                    if (osuCurrObj.Angle != null)
-                    {
-                        double angle = osuCurrObj.Angle.Value;
-                        double angleBonus = Math.Min(currVector.Length, prevVector.Length); // Rewarding angles, take the smaller velocity as base.
-
-                        double wideAngleBonus = calcWideAngleBonus(angle);
-                        double acuteAngleBonus = calcAcuteAngleBonus(angle);
-
-                        if (osuCurrObj.StrainTime > 100)
-                            acuteAngleBonus = 0;
-                        else
-                        {
-                            acuteAngleBonus *= Math.Min(2, Math.Pow((100 - osuCurrObj.StrainTime) / 15, 1.5));
-                            wideAngleBonus *= Math.Pow(osuCurrObj.StrainTime / 100, 6);
-                        }
-
-                        if (acuteAngleBonus > wideAngleBonus)
-                            angleBonus = Math.Min(angleBonus, 150 / osuCurrObj.StrainTime) * Math.Min(1, Math.Pow(Math.Min(osuCurrObj.JumpDistance, osuPrevObj.JumpDistance) / 150, 2));
-
-                        angleBonus *= Math.Max(acuteAngleBonus * acute_angle_multiplier, wideAngleBonus * wide_angle_multiplier);
-
-                        aimStrain += angleBonus; // add in angle velocity.
-                    }
-
-                    if (prevVector.Length > currVector.Length)
-                    {
-                        double velChangeBonus = Math.Max(0, Math.Sqrt((prevVector.Length - currVector.Length) * currVector.Length) - currVector.Length) * Math.Min(1, osuCurrObj.JumpDistance / 100);
-
-                        aimStrain += velChangeBonus * vel_change_multiplier * Math.Sqrt(100 / osuCurrObj.StrainTime);
-                    }
-                }
-                else // There is a rhythm change
-                {
-                    double rhythmBonus = Math.Min(currVector.Length, prevVector.Length); // Rewarding rhythm, take the smaller velocity as base.
-
-                    if (osuCurrObj.StrainTime + 10 < osuPrevObj.StrainTime && osuPrevObj.StrainTime > osuLastObj.StrainTime + 10)
-                        rhythmBonus = 0; // Don't want to reward for a rhythm change back to back (unless its a double, which is why this only checks for fast -> slow -> fast).
-
-                    aimStrain += rhythmBonus * rhythm_variance_multiplier; // add in rhythm velocity.
-                }
-
-                if (osuCurrObj.TravelDistance != 0)
-                {
-                    double sliderBonus = Math.Max(osuCurrObj.TravelDistance, slider_jump_multiplier * Math.Sqrt(osuCurrObj.TravelDistance * osuCurrObj.JumpDistance)) / osuCurrObj.StrainTime;
-
-                    aimStrain += sliderBonus * slider_multiplier; // Add in slider velocity.
-                }
-
-                aimStrain += currVector.Length; // Add in regular velocity.
-            }
-
-            return aimStrain;
+            return 0;
         }
     }
 }
