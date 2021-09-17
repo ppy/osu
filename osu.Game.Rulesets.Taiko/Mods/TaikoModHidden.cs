2bcf36b0f4cc0b8a2577be7ff75d2de13264535d
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-09-17 17:33:32 +0900

    Don't apply normal visibility to increased visibility state

--- osu.Game.Rulesets.Taiko/Mods/TaikoModHidden.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModHidden.cs
@@ -39,7 +39,6 @@ namespace osu.Game.Rulesets.Taiko.Mods
 
         protected override void ApplyIncreasedVisibilityState(DrawableHitObject hitObject, ArmedState state)
         {
-            ApplyNormalVisibilityState(hitObject, state);
         }
 
         protected override void ApplyNormalVisibilityState(DrawableHitObject hitObject, ArmedState state)
