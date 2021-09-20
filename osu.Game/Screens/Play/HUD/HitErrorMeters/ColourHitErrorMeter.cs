36a20ab0b365d77878a76c0fbf4444bd6b67380f
sh0ckR6 <sh0ck.04@outlook.com>
2021-09-20 10:22:36 -0400

    Resolve failed test compilation

--- osu.Game/Screens/Play/HUD/HitErrorMeters/ColourHitErrorMeter.cs
+++ osu.Game/Screens/Play/HUD/HitErrorMeters/ColourHitErrorMeter.cs
@@ -33,7 +33,7 @@ namespace osu.Game.Screens.Play.HUD.HitErrorMeters
             judgementsFlow.Push(GetColourForHitResult(judgement.Type));
         }
 
-        protected override void Clear() => judgementsFlow.Clear();
+        public override void Clear() => judgementsFlow.Clear();
 
         private class JudgementFlow : FillFlowContainer<HitErrorCircle>
         {
