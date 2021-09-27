1dac1acf8ba021f8b5b52c3e98f257a7a30ad13b
GoldenMine0502 <ehe123@naver.com>
2021-09-28 02:37:53 +0900

    hidamari nerf

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -29,7 +29,12 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private const double min_speed_bonus = 75; // ~200BPM
         private const double speed_balancing_factor = 40;
 
+        private const double min_doubletap_nerf = 0.5; // minimum speedBonus value (eventually on stacked)
+        private const double max_doubletap_nerf = 1.0; // maximum speedBonus value 
+        private const double threshold_fully_contributing = 0.75; // minimum distance not influenced
+
         private readonly double greatWindow;
+
 
         public Speed(Mod[] mods, double hitWindowGreat)
             : base(mods)
@@ -59,9 +64,30 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             // 0.93 is derived from making sure 260bpm OD8 streams aren't nerfed harshly, whilst 0.92 limits the effect of the cap.
             strainTime /= Math.Clamp((strainTime / greatWindowFull) / 0.93, 0.92, 1);
 
+
+
             double speedBonus = 1.0;
             if (strainTime < min_speed_bonus)
-                speedBonus = 1 + Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor, 2);
+            {
+                double radius = ((OsuHitObject)osuCurrent.BaseObject).Radius;
+                /* a basecode to nerf acute high bpm stream and doubletap. 
+                 * because no one could do doubletap on spaced streams.
+                 * finally it is multiplied on speedBonus. 
+                 *
+                 * but this code make some speed players discourage.
+                 * it makes hidamari no uta be taken about 150pp.
+                 * and other stream maps be taken about 0-5pp in average. 
+                 
+                 ** We could consider to apply this code on angleBonus **/
+                double multiplierForSpeedBonus = min_doubletap_nerf +
+                    Math.Min(Math.Max(distance / (radius * threshold_fully_contributing), 1.0), 0.0)
+                    * (max_doubletap_nerf - min_doubletap_nerf)
+                    ;
+                speedBonus = 1 + Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor
+                    * multiplierForSpeedBonus
+                    , 2);
+            }
+
 
             double angleBonus = 1.0;
 
@@ -82,7 +108,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             return (1 + (speedBonus - 1) * 0.75)
                    * angleBonus
                    * (0.95 + speedBonus * Math.Pow(distance / single_spacing_threshold, 3.5))
-                   / strainTime;
+                   / strainTime
+                   //* multiplier
+                   ;
         }
     }
 }
