18ece6d6d150e8792ae73c3a0f3fc046e6dd737f
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-09-29 19:15:58 +0900

    Rename RecentParticipantsList -> DrawableRoomParticipantsList

--- osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoomParticipantsList.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneDrawableRoomParticipantsList.cs
@@ -13,16 +13,16 @@ using osu.Game.Users.Drawables;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
-    public class TestSceneRecentParticipantsList : OnlinePlayTestScene
+    public class TestSceneDrawableRoomParticipantsList : OnlinePlayTestScene
     {
-        private RecentParticipantsList list;
+        private DrawableRoomParticipantsList list;
 
         [SetUp]
         public new void Setup() => Schedule(() =>
         {
             SelectedRoom.Value = new Room { Name = { Value = "test room" } };
 
-            Child = list = new RecentParticipantsList
+            Child = list = new DrawableRoomParticipantsList
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
@@ -40,19 +40,19 @@ namespace osu.Game.Tests.Visual.Multiplayer
             });
 
             AddStep("set 8 circles", () => list.NumberOfCircles = 8);
-            AddAssert("0 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 0);
+            AddAssert("0 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 0);
 
             AddStep("add one more user", () => addUser(9));
-            AddAssert("2 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 2);
+            AddAssert("2 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 2);
 
             AddStep("remove first user", () => removeUserAt(0));
-            AddAssert("0 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 0);
+            AddAssert("0 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 0);
 
             AddStep("add one more user", () => addUser(9));
-            AddAssert("2 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 2);
+            AddAssert("2 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 2);
 
             AddStep("remove last user", () => removeUserAt(8));
-            AddAssert("0 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 0);
+            AddAssert("0 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 0);
         }
 
         [Test]
@@ -87,11 +87,11 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("set 3 circles", () => list.NumberOfCircles = 3);
             AddAssert("2 users displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 2);
-            AddAssert("48 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 48);
+            AddAssert("48 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 48);
 
             AddStep("set 10 circles", () => list.NumberOfCircles = 10);
             AddAssert("9 users displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 9);
-            AddAssert("41 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 41);
+            AddAssert("41 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 41);
         }
 
         [Test]
@@ -105,20 +105,20 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("remove from start", () => removeUserAt(0));
             AddAssert("3 circles displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 3);
-            AddAssert("46 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 46);
+            AddAssert("46 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 46);
 
             AddStep("remove from end", () => removeUserAt(SelectedRoom.Value.RecentParticipants.Count - 1));
             AddAssert("3 circles displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 3);
-            AddAssert("45 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 45);
+            AddAssert("45 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 45);
 
             AddRepeatStep("remove 45 users", () => removeUserAt(0), 45);
             AddAssert("3 circles displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 3);
-            AddAssert("0 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 0);
-            AddAssert("hidden users bubble hidden", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Alpha < 0.5f);
+            AddAssert("0 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 0);
+            AddAssert("hidden users bubble hidden", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Alpha < 0.5f);
 
             AddStep("remove another user", () => removeUserAt(0));
             AddAssert("2 circles displayed", () => list.ChildrenOfType<UpdateableAvatar>().Count() == 2);
-            AddAssert("0 hidden users", () => list.ChildrenOfType<RecentParticipantsList.HiddenUserCount>().Single().Count == 0);
+            AddAssert("0 hidden users", () => list.ChildrenOfType<DrawableRoomParticipantsList.HiddenUserCount>().Single().Count == 0);
 
             AddRepeatStep("remove the remaining two users", () => removeUserAt(0), 2);
             AddAssert("0 circles displayed", () => !list.ChildrenOfType<UpdateableAvatar>().Any());
