d40d473da2577512fd2395f74c68276db9ed4701
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-10-01 14:34:11 +0900

    Add new interface base types for models

---
+++ osu.Game/Beatmaps/IBeatmapMetadataInfo.cs
@@ -0,0 +1,117 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Linq;
+using osu.Framework.Localisation;
+
+#nullable enable
+
+namespace osu.Game.Beatmaps
+{
+    /// <summary>
+    /// Metadata representing a beatmap. May be shared between multiple beatmap difficulties.
+    /// </summary>
+    public interface IBeatmapMetadataInfo : IEquatable<IBeatmapMetadataInfo>
+    {
+        /// <summary>
+        /// The romanised title of this beatmap.
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// The unicode title of this beatmap.
+        /// </summary>
+        string TitleUnicode { get; }
+
+        /// <summary>
+        /// The romanised artist of this beatmap.
+        /// </summary>
+        string Artist { get; }
+
+        /// <summary>
+        /// The unicode artist of this beatmap.
+        /// </summary>
+        string ArtistUnicode { get; }
+
+        /// <summary>
+        /// The author of this beatmap.
+        /// </summary>
+        string Author { get; } // eventually should be linked to a persisted User.
+
+        /// <summary>
+        /// The source of this beatmap.
+        /// </summary>
+        string Source { get; }
+
+        /// <summary>
+        /// The tags of this beatmap.
+        /// </summary>
+        string Tags { get; }
+
+        /// <summary>
+        /// The time in milliseconds to begin playing the track for preview purposes.
+        /// If -1, the track should begin playing at 40% of its length.
+        /// </summary>
+        int PreviewTime { get; }
+
+        /// <summary>
+        /// The filename of the audio file consumed by this beatmap.
+        /// </summary>
+        string AudioFile { get; }
+
+        /// <summary>
+        /// The filename of the background image file consumed by this beatmap.
+        /// </summary>
+        string BackgroundFile { get; }
+
+        string DisplayTitle
+        {
+            get
+            {
+                string author = string.IsNullOrEmpty(Author) ? string.Empty : $"({Author})";
+                return $"{Artist} - {Title} {author}".Trim();
+            }
+        }
+
+        RomanisableString DisplayTitleRomanisable
+        {
+            get
+            {
+                string author = string.IsNullOrEmpty(Author) ? string.Empty : $"({Author})";
+                var artistUnicode = string.IsNullOrEmpty(ArtistUnicode) ? Artist : ArtistUnicode;
+                var titleUnicode = string.IsNullOrEmpty(TitleUnicode) ? Title : TitleUnicode;
+
+                return new RomanisableString($"{artistUnicode} - {titleUnicode} {author}".Trim(), $"{Artist} - {Title} {author}".Trim());
+            }
+        }
+
+        string[] SearchableTerms => new[]
+        {
+            Author,
+            Artist,
+            ArtistUnicode,
+            Title,
+            TitleUnicode,
+            Source,
+            Tags
+        }.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+        bool IEquatable<IBeatmapMetadataInfo>.Equals(IBeatmapMetadataInfo? other)
+        {
+            if (other == null)
+                return false;
+
+            return Title == other.Title
+                   && TitleUnicode == other.TitleUnicode
+                   && Artist == other.Artist
+                   && ArtistUnicode == other.ArtistUnicode
+                   && Author == other.Author
+                   && Source == other.Source
+                   && Tags == other.Tags
+                   && PreviewTime == other.PreviewTime
+                   && AudioFile == other.AudioFile
+                   && BackgroundFile == other.BackgroundFile;
+        }
+    }
+}
---
+++ osu.Game/Beatmaps/IRulesetInfo.cs
@@ -0,0 +1,46 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Game.Rulesets;
+
+#nullable enable
+
+namespace osu.Game.Beatmaps
+{
+    /// <summary>
+    /// A representation of a ruleset's metadata.
+    /// </summary>
+    public interface IRulesetInfo : IHasOnlineID
+    {
+        /// <summary>
+        /// The user-exposed name of this ruleset.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// An acronym defined by the ruleset that can be used as a permanent identifier.
+        /// </summary>
+        string ShortName { get; }
+
+        /// <summary>
+        /// A string representation of this ruleset, to be used with reflection to instantiate the ruleset represented by this metadata.
+        /// </summary>
+        string InstantiationInfo { get; }
+
+        public Ruleset? CreateInstance()
+        {
+            var type = Type.GetType(InstantiationInfo);
+
+            if (type == null)
+                return null;
+
+            var ruleset = Activator.CreateInstance(type) as Ruleset;
+
+            // overwrite the pre-populated RulesetInfo with a potentially database attached copy.
+            // ruleset.RulesetInfo = this;
+
+            return ruleset;
+        }
+    }
+}
