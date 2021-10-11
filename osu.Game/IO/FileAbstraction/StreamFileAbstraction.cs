8f093b9a11d5ec78a135b701e771cff4800a907d
Naxesss <30292137+Naxesss@users.noreply.github.com>
2021-10-11 13:56:26 +0200

    Use TagLib instead of ManagedBass

---
+++ osu.Game/IO/FileAbstraction/StreamFileAbstraction.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.IO;
+
+namespace osu.Game.IO.FileAbstraction
+{
+    public class StreamFileAbstraction : TagLib.File.IFileAbstraction
+    {
+        public StreamFileAbstraction(string filename, Stream fileStream)
+        {
+            ReadStream = fileStream;
+            Name = filename;
+        }
+
+        public string Name { get; }
+
+        public Stream ReadStream { get; }
+        public Stream WriteStream => ReadStream;
+
+        public void CloseStream(Stream aStream)
+        {
+            aStream.Position = 0;
+        }
+    }
+}
