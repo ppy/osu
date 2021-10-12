eed0770f4bef83665de56ba5c2264a3c8f859a18
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-10-12 17:17:40 +0900

    Remove unused using statements

--- osu.Game/Audio/Effects/AudioFilter.cs
+++ osu.Game/Audio/Effects/AudioFilter.cs
@@ -1,13 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Diagnostics;
 using ManagedBass.Fx;
 using osu.Framework.Audio.Mixing;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
-using osu.Framework.Logging;
 
 namespace osu.Game.Audio.Effects
 {
