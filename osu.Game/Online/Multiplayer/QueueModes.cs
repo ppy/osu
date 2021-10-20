c13dc52335df6b076d17020106c5835ba7b6f8ef
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-10-20 17:50:02 +0900

    Add english descriptions

--- osu.Game/Online/Multiplayer/QueueModes.cs
+++ osu.Game/Online/Multiplayer/QueueModes.cs
@@ -1,13 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.ComponentModel;
+
 namespace osu.Game.Online.Multiplayer
 {
     public enum QueueModes
     {
         // used for osu-web deserialization so names shouldn't be changed.
 
+        [Description("Host pick")]
         HostPick,
+
+        [Description("Karaoke")]
         Karaoke,
     }
 }
