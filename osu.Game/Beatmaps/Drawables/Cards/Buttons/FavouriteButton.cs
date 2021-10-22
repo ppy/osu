b5cbdcf9819dff87a57758b4577c80da49f9ed85
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-10-22 23:36:27 +0200

    Implement basic behaviour of favourite button

--- osu.Game/Beatmaps/Drawables/Cards/Buttons/FavouriteButton.cs
+++ osu.Game/Beatmaps/Drawables/Cards/Buttons/FavouriteButton.cs
@@ -1,8 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Framework.Logging;
+using osu.Game.Online.API;
+using osu.Game.Online.API.Requests;
+using osu.Game.Resources.Localisation.Web;
 
 namespace osu.Game.Beatmaps.Drawables.Cards.Buttons
 {
@@ -10,13 +15,54 @@ namespace osu.Game.Beatmaps.Drawables.Cards.Buttons
     {
         private readonly APIBeatmapSet beatmapSet;
 
+        private PostBeatmapFavouriteRequest favouriteRequest;
+
         public FavouriteButton(APIBeatmapSet beatmapSet)
         {
             this.beatmapSet = beatmapSet;
 
-            Icon.Icon = FontAwesome.Regular.Heart;
+            updateState();
         }
 
-        // TODO: implement behaviour
+        [BackgroundDependencyLoader]
+        private void load(IAPIProvider api)
+        {
+            Action = () =>
+            {
+                var actionType = beatmapSet.HasFavourited ? BeatmapFavouriteAction.UnFavourite : BeatmapFavouriteAction.Favourite;
+
+                favouriteRequest?.Cancel();
+                favouriteRequest = new PostBeatmapFavouriteRequest(beatmapSet.OnlineID, actionType);
+
+                Enabled.Value = false;
+                favouriteRequest.Success += () =>
+                {
+                    beatmapSet.HasFavourited = actionType == BeatmapFavouriteAction.Favourite;
+                    Enabled.Value = true;
+                    updateState();
+                };
+                favouriteRequest.Failure += e =>
+                {
+                    Logger.Error(e, $"Failed to {actionType.ToString().ToLower()} beatmap: {e.Message}");
+                    Enabled.Value = true;
+                };
+
+                api.Queue(favouriteRequest);
+            };
+        }
+
+        private void updateState()
+        {
+            if (beatmapSet.HasFavourited)
+            {
+                Icon.Icon = FontAwesome.Solid.Heart;
+                TooltipText = BeatmapsetsStrings.ShowDetailsUnfavourite;
+            }
+            else
+            {
+                Icon.Icon = FontAwesome.Regular.Heart;
+                TooltipText = BeatmapsetsStrings.ShowDetailsFavourite;
+            }
+        }
     }
 }
