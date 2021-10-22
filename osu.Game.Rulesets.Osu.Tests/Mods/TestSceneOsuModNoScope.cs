9bd3ba4abcb4e320f6c0b3e9cee8971ba6bb2a9a
Semyon Rozhkov <semro@outlook.com>
2021-10-23 02:26:51 +0300

    Add initial tests

--- osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModNoScope.cs
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModNoScope.cs
@@ -2,68 +2,142 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using osu.Framework.Testing;
+using osu.Framework.Utils;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Osu.Mods;
 using osu.Game.Rulesets.Osu.Objects;
 using osuTK;
 using osu.Game.Beatmaps.Timing;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Osu.Tests.Mods
 {
     public class TestSceneOsuModNoScope : OsuModTestScene
     {
         [Test]
-        public void BreaksAndSpinners() => CreateModTest(new ModTestData
+        public void CursorVisibleDuringBreak()
         {
-            Mod = new OsuModNoScope
+            CreateModTest(new ModTestData
             {
-                HiddenComboCount = { Value = 0 },
-                AlwaysHidden = { Value = false },
-            },
-            Autoplay = true,
-            Beatmap = new Beatmap
-            {
-                HitObjects = new List<HitObject>
+                Mod = new OsuModNoScope
                 {
-                    new HitCircle
+                    HiddenComboCount = { Value = 0 },
+                },
+                Autoplay = true,
+                Beatmap = new Beatmap
+                {
+                    HitObjects = new List<HitObject>
                     {
-                        Position = new Vector2(300, 192),
-                        StartTime = 0,
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 0,
+                        },
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 6000,
+                        }
                     },
-                    new HitCircle
+                    Breaks = new List<BreakPeriod>
                     {
-                        Position = new Vector2(300, 192),
-                        StartTime = 5000,
-                    },
-                    new Spinner
+                        new BreakPeriod
+                        (
+                            2000,
+                            4000
+                        ),
+                    }
+                },
+                PassCondition = () => breakTime() && cursorVisible(true)
+            });
+        }
+
+        [Test]
+        public void CursorVisibleDuringSpinner()
+        {
+            CreateModTest(new ModTestData
+            {
+                Mod = new OsuModNoScope
+                {
+                    HiddenComboCount = { Value = 0 },
+                },
+                Autoplay = true,
+                Beatmap = new Beatmap
+                {
+                    HitObjects = new List<HitObject>
                     {
-                        Position = new Vector2(256, 192),
-                        StartTime = 6000,
-                        EndTime = 9000,
-                    },
-                    new HitCircle
-                    {
-                        Position = new Vector2(300, 192),
-                        StartTime = 10000,
-                    },
-                    new HitCircle
-                    {
-                        Position = new Vector2(300, 192),
-                        StartTime = 12000,
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 0,
+                        },
+                        new Spinner
+                        {
+                            Position = new Vector2(256, 192),
+                            StartTime = 2000,
+                            EndTime = 4000,
+                        },
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 6000,
+                        }
                     },
                 },
-                Breaks = new List<BreakPeriod>
+                PassCondition = () => spinnerTime() && cursorVisible(true)
+            });
+        }
+
+        [Test]
+        public void CursorHiddenAfterHit()
+        {
+            CreateModTest(new ModTestData
+            {
+                Mod = new OsuModNoScope
                 {
-                    new BreakPeriod
-                    (
-                        1000,
-                        4000
-                    ),
-                }
-            },
-            PassCondition = () => Player.ScoreProcessor.Combo.Value >= 5
-        });
+                    HiddenComboCount = { Value = 1 },
+                },
+                Autoplay = true,
+                Beatmap = new Beatmap
+                {
+                    HitObjects = new List<HitObject>
+                    {
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 0,
+                        },
+                        new HitCircle
+                        {
+                            Position = new Vector2(300, 192),
+                            StartTime = 2000,
+                        },
+                    },
+                },
+                PassCondition = () => checkSomeHit() && cursorVisible(false)
+            });
+        }
+
+        private bool checkSomeHit() => Player.ScoreProcessor.JudgedHits >= 1;
+
+        private bool cursorVisible(bool visible)
+        {
+            float alpha = visible ? 1 : 0;
+            return Precision.AlmostEquals(Player.DrawableRuleset.Cursor.Alpha, alpha, 0.1);
+        }
+
+        private bool spinnerTime()
+        {
+            return Player.ChildrenOfType<DrawableSpinner>().SingleOrDefault()?.Progress > 0;
+        }
+
+        private bool breakTime()
+        {
+            return Player.IsBreakTime.Value;
+        }
     }
 }
