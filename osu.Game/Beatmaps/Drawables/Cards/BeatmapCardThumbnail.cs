a9ef56b89002ae229eda169c9a7895c36526ce65
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-10-23 17:59:52 +0200

    Split off thumbnail to separate component

---
+++ osu.Game/Beatmaps/Drawables/Cards/BeatmapCardThumbnail.cs
@@ -0,0 +1,54 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics;
+using osu.Game.Online.API.Requests.Responses;
+using osuTK.Graphics;
+
+namespace osu.Game.Beatmaps.Drawables.Cards
+{
+    public class BeatmapCardThumbnail : Container
+    {
+        public BindableBool Dimmed { get; } = new BindableBool();
+
+        private readonly APIBeatmapSet beatmapSetInfo;
+
+        private readonly UpdateableOnlineBeatmapSetCover cover;
+        private readonly Container content;
+
+        protected override Container<Drawable> Content => content;
+
+        public BeatmapCardThumbnail(APIBeatmapSet beatmapSetInfo)
+        {
+            this.beatmapSetInfo = beatmapSetInfo;
+
+            InternalChildren = new Drawable[]
+            {
+                cover = new UpdateableOnlineBeatmapSetCover(BeatmapSetCoverType.List)
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    OnlineInfo = beatmapSetInfo
+                },
+                content = new Container
+                {
+                    RelativeSizeAxes = Axes.Both
+                }
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            Dimmed.BindValueChanged(_ => updateState(), true);
+            FinishTransforms(true);
+        }
+
+        private void updateState()
+        {
+            cover.FadeColour(Dimmed.Value ? OsuColour.Gray(0.2f) : Color4.White, BeatmapCard.TRANSITION_DURATION, Easing.OutQuint);
+        }
+    }
+}
