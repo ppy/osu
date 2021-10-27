0644828e359afdfe85392bd955a2833e13d980e0
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-10-27 20:02:51 +0900

    Refactor `ModelDownloader` to allow for different `OnlineID matching`

--- osu.Game/Scoring/ScoreModelDownloader.cs
+++ osu.Game/Scoring/ScoreModelDownloader.cs
@@ -16,5 +16,8 @@ namespace osu.Game.Scoring
         }
 
         protected override ArchiveDownloadRequest<ScoreInfo> CreateDownloadRequest(ScoreInfo score, bool minimiseDownload) => new DownloadReplayRequest(score);
+
+        public override ArchiveDownloadRequest<ScoreInfo> GetExistingDownload(ScoreInfo model)
+            => CurrentDownloads.Find(r => r.Model.OnlineScoreID == model.OnlineScoreID);
     }
 }
