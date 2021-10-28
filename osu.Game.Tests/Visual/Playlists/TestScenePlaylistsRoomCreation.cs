7a3d9d72e59fed341942cb0f6f56d053e78063ff
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-10-28 15:28:16 +0900

    Rename test scene

--- osu.Game.Tests/Visual/Playlists/TestScenePlaylistsRoomCreation.cs
+++ osu.Game.Tests/Visual/Playlists/TestScenePlaylistsRoomCreation.cs
@@ -22,7 +22,7 @@ using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.Playlists
 {
-    public class TestScenePlaylistCreation : OnlinePlayTestScene
+    public class TestScenePlaylistsRoomCreation : OnlinePlayTestScene
     {
         private BeatmapManager manager;
         private RulesetStore rulesets;
