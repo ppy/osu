06fe6f063d40cbf776e3bb6aae10b1fbd34957d8
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-10-29 13:56:07 +0900

    Add base test scene

---
+++ osu.Game.Tests/Visual/Multiplayer/QueueingModes/TestSceneHostOnlyQueueingMode.cs
@@ -0,0 +1,12 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Online.Multiplayer.Queueing;
+
+namespace osu.Game.Tests.Visual.Multiplayer.QueueingModes
+{
+    public class TestSceneHostOnlyQueueingMode : QueueModeTestScene
+    {
+        protected override QueueModes Mode => QueueModes.HostOnly;
+    }
+}
