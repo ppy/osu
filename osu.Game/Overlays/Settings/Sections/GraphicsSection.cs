4d988340eb02121e4f4645b480d657bbafbbfe4b
Opelkuh <25430283+Opelkuh@users.noreply.github.com>
2021-10-30 01:02:53 +0200

    Add hardware acceleration toggle

--- osu.Game/Overlays/Settings/Sections/GraphicsSection.cs
+++ osu.Game/Overlays/Settings/Sections/GraphicsSection.cs
@@ -25,6 +25,7 @@ namespace osu.Game.Overlays.Settings.Sections
                 new LayoutSettings(),
                 new RendererSettings(),
                 new ScreenshotSettings(),
+                new VideoSettings(),
             };
         }
     }
