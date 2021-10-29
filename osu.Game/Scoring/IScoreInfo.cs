30f0957fb8f3059ebdde4b54749beb9f356c064d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-10-29 14:18:10 +0900

    Remove `Statistics` from interface until we figure how to properly deserialise

--- osu.Game/Scoring/IScoreInfo.cs
+++ osu.Game/Scoring/IScoreInfo.cs
@@ -2,11 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
 using osu.Game.Rulesets;
-using osu.Game.Rulesets.Scoring;
 using osu.Game.Users;
 
 namespace osu.Game.Scoring
@@ -29,13 +27,13 @@ namespace osu.Game.Scoring
 
         IBeatmapInfo Beatmap { get; }
 
-        Dictionary<HitResult, int> Statistics { get; }
-
         IRulesetInfo Ruleset { get; }
 
         public ScoreRank Rank { get; }
 
         // Mods is currently missing from this interface as the `IMod` class has properties which can't be fulfilled by `APIMod`,
         // but also doesn't expose `Settings`. We can consider how to implement this in the future if required.
+
+        // Statistics is also missing. This can be reconsidered once changes in serialisation have been completed.
     }
 }
