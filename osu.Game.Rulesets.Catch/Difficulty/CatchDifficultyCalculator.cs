39af2d95e2ffaf83e210f39f8fdd27385d88ab20
bastoo0 <bastoo0.yt@gmail.com>
2021-11-01 04:20:46 +0100

    remove circle size from diff calc

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -43,8 +43,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty
                 Mods = mods,
                 ApproachRate = preempt > 1200.0 ? -(preempt - 1800.0) / 120.0 : -(preempt - 1200.0) / 150.0 + 5.0,
                 MaxCombo = beatmap.HitObjects.Count(h => h is Fruit) + beatmap.HitObjects.OfType<JuiceStream>().SelectMany(j => j.NestedHitObjects).Count(h => !(h is TinyDroplet)),
-                Skills = skills,
-                CircleSize = beatmap.BeatmapInfo.BaseDifficulty.CircleSize
+                Skills = skills
             };
         }
 
