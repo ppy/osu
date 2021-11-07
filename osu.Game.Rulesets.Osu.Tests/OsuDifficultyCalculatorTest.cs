539a9b8d2f16f556dec2cecbe89e3ce5b92a3a3b
smoogipoo <87902114+bubb13gummy@users.noreply.github.com>
2021-11-08 00:33:57 +0900

    Update tests

--- osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
+++ osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
@@ -15,13 +15,13 @@ namespace osu.Game.Rulesets.Osu.Tests
     {
         protected override string ResourceAssembly => "osu.Game.Rulesets.Osu";
 
-        [TestCase(6.531832890435525d, "diffcalc-test")]
-        [TestCase(1.4644923495008817d, "zero-length-sliders")]
+        [TestCase(6.6975550434910005d, "diffcalc-test")]
+        [TestCase(1.4673500058356748d, "zero-length-sliders")]
         public void Test(double expected, string name)
             => base.Test(expected, name);
 
-        [TestCase(8.8067616302940852d, "diffcalc-test")]
-        [TestCase(1.7763214959309293d, "zero-length-sliders")]
+        [TestCase(8.938989502378238d, "diffcalc-test")]
+        [TestCase(1.779323508403831d, "zero-length-sliders")]
         public void TestClockRateAdjusted(double expected, string name)
             => Test(expected, name, new OsuModDoubleTime());
 
