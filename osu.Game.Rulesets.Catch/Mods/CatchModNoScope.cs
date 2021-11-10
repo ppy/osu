24b58b7bf3ed6e81c269761b4bb341052190990b
Semyon Rozhkov <87902114+bubb13gummy@users.noreply.github.com>
2021-11-10 04:10:05 +0300

    Hide catcher during banana shower

--- osu.Game.Rulesets.Catch/Mods/CatchModNoScope.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModNoScope.cs
@@ -2,25 +2,19 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Linq;
 using osu.Framework.Bindables;
-using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 using osu.Framework.Utils;
 using osu.Game.Configuration;
 using osu.Game.Overlays.Settings;
-using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.UI;
-using osu.Game.Utils;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
-    public class CatchModNoScope : ModNoScope, IApplicableToBeatmap, IUpdatableByPlayfield
+    public class CatchModNoScope : ModNoScope, IUpdatableByPlayfield
     {
         public override string Description => "Where's the catcher?";
-
-        public PeriodTracker BananaShowerPeriods;
 
         [SettingSource(
             "Hidden at combo",
@@ -35,15 +29,10 @@ namespace osu.Game.Rulesets.Catch.Mods
             MaxValue = 50,
         };
 
-        public void ApplyToBeatmap(IBeatmap beatmap)
-        {
-            BananaShowerPeriods = new PeriodTracker(beatmap.HitObjects.OfType<BananaShower>().Select(b => new Period(b.StartTime - TRANSITION_DURATION, b.EndTime)));
-        }
-
         public virtual void Update(Playfield playfield)
         {
             var catchPlayfield = (CatchPlayfield)playfield;
-            bool shouldAlwaysShowCatcher = IsBreakTime.Value || BananaShowerPeriods.IsInAny(catchPlayfield.Clock.CurrentTime);
+            bool shouldAlwaysShowCatcher = IsBreakTime.Value;
             float targetAlpha = shouldAlwaysShowCatcher ? 1 : ComboBasedAlpha;
             catchPlayfield.CatcherArea.Alpha = (float)Interpolation.Lerp(catchPlayfield.CatcherArea.Alpha, targetAlpha, Math.Clamp(catchPlayfield.Time.Elapsed / TRANSITION_DURATION, 0, 1));
         }
