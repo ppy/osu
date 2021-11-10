577bdade5b6369952a65fd44d8c3ad8663da92cd
Semyon Rozhkov <semro@outlook.com>
2021-11-10 04:10:05 +0300

    Hide catcher during banana shower

--- osu.Game.Rulesets.Catch.Tests/Mods/TestSceneCatchModNoScope.cs
+++ osu.Game.Rulesets.Catch.Tests/Mods/TestSceneCatchModNoScope.cs
@@ -2,15 +2,12 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Utils;
-using osu.Framework.Testing;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Timing;
 using osu.Game.Rulesets.Catch.Mods;
 using osu.Game.Rulesets.Catch.Objects;
-using osu.Game.Rulesets.Catch.Objects.Drawables;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Tests.Visual;
@@ -62,47 +59,6 @@ namespace osu.Game.Rulesets.Catch.Tests.Mods
         }
 
         [Test]
-        public void TestVisibleDuringBananaShower()
-        {
-            CreateModTest(new ModTestData
-            {
-                Mod = new CatchModNoScope
-                {
-                    HiddenComboCount = { Value = 0 },
-                },
-                Autoplay = true,
-                PassCondition = () => true,
-                Beatmap = new Beatmap
-                {
-                    HitObjects = new List<HitObject>
-                    {
-                        new Fruit
-                        {
-                            X = CatchPlayfield.CENTER_X,
-                            StartTime = 1000,
-                        },
-                        new BananaShower
-                        {
-                            StartTime = 2000,
-                            Duration = 2000,
-                        },
-                        new Fruit
-                        {
-                            X = CatchPlayfield.CENTER_X,
-                            StartTime = 5000,
-                        }
-                    }
-                }
-            });
-
-            AddUntilStep("wait for catcher to hide", () => catcherAlphaAlmostEquals(0));
-            AddUntilStep("wait for start of banana shower", isBananaShower);
-            AddUntilStep("wait for catcher to show", () => catcherAlphaAlmostEquals(1));
-            AddUntilStep("wait for end of banana shower", () => !isBananaShower());
-            AddUntilStep("wait for catcher to hide", () => catcherAlphaAlmostEquals(0));
-        }
-
-        [Test]
         public void TestVisibleAfterComboBreak()
         {
             CreateModTest(new ModTestData
@@ -142,8 +98,6 @@ namespace osu.Game.Rulesets.Catch.Tests.Mods
             AddStep("break combo", () => Player.ScoreProcessor.Combo.Set(0));
             AddUntilStep("wait for catcher to show", () => catcherAlphaAlmostEquals(1));
         }
-
-        private bool isBananaShower() => Player.ChildrenOfType<DrawableBananaShower>().SingleOrDefault() != null;
 
         private bool isBreak() => Player.IsBreakTime.Value;
 
