3b9a1b2b2218c931e4348c6ca80f8c1195fdd54c
apollo-dw <87902114+bubb13gummy@users.noreply.github.com>
2021-11-11 02:57:34 +0000

    Clean up clamp logic relating to slider end estimate

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -127,9 +127,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 
             // We assume 15% of sliders in a map are difficult since there's no way to tell from the performance calculator.
             double estimateDifficultSliders = Attributes.SliderCount * 0.15;
-            double estimateSliderEndsDropped = Math.Min(Attributes.SliderCount, Math.Min(countOk + countMeh + countMiss, Attributes.MaxCombo - scoreMaxCombo));
-
-            estimateSliderEndsDropped = Math.Min(estimateSliderEndsDropped, estimateDifficultSliders);
+            double estimateSliderEndsDropped = Math.Min(estimateDifficultSliders, Math.Min(countOk + countMeh + countMiss, Attributes.MaxCombo - scoreMaxCombo));
 
             double sliderNerfFactor = (1 - Attributes.SliderFactor) * Math.Pow(1 - estimateSliderEndsDropped / estimateDifficultSliders, 3) + Attributes.SliderFactor;
             aimValue *= Math.Max(Attributes.SliderFactor, sliderNerfFactor);
