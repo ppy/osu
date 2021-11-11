0c4a314fda7eb6abe4b0fcd6fcc32a3da3e6d907
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2021-11-11 13:09:48 +0100

    Fix HUD unstable rate counter not including judgements before load complete
    Also unifies UR calculation logic with the results screen statistic item
    to reduce duplication.

--- osu.Game/Screens/Play/HUD/UnstableRateCounter.cs
+++ osu.Game/Screens/Play/HUD/UnstableRateCounter.cs
@@ -1,9 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
@@ -29,8 +26,6 @@ namespace osu.Game.Screens.Play.HUD
         private const float alpha_when_invalid = 0.3f;
         private readonly Bindable<bool> valid = new Bindable<bool>();
 
-        private readonly List<double> hitOffsets = new List<double>();
-
         [Resolved]
         private ScoreProcessor scoreProcessor { get; set; }
 
@@ -54,41 +49,20 @@ namespace osu.Game.Screens.Play.HUD
         {
             base.LoadComplete();
 
-            scoreProcessor.NewJudgement += onJudgementAdded;
-            scoreProcessor.JudgementReverted += onJudgementReverted;
-        }
-
-        private void onJudgementAdded(JudgementResult judgement)
-        {
-            if (!changesUnstableRate(judgement)) return;
-
-            hitOffsets.Add(judgement.TimeOffset);
+            scoreProcessor.NewJudgement += updateDisplay;
+            scoreProcessor.JudgementReverted += updateDisplay;
             updateDisplay();
         }
 
-        private void onJudgementReverted(JudgementResult judgement)
-        {
-            if (judgement.FailedAtJudgement || !changesUnstableRate(judgement)) return;
-
-            hitOffsets.RemoveAt(hitOffsets.Count - 1);
-            updateDisplay();
-        }
+        private void updateDisplay(JudgementResult _) => Scheduler.AddOnce(updateDisplay);
 
         private void updateDisplay()
         {
-            // At Count = 0, we get NaN, While we are allowing count = 1, it will be 0 since average = offset.
-            if (hitOffsets.Count > 0)
-            {
-                double mean = hitOffsets.Average();
-                double squares = hitOffsets.Select(offset => Math.Pow(offset - mean, 2)).Sum();
-                Current.Value = (int)(Math.Sqrt(squares / hitOffsets.Count) * 10);
-                valid.Value = true;
-            }
-            else
-            {
-                Current.Value = 0;
-                valid.Value = false;
-            }
+            double? unstableRate = scoreProcessor.HitEvents.CalculateUnstableRate();
+
+            valid.Value = unstableRate != null;
+            if (unstableRate != null)
+                Current.Value = (int)unstableRate.Value;
         }
 
         protected override IHasText CreateText() => new TextComponent
@@ -102,8 +76,8 @@ namespace osu.Game.Screens.Play.HUD
 
             if (scoreProcessor == null) return;
 
-            scoreProcessor.NewJudgement -= onJudgementAdded;
-            scoreProcessor.JudgementReverted -= onJudgementReverted;
+            scoreProcessor.NewJudgement -= updateDisplay;
+            scoreProcessor.JudgementReverted -= updateDisplay;
         }
 
         private class TextComponent : CompositeDrawable, IHasText
