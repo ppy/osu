385df51b06d83e72bce2b3cc6b4a2d545e9f614b
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-11-11 15:25:58 +0100

    Ensure editor test player is exited on completion

--- osu.Game/Screens/Edit/EditorPlayer.cs
+++ osu.Game/Screens/Edit/EditorPlayer.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Screens;
 using osu.Game.Screens.Play;
 
 namespace osu.Game.Screens.Edit
@@ -10,6 +11,16 @@ namespace osu.Game.Screens.Edit
         public EditorPlayer()
             : base(new PlayerConfiguration { ShowResults = false })
         {
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            ScoreProcessor.HasCompleted.BindValueChanged(completed =>
+            {
+                if (completed.NewValue)
+                    Scheduler.AddDelayed(this.Exit, RESULTS_DISPLAY_DELAY);
+            });
         }
 
         protected override void PrepareReplay()
