62b0da6358033ed5595d04eee543c467737cb543
Dean Herbert <pe@ppy.sh>
2021-11-15 18:46:35 +0900

    Expose `IFileInfo.StoragePath` helper method on interface type

--- osu.Game/IO/IFileInfo.cs
+++ osu.Game/IO/IFileInfo.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.IO;
+
 #nullable enable
 
 namespace osu.Game.IO
@@ -14,5 +16,7 @@ namespace osu.Game.IO
         /// SHA-256 hash of the file content.
         /// </summary>
         string Hash { get; }
+
+        string StoragePath => Path.Combine(Hash.Remove(1), Hash.Remove(2), Hash);
     }
 }
