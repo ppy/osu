250e34c326b886c7ced85219d962ec52178fe47a
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2021-11-15 16:06:46 +0900

    Add to/from database mapping functions to difficulty attributes

--- osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyAttributes.cs
+++ osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyAttributes.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using osu.Game.Rulesets.Difficulty;
 
@@ -13,5 +14,25 @@ namespace osu.Game.Rulesets.Mania.Difficulty
 
         [JsonProperty("score_multiplier")]
         public double ScoreMultiplier { get; set; }
+
+        public override IEnumerable<(int attributeId, object value)> ToDatabase()
+        {
+            foreach (var v in base.ToDatabase())
+                yield return v;
+
+            // Todo: Mania doesn't output MaxCombo attribute for some reason.
+            yield return (11, StarRating);
+            yield return (13, GreatHitWindow);
+            yield return (15, ScoreMultiplier);
+        }
+
+        public override void FromDatabase(IReadOnlyDictionary<int, double> values, int hitCircleCount, int spinnerCount)
+        {
+            base.FromDatabase(values, hitCircleCount, spinnerCount);
+
+            StarRating = values[11];
+            GreatHitWindow = values[13];
+            ScoreMultiplier = values[15];
+        }
     }
 }
