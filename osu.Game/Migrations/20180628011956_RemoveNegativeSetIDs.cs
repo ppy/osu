b4b35ed3f4cd09434fbe68e9d75e030e18b95558
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-11-19 13:27:06 +0900

    Fix stray typo

--- osu.Game/Migrations/20180628011956_RemoveNegativeSetIDs.cs
+++ osu.Game/Migrations/20180628011956_RemoveNegativeSetIDs.cs
@@ -6,7 +6,7 @@ namespace osu.Game.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // There was a change that baetmaps were being loaded with "-1" online IDs, which is completely incorrect.
+            // There was a change that beatmaps were being loaded with "-1" online IDs, which is completely incorrect.
             // This ensures there will not be unique key conflicts as a result of these incorrectly imported beatmaps.
             migrationBuilder.Sql("UPDATE BeatmapSetInfo SET OnlineBeatmapSetID = null WHERE OnlineBeatmapSetID <= 0");
         }
