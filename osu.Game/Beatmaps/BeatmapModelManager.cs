416ee2447a02f60b29d39b19d65851628b139236
Dean Herbert <pe@ppy.sh>
2021-11-25 16:33:35 +0900

    Move archive filename helper method to extension method

--- osu.Game/Beatmaps/BeatmapModelManager.cs
+++ osu.Game/Beatmaps/BeatmapModelManager.cs
@@ -212,7 +212,7 @@ namespace osu.Game.Beatmaps
                     var fileInfo = setInfo.Files.SingleOrDefault(f => string.Equals(f.Filename, beatmapInfo.Path, StringComparison.OrdinalIgnoreCase)) ?? new BeatmapSetFileInfo();
 
                     // metadata may have changed; update the path with the standard format.
-                    beatmapInfo.Path = GetValidFilename($"{metadata.Artist} - {metadata.Title} ({metadata.Author}) [{beatmapInfo.DifficultyName}].osu");
+                    beatmapInfo.Path = $"{metadata.Artist} - {metadata.Title} ({metadata.Author}) [{beatmapInfo.DifficultyName}].osu".GetValidArchiveContentFilename();
 
                     beatmapInfo.MD5Hash = stream.ComputeMD5Hash();
 
