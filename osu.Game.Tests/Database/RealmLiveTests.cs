13612c0d02b0b1d94d3b8b293c025757a452cf01
Dean Herbert <pe@ppy.sh>
2021-11-26 14:38:39 +0900

    Add equality support to `ILive` types

--- osu.Game.Tests/Database/RealmLiveTests.cs
+++ osu.Game.Tests/Database/RealmLiveTests.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using osu.Game.Beatmaps;
 using osu.Game.Database;
 using osu.Game.Models;
 using Realms;
@@ -18,15 +17,15 @@ namespace osu.Game.Tests.Database
     public class RealmLiveTests : RealmTest
     {
         [Test]
-        public void TestLiveCastability()
+        public void TestLiveEquality()
         {
             RunTestWithRealm((realmFactory, _) =>
             {
-                RealmLive<RealmBeatmap> beatmap = realmFactory.CreateContext().Write(r => r.Add(new RealmBeatmap(CreateRuleset(), new RealmBeatmapDifficulty(), new RealmBeatmapMetadata()))).ToLive();
+                ILive<RealmBeatmap> beatmap = realmFactory.CreateContext().Write(r => r.Add(new RealmBeatmap(CreateRuleset(), new RealmBeatmapDifficulty(), new RealmBeatmapMetadata()))).ToLive();
 
-                ILive<IBeatmapInfo> iBeatmap = beatmap;
+                ILive<RealmBeatmap> beatmap2 = realmFactory.CreateContext().All<RealmBeatmap>().First().ToLive();
 
-                Assert.AreEqual(0, iBeatmap.Value.Length);
+                Assert.AreEqual(beatmap, beatmap2);
             });
         }
 
