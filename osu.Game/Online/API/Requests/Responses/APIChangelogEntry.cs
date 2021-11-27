1477cdb9e52e9a92ca95b3c02763ed84ef66bc06
Jai Sharma <jai@jai.moe>
2021-11-27 16:44:32 +0000

    Add support for raw markdown `Message` field in `APIChangelogEntry`

--- osu.Game/Online/API/Requests/Responses/APIChangelogEntry.cs
+++ osu.Game/Online/API/Requests/Responses/APIChangelogEntry.cs
@@ -32,6 +32,9 @@ namespace osu.Game.Online.API.Requests.Responses
         [JsonProperty("title")]
         public string Title { get; set; }
 
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
         [JsonProperty("message_html")]
         public string MessageHtml { get; set; }
 
