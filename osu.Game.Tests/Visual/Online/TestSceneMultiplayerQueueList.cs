1a0945dabadbe14990004cd8dfbbb02a8d8271eb
Dan Balasescu <smoogipoo@smgi.me>
2021-12-09 14:11:29 +0900

    Siplify condition, allow host to always remove items

--- osu.Game.Tests/Visual/Online/TestSceneMultiplayerQueueList.cs
+++ osu.Game.Tests/Visual/Online/TestSceneMultiplayerQueueList.cs
@@ -13,6 +13,7 @@ using osu.Framework.Testing;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
 using osu.Game.Graphics.Containers;
+using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Online.Rooms;
 using osu.Game.Rulesets;
@@ -55,6 +56,7 @@ namespace osu.Game.Tests.Visual.Online
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     Size = new Vector2(500, 300),
+                    SelectedItem = { BindTarget = Client.CurrentMatchPlayingItem },
                     Items = { BindTarget = Client.APIRoom!.Playlist }
                 };
             });
@@ -70,69 +72,59 @@ namespace osu.Game.Tests.Visual.Online
         }
 
         [Test]
-        public void TestDeleteButtonHiddenWithSingleItem()
-        {
-            AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
-            AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
-
-            assertDeleteButtonVisibility(0, false);
-
-            addPlaylistItem(() => API.LocalUser.Value.OnlineID);
-            assertDeleteButtonVisibility(0, true);
-
-            deleteItem(1);
-            assertDeleteButtonVisibility(0, false);
-        }
-
-        [Test]
-        public void TestDeleteButtonHiddenInHostOnlyMode()
-        {
-            AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
-            AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
-            addPlaylistItem(() => 1234);
-
-            AddStep("set host-only queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.HostOnly }));
-            AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.HostOnly);
-
-            assertDeleteButtonVisibility(0, false);
-        }
-
-        [Test]
-        public void TestOnlyItemOwnerHasDeleteButton()
+        public void TestDeleteButtonAlwaysVisibleForHost()
         {
             AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
             AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
 
             addPlaylistItem(() => API.LocalUser.Value.OnlineID);
-            assertDeleteButtonVisibility(0, true);
             assertDeleteButtonVisibility(1, true);
+            addPlaylistItem(() => 1234);
+            assertDeleteButtonVisibility(2, true);
+        }
 
+        [Test]
+        public void TestDeleteButtonOnlyVisibleForItemOwnerIfNotHost()
+        {
+            AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
+            AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
+
+            AddStep("join other user", () => Client.AddUser(new APIUser { Id = 1234 }));
+            AddStep("set other user as host", () => Client.TransferHost(1234));
+
+            addPlaylistItem(() => API.LocalUser.Value.OnlineID);
+            assertDeleteButtonVisibility(1, true);
             addPlaylistItem(() => 1234);
             assertDeleteButtonVisibility(2, false);
+
+            AddStep("set local user as host", () => Client.TransferHost(API.LocalUser.Value.OnlineID));
+            assertDeleteButtonVisibility(1, true);
+            assertDeleteButtonVisibility(2, true);
         }
 
         [Test]
-        public void TestNonOwnerDoesNotHaveDeleteButton()
+        public void TestCurrentItemDoesNotHaveDeleteButton()
         {
             AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
             AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
 
-            addPlaylistItem(() => 1234);
-            assertDeleteButtonVisibility(0, true);
-            assertDeleteButtonVisibility(1, false);
-        }
-
-        [Test]
-        public void TestSelectedItemDoesNotHaveDeleteButton()
-        {
-            AddStep("set all players queue mode", () => Client.ChangeSettings(new MultiplayerRoomSettings { QueueMode = QueueMode.AllPlayers }));
-            AddUntilStep("wait for queue mode change", () => Client.APIRoom?.QueueMode.Value == QueueMode.AllPlayers);
+            assertDeleteButtonVisibility(0, false);
 
             addPlaylistItem(() => API.LocalUser.Value.OnlineID);
-            assertDeleteButtonVisibility(0, true);
-
-            AddStep("set first playlist item as selected", () => playlist.SelectedItem.Value = playlist.Items[0]);
             assertDeleteButtonVisibility(0, false);
+            assertDeleteButtonVisibility(1, true);
+
+            // Run through gameplay.
+            AddStep("set state to ready", () => Client.ChangeUserState(API.LocalUser.Value.Id, MultiplayerUserState.Ready));
+            AddUntilStep("local state is ready", () => Client.LocalUser?.State == MultiplayerUserState.Ready);
+            AddStep("start match", () => Client.StartMatch());
+            AddUntilStep("match started", () => Client.LocalUser?.State == MultiplayerUserState.WaitingForLoad);
+            AddStep("set state to loaded", () => Client.ChangeUserState(API.LocalUser.Value.Id, MultiplayerUserState.Loaded));
+            AddUntilStep("local state is playing", () => Client.LocalUser?.State == MultiplayerUserState.Playing);
+            AddStep("set state to finished play", () => Client.ChangeUserState(API.LocalUser.Value.Id, MultiplayerUserState.FinishedPlay));
+            AddUntilStep("local state is results", () => Client.LocalUser?.State == MultiplayerUserState.Results);
+
+            assertDeleteButtonVisibility(1, false);
         }
 
         private void addPlaylistItem(Func<int> userId)
