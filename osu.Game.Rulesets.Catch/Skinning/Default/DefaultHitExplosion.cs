98524d60a4a299ed10babd237515ca897c0b39fe
Joseph Madamba <madamba.joehu@outlook.com>
2021-12-09 21:04:31 -0800

    Fix clear identifier typos

--- osu.Game.Rulesets.Catch/Skinning/Default/DefaultHitExplosion.cs
+++ osu.Game.Rulesets.Catch/Skinning/Default/DefaultHitExplosion.cs
@@ -90,9 +90,9 @@ namespace osu.Game.Rulesets.Catch.Skinning.Default
                 .ResizeTo(largeFaint.Size * new Vector2(5, 1), duration, Easing.OutQuint)
                 .FadeOut(duration * 2);
 
-            const float angle_variangle = 15; // should be less than 45
-            directionalGlow1.Rotation = StatelessRNG.NextSingle(-angle_variangle, angle_variangle, randomSeed, 4);
-            directionalGlow2.Rotation = StatelessRNG.NextSingle(-angle_variangle, angle_variangle, randomSeed, 5);
+            const float angle_variable = 15; // should be less than 45
+            directionalGlow1.Rotation = StatelessRNG.NextSingle(-angle_variable, angle_variable, randomSeed, 4);
+            directionalGlow2.Rotation = StatelessRNG.NextSingle(-angle_variable, angle_variable, randomSeed, 5);
 
             this.FadeInFromZero(50).Then().FadeOut(duration, Easing.Out);
         }
