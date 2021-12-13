f2f5c36e9b947b07a06af5b1c966d135215db25a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2021-12-13 16:34:48 +0900

    Move test `ScoreInfo` creation to `TestResources`
    The main goal here is to remove the inheritance, since realm doesn't
    like that. Unfortunate that we can't use object initialisers in a few of
    these places, but no real way around that.

--- osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneStatisticsPanel.cs
@@ -11,6 +11,7 @@ using osu.Game.Scoring;
 using osu.Game.Screens.Ranking.Statistics;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Scoring;
+using osu.Game.Tests.Resources;
 using osuTK;
 
 namespace osu.Game.Tests.Visual.Ranking
@@ -20,10 +21,8 @@ namespace osu.Game.Tests.Visual.Ranking
         [Test]
         public void TestScoreWithTimeStatistics()
         {
-            var score = new TestScoreInfo(new OsuRuleset().RulesetInfo)
-            {
-                HitEvents = TestSceneHitEventTimingDistributionGraph.CreateDistributedHitEvents()
-            };
+            var score = TestResources.CreateTestScoreInfo();
+            score.HitEvents = TestSceneHitEventTimingDistributionGraph.CreateDistributedHitEvents();
 
             loadPanel(score);
         }
@@ -31,10 +30,8 @@ namespace osu.Game.Tests.Visual.Ranking
         [Test]
         public void TestScoreWithPositionStatistics()
         {
-            var score = new TestScoreInfo(new OsuRuleset().RulesetInfo)
-            {
-                HitEvents = createPositionDistributedHitEvents()
-            };
+            var score = TestResources.CreateTestScoreInfo();
+            score.HitEvents = createPositionDistributedHitEvents();
 
             loadPanel(score);
         }
@@ -42,7 +39,7 @@ namespace osu.Game.Tests.Visual.Ranking
         [Test]
         public void TestScoreWithoutStatistics()
         {
-            loadPanel(new TestScoreInfo(new OsuRuleset().RulesetInfo));
+            loadPanel(TestResources.CreateTestScoreInfo(new OsuRuleset().RulesetInfo));
         }
 
         [Test]
