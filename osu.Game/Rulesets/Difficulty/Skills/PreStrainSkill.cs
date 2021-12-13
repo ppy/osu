a8400346fb4b6f3f1f57184b70cb4e391ea3bd83
GoldenMine0502 <ehe123@naver.com>
2021-12-13 20:40:01 +0900

    code quality

--- osu.Game/Rulesets/Difficulty/Skills/PreStrainSkill.cs
+++ osu.Game/Rulesets/Difficulty/Skills/PreStrainSkill.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
-using osu.Game.Rulesets.Difficulty.Utils;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Difficulty.Skills
