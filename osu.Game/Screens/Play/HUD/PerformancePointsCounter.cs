97b58458f11386c9d665cc175364c629e9e2b5d0
denamo <artstarxd@outlook.com>
2021-12-14 22:21:08 +0300

    Fix PPcounter

--- osu.Game/Screens/Play/HUD/PerformancePointsCounter.cs
+++ osu.Game/Screens/Play/HUD/PerformancePointsCounter.cs
@@ -221,11 +221,11 @@ namespace osu.Game.Screens.Play.HUD
 
             protected override IBeatmap GetBeatmap() => gameplayBeatmap;
 
-            protected override Texture GetBackground() => throw new NotImplementedException();
+            protected override Texture GetBackground() => Background;
 
-            protected override Track GetBeatmapTrack() => throw new NotImplementedException();
+            protected override Track GetBeatmapTrack() => Track;
 
-            protected internal override ISkin GetSkin() => throw new NotImplementedException();
+            protected internal override ISkin GetSkin() => Skin;
 
             public override Stream GetStream(string storagePath) => throw new NotImplementedException();
         }
