0f65c4e960667785802e2ef9f102d683628da202
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-12-23 18:59:02 +0100

    Extract interface for ruleset config cache

--- osu.Game/Rulesets/RulesetConfigCache.cs
+++ osu.Game/Rulesets/RulesetConfigCache.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Game.Configuration;
@@ -10,11 +9,7 @@ using osu.Game.Rulesets.Configuration;
 
 namespace osu.Game.Rulesets
 {
-    /// <summary>
-    /// A cache that provides a single <see cref="IRulesetConfigManager"/> per-ruleset.
-    /// This is done to support referring to and updating ruleset configs from multiple locations in the absence of inter-config bindings.
-    /// </summary>
-    public class RulesetConfigCache : Component
+    public class RulesetConfigCache : Component, IRulesetConfigCache
     {
         private readonly RealmContextFactory realmFactory;
         private readonly RulesetStore rulesets;
@@ -43,12 +38,6 @@ namespace osu.Game.Rulesets
             }
         }
 
-        /// <summary>
-        /// Retrieves the <see cref="IRulesetConfigManager"/> for a <see cref="Ruleset"/>.
-        /// </summary>
-        /// <param name="ruleset">The <see cref="Ruleset"/> to retrieve the <see cref="IRulesetConfigManager"/> for.</param>
-        /// <returns>The <see cref="IRulesetConfigManager"/> defined by <paramref name="ruleset"/>, null if <paramref name="ruleset"/> doesn't define one.</returns>
-        /// <exception cref="InvalidOperationException">If <paramref name="ruleset"/> doesn't have a valid <see cref="RulesetInfo.ID"/>.</exception>
         public IRulesetConfigManager GetConfigFor(Ruleset ruleset)
         {
             if (!configCache.TryGetValue(ruleset.RulesetInfo.ShortName, out var config))
