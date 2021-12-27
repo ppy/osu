9f8ad9f833f5057150297c455fee597dc6bc7db1
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-12-27 11:28:20 +0100

    Fix menu background storyboard stopping after track reload

--- osu.Game/Graphics/Backgrounds/BeatmapBackgroundWithStoryboard.cs
+++ osu.Game/Graphics/Backgrounds/BeatmapBackgroundWithStoryboard.cs
@@ -1,17 +1,25 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable enable
+
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Timing;
 using osu.Game.Beatmaps;
+using osu.Game.Overlays;
 using osu.Game.Storyboards.Drawables;
 
 namespace osu.Game.Graphics.Backgrounds
 {
     public class BeatmapBackgroundWithStoryboard : BeatmapBackground
     {
+        private InterpolatingFramedClock storyboardClock = null!;
+
+        [Resolved(CanBeNull = true)]
+        private MusicController? musicController { get; set; }
+
         public BeatmapBackgroundWithStoryboard(WorkingBeatmap beatmap, string fallbackTextureName = "Backgrounds/bg1")
             : base(beatmap, fallbackTextureName)
         {
@@ -30,8 +38,33 @@ namespace osu.Game.Graphics.Backgrounds
             {
                 RelativeSizeAxes = Axes.Both,
                 Volume = { Value = 0 },
-                Child = new DrawableStoryboard(Beatmap.Storyboard) { Clock = new InterpolatingFramedClock(Beatmap.Track) }
+                Child = new DrawableStoryboard(Beatmap.Storyboard) { Clock = storyboardClock = new InterpolatingFramedClock(Beatmap.Track) }
             }, AddInternal);
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            if (musicController != null)
+                musicController.TrackChanged += onTrackChanged;
+        }
+
+        private void onTrackChanged(WorkingBeatmap newBeatmap, TrackChangeDirection _)
+        {
+            if (newBeatmap != Beatmap)
+                return;
+
+            // `MusicController` will sometimes reload the track, even when the working beatmap technically hasn't changed.
+            // ensure that the storyboard's clock is always using the latest track instance.
+            storyboardClock.ChangeSource(newBeatmap.Track);
+            storyboardClock.ProcessFrame();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            if (musicController != null)
+                musicController.TrackChanged -= onTrackChanged;
         }
     }
 }
