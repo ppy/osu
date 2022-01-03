6356180b6abd5a11ab32508ad3dcd2984c561aee
Dean Herbert <pe@ppy.sh>
2022-01-03 12:53:58 +0900

    Remove unnecessary code and fix double nesting causing filtering to not work

--- osu.Game/Overlays/Settings/Sections/Gameplay/AudioSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Gameplay/AudioSettings.cs
@@ -2,12 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Localisation;
 using osu.Game.Configuration;
 using osu.Game.Localisation;
-using osu.Framework.Graphics.Containers;
 
 namespace osu.Game.Overlays.Settings.Sections.Gameplay
 {
@@ -15,32 +13,18 @@ namespace osu.Game.Overlays.Settings.Sections.Gameplay
     {
         protected override LocalisableString Header => GameplaySettingsStrings.AudioHeader;
 
-        private Bindable<float> positionalHitsoundsLevel;
-
-        private FillFlowContainer<SettingsSlider<float>> positionalHitsoundsSettings;
-
         [BackgroundDependencyLoader]
         private void load(OsuConfigManager config, OsuConfigManager osuConfig)
         {
-            positionalHitsoundsLevel = osuConfig.GetBindable<float>(OsuSetting.PositionalHitsoundsLevel);
             Children = new Drawable[]
             {
-                positionalHitsoundsSettings = new FillFlowContainer<SettingsSlider<float>>
+                new SettingsSlider<float>
                 {
-                    Direction = FillDirection.Vertical,
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Masking = true,
-                    Children = new[]
-                    {
-                        new SettingsSlider<float>
-                        {
-                            LabelText = AudioSettingsStrings.PositionalLevel,
-                            Current = positionalHitsoundsLevel,
-                            KeyboardStep = 0.01f,
-                            DisplayAsPercentage = true
-                        }
-                    }
+                    LabelText = AudioSettingsStrings.PositionalLevel,
+                    Keywords = new[] { @"positional", @"balance" },
+                    Current = osuConfig.GetBindable<float>(OsuSetting.PositionalHitsoundsLevel),
+                    KeyboardStep = 0.01f,
+                    DisplayAsPercentage = true
                 },
                 new SettingsCheckbox
                 {
