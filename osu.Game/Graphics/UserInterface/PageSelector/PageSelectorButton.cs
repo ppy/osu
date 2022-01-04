ecc8d7ee4f5c23db8ed8797bb20fc607438e4aa9
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-04 16:58:32 +0900

    Clean up unnecessary complexity

--- osu.Game/Graphics/UserInterface/PageSelector/PageSelectorButton.cs
+++ osu.Game/Graphics/UserInterface/PageSelector/PageSelectorButton.cs
@@ -1,61 +1,64 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Extensions.Color4Extensions;
-using osu.Game.Graphics.Sprites;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osuTK.Graphics;
+using osu.Game.Graphics.Sprites;
 using osuTK;
-using osu.Framework.Extensions.IEnumerableExtensions;
+using osuTK.Graphics;
 
 namespace osu.Game.Graphics.UserInterface.PageSelector
 {
     public class PageSelectorButton : PageSelectorItem
     {
-        private readonly Box fadeBox;
+        private readonly string text;
+
+        private Box fadeBox;
         private SpriteIcon icon;
         private OsuSpriteText name;
-        private FillFlowContainer buttonContent;
+
+        private readonly Anchor alignment;
 
         public PageSelectorButton(bool rightAligned, string text)
         {
-            Add(fadeBox = new Box
-            {
-                RelativeSizeAxes = Axes.Both,
-                Colour = Color4.Black.Opacity(100)
-            });
-
-            var alignment = rightAligned ? Anchor.x0 : Anchor.x2;
-
-            buttonContent.ForEach(drawable =>
-            {
-                drawable.Anchor = Anchor.y1 | alignment;
-                drawable.Origin = Anchor.y1 | alignment;
-            });
-
-            icon.Icon = alignment == Anchor.x2 ? FontAwesome.Solid.ChevronLeft : FontAwesome.Solid.ChevronRight;
-
-            name.Text = text.ToUpper();
+            this.text = text;
+            alignment = rightAligned ? Anchor.x0 : Anchor.x2;
         }
 
-        protected override Drawable CreateContent() => buttonContent = new FillFlowContainer
+        protected override Drawable CreateContent() => new Container
         {
-            AutoSizeAxes = Axes.Both,
-            Direction = FillDirection.Horizontal,
-            Spacing = new Vector2(3, 0),
+            RelativeSizeAxes = Axes.Y,
+            AutoSizeAxes = Axes.X,
             Children = new Drawable[]
             {
-                name = new OsuSpriteText
+                new FillFlowContainer
                 {
-                    Font = OsuFont.GetFont(size: 12),
-                },
-                icon = new SpriteIcon
-                {
-                    Size = new Vector2(8),
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Spacing = new Vector2(3, 0),
+                    Children = new Drawable[]
+                    {
+                        name = new OsuSpriteText
+                        {
+                            Font = OsuFont.GetFont(size: 12),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            Text = text.ToUpper(),
+                        },
+                        icon = new SpriteIcon
+                        {
+                            Icon = alignment == Anchor.x2 ? FontAwesome.Solid.ChevronLeft : FontAwesome.Solid.ChevronRight,
+                            Size = new Vector2(8),
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                        },
+                    }
                 },
             }
         };
@@ -70,6 +73,13 @@ namespace osu.Game.Graphics.UserInterface.PageSelector
         protected override void LoadComplete()
         {
             base.LoadComplete();
+
+            CircularContent.Add(fadeBox = new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Color4.Black.Opacity(100)
+            });
+
             Enabled.BindValueChanged(enabled => fadeBox.FadeTo(enabled.NewValue ? 0 : 1, DURATION), true);
         }
 
