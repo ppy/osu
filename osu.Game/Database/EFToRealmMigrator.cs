45a23e5a43f90e897a4761774147540772c7d430
Dean Herbert <pe@ppy.sh>
2022-01-13 17:51:23 +0900

    Add EF to realm score migration

--- osu.Game/Database/EFToRealmMigrator.cs
+++ osu.Game/Database/EFToRealmMigrator.cs
@@ -3,9 +3,13 @@
 
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using osu.Game.Beatmaps;
 using osu.Game.Configuration;
 using osu.Game.Models;
+using osu.Game.Rulesets;
+using osu.Game.Scoring;
 using osu.Game.Skinning;
+using Realms;
 
 #nullable enable
 
@@ -30,6 +34,70 @@ namespace osu.Game.Database
             {
                 migrateSettings(db);
                 migrateSkins(db);
+
+                migrateScores(db);
+            }
+        }
+
+        private void migrateScores(DatabaseWriteUsage db)
+        {
+            // can be removed 20220730.
+            var existingScores = db.Context.ScoreInfo
+                                   .Include(s => s.Ruleset)
+                                   .Include(s => s.BeatmapInfo)
+                                   .Include(s => s.Files)
+                                   .ThenInclude(f => f.FileInfo)
+                                   .ToList();
+
+            // previous entries in EF are removed post migration.
+            if (!existingScores.Any())
+                return;
+
+            using (var realm = realmContextFactory.CreateContext())
+            using (var transaction = realm.BeginWrite())
+            {
+                // only migrate data if the realm database is empty.
+                // note that this cannot be written as: `realm.All<ScoreInfo>().All(s => s.Protected)`, because realm does not support `.All()`.
+                if (!realm.All<ScoreInfo>().Any())
+                {
+                    foreach (var score in existingScores)
+                    {
+                        var realmScore = new ScoreInfo
+                        {
+                            Hash = score.Hash,
+                            DeletePending = score.DeletePending,
+                            OnlineID = score.OnlineID ?? -1,
+                            ModsJson = score.ModsJson,
+                            StatisticsJson = score.StatisticsJson,
+                            User = score.User,
+                            TotalScore = score.TotalScore,
+                            MaxCombo = score.MaxCombo,
+                            Accuracy = score.Accuracy,
+                            HasReplay = ((IScoreInfo)score).HasReplay,
+                            Date = score.Date,
+                            PP = score.PP,
+                            BeatmapInfo = realm.All<BeatmapInfo>().First(b => b.Hash == score.BeatmapInfo.Hash),
+                            Ruleset = realm.Find<RulesetInfo>(score.Ruleset.ShortName),
+                            Rank = score.Rank,
+                            HitEvents = score.HitEvents,
+                            Passed = score.Passed,
+                            Combo = score.Combo,
+                            Position = score.Position,
+                            Statistics = score.Statistics,
+                            Mods = score.Mods,
+                            APIMods = score.APIMods,
+                        };
+
+                        migrateFiles(score, realm, realmScore);
+
+                        realm.Add(realmScore);
+                    }
+                }
+
+                db.Context.RemoveRange(existingScores);
+                // Intentionally don't clean up the files, so they don't get purged by EF.
+
+                transaction.Commit();
             }
         }
 
@@ -77,15 +145,7 @@ namespace osu.Game.Database
                             InstantiationInfo = skin.InstantiationInfo,
                         };
 
-                        foreach (var file in skin.Files)
-                        {
-                            var realmFile = realm.Find<RealmFile>(file.FileInfo.Hash);
-
-                            if (realmFile == null)
-                                realm.Add(realmFile = new RealmFile { Hash = file.FileInfo.Hash });
-
-                            realmSkin.Files.Add(new RealmNamedFileUsage(realmFile, file.Filename));
-                        }
+                        migrateFiles(skin, realm, realmSkin);
 
                         realm.Add(realmSkin);
 
@@ -98,6 +158,19 @@ namespace osu.Game.Database
                 // Intentionally don't clean up the files, so they don't get purged by EF.
 
                 transaction.Commit();
+            }
+        }
+
+        private static void migrateFiles<T>(IHasFiles<T> fileSource, Realm realm, IHasRealmFiles realmObject) where T : INamedFileInfo
+        {
+            foreach (var file in fileSource.Files)
+            {
+                var realmFile = realm.Find<RealmFile>(file.FileInfo.Hash);
+
+                if (realmFile == null)
+                    realm.Add(realmFile = new RealmFile { Hash = file.FileInfo.Hash });
+
+                realmObject.Files.Add(new RealmNamedFileUsage(realmFile, file.Filename));
             }
         }
 
