9cb15cf74d0adf9a0e6bb2a6ef6f60c86cb93a42
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-14 21:39:42 +0900

    Update one more mismatched test implementation

--- osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapLeaderboard.cs
+++ osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapLeaderboard.cs
@@ -45,7 +45,7 @@ namespace osu.Game.Tests.Visual.SongSelect
 
             dependencies.Cache(rulesetStore = new RulesetStore(ContextFactory));
             dependencies.Cache(beatmapManager = new BeatmapManager(LocalStorage, ContextFactory, rulesetStore, null, dependencies.Get<AudioManager>(), Resources, dependencies.Get<GameHost>(), Beatmap.Default));
-            dependencies.Cache(scoreManager = new ScoreManager(dependencies.Get<RulesetStore>(), () => beatmapManager, LocalStorage, dependencies.Get<RealmContextFactory>(), Scheduler));
+            dependencies.Cache(scoreManager = new ScoreManager(rulesetStore, () => beatmapManager, LocalStorage, dependencies.Get<RealmContextFactory>(), Scheduler));
             Dependencies.Cache(ContextFactory);
 
             return dependencies;
