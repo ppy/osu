b5155ff746008d0a47883762b8d793f23e255205
Henry Lin <87902114+bubb13gummy@users.noreply.github.com>
2022-01-17 18:28:17 +0800

    Display performance breakdown in a tooltip

---
+++ osu.Game/Screens/Ranking/Expanded/Statistics/PerformanceStatisticTooltip.cs
@@ -0,0 +1,80 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+using System;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.Difficulty;
+using osuTK;
+
+namespace osu.Game.Screens.Ranking.Expanded.Statistics
+{
+    public class PerformanceStatisticTooltip : VisibilityContainer, ITooltip<PerformanceAttributes>
+    {
+        private readonly Box background;
+
+        protected override Container<Drawable> Content { get; }
+
+        public PerformanceStatisticTooltip()
+        {
+            AutoSizeAxes = Axes.Both;
+            Masking = true;
+            CornerRadius = 5;
+
+            InternalChildren = new Drawable[]
+            {
+                background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both
+                },
+                Content = new FillFlowContainer
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Padding = new MarginPadding { Left = 10, Right = 10, Top = 5, Bottom = 5 }
+                },
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            background.Colour = colours.Gray3;
+        }
+
+        protected override void PopIn() => this.FadeIn(200, Easing.OutQuint);
+        protected override void PopOut() => this.FadeOut(200, Easing.OutQuint);
+
+        private PerformanceAttributes lastAttributes;
+
+        public void SetContent(PerformanceAttributes attributes)
+        {
+            if (attributes == lastAttributes)
+                return;
+
+            lastAttributes = attributes;
+
+            UpdateDisplay(attributes);
+        }
+
+        protected virtual void UpdateDisplay(PerformanceAttributes attributes)
+        {
+            Content.Clear();
+
+            foreach (PerformanceDisplayAttribute attr in attributes.GetAttributesForDisplay())
+            {
+                Content.Add(new OsuSpriteText
+                {
+                    Font = OsuFont.GetFont(weight: FontWeight.Regular),
+                    Text = $"{attr.DisplayName}: {(int)Math.Round(attr.Value, MidpointRounding.AwayFromZero)}"
+                });
+            }
+        }
+
+        public void Move(Vector2 pos) => Position = pos;
+    }
+}
