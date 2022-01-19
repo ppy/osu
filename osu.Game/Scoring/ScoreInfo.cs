d5309fba3aa1020349ba51329e5ab410c36869eb
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-19 14:17:56 +0900

    Fix legacy score imports not correctly getting classic mod assigned

--- osu.Game/Scoring/ScoreInfo.cs
+++ osu.Game/Scoring/ScoreInfo.cs
@@ -191,9 +191,8 @@ namespace osu.Game.Scoring
             }
             set
             {
-                apiMods = null;
+                clearAllMods();
                 mods = value;
-
                 updateModsJson();
             }
         }
@@ -220,12 +219,17 @@ namespace osu.Game.Scoring
             }
             set
             {
+                clearAllMods();
                 apiMods = value;
-                mods = null;
-
-                // We potentially can't update this yet due to Ruleset being late-bound, so instead update on read as necessary.
                 updateModsJson();
             }
+        }
+
+        private void clearAllMods()
+        {
+            ModsJson = string.Empty;
+            mods = null;
+            apiMods = null;
         }
 
         private void updateModsJson()
