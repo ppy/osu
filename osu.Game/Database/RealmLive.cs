23044992092ac2601951c58ab7196c04a9f4870a
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-21 02:10:14 +0900

    Fix `RealmLive` not refetching if update thread context was closed at some point

--- osu.Game/Database/RealmLive.cs
+++ osu.Game/Database/RealmLive.cs
@@ -130,7 +130,7 @@ namespace osu.Game.Database
         {
             Debug.Assert(ThreadSafety.IsUpdateThread);
 
-            if (dataIsFromUpdateThread)
+            if (dataIsFromUpdateThread && !data.Realm.IsClosed)
                 return;
 
             dataIsFromUpdateThread = true;
