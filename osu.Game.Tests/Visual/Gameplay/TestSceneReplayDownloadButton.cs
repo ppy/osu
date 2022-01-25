8518eff44f0c189169ef208266c1c943008fa18b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-25 15:23:51 +0900

    Remove `Task` from the inner-most `Import` method in `RealmArchiveModelImporter`
    One of my pending work items for post-realm merge.
    
    The lowest-level import task is no longer asynchronous, as we don't want
    it to span multiple threads to allow easier interaction with realm.
    Removing the `Task` spec simplifies a heap of usages.
    
    Individual usages should decide whether they want to run the import
    asynchronously, by either using an alternative override or spooling up a
    thread themselves.

--- osu.Game.Tests/Visual/Gameplay/TestSceneReplayDownloadButton.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneReplayDownloadButton.cs
@@ -147,7 +147,7 @@ namespace osu.Game.Tests.Visual.Gameplay
 
             AddUntilStep("state is not downloaded", () => downloadButton.State.Value == DownloadState.NotDownloaded);
 
-            AddStep("import score", () => imported = scoreManager.Import(getScoreInfo(true)).GetResultSafely());
+            AddStep("import score", () => imported = scoreManager.Import(getScoreInfo(true)));
 
             AddUntilStep("state is available", () => downloadButton.State.Value == DownloadState.LocallyAvailable);
 
