70d8587b158762b8d04ca50f24484543ebc57b51
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-01-27 21:34:23 +0100

    Add support for nested realm write calls

--- osu.Game/Database/RealmExtensions.cs
+++ osu.Game/Database/RealmExtensions.cs
@@ -10,6 +10,14 @@ namespace osu.Game.Database
     {
         public static void Write(this Realm realm, Action<Realm> function)
         {
+            // if a higher-level transaction is already taking place, simply proceed with the write.
+            // the assumption is that the write will be committed by the aforementioned higher-level transaction.
+            if (realm.IsInTransaction)
+            {
+                function(realm);
+                return;
+            }
+
             using var transaction = realm.BeginWrite();
             function(realm);
             transaction.Commit();
@@ -17,6 +25,11 @@ namespace osu.Game.Database
 
         public static T Write<T>(this Realm realm, Func<Realm, T> function)
         {
+            // if a higher-level transaction is already taking place, simply proceed with the write.
+            // the assumption is that the write will be committed by the aforementioned higher-level transaction.
+            if (realm.IsInTransaction)
+                return function(realm);
+
             using var transaction = realm.BeginWrite();
             var result = function(realm);
             transaction.Commit();
