37218e7cc0a7ccf4411c79d22a914ece62ebd33d
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-01-27 21:34:03 +0100

    Add failing test coverage for desired nested write behaviour

--- osu.Game.Tests/Database/GeneralUsageTests.cs
+++ osu.Game.Tests/Database/GeneralUsageTests.cs
@@ -2,11 +2,14 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using osu.Framework.Extensions;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
+using osu.Game.Rulesets;
 
 #nullable enable
 
@@ -101,6 +104,46 @@ namespace osu.Game.Tests.Database
                 {
                 }
             });
+        }
+
+        /// <summary>
+        /// This test ensures that the <see cref="RealmExtensions.Write"/> and <see cref="RealmExtensions.Write{T}"/> methods
+        /// can be used in a nested fashion.
+        /// </summary>
+        [Test]
+        public void TestNestedWrites()
+        {
+            RunTestWithRealm((realm, __) =>
+            {
+                realm.Write(r =>
+                {
+                    r.Add(new RulesetInfo { ShortName = "ruleset1" });
+                    checkCountOffThread(0, realm);
+
+                    // check the non-generic variant. note that the braces are required to exercise this.
+                    r.Write(r2 =>
+                    {
+                        r2.Add(new RulesetInfo { ShortName = "ruleset2" });
+                    });
+                    checkCountOffThread(0, realm);
+
+                    // check the generic variant.
+                    _ = r.Write(r3 => r3.Add(new RulesetInfo { ShortName = "ruleset3" }));
+                    checkCountOffThread(0, realm);
+                });
+
+                int finalCount = realm.Run(r => r.All<RulesetInfo>().Count());
+                Assert.AreEqual(3, finalCount);
+                checkCountOffThread(3, realm);
+            });
+
+            void checkCountOffThread(int expectedCount, RealmAccess realmAccess)
+            {
+                int actualCount = -1;
+                // run a refetch from another thread to check what is the actual count which will be seen by other threads.
+                Task.Factory.StartNew(() => actualCount = realmAccess.Run(r1 => r1.All<RulesetInfo>().Count()), TaskCreationOptions.LongRunning | TaskCreationOptions.HideScheduler).WaitSafely();
+                Assert.AreEqual(expectedCount, actualCount);
+            }
         }
     }
 }
