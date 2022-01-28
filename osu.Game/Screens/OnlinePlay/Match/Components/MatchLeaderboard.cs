6d4e4a4855f19aee3641f5332460dbc1d2b58b0c
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-01-28 22:47:45 +0900

    Remove fetch callback logic completely

--- osu.Game/Screens/OnlinePlay/Match/Components/MatchLeaderboard.cs
+++ osu.Game/Screens/OnlinePlay/Match/Components/MatchLeaderboard.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Game.Online.API;
@@ -32,7 +30,7 @@ namespace osu.Game.Screens.OnlinePlay.Match.Components
 
         protected override bool IsOnlineScope => true;
 
-        protected override APIRequest FetchScores(Action<IEnumerable<APIUserScoreAggregate>> scoresCallback)
+        protected override APIRequest FetchScores()
         {
             if (roomId.Value == null)
                 return null;
@@ -41,7 +39,7 @@ namespace osu.Game.Screens.OnlinePlay.Match.Components
 
             req.Success += r =>
             {
-                scoresCallback?.Invoke(r.Leaderboard);
+                Scores = r.Leaderboard;
                 TopScore = r.UserScore;
             };
 
