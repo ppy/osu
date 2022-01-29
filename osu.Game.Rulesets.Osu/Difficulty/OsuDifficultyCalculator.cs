ee3a9da0021086b811382a46f67c6d07564fc583
James <51536154+tsunyoku@users.noreply.github.com>
2022-01-29 23:38:15 +0000

    Fix CodeFactor issues

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -40,7 +40,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 
             double sliderFactor = aimRating > 0 ? aimRatingNoSliders / aimRating : 1;
 
-
             double baseAimPerformance = Math.Pow(5 * Math.Max(1, aimRating / 0.0675) - 4, 3) / 100000;
             double baseSpeedPerformance = Math.Pow(5 * Math.Max(1, speedRating / 0.0675) - 4, 3) / 100000;
             double baseFlashlightPerformance = 0.0;
@@ -50,7 +49,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 
             if (mods.Any(h => h is OsuModRelax))
             {
-
                 // Similar idea to the PP system speed crosscheck, but for star rating.
                 double speedCrosscheck = baseAimPerformance / baseSpeedPerformance;
                 if (speedCrosscheck < 1.0) // From testing, higher values tend to be entirely unrelated or ridiculously difficult.
