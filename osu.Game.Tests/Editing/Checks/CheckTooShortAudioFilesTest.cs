b978010b481fd8e46a7b0448b813a3e547d375ce
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-02-01 21:47:55 +0100

    Remove unused `allowMissing` parameter in audio file check test
    No longer used since 7f95400f466beafe6fa94c002cfd6ed8dc3f4f94.

--- osu.Game.Tests/Editing/Checks/CheckTooShortAudioFilesTest.cs
+++ osu.Game.Tests/Editing/Checks/CheckTooShortAudioFilesTest.cs
@@ -52,7 +52,7 @@ namespace osu.Game.Tests.Editing.Checks
             beatmap.BeatmapInfo.BeatmapSet.Files.Add(CheckTestHelpers.CreateMockFile("jpg"));
 
             // Should fail to load, but not produce an error due to the extension not being expected to load.
-            Assert.IsEmpty(check.Run(getContext(null, allowMissing: true)));
+            Assert.IsEmpty(check.Run(getContext(null)));
         }
 
         [Test]
@@ -91,7 +91,7 @@ namespace osu.Game.Tests.Editing.Checks
         {
             using (var resourceStream = TestResources.OpenResource("Samples/missing.mp3"))
             {
-                Assert.IsEmpty(check.Run(getContext(resourceStream, allowMissing: true)));
+                Assert.IsEmpty(check.Run(getContext(resourceStream)));
             }
         }
 
@@ -107,7 +107,7 @@ namespace osu.Game.Tests.Editing.Checks
             }
         }
 
-        private BeatmapVerifierContext getContext(Stream resourceStream, bool allowMissing = false)
+        private BeatmapVerifierContext getContext(Stream resourceStream)
         {
             var mockWorkingBeatmap = new Mock<TestWorkingBeatmap>(beatmap, null, null);
             mockWorkingBeatmap.Setup(w => w.GetStream(It.IsAny<string>())).Returns(resourceStream);
