0b1fef38af1660d6c17c87bdb0c94cff9770cfb0
Henry Lin <henry.ys.lin@gmail.com>
2022-02-05 21:36:34 +0800

    Use the playable beatmap provided in `CreateStatisticsForScore`

--- osu.Game.Rulesets.Taiko/TaikoRuleset.cs
+++ osu.Game.Rulesets.Taiko/TaikoRuleset.cs
@@ -224,7 +224,7 @@ namespace osu.Game.Rulesets.Taiko
                 {
                     Columns = new[]
                     {
-                        new StatisticItem("Performance Breakdown", () => new PerformanceBreakdownChart(score)
+                        new StatisticItem("Performance Breakdown", () => new PerformanceBreakdownChart(score, playableBeatmap)
                         {
                             RelativeSizeAxes = Axes.X,
                             AutoSizeAxes = Axes.Y
