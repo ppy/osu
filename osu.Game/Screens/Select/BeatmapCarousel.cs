eb25730b614ececc5f6672e298dc5e786984c92e
Dean Herbert <pe@ppy.sh>
2022-02-05 16:12:58 +0900

    Revert "Merge pull request #16716 from peppy/carousel-less-invalidations"
    This reverts commit 8d13e0514b27ce5e6f587ba8918c165d3e1ddbbf, reversing
    changes made to 95582a9023488da37edb3b9236f6b4aeb197ccb9.

--- osu.Game/Screens/Select/BeatmapCarousel.cs
+++ osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -925,8 +925,10 @@ namespace osu.Game.Screens.Select
             // child items (difficulties) are still visible.
             item.Header.X = offsetX(dist, visibleHalfHeight) - (parent?.X ?? 0);
 
-            // We are applying alpha to the header here such that we can layer alpha transformations on top.
-            item.Header.Alpha = Math.Clamp(1.75f - 1.5f * dist, 0, 1);
+            // We are applying a multiplicative alpha (which is internally done by nesting an
+            // additional container and setting that container's alpha) such that we can
+            // layer alpha transformations on top.
+            item.SetMultiplicativeAlpha(Math.Clamp(1.75f - 1.5f * dist, 0, 1));
         }
 
         private enum PendingScrollOperation
