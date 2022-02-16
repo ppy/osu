5db63a8751e38d9701b9987ae30508b9be516475
Dan Balasescu <smoogipoo@smgi.me>
2022-02-16 11:30:03 +0900

    Expose read-only list from request

--- osu.Game/Online/API/Requests/GetBeatmapsRequest.cs
+++ osu.Game/Online/API/Requests/GetBeatmapsRequest.cs
@@ -2,12 +2,13 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 
 namespace osu.Game.Online.API.Requests
 {
     public class GetBeatmapsRequest : APIRequest<GetBeatmapsResponse>
     {
-        public readonly int[] BeatmapIds;
+        public readonly IReadOnlyList<int> BeatmapIds;
 
         private const int max_ids_per_request = 50;
 
