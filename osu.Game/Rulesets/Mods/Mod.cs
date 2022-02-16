23933fc881f07c2cc96b10589531387e6b2658bc
Dean Herbert <pe@ppy.sh>
2022-02-16 17:32:22 +0900

    Update xmldoc to mention that multipliers are not applied anywhere

--- osu.Game/Rulesets/Mods/Mod.cs
+++ osu.Game/Rulesets/Mods/Mod.cs
@@ -80,8 +80,11 @@ namespace osu.Game.Rulesets.Mods
         }
 
         /// <summary>
-        /// The score multiplier of this mod.
+        /// The (legacy) score multiplier of this mod.
         /// </summary>
+        /// <remarks>
+        /// This is not applied for newly set scores, but may be required for display purposes when showing legacy scores.
+        /// </remarks>
         [JsonIgnore]
         public virtual double ScoreMultiplier => 1;
 
