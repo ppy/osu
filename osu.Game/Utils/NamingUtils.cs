29a962ff9932788721557bb46a7ee94b99393385
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2022-02-16 22:32:42 +0100

    Add tests showing expected behaviour of naming helper

---
+++ osu.Game/Utils/NamingUtils.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+
+namespace osu.Game.Utils
+{
+    public static class NamingUtils
+    {
+        public static string GetNextBestName(IEnumerable<string> existingNames, string desiredName)
+        {
+            return null;
+        }
+    }
+}
