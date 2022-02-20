1aee91df4fe0f1fe3dfdde35672df37559990128
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2022-02-20 13:09:06 +0100

    Add test coverage of multiselection behaviour

--- osu.Game.Tests/Visual/UserInterface/TestSceneModColumn.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneModColumn.cs
@@ -1,10 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Mods;
 using osu.Game.Rulesets.Catch;
@@ -12,11 +16,12 @@ using osu.Game.Rulesets.Mania;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Rulesets.Taiko;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
     [TestFixture]
-    public class TestSceneModColumn : OsuTestScene
+    public class TestSceneModColumn : OsuManualInputManagerTestScene
     {
         [Cached]
         private OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Green);
@@ -57,6 +62,25 @@ namespace osu.Game.Tests.Visual.UserInterface
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre
                 }
+            });
+
+            clickToggle();
+            AddUntilStep("all panels selected", () => this.ChildrenOfType<ModPanel>().All(panel => panel.Active.Value));
+
+            clickToggle();
+            AddUntilStep("all panels deselected", () => this.ChildrenOfType<ModPanel>().All(panel => !panel.Active.Value));
+
+            AddStep("manually activate all panels", () => this.ChildrenOfType<ModPanel>().ForEach(panel => panel.Active.Value = true));
+            AddUntilStep("checkbox selected", () => this.ChildrenOfType<OsuCheckbox>().Single().Current.Value);
+
+            AddStep("deselect first panel", () => this.ChildrenOfType<ModPanel>().First().Active.Value = false);
+            AddUntilStep("checkbox selected", () => !this.ChildrenOfType<OsuCheckbox>().Single().Current.Value);
+
+            void clickToggle() => AddStep("click toggle", () =>
+            {
+                var checkbox = this.ChildrenOfType<OsuCheckbox>().Single();
+                InputManager.MoveMouseTo(checkbox);
+                InputManager.Click(MouseButton.Left);
             });
         }
     }
