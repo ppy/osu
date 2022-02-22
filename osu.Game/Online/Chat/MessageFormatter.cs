c2b5362f8e79298747ee78e02d2f3631bca248ec
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-02-22 16:45:04 +0900

    Fix missing escaping causing test failures
    Co-authored-by: Bartłomiej Dach <dach.bartlomiej@gmail.com>

--- osu.Game/Online/Chat/MessageFormatter.cs
+++ osu.Game/Online/Chat/MessageFormatter.cs
@@ -269,7 +269,7 @@ namespace osu.Game.Online.Chat
             handleAdvanced(advanced_link_regex, result, startIndex);
 
             // handle editor times
-            handleMatches(time_regex, "{0}", $"{OsuGameBase.OSU_PROTOCOL}edit/{0}", result, startIndex, LinkAction.OpenEditorTimestamp);
+            handleMatches(time_regex, "{0}", $@"{OsuGameBase.OSU_PROTOCOL}edit/{{0}}", result, startIndex, LinkAction.OpenEditorTimestamp);
 
             // handle channels
             handleMatches(channel_regex, "{0}", $"{OsuGameBase.OSU_PROTOCOL}chan/{0}", result, startIndex, LinkAction.OpenChannel);
