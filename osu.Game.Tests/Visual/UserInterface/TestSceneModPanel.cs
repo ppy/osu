713f89a59c7d0c964d5c4f1c0760dd45fe596bdf
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-02-23 23:11:38 +0100

    Implement incompatibility-displaying variant of mod panel

--- osu.Game.Tests/Visual/UserInterface/TestSceneModPanel.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneModPanel.cs
@@ -1,14 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Mods;
+using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Mods;
 using osuTK;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
@@ -36,6 +39,42 @@ namespace osu.Game.Tests.Visual.UserInterface
                     new ModPanel(new OsuModAlternate()),
                     new ModPanel(new OsuModApproachDifferent())
                 }
+            });
+        }
+
+        [Test]
+        public void TestIncompatibilityDisplay()
+        {
+            IncompatibilityDisplayingModPanel panel = null;
+
+            AddStep("create panel with DT", () => Child = panel = new IncompatibilityDisplayingModPanel(new OsuModDoubleTime())
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.None,
+                Width = 300
+            });
+
+            clickPanel();
+            AddAssert("panel active", () => panel.Active.Value);
+
+            clickPanel();
+            AddAssert("panel not active", () => !panel.Active.Value);
+
+            AddStep("set incompatible mod", () => SelectedMods.Value = new[] { new OsuModHalfTime() });
+
+            clickPanel();
+            AddAssert("panel not active", () => !panel.Active.Value);
+
+            AddStep("reset mods", () => SelectedMods.Value = Array.Empty<Mod>());
+
+            clickPanel();
+            AddAssert("panel active", () => panel.Active.Value);
+
+            void clickPanel() => AddStep("click panel", () =>
+            {
+                InputManager.MoveMouseTo(panel);
+                InputManager.Click(MouseButton.Left);
             });
         }
     }
