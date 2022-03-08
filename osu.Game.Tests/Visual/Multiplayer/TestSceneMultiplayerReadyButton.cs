5fcd5e18318e4381591e8f28ad64a53eee049235
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-03-09 00:48:03 +0900

    Fix test failure ripple through entire TestScene

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerReadyButton.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerReadyButton.cs
@@ -43,8 +43,6 @@ namespace osu.Game.Tests.Visual.Multiplayer
             Dependencies.Cache(rulesets = new RealmRulesetStore(Realm));
             Dependencies.Cache(beatmaps = new BeatmapManager(LocalStorage, Realm, rulesets, null, audio, Resources, host, Beatmap.Default));
             Dependencies.Cache(Realm);
-
-            beatmaps.Import(TestResources.GetQuickTestBeatmapForImport()).WaitSafely();
         }
 
         [SetUp]
@@ -52,8 +50,10 @@ namespace osu.Game.Tests.Visual.Multiplayer
         {
             AvailabilityTracker.SelectedItem.BindTo(selectedItem);
 
+            beatmaps.Import(TestResources.GetQuickTestBeatmapForImport()).WaitSafely();
             importedSet = beatmaps.GetAllUsableBeatmapSets().First();
             Beatmap.Value = beatmaps.GetWorkingBeatmap(importedSet.Beatmaps.First());
+
             selectedItem.Value = new PlaylistItem(Beatmap.Value.BeatmapInfo)
             {
                 RulesetID = Beatmap.Value.BeatmapInfo.Ruleset.OnlineID
