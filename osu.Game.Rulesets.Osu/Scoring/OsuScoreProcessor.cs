8a5081c9391a30446d6e5a970ecab786f3546283
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-03-10 10:26:09 +0900

    Add per-ruleset score multipliers for classic scoring

--- osu.Game.Rulesets.Osu/Scoring/OsuScoreProcessor.cs
+++ osu.Game.Rulesets.Osu/Scoring/OsuScoreProcessor.cs
@@ -11,6 +11,8 @@ namespace osu.Game.Rulesets.Osu.Scoring
 {
     public class OsuScoreProcessor : ScoreProcessor
     {
+        protected override double ClassicScoreMultiplier => 36;
+
         protected override HitEvent CreateHitEvent(JudgementResult result)
             => base.CreateHitEvent(result).With((result as OsuHitCircleJudgementResult)?.CursorPositionAtHit);
 
