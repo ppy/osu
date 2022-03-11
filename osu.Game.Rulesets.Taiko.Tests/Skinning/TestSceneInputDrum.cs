4e52bc3b6965f7d56f3ef4bc62e49d827e22c3f5
Aaron Hong <87902114+bubb13gummy@users.noreply.github.com>
2022-03-11 02:49:10 -0800

    Update osu!taiko TestSceneInputDrum with InputDrum changes for touch controls

--- osu.Game.Rulesets.Taiko.Tests/Skinning/TestSceneInputDrum.cs
+++ osu.Game.Rulesets.Taiko.Tests/Skinning/TestSceneInputDrum.cs
@@ -31,7 +31,7 @@ namespace osu.Game.Rulesets.Taiko.Tests.Skinning
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     Size = new Vector2(200),
-                    Child = new InputDrum(playfield.HitObjectContainer)
+                    Child = new InputDrum()
                 }
             });
         }
