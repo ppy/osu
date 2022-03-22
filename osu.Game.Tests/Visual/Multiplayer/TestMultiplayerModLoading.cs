1fb2e63bed287f04de5d09317a73b9f30298b5fe
NAGILSON <87902114+bubb13gummy@users.noreply.github.com>
2022-03-21 22:58:53 -0400

    Fill in assert details, remove whitespace

--- osu.Game.Tests/Visual/Multiplayer/TestMultiplayerModLoading.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestMultiplayerModLoading.cs
@@ -10,13 +10,11 @@ namespace osu.Game.Tests.Visual.Multiplayer
 {
     public class TestMultiplayerModLoading : OsuGameTestScene
     {
-
         [SetUp]
         public void SetUp() => Schedule(() =>
         {
             SelectedMods.Value = Array.Empty<Mod>();
         });
-
 
         /// <summary>
         /// This is a regression test that tests whether a singleplayer mod can transfer over to a multiplayer screen.
@@ -32,13 +30,13 @@ namespace osu.Game.Tests.Visual.Multiplayer
             var expectedMods = new[] { osuAutomationMod };
 
             AddStep("Toggle on the automation Mod.", () => { SelectedMods.Value = expectedMods; });
-            AddAssert("", () => SelectedMods.Value == expectedMods);
+            AddAssert("Mods are loaded before the multiplayer screen is pushed.", () => SelectedMods.Value == expectedMods);
 
             PushAndConfirm(() => new TestMultiplayerComponents());
             AddAssert("Mods are Empty After A Multiplayer Screen Loads", () => SelectedMods.Value.Count == 0);
 
             AddStep("Retoggle on the automation Mod.", () => { SelectedMods.Value = expectedMods; });
-            AddAssert("", () => SelectedMods.Value == expectedMods);
+            AddAssert("Mods are loaded before the playlist screen is pushed", () => SelectedMods.Value == expectedMods);
 
             // TODO: Implement TestPlaylistComponents? 
             //PushAndConfirm(() => new TestPlaylistComponents());
