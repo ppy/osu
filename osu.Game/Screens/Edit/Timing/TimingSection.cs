e14d5b8adbc26bff6b9f9f184a9a9444ba18bb8d
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-03-31 21:20:30 +0200

    Remove unused using directives

--- osu.Game/Screens/Edit/Timing/TimingSection.cs
+++ osu.Game/Screens/Edit/Timing/TimingSection.cs
@@ -1,13 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Graphics.UserInterfaceV2;
-using osu.Game.Overlays.Settings;
 
 namespace osu.Game.Screens.Edit.Timing
 {
