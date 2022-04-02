b56f0c81e6477f23459efeb8883142b46876ea48
CenTdemeern1 <87902114+bubb13gummy@users.noreply.github.com>
2022-04-02 04:41:05 +0200

    Remove hover test

--- osu.Game.Tests/Visual/Menus/TestSceneToolbarClock.cs
+++ osu.Game.Tests/Visual/Menus/TestSceneToolbarClock.cs
@@ -90,21 +90,6 @@ namespace osu.Game.Tests.Visual.Menus
         }
 
         [Test]
-        public void TestHoverBackground()
-        {
-            Box hoverBackground = null;
-
-            AddStep("Retrieve hover background", () => hoverBackground = (Box)toolbarClock.Children[0]);
-
-            AddStep("Move mouse away from clock", () => InputManager.MoveMouseTo(toolbarClock, new Vector2(0, 200)));
-            AddAssert("Hover background is not visible", () => hoverBackground.Alpha == 0);
-            AddStep("Move mouse on top of clock", () => InputManager.MoveMouseTo(toolbarClock));
-            AddAssert("Hover background is visible", () => hoverBackground.Alpha != 0);
-            AddStep("Move mouse away from clock", () => InputManager.MoveMouseTo(toolbarClock, new Vector2(0, 200)));
-            AddUntilStep("Hover background is not visible", () => hoverBackground.Alpha == 0);
-        }
-
-        [Test]
         public void TestDisplayModeChange()
         {
             ToolbarClockDisplayMode initialDisplayMode = 0;
