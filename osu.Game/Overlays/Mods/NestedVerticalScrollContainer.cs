a9d7c294f12821252530b52b80b16e4f1c7670af
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-04-05 19:25:47 +0900

    Add missing newline

--- osu.Game/Overlays/Mods/NestedVerticalScrollContainer.cs
+++ osu.Game/Overlays/Mods/NestedVerticalScrollContainer.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 #nullable enable
+
 using osu.Framework.Graphics;
 using osu.Framework.Input.Events;
 using osu.Game.Graphics.Containers;
