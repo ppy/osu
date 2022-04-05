8f8163f7d83c6739428951d190519b65428944d6
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-04-06 01:21:19 +0300

    Add legacy classic taiko player test scene

--- osu.Game.Rulesets.Taiko.Tests/TestSceneLegacyTaikoPlayer.cs
+++ osu.Game.Rulesets.Taiko.Tests/TestSceneLegacyTaikoPlayer.cs
@@ -3,19 +3,26 @@
 
 using osu.Framework.Allocation;
 using osu.Game.Database;
+using osu.Game.Rulesets.Taiko.Mods;
 using osu.Game.Skinning;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Taiko.Tests
 {
-    public class TestSceneTaikoPlayer : PlayerTestScene
+    public class TestSceneLegacyTaikoPlayer : PlayerTestScene
     {
         protected override Ruleset CreatePlayerRuleset() => new TaikoRuleset();
 
         [BackgroundDependencyLoader]
         private void load(SkinManager skins)
         {
-            skins.CurrentSkinInfo.Value = DefaultSkin.CreateInfo().ToLiveUnmanaged();
+            skins.CurrentSkinInfo.Value = DefaultLegacySkin.CreateInfo().ToLiveUnmanaged();
+        }
+
+        protected override TestPlayer CreatePlayer(Ruleset ruleset)
+        {
+            SelectedMods.Value = new[] { new TaikoModClassic() };
+            return base.CreatePlayer(ruleset);
         }
     }
 }
