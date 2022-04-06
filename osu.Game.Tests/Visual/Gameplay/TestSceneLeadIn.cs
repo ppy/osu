065bb60324c6f62393aee92799b4a6d916a9fa77
Dean Herbert <pe@ppy.sh>
2022-04-06 16:05:11 +0900

    Remove unused using statements

--- osu.Game.Tests/Visual/Gameplay/TestSceneLeadIn.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneLeadIn.cs
@@ -1,12 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Diagnostics;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Timing;
-using osu.Framework.Utils;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Rulesets.Osu;
