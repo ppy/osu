6e99cca5d90ad183b0f36d7b6ae9d8ba78525f6d
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-04-07 00:12:40 +0300

    Use `LegacySkinPlayerTestScene` instead of reimplementing

--- osu.Game.Rulesets.Taiko.Tests/TestSceneTaikoPlayer.cs
+++ osu.Game.Rulesets.Taiko.Tests/TestSceneTaikoPlayer.cs
@@ -1,9 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Game.Database;
-using osu.Game.Skinning;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Taiko.Tests
@@ -11,11 +8,5 @@ namespace osu.Game.Rulesets.Taiko.Tests
     public class TestSceneTaikoPlayer : PlayerTestScene
     {
         protected override Ruleset CreatePlayerRuleset() => new TaikoRuleset();
-
-        [BackgroundDependencyLoader]
-        private void load(SkinManager skins)
-        {
-            skins.CurrentSkinInfo.Value = DefaultSkin.CreateInfo().ToLiveUnmanaged();
-        }
     }
 }
