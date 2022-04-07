affdf7bfe4f882f9f0de25987e784b60a0c5e2d9
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2022-04-07 21:37:42 +0200

    Add null check guards to unsubscriptions in `Dispose()`

--- osu.Game/Storyboards/Drawables/DrawableStoryboardSprite.cs
+++ osu.Game/Storyboards/Drawables/DrawableStoryboardSprite.cs
@@ -110,7 +110,9 @@ namespace osu.Game.Storyboards.Drawables
         protected override void Dispose(bool isDisposing)
         {
             base.Dispose(isDisposing);
-            skin.SourceChanged -= skinSourceChanged;
+
+            if (skin != null)
+                skin.SourceChanged -= skinSourceChanged;
         }
     }
 }
