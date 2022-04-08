7c83f9119742cc2e2fa4eb44aa0986afab580061
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-04-08 19:27:59 +0300

    Perform `Show()` at the bottom of the method
    Probably not affecting behaviour, but makes more sense.

--- osu.Game/Overlays/ChatOverlay.cs
+++ osu.Game/Overlays/ChatOverlay.cs
@@ -313,8 +313,6 @@ namespace osu.Game.Overlays
         /// <param name="channel">The channel containing the message.</param>
         public void HighlightMessage(Message message, Channel channel)
         {
-            Show();
-
             Debug.Assert(channel.Id == message.ChannelId);
 
             if (currentChannel.Value?.Id != channel.Id)
@@ -326,6 +324,8 @@ namespace osu.Game.Overlays
             }
 
             channel.HighlightedMessage.Value = message;
+
+            Show();
         }
 
         private float startDragChatHeight;
