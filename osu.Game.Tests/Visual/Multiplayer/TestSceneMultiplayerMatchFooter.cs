2ecf1dfe412ae09d0c6a092883b01c0b7e868a12
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-04-12 23:01:37 +0900

    Fix `MultiplayerMatchFooter` test crash due to missing `PopoverContainer`
    Clicking the countdown button would crash. I did consider removing the
    test altogether but maybe it'll be useful in the future?

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerMatchFooter.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerMatchFooter.cs
@@ -3,7 +3,7 @@
 
 using NUnit.Framework;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Game.Screens.OnlinePlay.Multiplayer.Match;
 
 namespace osu.Game.Tests.Visual.Multiplayer
@@ -13,7 +13,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
         [SetUp]
         public new void Setup() => Schedule(() =>
         {
-            Child = new Container
+            Child = new PopoverContainer
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
