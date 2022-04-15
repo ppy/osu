93d158d315328de9bc6f8ef0bec358a12848e515
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-04-15 10:18:29 +0900

    Remove ununsed using statement

--- osu.Game.Tests/Visual/Settings/TestSceneSettingsPanel.cs
+++ osu.Game.Tests/Visual/Settings/TestSceneSettingsPanel.cs
@@ -4,7 +4,6 @@
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 using osu.Game.Graphics.UserInterface;
