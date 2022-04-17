4b6d42c7e890bcd797535fc6c5203e4fc70112aa
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-04-17 16:54:30 +0200

    Add assertion covering free mod selection mod validity filter

--- osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectScreen.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectScreen.cs
@@ -1,8 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Overlays.Mods;
 using osu.Game.Screens.OnlinePlay;
 
 namespace osu.Game.Tests.Visual.Multiplayer
@@ -18,6 +21,12 @@ namespace osu.Game.Tests.Visual.Multiplayer
             {
                 State = { Value = Visibility.Visible }
             });
+
+            AddAssert("all visible mods are playable",
+                () => this.ChildrenOfType<ModPanel>()
+                          .Where(panel => panel.IsPresent)
+                          .All(panel => panel.Mod.HasImplementation && panel.Mod.UserPlayable));
+
             AddToggleStep("toggle visibility", visible =>
             {
                 if (freeModSelectScreen != null)
