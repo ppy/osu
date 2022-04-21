c6f8ab571123fabf70c95e72839b0911703d6a28
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-04-21 22:38:58 +0900

    Add LoadAborted() event

--- osu.Game/Online/Multiplayer/OnlineMultiplayerClient.cs
+++ osu.Game/Online/Multiplayer/OnlineMultiplayerClient.cs
@@ -55,6 +55,7 @@ namespace osu.Game.Online.Multiplayer
                     connection.On<int, MultiplayerUserState>(nameof(IMultiplayerClient.UserStateChanged), ((IMultiplayerClient)this).UserStateChanged);
                     connection.On(nameof(IMultiplayerClient.LoadRequested), ((IMultiplayerClient)this).LoadRequested);
                     connection.On(nameof(IMultiplayerClient.GameplayStarted), ((IMultiplayerClient)this).GameplayStarted);
+                    connection.On(nameof(IMultiplayerClient.LoadAborted), ((IMultiplayerClient)this).LoadAborted);
                     connection.On(nameof(IMultiplayerClient.ResultsReady), ((IMultiplayerClient)this).ResultsReady);
                     connection.On<int, IEnumerable<APIMod>>(nameof(IMultiplayerClient.UserModsChanged), ((IMultiplayerClient)this).UserModsChanged);
                     connection.On<int, BeatmapAvailability>(nameof(IMultiplayerClient.UserBeatmapAvailabilityChanged), ((IMultiplayerClient)this).UserBeatmapAvailabilityChanged);
