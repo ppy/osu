f282e5b0133785d8c9e6474cbe04bf5c79dc4811
Dean Herbert <pe@ppy.sh>
2022-04-22 15:32:23 +0900

    Remove one more unnecessary schedule

--- osu.Game/Graphics/Containers/SectionsContainer.cs
+++ osu.Game/Graphics/Containers/SectionsContainer.cs
@@ -195,11 +195,8 @@ namespace osu.Game.Graphics.Containers
 
         protected void InvalidateScrollPosition()
         {
-            Schedule(() =>
-            {
-                lastKnownScroll = null;
-                lastClickedSection = null;
-            });
+            lastKnownScroll = null;
+            lastClickedSection = null;
         }
 
         protected override void UpdateAfterChildren()
