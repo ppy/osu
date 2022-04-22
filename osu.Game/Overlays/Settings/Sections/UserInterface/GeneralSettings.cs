c69fc5799a56649d872c282f0e5a430848288064
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-04-22 18:05:24 +0900

    Add classic default values against setting controls

--- osu.Game/Overlays/Settings/Sections/UserInterface/GeneralSettings.cs
+++ osu.Game/Overlays/Settings/Sections/UserInterface/GeneralSettings.cs
@@ -37,6 +37,7 @@ namespace osu.Game.Overlays.Settings.Sections.UserInterface
                 },
                 new SettingsSlider<double, TimeSlider>
                 {
+                    GetClassicDefault = () => 0,
                     LabelText = UserInterfaceStrings.HoldToConfirmActivationTime,
                     Current = config.GetBindable<double>(OsuSetting.UIHoldActivationDelay),
                     Keywords = new[] { @"delay" },
