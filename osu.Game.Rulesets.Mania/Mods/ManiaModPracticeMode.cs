ca23bf005c9b5e210c2628b15418bc6b1ebd5e4a
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:17:08 -0500

    Add osu Copyright

--- osu.Game.Rulesets.Mania/Mods/ManiaModPracticeMode.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModPracticeMode.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Rulesets.Mania.Objects;
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Mania.Objects;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Mania.Mods
