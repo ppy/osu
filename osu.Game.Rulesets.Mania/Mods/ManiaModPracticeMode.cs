2d7e4f1dd96b70c66908db26e4577b0d41f0ed0d
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:10:23 -0500

    Push ManiaModPracticeMode

---
+++ osu.Game.Rulesets.Mania/Mods/ManiaModPracticeMode.cs
@@ -0,0 +1,8 @@
+﻿using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Mania.Mods
+{
+    public class ManiaModPracticeMode : ModPracticeMode<ManiaHitObject>
+    { }
+}
