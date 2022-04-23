7c44d9443bb24abe62b82244ec578b839a628289
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:17:40 -0500

    Add osu Copyright

--- osu.Game.Rulesets.Taiko/Mods/TaikoModPracticeMode.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModPracticeMode.cs
@@ -1,4 +1,7 @@
-﻿using osu.Game.Rulesets.Mods;
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Taiko.Objects;
 
 namespace osu.Game.Rulesets.Taiko.Mods
