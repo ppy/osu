5d0842ac447f2912872e6e37d029305baf69b0b7
Dean Herbert <pe@ppy.sh>
2022-04-23 21:53:47 +0900

    Add a few more classic defaults to missed settings

--- osu.Game/Overlays/Settings/Sections/Gameplay/HUDSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Gameplay/HUDSettings.cs
@@ -30,6 +30,7 @@ namespace osu.Game.Overlays.Settings.Sections.Gameplay
                 },
                 new SettingsCheckbox
                 {
+                    GetClassicDefault = () => false,
                     LabelText = GameplaySettingsStrings.ShowHealthDisplayWhenCantFail,
                     Current = config.GetBindable<bool>(OsuSetting.ShowHealthDisplayWhenCantFail),
                     Keywords = new[] { "hp", "bar" }
