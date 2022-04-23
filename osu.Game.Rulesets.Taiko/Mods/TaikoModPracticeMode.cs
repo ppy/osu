1bd6c34d8f0879d8a17c6dafdd2d8ac9cc46752c
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:12:10 -0500

    Push TaikoModPracticeMode

---
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModPracticeMode.cs
@@ -0,0 +1,8 @@
+﻿using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Taiko.Objects;
+
+namespace osu.Game.Rulesets.Taiko.Mods
+{
+    public class TaikoModPracticeMode : ModPracticeMode<TaikoHitObject>
+    { }
+}
