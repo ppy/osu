9bcea13cb608c460c56667bf5f3d2be0e9b83ef5
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:11:03 -0500

    Push OsuModPracticeMode

---
+++ osu.Game.Rulesets.Osu/Mods/OsuModPracticeMode.cs
@@ -0,0 +1,9 @@
+﻿
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+
+namespace osu.Game.Rulesets.Osu.Mods
+{
+    public class OsuModPracticeMode : ModPracticeMode<OsuHitObject>
+    { }
+}
