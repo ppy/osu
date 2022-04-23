89d9ef0ec9950206b86eda11b80140e6dfb579dc
FluffyDoggo <jjireland@stu.naperville203.org>
2022-04-23 12:11:34 -0500

    Push CatchModPracticeMode

---
+++ osu.Game.Rulesets.Catch/Mods/CatchModPracticeMode.cs
@@ -0,0 +1,8 @@
+﻿using osu.Game.Rulesets.Catch.Objects;
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Catch.Mods
+{
+    public class CatchModPracticeMode : ModPracticeMode<CatchHitObject>
+    { }
+}
