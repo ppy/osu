ef2289ccdee6d7139bf98d9f69734b82461d789d
hwabis <kennysun23@gmail.com>
2022-05-03 11:25:25 -0400

    Fix anchors when rotated

--- osu.Game/Extensions/ContainerExtensions.cs
+++ osu.Game/Extensions/ContainerExtensions.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Extensions.EnumExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osuTK;
@@ -30,6 +31,142 @@ namespace osu.Game.Extensions
                     child.ScaleTo(new Vector2(parentScale.Y, parentScale.X));
                 else
                     child.ScaleTo(parentScale);
+
+                int parentRotationInQuarterTurnsModFour = parentRotationInQuarterTurns % 4;
+                if (parentRotationInQuarterTurnsModFour < 0) parentRotationInQuarterTurnsModFour += 4;
+
+                switch (parentRotationInQuarterTurnsModFour)
+                {
+                    // Rotated by 90
+                    case 1:
+                        child.Origin = rotateAnchor90(child.Origin, true);
+                        break;
+
+                    // Rotated by 180
+                    case 2:
+                        child.Origin = flipBothAnchors(child.Origin);
+                        break;
+
+                    // Rotated by 270
+                    case 3:
+                        child.Origin = rotateAnchor90(child.Origin, false);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static Anchor rotateAnchor90(Anchor anchor, bool clockwise)
+        {
+            Anchor rotatedAnchor;
+
+            switch (anchor)
+            {
+                case Anchor.TopLeft:
+                    rotatedAnchor = Anchor.TopRight;
+                    break;
+
+                case Anchor.TopCentre:
+                    rotatedAnchor = Anchor.CentreRight;
+                    break;
+
+                case Anchor.TopRight:
+                    rotatedAnchor = Anchor.BottomRight;
+                    break;
+
+                case Anchor.CentreLeft:
+                    rotatedAnchor = Anchor.TopCentre;
+                    break;
+
+                case Anchor.Centre:
+                    rotatedAnchor = Anchor.Centre;
+                    break;
+
+                case Anchor.CentreRight:
+                    rotatedAnchor = Anchor.BottomCentre;
+                    break;
+
+                case Anchor.BottomLeft:
+                    rotatedAnchor = Anchor.TopLeft;
+                    break;
+
+                case Anchor.BottomCentre:
+                    rotatedAnchor = Anchor.CentreLeft;
+                    break;
+
+                case Anchor.BottomRight:
+                    rotatedAnchor = Anchor.BottomLeft;
+                    break;
+
+                default: throw new ArgumentOutOfRangeException();
+            }
+
+            if (!clockwise)
+            {
+                // Correct if rotating by 270 instead of 90
+                rotatedAnchor = flipBothAnchors(rotatedAnchor);
+            }
+
+            return rotatedAnchor;
+        }
+
+        private static Anchor flipBothAnchors(Anchor anchor)
+        {
+            Anchor anchorX = getAnchorX(anchor);
+            Anchor anchorY = getAnchorY(anchor);
+
+            return flipAnchorX(anchorX) | flipAnchorY(anchorY);
+        }
+
+        private static Anchor getAnchorX(Anchor anchor)
+        {
+            if (anchor.HasFlagFast(Anchor.x0)) return Anchor.x0;
+
+            if (anchor.HasFlagFast(Anchor.x1)) return Anchor.x1;
+
+            if (anchor.HasFlagFast(Anchor.x2)) return Anchor.x2;
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private static Anchor getAnchorY(Anchor anchor)
+        {
+            if (anchor.HasFlagFast(Anchor.y0)) return Anchor.y0;
+
+            if (anchor.HasFlagFast(Anchor.y1)) return Anchor.y1;
+
+            if (anchor.HasFlagFast(Anchor.y2)) return Anchor.y2;
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private static Anchor flipAnchorX(Anchor anchor)
+        {
+            switch (anchor)
+            {
+                case Anchor.x0: return Anchor.x2;
+
+                case Anchor.x1: return Anchor.x1;
+
+                case Anchor.x2: return Anchor.x0;
+
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static Anchor flipAnchorY(Anchor anchor)
+        {
+            switch (anchor)
+            {
+                case Anchor.y0: return Anchor.y2;
+
+                case Anchor.y1: return Anchor.y1;
+
+                case Anchor.y2: return Anchor.y0;
+
+                default: throw new ArgumentOutOfRangeException();
             }
         }
     }
