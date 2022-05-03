aedbaa665eb8be34a333a382dce7d38100e89754
hwabis <kennysun23@gmail.com>
2022-05-02 23:22:30 -0400

    Prevent children from flipping with container

--- osu.Game/Extensions/ContainerExtensions.cs
+++ osu.Game/Extensions/ContainerExtensions.cs
@@ -3,22 +3,26 @@
 
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Logging;
+using osuTK;
 
 namespace osu.Game.Extensions
 {
     public static class ContainerExtensions
     {
         /// <summary>
-        /// Keeps the transformations of this container's children the same as the transformations of this container's parent.
+        /// Keeps the transformations of this container's children upright relative to the container's parent.
         /// </summary>
         /// <param name="target">The target to prevent keep its childrens' transformation.</param>
         /// <param name="parent">The parent of the target.</param>
         public static void KeepChildrenUpright(this Container target, Container parent)
         {
             float parentRotation = parent.Rotation;
+            Vector2 parentScale = parent.Scale;
             foreach (Drawable child in target)
             {
                 child.RotateTo(-parentRotation);
+                child.ScaleTo(parentScale);
             }
         }
     }
