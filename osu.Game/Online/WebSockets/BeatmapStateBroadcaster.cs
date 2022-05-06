d1e359bbc447febd7797cff5937fe90d9d395568
Nitrous <nathan.alo2000@gmail.com>
2022-05-07 06:37:04 +0800

    add broadcasters and broadcast server

---
+++ osu.Game/Online/WebSockets/BeatmapStateBroadcaster.cs
@@ -0,0 +1,23 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
+
+namespace osu.Game.Online.WebSockets
+{
+    public class BeatmapStateBroadcaster : GameStateBroadcaster<BeatmapInfo>
+    {
+        public override string Type => @"Beatmap";
+        public override BeatmapInfo Message => beatmap?.Value.BeatmapInfo;
+        private IBindable<WorkingBeatmap> beatmap;
+
+        [BackgroundDependencyLoader]
+        private void load(IBindable<WorkingBeatmap> beatmap)
+        {
+            this.beatmap = beatmap.GetBoundCopy();
+            this.beatmap.ValueChanged += _ => Broadcast();
+        }
+    }
+}
---
+++ osu.Game/Online/WebSockets/UserActivityStateBroadcaster.cs
@@ -0,0 +1,25 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Game.Online.API;
+using osu.Game.Users;
+
+namespace osu.Game.Online.WebSockets
+{
+    public class UserActivityStateBroadcaster : GameStateBroadcaster<string>
+    {
+        public override string Type => @"Activity";
+        public override string Message => activity?.Value?.GetType().Name;
+
+        private IBindable<UserActivity> activity;
+
+        [BackgroundDependencyLoader]
+        private void load(IAPIProvider api)
+        {
+            activity = api.Activity.GetBoundCopy();
+            activity.ValueChanged += _ => Broadcast();
+        }
+    }
+}
