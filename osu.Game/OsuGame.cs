66fa5cbe193555c98d9bd1eee7f49eecb2fe83cc
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-05-06 22:20:04 +0300

    Apply offset by a factor of 0.8x from screen offset

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -1153,7 +1153,7 @@ namespace osu.Game
                 horizontalOffset += (Content.ToLocalSpace(Notifications.ScreenSpaceDrawQuad.TopLeft).X - Content.DrawWidth) * SIDE_OVERLAY_OFFSET_RATIO;
 
             ScreenOffsetContainer.X = horizontalOffset;
-            overlayContent.X = horizontalOffset;
+            overlayContent.X = horizontalOffset * 0.8f;
 
             MenuCursorContainer.CanShowCursor = (ScreenStack.CurrentScreen as IOsuScreen)?.CursorVisible ?? false;
         }
