380cd1e03619e45ad580356342f99498c0f59592
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-05-06 21:35:22 +0200

    Add test coverage for lack of customisation on free mod select

--- osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectScreen.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectScreen.cs
@@ -6,24 +6,19 @@ using NUnit.Framework;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 using osu.Game.Overlays.Mods;
+using osu.Game.Rulesets.Osu.Mods;
 using osu.Game.Screens.OnlinePlay;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
     public class TestSceneFreeModSelectScreen : MultiplayerTestScene
     {
+        private FreeModSelectScreen freeModSelectScreen;
+
         [Test]
         public void TestFreeModSelect()
         {
-            FreeModSelectScreen freeModSelectScreen = null;
-
-            AddStep("create free mod select screen", () => Child = freeModSelectScreen = new FreeModSelectScreen
-            {
-                State = { Value = Visibility.Visible }
-            });
-            AddUntilStep("all column content loaded",
-                () => freeModSelectScreen.ChildrenOfType<ModColumn>().Any()
-                      && freeModSelectScreen.ChildrenOfType<ModColumn>().All(column => column.IsLoaded && column.ItemsLoaded));
+            createFreeModSelect();
 
             AddUntilStep("all visible mods are playable",
                 () => this.ChildrenOfType<ModPanel>()
@@ -35,6 +30,27 @@ namespace osu.Game.Tests.Visual.Multiplayer
                 if (freeModSelectScreen != null)
                     freeModSelectScreen.State.Value = visible ? Visibility.Visible : Visibility.Hidden;
             });
+        }
+
+        [Test]
+        public void TestCustomisationNotAvailable()
+        {
+            createFreeModSelect();
+
+            AddStep("select difficulty adjust", () => freeModSelectScreen.SelectedMods.Value = new[] { new OsuModDifficultyAdjust() });
+            AddWaitStep("wait some", 3);
+            AddAssert("customisation area not expanded", () => this.ChildrenOfType<ModSettingsArea>().Single().Height == 0);
+        }
+
+        private void createFreeModSelect()
+        {
+            AddStep("create free mod select screen", () => Child = freeModSelectScreen = new FreeModSelectScreen
+            {
+                State = { Value = Visibility.Visible }
+            });
+            AddUntilStep("all column content loaded",
+                () => freeModSelectScreen.ChildrenOfType<ModColumn>().Any()
+                      && freeModSelectScreen.ChildrenOfType<ModColumn>().All(column => column.IsLoaded && column.ItemsLoaded));
         }
     }
 }
