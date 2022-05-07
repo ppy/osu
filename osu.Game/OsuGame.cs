ce14fddcb2c2950285dd89d96b8587cd9c3fbf6f
Dean Herbert <pe@ppy.sh>
2022-05-07 14:02:07 +0900

    Use above-1 offset instead for more correct feeling parallax

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -1153,7 +1153,7 @@ namespace osu.Game
                 horizontalOffset += (Content.ToLocalSpace(Notifications.ScreenSpaceDrawQuad.TopLeft).X - Content.DrawWidth) * SIDE_OVERLAY_OFFSET_RATIO;
 
             ScreenOffsetContainer.X = horizontalOffset;
-            overlayContent.X = horizontalOffset * 0.8f;
+            overlayContent.X = horizontalOffset * 1.2f;
 
             MenuCursorContainer.CanShowCursor = (ScreenStack.CurrentScreen as IOsuScreen)?.CursorVisible ?? false;
         }
