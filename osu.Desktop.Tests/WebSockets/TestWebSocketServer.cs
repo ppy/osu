d1599d69d2dcb72172b939f0de1c023c7c887bd3
Nitrous <nathan.alo2000@gmail.com>
2022-05-09 18:15:42 +0800

    add desktop test project and websocket test

--- osu.Desktop.Tests/WebSockets/TestWebSocketServer.cs
+++ osu.Desktop.Tests/WebSockets/TestWebSocketServer.cs
@@ -7,10 +7,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using osu.Framework.Extensions;
-using osu.Game.Online.WebSockets;
+using osu.Desktop.WebSockets;
 
-namespace osu.Game.Tests.Online
+namespace osu.Desktop.Tests.WebSockets
 {
     [TestFixture]
     public class TestWebSocketServer
@@ -19,13 +18,13 @@ namespace osu.Game.Tests.Online
         private ServerWebSocket server;
 
         [SetUp]
-        public void SetUp()
+        public async Task SetUp()
         {
             client = new ClientWebSocket();
             server = new ServerWebSocket();
 
-            server.Start();
-            client.ConnectAsync(new Uri("ws://localhost:7270/test/"), CancellationToken.None).WaitSafely();
+            await server.Start().ConfigureAwait(false);
+            await client.ConnectAsync(new Uri("ws://localhost:7270/test/"), CancellationToken.None).ConfigureAwait(false);
         }
 
         [TearDown]
@@ -53,8 +52,8 @@ namespace osu.Game.Tests.Online
         public async Task TestClientMessage()
         {
             var sent = new ArraySegment<byte>(Encoding.UTF8.GetBytes("Hello World"));
-            await client.SendAsync(sent, WebSocketMessageType.Text, true, CancellationToken.None);
-            await Task.Delay(500);
+            await client.SendAsync(sent, WebSocketMessageType.Text, true, CancellationToken.None).ConfigureAwait(false);
+            await Task.Delay(500).ConfigureAwait(false);
             Assert.AreEqual("Hello World", server.LastMessageReceived);
         }
 
@@ -62,8 +61,8 @@ namespace osu.Game.Tests.Online
         public async Task TestClientClosing()
         {
             Assert.NotZero(server.Connected);
-            await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
-            await Task.Delay(500);
+            await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None).ConfigureAwait(false);
+            await Task.Delay(500).ConfigureAwait(false);
             Assert.IsTrue(server.RemoteInitiatedDisconnect);
             Assert.Zero(server.Connected);
         }
@@ -72,7 +71,7 @@ namespace osu.Game.Tests.Online
         public async Task TestServerClosing()
         {
             Assert.NotZero(server.Connected);
-            await server.Close();
+            await server.Close().ConfigureAwait(false);
             Assert.IsFalse(server.RemoteInitiatedDisconnect);
             Assert.Zero(server.Connected);
         }
