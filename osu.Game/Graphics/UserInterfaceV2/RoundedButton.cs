172524ff8a3812ffce89361c5552a6426529df33
Salman Ahmed <frenzibyte@gmail.com>
2022-05-09 18:21:00 +0300

    Move default background colour specification to `OsuButton`

--- osu.Game/Graphics/UserInterfaceV2/RoundedButton.cs
+++ osu.Game/Graphics/UserInterfaceV2/RoundedButton.cs
@@ -2,13 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
-using osu.Game.Overlays;
-using osuTK.Graphics;
 
 namespace osu.Game.Graphics.UserInterfaceV2
 {
@@ -24,13 +20,6 @@ namespace osu.Game.Graphics.UserInterfaceV2
                 if (IsLoaded)
                     updateCornerRadius();
             }
-        }
-
-        [BackgroundDependencyLoader(true)]
-        private void load([CanBeNull] OverlayColourProvider overlayColourProvider, OsuColour colours)
-        {
-            if (BackgroundColour == Color4.White)
-                BackgroundColour = overlayColourProvider?.Highlight1 ?? colours.Blue3;
         }
 
         protected override void LoadComplete()
