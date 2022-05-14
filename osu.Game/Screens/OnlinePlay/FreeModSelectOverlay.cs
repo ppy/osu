b6575c216b8945bceee462216d87b4ac79d7e0a4
Dean Herbert <pe@ppy.sh>
2022-05-15 03:18:44 +0900

    Allow selecting all mods at free mod select using `ctrl`+`a`

--- osu.Game/Screens/OnlinePlay/FreeModSelectOverlay.cs
+++ osu.Game/Screens/OnlinePlay/FreeModSelectOverlay.cs
@@ -6,6 +6,9 @@ using osu.Game.Overlays;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Graphics;
+using osu.Framework.Input;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.Events;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays.Mods;
 using osu.Game.Rulesets.Mods;
@@ -14,7 +17,7 @@ using osu.Game.Localisation;
 
 namespace osu.Game.Screens.OnlinePlay
 {
-    public class FreeModSelectOverlay : ModSelectOverlay
+    public class FreeModSelectOverlay : ModSelectOverlay, IKeyBindingHandler<PlatformAction>
     {
         protected override bool ShowTotalMultiplier => false;
 
@@ -26,6 +29,8 @@ namespace osu.Game.Screens.OnlinePlay
             set => base.IsValidMod = m => m.UserPlayable && value.Invoke(m);
         }
 
+        private ShearedButton selectAllButton;
+
         public FreeModSelectOverlay()
             : base(OverlayColourScheme.Plum)
         {
@@ -35,12 +40,31 @@ namespace osu.Game.Screens.OnlinePlay
         protected override ModColumn CreateModColumn(ModType modType, Key[] toggleKeys = null) => new ModColumn(modType, true, toggleKeys);
 
         protected override IEnumerable<ShearedButton> CreateFooterButtons() => base.CreateFooterButtons().Prepend(
-            new ShearedButton(BUTTON_WIDTH)
+            selectAllButton = new ShearedButton(BUTTON_WIDTH)
             {
                 Anchor = Anchor.BottomLeft,
                 Origin = Anchor.BottomLeft,
                 Text = CommonStrings.SelectAll,
                 Action = SelectAll
             });
+
+        public bool OnPressed(KeyBindingPressEvent<PlatformAction> e)
+        {
+            if (e.Repeat)
+                return false;
+
+            switch (e.Action)
+            {
+                case PlatformAction.SelectAll:
+                    selectAllButton.TriggerClick();
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void OnReleased(KeyBindingReleaseEvent<PlatformAction> e)
+        {
+        }
     }
 }
