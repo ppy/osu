c1fbb8560e06079dd88c5f1cf0d3f45e5e031988
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-05-14 19:46:50 +0300

    Add "spotlight" beatmap badge

---
+++ osu.Game/Overlays/BeatmapSet/SpotlightBeatmapPill.cs
@@ -0,0 +1,17 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Localisation;
+using osu.Game.Resources.Localisation.Web;
+
+#nullable enable
+
+namespace osu.Game.Overlays.BeatmapSet
+{
+    public class SpotlightBeatmapPill : BeatmapBadgePill
+    {
+        public override LocalisableString BadgeText => BeatmapsetsStrings.SpotlightBadgeLabel;
+        public override Colour4 BadgeColour => Colours.Pink1;
+    }
+}
