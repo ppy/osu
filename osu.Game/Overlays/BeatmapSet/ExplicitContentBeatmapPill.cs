7a1ce9da93c108bdcb1eabb69640c893ae61b3f3
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-05-14 19:46:34 +0300

    Abstractify beatmap badge logic to own class

--- osu.Game/Overlays/BeatmapSet/ExplicitContentBeatmapPill.cs
+++ osu.Game/Overlays/BeatmapSet/ExplicitContentBeatmapPill.cs
@@ -1,47 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
+using osu.Framework.Localisation;
 using osu.Game.Resources.Localisation.Web;
+
+#nullable enable
 
 namespace osu.Game.Overlays.BeatmapSet
 {
-    public class ExplicitContentBeatmapPill : CompositeDrawable
+    public class ExplicitContentBeatmapPill : BeatmapBadgePill
     {
-        public ExplicitContentBeatmapPill()
-        {
-            AutoSizeAxes = Axes.Both;
-        }
-
-        [BackgroundDependencyLoader(true)]
-        private void load(OsuColour colours, OverlayColourProvider colourProvider)
-        {
-            InternalChild = new CircularContainer
-            {
-                Masking = true,
-                AutoSizeAxes = Axes.Both,
-                Children = new Drawable[]
-                {
-                    new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = colourProvider?.Background5 ?? colours.Gray2,
-                    },
-                    new OsuSpriteText
-                    {
-                        Margin = new MarginPadding { Horizontal = 10f, Vertical = 2f },
-                        Text = BeatmapsetsStrings.NsfwBadgeLabel.ToUpper(),
-                        Font = OsuFont.GetFont(size: 10, weight: FontWeight.SemiBold),
-                        Colour = colours.Orange2
-                    }
-                }
-            };
-        }
+        public override LocalisableString BadgeText => BeatmapsetsStrings.NsfwBadgeLabel;
+        public override Colour4 BadgeColour => Colours.Orange2;
     }
 }
