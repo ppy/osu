8336f4f7ba612388b6177e6dddca77775806f0ee
Tsuki Ayukawa <markus.residence@gmail.com>
2022-05-16 23:19:00 +0300

    Revert "fix: not set beatmap ruleset for Edit Editor"
    This reverts commit cabeb8950477960628d3d84b244a6b437a80fae9.

--- osu.Game/Screens/Select/SongSelect.cs
+++ osu.Game/Screens/Select/SongSelect.cs
@@ -367,8 +367,6 @@ namespace osu.Game.Screens.Select
                 throw new InvalidOperationException($"Attempted to edit when {nameof(AllowEditing)} is disabled");
 
             Beatmap.Value = beatmaps.GetWorkingBeatmap(beatmapInfo ?? beatmapInfoNoDebounce);
-            Ruleset.Value = beatmapInfo?.Ruleset;
-
             this.Push(new EditorLoader());
         }
 
