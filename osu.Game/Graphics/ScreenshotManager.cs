7dcb88759afcf00ce46e930ce117729fe9d7416f
Dean Herbert <pe@ppy.sh>
2022-05-21 00:28:17 +0900

    Remove unnecessary using statement

--- osu.Game/Graphics/ScreenshotManager.cs
+++ osu.Game/Graphics/ScreenshotManager.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
