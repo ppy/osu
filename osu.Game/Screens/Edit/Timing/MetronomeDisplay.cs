b8575c736facedc9b81d7b2af4f063ac5b5804fd
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-05-21 21:56:05 +0900

    Remove unused using

--- osu.Game/Screens/Edit/Timing/MetronomeDisplay.cs
+++ osu.Game/Screens/Edit/Timing/MetronomeDisplay.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Globalization;
 using osu.Framework.Allocation;
 using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
