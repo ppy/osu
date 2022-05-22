3a7233bd6ed5e2a3784de1825ac58ea1ec7ae840
Dean Herbert <pe@ppy.sh>
2022-05-22 22:49:24 +0900

    Move interface to more appropriate namespace

--- osu.Game/Beatmaps/IBeatSyncProvider.cs
+++ osu.Game/Beatmaps/IBeatSyncProvider.cs
@@ -7,8 +7,9 @@ using osu.Framework.Allocation;
 using osu.Framework.Audio.Track;
 using osu.Framework.Timing;
 using osu.Game.Beatmaps.ControlPoints;
+using osu.Game.Graphics.Containers;
 
-namespace osu.Game.Graphics.Containers
+namespace osu.Game.Beatmaps
 {
     /// <summary>
     /// Provides various data sources which allow for synchronising visuals to a known beat.
