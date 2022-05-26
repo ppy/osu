7f4ea5d522de3dc6c53d9e6cb892720b580c47c5
Dean Herbert <pe@ppy.sh>
2022-05-26 18:12:28 +0900

    Use left alignment and remove hacky text positioning code

--- osu.Game/Screens/Edit/BottomBar.cs
+++ osu.Game/Screens/Edit/BottomBar.cs
@@ -41,7 +41,7 @@ namespace osu.Game.Screens.Edit
                         RelativeSizeAxes = Axes.Both,
                         ColumnDimensions = new[]
                         {
-                            new Dimension(GridSizeMode.Absolute, 220),
+                            new Dimension(GridSizeMode.Absolute, 170),
                             new Dimension(),
                             new Dimension(GridSizeMode.Absolute, 220),
                             new Dimension(GridSizeMode.Absolute, 120),
