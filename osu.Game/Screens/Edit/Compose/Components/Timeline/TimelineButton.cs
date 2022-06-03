42df586ed955a759020ea1ee3cf7b5778631e14b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-06-03 17:00:00 +0900

    Remove unnecessary nesting of `IconButton` and update design a touch

--- osu.Game/Screens/Edit/Compose/Components/Timeline/TimelineButton.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/TimelineButton.cs
@@ -1,62 +1,29 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Sprites;
-using osu.Game.Graphics;
+using osu.Framework.Allocation;
 using osu.Game.Graphics.UserInterface;
+using osu.Game.Overlays;
 using osu.Game.Screens.Edit.Timing;
-using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Screens.Edit.Compose.Components.Timeline
 {
-    public class TimelineButton : CompositeDrawable
+    public class TimelineButton : IconButton
     {
-        public Action Action;
-        public readonly BindableBool Enabled = new BindableBool(true);
-
-        public IconUsage Icon
+        [BackgroundDependencyLoader]
+        private void load(OverlayColourProvider colourProvider)
         {
-            get => button.Icon;
-            set => button.Icon = value;
+            // These are using colourProvider but don't match the design.
+            // Just something to fit until someone implements the updated design.
+            IconColour = colourProvider.Background1;
+            IconHoverColour = colourProvider.Content2;
+
+            HoverColour = colourProvider.Background1;
+            FlashColour = colourProvider.Content2;
+
+            Add(new RepeatingButtonBehaviour(this));
         }
 
-        private readonly TimelineIconButton button;
-
-        public TimelineButton()
-        {
-            InternalChild = button = new TimelineIconButton { Action = () => Action?.Invoke() };
-
-            button.Enabled.BindTo(Enabled);
-            Width = button.Width;
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-
-            button.Size = new Vector2(button.Width, DrawHeight);
-        }
-
-        private class TimelineIconButton : IconButton
-        {
-            public TimelineIconButton()
-            {
-                Anchor = Anchor.Centre;
-                Origin = Anchor.Centre;
-                IconColour = OsuColour.Gray(0.35f);
-                IconHoverColour = Color4.White;
-                HoverColour = OsuColour.Gray(0.25f);
-                FlashColour = OsuColour.Gray(0.5f);
-
-                Add(new RepeatingButtonBehaviour(this));
-            }
-
-            protected override HoverSounds CreateHoverSounds(HoverSampleSet sampleSet) => new HoverSounds(sampleSet);
-        }
+        protected override HoverSounds CreateHoverSounds(HoverSampleSet sampleSet) => new HoverSounds(sampleSet);
     }
 }
