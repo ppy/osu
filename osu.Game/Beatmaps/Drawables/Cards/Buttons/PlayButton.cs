47dfb2e452a002c25a5f1c3ccc91c670a8c51332
Joseph Madamba <87902114+bubb13gummy@users.noreply.github.com>
2022-06-10 14:04:03 -0700

    Fix beatmap card play button not working with touch inputs when not hovered

--- osu.Game/Beatmaps/Drawables/Cards/Buttons/PlayButton.cs
+++ osu.Game/Beatmaps/Drawables/Cards/Buttons/PlayButton.cs
@@ -43,6 +43,9 @@ namespace osu.Game.Beatmaps.Drawables.Cards.Buttons
 
             Anchor = Origin = Anchor.Centre;
 
+            // needed for touch input to work when card is not hovered/expanded
+            AlwaysPresent = true;
+
             Children = new Drawable[]
             {
                 icon = new SpriteIcon
