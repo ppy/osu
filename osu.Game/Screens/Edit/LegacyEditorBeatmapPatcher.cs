a8286bdf0486b5c18e480afd2facf77c24714b58
Dan Balasescu <smoogipoo@smgi.me>
2022-06-13 16:56:08 +0900

    Fix assertion failures

--- osu.Game/Screens/Edit/LegacyEditorBeatmapPatcher.cs
+++ osu.Game/Screens/Edit/LegacyEditorBeatmapPatcher.cs
@@ -82,14 +82,18 @@ namespace osu.Game.Screens.Edit
 
             // Find the index of [HitObject] sections. Lines changed prior to this index are ignored.
             int oldSectionStartIndex = Array.IndexOf(result.PiecesOld, $"[{section}]");
-            int oldSectionEndIndex = Array.FindIndex(result.PiecesOld, oldSectionStartIndex + 1, s => s.StartsWith('['));
+            if (oldSectionStartIndex == -1)
+                return;
 
+            int oldSectionEndIndex = Array.FindIndex(result.PiecesOld, oldSectionStartIndex + 1, s => s.StartsWith('['));
             if (oldSectionEndIndex == -1)
                 oldSectionEndIndex = result.PiecesOld.Length;
 
             int newSectionStartIndex = Array.IndexOf(result.PiecesNew, $"[{section}]");
-            int newSectionEndIndex = Array.FindIndex(result.PiecesNew, newSectionStartIndex + 1, s => s.StartsWith('['));
+            if (newSectionStartIndex == -1)
+                return;
 
+            int newSectionEndIndex = Array.FindIndex(result.PiecesNew, newSectionStartIndex + 1, s => s.StartsWith('['));
             if (newSectionEndIndex == -1)
                 newSectionEndIndex = result.PiecesNew.Length;
 
