dfc643fd8ccce3266ba5a0cd326466e118732e6e
Dan Balasescu <smoogipoo@smgi.me>
2022-06-13 12:35:09 +0900

    Match family string via GetFamilyString()

--- osu.Game/Graphics/OsuFont.cs
+++ osu.Game/Graphics/OsuFont.cs
@@ -80,7 +80,7 @@ namespace osu.Game.Graphics
         /// <returns>The string representation of <paramref name="weight"/> in the specified <paramref name="family"/>.</returns>
         public static string GetWeightString(string family, FontWeight weight)
         {
-            if ((family == @"Torus" || family == @"Torus-Alternate") && weight == FontWeight.Medium)
+            if ((family == GetFamilyString(Typeface.Torus) || family == GetFamilyString(Typeface.TorusAlternate)) && weight == FontWeight.Medium)
                 // torus doesn't have a medium; fallback to regular.
                 weight = FontWeight.Regular;
 
