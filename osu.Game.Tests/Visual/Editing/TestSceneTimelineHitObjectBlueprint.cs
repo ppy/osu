f8830c6850128456266c82de83273204f8b74ac0
Dan Balasescu <smoogipoo@smgi.me>
2022-06-17 16:37:17 +0900

    Automated #nullable processing

--- osu.Game.Tests/Visual/Editing/TestSceneTimelineHitObjectBlueprint.cs
+++ osu.Game.Tests/Visual/Editing/TestSceneTimelineHitObjectBlueprint.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable disable
+
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
