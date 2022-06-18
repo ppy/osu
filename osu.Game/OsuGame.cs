56abe54fe6331f88c014baf7e34128bc070e8cec
Dean Herbert <pe@ppy.sh>
2022-06-18 22:59:19 +0900

    Redirect graceful exiting to standard exit flow
    Rather than doing a completely forced exit as we were, this will now
    follow a more standard flow with the ability for the user to abort along
    the way. This is more in line with how I wanted this to work.
    
    Note that this means a confirmation is now shown. It has been discussed
    in the past that this confirmation should only show when there's an
    ongoing action implies the user may want to cancel the exit. For now I
    think this is fine.
    
    Addresses
    https://github.com/ppy/osu/discussions/18399#discussioncomment-2811311

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -637,6 +637,12 @@ namespace osu.Game
             Add(performFromMainMenuTask = new PerformFromMenuRunner(action, validScreens, () => ScreenStack.CurrentScreen));
         }
 
+        public override void GracefullyExit()
+        {
+            // Using PerformFromScreen gives the user a chance to interrupt the exit process if needed.
+            PerformFromScreen(menu => menu.Exit());
+        }
+
         /// <summary>
         /// Wait for the game (and target component) to become loaded and then run an action.
         /// </summary>
