26c5b59f6d55976d36a6007aecab4b1e7fb9f39a
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-06-20 14:39:47 +0200

    Replace usages of `string.To{Lower,Upper}()`

--- osu.Game.Tournament/Screens/Ladder/Components/DrawableTournamentRound.cs
+++ osu.Game.Tournament/Screens/Ladder/Components/DrawableTournamentRound.cs
@@ -49,10 +49,10 @@ namespace osu.Game.Tournament.Screens.Ladder.Components
             };
 
             name = round.Name.GetBoundCopy();
-            name.BindValueChanged(n => textName.Text = ((losers ? "Losers " : "") + round.Name).ToUpper(), true);
+            name.BindValueChanged(n => textName.Text = ((losers ? "Losers " : "") + round.Name).ToUpperInvariant(), true);
 
             description = round.Description.GetBoundCopy();
-            description.BindValueChanged(n => textDescription.Text = round.Description.Value?.ToUpper(), true);
+            description.BindValueChanged(n => textDescription.Text = round.Description.Value?.ToUpperInvariant(), true);
         }
     }
 }
