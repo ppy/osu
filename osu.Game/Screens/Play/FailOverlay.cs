db2371fdca857a4f69a64d064e54a5743c4b444d
cdwcgt <87902114+bubb13gummy@users.noreply.github.com>
2022-06-21 19:06:38 +0800

    Use better design and fix some problem
    Let saveReplay async but still void
    Make failed score's rank = F

--- osu.Game/Screens/Play/FailOverlay.cs
+++ osu.Game/Screens/Play/FailOverlay.cs
@@ -5,8 +5,14 @@
 
 using System;
 using osu.Game.Graphics;
+using osu.Game.Graphics.UserInterface;
+using osuTK;
 using osuTK.Graphics;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 
 namespace osu.Game.Screens.Play
 {
@@ -21,7 +27,41 @@ namespace osu.Game.Screens.Play
         {
             AddButton("Retry", colours.YellowDark, () => OnRetry?.Invoke());
             AddButton("Quit", new Color4(170, 27, 39, 255), () => OnQuit?.Invoke());
-            AddButton("Save replay and Quit", colours.Blue, () => SaveReplay?.Invoke());
+            // from #10339 maybe this is a better visual effect
+            Add(new Container
+            {
+                Anchor = Anchor.BottomLeft,
+                Origin = Anchor.BottomLeft,
+                RelativeSizeAxes = Axes.X,
+                Height = TwoLayerButton.SIZE_EXTENDED.Y,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4Extensions.FromHex("#333")
+                    },
+                    new FillFlowContainer
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        AutoSizeAxes = Axes.X,
+                        RelativeSizeAxes = Axes.Y,
+                        Spacing = new Vector2(5),
+                        Padding = new MarginPadding(10),
+                        Direction = FillDirection.Horizontal,
+                        Children = new Drawable[]
+                        {
+                            new SaveFailedScoreButton()
+                            {
+                                OnSave = SaveReplay,
+                                RelativeSizeAxes = Axes.Y,
+                                Width = 300
+                            },
+                        }
+                    }
+                }
+            });
         }
     }
 }
