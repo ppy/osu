4aa86b0a8001fa1e0f2f6895f7160d226b80cb6d
Supersonicboss1 <leodalgado@gmail.com>
2022-06-21 23:41:25 +0100

    fixed duplicates and cleaned up code

--- osu.Game/Overlays/Settings/Sections/Maintenance/CollectionsSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Maintenance/CollectionsSettings.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>.Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 #nullable disable
@@ -20,30 +20,30 @@ namespace osu.Game.Overlays.Settings.Sections.Maintenance
         [BackgroundDependencyLoader(permitNulls: true)]
         private void load([CanBeNull] CollectionManager collectionManager, [CanBeNull] LegacyImportManager legacyImportManager, IDialogOverlay dialogOverlay)
         {
-            if (collectionManager != null)
-            {
-                if (legacyImportManager?.SupportsImportFromStable == true)
-                {
-                    Add(importCollectionsButton = new SettingsButton
-                    {
-                        Text = MaintenanceSettingsStrings.ImportCollectionsFromStable,
-                        Action = () =>
-                        {
-                            importCollectionsButton.Enabled.Value = false;
-                            legacyImportManager.ImportFromStableAsync(StableContent.Collections).ContinueWith(t => Schedule(() => importCollectionsButton.Enabled.Value = true));
-                        }
-                    });
-                }
+            if (collectionManager == null) return;
 
-                Add(new DangerousSettingsButton
+            if (legacyImportManager?.SupportsImportFromStable == true)
+
+            {
+                Add(importCollectionsButton = new SettingsButton
                 {
-                    Text = MaintenanceSettingsStrings.DeleteAllCollections,
+                    Text = MaintenanceSettingsStrings.ImportCollectionsFromStable,
                     Action = () =>
                     {
-                        dialogOverlay?.Push(new MassDeleteConfirmationDialog(collectionManager.DeleteAll));
+                        importCollectionsButton.Enabled.Value = false;
+                        legacyImportManager.ImportFromStableAsync(StableContent.Collections).ContinueWith(t => Schedule(() => importCollectionsButton.Enabled.Value = true));
                     }
                 });
             }
+
+            Add(new DangerousSettingsButton
+            {
+                Text = MaintenanceSettingsStrings.DeleteAllCollections,
+                Action = () =>
+                {
+                    dialogOverlay?.Push(new MassDeleteConfirmationDialog(collectionManager.DeleteAll));
+                }
+            });
         }
     }
 }
