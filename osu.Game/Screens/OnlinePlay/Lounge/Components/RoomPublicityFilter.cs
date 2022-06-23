2def6d809bf4c48108a95324101c5a1c107b3bfa
Gabe Livengood <47010459+ggliv@users.noreply.github.com>
2022-06-23 11:02:27 -0400

    refactor to use enum dropdown

---
+++ osu.Game/Screens/OnlinePlay/Lounge/Components/RoomPublicityFilter.cs
@@ -0,0 +1,13 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
+
+namespace osu.Game.Screens.OnlinePlay.Lounge.Components
+{
+    public enum RoomPublicityFilter
+    {
+        Public,
+        Private
+    }
+}
