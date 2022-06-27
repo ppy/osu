5af1106fa5f0e9f49cadbbca052c3ee5d65d2df5
Dean Herbert <pe@ppy.sh>
2022-06-27 18:04:52 +0900

    Remove unused using statement

--- osu.Game.Tests/Database/BeatmapImporterTests.cs
+++ osu.Game.Tests/Database/BeatmapImporterTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
