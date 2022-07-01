acc423054f7354645f340519f6e75a8e15a2e5fb
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-07-01 16:06:18 +0900

    Add attribute to retry flaky tests on normal CI runs

---
+++ osu.Game/Tests/FlakyTestAttribute.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
+using System;
+using NUnit.Framework;
+
+namespace osu.Game.Tests
+{
+    /// <summary>
+    /// An attribute to mark any flaky tests.
+    /// Will add a retry count unless environment variable `FAIL_FLAKY_TESTS` is set to `1`.
+    /// </summary>
+    public class FlakyTestAttribute : RetryAttribute
+    {
+        public FlakyTestAttribute()
+            : this(10)
+        {
+        }
+
+        public FlakyTestAttribute(int tryCount)
+            : base(Environment.GetEnvironmentVariable("FAIL_FLAKY_TESTS") == "1" ? 0 : tryCount)
+        {
+        }
+    }
+}
