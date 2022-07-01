def33e0460b4cc1e35cdbf079dc4c88692151ebd
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-07-01 15:28:55 +0900

    Add test coverage of difficulty filter reset

--- osu.Game.Tests/Visual/SongSelect/TestScenePlaySongSelect.cs
+++ osu.Game.Tests/Visual/SongSelect/TestScenePlaySongSelect.cs
@@ -98,6 +98,22 @@ namespace osu.Game.Tests.Visual.SongSelect
         }
 
         [Test]
+        public void TestPlaceholderStarDifficulty()
+        {
+            addRulesetImportStep(0);
+            AddStep("change star filter", () => config.SetValue(OsuSetting.DisplayStarsMinimum, 10.0));
+
+            createSongSelect();
+
+            AddUntilStep("wait for placeholder visible", () => getPlaceholder()?.State.Value == Visibility.Visible);
+
+            AddStep("click link in placeholder", () => getPlaceholder().ChildrenOfType<DrawableLinkCompiler>().First().TriggerClick());
+
+            AddUntilStep("star filter reset", () => config.Get<double>(OsuSetting.DisplayStarsMinimum) == 0.0);
+            AddUntilStep("wait for placeholder visible", () => getPlaceholder()?.State.Value == Visibility.Hidden);
+        }
+
+        [Test]
         public void TestPlaceholderConvertSetting()
         {
             addRulesetImportStep(0);
