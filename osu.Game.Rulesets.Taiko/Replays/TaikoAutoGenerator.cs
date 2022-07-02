27f386959933baf31b0ae2d63bad2cfec9b3dcec
andy840119 <87902114+bubb13gummy@users.noreply.github.com>
2022-07-02 13:33:51 +0800

    Mark as non-null because next object in here will always has value.

--- osu.Game.Rulesets.Taiko/Replays/TaikoAutoGenerator.cs
+++ osu.Game.Rulesets.Taiko/Replays/TaikoAutoGenerator.cs
@@ -117,7 +117,7 @@ namespace osu.Game.Rulesets.Taiko.Replays
                 var nextHitObject = GetNextObject(i); // Get the next object that requires pressing the same button
 
                 bool canDelayKeyUp = nextHitObject == null || nextHitObject.StartTime > endTime + KEY_UP_DELAY;
-                double calculatedDelay = canDelayKeyUp ? KEY_UP_DELAY : (nextHitObject.StartTime - endTime) * 0.9;
+                double calculatedDelay = canDelayKeyUp ? KEY_UP_DELAY : (nextHitObject!.StartTime - endTime) * 0.9;
                 Frames.Add(new TaikoReplayFrame(endTime + calculatedDelay));
 
                 hitButton = !hitButton;
