e6a05ce3e2d8312581dd660ec9518bebee4b230f
goodtrailer <aldenwu0@gmail.com>
2022-07-03 13:51:30 -0700

    Slow down legacy followcircle animations

--- osu.Game.Rulesets.Osu/Skinning/Legacy/LegacyFollowCircle.cs
+++ osu.Game.Rulesets.Osu/Skinning/Legacy/LegacyFollowCircle.cs
@@ -1,13 +1,21 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Osu.Skinning.Legacy
 {
     public class LegacyFollowCircle : CompositeDrawable
     {
+        [Resolved(canBeNull: true)]
+        private DrawableHitObject? parentObject { get; set; }
+
         public LegacyFollowCircle(Drawable animationContent)
         {
             // follow circles are 2x the hitcircle resolution in legacy skins (since they are scaled down from >1x
@@ -15,8 +23,65 @@ namespace osu.Game.Rulesets.Osu.Skinning.Legacy
             animationContent.Anchor = Anchor.Centre;
             animationContent.Origin = Anchor.Centre;
 
+            Alpha = 0f;
             RelativeSizeAxes = Axes.Both;
             InternalChild = animationContent;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            if (parentObject != null)
+            {
+                var slider = (DrawableSlider)parentObject;
+                slider.Tracking.BindValueChanged(trackingChanged, true);
+            }
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            if (parentObject != null)
+            {
+                parentObject.ApplyCustomUpdateState += updateStateTransforms;
+                updateStateTransforms(parentObject, parentObject.State.Value);
+            }
+        }
+
+        private void trackingChanged(ValueChangedEvent<bool> tracking)
+        {
+            Debug.Assert(parentObject != null);
+
+            if (parentObject.Judged)
+                return;
+
+            const float scale_duration = 180f;
+            const float fade_duration = 90f;
+
+            double maxScaleDuration = parentObject.HitStateUpdateTime - Time.Current;
+            double realScaleDuration = scale_duration;
+            if (tracking.NewValue && maxScaleDuration < realScaleDuration)
+                realScaleDuration = maxScaleDuration;
+            double realFadeDuration = fade_duration * realScaleDuration / fade_duration;
+
+            this.ScaleTo(tracking.NewValue ? DrawableSliderBall.FOLLOW_AREA : 1f, realScaleDuration, Easing.OutQuad)
+                .FadeTo(tracking.NewValue ? 1f : 0f, realFadeDuration, Easing.OutQuad);
+        }
+
+        private void updateStateTransforms(DrawableHitObject drawableObject, ArmedState state)
+        {
+            if (drawableObject is not DrawableSlider)
+                return;
+
+            const float shrink_duration = 200f;
+            const float fade_duration = 240f;
+
+            using (BeginAbsoluteSequence(drawableObject.HitStateUpdateTime))
+            {
+                this.ScaleTo(DrawableSliderBall.FOLLOW_AREA * 0.8f, shrink_duration, Easing.OutQuint)
+                    .FadeOut(fade_duration, Easing.InQuint);
+            }
         }
     }
 }
