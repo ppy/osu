45efc2388e8e481f9910da666b40b6030989324d
goodtrailer <87902114+bubb13gummy@users.noreply.github.com>
2022-07-03 13:51:30 -0700

    Slow down legacy followcircle animations

--- osu.Game.Rulesets.Osu/Skinning/Default/DefaultFollowCircle.cs
+++ osu.Game.Rulesets.Osu/Skinning/Default/DefaultFollowCircle.cs
@@ -1,17 +1,25 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Osu.Skinning.Default
 {
     public class DefaultFollowCircle : CompositeDrawable
     {
+        [Resolved(canBeNull: true)]
+        private DrawableHitObject? parentObject { get; set; }
+
         public DefaultFollowCircle()
         {
+            Alpha = 0f;
             RelativeSizeAxes = Axes.Both;
 
             InternalChild = new CircularContainer
@@ -28,6 +36,48 @@ namespace osu.Game.Rulesets.Osu.Skinning.Default
                     Alpha = 0.2f,
                 }
             };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            if (parentObject != null)
+            {
+                var slider = (DrawableSlider)parentObject;
+                slider.Tracking.BindValueChanged(trackingChanged, true);
+            }
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            if (parentObject != null)
+            {
+                parentObject.ApplyCustomUpdateState += updateStateTransforms;
+                updateStateTransforms(parentObject, parentObject.State.Value);
+            }
+        }
+
+        private void trackingChanged(ValueChangedEvent<bool> tracking)
+        {
+            const float scale_duration = 300f;
+            const float fade_duration = 300f;
+
+            this.ScaleTo(tracking.NewValue ? DrawableSliderBall.FOLLOW_AREA : 1f, scale_duration, Easing.OutQuint)
+                .FadeTo(tracking.NewValue ? 1f : 0, fade_duration, Easing.OutQuint);
+        }
+
+        private void updateStateTransforms(DrawableHitObject drawableObject, ArmedState state)
+        {
+            if (drawableObject is not DrawableSlider)
+                return;
+
+            const float fade_time = 450f;
+
+            using (BeginAbsoluteSequence(drawableObject.HitStateUpdateTime))
+                // intentionally pile on an extra FadeOut to make it happen much faster.
+                this.FadeOut(fade_time / 4, Easing.Out);
         }
     }
 }
