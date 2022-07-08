cb1c042c83cabc03e3d9e0282a7be6447aac9123
goodtrailer <87902114+bubb13gummy@users.noreply.github.com>
2022-07-07 20:30:31 -0700

    Protect duration calculations against unstable fps

--- osu.Game.Rulesets.Osu/Skinning/Default/DefaultFollowCircle.cs
+++ osu.Game.Rulesets.Osu/Skinning/Default/DefaultFollowCircle.cs
@@ -1,13 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Diagnostics;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Logging;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osuTK.Graphics;
