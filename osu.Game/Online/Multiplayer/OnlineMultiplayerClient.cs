8e7e1e6b516bf7dcf1a3088fef794bd116226bf4
Dean Herbert <pe@ppy.sh>
2022-07-17 05:20:05 +0900

    Fix spectator client not correctly reconnecting after shutdown

--- osu.Game/Online/Multiplayer/OnlineMultiplayerClient.cs
+++ osu.Game/Online/Multiplayer/OnlineMultiplayerClient.cs
@@ -80,6 +80,7 @@ namespace osu.Game.Online.Multiplayer
 
             try
             {
+                // Importantly, use Invoke rather than Send to capture exceptions.
                 return await connection.InvokeAsync<MultiplayerRoom>(nameof(IMultiplayerServer.JoinRoomWithPassword), roomId, password ?? string.Empty);
             }
             catch (HubException exception)
