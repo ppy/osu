c836e91a4883c713efcef695e90a72f206a84ee3
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-07-18 08:45:54 +0300

    `CountryCode.XX` -> `CountryCode.Unknown`

--- osu.Game.Tests/Visual/Online/TestSceneUserProfileOverlay.cs
+++ osu.Game.Tests/Visual/Online/TestSceneUserProfileOverlay.cs
@@ -24,7 +24,7 @@ namespace osu.Game.Tests.Visual.Online
         {
             Username = @"Somebody",
             Id = 1,
-            CountryCode = CountryCode.XX,
+            CountryCode = CountryCode.Unknown,
             CoverUrl = @"https://osu.ppy.sh/images/headers/profile-covers/c1.jpg",
             JoinDate = DateTimeOffset.Now.AddDays(-1),
             LastVisit = DateTimeOffset.Now,
