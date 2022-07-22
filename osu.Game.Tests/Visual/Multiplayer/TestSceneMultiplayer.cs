824d48b21cdb0e90c723b3d187cbce917bfdfa6f
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-07-22 17:24:46 +0900

    Remove unused using

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayer.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayer.cs
@@ -24,7 +24,6 @@ using osu.Game.Online.API;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Online.Rooms;
-using osu.Game.Overlays.Mods;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Catch;
 using osu.Game.Rulesets.Mods;
