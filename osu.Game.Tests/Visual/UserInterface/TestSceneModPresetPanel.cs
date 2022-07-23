25daaa56e2147c3339ed0ea9a95e938a95d21c61
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-07-23 23:08:05 +0200

    Add test coverage for desired external selection behaviour

--- osu.Game.Tests/Visual/UserInterface/TestSceneModPresetPanel.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneModPresetPanel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Database;
@@ -35,6 +36,48 @@ namespace osu.Game.Tests.Visual.UserInterface
                 Spacing = new Vector2(0, 5),
                 ChildrenEnumerable = createTestPresets().Select(preset => new ModPresetPanel(preset.ToLiveUnmanaged()))
             });
+        }
+
+        [Test]
+        public void TestPresetSelectionStateAfterExternalModChanges()
+        {
+            ModPresetPanel? panel = null;
+
+            AddStep("create panel", () => Child = panel = new ModPresetPanel(createTestPresets().First().ToLiveUnmanaged())
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Width = 0.5f
+            });
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to HR", () => SelectedMods.Value = new[] { new OsuModHardRock() });
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to DT", () => SelectedMods.Value = new[] { new OsuModDoubleTime() });
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to HR+DT", () => SelectedMods.Value = new Mod[] { new OsuModHardRock(), new OsuModDoubleTime() });
+            AddAssert("panel is active", () => panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to HR+customised DT", () => SelectedMods.Value = new Mod[]
+            {
+                new OsuModHardRock(),
+                new OsuModDoubleTime
+                {
+                    SpeedChange = { Value = 1.25 }
+                }
+            });
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to HR+DT", () => SelectedMods.Value = new Mod[] { new OsuModHardRock(), new OsuModDoubleTime() });
+            AddAssert("panel is active", () => panel.AsNonNull().Active.Value);
+
+            AddStep("customise mod in place", () => SelectedMods.Value.OfType<OsuModDoubleTime>().Single().SpeedChange.Value = 1.33);
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
+
+            AddStep("set mods to HD+HR+DT", () => SelectedMods.Value = new Mod[] { new OsuModHidden(), new OsuModHardRock(), new OsuModDoubleTime() });
+            AddAssert("panel is not active", () => !panel.AsNonNull().Active.Value);
         }
 
         private static IEnumerable<ModPreset> createTestPresets() => new[]
