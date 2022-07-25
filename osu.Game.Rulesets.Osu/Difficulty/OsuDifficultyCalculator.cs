8eec063ec72c6276240c8294cec82b8e5e4b561d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-25 14:23:53 +0700

    Set difficulty multiplier to 0.18

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -22,7 +22,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 {
     public class OsuDifficultyCalculator : DifficultyCalculator
     {
-        private const double difficulty_multiplier = 0.0675;
+        private const double difficulty_multiplier = 0.18;
         private double hitWindowGreat;
 
         public override int Version => 20220701;
