e6fad601cc2106e983d13ba9a2383f8500b917ba
Micahel Kelly <michael.kelly.p@outlook.com>
2022-07-27 19:15:43 +1000

    Adds delete difficulty option to editor

---
+++ osu.Game/Screens/Edit/PromptForDifficultyDeleteDialog.cs
@@ -0,0 +1,33 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Overlays.Dialog;
+
+namespace osu.Game.Screens.Edit
+{
+    public class PromptForDifficultyDeleteDialog : PopupDialog
+    {
+        public PromptForDifficultyDeleteDialog(Action delete, Action cancel)
+        {
+            HeaderText = "Are you sure you want to delete this difficulty?";
+
+            Icon = FontAwesome.Regular.Save;
+
+            Buttons = new PopupDialogButton[]
+            {
+                new PopupDialogDangerousButton
+                {
+                    Text = @"Yes delete this difficulty!",
+                    Action = delete
+                },
+                new PopupDialogCancelButton
+                {
+                    Text = @"Oops, continue editing",
+                    Action = cancel
+                },
+            };
+        }
+    }
+}
