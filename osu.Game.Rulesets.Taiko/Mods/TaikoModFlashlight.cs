4e32d510c33ee3f501610818cb7bfa5dc7da2704
NaiPofo <50967056+naipofo@users.noreply.github.com>
2022-07-29 20:08:32 +0200

    Invalidate flashlightProperties on DrawInfo

--- osu.Game.Rulesets.Taiko/Mods/TaikoModFlashlight.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModFlashlight.cs
@@ -49,7 +49,7 @@ namespace osu.Game.Rulesets.Taiko.Mods
 
         private class TaikoFlashlight : Flashlight
         {
-            private readonly LayoutValue flashlightProperties = new LayoutValue(Invalidation.DrawSize);
+            private readonly LayoutValue flashlightProperties = new LayoutValue(Invalidation.DrawSize | Invalidation.DrawInfo);
             private readonly TaikoPlayfield taikoPlayfield;
 
             public TaikoFlashlight(TaikoModFlashlight modFlashlight, TaikoPlayfield taikoPlayfield)
