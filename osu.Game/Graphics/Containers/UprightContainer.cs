ec34a4d6f5ca0de60a671f5921161eccc382c0d0
HiddenNode <87902114+bubb13gummy@users.noreply.github.com>
2022-08-07 13:18:29 +0100

    Implement UprightUnscaledContainer

---
+++ osu.Game/Graphics/Containers/UprightContainer.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Layout;
+
+namespace osu.Game.Graphics.Containers
+{
+    /// <summary>
+    /// A container that prevents itself and its children from getting rotated, scaled or flipped with its Parent.
+    /// </summary>
+    public class UprightUnscaledContainer : Container
+    {
+        public UprightUnscaledContainer()
+        {
+            AddLayout(layout);
+        }
+
+        private LayoutValue layout = new LayoutValue(Invalidation.DrawInfo);
+
+        protected override void Update()
+        {
+            base.Update();
+            if (!layout.IsValid)
+            {
+                Extensions.DrawableExtensions.KeepUprightAndUnscaled(this);
+                layout.Validate();
+            }
+        }
+    }
+}
