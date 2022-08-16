56d0f50dc872e4a49f20cf00bce242322e00667d
o-dasher <odasher896@gmail.com>
2022-08-15 22:50:16 -0400

    Remove unused dependency

--- osu.Game.Rulesets.Osu/Mods/OsuModBlindTravel.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModBlindTravel.cs
@@ -1,9 +1,8 @@
-using System.Diagnostics;
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
 using System;
+using System.Diagnostics;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
