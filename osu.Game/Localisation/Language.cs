38a533d179be81e90d7f89e45ab3adc5835258e0
Wudbirt <88958110+Wudbirt@users.noreply.github.com>
2022-08-20 14:51:54 -0400

    Add missing capitals to language names

--- osu.Game/Localisation/Language.cs
+++ osu.Game/Localisation/Language.cs
@@ -34,13 +34,13 @@ namespace osu.Game.Localisation
         [Description(@"Ελληνικά")]
         el,
 
-        [Description(@"español")]
+        [Description(@"Español")]
         es,
 
         [Description(@"Suomi")]
         fi,
 
-        [Description(@"français")]
+        [Description(@"Français")]
         fr,
 
         [Description(@"Magyar")]
