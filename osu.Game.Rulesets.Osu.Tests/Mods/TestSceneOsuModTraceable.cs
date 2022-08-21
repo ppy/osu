e3ada0f3927af51cdcab5f82364c612185ddb705
Mk-56spn <darioheadleysilva@gmail.com>
2022-08-21 21:06:49 +0200

    Add test for default state of traceable

--- osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModTraceable.cs
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModTraceable.cs
@@ -15,6 +15,42 @@ namespace osu.Game.Rulesets.Osu.Tests.Mods
     public class TestSceneOsuModTraceable : OsuModTestScene
     {
         [Test]
+        public void TestDefaultBeatmapTest() => CreateModTest(new ModTestData
+        {
+            Mod = new OsuModTraceable(),
+            Autoplay = true,
+            Beatmap = new Beatmap
+            {
+                HitObjects = new List<HitObject>
+                {
+                    new HitCircle
+                    {
+                        StartTime = 1000,
+                        Position = new Vector2(206, 142)
+                    },
+                    new HitCircle
+                    {
+                        StartTime = 2000,
+                        Position = new Vector2(306, 142)
+                    },
+                    new Slider
+                    {
+                        StartTime = 3000,
+                        Position = new Vector2(156, 242),
+                        Path = new SliderPath(PathType.Linear, new[] { Vector2.Zero, new Vector2(200, 0), })
+                    },
+                    new Spinner
+                    {
+                        StartTime = 7000,
+                        Position = new Vector2(256, 192),
+                        EndTime = 8000,
+                    }
+                }
+            },
+            PassCondition = checkSomeHit
+        });
+
+        [Test]
         public void TestFadeOutEffect() => CreateModTest(new ModTestData
         {
             Mod = new OsuModTraceable { FadeOutEffect = { Value = true } },
