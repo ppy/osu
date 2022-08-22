423f6f90f2d28bf81ff5096b4b3a8156e7ad9caa
Dan Balasescu <smoogipoo@smgi.me>
2022-08-22 21:31:30 +0900

    Remove async calls from ScoreManager

--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -273,7 +273,7 @@ namespace osu.Game
             dependencies.Cache(difficultyCache = new BeatmapDifficultyCache());
 
             // ordering is important here to ensure foreign keys rules are not broken in ModelStore.Cleanup()
-            dependencies.Cache(ScoreManager = new ScoreManager(RulesetStore, () => BeatmapManager, Storage, realm, Scheduler, API, difficultyCache, LocalConfig));
+            dependencies.Cache(ScoreManager = new ScoreManager(RulesetStore, () => BeatmapManager, Storage, realm, API, LocalConfig));
 
             dependencies.Cache(BeatmapManager = new BeatmapManager(Storage, realm, API, Audio, Resources, Host, defaultBeatmap, difficultyCache, performOnlineLookups: true));
 
