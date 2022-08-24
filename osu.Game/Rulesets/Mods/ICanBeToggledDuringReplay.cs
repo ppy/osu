8e110ce11904245eeadb1b70bf4ef18dabc72211
cdwcgt <cdwcgt@cdwcgt.top>
2022-08-24 19:22:26 +0900

    add description to interface

--- osu.Game/Rulesets/Mods/ICanBeToggledDuringReplay.cs
+++ osu.Game/Rulesets/Mods/ICanBeToggledDuringReplay.cs
@@ -5,18 +5,31 @@ using osu.Framework.Bindables;
 
 namespace osu.Game.Rulesets.Mods
 {
+    /// <summary>
+    /// An interface for mods that can be toggle during replay.
+    /// </summary>
     public interface ICanBeToggledDuringReplay : IApplicableMod
     {
+        /// <summary>
+        /// A property to get a bool value whether the current replay state.
+        /// </summary>
         Bindable<bool> ReplayLoaded
         {
             get;
         }
 
+        /// <summary>
+        /// A property to set whether the mod has been disabled.
+        /// </summary>
         bool IsDisable
         {
             get;
         }
 
+        /// <summary>
+        /// Method called when mod toggle.
+        /// Need check <see cref="ReplayLoaded"/> is not false and toggle the value of IsDisable.
+        /// </summary>
         void OnToggle();
     }
 }
