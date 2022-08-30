95ce78a50c72df36eaaa2c63101ea9939f930a28
Dean Herbert <pe@ppy.sh>
2022-08-30 21:04:38 +0900

    Reduce notification post delay now that it's less important

--- osu.Game/Overlays/NotificationOverlay.cs
+++ osu.Game/Overlays/NotificationOverlay.cs
@@ -117,7 +117,7 @@ namespace osu.Game.Overlays
 
             if (enabled)
                 // we want a slight delay before toggling notifications on to avoid the user becoming overwhelmed.
-                notificationsEnabler = Scheduler.AddDelayed(() => processingPosts = true, State.Value == Visibility.Visible ? 0 : 1000);
+                notificationsEnabler = Scheduler.AddDelayed(() => processingPosts = true, State.Value == Visibility.Visible ? 0 : 100);
             else
                 processingPosts = false;
         }
