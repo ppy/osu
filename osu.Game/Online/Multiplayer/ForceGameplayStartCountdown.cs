47efaafbc6ff3064ae4d23ebe5b60491b4154902
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-09-05 20:13:23 +0900

    Add countdown exclusivity

--- osu.Game/Online/Multiplayer/ForceGameplayStartCountdown.cs
+++ osu.Game/Online/Multiplayer/ForceGameplayStartCountdown.cs
@@ -13,7 +13,8 @@ namespace osu.Game.Online.Multiplayer
     /// and forcing progression of any clients that are blocking load due to user interaction.
     /// </summary>
     [MessagePackObject]
-    public class ForceGameplayStartCountdown : MultiplayerCountdown
+    public sealed class ForceGameplayStartCountdown : MultiplayerCountdown
     {
+        public override bool IsExclusive => true;
     }
 }
