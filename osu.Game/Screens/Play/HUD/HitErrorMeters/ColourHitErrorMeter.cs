aace334fb365485ed41e809d19d34961b57d6920
Mk-56spn <darioheadleysilva@gmail.com>
2022-09-05 04:49:48 +0200

    Fix some test issues

--- osu.Game/Screens/Play/HUD/HitErrorMeters/ColourHitErrorMeter.cs
+++ osu.Game/Screens/Play/HUD/HitErrorMeters/ColourHitErrorMeter.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -110,7 +110,7 @@ namespace osu.Game.Screens.Play.HUD.HitErrorMeters
             }
         }
 
-        private class HitErrorShape : Container
+        public class HitErrorShape : Container
         {
             public bool IsRemoved { get; private set; }
 
