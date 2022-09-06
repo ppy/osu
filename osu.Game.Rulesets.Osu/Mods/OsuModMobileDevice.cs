73c787025e470fb400158ac1ee96d58bfaff6baa
o-dasher <odasher896@gmail.com>
2022-09-06 17:13:22 -0400

    Improves mobile device mod

--- osu.Game.Rulesets.Osu/Mods/OsuModMobileDevice.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModMobileDevice.cs
@@ -2,9 +2,11 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using static osu.Framework.RuntimeInfo;
 
@@ -22,16 +24,27 @@ namespace osu.Game.Rulesets.Osu.Mods
 
         public override double ScoreMultiplier => 1;
 
+        public override ModType Type => ModType.Conversion;
+
         public override Type[] IncompatibleMods => new Type[] { typeof(OsuModDifficultyAdjust) };
 
         public override IconUsage? Icon => FontAwesome.Solid.Mobile;
 
         public Platform[] AllowedPlatforms => new Platform[] { Platform.iOS, Platform.Android };
 
+        [SettingSource("Mobile friendly circles", "Adjust the circle size to be mobile friendly.")]
+        public BindableBool MobileFriendlyCircleSize { get; } = new BindableBool(true);
+
+        [SettingSource("Mobile friendly overall difficulty", "Adjust the overall difficulty to be mobile friendly.")]
+        public BindableBool MobileFriendlyOverallDifficulty { get; } = new BindableBool(true);
+
         public void ApplyToDifficulty(BeatmapDifficulty difficulty)
         {
-            difficulty.CircleSize -= small_touch_device_difficulty_decrease;
-            difficulty.OverallDifficulty -= small_touch_device_difficulty_decrease;
+            if (MobileFriendlyCircleSize.Value)
+                difficulty.CircleSize -= small_touch_device_difficulty_decrease;
+
+            if (MobileFriendlyOverallDifficulty.Value)
+                difficulty.OverallDifficulty -= small_touch_device_difficulty_decrease;
         }
     }
 }
