7f6a415edf2b40df8bca3c646feb8e6dd48d6fee
cdwcgt <cdwcgt@cdwcgt.top>
2022-09-06 21:34:55 +0800

    implement `ICanBeToggledDuringReplay` for `ModWithVisibilityAdjustment`

--- osu.Game.Rulesets.Mania/Mods/ManiaModPlayfieldCover.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModPlayfieldCover.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Linq;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Mania.Objects;
@@ -14,11 +13,9 @@ using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Mania.Mods
 {
-    public abstract class ManiaModPlayfieldCover : ModHidden, IApplicableToDrawableRuleset<ManiaHitObject>, ICanBeToggledDuringReplay
+    public abstract class ManiaModPlayfieldCover : ModHidden, IApplicableToDrawableRuleset<ManiaHitObject>
     {
         public override Type[] IncompatibleMods => new[] { typeof(ModFlashlight<ManiaHitObject>) };
-
-        public BindableBool IsDisabled { get; } = new BindableBool();
 
         /// <summary>
         /// The direction in which the cover should expand.
