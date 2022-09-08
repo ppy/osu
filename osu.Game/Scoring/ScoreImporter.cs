fcf59bafdcc4605016707944a2f6b97d284284e6
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2022-09-08 22:06:44 +0900

    Add MaximumStatistics upgrade for databased scores

--- osu.Game/Scoring/ScoreImporter.cs
+++ osu.Game/Scoring/ScoreImporter.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
+using osu.Framework.Extensions;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
 using osu.Game.Beatmaps;
@@ -16,6 +18,8 @@ using osu.Game.Scoring.Legacy;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Scoring;
 using Realms;
 
 namespace osu.Game.Scoring
@@ -28,15 +32,17 @@ namespace osu.Game.Scoring
 
         private readonly RulesetStore rulesets;
         private readonly Func<BeatmapManager> beatmaps;
+        private readonly BeatmapDifficultyCache? difficultyCache;
 
         private readonly IAPIProvider api;
 
-        public ScoreImporter(RulesetStore rulesets, Func<BeatmapManager> beatmaps, Storage storage, RealmAccess realm, IAPIProvider api)
+        public ScoreImporter(RulesetStore rulesets, Func<BeatmapManager> beatmaps, Storage storage, RealmAccess realm, IAPIProvider api, BeatmapDifficultyCache? difficultyCache = null)
             : base(storage, realm)
         {
             this.rulesets = rulesets;
             this.beatmaps = beatmaps;
             this.api = api;
+            this.difficultyCache = difficultyCache;
         }
 
         protected override ScoreInfo? CreateModel(ArchiveReader archive)
@@ -71,11 +77,76 @@ namespace osu.Game.Scoring
             if (model.BeatmapInfo == null) throw new ArgumentNullException(nameof(model.BeatmapInfo));
             if (model.Ruleset == null) throw new ArgumentNullException(nameof(model.Ruleset));
 
+            PopulateMaximumStatistics(model).WaitSafely();
+
             if (string.IsNullOrEmpty(model.StatisticsJson))
                 model.StatisticsJson = JsonConvert.SerializeObject(model.Statistics);
 
             if (string.IsNullOrEmpty(model.MaximumStatisticsJson))
                 model.MaximumStatisticsJson = JsonConvert.SerializeObject(model.MaximumStatistics);
+        }
+
+        /// <summary>
+        /// Populates the <see cref="ScoreInfo.MaximumStatistics"/> for a given <see cref="ScoreInfo"/>.
+        /// </summary>
+        /// <param name="score">The score to populate the statistics of.</param>
+        public async Task PopulateMaximumStatistics(ScoreInfo score)
+        {
+            if (score.MaximumStatistics.Select(kvp => kvp.Value).Sum() > 0)
+                return;
+
+            var beatmap = score.BeatmapInfo.Detach();
+            var ruleset = score.Ruleset.Detach();
+
+            // Populate the maximum statistics.
+            HitResult maxBasicResult = ruleset.CreateInstance().GetHitResults()
+                                              .Select(h => h.result)
+                                              .Where(h => h.IsBasic())
+                                              .OrderByDescending(Judgement.ToNumericResult).First();
+
+            foreach ((HitResult result, int count) in score.Statistics)
+            {
+                switch (result)
+                {
+                    case HitResult.LargeTickHit:
+                    case HitResult.LargeTickMiss:
+                        score.MaximumStatistics[HitResult.LargeTickHit] = score.MaximumStatistics.GetValueOrDefault(HitResult.LargeTickHit) + count;
+                        break;
+
+                    case HitResult.SmallTickHit:
+                    case HitResult.SmallTickMiss:
+                        score.MaximumStatistics[HitResult.SmallTickHit] = score.MaximumStatistics.GetValueOrDefault(HitResult.SmallTickHit) + count;
+                        break;
+
+                    case HitResult.IgnoreHit:
+                    case HitResult.IgnoreMiss:
+                    case HitResult.SmallBonus:
+                    case HitResult.LargeBonus:
+                        break;
+
+                    default:
+                        score.MaximumStatistics[maxBasicResult] = score.MaximumStatistics.GetValueOrDefault(maxBasicResult) + count;
+                        break;
+                }
+            }
+
+            if (!score.IsLegacyScore)
+                return;
+
+#pragma warning disable CS0618
+            if (difficultyCache == null)
+                throw new InvalidOperationException($"Cannot populate legacy score statistics without a {nameof(BeatmapDifficultyCache)}.");
+
+            // In osu! and osu!mania, some judgements affect combo but aren't stored to scores.
+            // A special hit result is used to pad out the combo value to match, based on the max combo from the difficulty attributes.
+            StarDifficulty? difficulty = await difficultyCache.GetDifficultyAsync(beatmap, ruleset, score.Mods);
+            if (difficulty == null)
+                throw new InvalidOperationException("Failed to populate maximum statistics due to missing difficulty attributes.");
+
+            int maxComboFromStatistics = score.MaximumStatistics.Where(kvp => kvp.Key.AffectsCombo()).Select(kvp => kvp.Value).DefaultIfEmpty(0).Sum();
+            if (difficulty.Value.MaxCombo > maxComboFromStatistics)
+                score.MaximumStatistics[HitResult.LegacyComboIncrease] = difficulty.Value.MaxCombo - maxComboFromStatistics;
+#pragma warning restore CS0618
         }
 
         protected override void PostImport(ScoreInfo model, Realm realm, bool batchImport)
