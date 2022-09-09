fed9a47866664a689ac6dd0c916e578b7a653970
Salman Ahmed <frenzibyte@gmail.com>
2022-09-10 01:12:57 +0300

    Add test coverage

---
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSoloGameplayLeaderboard.cs
@@ -0,0 +1,64 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Utils;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Scoring;
+using osu.Game.Screens.Play.HUD;
+
+namespace osu.Game.Tests.Visual.Gameplay
+{
+    public class TestSceneSoloGameplayLeaderboard : OsuTestScene
+    {
+        [Cached]
+        private readonly ScoreProcessor scoreProcessor = new ScoreProcessor(new OsuRuleset());
+
+        private SoloGameplayLeaderboard leaderboard = null!;
+
+        [SetUp]
+        public void SetUp() => Schedule(() =>
+        {
+            var trackingUser = new APIUser
+            {
+                Username = "local user",
+                Id = 2,
+            };
+
+            Child = leaderboard = new SoloGameplayLeaderboard(trackingUser)
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Expanded = { Value = true },
+            };
+
+            leaderboard.ShowScores(createSampleScores());
+        });
+
+        [Test]
+        public void TestLocalUser()
+        {
+            AddSliderStep("score", 0, 1000000, 500000, v => scoreProcessor.TotalScore.Value = v);
+            AddSliderStep("accuracy", 0f, 1f, 0.5f, v => scoreProcessor.Accuracy.Value = v);
+            AddSliderStep("combo", 0, 1000, 0, v => scoreProcessor.Combo.Value = v);
+        }
+
+        private static List<ScoreInfo> createSampleScores()
+        {
+            return new[]
+            {
+                new ScoreInfo { User = new APIUser { Username = @"peppy" }, TotalScore = RNG.Next(500000, 1000000) },
+                new ScoreInfo { User = new APIUser { Username = @"smoogipoo" }, TotalScore = RNG.Next(500000, 1000000) },
+                new ScoreInfo { User = new APIUser { Username = @"spaceman_atlas" }, TotalScore = RNG.Next(500000, 1000000) },
+                new ScoreInfo { User = new APIUser { Username = @"frenzibyte" }, TotalScore = RNG.Next(500000, 1000000) },
+                new ScoreInfo { User = new APIUser { Username = @"Susko3" }, TotalScore = RNG.Next(500000, 1000000) },
+            }.Concat(Enumerable.Range(0, 50).Select(i => new ScoreInfo { User = new APIUser { Username = $"User {i + 1}" }, TotalScore = 500000 + i * 10000 })).ToList();
+        }
+    }
+}
