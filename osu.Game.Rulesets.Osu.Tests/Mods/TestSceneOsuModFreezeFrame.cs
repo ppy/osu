7fc0366afd56bfea4fcc91c45fc14124924218a6
Mk-56spn <darioheadleysilva@gmail.com>
2022-09-16 13:18:38 +0200

    Improve Tests
    Fix divisor in test

--- osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModFreezeFrame.cs
+++ osu.Game.Rulesets.Osu.Tests/Mods/TestSceneOsuModFreezeFrame.cs
@@ -1,27 +1,40 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using NUnit.Framework;
+using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Mods;
 
 namespace osu.Game.Rulesets.Osu.Tests.Mods
 {
     public class TestSceneOsuModFreezeFrame : OsuModTestScene
     {
-        [TestCase(0.5f)]
-        [TestCase(1)]
-        [TestCase(2)]
-        public void TestFreezeFrequency(float beatMeasure)
+        [TestCase(OsuModFreezeFrame.BeatDivisor.Quarter_Measure)]
+        [TestCase(OsuModFreezeFrame.BeatDivisor.Single_Measure)]
+        [TestCase(OsuModFreezeFrame.BeatDivisor.Quadruple_Measure)]
+        public void TestFreezeFrequency(OsuModFreezeFrame.BeatDivisor divisor)
         {
             CreateModTest(new ModTestData
             {
-                Mod = new OsuModFreezeFrame
-                {
-                    BeatDivisor = { Value = beatMeasure }
-                },
-                PassCondition = () => true,
+                Mod = new OsuModFreezeFrame { Divisor = { Value = divisor } },
+                PassCondition = checkSomeHit,
                 Autoplay = true
             });
         }
+
+        [Test]
+        public void TestWithHidden()
+        {
+            var mods = new List<Mod> { new OsuModHidden(), new OsuModFreezeFrame { Divisor = { Value = OsuModFreezeFrame.BeatDivisor.Quadruple_Measure } } };
+            CreateModTest(new ModTestData
+            {
+                Mods = mods,
+                PassCondition = checkSomeHit,
+                Autoplay = true
+            });
+        }
+
+        private bool checkSomeHit() => Player.ScoreProcessor.JudgedHits >= 8;
     }
 }
