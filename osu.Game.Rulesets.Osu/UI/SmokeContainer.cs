613564b5b9ecdb56953aa093982b3c22a565f8e4
Alden Wu <aldenwu0@gmail.com>
2022-09-18 12:10:01 -0700

    Full legacy smoke implementation and temp default smoke

--- osu.Game.Rulesets.Osu/UI/SmokeContainer.cs
+++ osu.Game.Rulesets.Osu/UI/SmokeContainer.cs
@@ -3,7 +3,6 @@
 
 using System;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Input;
 using osu.Framework.Input.Bindings;
