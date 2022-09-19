2b1c5b2c4a11ffbafbf3fb0361647527de2baaac
Dean Herbert <pe@ppy.sh>
2022-09-19 12:34:50 +0900

    Fix test failure due to triangle skin no longer being null intests

--- osu.Game.Tests/Visual/Gameplay/SkinnableHUDComponentTestScene.cs
+++ osu.Game.Tests/Visual/Gameplay/SkinnableHUDComponentTestScene.cs
@@ -1,12 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Osu;
+using osu.Game.Skinning;
 
 namespace osu.Game.Tests.Visual.Gameplay
 {
@@ -19,7 +18,7 @@ namespace osu.Game.Tests.Visual.Gameplay
         {
             SetContents(skin =>
             {
-                var implementation = skin != null
+                var implementation = skin is not TrianglesSkin
                     ? CreateLegacyImplementation()
                     : CreateDefaultImplementation();
 
