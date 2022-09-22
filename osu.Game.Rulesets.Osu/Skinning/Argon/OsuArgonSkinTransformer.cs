5033861279373ec4c9af5caec8484d35bcd18581
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-09-22 18:50:19 +0900

    Move basic transformer behaviour to base `abstract` class

--- osu.Game.Rulesets.Osu/Skinning/Argon/OsuArgonSkinTransformer.cs
+++ osu.Game.Rulesets.Osu/Skinning/Argon/OsuArgonSkinTransformer.cs
@@ -1,26 +1,20 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Audio.Sample;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Textures;
-using osu.Game.Audio;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Skinning;
 
 namespace osu.Game.Rulesets.Osu.Skinning.Argon
 {
-    public class OsuArgonSkinTransformer : ISkinTransformer
+    public class OsuArgonSkinTransformer : SkinTransformer
     {
-        public ISkin Skin { get; }
-
         public OsuArgonSkinTransformer(ISkin skin)
+            : base(skin)
         {
-            Skin = skin;
         }
 
-        public Drawable? GetDrawableComponent(ISkinComponent component)
+        public override Drawable? GetDrawableComponent(ISkinComponent component)
         {
             switch (component)
             {
@@ -55,16 +49,7 @@ namespace osu.Game.Rulesets.Osu.Skinning.Argon
                     break;
             }
 
-            return null;
-        }
-
-        public Texture? GetTexture(string componentName, WrapMode wrapModeS, WrapMode wrapModeT) => Skin.GetTexture(componentName, wrapModeS, wrapModeT);
-
-        public ISample? GetSample(ISampleInfo sampleInfo) => Skin.GetSample(sampleInfo);
-
-        public IBindable<TValue>? GetConfig<TLookup, TValue>(TLookup lookup) where TLookup : notnull where TValue : notnull
-        {
-            return null;
+            return base.GetDrawableComponent(component);
         }
     }
 }
