da1201c72a1518d66d5d55006a580370bef6cc1e
nanashi-1 <87902114+bubb13gummy@users.noreply.github.com>
2022-09-26 20:08:58 +0800

    add gameplay leaderboard config

--- osu.Game/Overlays/Settings/Sections/Gameplay/HUDSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Gameplay/HUDSettings.cs
@@ -38,6 +38,12 @@ namespace osu.Game.Overlays.Settings.Sections.Gameplay
                     Current = config.GetBindable<bool>(OsuSetting.KeyOverlay),
                     Keywords = new[] { "counter" },
                 },
+                new SettingsCheckbox
+                {
+                    LabelText = GameplaySettingsStrings.AlwaysShowGameplayLeaderboard,
+                    Current = config.GetBindable<bool>(OsuSetting.GameplayLeaderboard),
+                    Keywords = new[] { "leaderboard", "score" },
+                },
             };
         }
     }
