97207c11f55f5a0f464a67c7543c312e2b81ee7c
Dean Herbert <pe@ppy.sh>
2022-09-27 18:52:21 +0900

    Add base transformer for osu!mania argon skin

---
+++ osu.Game.Rulesets.Mania/Skinning/Argon/ManiaArgonSkinTransformer.cs
@@ -0,0 +1,35 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Mania.Skinning.Argon
+{
+    public class ManiaArgonSkinTransformer : SkinTransformer
+    {
+        public ManiaArgonSkinTransformer(ISkin skin)
+            : base(skin)
+        {
+        }
+
+        public override Drawable? GetDrawableComponent(ISkinComponent component)
+        {
+            switch (component)
+            {
+                case ManiaSkinComponent maniaComponent:
+                    switch (maniaComponent.Component)
+                    {
+                        case ManiaSkinComponents.KeyArea:
+                            return new ArgonKeyArea();
+
+                        // TODO: Once everything is finalised, consider throwing UnsupportedSkinComponentException on missing entries.
+                    }
+
+                    break;
+            }
+
+            return base.GetDrawableComponent(component);
+        }
+    }
+}
