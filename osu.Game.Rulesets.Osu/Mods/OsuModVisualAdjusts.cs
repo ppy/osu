2691605e1dd6a7c32943bed95d5e8a7787fe867a
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-09-27 19:15:36 -0400

    Adds `WhiteComboColours` setting.

--- osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Objects;
@@ -12,5 +13,8 @@ namespace osu.Game.Rulesets.Osu.Mods
     {
         [SettingSource("Disable follow points", "No more hints for where to follow...")]
         public DrawableRulesetVisualAdjustSetting DisableFollowPoints { get; } = new DrawableRulesetVisualAdjustSetting(ruleset => ruleset.Playfield.FollowPoints.Hide());
+
+        [SettingSource("White combo colours", "All combos will now be white!")]
+        public DrawableHitObjectVisualAdjustSetting WhiteComboColours { get; } = new DrawableHitObjectVisualAdjustSetting(dho => dho.AccentColour.Value = Colour4.White);
     }
 }
