926ff557960dd2058f5ee0894d6fe6a8d9ab3f19
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-09-28 19:14:49 -0400

    Completely removes overengineered jank

--- osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
@@ -1,28 +1,45 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Graphics;
+using osu.Framework.Bindables;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.UI;
+using osu.Game.Rulesets.UI;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    internal class OsuModVisualAdjusts : ModVisualAdjusts<OsuHitObject, DrawableOsuRuleset>
+    internal class OsuModVisualAdjusts : ModVisualAdjusts, IApplicableToDrawableRuleset<OsuHitObject>
     {
         [SettingSource("Disable follow points", "No more hints for where to follow...")]
-        public DrawableRulesetVisualAdjustSetting DisableFollowPoints { get; } = new DrawableRulesetVisualAdjustSetting(ruleset => ruleset.Playfield.FollowPoints.Hide());
+        public BindableBool DisableFollowPoints { get; } = new BindableBool();
 
         [SettingSource("No combo colours", "The combo colours won't tell you anything now...")]
-        public HitObjectVisibilityVisualAdjustSetting NoComboColours { get; }
+        public BindableBool NoComboColours { get; } = new BindableBool();
 
-        private void applyNoComboColours(DrawableHitObject dho) => dho.AccentColour.Value = Colour4.White;
-
-        public OsuModVisualAdjusts()
+        public void ApplyToDrawableRuleset(DrawableRuleset<OsuHitObject> drawableRuleset)
         {
-            NoComboColours = new HitObjectVisibilityVisualAdjustSetting(applyNoComboColours, applyNoComboColours);
+            if (DisableFollowPoints.Value)
+                ((DrawableOsuRuleset)drawableRuleset).Playfield.FollowPoints.Hide();
+        }
+
+        private void tryApplyNoComboColours(DrawableHitObject hitObject)
+        {
+            if (NoComboColours.Value)
+                hitObject.AccentColour.Value = Color4.White;
+        }
+
+        protected override void ApplyNormalVisibilityState(DrawableHitObject hitObject, ArmedState state)
+        {
+            tryApplyNoComboColours(hitObject);
+        }
+
+        protected override void ApplyIncreasedVisibilityState(DrawableHitObject hitObject, ArmedState state)
+        {
+            tryApplyNoComboColours(hitObject);
         }
     }
 }
