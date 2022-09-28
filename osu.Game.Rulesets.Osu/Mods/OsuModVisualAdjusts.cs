65020e07ff6e537ed1a84480e13e0cca7c578985
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-09-28 17:59:15 -0400

    Properly setup `ModWithVisibilityAdjustment` for `ModVisualAdjust`

--- osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModVisualAdjusts.cs
@@ -4,6 +4,7 @@
 using osu.Framework.Graphics;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.UI;
 
@@ -15,6 +16,13 @@ namespace osu.Game.Rulesets.Osu.Mods
         public DrawableRulesetVisualAdjustSetting DisableFollowPoints { get; } = new DrawableRulesetVisualAdjustSetting(ruleset => ruleset.Playfield.FollowPoints.Hide());
 
         [SettingSource("No combo colours", "The combo colours won't tell you anything now...")]
-        public DrawableHitObjectVisualAdjustSetting NoComboColours { get; } = new DrawableHitObjectVisualAdjustSetting(dho => dho.AccentColour.Value = Colour4.White);
+        public HitObjectVisibilityVisualAdjustSetting NoComboColours { get; }
+
+        private void applyNoComboColours(DrawableHitObject dho) => dho.AccentColour.Value = Colour4.White;
+
+        public OsuModVisualAdjusts()
+        {
+            NoComboColours = new HitObjectVisibilityVisualAdjustSetting(applyNoComboColours, applyNoComboColours);
+        }
     }
 }
