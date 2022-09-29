bb31eb5aed50b9d4569904db98d18e48a2369e47
cdwcgt <cdwcgt@cdwcgt.top>
2022-09-29 21:02:42 +0900

    Add xmldoc
    oversight

--- osu.Game/Rulesets/Mods/IApplicableFailOverride.cs
+++ osu.Game/Rulesets/Mods/IApplicableFailOverride.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Scoring;
+
 namespace osu.Game.Rulesets.Mods
 {
     /// <summary>
@@ -11,7 +13,7 @@ namespace osu.Game.Rulesets.Mods
         /// <summary>
         /// Whether we should allow failing at the current point in time.
         /// </summary>
-        /// <returns>Whether the fail need to proceed. Return <see cref="FailType"/> to decide whether to fail.</returns>
+        /// <returns> Whether the fail need to proceed. Return <see cref="FailType"/> to decide whether to fail.</returns>
         FailType PerformFail();
 
         /// <summary>
@@ -22,9 +24,27 @@ namespace osu.Game.Rulesets.Mods
 
     public enum FailType
     {
+        /// <summary>
+        /// Players will never fail even have ForceFail or AllowFail.
+        /// Aims for certain mods <see cref="ModBlockFail"/> that don't allow the player to fail.
+        /// </summary>
         BlockFail,
+
+        /// <summary>
+        /// Player will fail when <see cref="HealthProcessor"/> handle a fail anyway, will override by BlockFail.
+        /// Aims for certain mods <see cref="ModFailCondition"/> that change fai condition and will not override by AvoidFail.
+        /// </summary>
         ForceFail,
+
+        /// <summary>
+        /// Player will not fail when <see cref="HealthProcessor"/> handle a fail, will override by ForceFail.
+        /// Aims not to overwrite when certain mods change fail conditions like <see cref="ModFailCondition"/>.
+        /// </summary>
         AvoidFail,
+
+        /// <summary>
+        /// Player will fail when <see cref="HealthProcessor"/> handle a fail.
+        /// </summary>
         AllowFail
     }
 }
