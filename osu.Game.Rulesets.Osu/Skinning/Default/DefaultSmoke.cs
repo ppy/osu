04abb2ce8f1723fbf342d7cb215eea0d20d221bb
Dean Herbert <pe@ppy.sh>
2022-10-05 18:26:17 +0900

    Update default cursor smoke implementation to use a texture

--- osu.Game.Rulesets.Osu/Skinning/Default/DefaultSmoke.cs
+++ osu.Game.Rulesets.Osu/Skinning/Default/DefaultSmoke.cs
@@ -1,13 +1,19 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Textures;
+
 namespace osu.Game.Rulesets.Osu.Skinning.Default
 {
     public class DefaultSmoke : Smoke
     {
-        public DefaultSmoke()
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
         {
-            Radius = 2;
+            // ISkinSource doesn't currently fallback to global textures.
+            // We might want to change this in the future if the intention is to allow the user to skin this as per legacy skins.
+            Texture = textures.Get("Gameplay/osu/cursor-smoke");
         }
     }
 }
