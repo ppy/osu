9b8e06fddbf038748a5a4ba2b525c959e8764585
Dario Headley <87902114+bubb13gummy@users.noreply.github.com>
2022-10-12 09:39:57 +0200

    Max Combo Counter added

--- osu.Game/Screens/Play/HUD/ComboCounters/DefaultComboCounter.cs
+++ osu.Game/Screens/Play/HUD/ComboCounters/DefaultComboCounter.cs
@@ -3,27 +3,17 @@
 
 #nullable disable
 
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
-using osu.Game.Graphics.UserInterface;
 using osu.Game.Rulesets.Scoring;
-using osu.Game.Skinning;
 
-namespace osu.Game.Screens.Play.HUD
+namespace osu.Game.Screens.Play.HUD.ComboCounters
 {
-    public class DefaultComboCounter : RollingCounter<int>, ISkinnableDrawable
+    public class DefaultComboCounter : ComboCounter
     {
-        public bool UsesFixedAnchor { get; set; }
-
-        public DefaultComboCounter()
-        {
-            Current.Value = DisplayedCount = 0;
-        }
-
         [BackgroundDependencyLoader]
         private void load(OsuColour colours, ScoreProcessor scoreProcessor)
         {
@@ -31,17 +21,12 @@ namespace osu.Game.Screens.Play.HUD
             Current.BindTo(scoreProcessor.Combo);
         }
 
+        protected override OsuSpriteText CreateSpriteText()
+            => base.CreateSpriteText().With(s => s.Font = s.Font.With(size: 20f));
+
         protected override LocalisableString FormatCount(int count)
         {
             return $@"{count}x";
         }
-
-        protected override double GetProportionalDuration(int currentValue, int newValue)
-        {
-            return Math.Abs(currentValue - newValue) * RollingDuration * 100.0f;
-        }
-
-        protected override OsuSpriteText CreateSpriteText()
-            => base.CreateSpriteText().With(s => s.Font = s.Font.With(size: 20f));
     }
 }
