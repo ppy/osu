c4db9200d500a9fb78fe1a0986c6753b35657cd8
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2022-10-15 00:18:24 +0300

    Move mouse input detection inside `MenuCursorContainer` to allow testing

--- osu.Game/Input/OsuUserInputManager.cs
+++ osu.Game/Input/OsuUserInputManager.cs
@@ -3,41 +3,15 @@
 
 #nullable disable
 
-using osu.Framework.Bindables;
 using osu.Framework.Input;
-using osu.Framework.Input.StateChanges.Events;
 using osuTK.Input;
 
 namespace osu.Game.Input
 {
     public class OsuUserInputManager : UserInputManager
     {
-        /// <summary>
-        /// Whether the last input applied to the game is sourced from mouse.
-        /// </summary>
-        public IBindable<bool> LastInputWasMouseSource => lastInputWasMouseSource;
-
-        private readonly Bindable<bool> lastInputWasMouseSource = new Bindable<bool>();
-
         internal OsuUserInputManager()
         {
-        }
-
-        public override void HandleInputStateChange(InputStateChangeEvent inputStateChange)
-        {
-            switch (inputStateChange)
-            {
-                case ButtonStateChangeEvent<MouseButton>:
-                case MousePositionChangeEvent:
-                    lastInputWasMouseSource.Value = true;
-                    break;
-
-                default:
-                    lastInputWasMouseSource.Value = false;
-                    break;
-            }
-
-            base.HandleInputStateChange(inputStateChange);
         }
 
         protected override MouseButtonEventManager CreateButtonEventManagerFor(MouseButton button)
