528078822cf3123cf536be7c9238d68cfe8b6381
Dario Headley <87902114+bubb13gummy@users.noreply.github.com>
2022-10-18 13:39:40 +0200

    Fixed approachCircle preempt

--- osu.Game.Rulesets.Osu/Mods/OsuModFreezeFrame.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModFreezeFrame.cs
@@ -1,17 +1,20 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
+using osu.Framework.Graphics;
 using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Osu.Objects;
-using osu.Game.Rulesets.Osu.UI;
-using osu.Game.Rulesets.UI;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModFreezeFrame : Mod, IApplicableToBeatmap, IApplicableToDrawableRuleset<OsuHitObject>
+    public class OsuModFreezeFrame : Mod, IApplicableToDrawableHitObject, IApplicableToBeatmap
+
     {
         public override string Name => "Freeze Frame";
 
@@ -21,16 +24,18 @@ namespace osu.Game.Rulesets.Osu.Mods
 
         public override LocalisableString Description => "Burn the notes into your memory.";
 
+        //Alters the transforms of the approach circles, breaking the effects of these mods.
+        public override Type[] IncompatibleMods => new[] { typeof(OsuModApproachDifferent), /*typeof(OsuModHidden)*/ };
+
         public override ModType Type => ModType.Fun;
 
-        public void ApplyToDrawableRuleset(DrawableRuleset<OsuHitObject> drawableRuleset)
-        {
-            (drawableRuleset.Playfield as OsuPlayfield)?.FollowPoints.Hide();
-        }
+        //mod breaks normal approach circle preempt
+        private double approachCircleTimePreempt;
 
         public void ApplyToBeatmap(IBeatmap beatmap)
         {
             double lastNewComboTime = 0;
+            approachCircleTimePreempt = beatmap.HitObjects.OfType<OsuHitObject>().FirstOrDefault()!.TimePreempt;
 
             foreach (var obj in beatmap.HitObjects.OfType<OsuHitObject>())
             {
@@ -57,6 +62,29 @@ namespace osu.Game.Rulesets.Osu.Mods
                     }
                 }
             }
+        }
+
+        public void ApplyToDrawableHitObject(DrawableHitObject drawableObject)
+        {
+            drawableObject.ApplyCustomUpdateState += (drawableHitObject, _) =>
+            {
+                if (drawableHitObject is not DrawableHitCircle drawableHitCircle) return;
+
+                var hitCircle = drawableHitCircle.HitObject;
+                var approachCircle = drawableHitCircle.ApproachCircle;
+
+                approachCircle.ClearTransforms();
+                approachCircle.ScaleTo(4);
+                approachCircle.FadeTo(0);
+
+                using (drawableHitCircle.ApproachCircle.BeginAbsoluteSequence(hitCircle.StartTime - approachCircleTimePreempt))
+                {
+                    //Redo ApproachCircle animation with correct startTime.
+                    approachCircle.LifetimeStart = hitCircle.StartTime - approachCircleTimePreempt;
+                    approachCircle.FadeTo(1, Math.Min(hitCircle.TimeFadeIn * 2, hitCircle.TimePreempt));
+                    approachCircle.ScaleTo(1, approachCircleTimePreempt).Then().Expire();
+                }
+            };
         }
     }
 }
