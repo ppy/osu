a7070b5168549a565bf707016e4f00c92635717c
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2022-10-19 12:56:21 +0100

    Improve code quality

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -26,15 +26,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             // Maybe I should just pass in clockrate...
             var clockRateEstimate = current.BaseObject.StartTime / currObj.StartTime;
 
-            List<OsuDifficultyHitObject> pastVisibleObjects = retrievePastVisibleObjects(currObj);
-
-            // Rather than note density being the number of on-screen objects visible at the current object,
-            // consider it as how many objects the current object has been visible for.
-            double noteDensityDifficulty = 1.0;
-
             double pastObjectDifficultyInfluence = 1.0;
 
-            foreach (var loopObj in pastVisibleObjects)
+            foreach (var loopObj in retrievePastVisibleObjects(currObj))
             {
                 double loopDifficulty = currObj.OpacityAt(loopObj.BaseObject.StartTime, false);
 
@@ -47,17 +41,17 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 pastObjectDifficultyInfluence += loopDifficulty;
             }
 
-            noteDensityDifficulty = Math.Pow(3 * Math.Log(Math.Max(1, pastObjectDifficultyInfluence - 1)), 2.3);
+            double noteDensityDifficulty = Math.Pow(3 * Math.Log(Math.Max(1, pastObjectDifficultyInfluence - 1)), 2.3);
 
             double hiddenDifficulty = 0;
 
             if (hidden)
             {
-                var timeSpentInvisible = getDurationSpentInvisible(currObj) / clockRateEstimate;
-                var timeDifficultyFactor = 800 / pastObjectDifficultyInfluence;
+                double timeSpentInvisible = getDurationSpentInvisible(currObj) / clockRateEstimate;
+                double timeDifficultyFactor = 800 / pastObjectDifficultyInfluence;
 
-                hiddenDifficulty += Math.Pow(7 * timeSpentInvisible / timeDifficultyFactor, 1);
-                hiddenDifficulty += 2 * currVelocity;
+                hiddenDifficulty += Math.Pow(7 * timeSpentInvisible / timeDifficultyFactor, 1) +
+                                    2 * currVelocity;
             }
 
             double difficulty = hiddenDifficulty + noteDensityDifficulty;
@@ -68,21 +62,19 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
         // Returns a list of objects that are visible on screen at
         // the point in time at which the current object becomes visible.
-        private static List<OsuDifficultyHitObject> retrievePastVisibleObjects(OsuDifficultyHitObject current)
+        private static IEnumerable<OsuDifficultyHitObject> retrievePastVisibleObjects(OsuDifficultyHitObject current)
         {
-            List<OsuDifficultyHitObject> objects = new List<OsuDifficultyHitObject>();
-
             for (int i = 0; i < current.Index; i++)
             {
-                OsuDifficultyHitObject loopObj = (OsuDifficultyHitObject)current.Previous(i);
+                OsuDifficultyHitObject hitObject = (OsuDifficultyHitObject)current.Previous(i);
 
-                if (loopObj.IsNull() || current.StartTime - loopObj.StartTime > reading_window_size)
+                if (hitObject.IsNull() ||
+                    current.StartTime - hitObject.StartTime > reading_window_size ||
+                    hitObject.StartTime < current.StartTime - current.preempt)
                     break;
 
-                objects.Add(loopObj);
+                yield return hitObject;
             }
-
-            return objects;
         }
 
         private static double getDurationSpentInvisible(OsuDifficultyHitObject current)
@@ -101,7 +93,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             const double time_limit_low = 200;
 
             double constantAngleCount = 0;
-
             int index = 0;
             double currentTimeGap = 0;
 
@@ -117,7 +108,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 if (loopObj.Angle.IsNotNull() && current.Angle.IsNotNull())
                 {
                     double angleDifference = Math.Abs(current.Angle.Value - loopObj.Angle.Value);
-
                     constantAngleCount += Math.Cos(4 * Math.Min(Math.PI / 8, angleDifference)) * longIntervalFactor;
                 }
 
@@ -125,14 +115,12 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 index++;
             }
 
-            double difficulty = Math.Pow(Math.Min(1, 2 / constantAngleCount), 2);
-
-            return difficulty;
+            return Math.Pow(Math.Min(1, 2 / constantAngleCount), 2);
         }
 
         private static double getTimeNerfFactor(double deltaTime)
         {
-            return Math.Clamp(2 - (deltaTime / 1500), 0, 1);
+            return Math.Clamp(2 - deltaTime / (reading_window_size / 2), 0, 1);
         }
 
         private static double logistic(double x) => 1 / (1 + Math.Pow(Math.E, -x));
