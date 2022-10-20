42bb9dc3e3db17b9cb39da3103ce10820a683db9
cdwcgt <cdwcgt@cdwcgt.top>
2022-10-20 22:17:26 +0900

    revert EndPlacement change

--- osu.Game.Rulesets.Taiko/Edit/TaikoHitObjectComposer.cs
+++ osu.Game.Rulesets.Taiko/Edit/TaikoHitObjectComposer.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System.Collections.Generic;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Edit.Tools;
@@ -17,16 +15,6 @@ namespace osu.Game.Rulesets.Taiko.Edit
         public TaikoHitObjectComposer(TaikoRuleset ruleset)
             : base(ruleset)
         {
-        }
-
-        public override void EndPlacement(HitObject hitObject, bool commit)
-        {
-            EditorBeatmap.PlacementObject.Value = null;
-
-            if (commit)
-            {
-                EditorBeatmap.Add(hitObject);
-            }
         }
 
         protected override IReadOnlyList<HitObjectCompositionTool> CompositionTools => new HitObjectCompositionTool[]
