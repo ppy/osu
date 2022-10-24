003c7480ebdca366a0ba28811c1fc7fb5b93a113
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-10-24 16:57:45 +0900

    Add argon transformer for osu!catch (and basic new catcher design)

---
+++ osu.Game.Rulesets.Catch/Skinning/Argon/CatchArgonSkinTransformer.cs
@@ -0,0 +1,34 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Catch.Skinning.Argon
+{
+    public class CatchArgonSkinTransformer : SkinTransformer
+    {
+        public CatchArgonSkinTransformer(ISkin skin)
+            : base(skin)
+        {
+        }
+
+        public override Drawable? GetDrawableComponent(ISkinComponent component)
+        {
+            switch (component)
+            {
+                case CatchSkinComponent osuComponent:
+                    // TODO: Once everything is finalised, consider throwing UnsupportedSkinComponentException on missing entries.
+                    switch (osuComponent.Component)
+                    {
+                        case CatchSkinComponents.Catcher:
+                            return new ArgonCatcher();
+                    }
+
+                    break;
+            }
+
+            return base.GetDrawableComponent(component);
+        }
+    }
+}
