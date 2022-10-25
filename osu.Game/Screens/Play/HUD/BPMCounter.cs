82f3ced868f7bf08c9227907e22fe6cb3427c762
WyvernX <58601449+WyvernX-YT@users.noreply.github.com>
2022-10-25 13:25:33 +0100

    Lower spacing!

--- osu.Game/Screens/Play/HUD/BPMCounter.cs
+++ osu.Game/Screens/Play/HUD/BPMCounter.cs
@@ -71,7 +71,7 @@ namespace osu.Game.Screens.Play.HUD
                 {
                     AutoSizeAxes = Axes.Both,
                     Direction = FillDirection.Horizontal,
-                    Spacing = new Vector2(5, 0),
+                    Spacing = new Vector2(3, 0),
                     Children = new Drawable[]
                     {
                         text = new OsuSpriteText
