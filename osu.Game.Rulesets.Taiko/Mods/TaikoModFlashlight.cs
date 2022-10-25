aa2357e6836bd6d5673ec1624274b94cb50e46c0
WyvernX <58601449+WyvernX-YT@users.noreply.github.com>
2022-10-25 10:43:42 +0100

    Fixed slight issue

--- osu.Game.Rulesets.Taiko/Mods/TaikoModFlashlight.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModFlashlight.cs
@@ -81,7 +81,7 @@ namespace osu.Game.Rulesets.Taiko.Mods
                 }
                 else
                 {
-                    UpdateFlashlightSize(200);
+                    UpdateFlashlightSize(GetSize());
                 }
             }
         }
