2deaae270c19d991e7b12fc1280146efa3c11b2c
Dean Herbert <pe@ppy.sh>
2022-10-26 17:12:20 +0900

    Fix catcher not consdiering allowable catch range

--- osu.Game.Rulesets.Catch/Skinning/Argon/ArgonCatcher.cs
+++ osu.Game.Rulesets.Catch/Skinning/Argon/ArgonCatcher.cs
@@ -34,48 +34,51 @@ namespace osu.Game.Rulesets.Catch.Skinning.Argon
                         new Circle
                         {
                             RelativeSizeAxes = Axes.Both,
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
                             Colour = Color4.White,
+                            Width = Catcher.ALLOWED_CATCH_RANGE,
                         },
                         new Box
                         {
+                            Name = "long line left",
                             Anchor = Anchor.CentreLeft,
                             Origin = Anchor.CentreRight,
                             Colour = Color4.White,
                             Alpha = 0.25f,
                             RelativeSizeAxes = Axes.X,
-                            X = -2,
                             Width = 20,
                             Height = 1.8f,
                         },
                         new Circle
                         {
+                            Name = "bumper left",
                             Anchor = Anchor.CentreLeft,
-                            Origin = Anchor.CentreRight,
+                            Origin = Anchor.CentreLeft,
                             Colour = Color4.White,
                             RelativeSizeAxes = Axes.X,
-                            X = -2,
-                            Width = 15 / 170f,
+                            Width = (1 - Catcher.ALLOWED_CATCH_RANGE) / 2,
                             Height = 4,
                         },
                         new Box
                         {
+                            Name = "long line right",
                             Anchor = Anchor.CentreRight,
                             Origin = Anchor.CentreLeft,
                             Colour = Color4.White,
                             Alpha = 0.25f,
                             RelativeSizeAxes = Axes.X,
-                            X = 2,
                             Width = 20,
                             Height = 1.8f,
                         },
                         new Circle
                         {
+                            Name = "bumper right",
                             Anchor = Anchor.CentreRight,
-                            Origin = Anchor.CentreLeft,
+                            Origin = Anchor.CentreRight,
                             Colour = Color4.White,
-                            X = 2,
                             RelativeSizeAxes = Axes.X,
-                            Width = 15 / 170f,
+                            Width = (1 - Catcher.ALLOWED_CATCH_RANGE) / 2,
                             Height = 4,
                         },
                     }
