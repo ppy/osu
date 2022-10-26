bbe26e39911b363676ec1b82d554de48b45980f2
NullifiedJosh <86538544+NullifiedJosh@users.noreply.github.com>
2022-10-27 00:52:34 +0800

    Fix formatting

--- osu.Game/Utils/KeyboardService.cs
+++ osu.Game/Utils/KeyboardService.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
+
 namespace osu.Game.Utils
 {
     /// <summary>
