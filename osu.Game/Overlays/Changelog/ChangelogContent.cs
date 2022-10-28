b6be4ad14c76152d02735b11df62d259e1034d2d
ansel <87902114+bubb13gummy@users.noreply.github.com>
2022-10-28 22:17:45 +0300

    Remove local popover containers

--- osu.Game/Overlays/Changelog/ChangelogContent.cs
+++ osu.Game/Overlays/Changelog/ChangelogContent.cs
@@ -1,35 +1,24 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Online.API.Requests.Responses;
 using System;
-using osu.Framework.Graphics.Cursor;
 
 namespace osu.Game.Overlays.Changelog
 {
-    public class ChangelogContent : PopoverContainer
+    public class ChangelogContent : FillFlowContainer
     {
-        public Action<APIChangelogBuild> BuildSelected;
+        public Action<APIChangelogBuild>? BuildSelected;
 
         public void SelectBuild(APIChangelogBuild build) => BuildSelected?.Invoke(build);
-
-        protected override Container<Drawable> Content { get; }
 
         public ChangelogContent()
         {
             RelativeSizeAxes = Axes.X;
             AutoSizeAxes = Axes.Y;
-
-            base.Content.Add(Content = new FillFlowContainer
-            {
-                RelativeSizeAxes = Axes.X,
-                AutoSizeAxes = Axes.Y,
-                Direction = FillDirection.Vertical
-            });
+            Direction = FillDirection.Vertical;
         }
     }
 }
