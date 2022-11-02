58c6b026ae503097b86f8c8f139912042fa3360a
Dan Balasescu <smoogipoo@smgi.me>
2022-11-02 15:23:11 +0900

    Remove unused using

--- osu.Game.Tournament/Components/TournamentMatchChatDisplay.cs
+++ osu.Game.Tournament/Components/TournamentMatchChatDisplay.cs
@@ -9,7 +9,6 @@ using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Online.API;
 using osu.Game.Online.Chat;
-using osu.Game.Online.Notifications.WebSocket;
 using osu.Game.Overlays.Chat;
 using osu.Game.Tournament.IPC;
 using osu.Game.Tournament.Models;
