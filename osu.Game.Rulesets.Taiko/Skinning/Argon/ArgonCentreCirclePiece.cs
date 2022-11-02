bc0e9375afcc54b108246f6bb60e2b3e50425b82
Dean Herbert <pe@ppy.sh>
2022-11-02 16:52:09 +0900

    Add basic argon hits

---
+++ osu.Game.Rulesets.Taiko/Skinning/Argon/ArgonCentreCirclePiece.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Colour;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Taiko.Skinning.Argon
+{
+    public class ArgonCentreCirclePiece : ArgonCirclePiece
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AccentColour = ColourInfo.GradientVertical(
+                new Color4(241, 0, 0, 255),
+                new Color4(167, 0, 0, 255)
+            );
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Taiko/Skinning/Argon/ArgonRimCirclePiece.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Colour;
+using osuTK.Graphics;
+
+namespace osu.Game.Rulesets.Taiko.Skinning.Argon
+{
+    public class ArgonRimCirclePiece : ArgonCirclePiece
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AccentColour = ColourInfo.GradientVertical(
+                new Color4(0, 161, 241, 255),
+                new Color4(0, 111, 167, 255)
+            );
+        }
+    }
+}
