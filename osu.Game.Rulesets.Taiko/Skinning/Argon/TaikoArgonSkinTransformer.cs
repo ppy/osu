8f4aec4381f81558559e90568ee4c4213689f5f9
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-11-02 15:45:32 +0900

    Add basic setup for taiko argon skinning

---
+++ osu.Game.Rulesets.Taiko/Skinning/Argon/TaikoArgonSkinTransformer.cs
@@ -0,0 +1,34 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Skinning;
+
+namespace osu.Game.Rulesets.Taiko.Skinning.Argon
+{
+    public class TaikoArgonSkinTransformer : SkinTransformer
+    {
+        public TaikoArgonSkinTransformer(ISkin skin)
+            : base(skin)
+        {
+        }
+
+        public override Drawable? GetDrawableComponent(ISkinComponent component)
+        {
+            switch (component)
+            {
+                case TaikoSkinComponent catchComponent:
+                    // TODO: Once everything is finalised, consider throwing UnsupportedSkinComponentException on missing entries.
+                    switch (catchComponent.Component)
+                    {
+                        case TaikoSkinComponents.CentreHit:
+                            return Drawable.Empty();
+                    }
+
+                    break;
+            }
+
+            return base.GetDrawableComponent(component);
+        }
+    }
+}
