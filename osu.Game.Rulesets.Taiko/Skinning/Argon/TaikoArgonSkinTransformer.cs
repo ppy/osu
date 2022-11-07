d5c375b139f135e9ffc49ae4c9e67f8979b07865
Dean Herbert <pe@ppy.sh>
2022-11-07 16:18:03 +0900

    Add argon hit explosion implementation

--- osu.Game.Rulesets.Taiko/Skinning/Argon/TaikoArgonSkinTransformer.cs
+++ osu.Game.Rulesets.Taiko/Skinning/Argon/TaikoArgonSkinTransformer.cs
@@ -44,6 +44,15 @@ namespace osu.Game.Rulesets.Taiko.Skinning.Argon
 
                         case TaikoSkinComponents.DrumRollTick:
                             return new ArgonTickPiece();
+
+                        case TaikoSkinComponents.TaikoExplosionKiai:
+                            // the drawable needs to expire as soon as possible to avoid accumulating empty drawables on the playfield.
+                            return Drawable.Empty().With(d => d.Expire());
+
+                        case TaikoSkinComponents.TaikoExplosionGreat:
+                        case TaikoSkinComponents.TaikoExplosionMiss:
+                        case TaikoSkinComponents.TaikoExplosionOk:
+                            return new ArgonHitExplosion(catchComponent.Component);
                     }
 
                     break;
