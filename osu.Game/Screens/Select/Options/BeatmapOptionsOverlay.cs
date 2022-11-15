cf969ac3c11226ecf90bddad30b3dc898ba83eaa
nanashi-1 <87902114+bubb13gummy@users.noreply.github.com>
2022-11-15 20:57:31 +0800

    AddButton now returns the button

--- osu.Game/Screens/Select/Options/BeatmapOptionsOverlay.cs
+++ osu.Game/Screens/Select/Options/BeatmapOptionsOverlay.cs
@@ -66,7 +66,7 @@ namespace osu.Game.Screens.Select.Options
         /// <param name="colour">Colour of the button.</param>
         /// <param name="icon">Icon of the button.</param>
         /// <param name="action">Binding the button does.</param>
-        public void AddButton(LocalisableString firstLine, string secondLine, IconUsage icon, Color4 colour, Action action)
+        public BeatmapOptionsButton AddButton(LocalisableString firstLine, string secondLine, IconUsage icon, Color4 colour, Action action)
         {
             var button = new BeatmapOptionsButton
             {
@@ -82,6 +82,8 @@ namespace osu.Game.Screens.Select.Options
             };
 
             buttonsContainer.Add(button);
+
+            return button;
         }
 
         protected override void PopIn()
