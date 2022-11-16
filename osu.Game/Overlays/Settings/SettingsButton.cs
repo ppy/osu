ced5fbf4c1d0d3a33054dcb8802ddb5441d8832a
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2022-11-16 22:58:32 +0900

    Fix multiple issues with settings items unhiding on search

--- osu.Game/Overlays/Settings/SettingsButton.cs
+++ osu.Game/Overlays/Settings/SettingsButton.cs
@@ -3,14 +3,16 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Cursor;
 using osu.Framework.Localisation;
 using osu.Game.Graphics.UserInterfaceV2;
 
 namespace osu.Game.Overlays.Settings
 {
-    public partial class SettingsButton : RoundedButton, IHasTooltip
+    public partial class SettingsButton : RoundedButton, IHasTooltip, IConditionalFilterable
     {
         public SettingsButton()
         {
@@ -19,6 +21,9 @@ namespace osu.Game.Overlays.Settings
         }
 
         public LocalisableString TooltipText { get; set; }
+
+        public BindableBool CanBeShown { get; } = new BindableBool(true);
+        IBindable<bool> IConditionalFilterable.CanBeShown => CanBeShown;
 
         public override IEnumerable<LocalisableString> FilterTerms
         {
