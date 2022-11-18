d47c46d1443be45ad90ca2e70780b21a94aa1080
Dan Balasescu <smoogipoo@smgi.me>
2022-11-18 14:16:12 +0900

    Compute raw score in-line
    Saves on an allocation (though it seems to only be a ValueType so it
    shouldn't be a big deal) and a tail-call.

--- osu.Game/Rulesets/Scoring/ScoreProcessor.cs
+++ osu.Game/Rulesets/Scoring/ScoreProcessor.cs
@@ -335,24 +335,21 @@ namespace osu.Game.Rulesets.Scoring
         [Pure]
         public long ComputeScore(ScoringMode mode, double accuracyRatio, double comboRatio, long bonusScore, int totalBasicHitObjects)
         {
+            double accuracyScore = accuracyPortion * accuracyRatio;
+            double comboScore = comboPortion * comboRatio;
+            double rawScore = (max_score * (accuracyScore + comboScore) + bonusScore) * scoreMultiplier;
+
             switch (mode)
             {
                 default:
                 case ScoringMode.Standardised:
-                    return (long)Math.Round(computeRawScore());
+                    return (long)Math.Round(rawScore);
 
                 case ScoringMode.Classic:
                     // This gives a similar feeling to osu!stable scoring (ScoreV1) while keeping classic scoring as only a constant multiple of standardised scoring.
                     // The invariant is important to ensure that scores don't get re-ordered on leaderboards between the two scoring modes.
-                    double scaledStandardised = computeRawScore() / max_score;
-                    return (long)Math.Round(Math.Pow(scaledStandardised * Math.Max(1, totalBasicHitObjects), 2) * ClassicScoreMultiplier);
-            }
-
-            double computeRawScore()
-            {
-                double accuracyScore = accuracyPortion * accuracyRatio;
-                double comboScore = comboPortion * comboRatio;
-                return (max_score * (accuracyScore + comboScore) + bonusScore) * scoreMultiplier;
+                    double scaledRawScore = rawScore / max_score;
+                    return (long)Math.Round(Math.Pow(scaledRawScore * Math.Max(1, totalBasicHitObjects), 2) * ClassicScoreMultiplier);
             }
         }
 
