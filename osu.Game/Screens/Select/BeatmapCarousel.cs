ppy/osu

commit c8a85fb1a19b2f92138105f3f0f18997c13f4e68

Improve song select transition to gameplay

osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -770,6 +770,26 @@ namespace osu.Game.Screens.Select
             {
                 updateItem(item);
 
+                if (!item.Item.Filtered.Value)
+                {
+                    bool isSelected = item.Item.State.Value == CarouselItemState.Selected;
+
+                    // Cheap way of doing animations when entering / exiting song select.
+                    const double half_time = 50;
+                    const float panel_x_offset_when_inactive = 200;
+
+                    if (isSelected || AllowSelection)
+                    {
+                        item.Alpha = (float)Interpolation.DampContinuously(item.Alpha, 1, half_time, Clock.ElapsedFrameTime);
+                        item.X = (float)Interpolation.DampContinuously(item.X, 0, half_time, Clock.ElapsedFrameTime);
+                    }
+                    else
+                    {
+                        item.Alpha = (float)Interpolation.DampContinuously(item.Alpha, 0, half_time, Clock.ElapsedFrameTime);
+                        item.X = (float)Interpolation.DampContinuously(item.X, panel_x_offset_when_inactive, half_time, Clock.ElapsedFrameTime);
+                    }
+                }
+
                 if (item is DrawableCarouselBeatmapSet set)
                 {
                     foreach (var diff in set.DrawableBeatmaps)

osu.Game/Screens/Select/SongSelect.cs
@@ -89,6 +89,8 @@ namespace osu.Game.Screens.Select
 
         protected BeatmapCarousel Carousel { get; private set; }
 
+        private ParallaxContainer wedgeBackground;
+
         protected Container LeftArea { get; private set; }
 
         private BeatmapInfoWedge beatmapInfoWedge;
@@ -165,10 +167,12 @@ namespace osu.Game.Screens.Select
                             {
                                 new Drawable[]
                                 {
-                                    new ParallaxContainer
+                                    wedgeBackground = new ParallaxContainer
                                     {
                                         ParallaxAmount = 0.005f,
                                         RelativeSizeAxes = Axes.Both,
+                                        Anchor = Anchor.Centre,
+                                        Origin = Anchor.Centre,
                                         Child = new WedgeBackground
                                         {
                                             RelativeSizeAxes = Axes.Both,
@@ -609,9 +613,15 @@ namespace osu.Game.Screens.Select
                 }
             }
 
-            this.FadeIn(250);
+            LeftArea.MoveToX(0, 400, Easing.OutQuint);
+            LeftArea.FadeIn(100, Easing.OutQuint);
 
-            this.ScaleTo(1, 250, Easing.OutSine);
+            FilterControl.MoveToY(0, 400, Easing.OutQuint);
+            FilterControl.FadeIn(100, Easing.OutQuint);
+
+            this.FadeIn(250, Easing.OutQuint);
+
+            wedgeBackground.ScaleTo(1, 500, Easing.OutQuint);
 
             FilterControl.Activate();
         }
@@ -629,9 +639,15 @@ namespace osu.Game.Screens.Select
 
             endLooping();
 
-            this.ScaleTo(1.1f, 250, Easing.InSine);
+            FilterControl.MoveToY(-120, 250, Easing.OutQuint);
+            FilterControl.FadeOut(100);
 
-            this.FadeOut(250);
+            LeftArea.MoveToX(-150, 1800, Easing.OutQuint);
+            LeftArea.FadeOut(200, Easing.OutQuint);
+
+            wedgeBackground.ScaleTo(2.4f, 400, Easing.OutQuint);
+
+            this.FadeOut(400, Easing.OutQuint);
 
             FilterControl.Deactivate();
             base.OnSuspending(e);

commit 8befcb538ed14bdf3844d0f283314f768081be62

Fix phantom beatmap sets appearing on carousel after delete/update
Regressed in c8a85fb1a19b2f92138105f3f0f18997c13f4e68.

As it turns out, `item.Item.Filtered.Value` is not the only condition
that should be checked to determine if a carousel item should be hidden
or not - `item.Item.State.Value != CarouselItemState.Collapsed` should
also be true. This was even available as the `item.Item.Visible`
convenience property, which is used in this commit.

Failing to check `item.Item.State.Value` led to setting non-zero alpha
on collapsed carousel items, leading to phantom beatmap sets appearing,
as the alpha was set in the entire carousel's `Update()` method, thus
firing every frame.

osu.Game/Screens/Select/BeatmapCarousel.cs
@@ -770,7 +770,7 @@ namespace osu.Game.Screens.Select
             {
                 updateItem(item);
 
-                if (!item.Item.Filtered.Value)
+                if (item.Item.Visible)
                 {
                     bool isSelected = item.Item.State.Value == CarouselItemState.Selected;
 