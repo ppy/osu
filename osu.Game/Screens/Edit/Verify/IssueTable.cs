6000e146681d2feb5423addf7dbbd8edeffa99e6
Jai Sharma <jai@jai.moe>
2022-11-27 03:08:54 +0000

    Correctly select issue row

--- osu.Game/Screens/Edit/Verify/IssueTable.cs
+++ osu.Game/Screens/Edit/Verify/IssueTable.cs
@@ -77,7 +77,7 @@ namespace osu.Game.Screens.Edit.Verify
             selectedIssue = verify.SelectedIssue.GetBoundCopy();
             selectedIssue.BindValueChanged(issue =>
             {
-                SetRowSelected(issue);
+                SetRowSelected(issue.NewValue);
             }, true);
         }
 
