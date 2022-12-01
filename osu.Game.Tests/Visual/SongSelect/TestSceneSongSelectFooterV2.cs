5435377ae52df93815997df66c33290bcc11c4b4
mk56-spn <87902114+bubb13gummy@users.noreply.github.com>
2022-12-01 11:57:50 +0100

    Create Basic V2 footer and test

---
+++ osu.Game.Tests/Visual/SongSelect/TestSceneSongSelectFooterV2.cs
@@ -0,0 +1,29 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Game.Screens.Select.FooterV2;
+
+namespace osu.Game.Tests.Visual.SongSelect
+{
+    public partial class TestSceneSongSelectFooterV2 : OsuManualInputManagerTestScene
+    {
+        [SetUp]
+        public void SetUp() => Schedule(() =>
+        {
+            FooterV2 footer;
+
+            Child = footer = new FooterV2
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            };
+        });
+
+        [Test]
+        public void TestBasic()
+        {
+        }
+    }
+}
