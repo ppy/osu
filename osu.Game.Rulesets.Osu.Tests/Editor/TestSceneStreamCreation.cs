a866a77574b6120d24cd829cbc0adf6478ec704f
OliBomby <olivierschipper@gmail.com>
2022-12-02 08:47:53 +0100

    add test for illegal stream

--- osu.Game.Rulesets.Osu.Tests/Editor/TestSceneStreamCreation.cs
+++ osu.Game.Rulesets.Osu.Tests/Editor/TestSceneStreamCreation.cs
@@ -114,6 +114,36 @@ namespace osu.Game.Rulesets.Osu.Tests.Editor
             assertPlaced(false);
         }
 
+        [Test]
+        public void TestCreateIllegalStream()
+        {
+            addMovementStep(new Vector2(210, 10));
+            addSeekStep(4);
+            addClickStep(MouseButton.Left);
+            addClickStep(MouseButton.Left);
+            addMovementStep(new Vector2(210, 210));
+            addSeekStep(2);
+            addClickStep(MouseButton.Right);
+            assertCircleCount(6);
+            assertControlPointTime(0, 0);
+            assertControlPointTime(1, 4);
+            assertControlPointTime(2, 5);
+            AddStep("get timeline blueprint", () => timelineBlueprint = Editor.ChildrenOfType<TimelineHitObjectBlueprint>().First());
+            addMoveToTimelineDragArea(0);
+            AddStep("start drag", () => InputManager.PressButton(MouseButton.Left));
+            AddStep("drag by 6 ticks", () => InputManager.MoveMouseTo(InputManager.CurrentState.Mouse.Position + new Vector2(80, 0)));
+            AddStep("end drag", () => InputManager.ReleaseButton(MouseButton.Left));
+            assertControlPointTime(1, 4);
+            assertControlPointTime(2, 5);
+            addMoveToTimelineDragArea(0);
+            AddStep("start drag", () => InputManager.PressButton(MouseButton.Left));
+            AddStep("drag by -6 ticks", () => InputManager.MoveMouseTo(InputManager.CurrentState.Mouse.Position - new Vector2(80, 0)));
+            AddStep("end drag", () => InputManager.ReleaseButton(MouseButton.Left));
+            assertControlPointTime(1, 1);
+            assertControlPointTime(2, 5);
+            assertCircleCount(6);
+        }
+
         private void addMovementStep(Vector2 position) => AddStep($"move mouse to {position}", () => InputManager.MoveMouseTo(playfield.ToScreenSpace(position)));
 
         private void addClickStep(MouseButton button) => AddStep($"click {button}", () => InputManager.Click(button));
