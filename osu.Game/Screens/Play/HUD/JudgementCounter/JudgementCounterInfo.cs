f798951f0974b49592d10b1f7959d6e0e0a8ce46
mk56-spn <darioheadleysilva@gmail.com>
2022-12-11 22:09:14 +0100

    Create Judgement Info struct

---
+++ osu.Game/Screens/Play/HUD/JudgementCounter/JudgementCounterInfo.cs
@@ -0,0 +1,16 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Framework.Localisation;
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Screens.Play.HUD.JudgementCounter
+{
+    public struct JudgementCounterInfo
+    {
+        public (HitResult Type, LocalisableString Displayname) ResultInfo { get; set; }
+
+        public BindableInt ResultCount { get; set; }
+    }
+}
