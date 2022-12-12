d6dd302d606deb57a545d04856ad8040390b8232
mk56-spn <darioheadleysilva@gmail.com>
2022-12-12 13:48:54 +0100

    Make preview use the converted ruleset

--- osu.Game/Overlays/Practice/PracticeOverlayComponents/PracticeGameplayPreview.cs
+++ osu.Game/Overlays/Practice/PracticeOverlayComponents/PracticeGameplayPreview.cs
@@ -1,12 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable disable
+using System;
 using System.Linq;
+using System.Threading;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Logging;
 using osu.Game.Beatmaps;
+using osu.Game.Rulesets;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.UI;
@@ -31,13 +36,22 @@ namespace osu.Game.Overlays.Practice.PracticeOverlayComponents
         private double startTime;
         private double lastTime;
 
+        private Ruleset ruleset;
+
+        [Resolved]
+        private Bindable<WorkingBeatmap> beatmap { get; set; } = null!;
+
+        [Resolved]
+        private Bindable<RulesetInfo> targetRuleset { get; set; } = null!;
+
         [BackgroundDependencyLoader]
-        private void load(IBindable<WorkingBeatmap> beatmap)
+        private void load()
         {
-            var currentRuleset = beatmap.Value.BeatmapInfo.Ruleset.CreateInstance();
+            var currentRuleset = targetRuleset.Value.CreateInstance();
             var modAutoplay = currentRuleset.RulesetInfo.CreateInstance().GetAutoplayMod();
 
-            var playableBeatmap = beatmap.Value.GetPlayableBeatmap(currentRuleset.RulesetInfo);
+            IBeatmap playableBeatmap = loadPlayableBeatmap(new Mod[] { modAutoplay! }, new CancellationToken());
+
             var rulesetSkinProvider = new RulesetSkinProvidingContainer(currentRuleset, playableBeatmap, beatmap.Value.Skin);
 
             InternalChild = gameplayClockContainer = new GameplayClockContainer(new FramedBeatmapClock());
@@ -58,6 +72,55 @@ namespace osu.Game.Overlays.Practice.PracticeOverlayComponents
 
             startTime = playableBeatmap.HitObjects.FirstOrDefault()!.StartTime;
             lastTime = playableBeatmap.HitObjects.LastOrDefault()!.GetEndTime();
+        }
+
+        private IBeatmap loadPlayableBeatmap(Mod[] gameplayMods, CancellationToken cancellationToken)
+        {
+            IBeatmap playable;
+
+            try
+            {
+                if (beatmap.Value.Beatmap == null)
+                    throw new InvalidOperationException("Beatmap was not loaded");
+
+                var rulesetInfo = targetRuleset.Value ?? beatmap.Value.BeatmapInfo.Ruleset;
+                ruleset = rulesetInfo.CreateInstance();
+
+                if (ruleset == null)
+                    throw new RulesetLoadException("Instantiation failure");
+
+                try
+                {
+                    playable = beatmap.Value.GetPlayableBeatmap(ruleset.RulesetInfo, gameplayMods, cancellationToken);
+                }
+                catch (BeatmapInvalidForRulesetException)
+                {
+                    // A playable beatmap may not be creatable with the user's preferred ruleset, so try using the beatmap's default ruleset
+                    rulesetInfo = beatmap.Value.BeatmapInfo.Ruleset;
+                    ruleset = rulesetInfo.CreateInstance();
+
+                    playable = beatmap.Value.GetPlayableBeatmap(rulesetInfo, gameplayMods, cancellationToken);
+                }
+
+                if (playable.HitObjects.Count == 0)
+                {
+                    Logger.Log("Beatmap contains no hit objects!", level: LogLevel.Error);
+                    return null;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Load has been cancelled. No logging is required.
+                return null;
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "Could not load beatmap successfully!");
+                //couldn't load, hard abort!
+                return null;
+            }
+
+            return playable;
         }
 
         public void SeekTo(double percent)
