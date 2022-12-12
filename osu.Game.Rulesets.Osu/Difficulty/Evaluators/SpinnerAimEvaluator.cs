2a972b4692184d319f2925229eab42d36a93c5e6
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2022-12-12 01:42:21 +0000

    Use clockrate-adjusted spinner duration and balance around it

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpinnerAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpinnerAimEvaluator.cs
@@ -2,7 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Extensions.ObjectExtensions;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Objects;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
@@ -11,15 +13,21 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     {
         public static double EvaluateDifficultyOf(DifficultyHitObject current, bool hasSpunOut)
         {
-            if (current.BaseObject is not Spinner spinner || hasSpunOut) return 0;
+            if (current.BaseObject is not Spinner spinner || hasSpunOut || spinner.Duration <= 0)
+                return 0;
 
-            if (spinner.Duration <= 0)
+            var osuCurrent = (OsuDifficultyHitObject)current;
+
+            if (osuCurrent.SpinnerDuration.IsNull())
                 return 0;
 
             double r = 0.05 / (0.00008 + Math.Max(0, (5000 - spinner.Duration) / (1000 * 2000)));
             double numerator = -spinner.Duration + Math.Sqrt(spinner.Duration * spinner.Duration - 40 * r * spinner.SpinsRequired);
             double denominator = -20 * r;
-            return (500000 * numerator / denominator) / spinner.Duration;
+
+            // This value is scaled by the spinner's overall duration to account
+            // for the fact that longer spinners are generally more lenient.
+            return (400000 * numerator / denominator) / osuCurrent.SpinnerDuration.Value;
         }
     }
 }
