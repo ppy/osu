8c67c32b15576ad787ecfdb850960bb55b25fd89
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2022-12-13 18:57:17 +0900

    Remove unused using statements

--- osu.Game.Rulesets.Catch/UI/CatchPlayfield.cs
+++ osu.Game.Rulesets.Catch/UI/CatchPlayfield.cs
@@ -3,14 +3,12 @@
 
 #nullable disable
 
-using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.Objects.Drawables;
 using osu.Game.Rulesets.Judgements;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.UI;
 using osu.Game.Rulesets.UI.Scrolling;
