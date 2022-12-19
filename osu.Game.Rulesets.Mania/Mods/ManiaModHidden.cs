87a29eb761d0b5bbd8ae748188febe2e6ef7a739
Dylan Nantz <87902114+bubb13gummy@users.noreply.github.com>
2022-12-19 18:31:28 -0500

    Add mania hidden mod configuration

--- osu.Game.Rulesets.Mania/Mods/ManiaModHidden.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModHidden.cs
@@ -4,7 +4,9 @@
 using System;
 using System.Linq;
 using osu.Framework.Localisation;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Mania.UI;
+using osu.Framework.Bindables;
 
 namespace osu.Game.Rulesets.Mania.Mods
 {
@@ -12,6 +14,16 @@ namespace osu.Game.Rulesets.Mania.Mods
     {
         public override LocalisableString Description => @"Keys fade out before you hit them!";
         public override double ScoreMultiplier => 1;
+
+        [SettingSource("Coverage", "How much of the playfield notes should be hidden for.")]
+        public BindableNumber<float> CoverageAmount { get; } = new BindableFloat(0.5f)
+        {
+            Precision = 0.01f,
+            MinValue = 0.2f,
+            MaxValue = 0.8f,
+            Default = 0.5f,
+        };
+        public override float Coverage => CoverageAmount.Value;
 
         public override Type[] IncompatibleMods => base.IncompatibleMods.Append(typeof(ManiaModFadeIn)).ToArray();
 
