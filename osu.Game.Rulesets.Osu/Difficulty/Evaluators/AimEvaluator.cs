8bbea1216ee7cf5ca8f7cabf0f617ebb07f7258c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-20 22:11:23 +0800

    Global aim buff

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -12,10 +12,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 {
     public static class AimEvaluator
     {
-        private const double wide_angle_multiplier = 1.5;
+        private const double wide_angle_multiplier = 1.65;
         private const double acute_angle_multiplier = 1.95;
-        private const double slider_multiplier = 1.35;
-        private const double velocity_change_multiplier = 0.75;
+        private const double slider_multiplier = 1.5;
+        private const double velocity_change_multiplier = 0.85;
         private const double min_speed_bonus = 150;
         private const double single_spacing_threshold = 175;
 
