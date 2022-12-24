91bde14fb345c034589738e8361fd5ad39df991a
Dean Herbert <pe@ppy.sh>
2022-12-24 15:42:22 +0800

    Add button to settings to show lazer upgrade guide

--- osu.Game/Overlays/Settings/Sections/GeneralSection.cs
+++ osu.Game/Overlays/Settings/Sections/GeneralSection.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
+using osu.Game.Graphics;
 using osu.Game.Localisation;
 using osu.Game.Overlays.Settings.Sections.General;
 
@@ -15,7 +14,10 @@ namespace osu.Game.Overlays.Settings.Sections
     public partial class GeneralSection : SettingsSection
     {
         [Resolved(CanBeNull = true)]
-        private FirstRunSetupOverlay firstRunSetupOverlay { get; set; }
+        private FirstRunSetupOverlay? firstRunSetupOverlay { get; set; }
+
+        [Resolved(CanBeNull = true)]
+        private OsuGame? game { get; set; }
 
         public override LocalisableString Header => GeneralSettingsStrings.GeneralSectionHeader;
 
@@ -24,14 +26,23 @@ namespace osu.Game.Overlays.Settings.Sections
             Icon = FontAwesome.Solid.Cog
         };
 
-        public GeneralSection()
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
         {
             Children = new Drawable[]
             {
                 new SettingsButton
                 {
                     Text = GeneralSettingsStrings.RunSetupWizard,
+                    TooltipText = FirstRunSetupOverlayStrings.FirstRunSetupDescription,
                     Action = () => firstRunSetupOverlay?.Show(),
+                },
+                new SettingsButton
+                {
+                    Text = "Learn more about lazer",
+                    TooltipText = "Check out the feature comparison and FAQ",
+                    BackgroundColour = colours.YellowDark,
+                    Action = () => game?.ShowWiki(@"Help_centre/Upgrading_to_lazer")
                 },
                 new LanguageSettings(),
                 new UpdateSettings(),
