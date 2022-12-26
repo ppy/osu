fcbb21c75eb35a1313f9e998bed1d335f09a40a2
Berkan Diler <berkan.diler1@ingka.ikea.com>
2022-12-26 20:38:35 +0100

    Fix typo

--- osu.Game/Beatmaps/Formats/LegacyBeatmapDecoder.cs
+++ osu.Game/Beatmaps/Formats/LegacyBeatmapDecoder.cs
@@ -218,7 +218,7 @@ namespace osu.Game.Beatmaps.Formats
                     break;
 
                 case @"Countdown":
-                    beatmap.BeatmapInfo.Countdown = Enum.Parse<CountdownType>(tpair.Value);
+                    beatmap.BeatmapInfo.Countdown = Enum.Parse<CountdownType>(pair.Value);
                     break;
 
                 case @"CountdownOffset":
