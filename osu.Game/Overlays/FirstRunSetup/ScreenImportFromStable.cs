d4b3965967ea155e94975409de1a5883c3981523
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-12-28 21:25:24 +0100

    Change warning message about file duplication
    - It was being glued in an ugly way that would have prevented sanely
      localising it.
    
    - Even on Linux, the filesystem (whichever one the user has chosen out
      of the multitude available) still needs to support hard links for them
      to have a chance of working.

--- osu.Game/Overlays/FirstRunSetup/ScreenImportFromStable.cs
+++ osu.Game/Overlays/FirstRunSetup/ScreenImportFromStable.cs
@@ -136,10 +136,9 @@ namespace osu.Game.Overlays.FirstRunSetup
                 copyInformation.Text = "Lightweight linking of files is not supported on your operating system yet, so a copy of all files will be made during import.";
             else
             {
-                string mentionNtfs = RuntimeInfo.OS == RuntimeInfo.Platform.Windows ? " (and the file system is NTFS)." : ".";
-
-                copyInformation.Text =
-                    $"A second copy of all files will be made during import. To avoid this, please make sure the lazer data folder is on the same drive as your previous osu! install{mentionNtfs}";
+                copyInformation.Text = RuntimeInfo.OS == RuntimeInfo.Platform.Windows
+                    ? "A second copy of all files will be made during import. To avoid this, please make sure the lazer data folder is on the same drive as your previous osu! install (and the file system is NTFS)."
+                    : "A second copy of all files will be made during import. To avoid this, please make sure the lazer data folder is on the same drive as your previous osu! install (and the file system supports hard links).";
                 copyInformation.AddLink(GeneralSettingsStrings.ChangeFolderLocation, () =>
                 {
                     game?.PerformFromScreen(menu => menu.Push(new MigrationSelectScreen()));
