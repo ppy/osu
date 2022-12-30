7f970f3cd846068bb775586f398e8fce73329089
Joseph Madamba <madamba.joehu@outlook.com>
2022-12-29 21:17:24 -0800

    Display nominators on beatmap set overlay

--- osu.Game/Online/API/Requests/Responses/APIBeatmapSet.cs
+++ osu.Game/Online/API/Requests/Responses/APIBeatmapSet.cs
@@ -111,6 +111,12 @@ namespace osu.Game.Online.API.Requests.Responses
         [JsonProperty(@"language")]
         public BeatmapSetOnlineLanguage Language { get; set; }
 
+        [JsonProperty(@"current_nominations")]
+        public BeatmapSetOnlineNominations[] CurrentNominations { get; set; } = Array.Empty<BeatmapSetOnlineNominations>();
+
+        [JsonProperty(@"related_users")]
+        public APIUser[] RelatedUsers { get; set; } = Array.Empty<APIUser>();
+
         public string Source { get; set; } = string.Empty;
 
         [JsonProperty(@"tags")]
