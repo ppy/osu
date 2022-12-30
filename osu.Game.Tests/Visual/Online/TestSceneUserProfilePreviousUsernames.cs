7d75b7ba3651e041d258ca7a238afabbc5ad337b
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2022-12-30 15:05:04 +0100

    Enable NRT in user profile overlay test scenes

--- osu.Game.Tests/Visual/Online/TestSceneUserProfilePreviousUsernames.cs
+++ osu.Game.Tests/Visual/Online/TestSceneUserProfilePreviousUsernames.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System;
 using NUnit.Framework;
 using osu.Framework.Graphics;
@@ -14,7 +12,7 @@ namespace osu.Game.Tests.Visual.Online
     [TestFixture]
     public partial class TestSceneUserProfilePreviousUsernames : OsuTestScene
     {
-        private PreviousUsernames container;
+        private PreviousUsernames container = null!;
 
         [SetUp]
         public void SetUp() => Schedule(() =>
@@ -50,7 +48,7 @@ namespace osu.Game.Tests.Visual.Online
             AddUntilStep("Is hidden", () => container.Alpha == 0);
         }
 
-        private static readonly APIUser[] users =
+        private static readonly APIUser?[] users =
         {
             new APIUser { Id = 1, PreviousUsernames = new[] { "username1" } },
             new APIUser { Id = 2, PreviousUsernames = new[] { "longusername", "longerusername" } },
