a8f4f0042179cc20045b9b66f77e6583fec7a2f3
OpenSauce <opensauce04@gmail.com>
2023-01-10 12:04:06 +0000

    Removed nullable

--- osu.Game.Rulesets.Taiko/UI/DrumTouchInputArea.cs
+++ osu.Game.Rulesets.Taiko/UI/DrumTouchInputArea.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
 #pragma warning disable IDE0001 // Simplify Names
 
 using System;
