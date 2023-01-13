e07c0c7c1fc215b24c16f82c4d015235ea2714f6
StanR <hi@stanr.info>
2023-01-14 02:58:18 +0300

    Using

--- osu.Game/Overlays/Login/UserDropdown.cs
+++ osu.Game/Overlays/Login/UserDropdown.cs
@@ -7,7 +7,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Effects;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Game.Graphics;
 using osu.Game.Graphics.UserInterface;
