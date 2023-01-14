97ecb6609828ac6daf311e87ab96595d05b960cb
StanR <hi@stanr.info>
2023-01-14 16:25:57 +0300

    Using

--- osu.Game/Users/UserGridPanel.cs
+++ osu.Game/Users/UserGridPanel.cs
@@ -10,7 +10,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Overlays.Profile;
