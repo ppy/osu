9ce7c51b149401e3988eafbbe8461de7a86f1993
ansel <79257300125@ya.ru>
2023-01-15 23:29:58 +0300

    Localise osu! settings

--- osu.Game/Localisation/RulesetSettingsStrings.cs
+++ osu.Game/Localisation/RulesetSettingsStrings.cs
@@ -15,6 +15,26 @@ namespace osu.Game.Localisation
         public static LocalisableString Rulesets => new TranslatableString(getKey(@"rulesets"), @"Rulesets");
 
         /// <summary>
+        /// "Snaking in sliders"
+        /// </summary>
+        public static LocalisableString SnakingInSliders => new TranslatableString(getKey(@"snaking_in_sliders"), @"Snaking in sliders");
+
+        /// <summary>
+        /// "Snaking out sliders"
+        /// </summary>
+        public static LocalisableString SnakingOutSliders => new TranslatableString(getKey(@"snaking_out_sliders"), @"Snaking out sliders");
+
+        /// <summary>
+        /// "Cursor trail"
+        /// </summary>
+        public static LocalisableString CursorTrail => new TranslatableString(getKey(@"cursor_trail"), @"Cursor trail");
+
+        /// <summary>
+        /// "Playfield border style"
+        /// </summary>
+        public static LocalisableString PlayfieldBorderStyle => new TranslatableString(getKey(@"playfield_border_style"), @"Playfield border style");
+
+        /// <summary>
         /// "None"
         /// </summary>
         public static LocalisableString BorderNone => new TranslatableString(getKey(@"no_borders"), @"None");
