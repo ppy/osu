0ff143d4c86d7e6ae19f703201046cab5592c01a
Joseph Madamba <madamba.joehu@outlook.com>
2023-01-15 15:19:06 -0800

    Add argument for play some ruleset string

--- osu.Game/Overlays/Toolbar/ToolbarRulesetTabButton.cs
+++ osu.Game/Overlays/Toolbar/ToolbarRulesetTabButton.cs
@@ -7,7 +7,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
-using osu.Framework.Localisation;
 using osu.Game.Localisation;
 using osu.Game.Rulesets;
 using osuTK.Graphics;
@@ -31,7 +30,7 @@ namespace osu.Game.Overlays.Toolbar
             var rInstance = value.CreateInstance();
 
             ruleset.TooltipMain = rInstance.Description;
-            ruleset.TooltipSub = LocalisableString.Format("{0} {1}", ToolbarStrings.RulesetHeaderDescription, ($"{rInstance.Description}"));
+            ruleset.TooltipSub = ToolbarStrings.PlaySomeRuleset(rInstance.Description);
             ruleset.SetIcon(rInstance.CreateIcon());
         }
 
