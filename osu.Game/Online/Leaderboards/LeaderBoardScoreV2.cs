b7584b4a02471c5d8894f30189da61075b8e46ec
mk56-spn <darioheadleysilva@gmail.com>
2023-01-16 17:15:37 +0100

    Add grid container with sections, add background colours and hover logic

--- osu.Game/Online/Leaderboards/LeaderBoardScoreV2.cs
+++ osu.Game/Online/Leaderboards/LeaderBoardScoreV2.cs
@@ -5,23 +5,43 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Events;
 using osu.Game.Graphics.Containers;
+using osu.Game.Overlays;
 using osuTK;
 
 namespace osu.Game.Online.Leaderboards
 {
     public partial class LeaderBoardScoreV2 : OsuClickableContainer
     {
+        private readonly bool isPersonalBest;
         private const int HEIGHT = 60;
         private const int corner_radius = 10;
+        private const int transition_duration = 200;
+
+        private Colour4 foregroundColour;
+        private Colour4 backgroundColour;
 
         private static readonly Vector2 shear = new Vector2(0.15f, 0);
 
+        [Cached]
+        private OverlayColourProvider colourProvider { get; set; } = new OverlayColourProvider(OverlayColourScheme.Aquamarine);
+
         private Container content = null!;
+        private Box background = null!;
+        private Box foreground = null!;
+
+        public LeaderBoardScoreV2(bool isPersonalBest = false)
+        {
+            this.isPersonalBest = isPersonalBest;
+        }
 
         [BackgroundDependencyLoader]
         private void load()
         {
+            foregroundColour = isPersonalBest ? colourProvider.Background1 : colourProvider.Background5;
+            backgroundColour = isPersonalBest ? colourProvider.Background2 : colourProvider.Background4;
+
             Shear = shear;
             RelativeSizeAxes = Axes.X;
             Height = HEIGHT;
@@ -32,12 +52,68 @@ namespace osu.Game.Online.Leaderboards
                 RelativeSizeAxes = Axes.Both,
                 Children = new Drawable[]
                 {
-                    new Box
+                    background = new Box
                     {
                         RelativeSizeAxes = Axes.Both,
+                        Colour = backgroundColour
+                    },
+                    new GridContainer
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        ColumnDimensions = new[]
+                        {
+                            new Dimension(GridSizeMode.Absolute, 65),
+                            new Dimension(),
+                            new Dimension(GridSizeMode.Absolute, 176)
+                        },
+                        Content = new[]
+                        {
+                            new[]
+                            {
+                                Empty(),
+                                createCentreContent(),
+                                Empty(),
+                            }
+                        }
                     }
                 }
             };
+        }
+
+        private Container createCentreContent() =>
+            new Container
+            {
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreLeft,
+                Masking = true,
+                CornerRadius = corner_radius,
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    foreground = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = foregroundColour
+                    }
+                }
+            };
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            updateState();
+            return base.OnHover(e);
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            updateState();
+            base.OnHoverLost(e);
+        }
+
+        private void updateState()
+        {
+            foreground.FadeColour(IsHovered ? foregroundColour.Lighten(0.2f) : foregroundColour, transition_duration, Easing.OutQuint);
+            background.FadeColour(IsHovered ? backgroundColour.Lighten(0.2f) : backgroundColour, transition_duration, Easing.OutQuint);
         }
     }
 }
