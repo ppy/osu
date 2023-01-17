c5367a8e12c45fa0e692b91e0de550f53d5a1317
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-01-17 19:11:22 +0900

    Remove unnecessary touch interception from `OsuInputManager`

--- osu.Game.Rulesets.Osu/OsuInputManager.cs
+++ osu.Game.Rulesets.Osu/OsuInputManager.cs
@@ -60,19 +60,6 @@ namespace osu.Game.Rulesets.Osu
             return base.Handle(e);
         }
 
-        protected override bool HandleMouseTouchStateChange(TouchStateChangeEvent e)
-        {
-            if (!AllowUserCursorMovement)
-            {
-                // Still allow for forwarding of the "touch" part, but replace the positional data with that of the mouse.
-                // Primarily relied upon by the "autopilot" osu! mod.
-                var touch = new Touch(e.Touch.Source, CurrentState.Mouse.Position);
-                e = new TouchStateChangeEvent(e.State, e.Input, touch, e.IsActive, null);
-            }
-
-            return base.HandleMouseTouchStateChange(e);
-        }
-
         private partial class OsuKeyBindingContainer : RulesetKeyBindingContainer
         {
             private bool allowGameplayInputs = true;
