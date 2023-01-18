a0c1e9cee636595e4615476939fc74bf320c15b4
Wleter <87902114+bubb13gummy@users.noreply.github.com>
2023-01-18 21:34:23 +0100

    add collection of selection points.

--- osu.Game/Rulesets/Edit/HitObjectSelectionBlueprint.cs
+++ osu.Game/Rulesets/Edit/HitObjectSelectionBlueprint.cs
@@ -48,8 +48,6 @@ namespace osu.Game.Rulesets.Edit
 
         public override Vector2 ScreenSpaceSelectionPoint => DrawableObject.ScreenSpaceDrawQuad.Centre;
 
-        public override Vector2 ScreenSpaceEndPoint => DrawableObject.ScreenSpaceDrawQuad.Centre;
-
         public override Quad SelectionQuad => DrawableObject.ScreenSpaceDrawQuad;
     }
 
