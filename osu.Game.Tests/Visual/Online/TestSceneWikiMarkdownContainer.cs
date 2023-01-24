4d223b62c5692fce41e9bc4b0281384d686b3b68
Jai Sharma <87902114+bubb13gummy@users.noreply.github.com>
2023-01-24 00:51:07 +0000

    TestSceneWikiMarkdowContainer: Update image paths

--- osu.Game.Tests/Visual/Online/TestSceneWikiMarkdownContainer.cs
+++ osu.Game.Tests/Visual/Online/TestSceneWikiMarkdownContainer.cs
@@ -122,8 +122,8 @@ needs_cleanup: true
         {
             AddStep("Add absolute image", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh";
-                markdownContainer.Text = "![intro](/wiki/Interface/img/intro-screen.jpg)";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/images/Client/Interface/";
+                markdownContainer.Text = "![intro](img/intro-screen.jpg)";
             });
         }
 
@@ -132,7 +132,7 @@ needs_cleanup: true
         {
             AddStep("Add relative image", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/Interface/";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/images/Client/Interface/";
                 markdownContainer.Text = "![intro](img/intro-screen.jpg)";
             });
         }
@@ -142,7 +142,7 @@ needs_cleanup: true
         {
             AddStep("Add paragraph with block image", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/Interface/";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/images/Client/Interface/";
                 markdownContainer.Text = @"Line before image
 
 ![play menu](img/play-menu.jpg ""Main Menu in osu!"")
@@ -156,8 +156,8 @@ Line after image";
         {
             AddStep("Add inline image", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh";
-                markdownContainer.Text = "![osu! mode icon](/wiki/shared/mode/osu.png) osu!";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/shared/";
+                markdownContainer.Text = "![osu! mode icon](mode/osu.png) osu!";
             });
         }
 
@@ -166,16 +166,16 @@ Line after image";
         {
             AddStep("Add Table", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/images/shared/judgement/";
                 markdownContainer.Text = @"
 | Image | Name | Effect |
 | :-: | :-: | :-- |
-| ![](/wiki/Skinning/Interface/img/hit300.png ""300"") | 300 | A possible score when tapping a hit circle precisely on time, completing a Slider and keeping the cursor over every tick, or completing a Spinner with the Spinner Metre full. A score of 300 appears in an blue score by default. Scoring nothing except 300s in a beatmap will award the player with the SS or SSH grade. |
-| ![](/wiki/Skinning/Interface/img/hit300g.png ""Geki"") | (激) Geki | A term from Ouendan, called Elite Beat! in EBA. Appears when playing the last element in a combo in which the player has scored only 300s. Getting a Geki will give a sizable boost to the Life Bar. By default, it is blue. |
-| ![](/wiki/Skinning/Interface/img/hit100.png ""100"") | 100 | A possible score one can get when tapping a Hit Object slightly late or early, completing a Slider and missing a number of ticks, or completing a Spinner with the Spinner Meter almost full. A score of 100 appears in a green score by default. When very skilled players test a beatmap and they get a lot of 100s, this may mean that the beatmap does not have correct timing. |
-| ![](/wiki/Skinning/Interface/img/hit300k.png ""300 Katu"") ![](/wiki/Skinning/Interface/img/hit100k.png ""100 Katu"") | (喝) Katu or Katsu | A term from Ouendan, called Beat! in EBA. Appears when playing the last element in a combo in which the player has scored at least one 100, but no 50s or misses. Getting a Katu will give a small boost to the Life Bar. By default, it is coloured green or blue depending on whether the Katu itself is a 100 or a 300. |
-| ![](/wiki/Skinning/Interface/img/hit50.png ""50"") | 50 | A possible score one can get when tapping a hit circle rather early or late but not early or late enough to cause a miss, completing a Slider and missing a lot of ticks, or completing a Spinner with the Spinner Metre close to full. A score of 50 appears in a orange score by default. Scoring a 50 in a combo will prevent the appearance of a Katu or a Geki at the combo's end. |
-| ![](/wiki/Skinning/Interface/img/hit0.png ""Miss"") | Miss | A possible score one can get when not tapping a hit circle or too early (based on OD and AR, it may *shake* instead), not tapping or holding the Slider at least once, or completing a Spinner with low Spinner Metre fill. Scoring a Miss will reset the current combo to 0 and will prevent the appearance of a Katu or a Geki at the combo's end. |
+| ![](osu!/hit300.png ""300"") | 300 | A possible score when tapping a hit circle precisely on time, completing a Slider and keeping the cursor over every tick, or completing a Spinner with the Spinner Metre full. A score of 300 appears in an blue score by default. Scoring nothing except 300s in a beatmap will award the player with the SS or SSH grade. |
+| ![](osu!/hit300g.png ""Geki"") | (激) Geki | A term from Ouendan, called Elite Beat! in EBA. Appears when playing the last element in a combo in which the player has scored only 300s. Getting a Geki will give a sizable boost to the Life Bar. By default, it is blue. |
+| ![](osu!/hit100.png ""100"") | 100 | A possible score one can get when tapping a Hit Object slightly late or early, completing a Slider and missing a number of ticks, or completing a Spinner with the Spinner Meter almost full. A score of 100 appears in a green score by default. When very skilled players test a beatmap and they get a lot of 100s, this may mean that the beatmap does not have correct timing. |
+| ![](osu!/hit300k.png ""300 Katu"") ![](/wiki/Skinning/Interface/img/hit100k.png ""100 Katu"") | (喝) Katu or Katsu | A term from Ouendan, called Beat! in EBA. Appears when playing the last element in a combo in which the player has scored at least one 100, but no 50s or misses. Getting a Katu will give a small boost to the Life Bar. By default, it is coloured green or blue depending on whether the Katu itself is a 100 or a 300. |
+| ![](osu!/hit50.png ""50"") | 50 | A possible score one can get when tapping a hit circle rather early or late but not early or late enough to cause a miss, completing a Slider and missing a lot of ticks, or completing a Spinner with the Spinner Metre close to full. A score of 50 appears in a orange score by default. Scoring a 50 in a combo will prevent the appearance of a Katu or a Geki at the combo's end. |
+| ![](osu!/hit0.png ""Miss"") | Miss | A possible score one can get when not tapping a hit circle or too early (based on OD and AR, it may *shake* instead), not tapping or holding the Slider at least once, or completing a Spinner with low Spinner Metre fill. Scoring a Miss will reset the current combo to 0 and will prevent the appearance of a Katu or a Geki at the combo's end. |
 ";
             });
         }
@@ -185,7 +185,7 @@ Line after image";
         {
             AddStep("Add image", () =>
             {
-                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/osu!_Program_Files/";
+                markdownContainer.CurrentPath = "https://dev.ppy.sh/wiki/images/Client/Program_files/";
                 markdownContainer.Text = "![](img/file_structure.jpg \"The file structure of osu!'s installation folder, on Windows and macOS\")";
             });
 
