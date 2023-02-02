26efb8e8423ccc7e4aa559d2aea28090d4fc3b61
Dean Herbert <pe@ppy.sh>
2023-02-02 18:33:31 +0900

    Replace white box legacy placeholder with something that visually fits

--- osu.Game/Overlays/SkinEditor/SkinBlueprintContainer.cs
+++ osu.Game/Overlays/SkinEditor/SkinBlueprintContainer.cs
@@ -8,11 +8,15 @@ using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
-using osu.Framework.Screens;
 using osu.Framework.Testing;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Rulesets.Edit;
-using osu.Game.Screens;
 using osu.Game.Screens.Edit.Compose.Components;
 using osu.Game.Skinning;
 using osuTK;
@@ -45,9 +49,7 @@ namespace osu.Game.Overlays.SkinEditor
 
             if (targetContainers.Length == 0)
             {
-                string targetScreen = target.ChildrenOfType<Screen>().LastOrDefault()?.GetType().Name ?? "this screen";
-
-                AddInternal(new ScreenWhiteBox.UnderConstructionMessage(targetScreen, "doesn't support skin customisation just yet."));
+                AddInternal(new NonSkinnableScreenPlaceholder());
                 return;
             }
 
@@ -157,6 +159,66 @@ namespace osu.Game.Overlays.SkinEditor
 
             foreach (var list in targetComponents)
                 list.UnbindAll();
+        }
+
+        public partial class NonSkinnableScreenPlaceholder : CompositeDrawable
+        {
+            [Resolved]
+            private SkinEditorOverlay? skinEditorOverlay { get; set; }
+
+            [BackgroundDependencyLoader]
+            private void load(OverlayColourProvider colourProvider)
+            {
+                RelativeSizeAxes = Axes.Both;
+
+                InternalChildren = new Drawable[]
+                {
+                    new Box
+                    {
+                        Colour = colourProvider.Dark6,
+                        RelativeSizeAxes = Axes.Both,
+                        Alpha = 0.95f,
+                    },
+                    new FillFlowContainer
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+                        Spacing = new Vector2(0, 5),
+                        Direction = FillDirection.Vertical,
+                        Children = new Drawable[]
+                        {
+                            new SpriteIcon
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                Icon = FontAwesome.Solid.ExclamationCircle,
+                                Size = new Vector2(24),
+                                Y = -5,
+                            },
+                            new OsuTextFlowContainer(t => t.Font = OsuFont.Default.With(weight: FontWeight.SemiBold, size: 18))
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                TextAnchor = Anchor.Centre,
+                                RelativeSizeAxes = Axes.X,
+                                AutoSizeAxes = Axes.Y,
+                                Text = "Please navigate to a skinnable screen using the scene library",
+                            },
+                            new RoundedButton
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                Width = 200,
+                                Margin = new MarginPadding { Top = 20 },
+                                Action = () => skinEditorOverlay?.Hide(),
+                                Text = "Return to game"
+                            }
+                        }
+                    },
+                };
+            }
         }
     }
 }
