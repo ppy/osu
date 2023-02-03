d06b2a02391548f47fb6e9af8a1f2246f6798978
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-02-03 18:53:22 +0900

    Add a basic change handler to the skin editor

---
+++ osu.Game/Overlays/SkinEditor/SkinEditorChangeHandler.cs
@@ -0,0 +1,73 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using osu.Framework.Graphics;
+using osu.Framework.Testing;
+using osu.Game.Extensions;
+using osu.Game.Screens.Edit;
+using osu.Game.Screens.Play.HUD;
+using osu.Game.Skinning;
+
+namespace osu.Game.Overlays.SkinEditor
+{
+    public partial class SkinEditorChangeHandler : EditorChangeHandler
+    {
+        private readonly Drawable targetScreen;
+
+        private ISkinnableTarget? firstTarget => targetScreen.ChildrenOfType<ISkinnableTarget>().FirstOrDefault();
+
+        public SkinEditorChangeHandler(Drawable targetScreen)
+        {
+            // To keep things simple, we are currently only handling the current target screen for undo / redo.
+            // In the future we'll want this to cover all changes, even to skin's `InstantiationInfo`.
+            // We'll also need to consider cases where multiple targets are on screen at the same time.
+
+            this.targetScreen = targetScreen;
+
+            // Save initial state.
+            SaveState();
+        }
+
+        protected override void WriteCurrentStateToStream(MemoryStream stream)
+        {
+            if (firstTarget == null)
+                return;
+
+            var skinnableInfos = firstTarget.CreateSkinnableInfo().ToArray();
+            string json = JsonConvert.SerializeObject(skinnableInfos, new JsonSerializerSettings { Formatting = Formatting.Indented });
+            stream.Write(Encoding.UTF8.GetBytes(json));
+        }
+
+        protected override void ApplyStateChange(byte[] previousState, byte[] newState)
+        {
+            if (firstTarget == null)
+                return;
+
+            var deserializedContent = JsonConvert.DeserializeObject<IEnumerable<SkinnableInfo>>(Encoding.UTF8.GetString(newState));
+
+            if (deserializedContent == null)
+                return;
+
+            SkinnableInfo[] skinnableInfo = deserializedContent.ToArray();
+            Drawable[] targetComponents = firstTarget.Components.OfType<Drawable>().ToArray();
+
+            if (!skinnableInfo.Select(s => s.Type).SequenceEqual(targetComponents.Select(d => d.GetType())))
+            {
+                // Perform a naive full reload for now.
+                firstTarget.Reload(skinnableInfo);
+            }
+            else
+            {
+                int i = 0;
+
+                foreach (var drawable in targetComponents)
+                    drawable.ApplySkinnableInfo(skinnableInfo[i++]);
+            }
+        }
+    }
+}
