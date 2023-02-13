f57d29b069f8f3dfc25e4118b7feebeed7878d7f
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-02-13 17:43:52 +0900

    Add comment regarding unused enum members in `LegacyNoteBodyStyle`

--- osu.Game/Skinning/LegacyNoteBodyStyle.cs
+++ osu.Game/Skinning/LegacyNoteBodyStyle.cs
@@ -6,10 +6,12 @@ namespace osu.Game.Skinning
     public enum LegacyNoteBodyStyle
     {
         Stretch = 0,
+
+        // listed as the default on https://osu.ppy.sh/wiki/en/Skinning/skin.ini, but is seemingly not according to the source.
+        // Repeat = 1,
+
         RepeatTop = 2,
         RepeatBottom = 3,
         RepeatTopAndBottom = 4,
-        //Repeat = 1,
-        //RepeatMiddle = 5,
     }
 }
