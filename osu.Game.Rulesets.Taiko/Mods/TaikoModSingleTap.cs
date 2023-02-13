17a05de107f15a3336b3410ce87c4c276e22f2ac
OpenSauce04 <opensauce04@gmail.com>
2023-02-13 21:25:52 +0000

    Initial Taiko single tap implementation (still needs code to allow for Strong hits)

---
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModSingleTap.cs
@@ -0,0 +1,16 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Taiko.Mods
+{
+    public class TaikoModSingleTap : InputBlockingMod
+    {
+        public override string Name => @"Single Tap";
+        public override string Acronym => @"SG";
+        public override LocalisableString Description => @"You must only use one key!";
+
+        protected override bool CheckValidNewAction(TaikoAction action) => LastAcceptedDonAction == null || LastAcceptedDonAction == action || LastAcceptedKatAction == null || LastAcceptedKatAction == action;
+    }
+}
