6d44c94df0868f57d1fb586a6e3e3520dadc0fc1
Andrei Zavatski <megaman9919@gmail.com>
2023-02-13 14:31:54 +0300

    Rework LetterboxOverlay to use shader

--- osu.Game/Screens/Play/Break/LetterboxOverlay.cs
+++ osu.Game/Screens/Play/Break/LetterboxOverlay.cs
@@ -1,52 +1,74 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osuTK.Graphics;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shaders;
+using osuTK;
 
 namespace osu.Game.Screens.Play.Break
 {
-    public partial class LetterboxOverlay : CompositeDrawable
+    public partial class LetterboxOverlay : Drawable
     {
         private const int height = 350;
-
-        private static readonly Color4 transparent_black = new Color4(0, 0, 0, 0);
 
         public LetterboxOverlay()
         {
             RelativeSizeAxes = Axes.Both;
-            InternalChildren = new Drawable[]
+        }
+
+        private IShader shader = null!;
+
+        [BackgroundDependencyLoader]
+        private void load(ShaderManager shaders)
+        {
+            shader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, "LetterboxOverlay");
+        }
+
+        protected override DrawNode CreateDrawNode() => new LetterboxOverlayDrawNode(this);
+
+        protected class LetterboxOverlayDrawNode : DrawNode
+        {
+            public new LetterboxOverlay Source => (LetterboxOverlay)base.Source;
+
+            public LetterboxOverlayDrawNode(LetterboxOverlay source)
+                : base(source)
             {
-                new Container
-                {
-                    Anchor = Anchor.TopLeft,
-                    Origin = Anchor.TopLeft,
-                    RelativeSizeAxes = Axes.X,
-                    Height = height,
-                    Child = new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = ColourInfo.GradientVertical(Color4.Black, transparent_black),
-                    }
-                },
-                new Container
-                {
-                    Anchor = Anchor.BottomLeft,
-                    Origin = Anchor.BottomLeft,
-                    RelativeSizeAxes = Axes.X,
-                    Height = height,
-                    Child = new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = ColourInfo.GradientVertical(transparent_black, Color4.Black),
-                    }
-                }
-            };
+            }
+
+            private IShader shader = null!;
+            private float relativeHeight;
+            private Vector2 drawSize;
+
+            public override void ApplyState()
+            {
+                base.ApplyState();
+
+                shader = Source.shader;
+                drawSize = Source.DrawSize;
+                relativeHeight = height / Source.DrawHeight;
+            }
+
+            public override void Draw(IRenderer renderer)
+            {
+                base.Draw(renderer);
+
+                shader.Bind();
+                shader.GetUniform<float>("relativeHeight").UpdateValue(ref relativeHeight);
+
+                var drawQuad = new Quad(
+                    Vector2Extensions.Transform(Vector2.Zero, DrawInfo.Matrix),
+                    Vector2Extensions.Transform(new Vector2(drawSize.X, 0f), DrawInfo.Matrix),
+                    Vector2Extensions.Transform(new Vector2(0f, drawSize.Y), DrawInfo.Matrix),
+                    Vector2Extensions.Transform(drawSize, DrawInfo.Matrix)
+                );
+
+                renderer.DrawQuad(renderer.WhitePixel, drawQuad, DrawColourInfo.Colour);
+
+                shader.Unbind();
+            }
         }
     }
 }
