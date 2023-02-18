309e9b24e28135dc9ef9e341292ddb7137efaa33
cdwcgt <cdwcgt@cdwcgt.top>
2023-02-19 01:18:27 +0900

    split LegacyArchiveExporter

--- osu.Game/Database/LegacyModelExporter.cs
+++ osu.Game/Database/LegacyModelExporter.cs
@@ -13,9 +13,6 @@ using osu.Game.Extensions;
 using osu.Game.Overlays.Notifications;
 using osu.Game.Utils;
 using Realms;
-using SharpCompress.Common;
-using SharpCompress.Writers;
-using SharpCompress.Writers.Zip;
 
 namespace osu.Game.Database
 {
@@ -134,62 +131,5 @@ namespace osu.Game.Database
         /// <param name="notification">The notification will displayed to the user</param>
         /// <param name="cancellationToken">The Cancellation token that can cancel the exporting.</param>
         protected abstract void ExportToStream(TModel model, Stream outputStream, ProgressNotification notification, CancellationToken cancellationToken = default);
-    }
-
-    public abstract class LegacyArchiveExporter<TModel> : LegacyModelExporter<TModel>
-        where TModel : RealmObject, IHasNamedFiles, IHasGuidPrimaryKey
-    {
-        protected LegacyArchiveExporter(Storage storage, RealmAccess realm)
-            : base(storage, realm)
-        {
-        }
-
-        protected override void ExportToStream(TModel model, Stream outputStream, ProgressNotification notification, CancellationToken cancellationToken = default)
-            => exportZipArchive(model, outputStream, notification, cancellationToken);
-
-        /// <summary>
-        /// Exports an item to Stream as a legacy (.zip based) package.
-        /// </summary>
-        /// <param name="model">The model will be exported.</param>
-        /// <param name="outputStream">The output stream to export to.</param>
-        /// <param name="notification">The notification will displayed to the user</param>
-        /// <param name="cancellationToken">The Cancellation token that can cancel the exporting.</param>
-        private void exportZipArchive(TModel model, Stream outputStream, ProgressNotification notification, CancellationToken cancellationToken = default)
-        {
-            using var writer = new ZipWriter(outputStream, new ZipWriterOptions(CompressionType.Deflate));
-
-            float i = 0;
-            bool fileMissing = false;
-
-            foreach (var file in model.Files)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                using (var stream = UserFileStorage.GetStream(file.File.GetStoragePath()))
-                {
-                    // Sometimes we cannot find the file(probably deleted by the user), so we handle this and post a error.
-                    if (stream == null)
-                    {
-                        // Only pop up once to prevent spam.
-                        if (!fileMissing)
-                        {
-                            PostNotification?.Invoke(new SimpleErrorNotification
-                            {
-                                Text = "Some of your files are missing, they will not be included in the archive"
-                            });
-                            fileMissing = true;
-                        }
-                    }
-                    else
-                    {
-                        writer.Write(file.Filename, stream);
-                    }
-                }
-
-                i++;
-                notification.Progress = i / model.Files.Count();
-                notification.Text = $"Exporting... ({i}/{model.Files.Count()})";
-            }
-        }
     }
 }
