73f5b98100f289d7d4c9beab9a4e327c91c90299
Natelytle <87902114+bubb13gummy@users.noreply.github.com>
2023-02-22 21:03:02 -0500

    Change accuracy scaling

--- osu.Game.Rulesets.Taiko/Difficulty/TaikoPerformanceCalculator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/TaikoPerformanceCalculator.cs
@@ -107,7 +107,7 @@ namespace osu.Game.Rulesets.Taiko.Difficulty
             if (attributes.GreatHitWindow <= 0 || estimatedUr == null)
                 return 0;
 
-            double accuracyValue = Math.Pow(75 / estimatedUr.Value, 1.1) * Math.Pow(attributes.StarRating / 2.7, 0.8) * 100.0;
+            double accuracyValue = Math.Pow(60 / estimatedUr.Value, 1.1) * Math.Pow(attributes.StarRating, 0.4) * 100.0;
 
             double lengthBonus = Math.Min(1.15, Math.Pow(totalHits / 1500.0, 0.3));
 
