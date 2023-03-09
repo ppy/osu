f4e262040275e4466f4e43def09980a6a1b6d432
cdwcgt <cdwcgt@cdwcgt.top>
2023-03-10 00:56:22 +0900

    fix test

--- osu.Game/Overlays/Mods/EditPresetPopover.cs
+++ osu.Game/Overlays/Mods/EditPresetPopover.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
