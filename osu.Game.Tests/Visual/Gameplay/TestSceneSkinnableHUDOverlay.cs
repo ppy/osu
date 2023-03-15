f86f97d286d7108525ea3a0e39cb2183dd044e53
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-03-15 18:01:22 +0900

    Fix super-dodgy cast of `IEnumerable` to `Drawable`

--- osu.Game.Tests/Visual/Gameplay/TestSceneSkinnableHUDOverlay.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneSkinnableHUDOverlay.cs
@@ -42,7 +42,7 @@ namespace osu.Game.Tests.Visual.Gameplay
 
         // best way to check without exposing.
         private Drawable hideTarget => hudOverlay.KeyCounter;
-        private Drawable keyCounterFlow => (Drawable)hudOverlay.KeyCounter.Counters;
+        private Drawable keyCounterFlow => hudOverlay.KeyCounter.ChildrenOfType<FillFlowContainer<DefaultKeyCounter>>().Single();
 
         [Test]
         public void TestComboCounterIncrementing()
