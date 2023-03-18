3567e48c1d2ebff9c1f0ab9998b64f33b24e0ac8
Elvendir <87902114+bubb13gummy@users.noreply.github.com>
2023-03-18 17:53:41 +0100

    adddede LastPlayed as filter option in beatmap carousel

--- osu.Game/Screens/Select/FilterCriteria.cs
+++ osu.Game/Screens/Select/FilterCriteria.cs
@@ -31,6 +31,7 @@ namespace osu.Game.Screens.Select
         public OptionalRange<double> BPM;
         public OptionalRange<int> BeatDivisor;
         public OptionalRange<BeatmapOnlineStatus> OnlineStatus;
+        public OptionalRange<DateTimeOffset> LastPlayed;
         public OptionalTextFilter Creator;
         public OptionalTextFilter Artist;
 
