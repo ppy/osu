cf084702d1119e554c166563963c95f790dcdda3
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-03-20 14:01:35 +0900

    Remove unused using statement

--- osu.Desktop/OsuGameDesktop.cs
+++ osu.Desktop/OsuGameDesktop.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
