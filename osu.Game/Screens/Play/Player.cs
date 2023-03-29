1b248b0122451c3495c307a028abd29fb3332a95
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-03-29 14:11:56 +0900

    Add assert ensuring we don't ever get to the results screen with an F rank
    Intentionally an assertion as I want tests to fail, but I don't want
    this to cause crashes for an end user if it does happen to occur.

--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -788,6 +788,8 @@ namespace osu.Game.Screens.Play
                     // This player instance may already be in the process of exiting.
                     return;
 
+                Debug.Assert(ScoreProcessor.Rank.Value != ScoreRank.F);
+
                 this.Push(CreateResults(prepareScoreForDisplayTask.GetResultSafely()));
             }, Time.Current + delay, 50);
 
