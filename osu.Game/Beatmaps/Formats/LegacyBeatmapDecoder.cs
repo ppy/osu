e90660c1a48c7eb46447348756ca2b976ca1bbf6
Haspamelodica <melodicahaspa@gmail.com>
2023-04-21 02:35:28 +0200

    Switched to lowercase

--- osu.Game/Beatmaps/Formats/LegacyBeatmapDecoder.cs
+++ osu.Game/Beatmaps/Formats/LegacyBeatmapDecoder.cs
@@ -369,7 +369,7 @@ namespace osu.Game.Beatmaps.Formats
                     // Some very old beatmaps had incorrect type specifications for their backgrounds (ie. using 1 for VIDEO
                     // instead of 0 for BACKGROUND). To handle this gracefully, check the file extension against known supported
                     // video extensions and handle similar to a background if it doesn't match.
-                    if (!OsuGameBase.VIDEO_EXTENSIONS.Contains(Path.GetExtension(filename).ToUpperInvariant()))
+                    if (!OsuGameBase.VIDEO_EXTENSIONS.Contains(Path.GetExtension(filename).ToLowerInvariant()))
                     {
                         beatmap.BeatmapInfo.Metadata.BackgroundFile = filename;
                     }
