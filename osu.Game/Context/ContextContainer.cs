a6346171575e76ba0bf1b043040bbd0db8188b4e
OliBomby <olivierschipper@gmail.com>
2023-04-25 16:01:06 +0200

    Fix file header notice

--- osu.Game/Context/ContextContainer.cs
+++ osu.Game/Context/ContextContainer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
 using System.Collections.Generic;
 
 namespace osu.Game.Context
