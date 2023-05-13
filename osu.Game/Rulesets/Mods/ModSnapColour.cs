0984f796ebf0465617ed13d46e9700a820c93943
John <87902114+bubb13gummy@users.noreply.github.com>
2023-05-12 19:07:25 -0700

    Initial commit for the snap colour mod. Implements basic functionality.

---
+++ osu.Game/Rulesets/Mods/ModSnapColour.cs
@@ -0,0 +1,19 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mods
+{
+    /// <summary>
+    /// Mod that colours hitobjects based on the musical division they are on
+    /// </summary>
+    public class ModSnapColour : Mod
+    {
+        public override string Name => "Snap Colour";
+        public override string Acronym => "SC";
+        public override LocalisableString Description => new LocalisableString("Colours hit objects based on the rhythm.");
+        public override double ScoreMultiplier => 1;
+        public override ModType Type => ModType.Conversion;
+    }
+}
