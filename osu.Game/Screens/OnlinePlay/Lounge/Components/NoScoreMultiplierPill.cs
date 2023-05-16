8ab2c1517c8c6bfb42c64c290ebb51da2a263023
timiimit <timi.korda@gmail.com>
2023-05-16 10:45:25 +0200

    Remove unused namespace

--- osu.Game/Screens/OnlinePlay/Lounge/Components/NoScoreMultiplierPill.cs
+++ osu.Game/Screens/OnlinePlay/Lounge/Components/NoScoreMultiplierPill.cs
@@ -6,7 +6,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
-using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 
