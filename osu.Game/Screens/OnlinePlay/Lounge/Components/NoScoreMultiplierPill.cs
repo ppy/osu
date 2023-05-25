5f17caf8dc48e5f3d8c6733709416cecad84b793
timiimit <timi.korda@gmail.com>
2023-05-25 16:58:54 +0200

    Use newly merged `OnlinePlayPill`

--- osu.Game/Screens/OnlinePlay/Lounge/Components/NoScoreMultiplierPill.cs
+++ osu.Game/Screens/OnlinePlay/Lounge/Components/NoScoreMultiplierPill.cs
@@ -3,42 +3,18 @@
 
 #nullable disable
 
-using osu.Framework.Allocation;
+
 using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Game.Graphics;
-using osu.Game.Graphics.Containers;
 
 namespace osu.Game.Screens.OnlinePlay.Lounge.Components
 {
-    public partial class NoScoreMultiplierPill : OnlinePlayComposite
+    public partial class NoScoreMultiplierPill : OnlinePlayPill
     {
-        private OsuTextFlowContainer textFlow;
-
-        public NoScoreMultiplierPill()
-        {
-            AutoSizeAxes = Axes.Both;
-        }
-
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            InternalChild = new PillContainer
-            {
-                Child = textFlow = new OsuTextFlowContainer(s => s.Font = OsuFont.GetFont(size: 12))
-                {
-                    Anchor = Anchor.CentreLeft,
-                    Origin = Anchor.CentreLeft,
-                    AutoSizeAxes = Axes.Both
-                }
-            };
-            textFlow.AddText("No mod multipliers");
-        }
-
         protected override void LoadComplete()
         {
             base.LoadComplete();
 
+            TextFlow.Text = "No mod multipliers";
             NoScoreMultiplier.BindValueChanged(onNoScoreMultiplierChanged, true);
         }
 
