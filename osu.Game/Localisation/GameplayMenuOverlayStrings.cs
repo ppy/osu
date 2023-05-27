d01c2e527ec8d74e4e5d4eea355536d0deae7fd2
Robin Oger <87902114+bubb13gummy@users.noreply.github.com>
2023-05-27 12:29:14 +0200

    Adapt changes to fit master

--- osu.Game/Localisation/GameplayMenuOverlayStrings.cs
+++ osu.Game/Localisation/GameplayMenuOverlayStrings.cs
@@ -34,16 +34,6 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString PausedHeader => new TranslatableString(getKey(@"paused_header"), @"paused");
 
-        /// <summary>
-        /// "You're dead, try again?"
-        /// </summary>
-        public static LocalisableString FailedDescription => new TranslatableString(getKey(@"failed_description"), @"You're dead, try again?");
-
-        /// <summary>
-        /// "You're not going to do what i think you're going to do, are ya?"
-        /// </summary>
-        public static LocalisableString PausedDescription => new TranslatableString(getKey(@"paused_description"), @"You're not going to do what i think you're going to do, are ya?");
-
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }
