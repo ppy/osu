ed850196d9b780b4cd30511f8b0c37750a0c30ab
John <johntbiddle@gmail.com>
2023-05-30 01:43:08 -0700

    Reverted to applying the color change in OnUpdate, removed EnableComboColour flag from DrawableHitObject.cs

--- osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
+++ osu.Game/Rulesets/Objects/Drawables/DrawableHitObject.cs
@@ -75,11 +75,6 @@ namespace osu.Game.Rulesets.Objects.Drawables
         public override bool PropagateNonPositionalInputSubTree => HandleUserInput;
 
         /// <summary>
-        /// Whether this object should be coloured using its combo position
-        /// </summary>
-        public bool EnableComboColour { get; set; } = true;
-
-        /// <summary>
         /// Invoked by this or a nested <see cref="DrawableHitObject"/> after a <see cref="JudgementResult"/> has been applied.
         /// </summary>
         public event Action<DrawableHitObject, JudgementResult> OnNewResult;
@@ -533,7 +528,6 @@ namespace osu.Game.Rulesets.Objects.Drawables
         protected void UpdateComboColour()
         {
             if (!(HitObject is IHasComboInformation combo)) return;
-            if (!EnableComboColour) return;
 
             Color4 colour = combo.GetComboColour(CurrentSkin);
 
