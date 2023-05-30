ccb034c9402fb0d5f710a1167911bb81288b52fd
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-05-30 21:54:56 +0200

    Apply same comment & reworded condition to suspend too

--- osu.Game/Screens/OnlinePlay/OnlinePlayScreen.cs
+++ osu.Game/Screens/OnlinePlay/OnlinePlayScreen.cs
@@ -133,7 +133,7 @@ namespace osu.Game.Screens.OnlinePlay
 
             Debug.Assert(screenStack.CurrentScreen != null);
 
-            // if a subscreen was pushed to the nested stack while this screen was not present, this path will proxy `OnResuming()`
+            // if a subscreen was pushed to the nested stack while the stack was not present, this path will proxy `OnResuming()`
             // to the subscreen before `OnEntering()` can even be called for the subscreen, breaking ordering expectations.
             // to work around this, do not proxy resume to screens that haven't loaded yet.
             if ((screenStack.CurrentScreen as Drawable)?.IsLoaded == true)
@@ -149,7 +149,10 @@ namespace osu.Game.Screens.OnlinePlay
 
             Debug.Assert(screenStack.CurrentScreen != null);
 
-            if (screenStack.CurrentScreen.IsCurrentScreen())
+            // if a subscreen was pushed to the nested stack while the stack was not present, this path will proxy `OnSuspending()`
+            // to the subscreen before `OnEntering()` can even be called for the subscreen, breaking ordering expectations.
+            // to work around this, do not proxy suspend to screens that haven't loaded yet.
+            if ((screenStack.CurrentScreen as Drawable)?.IsLoaded == true)
                 screenStack.CurrentScreen.OnSuspending(e);
         }
 
