56758afeed0ff662f107be0584b53261c81a8a52
Dean Herbert <pe@ppy.sh>
2023-06-06 16:59:28 +0900

    Add note about `lastTickHittable` requirement

--- osu.Game.Rulesets.Taiko/Beatmaps/TaikoBeatmapConverter.cs
+++ osu.Game.Rulesets.Taiko/Beatmaps/TaikoBeatmapConverter.cs
@@ -92,6 +92,14 @@ namespace osu.Game.Rulesets.Taiko.Beatmaps
                 }).ToList();
             }
 
+            // TODO: stable makes the last tick of a drumroll non-required when the next object is too close.
+            // This probably needs to be reimplemented:
+            //
+            // List<HitObject> hitobjects = hitObjectManager.hitObjects;
+            // int ind = hitobjects.IndexOf(this);
+            // if (i < hitobjects.Count - 1 && hitobjects[i + 1].HittableStartTime - (EndTime + (int)TickSpacing) <= (int)TickSpacing)
+            //     lastTickHittable = false;
+
             return converted;
         }
 
