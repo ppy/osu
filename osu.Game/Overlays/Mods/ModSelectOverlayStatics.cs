9224486ec7b5c4f868438b395458b5c5328ca69a
Cootz <artem2004799@gmail.com>
2023-06-10 12:38:26 +0300

    Add mod select overlay statics

---
+++ osu.Game/Overlays/Mods/ModSelectOverlayStatics.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Configuration;
+
+namespace osu.Game.Overlays.Mods
+{
+    public class ModSelectOverlayStatics : InMemoryConfigManager<Static>
+    {
+        protected override void InitialiseDefaults()
+        {
+            SetDefault(Static.LastModSelectPanelSoundPlaybackTime, (double?)null);
+        }
+    }
+
+    public enum Static
+    {
+        LastModSelectPanelSoundPlaybackTime
+    }
+}
