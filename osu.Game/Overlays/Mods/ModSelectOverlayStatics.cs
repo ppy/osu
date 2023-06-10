31f370ec9b944ab994c3b4100b835f2a7d780fdb
Cootz <artem2004799@gmail.com>
2023-06-10 13:50:45 +0300

    Add comments for `ModSelectOverlayStatics`

--- osu.Game/Overlays/Mods/ModSelectOverlayStatics.cs
+++ osu.Game/Overlays/Mods/ModSelectOverlayStatics.cs
@@ -5,6 +5,9 @@ using osu.Game.Configuration;
 
 namespace osu.Game.Overlays.Mods
 {
+    /// <summary>
+    /// Stores global mod overlay statics. These will not be stored after disposal of <see cref="ModSelectOverlay"/>
+    /// </summary>
     public class ModSelectOverlayStatics : InMemoryConfigManager<Static>
     {
         protected override void InitialiseDefaults()
@@ -15,6 +18,10 @@ namespace osu.Game.Overlays.Mods
 
     public enum Static
     {
+        /// <summary>
+        /// The last playback time in milliseconds of an on/off sample (from <see cref="ModSelectPanel"/>).
+        /// Used to debounce <see cref="ModSelectPanel"/> on/off sounds game-wide to avoid volume saturation, especially in activating mod presets with many mods.
+        /// </summary>
         LastModSelectPanelSamplePlaybackTime
     }
 }
