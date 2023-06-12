8ae4a6f8503b6eacd3cb0063de593dc5cbc7ffcf
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-06-13 01:48:32 +0900

    Add basic profiling output of realm migrations

--- osu.Game/Database/StandardisedScoreMigrationTools.cs
+++ osu.Game/Database/StandardisedScoreMigrationTools.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Judgements;
