8864014af84f1dcb6932d6f4573b1c9dae6d38f1
Dean Herbert <pe@ppy.sh>
2023-06-12 15:25:12 +0900

    Add xmldoc

--- osu.Game/Graphics/Sprites/TruncatingSpriteText.cs
+++ osu.Game/Graphics/Sprites/TruncatingSpriteText.cs
@@ -7,8 +7,14 @@ using osu.Framework.Localisation;
 
 namespace osu.Game.Graphics.Sprites
 {
+    /// <summary>
+    /// A derived version of <see cref="OsuSpriteText"/> which automatically shows non-truncated text in tooltip when required.
+    /// </summary>
     public sealed partial class TruncatingSpriteText : OsuSpriteText, IHasTooltip
     {
+        /// <summary>
+        /// Whether a tooltip should be shown with non-truncated text on hover.
+        /// </summary>
         public bool ShowTooltip { get; init; } = true;
 
         public LocalisableString TooltipText => Text;
