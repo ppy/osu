3abf94cf4e105be87e9097b5c43addc34095ebde
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2023-06-13 23:22:27 +0900

    Add ScoreV1 calculation for ManiaRuleset

---
+++ osu.Game.Rulesets.Mania/Difficulty/ManiaScoreV1Processor.cs
@@ -0,0 +1,24 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Rulesets.Mania.Mods;
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Mania.Difficulty
+{
+    internal class ManiaScoreV1Processor
+    {
+        public int TotalScore { get; private set; }
+
+        public ManiaScoreV1Processor(IReadOnlyList<Mod> mods)
+        {
+            double multiplier = mods.Where(m => m is not (ModHidden or ModHardRock or ModDoubleTime or ModFlashlight or ManiaModFadeIn))
+                                    .Select(m => m.ScoreMultiplier)
+                                    .Aggregate((c, n) => c * n);
+
+            TotalScore = (int)(1000000 * multiplier);
+        }
+    }
+}
