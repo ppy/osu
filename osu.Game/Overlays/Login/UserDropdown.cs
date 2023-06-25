5477ef6bfb7a253ed1bc93356c48de7ceb218bcb
Joseph Madamba <madamba.joehu@outlook.com>
2023-06-25 12:58:51 -0700

    Remove unused usings

--- osu.Game/Overlays/Login/UserDropdown.cs
+++ osu.Game/Overlays/Login/UserDropdown.cs
@@ -1,12 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.UserInterface;
-using osu.Game.Graphics;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Users.Drawables;
 using osuTK;
