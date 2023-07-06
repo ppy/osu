04471237e96517e7f0a137bbe02960d0eb01d8f4
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-07-06 14:06:40 +0900

    Fix `GameplaySampleTriggerSource` not handling rewinds correctly

--- osu.Game/Rulesets/UI/GameplaySampleTriggerSource.cs
+++ osu.Game/Rulesets/UI/GameplaySampleTriggerSource.cs
@@ -69,6 +69,14 @@ namespace osu.Game.Rulesets.UI
             hitSound.Play();
         });
 
+        protected override void Update()
+        {
+            base.Update();
+
+            if (gameplayClock?.IsRewinding == true)
+                mostValidObject = null;
+        }
+
         protected HitObject? GetMostValidObject()
         {
             if (mostValidObject == null || isAlreadyHit(mostValidObject))
