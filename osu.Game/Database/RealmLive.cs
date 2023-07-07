8978f2ddd8c5cc366481dedde5a1a036abf383e2
Dean Herbert <pe@ppy.sh>
2023-07-07 10:09:24 +0900

    Remove all usage of `!something!.something`
    aka don't mix NRT forgiving syntax with not syntax

--- osu.Game/Database/RealmLive.cs
+++ osu.Game/Database/RealmLive.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using osu.Framework.Development;
+using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Statistics;
 using Realms;
 
@@ -133,7 +134,7 @@ namespace osu.Game.Database
         {
             Debug.Assert(ThreadSafety.IsUpdateThread);
 
-            if (dataIsFromUpdateThread && !data.Realm!.IsClosed)
+            if (dataIsFromUpdateThread && !data.Realm.AsNonNull().IsClosed)
             {
                 RealmLiveStatistics.USAGE_UPDATE_IMMEDIATE.Value++;
                 return;
