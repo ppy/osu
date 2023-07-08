1cd1aa96b02de155c0908150374ae8bbf5ad5f5c
Zyf <87902114+bubb13gummy@users.noreply.github.com>
2023-07-08 17:54:23 +0200

    Scoring : Use square-root of combo

--- osu.Game/Rulesets/Scoring/ScoreProcessor.cs
+++ osu.Game/Rulesets/Scoring/ScoreProcessor.cs
@@ -293,7 +293,7 @@ namespace osu.Game.Rulesets.Scoring
 
         protected virtual double GetBonusScoreChange(JudgementResult result) => Judgement.ToNumericResult(result.Type);
 
-        protected virtual double GetComboScoreChange(JudgementResult result) => Judgement.ToNumericResult(result.Type) * (1 + result.ComboAfterJudgement / 10d);
+        protected virtual double GetComboScoreChange(JudgementResult result) => Judgement.ToNumericResult(result.Type) * Math.Pow(result.ComboAfterJudgement, 0.5);
 
         protected virtual void ApplyScoreChange(JudgementResult result)
         {
