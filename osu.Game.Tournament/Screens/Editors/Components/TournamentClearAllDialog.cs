e67253ed1e6b70dd2c6a6e89dd60c5f8df97270d
Dao Heng Liu <daohengliu@gmail.com>
2023-07-19 00:25:07 +0100

    remove unused code

--- osu.Game.Tournament/Screens/Editors/Components/TournamentClearAllDialog.cs
+++ osu.Game.Tournament/Screens/Editors/Components/TournamentClearAllDialog.cs
@@ -16,31 +16,5 @@ namespace osu.Game.Tournament.Screens.Editors.Components
             Icon = FontAwesome.Solid.Trash;
             DangerousAction = storage.Clear;
         }
-
-    // // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-    // // See the LICENCE file in the repository root for full licence text.
-    //
-    // using osu.Framework.Graphics.Containers;
-    // using osu.Framework.Graphics.Sprites;
-    // using osu.Game.Overlays.Dialog;
-    // using osu.Game.Tournament.Screens.Ladder.Components;
-    //
-    // namespace osu.Game.Tournament.Screens.Editors.Components
-    // {
-    //     public partial class LadderResetTeamsDialog : DangerousActionDialog
-    //     {
-    //         public LadderResetTeamsDialog(Container<DrawableTournamentMatch> matchesContainer)
-    //         {
-    //             HeaderText = @"Confirm reset teams?";
-    //             Icon = FontAwesome.Solid.Undo;
-    //             DangerousAction = () =>
-    //             {
-    //                 foreach (var p in matchesContainer)
-    //                     p.Match.Reset();
-    //             };
-    //         }
-    //     }
-    // }
-
     }
 }
