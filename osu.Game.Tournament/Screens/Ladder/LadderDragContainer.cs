e373ce02a81bd9728bb2e02a2705b28e408e69e8
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-07-20 19:56:24 +0900

    Make dragging ladder more snappy

--- osu.Game.Tournament/Screens/Ladder/LadderDragContainer.cs
+++ osu.Game.Tournament/Screens/Ladder/LadderDragContainer.cs
@@ -36,8 +36,8 @@ namespace osu.Game.Tournament.Screens.Ladder
         {
             float newScale = Math.Clamp(scale + e.ScrollDelta.Y / 15 * scale, min_scale, max_scale);
 
-            this.MoveTo(target -= e.MousePosition * (newScale - scale), 2000, Easing.OutQuint);
-            this.ScaleTo(scale = newScale, 2000, Easing.OutQuint);
+            this.MoveTo(target -= e.MousePosition * (newScale - scale), 1000, Easing.OutQuint);
+            this.ScaleTo(scale = newScale, 1000, Easing.OutQuint);
 
             return true;
         }
