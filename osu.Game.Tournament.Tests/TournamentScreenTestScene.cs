e7c1664adbeedcc4f2721482a0fe484b26c01f5f
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-07-24 22:07:11 +0200

    Fix blank space under tournament screen test scenes on very wide screens

--- osu.Game.Tournament.Tests/TournamentScreenTestScene.cs
+++ osu.Game.Tournament.Tests/TournamentScreenTestScene.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -10,17 +11,28 @@ namespace osu.Game.Tournament.Tests
 {
     public abstract partial class TournamentScreenTestScene : TournamentTestScene
     {
-        protected override Container<Drawable> Content { get; } = new DrawSizePreservingFillContainer
-        {
-            TargetDrawSize = new Vector2(1920, 1080),
-            Scale = new Vector2(1920 / (float)(1920 + TournamentSceneManager.CONTROL_AREA_WIDTH)),
-            RelativeSizeAxes = Axes.Both
-        };
+        protected override Container<Drawable> Content { get; } = new TournamentScalingContainer();
 
         [BackgroundDependencyLoader]
         private void load()
         {
             base.Content.Add(Content);
+        }
+
+        private partial class TournamentScalingContainer : DrawSizePreservingFillContainer
+        {
+            public TournamentScalingContainer()
+            {
+                TargetDrawSize = new Vector2(1920, 1080);
+                RelativeSizeAxes = Axes.Both;
+            }
+
+            protected override void Update()
+            {
+                base.Update();
+
+                Scale = new Vector2(Math.Min(1, Content.DrawWidth / (1920 + TournamentSceneManager.CONTROL_AREA_WIDTH)));
+            }
         }
     }
 }
