6d99a0b84f49966421b9368fd6a14e3cfa9916ce
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-07-24 21:32:05 +0200

    Fix test failures due to `Content` override being applied in two places weirdly

--- osu.Game.Tournament.Tests/TournamentScreenTestScene.cs
+++ osu.Game.Tournament.Tests/TournamentScreenTestScene.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osuTK;
@@ -15,5 +16,11 @@ namespace osu.Game.Tournament.Tests
             Scale = new Vector2(1920 / (float)(1920 + TournamentSceneManager.CONTROL_AREA_WIDTH)),
             RelativeSizeAxes = Axes.Both
         };
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            base.Content.Add(Content);
+        }
     }
 }
