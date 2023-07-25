43107f8cf07619a78335fdf62aaf3157ec167cfe
Dean Herbert <pe@ppy.sh>
2023-07-25 17:46:05 +0900

    Fix skin mapping not working for menu fountains

--- osu.Game/Skinning/LegacySkinTransformer.cs
+++ osu.Game/Skinning/LegacySkinTransformer.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Audio.Sample;
-using osu.Framework.Graphics.Textures;
 using osu.Game.Audio;
 using osu.Game.Rulesets.Objects.Legacy;
 using static osu.Game.Skinning.SkinConfiguration;
@@ -22,18 +21,6 @@ namespace osu.Game.Skinning
         protected LegacySkinTransformer(ISkin skin)
             : base(skin)
         {
-        }
-
-        public override Texture? GetTexture(string componentName, WrapMode wrapModeS, WrapMode wrapModeT)
-        {
-            switch (componentName)
-            {
-                case "fountain-star":
-                    componentName = "star2";
-                    break;
-            }
-
-            return base.GetTexture(componentName, wrapModeS, wrapModeT);
         }
 
         public override ISample? GetSample(ISampleInfo sampleInfo)
