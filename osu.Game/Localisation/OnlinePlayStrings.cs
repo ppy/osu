0bd34a4f13eb79a0dba578f5a17c20e872976a09
Joseph Madamba <madamba.joehu@outlook.com>
2023-07-25 21:14:21 -0700

    Always show supporter-only playlist durations

---
+++ osu.Game/Localisation/OnlinePlayStrings.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Localisation
+{
+    public static class OnlinePlayStrings
+    {
+        private const string prefix = @"osu.Game.Resources.Localisation.OnlinePlay";
+
+        /// <summary>
+        /// "This duration is only available for osu!supporters."
+        /// </summary>
+        public static LocalisableString SupporterOnlyDurationNotice => new TranslatableString(getKey(@"supporter_only_duration_notice"), @"This duration is only available for osu!supporters.");
+
+        private static string getKey(string key) => $@"{prefix}:{key}";
+    }
+}
