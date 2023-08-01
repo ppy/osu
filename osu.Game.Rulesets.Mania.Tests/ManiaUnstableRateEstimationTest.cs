b131ac80c8fd0fd8f7c665e9fc94554994ae5d1e
Evening <johnchangqi@hotmail.com>
2023-08-01 16:07:36 +0800

    Move test case code closer to test callables

--- osu.Game.Rulesets.Mania.Tests/ManiaUnstableRateEstimationTest.cs
+++ osu.Game.Rulesets.Mania.Tests/ManiaUnstableRateEstimationTest.cs
@@ -18,34 +18,52 @@ namespace osu.Game.Rulesets.Mania.Tests
 {
     public class ManiaUnstableRateEstimationTest
     {
-        private const string resource_namespace = "Testing.Beatmaps";
-        protected string ResourceAssembly => "osu.Game.Rulesets.Mania";
-
-        // Test that both SS scores and near 0% scores are handled properly, within a margin of +-0.001 UR
-        [TestCase(42.978515625d, new[] { 11847, 0, 0, 0, 0, 0 })]
-        [TestCase(9523485.0d, new[] { 0, 0, 0, 0, 1, 11846 })]
-        public void Test1(double expectedEstimatedUnstableRate, int[] judgements)
-            => TestUnstableRate(expectedEstimatedUnstableRate, judgements);
+        public static IEnumerable<TestCaseData> TestCaseSourceData()
+        {
+            yield return new TestCaseData(309.990234375d, new[] { 5336, 3886, 1661, 445, 226, 293 }, new[] { new ManiaModDoubleTime() });
+            // Test that both SS scores and near 0% scores are handled properly, within a margin of +-0.001 UR
+            yield return new TestCaseData(42.978515625d, new[] { 11847, 0, 0, 0, 0, 0 }, null);
+            yield return new TestCaseData(9523485.0d, new[] { 0, 0, 0, 0, 1, 11846 }, null);
+        }
 
         // General test to make sure UR estimation isn't changed by anything, inclusive of rate changing, within a margin of +-0.001 UR.
-        [TestCase(309.990234375d, new[] { 5336, 3886, 1661, 445, 226, 293 })]
-        public void Test1ClockRateAdjusted(double expectedEstimatedUnstableRate, int[] judgements)
-            => TestUnstableRate(expectedEstimatedUnstableRate, judgements, new ManiaModDoubleTime());
+        [TestCaseSource(nameof(TestCaseSourceData))]
+        public void TestUnstableRate(double expectedEstimatedUnstableRate, int[] judgementCounts, params Mod[] mods)
+        {
+            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = judgementCounts.Sum(), OverallDifficulty = 7 };
+
+            double? estimatedUr = computeUnstableRate(attributes, judgementCounts, mods);
+            // Platform-dependent math functions (Pow, Cbrt, Exp, etc) and advanced math functions (Erf, FindMinimum) may result in slight differences.
+            Assert.That(estimatedUr, Is.EqualTo(expectedEstimatedUnstableRate).Within(0.001), "The estimated mania UR differed from the expected value.");
+        }
 
         // Ensure the UR estimation only returns null when it is supposed to.
         [TestCase(false, new[] { 1, 0, 0, 0, 0, 0 })]
         [TestCase(true, new[] { 0, 0, 0, 0, 0, 1 })]
         [TestCase(true, new[] { 0, 0, 0, 0, 0, 0 })]
-        public void Test2(bool returnsNull, int[] judgements)
-            => TestNullUnstableRate(returnsNull, judgements);
+        public void TestNullUnstableRate(bool expectedNullStatus, int[] judgementCounts)
+        {
+            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = 1, OverallDifficulty = 10 };
+
+            double? estimatedUr = computeUnstableRate(attributes, judgementCounts);
+            bool isNull = estimatedUr == null;
+
+            // Platform-dependent math functions (Pow, Cbrt, Exp, etc) and advanced math functions (Erf, FindMinimum) may result in slight differences.
+            Assert.That(isNull, Is.EqualTo(expectedNullStatus), "The estimated mania UR was/wasn't null.");
+        }
 
         // Ensure the estimated deviation doesn't reach too high of a value in a single note situation, as a sanity check.
         [TestCase(new[] { 0, 0, 0, 0, 1, 0 })]
-        public void Test3(int[] judgements)
-            => TestSingleNoteBound(judgements);
+        public void TestSingleNoteBound(int[] judgementCounts)
+        {
+            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = 1, OverallDifficulty = 0 };
+
+            double? estimatedUr = computeUnstableRate(attributes, judgementCounts);
+            Assert.That(estimatedUr, Is.AtMost(10000.0), "The estimated mania UR returned too high for a single note.");
+        }
 
         // Evaluates the Unstable Rate estimation of a beatmap with the given judgements.
-        private double? computeUnstableRate(DifficultyAttributes attr, int[] judgementCounts, params Mod[] mods)
+        private double? computeUnstableRate(DifficultyAttributes attr, IReadOnlyList<int> judgementCounts, params Mod[] mods)
         {
             var judgements = new Dictionary<HitResult, int>
             {
@@ -66,34 +84,6 @@ namespace osu.Game.Rulesets.Mania.Tests
             );
 
             return perfAttributes.EstimatedUr;
-        }
-
-        protected void TestUnstableRate(double expectedEstimatedUnstableRate, int[] judgementCounts, params Mod[] mods)
-        {
-            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = judgementCounts.Sum(), OverallDifficulty = 7 };
-
-            double? estimatedUr = computeUnstableRate(attributes, judgementCounts, mods);
-            // Platform-dependent math functions (Pow, Cbrt, Exp, etc) and advanced math functions (Erf, FindMinimum) may result in slight differences.
-            Assert.That(estimatedUr, Is.EqualTo(expectedEstimatedUnstableRate).Within(0.001), "The estimated mania UR differed from the expected value.");
-        }
-
-        protected void TestNullUnstableRate(bool expectedNullStatus, int[] judgementCounts)
-        {
-            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = 1, OverallDifficulty = 10 };
-
-            double? estimatedUr = computeUnstableRate(attributes, judgementCounts);
-            bool isNull = estimatedUr == null;
-
-            // Platform-dependent math functions (Pow, Cbrt, Exp, etc) and advanced math functions (Erf, FindMinimum) may result in slight differences.
-            Assert.That(isNull, Is.EqualTo(expectedNullStatus), "The estimated mania UR was/wasn't null.");
-        }
-
-        protected void TestSingleNoteBound(int[] judgementCounts)
-        {
-            DifficultyAttributes attributes = new ManiaDifficultyAttributes { NoteCount = 1, OverallDifficulty = 0 };
-
-            double? estimatedUr = computeUnstableRate(attributes, judgementCounts);
-            Assert.That(estimatedUr, Is.AtMost(10000.0), "The estimated mania UR returned too high for a single note.");
         }
 
         // TODO: @Natelytle Is this used for anything?
