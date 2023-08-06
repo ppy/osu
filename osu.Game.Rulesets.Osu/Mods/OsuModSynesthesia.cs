954c2ca905a1ef4c58c7c5da3e5a1223ada50ada
cdwcgt <cdwcgt@cdwcgt.top>
2023-08-07 03:22:20 +0900

    implement `ICanBeToggledDuringReplay` for `OsuModSynesthesia`

--- osu.Game.Rulesets.Osu/Mods/OsuModSynesthesia.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModSynesthesia.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mods;
@@ -15,11 +16,13 @@ namespace osu.Game.Rulesets.Osu.Mods
     /// <summary>
     /// Mod that colours <see cref="HitObject"/>s based on the musical division they are on
     /// </summary>
-    public class OsuModSynesthesia : ModSynesthesia, IApplicableToBeatmap, IApplicableToDrawableHitObject
+    public class OsuModSynesthesia : ModSynesthesia, IApplicableToBeatmap, IApplicableToDrawableHitObject, ICanBeToggledDuringReplay
     {
         private readonly OsuColour colours = new OsuColour();
 
         private IBeatmap? currentBeatmap { get; set; }
+
+        public BindableBool IsDisabled { get; } = new BindableBool();
 
         public void ApplyToBeatmap(IBeatmap beatmap)
         {
@@ -36,6 +39,8 @@ namespace osu.Game.Rulesets.Osu.Mods
 
             d.HitObjectApplied += _ =>
             {
+                if (IsDisabled.Value) return;
+
                 // slider tails are a painful edge case, as their start time is offset 36ms back (see `LegacyLastTick`).
                 // to work around this, look up the slider tail's parenting slider's end time instead to ensure proper snap.
                 double snapTime = d is DrawableSliderTail tail
@@ -47,6 +52,8 @@ namespace osu.Game.Rulesets.Osu.Mods
             // Need to set this every update to ensure it doesn't get overwritten by DrawableHitObject.OnApply() -> UpdateComboColour().
             d.OnUpdate += _ =>
             {
+                if (IsDisabled.Value) return;
+
                 if (timingBasedColour != null)
                     d.AccentColour.Value = timingBasedColour.Value;
             };
