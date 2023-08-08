fed338a42b12a248b902f2ea1389f06fcec0ec70
nanashi-1 <bagaresalken@gmail.com>
2023-08-09 07:34:30 +0800

    improve code

--- osu.Game/Skinning/LegacyRankDisplay.cs
+++ osu.Game/Skinning/LegacyRankDisplay.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -33,13 +31,7 @@ namespace osu.Game.Skinning
 
         protected override void LoadComplete()
         {
-            var skin = source.FindProvider(s => getTexture(s, "A") != null);
-
-            rank.Texture = getTexture(skin, scoreProcessor.Rank.Value.ToString());
-
-            scoreProcessor.Rank.BindValueChanged(v => rank.Texture = getTexture(skin, v.NewValue.ToString()));
+            scoreProcessor.Rank.BindValueChanged(v => rank.Texture = source.GetTexture($"ranking-{v.NewValue}-small"), true);
         }
-
-        private static Texture getTexture(ISkin skin, string name) => skin?.GetTexture($"ranking-{name}-small");
     }
 }
