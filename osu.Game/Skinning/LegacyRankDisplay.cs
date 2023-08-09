2c3d5dc21a31b4d65d618fbe8dedf714135f32a2
nanashi-1 <bagaresalken@gmail.com>
2023-08-09 08:44:01 +0800

    remove unnecesary directive

--- osu.Game/Skinning/LegacyRankDisplay.cs
+++ osu.Game/Skinning/LegacyRankDisplay.cs
@@ -5,7 +5,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Textures;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Skinning
