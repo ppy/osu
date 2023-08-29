b658b0e3462170adcb3a57901de17ee514a8d9c1
Nabile Rahmani <nabile13.pc@gmail.com>
2023-08-29 23:28:50 +0200

    Fix and use score user's IsBot property in results screen animation
    While a mod-created replay did flag itself as performed by a bot, the extension method converting it into a Score did not copy all the generated properties.
    
    As noted, it might be preferable for ModCreatedUser to inherit APIUser and forward it as-is to the Score instance.
    
    Related to PR #24675

--- osu.Game/Rulesets/Mods/ModExtensions.cs
+++ osu.Game/Rulesets/Mods/ModExtensions.cs
@@ -21,8 +21,10 @@ namespace osu.Game.Rulesets.Mods
                 {
                     User = new APIUser
                     {
-                        Id = APIUser.SYSTEM_USER_ID,
+                        // TODO: Some fields weren't copied from replayData.User (namely IsBot and Id). Should ModCreatedUser inherit from APIUser so we could pass it verbatim to avoid future mistakes ?
+                        Id = replayData.User.OnlineID,
                         Username = replayData.User.Username,
+                        IsBot = replayData.User.IsBot,
                     }
                 }
             };
