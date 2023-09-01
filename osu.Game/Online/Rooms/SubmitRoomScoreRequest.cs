84adcfccd3b635baced8f067fff37c291f9a64f2
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-09-01 14:50:59 +0200

    Add extra safety token type checks in submission requests
    Aside from more safety, also resolves "field never accessed"
    inspections.

--- osu.Game/Online/Rooms/SubmitRoomScoreRequest.cs
+++ osu.Game/Online/Rooms/SubmitRoomScoreRequest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Scoring;
 
 namespace osu.Game.Online.Rooms
@@ -13,6 +14,9 @@ namespace osu.Game.Online.Rooms
         public SubmitRoomScoreRequest(ScoreInfo scoreInfo, ScoreToken scoreToken, long roomId, long playlistItemId)
             : base(scoreInfo, scoreToken.ID)
         {
+            if (scoreToken.Type != ScoreTokenType.Multiplayer)
+                throw new ArgumentException($@"Invalid score token type supplied (expected {nameof(ScoreTokenType.Multiplayer)}, got {scoreToken.Type})", nameof(scoreToken));
+
             this.roomId = roomId;
             this.playlistItemId = playlistItemId;
         }
--- osu.Game/Online/Solo/SubmitSoloScoreRequest.cs
+++ osu.Game/Online/Solo/SubmitSoloScoreRequest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Online.Rooms;
 using osu.Game.Scoring;
 
@@ -13,6 +14,9 @@ namespace osu.Game.Online.Solo
         public SubmitSoloScoreRequest(ScoreInfo scoreInfo, ScoreToken scoreToken, int beatmapId)
             : base(scoreInfo, scoreToken.ID)
         {
+            if (scoreToken.Type != ScoreTokenType.Solo)
+                throw new ArgumentException($@"Invalid score token type supplied (expected {nameof(ScoreTokenType.Solo)}, got {scoreToken.Type}", nameof(scoreToken));
+
             this.beatmapId = beatmapId;
         }
 
