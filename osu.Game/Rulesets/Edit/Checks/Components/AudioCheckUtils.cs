d21a143afbbc7c5f02a58008976e731aed4d01b3
tsrk <87902114+bubb13gummy@users.noreply.github.com>
2023-09-01 20:56:42 +0200

    fix(AudioCheckUtils): use `Path.GetExtension()`
    To avoid filename escaping

--- osu.Game/Rulesets/Edit/Checks/Components/AudioCheckUtils.cs
+++ osu.Game/Rulesets/Edit/Checks/Components/AudioCheckUtils.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.IO;
 using System.Linq;
 
 namespace osu.Game.Rulesets.Edit.Checks.Components
@@ -9,6 +10,6 @@ namespace osu.Game.Rulesets.Edit.Checks.Components
     {
         public static readonly string[] AUDIO_EXTENSIONS = { "mp3", "ogg", "wav" };
 
-        public static bool HasAudioExtension(string filename) => AUDIO_EXTENSIONS.Any(filename.ToLowerInvariant().EndsWith);
+        public static bool HasAudioExtension(string filename) => AUDIO_EXTENSIONS.Any(Path.GetExtension(filename).ToLowerInvariant().EndsWith);
     }
 }
