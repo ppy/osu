f182f571cbaa019ed3ad3272ff208dea98ac51a0
Salman Ahmed <frenzibyte@gmail.com>
2023-09-03 02:22:41 +0300

    Add limit to catch palpable object elements

--- osu.Game.Rulesets.Catch/Skinning/Legacy/LegacyBananaPiece.cs
+++ osu.Game.Rulesets.Catch/Skinning/Legacy/LegacyBananaPiece.cs
@@ -2,17 +2,21 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Graphics.Textures;
+using osu.Game.Skinning;
+using osuTK;
 
 namespace osu.Game.Rulesets.Catch.Skinning.Legacy
 {
     public partial class LegacyBananaPiece : LegacyCatchHitObjectPiece
     {
+        private static readonly Vector2 banana_max_size = new Vector2(128);
+
         protected override void LoadComplete()
         {
             base.LoadComplete();
 
-            Texture? texture = Skin.GetTexture("fruit-bananas");
-            Texture? overlayTexture = Skin.GetTexture("fruit-bananas-overlay");
+            Texture? texture = Skin.GetTextureWithMaxSize("fruit-bananas", banana_max_size);
+            Texture? overlayTexture = Skin.GetTextureWithMaxSize("fruit-bananas-overlay", banana_max_size);
 
             SetTexture(texture, overlayTexture);
         }
