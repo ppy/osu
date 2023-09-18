ppy/osu

commit c6f855f71ccb1ac09759f8a238127530c7d51dc7

Avoid `MemoryStream.ToArray` overhead in `LegacyByteArrayReader`

osu.Game/IO/Archives/MemoryStreamArchiveReader.cs
@@ -1,0 +1,30 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace osu.Game.IO.Archives
+{
+    /// <summary>
+    /// Allows reading a single file from the provided memory stream.
+    /// </summary>
+    public class MemoryStreamArchiveReader : ArchiveReader
+    {
+        private readonly MemoryStream stream;
+
+        public MemoryStreamArchiveReader(MemoryStream stream, string filename)
+            : base(filename)
+        {
+            this.stream = stream;
+        }
+
+        public override Stream GetStream(string name) => new MemoryStream(stream.GetBuffer(), 0, (int)stream.Length);
+
+        public override void Dispose()
+        {
+        }
+
+        public override IEnumerable<string> Filenames => new[] { Name };
+    }
+}

osu.Game/Database/ImportTask.cs
@@ -54,7 +54,7 @@ namespace osu.Game.Database
                 if (ZipUtils.IsZipArchive(memoryStream))
                     return new ZipArchiveReader(memoryStream, Path);
 
-                return new LegacyByteArrayReader(memoryStream.ToArray(), Path);
+                return new MemoryStreamArchiveReader(memoryStream, Path);
             }
 
             // This isn't used in any current path. May need to reconsider for performance reasons (ie. if we don't expect the incoming stream to be copied out).

commit 4d3f1750f620a1d2d251e5f6ed36fe60e36b8256

Fix `MemoryStreamArchiveReader.GetStream()` failing in some cases
`MemoryStreamArchiveReader` introduced in
c6f855f71ccb1ac09759f8a238127530c7d51dc7 would previously use
`MemoryStream.GetBuffer()` to retrieve the underlying byte buffer with
stream data. However, this is not generally the method you would want,
for two reasons:

1. It can fail if the stream wasn't created in the way that supports it.
2. As per

	https://learn.microsoft.com/en-us/dotnet/api/system.io.memorystream.getbuffer?view=net-7.0#system-io-memorystream-getbuffer,

   it will return the _raw_ contents of the buffer, including
   potentially unused bytes.

To fix, use `MemoryStream.ToArray()` instead, which avoids both
pitfalls. Notably, `ToArray()` always returns the full contents of the
buffer, regardless of `Position`, as documented in:

    https://learn.microsoft.com/en-us/dotnet/api/system.io.memorystream.toarray?view=net-7.0#system-io-memorystream-toarray

osu.Game/IO/Archives/MemoryStreamArchiveReader.cs
@@ -19,7 +19,7 @@ namespace osu.Game.IO.Archives
             this.stream = stream;
         }
 
-        public override Stream GetStream(string name) => new MemoryStream(stream.GetBuffer(), 0, (int)stream.Length);
+        public override Stream GetStream(string name) => new MemoryStream(stream.ToArray(), 0, (int)stream.Length);
 
         public override void Dispose()
         {