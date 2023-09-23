3fdb420f736d569754c9b3105385a116ebc0b5c1
Salman Ahmed <frenzibyte@gmail.com>
2023-09-23 23:44:18 +0300

    Adjust catch playfield layout to further match stable

--- osu.Game.Rulesets.Catch/UI/CatchPlayfieldAdjustmentContainer.cs
+++ osu.Game.Rulesets.Catch/UI/CatchPlayfieldAdjustmentContainer.cs
@@ -17,12 +17,15 @@ namespace osu.Game.Rulesets.Catch.UI
 
         public CatchPlayfieldAdjustmentContainer()
         {
-            // because we are using centre anchor/origin, we will need to limit visibility in the future
-            // to ensure tall windows do not get a readability advantage.
-            // it may be possible to bake the catch-specific offsets (-100..340 mentioned below) into new values
-            // which are compatible with TopCentre alignment.
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
+            // playfields in stable are usually positioned vertically at exactly three quarters of the window's first quarter.
+            // the code below essentially sets Y to 0.25 * 0.75 (i.e. 0.1875), but it's done in a way that matches stable code concept-wise.
+            const float stable_playfield_ratio = 1f;
+            const float stable_window_ratio = 1f / playfield_size_adjust;
+
+            Anchor = Anchor.TopCentre;
+            Origin = Anchor.TopCentre;
+            RelativePositionAxes = Axes.Y;
+            Y = (stable_window_ratio - stable_playfield_ratio) / 4 * 3;
 
             Size = new Vector2(playfield_size_adjust);
 
@@ -49,10 +52,13 @@ namespace osu.Game.Rulesets.Catch.UI
                 // in stable, fruit fall vertically from -100 to 340.
                 // to emulate this, we want to make our playfield 440 gameplay pixels high.
                 // we then offset it -100 vertically in the position set below.
-                const float stable_v_offset_ratio = 440 / 384f;
+                const float stable_v_offset_ratio = 440 / CatchPlayfield.HEIGHT;
 
-                Scale = new Vector2(Parent.ChildSize.X / CatchPlayfield.WIDTH);
-                Position = new Vector2(0, -100 * stable_v_offset_ratio + Scale.X);
+                // there is no explanation to this offset, but applying it brings lazer nearly 1:1 with stable.
+                const float stable_offset = 6f;
+
+                Scale = new Vector2(Parent!.ChildSize.X / CatchPlayfield.WIDTH);
+                Position = new Vector2(0f, -100 * Scale.Y * stable_v_offset_ratio - stable_offset);
                 Size = Vector2.Divide(new Vector2(1, stable_v_offset_ratio), Scale);
             }
         }
