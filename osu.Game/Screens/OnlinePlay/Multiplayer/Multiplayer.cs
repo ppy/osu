2974150eb7e4d537bfbd8dcbf32c9d7ddecc3aed
Marvin Schürz <87902114+bubb13gummy@users.noreply.github.com>
2023-10-03 01:31:30 +0200

    Join multiplayer match when clicking the invite notification

--- osu.Game/Screens/OnlinePlay/Multiplayer/Multiplayer.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Multiplayer.cs
@@ -7,6 +7,7 @@ using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Logging;
 using osu.Framework.Screens;
 using osu.Game.Online.Multiplayer;
+using osu.Game.Online.Rooms;
 using osu.Game.Screens.OnlinePlay.Components;
 using osu.Game.Screens.OnlinePlay.Lounge;
 
@@ -89,6 +90,11 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
         protected override RoomManager CreateRoomManager() => new MultiplayerRoomManager();
 
         protected override LoungeSubScreen CreateLounge() => new MultiplayerLoungeSubScreen();
+
+        public void Join(Room room, string? password)
+        {
+            LoungeSubScreen.Join(room, password);
+        }
 
         protected override void Dispose(bool isDisposing)
         {
