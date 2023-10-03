5469d134cb0919b70fc6168d24d948d3133b67ea
Marvin Schürz <minetoblend@gmail.com>
2023-10-04 00:28:01 +0200

    Add missing parameter description to docs.

--- osu.Game/Online/Multiplayer/IMultiplayerRoomServer.cs
+++ osu.Game/Online/Multiplayer/IMultiplayerRoomServer.cs
@@ -103,7 +103,7 @@ namespace osu.Game.Online.Multiplayer
         /// <summary>
         /// Invites a player to the current room.
         /// </summary>
-        /// <param name="userId"></param>
+        /// <param name="userId">The user to invite.</param>
         /// <exception cref="UserBlockedException">The user has blocked or has been blocked by the invited user.</exception>
         /// <exception cref="UserBlocksPMsException">The invited user does not accept private messages.</exception>
         Task InvitePlayer(int userId);
