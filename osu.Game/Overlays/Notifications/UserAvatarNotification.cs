fe5177fa4fe29ea35b3fac74f50b330899dd7066
Marvin Schürz <minetoblend@gmail.com>
2023-10-04 00:50:48 +0200

    Remove unused import

--- osu.Game/Overlays/Notifications/UserAvatarNotification.cs
+++ osu.Game/Overlays/Notifications/UserAvatarNotification.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
