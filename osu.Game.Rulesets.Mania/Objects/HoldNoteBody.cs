f0da7f016d74f7002c76e8d8c1b4b208e133a447
Dan Balasescu <smoogipoo@smgi.me>
2023-10-09 11:49:54 +0900

    Actually use the judgement

--- osu.Game.Rulesets.Mania/Objects/HoldNoteBody.cs
+++ osu.Game.Rulesets.Mania/Objects/HoldNoteBody.cs
@@ -1,6 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Mania.Judgements;
+
 namespace osu.Game.Rulesets.Mania.Objects
 {
     /// <summary>
@@ -11,5 +14,6 @@ namespace osu.Game.Rulesets.Mania.Objects
     /// </summary>
     public class HoldNoteBody : ManiaHitObject
     {
+        public override Judgement CreateJudgement() => new HoldNoteBodyJudgement();
     }
 }
