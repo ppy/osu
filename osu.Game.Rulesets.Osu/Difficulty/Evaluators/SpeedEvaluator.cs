f683a45b15c31dd956fe6354830186eb173d6919
StanR <hi@stanr.info>
2023-10-14 01:08:49 +0300

    Adjust speed distance influence scaling

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -13,6 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double single_spacing_threshold = 125;
         private const double min_speed_bonus = 75; // ~200BPM
         private const double speed_balancing_factor = 40;
+        private const double distance_scale = 0.6;
 
         /// <summary>
         /// Evaluates the difficulty of tapping the current object, based on:
@@ -59,7 +60,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             double travelDistance = osuPrevObj?.TravelDistance ?? 0;
             double distance = Math.Min(single_spacing_threshold, travelDistance + osuCurrObj.MinimumJumpDistance);
 
-            return (speedBonus + speedBonus * Math.Pow(distance / single_spacing_threshold, 3.5)) * doubletapness / strainTime;
+            return (speedBonus + speedBonus * (Math.Pow(distance / single_spacing_threshold, 3.5)) * distance_scale) * doubletapness / strainTime;
         }
     }
 }
