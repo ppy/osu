630b83d7573522bae13b0e3f63ab0015ed4c8db3
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-10-18 12:27:16 +0200

    Add inline commentary in `Mod{Day,Night}core` about different speed adjustments

--- osu.Game/Rulesets/Mods/ModDaycore.cs
+++ osu.Game/Rulesets/Mods/ModDaycore.cs
@@ -33,6 +33,8 @@ namespace osu.Game.Rulesets.Mods
         {
             rateAdjustHelper = new RateAdjustModHelper(SpeedChange);
 
+            // intentionally not deferring the speed change handling to `RateAdjustModHelper`
+            // as the expected result of operation is not the same (daycore should preserve constant pitch).
             SpeedChange.BindValueChanged(val =>
             {
                 freqAdjust.Value = SpeedChange.Default;
