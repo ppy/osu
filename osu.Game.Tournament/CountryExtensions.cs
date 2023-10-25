d284f6cee464cf3a1f428b774bafde57929ecc41
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-10-25 13:01:23 +0900

    Add fallback case to `CountryExtensions.GetAcronym`

--- osu.Game.Tournament/CountryExtensions.cs
+++ osu.Game.Tournament/CountryExtensions.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Game.Users;
 
 namespace osu.Game.Tournament
@@ -763,7 +762,7 @@ namespace osu.Game.Tournament
                     return "MOZ";
 
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(country));
+                    return country.ToString();
             }
         }
     }
