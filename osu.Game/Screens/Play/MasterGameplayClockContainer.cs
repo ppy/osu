ppy/osu

commit e33486a766044c17c2f254f5e8df6d72b29c341e

Implement `IAdjustableAudioComponent` in `MasterGameplayClockContainer`

osu.Game/Screens/Play/MasterGameplayClockContainer.cs
@@ -24,7 +24,7 @@ namespace osu.Game.Screens.Play
     /// <remarks>
     /// This is intended to be used as a single controller for gameplay, or as a reference source for other <see cref="GameplayClockContainer"/>s.
     /// </remarks>
-    public class MasterGameplayClockContainer : GameplayClockContainer, IBeatSyncProvider
+    public class MasterGameplayClockContainer : GameplayClockContainer, IBeatSyncProvider, IAdjustableAudioComponent
     {
         /// <summary>
         /// Duration before gameplay start time required before skip button displays.
@@ -40,6 +40,8 @@ namespace osu.Game.Screens.Play
         };
 
         private readonly WorkingBeatmap beatmap;
+
+        private readonly Track track;
 
         private readonly double skipTargetTime;
 
@@ -66,6 +68,7 @@ namespace osu.Game.Screens.Play
         public MasterGameplayClockContainer(WorkingBeatmap beatmap, double skipTargetTime)
             : base(beatmap.Track, true)
         {
+            track = beatmap.Track;
             this.beatmap = beatmap;
             this.skipTargetTime = skipTargetTime;
 
@@ -195,9 +198,6 @@ namespace osu.Game.Screens.Play
             if (speedAdjustmentsApplied)
                 return;
 
-            if (SourceClock is not Track track)
-                return;
-
             track.AddAdjustment(AdjustableProperty.Frequency, GameplayClock.ExternalPauseFrequencyAdjust);
             track.AddAdjustment(AdjustableProperty.Tempo, UserPlaybackRate);
 
@@ -210,9 +210,6 @@ namespace osu.Game.Screens.Play
         private void removeSourceClockAdjustments()
         {
             if (!speedAdjustmentsApplied)
-                return;
-
-            if (SourceClock is not Track track)
                 return;
 
             track.RemoveAdjustment(AdjustableProperty.Frequency, GameplayClock.ExternalPauseFrequencyAdjust);
@@ -234,5 +231,29 @@ namespace osu.Game.Screens.Play
         IClock IBeatSyncProvider.Clock => this;
 
         ChannelAmplitudes IHasAmplitudes.CurrentAmplitudes => beatmap.TrackLoaded ? beatmap.Track.CurrentAmplitudes : ChannelAmplitudes.Empty;
+
+        void IAdjustableAudioComponent.AddAdjustment(AdjustableProperty type, IBindable<double> adjustBindable) =>
+            track.AddAdjustment(type, adjustBindable);
+
+        void IAdjustableAudioComponent.RemoveAdjustment(AdjustableProperty type, IBindable<double> adjustBindable) =>
+            track.RemoveAdjustment(type, adjustBindable);
+
+        public void RemoveAllAdjustments(AdjustableProperty type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BindAdjustments(IAggregateAudioAdjustment component) => throw new NotImplementedException();
+        public void UnbindAdjustments(IAggregateAudioAdjustment component) => throw new NotImplementedException();
+
+        public BindableNumber<double> Volume => throw new NotImplementedException();
+        public BindableNumber<double> Balance => throw new NotImplementedException();
+        public BindableNumber<double> Frequency => throw new NotImplementedException();
+        public BindableNumber<double> Tempo => throw new NotImplementedException();
+
+        public IBindable<double> AggregateVolume => throw new NotImplementedException();
+        public IBindable<double> AggregateBalance => throw new NotImplementedException();
+        public IBindable<double> AggregateFrequency => throw new NotImplementedException();
+        public IBindable<double> AggregateTempo => throw new NotImplementedException();
     }
 }

commit 565ae99e0dfddbecef6bc24828ba6c39c837f5cd

Fix `StopUsingBeatmapClock()` applying adjustments to track it was supposed to stop using
- Closes https://github.com/ppy/osu/issues/25248
- Possibly also closes https://github.com/ppy/osu/issues/20475

Regressed in e33486a766044c17c2f254f5e8df6d72b29c341e.

`StopUsingBeatmapClock()` intends to, as the name says, stop operating
on the working beatmap clock to yield its usage to other components on
exit. As part of that it tries to unapply audio adjustments so that
other screens can apply theirs freely instead.

However, the aforementioned commit introduced a bug in this. Previously
to it, `track` was an alias for the `SourceClock`, which could be
mutated in an indirect way via `ChangeSource()` calls. The
aforementioned commit made `track` a `readonly` field, initialised in
constructor, which would _never_ change value. In particular, it would
_always_ be the beatmap track, which meant that
`StopUsingBeatmapClock()` would remove the adjustments from the beatmap
track, but then at the end of the method, _apply them onto that same
track again_.

This was only saved by the fact that clock adjustments are removed again
on disposal of the `MasterGameplayClockContainer()`. This - due to async
disposal pressure - could explain infrequently reported cases wherein
the track would just continue to speed up ad infinitum.

To fix, fully substitute the beatmap track for a virtual track at the
point of calling `StopUsingBeatmapClock()`.

osu.Game/Screens/Play/MasterGameplayClockContainer.cs
@@ -41,7 +41,7 @@ namespace osu.Game.Screens.Play
 
         private readonly WorkingBeatmap beatmap;
 
-        private readonly Track track;
+        private Track track;
 
         private readonly double skipTargetTime;
 
@@ -188,11 +188,11 @@ namespace osu.Game.Screens.Play
         {
             removeSourceClockAdjustments();
 
-            var virtualTrack = new TrackVirtual(beatmap.Track.Length);
-            virtualTrack.Seek(CurrentTime);
+            track = new TrackVirtual(beatmap.Track.Length);
+            track.Seek(CurrentTime);
             if (IsRunning)
-                virtualTrack.Start();
-            ChangeSource(virtualTrack);
+                track.Start();
+            ChangeSource(track);
 
             addSourceClockAdjustments();
         }