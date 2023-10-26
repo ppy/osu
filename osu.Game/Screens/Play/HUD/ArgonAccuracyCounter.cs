2890e6ac8ebad5961c7a7cb2a7d8344ff8560467
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2023-10-26 08:58:47 +0300

    Add "Argon" accuracy counter

---
+++ osu.Game/Screens/Play/HUD/ArgonAccuracyCounter.cs
@@ -0,0 +1,18 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Skinning;
+
+namespace osu.Game.Screens.Play.HUD
+{
+    public partial class ArgonAccuracyCounter : GameplayAccuracyCounter, ISerialisableDrawable
+    {
+        public bool UsesFixedAnchor { get; set; }
+
+        protected override OsuSpriteText CreateSpriteText()
+            => base.CreateSpriteText().With(s => s.Font = OsuFont.Default.With(size: 19.2f));
+    }
+}
