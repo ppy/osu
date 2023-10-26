4a93d7145f9d0b1428574f8eef4a358a3fd9cd48
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-10-26 12:04:27 +0200

    Do not revert to default value when double-clicking disabled slider
    Closes https://github.com/ppy/osu/issues/25228.

--- osu.Game/Graphics/UserInterface/ShearedSliderBar.cs
+++ osu.Game/Graphics/UserInterface/ShearedSliderBar.cs
@@ -101,7 +101,11 @@ namespace osu.Game.Graphics.UserInterface
                         Origin = Anchor.TopCentre,
                         RelativePositionAxes = Axes.X,
                         Current = { Value = true },
-                        OnDoubleClicked = () => Current.SetDefault(),
+                        OnDoubleClicked = () =>
+                        {
+                            if (!Current.Disabled)
+                                Current.SetDefault();
+                        },
                     },
                 },
                 hoverClickSounds = new HoverClickSounds()
