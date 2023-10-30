9f11a04cc731159c6d2c0e4f7084238f6bd88360
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-10-30 13:24:01 +0100

    Generalise notion of 'touch device' mod

---
+++ osu.Game/Rulesets/Mods/ModTouchDevice.cs
@@ -0,0 +1,16 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mods
+{
+    public class ModTouchDevice : Mod
+    {
+        public sealed override string Name => "Touch Device";
+        public sealed override string Acronym => "TD";
+        public sealed override LocalisableString Description => "Automatically applied to plays on devices with a touchscreen.";
+        public sealed override double ScoreMultiplier => 1;
+        public sealed override ModType Type => ModType.System;
+    }
+}
