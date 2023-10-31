dece263257b892766ec807934e835b7f3dff16ae
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-10-31 16:16:57 +0100

    Mark `ModScoreV2` as invalid for multiplayer
    Doesn't do much for the client; mostly a safety for osu-web's sake,
    as without the change it could theoretically fail to validate the mod
    properly in multiplayer contexts.

--- osu.Game/Rulesets/Mods/ModScoreV2.cs
+++ osu.Game/Rulesets/Mods/ModScoreV2.cs
@@ -17,5 +17,7 @@ namespace osu.Game.Rulesets.Mods
         public override LocalisableString Description => "Score set on earlier osu! versions with the V2 scoring algorithm active.";
         public override double ScoreMultiplier => 1;
         public override bool UserPlayable => false;
+        public override bool ValidForMultiplayer => false;
+        public override bool ValidForMultiplayerAsFreeMod => false;
     }
 }
