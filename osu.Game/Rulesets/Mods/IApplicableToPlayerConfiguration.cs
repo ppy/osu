def8133bb6be490aa5c0c44f0bf436750c929835
cdwcgt <cdwcgt@cdwcgt.top>
2023-11-01 07:33:02 +0800

    add xmldoc for IApplicableToPlayerConfiguration

--- osu.Game/Rulesets/Mods/IApplicableToPlayerConfiguration.cs
+++ osu.Game/Rulesets/Mods/IApplicableToPlayerConfiguration.cs
@@ -5,6 +5,10 @@ using osu.Game.Screens.Play;
 
 namespace osu.Game.Rulesets.Mods
 {
+    /// <summary>
+    /// An interface for a mod which can adjust <see cref="PlayerConfiguration"/> settings.
+    /// <see cref="PlayerConfiguration"/> will adjust before <see cref="Player"/> loaded.
+    /// </summary>
     public interface IApplicableToPlayerConfiguration : IApplicableMod
     {
         void ApplyConfiguration(PlayerConfiguration configuration);
