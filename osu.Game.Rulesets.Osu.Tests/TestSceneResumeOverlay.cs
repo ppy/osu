57684d674e3ca7ca30b0c081c235afb9e0b2c599
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2023-11-06 18:35:46 +0300

    Allow tinkering with cursor-related settings in resume overlay test scene

--- osu.Game.Rulesets.Osu.Tests/TestSceneResumeOverlay.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneResumeOverlay.cs
@@ -2,11 +2,14 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using NUnit.Framework;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Cursor;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Screens.Play;
+using osu.Game.Tests.Gameplay;
 using osu.Game.Tests.Visual;
 
 namespace osu.Game.Rulesets.Osu.Tests
@@ -19,24 +22,37 @@ namespace osu.Game.Rulesets.Osu.Tests
 
         private bool resumeFired;
 
-        [SetUp]
-        public void SetUp() => Schedule(() =>
-        {
-            Child = osuInputManager = new ManualOsuInputManager(new OsuRuleset().RulesetInfo)
-            {
-                Children = new Drawable[]
-                {
-                    cursor = new CursorContainer(),
-                    resume = new OsuResumeOverlay
-                    {
-                        GameplayCursor = cursor
-                    },
-                }
-            };
+        private OsuConfigManager localConfig = null!;
 
-            resumeFired = false;
-            resume.ResumeAction = () => resumeFired = true;
-        });
+        [Cached]
+        private GameplayState gameplayState;
+
+        public TestSceneResumeOverlay()
+        {
+            gameplayState = TestGameplayState.Create(new OsuRuleset());
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Dependencies.Cache(localConfig = new OsuConfigManager(LocalStorage));
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            AddSliderStep("cursor size", 0.1f, 2f, 1f, v => localConfig.SetValue(OsuSetting.GameplayCursorSize, v));
+            AddSliderStep("circle size", 0f, 10f, 0f, val =>
+            {
+                gameplayState.Beatmap.Difficulty.CircleSize = val;
+                SetUp();
+            });
+
+            AddToggleStep("auto size", v => localConfig.SetValue(OsuSetting.AutoCursorSize, v));
+        }
+
+        [SetUp]
+        public void SetUp() => Schedule(loadContent);
 
         [Test]
         public void TestResume()
@@ -51,6 +67,14 @@ namespace osu.Game.Rulesets.Osu.Tests
             AddStep("move mouse back", () => InputManager.MoveMouseTo(ScreenSpaceDrawQuad.Centre));
             AddStep("click", () => osuInputManager.GameClick());
             AddAssert("dismissed", () => resumeFired && resume.State.Value == Visibility.Hidden);
+        }
+
+        private void loadContent()
+        {
+            Child = osuInputManager = new ManualOsuInputManager(new OsuRuleset().RulesetInfo) { Children = new Drawable[] { cursor = new CursorContainer(), resume = new OsuResumeOverlay { GameplayCursor = cursor }, } };
+
+            resumeFired = false;
+            resume.ResumeAction = () => resumeFired = true;
         }
 
         private partial class ManualOsuInputManager : OsuInputManager
