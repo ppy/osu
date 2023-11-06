959940b247788ce28e2d302aefb36a499b476e27
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-11-06 10:14:56 +0100

    Use placeholder mod icon for touch device

--- osu.Game/Rulesets/Mods/ModTouchDevice.cs
+++ osu.Game/Rulesets/Mods/ModTouchDevice.cs
@@ -2,7 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
+using osu.Game.Graphics;
 
 namespace osu.Game.Rulesets.Mods
 {
@@ -10,6 +12,7 @@ namespace osu.Game.Rulesets.Mods
     {
         public sealed override string Name => "Touch Device";
         public sealed override string Acronym => "TD";
+        public sealed override IconUsage? Icon => OsuIcon.PlayStyleTouch;
         public sealed override LocalisableString Description => "Automatically applied to plays on devices with a touchscreen.";
         public sealed override double ScoreMultiplier => 1;
         public sealed override ModType Type => ModType.System;
