9d10d93085cd3cde44d63836911bc607d5618901
Andrei Zavatski <megaman9919@gmail.com>
2023-11-06 20:45:52 +0300

    Adjust test scene to see graph flickering

--- osu.Game.Tests/Visual/Online/TestSceneGraph.cs
+++ osu.Game.Tests/Visual/Online/TestSceneGraph.cs
@@ -13,22 +13,20 @@ namespace osu.Game.Tests.Visual.Online
     [TestFixture]
     public partial class TestSceneGraph : OsuTestScene
     {
+        private readonly BarGraph graph;
+
         public TestSceneGraph()
         {
-            BarGraph graph;
-
-            Children = new[]
+            Child = graph = new BarGraph
             {
-                graph = new BarGraph
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Size = new Vector2(0.5f),
-                },
+                RelativeSizeAxes = Axes.Both,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(0.5f),
             };
 
             AddStep("values from 1-10", () => graph.Values = Enumerable.Range(1, 10).Select(i => (float)i));
+            AddStep("small values", () => graph.Values = Enumerable.Range(1, 10).Select(i => i * 0.01f).Concat(new[] { 100f }));
             AddStep("values from 1-100", () => graph.Values = Enumerable.Range(1, 100).Select(i => (float)i));
             AddStep("reversed values from 1-10", () => graph.Values = Enumerable.Range(1, 10).Reverse().Select(i => (float)i));
             AddStep("empty values", () => graph.Values = Array.Empty<float>());
@@ -36,6 +34,13 @@ namespace osu.Game.Tests.Visual.Online
             AddStep("Top to bottom", () => graph.Direction = BarDirection.TopToBottom);
             AddStep("Left to right", () => graph.Direction = BarDirection.LeftToRight);
             AddStep("Right to left", () => graph.Direction = BarDirection.RightToLeft);
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            graph.MoveToY(-10, 1000).Then().MoveToY(10, 1000).Loop();
         }
     }
 }
