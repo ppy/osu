3e257f1e6c02795c6bbb6bf08f542707e8e6e827
Dean Herbert <pe@ppy.sh>
2023-11-07 23:21:51 +0900

    Remove unused using statements

--- osu.Game/Screens/Play/PlayerSettings/InputSettings.cs
+++ osu.Game/Screens/Play/PlayerSettings/InputSettings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Configuration;
