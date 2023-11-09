2b459e069cc5be45df7ac2e82951b650f5672fc0
Joshua Hegedus <87902114+bubb13gummy@users.noreply.github.com>
2023-11-09 13:09:59 +0100

    fixed tests

--- osu.Game.Tests/Visual/Online/TestSceneUserClickableAvatar.cs
+++ osu.Game.Tests/Visual/Online/TestSceneUserClickableAvatar.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Effects;
+using osu.Framework.Testing;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Users;
 using osu.Game.Users.Drawables;
@@ -28,8 +30,10 @@ namespace osu.Game.Tests.Visual.Online
                 Children = new[]
                 {
                     generateUser(@"peppy", 2, CountryCode.AU, @"https://osu.ppy.sh/images/headers/profile-covers/c3.jpg", false, "99EB47"),
-                    generateUser(@"flyte", 3103765, CountryCode.JP, @"https://osu.ppy.sh/images/headers/profile-covers/c6.jpg", false),
-                    generateUser(@"joshika39", 17032217, CountryCode.RS, @"https://osu.ppy.sh/images/headers/profile-covers/c3.jpg", true),
+                    generateUser(@"flyte", 3103765, CountryCode.JP, @"https://osu.ppy.sh/images/headers/profile-covers/c6.jpg", true),
+                    generateUser(@"joshika39", 17032217, CountryCode.RS, @"https://osu.ppy.sh/images/headers/profile-covers/c3.jpg", false),
+                    new UpdateableAvatar(),
+                    new UpdateableAvatar()
                 },
             };
         });
@@ -37,68 +41,68 @@ namespace osu.Game.Tests.Visual.Online
         [Test]
         public void TestClickableAvatarHover()
         {
-            // AddStep($"click {1}. {nameof(ClickableAvatar)}", () =>
-            // {
-            //     var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
-            //     if (targets.Count < 1)
-            //         return;
-            //
-            //     InputManager.MoveMouseTo(targets[0]);
-            // });
-            // AddWaitStep("wait for tooltip to show", 5);
-            // AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
-            // AddWaitStep("wait for tooltip to hide", 3);
-            //
-            // AddStep($"click {2}. {nameof(ClickableAvatar)}", () =>
-            // {
-            //     var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
-            //     if (targets.Count < 2)
-            //         return;
-            //
-            //     InputManager.MoveMouseTo(targets[1]);
-            // });
-            // AddWaitStep("wait for tooltip to show", 5);
-            // AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
-            // AddWaitStep("wait for tooltip to hide", 3);
-            //
-            // AddStep($"click {3}. {nameof(ClickableAvatar)}", () =>
-            // {
-            //     var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
-            //     if (targets.Count < 3)
-            //         return;
-            //
-            //     InputManager.MoveMouseTo(targets[2]);
-            // });
-            // AddWaitStep("wait for tooltip to show", 5);
-            // AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
-            // AddWaitStep("wait for tooltip to hide", 3);
-            //
-            // AddStep($"click null user {4}. {nameof(ClickableAvatar)}", () =>
-            // {
-            //     var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
-            //     if (targets.Count < 4)
-            //         return;
-            //
-            //     InputManager.MoveMouseTo(targets[3]);
-            // });
-            // AddWaitStep("wait for tooltip to show", 5);
-            // AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
-            // AddWaitStep("wait for tooltip to hide", 3);
-            //
-            // AddStep($"click null user {5}. {nameof(ClickableAvatar)}", () =>
-            // {
-            //     var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
-            //     if (targets.Count < 5)
-            //         return;
-            //
-            //     InputManager.MoveMouseTo(targets[4]);
-            // });
-            // AddWaitStep("wait for tooltip to show", 5);
-            // AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
-            // AddWaitStep("wait for tooltip to hide", 3);
+            AddStep($"click user {1} with UserGridPanel {nameof(ClickableAvatar)}", () =>
+            {
+                var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
+                if (targets.Count < 1)
+                    return;
+
+                InputManager.MoveMouseTo(targets[0]);
+            });
+            AddWaitStep("wait for tooltip to show", 5);
+            AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
+            AddWaitStep("wait for tooltip to hide", 3);
+
+            AddStep($"click user {2} with username only. {nameof(ClickableAvatar)}", () =>
+            {
+                var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
+                if (targets.Count < 2)
+                    return;
+
+                InputManager.MoveMouseTo(targets[1]);
+            });
+            AddWaitStep("wait for tooltip to show", 5);
+            AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
+            AddWaitStep("wait for tooltip to hide", 3);
+
+            AddStep($"click user {3} with UserGridPanel {nameof(ClickableAvatar)}", () =>
+            {
+                var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
+                if (targets.Count < 3)
+                    return;
+
+                InputManager.MoveMouseTo(targets[2]);
+            });
+            AddWaitStep("wait for tooltip to show", 5);
+            AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
+            AddWaitStep("wait for tooltip to hide", 3);
+
+            AddStep($"click null user {4}. {nameof(ClickableAvatar)}", () =>
+            {
+                var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
+                if (targets.Count < 4)
+                    return;
+
+                InputManager.MoveMouseTo(targets[3]);
+            });
+            AddWaitStep("wait for tooltip to show", 5);
+            AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
+            AddWaitStep("wait for tooltip to hide", 3);
+
+            AddStep($"click null user {5}. {nameof(ClickableAvatar)}", () =>
+            {
+                var targets = this.ChildrenOfType<ClickableAvatar>().ToList();
+                if (targets.Count < 5)
+                    return;
+
+                InputManager.MoveMouseTo(targets[4]);
+            });
+            AddWaitStep("wait for tooltip to show", 5);
+            AddStep("Hover out", () => InputManager.MoveMouseTo(new Vector2(0)));
+            AddWaitStep("wait for tooltip to hide", 3);
         }
 
-        private Drawable generateUser(string username, int id, CountryCode countryCode, string cover, bool isTooltipEnabled, string? color = null)
+        private Drawable generateUser(string username, int id, CountryCode countryCode, string cover, bool onlyUsername, string? color = null)
         {
             return new ClickableAvatar(new APIUser
                 {
@@ -121,7 +125,7 @@ namespace osu.Game.Tests.Visual.Online
                     {
                         Type = EdgeEffectType.Shadow, Radius = 1, Colour = Color4.Black.Opacity(0.2f),
                     },
-                    IsTooltipEnabled = isTooltipEnabled,
+                    ShowUsernameOnly = onlyUsername,
                 };
         }
     }
