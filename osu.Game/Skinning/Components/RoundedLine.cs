99d9db5b76ba5292398201039e75fcb0a9bcc1d7
Dean Herbert <pe@ppy.sh>
2023-11-10 14:03:24 +0900

    Use a better default size for line

--- osu.Game/Skinning/Components/RoundedLine.cs
+++ osu.Game/Skinning/Components/RoundedLine.cs
@@ -1,29 +1,18 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osuTK;
 
 namespace osu.Game.Skinning.Components
 {
-    public partial class RoundedLine : CompositeDrawable, ISerialisableDrawable
+    public partial class RoundedLine : Circle, ISerialisableDrawable
     {
         public bool UsesFixedAnchor { get; set; }
 
-        [BackgroundDependencyLoader]
-        private void load()
+        public RoundedLine()
         {
-            AutoSizeAxes = Axes.Both;
-
-            InternalChild = new Circle
-            {
-                Anchor = Anchor.CentreLeft,
-                Origin = Anchor.CentreLeft,
-                Width = 45f,
-                Height = 3f,
-            };
+            Size = new Vector2(200, 8);
         }
     }
 }
