535282ba7d90303058c429c564c9d473afaf8e99
Joseph Madamba <madamba.joehu@outlook.com>
2023-11-14 14:13:20 -0800

    Use existing localisation for corner radius in `BoxElement`

--- osu.Game/Skinning/Components/BoxElement.cs
+++ osu.Game/Skinning/Components/BoxElement.cs
@@ -7,6 +7,8 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Configuration;
+using osu.Game.Localisation.SkinComponents;
+using osu.Game.Overlays.Settings;
 using osuTK;
 using osuTK.Graphics;
 
@@ -16,7 +18,8 @@ namespace osu.Game.Skinning.Components
     {
         public bool UsesFixedAnchor { get; set; }
 
-        [SettingSource("Corner rounding", "How round the corners of the box should be.")]
+        [SettingSource(typeof(SkinnableComponentStrings), nameof(SkinnableComponentStrings.CornerRadius), nameof(SkinnableComponentStrings.CornerRadiusDescription),
+            SettingControlType = typeof(SettingsPercentageSlider<float>))]
         public BindableFloat CornerRounding { get; } = new BindableFloat(1)
         {
             Precision = 0.01f,
