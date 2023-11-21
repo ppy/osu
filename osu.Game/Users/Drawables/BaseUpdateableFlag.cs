cd7e0bf620c0fd1617f1d99038dbbb85e52acd39
yesseruser <yesseruseryt@gmail.com>
2023-11-21 19:27:33 +0100

    Created and implemened a BaseUpdateableFlag. The tooltip still shows.

--- osu.Game/Users/Drawables/BaseUpdateableFlag.cs
+++ osu.Game/Users/Drawables/BaseUpdateableFlag.cs
@@ -1,25 +1,25 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Input.Events;
-using osu.Game.Graphics.UserInterface;
-using osu.Game.Overlays;
 
 namespace osu.Game.Users.Drawables
 {
-    public partial class ClickableUpdateableFlag : BaseUpdateableFlag
+    public partial class BaseUpdateableFlag : ModelBackedDrawable<CountryCode>
     {
-        /// <summary>
-        /// Perform an action in addition to showing the country ranking.
-        /// This should be used to perform auxiliary tasks and not as a primary action for clicking a flag (to maintain a consistent UX).
-        /// </summary>
-        public Action? Action;
+        public CountryCode CountryCode
+        {
+            get => Model;
+            set => Model = value;
+        }
 
-        public ClickableUpdateableFlag(CountryCode countryCode = CountryCode.Unknown)
+        /// <summary>
+        /// Whether to show a place holder on unknown country.
+        /// </summary>
+        public bool ShowPlaceholderOnUnknown = true;
+
+        public BaseUpdateableFlag(CountryCode countryCode = CountryCode.Unknown)
         {
             CountryCode = countryCode;
         }
@@ -38,19 +38,8 @@ namespace osu.Game.Users.Drawables
                     {
                         RelativeSizeAxes = Axes.Both
                     },
-                    new HoverClickSounds()
                 }
             };
-        }
-
-        [Resolved]
-        private RankingsOverlay? rankingsOverlay { get; set; }
-
-        protected override bool OnClick(ClickEvent e)
-        {
-            Action?.Invoke();
-            rankingsOverlay?.ShowCountry(CountryCode);
-            return true;
         }
     }
 }
