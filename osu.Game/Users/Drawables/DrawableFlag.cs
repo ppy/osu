92fc9e53ca0f5e82d5a26b30a43a4931694a3036
yesseruser <87902114+bubb13gummy@users.noreply.github.com>
2023-11-22 15:25:17 +0100

    Revert "Added a BaseDrawableFlag without a tooltip, and used it."
    This reverts commit 448955e15f54ab8d941ece36eee4f3db998a9b3a.

--- osu.Game/Users/Drawables/DrawableFlag.cs
+++ osu.Game/Users/Drawables/DrawableFlag.cs
@@ -5,23 +5,29 @@ using System;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions;
 using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Localisation;
 
 namespace osu.Game.Users.Drawables
 {
-    public partial class DrawableFlag : BaseDrawableFlag, IHasTooltip
+    public partial class DrawableFlag : Sprite, IHasTooltip
     {
-        public LocalisableString TooltipText => CountryCode == CountryCode.Unknown ? string.Empty : CountryCode.GetDescription();
+        private readonly CountryCode countryCode;
 
-        public DrawableFlag(CountryCode countryCode) : base(countryCode) { }
+        public LocalisableString TooltipText => countryCode == CountryCode.Unknown ? string.Empty : countryCode.GetDescription();
+
+        public DrawableFlag(CountryCode countryCode)
+        {
+            this.countryCode = countryCode;
+        }
 
         [BackgroundDependencyLoader]
         private void load(TextureStore ts)
         {
             ArgumentNullException.ThrowIfNull(ts);
 
-            string textureName = CountryCode == CountryCode.Unknown ? "__" : CountryCode.ToString();
+            string textureName = countryCode == CountryCode.Unknown ? "__" : countryCode.ToString();
             Texture = ts.Get($@"Flags/{textureName}") ?? ts.Get(@"Flags/__");
         }
     }
