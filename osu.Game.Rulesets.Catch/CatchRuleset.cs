bb662676347c79343da3ba480693ff5c272b6878
Dan Balasescu <smoogipoo@smgi.me>
2023-11-24 13:46:41 +0900

    Actually use CatchHealthProcessor for the ruleset

--- osu.Game.Rulesets.Catch/CatchRuleset.cs
+++ osu.Game.Rulesets.Catch/CatchRuleset.cs
@@ -39,6 +39,8 @@ namespace osu.Game.Rulesets.Catch
 
         public override ScoreProcessor CreateScoreProcessor() => new CatchScoreProcessor();
 
+        public override HealthProcessor CreateHealthProcessor(double drainStartTime) => new CatchHealthProcessor(drainStartTime);
+
         public override IBeatmapConverter CreateBeatmapConverter(IBeatmap beatmap) => new CatchBeatmapConverter(beatmap, this);
 
         public override IBeatmapProcessor CreateBeatmapProcessor(IBeatmap beatmap) => new CatchBeatmapProcessor(beatmap);
