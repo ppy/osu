b36db3518c79486a94fc97411fbf340fd3741a38
Dan Balasescu <smoogipoo@smgi.me>
2023-12-09 22:09:49 +0900

    Add keycount to song select details panel and carousel panels

--- osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
+++ osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
@@ -57,10 +57,11 @@ namespace osu.Game.Rulesets.Mania.Beatmaps
 
         public static int GetColumnCount(LegacyBeatmapConversionDifficultyInfo difficulty)
         {
-            if (new ManiaRuleset().RulesetInfo.Equals(difficulty.SourceRuleset))
-                return GetColumnCountForNonConvert(difficulty);
-
             double roundedCircleSize = Math.Round(difficulty.CircleSize);
+
+            if (new ManiaRuleset().RulesetInfo.Equals(difficulty.SourceRuleset))
+                return (int)Math.Max(1, roundedCircleSize);
+
             double roundedOverallDifficulty = Math.Round(difficulty.OverallDifficulty);
 
             int countSliderOrSpinner = difficulty.TotalObjectCount - difficulty.CircleCount;
@@ -74,12 +75,6 @@ namespace osu.Game.Rulesets.Mania.Beatmaps
                 return roundedOverallDifficulty > 4 ? 5 : 4;
 
             return Math.Max(4, Math.Min((int)roundedOverallDifficulty + 1, 7));
-        }
-
-        public static int GetColumnCountForNonConvert(IBeatmapDifficultyInfo difficulty)
-        {
-            double roundedCircleSize = Math.Round(difficulty.CircleSize);
-            return (int)Math.Max(1, roundedCircleSize);
         }
 
         public override bool CanConvert() => Beatmap.HitObjects.All(h => h is IHasXPosition);
