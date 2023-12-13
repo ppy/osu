e15b42ced142c66b8e3c120140b57d7ecfafd9a4
cdwcgt <87902114+bubb13gummy@users.noreply.github.com>
2023-12-13 18:59:03 +0800

    fix getkey isn't match the name

--- osu.Game/Localisation/AimErrorMeterStrings.cs
+++ osu.Game/Localisation/AimErrorMeterStrings.cs
@@ -12,42 +12,42 @@ namespace osu.Game.Localisation
         /// <summary>
         /// "Judgement position size"
         /// </summary>
-        public static LocalisableString JudgementSize => new TranslatableString(getKey(@"judgement_line_thickness"), "Judgement position size");
+        public static LocalisableString JudgementSize => new TranslatableString(getKey(@"judgement_size"), "Judgement position size");
 
         /// <summary>
         /// "How big of judgement position should be."
         /// </summary>
-        public static LocalisableString JudgementSizeDescription => new TranslatableString(getKey("judgement_line_thickness"), "How big of judgement position should be.");
+        public static LocalisableString JudgementSizeDescription => new TranslatableString(getKey("judgement_size_description"), "How big of judgement position should be.");
 
         /// <summary>
         /// "Judgement position style"
         /// </summary>
-        public static LocalisableString JudgementStyle => new TranslatableString(getKey(@"judgement_line_thickness"), "Judgement position style");
+        public static LocalisableString JudgementStyle => new TranslatableString(getKey(@"judgement_style"), "Judgement position style");
 
         /// <summary>
         /// "The style of judgement position."
         /// </summary>
-        public static LocalisableString JudgementStyleDescription => new TranslatableString(getKey("judgement_line_thickness"), "The style of judgement position.");
+        public static LocalisableString JudgementStyleDescription => new TranslatableString(getKey("judgement_style_description"), "The style of judgement position.");
 
         /// <summary>
         /// "Average position size"
         /// </summary>
-        public static LocalisableString AverageSize => new TranslatableString(getKey(@"judgement_line_thickness"), "Average position size");
+        public static LocalisableString AverageSize => new TranslatableString(getKey(@"average_size"), "Average position size");
 
         /// <summary>
         /// "How big of average position should be."
         /// </summary>
-        public static LocalisableString AverageSizeDescription => new TranslatableString(getKey("judgement_line_thickness"), "How big of average position should be.");
+        public static LocalisableString AverageSizeDescription => new TranslatableString(getKey("average_size_description"), "How big of average position should be.");
 
         /// <summary>
         /// "Average position style"
         /// </summary>
-        public static LocalisableString AverageStyle => new TranslatableString(getKey(@"judgement_line_thickness"), "Average position style");
+        public static LocalisableString AverageStyle => new TranslatableString(getKey(@"average_style"), "Average position style");
 
         /// <summary>
         /// "The style of average position."
         /// </summary>
-        public static LocalisableString AverageStyleDescription => new TranslatableString(getKey("judgement_line_thickness"), "The style of average position.");
+        public static LocalisableString AverageStyleDescription => new TranslatableString(getKey("average_style_description"), "The style of average position.");
 
         /// <summary>
         /// "X"
