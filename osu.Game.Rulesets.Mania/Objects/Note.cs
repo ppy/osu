472aaf55da08e430eedf88d557a2afe0f5dfad6b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2023-12-20 19:47:36 +0900

    Remove remaining usage of `NotePerfectBonus`

--- osu.Game.Rulesets.Mania/Objects/Note.cs
+++ osu.Game.Rulesets.Mania/Objects/Note.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Threading;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Mania.Judgements;
 
@@ -13,12 +12,5 @@ namespace osu.Game.Rulesets.Mania.Objects
     public class Note : ManiaHitObject
     {
         public override Judgement CreateJudgement() => new ManiaJudgement();
-
-        protected override void CreateNestedHitObjects(CancellationToken cancellationToken)
-        {
-            base.CreateNestedHitObjects(cancellationToken);
-
-            AddNested(new NotePerfectBonus { StartTime = StartTime });
-        }
     }
 }
