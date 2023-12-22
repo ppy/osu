3bb33439cf7dfee417b358dfd318a1500f30e35e
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2023-12-22 16:43:17 +0900

    Revert change to ModSuddenDeath

--- osu.Game/Rulesets/Mods/ModSuddenDeath.cs
+++ osu.Game/Rulesets/Mods/ModSuddenDeath.cs
@@ -23,7 +23,7 @@ namespace osu.Game.Rulesets.Mods
         public override Type[] IncompatibleMods => base.IncompatibleMods.Append(typeof(ModPerfect)).ToArray();
 
         protected override bool FailCondition(HealthProcessor healthProcessor, JudgementResult result)
-            => (result.Judgement.MinResult.AffectsCombo() || result.Judgement.MaxResult.AffectsCombo() || result.Type.AffectsCombo())
+            => result.Type.AffectsCombo()
                && !result.IsHit;
     }
 }
