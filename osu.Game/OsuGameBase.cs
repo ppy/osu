b23fb5740f54ab1eaf0e58300116bd81121494a9
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-12-27 15:17:39 +0100

    Implement new icon store

--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -479,6 +479,7 @@ namespace osu.Game
             AddFont(Resources, @"Fonts/Venera/Venera-Black");
 
             Fonts.AddStore(new HexaconsIcons.HexaconsStore(Textures));
+            Fonts.AddStore(new OsuIcon.OsuIconStore(Textures));
         }
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
