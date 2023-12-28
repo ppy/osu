93a8afe96e101a63f65e6bcca92d9707c1fde767
Dean Herbert <pe@ppy.sh>
2023-12-28 14:40:10 +0900

    Add very simple cache-busting (30 minutes)

--- osu.Game/Online/API/Requests/GetSystemTitleRequest.cs
+++ osu.Game/Online/API/Requests/GetSystemTitleRequest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Online.API.Requests.Responses;
 
 namespace osu.Game.Online.API.Requests
@@ -8,7 +9,7 @@ namespace osu.Game.Online.API.Requests
     public class GetSystemTitleRequest : OsuJsonWebRequest<APISystemTitle>
     {
         public GetSystemTitleRequest()
-            : base(@"https://assets.ppy.sh/lazer-status.json")
+            : base($@"https://assets.ppy.sh/lazer-status.json?{DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 1800}")
         {
         }
     }
