e6f1d7db4446e9e0c2308b5d17c937c65a4f7a27
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-12-28 14:10:03 +0100

    Move `SessionAverageHitErrorTracker` to more general namespace

--- osu.Game/Overlays/Settings/Sections/Audio/AudioOffsetAdjustControl.cs
+++ osu.Game/Overlays/Settings/Sections/Audio/AudioOffsetAdjustControl.cs
@@ -12,6 +12,7 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
+using osu.Game.Configuration;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
