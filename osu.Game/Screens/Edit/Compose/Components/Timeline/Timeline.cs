5b95b7ba7af6eb0ae69f4429f350c616428753ca
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2023-12-29 11:07:45 +0100

    Use alternative workaround

--- osu.Game/Screens/Edit/Compose/Components/Timeline/Timeline.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/Timeline.cs
@@ -141,15 +141,27 @@ namespace osu.Game.Screens.Edit.Compose.Components.Timeline
             waveformOpacity = config.GetBindable<float>(OsuSetting.EditorWaveformOpacity);
 
             track.BindTo(editorClock.Track);
-            // schedule added as without it, `beatmap.Value.Track.Length` can be 0 immediately after a track switch.
-            track.BindValueChanged(_ => Schedule(() =>
+            track.BindValueChanged(_ =>
             {
                 waveform.Waveform = beatmap.Value.Waveform;
-                waveform.RelativePositionAxes = Axes.X;
-                waveform.X = -(float)(Editor.WAVEFORM_VISUAL_OFFSET / beatmap.Value.Track.Length);
-            }), true);
+                Scheduler.AddOnce(applyVisualOffset, beatmap);
+            }, true);
 
             Zoom = (float)(defaultTimelineZoom * editorBeatmap.BeatmapInfo.TimelineZoom);
+        }
+
+        private void applyVisualOffset(IBindable<WorkingBeatmap> beatmap)
+        {
+            waveform.RelativePositionAxes = Axes.X;
+
+            if (beatmap.Value.Track.Length > 0)
+                waveform.X = -(float)(Editor.WAVEFORM_VISUAL_OFFSET / beatmap.Value.Track.Length);
+            else
+            {
+                // sometimes this can be the case immediately after a track switch.
+                // reschedule with the hope that the track length eventually populates.
+                Scheduler.AddOnce(applyVisualOffset, beatmap);
+            }
         }
 
         protected override void LoadComplete()
