95893994a53edcf31b83913b1e60730fb211a26f
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-01-08 12:54:16 +0100

    Use override that was intended to steer global track control rather than local sets

--- osu.Game/Screens/Menu/IntroScreen.cs
+++ osu.Game/Screens/Menu/IntroScreen.cs
@@ -95,6 +95,8 @@ namespace osu.Game.Screens.Menu
             Colour = Color4.Black
         };
 
+        public override bool? AllowGlobalTrackControl => false;
+
         protected IntroScreen([CanBeNull] Func<MainMenu> createNextScreen = null)
         {
             this.createNextScreen = createNextScreen;
@@ -108,8 +110,6 @@ namespace osu.Game.Screens.Menu
         {
             // prevent user from changing beatmap while the intro is still running.
             beatmap = Beatmap.BeginLease(false);
-
-            musicController.AllowTrackControl.Value = false;
 
             MenuVoice = config.GetBindable<bool>(OsuSetting.MenuVoice);
             MenuMusic = config.GetBindable<bool>(OsuSetting.MenuMusic);
