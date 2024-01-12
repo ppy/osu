e861661037cce1079aee84e00b95efae50804873
Andrei Zavatski <megaman9919@gmail.com>
2024-01-12 03:03:38 +0300

    Remove invalidations in update
    oops

--- osu.Game/Screens/Play/HUD/ArgonHealthDisplayParts/ArgonHealthDisplayBar.cs
+++ osu.Game/Screens/Play/HUD/ArgonHealthDisplayParts/ArgonHealthDisplayBar.cs
@@ -116,12 +116,6 @@ namespace osu.Game.Screens.Play.HUD.ArgonHealthDisplayParts
             TextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, "ArgonBarPath");
         }
 
-        protected override void Update()
-        {
-            base.Update();
-            Invalidate(Invalidation.DrawNode);
-        }
-
         protected override DrawNode CreateDrawNode() => new ArgonBarPathDrawNode(this);
 
         private class ArgonBarPathDrawNode : SpriteDrawNode
