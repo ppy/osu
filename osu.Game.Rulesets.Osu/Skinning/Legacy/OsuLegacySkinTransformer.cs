dfddb077dfee81284854a41ec7db9df588d4a8e6
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2024-01-17 18:35:34 +0300

    Remove unused using directive

--- osu.Game.Rulesets.Osu/Skinning/Legacy/OsuLegacySkinTransformer.cs
+++ osu.Game.Rulesets.Osu/Skinning/Legacy/OsuLegacySkinTransformer.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using NUnit.Framework;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Osu.Objects;
