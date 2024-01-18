ee427160275d93cd5c846fa54056cc767e7bf5ea
smallketchup82 <87902114+bubb13gummy@users.noreply.github.com>
2024-01-18 03:17:37 -0500

    Add tests for DifficultyIcon

---
+++ osu.Game.Tests/Visual/Beatmaps/TestSceneDifficultyIcon.cs
@@ -0,0 +1,42 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Platform;
+using osu.Game.Beatmaps.Drawables;
+using osu.Game.Rulesets;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Tests.Beatmaps;
+
+namespace osu.Game.Tests.Visual.Beatmaps
+{
+    public partial class TestSceneDifficultyIcon : OsuTestScene
+    {
+        [Test]
+        public void createDifficultyIcon()
+        {
+            DifficultyIcon difficultyIcon = null;
+
+            AddStep("create difficulty icon", () =>
+            {
+                Child = difficultyIcon = new DifficultyIcon(new TestBeatmap(new OsuRuleset().RulesetInfo).BeatmapInfo, new OsuRuleset().RulesetInfo)
+                {
+                    ShowTooltip = true,
+                    ShowExtendedTooltip = true
+                };
+            });
+
+            AddStep("hide extended tooltip", () => difficultyIcon.ShowExtendedTooltip = false);
+
+            AddStep("hide tooltip", () => difficultyIcon.ShowTooltip = false);
+
+            AddStep("show tooltip", () => difficultyIcon.ShowTooltip = true);
+
+            AddStep("show extended tooltip", () => difficultyIcon.ShowExtendedTooltip = true);
+        }
+    }
+}
