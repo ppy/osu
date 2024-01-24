b6be380d677cdee642696663a92e3259b9326bed
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-01-24 14:24:33 +0100

    Add `session_verified` attribute to `/me` response

--- osu.Game/Online/API/Requests/Responses/APIMe.cs
+++ osu.Game/Online/API/Requests/Responses/APIMe.cs
@@ -1,9 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using Newtonsoft.Json;
+
 namespace osu.Game.Online.API.Requests.Responses
 {
     public class APIMe : APIUser
     {
+        [JsonProperty("session_verified")]
+        public bool SessionVerified { get; set; }
     }
 }
