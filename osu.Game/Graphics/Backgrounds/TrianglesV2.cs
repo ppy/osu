586091371d1292dc4a357e5396017a3ca7caaa7c
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-01-26 00:39:29 +0900

    Remove unused using statements

--- osu.Game/Graphics/Backgrounds/TrianglesV2.cs
+++ osu.Game/Graphics/Backgrounds/TrianglesV2.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Shaders;
