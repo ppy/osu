19b7cfb1d9d8f0a6aa681e447fcfa39c637eb455
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2024-01-28 18:06:09 +0300

    Stop using SliderBar as a base

--- osu.Game/Screens/Play/HUD/SongProgressBar.cs
+++ osu.Game/Screens/Play/HUD/SongProgressBar.cs
@@ -3,12 +3,14 @@
 
 using System;
 using osu.Framework.Bindables;
-using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Input.Events;
 using osu.Framework.Threading;
+using osuTK;
 
 namespace osu.Game.Screens.Play.HUD
 {
-    public abstract partial class SongProgressBar : SliderBar<double>
+    public abstract partial class SongProgressBar : Container
     {
         /// <summary>
         /// Action which is invoked when a seek is requested, with the proposed millisecond value for the seek operation.
@@ -26,45 +28,66 @@ namespace osu.Game.Screens.Play.HUD
 
         protected readonly BindableBool InteractiveBindable = new BindableBool();
 
-        public double StartTime
+        public double StartTime { get; set; }
+
+        public double EndTime { get; set; } = 1.0;
+
+        public double CurrentTime { get; set; }
+
+        private double length => EndTime - StartTime;
+
+        protected double NormalizedValue => length == 0 ? 1 : Math.Clamp(CurrentTime - StartTime, 0.0, length) / length;
+
+        private bool handleClick;
+
+        protected override bool OnMouseDown(MouseDownEvent e)
         {
-            get => CurrentNumber.MinValue;
-            set => CurrentNumber.MinValue = value;
+            handleClick = true;
+            return base.OnMouseDown(e);
         }
 
-        public double EndTime
+        protected override bool OnClick(ClickEvent e)
         {
-            get => CurrentNumber.MaxValue;
-            set => CurrentNumber.MaxValue = value;
+            if (handleClick)
+                handleMouseInput(e);
+
+            return true;
         }
 
-        public double CurrentTime
+        protected override void OnDrag(DragEvent e)
         {
-            get => CurrentNumber.Value;
-            set => CurrentNumber.Value = value;
+            handleMouseInput(e);
         }
 
-        protected SongProgressBar()
+        protected override bool OnDragStart(DragStartEvent e)
         {
-            StartTime = 0;
-            EndTime = 1;
+            Vector2 posDiff = e.MouseDownPosition - e.MousePosition;
+
+            if (Math.Abs(posDiff.X) < Math.Abs(posDiff.Y))
+            {
+                handleClick = false;
+                return false;
+            }
+
+            handleMouseInput(e);
+            return true;
         }
 
-        protected override void UpdateValue(float value)
+        private void handleMouseInput(UIEvent e)
         {
-            // handled in update
+            if (!Interactive)
+                return;
+
+            double relativeX = Math.Clamp(ToLocalSpace(e.ScreenSpaceMousePosition).X / DrawWidth, 0, 1);
+            onUserChange(StartTime + (EndTime - StartTime) * relativeX);
         }
 
         private ScheduledDelegate? scheduledSeek;
 
-        protected override void OnUserChange(double value)
+        private void onUserChange(double value)
         {
             scheduledSeek?.Cancel();
-            scheduledSeek = Schedule(() =>
-            {
-                if (Interactive)
-                    OnSeek?.Invoke(value);
-            });
+            scheduledSeek = Schedule(() => OnSeek?.Invoke(value));
         }
     }
 }
