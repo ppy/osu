034f8c0388c84333961d5ad6702c044dc83e519f
Dean Herbert <pe@ppy.sh>
2024-01-29 20:36:59 +0900

    Also fix spinner case

--- osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSpinner.cs
+++ osu.Game.Rulesets.Osu/Objects/Drawables/DrawableSpinner.cs
@@ -153,7 +153,7 @@ namespace osu.Game.Rulesets.Osu.Objects.Drawables
         {
             if (tracking.NewValue)
             {
-                if (!spinningSample.IsPlaying)
+                if (!spinningSample.RequestedPlaying)
                     spinningSample.Play();
 
                 spinningSample.VolumeTo(1, 300);
