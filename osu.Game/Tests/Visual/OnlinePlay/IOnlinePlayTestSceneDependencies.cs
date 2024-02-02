20ae88b0a0dd9a23dfbb7982d292e475f3a959c7
Dan Balasescu <smoogipoo@smgi.me>
2024-02-02 21:05:12 +0900

    Revert unnecessary changes

--- osu.Game/Tests/Visual/OnlinePlay/IOnlinePlayTestSceneDependencies.cs
+++ osu.Game/Tests/Visual/OnlinePlay/IOnlinePlayTestSceneDependencies.cs
@@ -16,31 +16,31 @@ namespace osu.Game.Tests.Visual.OnlinePlay
         /// <summary>
         /// The cached <see cref="Room"/>.
         /// </summary>
-        Bindable<Room>? SelectedRoom { get; }
+        Bindable<Room> SelectedRoom { get; }
 
         /// <summary>
         /// The cached <see cref="IRoomManager"/>
         /// </summary>
-        IRoomManager? RoomManager { get; }
+        IRoomManager RoomManager { get; }
 
         /// <summary>
         /// The cached <see cref="OngoingOperationTracker"/>.
         /// </summary>
-        OngoingOperationTracker? OngoingOperationTracker { get; }
+        OngoingOperationTracker OngoingOperationTracker { get; }
 
         /// <summary>
         /// The cached <see cref="OnlinePlayBeatmapAvailabilityTracker"/>.
         /// </summary>
-        OnlinePlayBeatmapAvailabilityTracker? AvailabilityTracker { get; }
+        OnlinePlayBeatmapAvailabilityTracker AvailabilityTracker { get; }
 
         /// <summary>
         /// The cached <see cref="UserLookupCache"/>.
         /// </summary>
-        TestUserLookupCache? UserLookupCache { get; }
+        TestUserLookupCache UserLookupCache { get; }
 
         /// <summary>
         /// The cached <see cref="BeatmapLookupCache"/>.
         /// </summary>
-        BeatmapLookupCache? BeatmapLookupCache { get; }
+        BeatmapLookupCache BeatmapLookupCache { get; }
     }
 }
