0788e095d322af16178e8907ad889b5d84d6507a
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-02-05 18:06:51 +0100

    Use more proper method of formatting

--- osu.Game/Overlays/Settings/Sections/Audio/AudioOffsetAdjustControl.cs
+++ osu.Game/Overlays/Settings/Sections/Audio/AudioOffsetAdjustControl.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.IEnumerableExtensions;
+using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -156,7 +157,7 @@ namespace osu.Game.Overlays.Settings.Sections.Audio
             {
                 hintText.Text = SuggestedOffset.Value == null
                     ? AudioSettingsStrings.SuggestedOffsetNote
-                    : AudioSettingsStrings.SuggestedOffsetValueReceived(averageHitErrorHistory.Count, $"{SuggestedOffset.Value:N0}");
+                    : AudioSettingsStrings.SuggestedOffsetValueReceived(averageHitErrorHistory.Count, SuggestedOffset.Value.ToLocalisableString(@"N0"));
                 applySuggestion.Enabled.Value = SuggestedOffset.Value != null;
             }
 
