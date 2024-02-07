57d5717e6a6d4fb4007e6f42aa7bca525ea176e6
Susko3 <Susko3@protonmail.com>
2024-02-07 21:33:23 +0100

    Remove `Win32Icon` class and use plain strings instead

--- osu.Desktop/Windows/Icons.cs
+++ osu.Desktop/Windows/Icons.cs
@@ -1,10 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.IO;
+
 namespace osu.Desktop.Windows
 {
     public static class Icons
     {
-        public static Win32Icon Lazer => new Win32Icon(@"lazer.ico");
+        /// <summary>
+        /// Fully qualified path to the directory that contains icons (in the installation folder).
+        /// </summary>
+        private static readonly string icon_directory = Path.GetDirectoryName(typeof(Icons).Assembly.Location)!;
+
+        public static string Lazer => Path.Join(icon_directory, "lazer.ico");
     }
 }
