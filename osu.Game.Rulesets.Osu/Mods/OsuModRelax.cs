5101979ac099b7e590e020020bf3b0a7bf134164
James Wilson <tsunyoku@gmail.com>
2024-02-13 00:34:06 +0000

    only use `LegacyReplayPressHandler` on legacy replays

--- osu.Game.Rulesets.Osu/Mods/OsuModRelax.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModRelax.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -61,7 +61,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                 Debug.Assert(ruleset.ReplayScore != null);
                 legacyReplay = ruleset.ReplayScore.ScoreInfo.IsLegacyScore;
 
-                pressHandler = new LegacyReplayPressHandler(this);
+                pressHandler = legacyReplay ? new LegacyReplayPressHandler(this) : new PressHandler(this);
 
                 return;
             }
