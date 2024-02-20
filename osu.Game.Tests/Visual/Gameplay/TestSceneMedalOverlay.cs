12a4cbc35e6acb5c244c3c6c592a6346adcfca2f
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-02-20 13:44:25 +0100

    Make medal overlay respect overlay disable via activation mode

--- osu.Game.Tests/Visual/Gameplay/TestSceneMedalOverlay.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneMedalOverlay.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using Moq;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 using osu.Game.Online.API;
@@ -16,14 +19,26 @@ namespace osu.Game.Tests.Visual.Gameplay
     [TestFixture]
     public partial class TestSceneMedalOverlay : OsuManualInputManagerTestScene
     {
-        private DummyAPIAccess dummyAPI => (DummyAPIAccess)API;
+        private readonly Bindable<OverlayActivation> overlayActivationMode = new Bindable<OverlayActivation>(OverlayActivation.All);
 
+        private DummyAPIAccess dummyAPI => (DummyAPIAccess)API;
         private MedalOverlay overlay = null!;
 
         [SetUpSteps]
         public void SetUpSteps()
         {
-            AddStep("create overlay", () => Child = overlay = new MedalOverlay());
+            var overlayManagerMock = new Mock<IOverlayManager>();
+            overlayManagerMock.Setup(mock => mock.OverlayActivationMode).Returns(overlayActivationMode);
+
+            AddStep("create overlay", () => Child = new DependencyProvidingContainer
+            {
+                Child = overlay = new MedalOverlay(),
+                RelativeSizeAxes = Axes.Both,
+                CachedDependencies =
+                [
+                    (typeof(IOverlayManager), overlayManagerMock.Object)
+                ]
+            });
         }
 
         [Test]
@@ -61,6 +76,22 @@ namespace osu.Game.Tests.Visual.Gameplay
                 Description = "500 big ones! You're moving up in the world!",
                 Slug = @"osu-combo-500"
             });
+        }
+
+        [Test]
+        public void TestDelayMedalDisplayUntilActivationModeAllowsIt()
+        {
+            AddStep("disable overlay activation", () => overlayActivationMode.Value = OverlayActivation.Disabled);
+            awardMedal(new UserAchievementUnlock
+            {
+                Title = "Time And A Half",
+                Description = "Having a right ol' time. One and a half of them, almost.",
+                Slug = @"all-intro-doubletime"
+            });
+            AddUntilStep("overlay hidden", () => overlay.State.Value, () => Is.EqualTo(Visibility.Hidden));
+
+            AddStep("re-enable overlay activation", () => overlayActivationMode.Value = OverlayActivation.All);
+            AddUntilStep("overlay shown", () => overlay.State.Value, () => Is.EqualTo(Visibility.Visible));
         }
 
         private void awardMedal(UserAchievementUnlock unlock) => AddStep("award medal", () => dummyAPI.NotificationsClient.Receive(new SocketMessage
