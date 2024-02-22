6e67b3da00706feef2d7c29463db1a2c73af07b7
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-02-22 11:45:57 +0100

    Enable NRT in `TestSceneDifficultyIcon`

--- osu.Game.Tests/Visual/Beatmaps/TestSceneDifficultyIcon.cs
+++ osu.Game.Tests/Visual/Beatmaps/TestSceneDifficultyIcon.cs
@@ -1,8 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -16,7 +14,7 @@ namespace osu.Game.Tests.Visual.Beatmaps
 {
     public partial class TestSceneDifficultyIcon : OsuTestScene
     {
-        private FillFlowContainer fill;
+        private FillFlowContainer fill = null!;
 
         protected override void LoadComplete()
         {
@@ -35,7 +33,7 @@ namespace osu.Game.Tests.Visual.Beatmaps
         [Test]
         public void CreateDifficultyIcon()
         {
-            DifficultyIcon difficultyIcon = null;
+            DifficultyIcon difficultyIcon = null!;
 
             AddRepeatStep("create difficulty icon", () =>
             {
