add2907b7717b6687ae3edffd598096914069a5f
Hivie <87902114+bubb13gummy@users.noreply.github.com>
2024-02-23 14:01:12 +0100

    use more correct implementation

--- osu.Game.Rulesets.Taiko/Mods/TaikoModConstantSpeed.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModConstantSpeed.cs
@@ -1,15 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Localisation;
 using osu.Framework.Graphics.Sprites;
-using osu.Game.Beatmaps;
+using osu.Framework.Localisation;
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Taiko.Objects;
+using osu.Game.Rulesets.Taiko.UI;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Taiko.Beatmaps;
+using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Taiko.Mods
 {
-    public class TaikoModConstantSpeed : Mod, IApplicableToBeatmap
+    public class TaikoModConstantSpeed : Mod, IApplicableToDrawableRuleset<TaikoHitObject>
     {
         public override string Name => "Constant Speed";
         public override string Acronym => "CS";
@@ -18,14 +20,10 @@ namespace osu.Game.Rulesets.Taiko.Mods
         public override IconUsage? Icon => FontAwesome.Solid.Equals;
         public override ModType Type => ModType.Conversion;
 
-        public void ApplyToBeatmap(IBeatmap beatmap)
+        public void ApplyToDrawableRuleset(DrawableRuleset<TaikoHitObject> drawableRuleset)
         {
-            var taikoBeatmap = (TaikoBeatmap)beatmap;
-
-            foreach (var effectControlPoint in taikoBeatmap.ControlPointInfo.EffectPoints)
-            {
-                effectControlPoint.ScrollSpeed = 1;
-            }
+            var taikoRuleset = (DrawableTaikoRuleset)drawableRuleset;
+            taikoRuleset.VisualisationMethod = ScrollVisualisationMethod.Constant;
         }
     }
 }
