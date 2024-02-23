d6beae2ce1d34d0efb17eaf9ccc531a72a757ecf
Brandon <brandondong96@gmail.com>
2024-02-22 19:10:15 -0800

    Update delete/restore mod presets message when none

--- osu.Game/Localisation/MaintenanceSettingsStrings.cs
+++ osu.Game/Localisation/MaintenanceSettingsStrings.cs
@@ -110,9 +110,19 @@ namespace osu.Game.Localisation
         public static LocalisableString DeletedAllModPresets => new TranslatableString(getKey(@"deleted_all_mod_presets"), @"Deleted all mod presets!");
 
         /// <summary>
+        /// "No mod presets found to delete!"
+        /// </summary>
+        public static LocalisableString NoModPresetsFoundToDelete => new TranslatableString(getKey(@"no_mod_presets_found_to_delete"), @"No mod presets found to delete!");
+
+        /// <summary>
         /// "Restored all deleted mod presets!"
         /// </summary>
         public static LocalisableString RestoredAllDeletedModPresets => new TranslatableString(getKey(@"restored_all_deleted_mod_presets"), @"Restored all deleted mod presets!");
+
+        /// <summary>
+        /// "No mod presets found to restore!"
+        /// </summary>
+        public static LocalisableString NoModPresetsFoundToRestore => new TranslatableString(getKey(@"no_mod_presets_found_to_restore"), @"No mod presets found to restore!");
 
         /// <summary>
         /// "Please select your osu!stable install location"
