e7d8ca3292e4213b248914c7a0416b48b84b2f18
Andrei Zavatski <megaman9919@gmail.com>
2024-02-24 14:22:34 +0300

    Fix Argon and Trianles spinner stutter

--- osu.Game.Rulesets.Osu/Skinning/Argon/ArgonSpinner.cs
+++ osu.Game.Rulesets.Osu/Skinning/Argon/ArgonSpinner.cs
@@ -120,7 +120,7 @@ namespace osu.Game.Rulesets.Osu.Skinning.Argon
         {
             base.Update();
 
-            if (!spmContainer.IsPresent && drawableSpinner.Result?.TimeStarted != null)
+            if (spmContainer.Alpha != 0 && drawableSpinner.Result?.TimeStarted != null)
                 fadeCounterOnTimeStart();
         }
 
