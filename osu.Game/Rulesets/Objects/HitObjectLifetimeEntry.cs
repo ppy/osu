9e3defebda1d446f815cb0cae6618f822ba482d2
Andrei Zavatski <megaman9919@gmail.com>
2024-02-25 19:05:40 +0300

    Remove unused using

--- osu.Game/Rulesets/Objects/HitObjectLifetimeEntry.cs
+++ osu.Game/Rulesets/Objects/HitObjectLifetimeEntry.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics.Performance;
 using osu.Game.Rulesets.Judgements;
