3e9425fdf2d57dbeff8819bca208a63cb947eda9
Dan Balasescu <smoogipoo@smgi.me>
2024-02-26 22:12:04 +0900

    Adjust API of HighPerformanceSession + rename

---
+++ osu.Game/Performance/HighPerformanceSessionManager.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Runtime;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+
+namespace osu.Game.Performance
+{
+    public partial class HighPerformanceSessionManager : Component
+    {
+        private GCLatencyMode originalGCMode;
+
+        public IDisposable BeginSession()
+        {
+            EnableHighPerformanceSession();
+            return new InvokeOnDisposal<HighPerformanceSessionManager>(this, static m => m.DisableHighPerformanceSession());
+        }
+
+        protected virtual void EnableHighPerformanceSession()
+        {
+            originalGCMode = GCSettings.LatencyMode;
+            GCSettings.LatencyMode = GCLatencyMode.LowLatency;
+            GC.Collect(0);
+        }
+
+        protected virtual void DisableHighPerformanceSession()
+        {
+            if (GCSettings.LatencyMode == GCLatencyMode.LowLatency)
+                GCSettings.LatencyMode = originalGCMode;
+        }
+    }
+}
