f51c1dde23e41aad5869e82847b76e6e6bb5915c
Salman Ahmed <frenzibyte@gmail.com>
2024-03-01 17:11:15 +0300

    Add test coverage

--- osu.Game.Tests/Visual/UserInterface/TestSceneTabControl.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneTabControl.cs
@@ -1,34 +1,41 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.ComponentModel;
+using System.Linq;
+using NUnit.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Testing;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Screens.Select.Filter;
 using osuTK;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
-    [Description("SongSelect filter control")]
-    public partial class TestSceneTabControl : OsuTestScene
+    public partial class TestSceneTabControl : OsuManualInputManagerTestScene
     {
-        public TestSceneTabControl()
+        private OsuSpriteText text = null!;
+        private OsuTabControl<GroupMode> filter = null!;
+
+        [SetUp]
+        public void SetUp() => Schedule(() =>
         {
-            OsuSpriteText text;
-            OsuTabControl<GroupMode> filter;
-            Add(filter = new OsuTabControl<GroupMode>
+            Children = new Drawable[]
             {
-                Margin = new MarginPadding(4),
-                Size = new Vector2(229, 24),
-                AutoSort = true
-            });
-            Add(text = new OsuSpriteText
-            {
-                Text = "None",
-                Margin = new MarginPadding(4),
-                Position = new Vector2(275, 5)
-            });
+                filter = new OsuTabControl<GroupMode>
+                {
+                    Margin = new MarginPadding(4),
+                    Size = new Vector2(229, 24),
+                    AutoSort = true
+                },
+                text = new OsuSpriteText
+                {
+                    Text = "None",
+                    Margin = new MarginPadding(4),
+                    Position = new Vector2(275, 5)
+                }
+            };
 
             filter.PinItem(GroupMode.All);
             filter.PinItem(GroupMode.RecentlyPlayed);
@@ -37,6 +44,29 @@ namespace osu.Game.Tests.Visual.UserInterface
             {
                 text.Text = "Currently Selected: " + grouping.NewValue.ToString();
             };
+        });
+
+        [Test]
+        public void TestSelection()
+        {
+            AddStep("select by user click", () =>
+            {
+                filter.Current.SetDefault();
+                InputManager.MoveMouseTo(filter.ChildrenOfType<OsuTabControl<GroupMode>.OsuTabItem>().Single(t => t.Value == GroupMode.RecentlyPlayed));
+                InputManager.Click(MouseButton.Left);
+            });
+
+            AddStep("select programmatically with feedback", () =>
+            {
+                filter.Current.SetDefault();
+                filter.SelectTabWithSound(GroupMode.RecentlyPlayed);
+            });
+
+            AddStep("select programmatically by bindable", () =>
+            {
+                filter.Current.SetDefault();
+                filter.Current.Value = GroupMode.RecentlyPlayed;
+            });
         }
     }
 }
