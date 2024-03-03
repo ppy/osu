736b38cf3a0dd47e76b0e020c73d87534a05d706
Andrei Zavatski <87902114+bubb13gummy@users.noreply.github.com>
2024-03-03 21:54:37 +0300

    Remove unused transform extensions

--- osu.Game/Storyboards/Drawables/IVectorScalable.cs
+++ osu.Game/Storyboards/Drawables/IVectorScalable.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.Transforms;
 using osuTK;
 
@@ -10,12 +9,5 @@ namespace osu.Game.Storyboards.Drawables
     internal interface IVectorScalable : ITransformable
     {
         Vector2 VectorScale { get; set; }
-    }
-
-    internal static class VectorScalableExtensions
-    {
-        public static TransformSequence<T> VectorScaleTo<T>(this T target, Vector2 newVectorScale, double duration = 0, Easing easing = Easing.None)
-            where T : class, IVectorScalable
-            => target.TransformTo(nameof(IVectorScalable.VectorScale), newVectorScale, duration, easing);
     }
 }
