53fffc6a75df70e82be5c1e1a3d2fe633f86c834
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-03-06 07:57:59 +0100

    Remove unused using directives

--- osu.Game/Overlays/Settings/SettingsNumberBox.cs
+++ osu.Game/Overlays/Settings/SettingsNumberBox.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Bindables;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.UserInterface;
