4b5d4635c9432da2f306a727709e3dcb6f1c64ad
Givikap120 <givikap120@gmail.com>
2024-03-07 18:34:07 +0200

    Update Reading.cs

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
@@ -114,6 +114,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             return currentStrain;
         }
 
-        public static double DifficultyToPerformance(double difficulty) => Math.Pow(difficulty, 2) * 25.0;
+        public new static double DifficultyToPerformance(double difficulty) => Math.Pow(difficulty, 2) * 25.0;
     }
 }
