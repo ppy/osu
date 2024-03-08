a8792b35850c53a5946e6e20176282dc33292075
Hivie <rayenattia@gmail.com>
2024-03-08 16:02:17 +0100

    better assertion

--- osu.Game.Rulesets.Taiko/UI/DrawableTaikoRuleset.cs
+++ osu.Game.Rulesets.Taiko/UI/DrawableTaikoRuleset.cs
@@ -72,7 +72,7 @@ namespace osu.Game.Rulesets.Taiko.UI
         protected virtual double ComputeTimeRange()
         {
             // Adjust when we're using constant algorithm to not be sluggish.
-            double multiplier = VisualisationMethod == ScrollVisualisationMethod.Overlapping ? 1 : 4 * Beatmap.Difficulty.SliderMultiplier;
+            double multiplier = VisualisationMethod == ScrollVisualisationMethod.Constant ? 4 * Beatmap.Difficulty.SliderMultiplier : 1;
             return PlayfieldAdjustmentContainer.ComputeTimeRange() / multiplier;
         }
 
