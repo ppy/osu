4e26299a13a582cd94057b5edc27c64fc0ab0e30
Arthur Araujo <87902114+bubb13gummy@users.noreply.github.com>
2024-03-20 15:52:16 -0300

    Add mania keycount check

---
+++ osu.Game.Rulesets.Mania.Tests/Editor/Checks/CheckKeyCountTest.cs
@@ -0,0 +1,75 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Tests.Beatmaps;
+using osu.Game.Rulesets.Mania.Edit.Checks;
+using System.Linq;
+
+namespace osu.Game.Rulesets.Mania.Tests.Editor.Checks
+{
+    [TestFixture]
+    public class CheckKeyCountTest
+    {
+        private CheckKeyCount check = null!;
+
+        private IBeatmap beatmap = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            check = new CheckKeyCount();
+
+            beatmap = new Beatmap<HitObject>()
+            {
+                BeatmapInfo = new BeatmapInfo
+                {
+                    Ruleset = new ManiaRuleset().RulesetInfo
+                }
+            };
+        }
+
+        [Test]
+        public void TestKeycountFour()
+        {
+            beatmap.Difficulty.CircleSize = 4;
+
+            var context = getContext();
+            var issues = check.Run(context).ToList();
+
+            Assert.That(issues, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void TestKeycountSmallerThanFour()
+        {
+            beatmap.Difficulty.CircleSize = 1;
+
+            var context = getContext();
+            var issues = check.Run(context).ToList();
+
+            Assert.That(issues, Has.Count.EqualTo(1));
+            Assert.That(issues.Single().Template is CheckKeyCount.IssueTemplateKeycountTooLow);
+        }
+
+        [Test]
+        public void TestKeycountHigherThanTen()
+        {
+            beatmap.Difficulty.CircleSize = 11;
+
+            var context = getContext();
+            var issues = check.Run(context).ToList();
+
+            Assert.That(issues, Has.Count.EqualTo(1));
+            Assert.That(issues.Single().Template is CheckKeyCount.IssueTemplateKeycountNonStandard);
+        }
+
+        private BeatmapVerifierContext getContext()
+        {
+            return new BeatmapVerifierContext(beatmap, new TestWorkingBeatmap(beatmap));
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Mania/Edit/ManiaBeatmapVerifier.cs
@@ -0,0 +1,24 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Edit.Checks.Components;
+using osu.Game.Rulesets.Mania.Edit.Checks;
+
+namespace osu.Game.Rulesets.Mania.Edit
+{
+    public class ManiaBeatmapVerifier : IBeatmapVerifier
+    {
+        private readonly List<ICheck> checks = new List<ICheck>
+        {
+            new CheckKeyCount(),
+        };
+
+        public IEnumerable<Issue> Run(BeatmapVerifierContext context)
+        {
+            return checks.SelectMany(check => check.Run(context));
+        }
+    }
+}
