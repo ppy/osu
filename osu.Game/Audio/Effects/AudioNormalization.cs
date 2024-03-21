e7366fb0ecc90df017867866068619b04ad5cd71
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-03-21 10:31:25 -0400

    Not sure

--- osu.Game/Audio/Effects/AudioNormalization.cs
+++ osu.Game/Audio/Effects/AudioNormalization.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Runtime.InteropServices;
 using ManagedBass.Loud;
 using ManagedBass;
 
@@ -11,19 +12,53 @@ namespace osu.Game.Audio.Effects
     {
         public AudioNormalization(string file)
         {
+            Bass.Free();
+            Bass.Init();
+            Console.WriteLine("bassloud version: " + BassLoud.Version);
             var idk = Bass.CreateStream(file, 0, 0, BassFlags.Decode | BassFlags.Float);
+            Console.WriteLine("idk: " + idk);
 
-            BassLoud.BASS_Loudness_Start(idk, BassFlags.BassLoudnessIntegrated | BassFlags.BassLoudnessAutofree, 0);
+            if (idk == 0)
+            {
+                Console.WriteLine("cant decode stream");
+                Console.WriteLine(Bass.LastError);
+                return;
+            }
 
-            IntPtr fart = new IntPtr();
-            Bass.ChannelGetData(idk, fart, 0);
+            var loudness = BassLoud.BASS_Loudness_Start(idk, BassFlags.BassLoudnessIntegrated, 0);
+            Console.WriteLine(Bass.LastError);
 
-            float level = 0;
-            BassLoud.BASS_Loudness_GetLevel(idk, BassFlags.BassLoudnessIntegrated, level);
+            Console.WriteLine("loudness: " + loudness);
+
+            if (loudness == 0)
+            {
+                Console.WriteLine("cant start loudness measurement");
+                Console.WriteLine(Bass.LastError);
+                return;
+            }
+
+            int data;
+            while (true)
+            {
+                IntPtr buffer = new IntPtr();
+                data = Bass.ChannelGetData(idk, buffer,Marshal.SizeOf(buffer));
+
+                if (data < 0) break;
+            }
 
             Bass.StreamFree(idk);
 
+            float level = 0;
+            var gotlevel = BassLoud.BASS_Loudness_GetLevel(loudness, BassFlags.BassLoudnessIntegrated, level);
             Console.WriteLine("level: " + level);
+            Console.WriteLine("gotlevel: " + gotlevel);
+
+            if (gotlevel == false)
+            {
+                Console.WriteLine("failed to get level");
+                Console.WriteLine(Bass.LastError);
+            }
+
         }
     }
 }
