eb938272040be14caa292fe65800356a6ae03a1a
Arthur Araujo <arthur.araujo@tutanota.com>
2024-03-23 23:11:13 -0300

    Add missing copyright header

--- osu.Game.Tests/Editing/Checks/CheckVideoResolutionTest.cs
+++ osu.Game.Tests/Editing/Checks/CheckVideoResolutionTest.cs
@@ -1,3 +1,6 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
 using System.IO;
 using System.Linq;
 using Moq;
