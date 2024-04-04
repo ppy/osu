9468031f199db3d847baf213f72c5830c114d5e2
Dan Balasescu <smoogipoo@smgi.me>
2024-04-04 17:28:12 +0900

    Adjust dropdown text

--- osu.Game.Rulesets.Mania/ManiaSkinComponentLookup.cs
+++ osu.Game.Rulesets.Mania/ManiaSkinComponentLookup.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.ComponentModel;
 using osu.Game.Skinning;
 
 namespace osu.Game.Rulesets.Mania
@@ -34,6 +35,8 @@ namespace osu.Game.Rulesets.Mania
         BarLine,
 
         Stage,
+
+        [Description("Touch Overlay")]
         TouchOverlay
     }
 }
