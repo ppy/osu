c6b9aec8ca10c97da954a2152956d2e55699c768
Arthur Araujo <87902114+bubb13gummy@users.noreply.github.com>
2024-04-16 06:15:21 -0300

    Add checks for audio formats

---
+++ osu.Game.Tests/Editing/Checks/CheckSongFormatTest.cs
@@ -0,0 +1,100 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.IO;
+using System.Linq;
+using ManagedBass;
+using Moq;
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Edit;
+using osu.Game.Rulesets.Edit.Checks;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Tests.Beatmaps;
+using osu.Game.Tests.Resources;
+using osuTK.Audio;
+
+namespace osu.Game.Tests.Editing.Checks
+{
+    [TestFixture]
+    public partial class CheckSongFormatTest
+    {
+        private CheckSongFormat check = null!;
+
+        private IBeatmap beatmap = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            check = new CheckSongFormat();
+            beatmap = new Beatmap<HitObject>
+            {
+                BeatmapInfo = new BeatmapInfo
+                {
+                    BeatmapSet = new BeatmapSetInfo
+                    {
+                        Files = { CheckTestHelpers.CreateMockFile("mp3") }
+                    }
+                }
+            };
+
+            // 0 = No output device. This still allows decoding.
+            if (!Bass.Init(0) && Bass.LastError != Errors.Already)
+                throw new AudioException("Could not initialize Bass.");
+        }
+
+        [Test]
+        public void TestMP3Audio()
+        {
+            using (var resourceStream = TestResources.OpenResource("Samples/test-sample-cut.mp3"))
+            {
+                beatmap.Metadata.AudioFile = "abc123.mp3";
+                var issues = check.Run(getContext(resourceStream)).ToList();
+                Assert.That(issues, Has.Count.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public void TestOGGAudio()
+        {
+            using (var resourceStream = TestResources.OpenResource("Samples/test-sample.ogg"))
+            {
+                beatmap.Metadata.AudioFile = "abc123.mp3";
+                var issues = check.Run(getContext(resourceStream)).ToList();
+                Assert.That(issues, Has.Count.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public void TestWAVAudio()
+        {
+            using (var resourceStream = TestResources.OpenResource("Samples/hitsound-delay.wav"))
+            {
+                beatmap.Metadata.AudioFile = "abc123.mp3";
+                var issues = check.Run(getContext(resourceStream)).ToList();
+                Assert.That(issues, Has.Count.EqualTo(1));
+                Assert.That(issues.Single().Template is CheckSongFormat.IssueTemplateIncorrectFormat);
+            }
+        }
+
+        [Test]
+        public void TestWEBMAudio()
+        {
+            using (var resourceStream = TestResources.OpenResource("Samples/test-sample.webm"))
+            {
+                beatmap.Metadata.AudioFile = "abc123.mp3";
+                var issues = check.Run(getContext(resourceStream)).ToList();
+                Assert.That(issues, Has.Count.EqualTo(1));
+                Assert.That(issues.Single().Template is CheckSongFormat.IssueTemplateFormatUnsupported);
+            }
+        }
+
+        private BeatmapVerifierContext getContext(Stream? resourceStream)
+        {
+            var mockWorkingBeatmap = new Mock<TestWorkingBeatmap>(beatmap, null, null);
+            mockWorkingBeatmap.Setup(w => w.GetStream(It.IsAny<string>())).Returns(resourceStream);
+
+            return new BeatmapVerifierContext(beatmap, mockWorkingBeatmap.Object);
+        }
+    }
+}
---
+++ osu.Game/Rulesets/Edit/Checks/CheckHitsoundsFormat.cs
@@ -0,0 +1,93 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ManagedBass;
+using osu.Framework.Audio.Callbacks;
+using osu.Game.Beatmaps;
+using osu.Game.Extensions;
+using osu.Game.Rulesets.Edit.Checks.Components;
+
+namespace osu.Game.Rulesets.Edit.Checks
+{
+    public class CheckHitsoundsFormat : ICheck
+    {
+        public CheckMetadata Metadata => new CheckMetadata(CheckCategory.Audio, "Checks for hitsound formats.");
+
+        public IEnumerable<IssueTemplate> PossibleTemplates => new IssueTemplate[]
+        {
+            new IssueTemplateFormatUnsupported(this),
+            new IssueTemplateIncorrectFormat(this),
+        };
+
+        private IEnumerable<ChannelType> allowedFormats => new ChannelType[]
+        {
+            ChannelType.WavePCM,
+            ChannelType.WaveFloat,
+            ChannelType.OGG,
+            ChannelType.Wave | ChannelType.OGG,
+        };
+
+        public IEnumerable<Issue> Run(BeatmapVerifierContext context)
+        {
+            var beatmapSet = context.Beatmap.BeatmapInfo.BeatmapSet;
+            var audioFile = beatmapSet?.GetFile(context.Beatmap.Metadata.AudioFile);
+
+            if (beatmapSet == null) yield break;
+
+            foreach (var file in beatmapSet.Files)
+            {
+                if (audioFile != null && file.File == audioFile.File) continue;
+
+                using (Stream data = context.WorkingBeatmap.GetStream(file.File.GetStoragePath()))
+                {
+                    if (data == null)
+                        continue;
+
+                    var fileCallbacks = new FileCallbacks(new DataStreamFileProcedures(data));
+                    int decodeStream = Bass.CreateStream(StreamSystem.NoBuffer, BassFlags.Decode | BassFlags.Prescan, fileCallbacks.Callbacks, fileCallbacks.Handle);
+
+                    // If the format is not supported by BASS
+                    if (decodeStream == 0)
+                    {
+                        if (AudioCheckUtils.HasAudioExtension(file.Filename) && probablyHasAudioData(data))
+                            yield return new IssueTemplateFormatUnsupported(this).Create(file.Filename);
+
+                        continue;
+                    }
+
+                    var audioInfo = Bass.ChannelGetInfo(decodeStream);
+
+                    if (!allowedFormats.Contains(audioInfo.ChannelType))
+                    {
+                        yield return new IssueTemplateIncorrectFormat(this).Create(file.Filename, audioInfo.ChannelType.ToString());
+                    }
+                }
+            }
+        }
+
+        private bool probablyHasAudioData(Stream data) => data.Length > 100;
+
+        public class IssueTemplateFormatUnsupported : IssueTemplate
+        {
+            public IssueTemplateFormatUnsupported(ICheck check)
+                : base(check, IssueType.Problem, "\"{0}\" is using a unsupported format; Use wav or ogg for hitsounds.")
+            {
+            }
+
+            public Issue Create(string file) => new Issue(this, file);
+        }
+
+        public class IssueTemplateIncorrectFormat : IssueTemplate
+        {
+            public IssueTemplateIncorrectFormat(ICheck check)
+                : base(check, IssueType.Problem, "\"{0}\" is using a incorrect format ({1}); Use wav or ogg for hitsounds.")
+            {
+            }
+
+            public Issue Create(string file, string format) => new Issue(this, file, format);
+        }
+    }
+}
