ppy/osu

commit bce3bd55e5a863e52f41598c306a248a79638843

Fix catch by moving cursor-specific handling local

osu.Game.Rulesets.Osu/UI/OsuResumeOverlay.cs
@@ -39,6 +39,13 @@ namespace osu.Game.Rulesets.Osu.UI
 
         protected override void PopIn()
         {
+            // Can't display if the cursor is outside the window.
+            if (GameplayCursor.LastFrameState == Visibility.Hidden || !Contains(GameplayCursor.ActiveCursor.ScreenSpaceDrawQuad.Centre))
+            {
+                Resume();
+                return;
+            }
+
             base.PopIn();
 
             GameplayCursor.ActiveCursor.Hide();

osu.Game.Rulesets.Osu.Tests/TestSceneResumeOverlay.cs
@@ -6,11 +6,11 @@ using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Cursor;
 using osu.Framework.Testing;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.Osu.UI.Cursor;
+using osu.Game.Rulesets.UI;
 using osu.Game.Screens.Play;
 using osu.Game.Tests.Gameplay;
 using osu.Game.Tests.Visual;
@@ -21,7 +21,7 @@ namespace osu.Game.Rulesets.Osu.Tests
     public partial class TestSceneResumeOverlay : OsuManualInputManagerTestScene
     {
         private ManualOsuInputManager osuInputManager = null!;
-        private CursorContainer cursor = null!;
+        private GameplayCursorContainer cursor = null!;
         private ResumeOverlay resume = null!;
 
         private bool resumeFired;
@@ -99,7 +99,17 @@ namespace osu.Game.Rulesets.Osu.Tests
 
         private void loadContent()
         {
-            Child = osuInputManager = new ManualOsuInputManager(new OsuRuleset().RulesetInfo) { Children = new Drawable[] { cursor = new CursorContainer(), resume = new OsuResumeOverlay { GameplayCursor = cursor }, } };
+            Child = osuInputManager = new ManualOsuInputManager(new OsuRuleset().RulesetInfo)
+            {
+                Children = new Drawable[]
+                {
+                    cursor = new GameplayCursorContainer(),
+                    resume = new OsuResumeOverlay
+                    {
+                        GameplayCursor = cursor
+                    },
+                }
+            };
 
             resumeFired = false;
             resume.ResumeAction = () => resumeFired = true;

osu.Game/Rulesets/UI/DrawableRuleset.cs
@@ -227,7 +227,7 @@ namespace osu.Game.Rulesets.UI
 
         public override void RequestResume(Action continueResume)
         {
-            if (ResumeOverlay != null && UseResumeOverlay && (Cursor == null || (Cursor.LastFrameState == Visibility.Visible && Contains(Cursor.ActiveCursor.ScreenSpaceDrawQuad.Centre))))
+            if (ResumeOverlay != null && UseResumeOverlay)
             {
                 ResumeOverlay.GameplayCursor = Cursor;
                 ResumeOverlay.ResumeAction = continueResume;

osu.Game/Screens/Play/ResumeOverlay.cs
@@ -11,6 +11,7 @@ using osu.Framework.Graphics.Cursor;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
+using osu.Game.Rulesets.UI;
 using osuTK;
 using osuTK.Graphics;
 
@@ -21,7 +22,7 @@ namespace osu.Game.Screens.Play
     /// </summary>
     public abstract partial class ResumeOverlay : VisibilityContainer
     {
-        public CursorContainer GameplayCursor { get; set; }
+        public GameplayCursorContainer GameplayCursor { get; set; }
 
         /// <summary>
         /// The action to be performed to complete resuming.

commit e5e345712efea6467ae895219cad53a461403672

Fix resume overlay not appearing after pausing inside window but outside of actual playfield area
Related to https://github.com/ppy/osu/discussions/27871 (although does
not actually fix the issue with the pause button, _if_ it is to be
considered an issue - the problem there is that the gameplay cursor
gets hidden, so the other condition in the modified check takes over).

Regressed in
https://github.com/ppy/osu/commit/bce3bd55e5a863e52f41598c306a248a79638843.
Reasoning for breakage is silent change in `this` when moving the
`Contains()` check (`DrawableRuleset` will encompass screen bounds,
while `OsuResumeOverlay` is only as big as the actual playfield).

osu.Game.Rulesets.Osu/UI/OsuResumeOverlay.cs
@@ -10,6 +10,7 @@ using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
 using osu.Game.Rulesets.Osu.UI.Cursor;
+using osu.Game.Rulesets.UI;
 using osu.Game.Screens.Play;
 using osuTK.Graphics;
 
@@ -26,6 +27,9 @@ namespace osu.Game.Rulesets.Osu.UI
 
         protected override LocalisableString Message => "Click the orange cursor to resume";
 
+        [Resolved]
+        private DrawableRuleset? drawableRuleset { get; set; }
+
         [BackgroundDependencyLoader]
         private void load()
         {
@@ -38,7 +42,7 @@ namespace osu.Game.Rulesets.Osu.UI
         protected override void PopIn()
         {
             // Can't display if the cursor is outside the window.
-            if (GameplayCursor.LastFrameState == Visibility.Hidden || !Contains(GameplayCursor.ActiveCursor.ScreenSpaceDrawQuad.Centre))
+            if (GameplayCursor.LastFrameState == Visibility.Hidden || drawableRuleset?.Contains(GameplayCursor.ActiveCursor.ScreenSpaceDrawQuad.Centre) == false)
             {
                 Resume();
                 return;