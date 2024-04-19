709bf7cc4e674533695da9e5cfd2d4b34001d397
Arthur Araujo <87902114+bubb13gummy@users.noreply.github.com>
2024-04-19 07:38:55 -0300

    Remove unsued import and add a blankline before `yield break;`

--- osu.Game/Rulesets/Edit/Checks/CheckSongFormat.cs
+++ osu.Game/Rulesets/Edit/Checks/CheckSongFormat.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -48,6 +47,7 @@ namespace osu.Game.Rulesets.Edit.Checks
                 if (decodeStream == 0)
                 {
                     yield return new IssueTemplateFormatUnsupported(this).Create(audioFile.Filename);
+
                     yield break;
                 }
 
