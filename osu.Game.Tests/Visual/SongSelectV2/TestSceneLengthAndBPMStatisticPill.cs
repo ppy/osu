0c3662bff271317c4fc717f3258c778d43f1b1ca
Joseph Madamba <madamba.joehu@outlook.com>
2024-04-25 13:29:44 -0700

    Add length and bpm statistic pill

---
+++ osu.Game.Tests/Visual/SongSelectV2/TestSceneLengthAndBPMStatisticPill.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Testing;
+using osu.Game.Beatmaps;
+using osu.Game.Graphics;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Overlays;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu.Mods;
+using osu.Game.Screens.Select;
+
+namespace osu.Game.Tests.Visual.SongSelectV2
+{
+    public partial class TestSceneLengthAndBPMStatisticPill : SongSelectComponentsTestScene
+    {
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Aquamarine);
+
+        [Cached(typeof(IBindable<IBeatmapInfo?>))]
+        private readonly Bindable<IBeatmapInfo?> beatmapInfo = new Bindable<IBeatmapInfo?>();
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            Beatmap.BindValueChanged(b =>
+            {
+                beatmapInfo.Value = b.NewValue.BeatmapInfo;
+            }, true);
+        }
+
+        [SetUpSteps]
+        public override void SetUpSteps()
+        {
+            base.SetUpSteps();
+
+            AddStep("set pill", () => Child = new LengthAndBPMStatisticPill());
+        }
+
+        [Test]
+        public void TestLocalBeatmap()
+        {
+            AddStep("set beatmap", () => Beatmap.Value = CreateWorkingBeatmap(new Beatmap
+            {
+                BeatmapInfo = new BeatmapInfo
+                {
+                    Length = 83000,
+                    // Drain not tested
+                    // BPM is 60
+                    OnlineID = 1,
+                },
+            }));
+
+            AddStep("set double time", () => SelectedMods.Value = new[] { new OsuModDoubleTime() });
+
+            AddAssert("length value is red", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(0).ValueColour,
+                () => Is.EqualTo(colours.ForModType(ModType.DifficultyIncrease)));
+            AddAssert("bpm value is red", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(1).ValueColour,
+                () => Is.EqualTo(colours.ForModType(ModType.DifficultyIncrease)));
+
+            AddStep("set half time", () => SelectedMods.Value = new[] { new OsuModHalfTime() });
+
+            AddAssert("length value is green", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(0).ValueColour,
+                () => Is.EqualTo(colours.ForModType(ModType.DifficultyReduction)));
+            AddAssert("bpm value is green", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(1).ValueColour,
+                () => Is.EqualTo(colours.ForModType(ModType.DifficultyReduction)));
+        }
+
+        [Test]
+        public void TestAPIBeatmap()
+        {
+            AddStep("set beatmap", () => beatmapInfo.Value = new APIBeatmap
+            {
+                Length = 83000,
+                HitLength = 62000,
+                BPM = 321,
+                OnlineID = 2,
+            });
+
+            AddStep("set double time", () => SelectedMods.Value = new[] { new OsuModDoubleTime() });
+
+            AddAssert("length value color not changed", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(0).ValueColour,
+                () => Is.EqualTo(colourProvider.Content2));
+            AddAssert("bpm value color not changed", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(1).ValueColour,
+                () => Is.EqualTo(colourProvider.Content2));
+
+            AddStep("set half time", () => SelectedMods.Value = new[] { new OsuModHalfTime() });
+
+            AddAssert("length value color not changed", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(0).ValueColour,
+                () => Is.EqualTo(colourProvider.Content2));
+            AddAssert("bpm value color not changed", () => this.ChildrenOfType<LengthAndBPMStatisticPill.PillStatistic>().ElementAt(1).ValueColour,
+                () => Is.EqualTo(colourProvider.Content2));
+        }
+    }
+}
---
+++ osu.Game/Screens/Select/LengthAndBPMStatisticPill.cs
@@ -0,0 +1,222 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.LocalisationExtensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Localisation;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
+using osu.Game.Extensions;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Overlays;
+using osu.Game.Resources.Localisation.Web;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Utils;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Game.Screens.Select
+{
+    public partial class LengthAndBPMStatisticPill : CompositeDrawable
+    {
+        private PillStatistic lengthStatistic = null!;
+        private PillStatistic bpmStatistic = null!;
+
+        private ModSettingChangeTracker? settingChangeTracker;
+
+        [Resolved]
+        private IBindable<IBeatmapInfo?> beatmapInfo { get; set; } = null!;
+
+        [Resolved]
+        private IBindable<IReadOnlyList<Mod>> mods { get; set; } = null!;
+
+        [Resolved]
+        private Bindable<WorkingBeatmap> workingBeatmap { get; set; } = null!;
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        [Resolved]
+        private OverlayColourProvider colourProvider { get; set; } = null!;
+
+        public LengthAndBPMStatisticPill()
+        {
+            AutoSizeAxes = Axes.X;
+            Height = 20;
+            CornerRadius = 10;
+            Masking = true;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OverlayColourProvider colourProvider)
+        {
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.Background4
+                },
+                new FillFlowContainer
+                {
+                    AutoSizeAxes = Axes.X,
+                    RelativeSizeAxes = Axes.Y,
+                    Direction = FillDirection.Horizontal,
+                    Spacing = new Vector2(10),
+                    Padding = new MarginPadding { Horizontal = 20 },
+                    Children = new Drawable[]
+                    {
+                        lengthStatistic = new PillStatistic(new BeatmapStatistic { Name = "Length" })
+                        {
+                            Value = "-"
+                        },
+                        bpmStatistic = new PillStatistic(new BeatmapStatistic { Name = BeatmapsetsStrings.ShowStatsBpm })
+                        {
+                            Value = "-"
+                        },
+                    }
+                }
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            beatmapInfo.BindValueChanged(b => updateStatistics());
+
+            mods.BindValueChanged(m =>
+            {
+                // only valid in song select context
+                if (beatmapInfo.Value is not BeatmapInfo) return;
+
+                settingChangeTracker?.Dispose();
+
+                updateStatistics();
+
+                settingChangeTracker = new ModSettingChangeTracker(m.NewValue);
+                settingChangeTracker.SettingChanged += _ => updateStatistics();
+            }, true);
+        }
+
+        private void updateStatistics()
+        {
+            switch (beatmapInfo.Value)
+            {
+                case BeatmapInfo:
+                    // TODO: consider mods which apply variable rates.
+                    double rate = 1;
+                    foreach (var mod in mods.Value.OfType<IApplicableToRate>())
+                        rate = mod.ApplyToRate(0, rate);
+
+                    var beatmap = workingBeatmap.Value.Beatmap;
+
+                    int bpmMax = FormatUtils.RoundBPM(beatmap.ControlPointInfo.BPMMaximum, rate);
+                    int bpmMin = FormatUtils.RoundBPM(beatmap.ControlPointInfo.BPMMinimum, rate);
+                    int mostCommonBPM = FormatUtils.RoundBPM(60000 / beatmap.GetMostCommonBeatLength(), rate);
+
+                    string labelText = bpmMin == bpmMax
+                        ? $"{bpmMin}"
+                        : $"{bpmMin}-{bpmMax} (mostly {mostCommonBPM})";
+
+                    bpmStatistic.Value = labelText;
+
+                    double drainLength = Math.Round(beatmap.CalculateDrainLength() / rate);
+                    double hitLength = Math.Round(beatmap.BeatmapInfo.Length / rate);
+
+                    lengthStatistic.Value = hitLength.ToFormattedDuration();
+                    lengthStatistic.TooltipText = BeatmapsetsStrings.ShowStatsTotalLength(drainLength.ToFormattedDuration());
+
+                    bpmStatistic.ValueColour = getColourByRate(rate);
+                    lengthStatistic.ValueColour = getColourByRate(rate);
+                    break;
+
+                case APIBeatmap apiBeatmap:
+                    lengthStatistic.Value = apiBeatmap.Length.ToFormattedDuration();
+                    lengthStatistic.TooltipText = BeatmapsetsStrings.ShowStatsTotalLength(apiBeatmap.HitLength.ToFormattedDuration());
+                    bpmStatistic.Value = apiBeatmap.BPM.ToLocalisableString(@"0.##");
+                    break;
+            }
+        }
+
+        private Color4 getColourByRate(double rate)
+        {
+            switch (rate)
+            {
+                case < 1:
+                    return colours.ForModType(ModType.DifficultyReduction);
+
+                case > 1:
+                    return colours.ForModType(ModType.DifficultyIncrease);
+
+                default:
+                    return colourProvider.Content2;
+            }
+        }
+
+        public partial class PillStatistic : FillFlowContainer, IHasTooltip
+        {
+            private readonly BeatmapStatistic statistic;
+            private OsuSpriteText valueText = null!;
+
+            public LocalisableString Value
+            {
+                set => Schedule(() => valueText.Text = LocalisableString.IsNullOrEmpty(value) ? "-" : value);
+            }
+
+            public Color4 ValueColour
+            {
+                get => valueText.Colour;
+                set => Schedule(() => valueText.Colour = value);
+            }
+
+            public PillStatistic(BeatmapStatistic statistic)
+            {
+                this.statistic = statistic;
+
+                Anchor = Anchor.CentreLeft;
+                Origin = Anchor.CentreLeft;
+                AutoSizeAxes = Axes.Both;
+                Direction = FillDirection.Horizontal;
+                Spacing = new Vector2(5);
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OverlayColourProvider colourProvider)
+            {
+                Children = new Drawable[]
+                {
+                    new OsuSpriteText
+                    {
+                        Text = statistic.Name,
+                        Font = OsuFont.GetFont(weight: FontWeight.SemiBold, size: 14),
+                    },
+                    valueText = new OsuSpriteText
+                    {
+                        Font = OsuFont.GetFont(weight: FontWeight.SemiBold, size: 14),
+                        Text = statistic.Content,
+                        Colour = colourProvider.Content2,
+                    }
+                };
+            }
+
+            public LocalisableString TooltipText { get; set; }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            settingChangeTracker?.Dispose();
+        }
+    }
+}
