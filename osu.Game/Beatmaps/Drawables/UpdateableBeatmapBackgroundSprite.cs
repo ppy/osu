d1a50ff85bbfb4a502bd4df25a53d93386d7f920
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-05-03 08:11:00 +0200

    Remove redundant conditional access

--- osu.Game/Beatmaps/Drawables/UpdateableBeatmapBackgroundSprite.cs
+++ osu.Game/Beatmaps/Drawables/UpdateableBeatmapBackgroundSprite.cs
@@ -56,7 +56,7 @@ namespace osu.Game.Beatmaps.Drawables
                 return Empty();
 
             // prefer online cover where available.
-            if (model?.BeatmapSet is IBeatmapSetOnlineInfo online)
+            if (model.BeatmapSet is IBeatmapSetOnlineInfo online)
                 return new OnlineBeatmapSetCover(online, beatmapSetCoverType);
 
             if (model is BeatmapInfo localModel)
