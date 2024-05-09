8d2ebd3fb5137e3a28d70bf8aef4cd596301794f
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-05-09 22:17:00 +0800

    Avoid weird codestyle rules

--- osu.Desktop/Program.cs
+++ osu.Desktop/Program.cs
@@ -107,11 +107,13 @@ namespace osu.Desktop
                 }
             }
 
-            using (DesktopGameHost host = Host.GetSuitableDesktopHost(gameName, new HostOptions
-                   {
-                       IPCPort = !tournamentClient ? OsuGame.IPC_PORT : null,
-                       FriendlyGameName = OsuGameBase.GAME_NAME,
-                   }))
+            var hostOptions = new HostOptions
+            {
+                IPCPort = !tournamentClient ? OsuGame.IPC_PORT : null,
+                FriendlyGameName = OsuGameBase.GAME_NAME,
+            };
+
+            using (DesktopGameHost host = Host.GetSuitableDesktopHost(gameName, hostOptions))
             {
                 if (!host.IsPrimaryInstance)
                 {
