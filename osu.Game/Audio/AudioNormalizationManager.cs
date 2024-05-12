89aabaeff21196f338ee621d971bb74130882299
hwsmm <hwsnemo@gmail.com>
2024-05-12 14:08:17 +0900

    Add docs in AudioNormalizationManager

--- osu.Game/Audio/AudioNormalizationManager.cs
+++ osu.Game/Audio/AudioNormalizationManager.cs
@@ -2,20 +2,38 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Audio;
+using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
+using osu.Framework.Logging;
 using osu.Game.Configuration;
+using osu.Game.Overlays;
 
 namespace osu.Game.Audio
 {
+    /// <summary>
+    /// Provides bindables for track/sample volume.
+    /// </summary>
     public class AudioNormalizationManager
     {
         private readonly Bindable<bool> beatmapHitsoundBind;
 
-        // drop track volume game-wide to leave some head-room for UI effects / samples.
+        /// <summary>
+        /// Default <see cref="ITrackStore"/> will bind to this bindable.
+        /// <see cref="MusicController"/> updates this on every track change.
+        /// It's set to 0.8 by default to leave some head-room for UI effects / samples.
+        /// </summary>
         public readonly BindableDouble TrackNormalizeVolume = new BindableDouble(0.8);
 
+        /// <summary>
+        /// Samples assigned to hitobjects needs to bind to this bindable to normalize their volume in line with track.
+        /// </summary>
         public readonly BindableDouble SampleNormalizeVolume = new BindableDouble(1.0);
 
+        /// <summary>
+        /// Creates a new <see cref="AudioNormalizationManager"/>.
+        /// </summary>
+        /// <param name="audioManager"></param>
+        /// <param name="config"></param>
         public AudioNormalizationManager(AudioManager audioManager, OsuConfigManager config)
         {
             audioManager.Tracks.AddAdjustment(AdjustableProperty.Volume, TrackNormalizeVolume);
@@ -24,6 +42,8 @@ namespace osu.Game.Audio
 
             normalizeSampleIfTrue(beatmapHitsoundBind.Value);
             beatmapHitsoundBind.BindValueChanged(change => normalizeSampleIfTrue(change.NewValue));
+
+            TrackNormalizeVolume.BindValueChanged(va => Logger.Log($"New track loudness normalization value is {va.NewValue}", level: LogLevel.Debug));
         }
 
         private void normalizeSampleIfTrue(bool value)
