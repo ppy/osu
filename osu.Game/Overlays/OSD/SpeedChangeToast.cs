57da4229ff621a12a43ae704bbd21884a9039d74
Fabian van Oeffelt <fabiano2002@gmx.de>
2024-05-22 13:58:59 +0200

    Add speed value to Toast

--- osu.Game/Overlays/OSD/SpeedChangeToast.cs
+++ osu.Game/Overlays/OSD/SpeedChangeToast.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Threading;
 using osu.Game.Configuration;
 using osu.Game.Input.Bindings;
 using osu.Game.Localisation;
@@ -9,8 +10,8 @@ namespace osu.Game.Overlays.OSD
 {
     public partial class SpeedChangeToast : Toast
     {
-        public SpeedChangeToast(OsuConfigManager config, double delta)
-            : base(CommonStrings.Beatmaps, ToastStrings.SpeedChanged, config.LookupKeyBindings(GlobalAction.IncreaseModSpeed) + " / " + config.LookupKeyBindings(GlobalAction.DecreaseModSpeed))
+        public SpeedChangeToast(OsuConfigManager config, double newSpeed)
+            : base(CommonStrings.Beatmaps, ToastStrings.SpeedChangedTo + " " + newSpeed.ToString(Thread.CurrentThread.CurrentCulture), config.LookupKeyBindings(GlobalAction.IncreaseModSpeed) + " / " + config.LookupKeyBindings(GlobalAction.DecreaseModSpeed))
         {
         }
     }
