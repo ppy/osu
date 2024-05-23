636061442e884a7e306e148ca56491004544242b
danielthirtle <drthirtle@gmail.com>
2024-05-24 00:52:02 +1200

    adjust multiplier

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -11,7 +11,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     public static class AimEvaluator
     {
         private const double wide_angle_multiplier = 1.7;
-        private const double acute_angle_multiplier = 2.727;
+        private const double acute_angle_multiplier = 3.2;
         private const double slider_multiplier = 1.35;
         private const double velocity_change_multiplier = 0.75;
 
