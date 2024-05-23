ccf8473aae70b4898c7289c2601a07e418e23257
Susko3 <Susko3@protonmail.com>
2024-05-23 13:00:18 +0200

    Use appropriate `SDL_ShowSimpleMessageBox`

--- osu.Desktop/Program.cs
+++ osu.Desktop/Program.cs
@@ -28,6 +28,14 @@ namespace osu.Desktop
 
         private static LegacyTcpIpcProvider? legacyIpc;
 
+        private static unsafe void showMessageBox(string title, string message)
+        {
+            if (FrameworkEnvironment.UseSDL3)
+                SDL3.SDL_ShowSimpleMessageBox(SDL_MessageBoxFlags.SDL_MESSAGEBOX_ERROR, title, message, null);
+            else
+                SDL2.SDL.SDL_ShowSimpleMessageBox(SDL2.SDL.SDL_MessageBoxFlags.SDL_MESSAGEBOX_ERROR, title, message, IntPtr.Zero);
+        }
+
         [STAThread]
         public static void Main(string[] args)
         {
@@ -52,19 +60,15 @@ namespace osu.Desktop
                 // See https://www.mongodb.com/docs/realm/sdk/dotnet/compatibility/
                 if (windowsVersion.Major < 6 || (windowsVersion.Major == 6 && windowsVersion.Minor <= 2))
                 {
-                    unsafe
-                    {
-                        // If users running in compatibility mode becomes more of a common thing, we may want to provide better guidance or even consider
-                        // disabling it ourselves.
-                        // We could also better detect compatibility mode if required:
-                        // https://stackoverflow.com/questions/10744651/how-i-can-detect-if-my-application-is-running-under-compatibility-mode#comment58183249_10744730
-                        SDL3.SDL_ShowSimpleMessageBox(SDL_MessageBoxFlags.SDL_MESSAGEBOX_ERROR,
-                            "Your operating system is too old to run osu!"u8,
-                            "This version of osu! requires at least Windows 8.1 to run.\n"u8
-                            + "Please upgrade your operating system or consider using an older version of osu!.\n\n"u8
-                            + "If you are running a newer version of windows, please check you don't have \"Compatibility mode\" turned on for osu!"u8, null);
-                        return;
-                    }
+                    // If users running in compatibility mode becomes more of a common thing, we may want to provide better guidance or even consider
+                    // disabling it ourselves.
+                    // We could also better detect compatibility mode if required:
+                    // https://stackoverflow.com/questions/10744651/how-i-can-detect-if-my-application-is-running-under-compatibility-mode#comment58183249_10744730
+                    showMessageBox("Your operating system is too old to run osu!",
+                        "This version of osu! requires at least Windows 8.1 to run.\n"
+                        + "Please upgrade your operating system or consider using an older version of osu!.\n\n"
+                        + "If you are running a newer version of windows, please check you don't have \"Compatibility mode\" turned on for osu!");
+                    return;
                 }
 
                 setupSquirrel();
