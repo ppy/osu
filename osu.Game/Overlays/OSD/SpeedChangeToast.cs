345fb60679c3017f75b1b2d9dd54c210fde50a25
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-05-24 13:08:17 +0200

    Fix toast strings

--- osu.Game/Overlays/OSD/SpeedChangeToast.cs
+++ osu.Game/Overlays/OSD/SpeedChangeToast.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Threading;
 using osu.Game.Configuration;
 using osu.Game.Input.Bindings;
 using osu.Game.Localisation;
@@ -11,7 +10,7 @@ namespace osu.Game.Overlays.OSD
     public partial class SpeedChangeToast : Toast
     {
         public SpeedChangeToast(OsuConfigManager config, double newSpeed)
-            : base(CommonStrings.Beatmaps, ToastStrings.SpeedChangedTo + " " + newSpeed.ToString(Thread.CurrentThread.CurrentCulture), config.LookupKeyBindings(GlobalAction.IncreaseModSpeed) + " / " + config.LookupKeyBindings(GlobalAction.DecreaseModSpeed))
+            : base(ModSelectOverlayStrings.ModCustomisation, ToastStrings.SpeedChangedTo(newSpeed), config.LookupKeyBindings(GlobalAction.IncreaseModSpeed) + " / " + config.LookupKeyBindings(GlobalAction.DecreaseModSpeed))
         {
         }
     }
