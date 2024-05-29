17145673421118baee2d6971277771625512a78a
Nathen <nathenbr@gmail.com>
2024-05-29 09:40:39 -0400

    Save deviation calculations to variables

--- osu.Game.Rulesets.Taiko/Difficulty/TaikoPerformanceCalculator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/TaikoPerformanceCalculator.cs
@@ -129,6 +129,14 @@ namespace osu.Game.Rulesets.Taiko.Difficulty
 
             const double z = 2.32634787404; // 99% critical value for the normal distribution (one-tailed).
 
+            double? deviationGreatWindow = calcDeviationGreatWindow();
+            double? deviationGoodWindow = calcDeviationGoodWindow();
+
+            if (deviationGreatWindow is null)
+                return deviationGoodWindow;
+
+            return Math.Min(deviationGreatWindow.Value, deviationGoodWindow!.Value);
+
             // The upper bound on deviation, calculated with the ratio of 300s to objects, and the great hit window.
             double? calcDeviationGreatWindow()
             {
@@ -163,11 +171,6 @@ namespace osu.Game.Rulesets.Taiko.Difficulty
                 // We can be 99% confident that the deviation is not higher than:
                 return h100 / (Math.Sqrt(2) * SpecialFunctions.ErfInv(pLowerBound));
             }
-
-            if (calcDeviationGreatWindow() is null)
-                return calcDeviationGoodWindow();
-
-            return Math.Min(calcDeviationGreatWindow()!.Value, calcDeviationGoodWindow()!.Value);
         }
 
         private int totalHits => countGreat + countOk + countMeh + countMiss;
