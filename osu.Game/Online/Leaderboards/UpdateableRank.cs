8510c8dd7bab7e45019a48ba5b559cc65c4a83e4
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-06-07 16:54:12 +0800

    Apply NRT to `UpdateableRank`

--- osu.Game/Online/Leaderboards/UpdateableRank.cs
+++ osu.Game/Online/Leaderboards/UpdateableRank.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Scoring;
@@ -22,7 +20,7 @@ namespace osu.Game.Online.Leaderboards
             Rank = rank;
         }
 
-        protected override Drawable CreateDrawable(ScoreRank? rank)
+        protected override Drawable? CreateDrawable(ScoreRank? rank)
         {
             if (rank.HasValue)
             {
