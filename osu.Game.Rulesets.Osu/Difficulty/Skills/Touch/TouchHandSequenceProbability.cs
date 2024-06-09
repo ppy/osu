f4eead4f78c90152d3d22ae3b479ad4dd189adf8
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-06-09 09:51:17 +0800

    Document `TouchHandSequenceProbability`

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Touch/TouchHandSequenceProbability.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Touch/TouchHandSequenceProbability.cs
@@ -6,9 +6,21 @@ using osu.Game.Rulesets.Osu.Difficulty.Utils;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills.Touch
 {
+    /// <summary>
+    /// Represents the probability of a hand sequence to be taken by the player in Touch Device gameplay
+    /// environment to hit <see cref="OsuDifficultyHitObject"/>s that have been processed up to this point.
+    /// </summary>
     public class TouchHandSequenceProbability
     {
+        /// <summary>
+        /// The probability that the player performs the hand sequence represented by this <see cref="TouchHandSequenceProbability"/>.
+        /// </summary>
         public double Probability = 1;
+
+        /// <summary>
+        /// The <see cref="TouchHandSequenceSkill"/>s that influence the probability of the hand sequence
+        /// represented by this <see cref="TouchHandSequenceProbability"/> to be taken by the player.
+        /// </summary>
         public readonly TouchHandSequenceSkill[] Skills;
 
         public TouchHandSequenceProbability(TouchHandSequenceSkill[] skills)
