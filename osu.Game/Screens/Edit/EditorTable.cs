649ef46e93845916f9b77aee310d62e545de9e13
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-06-21 10:54:40 +0200

    Rewrite `ControlPointTable` to use virtualised list

--- osu.Game/Screens/Edit/EditorTable.cs
+++ osu.Game/Screens/Edit/EditorTable.cs
@@ -4,15 +4,11 @@
 using System;
 using System.Diagnostics;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
-using osu.Framework.Localisation;
-using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
-using osu.Game.Graphics.Sprites;
 using osu.Game.Overlays;
 using osuTK.Graphics;
 
@@ -89,16 +85,7 @@ namespace osu.Game.Screens.Edit
             return BackgroundFlow[index].Item;
         }
 
-        protected override Drawable CreateHeader(int index, TableColumn? column) => new HeaderText(column?.Header ?? default);
-
-        private partial class HeaderText : OsuSpriteText
-        {
-            public HeaderText(LocalisableString text)
-            {
-                Text = text.ToUpper();
-                Font = OsuFont.GetFont(size: 12, weight: FontWeight.Bold);
-            }
-        }
+        protected override Drawable CreateHeader(int index, TableColumn? column) => new TableHeaderText(column?.Header ?? default);
 
         public partial class RowBackground : OsuClickableContainer
         {
