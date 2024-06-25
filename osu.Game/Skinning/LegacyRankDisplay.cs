931ea8bfd72c911a5ae13128eec55e4a3991aeec
normalid <rluo2018@gmail.com>
2024-06-25 18:06:39 +0800

    Add legacy rank display support the max and min option

--- osu.Game/Skinning/LegacyRankDisplay.cs
+++ osu.Game/Skinning/LegacyRankDisplay.cs
@@ -5,13 +5,19 @@ using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Bindables;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Scoring;
+using osu.Game.Screens.Play.HUD;
 using osuTK;
 
 namespace osu.Game.Skinning
 {
     public partial class LegacyRankDisplay : CompositeDrawable, ISerialisableDrawable
     {
+        [SettingSource(typeof(Localisation.HUD.GameplayRankDisplayStrings), nameof(Localisation.HUD.GameplayRankDisplayStrings.RankDisplay), nameof(Localisation.HUD.GameplayRankDisplayStrings.RankDisplayDescription))]
+        public Bindable<DefaultRankDisplay.RankDisplayMode> RankDisplay { get; } = new Bindable<DefaultRankDisplay.RankDisplayMode>();
+
         public bool UsesFixedAnchor { get; set; }
 
         [Resolved]
@@ -35,29 +41,61 @@ namespace osu.Game.Skinning
 
         protected override void LoadComplete()
         {
-            scoreProcessor.Rank.BindValueChanged(v =>
+            RankDisplay.BindValueChanged(mode =>
             {
-                var texture = source.GetTexture($"ranking-{v.NewValue}-small");
-
-                rank.Texture = texture;
-
-                if (texture != null)
+                switch (mode.OldValue)
                 {
-                    var transientRank = new Sprite
-                    {
-                        Texture = texture,
-                        Blending = BlendingParameters.Additive,
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        BypassAutoSizeAxes = Axes.Both,
-                    };
-                    AddInternal(transientRank);
-                    transientRank.FadeOutFromOne(500, Easing.Out)
-                                 .ScaleTo(new Vector2(1.625f), 500, Easing.Out)
-                                 .Expire();
+                    case DefaultRankDisplay.RankDisplayMode.Standard:
+                        scoreProcessor.Rank.UnbindBindings();
+                        break;
+
+                    case DefaultRankDisplay.RankDisplayMode.MinimumAchievable:
+                        scoreProcessor.MinimumRank.UnbindBindings();
+                        break;
+
+                    case DefaultRankDisplay.RankDisplayMode.MaximumAchievable:
+                        scoreProcessor.MaximumRank.UnbindBindings();
+                        break;
+                }
+                switch (mode.NewValue)
+                {
+                    case DefaultRankDisplay.RankDisplayMode.Standard:
+                        scoreProcessor.Rank.BindValueChanged(v => updateValue(v), true);
+                        break;
+
+                    case DefaultRankDisplay.RankDisplayMode.MinimumAchievable:
+                        scoreProcessor.MinimumRank.BindValueChanged(v => updateValue(v), true);
+                        break;
+
+                    case DefaultRankDisplay.RankDisplayMode.MaximumAchievable:
+                        scoreProcessor.MaximumRank.BindValueChanged(v => updateValue(v), true);
+                        break;
                 }
             }, true);
-            FinishTransforms(true);
+            //FinishTransforms(true);
+        }
+
+        private void updateValue(ValueChangedEvent<Scoring.ScoreRank> v)
+        {
+            var texture = source.GetTexture($"ranking-{v.NewValue}-small");
+
+            rank.Texture = texture;
+
+            if (texture != null)
+            {
+                var transientRank = new Sprite
+                {
+                    Texture = texture,
+                    Blending = BlendingParameters.Additive,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    BypassAutoSizeAxes = Axes.Both,
+                };
+                AddInternal(transientRank);
+                transientRank.FadeOutFromOne(500, Easing.Out)
+                             .ScaleTo(new Vector2(1.625f), 500, Easing.Out)
+                             .Expire();
+            }
         }
     }
 }
