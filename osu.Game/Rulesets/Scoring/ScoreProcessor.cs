7f8799169b45070b43d835328eb31d3bf033c15c
normalid <rluo2018@gmail.com>
2024-07-03 23:09:21 +0800

    Add test and optimize the performance

--- osu.Game/Rulesets/Scoring/ScoreProcessor.cs
+++ osu.Game/Rulesets/Scoring/ScoreProcessor.cs
@@ -204,7 +204,6 @@ namespace osu.Game.Rulesets.Scoring
         public ScoreProcessor(Ruleset ruleset)
         {
             Ruleset = ruleset;
-            Accuracy.ValueChanged += _ => updateRank();
 
             Combo.ValueChanged += combo => HighestCombo.Value = Math.Max(HighestCombo.Value, combo.NewValue);
 
