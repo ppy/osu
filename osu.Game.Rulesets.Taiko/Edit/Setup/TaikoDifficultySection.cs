95978905cdb7da9e672af48a8f3d1e816b40c5cd
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-07-10 14:42:11 +0200

    Remove setup screen controls that do nothing useful
    Before I go with a hammer to redesign these, I want to remove stuff that
    does nothing first.
    
    Hard-breaks API to allow rulesets to specify an enumerable of custom
    sections rather than two specific weird ones.
    
    For specific rulesets:
    
    - osu!:
      - Stack leniency slider merged into difficulty section.
    - osu!taiko:
      - Approach rate and circle size sliders removed.
      - Colours section removed.
    - osu!catch:
      - No functional changes.
    - osu!mania:
      - Special style toggle merged into difficulty section.
      - Colours section removed.

--- osu.Game.Rulesets.Taiko/Edit/Setup/TaikoDifficultySection.cs
+++ osu.Game.Rulesets.Taiko/Edit/Setup/TaikoDifficultySection.cs
@@ -1,62 +1,33 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Localisation;
 using osu.Game.Resources.Localisation.Web;
-using osu.Game.Screens.Edit;
 using osu.Game.Screens.Edit.Setup;
 
-namespace osu.Game.Rulesets.Mania.Edit.Setup
+namespace osu.Game.Rulesets.Taiko.Edit.Setup
 {
-    public partial class ManiaDifficultySection : SetupSection
+    public partial class TaikoDifficultySection : SetupSection
     {
-        public override LocalisableString Title => EditorSetupStrings.DifficultyHeader;
-
-        private LabelledSliderBar<float> keyCountSlider { get; set; } = null!;
-        private LabelledSwitchButton specialStyle { get; set; } = null!;
         private LabelledSliderBar<float> healthDrainSlider { get; set; } = null!;
         private LabelledSliderBar<float> overallDifficultySlider { get; set; } = null!;
         private LabelledSliderBar<double> baseVelocitySlider { get; set; } = null!;
         private LabelledSliderBar<double> tickRateSlider { get; set; } = null!;
 
-        [Resolved]
-        private Editor? editor { get; set; }
-
-        [Resolved]
-        private IEditorChangeHandler? changeHandler { get; set; }
+        public override LocalisableString Title => EditorSetupStrings.DifficultyHeader;
 
         [BackgroundDependencyLoader]
         private void load()
         {
             Children = new Drawable[]
             {
-                keyCountSlider = new LabelledSliderBar<float>
-                {
-                    Label = BeatmapsetsStrings.ShowStatsCsMania,
-                    FixedLabelWidth = LABEL_WIDTH,
-                    Description = "The number of columns in the beatmap",
-                    Current = new BindableFloat(Beatmap.Difficulty.CircleSize)
-                    {
-                        Default = BeatmapDifficulty.DEFAULT_DIFFICULTY,
-                        MinValue = 0,
-                        MaxValue = 10,
-                        Precision = 1,
-                    }
-                },
-                specialStyle = new LabelledSwitchButton
-                {
-                    Label = "Use special (N+1) style",
-                    FixedLabelWidth = LABEL_WIDTH,
-                    Description = "Changes one column to act as a classic \"scratch\" or \"special\" column, which can be moved around by the user's skin (to the left/right/centre). Generally used in 6K (5+1) or 8K (7+1) configurations.",
-                    Current = { Value = Beatmap.BeatmapInfo.SpecialStyle }
-                },
                 healthDrainSlider = new LabelledSliderBar<float>
                 {
                     Label = BeatmapsetsStrings.ShowStatsDrain,
@@ -111,49 +82,17 @@ namespace osu.Game.Rulesets.Mania.Edit.Setup
                 },
             };
 
-            keyCountSlider.Current.BindValueChanged(updateKeyCount);
-            healthDrainSlider.Current.BindValueChanged(_ => updateValues());
-            overallDifficultySlider.Current.BindValueChanged(_ => updateValues());
-            baseVelocitySlider.Current.BindValueChanged(_ => updateValues());
-            tickRateSlider.Current.BindValueChanged(_ => updateValues());
-        }
+            foreach (var item in Children.OfType<LabelledSliderBar<float>>())
+                item.Current.ValueChanged += _ => updateValues();
 
-        private bool updatingKeyCount;
-
-        private void updateKeyCount(ValueChangedEvent<float> keyCount)
-        {
-            if (updatingKeyCount) return;
-
-            updateValues();
-
-            if (editor == null) return;
-
-            updatingKeyCount = true;
-
-            editor.Reload().ContinueWith(t =>
-            {
-                if (!t.GetResultSafely())
-                {
-                    Schedule(() =>
-                    {
-                        changeHandler!.RestoreState(-1);
-                        Beatmap.Difficulty.CircleSize = keyCountSlider.Current.Value = keyCount.OldValue;
-                        updatingKeyCount = false;
-                    });
-                }
-                else
-                {
-                    updatingKeyCount = false;
-                }
-            });
+            foreach (var item in Children.OfType<LabelledSliderBar<double>>())
+                item.Current.ValueChanged += _ => updateValues();
         }
 
         private void updateValues()
         {
             // for now, update these on commit rather than making BeatmapMetadata bindables.
             // after switching database engines we can reconsider if switching to bindables is a good direction.
-            Beatmap.Difficulty.CircleSize = keyCountSlider.Current.Value;
-            Beatmap.BeatmapInfo.SpecialStyle = specialStyle.Current.Value;
             Beatmap.Difficulty.DrainRate = healthDrainSlider.Current.Value;
             Beatmap.Difficulty.OverallDifficulty = overallDifficultySlider.Current.Value;
             Beatmap.Difficulty.SliderMultiplier = baseVelocitySlider.Current.Value;
