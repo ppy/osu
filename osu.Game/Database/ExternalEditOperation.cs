704e7e843fc6769b391a3b455b723a9c58d335be
Dean Herbert <pe@ppy.sh>
2024-07-10 18:28:11 +0900

    More xmldoc across new methods and classes

--- osu.Game/Database/ExternalEditOperation.cs
+++ osu.Game/Database/ExternalEditOperation.cs
@@ -7,14 +7,23 @@ using osu.Game.Overlays.Notifications;
 
 namespace osu.Game.Database
 {
+    /// <summary>
+    /// Contains information related to an active external edit operation.
+    /// </summary>
     public class ExternalEditOperation<TModel> where TModel : class, IHasGuidPrimaryKey
     {
+        /// <summary>
+        /// The temporary path at which the model has been exported to for editing.
+        /// </summary>
         public readonly string MountedPath;
+
+        /// <summary>
+        /// Whether the model is still mounted at <see cref="MountedPath"/>.
+        /// </summary>
+        public bool IsMounted { get; private set; }
 
         private readonly IModelImporter<TModel> importer;
         private readonly TModel original;
-
-        private bool isMounted;
 
         public ExternalEditOperation(IModelImporter<TModel> importer, TModel original, string path)
         {
@@ -23,13 +32,23 @@ namespace osu.Game.Database
 
             MountedPath = path;
 
-            isMounted = true;
+            IsMounted = true;
         }
 
+        /// <summary>
+        /// Finish the external edit operation.
+        /// </summary>
+        /// <remarks>
+        /// This will trigger an asynchronous reimport of the model.
+        /// Subsequent calls will be a no-op.
+        /// </remarks>
+        /// <returns>A task which will eventuate in the newly imported model with changes applied.</returns>
         public async Task<Live<TModel>?> Finish()
         {
-            if (!Directory.Exists(MountedPath) || !isMounted)
+            if (!Directory.Exists(MountedPath) || !IsMounted)
                 return null;
+
+            IsMounted = false;
 
             Live<TModel>? imported = await importer.ImportAsUpdate(new ProgressNotification(), new ImportTask(MountedPath), original)
                                                    .ConfigureAwait(false);
@@ -39,8 +58,6 @@ namespace osu.Game.Database
                 Directory.Delete(MountedPath, true);
             }
             catch { }
-
-            isMounted = false;
 
             return imported;
         }
