97cde4f7d010b95a32c1a2f226594c7edf228729
Peter-io <93472377+Peter-io@users.noreply.github.com>
2024-07-11 20:22:00 +0200

    Change condition in Player

--- osu.Game/Screens/Play/Player.cs
+++ osu.Game/Screens/Play/Player.cs
@@ -955,7 +955,7 @@ namespace osu.Game.Screens.Play
                     ScoreProcessor.FailScore(Score.ScoreInfo);
                     OnFail();
 
-                    if (GameplayState.Mods.OfType<IApplicableFailOverride>().Any(m => m.RestartOnFail && m == HealthProcessor.FailTrigger))
+                    if (HealthProcessor.FailTriggers.OfType<IApplicableFailOverride>().Any(m => m.RestartOnFail))
                         Restart(true);
                 });
             }
