8478d9ccc76d8fd36cbf8d1c3574d6fa00c6c9e0
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-07-12 16:26:38 +0900

    Adjust timeline centre marker visuals and bring in front of ticks

--- osu.Game/Screens/Edit/Compose/Components/Timeline/CentreMarker.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/CentreMarker.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
