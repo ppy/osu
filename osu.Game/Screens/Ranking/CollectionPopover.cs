ce1119ba785f7ef52aabe00c3e03116ed74671fe
Layendan <87902114+bubb13gummy@users.noreply.github.com>
2024-07-21 17:51:30 -0700

    Fix collection popover

--- osu.Game/Screens/Ranking/CollectionPopover.cs
+++ osu.Game/Screens/Ranking/CollectionPopover.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
@@ -23,7 +21,7 @@ namespace osu.Game.Screens.Ranking
         private RealmAccess realm { get; set; } = null!;
 
         [Resolved]
-        private ManageCollectionsDialog manageCollectionsDialog { get; set; }
+        private ManageCollectionsDialog? manageCollectionsDialog { get; set; }
 
         public CollectionPopover(BeatmapInfo beatmapInfo)
             : base(false)
