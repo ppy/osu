0600f2ed8dcac41647549a55785b178f67faa501
Salman Ahmed <87902114+bubb13gummy@users.noreply.github.com>
2024-07-25 08:51:48 +0300

    Revert "Add failing test case"
    This reverts commit 1f98480717aaa2bb7a6a57ca2715643f4d8ec92b.

--- osu.Game.Tests/Visual/DailyChallenge/TestSceneDailyChallenge.cs
+++ osu.Game.Tests/Visual/DailyChallenge/TestSceneDailyChallenge.cs
@@ -4,12 +4,9 @@
 using System;
 using System.Linq;
 using NUnit.Framework;
-using osu.Framework.Testing;
 using osu.Game.Online.API;
 using osu.Game.Online.Rooms;
 using osu.Game.Rulesets.Osu.Mods;
-using osu.Game.Screens.Ranking;
-using osu.Game.Screens.SelectV2.Leaderboards;
 using osu.Game.Tests.Resources;
 using osu.Game.Tests.Visual.OnlinePlay;
 
@@ -17,14 +14,10 @@ namespace osu.Game.Tests.Visual.DailyChallenge
 {
     public partial class TestSceneDailyChallenge : OnlinePlayTestScene
     {
-        [SetUpSteps]
-        public override void SetUpSteps()
+        [Test]
+        public void TestDailyChallenge()
         {
-            base.SetUpSteps();
-
-            Room room = null!;
-
-            AddStep("add room", () => API.Perform(new CreateRoomRequest(room = new Room
+            var room = new Room
             {
                 RoomID = { Value = 1234 },
                 Name = { Value = "Daily Challenge: June 4, 2024" },
@@ -38,22 +31,10 @@ namespace osu.Game.Tests.Visual.DailyChallenge
                 },
                 EndDate = { Value = DateTimeOffset.Now.AddHours(12) },
                 Category = { Value = RoomCategory.DailyChallenge }
-            })));
+            };
 
+            AddStep("add room", () => API.Perform(new CreateRoomRequest(room)));
             AddStep("push screen", () => LoadScreen(new Screens.OnlinePlay.DailyChallenge.DailyChallenge(room)));
-        }
-
-        [Test]
-        public void TestDailyChallenge()
-        {
-        }
-
-        [Test]
-        public void TestScoreNavigation()
-        {
-            AddStep("click on score", () => this.ChildrenOfType<LeaderboardScoreV2>().First().TriggerClick());
-            AddUntilStep("wait for load", () => Stack.CurrentScreen is ResultsScreen results && results.IsLoaded);
-            AddAssert("replay download button exists", () => this.ChildrenOfType<ReplayDownloadButton>().Count(), () => Is.EqualTo(1));
         }
     }
 }
