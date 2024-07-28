17f5d58be2fcf5c2ead8d8b76a7b2ae5281d1197
Salman Ahmed <frenzibyte@gmail.com>
2024-07-28 05:24:29 +0300

    Add daily challenge streak display and tooltip

---
+++ osu.Game/Overlays/Profile/Header/Components/DailyChallengeStreakDisplay.cs
@@ -0,0 +1,112 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Online.API.Requests.Responses;
+
+namespace osu.Game.Overlays.Profile.Header.Components
+{
+    public partial class DailyChallengeStreakDisplay : CompositeDrawable, IHasCustomTooltip<APIUserDailyChallengeStatistics>
+    {
+        public readonly Bindable<UserProfileData?> User = new Bindable<UserProfileData?>();
+
+        public APIUserDailyChallengeStatistics? TooltipContent { get; private set; }
+
+        private OsuSpriteText dailyStreak = null!;
+
+        [Resolved]
+        private OverlayColourProvider colourProvider { get; set; } = null!;
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AutoSizeAxes = Axes.Both;
+            CornerRadius = 5;
+            Masking = true;
+
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.Background4,
+                },
+                new FillFlowContainer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Padding = new MarginPadding(5f),
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Children = new Drawable[]
+                    {
+                        new OsuTextFlowContainer(s => s.Font = OsuFont.GetFont(size: 12))
+                        {
+                            AutoSizeAxes = Axes.Both,
+                            // Text = UsersStrings.ShowDailyChallengeTitle
+                            Text = "Daily\nChallenge",
+                            Margin = new MarginPadding { Horizontal = 5f, Bottom = 2f },
+                        },
+                        new Container
+                        {
+                            AutoSizeAxes = Axes.X,
+                            RelativeSizeAxes = Axes.Y,
+                            CornerRadius = 5f,
+                            Masking = true,
+                            Children = new Drawable[]
+                            {
+                                new Box
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Colour = colourProvider.Background6,
+                                },
+                                dailyStreak = new OsuSpriteText
+                                {
+                                    Anchor = Anchor.Centre,
+                                    Origin = Anchor.Centre,
+                                    UseFullGlyphHeight = false,
+                                    Colour = colourProvider.Content2,
+                                    Margin = new MarginPadding { Horizontal = 10f, Vertical = 5f },
+                                },
+                            }
+                        },
+                    }
+                },
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            User.BindValueChanged(_ => updateDisplay(), true);
+        }
+
+        private void updateDisplay()
+        {
+            if (User.Value == null)
+            {
+                dailyStreak.Text = "-";
+                return;
+            }
+
+            var statistics = User.Value.User.DailyChallengeStatistics;
+            // dailyStreak.Text = UsersStrings.ShowDailyChallengeUnitDay(statistics.DailyStreakCurrent);
+            dailyStreak.Text = $"{statistics.DailyStreakCurrent}d";
+            dailyStreak.Colour = colours.ForRankingTier(DailyChallengeStreakTooltip.TierForDaily(statistics.DailyStreakCurrent));
+            TooltipContent = statistics;
+        }
+
+        public ITooltip<APIUserDailyChallengeStatistics> GetCustomTooltip() => new DailyChallengeStreakTooltip(colourProvider);
+    }
+}
