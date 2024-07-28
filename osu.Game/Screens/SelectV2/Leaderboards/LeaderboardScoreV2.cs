374eaf21c4b544590d7b8060653f112ce019e327
Joseph Madamba <87902114+bubb13gummy@users.noreply.github.com>
2024-07-27 23:50:52 -0700

    Add ability to copy leaderboard mods in daily challenge

--- osu.Game/Screens/SelectV2/Leaderboards/LeaderboardScoreV2.cs
+++ osu.Game/Screens/SelectV2/Leaderboards/LeaderboardScoreV2.cs
@@ -43,6 +43,21 @@ namespace osu.Game.Screens.SelectV2.Leaderboards
 {
     public partial class LeaderboardScoreV2 : OsuClickableContainer, IHasContextMenu, IHasCustomTooltip<ScoreInfo>
     {
+        public Bindable<IReadOnlyList<Mod>> SelectedMods = new Bindable<IReadOnlyList<Mod>>();
+
+        private Func<Mod, bool> isValidMod = _ => true;
+
+        /// <summary>
+        /// A function determining whether each mod in the score can be selected.
+        /// A return value of <see langword="true"/> means that the mod can be selected in the current context.
+        /// A return value of <see langword="false"/> means that the mod cannot be selected in the current context.
+        /// </summary>
+        public Func<Mod, bool> IsValidMod
+        {
+            get => isValidMod;
+            set => isValidMod = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
         public int? Rank { get; init; }
         public bool IsPersonalBest { get; init; }
 
@@ -67,9 +82,6 @@ namespace osu.Game.Screens.SelectV2.Leaderboards
 
         [Resolved]
         private OverlayColourProvider colourProvider { get; set; } = null!;
-
-        [Resolved]
-        private SongSelect? songSelect { get; set; }
 
         [Resolved]
         private IDialogOverlay? dialogOverlay { get; set; }
@@ -738,8 +750,8 @@ namespace osu.Game.Screens.SelectV2.Leaderboards
             {
                 List<MenuItem> items = new List<MenuItem>();
 
-                if (score.Mods.Length > 0 && songSelect != null)
-                    items.Add(new OsuMenuItem("Use these mods", MenuItemType.Highlighted, () => songSelect.Mods.Value = score.Mods));
+                if (score.Mods.Length > 0)
+                    items.Add(new OsuMenuItem("Use these mods", MenuItemType.Highlighted, () => SelectedMods.Value = score.Mods.Where(m => isValidMod.Invoke(m)).ToArray()));
 
                 if (score.Files.Count <= 0) return items.ToArray();
 
