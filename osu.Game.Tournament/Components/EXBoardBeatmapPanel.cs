aec1016a56ed3c0a87442d443ccd7dcc77f61783
CloneWith <clonewith@qq.com>
2024-08-06 15:13:16 +0800

    Extract the title from song information

--- osu.Game.Tournament/Components/EXBoardBeatmapPanel.cs
+++ osu.Game.Tournament/Components/EXBoardBeatmapPanel.cs
@@ -52,13 +52,36 @@ namespace osu.Game.Tournament.Components
             currentMatch.BindTo(ladder.CurrentMatch);
 
             var displayTitle = Beatmap?.GetDisplayTitleRomanisable(false, false) ?? (LocalisableString)@"unknown";
-            string songName = displayTitle.ToString().Split('-').Last().Trim();
-            string truncatedSongName = songName.TruncateWithEllipsis(39);
+
+            string[] songNameList = displayTitle.ToString().Split(' ');
+
+            int firstHyphenIndex = 0;
+
+            // Find the first " - " (Hopefully it isn't in the Artists field)
+            for (int i = 0; i < songNameList.Count(); i++)
+            {
+                string obj = songNameList.ElementAt(i);
+                if (obj == "-")
+                {
+                    firstHyphenIndex = i;
+                    break;
+                }
+            }
+
+            var TitleList = songNameList.Skip(firstHyphenIndex + 1);
+
+            // Re-construct
+            string songName = string.Empty;
+            for (int i = 0; i < TitleList.Count(); i++)
+            {
+                songName += TitleList.ElementAt(i).Trim();
+                if (i != TitleList.Count() - 1) songName += ' ';
+            }
+
+            string truncatedSongName = songName.Trim().TruncateWithEllipsis(39);
 
             string displayDifficulty = Beatmap?.DifficultyName ?? "unknown";
-            string difficultyName = displayDifficulty.ToString().Split('-').Last().Trim();
-            string truncatedDifficultyName = difficultyName.TruncateWithEllipsis(25);
-
+            string truncatedDifficultyName = displayDifficulty.TruncateWithEllipsis(25);
 
             Masking = true;
             CornerRadius = 10;
