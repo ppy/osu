518c1aa5a0a823a88365ca66a4cce8dae9fdbea8
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-08-07 14:01:30 +0200

    Remove weird `Expanded` / `ExpandedState` duality

--- osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectOverlay.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneFreeModSelectOverlay.cs
@@ -61,7 +61,9 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("select difficulty adjust", () => freeModSelectOverlay.SelectedMods.Value = new[] { new OsuModDifficultyAdjust() });
             AddWaitStep("wait some", 3);
-            AddAssert("customisation area not expanded", () => !this.ChildrenOfType<ModCustomisationPanel>().Single().Expanded);
+            AddAssert("customisation area not expanded",
+                () => this.ChildrenOfType<ModCustomisationPanel>().Single().ExpandedState.Value,
+                () => Is.EqualTo(ModCustomisationPanel.ModCustomisationPanelState.Collapsed));
         }
 
         [Test]
