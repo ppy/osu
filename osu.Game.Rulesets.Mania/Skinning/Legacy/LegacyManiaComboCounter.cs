66adddbfb8020c019bbdf672ade3f00b4aead7ef
Salman Ahmed <frenzibyte@gmail.com>
2024-08-15 15:48:17 +0900

    Actually bring back position-flipping logic and disable "closest" anchor

--- osu.Game.Rulesets.Mania/Skinning/Legacy/LegacyManiaComboCounter.cs
+++ osu.Game.Rulesets.Mania/Skinning/Legacy/LegacyManiaComboCounter.cs
@@ -2,7 +2,9 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Skinning;
 using osuTK;
 using osuTK.Graphics;
@@ -20,6 +22,38 @@ namespace osu.Game.Rulesets.Mania.Skinning.Legacy
             PopOutCountText.Anchor = Anchor.Centre;
             PopOutCountText.Origin = Anchor.Centre;
             PopOutCountText.Colour = skin.GetManiaSkinConfig<Color4>(LegacyManiaSkinConfigurationLookups.ComboBreakColour)?.Value ?? Color4.Red;
+        }
+
+        [Resolved]
+        private IScrollingInfo scrollingInfo { get; set; } = null!;
+
+        private IBindable<ScrollingDirection> direction = null!;
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            direction = scrollingInfo.Direction.GetBoundCopy();
+            direction.BindValueChanged(_ => updateAnchor());
+
+            // two schedules are required so that updateAnchor is executed in the next frame,
+            // which is when the combo counter receives its Y position by the default layout in LegacyManiaSkinTransformer.
+            Schedule(() => Schedule(updateAnchor));
+        }
+
+        private void updateAnchor()
+        {
+            // if the anchor isn't a vertical center, set top or bottom anchor based on scroll direction
+            if (!Anchor.HasFlag(Anchor.y1))
+            {
+                Anchor &= ~(Anchor.y0 | Anchor.y2);
+                Anchor |= direction.Value == ScrollingDirection.Up ? Anchor.y2 : Anchor.y0;
+            }
+
+            // since we flip the vertical anchor when changing scroll direction,
+            // we can use the sign of the Y value as an indicator to make the combo counter displayed correctly.
+            if ((Y < 0 && direction.Value == ScrollingDirection.Down) || (Y > 0 && direction.Value == ScrollingDirection.Up))
+                Y = -Y;
         }
 
         protected override void OnCountIncrement()
