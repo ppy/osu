ff1ab2bb0ef28335a834a1175a6dbc55b9e3f8b6
Salman Ahmed <frenzibyte@gmail.com>
2024-08-15 14:59:40 +0900

    Remove position-flipping logic from mania combo counters for now
    We need a general method to do this amicably, such as an HUD target that flips the position of its children when the direction is flipped. Something to consider later.

--- osu.Game.Rulesets.Mania/Skinning/Argon/ArgonManiaComboCounter.cs
+++ osu.Game.Rulesets.Mania/Skinning/Argon/ArgonManiaComboCounter.cs
@@ -1,46 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Allocation;
-using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Screens.Play.HUD;
 
 namespace osu.Game.Rulesets.Mania.Skinning.Argon
 {
     public partial class ArgonManiaComboCounter : ArgonComboCounter
     {
-        [Resolved]
-        private IScrollingInfo scrollingInfo { get; set; } = null!;
 
-        private IBindable<ScrollingDirection> direction = null!;
-
-        protected override void LoadComplete()
-        {
-            base.LoadComplete();
-
-            direction = scrollingInfo.Direction.GetBoundCopy();
-            direction.BindValueChanged(_ => updateAnchor());
-
-            // two schedules are required so that updateAnchor is executed in the next frame,
-            // which is when the combo counter receives its Y position by the default layout in ArgonManiaSkinTransformer.
-            Schedule(() => Schedule(updateAnchor));
-        }
-
-        private void updateAnchor()
-        {
-            // if the anchor isn't a vertical center, set top or bottom anchor based on scroll direction
-            if (!Anchor.HasFlag(Anchor.y1))
-            {
-                Anchor &= ~(Anchor.y0 | Anchor.y2);
-                Anchor |= direction.Value == ScrollingDirection.Up ? Anchor.y2 : Anchor.y0;
-            }
-
-            // since we flip the vertical anchor when changing scroll direction,
-            // we can use the sign of the Y value as an indicator to make the combo counter displayed correctly.
-            if ((Y < 0 && direction.Value == ScrollingDirection.Down) || (Y > 0 && direction.Value == ScrollingDirection.Up))
-                Y = -Y;
-        }
     }
 }
