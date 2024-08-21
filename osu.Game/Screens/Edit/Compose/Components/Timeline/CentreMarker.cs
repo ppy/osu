a0002943a1ac11f653570366710f61ef45cd289c
Dean Herbert <pe@ppy.sh>
2024-08-21 15:51:02 +0900

    Adjust centre marker visuals a bit

--- osu.Game/Screens/Edit/Compose/Components/Timeline/CentreMarker.cs
+++ osu.Game/Screens/Edit/Compose/Components/Timeline/CentreMarker.cs
@@ -14,22 +14,19 @@ namespace osu.Game.Screens.Edit.Compose.Components.Timeline
 {
     public partial class CentreMarker : CompositeDrawable
     {
-        private const float triangle_width = 8;
-
-        private const float bar_width = 1.6f;
-
-        public CentreMarker()
-        {
-            RelativeSizeAxes = Axes.Y;
-            Size = new Vector2(triangle_width, 1);
-
-            Anchor = Anchor.TopCentre;
-            Origin = Anchor.TopCentre;
-        }
-
         [BackgroundDependencyLoader]
         private void load(OverlayColourProvider colours)
         {
+            const float triangle_width = 8;
+            const float bar_width = 2f;
+
+            RelativeSizeAxes = Axes.Y;
+
+            Anchor = Anchor.TopCentre;
+            Origin = Anchor.TopCentre;
+
+            Size = new Vector2(triangle_width, 1);
+
             InternalChildren = new Drawable[]
             {
                 new Box
@@ -47,6 +44,15 @@ namespace osu.Game.Screens.Edit.Compose.Components.Timeline
                     Origin = Anchor.BottomCentre,
                     Size = new Vector2(triangle_width, triangle_width * 0.8f),
                     Scale = new Vector2(1, -1),
+                    EdgeSmoothness = new Vector2(1, 0),
+                    Colour = colours.Colour2,
+                },
+                new Triangle
+                {
+                    Anchor = Anchor.BottomCentre,
+                    Origin = Anchor.BottomCentre,
+                    Size = new Vector2(triangle_width, triangle_width * 0.8f),
+                    Scale = new Vector2(1, 1),
                     Colour = colours.Colour2,
                 },
             };
