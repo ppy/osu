4d42274771b770c4cb36e05a0611a2e20a4db324
Dean Herbert <pe@ppy.sh>
2024-08-27 18:13:52 +0900

    Use bindable list implementation

--- osu.Game/Database/DetachedBeatmapStore.cs
+++ osu.Game/Database/DetachedBeatmapStore.cs
@@ -2,10 +2,10 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Logging;
 using osu.Game.Beatmaps;
@@ -13,42 +13,36 @@ using Realms;
 
 namespace osu.Game.Database
 {
-    // TODO: handle realm migration
     public partial class DetachedBeatmapStore : Component
     {
         private readonly ManualResetEventSlim loaded = new ManualResetEventSlim();
 
-        private List<BeatmapSetInfo> originalBeatmapSetsDetached = new List<BeatmapSetInfo>();
+        private readonly BindableList<BeatmapSetInfo> detachedBeatmapSets = new BindableList<BeatmapSetInfo>();
 
-        private IDisposable? subscriptionSets;
-
-        /// <summary>
-        /// Track GUIDs of all sets in realm to allow handling deletions.
-        /// </summary>
-        private readonly List<Guid> realmBeatmapSets = new List<Guid>();
+        private IDisposable? realmSubscription;
 
         [Resolved]
         private RealmAccess realm { get; set; } = null!;
 
-        public IReadOnlyList<BeatmapSetInfo> GetDetachedBeatmaps()
+        public IBindableList<BeatmapSetInfo> GetDetachedBeatmaps()
         {
             if (!loaded.Wait(60000))
                 Logger.Error(new TimeoutException("Beatmaps did not load in an acceptable time"), $"{nameof(DetachedBeatmapStore)} fell over");
 
-            return originalBeatmapSetsDetached;
+            return detachedBeatmapSets.GetBoundCopy();
         }
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            subscriptionSets = realm.RegisterForNotifications(getBeatmapSets, beatmapSetsChanged);
+            realmSubscription = realm.RegisterForNotifications(getBeatmapSets, beatmapSetsChanged);
         }
 
         private void beatmapSetsChanged(IRealmCollection<BeatmapSetInfo> sender, ChangeSet? changes)
         {
             if (changes == null)
             {
-                if (originalBeatmapSetsDetached.Count > 0 && sender.Count == 0)
+                if (detachedBeatmapSets.Count > 0 && sender.Count == 0)
                 {
                     // Usually we'd reset stuff here, but doing so triggers a silly flow which ends up deadlocking realm.
                     // Additionally, user should not be at song select when realm is blocking all operations in the first place.
@@ -59,57 +53,29 @@ namespace osu.Game.Database
                     return;
                 }
 
-                originalBeatmapSetsDetached = sender.Detach();
-
-                realmBeatmapSets.Clear();
-                realmBeatmapSets.AddRange(sender.Select(r => r.ID));
+                detachedBeatmapSets.Clear();
+                detachedBeatmapSets.AddRange(sender.Detach());
 
                 loaded.Set();
                 return;
             }
 
-            HashSet<Guid> setsRequiringUpdate = new HashSet<Guid>();
-            HashSet<Guid> setsRequiringRemoval = new HashSet<Guid>();
-
             foreach (int i in changes.DeletedIndices.OrderDescending())
-            {
-                Guid id = realmBeatmapSets[i];
-
-                setsRequiringRemoval.Add(id);
-                setsRequiringUpdate.Remove(id);
-
-                realmBeatmapSets.RemoveAt(i);
-            }
+                detachedBeatmapSets.RemoveAt(i);
 
             foreach (int i in changes.InsertedIndices)
             {
-                Guid id = sender[i].ID;
-
-                setsRequiringRemoval.Remove(id);
-                setsRequiringUpdate.Add(id);
-
-                realmBeatmapSets.Insert(i, id);
+                detachedBeatmapSets.Insert(i, sender[i].Detach());
             }
 
             foreach (int i in changes.NewModifiedIndices)
-                setsRequiringUpdate.Add(sender[i].ID);
-
-            // deletions
-            foreach (Guid g in setsRequiringRemoval)
-                originalBeatmapSetsDetached.RemoveAll(set => set.ID == g);
-
-            // updates
-            foreach (Guid g in setsRequiringUpdate)
-            {
-                originalBeatmapSetsDetached.RemoveAll(set => set.ID == g);
-                originalBeatmapSetsDetached.Add(fetchFromID(g)!);
-            }
+                detachedBeatmapSets.ReplaceRange(i, 1, new[] { sender[i].Detach() });
         }
 
         protected override void Dispose(bool isDisposing)
         {
             base.Dispose(isDisposing);
-            subscriptionSets?.Dispose();
+            realmSubscription?.Dispose();
         }
 
         private IQueryable<BeatmapSetInfo> getBeatmapSets(Realm realm) => realm.All<BeatmapSetInfo>().Where(s => !s.DeletePending && !s.Protected);
