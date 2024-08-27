63e1d22aa8481bb16198dbd5b5537bdf93ec1d61
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-08-27 16:30:49 +0900

    Add beat-synced animation to break overlay
    I've been meaning to make the progress bar synchronise with the beat
    rather than a continuous countdown, just to give the overlay a bit more
    of a rhythmic feel.
    
    Not completely happy with how this feels but I think it's a start?
    
    I had to refactor how the break overlay works in the process. It no
    longer creates transforms for all breaks ahead-of-time, which could be
    argued as a better way of doing things. It's more dynamically able to
    handle breaks now (maybe useful for the future, who knows).

--- osu.Game.Tests/Visual/Gameplay/TestSceneBreakTracker.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneBreakTracker.cs
@@ -10,6 +10,8 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Timing;
 using osu.Game.Beatmaps.Timing;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens.Play;
 using osuTK.Graphics;
 
@@ -38,9 +40,10 @@ namespace osu.Game.Tests.Visual.Gameplay
                     RelativeSizeAxes = Axes.Both,
                 },
                 breakTracker = new TestBreakTracker(),
-                breakOverlay = new BreakOverlay(true, null)
+                breakOverlay = new BreakOverlay(true, new ScoreProcessor(new OsuRuleset()))
                 {
                     ProcessCustomClock = false,
+                    BreakTracker = breakTracker,
                 }
             };
         }
@@ -55,9 +58,6 @@ namespace osu.Game.Tests.Visual.Gameplay
         [Test]
         public void TestShowBreaks()
         {
-            setClock(false);
-
-            addShowBreakStep(2);
             addShowBreakStep(5);
             addShowBreakStep(15);
         }
@@ -122,7 +122,7 @@ namespace osu.Game.Tests.Visual.Gameplay
         {
             AddStep($"show '{seconds}s' break", () =>
             {
-                breakOverlay.Breaks = breakTracker.Breaks = new List<BreakPeriod>
+                breakTracker.Breaks = new List<BreakPeriod>
                 {
                     new BreakPeriod(Clock.CurrentTime, Clock.CurrentTime + seconds * 1000)
                 };
@@ -136,7 +136,7 @@ namespace osu.Game.Tests.Visual.Gameplay
 
         private void loadBreaksStep(string breakDescription, IReadOnlyList<BreakPeriod> breaks)
         {
-            AddStep($"load {breakDescription}", () => breakOverlay.Breaks = breakTracker.Breaks = breaks);
+            AddStep($"load {breakDescription}", () => breakTracker.Breaks = breaks);
             seekAndAssertBreak("seek back to 0", 0, false);
         }
 
@@ -182,6 +182,7 @@ namespace osu.Game.Tests.Visual.Gameplay
             }
 
             public TestBreakTracker()
+                : base(0, new ScoreProcessor(new OsuRuleset()))
             {
                 FramedManualClock = new FramedClock(manualClock = new ManualClock());
                 ProcessCustomClock = false;
