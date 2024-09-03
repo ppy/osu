a549cdd5b9f1b8a968e9a3e5d57c926db22b8675
Sheppsu <apc2005osu@gmail.com>
2024-09-03 04:49:50 -0400

    persist analysis settings

--- osu.Game.Rulesets.Osu/UI/OsuAnalysisSettings.cs
+++ osu.Game.Rulesets.Osu/UI/OsuAnalysisSettings.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Game.Configuration;
 using osu.Game.Screens.Play.PlayerSettings;
@@ -20,5 +21,14 @@ namespace osu.Game.Rulesets.Osu.UI
 
         [SettingSource("Hide cursor", SettingControlType = typeof(PlayerCheckbox))]
         public BindableBool CursorHideEnabled { get; } = new BindableBool();
+
+        [BackgroundDependencyLoader]
+        private void load(OsuConfigManager config)
+        {
+            config.BindWith(OsuSetting.ReplayHitMarkersEnabled, HitMarkersEnabled);
+            config.BindWith(OsuSetting.ReplayAimMarkersEnabled, AimMarkersEnabled);
+            config.BindWith(OsuSetting.ReplayAimLinesEnabled, AimLinesEnabled);
+            config.BindWith(OsuSetting.ReplayCursorHideEnabled, CursorHideEnabled);
+        }
     }
 }
