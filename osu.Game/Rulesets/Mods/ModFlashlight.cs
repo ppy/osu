ppy/osu

commit d7b6969b9e5d2694d8edf86fecbd672b92799a61

Fix 1px flashlight gaps when gameplay scaling mode is active
Closes https://github.com/ppy/osu/issues/27522.

Concerns mostly taiko and catch.

Not much of a proper fix rather than a workaround but it is what it is.
I tried a few other things, including setting `MaskingSmoothness = 0` on
the scaling container itself, to no avail.

osu.Game/Rulesets/Mods/ModFlashlight.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Rendering.Vertices;
@@ -88,7 +89,13 @@ namespace osu.Game.Rulesets.Mods
             flashlight.Combo.BindTo(Combo);
             flashlight.GetPlayfieldScale = () => drawableRuleset.Playfield.Scale;
 
-            drawableRuleset.Overlays.Add(flashlight);
+            drawableRuleset.Overlays.Add(new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                // workaround for 1px gaps on the edges of the playfield which would sometimes show with "gameplay" screen scaling active.
+                Padding = new MarginPadding(-1),
+                Child = flashlight,
+            });
         }
 
         protected abstract Flashlight CreateFlashlight();

commit 9347add0378434efdf2467f6a8f6d23848d44511

Fix Flashlight not appearing on top of bubbles from Bubbles mod
Inadvertently regressed in d7b6969b9e5d2694d8edf86fecbd672b92799a61.

osu.Game/Rulesets/Mods/ModFlashlight.cs
@@ -83,8 +83,6 @@ namespace osu.Game.Rulesets.Mods
 
             flashlight.RelativeSizeAxes = Axes.Both;
             flashlight.Colour = Color4.Black;
-            // Flashlight mods should always draw above any other mod adding overlays.
-            flashlight.Depth = float.MinValue;
 
             flashlight.Combo.BindTo(Combo);
             flashlight.GetPlayfieldScale = () => drawableRuleset.Playfield.Scale;
@@ -95,6 +93,9 @@ namespace osu.Game.Rulesets.Mods
                 // workaround for 1px gaps on the edges of the playfield which would sometimes show with "gameplay" screen scaling active.
                 Padding = new MarginPadding(-1),
                 Child = flashlight,
+                // Flashlight mods should always draw above any other mod adding overlays.
+                // NegativeInfinity is not used to allow one more thing drawn on top (used in replay analysis overlay in osu!).
+                Depth = float.MinValue,
             });
         }
 