b778589242f241150dbb5ca5f1b1c53883f10a5f
Joseph Madamba <madamba.joehu@outlook.com>
2024-09-16 22:31:18 -0700

    Implement sheared filter text box
    Apply minor adjustments to text box implementation

--- osu.Game.Tests/Visual/UserInterface/TestSceneShearedSearchTextBox.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneShearedSearchTextBox.cs
@@ -5,8 +5,10 @@ using System;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Overlays;
+using osuTK;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
@@ -30,16 +32,32 @@ namespace osu.Game.Tests.Visual.UserInterface
                 {
                     (typeof(OverlayColourProvider), colourProvider)
                 },
-                Children = new Drawable[]
+                Child = new FillFlowContainer
                 {
-                    new ShearedSearchTextBox
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Spacing = new Vector2(0f, 5f),
+                    Children = new Drawable[]
                     {
-                        Origin = Anchor.Centre,
-                        Anchor = Anchor.Centre,
-                        RelativeSizeAxes = Axes.X,
-                        Width = 0.5f
+                        new ShearedSearchTextBox
+                        {
+                            Origin = Anchor.Centre,
+                            Anchor = Anchor.Centre,
+                            RelativeSizeAxes = Axes.X,
+                            Width = 0.5f
+                        },
+                        new ShearedFilterTextBox
+                        {
+                            Origin = Anchor.Centre,
+                            Anchor = Anchor.Centre,
+                            RelativeSizeAxes = Axes.X,
+                            Width = 0.5f,
+                            FilterText = "12345 matches",
+                        },
                     }
-                }
+                },
             };
         }
     }
