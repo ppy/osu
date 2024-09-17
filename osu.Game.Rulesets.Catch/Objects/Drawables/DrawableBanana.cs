dd00956e7ddac7038a87ed4b3c34e20531ced1ed
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-09-17 15:27:16 +0900

    Also fix banana showers

--- osu.Game.Rulesets.Catch/Objects/Drawables/DrawableBanana.cs
+++ osu.Game.Rulesets.Catch/Objects/Drawables/DrawableBanana.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
+using osu.Framework.Utils;
 using osu.Game.Rulesets.Catch.Skinning.Default;
 using osu.Game.Skinning;
+using osuTK;
 
 namespace osu.Game.Rulesets.Catch.Objects.Drawables
 {
@@ -36,21 +38,35 @@ namespace osu.Game.Rulesets.Catch.Objects.Drawables
             StartTimeBindable.BindValueChanged(_ => UpdateComboColour());
         }
 
-        protected override void UpdateInitialTransforms()
+        private float startScale;
+        private float endScale;
+
+        private float startAngle;
+        private float endAngle;
+
+        protected override void OnApply()
         {
-            base.UpdateInitialTransforms();
+            base.OnApply();
 
             const float end_scale = 0.6f;
             const float random_scale_range = 1.6f;
 
-            ScalingContainer.ScaleTo(HitObject.Scale * (end_scale + random_scale_range * RandomSingle(3)))
-                            .Then().ScaleTo(HitObject.Scale * end_scale, HitObject.TimePreempt);
+            startScale = end_scale + random_scale_range * RandomSingle(3);
+            endScale = end_scale;
 
-            ScalingContainer.RotateTo(getRandomAngle(1))
-                            .Then()
-                            .RotateTo(getRandomAngle(2), HitObject.TimePreempt);
+            startAngle = getRandomAngle(1);
+            endAngle = getRandomAngle(2);
 
             float getRandomAngle(int series) => 180 * (RandomSingle(series) * 2 - 1);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            double preemptProgress = Math.Min(1, (Time.Current - (HitObject.StartTime - InitialLifetimeOffset)) / HitObject.TimePreempt);
+            ScalingContainer.Scale = new Vector2(HitObject.Scale * (float)Interpolation.Lerp(startScale, endScale, preemptProgress));
+            ScalingContainer.Rotation = (float)Interpolation.Lerp(startAngle, endAngle, preemptProgress);
         }
 
         public override void PlaySamples()
