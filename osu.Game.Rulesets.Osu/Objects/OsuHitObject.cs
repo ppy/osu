307d52549e16fc13b71134b7da1d28430c8074b1
Marvin Schürz <minetoblend@gmail.com>
2024-10-08 21:25:03 +0200

    Make x/y position mutable in OsuHitObject

--- osu.Game.Rulesets.Osu/Objects/OsuHitObject.cs
+++ osu.Game.Rulesets.Osu/Objects/OsuHitObject.cs
@@ -59,8 +59,17 @@ namespace osu.Game.Rulesets.Osu.Objects
             set => position.Value = value;
         }
 
-        public float X => Position.X;
-        public float Y => Position.Y;
+        public float X
+        {
+            get => Position.X;
+            set => Position = Position with { X = value };
+        }
+
+        public float Y
+        {
+            get => Position.Y;
+            set => Position = Position with { Y = value };
+        }
 
         public Vector2 StackedPosition => Position + StackOffset;
 
