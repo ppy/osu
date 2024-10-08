19828d601ef5799566a8e178d8051e4a69ce9b2b
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-10-08 23:08:18 +0200

    Fix any and all migration attempts dying on `MusicController`
    I'm not sure why this was "fine" for as long as it apparently was,
    but what `MusicController` was doing was completely incorrect and
    playing with fire (accessing raw managed realm objects), which went
    wrong somewhere around - admittedly -
    https://github.com/ppy/osu/pull/29917, likely because that one started
    *storing* these raw managed realm objects to fields, and you know what
    will happen to those after you do a migration and recycle realms.
    
    To attempt to circumvent this, (ab)use `DetachedBeatmapStore` instead.
    Which does necessitate moving it to `OsuGameBase`, but it's the simplest
    way out I can see. I guess the alternative would be to faff around with
    `Live<T>` but it's ugly and I'm attempting to fix this relatively quick
    right now.

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -1137,7 +1137,6 @@ namespace osu.Game
             loadComponentSingleFile(new MedalOverlay(), topMostOverlayContent.Add);
 
             loadComponentSingleFile(new BackgroundDataStoreProcessor(), Add);
-            loadComponentSingleFile(new DetachedBeatmapStore(), Add, true);
 
             Add(difficultyRecommender);
             Add(externalLinkOpener = new ExternalLinkOpener());
