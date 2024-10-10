4a2995d7e5c8c85b0c8fe586f6616a1e886b96f0
Marvin Schürz <m.schuerz@hautzy.com>
2024-10-10 15:27:03 +0200

    Add PropertyChangeCommand

---
+++ osu.Game/Screens/Edit/Commands/PropertyChangeCommand.cs
@@ -0,0 +1,36 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Game.Screens.Edit.Commands
+{
+public abstract class PropertyChangeCommand<TTarget, TProperty> : IEditorCommand, IMergeableCommand where TTarget : class
+{
+    protected abstract TProperty ReadValue(TTarget target);
+
+    protected abstract void WriteValue(TTarget target, TProperty value);
+
+    protected abstract PropertyChangeCommand<TTarget, TProperty> CreateInstance(TTarget target, TProperty value);
+
+    public readonly TTarget Target;
+
+    public readonly TProperty Value;
+
+    protected PropertyChangeCommand(TTarget target, TProperty value)
+    {
+        Target = target;
+        Value = value;
+    }
+
+    public void Apply() => WriteValue(Target, Value);
+
+    public IEditorCommand CreateUndo() => CreateInstance(Target, Value);
+
+    public IEditorCommand? MergeWith(IEditorCommand previous)
+    {
+        if (previous is PropertyChangeCommand<TTarget, TProperty> command && command.Target == Target)
+            return this;
+
+        return null;
+    }
+}
+}
