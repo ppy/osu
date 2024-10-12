11541389d81298b58d597f393fdc3a2dbad69e9e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2024-10-12 13:09:34 +0800

    Unify hard link and cow detection logic

--- osu.Game/IO/CopyOnWriteHelper.cs
+++ osu.Game/IO/CopyOnWriteHelper.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
 
 namespace osu.Game.IO
 {
@@ -16,32 +14,17 @@ namespace osu.Game.IO
             {
                 // Starting from Windows 11 24H2, the CopyFile syscall will perform CoW cloning on ReFS drives.
 
-                const string test_filename = "_cow_test";
-
-                testDestinationPath = Path.Combine(testDestinationPath, test_filename);
-                testSourcePath = Path.Combine(testSourcePath, test_filename);
-
-                try
-                {
-                    using var sourceFileHandle = File.OpenHandle(testSourcePath, FileMode.CreateNew, FileAccess.Write, FileShare.Write, FileOptions.DeleteOnClose);
-                    using var destinationFileHandle = File.OpenHandle(testDestinationPath, FileMode.CreateNew, FileAccess.Write, FileShare.Write, FileOptions.DeleteOnClose);
-
-                    // Attention: Windows supports mounting volume into folders. Don't detect volumn from the volume letter of path.
-                    if (!GetVolumeInformationByHandle(sourceFileHandle, IntPtr.Zero, 0, out uint sourceVolume, out _, out uint flags, IntPtr.Zero, 0))
-                        return false;
-
-                    if (!GetVolumeInformationByHandle(destinationFileHandle, IntPtr.Zero, 0, out uint destinationVolume, out _, out _, IntPtr.Zero, 0))
-                        return false;
-
-                    if (sourceVolume != destinationVolume)
-                        return false;
-
-                    return (flags & FILE_SUPPORTS_BLOCK_REFCOUNTING) != 0;
-                }
-                catch
-                {
+                // Attention: Windows supports mounting volume into folders. Don't detect volumn from the volume letter of path.
+                if (!HardLinkHelper.GetVolumeInformationForDirectory(testSourcePath, out uint sourceVolume, out uint flags))
                     return false;
-                }
+
+                if (!HardLinkHelper.GetVolumeInformationForDirectory(testDestinationPath, out uint destinationVolume, out _))
+                    return false;
+
+                if (sourceVolume != destinationVolume)
+                    return false;
+
+                return (flags & HardLinkHelper.FILE_SUPPORTS_HARD_LINKS) != 0;
             }
 
             return false;
@@ -58,22 +41,5 @@ namespace osu.Game.IO
 
             return false;
         }
-
-        #region Windows native methods
-
-        public const uint FILE_SUPPORTS_BLOCK_REFCOUNTING = 0x08000000;
-
-        [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
-        private static extern bool GetVolumeInformationByHandle(
-            SafeFileHandle hFile,
-            IntPtr lpVolumeNameBuffer,
-            uint nVolumeNameSize,
-            out uint lpVolumeSerialNumber,
-            out uint lpMaximumComponentLength,
-            out uint lpFileSystemFlags,
-            IntPtr lpFileSystemNameBuffer,
-            uint nFileSystemNameSize);
-
-        #endregion
     }
 }
