7bbb6b7dc24f9815dc71b737a1ecf8d2852a82bd
smallketchup82 <ketchup@smallketchup.ca>
2024-10-16 19:51:33 -0400

    Rename setting to be more descriptive
    this is honestly a nitpick on my behalf, but i figured it's better than
    what we had before.
    
    when i was looking at the setting in the settings menu, simulating a new
    player, i asked myself "normalize audio? what audio?". for all the user
    knows, this could be effects, beatmaps, hitsounds, anything.
    
    this commit changes the text of the setting to read "Normalize Beatmap
    Audio" to further describe that it's normalizing only beatmap audio

--- osu.Game/Localisation/AudioSettingsStrings.cs
+++ osu.Game/Localisation/AudioSettingsStrings.cs
@@ -65,9 +65,9 @@ namespace osu.Game.Localisation
         public static LocalisableString AudioOffset => new TranslatableString(getKey(@"audio_offset"), @"Audio offset");
 
         /// <summary>
-        /// "Normalize Audio"
+        /// "Normalize Beatmap Audio"
         /// </summary>
-        public static LocalisableString AudioNormalization => new TranslatableString(getKey(@"audio_normalization"), @"Normalize Audio");
+        public static LocalisableString AudioNormalization => new TranslatableString(getKey(@"audio_normalization"), @"Normalize Beatmap Audio");
 
         /// <summary>
         /// "Play a few beatmaps to receive a suggested offset!"
