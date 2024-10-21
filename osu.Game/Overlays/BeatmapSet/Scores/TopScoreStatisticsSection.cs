8181b2ebe081e1d1848bed3e33ac8d77e6710f19
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-10-21 13:52:17 +0200

    Remove unused using directive

--- osu.Game/Overlays/BeatmapSet/Scores/TopScoreStatisticsSection.cs
+++ osu.Game/Overlays/BeatmapSet/Scores/TopScoreStatisticsSection.cs
@@ -9,7 +9,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
