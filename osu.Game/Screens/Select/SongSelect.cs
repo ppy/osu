42d03c7602dadc098cf95e9f9fd298a6642e2c3e
jhk2601 <jhk2601@rit.edu>
2024-10-28 15:45:47 -0400

    moved comment

--- osu.Game/Screens/Select/SongSelect.cs
+++ osu.Game/Screens/Select/SongSelect.cs
@@ -957,8 +957,6 @@ namespace osu.Game.Screens.Select
 
         private void updateVisibleBeatmapCount()
         {
-            // Intentionally not localised until we have proper support for this (see https://github.com/ppy/osu-framework/pull/4918
-            // but also in this case we want support for formatting a number within a string).
             int carouselCountDisplayed = Carousel.CountDisplayed;
 
             if (FilterControl.CurrentTextSearch.Value == string.Empty)
@@ -967,6 +965,8 @@ namespace osu.Game.Screens.Select
             }
             else
             {
+                // Intentionally not localised until we have proper support for this (see https://github.com/ppy/osu-framework/pull/4918
+                // but also in this case we want support for formatting a number within a string).
                 FilterControl.InformationalText = carouselCountDisplayed != 1 ? $"{carouselCountDisplayed:#,0} matches" : $"{carouselCountDisplayed:#,0} match";
             }
         }
