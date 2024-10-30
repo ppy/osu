776fabd77c5a6225e69a0b14e79b9e097692320c
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-10-30 08:00:57 +0100

    Only use MD5 when performing metadata lookups
    Both online and offline using the cache.
    
    The rationale behind this change is that in the current state of
    affairs, `TestPartiallyMaliciousSet()` fails in a way that cannot be
    reconciled without this sort of change.
    
    The test exercises a scenario where the beatmap being imported has an
    online ID in the `.osu` file, but its hash does not match the online
    hash of the beatmap. This turns out to be a more frequent scenario than
    envisioned because of users doing stupid things with manual file editing
    rather than reporting issues properly.
    
    The scenario is realistic only because the behaviour of the endpoint
    responsible for looking up beatmaps is such that if multiple parameters
    are given (e.g. all three of beatmap MD5, online ID, and filename), it
    will try the three in succession:
    
    	https://github.com/ppy/osu-web/blob/f6b341813be270de59ec8f9698428aa6422bc8ae/app/Http/Controllers/BeatmapsController.php#L260-L266
    
    and the local metadata cache implementation reflected this
    implementation.
    
    Because online ID and filename are inherently unreliable in this
    scenario due to being directly manipulable by clueless or malicious
    users, neither should not be used as a fallback.

--- osu.Game/Beatmaps/LocalCachedBeatmapMetadataSource.cs
+++ osu.Game/Beatmaps/LocalCachedBeatmapMetadataSource.cs
@@ -89,9 +89,7 @@ namespace osu.Game.Beatmaps
                 return false;
             }
 
-            if (string.IsNullOrEmpty(beatmapInfo.MD5Hash)
-                && string.IsNullOrEmpty(beatmapInfo.Path)
-                && beatmapInfo.OnlineID <= 0)
+            if (string.IsNullOrEmpty(beatmapInfo.MD5Hash))
             {
                 onlineMetadata = null;
                 return false;
@@ -240,11 +238,9 @@ namespace osu.Game.Beatmaps
             using var cmd = db.CreateCommand();
 
             cmd.CommandText =
-                @"SELECT beatmapset_id, beatmap_id, approved, user_id, checksum, last_update FROM osu_beatmaps WHERE checksum = @MD5Hash OR beatmap_id = @OnlineID OR filename = @Path";
+                @"SELECT beatmapset_id, beatmap_id, approved, user_id, checksum, last_update FROM osu_beatmaps WHERE checksum = @MD5Hash";
 
             cmd.Parameters.Add(new SqliteParameter(@"@MD5Hash", beatmapInfo.MD5Hash));
-            cmd.Parameters.Add(new SqliteParameter(@"@OnlineID", beatmapInfo.OnlineID));
-            cmd.Parameters.Add(new SqliteParameter(@"@Path", beatmapInfo.Path));
 
             using var reader = cmd.ExecuteReader();
 
@@ -281,12 +277,10 @@ namespace osu.Game.Beatmaps
                 SELECT `b`.`beatmapset_id`, `b`.`beatmap_id`, `b`.`approved`, `b`.`user_id`, `b`.`checksum`, `b`.`last_update`, `s`.`submit_date`, `s`.`approved_date`
                 FROM `osu_beatmaps` AS `b`
                 JOIN `osu_beatmapsets` AS `s` ON `s`.`beatmapset_id` = `b`.`beatmapset_id`
-                WHERE `b`.`checksum` = @MD5Hash OR `b`.`beatmap_id` = @OnlineID OR `b`.`filename` = @Path
+                WHERE `b`.`checksum` = @MD5Hash
                 """;
 
             cmd.Parameters.Add(new SqliteParameter(@"@MD5Hash", beatmapInfo.MD5Hash));
-            cmd.Parameters.Add(new SqliteParameter(@"@OnlineID", beatmapInfo.OnlineID));
-            cmd.Parameters.Add(new SqliteParameter(@"@Path", beatmapInfo.Path));
 
             using var reader = cmd.ExecuteReader();
 
