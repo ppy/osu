932c4b218f36577c780cb0f7f66a73bc805ff817
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2024-11-01 18:36:01 +0100

    Remove hide sample toggle
    In line with feedback from
    https://github.com/ppy/osu/pull/29896#issuecomment-2378676851.

--- osu.Game/Localisation/EditorStrings.cs
+++ osu.Game/Localisation/EditorStrings.cs
@@ -139,11 +139,6 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString TimelineShowTicks => new TranslatableString(getKey(@"timeline_show_ticks"), @"Show ticks");
 
-        /// <summary>
-        /// "Show samples"
-        /// </summary>
-        public static LocalisableString TimelineShowSamples => new TranslatableString(getKey(@"timeline_show_samples"), @"Show samples");
-
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }
