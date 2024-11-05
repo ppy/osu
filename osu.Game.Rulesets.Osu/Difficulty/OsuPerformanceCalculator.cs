404bf21bf140a9b232dc7eb5cb3de7a5d7e29a69
Givikap120 <87902114+bubb13gummy@users.noreply.github.com>
2024-11-05 22:08:43 +0200

    Update OsuPerformanceCalculator.cs

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -59,6 +59,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             countMiss = score.Statistics.GetValueOrDefault(HitResult.Miss);
             countSliderEndsDropped = osuAttributes.SliderCount - score.Statistics.GetValueOrDefault(HitResult.SliderTailHit);
             countSliderTickMiss = score.Statistics.GetValueOrDefault(HitResult.LargeTickMiss);
+            effectiveMissCount = 0;
 
             if (osuAttributes.SliderCount > 0)
             {
