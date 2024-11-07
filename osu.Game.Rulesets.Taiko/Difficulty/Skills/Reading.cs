cb0f198e426d044c2f202d36e0265d3bf9669b05
Jay Lawton <jaime@merkids.com>
2024-11-08 00:03:06 +1000

    balancing

--- osu.Game.Rulesets.Taiko/Difficulty/Skills/Reading.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Skills/Reading.cs
@@ -12,10 +12,14 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
         protected override double SkillMultiplier => 1.0;
         protected override double StrainDecayBase => 0.4;
 
+        public static double LowSvBonus;
+        public static double HighSvBonus;
+        public static double ObjectDensity;
+
         private double currentStrain;
 
-        private const double high_sv_multiplier = 1;
-        private const double low_sv_multiplier = 0.25;
+        private const double high_sv_multiplier = 1.0;
+        private const double low_sv_multiplier = 1.0;
 
         /// <summary>
         /// Creates a <see cref="Rhythm"/> skill.
@@ -51,19 +55,19 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Skills
             const double low_sv_delta_time_range = 300; // The range for the transition around the delta time.
 
             // Center range for low SV adjustment based on hit object density.
-            const double low_sv_max_center = 200; // Upper bound for low SV adjustment.
-            const double low_sv_min_center = 100; // Lower bound for low SV adjustment.
-            const double low_sv_range = 100; // Reduced range to make the impact more abrupt at very low SVs.
+            const double low_sv_max_center = 150; // Upper bound for low SV adjustment.
+            const double low_sv_min_center = 50; // Lower bound for low SV adjustment.
+            const double low_sv_range = 240; // Reduced range to make the impact more abrupt at very low SVs.
 
             // Calculate the adjusted center for low SV influence based on object density.
-            double objectDensity = low_sv_max_center - (low_sv_max_center - low_sv_min_center)
+            ObjectDensity = low_sv_max_center - (low_sv_max_center - low_sv_min_center)
                 * sigmoid(current.DeltaTime, low_sv_delta_time_center, low_sv_delta_time_range);
 
             // Calculate bonuses based on high and low SV impact.
-            double highSvBonus = sigmoid(hitObject.EffectiveBPM, high_sv_center, high_sv_range);
-            double lowSvBonus = 1 - sigmoid(hitObject.EffectiveBPM, objectDensity, low_sv_range);
+            HighSvBonus = sigmoid(hitObject.EffectiveBPM, high_sv_center, high_sv_range);
+            LowSvBonus = 1 - sigmoid(hitObject.EffectiveBPM, ObjectDensity, low_sv_range);
 
-            return high_sv_multiplier * highSvBonus + low_sv_multiplier * lowSvBonus;
+            return high_sv_multiplier * HighSvBonus; // No bonuses to lowSV within starRating.
         }
 
         private double sigmoid(double value, double center, double range)
