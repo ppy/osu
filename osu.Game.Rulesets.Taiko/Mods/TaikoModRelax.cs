955a2dbe9194aabf5674c623227fc4c515b971b2
Jay Lawton <jaime@merkids.com>
2024-11-12 20:06:41 +1000

    make Relax incompatible with Swap and Random

--- osu.Game.Rulesets.Taiko/Mods/TaikoModRelax.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModRelax.cs
@@ -14,7 +14,8 @@ namespace osu.Game.Rulesets.Taiko.Mods
     {
         public override LocalisableString Description => @"No need to remember which key is correct anymore!";
 
-        public override Type[] IncompatibleMods => base.IncompatibleMods.Concat(new[] { typeof(TaikoModSingleTap) }).ToArray();
+        public override Type[] IncompatibleMods =>
+            base.IncompatibleMods.Concat(new[] { typeof(TaikoModSingleTap), typeof(TaikoModRandom), typeof(TaikoModSwap) }).ToArray();
 
         public void ApplyToDrawableHitObject(DrawableHitObject drawable)
         {
