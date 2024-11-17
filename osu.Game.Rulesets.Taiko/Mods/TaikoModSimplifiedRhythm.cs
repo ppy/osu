72210bf9fef29c50a4a9d7eea474b40a05c089c1
Hiviexd <rayenattia@gmail.com>
2024-11-17 23:34:30 +0100

    blank line

--- osu.Game.Rulesets.Taiko/Mods/TaikoModSimplifiedRhythm.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModSimplifiedRhythm.cs
@@ -69,6 +69,7 @@ namespace osu.Game.Rulesets.Taiko.Mods
                         {
                             patternStartIndex = i;
                         }
+
                         inPattern = true;
                     }
 
