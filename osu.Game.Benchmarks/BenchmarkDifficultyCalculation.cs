26e9c21d9ad5f5cacaffe4a1a82af7ef30e6f9e5
minisbett <39670899+minisbett@users.noreply.github.com>
2024-11-18 21:27:23 +0100

    Fix code quality

--- osu.Game.Benchmarks/BenchmarkDifficultyCalculation.cs
+++ osu.Game.Benchmarks/BenchmarkDifficultyCalculation.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.IO;
 using BenchmarkDotNet.Attributes;
 using osu.Framework.IO.Stores;
 using osu.Game.Beatmaps;
