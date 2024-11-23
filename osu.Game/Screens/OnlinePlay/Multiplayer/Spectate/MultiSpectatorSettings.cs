3713bb48b775c00da2fbbb6d86b98fc7b8ddafa5
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-11-23 01:09:58 -0500

    expand and contract settings from hover

--- osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorSettings.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorSettings.cs
@@ -1,80 +1,60 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input.Events;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Screens.Play.HUD;
-using osuTK;
 
 namespace osu.Game.Screens.OnlinePlay.Multiplayer.Spectate
 {
-    public partial class MultiSpectatorSettings : CompositeDrawable
+    public partial class MultiSpectatorSettings : ExpandingContainer
     {
-        private const double slide_duration = 200;
-
-        private readonly PlayerSettingsOverlay playerSettingsOverlay;
-        private readonly Container slidingContainer;
-
-        private readonly BindableBool opened = new BindableBool();
+        public const float CONTRACTED_WIDTH = 30;
+        public const int EXPANDED_WIDTH = 300;
 
         public MultiSpectatorSettings()
+            : base(CONTRACTED_WIDTH, EXPANDED_WIDTH)
         {
-            Origin = Anchor.TopLeft;
+            Origin = Anchor.TopRight;
             Anchor = Anchor.TopRight;
-            AutoSizeAxes = Axes.Both;
 
-            InternalChildren = new Drawable[]
+            PlayerSettingsOverlay playerSettingsOverlay;
+
+            InternalChild = new FillFlowContainer
             {
-                slidingContainer = new Container
+                Origin = Anchor.TopLeft,
+                Anchor = Anchor.TopLeft,
+                Direction = FillDirection.Horizontal,
+                AutoSizeAxes = Axes.Both,
+                Children = new Drawable[]
                 {
-                    Origin = Anchor.TopLeft,
-                    Anchor = Anchor.TopLeft,
-                    AutoSizeAxes = Axes.Both,
-                    Children = new Drawable[]
+                    new IconButton
                     {
-                        new IconButton
-                        {
-                            Icon = FontAwesome.Solid.Cog,
-                            Origin = Anchor.TopLeft,
-                            Anchor = Anchor.TopLeft,
-                            Position = new Vector2(-30, 0),
-                            Action = () => opened.Toggle()
-                        },
-                        playerSettingsOverlay = new PlayerSettingsOverlay()
+                        Icon = FontAwesome.Solid.Cog,
+                        Origin = Anchor.TopLeft,
+                        Anchor = Anchor.TopLeft,
+                        Action = () => Expanded.Toggle()
+                    },
+                    playerSettingsOverlay = new PlayerSettingsOverlay
+                    {
+                        Anchor = Anchor.TopLeft,
+                        Origin = Anchor.TopLeft
                     }
                 }
             };
 
             playerSettingsOverlay.Show();
-
-            opened.BindValueChanged(value =>
-            {
-                if (value.NewValue)
-                    open();
-                else
-                    close();
-            });
         }
 
-        private void open()
+        protected override void OnHoverLost(HoverLostEvent e)
         {
-            slidingContainer.MoveToOffset(new Vector2(-playerSettingsOverlay.Width, 0), slide_duration, Easing.Out).Then().OnComplete(c =>
-            {
-                c.Origin = Anchor.TopRight;
-                c.Position = Vector2.Zero;
-            });
-        }
-
-        private void close()
-        {
-            slidingContainer.MoveToOffset(new Vector2(playerSettingsOverlay.Width, 0), slide_duration, Easing.Out).Then().OnComplete(c =>
-            {
-                c.Origin = Anchor.TopLeft;
-                c.Position = Vector2.Zero;
-            });
+            // Prevent unexpanding when hovering player settings
+            if (!Contains(e.ScreenSpaceMousePosition))
+                base.OnHoverLost(e);
         }
     }
 }
