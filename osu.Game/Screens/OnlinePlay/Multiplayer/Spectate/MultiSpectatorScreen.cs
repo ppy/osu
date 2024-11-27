fed54ac1db6bf95cba714cee618afeb3809b88e3
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2024-11-27 17:47:42 +0900

    Combine new implementation back into the old one and use everywhere

--- osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorScreen.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Spectate/MultiSpectatorScreen.cs
@@ -127,7 +127,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Spectate
                 {
                     ReadyToStart = performInitialSeek,
                 },
-                new ExpandingPlayerSettingsOverlay()
+                new PlayerSettingsOverlay()
             };
 
             for (int i = 0; i < Users.Count; i++)
