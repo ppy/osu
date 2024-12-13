af1c56add447fbde1d755b7fcd11d16f67c65eae
StanR <hi@stanr.info>
2024-12-13 14:00:24 +0500

    Remove follow circle assumptions in slider distance calculation

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     {
         private const double wide_angle_multiplier = 1.5;
         private const double acute_angle_multiplier = 1.95;
-        private const double slider_multiplier = 1.35;
+        private const double slider_multiplier = 1.05;
         private const double velocity_change_multiplier = 0.75;
 
         /// <summary>
@@ -127,7 +127,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             // Add in additional slider velocity bonus.
             if (withSliderTravelDistance)
-                aimStrain += sliderBonus * slider_multiplier;
+                aimStrain += sliderBonus * 1.0;
 
             return aimStrain;
         }
