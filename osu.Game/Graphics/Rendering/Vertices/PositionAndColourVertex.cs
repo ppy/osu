ab4c83420e45067c2cbce15cfb18f6e5070cf2fe
Salman Alshamrani <frenzibyte@gmail.com>
2024-12-14 15:57:49 -0500

    Fix vertex class namespace

--- osu.Game/Graphics/Rendering/Vertices/PositionAndColourVertex.cs
+++ osu.Game/Graphics/Rendering/Vertices/PositionAndColourVertex.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -8,7 +8,7 @@ using osu.Framework.Graphics.Rendering.Vertices;
 using osuTK;
 using osuTK.Graphics.ES30;
 
-namespace osu.Game.Graphics.OpenGL.Vertices
+namespace osu.Game.Graphics.Rendering.Vertices
 {
     [StructLayout(LayoutKind.Sequential)]
     public struct PositionAndColourVertex : IEquatable<PositionAndColourVertex>, IVertex
