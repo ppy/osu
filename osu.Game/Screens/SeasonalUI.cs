34c255c9410430536c45bf1ac13f1a533e13fbc0
Dean Herbert <pe@ppy.sh>
2024-12-19 17:59:48 +0900

    Add christmas intro

---
+++ osu.Game/Screens/SeasonalUI.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Extensions.Color4Extensions;
+using osuTK.Graphics;
+
+namespace osu.Game.Screens
+{
+    public static class SeasonalUI
+    {
+        public static readonly bool ENABLED = true;
+
+        public static readonly Color4 PRIMARY_COLOUR_1 = Color4Extensions.FromHex("D32F2F");
+
+        public static readonly Color4 PRIMARY_COLOUR_2 = Color4Extensions.FromHex("388E3C");
+
+        public static readonly Color4 AMBIENT_COLOUR_1 = Color4Extensions.FromHex("FFC");
+
+        public static readonly Color4 AMBIENT_COLOUR_2 = Color4Extensions.FromHex("FFE4B5");
+    }
+}
