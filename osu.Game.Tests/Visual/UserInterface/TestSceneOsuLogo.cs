22f3831c0d46d11f7770c62c2dab4c2ee1132e36
Dean Herbert <pe@ppy.sh>
2024-12-19 18:44:44 +0900

    Add logo hat

--- osu.Game.Tests/Visual/UserInterface/TestSceneOsuLogo.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneOsuLogo.cs
@@ -4,21 +4,30 @@
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Game.Screens.Menu;
+using osuTK;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
     public partial class TestSceneOsuLogo : OsuTestScene
     {
+        private OsuLogo? logo;
+
         [Test]
         public void TestBasic()
         {
             AddStep("Add logo", () =>
             {
-                Child = new OsuLogo
+                Child = logo = new OsuLogo
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                 };
+            });
+
+            AddSliderStep("scale", 0.1, 2, 1, scale =>
+            {
+                if (logo != null)
+                    Child.Scale = new Vector2((float)scale);
             });
         }
     }
