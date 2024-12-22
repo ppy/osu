da0f2cddd20121ee611dae7c87f220e175739287
Plextora <87902114+bubb13gummy@users.noreply.github.com>
2024-12-22 12:09:27 -0500

    Return base for default FailCondition

--- osu.Game.Rulesets.Osu/Mods/OsuModSuddenDeath.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModSuddenDeath.cs
@@ -22,7 +22,7 @@ namespace osu.Game.Rulesets.Osu.Mods
         [SettingSource("Fail when missing on a slider tail")]
         public BindableBool SliderTailMiss { get; } = new BindableBool();
 
-        protected override bool FailCondition(HealthProcessor healthProcessor, JudgementResult result) => (
-            SliderTailMiss.Value && result.HitObject is SliderTailCircle && result.Type == HitResult.IgnoreMiss) || (result.Type.AffectsCombo() && !result.IsHit);
+        protected override bool FailCondition(HealthProcessor healthProcessor, JudgementResult result) =>
+            (SliderTailMiss.Value && result.HitObject is SliderTailCircle && result.Type == HitResult.IgnoreMiss) || base.FailCondition(healthProcessor, result);
     }
 }
