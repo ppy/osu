2c73b13b13568a928ebb6c671fcac78a5a546528
jkh675 <rluo2018@gmail.com>
2024-12-22 12:31:27 +0800

    Add timeline to audio screen

--- osu.Game/Screens/Edit/Audio/AudioScreen.cs
+++ osu.Game/Screens/Edit/Audio/AudioScreen.cs
@@ -1,14 +1,19 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+
 namespace osu.Game.Screens.Edit.Audio
 {
-    public partial class AudioScreen : EditorScreen
+    public partial class AudioScreen : EditorScreenWithTimeline
     {
         public AudioScreen()
             : base(EditorScreenMode.Audio)
+        { }
+
+        protected override Drawable CreateMainContent()
         {
-            Child = new ScreenWhiteBox.UnderConstructionMessage("Audio mode");
+            return new ScreenWhiteBox.UnderConstructionMessage("Audio mode");
         }
     }
 }
