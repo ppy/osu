6579b055618f375e06437f05ff70f612316e72a6
Dan Balasescu <smoogipoo@smgi.me>
2024-12-25 23:45:36 +0900

    Remove unused usings

--- osu.Game/Screens/OnlinePlay/OnlinePlayStyleSelect.cs
+++ osu.Game/Screens/OnlinePlay/OnlinePlayStyleSelect.cs
@@ -1,14 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Humanizer;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Screens;
 using osu.Game.Beatmaps;
 using osu.Game.Database;
 using osu.Game.Online.Rooms;
