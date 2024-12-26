901c9681306d3f514028bd632a123d52629fd554
kongehund <63306696+kongehund@users.noreply.github.com>
2024-12-26 01:24:42 +0100

    Remove unnecessasry using

--- osu.Game/Overlays/Volume/GlobalScrollAdjustsVolume.cs
+++ osu.Game/Overlays/Volume/GlobalScrollAdjustsVolume.cs
@@ -4,7 +4,6 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Events;
 using osu.Game.Input.Bindings;
 
