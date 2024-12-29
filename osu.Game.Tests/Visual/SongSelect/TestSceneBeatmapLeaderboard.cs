4972d3f129247bd06c2c49264370671380849b7e
Wleter <marcraftzastepczy@gmail.com>
2024-12-29 20:39:22 +0100

    fix code quality

--- osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapLeaderboard.cs
+++ osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapLeaderboard.cs
@@ -465,6 +465,7 @@ namespace osu.Game.Tests.Visual.SongSelect
         private partial class FailableScoresProvider : BeatmapLeaderboardScoresProvider
         {
             public new void SetErrorState(LeaderboardState state) => base.SetErrorState(state);
+
             public new void SetScores(IEnumerable<ScoreInfo>? scores, ScoreInfo? userScore = null)
             {
                 SetState(LeaderboardState.Retrieving);
