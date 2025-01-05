fcc7b73f455677f7f873b32a046f421edbce46c7
Eloise <eloisesharpe2708@gmail.com>
2025-01-05 15:16:40 +0000

    Update Desmos link

--- osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -41,8 +41,7 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Evaluators
 			// and comparing it to the expected time at this note's effective BPM
 			double density = (21000.0 / effectiveBPM) / noteObject.DeltaTime;
 			
-			// Explain properly soon
-			// https://www.desmos.com/calculator/ftvspcqgqd
+			// https://www.desmos.com/calculator/mx4dyydxji
 			double densityPenalty = 0.5 * DifficultyCalculationUtils.Logistic(density, 1, 15);
 
 			double midVelDifficulty = 0.5 * DifficultyCalculationUtils.Logistic(effectiveBPM, midVelocity.Center, 1.0 / (midVelocity.Range / 10));
