3a1ca52545528a2bbbe2228f770efd7815c484c4
StanR <87902114+bubb13gummy@users.noreply.github.com>
2025-01-06 20:08:14 +0500

    Fix angle bonuses calculating repetition incorrectly, apply distance scaling to wide bonus (#31320)
    * Fix angle bonuses calculating repetition incorrectly, apply distance scaling to wide bonus
    
    * Buff speed to compensate for streams losing pp
    
    * Adjust speed multiplier
    
    * Adjust wide scaling
    
    * Fix tests

--- osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
+++ osu.Game.Rulesets.Osu.Tests/OsuDifficultyCalculatorTest.cs
@@ -15,22 +15,22 @@ namespace osu.Game.Rulesets.Osu.Tests
     {
         protected override string ResourceAssembly => "osu.Game.Rulesets.Osu.Tests";
 
-        [TestCase(6.718709884850683d, 239, "diffcalc-test")]
-        [TestCase(1.4485749025771304d, 54, "zero-length-sliders")]
-        [TestCase(0.42630400627180914d, 4, "very-fast-slider")]
+        [TestCase(6.7153612142198682d, 239, "diffcalc-test")]
+        [TestCase(1.4484916289194889d, 54, "zero-length-sliders")]
+        [TestCase(0.42912495021837549d, 4, "very-fast-slider")]
         [TestCase(0.14143808967817237d, 2, "nan-slider")]
         public void Test(double expectedStarRating, int expectedMaxCombo, string name)
             => base.Test(expectedStarRating, expectedMaxCombo, name);
 
-        [TestCase(9.6343245007055653d, 239, "diffcalc-test")]
-        [TestCase(1.7550169162648608d, 54, "zero-length-sliders")]
-        [TestCase(0.55231632896800109d, 4, "very-fast-slider")]
+        [TestCase(9.6358837846598835d, 239, "diffcalc-test")]
+        [TestCase(1.754888327422514d, 54, "zero-length-sliders")]
+        [TestCase(0.55601568006454294d, 4, "very-fast-slider")]
         public void TestClockRateAdjusted(double expectedStarRating, int expectedMaxCombo, string name)
             => Test(expectedStarRating, expectedMaxCombo, name, new OsuModDoubleTime());
 
-        [TestCase(6.718709884850683d, 239, "diffcalc-test")]
-        [TestCase(1.4485749025771304d, 54, "zero-length-sliders")]
-        [TestCase(0.42630400627180914d, 4, "very-fast-slider")]
+        [TestCase(6.7153612142198682d, 239, "diffcalc-test")]
+        [TestCase(1.4484916289194889d, 54, "zero-length-sliders")]
+        [TestCase(0.42912495021837549d, 4, "very-fast-slider")]
         public void TestClassicMod(double expectedStarRating, int expectedMaxCombo, string name)
             => Test(expectedStarRating, expectedMaxCombo, name, new OsuModClassic());
 
