de0bdf0afd5b96c932c7d0bfb94818dc92cbb32c
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-01-10 19:34:56 +0900

    Add basic selection support

--- osu.Game/Screens/SelectV2/CarouselItem.cs
+++ osu.Game/Screens/SelectV2/CarouselItem.cs
@@ -2,22 +2,25 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using osu.Framework.Bindables;
 
 namespace osu.Game.Screens.SelectV2
 {
     /// <summary>
-    /// Represents a single display item for display in a <see cref="Carousel"/>.
+    /// Represents a single display item for display in a <see cref="Carousel{T}"/>.
     /// This is used to house information related to the attached model that helps with display and tracking.
     /// </summary>
     public abstract class CarouselItem : IComparable<CarouselItem>
     {
+        public readonly BindableBool Selected = new BindableBool();
+
         /// <summary>
         /// The model this item is representing.
         /// </summary>
         public readonly object Model;
 
         /// <summary>
-        /// The current Y position in the carousel. This is managed by <see cref="Carousel"/> and should not be set manually.
+        /// The current Y position in the carousel. This is managed by <see cref="Carousel{T}"/> and should not be set manually.
         /// </summary>
         public double CarouselYPosition { get; set; }
 
