73324654eed7174d31ea10e95858044f8ad8c87f
OliBomby <olivierschipper@gmail.com>
2025-01-11 03:18:17 +0100

    Fix SamplesChange not saving original value

--- osu.Game/Screens/Edit/Changes/SamplesChange.cs
+++ osu.Game/Screens/Edit/Changes/SamplesChange.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using osu.Game.Audio;
 using osu.Game.Rulesets.Objects;
 
@@ -14,7 +15,8 @@ namespace osu.Game.Screens.Edit.Changes
         {
         }
 
-        protected override IList<HitSampleInfo> ReadValue(HitObject target) => target.Samples;
+        // We have to copy the list because the Samples setter mutates the list instead of replacing the reference.
+        protected override IList<HitSampleInfo> ReadValue(HitObject target) => target.Samples.ToList();
 
         protected override void WriteValue(HitObject target, IList<HitSampleInfo> value) => target.Samples = value;
     }
