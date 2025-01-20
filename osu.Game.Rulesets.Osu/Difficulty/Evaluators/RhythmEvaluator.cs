71d1a181a6cc57c11779fbf549d74f549ae5706a
StanR <hi@stanr.info>
2025-01-21 00:58:19 +0500

    Adjust multipliers

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
@@ -15,8 +15,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
     {
         private const int history_time_max = 5 * 1000; // 5 seconds
         private const int history_objects_max = 32;
-        private const double rhythm_overall_multiplier = 0.85;
-        private const double rhythm_ratio_multiplier = 6.0;
+        private const double rhythm_overall_multiplier = 0.9;
+        private const double rhythm_ratio_multiplier = 5.7;
 
         /// <summary>
         /// Calculates a rhythm multiplier for the difficulty of the tap associated with historic data of the current <see cref="OsuDifficultyHitObject"/>.
