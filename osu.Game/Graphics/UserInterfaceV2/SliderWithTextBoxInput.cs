fd3ae66aa40d383a0bbf8a3cddda91c61f10109e
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-01-20 16:17:39 +0900

    Always select text when an editor slider-textbox is focused

--- osu.Game/Graphics/UserInterfaceV2/SliderWithTextBoxInput.cs
+++ osu.Game/Graphics/UserInterfaceV2/SliderWithTextBoxInput.cs
@@ -74,6 +74,7 @@ namespace osu.Game.Graphics.UserInterfaceV2
                         textBox = new LabelledTextBox
                         {
                             Label = labelText,
+                            SelectAllOnFocus = true,
                         },
                         slider = new SettingsSlider<T>
                         {
@@ -91,8 +92,6 @@ namespace osu.Game.Graphics.UserInterfaceV2
         }
 
         public bool TakeFocus() => GetContainingFocusManager()?.ChangeFocus(textBox) == true;
-
-        public bool SelectAll() => textBox.SelectAll();
 
         private bool updatingFromTextBox;
 
