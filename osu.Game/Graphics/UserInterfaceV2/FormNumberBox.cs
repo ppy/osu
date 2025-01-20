2b5ea4e6e0e859599affbcb5cf9151060679450b
Salman Alshamrani <frenzibyte@gmail.com>
2025-01-20 03:17:01 -0500

    Fix recent editor textbox regressions

--- osu.Game/Graphics/UserInterfaceV2/FormNumberBox.cs
+++ osu.Game/Graphics/UserInterfaceV2/FormNumberBox.cs
@@ -1,32 +1,30 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Globalization;
 using osu.Framework.Input;
 
 namespace osu.Game.Graphics.UserInterfaceV2
 {
     public partial class FormNumberBox : FormTextBox
     {
-        public bool AllowDecimals { get; init; }
+        private readonly bool allowDecimals;
 
-        internal override InnerTextBox CreateTextBox() => new InnerNumberBox
+        public FormNumberBox(bool allowDecimals = false)
         {
-            AllowDecimals = AllowDecimals,
+            this.allowDecimals = allowDecimals;
+        }
+
+        internal override InnerTextBox CreateTextBox() => new InnerNumberBox(allowDecimals)
+        {
             SelectAllOnFocus = true,
         };
 
         internal partial class InnerNumberBox : InnerTextBox
         {
-            public bool AllowDecimals { get; init; }
-
-            public InnerNumberBox()
+            public InnerNumberBox(bool allowDecimals)
             {
-                InputProperties = new TextInputProperties(TextInputType.Number, false);
+                InputProperties = new TextInputProperties(allowDecimals ? TextInputType.Decimal : TextInputType.Number, false);
             }
-
-            protected override bool CanAddCharacter(char character)
-                => char.IsAsciiDigit(character) || (AllowDecimals && CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator.Contains(character));
         }
     }
 }
