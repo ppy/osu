7b89bdbcc25480c650a47bbf6bef8d3cc4edbe85
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2025-01-23 18:25:55 +0900

    Fix incorrect continuation

--- osu.Game/Screens/OnlinePlay/Multiplayer/Match/MultiplayerMatchSettingsOverlay.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Match/MultiplayerMatchSettingsOverlay.cs
@@ -472,7 +472,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
                 {
                     client.CreateRoom(room).ContinueWith(t => Schedule(() =>
                     {
-                        if (t.IsCompleted)
+                        if (t.IsCompletedSuccessfully)
                             onSuccess(room);
                         else if (t.IsFaulted)
                         {
