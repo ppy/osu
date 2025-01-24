5da4505d68e41b936a4def2e7f4d40e63ad3dc9b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-01-24 18:34:04 +0900

    Adjust comments on `ICarouselPanel` to imply external use

--- osu.Game/Screens/SelectV2/ICarouselPanel.cs
+++ osu.Game/Screens/SelectV2/ICarouselPanel.cs
@@ -3,33 +3,33 @@
 
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Pooling;
 
 namespace osu.Game.Screens.SelectV2
 {
     /// <summary>
     /// An interface to be attached to any <see cref="Drawable"/>s which are used for display inside a <see cref="Carousel{T}"/>.
+    /// Importantly, all properties in this interface are managed by <see cref="Carousel{T}"/> and should not be written to elsewhere.
     /// </summary>
     public interface ICarouselPanel
     {
         /// <summary>
-        /// Whether this item has selection.
-        /// This is managed by <see cref="Carousel{T}"/> and should not be set manually.
+        /// Whether this item has selection. Should be read from to update the visual state.
         /// </summary>
         BindableBool Selected { get; }
 
         /// <summary>
-        /// Whether this item has keyboard selection.
-        /// This is managed by <see cref="Carousel{T}"/> and should not be set manually.
+        /// Whether this item has keyboard selection. Should be read from to update the visual state.
         /// </summary>
         BindableBool KeyboardSelected { get; }
 
         /// <summary>
-        /// The Y position which should be used for displaying this item within the carousel. This is managed by <see cref="Carousel{T}"/> and should not be set manually.
+        /// The Y position used internally for positioning in the carousel.
         /// </summary>
         double DrawYPosition { get; set; }
 
         /// <summary>
-        /// The carousel item this drawable is representing. This is managed by <see cref="Carousel{T}"/> and should not be set manually.
+        /// The carousel item this drawable is representing. Will be set before <see cref="PoolableDrawable.PrepareForUse"/> is called.
         /// </summary>
         CarouselItem? Item { get; set; }
     }
