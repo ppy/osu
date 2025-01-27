a6487fd44319e06023b3b656c293476022ff64e9
mrgamernavshorts <sandhucable02@gmail.com>
2025-01-27 12:18:52 +0530

    removed anime girls

--- osu.Android/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using Android;
-using Android.App;
-
-// used for AndroidBatteryInfo
-[assembly: UsesPermission(Manifest.Permission.BatteryStats)]
