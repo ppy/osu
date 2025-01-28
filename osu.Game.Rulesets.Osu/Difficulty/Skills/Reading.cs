206c17e0365e52dc67787d8c4f2caeefceb4c35e
kwotaq <PJXORISJ@gmail.com>
2025-01-28 23:10:29 +0200

    convert reading to a strain skill

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
@@ -1,45 +1,33 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
-using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
-    public class Reading : Skill
+    public class Reading : OsuStrainSkill
     {
-        private readonly List<double> difficulties = new List<double>();
-        private const double skill_multiplier = 2.4;
+        private double skillMultiplier => 1.1;
+        private double currentStrain;
+        private double strainDecayBase => 0.1;
 
         public Reading(Mod[] mods)
             : base(mods)
         {
         }
 
-        public override void Process(DifficultyHitObject current) => difficulties.Add(ReadingEvaluator.EvaluateDifficultyOf(current, Mods) * skill_multiplier);
+        private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
+        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => currentStrain * strainDecay(time - current.Previous(0).StartTime);
 
-        public override double DifficultyValue()
+        protected override double StrainValueAt(DifficultyHitObject current)
         {
-            double difficulty = 0;
+            currentStrain *= strainDecay(current.DeltaTime);
+            currentStrain += ReadingEvaluator.EvaluateDifficultyOf(current, Mods) * skillMultiplier;
 
-            // Sections with 0 difficulty are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
-            // These sections will not contribute to the difficulty.
-            var peaks = difficulties.Where(p => p > 0);
-
-            List<double> values = peaks.OrderByDescending(d => d).ToList();
-
-            // Difficulty is the weighted sum of the highest strains from every section.
-            // We're sorting from highest to lowest strain.
-            for (int i = 0; i < values.Count; i++)
-            {
-                difficulty += values[i] / (i + 1);
-            }
-
-            return difficulty;
+            return currentStrain;
         }
     }
 }
