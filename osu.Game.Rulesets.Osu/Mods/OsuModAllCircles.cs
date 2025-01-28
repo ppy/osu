99966a99e63e4734b4ff9ebe9ca8f36c6ca905e8
Potenchill <tsunamitacoman@gmail.com>
2025-01-28 10:26:55 -0600

    Make a function to convert objects to circles

--- osu.Game.Rulesets.Osu/Mods/OsuModAllCircles.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModAllCircles.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Beatmaps;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Osu.Scoring;
+using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
@@ -21,50 +23,54 @@ namespace osu.Game.Rulesets.Osu.Mods
                 if (osuBeatmap.HitObjects[i] is not Slider slider)
                     continue;
 
-                HitCircle newCircle = new HitCircle
-                {
-                    ComboIndex = slider.ComboIndex,
-                    IndexInCurrentCombo = slider.IndexInCurrentCombo,
-                    LastInCombo = slider.LastInCombo,
-                    NewCombo = slider.NewCombo,
-                    Position = slider.Position,
-                    Samples = slider.Samples,
-                    Scale = slider.Scale,
-                    StackHeight = slider.StackHeight,
-                    StartTime = slider.StartTime,
-                    TimeFadeIn = slider.TimeFadeIn,
-                    TimePreempt = slider.TimePreempt,
-                    HitWindows = new OsuHitWindows()
-                };
-                newCircle.HitWindows.SetDifficulty(osuBeatmap.Difficulty.OverallDifficulty);
+                HitCircle newCircle = convertToCircle(slider.HeadCircle, osuBeatmap);
 
                 osuBeatmap.HitObjects[i] = newCircle;
 
                 if (!ConvertEnds.Value)
                     continue;
-                
+
+                Vector2 otherEnd = slider.RepeatCount > 0 ? slider.NestedHitObjects.OfType<SliderRepeat>().First().Position : slider.EndPosition;
                 for (int k = 0; k <= slider.RepeatCount; k++)
                 {
-                    HitCircle repeatCircle = new HitCircle
-                    {
-                        ComboIndex = slider.ComboIndex + 1,
-                        IndexInCurrentCombo = slider.IndexInCurrentCombo + 1 + k,
-                        LastInCombo = slider.LastInCombo,
-                        NewCombo = slider.NewCombo,
-                        Position = k % 2 == 0 ? (slider.LastRepeat != null ? slider.LastRepeat.Position : slider.TailCircle.Position) : slider.HeadCircle.Position,
-                        Samples = slider.Samples,
-                        Scale = slider.Scale,
-                        StackHeight = slider.StackHeight,
-                        StartTime = slider.StartTime,
-                        TimeFadeIn = slider.TimeFadeIn,
-                        TimePreempt = slider.TimePreempt,
-                        HitWindows = new OsuHitWindows()
-                    };
-                    repeatCircle.HitWindows.SetDifficulty(osuBeatmap.Difficulty.OverallDifficulty);
+                    HitCircle repeatCircle;
+                    if (k % 2 == 0)
+                    { // If this should be at the other end of the slider
+                        repeatCircle = convertToCircle(slider.HeadCircle, osuBeatmap);
 
+                        repeatCircle.Position = otherEnd;
+                        repeatCircle.StartTime = slider.StartTime;
+                    }
+                    else
+                    { // If this should overlap with the beginning of the slider
+                        repeatCircle = convertToCircle(slider.HeadCircle, osuBeatmap);
+                    }
+
+                    repeatCircle.IndexInCurrentCombo += k + 1;
                     osuBeatmap.HitObjects.Insert(i + k + 1, repeatCircle);
                 }
             }
+        }
+        private HitCircle convertToCircle(HitCircle objectToConvert, OsuBeatmap osuBeatmap)
+        {
+            HitCircle newCircle = new HitCircle
+            {
+                ComboIndex = objectToConvert.ComboIndex,
+                IndexInCurrentCombo = objectToConvert.IndexInCurrentCombo,
+                LastInCombo = objectToConvert.LastInCombo,
+                NewCombo = objectToConvert.NewCombo,
+                Position = objectToConvert.Position,
+                Samples = objectToConvert.Samples,
+                Scale = objectToConvert.Scale,
+                StackHeight = objectToConvert.StackHeight,
+                StartTime = objectToConvert.StartTime,
+                TimeFadeIn = objectToConvert.TimeFadeIn,
+                TimePreempt = objectToConvert.TimePreempt,
+                HitWindows = new OsuHitWindows()
+            };
+            newCircle.HitWindows.SetDifficulty(osuBeatmap.Difficulty.OverallDifficulty);
+
+            return newCircle;
         }
     }
 }
