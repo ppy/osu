b4f63da048e16c9f0fd0d339ea13f33637dade9c
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-01-30 15:23:22 +0100

    Move control point double-up logic to `LegacyBeatmapExporter`
    Done for two reasons:
    
    - During review it was requested for the logic to be moved out of
      `BezierConverter` as `BezierConverter` was intended to produce
      "lazer style" sliders with per-control-point curve types,
      as a future usability / code layering concern.
    
    - It is also relevant for encode-decode stability. With how the logic
      was structured between the Bezier converter and the legacy beatmap
      encoder, the encoder would leave behind per-control-point Bezier curve
      specs that stable ignored, but subsequent encodes and decodes in lazer
      would end up multiplying the doubled-up control points ad nauseam.
      Instead, it is sufficient to only specify the curve type for the
      head control point as Bezier, not specify any further curve types
      later on, and instead just keep the double-up-control-point for new
      implicit segment logic which is enough to make stable cooperate
      (and also as close to outputting the slider exactly as stable would
      have produced it as we've ever been)

--- osu.Game/Rulesets/Objects/BezierConverter.cs
+++ osu.Game/Rulesets/Objects/BezierConverter.cs
@@ -136,7 +136,6 @@ namespace osu.Game.Rulesets.Objects
                         {
                             for (int j = 0; j < segment.Length - 1; j++)
                             {
-                                if (result.Count > 0 && j == 0) result.Add(new PathControlPoint(segment[j]));
                                 result.Add(new PathControlPoint(segment[j], j == 0 ? PathType.BEZIER : null));
                             }
                         }
@@ -148,7 +147,6 @@ namespace osu.Game.Rulesets.Objects
                         {
                             for (int j = 0; j < segment.Length - 1; j++)
                             {
-                                if (result.Count > 0 && j == 0) result.Add(new PathControlPoint(segment[j]));
                                 result.Add(new PathControlPoint(segment[j], j == 0 ? PathType.BEZIER : null));
                             }
                         }
@@ -160,7 +158,6 @@ namespace osu.Game.Rulesets.Objects
 
                         for (int j = 0; j < circleResult.Length - 1; j++)
                         {
-                            if (result.Count > 0 && j == 0) result.Add(new PathControlPoint(circleResult[j]));
                             result.Add(new PathControlPoint(circleResult[j], j == 0 ? PathType.BEZIER : null));
                         }
 
@@ -173,7 +170,6 @@ namespace osu.Game.Rulesets.Objects
 
                         for (int j = 0; j < bSplineResult.Length - 1; j++)
                         {
-                            if (result.Count > 0 && j == 0) result.Add(new PathControlPoint(bSplineResult[j]));
                             result.Add(new PathControlPoint(bSplineResult[j], j == 0 ? PathType.BEZIER : null));
                         }
 
