4d8ae221c47693d44c2f6b7d18c193d72147988b
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-02-05 15:10:15 +0900

    Remove unused `HitObjectInterval`

--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/TaikoDifficultyHitObject.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/TaikoDifficultyHitObject.cs
@@ -44,11 +44,6 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing
         public readonly TaikoRhythmData RhythmData;
 
         /// <summary>
-        /// The interval between this hit object and the surrounding hit objects in its rhythm group.
-        /// </summary>
-        public double? HitObjectInterval { get; set; }
-
-        /// <summary>
         /// Colour data for this hit object. This is used by colour evaluator to calculate colour difficulty, but can be used
         /// by other skills in the future.
         /// </summary>
