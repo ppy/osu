9d979dc3f4adb523269fb14f6d049986dab9d61b
Dean Herbert <pe@ppy.sh>
2025-02-07 02:37:16 +0900

    Refactor grouping to be much more efficient

--- osu.Game/Screens/SelectV2/BeatmapCarousel.cs
+++ osu.Game/Screens/SelectV2/BeatmapCarousel.cs
@@ -289,5 +289,5 @@ namespace osu.Game.Screens.SelectV2
         #endregion
     }
 
-    public record GroupDefinition(string Title);
+    public record GroupDefinition(object Data, string Title);
 }
