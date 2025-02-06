4f7089bee02a13668ffdaf3da5c5eb9baa13f10d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-02-06 13:32:11 +0900

    Adjust xmldoc and avoid LINQ overheads

--- osu.Game/Screens/SelectV2/CarouselItem.cs
+++ osu.Game/Screens/SelectV2/CarouselItem.cs
@@ -30,10 +30,9 @@ namespace osu.Game.Screens.SelectV2
         public float DrawHeight { get; set; } = DEFAULT_HEIGHT;
 
         /// <summary>
-        /// A number that defines the layer which this <see cref="CarouselItem"/> should be placed on depth-wise.
-        /// The higher the number, the farther the panel associated with this item is taken to the background.
+        /// Defines the display depth relative to other <see cref="CarouselItem"/>s.
         /// </summary>
-        public int DepthLayer { get; set; } = 0;
+        public int DepthLayer { get; set; }
 
         /// <summary>
         /// Whether this item is visible or hidden.
