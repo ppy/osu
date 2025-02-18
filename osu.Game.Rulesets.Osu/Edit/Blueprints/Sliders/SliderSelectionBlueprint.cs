ppy/osu

commit 1648f2efa306f587714178f113e69d8ad8c4ac02

Ensure slider is not selectable when body is not visible

osu.Game.Rulesets.Osu/Edit/Blueprints/Sliders/SliderSelectionBlueprint.cs
@@ -626,7 +626,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.Sliders
 
         public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
         {
-            if (BodyPiece.ReceivePositionalInputAt(screenSpacePos))
+            if (BodyPiece.ReceivePositionalInputAt(screenSpacePos) && DrawableObject.Body.Alpha > 0)
                 return true;
 
             if (ControlPointVisualiser == null)

commit 77e40140e5b5fc1c83892492e9809dc4b1b708e3

Fix selected sliders sometimes not being clickable in editor
Closes https://github.com/ppy/osu/issues/31918.
Regressed with
https://github.com/ppy/osu/commit/1648f2efa306f587714178f113e69d8ad8c4ac02
for obvious reasons.

osu.Game.Rulesets.Osu/Edit/Blueprints/Sliders/SliderSelectionBlueprint.cs
@@ -626,7 +626,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Blueprints.Sliders
 
         public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
         {
-            if (BodyPiece.ReceivePositionalInputAt(screenSpacePos) && DrawableObject.Body.Alpha > 0)
+            if (BodyPiece.ReceivePositionalInputAt(screenSpacePos) && (IsSelected || DrawableObject.Body.Alpha > 0))
                 return true;
 
             if (ControlPointVisualiser == null)