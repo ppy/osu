4ac4b308e10d041dec5960f808ce2d295171f3d2
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-02-24 09:48:03 +0100

    Add visual test coverage of team channels

--- osu.Game.Tests/Visual/Online/TestSceneChannelList.cs
+++ osu.Game.Tests/Visual/Online/TestSceneChannelList.cs
@@ -115,6 +115,12 @@ namespace osu.Game.Tests.Visual.Online
                     channelList.AddChannel(createRandomPrivateChannel());
             });
 
+            AddStep("Add Team Channels", () =>
+            {
+                for (int i = 0; i < 10; i++)
+                    channelList.AddChannel(createRandomTeamChannel());
+            });
+
             AddStep("Add Announce Channels", () =>
             {
                 for (int i = 0; i < 2; i++)
@@ -186,6 +192,17 @@ namespace osu.Game.Tests.Visual.Online
             {
                 Name = $"Announce {id}",
                 Type = ChannelType.Announce,
+                Id = id,
+            };
+        }
+
+        private Channel createRandomTeamChannel()
+        {
+            int id = TestResources.GetNextTestID();
+            return new Channel
+            {
+                Name = $"Team {id}",
+                Type = ChannelType.Team,
                 Id = id,
             };
         }
