1d9767380edd275a66858c5ffa30b23257157a6f
CloneWith <clonewith@qq.com>
2025-02-25 22:46:47 +0800

    Release focus on commit of resolution number box

--- osu.Game.Tournament/Screens/Setup/ResolutionSelector.cs
+++ osu.Game.Tournament/Screens/Setup/ResolutionSelector.cs
@@ -24,7 +24,6 @@ namespace osu.Game.Tournament.Screens.Setup
                 Text = "1080",
                 Width = 100,
                 CommitOnFocusLost = false,
-                ReleaseFocusOnCommit = false,
             });
 
             numberBox.OnCommit += (_, _) => checkAndApply();
