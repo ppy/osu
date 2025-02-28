c8fb5b7f5f3d91e58a42101cea78346299bdec9b
Dani211e <daniel.jenrich@gmail.com>
2025-02-28 21:30:34 +0100

    Hide dropdown when not on local tab

--- osu.Game/Screens/Select/BeatmapDetailAreaTabControl.cs
+++ osu.Game/Screens/Select/BeatmapDetailAreaTabControl.cs
@@ -7,11 +7,15 @@ using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Game.Graphics;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Screens.Select.Filter;
+using osu.Game.Screens.Select.Leaderboards;
+using osuTK.Graphics;
 
 namespace osu.Game.Screens.Select
 {
@@ -49,6 +53,7 @@ namespace osu.Game.Screens.Select
         private readonly OsuTabControl<BeatmapDetailAreaTabItem> tabs;
         private readonly SlimEnumDropdown<ScoreSortMode> sortModeDropdown;
         private readonly Container tabsContainer;
+        private readonly Container dropdownContainer;
 
         public BeatmapDetailAreaTabControl()
         {
@@ -56,49 +61,54 @@ namespace osu.Game.Screens.Select
 
             Children = new Drawable[]
             {
+                new Box
+                {
+                    Anchor = Anchor.BottomLeft,
+                    Origin = Anchor.BottomLeft,
+                    RelativeSizeAxes = Axes.X,
+                    Height = 1,
+                    Colour = Color4.White.Opacity(0.2f),
+                },
+                tabsContainer = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Child = tabs = new OsuTabControl<BeatmapDetailAreaTabItem>
+                    {
+                        Anchor = Anchor.BottomLeft,
+                        Origin = Anchor.BottomLeft,
+                        RelativeSizeAxes = Axes.Both,
+                        IsSwitchable = true,
+                    },
+                },
                 new GridContainer
                 {
+                    Origin = Anchor.BottomRight,
+                    Anchor = Anchor.BottomRight,
                     Height = HEIGHT,
-                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.X,
                     ColumnDimensions = new[]
                     {
                         new Dimension(),
                         new Dimension(GridSizeMode.AutoSize),
-                        new Dimension(GridSizeMode.Relative, 0.2f)
                     },
-                    RowDimensions = new[]
-                    {
-                        new Dimension(),
-                    },
+                    RowDimensions = new[] { new Dimension() },
                     Content = new[]
                     {
                         new Drawable[]
                         {
-                            tabsContainer = new Container
-                            {
-                                RelativeSizeAxes = Axes.Both,
-                                Child = tabs = new OsuTabControl<BeatmapDetailAreaTabItem>
-                                {
-                                    Anchor = Anchor.BottomLeft,
-                                    Origin = Anchor.BottomLeft,
-                                    RelativeSizeAxes = Axes.Both,
-                                    IsSwitchable = true,
-                                },
-                            },
                             modsCheckbox = new OsuTabControlCheckbox
                             {
-                                Padding = new MarginPadding { Right = 10 },
                                 Anchor = Anchor.BottomRight,
                                 Origin = Anchor.BottomRight,
                                 Text = @"Selected Mods",
                                 Alpha = 0,
                             },
-                            new Container
+                            dropdownContainer = new Container
                             {
-                                Anchor = Anchor.CentreLeft,
-                                Origin = Anchor.CentreLeft,
-                                Height = HEIGHT,
-                                RelativeSizeAxes = Axes.X,
+                                Anchor = Anchor.CentreRight,
+                                Origin = Anchor.CentreRight,
+                                RelativeSizeAxes = Axes.Y,
+                                Width = 110,
                                 Child = sortModeDropdown = new SlimEnumDropdown<ScoreSortMode>
                                 {
                                     RelativeSizeAxes = Axes.X,
@@ -125,16 +135,27 @@ namespace osu.Game.Screens.Select
         {
             OnFilter?.Invoke(tabs.Current.Value, modsCheckbox.Current.Value, sortModeDropdown.Current.Value);
 
+            if (tabs.Current.Value is BeatmapDetailAreaLeaderboardTabItem<BeatmapLeaderboardScope> leaderboard && leaderboard.Scope == BeatmapLeaderboardScope.Local)
+            {
+                dropdownContainer.ResizeWidthTo(110, 200, Easing.OutQuint);
+                dropdownContainer.FadeTo(1, 200, Easing.OutQuint);
+                modsCheckbox.Padding = new MarginPadding { Right = 5 };
+            }
+            else
+            {
+                dropdownContainer.ResizeWidthTo(0, 200, Easing.OutQuint);
+                dropdownContainer.FadeTo(0, 200, Easing.OutQuint);
+                modsCheckbox.Padding = new MarginPadding();
+            }
+
             if (tabs.Current.Value.FilterableByMods)
             {
                 modsCheckbox.FadeTo(1, 200, Easing.OutQuint);
-                sortModeDropdown.FadeTo(1, 200, Easing.OutQuint);
-                tabsContainer.Padding = new MarginPadding { Right = 20 };
+                tabsContainer.Padding = new MarginPadding { Right = 110 };
             }
             else
             {
                 modsCheckbox.FadeTo(0, 200, Easing.OutQuint);
-                sortModeDropdown.FadeTo(0, 200, Easing.OutQuint);
                 tabsContainer.Padding = new MarginPadding();
             }
         }
