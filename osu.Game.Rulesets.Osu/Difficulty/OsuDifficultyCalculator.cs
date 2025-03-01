6174c88e375241eafe52eae355710ca5cc1650c0
kwotaq <PJXORISJ@gmail.com>
2025-03-01 16:13:34 +0200

    square root length bonus instead of cubing it because thats how it works

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -56,8 +56,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             var reading = skills.OfType<Reading>().SingleOrDefault();
             double readingRating = reading == null ? 0.0 : Math.Sqrt(reading.DifficultyValue()) * difficulty_multiplier;
             double readingDifficultyStrainCount = reading?.CountTopWeightedStrains() ?? 0.0;
-            double readingLengthBonus = 1 + readingDifficultyStrainCount / 450.0;
-            readingRating *= Math.Cbrt(readingLengthBonus);
+            double readingLengthBonus = 1 + readingDifficultyStrainCount / 700.0;
+            readingRating *= Math.Sqrt(readingLengthBonus);
 
             if (mods.Any(m => m is OsuModTouchDevice))
             {
