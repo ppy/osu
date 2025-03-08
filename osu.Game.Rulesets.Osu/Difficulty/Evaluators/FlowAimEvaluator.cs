634edd3c55df1839b3b2f00575beca1b8ca8f8a4
Givikap120 <givikap120@gmail.com>
2025-03-08 10:43:29 +0200

    fix cheese

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/FlowAimEvaluator.cs
@@ -72,30 +72,26 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                     overlappedNotesWeight = 1 - o1 * o2 * o3;
                 }
 
+                var osuLast2Obj = (OsuDifficultyHitObject)current.Previous(2);
 
-                if (osuCurrObj.Index >= 4)
-                {
-                    var osuLast2Obj = (OsuDifficultyHitObject)current.Previous(2);
+                // Angle is constant
+                double angleCurr = osuCurrObj.Angle ?? 0;
+                double angleLast = osuLast1Obj.Angle ?? 0;
+                double angleLastLast = osuLast2Obj.Angle ?? 0;
 
-                    // Angle is constant
-                    double angleCurr = osuCurrObj.Angle ?? 0;
-                    double angleLast = osuLast1Obj.Angle ?? 0;
-                    double angleLastLast = osuLast2Obj.Angle ?? 0;
+                double deltaAngle = Math.Abs(angleLast - angleLastLast);
+                double isSameAngle = DifficultyCalculationUtils.Smoothstep(deltaAngle, 0.25, 0.15);
 
-                    double deltaAngle = Math.Abs(angleLast - angleLastLast);
-                    double isSameAngle = DifficultyCalculationUtils.Smoothstep(deltaAngle, 0.25, 0.15);
+                double currAngleBonus = AimEvaluator.CalcAcuteAngleBonus(angleCurr);
+                double prevAngleBonus = AimEvaluator.CalcAcuteAngleBonus(angleLastLast);
 
-                    double currAngleBonus = AimEvaluator.CalcAcuteAngleBonus(angleCurr);
-                    double prevAngleBonus = AimEvaluator.CalcAcuteAngleBonus(angleLastLast);
+                double angleBonusDifference = currAngleBonus > 0 ? Math.Clamp(prevAngleBonus / currAngleBonus, 0, 1) : 1;
 
-                    double angleBonusDifference = currAngleBonus > 0 ? Math.Clamp(prevAngleBonus / currAngleBonus, 0, 1) : 1;
+                // Or there was no stream before
+                double timeDifferenceFactor = DifficultyCalculationUtils.Smoothstep(osuCurrObj.StrainTime, osuLast1Obj.StrainTime * 0.75, osuLast1Obj.StrainTime * 0.55);
 
-                    // Or there was no stream before
-                    double timeDifferenceFactor = DifficultyCalculationUtils.Smoothstep(osuCurrObj.StrainTime, osuLast1Obj.StrainTime * 0.75, osuLast1Obj.StrainTime * 0.55);
-
-                    acuteAngleBonus *= 1 - 0.5 * isSameAngle * (1 - angleBonusDifference);
-                    angleChangeBonus *= 1 - 0.5 * Math.Max(isSameAngle * (1 - prevAngleBonus), timeDifferenceFactor);
-                }
+                acuteAngleBonus *= 1 - 0.5 * isSameAngle * (1 - angleBonusDifference);
+                angleChangeBonus *= 1 - 0.5 * Math.Max(isSameAngle * (1 - prevAngleBonus), timeDifferenceFactor);
 
                 // Don't apply both angle change and acute angle bonus at the same time if change is consistent
                 double angleChangeCurrent = Math.Abs((double)(osuCurrObj.AngleSigned - osuLast0Obj.AngleSigned));
@@ -135,13 +131,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             OsuHitObject o1 = (OsuHitObject)odho1.BaseObject, o2 = (OsuHitObject)odho2.BaseObject;
 
             double distance = Vector2.Distance(o1.StackedPosition, o2.StackedPosition);
-            double radius = o1.Radius * 0.85; // We want to decrease radius because you can't cheese with very small space
+            double radius = o1.Radius;
 
-            if (distance >= radius * 2)
-                return 0;
-            if (distance <= radius)
-                return 1;
-            return 1 - Math.Pow((distance - radius) / radius, 2);
+            return Math.Clamp(1 - Math.Pow((distance - radius) / radius, 2), 0, 1);
         }
 
         public static double CalculateFlowAcuteAngleBonus(DifficultyHitObject current)
