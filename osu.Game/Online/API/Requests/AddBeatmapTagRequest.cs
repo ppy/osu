4279efbe49f2ff715b2196e8e0c6d9a314a05946
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-03-17 12:20:37 +0900

    Fix tag add request using wrong method

--- osu.Game/Online/API/Requests/AddBeatmapTagRequest.cs
+++ osu.Game/Online/API/Requests/AddBeatmapTagRequest.cs
@@ -20,7 +20,7 @@ namespace osu.Game.Online.API.Requests
         protected override WebRequest CreateWebRequest()
         {
             var req = base.CreateWebRequest();
-            req.Method = HttpMethod.Post;
+            req.Method = HttpMethod.Put;
             return req;
         }
 
