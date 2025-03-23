ec5c5d85bfa1d1d0616dd01fb85c23f77ab43a7e
wulpine <wulpine@proton.me>
2025-03-23 23:20:11 +0300

    Use `CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH` directly
    Requested during code review.

--- osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Skills/Movement.cs
@@ -11,7 +11,6 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 {
     public class Movement : StrainDecaySkill
     {
-        private const float normalized_half_catcher_width = CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH;
         private const float absolute_player_positioning_error = 16f;
         private const double direction_change_bonus = 21.0;
 
@@ -55,8 +54,8 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 
             float playerPosition = Math.Clamp(
                 lastPlayerPosition.Value,
-                catchCurrent.NormalizedPosition - (normalized_half_catcher_width - absolute_player_positioning_error),
-                catchCurrent.NormalizedPosition + (normalized_half_catcher_width - absolute_player_positioning_error)
+                catchCurrent.NormalizedPosition - (CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH - absolute_player_positioning_error),
+                catchCurrent.NormalizedPosition + (CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH - absolute_player_positioning_error)
             );
 
             float distanceMoved = playerPosition - lastPlayerPosition.Value;
@@ -83,7 +82,8 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
                 }
 
                 // Base bonus for every movement, giving some weight to streams.
-                distanceAddition += 12.5 * Math.Min(Math.Abs(distanceMoved), normalized_half_catcher_width * 2) / (normalized_half_catcher_width * 6) / sqrtStrain;
+                distanceAddition += 12.5 * Math.Min(Math.Abs(distanceMoved), CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH * 2) / (CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH * 6)
+                    / sqrtStrain;
             }
 
             // Bonus for edge dashes.
@@ -102,10 +102,11 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
             }
 
             // There is an edge case where horizontal back and forth sliders create "buzz" patterns which are repeated "movements" with a distance lower than
-            // the platter's width but high enough to be considered a movement due to the absolute_player_positioning_error and normalized_half_catcher_width offsets
+            // the platter's width but high enough to be considered a movement due to the absolute_player_positioning_error and NORMALIZED_HALF_CATCHER_WIDTH offsets
             // We are detecting this exact scenario. The first back and forth is counted but all subsequent ones are nullified.
-            // To achieve that, we need to store the exact distances (distance ignoring absolute_player_positioning_error and normalized_half_catcher_width)
-            if (Math.Abs(exactDistanceMoved) <= normalized_half_catcher_width * 2 && exactDistanceMoved == -lastExactDistanceMoved && catchCurrent.StrainTime == lastStrainTime)
+            // To achieve that, we need to store the exact distances (distance ignoring absolute_player_positioning_error and NORMALIZED_HALF_CATCHER_WIDTH)
+            if (Math.Abs(exactDistanceMoved) <= CatchDifficultyHitObject.NORMALIZED_HALF_CATCHER_WIDTH * 2 && exactDistanceMoved == -lastExactDistanceMoved
+                                                                                                           && catchCurrent.StrainTime == lastStrainTime)
             {
                 if (isInBuzzSection)
                     distanceAddition = 0;
