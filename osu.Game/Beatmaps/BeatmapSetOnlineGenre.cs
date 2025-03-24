8e15c97442a3def39d85f784c8e50f58c9b5e8c1
Dan Balasescu <smoogipoo@smgi.me>
2025-03-24 12:48:48 +0900

    Fix deserialisation of null genre IDs

--- osu.Game/Beatmaps/BeatmapSetOnlineGenre.cs
+++ osu.Game/Beatmaps/BeatmapSetOnlineGenre.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using Newtonsoft.Json;
+
 namespace osu.Game.Beatmaps
 {
     public struct BeatmapSetOnlineGenre
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
