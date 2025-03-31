4936b197f1311e535e98bc283bc44b27f548bdf5
Dushyant Makwana <dushyantmakwana2004@gmail.com>
2025-03-31 20:10:34 +0530

    Update UseLocalResources.ps1

--- UseLocalResources.ps1
+++ UseLocalResources.ps1
@@ -1,12 +1,40 @@
-$CSPROJ="osu.Game/osu.Game.csproj"
-$SLN="osu.sln"
+$CSPROJ = Join-Path "osu.Game" "osu.Game.csproj"
+$SLN = "osu.sln"
+$RESOURCE_PATH = Join-Path ".." "osu-resources/osu.Game.Resources/osu.Game.Resources.csproj"
+$SLNF_PATH = "osu.Desktop.slnf"
 
-dotnet remove $CSPROJ package ppy.osu.Game.Resources;
-dotnet sln $SLN add ../osu-resources/osu.Game.Resources/osu.Game.Resources.csproj
-dotnet add $CSPROJ reference ../osu-resources/osu.Game.Resources/osu.Game.Resources.csproj
+# Validate project and solution files
+if (-Not (Test-Path $CSPROJ)) {
+    Write-Error "Project file $CSPROJ not found!"
+    exit
+}
+if (-Not (Test-Path $SLN)) {
+    Write-Error "Solution file $SLN not found!"
+    exit
+}
 
-$SLNF=Get-Content "osu.Desktop.slnf" | ConvertFrom-Json
-$TMP=New-TemporaryFile
-$SLNF.solution.projects += ("../osu-resources/osu.Game.Resources/osu.Game.Resources.csproj")
-ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
-Move-Item -Path $TMP -Destination "osu.Desktop.slnf" -Force
+# Remove package reference
+dotnet remove $CSPROJ package ppy.osu.Game.Resources
+
+# Add new project reference
+dotnet sln $SLN add $RESOURCE_PATH
+dotnet add $CSPROJ reference $RESOURCE_PATH
+
+# Update .slnf file if necessary
+if (-Not (Test-Path $SLNF_PATH)) {
+    Write-Error "Solution filter file $SLNF_PATH not found!"
+    exit
+}
+
+$SLNF = Get-Content $SLNF_PATH | ConvertFrom-Json
+$TMP = New-TemporaryFile
+
+if (-Not ($SLNF.solution.projects -contains $RESOURCE_PATH)) {
+    $SLNF.solution.projects += $RESOURCE_PATH
+    ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
+    Move-Item -Path $TMP -Destination $SLNF_PATH -Force
+    Write-Output "Updated $SLNF_PATH successfully!"
+}
+else {
+    Write-Output "Project already exists in $SLNF_PATH. No changes made."
+}
