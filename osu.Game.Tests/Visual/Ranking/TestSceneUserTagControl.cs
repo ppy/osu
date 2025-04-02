f5033c954f12fd9911b94d1fa2b9188d673338f7
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-04-02 13:25:18 +0200

    Add rudimentary test coverage

--- osu.Game.Tests/Visual/Ranking/TestSceneUserTagControl.cs
+++ osu.Game.Tests/Visual/Ranking/TestSceneUserTagControl.cs
@@ -9,6 +9,8 @@ using osu.Game.Beatmaps;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Taiko;
 using osu.Game.Screens.Ranking;
 
 namespace osu.Game.Tests.Visual.Ranking
@@ -20,10 +22,6 @@ namespace osu.Game.Tests.Visual.Ranking
         [SetUpSteps]
         public void SetUpSteps()
         {
-            AddStep("set up working beatmap", () =>
-            {
-                Beatmap.Value.BeatmapInfo.OnlineID = 42;
-            });
             AddStep("set up network requests", () =>
             {
                 dummyAPI.HandleRequest = request =>
@@ -40,6 +38,7 @@ namespace osu.Game.Tests.Visual.Ranking
                                     new APITag { Id = 2, Name = "alt", Description = "Colloquial term for maps which use rhythms that encourage the player to alternate notes. Typically distinct from burst or stream maps.", },
                                     new APITag { Id = 3, Name = "aim", Description = "Category for difficulty relating to cursor movement.", },
                                     new APITag { Id = 4, Name = "tap", Description = "Category for difficulty relating to tapping input.", },
+                                    new APITag { Id = 5, Name = "mono-heavy", Description = "Features monos used in large amounts.", RulesetId = 1, },
                                 ]
                             }), 500);
                             return true;
@@ -67,19 +66,34 @@ namespace osu.Game.Tests.Visual.Ranking
                     return false;
                 };
             });
-            AddStep("create control", () =>
+            AddStep("show for osu! beatmap", () =>
             {
-                Child = new PopoverContainer
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    Child = new UserTagControl(Beatmap.Value.BeatmapInfo)
-                    {
-                        Width = 500,
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                    }
-                };
+                var working = CreateWorkingBeatmap(new OsuRuleset().RulesetInfo);
+                working.BeatmapInfo.OnlineID = 42;
+                Beatmap.Value = working;
+                recreateControl();
             });
+            AddStep("show for taiko beatmap", () =>
+            {
+                var working = CreateWorkingBeatmap(new TaikoRuleset().RulesetInfo);
+                working.BeatmapInfo.OnlineID = 44;
+                Beatmap.Value = working;
+                recreateControl();
+            });
+        }
+
+        private void recreateControl()
+        {
+            Child = new PopoverContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Child = new UserTagControl(Beatmap.Value.BeatmapInfo)
+                {
+                    Width = 500,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                }
+            };
         }
     }
 }
