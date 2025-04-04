f5b849b4f36a27d06ed42efff921504749d49e3e
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-04-04 11:17:04 +0200

    Remove filtering & rearranging controls from now playing overlay

--- osu.Game/Overlays/Music/PlaylistItem.cs
+++ osu.Game/Overlays/Music/PlaylistItem.cs
@@ -1,15 +1,13 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Pooling;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
@@ -20,118 +18,106 @@ using osuTK.Graphics;
 
 namespace osu.Game.Overlays.Music
 {
-    public partial class PlaylistItem : OsuRearrangeableListItem<Live<BeatmapSetInfo>>, IFilterable
+    public partial class PlaylistItem : PoolableDrawable, IHasCurrentValue<Live<BeatmapSetInfo>>
     {
-        public readonly Bindable<Live<BeatmapSetInfo>> SelectedSet = new Bindable<Live<BeatmapSetInfo>>();
+        public Bindable<Live<BeatmapSetInfo>> Current
+        {
+            get => current.Current;
+            set => current.Current = value;
+        }
 
-        public Action<Live<BeatmapSetInfo>> RequestSelection;
+        private readonly BindableWithCurrent<Live<BeatmapSetInfo>> current = new BindableWithCurrent<Live<BeatmapSetInfo>>();
 
-        private TextFlowContainer text;
-        private ITextPart titlePart;
+        private readonly Bindable<Live<BeatmapSetInfo>?> selectedSet = new Bindable<Live<BeatmapSetInfo>?>();
+        private Action<Live<BeatmapSetInfo>>? requestSelection;
+
+        private TextFlowContainer text = null!;
+        private ITextPart? titlePart;
 
         [Resolved]
-        private OsuColour colours { get; set; }
+        private OsuColour colours { get; set; } = null!;
 
-        public PlaylistItem(Live<BeatmapSetInfo> item)
-            : base(item)
+        [Resolved]
+        private PlaylistOverlay playlistOverlay { get; set; } = null!;
+
+        public PlaylistItem()
         {
-            Padding = new MarginPadding { Left = 5 };
+            Padding = new MarginPadding { Horizontal = 10 };
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(PlaylistOverlay playlistOverlay)
         {
-            HandleColour = colours.Gray5;
+            RelativeSizeAxes = Axes.X;
+            Height = 20;
+
+            InternalChild = text = new OsuTextFlowContainer
+            {
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreLeft,
+                RelativeSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal,
+            };
+
+            selectedSet.BindTo(playlistOverlay.SelectedSet);
+            requestSelection = playlistOverlay.ItemSelected;
         }
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
-
-            Model.PerformRead(m =>
-            {
-                var metadata = m.Metadata;
-
-                var title = new RomanisableString(metadata.TitleUnicode, metadata.Title);
-                var artist = new RomanisableString(metadata.ArtistUnicode, metadata.Artist);
-
-                titlePart = text.AddText(title, sprite => sprite.Font = OsuFont.GetFont(weight: FontWeight.Regular));
-                titlePart.DrawablePartsRecreated += _ => updateSelectionState(SelectedSet.Value, applyImmediately: true);
-
-                text.AddText(@"  "); // to separate the title from the artist.
-                text.AddText(artist, sprite =>
-                {
-                    sprite.Font = OsuFont.GetFont(size: 14, weight: FontWeight.Bold);
-                    sprite.Colour = colours.Gray9;
-                });
-
-                SelectedSet.BindValueChanged(set => updateSelectionState(set.NewValue));
-                updateSelectionState(SelectedSet.Value, applyImmediately: true);
-            });
+            Current.BindValueChanged(_ => onItemChanged(), true);
+            selectedSet.BindValueChanged(updateSelectionState, true);
         }
 
-        private bool selected;
-
-        private void updateSelectionState(Live<BeatmapSetInfo> selectedSet, bool applyImmediately = false)
+        private void onItemChanged() => Current.Value.PerformRead(m =>
         {
-            bool wasSelected = selected;
-            selected = selectedSet?.Equals(Model) == true;
+            var metadata = m.Metadata;
 
-            // Immediate updates should forcibly set correct state regardless of previous state.
-            // This ensures that the initial state is correctly applied.
-            if (wasSelected == selected && !applyImmediately)
+            var title = new RomanisableString(metadata.TitleUnicode, metadata.Title);
+            var artist = new RomanisableString(metadata.ArtistUnicode, metadata.Artist);
+
+            text.Clear();
+
+            titlePart = text.AddText(title, sprite => sprite.Font = OsuFont.GetFont(weight: FontWeight.Regular));
+            titlePart.DrawablePartsRecreated += _ =>
+            {
+                selectedSet.TriggerChange();
+                FinishTransforms(true);
+            };
+
+            text.AddText(@"  "); // to separate the title from the artist.
+            text.AddText(artist, sprite =>
+            {
+                sprite.Font = OsuFont.GetFont(size: 14, weight: FontWeight.Bold);
+                sprite.Colour = colours.Gray9;
+            });
+
+            selectedSet.TriggerChange();
+            FinishTransforms(true);
+        });
+
+        private bool? selected;
+
+        private void updateSelectionState(ValueChangedEvent<Live<BeatmapSetInfo>?> selected)
+        {
+            bool? wasSelected = this.selected;
+            this.selected = selected.NewValue?.Equals(Current.Value) == true;
+
+            if (wasSelected == this.selected)
                 return;
 
-            foreach (Drawable s in titlePart.Drawables)
-                s.FadeColour(selected ? colours.Yellow : Color4.White, applyImmediately ? 0 : FADE_DURATION);
+            if (titlePart != null)
+            {
+                foreach (Drawable s in titlePart.Drawables)
+                    s.FadeColour(this.selected == true ? colours.Yellow : Color4.White, 100);
+            }
         }
-
-        protected override Drawable CreateContent() => new DelayedLoadWrapper(text = new OsuTextFlowContainer
-        {
-            RelativeSizeAxes = Axes.X,
-            AutoSizeAxes = Axes.Y,
-        });
 
         protected override bool OnClick(ClickEvent e)
         {
-            RequestSelection?.Invoke(Model);
+            requestSelection?.Invoke(Current.Value);
             return true;
         }
-
-        private bool inSelectedCollection = true;
-
-        public bool InSelectedCollection
-        {
-            get => inSelectedCollection;
-            set
-            {
-                if (inSelectedCollection == value)
-                    return;
-
-                inSelectedCollection = value;
-                updateFilter();
-            }
-        }
-
-        public IEnumerable<LocalisableString> FilterTerms => Model.PerformRead(m => m.Metadata.GetSearchableTerms()).Select(s => (LocalisableString)s).ToArray();
-
-        private bool matchingFilter = true;
-
-        public bool MatchingFilter
-        {
-            get => matchingFilter && inSelectedCollection;
-            set
-            {
-                if (matchingFilter == value)
-                    return;
-
-                matchingFilter = value;
-                updateFilter();
-            }
-        }
-
-        private void updateFilter() => this.FadeTo(MatchingFilter ? 1 : 0, 200);
-
-        public bool FilteringActive { get; set; }
     }
 }
