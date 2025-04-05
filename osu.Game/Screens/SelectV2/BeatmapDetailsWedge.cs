18b0b76a618d920a0a872464b77ce9d98cc7c79a
Salman Alshamrani <frenzibyte@gmail.com>
2025-04-05 01:16:06 -0400

    Remove unused using directive

--- osu.Game/Screens/SelectV2/BeatmapDetailsWedge.cs
+++ osu.Game/Screens/SelectV2/BeatmapDetailsWedge.cs
@@ -6,7 +6,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
