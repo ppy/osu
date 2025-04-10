ba5932c1dd4eaff2d6e61a9b4dabc3e52dc5e36b
Dean Herbert <pe@ppy.sh>
2025-04-11 02:11:45 +0900

    Revert "Use median for statistic display"
    This reverts commit fa06643bb6c0aacde659640ae0a65c68ab9b0c61.
    
    Revert "Remove mean hit error calculation"
    
    This reverts commit b3c578e5455c572e34e2def301ba657182747149.

--- osu.Game/Rulesets/Scoring/HitEventExtensions.cs
+++ osu.Game/Rulesets/Scoring/HitEventExtensions.cs
@@ -55,6 +55,23 @@ namespace osu.Game.Rulesets.Scoring
         }
 
         /// <summary>
+        /// Calculates the average hit offset/error for a sequence of <see cref="HitEvent"/>s, where negative numbers mean the user hit too early on average.
+        /// </summary>
+        /// <returns>
+        /// A non-null <see langword="double"/> value if unstable rate could be calculated,
+        /// and <see langword="null"/> if unstable rate cannot be calculated due to <paramref name="hitEvents"/> being empty.
+        /// </returns>
+        public static double? CalculateAverageHitError(this IEnumerable<HitEvent> hitEvents)
+        {
+            double[] timeOffsets = hitEvents.Where(AffectsUnstableRate).Select(ev => ev.TimeOffset).ToArray();
+
+            if (timeOffsets.Length == 0)
+                return null;
+
+            return timeOffsets.Average();
+        }
+
+        /// <summary>
         /// Calculates the median hit offset/error for a sequence of <see cref="HitEvent"/>s, where negative numbers mean the user hit too early on average.
         /// </summary>
         /// <returns>
