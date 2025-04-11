85556e0c3e29e3ccb12f7324db82d18ff949a7fa
Salman Alshamrani <frenzibyte@gmail.com>
2025-04-11 08:31:03 -0400

    Introduce numeric value in beatmap hit count statistics

--- osu.Game.Rulesets.Osu/Beatmaps/OsuBeatmap.cs
+++ osu.Game.Rulesets.Osu/Beatmaps/OsuBeatmap.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
@@ -15,6 +16,7 @@ namespace osu.Game.Rulesets.Osu.Beatmaps
             int circles = HitObjects.Count(c => c is HitCircle);
             int sliders = HitObjects.Count(s => s is Slider);
             int spinners = HitObjects.Count(s => s is Spinner);
+            int sum = Math.Max(1, circles + sliders + spinners);
 
             return new[]
             {
@@ -23,18 +25,21 @@ namespace osu.Game.Rulesets.Osu.Beatmaps
                     Name = "Circles",
                     Content = circles.ToString(),
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
+                    Ratio = circles / (float)sum,
                 },
                 new BeatmapStatistic
                 {
                     Name = "Sliders",
                     Content = sliders.ToString(),
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Sliders),
+                    Ratio = sliders / (float)sum,
                 },
                 new BeatmapStatistic
                 {
                     Name = @"Spinners",
                     Content = spinners.ToString(),
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Spinners),
+                    Ratio = Math.Min(spinners / 10f, 1),
                 }
             };
         }
--- osu.Game.Rulesets.Taiko/Beatmaps/TaikoBeatmap.cs
+++ osu.Game.Rulesets.Taiko/Beatmaps/TaikoBeatmap.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
@@ -15,6 +16,7 @@ namespace osu.Game.Rulesets.Taiko.Beatmaps
             int hits = HitObjects.Count(s => s is Hit);
             int drumRolls = HitObjects.Count(s => s is DrumRoll);
             int swells = HitObjects.Count(s => s is Swell);
+            int sum = Math.Max(1, hits + drumRolls + swells);
 
             return new[]
             {
@@ -23,18 +25,21 @@ namespace osu.Game.Rulesets.Taiko.Beatmaps
                     Name = @"Hits",
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Circles),
                     Content = hits.ToString(),
+                    Ratio = hits / (float)sum,
                 },
                 new BeatmapStatistic
                 {
                     Name = @"Drumrolls",
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Sliders),
                     Content = drumRolls.ToString(),
+                    Ratio = drumRolls / (float)sum,
                 },
                 new BeatmapStatistic
                 {
                     Name = @"Swells",
                     CreateIcon = () => new BeatmapStatisticIcon(BeatmapStatisticsIconType.Spinners),
                     Content = swells.ToString(),
+                    Ratio = Math.Min(swells / 10f, 1),
                 }
             };
         }
