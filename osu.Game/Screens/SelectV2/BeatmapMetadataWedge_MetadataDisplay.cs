8dcc8110f3a75f7adc8694ea2bc865d19fa7e65b
Salman Alshamrani <frenzibyte@gmail.com>
2025-04-11 11:45:58 -0400

    Refactor metadata display to support truncation properly

--- osu.Game/Screens/SelectV2/BeatmapMetadataWedge_MetadataDisplay.cs
+++ osu.Game/Screens/SelectV2/BeatmapMetadataWedge_MetadataDisplay.cs
@@ -2,22 +2,14 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using Humanizer;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Cursor;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
-using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Overlays;
 using osuTK;
 
@@ -27,6 +19,14 @@ namespace osu.Game.Screens.SelectV2
     {
         private partial class MetadataDisplay : FillFlowContainer
         {
+            private readonly OsuSpriteText labelText;
+            private readonly OsuSpriteText contentText;
+            private readonly OsuSpriteText contentLinkText;
+            private readonly OsuHoverContainer contentLink;
+            private readonly DrawableDate contentDate;
+            private readonly TagsLine contentTags;
+            private readonly LoadingSpinner contentLoading;
+
             private (LocalisableString value, Action? linkAction)? data;
 
             public (LocalisableString value, Action? linkAction)? Data
@@ -36,28 +36,12 @@ namespace osu.Game.Screens.SelectV2
                 {
                     data = value;
 
-                    valueText.Clear();
-
-                    if (value.HasValue)
-                    {
-                        string valueString = value.Value.value.ToString();
-                        // todo: this logic is not perfect but we don't have a way to truncate text in TextFlowContainer yet.
-                        string truncatedValueString = valueString.Truncate(24);
-
-                        if (value.Value.linkAction != null)
-                            valueText.AddLink(truncatedValueString, value.Value.linkAction, valueString != truncatedValueString ? valueString : null);
-                        else
-                            valueText.AddText(truncatedValueString);
-                    }
+                    if (value?.linkAction != null)
+                        setLink(value.Value.value, value.Value.linkAction);
+                    else if (value.HasValue)
+                        setText(value.Value.value);
                     else
-                    {
-                        valueText.AddArbitraryDrawable(new LoadingSpinner
-                        {
-                            Size = new Vector2(10),
-                            Margin = new MarginPadding { Top = 3f },
-                            State = { Value = Visibility.Visible },
-                        });
-                    }
+                        setLoading();
                 }
             }
 
@@ -65,46 +49,24 @@ namespace osu.Game.Screens.SelectV2
             {
                 set
                 {
-                    valueText.Clear();
-
                     if (value != null)
-                        valueText.AddArbitraryDrawable(new DrawableDate(value.Value, textSize: OsuFont.Style.Caption1.Size, italic: false));
+                        setDate(value.Value);
                     else
-                        valueText.AddText("-");
+                        setText("-");
                 }
             }
 
             public (string[] tags, Action<string> linkAction) Tags
             {
-                set
-                {
-                    valueText.Clear();
-                    int total = 0;
-
-                    foreach (string tag in value.tags)
-                    {
-                        total += tag.Length + 1;
-
-                        // todo: this logic is not perfect but we don't have a way to truncate text in TextFlowContainer yet.
-                        if (total > 80)
-                        {
-                            valueText.AddArbitraryDrawable(new TagsOverflowButton(value.tags));
-                            break;
-                        }
-
-                        valueText.AddLink(tag, () => value.linkAction(tag));
-                        valueText.AddText(" ");
-                    }
-                }
+                set => setTags(value.tags, value.linkAction);
             }
-
-            private readonly OsuSpriteText labelText;
-            private readonly LinkFlowContainer valueText;
 
             public MetadataDisplay(LocalisableString label)
             {
                 RelativeSizeAxes = Axes.X;
                 AutoSizeAxes = Axes.Y;
+
+                Padding = new MarginPadding { Right = 10 };
 
                 InternalChildren = new Drawable[]
                 {
@@ -113,11 +75,37 @@ namespace osu.Game.Screens.SelectV2
                         Text = label,
                         Font = OsuFont.Style.Caption1.With(weight: FontWeight.SemiBold),
                     },
-                    valueText = new LinkFlowContainer(t => t.Font = OsuFont.Style.Caption1)
+                    new Container
                     {
                         RelativeSizeAxes = Axes.X,
                         Height = OsuFont.Style.Caption1.Size,
-                    }
+                        Children = new Drawable[]
+                        {
+                            contentText = new TruncatingSpriteText
+                            {
+                                RelativeSizeAxes = Axes.X,
+                                Font = OsuFont.Style.Caption1,
+                            },
+                            contentLink = new OsuHoverContainer
+                            {
+                                AutoSizeAxes = Axes.Both,
+                                Child = contentLinkText = new TruncatingSpriteText
+                                {
+                                    Font = OsuFont.Style.Caption1,
+                                },
+                            },
+                            contentDate = new DrawableDate(default, OsuFont.Style.Caption1.Size, false),
+                            contentTags = new TagsLine(),
+                            contentLoading = new LoadingSpinner
+                            {
+                                Anchor = Anchor.TopLeft,
+                                Origin = Anchor.TopLeft,
+                                Size = new Vector2(10),
+                                Margin = new MarginPadding { Top = 3f },
+                                State = { Value = Visibility.Visible },
+                            }
+                        },
+                    },
                 };
             }
 
@@ -125,105 +113,61 @@ namespace osu.Game.Screens.SelectV2
             private void load(OverlayColourProvider colourProvider)
             {
                 labelText.Colour = colourProvider.Content1;
-                valueText.Colour = colourProvider.Content2;
+                contentText.Colour = colourProvider.Content2;
+                contentLink.IdleColour = colourProvider.Light2;
             }
 
-            private partial class TagsOverflowButton : CompositeDrawable, IHasPopover, IHasLineBaseHeight
+            protected override void Update()
             {
-                private readonly string[] tags;
-
-                private Box box = null!;
-                private OsuSpriteText text = null!;
-
-                [Resolved]
-                private OverlayColourProvider colourProvider { get; set; } = null!;
-
-                [Resolved]
-                private SongSelect? songSelect { get; set; }
-
-                public float LineBaseHeight => text.LineBaseHeight;
-
-                public TagsOverflowButton(string[] tags)
-                {
-                    this.tags = tags;
-                }
-
-                [BackgroundDependencyLoader]
-                private void load()
-                {
-                    Size = new Vector2(OsuFont.Style.Caption1.Size);
-                    CornerRadius = 1.5f;
-                    Masking = true;
-
-                    InternalChildren = new Drawable[]
-                    {
-                        box = new Box
-                        {
-                            Colour = colourProvider.Light1,
-                            RelativeSizeAxes = Axes.Both,
-                        },
-                        text = new OsuSpriteText
-                        {
-                            Y = -2,
-                            Anchor = Anchor.TopCentre,
-                            Origin = Anchor.TopCentre,
-                            Text = "...",
-                            Colour = colourProvider.Background4,
-                            Font = OsuFont.Style.Caption1.With(weight: FontWeight.Bold),
-                        }
-                    };
-                }
-
-                protected override bool OnHover(HoverEvent e)
-                {
-                    box.FadeColour(colourProvider.Content2, 300, Easing.OutQuint);
-                    return base.OnHover(e);
-                }
-
-                protected override void OnHoverLost(HoverLostEvent e)
-                {
-                    box.FadeColour(colourProvider.Light1, 300, Easing.OutQuint);
-                    base.OnHoverLost(e);
-                }
-
-                protected override bool OnClick(ClickEvent e)
-                {
-                    box.FlashColour(colourProvider.Content1, 300, Easing.OutQuint);
-                    this.ShowPopover();
-                    return true;
-                }
-
-                public Popover GetPopover() => new TagsOverflowPopover(tags, songSelect);
+                base.Update();
+                contentLinkText.MaxWidth = ChildSize.X;
             }
 
-            public partial class TagsOverflowPopover : OsuPopover
+            private void clear()
             {
-                private readonly string[] tags;
-                private readonly SongSelect? songSelect;
+                contentText.Text = string.Empty;
+                contentLinkText.Text = string.Empty;
+                contentDate.Hide();
+                contentTags.Tags = Array.Empty<string>();
+                contentLoading.Hide();
+            }
 
-                public TagsOverflowPopover(string[] tags, SongSelect? songSelect)
-                {
-                    this.tags = tags;
-                    this.songSelect = songSelect;
-                }
+            private void setText(LocalisableString text)
+            {
+                clear();
 
-                [BackgroundDependencyLoader]
-                private void load()
-                {
-                    LinkFlowContainer textFlow;
+                contentText.Text = text;
+            }
 
-                    Child = textFlow = new LinkFlowContainer(t => t.Font = OsuFont.Style.Caption1)
-                    {
-                        Width = 200,
-                        AutoSizeAxes = Axes.Y,
-                    };
+            private void setLink(LocalisableString text, Action action) => Schedule(() =>
+            {
+                clear();
 
-                    foreach (string tag in tags)
-                    {
-                        textFlow.AddLink(tag, () => songSelect?.Search(tag));
-                        textFlow.AddText(" ");
-                    }
-                }
+                contentLinkText.Text = text;
+                contentLink.Action = action;
+            });
+
+            private void setDate(DateTimeOffset date)
+            {
+                clear();
+
+                contentDate.Show();
+                contentDate.Date = date;
+            }
+
+            private void setTags(string[] tags, Action<string> link)
+            {
+                clear();
+
+                contentTags.Tags = tags;
+                contentTags.Action = link;
+            }
+
+            private void setLoading()
+            {
+                clear();
+
+                contentLoading.Show();
             }
         }
     }
