a2a1ddaa79fe63bd0b97757b59009b6873182107
Salman Alshamrani <frenzibyte@gmail.com>
2025-04-11 06:55:39 -0400

    Increase group panel height
    Matches design and also because of the next commit which increases group
    label size to coexist visually with other panel types.

--- osu.Game/Screens/SelectV2/PanelGroup.cs
+++ osu.Game/Screens/SelectV2/PanelGroup.cs
@@ -18,7 +18,7 @@ namespace osu.Game.Screens.SelectV2
 {
     public partial class PanelGroup : PanelBase
     {
-        public const float HEIGHT = CarouselItem.DEFAULT_HEIGHT;
+        public const float HEIGHT = CarouselItem.DEFAULT_HEIGHT * 1.2f;
 
         private Drawable chevronIcon = null!;
         private OsuSpriteText titleText = null!;
