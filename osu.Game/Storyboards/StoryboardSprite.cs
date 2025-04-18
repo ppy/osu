ppy/osu

commit 102085668f84bd80f1717f101adc22fc7075e7fa

Fix video offset start time regression with `StartTime` calculation changes

osu.Game/Storyboards/StoryboardSprite.cs
@@ -24,7 +24,7 @@ namespace osu.Game.Storyboards
 
         public readonly StoryboardCommandGroup Commands = new StoryboardCommandGroup();
 
-        public double StartTime
+        public virtual double StartTime
         {
             get
             {

osu.Game/Storyboards/StoryboardVideo.cs
@@ -14,8 +14,10 @@ namespace osu.Game.Storyboards
         {
             // This is just required to get a valid StartTime based on the incoming offset.
             // Actual fades are handled inside DrawableStoryboardVideo for now.
-            Commands.AddAlpha(Easing.None, offset, offset, 0, 0);
+            StartTime = offset;
         }
+
+        public override double StartTime { get; }
 
         public override Drawable CreateDrawable() => new DrawableStoryboardVideo(this);
     }

commit 2761ee005dafb9f2f5eea1c5a958e2c1cdb64bb8

Fix storyboard videos not displaying
Regressed with 102085668f84bd80f1717f101adc22fc7075e7fa because the
stupid magic alpha transform addition was also implicitly changing the
value of `IsDrawable` from false to true because that property checks
for presence of any commands.

Apparently past me, in his infinite wisdom, did not decide it pertinent
to test that change against, you know, *a beatmap with a storyboard*.
Great job, past me, good show all around.

osu.Game/Storyboards/StoryboardSprite.cs
@@ -17,7 +17,7 @@ namespace osu.Game.Storyboards
         private readonly List<StoryboardTriggerGroup> triggerGroups = new List<StoryboardTriggerGroup>();
 
         public string Path { get; }
-        public bool IsDrawable => HasCommands;
+        public virtual bool IsDrawable => HasCommands;
 
         public Anchor Origin;
         public Vector2 InitialPosition;

osu.Game/Storyboards/StoryboardVideo.cs
@@ -19,6 +19,8 @@ namespace osu.Game.Storyboards
 
         public override double StartTime { get; }
 
+        public override bool IsDrawable => true;
+
         public override Drawable CreateDrawable() => new DrawableStoryboardVideo(this);
     }
 }