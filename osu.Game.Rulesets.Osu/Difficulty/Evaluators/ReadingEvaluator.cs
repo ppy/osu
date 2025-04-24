d7ec4a8af67306c7adaa78427b94c139db3f0171
kwotaq <PJXORISJ@gmail.com>
2025-04-24 21:51:48 +0300

    fix major issues with overlap bonus

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -36,7 +36,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             if (currObj.BaseObject is Slider currSlider)
                 // Longer sliders are inherently denser objects
-                pastObjectDifficultyInfluence += 3 * Math.Log10(Math.Max(1, currSlider.Velocity * currSlider.SpanDuration / currSlider.Radius));
+                pastObjectDifficultyInfluence += 2 * Math.Log10(Math.Max(1, currSlider.Velocity * currSlider.SpanDuration / currSlider.Radius));
 
             foreach (var loopObj in retrievePastVisibleObjects(currObj))
             {
@@ -57,12 +57,18 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 var loopObjPosition = loopObj.Position;
                 float visibleDistance = ((currPosition - loopObjPosition) / OsuDifficultyHitObject.NORMALISED_DIAMETER).Length;
 
-                overlapness += DifficultyCalculationUtils.Logistic((0.5 - visibleDistance) / 0.1) - 0.2;
+                overlapness += DifficultyCalculationUtils.Logistic(-(0.5 - visibleDistance) / 0.1) - 0.2;
 
                 if (loopObj.BaseObject is Slider loopSlider)
-                    overlapness *= 1.5 * Math.Max(1, Math.Log10(Math.Max(1, loopSlider.Velocity * loopSlider.SpanDuration / loopSlider.Radius)));
+                    overlapness *= Math.Max(1, Math.Log10(Math.Max(1, loopSlider.Velocity * loopSlider.SpanDuration / loopSlider.Radius)));
 
                 overlapness = Math.Max(0, overlapness);
+
+                // Nerf overlapness if loop object that overlaps current object also overlaps previous loop object
+                // This effectively awards overlap bonus only if there are spacing changes between current object and loop object
+                // Ignore if current object is a slider because sliders in bursts add difficulty
+                var prevLoopObj = (OsuDifficultyHitObject)loopObj.Next(0);
+                overlapness *= prevLoopObj.BaseObject.StartTime.Equals(currObj.BaseObject.StartTime) || loopObj.BaseObject is not Slider ? DifficultyCalculationUtils.Logistic(-(prevLoopObj.MinimumJumpDistance - 70) / 15) : 1;
 
                 double timeBetweenCurrAndLoopObj = (currObj.BaseObject.StartTime - loopObj.BaseObject.StartTime) / currObj.ClockRate;
                 double timeNerfFactor = getTimeNerfFactor(timeBetweenCurrAndLoopObj);
@@ -72,12 +78,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 pastObjectDifficultyInfluence += loopDifficulty;
             }
 
-            var prevObj = (OsuDifficultyHitObject)currObj.Previous(0);
-            double doubletapness = 1 - prevObj.GetDoubletapness(currObj);
-            overlapness *= Math.Pow(rhythmFactor * doubletapness, 3);
-            overlapness /= Math.Max(1, retrievePastVisibleObjects(currObj).Count());
-            overlapness *= angleNerfFactor;
-            overlapness *= 0.17;
+            overlapness *= rhythmFactor;
 
             double preemptDifficulty = 0.0;
 
@@ -92,6 +93,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 double densityDifficulty = 1 + DifficultyCalculationUtils.BellCurve(retrievePastVisibleObjects(currObj).Count(), 2, 1.5, 3.0);
                 preemptDifficulty *= currVelocity / densityDifficulty;
                 preemptDifficulty *= angleNerfFactor;
+                var prevObj = (OsuDifficultyHitObject)currObj.Previous(0);
+                double doubletapness = 1 - prevObj.GetDoubletapness(currObj);
                 preemptDifficulty *= doubletapness; // Doubletaps raise the density without adding significant reading difficulty
             }
 
@@ -102,7 +105,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 double timeSpentInvisible = getDurationSpentInvisible(currObj) / currObj.ClockRate;
 
                 // Nerf hidden difficulty less the more past object difficulty you have
-                double timeDifficultyFactor = 10000 / pastObjectDifficultyInfluence;
+                double timeDifficultyFactor = 9000 / pastObjectDifficultyInfluence;
 
                 // Cap objects because after a certain point hidden density is mainly memory
                 double visibleObjectFactor = Math.Min(retrieveCurrentVisibleObjects(currObj).Count, 8);
@@ -117,7 +120,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             }
 
             // Award only denser than average maps
-            double noteDensityDifficulty = Math.Max(0, pastObjectDifficultyInfluence - 2.5);
+            double noteDensityDifficulty = Math.Max(0, pastObjectDifficultyInfluence - 2.7);
             noteDensityDifficulty *= angleNerfFactor;
 
             double difficulty = preemptDifficulty + hiddenDifficulty + noteDensityDifficulty + overlapness;
