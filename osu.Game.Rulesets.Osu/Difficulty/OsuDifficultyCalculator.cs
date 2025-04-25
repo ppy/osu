2d210f9a086e61a84d9c55cbe10d3a8e2ab27759
tsunyoku <tsunyoku@gmail.com>
2025-04-25 13:35:07 +0100

    Revert star rating multiplier decrease

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -22,7 +22,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
     {
         private const double performance_base_multiplier = 1.15; // This is being adjusted to keep the final pp value scaled around what it used to be when changing things.
         private const double difficulty_multiplier = 0.0675;
-        private const double star_rating_multiplier = 0.026;
+        private const double star_rating_multiplier = 0.027;
 
         public override int Version => 20241007;
 
