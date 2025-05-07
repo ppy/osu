ppy/osu

commit c00604678695d1c3be082c26346b33702f70feb7

Separate gameplay leaderboard data management from display
This is a prerequisite for supporting skinning of leaderboards.

- New `IGameplayLeaderboardProvider` and `IGameplayLeaderboardScore`
  interfaces are introduced. They are strictly concerned with supplying
  leaderboard data.

- Logic of managing display, which was previously jammed into the
  inheritance hierarchy of `GameplayLeaderboard`, is now moved into
  `IGameplayLeaderboardProvider` implementations. Solo play,
  multiplayer, and multiplayer spectator get their own implementation of
  the interface.

- The inheritance hierarchy of `GameplayLeaderboard` and per-player
  overriding of the implementation of the gameplay leaderboard is gone.
  Only one drawable class (renamed to `DrawableGameplayLeaderboard`) is
  allowed to display the leaderboards, across all modes of play.

osu.Game/Screens/Select/Leaderboards/GameplayLeaderboardScore.cs
@@ -1,0 +1,59 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Game.Online.Spectator;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Scoring;
+using osu.Game.Scoring.Legacy;
+using osu.Game.Screens.Play.HUD;
+using osu.Game.Users;
+
+namespace osu.Game.Screens.Select.Leaderboards
+{
+    public class GameplayLeaderboardScore : IGameplayLeaderboardScore
+    {
+        public IUser User { get; }
+        public bool Tracked { get; }
+        public BindableLong TotalScore { get; } = new BindableLong();
+        public BindableDouble Accuracy { get; } = new BindableDouble();
+        public BindableInt Combo { get; } = new BindableInt();
+        public BindableBool HasQuit { get; } = new BindableBool();
+        public Bindable<long> DisplayOrder { get; } = new BindableLong();
+        public Func<ScoringMode, long> GetDisplayScore { get; set; }
+        public Colour4? TeamColour { get; init; }
+
+        public GameplayLeaderboardScore(IUser user, ScoreProcessor scoreProcessor, bool tracked)
+        {
+            User = user;
+            Tracked = tracked;
+            TotalScore.BindTarget = scoreProcessor.TotalScore;
+            Accuracy.BindTarget = scoreProcessor.Accuracy;
+            Combo.BindTarget = scoreProcessor.Combo;
+            GetDisplayScore = scoreProcessor.GetDisplayScore;
+        }
+
+        public GameplayLeaderboardScore(IUser user, SpectatorScoreProcessor scoreProcessor, bool tracked)
+        {
+            User = user;
+            Tracked = tracked;
+            TotalScore.BindTarget = scoreProcessor.TotalScore;
+            Accuracy.BindTarget = scoreProcessor.Accuracy;
+            Combo.BindTarget = scoreProcessor.Combo;
+            GetDisplayScore = scoreProcessor.GetDisplayScore;
+        }
+
+        public GameplayLeaderboardScore(ScoreInfo scoreInfo, bool tracked)
+        {
+            User = scoreInfo.User;
+            Tracked = tracked;
+            TotalScore.Value = scoreInfo.TotalScore;
+            Accuracy.Value = scoreInfo.Accuracy;
+            Combo.Value = scoreInfo.Combo;
+            DisplayOrder.Value = scoreInfo.OnlineID > 0 ? scoreInfo.OnlineID : scoreInfo.Date.ToUnixTimeSeconds();
+            GetDisplayScore = scoreInfo.GetDisplayScore;
+        }
+    }
+}

osu.Game.Tests/Visual/Gameplay/TestSceneGameplayLeaderboard.cs
@@ -1,11 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.PolygonExtensions;
 using osu.Framework.Graphics;
@@ -15,7 +15,10 @@ using osu.Framework.Testing;
 using osu.Framework.Utils;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens.Play.HUD;
+using osu.Game.Screens.Select.Leaderboards;
+using osu.Game.Users;
 using osuTK;
 
 namespace osu.Game.Tests.Visual.Gameplay
@@ -23,7 +26,10 @@ namespace osu.Game.Tests.Visual.Gameplay
     [TestFixture]
     public partial class TestSceneGameplayLeaderboard : OsuTestScene
     {
-        private TestGameplayLeaderboard leaderboard;
+        private TestDrawableGameplayLeaderboard leaderboard = null!;
+
+        [Cached(typeof(IGameplayLeaderboardProvider))]
+        private TestGameplayLeaderboardProvider leaderboardProvider = new TestGameplayLeaderboardProvider();
 
         private readonly BindableLong playerScore = new BindableLong();
 
@@ -57,10 +63,10 @@ namespace osu.Game.Tests.Visual.Gameplay
             // has caused layout to not work in the past.
 
             AddUntilStep("wait for fill flow layout",
-                () => leaderboard.ChildrenOfType<FillFlowContainer<GameplayLeaderboardScore>>().First().ScreenSpaceDrawQuad.Intersects(leaderboard.ScreenSpaceDrawQuad));
+                () => leaderboard.ChildrenOfType<FillFlowContainer<DrawableGameplayLeaderboardScore>>().First().ScreenSpaceDrawQuad.Intersects(leaderboard.ScreenSpaceDrawQuad));
 
             AddUntilStep("wait for some scores not masked away",
-                () => leaderboard.ChildrenOfType<GameplayLeaderboardScore>().Any(s => leaderboard.ScreenSpaceDrawQuad.Contains(s.ScreenSpaceDrawQuad.Centre)));
+                () => leaderboard.ChildrenOfType<DrawableGameplayLeaderboardScore>().Any(s => leaderboard.ScreenSpaceDrawQuad.Contains(s.ScreenSpaceDrawQuad.Centre)));
 
             AddUntilStep("wait for tracked score fully visible", () => leaderboard.ScreenSpaceDrawQuad.Intersects(leaderboard.TrackedScore!.ScreenSpaceDrawQuad));
 
@@ -139,7 +145,7 @@ namespace osu.Game.Tests.Visual.Gameplay
             checkHeight(8);
 
             void checkHeight(int panelCount)
-                => AddAssert($"leaderboard height is {panelCount} panels high", () => leaderboard.DrawHeight == (GameplayLeaderboardScore.PANEL_HEIGHT + leaderboard.Spacing) * panelCount);
+                => AddAssert($"leaderboard height is {panelCount} panels high", () => leaderboard.DrawHeight == (DrawableGameplayLeaderboardScore.PANEL_HEIGHT + leaderboard.Spacing) * panelCount);
         }
 
         [Test]
@@ -179,6 +185,27 @@ namespace osu.Game.Tests.Visual.Gameplay
                 () => Does.Contain("#FF549A"));
         }
 
+        [Test]
+        public void TestTrackedScorePosition([Values] bool partial)
+        {
+            createLeaderboard(partial);
+
+            AddStep("add many scores in one go", () =>
+            {
+                for (int i = 0; i < 49; i++)
+                    createRandomScore(new APIUser { Username = $"Player {i + 1}" });
+
+                // Add player at end to force an animation down the whole list.
+                playerScore.Value = 0;
+                createLeaderboardScore(playerScore, new APIUser { Username = "You", Id = 3 }, true);
+            });
+
+            if (partial)
+                AddUntilStep("tracked player has null position", () => leaderboard.TrackedScore?.ScorePosition, () => Is.Null);
+            else
+                AddUntilStep("tracked player is #50", () => leaderboard.TrackedScore?.ScorePosition, () => Is.EqualTo(50));
+        }
+
         private void addLocalPlayer()
         {
             AddStep("add local player", () =>
@@ -188,11 +215,13 @@ namespace osu.Game.Tests.Visual.Gameplay
             });
         }
 
-        private void createLeaderboard()
+        private void createLeaderboard(bool partial = false)
         {
             AddStep("create leaderboard", () =>
             {
-                Child = leaderboard = new TestGameplayLeaderboard
+                leaderboardProvider.Scores.Clear();
+                leaderboardProvider.IsPartial = partial;
+                Child = leaderboard = new TestDrawableGameplayLeaderboard
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
@@ -205,11 +234,11 @@ namespace osu.Game.Tests.Visual.Gameplay
 
         private void createLeaderboardScore(BindableLong score, APIUser user, bool isTracked = false)
         {
-            var leaderboardScore = leaderboard.Add(user, isTracked);
-            leaderboardScore.TotalScore.BindTo(score);
+            var leaderboardScore = new TestDrawableGameplayLeaderboardScore(user, isTracked, score);
+            leaderboardProvider.Scores.Add(leaderboardScore);
         }
 
-        private partial class TestGameplayLeaderboard : GameplayLeaderboard
+        private partial class TestDrawableGameplayLeaderboard : DrawableGameplayLeaderboard
         {
             public float Spacing => Flow.Spacing.Y;
 
@@ -220,8 +249,36 @@ namespace osu.Game.Tests.Visual.Gameplay
                 return scoreItem != null && scoreItem.ScorePosition == expectedPosition;
             }
 
-            public IEnumerable<GameplayLeaderboardScore> GetAllScoresForUsername(string username)
+            public IEnumerable<DrawableGameplayLeaderboardScore> GetAllScoresForUsername(string username)
                 => Flow.Where(i => i.User?.Username == username);
+        }
+
+        private class TestGameplayLeaderboardProvider : IGameplayLeaderboardProvider
+        {
+            IBindableList<IGameplayLeaderboardScore> IGameplayLeaderboardProvider.Scores => Scores;
+            public BindableList<IGameplayLeaderboardScore> Scores { get; } = new BindableList<IGameplayLeaderboardScore>();
+            public bool IsPartial { get; set; }
+        }
+
+        private class TestDrawableGameplayLeaderboardScore : IGameplayLeaderboardScore
+        {
+            public IUser User { get; }
+            public bool Tracked { get; }
+            public BindableLong TotalScore { get; } = new BindableLong();
+            public BindableDouble Accuracy { get; } = new BindableDouble();
+            public BindableInt Combo { get; } = new BindableInt();
+            public BindableBool HasQuit { get; } = new BindableBool();
+            public Bindable<long> DisplayOrder { get; } = new BindableLong();
+            public Func<ScoringMode, long> GetDisplayScore { get; set; }
+            public Colour4? TeamColour => null;
+
+            public TestDrawableGameplayLeaderboardScore(IUser user, bool isTracked, Bindable<long> totalScore)
+            {
+                User = user;
+                Tracked = isTracked;
+                TotalScore.BindTo(totalScore);
+                GetDisplayScore = _ => TotalScore.Value;
+            }
         }
     }
 }

osu.Game.Tests/Visual/Gameplay/TestSceneSoloGameplayLeaderboard.cs
@@ -1,124 +1,0 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
-using osu.Framework.Allocation;
-using osu.Framework.Bindables;
-using osu.Framework.Graphics;
-using osu.Framework.Testing;
-using osu.Framework.Utils;
-using osu.Game.Configuration;
-using osu.Game.Online.API.Requests.Responses;
-using osu.Game.Rulesets.Osu;
-using osu.Game.Rulesets.Scoring;
-using osu.Game.Scoring;
-using osu.Game.Screens.Play.HUD;
-using osu.Game.Screens.Select;
-using osu.Game.Tests.Gameplay;
-
-namespace osu.Game.Tests.Visual.Gameplay
-{
-    public partial class TestSceneSoloGameplayLeaderboard : OsuTestScene
-    {
-        [Cached(typeof(ScoreProcessor))]
-        private readonly ScoreProcessor scoreProcessor = TestGameplayState.Create(new OsuRuleset()).ScoreProcessor;
-
-        private readonly BindableList<ScoreInfo> scores = new BindableList<ScoreInfo>();
-
-        private readonly Bindable<bool> configVisibility = new Bindable<bool>();
-        private readonly Bindable<PlayBeatmapDetailArea.TabType> beatmapTabType = new Bindable<PlayBeatmapDetailArea.TabType>();
-
-        private SoloGameplayLeaderboard leaderboard = null!;
-
-        [BackgroundDependencyLoader]
-        private void load(OsuConfigManager config)
-        {
-            config.BindWith(OsuSetting.GameplayLeaderboard, configVisibility);
-            config.BindWith(OsuSetting.BeatmapDetailTab, beatmapTabType);
-        }
-
-        [SetUpSteps]
-        public void SetUpSteps()
-        {
-            AddStep("clear scores", () => scores.Clear());
-
-            AddStep("create component", () =>
-            {
-                var trackingUser = new APIUser
-                {
-                    Username = "local user",
-                    Id = 2,
-                };
-
-                Child = leaderboard = new SoloGameplayLeaderboard(trackingUser)
-                {
-                    Scores = { BindTarget = scores },
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    AlwaysVisible = { Value = false },
-                    Expanded = { Value = true },
-                };
-            });
-
-            AddStep("add scores", () => scores.AddRange(createSampleScores()));
-        }
-
-        [Test]
-        public void TestLocalUser()
-        {
-            AddSliderStep("score", 0, 1000000, 500000, v => scoreProcessor.TotalScore.Value = v);
-            AddSliderStep("accuracy", 0f, 1f, 0.5f, v => scoreProcessor.Accuracy.Value = v);
-            AddSliderStep("combo", 0, 10000, 0, v => scoreProcessor.HighestCombo.Value = v);
-            AddStep("toggle expanded", () => leaderboard.Expanded.Value = !leaderboard.Expanded.Value);
-        }
-
-        [TestCase(PlayBeatmapDetailArea.TabType.Local, 51)]
-        [TestCase(PlayBeatmapDetailArea.TabType.Global, null)]
-        [TestCase(PlayBeatmapDetailArea.TabType.Country, null)]
-        [TestCase(PlayBeatmapDetailArea.TabType.Friends, null)]
-        public void TestTrackedScorePosition(PlayBeatmapDetailArea.TabType tabType, int? expectedOverflowIndex)
-        {
-            AddStep($"change TabType to {tabType}", () => beatmapTabType.Value = tabType);
-            AddUntilStep("tracked player is #50", () => leaderboard.TrackedScore?.ScorePosition, () => Is.EqualTo(50));
-
-            AddStep("add one more score", () => scores.Add(new ScoreInfo { User = new APIUser { Username = "New player 1" }, TotalScore = RNG.Next(600000, 1000000) }));
-
-            AddUntilStep("wait for sort", () => leaderboard.ChildrenOfType<GameplayLeaderboardScore>().First().ScorePosition != null);
-
-            if (expectedOverflowIndex == null)
-                AddUntilStep("tracked player has null position", () => leaderboard.TrackedScore?.ScorePosition, () => Is.Null);
-            else
-                AddUntilStep($"tracked player is #{expectedOverflowIndex}", () => leaderboard.TrackedScore?.ScorePosition, () => Is.EqualTo(expectedOverflowIndex));
-        }
-
-        [Test]
-        public void TestVisibility()
-        {
-            AddStep("set config visible true", () => configVisibility.Value = true);
-            AddUntilStep("leaderboard visible", () => leaderboard.Alpha == 1);
-
-            AddStep("set config visible false", () => configVisibility.Value = false);
-            AddUntilStep("leaderboard not visible", () => leaderboard.Alpha == 0);
-
-            AddStep("set always visible", () => leaderboard.AlwaysVisible.Value = true);
-            AddUntilStep("leaderboard visible", () => leaderboard.Alpha == 1);
-
-            AddStep("set config visible true", () => configVisibility.Value = true);
-            AddAssert("leaderboard still visible", () => leaderboard.Alpha == 1);
-        }
-
-        private static List<ScoreInfo> createSampleScores()
-        {
-            return new[]
-            {
-                new ScoreInfo { User = new APIUser { Username = @"peppy" }, TotalScore = RNG.Next(500000, 1000000) },
-                new ScoreInfo { User = new APIUser { Username = @"smoogipoo" }, TotalScore = RNG.Next(500000, 1000000) },
-                new ScoreInfo { User = new APIUser { Username = @"spaceman_atlas" }, TotalScore = RNG.Next(500000, 1000000) },
-                new ScoreInfo { User = new APIUser { Username = @"frenzibyte" }, TotalScore = RNG.Next(500000, 1000000) },
-                new ScoreInfo { User = new APIUser { Username = @"Susko3" }, TotalScore = RNG.Next(500000, 1000000) },
-            }.Concat(Enumerable.Range(0, 44).Select(i => new ScoreInfo { User = new APIUser { Username = $"User {i + 1}" }, TotalScore = 1000000 - i * 10000 })).ToList();
-        }
-    }
-}

osu.Game.Tests/Visual/Multiplayer/MultiplayerGameplayLeaderboardTestScene.cs
@@ -10,6 +10,7 @@ using Moq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Testing;
 using osu.Framework.Utils;
 using osu.Game.Configuration;
@@ -20,6 +21,7 @@ using osu.Game.Online.Spectator;
 using osu.Game.Replays.Legacy;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens.Play.HUD;
+using osu.Game.Screens.Select.Leaderboards;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
@@ -29,11 +31,13 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
         protected readonly BindableList<MultiplayerRoomUser> MultiplayerUsers = new BindableList<MultiplayerRoomUser>();
 
-        protected MultiplayerGameplayLeaderboard? Leaderboard { get; private set; }
+        protected MultiplayerLeaderboardProvider? LeaderboardProvider { get; private set; }
+
+        protected DrawableGameplayLeaderboard? Leaderboard { get; private set; }
 
         protected virtual MultiplayerRoomUser CreateUser(int userId) => new MultiplayerRoomUser(userId);
 
-        protected abstract MultiplayerGameplayLeaderboard CreateLeaderboard();
+        protected abstract MultiplayerLeaderboardProvider CreateLeaderboardProvider();
 
         private readonly BindableList<int> multiplayerUserIds = new BindableList<int>();
         private readonly BindableDictionary<int, SpectatorState> watchedUserStates = new BindableDictionary<int, SpectatorState>();
@@ -124,11 +128,21 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("create leaderboard", () =>
             {
-                Leaderboard?.Expire();
+                Clear(true);
 
                 Beatmap.Value = CreateWorkingBeatmap(Ruleset.Value);
 
-                LoadComponentAsync(Leaderboard = CreateLeaderboard(), Add);
+                LoadComponentAsync(LeaderboardProvider = CreateLeaderboardProvider(), Add);
+                Add(new DependencyProvidingContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    CachedDependencies = [(typeof(IGameplayLeaderboardProvider), LeaderboardProvider)],
+                    Child = Leaderboard = new DrawableGameplayLeaderboard
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    }
+                });
             });
 
             AddUntilStep("wait for load", () => Leaderboard!.IsLoaded);
@@ -159,10 +173,18 @@ namespace osu.Game.Tests.Visual.Multiplayer
                 return false;
             });
 
-            AddStep("check stop watching requests were sent", () =>
+            AddUntilStep("check stop watching requests were sent", () =>
             {
-                foreach (var user in MultiplayerUsers)
-                    spectatorClient.Verify(s => s.StopWatchingUser(user.UserID), Times.Once);
+                try
+                {
+                    foreach (var user in MultiplayerUsers)
+                        spectatorClient.Verify(s => s.StopWatchingUser(user.UserID), Times.Once);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
             });
         }
 
@@ -204,12 +226,14 @@ namespace osu.Game.Tests.Visual.Multiplayer
                         header.Combo++;
                         header.MaxCombo = Math.Max(header.MaxCombo, header.Combo);
                         header.Statistics[HitResult.Meh]++;
+                        header.TotalScore += 50;
                         break;
 
                     default:
                         header.Combo++;
                         header.MaxCombo = Math.Max(header.MaxCombo, header.Combo);
                         header.Statistics[HitResult.Great]++;
+                        header.TotalScore += 300;
                         break;
                 }
 
@@ -218,3 +242,4 @@ namespace osu.Game.Tests.Visual.Multiplayer
         }
     }
 }
+

osu.Game.Tests/Visual/Multiplayer/TestSceneMultiSpectatorLeaderboard.cs
@@ -9,15 +9,16 @@ using osu.Framework.Testing;
 using osu.Framework.Timing;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Multiplayer;
-using osu.Game.Screens.OnlinePlay.Multiplayer.Spectate;
 using osu.Game.Screens.Play.HUD;
+using osu.Game.Screens.Select.Leaderboards;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
     public partial class TestSceneMultiSpectatorLeaderboard : MultiplayerTestScene
     {
         private Dictionary<int, ManualClock> clocks = null!;
-        private MultiSpectatorLeaderboard? leaderboard;
+        private MultiSpectatorLeaderboardProvider? leaderboardProvider;
+        private DrawableGameplayLeaderboard leaderboard = null!;
 
         [SetUpSteps]
         public override void SetUpSteps()
@@ -29,7 +30,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("reset", () =>
             {
-                leaderboard?.RemoveAndDisposeImmediately();
+                Clear(true);
 
                 clocks = new Dictionary<int, ManualClock>
                 {
@@ -48,21 +49,27 @@ namespace osu.Game.Tests.Visual.Multiplayer
             {
                 Beatmap.Value = CreateWorkingBeatmap(Ruleset.Value);
 
-                LoadComponentAsync(leaderboard = new MultiSpectatorLeaderboard(clocks.Keys.Select(id => new MultiplayerRoomUser(id)).ToArray())
+                LoadComponentAsync(leaderboardProvider = new MultiSpectatorLeaderboardProvider(clocks.Keys.Select(id => new MultiplayerRoomUser(id)).ToArray()), Add);
+                Add(new DependencyProvidingContainer
                 {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Expanded = { Value = true }
-                }, Add);
+                    RelativeSizeAxes = Axes.Both,
+                    CachedDependencies = [(typeof(IGameplayLeaderboardProvider), leaderboardProvider)],
+                    Child = leaderboard = new DrawableGameplayLeaderboard
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Expanded = { Value = true }
+                    }
+                });
             });
 
-            AddUntilStep("wait for load", () => leaderboard!.IsLoaded);
-            AddUntilStep("wait for user population", () => leaderboard.ChildrenOfType<GameplayLeaderboardScore>().Count() == 2);
+            AddUntilStep("wait for load", () => leaderboard.IsLoaded);
+            AddUntilStep("wait for user population", () => leaderboard.ChildrenOfType<DrawableGameplayLeaderboardScore>().Count() == 2);
 
             AddStep("add clock sources", () =>
             {
                 foreach ((int userId, var clock) in clocks)
-                    leaderboard!.AddClock(userId, clock);
+                    leaderboardProvider!.AddClock(userId, clock);
             });
         }
 
@@ -123,6 +130,6 @@ namespace osu.Game.Tests.Visual.Multiplayer
             => AddStep($"set user {userId} time {time}", () => clocks[userId].CurrentTime = time);
 
         private void assertCombo(int userId, int expectedCombo)
-            => AddUntilStep($"player {userId} has {expectedCombo} combo", () => this.ChildrenOfType<GameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId).Combo.Value == expectedCombo);
+            => AddUntilStep($"player {userId} has {expectedCombo} combo", () => this.ChildrenOfType<DrawableGameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId).Combo.Value == expectedCombo);
     }
 }

osu.Game.Tests/Visual/Multiplayer/TestSceneMultiSpectatorScreen.cs
@@ -560,7 +560,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
         private PlayerArea getInstance(int userId) => spectatorScreen.ChildrenOfType<PlayerArea>().Single(p => p.UserId == userId);
 
-        private GameplayLeaderboardScore getLeaderboardScore(int userId) => spectatorScreen.ChildrenOfType<GameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId);
+        private DrawableGameplayLeaderboardScore getLeaderboardScore(int userId) => spectatorScreen.ChildrenOfType<DrawableGameplayLeaderboardScore>().Single(s => s.User?.OnlineID == userId);
 
         private int[] getPlayerIds(int count) => Enumerable.Range(PLAYER_1_ID, count).ToArray();
     }

osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerGameplayLeaderboard.cs
@@ -9,7 +9,7 @@ using osu.Game.Online.API;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Mods;
-using osu.Game.Screens.Play.HUD;
+using osu.Game.Screens.Select.Leaderboards;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
@@ -25,27 +25,25 @@ namespace osu.Game.Tests.Visual.Multiplayer
             return user;
         }
 
-        protected override MultiplayerGameplayLeaderboard CreateLeaderboard()
-        {
-            return new TestLeaderboard(MultiplayerUsers.ToArray())
+        protected override MultiplayerLeaderboardProvider CreateLeaderboardProvider() =>
+            new TestLeaderboard(MultiplayerUsers.ToArray())
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
             };
-        }
 
         [Test]
         public void TestPerUserMods()
         {
-            AddStep("first user has no mods", () => Assert.That(((TestLeaderboard)Leaderboard!).UserMods[0], Is.Empty));
+            AddStep("first user has no mods", () => Assert.That(((TestLeaderboard)LeaderboardProvider!).UserMods[0], Is.Empty));
             AddStep("last user has NF mod", () =>
             {
-                Assert.That(((TestLeaderboard)Leaderboard!).UserMods[TOTAL_USERS - 1], Has.One.Items);
-                Assert.That(((TestLeaderboard)Leaderboard).UserMods[TOTAL_USERS - 1].Single(), Is.TypeOf<OsuModNoFail>());
+                Assert.That(((TestLeaderboard)LeaderboardProvider!).UserMods[TOTAL_USERS - 1], Has.One.Items);
+                Assert.That(((TestLeaderboard)LeaderboardProvider).UserMods[TOTAL_USERS - 1].Single(), Is.TypeOf<OsuModNoFail>());
             });
         }
 
-        private partial class TestLeaderboard : MultiplayerGameplayLeaderboard
+        private partial class TestLeaderboard : MultiplayerLeaderboardProvider
         {
             public Dictionary<int, IReadOnlyList<Mod>> UserMods => UserScores.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ScoreProcessor.Mods);
 

commit b768038c0256ae248187f6a7074b7ab10a04ce6f

Fix gameplay leaderboard score reading off wrong combo property
Closes https://github.com/ppy/osu/issues/33006.

Broke in c00604678695d1c3be082c26346b33702f70feb7.

The fact that this mistake can be made at all is... something, but it
was made.

osu.Game/Screens/Select/Leaderboards/GameplayLeaderboardScore.cs
@@ -113,7 +113,7 @@ namespace osu.Game.Screens.Select.Leaderboards
             Tracked = tracked;
             TotalScore.Value = scoreInfo.TotalScore;
             Accuracy.Value = scoreInfo.Accuracy;
-            Combo.Value = scoreInfo.Combo;
+            Combo.Value = scoreInfo.MaxCombo;
             TotalScoreTiebreaker = scoreInfo.OnlineID > 0 ? scoreInfo.OnlineID : scoreInfo.Date.ToUnixTimeSeconds();
             GetDisplayScore = scoreInfo.GetDisplayScore;
             InitialPosition = scoreInfo.Position;