2422e86fbc4aad241cf9f4a2bc13173545d138f9
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-05-13 15:29:33 +0900

    Fix test not showing immediately useful state
    Also standardises the testing leaderboard provider component for
    (immediate) future use.

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerPositionDisplay.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMultiplayerPositionDisplay.cs
@@ -4,7 +4,6 @@
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
 using osu.Game.Configuration;
 using osu.Game.Online.API;
@@ -13,6 +12,7 @@ using osu.Game.Screens.OnlinePlay.Multiplayer;
 using osu.Game.Screens.Play;
 using osu.Game.Screens.Select.Leaderboards;
 using osu.Game.Tests.Gameplay;
+using osu.Game.Tests.Visual.Gameplay;
 
 namespace osu.Game.Tests.Visual.Multiplayer
 {
@@ -21,25 +21,27 @@ namespace osu.Game.Tests.Visual.Multiplayer
         [Resolved]
         private OsuConfigManager config { get; set; } = null!;
 
+        private GameplayLeaderboardScore score = null!;
+
+        private readonly Bindable<int?> position = new Bindable<int?>(50);
+
+        private TestSceneGameplayLeaderboard.TestGameplayLeaderboardProvider leaderboardProvider = null!;
+        private MultiplayerPositionDisplay display = null!;
+        private GameplayState gameplayState = null!;
+
         [Test]
         public void TestAppearance()
         {
-            TestGameplayLeaderboardProvider leaderboard = null!;
-            MultiplayerPositionDisplay display = null!;
-            GameplayState gameplayState = null!;
-
             AddStep("create content", () =>
             {
-                leaderboard = new TestGameplayLeaderboardProvider();
                 Children = new Drawable[]
                 {
-                    leaderboard,
                     new DependencyProvidingContainer
                     {
                         RelativeSizeAxes = Axes.Both,
                         CachedDependencies =
                         [
-                            (typeof(IGameplayLeaderboardProvider), leaderboard),
+                            (typeof(IGameplayLeaderboardProvider), leaderboardProvider = new TestSceneGameplayLeaderboard.TestGameplayLeaderboardProvider()),
                             (typeof(GameplayState), gameplayState = TestGameplayState.Create(new OsuRuleset()))
                         ],
                         Child = display = new MultiplayerPositionDisplay
@@ -49,18 +51,17 @@ namespace osu.Game.Tests.Visual.Multiplayer
                         }
                     }
                 };
-            });
-            AddSliderStep("set score position", 1, 100, 50, r =>
-            {
-                if (leaderboard.IsNotNull() && leaderboard.Score.IsNotNull())
-                    leaderboard.Score.Position.Value = r;
-            });
-            AddStep("unset position", () => leaderboard.Score.Position.Value = null);
 
-            AddStep("toggle leaderboard on", () => config.SetValue(OsuSetting.GameplayLeaderboard, true));
+                score = leaderboardProvider.CreateLeaderboardScore(new BindableLong(), API.LocalUser.Value, true);
+                score.Position.BindTo(position);
+            });
+            AddSliderStep("set score position", 1, 100, 50, r => position.Value = r);
+            AddStep("unset position", () => position.Value = null);
+
+            AddStep("toggle leaderboardProvider on", () => config.SetValue(OsuSetting.GameplayLeaderboard, true));
             AddUntilStep("display visible", () => display.Alpha, () => Is.EqualTo(1));
 
-            AddStep("toggle leaderboard off", () => config.SetValue(OsuSetting.GameplayLeaderboard, false));
+            AddStep("toggle leaderboardProvider off", () => config.SetValue(OsuSetting.GameplayLeaderboard, false));
             AddUntilStep("display hidden", () => display.Alpha, () => Is.EqualTo(0));
 
             AddStep("enter break", () => ((Bindable<LocalUserPlayingState>)gameplayState.PlayingState).Value = LocalUserPlayingState.Break);
@@ -69,25 +70,11 @@ namespace osu.Game.Tests.Visual.Multiplayer
             AddStep("exit break", () => ((Bindable<LocalUserPlayingState>)gameplayState.PlayingState).Value = LocalUserPlayingState.Playing);
             AddUntilStep("display hidden", () => display.Alpha, () => Is.EqualTo(0));
 
-            AddStep("toggle leaderboard on", () => config.SetValue(OsuSetting.GameplayLeaderboard, true));
+            AddStep("toggle leaderboardProvider on", () => config.SetValue(OsuSetting.GameplayLeaderboard, true));
             AddUntilStep("display visible", () => display.Alpha, () => Is.EqualTo(1));
 
             AddStep("change local user", () => ((DummyAPIAccess)API).LocalUser.Value = new GuestUser());
             AddUntilStep("display hidden", () => display.Alpha, () => Is.EqualTo(0));
-        }
-
-        private partial class TestGameplayLeaderboardProvider : Component, IGameplayLeaderboardProvider
-        {
-            public GameplayLeaderboardScore Score { get; private set; } = null!;
-
-            IBindableList<GameplayLeaderboardScore> IGameplayLeaderboardProvider.Scores => scores;
-            private readonly BindableList<GameplayLeaderboardScore> scores = new BindableList<GameplayLeaderboardScore>();
-
-            [BackgroundDependencyLoader]
-            private void load(IAPIProvider api)
-            {
-                scores.Add(Score = new GameplayLeaderboardScore(api.LocalUser.Value, true, new BindableLong()));
-            }
         }
     }
 }
