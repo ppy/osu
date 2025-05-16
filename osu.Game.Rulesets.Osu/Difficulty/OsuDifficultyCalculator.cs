4784408794d4e99e8df5778aa2fde292f83d0b92
kwotaq <PJXORISJ@gmail.com>
2025-05-16 17:10:52 +0300

    speed only looks decent

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -82,7 +82,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             double speedNotes = speed.RelevantNoteCount();
 
             double aimDifficultStrainCount = aim.CountTopWeightedStrains();
-            double speedDifficultStrainCount = speed.CountTopWeightedStrains();
+            double speedDifficultStrainCount = speed.CountTopWeightedNotes();
 
             double aimNoSlidersTopWeightedSliderCount = aimWithoutSliders.CountTopWeightedSliders();
             double aimNoSlidersDifficultStrainCount = aimWithoutSliders.CountTopWeightedStrains();
--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -6,9 +6,10 @@ using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
-using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Objects;
 using System.Linq;
+using osu.Game.Rulesets.Difficulty.Skills;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Difficulty.Utils;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
@@ -16,17 +17,18 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// <summary>
     /// Represents the skill required to press keys with regards to keeping up with the speed at which objects need to be hit.
     /// </summary>
-    public class Speed : OsuStrainSkill
+    public class Speed : Skill
     {
-        private double skillMultiplier => 1.47;
-        private double strainDecayBase => 0.3;
+        private double skillMultiplier => 1.3;
 
-        private double currentStrain;
-        private double currentRhythm;
+        private readonly List<double> noteDifficulties = new List<double>();
+
+        private readonly List<double> noteWeights = new List<double>();
 
         private readonly List<double> sliderStrains = new List<double>();
 
-        protected override int ReducedSectionCount => 5;
+        private double currentDifficulty;
+        private double strainDecayBase => 0.3;
 
         public Speed(Mod[] mods)
             : base(mods)
@@ -35,33 +37,79 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
 
-        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => (currentStrain * currentRhythm) * strainDecay(time - current.Previous(0).StartTime);
-
-        protected override double StrainValueAt(DifficultyHitObject current)
+        public override void Process(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(((OsuDifficultyHitObject)current).StrainTime);
-            currentStrain += SpeedEvaluator.EvaluateDifficultyOf(current, Mods) * skillMultiplier;
+            currentDifficulty *= strainDecay(((OsuDifficultyHitObject)current).StrainTime);
 
-            currentRhythm = RhythmEvaluator.EvaluateDifficultyOf(current);
-
-            double totalStrain = currentStrain * currentRhythm;
+            currentDifficulty += SpeedEvaluator.EvaluateDifficultyOf(current, Mods) * RhythmEvaluator.EvaluateDifficultyOf(current) * skillMultiplier;
 
             if (current.BaseObject is Slider)
-                sliderStrains.Add(totalStrain);
+                sliderStrains.Add(currentDifficulty);
 
-            return totalStrain;
+            noteDifficulties.Add(currentDifficulty);
+        }
+
+        public override double DifficultyValue()
+        {
+            double difficulty = 0;
+
+            // Notes with 0 difficulty are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
+            // These notes will not contribute to the difficulty.
+            var peaks = noteDifficulties.Where(p => p > 0);
+
+            List<double> notes = peaks.ToList();
+
+            int index = 0;
+
+            // Difficulty is the weighted sum of the highest notes.
+            // We're sorting from highest to lowest note.
+            foreach (double note in notes.OrderDescending())
+            {
+                // Use a harmonic sum for note which effectively buffs maps with more notes, especially if note difficulties are consistent.
+                // Constants are arbitrary and give good values.
+                // https://www.desmos.com/calculator/gquji01mlg
+                double weight = (1.0 + (10.0 / (1 + index))) / (Math.Pow(index, 0.9) + 1.0 + (10.0 / (1.0 + index)));
+
+                noteWeights.Add(weight);
+
+                difficulty += note * weight;
+                index += 1;
+            }
+
+            return difficulty;
+        }
+
+        /// <summary>
+        /// Returns the number of relevant objects weighted against the top note.
+        /// </summary>
+        public double CountTopWeightedNotes()
+        {
+            if (noteDifficulties.Count == 0)
+                return 0.0;
+
+            double consistentTopNote = DifficultyValue() / noteWeights.Sum(); // What would the top note be if all note values were identical
+
+            if (consistentTopNote == 0)
+                return noteDifficulties.Count;
+
+            // Use a weighted sum of all notes. Constants are arbitrary and give nice values
+            return noteDifficulties.Sum(s => 1.1 / (1 + Math.Exp(-10 * (s / consistentTopNote - 0.88))));
         }
 
         public double RelevantNoteCount()
         {
-            if (ObjectStrains.Count == 0)
+            if (noteDifficulties.Count == 0)
                 return 0;
 
-            double maxStrain = ObjectStrains.Max();
-            if (maxStrain == 0)
-                return 0;
+            double consistentTopNote = DifficultyValue() / noteWeights.Sum(); // What would the top note be if all note values were identical
 
-            return ObjectStrains.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / maxStrain * 12.0 - 6.0))));
+            if (noteWeights.Sum() == 0)
+                return 0.0;
+
+            if (consistentTopNote == 0)
+                return noteDifficulties.Count;
+
+            return noteDifficulties.Sum(strain => 1.0 / (1.0 + Math.Exp(-(strain / consistentTopNote * 12.0 - 6.0))));
         }
 
         public double CountTopWeightedSliders() => OsuStrainUtils.CountTopWeightedSliders(sliderStrains, DifficultyValue());
