10ebe1d761f2922a3424e11826d52875f416514d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2025-05-25 20:40:45 +0900

    Add back missing using statement

--- osu.Game/Screens/SelectV2/BeatmapTitleWedge_DifficultyDisplay.cs
+++ osu.Game/Screens/SelectV2/BeatmapTitleWedge_DifficultyDisplay.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
