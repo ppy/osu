6fbc9e4580300d1c2b6a5cce99683fea9977e8c0
Dean Herbert <pe@ppy.sh>
2025-05-30 01:33:59 +0900

    Separate active standalone beatmap panels from the carousel vertically

--- osu.Game/Screens/SelectV2/PanelBeatmapStandalone.cs
+++ osu.Game/Screens/SelectV2/PanelBeatmapStandalone.cs
@@ -71,6 +71,22 @@ namespace osu.Game.Screens.SelectV2
         private OsuSpriteText difficultyText = null!;
         private OsuSpriteText authorText = null!;
 
+        public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
+        {
+            var inputRectangle = TopLevelContent.DrawRectangle;
+
+            if (Selected.Value)
+            {
+                // Cover the gaps introduced by the spacing between BeatmapPanels so that clicks will not fall through the carousel.
+                //
+                // Caveat is that for simplicity, we are covering the full spacing, so panels with frontmost depth will have a slightly
+                // larger hit target.
+                inputRectangle = inputRectangle.Inflate(new MarginPadding { Vertical = BeatmapCarousel.SPACING * 2 });
+            }
+
+            return inputRectangle.Contains(TopLevelContent.ToLocalSpace(screenSpacePos));
+        }
+
         public PanelBeatmapStandalone()
         {
             PanelXOffset = 20;
