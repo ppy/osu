1bc59ebd801e72792d1e5c823ea777ad86b52815
Jay Lawton <jaime@merkids.com>
2025-06-03 22:49:43 +1000

    additions

--- osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/Data/SameRhythmHitObjectGrouping.cs
+++ osu.Game.Rulesets.Taiko/Difficulty/Preprocessing/Rhythm/Data/SameRhythmHitObjectGrouping.cs
@@ -1,7 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Taiko.Difficulty.Utils;
 
@@ -29,16 +31,18 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm.Data
         public double Duration => HitObjects[^1].StartTime - HitObjects[0].StartTime;
 
         /// <summary>
-        /// The interval in ms of each hit object in this <see cref="SameRhythmHitObjectGrouping"/>. This is only defined if there is
+        /// The normalised interval in ms of each hit object in this <see cref="SameRhythmHitObjectGrouping"/>. This is only defined if there is
         /// more than two hit objects in this <see cref="SameRhythmHitObjectGrouping"/>.
         /// </summary>
         public readonly double? HitObjectInterval;
 
         /// <summary>
-        /// The ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="SameRhythmHitObjectGrouping"/>. In the
+        /// The normalised ratio of <see cref="HitObjectInterval"/> between this and the previous <see cref="SameRhythmHitObjectGrouping"/>. In the
         /// case where one or both of the <see cref="HitObjectInterval"/> is undefined, this will have a value of 1.
         /// </summary>
         public readonly double HitObjectIntervalRatio;
+
+        private const double snap_tolerance = 5.0; // Tolerance for snapping intervals to the previous group in ms.
 
         /// <inheritdoc/>
         public double Interval { get; }
@@ -48,16 +52,40 @@ namespace osu.Game.Rulesets.Taiko.Difficulty.Preprocessing.Rhythm.Data
             Previous = previous;
             HitObjects = hitObjects;
 
-            // Calculate the average interval between hitobjects, or null if there are fewer than two
-            HitObjectInterval = HitObjects.Count < 2 ? null : Duration / (HitObjects.Count - 1);
+            // Cluster and normalise each hitobjects delta-time.
+            var normaliseDeltaTime = DeltaTimeNormaliser.Normalise(hitObjects, 5.0);
+
+            var hitObjectDeltaTime = hitObjects
+                                     .Skip(1)
+                                     .Select(hitObject => normaliseDeltaTime[hitObject])
+                                     .ToList();
+
+            double modalDelta = hitObjectDeltaTime.Count > 0
+                ? hitObjectDeltaTime
+                  .Select(deltaTime => Math.Round(deltaTime))
+                  .GroupBy(deltaTime => deltaTime)
+                  .OrderByDescending(group => group.Count())
+                  .First().Key
+                : 0;
+
+            // Calculate the average interval between hitobjects.
+            HitObjectInterval = hitObjectDeltaTime.Count > 0
+                ? previous?.HitObjectInterval is double previousDelta && Math.Abs(modalDelta - previousDelta) <= snap_tolerance
+                    ? previousDelta
+                    : modalDelta
+                : null;
 
             // Calculate the ratio between this group's interval and the previous group's interval
-            HitObjectIntervalRatio = Previous?.HitObjectInterval != null && HitObjectInterval != null
-                ? HitObjectInterval.Value / Previous.HitObjectInterval.Value
-                : 1;
+            HitObjectIntervalRatio = previous?.HitObjectInterval is double previousInterval && HitObjectInterval is double currentInterval
+                ? currentInterval / previousInterval
+                : 1.0;
 
             // Calculate the interval from the previous group's start time
-            Interval = Previous != null ? StartTime - Previous.StartTime : double.PositiveInfinity;
+            Interval = previous == null
+                ? double.PositiveInfinity
+                : Math.Abs(StartTime - previous.StartTime) <= snap_tolerance
+                    ? 0
+                    : StartTime - previous.StartTime;
         }
     }
 }
