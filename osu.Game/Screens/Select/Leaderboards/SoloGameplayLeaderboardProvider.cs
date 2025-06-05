e47e4d35733028a72f9da64cba7df62c0453ac05
Dan Balasescu <87902114+bubb13gummy@users.noreply.github.com>
2025-06-05 23:01:31 +0900

    Fix extreme Drawable thrashing on initial leaderboard population

--- osu.Game/Screens/Select/Leaderboards/SoloGameplayLeaderboardProvider.cs
+++ osu.Game/Screens/Select/Leaderboards/SoloGameplayLeaderboardProvider.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
@@ -34,10 +35,12 @@ namespace osu.Game.Screens.Select.Leaderboards
 
             isPartial = leaderboardManager?.CurrentCriteria?.Scope != BeatmapLeaderboardScope.Local && globalScores?.TopScores.Count >= 50;
 
+            List<GameplayLeaderboardScore> newScores = new List<GameplayLeaderboardScore>();
+
             if (globalScores != null)
             {
                 foreach (var topScore in globalScores.AllScores.OrderByTotalScore())
-                    scores.Add(new GameplayLeaderboardScore(topScore, false));
+                    newScores.Add(new GameplayLeaderboardScore(topScore, false));
             }
 
             if (gameplayState != null)
@@ -48,8 +51,10 @@ namespace osu.Game.Screens.Select.Leaderboards
                     TotalScoreTiebreaker = long.MaxValue
                 };
                 localScore.TotalScore.BindValueChanged(_ => sorting.Invalidate());
-                scores.Add(localScore);
+                newScores.Add(localScore);
             }
+
+            scores.AddRange(newScores);
 
             Scheduler.AddDelayed(sort, 1000, true);
         }
