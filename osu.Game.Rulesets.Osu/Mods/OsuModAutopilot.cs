c3cec2ed315a7a020ff86a98e6b6c31d557048b7
TaterToes <nguyendamian2006@gmail.com>
2025-06-05 20:18:24 -0400

    Create new idea for autopilot

--- osu.Game.Rulesets.Osu/Mods/OsuModAutopilot.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModAutopilot.cs
@@ -2,17 +2,19 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.StateChanges;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Objects;
-using osu.Game.Rulesets.Osu.Replays;
+using osu.Game.Rulesets.Osu.Objects.Drawables;
 using osu.Game.Rulesets.Osu.UI;
 using osu.Game.Rulesets.UI;
+using osu.Game.Rulesets.Scoring;
+using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
@@ -35,29 +37,21 @@ namespace osu.Game.Rulesets.Osu.Mods
             typeof(ModTouchDevice)
         };
 
+        // When currentTime equals the start of the hitwindow minus the start offset, we start reducing availableTime
+        // from this value down to 1 when currentTime equals the end of the hitwindow minus the end offset.
+        // This ensures that, if we enter the window late, we still have some room for natural cursor movement.
+        private const double hitwindow_start_offset = 40;
+        private const double hitwindow_end_offset = 5;
+
+        // The spinner radius value from OsuAutoGeneratorBase
+        private const float spinner_radius = 50;
+
         private OsuInputManager inputManager = null!;
+        private Playfield playfield = null!;
 
-        private List<OsuReplayFrame> replayFrames = null!;
+        private readonly IBindable<bool> hasReplayLoaded = new Bindable<bool>();
 
-        private int currentFrame = -1;
-
-        public void Update(Playfield playfield)
-        {
-            if (currentFrame == replayFrames.Count - 1) return;
-
-            double time = playfield.Clock.CurrentTime;
-
-            // Very naive implementation of autopilot based on proximity to replay frames.
-            // Special case for the first frame is required to ensure the mouse is in a sane position until the actual time of the first frame is hit.
-            // TODO: this needs to be based on user interactions to better match stable (pausing until judgement is registered).
-            if (currentFrame < 0 || Math.Abs(replayFrames[currentFrame + 1].Time - time) <= Math.Abs(replayFrames[currentFrame].Time - time))
-            {
-                currentFrame++;
-                new MousePositionAbsoluteInput { Position = playfield.ToScreenSpace(replayFrames[currentFrame].Position) }.Apply(inputManager.CurrentState, inputManager);
-            }
-
-            // TODO: Implement the functionality to automatically spin spinners
-        }
+        private (Vector2 Position, double Time) lastHitInfo = (default, 0);
 
         public void ApplyToDrawableRuleset(DrawableRuleset<OsuHitObject> drawableRuleset)
         {
@@ -65,8 +59,206 @@ namespace osu.Game.Rulesets.Osu.Mods
             inputManager = ((DrawableOsuRuleset)drawableRuleset).KeyBindingInputManager;
             inputManager.AllowUserCursorMovement = false;
 
-            // Generate the replay frames the cursor should follow
-            replayFrames = new OsuAutoGenerator(drawableRuleset.Beatmap, drawableRuleset.Mods).Generate().Frames.Cast<OsuReplayFrame>().ToList();
+            playfield = drawableRuleset.Playfield;
+
+            hasReplayLoaded.BindTo(drawableRuleset.HasReplayLoaded);
+
+            playfield.NewResult += (drawableHitObject, result) =>
+            {
+                Vector2 screenCenter = drawableHitObject.ScreenSpaceDrawQuad.Centre;
+                Vector2 fieldPos = playfield.ScreenSpaceToGamefield(screenCenter);
+
+                if (drawableHitObject is DrawableSlider sliderDrawable)
+                {
+                    var slider = sliderDrawable.HitObject;
+
+                    Vector2 pathEnd = slider.Path.PositionAt(1);
+
+                    fieldPos = (slider.RepeatCount % 2 == 0)
+                    ? sliderDrawable.Position + (pathEnd * sliderDrawable.Scale)
+                    : slider.HeadCircle.Position;
+
+                }
+                else
+                {
+                    // fallback: centre of any other hit-object drawable
+                    screenCenter = drawableHitObject.ScreenSpaceDrawQuad.Centre;
+                    fieldPos = playfield.ScreenSpaceToGamefield(screenCenter);
+                }
+
+                lastHitInfo = (fieldPos, result.TimeAbsolute);
+            };
+        }
+
+        private bool firstTick = true;
+
+        public void Update(Playfield playfield)
+        {
+            double currentTime = playfield.Clock.CurrentTime;
+
+            var nextObject = playfield.HitObjectContainer.AliveObjects.FirstOrDefault(d => !d.Judged);
+
+            if (nextObject == null)
+                return;
+
+            // Set the mouse cursor on the first tick, then to be never used again during gameplay. :P
+            if (firstTick)
+                {
+                    Vector2 screenStart = inputManager.CurrentState.Mouse.Position;
+                    Vector2 fieldStart = playfield.ScreenSpaceToGamefield(screenStart);
+                    double timeStart = playfield.Clock.CurrentTime;
+
+                    lastHitInfo = (fieldStart, timeStart);
+
+                    firstTick = false;
+                }
+
+            // Sliders do not have hitwindows except for the HeadCircle, so we need to check for sliders.
+            double mehWindow = nextObject is DrawableSlider checkForSld
+                ? checkForSld.HeadCircle.HitObject.HitWindows.WindowFor(HitResult.Meh)
+                : nextObject.HitObject.HitWindows.WindowFor(HitResult.Meh);
+
+            var target = nextObject.Position;
+
+            double start = nextObject.HitObject.StartTime;
+
+            switch (nextObject)
+            {
+                case DrawableSpinner spinnerDrawable:
+                    handleSpinner(spinnerDrawable, currentTime, start);
+                    return;
+
+                case DrawableSlider sliderDrawable:
+                    if (!sliderDrawable.HeadCircle.Judged || hasReplayLoaded.Value)
+                        break;
+
+                    var slider = sliderDrawable.HitObject;
+                    double elapsed = currentTime - start;
+
+                    if (elapsed + mehWindow >= 0 && elapsed < slider.Duration)
+                    {
+                        double prog = Math.Clamp(elapsed / slider.Duration, 0, 1);
+                        double spans = (prog * (slider.RepeatCount + 1));
+                        spans = (spans > 1 && spans % 2 > 1) ? 1 - (spans % 1) : spans % 1;
+
+                        Vector2 pathPos = sliderDrawable.Position + (slider.Path.PositionAt(spans) * sliderDrawable.Scale);
+
+                        applyCursor(pathPos);
+                    }
+
+                    return;
+            }
+
+            if (hasReplayLoaded.Value)
+                return;
+
+            double hitWindowStart = start - mehWindow - hitwindow_start_offset;
+            double hitWindowEnd = start + mehWindow - hitwindow_end_offset;
+
+            // Compute how many ms remain for cursor movement toward the hit-object
+            double availableTime = handleTime(hitWindowStart, hitWindowEnd, nextObject.Entry.LifetimeStart);
+
+            moveTowards(target, availableTime);
+        }
+
+        private double handleTime(double hitWindowStart, double hitWindowEnd, double lifetimeStart)
+        {
+            // We want the cursor to eventually reach the center of the HitCircle.
+            // However, when it's inside the HitWindow, we want to the cursor to be fast enough
+            // where the player can't tap it, but slow enough so it doesn't seem like the cursor is teleporting.
+
+            // If the hitobject doesn't appear during the time it was judged, it will teleport.
+            // So, we want to save the time when the hitobject first appears.
+            var lastJudgedTime = lastHitInfo.Time;
+            if (lastHitInfo.Time < lifetimeStart)
+            {
+                lastHitInfo.Time = lifetimeStart;
+                lastJudgedTime = lifetimeStart;
+            }
+
+            // Compute scale from 0 to 1, then multiply by an offset. This will be used if we are inside between hitWindowStart and hitWindowEnd so we can prevent sudden cursor teleportation.
+            double scaledTime = 1 + (Math.Clamp((hitWindowEnd - lastJudgedTime) / (hitWindowEnd - hitWindowStart), 0, 1) * (hitwindow_start_offset - 1));
+
+            // Edge case where the cursor may not reach the hitobject in time.
+            scaledTime = scaledTime > (hitWindowEnd - lastJudgedTime)
+                ? hitWindowEnd - lastJudgedTime
+                : scaledTime;
+
+            double timeLeft = lastJudgedTime >= hitWindowStart
+                ? scaledTime
+                : hitWindowStart - lastJudgedTime;
+
+            // Don’t let it go below 1
+            return Math.Max(timeLeft, 1);
+        }
+
+        private void handleSpinner(DrawableSpinner spinnerDrawable, double currentTime, double start)
+        {
+            var spinner = spinnerDrawable.HitObject;
+
+            spinnerDrawable.RotationTracker.Tracking = spinnerDrawable.RotationTracker.IsSpinnableTime;
+            spinnerDrawable.HandleUserInput = false;
+
+            double elapsed = currentTime - start;
+
+            // Before spinner starts, move to position.
+            if (elapsed < 0 && !hasReplayLoaded.Value)
+            {
+                Vector2 spinnerTargetPosition = spinner.Position + new Vector2(
+                    -(float)Math.Sin(0) * spinner_radius,
+                    -(float)Math.Cos(0) * spinner_radius);
+
+                double hitWindowStart = start - hitwindow_start_offset;
+                double hitWindowEnd = start + spinner.Duration - hitwindow_end_offset;
+
+                double duration = handleTime(hitWindowStart, hitWindowEnd, spinnerDrawable.Entry.LifetimeStart);
+
+                moveTowards(spinnerTargetPosition, duration);
+
+                return;
+            }
+
+            double calculatedSpeed = 1.01 * (spinner.MaximumBonusSpins + spinner.SpinsRequiredForBonus) / spinner.Duration;
+            double rate = calculatedSpeed / playfield.Clock.Rate;
+            double elapsedTime = playfield.Clock.ElapsedFrameTime;
+
+            // Automatically spin spinner.
+            spinnerDrawable.RotationTracker.AddRotation(float.RadiansToDegrees((float)elapsedTime * (float)rate * MathF.PI * 2.0f));
+
+            if (hasReplayLoaded.Value)
+                return;
+
+            double angle = 2 * Math.PI * (elapsed * rate);
+            Vector2 circPos = spinner.Position + new Vector2(
+                -(float)Math.Sin(angle) * spinner_radius,
+                -(float)Math.Cos(angle) * spinner_radius);
+
+            applyCursor(circPos);
+        }
+
+        private void moveTowards(Vector2 target, double timeMs)
+        {
+            var (lastHitPosition, lastJudgedTime) = lastHitInfo;
+            double currentTime = playfield.Clock.CurrentTime;
+
+            double elapsed = currentTime - lastJudgedTime;
+
+            // The percentage of time between the lastJudgedObject and the time to reach the next HitObject's HitWindow.
+            float frac = (float)Math.Clamp(elapsed / timeMs, 0, 1);
+
+            // compute the new cursor position by Lerp
+            Vector2 newPos = Vector2.Lerp(lastHitPosition, target, frac);
+
+            // if we’re effectively at (or beyond) the target, snap there
+            if (frac >= 1 || Vector2.Distance(lastHitPosition, newPos) >= Vector2.Distance(lastHitPosition, target))
+                newPos = target;
+
+            applyCursor(newPos);
+        }
+
+        private void applyCursor(Vector2 playfieldPosition)
+        {
+            new MousePositionAbsoluteInput { Position = playfield.ToScreenSpace(playfieldPosition) }.Apply(inputManager.CurrentState, inputManager);
         }
     }
 }
