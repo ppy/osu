1ec56dbbf0280a2ef06fdc3b47761d74f0bce33c
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-06-25 09:46:54 +0200

    Add failing test

--- osu.Game.Tests/Visual/Navigation/TestSceneSongSelectNavigation.cs
+++ osu.Game.Tests/Visual/Navigation/TestSceneSongSelectNavigation.cs
@@ -9,6 +9,7 @@ using osu.Framework.Extensions;
 using osu.Framework.Extensions.TypeExtensions;
 using osu.Framework.Testing;
 using osu.Game.Beatmaps;
+using osu.Game.Online.Leaderboards;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Mods;
 using osu.Game.Rulesets.Mods;
@@ -166,6 +167,30 @@ namespace osu.Game.Tests.Visual.Navigation
 
             AddAssert("magnetised selected", () => Game.SelectedMods.Value.Single(), Is.TypeOf<OsuModMagnetised>);
             AddAssert("mod configured", () => ((OsuModMagnetised)Game.SelectedMods.Value.Single()).AttractionStrength.Value, () => Is.EqualTo(1.0f));
+        }
+
+        [Test]
+        public void TestLeaderboardCorrectInPlayer()
+        {
+            IWorkingBeatmap beatmap() => Game.Beatmap.Value;
+
+            PushAndConfirm(() => new SoloSongSelect());
+
+            AddStep("import beatmap", () => BeatmapImportHelper.LoadOszIntoOsu(Game, virtualTrack: true).WaitSafely());
+
+            AddUntilStep("wait for selected", () => !Game.Beatmap.IsDefault);
+            AddStep("switch to next difficulty and immediately press enter", () =>
+            {
+                InputManager.Key(Key.Down);
+                Schedule(() => InputManager.Key(Key.Enter));
+            });
+
+            AddUntilStep("wait for player", () =>
+            {
+                DismissAnyNotifications();
+                return Game.ScreenStack.CurrentScreen is Player;
+            });
+            AddAssert("leaderboard matches gameplay beatmap", () => Game.ChildrenOfType<LeaderboardManager>().Single().CurrentCriteria?.Beatmap, () => Is.EqualTo(beatmap().BeatmapInfo));
         }
 
         private Func<Player> playToResults()
