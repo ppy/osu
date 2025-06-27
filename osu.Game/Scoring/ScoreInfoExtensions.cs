6e73a9299ecda190f6d2cd17d099ebf2825e75ff
Dani211e <daniel.jenrich@gmail.com>
2025-06-27 04:28:44 +0200

    Throw ArgumentOutOfRangeException on default path

--- osu.Game/Scoring/ScoreInfoExtensions.cs
+++ osu.Game/Scoring/ScoreInfoExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
@@ -52,7 +53,7 @@ namespace osu.Game.Scoring
                 case LeaderboardSortMode.Date:
                     return scores.OrderByDescending(s => s.Date);
 
-                default: return scores;
+                default: throw new ArgumentOutOfRangeException();
             }
         }
 
