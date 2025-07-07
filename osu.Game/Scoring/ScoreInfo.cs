3cfe58e660dc38be387185e84bc1a1112beb0c30
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-07-07 09:53:26 +0200

    Track count of times gameplay was paused on `ScoreInfo`

--- osu.Game/Scoring/ScoreInfo.cs
+++ osu.Game/Scoring/ScoreInfo.cs
@@ -155,6 +155,9 @@ namespace osu.Game.Scoring
         [MapTo("MaximumStatistics")]
         public string MaximumStatisticsJson { get; set; } = string.Empty;
 
+        [Ignored]
+        public int PauseCount { get; set; }
+
         public ScoreInfo(BeatmapInfo? beatmap = null, RulesetInfo? ruleset = null, RealmUser? realmUser = null)
         {
             Ruleset = ruleset ?? new RulesetInfo();
