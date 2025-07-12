6c4d80501ea83cde45cd2a4b562b3525c5486089
VocalFan <45863583+FluffyOMC@users.noreply.github.com>
2025-07-12 12:34:25 -0400

    Fix duration by having it update on Beatmap creation

--- osu.Game/Screens/OnlinePlay/OnlinePlayFreestyleSelect.cs
+++ osu.Game/Screens/OnlinePlay/OnlinePlayFreestyleSelect.cs
@@ -99,15 +99,18 @@ namespace osu.Game.Screens.OnlinePlay
             private readonly PlaylistItem item;
             private double itemLength;
             private int beatmapSetId;
+            [Resolved]
+            private RealmAccess realm { get; set; } = null!;
 
             public DifficultySelectFilterControl(PlaylistItem item)
             {
                 this.item = item;
             }
 
-            [BackgroundDependencyLoader]
-            private void load(RealmAccess realm)
+            public override FilterCriteria CreateCriteria()
             {
+                var criteria = base.CreateCriteria();
+
                 realm.Run(r =>
                 {
                     int beatmapId = item.Beatmap.OnlineID;
@@ -116,11 +119,6 @@ namespace osu.Game.Screens.OnlinePlay
                     itemLength = beatmap?.Length ?? 0;
                     beatmapSetId = beatmap?.BeatmapSet?.OnlineID ?? 0;
                 });
-            }
-
-            public override FilterCriteria CreateCriteria()
-            {
-                var criteria = base.CreateCriteria();
 
                 // Must be from the same set as the playlist item.
                 criteria.BeatmapSetId = beatmapSetId;
