13fc37b2de614e7b8ed9ef09c710ed259db1d806
Dan Balasescu <smoogipoo@smgi.me>
2025-07-14 21:19:20 +0900

    Re-privatise `PlaylistItem.Beatmap`, adjust tests

--- osu.Game/Online/Rooms/PlaylistItem.cs
+++ osu.Game/Online/Rooms/PlaylistItem.cs
@@ -90,7 +90,7 @@ namespace osu.Game.Online.Rooms
         /// In many cases, this will *not* contain any usable information apart from OnlineID.
         /// </summary>
         [JsonIgnore]
-        public IBeatmapInfo Beatmap { get; set; }
+        public IBeatmapInfo Beatmap { get; private set; }
 
         [JsonIgnore]
         public IBindable<bool> Valid => valid;
