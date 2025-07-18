8362456148faa1239f4d49bfc1567af3f9c1d3c8
Andrei Zavatski <megaman9919@gmail.com>
2025-07-18 12:25:54 +0300

    Add antialiasing to triangles in MarkerVisualisation

--- osu.Game/Screens/Edit/Components/Timelines/Summary/Parts/MarkerPart.cs
+++ osu.Game/Screens/Edit/Components/Timelines/Summary/Parts/MarkerPart.cs
@@ -5,9 +5,14 @@ using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shaders;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
 using osu.Framework.Threading;
+using osu.Game.Graphics.Backgrounds;
 using osu.Game.Overlays;
 using osuTK;
 
@@ -78,21 +83,9 @@ namespace osu.Game.Screens.Edit.Components.Timelines.Summary.Parts
                 RelativePositionAxes = Axes.X;
                 RelativeSizeAxes = Axes.Y;
                 AutoSizeAxes = Axes.X;
+                Masking = true;
                 InternalChildren = new Drawable[]
                 {
-                    new Triangle
-                    {
-                        Anchor = Anchor.TopCentre,
-                        Origin = Anchor.BottomCentre,
-                        Scale = new Vector2(1, -1),
-                        Size = new Vector2(10, 5),
-                    },
-                    new Triangle
-                    {
-                        Anchor = Anchor.BottomCentre,
-                        Origin = Anchor.BottomCentre,
-                        Size = new Vector2(10, 5),
-                    },
                     new Box
                     {
                         Anchor = Anchor.Centre,
@@ -100,12 +93,103 @@ namespace osu.Game.Screens.Edit.Components.Timelines.Summary.Parts
                         RelativeSizeAxes = Axes.Y,
                         Width = 1.4f,
                         EdgeSmoothness = new Vector2(1, 0)
+                    },
+                    new VerticalTriangles
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Y,
+                        Width = 10
                     }
                 };
             }
 
             [BackgroundDependencyLoader]
             private void load(OverlayColourProvider colours) => Colour = colours.Highlight1;
+
+            private partial class VerticalTriangles : Sprite
+            {
+                [BackgroundDependencyLoader]
+                private void load(ShaderManager shaders, IRenderer renderer)
+                {
+                    TextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, "TriangleBorder");
+                    Texture = renderer.WhitePixel;
+                }
+
+                protected override DrawNode CreateDrawNode() => new VerticalTrianglesDrawNode(this);
+
+                private class VerticalTrianglesDrawNode : SpriteDrawNode
+                {
+                    private const float aa = 1.5f; // across how many pixels antialiasing is being applied
+
+                    public VerticalTrianglesDrawNode(VerticalTriangles source)
+                        : base(source)
+                    {
+                    }
+
+                    private float texelSize;
+                    private float triangleScreenSpaceHeight;
+
+                    public override void ApplyState()
+                    {
+                        base.ApplyState();
+
+                        triangleScreenSpaceHeight = ScreenSpaceDrawQuad.Width * 0.5f;
+                        texelSize = aa / Math.Max(ScreenSpaceDrawQuad.Width, 1);
+                    }
+
+                    protected override void Blit(IRenderer renderer)
+                    {
+                        if (triangleScreenSpaceHeight == 0)
+                            return;
+
+                        // TriangleBorder shader makes a smooth triangle for all its sides, which we want to avoid at the top and bottom.
+                        // To do that we are expanding triangles outside the drawable by the aa value and applying masking at the top level.
+                        Quad topTriangle = new Quad
+                        (
+                            ScreenSpaceDrawQuad.TopLeft + new Vector2(-aa, triangleScreenSpaceHeight),
+                            ScreenSpaceDrawQuad.TopRight + new Vector2(aa, triangleScreenSpaceHeight),
+                            ScreenSpaceDrawQuad.TopLeft - new Vector2(aa),
+                            ScreenSpaceDrawQuad.TopRight + new Vector2(aa, -aa)
+                        );
+
+                        Quad bottomTriangle = new Quad
+                        (
+                            ScreenSpaceDrawQuad.BottomLeft - new Vector2(aa, triangleScreenSpaceHeight),
+                            ScreenSpaceDrawQuad.BottomRight - new Vector2(-aa, triangleScreenSpaceHeight),
+                            ScreenSpaceDrawQuad.BottomLeft + new Vector2(-aa, aa),
+                            ScreenSpaceDrawQuad.BottomRight + new Vector2(aa)
+                        );
+
+                        renderer.DrawQuad(Texture, topTriangle, DrawColourInfo.Colour);
+                        renderer.DrawQuad(Texture, bottomTriangle, DrawColourInfo.Colour);
+                    }
+
+                    private IUniformBuffer<TriangleBorderData>? borderDataBuffer;
+
+                    protected override void BindUniformResources(IShader shader, IRenderer renderer)
+                    {
+                        base.BindUniformResources(shader, renderer);
+
+                        borderDataBuffer ??= renderer.CreateUniformBuffer<TriangleBorderData>();
+                        borderDataBuffer.Data = borderDataBuffer.Data with
+                        {
+                            Thickness = 1f,
+                            TexelSize = texelSize
+                        };
+
+                        shader.BindUniformBlock("m_BorderData", borderDataBuffer);
+                    }
+
+                    protected override bool CanDrawOpaqueInterior => false;
+
+                    protected override void Dispose(bool isDisposing)
+                    {
+                        base.Dispose(isDisposing);
+                        borderDataBuffer?.Dispose();
+                    }
+                }
+            }
         }
     }
 }
