7a376456f617e8dcf444c5b727ecde18778d9453
WebFreak001 <gh@webfreak.org>
2025-07-31 11:11:02 +0200

    support ruleset convert interface in song select v2

--- osu.Game/Screens/SelectV2/SongSelect.cs
+++ osu.Game/Screens/SelectV2/SongSelect.cs
@@ -559,7 +559,7 @@ namespace osu.Game.Screens.SelectV2
             if (criteria == null)
                 return false;
 
-            if (!beatmap.AllowGameplayWithRuleset(Ruleset.Value, criteria.AllowConvertedBeatmaps))
+            if (!beatmap.AllowGameplayWithRuleset(Ruleset.Value, criteria.AllowConvertedBeatmaps, criteria.RulesetConvertSupport))
                 return false;
 
             if (beatmap.Hidden)
