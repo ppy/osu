8e73c57470409c75193f068ce8f76f140c23af6c
Hivie <me@hivie.tn>
2025-08-01 21:40:33 +0100

    add `VerifyChecksScope` enum

---
+++ osu.Game/Rulesets/Edit/Checks/Components/VerifyChecksScope.cs
@@ -0,0 +1,23 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+using osu.Game.Localisation;
+
+namespace osu.Game.Rulesets.Edit.Checks.Components
+{
+    public enum VerifyChecksScope
+    {
+        /// <summary>
+        /// Run checks that apply to the current difficulty.
+        /// </summary>
+        [LocalisableDescription(typeof(EditorStrings), nameof(EditorStrings.ThisDifficulty))]
+        ThisDifficulty,
+
+        /// <summary>
+        /// Run checks that apply to the beatmapset as a whole.
+        /// </summary>
+        [LocalisableDescription(typeof(EditorStrings), nameof(EditorStrings.General))]
+        General,
+    }
+}
