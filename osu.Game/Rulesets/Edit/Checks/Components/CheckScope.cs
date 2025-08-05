c1c43fab175ab6e97b8135c34c014f7c79a0d953
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-08-05 08:23:47 +0200

    Bring back localisations

--- osu.Game/Rulesets/Edit/Checks/Components/CheckScope.cs
+++ osu.Game/Rulesets/Edit/Checks/Components/CheckScope.cs
@@ -1,6 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Localisation;
+using osu.Game.Localisation;
+
 namespace osu.Game.Rulesets.Edit.Checks.Components
 {
     public enum CheckScope
@@ -8,11 +11,13 @@ namespace osu.Game.Rulesets.Edit.Checks.Components
         /// <summary>
         /// Run checks that apply to the current difficulty.
         /// </summary>
+        [LocalisableDescription(typeof(EditorStrings), nameof(EditorStrings.CheckCurrentDifficulty))]
         Difficulty,
 
         /// <summary>
         /// Run checks that apply to the beatmap set as a whole.
         /// </summary>
+        [LocalisableDescription(typeof(EditorStrings), nameof(EditorStrings.CheckEntireBeatmapSet))]
         BeatmapSet,
     }
 }
