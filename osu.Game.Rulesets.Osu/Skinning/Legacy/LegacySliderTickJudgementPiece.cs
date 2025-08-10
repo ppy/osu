b8431b61b26760b139934fcc9a523bbf9ea13277
clayton <87902114+bubb13gummy@users.noreply.github.com>
2025-08-10 07:12:59 -0700

    Add sliderpoint10 and sliderpoint30 support

---
+++ osu.Game.Rulesets.Osu/Skinning/Legacy/LegacySliderTickJudgementPiece.cs
@@ -0,0 +1,42 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Skinning;
+using osuTK;
+
+namespace osu.Game.Rulesets.Osu.Skinning.Legacy
+{
+    public partial class LegacySliderTickJudgementPiece : Sprite, IAnimatableJudgement, IAppliesJudgementResult
+    {
+        private Texture? texture10;
+        private Texture? texture30;
+
+        [BackgroundDependencyLoader]
+        private void load(ISkinSource skin)
+        {
+            texture10 = skin.GetTexture("sliderpoint10");
+            texture30 = skin.GetTexture("sliderpoint30");
+        }
+
+        public void ApplyJudgementResult(JudgementResult result)
+        {
+            Texture = result.HitObject is SliderTick ? texture10 : texture30;
+        }
+
+        public void PlayAnimation()
+        {
+            // https://github.com/peppy/osu-stable-reference/blob/0e91e49bc83fe8b21c3ba5f1eb2d5d06456eae84/osu!/GameModes/Play/Rulesets/Ruleset.cs#L804-L806
+            this.MoveToOffset(new Vector2(0, -10), 300, Easing.Out)
+                .Then()
+                .FadeOut(60);
+        }
+
+        public Drawable GetAboveHitObjectsProxiedContent() => CreateProxy();
+    }
+}
