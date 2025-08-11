1f0fbff566341817059a00f6a9515525ef0bf33d
Hivie <87902114+bubb13gummy@users.noreply.github.com>
2025-08-11 17:07:03 +0100

    use `Prepend` to resolve dotnet code style test

--- osu.Game/Rulesets/Edit/BeatmapVerifierContext.cs
+++ osu.Game/Rulesets/Edit/BeatmapVerifierContext.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using osu.Game.Beatmaps;
 
 namespace osu.Game.Rulesets.Edit
@@ -35,7 +36,7 @@ namespace osu.Game.Rulesets.Edit
         /// <summary>
         /// All beatmaps in the same beatmapset.
         /// </summary>
-        public IEnumerable<VerifiedBeatmap> AllDifficulties => [CurrentDifficulty, ..OtherDifficulties];
+        public IEnumerable<VerifiedBeatmap> AllDifficulties => OtherDifficulties.Prepend(CurrentDifficulty);
 
         public BeatmapVerifierContext(VerifiedBeatmap currentDifficulty, DifficultyRating difficultyRating = DifficultyRating.ExpertPlus, IReadOnlyList<VerifiedBeatmap>? otherDifficulties = null)
         {
