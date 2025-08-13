a4b10d070e2b4b4a2014cc8500902aa8708091f1
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-08-13 12:32:17 +0200

    Clean up ctors
    There were too many of them, and they were a bit twisty for my liking.

--- osu.Game.Tests/Editing/Checks/CheckInconsistentSettingsTest.cs
+++ osu.Game.Tests/Editing/Checks/CheckInconsistentSettingsTest.cs
@@ -266,7 +266,7 @@ namespace osu.Game.Tests.Editing.Checks
             var verifiedCurrentBeatmap = new BeatmapVerifierContext.VerifiedBeatmap(new TestWorkingBeatmap(currentBeatmap, storyboard), currentBeatmap);
             var verifiedOtherBeatmaps = allDifficulties.Select(b => new BeatmapVerifierContext.VerifiedBeatmap(new TestWorkingBeatmap(b, storyboard), b)).ToList();
 
-            return new BeatmapVerifierContext(verifiedCurrentBeatmap, verifiedOtherBeatmaps);
+            return new BeatmapVerifierContext(verifiedCurrentBeatmap, verifiedOtherBeatmaps, DifficultyRating.ExpertPlus);
         }
     }
 }
