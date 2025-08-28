6ba72fa481462932cf770781a2ac28263fd3e4e9
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-08-28 10:35:31 +0200

    Adjust tests to new beatmap set model usage in carousel

--- osu.Game.Tests/Visual/SongSelectV2/TestScenePanelSet.cs
+++ osu.Game.Tests/Visual/SongSelectV2/TestScenePanelSet.cs
@@ -75,21 +75,21 @@ namespace osu.Game.Tests.Visual.SongSelectV2
                     {
                         new PanelBeatmapSet
                         {
-                            Item = new CarouselItem(beatmapSet)
+                            Item = new CarouselItem(new BeatmapSetUnderGrouping(null, beatmapSet))
                         },
                         new PanelBeatmapSet
                         {
-                            Item = new CarouselItem(beatmapSet),
+                            Item = new CarouselItem(new BeatmapSetUnderGrouping(null, beatmapSet)),
                             KeyboardSelected = { Value = true }
                         },
                         new PanelBeatmapSet
                         {
-                            Item = new CarouselItem(beatmapSet),
+                            Item = new CarouselItem(new BeatmapSetUnderGrouping(null, beatmapSet)),
                             Expanded = { Value = true }
                         },
                         new PanelBeatmapSet
                         {
-                            Item = new CarouselItem(beatmapSet),
+                            Item = new CarouselItem(new BeatmapSetUnderGrouping(null, beatmapSet)),
                             KeyboardSelected = { Value = true },
                             Expanded = { Value = true }
                         },
