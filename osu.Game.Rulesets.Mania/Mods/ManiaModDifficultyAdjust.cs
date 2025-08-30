baee6bb09ad74ff44db046211d4b588c508b829f
Krystian Ptach-Żurakowski <zielonookikrystian@gmail.com>
2025-08-30 05:18:09 +0200

    Add HardRock and Easy Hitwindow options for DifficultyAdjust

--- osu.Game.Rulesets.Mania/Mods/ManiaModDifficultyAdjust.cs
+++ osu.Game.Rulesets.Mania/Mods/ManiaModDifficultyAdjust.cs
@@ -1,11 +1,56 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Mania.Scoring;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
 
 namespace osu.Game.Rulesets.Mania.Mods
 {
-    public class ManiaModDifficultyAdjust : ModDifficultyAdjust
+    public class ManiaModDifficultyAdjust : ModDifficultyAdjust, IApplicableToHitObject
     {
+        [SettingSource("Apply to hit objects", "Adjust the timings as if Hard Rock or Easy is enabled.")]
+        public Bindable<HitWindowAdjustmentType> HitWindowAdjustment { get; } = new Bindable<HitWindowAdjustmentType>(HitWindowAdjustmentType.Off);
+
+        public static double CurrentHitWindowDifficultyMultiplier;
+
+        void IApplicableToHitObject.ApplyToHitObject(HitObject hitObject)
+        {
+            if (HitWindowAdjustment.Value == HitWindowAdjustmentType.HardRock)
+            {
+                CurrentHitWindowDifficultyMultiplier = 1.4;
+            }
+
+            else if (HitWindowAdjustment.Value == HitWindowAdjustmentType.Easy)
+            {
+                CurrentHitWindowDifficultyMultiplier = 1 / 1.4;
+            }
+            else
+            {
+                CurrentHitWindowDifficultyMultiplier = 1;
+            }
+
+            switch (hitObject)
+            {
+                case Note:
+                    ((ManiaHitWindows)hitObject.HitWindows).DifficultyMultiplier = CurrentHitWindowDifficultyMultiplier;
+                    break;
+
+                case HoldNote hold:
+                    ((ManiaHitWindows)hold.Head.HitWindows).DifficultyMultiplier = CurrentHitWindowDifficultyMultiplier;
+                    ((ManiaHitWindows)hold.Tail.HitWindows).DifficultyMultiplier = CurrentHitWindowDifficultyMultiplier;
+                    break;
+            }
+        }
+
+        public enum HitWindowAdjustmentType
+        {
+            Off,
+            HardRock,
+            Easy
+        }
     }
 }
