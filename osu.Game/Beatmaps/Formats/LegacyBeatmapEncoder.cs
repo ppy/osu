d21cdfdfedcd6fa1b5bfd99ae46bb767f027da81
tadatomix <hellowii345@gmail.com>
2025-09-04 23:59:20 +0300

    Fix `GridRotation` check

--- osu.Game/Beatmaps/Formats/LegacyBeatmapEncoder.cs
+++ osu.Game/Beatmaps/Formats/LegacyBeatmapEncoder.cs
@@ -116,7 +116,7 @@ namespace osu.Game.Beatmaps.Formats
             writer.WriteLine(FormattableString.Invariant($"BeatDivisor: {beatmap.BeatmapInfo.BeatDivisor}"));
             writer.WriteLine(FormattableString.Invariant($"GridSize: {beatmap.GridSize}"));
             writer.WriteLine(FormattableString.Invariant($"GridType: {(int)beatmap.GridType}"));
-            if (beatmap.GridRotation > 0)
+            if (beatmap.GridRotation != 0)
                 writer.WriteLine(FormattableString.Invariant($"GridRotation: {beatmap.GridRotation}"));
             writer.WriteLine(FormattableString.Invariant($"GridOffset: {beatmap.GridOffset.X},{beatmap.GridOffset.Y}"));
             writer.WriteLine(FormattableString.Invariant($"TimelineZoom: {beatmap.TimelineZoom}"));
