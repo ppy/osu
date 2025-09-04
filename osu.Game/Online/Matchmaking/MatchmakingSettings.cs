1627f67ada890829b5b0af7f964c8de8f61f966e
Dean Herbert <pe@ppy.sh>
2025-09-04 18:43:46 +0900

    Add variant to `MatchmakingSettings`

--- osu.Game/Online/Matchmaking/MatchmakingSettings.cs
+++ osu.Game/Online/Matchmaking/MatchmakingSettings.cs
@@ -14,16 +14,18 @@ namespace osu.Game.Online.Matchmaking
         [Key(0)]
         public int RulesetId { get; set; }
 
+        [Key(1)]
+        public int Variant { get; set; }
+
         public bool Equals(MatchmakingSettings? other)
-            => other != null && RulesetId == other.RulesetId;
+            => other != null
+               && RulesetId == other.RulesetId
+               && Variant == other.Variant;
 
         public override bool Equals(object? obj)
             => obj is MatchmakingSettings other && Equals(other);
 
         [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
-        public override int GetHashCode()
-        {
-            return RulesetId;
-        }
+        public override int GetHashCode() => HashCode.Combine(RulesetId, Variant);
     }
 }
