22e23a8d1c058a7b40c1ce38160c844e771be3f4
Valerus9 <valerus6969@gmail.com>
2025-09-07 10:07:06 +0200

    Add Separate All Difficulties setting

--- osu.Game.Tests/Visual/SongSelectV2/BeatmapCarouselFilterSortingTest.cs
+++ osu.Game.Tests/Visual/SongSelectV2/BeatmapCarouselFilterSortingTest.cs
@@ -139,6 +139,92 @@ namespace osu.Game.Tests.Visual.SongSelectV2
         }
 
         /// <summary>
+        /// Ensures that difficulty sorting still works and that any other sort mode will sort first by
+        /// itself then by the starrating
+        /// </summary>
+        [Test]
+        public async Task TestSeparateAllDifficultySort()
+        {
+            List<BeatmapSetInfo> beatmapSets = new List<BeatmapSetInfo>();
+            int amountOfSets = 4;
+            int amountOfBeatmaps = 2;
+
+            for (int i = 0; i < amountOfSets; i++)
+            {
+                int numberMetadata = (i - i % 2) / 2;
+                DateTimeOffset dateTimeOffset = new DateTimeOffset(2000, 1, 1, 12, 1, numberMetadata, new TimeSpan());
+
+                var set = new BeatmapSetInfo
+                {
+                    ID = Guid.NewGuid(),
+                    DateAdded = dateTimeOffset,
+                    DateRanked = dateTimeOffset,
+                    DateSubmitted = dateTimeOffset,
+                };
+
+                for (int j = 0; j < amountOfBeatmaps; j++)
+                {
+                    var metadata = new BeatmapMetadata
+                    {
+                        Artist = $"{numberMetadata}. Artist",
+                        Title = $"{numberMetadata}. beatmap",
+                        Author = { Username = $"{numberMetadata}. Guy " },
+                        Source = $"{numberMetadata}. source",
+                    };
+
+                    double starRating = 0;
+                    //Creates a stable pattern of "unsorted" starratings (0, n-1, 2, n-3, 4, n-5)
+                    if (amountOfBeatmaps % 2 == 0)
+                        starRating = j % 2 == 0 ? j : amountOfBeatmaps - j;
+                    else
+                        starRating = j % 2 == 0 ? j : amountOfBeatmaps - 1 - j;
+                    double BPM = numberMetadata * 30;
+                    double Length = numberMetadata * 20;
+
+                    BeatmapInfo beatmapInfo = new BeatmapInfo()
+                    {
+                        StarRating = starRating,
+                        ID = Guid.NewGuid(),
+                        Metadata = metadata,
+                        BeatmapSet = set,
+                        BPM = BPM,
+                        LastPlayed = dateTimeOffset,
+                        Length = numberMetadata,
+                    };
+
+                    set.Beatmaps.Add(beatmapInfo);
+                }
+
+                beatmapSets.Add(set);
+            }
+
+            foreach (var sortMode in Enum.GetValues<SortMode>())
+            {
+                if (sortMode == SortMode.Difficulty)
+                    continue;
+
+                var sortModeResults = await runSorting(sortMode, true, beatmapSets);
+
+                Assert.That(sortModeResults.Count(), Is.EqualTo(amountOfSets * amountOfBeatmaps));
+
+                for (int i = 0; i < amountOfSets * amountOfBeatmaps; ++i)
+                {
+                    Assert.That(sortModeResults.Skip(i).First().StarRating, Is.EqualTo(i % amountOfBeatmaps), () => $"{sortMode} incorrectly sorts.");
+                }
+            }
+
+            var difficultyResults = await runSorting(SortMode.Difficulty, true, beatmapSets);
+
+            Assert.That(difficultyResults.Count(), Is.EqualTo(amountOfSets * amountOfBeatmaps));
+
+            for (int i = 0; i < amountOfSets * amountOfBeatmaps; ++i)
+            {
+                double correctStarRating = (i - (i % amountOfSets)) / amountOfSets;
+                Assert.That(difficultyResults.Skip(i).First().StarRating, Is.EqualTo(correctStarRating), () => $"{SortMode.Difficulty} incorrectly sorts.");
+            }
+        }
+
+        /// <summary>
         /// Ensures stability is maintained on different sort modes for items with equal properties.
         /// </summary>
         [Test]
@@ -173,6 +259,13 @@ namespace osu.Game.Tests.Visual.SongSelectV2
         private static async Task<IEnumerable<BeatmapInfo>> runSorting(SortMode sort, List<BeatmapSetInfo> beatmapSets)
         {
             var sorter = new BeatmapCarouselFilterSorting(() => new FilterCriteria { Sort = sort });
+            var carouselItems = await sorter.Run(beatmapSets.SelectMany(s => s.Beatmaps.Select(b => new CarouselItem(b))), CancellationToken.None);
+            return carouselItems.Select(ci => ci.Model).OfType<BeatmapInfo>();
+        }
+
+        private static async Task<IEnumerable<BeatmapInfo>> runSorting(SortMode sort, bool separateAllDifficulties, List<BeatmapSetInfo> beatmapSets)
+        {
+            var sorter = new BeatmapCarouselFilterSorting(() => new FilterCriteria { Sort = sort, SeparateAllDifficulties = separateAllDifficulties });
             var carouselItems = await sorter.Run(beatmapSets.SelectMany(s => s.Beatmaps.Select(b => new CarouselItem(b))), CancellationToken.None);
             return carouselItems.Select(ci => ci.Model).OfType<BeatmapInfo>();
         }
