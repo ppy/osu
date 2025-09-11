2da9893251980918975ff56d18321a9396160416
Dan Balasescu <smoogipoo@smgi.me>
2025-09-11 11:49:11 +0900

    Add initial screen + test structure

---
+++ osu.Game/Screens/OnlinePlay/Playlists/PlaylistsSongSelectV2.cs
@@ -0,0 +1,14 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Screens.SelectV2;
+
+namespace osu.Game.Screens.OnlinePlay.Playlists
+{
+    public class PlaylistsSongSelectV2 : SongSelect
+    {
+        protected override void OnStart()
+        {
+        }
+    }
+}
