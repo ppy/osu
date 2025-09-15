956c86bead145a2b59171b07497f2b45062bd9e7
cdwcgt <cdwcgt@cdwcgt.top>
2025-09-15 11:41:59 +0800

    add wiki language picker
    not sure UI design here, only add the flag, decide to let colorProvider to manage color

--- osu.Game/Overlays/Wiki/WikiHeader.cs
+++ osu.Game/Overlays/Wiki/WikiHeader.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Localisation;
@@ -21,6 +22,10 @@ namespace osu.Game.Overlays.Wiki
 
         public readonly Bindable<APIWikiPage> WikiPageData = new Bindable<APIWikiPage>();
 
+        public readonly Bindable<Language> CurrentLanguage = new Bindable<Language>();
+
+        public WikiLanguageDropdown LanguageDropdown;
+
         public Action ShowIndexPage;
         public Action ShowParentPage;
 
@@ -31,6 +36,21 @@ namespace osu.Game.Overlays.Wiki
 
             WikiPageData.BindValueChanged(onWikiPageChange);
             Current.BindValueChanged(onCurrentChange);
+        }
+
+        protected override Drawable CreateTabControlContent()
+        {
+            return new Container
+            {
+                Height = 40,
+                AutoSizeAxes = Axes.X,
+                Child = LanguageDropdown = new WikiLanguageDropdown
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Width = 200,
+                }
+            };
         }
 
         private void onWikiPageChange(ValueChangedEvent<APIWikiPage> e)
