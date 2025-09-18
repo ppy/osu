1c83f4eeb73236680df2183d02051c60ef5a3a8e
qinvvv <qiinnv@gmail.com>
2025-09-18 08:19:11 -0300

    Ensure WidthForNoteHeightScale correctly defaults to MinimumColumnWidth

--- osu.Game/Skinning/LegacySkin.cs
+++ osu.Game/Skinning/LegacySkin.cs
@@ -145,8 +145,10 @@ namespace osu.Game.Skinning
                     return SkinUtils.As<TValue>(new Bindable<float>(existing.ColumnWidth[maniaLookup.ColumnIndex.Value]));
 
                 case LegacyManiaSkinConfigurationLookups.WidthForNoteHeightScale:
-                    var bindable = new Bindable<Single>(existing.WidthForNoteHeightScale);
-                    return bindable.Value == 0 ? null : SkinUtils.As<TValue>(bindable);
+                    float width = existing.WidthForNoteHeightScale;
+                    if (width <= 0)
+                        width = existing.MinimumColumnWidth;
+                    return SkinUtils.As<TValue>(new Bindable<float>(width));
 
                 case LegacyManiaSkinConfigurationLookups.HitPosition:
                     return SkinUtils.As<TValue>(new Bindable<float>(existing.HitPosition));
