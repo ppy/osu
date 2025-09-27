bc7d38bfe3db3f283ab21228389b38f1fb3e1386
asmorodinov <asmorodinov66@gmail.com>
2025-09-27 20:53:26 +0300

    fix inspect code

--- osu.Game/Screens/Edit/GenericChangeOpacityMenuItem.cs
+++ osu.Game/Screens/Edit/GenericChangeOpacityMenuItem.cs
@@ -12,17 +12,17 @@ namespace osu.Game.Screens.Edit
 {
     internal class GenericChangeOpacityMenuItem : MenuItem
     {
-        private readonly Bindable<float> opacity;
+        private readonly Bindable<float> bindableOpacity;
 
         private readonly Dictionary<float, TernaryStateRadioMenuItem> menuItemLookup = new Dictionary<float, TernaryStateRadioMenuItem>();
 
-        public GenericChangeOpacityMenuItem(Bindable<float> opacity, LocalisableString text, IEnumerable<float> availableOptions)
+        public GenericChangeOpacityMenuItem(Bindable<float> bindableOpacity, LocalisableString text, IEnumerable<float> availableOptions)
             : base(text)
         {
             Items = availableOptions.Select(createMenuItem).ToList().AsReadOnly();
 
-            this.opacity = opacity;
-            opacity.BindValueChanged(opacity =>
+            this.bindableOpacity = bindableOpacity;
+            bindableOpacity.BindValueChanged(opacity =>
             {
                 foreach (var kvp in menuItemLookup)
                     kvp.Value.State.Value = kvp.Key == opacity.NewValue ? TernaryState.True : TernaryState.False;
@@ -36,6 +36,6 @@ namespace osu.Game.Screens.Edit
             return item;
         }
 
-        private void updateOpacity(float opacity) => this.opacity.Value = opacity;
+        private void updateOpacity(float opacity) => bindableOpacity.Value = opacity;
     }
 }
