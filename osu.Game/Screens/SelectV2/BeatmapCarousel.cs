9132ac55ce388c59c92f9b668463b2baaea8395d
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-09-29 09:34:40 +0200

    Adjust initial pool size for group rank displays
    11 is excessive. There can ever be at most 9 of these panels, ever,
    because there are at most 9 possible letter grades at this time (F, D,
    C, B, A, S, S+, SS, SS+).

--- osu.Game/Screens/SelectV2/BeatmapCarousel.cs
+++ osu.Game/Screens/SelectV2/BeatmapCarousel.cs
@@ -789,7 +789,7 @@ namespace osu.Game.Screens.SelectV2
         private readonly DrawablePool<PanelBeatmapSet> setPanelPool = new DrawablePool<PanelBeatmapSet>(100);
         private readonly DrawablePool<PanelGroup> groupPanelPool = new DrawablePool<PanelGroup>(100);
         private readonly DrawablePool<PanelGroupStarDifficulty> starsGroupPanelPool = new DrawablePool<PanelGroupStarDifficulty>(11);
-        private readonly DrawablePool<PanelGroupRankDisplay> ranksGroupPanelPool = new DrawablePool<PanelGroupRankDisplay>(11);
+        private readonly DrawablePool<PanelGroupRankDisplay> ranksGroupPanelPool = new DrawablePool<PanelGroupRankDisplay>(9);
 
         private void setupPools()
         {
