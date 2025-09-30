ppy/osu

commit dfed564bda7408ec4fd1c82ab15f90f410cdd444

Allow `BeatmapCarousel.CurrentSelection` to accept raw `BeatmapInfo`s (and redirect to appropriate grouped beatmap)
This is probably where things get a little controversial.

There are some song select flows wherein song select just wants to
ensure sanity by authoritatively setting the global beatmap. The goal is
to change the beatmap immediately and instantly. Therefore it should
kind of be the carousel's job to figure out its grouping complications.

To that end, `CurrentSelection` is made virtual, and overridden in
`BeatmapCarousel` to perform a sort of reconciliation logic. If an
external component sets `CurrentSelection` to a `BeatmapInfo`, one of
the two following things happen:

- Nothing, if the current `GroupedBeatmap` is already a copy of the
  beatmap that needs to be selected, or

- The carousel looks at its items, finds any first copy which matches
  the beatmap that the external consumer wanted selected, and changes
  selection to that instead.

osu.Game/Screens/SelectV2/SongSelect.cs
@@ -535,8 +535,7 @@ namespace osu.Game.Screens.SelectV2
 
                 if (validBeatmaps.Any())
                 {
-                    // TODO: this needs a primitive that tells the carousel "I need this beatmap to be selected, you figure out the grouping".
-                    //requestRecommendedSelection(validBeatmaps);
+                    carousel.CurrentSelection = difficultyRecommender?.GetRecommendedBeatmap(validBeatmaps) ?? validBeatmaps.First();
                     return true;
                 }
             }

osu.Game/Graphics/Carousel/Carousel.cs
@@ -107,7 +107,7 @@ namespace osu.Game.Graphics.Carousel
         /// The selection is never reset due to not existing. It can be set to anything.
         /// If no matching carousel item exists, there will be no visually selected item while waiting for potential new item which matches.
         /// </remarks>
-        public object? CurrentSelection
+        public virtual object? CurrentSelection
         {
             get => currentSelection.Model;
             set

osu.Game/Screens/SelectV2/BeatmapCarousel.cs
@@ -295,6 +295,26 @@ namespace osu.Game.Screens.SelectV2
         protected override bool ShouldActivateOnKeyboardSelection(CarouselItem item) =>
             grouping.BeatmapSetsGroupedTogether && item.Model is GroupedBeatmap;
 
+        public override object? CurrentSelection
+        {
+            get => base.CurrentSelection;
+            set
+            {
+                // this is a special pathway for external consumers who only care about showing some particular copy of a beatmap
+                // (there could be multiple panels for one beatmap due to grouping).
+                // in this pathway we basically figure out what group to use internally, and continue working with `GroupedBeatmap` all the way after that.
+                if (value is BeatmapInfo beatmapInfo)
+                {
+                    if (CurrentSelection is GroupedBeatmap groupedBeatmap && beatmapInfo.Equals(groupedBeatmap.Beatmap))
+                        return;
+
+                    value = GetCarouselItems()?.Select(item => item.Model).OfType<GroupedBeatmap>().FirstOrDefault(gb => gb.Beatmap.Equals(beatmapInfo));
+                }
+
+                base.CurrentSelection = value;
+            }
+        }
+
         protected override void HandleItemActivated(CarouselItem item)
         {
             try

commit f66c8d10e0dd5606880243811d91c9ff57acdcbf

Fix song select not changing global beatmap correctly when switching rulesets
Closes https://github.com/ppy/osu/issues/35113.

Regressed in dfed564bda7408ec4fd1c82ab15f90f410cdd444 - setting
`Carousel.CurrentSelection` was not all that
`requestRecommendedSelection()` was doing there...

A potential point of discussion is whether this global beatmap switch
should be debounced or instant. I'm not sure I have a particularly
well-formed opinion on that. One argument in favour of not debouncing is
that if you look closely at the left side of the screen while the
debounce is in progress, you can still sort of see the broken behaviour
happen - it just doesn't stay there forever.

Thankfully `ensureGlobalBeatmapValid()` being called in every scenario
on screen suspension prevented this bug from being any worse than it is
right now.

osu.Game/Screens/SelectV2/SongSelect.cs
@@ -600,7 +600,9 @@ namespace osu.Game.Screens.SelectV2
 
                 if (validBeatmaps.Any())
                 {
-                    carousel.CurrentBeatmap = difficultyRecommender?.GetRecommendedBeatmap(validBeatmaps) ?? validBeatmaps.First();
+                    var beatmap = difficultyRecommender?.GetRecommendedBeatmap(validBeatmaps) ?? validBeatmaps.First();
+                    carousel.CurrentBeatmap = beatmap;
+                    debounceQueueSelection(beatmap);
                     return true;
                 }
             }