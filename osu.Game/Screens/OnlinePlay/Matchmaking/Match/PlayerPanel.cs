af839823126732e26eece8c94caa7851cd622e4c
Jamie Taylor <me@nekodex.net>
2025-10-24 22:42:28 +0900

    Add SFX for 'jumping' in quickplay

--- osu.Game/Screens/OnlinePlay/Matchmaking/Match/PlayerPanel.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/Match/PlayerPanel.cs
@@ -7,6 +7,8 @@ using System.Globalization;
 using System.Linq;
 using Humanizer;
 using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -15,6 +17,7 @@ using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
 using osu.Framework.Screens;
+using osu.Framework.Utils;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
@@ -112,7 +115,35 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
         private Box downloadProgressBar = null!;
 
         private PlayerPanelDisplayMode displayMode = PlayerPanelDisplayMode.Horizontal;
-        private bool hasQuit;
+
+        private Sample? jumpSample;
+        private SampleChannel? jumpSampleChannel;
+        private double samplePitch;
+
+        public PlayerPanelDisplayMode DisplayMode
+        {
+            get => displayMode;
+            set
+            {
+                displayMode = value;
+                if (IsLoaded)
+                    updateLayout(false);
+            }
+        }
+
+        public readonly APIUser User;
+
+        /// <summary>
+        /// Perform an action in addition to showing the user's profile.
+        /// This should be used to perform auxiliary tasks and not as a primary action for clicking a user panel (to maintain a consistent UX).
+        /// </summary>
+        public new Action? Action;
+
+        protected Action ViewProfile { get; private set; } = null!;
+
+        public Box SolidBackgroundLayer { get; private set; } = null!;
+
+        protected Drawable? Background { get; private set; }
 
         public PlayerPanel(MultiplayerRoomUser user)
             : base(HoverSampleSet.Button)
@@ -130,7 +161,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(AudioManager audio)
         {
             Content.Masking = true;
             Content.CornerRadius = 10;
@@ -255,6 +286,8 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
 
             // Allow avatar to exist outside of masking for when it jumps around and stuff.
             AddInternal(avatar.CreateProxy());
+
+            jumpSample = audio.Samples.Get(@"Multiplayer/Matchmaking/player-jump");
         }
 
         protected override void LoadComplete()
@@ -272,6 +305,9 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
             avatar.ScaleTo(0)
                   .ScaleTo(1, 500, Easing.OutElasticHalf)
                   .FadeIn(200);
+
+            // pick a random pitch to be used by the player for duration of this session
+            samplePitch = 0.75f + RNG.NextDouble(0f, 0.5f);
         }
 
         public PlayerPanelDisplayMode DisplayMode
@@ -481,6 +517,11 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
                             scale.Then().ScaleTo(new Vector2(1, 1.05f), 200, Easing.Out)
                                  .Then().ScaleTo(new Vector2(1, 0.95f), 200, Easing.In)
                                  .Then().ScaleTo(Vector2.One, 800, Easing.OutElastic);
+
+                            // only play jump sample if panel is visible
+                            if (Alpha > 0)
+                                playJumpSample();
+
                             break;
                     }
 
@@ -488,15 +529,22 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
             }
         }
 
-        private void onBeatmapAvailabilityChanged(MultiplayerRoomUser user, BeatmapAvailability availability) => Scheduler.Add(() =>
+        private void playJumpSample()
         {
-            if (availability.State == DownloadState.Downloading)
-                downloadProgressBar.FadeIn(200, Easing.OutPow10);
-            else
-                downloadProgressBar.FadeOut(200, Easing.OutPow10);
+            jumpSampleChannel?.Stop();
+            jumpSampleChannel = jumpSample?.GetChannel();
 
-            downloadProgressBar.ResizeWidthTo(availability.DownloadProgress ?? 0, 200, Easing.OutPow10);
-        });
+            if (jumpSampleChannel == null)
+                return;
+
+            float horizontalPos = BoundingBox.Centre.X / Parent!.ToLocalSpace(Parent!.ScreenSpaceDrawQuad).Width;
+            // rescale balance from 0..1 to -1..1
+            float balance = -1f + horizontalPos * 2f;
+
+            jumpSampleChannel.Frequency.Value = samplePitch;
+            jumpSampleChannel.Balance.Value = balance * OsuGameBase.SFX_STEREO_STRENGTH;
+            jumpSampleChannel?.Play();
+        }
 
         protected override void Dispose(bool isDisposing)
         {
