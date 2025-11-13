ff65941b1a80cd4265b1bca92665b4d30d3a188c
marvin <minetoblend@gmail.com>
2025-11-13 18:16:34 +0100

    Use dice icon for MatchmakingSelectPanelRandom

--- osu.Game/Screens/OnlinePlay/Matchmaking/Match/BeatmapSelect/MatchmakingSelectPanelRandom.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/Match/BeatmapSelect/MatchmakingSelectPanelRandom.cs
@@ -6,6 +6,8 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Utils;
+using osu.Game.Graphics.Backgrounds;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Overlays;
 using osuTK;
@@ -23,6 +25,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match.BeatmapSelect
 
         private Container? randomPanelContent;
         private BeatmapCardMatchmakingBeatmapContent? beatmapPanelContent;
+        private SpriteIcon dice = null!;
 
         [BackgroundDependencyLoader]
         private void load(OverlayColourProvider colourProvider)
@@ -35,33 +38,33 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match.BeatmapSelect
                     new Box
                     {
                         RelativeSizeAxes = Axes.Both,
-                        Colour = colourProvider.Background2,
+                        Colour = colourProvider.Dark5,
                     },
-                    new FillFlowContainer
+                    new TrianglesV2
                     {
+                        RelativeSizeAxes = Axes.Both,
+                        Alpha = 0.1f,
+                    },
+                    new OsuSpriteText
+                    {
+                        Y = 20,
                         Anchor = Anchor.Centre,
                         Origin = Anchor.Centre,
-                        AutoSizeAxes = Axes.Both,
-                        Direction = FillDirection.Vertical,
-                        Children =
-                        [
-                            new SpriteIcon
-                            {
-                                Anchor = Anchor.TopCentre,
-                                Origin = Anchor.TopCentre,
-                                Size = new Vector2(32),
-                                Icon = FontAwesome.Solid.Random,
-                            },
-                            new OsuSpriteText
-                            {
-                                Anchor = Anchor.TopCentre,
-                                Origin = Anchor.TopCentre,
-                                Text = "Random",
-                            }
-                        ]
+                        Text = "Random"
                     },
                 }
             });
+
+            ScaleContainer.Add(dice = new SpriteIcon
+            {
+                Y = -10,
+                Size = new Vector2(28),
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Icon = randomDiceIcon(),
+            });
+
+            dice.Spin(10_000, RotationDirection.Clockwise);
         }
 
         public override void PresentAsChosenBeatmap(MatchmakingPlaylistItemBeatmap item)
@@ -72,6 +75,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match.BeatmapSelect
                 .ScaleTo(1.5f, 1000, Easing.OutExpo);
 
             randomPanelContent?.Expire();
+            dice.FadeOut().Expire();
 
             var flashLayer = new Box { RelativeSizeAxes = Axes.Both };
 
@@ -85,5 +89,17 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match.BeatmapSelect
         }
 
         protected override float AvatarOverlayOffset => base.AvatarOverlayOffset + (beatmapPanelContent?.AvatarOffset ?? 0);
+
+        private static IconUsage[] diceIcons => new[]
+        {
+            FontAwesome.Solid.DiceOne,
+            FontAwesome.Solid.DiceTwo,
+            FontAwesome.Solid.DiceThree,
+            FontAwesome.Solid.DiceFour,
+            FontAwesome.Solid.DiceFive,
+            FontAwesome.Solid.DiceSix,
+        };
+
+        private static IconUsage randomDiceIcon() => diceIcons[RNG.Next(diceIcons.Length)];
     }
 }
