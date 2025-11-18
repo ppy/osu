50e5178ffa93fb91c4c5e4798104d19fe5aef5ad
Givikap120 <givikap120@gmail.com>
2025-11-18 03:35:29 +0100

    Change the formula

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -341,7 +341,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             {
                 // If sliders in the map are hard - it's likely for player to drop sliderends
                 // If map has easy sliders - it's more likely for player to sliderbreak
-                double likelyMissedSliderendPortion = 0.02 + 0.18 * (1 - Math.Pow(attributes.SliderFactor, 5));
+                double likelyMissedSliderendPortion = 0.05 + 0.15 * Math.Pow(Math.Min(attributes.AimTopWeightedSliderFactor, 1), 2);
 
                 // Consider that full combo is maximum combo minus dropped slider tails since they don't contribute to combo but also don't break it
                 // In classic scores we can't know the amount of dropped sliders so we estimate it
