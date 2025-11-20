78308b2f902324ebe1e235b34a6ab63c34584035
smallketchup82 <ketchup@smallketchup.ca>
2025-11-20 14:48:38 -0500

    Add documentation

--- osu.Desktop/LowLatency/NVAPILowLatencyProvider.cs
+++ osu.Desktop/LowLatency/NVAPILowLatencyProvider.cs
@@ -21,6 +21,11 @@ namespace osu.Desktop.LowLatency
 
         private IntPtr _deviceHandle;
 
+        /// <summary>
+        /// Initialize the NVAPI low latency provider with a native device handle. Ensure NVAPI is available before calling this method.
+        /// </summary>
+        /// <param name="nativeDeviceHandle">An <see cref="IntPtr"/> to the handle of the D3D11 device.</param>
+        /// <exception cref="InvalidOperationException">Throws an exception if NVAPI is unavailable, or the device handle provided was invalid.</exception>
         public void Initialize(IntPtr nativeDeviceHandle)
         {
             _deviceHandle = nativeDeviceHandle;
@@ -30,6 +35,11 @@ namespace osu.Desktop.LowLatency
                 throw new InvalidOperationException("NVAPI is not available or the provided device handle is invalid.");
         }
 
+        /// <summary>
+        /// Set the low latency mode.
+        /// </summary>
+        /// <param name="mode">The <see cref="LatencyMode"/> to use.</param>
+        /// <exception cref="InvalidOperationException">Throws an exception if an attempt to set the low latency mode was unsuccessful.</exception>
         public void SetMode(LatencyMode mode)
         {
             if (!IsAvailable || _deviceHandle == IntPtr.Zero)
@@ -43,6 +53,13 @@ namespace osu.Desktop.LowLatency
                 throw new InvalidOperationException($"Failed to set NVAPI low latency (Sleep) mode: {status}");
         }
 
+        /// <summary>
+        /// Set a latency marker for the current frame.
+        /// </summary>
+        /// <remarks>WARNING: Do not log any errors that come from this method, they should be ignored as this method runs in a realtime environment.</remarks>
+        /// <param name="marker">The <see cref="LatencyMarker"/> to set.</param>
+        /// <param name="frameId">The frame number this marker is for.</param>
+        /// <exception cref="InvalidOperationException">Throws an exception if the attempt to set the marker was unsuccessful. Please ensure this exception is ignored.</exception>
         public void SetMarker(LatencyMarker marker, ulong frameId)
         {
             if (!IsAvailable || _deviceHandle == IntPtr.Zero)
@@ -54,6 +71,10 @@ namespace osu.Desktop.LowLatency
                 throw new InvalidOperationException($"Failed to set NVAPI latency marker: {status}");
         }
 
+        /// <summary>
+        /// Ensure this is called once per frame, at the start of the Update phase, to allow NVAPI to manage frame sleep timing.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Throws an exception if the Sleep attempt was unsuccessful.</exception>
         public void FrameSleep()
         {
             if (!IsAvailable || _deviceHandle == IntPtr.Zero)
