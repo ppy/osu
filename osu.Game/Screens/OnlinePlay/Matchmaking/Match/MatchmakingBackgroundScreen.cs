9414997f33bb2b440fa1eb1b38da7570481b61eb
marvin <minetoblend@gmail.com>
2025-11-21 01:32:11 +0100

    Use single drawable for background

--- osu.Game/Screens/OnlinePlay/Matchmaking/Match/MatchmakingBackgroundScreen.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/Match/MatchmakingBackgroundScreen.cs
@@ -4,11 +4,9 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Game.Overlays;
-using osuTK.Graphics;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
 {
@@ -27,29 +25,15 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.Match
             [BackgroundDependencyLoader]
             private void load(TextureStore textures, OverlayColourProvider colourProvider)
             {
-                AddRangeInternal(new Drawable[]
+                InternalChild = new Sprite
                 {
-                    new Sprite
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Texture = textures.Get("Backgrounds/bg1"),
-                        Anchor = Anchor.Centre,
-                        Origin = Anchor.Centre,
-                        FillMode = FillMode.Fill,
-                    },
-                    new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = colourProvider.Colour4,
-                        Alpha = 0.5f,
-                    },
-                    new Box
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Colour = Color4.Black,
-                        Alpha = 0.6f,
-                    }
-                });
+                    RelativeSizeAxes = Axes.Both,
+                    Texture = textures.Get("Backgrounds/bg1"),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    FillMode = FillMode.Fill,
+                    Colour = colourProvider.Dark2
+                };
             }
         }
     }
