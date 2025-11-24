64668eafb96c08dcfad1f02e50053b688613eb6a
Dean Herbert <pe@ppy.sh>
2025-11-25 00:55:53 +0900

    Adjust some more visual metrics to feel better

--- osu.Game/Graphics/UserInterfaceV2/RoundedButton.cs
+++ osu.Game/Graphics/UserInterfaceV2/RoundedButton.cs
@@ -52,6 +52,7 @@ namespace osu.Game.Graphics.UserInterfaceV2
             // This doesn't match the latest design spec (should be 5) but is an in-between that feels right to the eye
             // until we move everything over to Form controls.
             Content.CornerRadius = 10;
+            Content.CornerExponent = 2.5f;
 
             Add(Triangles = new TrianglesV2
             {
