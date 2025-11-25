66a1ac3324c1ef20f32be4b592351e360b40957e
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-11-25 07:46:29 +0100

    Fix test not compiling

--- osu.Game.Tests/Visual/Settings/TestSceneKeyBindingPanel.cs
+++ osu.Game.Tests/Visual/Settings/TestSceneKeyBindingPanel.cs
@@ -121,7 +121,7 @@ namespace osu.Game.Tests.Visual.Settings
 
             AddStep("schedule button clicks", () =>
             {
-                var clearButton = firstRow.ChildrenOfType<KeyBindingRow.ClearButton>().Single();
+                var clearButton = firstRow.ChildrenOfType<DangerousRoundedButton>().Single();
 
                 InputManager.MoveMouseTo(clearButton);
 
@@ -179,7 +179,7 @@ namespace osu.Game.Tests.Visual.Settings
             {
                 AddStep("click clear button", () =>
                 {
-                    var clearButton = multiBindingRow.ChildrenOfType<KeyBindingRow.ClearButton>().Single();
+                    var clearButton = multiBindingRow.ChildrenOfType<DangerousRoundedButton>().Single();
 
                     InputManager.MoveMouseTo(clearButton);
                     InputManager.Click(MouseButton.Left);
@@ -386,7 +386,7 @@ namespace osu.Game.Tests.Visual.Settings
             AddStep("clear binding", () =>
             {
                 var row = panel.ChildrenOfType<KeyBindingRow>().First(r => r.ChildrenOfType<OsuSpriteText>().Any(s => s.Text.ToString() == "Left (centre)"));
-                row.ChildrenOfType<KeyBindingRow.ClearButton>().Single().TriggerClick();
+                row.ChildrenOfType<DangerousRoundedButton>().Single().TriggerClick();
             });
             scrollToAndStartBinding("Left (rim)");
             AddStep("bind M1", () => InputManager.Click(MouseButton.Left));
@@ -462,7 +462,7 @@ namespace osu.Game.Tests.Visual.Settings
             AddStep("clear binding", () =>
             {
                 var row = panel.ChildrenOfType<KeyBindingRow>().First(r => r.ChildrenOfType<OsuSpriteText>().Any(s => s.Text.ToString() == "Left (centre)"));
-                row.ChildrenOfType<KeyBindingRow.ClearButton>().Single().TriggerClick();
+                row.ChildrenOfType<DangerousRoundedButton>().Single().TriggerClick();
             });
         }
 
