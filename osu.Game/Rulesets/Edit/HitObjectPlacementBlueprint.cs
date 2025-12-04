12170df80add87e8f292545f67566f8d25465efc
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-12-04 14:15:52 +0100

    Disallow placing hit objects before first timing point
    Because they can break stable. See
    https://github.com/ppy/osu/issues/31591#issuecomment-3575270120 for
    detailed rationale.

--- osu.Game/Rulesets/Edit/HitObjectPlacementBlueprint.cs
+++ osu.Game/Rulesets/Edit/HitObjectPlacementBlueprint.cs
@@ -47,6 +47,8 @@ namespace osu.Game.Rulesets.Edit
 
         private HitObject? getPreviousHitObject() => beatmap.HitObjects.TakeWhile(h => h.StartTime <= startTimeBindable.Value).LastOrDefault();
 
+        protected override bool IsValidForPlacement => HitObject.StartTime >= beatmap.ControlPointInfo.TimingPoints[0].Time;
+
         [Resolved]
         private IPlacementHandler placementHandler { get; set; } = null!;
 
