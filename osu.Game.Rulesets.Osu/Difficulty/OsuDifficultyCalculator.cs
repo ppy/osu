4730653a3ba96451e8bdf5c76c620accbf03eea1
StanR <hi@stanr.info>
2025-12-17 01:41:19 +0500

    Simplify star rating calculations

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -21,8 +21,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 {
     public class OsuDifficultyCalculator : DifficultyCalculator
     {
-        private const double star_rating_multiplier = 0.0265;
-
         public override int Version => 20250306;
 
         public OsuDifficultyCalculator(IRulesetInfo ruleset, IWorkingBeatmap beatmap)
@@ -157,7 +155,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             if (basePerformance <= 0.00001)
                 return 0;
 
-            return Math.Cbrt(OsuPerformanceCalculator.PERFORMANCE_BASE_MULTIPLIER) * star_rating_multiplier * (Math.Cbrt(100000 / Math.Pow(2, 1 / 1.1) * basePerformance) + 4);
+            return Math.Cbrt(OsuPerformanceCalculator.PERFORMANCE_BASE_MULTIPLIER) * Math.Cbrt(basePerformance);
         }
 
         protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, double clockRate)
