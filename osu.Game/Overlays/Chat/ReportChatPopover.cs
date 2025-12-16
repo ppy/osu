d0c45d418c546e684c955831ec63ff4d7399be7f
Bartłomiej Dach <87902114+bubb13gummy@users.noreply.github.com>
2025-12-16 11:16:13 +0100

    Adjust message on successful report to match bancho
    Addresses https://github.com/ppy/osu/discussions/36004.
    
    Not adding localisation because the previous implementation was
    `.ToString()`ing anyway.
    
    Would have made the abuse e-mail a link but `mailto:` doesn't work with
    `MessageFormatter` and I don't want to go into that right now.
    
    The message *almost* matches stable. The "almost" is because it doesn't
    mention the `/ignore` chat command. I was just going to implement the
    command, but I went to check what it does, and backed away slowly
    because it has like weird scoping to chat, highlights, and PMs, so
    `nope.avi`.

--- osu.Game/Overlays/Chat/ReportChatPopover.cs
+++ osu.Game/Overlays/Chat/ReportChatPopover.cs
@@ -33,7 +33,27 @@ namespace osu.Game.Overlays.Chat
         {
             var request = new ChatReportRequest(message.Id, reason, comments);
 
-            request.Success += () => channelManager.CurrentChannel.Value.AddNewMessages(new InfoMessage(UsersStrings.ReportThanks.ToString()));
+            request.Success += () =>
+            {
+                string thanksMessage;
+
+                switch (channelManager.CurrentChannel.Value.Type)
+                {
+                    case ChannelType.PM:
+                        thanksMessage = """
+                                  Chat moderators have been alerted. You have reported a private message so they will not be able to read history to maintain your privacy. Please make sure to include as much details as you can.
+                                  You can submit a second report with more details if required, or contact abuse@ppy.sh if a user is being extremely offensive.
+                                  You can also block a user via the block button on their user profile, or by right-clicking on their name in the chat and selecting "Block".
+                                  """;
+                        break;
+
+                    default:
+                        thanksMessage = @"Chat moderators have been alerted. Thanks for your help.";
+                        break;
+                }
+
+                channelManager.CurrentChannel.Value.AddNewMessages(new InfoMessage(thanksMessage));
+            };
 
             api.Queue(request);
         }
