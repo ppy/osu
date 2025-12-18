d5335e7082bef3b97559d45fc2eef6b1f9d3a635
Kian Masri <kianmasri@gmail.com>
2025-12-18 14:24:57 -0500

    fix timeline zoom

--- osu.Game/Overlays/Volume/GlobalAltScrollAdjustsVolume.cs
+++ osu.Game/Overlays/Volume/GlobalAltScrollAdjustsVolume.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using osu.Game.Screens.Edit.Compose.Components.Timeline;
 using osu.Framework.Input.Events;
 
 namespace osu.Game.Overlays.Volume
@@ -13,6 +15,10 @@ namespace osu.Game.Overlays.Volume
         protected override bool OnScroll(ScrollEvent e)
         {
             if (!e.AltPressed || e.ControlPressed || e.ShiftPressed)
+                return false;
+
+            var hoveredDrawables = GetContainingInputManager()?.HoveredDrawables;
+            if (hoveredDrawables?.Any(d => d is ZoomableScrollContainer) == true)
                 return false;
 
             return base.OnScroll(e);
