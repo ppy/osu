a3a5b2ca92a9335a7233b89abc2801576bf7f069
Salman Alshamrani <frenzibyte@gmail.com>
2025-12-23 05:58:57 -0500

    Fix tests not having colour providers

--- osu.Game.Tests/Visual/UserInterface/TestSceneSwitchButton.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneSwitchButton.cs
@@ -4,16 +4,21 @@
 #nullable disable
 
 using NUnit.Framework;
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
 using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Overlays;
 using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.UserInterface
 {
     public partial class TestSceneSwitchButton : OsuManualInputManagerTestScene
     {
+        [Cached]
+        private readonly OverlayColourProvider colourProvider = new OverlayColourProvider(OverlayColourScheme.Pink);
+
         private SwitchButton switchButton;
 
         [SetUp]
