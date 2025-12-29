13b730bee3e816a074be8ce11f62ea3fd962d97e
Givikap120 <givikap120@gmail.com>
2025-12-29 18:39:50 +0100

    refactor window ratio handling

--- osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
@@ -207,15 +207,12 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
             double distanceFactor = DiffUtils.Pow(DiffUtils.ReverseLerp(LazyJumpDistance, NORMALISED_DIAMETER, NORMALISED_RADIUS), 2);
 
             // Use HitWindowGreat * 2, because even if you can't get 300 with doubletapping - you still can gallop
-            const double power = 2;
-            double windowRatio = Math.Pow(Math.Min(1, currDeltaTime / (HitWindowGreat * 2)), power);
+            double windowRatio = Math.Min(1, currDeltaTime / (HitWindowGreat * 2));
 
             // Nerf even more if you don't need to gallop anymore
-            double halfPoint = Math.Pow(0.5, power);
-            if (windowRatio < halfPoint)
-                windowRatio *= windowRatio / halfPoint;
+            windowRatio *= Math.Min(windowRatio, 0.5) * 2;
 
-            return 1 - Math.Pow(speedRatio, distanceFactor * (1 - windowRatio));
+            return 1 - Math.Pow(speedRatio, distanceFactor * (1 - Math.Pow(windowRatio, 2)));
         }
 
         private void setDistances(double clockRate)
