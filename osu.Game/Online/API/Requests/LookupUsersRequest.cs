41432194e835a65b843d8ecbdaaf440abc639c34
Dean Herbert <pe@ppy.sh>
2026-01-06 19:28:33 +0900

    Add support for global rank parsing in /users/ batch lookups
    See https://github.com/ppy/osu-web/pull/12651 for web-side
    implementation.
    To be used to fix
    https://github.com/ppy/osu/pull/33649/changes#diff-32784a778b34c671e1f72c00e1f4161a5e774e849aae5631ee71b31fc32e5d42R218
    (have tested this works there).
    
    Use simple set-get rather than transforming to `APIUser.Statistics`

--- osu.Game/Online/API/Requests/LookupUsersRequest.cs
+++ osu.Game/Online/API/Requests/LookupUsersRequest.cs
@@ -10,21 +10,26 @@ namespace osu.Game.Online.API.Requests
     /// Looks up users with the given <see cref="UserIds"/>.
     /// In comparison to <see cref="GetUsersRequest"/>, the response here does not contain <see cref="APIUser.RulesetsStatistics"/>,
     /// but in exchange is subject to less stringent rate limiting, making it suitable for mass user listings.
+    ///
+    /// Providing a ruleset ID will give `global_rank`s in the response.
     /// </summary>
     public class LookupUsersRequest : APIRequest<GetUsersResponse>
     {
         public readonly int[] UserIds;
 
+        public readonly int? RulesetId;
+
         private const int max_ids_per_request = 50;
 
-        public LookupUsersRequest(int[] userIds)
+        public LookupUsersRequest(int[] userIds, int? rulesetId = null)
         {
             if (userIds.Length > max_ids_per_request)
                 throw new ArgumentException($"{nameof(LookupUsersRequest)} calls only support up to {max_ids_per_request} IDs at once");
 
             UserIds = userIds;
+            RulesetId = rulesetId;
         }
 
-        protected override string Target => @"users/lookup/?ids[]=" + string.Join(@"&ids[]=", UserIds);
+        protected override string Target => @"users/lookup/?ids[]=" + string.Join(@"&ids[]=", UserIds) + (RulesetId != null ? "&ruleset_id=" + RulesetId : "");
     }
 }
