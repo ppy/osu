ppy/osu

commit 2b90cbf59f32c9cf98933a895890c3af1e9b05ca

Attempt to fix display issues at high UI scales

osu.Game/Screens/Play/PlayerLoader.cs
@@ -31,7 +31,9 @@ using osu.Game.Overlays;
 using osu.Game.Overlays.Notifications;
 using osu.Game.Overlays.Volume;
 using osu.Game.Performance;
+using osu.Game.Screens.Footer;
 using osu.Game.Screens.Menu;
+using osu.Game.Screens.Play.HUD;
 using osu.Game.Screens.Play.PlayerSettings;
 using osu.Game.Screens.Select.Leaderboards;
 using osu.Game.Skinning;
@@ -72,7 +74,7 @@ namespace osu.Game.Screens.Play
         /// <summary>
         /// A fill flow containing the player settings groups, exposed for the ability to hide it from inheritors of the player loader.
         /// </summary>
-        protected FillFlowContainer PlayerSettings { get; private set; } = null!;
+        protected FillFlowContainer<PlayerSettingsGroup> PlayerSettings { get; private set; } = null!;
 
         protected VisualSettings VisualSettings { get; private set; } = null!;
 
@@ -134,6 +136,8 @@ namespace osu.Game.Screens.Play
             && inputManager.FocusedDrawable is not OsuFocusedOverlayContainer
             // or if a child of a focused overlay is focused, like settings' search textbox.
             && inputManager.FocusedDrawable?.FindClosestParent<OsuFocusedOverlayContainer>() == null;
+
+        private bool holdForMenuExitButton => !AllowUserExit;
 
         private readonly Func<Player> createPlayer;
 
@@ -225,27 +229,69 @@ namespace osu.Game.Screens.Play
                     Padding = new MarginPadding(padding),
                     Spacing = new Vector2(20),
                 },
-                settingsScroll = new OsuScrollContainer
+                new GridContainer
                 {
                     Anchor = Anchor.TopRight,
                     Origin = Anchor.TopRight,
                     RelativeSizeAxes = Axes.Y,
                     Width = SettingsToolboxGroup.CONTAINER_WIDTH + padding * 2,
-                    Padding = new MarginPadding { Vertical = padding },
-                    Masking = false,
-                    Child = PlayerSettings = new FillFlowContainer
+                    Padding = new MarginPadding
                     {
-                        AutoSizeAxes = Axes.Both,
-                        Direction = FillDirection.Vertical,
-                        Spacing = new Vector2(0, 20),
-                        Padding = new MarginPadding { Horizontal = padding },
-                        Children = new PlayerSettingsGroup[]
-                        {
-                            VisualSettings = new VisualSettings(),
-                            AudioSettings = new AudioSettings(),
-                            new InputSettings()
-                        }
+                        Top = padding,
+                        Bottom = ScreenFooter.HEIGHT + padding
                     },
+                    RowDimensions =
+                    [
+                        new Dimension(),
+                        new Dimension(GridSizeMode.AutoSize)
+                    ],
+                    Content = new[]
+                    {
+                        new Drawable[]
+                        {
+                            settingsScroll = new OsuScrollContainer
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                Masking = holdForMenuExitButton,
+                                Child = PlayerSettings = new FillFlowContainer<PlayerSettingsGroup>
+                                {
+                                    AutoSizeAxes = Axes.Both,
+                                    Direction = FillDirection.Vertical,
+                                    Spacing = new Vector2(0, 20),
+                                    Padding = new MarginPadding { Horizontal = padding },
+                                    Children = new PlayerSettingsGroup[]
+                                    {
+                                        VisualSettings = new VisualSettings(),
+                                        AudioSettings = new AudioSettings(),
+                                        new InputSettings()
+                                    }
+                                },
+                            }
+                        },
+                        new Drawable[]
+                        {
+                            new Container
+                            {
+                                Anchor = Anchor.TopRight,
+                                Origin = Anchor.TopRight,
+                                Alpha = holdForMenuExitButton ? 1 : 0,
+                                AutoSizeAxes = Axes.Both,
+                                Child = new HoldForMenuButton(true)
+                                {
+                                    Margin = new MarginPadding
+                                    {
+                                        Top = 20,
+                                        Horizontal = padding,
+                                    },
+                                    Action = () =>
+                                    {
+                                        if (this.IsCurrentScreen())
+                                            this.Exit();
+                                    }
+                                }
+                            }
+                        }
+                    }
                 },
                 idleTracker = new IdleTracker(1500),
                 sampleRestart = new SkinnableSound(new SampleInfo(@"Gameplay/restart", @"pause-retry-click"))

osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerPlayerLoader.cs
@@ -4,13 +4,10 @@
 using System;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Logging;
 using osu.Framework.Screens;
 using osu.Game.Online.Multiplayer;
 using osu.Game.Screens.Play;
-using osu.Game.Screens.Play.HUD;
 
 namespace osu.Game.Screens.OnlinePlay.Multiplayer
 {
@@ -31,27 +28,6 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
         public MultiplayerPlayerLoader(Func<Player> createPlayer)
             : base(createPlayer)
         {
-        }
-
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            PlayerSettings.Add(new Container
-            {
-                RelativeSizeAxes = Axes.X,
-                AutoSizeAxes = Axes.Y,
-                Child = new HoldForMenuButton(true)
-                {
-                    Anchor = Anchor.CentreRight,
-                    Origin = Anchor.CentreRight,
-                    Margin = new MarginPadding(10),
-                    Action = () =>
-                    {
-                        if (this.IsCurrentScreen())
-                            this.Exit();
-                    }
-                }
-            });
         }
 
         protected override bool ReadyForGameplay =>

commit 1a2124e373fc33e4e1e3ef547e4cf51eaafd53e6

Fix broken transition of player loader right side content
regressed in 2b90cbf59f32c9cf98933a895890c3af1e9b05ca

osu.Game/Screens/Play/PlayerLoader.cs
@@ -85,7 +85,7 @@ namespace osu.Game.Screens.Play
         protected Task? DisposalTask { get; private set; }
 
         private FillFlowContainer disclaimers = null!;
-        private OsuScrollContainer settingsScroll = null!;
+        private GridContainer sideContent = null!;
 
         private Bindable<bool> showStoryboards = null!;
 
@@ -229,7 +229,7 @@ namespace osu.Game.Screens.Play
                     Padding = new MarginPadding(padding),
                     Spacing = new Vector2(20),
                 },
-                new GridContainer
+                sideContent = new GridContainer
                 {
                     Anchor = Anchor.TopRight,
                     Origin = Anchor.TopRight,
@@ -248,7 +248,7 @@ namespace osu.Game.Screens.Play
                     {
                         new Drawable[]
                         {
-                            settingsScroll = new OsuScrollContainer
+                            new OsuScrollContainer
                             {
                                 RelativeSizeAxes = Axes.Both,
                                 Child = PlayerSettings = new FillFlowContainer<PlayerSettingsGroup>
@@ -354,7 +354,7 @@ namespace osu.Game.Screens.Play
 
             // Start side content off-screen.
             disclaimers.MoveToX(-disclaimers.DrawWidth);
-            settingsScroll.MoveToX(settingsScroll.DrawWidth);
+            sideContent.MoveToX(sideContent.DrawWidth);
 
             content.ScaleTo(0.7f);
 
@@ -600,8 +600,8 @@ namespace osu.Game.Screens.Play
 
                 using (BeginDelayedSequence(delayBeforeSideDisplays))
                 {
-                    settingsScroll.FadeInFromZero(500, Easing.Out)
-                                  .MoveToX(0, 500, Easing.OutQuint);
+                    sideContent.FadeInFromZero(500, Easing.Out)
+                               .MoveToX(0, 500, Easing.OutQuint);
 
                     disclaimers.FadeInFromZero(500, Easing.Out)
                                .MoveToX(0, 500, Easing.OutQuint);
@@ -641,8 +641,8 @@ namespace osu.Game.Screens.Play
 
             disclaimers.FadeOut(CONTENT_OUT_DURATION, Easing.Out)
                        .MoveToX(-disclaimers.DrawWidth, CONTENT_OUT_DURATION * 2, Easing.OutQuint);
-            settingsScroll.FadeOut(CONTENT_OUT_DURATION, Easing.OutQuint)
-                          .MoveToX(settingsScroll.DrawWidth, CONTENT_OUT_DURATION * 2, Easing.OutQuint);
+            sideContent.FadeOut(CONTENT_OUT_DURATION, Easing.OutQuint)
+                       .MoveToX(sideContent.DrawWidth, CONTENT_OUT_DURATION * 2, Easing.OutQuint);
 
             lowPassFilter?.CutoffTo(AudioFilter.MAX_LOWPASS_CUTOFF, CONTENT_OUT_DURATION);
             highPassFilter?.CutoffTo(0, CONTENT_OUT_DURATION);