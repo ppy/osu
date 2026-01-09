5be484c77d3deb2877e72af9b2ce7dbcdbab5b0e
Dean Herbert <pe@ppy.sh>
2026-01-09 18:00:12 +0900

    Simplify logic

--- osu.Game/Screens/SelectV2/SongSelect.cs
+++ osu.Game/Screens/SelectV2/SongSelect.cs
@@ -115,6 +115,8 @@ namespace osu.Game.Screens.SelectV2
         private Container mainContent = null!;
         private SkinnableContainer skinnableContent = null!;
 
+        private GridContainer mainGridContainer = null!;
+
         private NoResultsPlaceholder noResultsPlaceholder = null!;
 
         public override bool? ApplyModTrackAdjustments => true;
@@ -824,7 +826,7 @@ namespace osu.Game.Screens.SelectV2
             // Probably needs more thought because this needs to be in every `ApplyToBackground` currently to restore sane defaults.
             backgroundModeBeatmap.FadeColour(Color4.White, 250);
 
-            bool backgroundRevealActive = revealingBackground?.State == ScheduledDelegate.RunState.Running || revealingBackground?.State == ScheduledDelegate.RunState.Complete;
+            bool backgroundRevealActive = revealBackgroundDelegate?.State == ScheduledDelegate.RunState.Running || revealBackgroundDelegate?.State == ScheduledDelegate.RunState.Complete;
             backgroundModeBeatmap.BlurAmount.Value = configBackgroundBlur.Value && !backgroundRevealActive ? 20 : 0f;
         });
 
@@ -920,28 +922,14 @@ namespace osu.Game.Screens.SelectV2
 
         #endregion
 
-        #region Input
+        #region Background reveal
 
-        private ScheduledDelegate? revealingBackground;
-        private bool isRevealingBackground;
-        private double? lastCursorMoveTimeDuringReveal;
-
-        private bool cursorRecentlyMoved => lastCursorMoveTimeDuringReveal.HasValue &&
-                                            Clock.CurrentTime - lastCursorMoveTimeDuringReveal.Value < 1000;
+        private ScheduledDelegate? revealBackgroundDelegate;
 
         public CursorContainer? Cursor => null;
-        public bool ProvidingUserCursor => isRevealingBackground && !cursorRecentlyMoved;
+        bool IProvideCursor.ProvidingUserCursor => revealBackgroundDelegate?.Completed == true;
 
         protected override bool OnHover(HoverEvent e) => true;
-
-        protected override bool OnMouseMove(MouseMoveEvent e)
-        {
-            if (isRevealingBackground)
-                lastCursorMoveTimeDuringReveal = Clock.CurrentTime;
-            return base.OnMouseMove(e);
-        }
-
-        private GridContainer mainGridContainer = null!;
 
         protected override bool OnMouseDown(MouseDownEvent e)
         {
@@ -956,13 +944,13 @@ namespace osu.Game.Screens.SelectV2
             // For simplicity, disable this functionality on mobile.
             bool isTouchInput = e.CurrentState.Mouse.LastSource is ISourcedFromTouch;
 
-            if (!carousel.AbsoluteScrolling && !isTouchInput && mouseDownPriority && revealingBackground == null)
+            if (!carousel.AbsoluteScrolling && !isTouchInput && mouseDownPriority && revealBackgroundDelegate == null)
             {
-                revealingBackground = Scheduler.AddDelayed(() =>
+                revealBackgroundDelegate = Scheduler.AddDelayed(() =>
                 {
                     if (containingInputManager.DraggedDrawable != null)
                     {
-                        revealingBackground = null;
+                        revealBackgroundDelegate = null;
                         return;
                     }
 
@@ -977,9 +965,6 @@ namespace osu.Game.Screens.SelectV2
                     updateBackgroundDim();
 
                     Footer?.Hide();
-
-                    isRevealingBackground = true;
-                    lastCursorMoveTimeDuringReveal = null;
                 }, 200);
             }
 
@@ -994,10 +979,10 @@ namespace osu.Game.Screens.SelectV2
 
         private void restoreBackground()
         {
-            if (revealingBackground == null)
+            if (revealBackgroundDelegate == null)
                 return;
 
-            if (revealingBackground.State == ScheduledDelegate.RunState.Complete)
+            if (revealBackgroundDelegate.State == ScheduledDelegate.RunState.Complete)
             {
                 mainContent.ResizeWidthTo(1f, 500, Easing.OutQuint);
                 mainContent.ScaleTo(1, 500, Easing.OutQuint);
@@ -1010,14 +995,15 @@ namespace osu.Game.Screens.SelectV2
                 Footer?.Show();
             }
 
-            revealingBackground.Cancel();
-            revealingBackground = null;
-
-            isRevealingBackground = false;
-            lastCursorMoveTimeDuringReveal = null;
+            revealBackgroundDelegate.Cancel();
+            revealBackgroundDelegate = null;
 
             updateBackgroundDim();
         }
+
+        #endregion
+
+        #region Input
 
         public virtual bool OnPressed(KeyBindingPressEvent<GlobalAction> e)
         {
