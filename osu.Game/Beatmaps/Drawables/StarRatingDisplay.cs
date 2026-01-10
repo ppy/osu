1527526629d50cea100f4cca6d64a5338fc2c6c0
StanR <hi@stanr.info>
2026-01-10 17:27:30 +0500

    Using

--- osu.Game/Beatmaps/Drawables/StarRatingDisplay.cs
+++ osu.Game/Beatmaps/Drawables/StarRatingDisplay.cs
@@ -11,7 +11,6 @@ using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Sprites;
-using osu.Game.Overlays;
 using osu.Game.Utils;
 using osuTK;
 using osuTK.Graphics;
