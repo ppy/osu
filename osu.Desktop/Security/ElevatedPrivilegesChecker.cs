76dc1b2416685917a3bef9de1ec44a0ad8c31cdb
Dean Herbert <pe@ppy.sh>
2026-01-16 22:56:40 +0900

    Change capitalisation of `Administrator` to match common usages

--- osu.Desktop/Security/ElevatedPrivilegesChecker.cs
+++ osu.Desktop/Security/ElevatedPrivilegesChecker.cs
@@ -33,7 +33,7 @@ namespace osu.Desktop.Security
         {
             public ElevatedPrivilegesNotification()
             {
-                Text = NotificationsStrings.ElevatedPrivileges(RuntimeInfo.IsUnix ? "root" : "administrator");
+                Text = NotificationsStrings.ElevatedPrivileges(RuntimeInfo.IsUnix ? "root" : "Administrator");
             }
 
             [BackgroundDependencyLoader]
