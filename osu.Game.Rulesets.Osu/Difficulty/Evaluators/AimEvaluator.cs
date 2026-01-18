c9b502845987b8bec1cc47b712489aff1e86fbd2
kwotaq <80002984+kwotaq@users.noreply.github.com>
2026-01-18 15:56:23 +0200

    Add comment for wiggle bonus multiplier (#36373)
    It's a common mistake by newer pp dev contributors to increase the
    wiggle multiplier not knowing its at its maximum, so I added a comment
    to try to avoid more people doing it.
    
    ---------
    
    Co-authored-by: StanR <8269193+stanriders@users.noreply.github.com>

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -15,7 +15,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double acute_angle_multiplier = 2.55;
         private const double slider_multiplier = 1.35;
         private const double velocity_change_multiplier = 0.75;
-        private const double wiggle_multiplier = 1.02;
+        private const double wiggle_multiplier = 1.02; // WARNING: Increasing this multiplier beyond 1.02 reduces difficulty as distance increases. Refer to the desmos link above the wiggle bonus calculation
 
         /// <summary>
         /// Evaluates the difficulty of aiming the current object, based on:
