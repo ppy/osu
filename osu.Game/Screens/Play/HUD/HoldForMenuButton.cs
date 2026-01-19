6cb423fbfe70d4e5d29f4333251e5c8ff4f50b26
MayoCollector <boomboxrapsody@gmail.com>
2026-01-19 10:56:39 +0900

    code quality

--- osu.Game/Screens/Play/HUD/HoldForMenuButton.cs
+++ osu.Game/Screens/Play/HUD/HoldForMenuButton.cs
@@ -7,7 +7,6 @@ using System;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
