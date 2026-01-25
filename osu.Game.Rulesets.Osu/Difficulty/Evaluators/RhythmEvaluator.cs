44ab308901a8af5fcda795604425f7140064c61e
StanR <8269193+stanriders@users.noreply.github.com>
2026-01-26 02:21:20 +0500

    Move speed distance bonus to aim (#36168)
    This doesn't solve _flow_ aim in any way, but makes it so that `Speed`
    doesn't have distance scaling (read as "aim") anymore which fixes some
    issues related to that like the length bonus behaving incorrectly, and
    in general `Speed` being an aim+tap skill instead of just a tapping
    skill
    
    ---------
    
    Co-authored-by: James Wilson <tsunyoku@gmail.com>

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
@@ -16,7 +16,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const int history_time_max = 5 * 1000; // 5 seconds
         private const int history_objects_max = 32;
         private const double rhythm_overall_multiplier = 1.0;
-        private const double rhythm_ratio_multiplier = 17.0;
+        private const double rhythm_ratio_multiplier = 30.0;
 
         /// <summary>
         /// Calculates a rhythm multiplier for the difficulty of the tap associated with historic data of the current <see cref="OsuDifficultyHitObject"/>.
