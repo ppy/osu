ppy/osu

commit c114fc438e69df81a4af071793cc7c224e97fac4

Fix border showing for inner text boxes incorrectly
I turned masking back on on these for better visuals (text masking
aligns with rest of elements) but turns out this implicitly makes
borders draw.

osu.Game/Graphics/UserInterface/OsuTextBox.cs
@@ -42,6 +42,8 @@ namespace osu.Game.Graphics.UserInterface
             Margin = new MarginPadding { Left = 2 },
         };
 
+        protected bool DrawBorder { get; init; }
+
         private OsuCaret? caret;
 
         private bool selectionStarted;
@@ -256,7 +258,7 @@ namespace osu.Game.Graphics.UserInterface
 
         protected override void OnFocus(FocusEvent e)
         {
-            if (Masking)
+            if (DrawBorder)
                 BorderThickness = 3;
 
             base.OnFocus(e);
@@ -268,7 +270,7 @@ namespace osu.Game.Graphics.UserInterface
 
         protected override void OnFocusLost(FocusLostEvent e)
         {
-            if (Masking)
+            if (DrawBorder)
                 BorderThickness = 0;
 
             base.OnFocusLost(e);

osu.Game/Graphics/UserInterfaceV2/FormTextBox.cs
@@ -198,6 +198,11 @@ namespace osu.Game.Graphics.UserInterfaceV2
 
             protected override float LeftRightPadding => 0;
 
+            public InnerTextBox()
+            {
+                DrawBorder = false;
+            }
+
             [BackgroundDependencyLoader]
             private void load()
             {

commit eaa6bc781646652c35efb0c2f96348264b681823

Fix some text boxes no longer having borders (#36526)
Regressed in c114fc438e69df81a4af071793cc7c224e97fac4.

One very visible instance of this regression is the login form.


https://github.com/user-attachments/assets/5ba10ac5-4cb1-49af-b55c-89cf58ca0b44

The `CommentEditor` usage was discovered with one of my favourite tricks
which is doing

```diff
diff --git a/osu.Game/Graphics/UserInterface/OsuTextBox.cs b/osu.Game/Graphics/UserInterface/OsuTextBox.cs
index fefe776b01..c17cca726b 100644
--- a/osu.Game/Graphics/UserInterface/OsuTextBox.cs
+++ b/osu.Game/Graphics/UserInterface/OsuTextBox.cs
@@ -42,6 +42,12 @@ public partial class OsuTextBox : BasicTextBox
             Margin = new MarginPadding { Left = 2 },
         };
 
+        public new bool Masking
+        {
+            get => base.Masking;
+            set => base.Masking = value;
+        }
+
         protected bool DrawBorder { get; init; } = true;
 
         private OsuCaret? caret;

```

and then looking for usages of the setter. That's all due diligence
you're getting here, I'm not auditing every single text box in the game.

And yes, the `CommentEditor` usage is OMEGA dodgy but the change applied
here is the only one that preserves its visual appearance. I'm not
putting in time to fix it.

osu.Game/Graphics/UserInterface/OsuTextBox.cs
@@ -42,7 +42,7 @@ namespace osu.Game.Graphics.UserInterface
             Margin = new MarginPadding { Left = 2 },
         };
 
-        protected bool DrawBorder { get; init; }
+        protected bool DrawBorder { get; init; } = true;
 
         private OsuCaret? caret;
 

osu.Game/Overlays/Comments/CommentEditor.cs
@@ -234,7 +234,7 @@ namespace osu.Game.Overlays.Comments
 
             public EditorTextBox()
             {
-                Masking = false;
+                Masking = DrawBorder = false;
                 TextContainer.Height = 0.4f;
             }
 