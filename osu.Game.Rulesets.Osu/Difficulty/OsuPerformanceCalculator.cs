33bb28385f805558971e1d77917664359b6e45f1
piiidosu <piiidosu24@gmail.com>
2026-01-31 21:04:37 -0500

    Halve high AR TC bonus, added a low AR low CS bonus based on circle radius differential

--- osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuPerformanceCalculator.cs
@@ -518,27 +518,40 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             double highCircleSizeSliderVisibilityFactor = Math.Pow(Math.Min(-0.2 * circleSize + 2.2, 1), 1.8);
 
             // Nerf low CS sliders at low AR less due to density increase from fully bright slider borders
-            double lowCircleSizeSliderVisibilityFactor = 0;
-            if (circleSize < 2.5)
-                lowCircleSizeSliderVisibilityFactor += Math.Min(-0.5 * circleSize + 1.25, 1);
+            // Buff low CS and low AR more
+            double lowCircleSizeVisibilityFactor = 0;
+            if (circleSize < 1)
+                lowCircleSizeVisibilityFactor += 1;
+            else if (circleSize < 2.5)
+                lowCircleSizeVisibilityFactor += -0.3 * circleSize + 1.3;
+            else if (circleSize < 5)
+                lowCircleSizeVisibilityFactor += -0.0352 * Math.Pow(circleSize - 5, 3);
+            double preemptTime = 0;
+            if (approachRate < 5)
+                preemptTime += 1200 + (120 * (5 - approachRate));
+            else
+                preemptTime += 1200 - (150 * (approachRate - 5));
+            double osuPixelCircleSize = (54.4 - (circleSize * 4.48)) * 1.00041;
+            double lowCircleSizeLowApproachRateVisibilityFactor = 1 + (Math.Log(preemptTime - (preemptTime * (1 - (2 / (3 * osuPixelCircleSize))))) / 10);
 
-            lowApproachRateSliderVisibilityFactor = 1 - ((1 - lowApproachRateSliderVisibilityFactor) * (1 - (lowCircleSizeSliderVisibilityFactor / 2)));
+            lowApproachRateSliderVisibilityFactor = 1 - ((1 - lowApproachRateSliderVisibilityFactor) * (1 - (lowCircleSizeVisibilityFactor / 3)));
 
             // Start from normal curve, rewarding lower AR up to AR7
             double traceableBonus = 0.025 * (12.0 - Math.Max(approachRate, 7)) * highApproachRateSliderVisibilityFactor * Math.Max(highCircleSizeSliderVisibilityFactor, highApproachRateSliderVisibilityFactor);
 
             // For AR up to 0 - reduce reward for very low ARs when object is visible
             if (approachRate < 7)
-                traceableBonus += 0.02 * (7.0 - Math.Max(approachRate, 0)) * lowApproachRateSliderVisibilityFactor * Math.Max(highCircleSizeSliderVisibilityFactor, lowApproachRateSliderVisibilityFactor);
+                traceableBonus += 0.02 * (7.0 - Math.Max(approachRate, 0)) * Math.Pow(lowApproachRateSliderVisibilityFactor, 1.5) * Math.Max(highCircleSizeSliderVisibilityFactor, lowApproachRateSliderVisibilityFactor);
 
             // Starting from AR0 - cap values so they won't grow to infinity
             if (approachRate < 0)
-                traceableBonus += 0.01 * (1 - Math.Pow(1.5, approachRate)) * lowApproachRateSliderVisibilityFactor * Math.Max(highCircleSizeSliderVisibilityFactor, lowApproachRateSliderVisibilityFactor);
+                traceableBonus += 0.01 * (1 - Math.Pow(1.5, approachRate)) * Math.Pow(lowApproachRateSliderVisibilityFactor, 1.5) * Math.Max(highCircleSizeSliderVisibilityFactor, lowApproachRateSliderVisibilityFactor);
 
             // AR8+ and especially AR10.3+ TC increases difficulty due to increased circle location uncertainty
             if (approachRate > 8)
-                traceableBonus += ((0.004 * Math.Pow(approachRate - 8, 2)) + (0.005 * (approachRate - 8))) * highApproachRateSliderVisibilityFactor * Math.Max(highCircleSizeSliderVisibilityFactor, highApproachRateSliderVisibilityFactor);
+                traceableBonus += ((0.002 * Math.Pow(approachRate - 8, 2)) + (0.0025 * (approachRate - 8))) * highApproachRateSliderVisibilityFactor * Math.Max(highCircleSizeSliderVisibilityFactor, highApproachRateSliderVisibilityFactor);
 
+            traceableBonus *= lowCircleSizeLowApproachRateVisibilityFactor;
             return traceableBonus;
         }
 
