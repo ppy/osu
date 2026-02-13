7967ea158cc22046cffed2214a47dd6670482892
netineko <netineko@gmail.com>
2026-02-13 00:42:47 -0500

    Change corner radius to 10

--- osu.Game/Screens/Ranking/UserTagControl.DrawableUserTag.cs
+++ osu.Game/Screens/Ranking/UserTagControl.DrawableUserTag.cs
@@ -66,7 +66,7 @@ namespace osu.Game.Screens.Ranking
             [BackgroundDependencyLoader]
             private void load()
             {
-                CornerRadius = 11;
+                CornerRadius = 10;
                 Masking = true;
 
                 EdgeEffect = new EdgeEffectParameters
