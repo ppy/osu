4d5c14a830df24df3b4f515e616eef5fbc9a7b0f
StanR <hi@stanr.info>
2026-02-18 22:29:33 +0500

    Nerf speed

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -31,7 +31,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentAimStrain;
         private double currentSpeedStrain;
 
-        private double skillMultiplierAim => 36.67;
+        private double skillMultiplierAim => 36.767;
         private double skillMultiplierSpeed => 1.35;
         private double skillMultiplierTotal => 1.5;
         private double meanExponent => 1.2;
