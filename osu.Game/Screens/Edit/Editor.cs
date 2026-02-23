4067b116bb8aeddfaaa043bee0f41c4dc5714c21
Dean Herbert <pe@ppy.sh>
2026-02-23 17:34:08 +0900

    Ensure editor mode screen is only loaded once when entering editor

--- osu.Game/Screens/Edit/Editor.cs
+++ osu.Game/Screens/Edit/Editor.cs
@@ -488,7 +488,10 @@ namespace osu.Game.Screens.Edit
             setUpClipboardActionAvailability();
 
             Mode.Value = isNewBeatmap ? EditorScreenMode.SongSetup : EditorScreenMode.Compose;
-            Mode.BindValueChanged(onModeChanged, true);
+
+            // AddOnce here ensures we don't unnecessarily load multiple modes when entering the editor.
+            // See case where Mode is restored via `RestoreState`.
+            Mode.BindValueChanged(_ => Scheduler.AddOnce(loadModeScreen), true);
 
             MutationTracker.InProgress.BindValueChanged(_ =>
             {
@@ -1072,15 +1075,16 @@ namespace osu.Game.Screens.Edit
             musicController.TrackChanged -= onTrackChanged;
         }
 
-        private void onModeChanged(ValueChangedEvent<EditorScreenMode> e)
+        private void loadModeScreen()
         {
             var lastScreen = currentScreen;
+            var newMode = Mode.Value;
 
             lastScreen?.Hide();
 
             try
             {
-                if ((currentScreen = screenContainer.SingleOrDefault(s => s.Type == e.NewValue)) != null)
+                if ((currentScreen = screenContainer.SingleOrDefault(s => s.Type == newMode)) != null)
                 {
                     screenContainer.ChangeChildDepth(currentScreen, lastScreen?.Depth + 1 ?? 0);
 
@@ -1088,7 +1092,7 @@ namespace osu.Game.Screens.Edit
                     return;
                 }
 
-                switch (e.NewValue)
+                switch (newMode)
                 {
                     case EditorScreenMode.SongSetup:
                         currentScreen = new SetupScreen();
@@ -1320,7 +1324,8 @@ namespace osu.Game.Screens.Edit
                 yield return new EditorMenuItem(EditorStrings.OpenInfoPage, MenuItemType.Standard,
                     () => (Game as OsuGame)?.OpenUrlExternally(editorBeatmap.BeatmapInfo.GetOnlineURL(api, editorBeatmap.BeatmapInfo.Ruleset)));
                 yield return new EditorMenuItem(EditorStrings.OpenDiscussionPage, MenuItemType.Standard,
-                    () => (Game as OsuGame)?.OpenUrlExternally($@"{api.Endpoints.WebsiteUrl}/beatmapsets/{editorBeatmap.BeatmapInfo.BeatmapSet!.OnlineID}/discussion/{editorBeatmap.BeatmapInfo.OnlineID}"));
+                    () => (Game as OsuGame)?.OpenUrlExternally(
+                        $@"{api.Endpoints.WebsiteUrl}/beatmapsets/{editorBeatmap.BeatmapInfo.BeatmapSet!.OnlineID}/discussion/{editorBeatmap.BeatmapInfo.OnlineID}"));
             }
 
             yield return new OsuMenuItemSpacer();
@@ -1604,8 +1609,13 @@ namespace osu.Game.Screens.Edit
 
             Mode.Value = EditorScreenMode.Compose;
 
-            // Delegate handling the selection to the ruleset.
-            currentScreen.Dependencies.Get<HitObjectComposer>().SelectFromTimestamp(position, selection);
+            Schedule(() =>
+            {
+                // Delegate handling the selection to the ruleset.
+                if (currentScreen is ComposeScreen composeScreen)
+                    composeScreen.Composer.SelectFromTimestamp(position, selection);
+            });
+
             return true;
         }
 
