8bbc1e1dfea1fa2813a177cf3068a490d4f903bc
CloneWith <clonewith@qq.com>
2026-02-26 14:47:38 +0800

    Add test case for front matter with commeents

--- osu.Game.Tests/Visual/Online/TestSceneWikiMarkdownContainer.cs
+++ osu.Game.Tests/Visual/Online/TestSceneWikiMarkdownContainer.cs
@@ -117,6 +117,19 @@ needs_cleanup: true
         }
 
         [Test]
+        public void TestOutdatedNoticeBoxWithComments()
+        {
+            AddStep("Add outdated yaml with comments", () =>
+            {
+                markdownContainer.Text = @"---
+outdated: true  # not sure about the format for ""list of mods"".
+---";
+            });
+
+            AddAssert("Outdated notice box visible", () => markdownContainer.ChildrenOfType<Container>().Any());
+        }
+
+        [Test]
         public void TestAbsoluteImage()
         {
             AddStep("Add absolute image", () =>
