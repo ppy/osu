9d51777f57b1465f88c199543ea5380e83cc9186
StanR <hi@stanr.info>
2026-03-03 15:46:02 +0500

    Adjust aim

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -31,7 +31,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentAimStrain;
         private double currentSpeedStrain;
 
-        private double skillMultiplierAim => 58.5;
+        private double skillMultiplierAim => 58.75;
         private double skillMultiplierSpeed => 1.75;
         private double skillMultiplierTotal => 1.0;
         private double meanExponent => 1.2;
