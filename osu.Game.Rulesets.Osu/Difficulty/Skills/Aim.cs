4bc5689f9be233f129379ddd382f62208f280284
StanR <hi@stanr.info>
2026-03-04 18:46:23 +0500

    Change multipliers

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -32,7 +32,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentSpeedStrain;
 
         private double skillMultiplierAim => 65.7;
-        private double skillMultiplierSpeed => 2.75;
+        private double skillMultiplierSpeed => 2.85;
         private double skillMultiplierTotal => 1.0;
         private double meanExponent => 1.2;
 
