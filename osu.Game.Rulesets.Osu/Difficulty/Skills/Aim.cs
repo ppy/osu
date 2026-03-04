c11c0fc2fefc464a5b45a02405d405bfd4d70bf7
StanR <hi@stanr.info>
2026-03-05 02:06:16 +0500

    Slight speedaim buff

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -32,7 +32,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentSpeedStrain;
 
         private double skillMultiplierAim => 65.2;
-        private double skillMultiplierSpeed => 2.8;
+        private double skillMultiplierSpeed => 2.85;
         private double skillMultiplierTotal => 1.0;
         private double meanExponent => 1.2;
 
