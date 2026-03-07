d31e609afbc8b6f8685c30e32a80e97ad1edf041
Dean Herbert <pe@ppy.sh>
2026-03-07 23:19:13 +0900

    Fix a few flaky tests
    See failures on https://github.com/ppy/osu/runs/66073876886#r3s1.

--- osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapTitleWedge.cs
+++ osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapTitleWedge.cs
@@ -240,11 +240,11 @@ namespace osu.Game.Tests.Visual.SongSelect
             });
             AddStep("reset event", resetEvent.Reset);
             AddStep("click favourite button", () => this.ChildrenOfType<BeatmapTitleWedge.FavouriteButton>().Single().TriggerClick());
-            AddAssert("spinner visible", () => this.ChildrenOfType<BeatmapTitleWedge.FavouriteButton>().Single()
-                                                   .ChildrenOfType<LoadingSpinner>().Single().State.Value, () => Is.EqualTo(Visibility.Visible));
+            AddUntilStep("spinner visible", () => this.ChildrenOfType<BeatmapTitleWedge.FavouriteButton>().Single()
+                                                      .ChildrenOfType<LoadingSpinner>().Single().State.Value, () => Is.EqualTo(Visibility.Visible));
             AddStep("allow request to complete", resetEvent.Set);
-            AddAssert("spinner hidden", () => this.ChildrenOfType<BeatmapTitleWedge.FavouriteButton>().Single()
-                                                  .ChildrenOfType<LoadingSpinner>().Single().State.Value, () => Is.EqualTo(Visibility.Hidden));
+            AddUntilStep("spinner hidden", () => this.ChildrenOfType<BeatmapTitleWedge.FavouriteButton>().Single()
+                                                     .ChildrenOfType<LoadingSpinner>().Single().State.Value, () => Is.EqualTo(Visibility.Hidden));
         }
 
         [TestCase(120, 125, null, "120-125 (mostly 120)")]
