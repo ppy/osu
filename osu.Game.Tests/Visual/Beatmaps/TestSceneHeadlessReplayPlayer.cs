d2c3765dd187ae2b09a09c7212819e48f2ba589e
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2026-03-09 22:19:29 +0100

    Create a component for headlessly processing replays

---
+++ osu.Game.Tests/Visual/Beatmaps/TestSceneHeadlessReplayPlayer.cs
@@ -0,0 +1,94 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions;
+using osu.Framework.Testing;
+using osu.Game.Beatmaps;
+using osu.Game.Replays;
+using osu.Game.Rulesets;
+using osu.Game.Scoring;
+using osu.Game.Scoring.Legacy;
+using osu.Game.Tests.Resources;
+
+namespace osu.Game.Tests.Visual.Beatmaps
+{
+    public partial class TestSceneHeadlessReplayPlayer : OsuTestScene
+    {
+        private HeadlessReplayPlayer player = null!;
+
+        [Resolved]
+        private BeatmapManager beatmaps { get; set; } = null!;
+
+        [Resolved]
+        private RulesetStore rulesets { get; set; } = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Clear();
+        }
+
+        [SetUpSteps]
+        public void SetUpSteps()
+        {
+            AddStep("import beatmaps", () => beatmaps.Import(TestResources.GetTestBeatmapForImport()).WaitSafely());
+        }
+
+        [Test]
+        public void TestBasic()
+        {
+            Score score = null!;
+            bool completed = false;
+
+            AddStep("import replay", () =>
+            {
+                using (var replayStream = TestResources.OpenResource($"Replays/osu-renatus-replay.osr"))
+                    score = new DatabasedLegacyScoreDecoder(rulesets, beatmaps).Parse(replayStream);
+            });
+            AddStep("create player", () =>
+            {
+                // Component isn't hidden in tests to allow for visually checking if everything works correctly.
+                LoadComponentAsync(player = new HeadlessReplayPlayer(score, beatmaps.GetWorkingBeatmap(score.ScoreInfo.BeatmapInfo))
+                {
+                    Width = 1024,
+                    Height = 768,
+                    PlaybackCompleted = _ => completed = true,
+                }, Add);
+            });
+            AddUntilStep("wait for load", () => player.IsLoaded, () => Is.True);
+            AddStep("start playback", () => Task.Run(() => player.Start()));
+            AddUntilStep("wait until completed", () => completed, () => Is.True);
+            AddAssert("hit events exist", () => score.ScoreInfo.HitEvents.Any());
+        }
+
+        [Test]
+        public void TestFailed()
+        {
+            Score score = null!;
+            bool completed = false;
+
+            AddStep("import replay", () =>
+            {
+                using (var replayStream = TestResources.OpenResource($"Replays/osu-renatus-replay-failed.osr"))
+                    score = new DatabasedLegacyScoreDecoder(rulesets, beatmaps).Parse(replayStream);
+            });
+            AddStep("create player", () =>
+            {
+                LoadComponentAsync(player = new HeadlessReplayPlayer(score, beatmaps.GetWorkingBeatmap(score.ScoreInfo.BeatmapInfo))
+                {
+                    Width = 1024,
+                    Height = 768,
+                    PlaybackCompleted = _ => completed = true,
+                }, Add);
+            });
+            AddUntilStep("wait for load", () => player.IsLoaded, () => Is.True);
+            AddStep("start playback", () => Task.Run(() => player.Start()));
+            AddUntilStep("wait until completed", () => completed, () => Is.True);
+            AddAssert("hit events exist", () => score.ScoreInfo.HitEvents.Any());
+        }
+    }
+}
---
+++ osu.Game/Replays/HeadlessReplayPlayer.cs
@@ -0,0 +1,222 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Timing;
+using osu.Game.Audio;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
+using osu.Game.Graphics.Containers;
+using osu.Game.Rulesets;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets.UI;
+using osu.Game.Rulesets.UI.Scrolling;
+using osu.Game.Scoring;
+using osu.Game.Screens.Play;
+using osu.Game.Skinning;
+
+namespace osu.Game.Replays
+{
+    public partial class HeadlessReplayPlayer : CompositeDrawable
+    {
+        public Action<ScoreInfo>? PlaybackCompleted;
+
+        private readonly Score score;
+        private readonly IBeatmap playableBeatmap;
+        private readonly Ruleset ruleset;
+
+        private readonly ManualAdjustableClock clock = new ManualAdjustableClock();
+
+        private DrawableRuleset drawableRuleset = null!;
+        private GameplayClockContainer gameplayClockContainer = null!;
+
+        private ScoreProcessor scoreProcessor = null!;
+
+        private HealthProcessor healthProcessor = null!;
+
+        private DependencyContainer dependencies = null!;
+
+        public HeadlessReplayPlayer(Score score, IWorkingBeatmap workingBeatmap)
+        {
+            this.score = score;
+
+            ruleset = workingBeatmap.BeatmapInfo.Ruleset.CreateInstance();
+            playableBeatmap = workingBeatmap.GetPlayableBeatmap(ruleset.RulesetInfo, score.ScoreInfo.Mods);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            drawableRuleset = ruleset.CreateDrawableRulesetWith(playableBeatmap, score.ScoreInfo.Mods);
+            dependencies.CacheAs(drawableRuleset);
+
+            if (drawableRuleset is IDrawableScrollingRuleset scrollingRuleset)
+                dependencies.CacheAs(scrollingRuleset.ScrollingInfo);
+
+            scoreProcessor = ruleset.CreateScoreProcessor();
+            scoreProcessor.Mods.Value = score.ScoreInfo.Mods;
+            scoreProcessor.ApplyBeatmap(playableBeatmap);
+
+            dependencies.CacheAs(scoreProcessor);
+
+            healthProcessor =
+                score.ScoreInfo.Mods.OfType<IApplicableHealthProcessor>().FirstOrDefault()?.CreateHealthProcessor(playableBeatmap.HitObjects[0].StartTime)
+                ?? ruleset.CreateHealthProcessor(playableBeatmap.HitObjects[0].StartTime);
+            healthProcessor.ApplyBeatmap(playableBeatmap);
+
+            dependencies.CacheAs(healthProcessor);
+
+            var rulesetSkinProvider = new HeadlessRulesetSkinProvidingContainer(ruleset, playableBeatmap, null);
+            InternalChild = gameplayClockContainer = new GameplayClockContainer(clock, false, false);
+
+            gameplayClockContainer.Add(rulesetSkinProvider);
+            rulesetSkinProvider.Add(createGameplayComponents());
+
+            dependencies.CacheAs(drawableRuleset.FrameStableClock);
+            dependencies.CacheAs<IGameplayClock>(drawableRuleset.FrameStableClock);
+
+            drawableRuleset.NewResult += result =>
+            {
+                healthProcessor.ApplyResult(result);
+                scoreProcessor.ApplyResult(result);
+            };
+
+            scoreProcessor.OnLoadComplete += _ =>
+            {
+                foreach (var mod in score.ScoreInfo.Mods.OfType<IApplicableToScoreProcessor>())
+                    mod.ApplyToScoreProcessor(scoreProcessor);
+            };
+
+            healthProcessor.OnLoadComplete += _ =>
+            {
+                foreach (var mod in score.ScoreInfo.Mods.OfType<IApplicableToHealthProcessor>())
+                    mod.ApplyToHealthProcessor(healthProcessor);
+            };
+
+            scoreProcessor.HasCompleted.BindValueChanged(_ => checkScoreCompleted());
+            healthProcessor.Failed += onFail;
+        }
+
+        private Drawable createGameplayComponents() => new ScalingContainer(ScalingMode.Gameplay)
+        {
+            Children = new Drawable[]
+            {
+                drawableRuleset.With(r =>
+                    r.FrameStableComponents.Children = new Drawable[]
+                    {
+                        scoreProcessor,
+                        healthProcessor,
+                    }),
+            },
+        };
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            drawableRuleset.SetReplayScore(score);
+
+            scoreProcessor.NewJudgement += _ => scoreProcessor.PopulateScore(score.ScoreInfo);
+            scoreProcessor.OnResetFromReplayFrame += () => scoreProcessor.PopulateScore(score.ScoreInfo);
+        }
+
+        private void checkScoreCompleted()
+        {
+            if (!scoreProcessor.HasCompleted.Value)
+                return;
+
+            PlaybackCompleted?.Invoke(score.ScoreInfo);
+        }
+
+        private bool onFail()
+        {
+            if (!score.ScoreInfo.Mods.OfType<IApplicableFailOverride>().All(m => m.PerformFail()))
+                return false;
+
+            Schedule(() =>
+            {
+                scoreProcessor.FailScore(score.ScoreInfo);
+                PlaybackCompleted?.Invoke(score.ScoreInfo);
+            });
+
+            return true;
+        }
+
+        public void Start()
+        {
+            if (!IsLoaded)
+                return;
+
+            double? lastFrameTime = score.Replay.Frames.LastOrDefault()?.Time;
+
+            if (lastFrameTime == null)
+                return;
+
+            clock.Seek(lastFrameTime.Value);
+        }
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+        {
+            return dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+        }
+
+        protected override DrawNode CreateDrawNode() => new DrawNode(this);
+
+        private class ManualAdjustableClock : IAdjustableClock
+        {
+            public double CurrentTime { get; set; }
+
+            public void Reset()
+            {
+            }
+
+            public void Start()
+            {
+                IsRunning = true;
+            }
+
+            public void Stop()
+            {
+                IsRunning = false;
+            }
+
+            public bool Seek(double position)
+            {
+                CurrentTime = position;
+                return true;
+            }
+
+            public void ResetSpeedAdjustments()
+            {
+                rate = 1;
+            }
+
+            private double rate;
+
+            double IAdjustableClock.Rate
+            {
+                get => rate;
+                set => rate = value;
+            }
+
+            double IClock.Rate => rate;
+
+            public bool IsRunning { get; private set; } = true;
+        }
+
+        private partial class HeadlessRulesetSkinProvidingContainer : RulesetSkinProvidingContainer
+        {
+            protected override bool AllowSampleLookup(ISampleInfo sampleInfo) => false;
+
+            public HeadlessRulesetSkinProvidingContainer(Ruleset ruleset, IBeatmap beatmap, ISkin? beatmapSkin)
+                : base(ruleset, beatmap, beatmapSkin)
+            {
+            }
+        }
+    }
+}
