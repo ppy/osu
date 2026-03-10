6c9a7292e7bdb6472bd218513b3626ff35351089
StanR <8269193+stanriders@users.noreply.github.com>
2026-03-11 03:48:09 +0500

    Adjust `Speed` harmonic summation params (#36923)
    Slight balancing. Mostly exists to reduce extremely consistent tapping
    maps (i.e [Age of
    Tyranny](https://osu.ppy.sh/beatmapsets/2219231#osu/4704022)) influence
    over the summation
    
    ---------
    
    Co-authored-by: James Wilson <tsunyoku@gmail.com>

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -34,7 +34,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.0;
-        private double skillMultiplierFlow => 238.0;
+        private double skillMultiplierFlow => 244.0;
         private double skillMultiplierTotal => 1.1;
         private double meanExponent => 1.2;
 
