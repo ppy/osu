59db7c8ff146c48dbf35a37bd3dd3878d2c09e16
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-03-12 14:07:32 +0100

    Add models for rolling in multiplayer

--- osu.Game/Online/Multiplayer/MatchUserRequest.cs
+++ osu.Game/Online/Multiplayer/MatchUserRequest.cs
@@ -22,6 +22,7 @@ namespace osu.Game.Online.Multiplayer
     [Union(3, typeof(MatchmakingAvatarActionRequest))]
     [Union(4, typeof(RankedPlayCardHandReplayRequest))]
     [Union(5, typeof(SetLockStateRequest))]
+    [Union(6, typeof(RollRequest))]
     public abstract class MatchUserRequest
     {
     }
