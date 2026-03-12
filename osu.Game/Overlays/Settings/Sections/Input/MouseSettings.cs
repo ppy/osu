177ff9cbf4db0ed2b89e36bb670efabac300933f
TaterToes <nguyendamian2006@gmail.com>
2026-03-12 18:04:12 -0400

    removed a line which I thought it would work so I didn't even decide to debug it

--- osu.Game/Overlays/Settings/Sections/Input/MouseSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Input/MouseSettings.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Math;
 using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
