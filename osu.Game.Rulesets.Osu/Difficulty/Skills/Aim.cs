81e858a09e720fc96b5881d34817b586a67acdc6
StanR <hi@stanr.info>
2026-03-14 18:18:53 +0500

    Buff flow slightly

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -33,7 +33,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.0;
-        private double skillMultiplierFlow => 244.0;
+        private double skillMultiplierFlow => 245.0;
         private double skillMultiplierTotal => 1.1;
         private double meanExponent => 1.2;
 
