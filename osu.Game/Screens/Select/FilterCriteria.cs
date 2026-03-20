240105bf09a4a30eff72000ff5b6491931738949
Simão Caixas <simaocaixas@tecnico.ulisboa.pt>
2026-03-20 16:48:41 +0000

    Apply suggestion: use \b word boundary instead of \p{P}

--- osu.Game/Screens/Select/FilterCriteria.cs
+++ osu.Game/Screens/Select/FilterCriteria.cs
@@ -219,7 +219,7 @@ namespace osu.Game.Screens.Select
                         break;
 
                     case MatchMode.IsolatedPhrase:
-                        result = Regex.IsMatch(value, $@"(^|[\s\p{{P}}]){Regex.Escape(searchTerm)}($|[\s\p{{P}}])", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                        result = Regex.IsMatch(value, $@"(^|\b){Regex.Escape(searchTerm)}($|\b)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
                         break;
 
                     case MatchMode.FullPhrase:
