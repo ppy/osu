6ed6b19ac3a6d9cb6169c2c8a3d9e46ff6a18711
StanR <8269193+stanriders@users.noreply.github.com>
2026-03-24 22:57:31 +0500

    Undo Agility strain influence rescaling (#37081)
    Now that agility isn't "speedaim" that covers both snap and flow we can
    restore it back into it's original scaling. Arguably it should be _more_
    than d/t^2 but that's a thing to explore separately

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
@@ -38,6 +38,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
             return strain * DifficultyCalculationUtils.Smootherstep(distance, 0, OsuDifficultyHitObject.NORMALISED_RADIUS);
         }
 
-        private static double highBpmBonus(double ms) => 1 / (1 - Math.Pow(0.3, Math.Pow(ms / 1000, 0.9)));
+        private static double highBpmBonus(double ms) => 1 / (1 - Math.Pow(0.15, ms / 1000));
     }
 }
