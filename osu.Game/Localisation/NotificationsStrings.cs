e320db7dbab85c46559df8963dd2f7349fd1a194
florious <florious@foxmail.com>
2026-03-24 22:50:38 +0800

    fix[typo]: better grammar structure

--- osu.Game/Localisation/NotificationsStrings.cs
+++ osu.Game/Localisation/NotificationsStrings.cs
@@ -236,9 +236,9 @@ Click to see what's new!", version);
         public static LocalisableString ElevatedPrivileges(LocalisableString user) => new TranslatableString(getKey(@"elevated_privileges"), @"Running osu! as {0} does not improve performance, may break integrations and poses a security risk. Please run the game as a normal user.", user);
 
         /// <summary>
-        /// "osu! should be located in /Applications or {0}/Applications on macOS. Please move the game into Applications folder."
+        /// "On macOS, installing osu! to a directory other than /Applications or {0}/Applications can cause issues with updating the game. Please move your game installation to one of them."
         /// </summary>
-        public static LocalisableString MacOSAppLocation(LocalisableString userProfile) => new TranslatableString(getKey(@"macos_app_location"), @"osu! should be located in /Applications or {0}/Applications on macOS. Please move the game into Applications folder.", userProfile);
+        public static LocalisableString MacOSAppLocation(LocalisableString userProfile) => new TranslatableString(getKey(@"macos_app_location"), @"On macOS, installing osu! to a directory other than /Applications or {0}/Applications can cause issues with updating the game. Please move your game installation to one of them.", userProfile);
 
         /// <summary>
         /// "Screenshot saved! Click to view.
