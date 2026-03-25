5f09dfa72bcd2776572b2c6738001317347af4d8
StanR <hi@stanr.info>
2026-03-25 17:29:24 +0500

    Little bit less maybe

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -32,7 +32,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double currentStrain;
 
         private double skillMultiplierSnap => 71.0;
-        private double skillMultiplierAgility => 2.4;
+        private double skillMultiplierAgility => 2.35;
         private double skillMultiplierFlow => 245.0;
         private double skillMultiplierTotal => 1.11;
         private double meanExponent => 1.2;
