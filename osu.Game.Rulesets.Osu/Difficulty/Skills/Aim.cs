0009471ded6b64d45c1d697e061e2d12cd6ef904
StanR <hi@stanr.info>
2026-03-28 01:42:40 +0500

    Very slight overall aim buff

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -34,7 +34,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.35;
         private double skillMultiplierFlow => 245.0;
-        private double skillMultiplierTotal => 1.11;
+        private double skillMultiplierTotal => 1.12;
         private double meanExponent => 1.2;
 
         /// <summary>
