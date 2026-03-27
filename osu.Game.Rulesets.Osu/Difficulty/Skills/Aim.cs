367c8d865e3a8d93a9c57926dc1d701edbca1466
kwotaq <PJXORISJ@gmail.com>
2026-03-27 17:14:52 +0200

    accept the relief fate

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -31,7 +31,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplierSnap => 71.3;
+        private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.35;
         private double skillMultiplierFlow => 245.0;
         private double skillMultiplierTotal => 1.11;
