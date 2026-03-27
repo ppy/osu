9d20cbb0217f8c8d949fc13c2f00f11ce8213770
Dean Herbert <pe@ppy.sh>
2026-03-27 22:59:36 +0900

    Add basic card back design (#37128)
    It's just something more than nothing.
    
    <img width="1079" height="554" alt="osu Game Tests 2026-03-27 at 08 26
    34"
    src="https://github.com/user-attachments/assets/de1485f7-b29c-4bc5-aec4-dbc736a87961"
    />

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Card/RankedPlayCardBackSide.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Card/RankedPlayCardBackSide.cs
@@ -3,9 +3,14 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Graphics.Backgrounds;
 using osu.Game.Overlays;
+using osuTK;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
 {
@@ -17,15 +22,35 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
         }
 
         [BackgroundDependencyLoader]
-        private void load(OverlayColourProvider colourProvider)
+        private void load(OverlayColourProvider colourProvider, TextureStore textures)
         {
             Masking = true;
             CornerRadius = RankedPlayCard.CORNER_RADIUS;
 
-            InternalChild = new Box
+            InternalChildren = new Drawable[]
             {
-                RelativeSizeAxes = Axes.Both,
-                Colour = colourProvider.Background1,
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour =
+                        ColourInfo.GradientVertical(
+                            colourProvider.Background3,
+                            colourProvider.Background4),
+                },
+                new TrianglesV2
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colourProvider.Background4,
+                    SpawnRatio = 1.2f,
+                    Velocity = 0.1f,
+                },
+                new Sprite
+                {
+                    Texture = textures.Get(@"Menu/logo"),
+                    Size = new Vector2(32),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                },
             };
         }
     }
