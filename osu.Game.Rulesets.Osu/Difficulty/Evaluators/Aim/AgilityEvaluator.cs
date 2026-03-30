630f3a6cc2032d2c184b3e80fcdb494731373f3e
Givy120 <givikap120@gmail.com>
2026-03-30 15:10:43 +0200

    Base wide angle bonus on min velocity just as in snap

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
@@ -24,27 +24,31 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
 
             var osuCurrObj = (OsuDifficultyHitObject)current;
             var osuPrevObj = current.Index > 0 ? (OsuDifficultyHitObject)current.Previous(0) : null;
+            var osuPrevObj1 = current.Index > 1 ? (OsuDifficultyHitObject)current.Previous(1) : null;
 
-            double travelDistance = osuPrevObj?.LazyTravelDistance ?? 0;
-            double distance = travelDistance + osuCurrObj.LazyJumpDistance;
-
-            double distanceScaled = Math.Min(distance, distance_cap) / distance_cap;
-
-            double strain = distanceScaled * 1000 / osuCurrObj.AdjustedDeltaTime;
+            double strain = getStrain(osuCurrObj, osuPrevObj);
 
             if (osuCurrObj.Angle != null && osuPrevObj != null)
             {
                 double wideAngleBonus = SnapAimEvaluator.CalcWideAngleBonus(osuCurrObj.Angle.Value);
                 wideAngleBonus *= DifficultyCalculationUtils.ReverseLerp(osuPrevObj.AdjustedDeltaTime, osuCurrObj.AdjustedDeltaTime * 0.5, osuCurrObj.AdjustedDeltaTime * 0.75);
-                wideAngleBonus *= DifficultyCalculationUtils.Smootherstep(Math.Min(osuCurrObj.JumpDistance, osuPrevObj.JumpDistance), 0, OsuDifficultyHitObject.NORMALISED_DIAMETER);
-                strain *= 1 + wideAngleBonus * wide_angle_multiplier;
+
+                double strainPrev = getStrain(osuPrevObj, osuPrevObj1);
+                strain += Math.Min(strain, strainPrev) * wideAngleBonus * wide_angle_multiplier;
             }
 
             strain *= Math.Pow(osuCurrObj.SmallCircleBonus, 1.5);
-
             strain *= highBpmBonus(osuCurrObj.AdjustedDeltaTime);
 
-            return strain * DifficultyCalculationUtils.Smootherstep(distance, 0, OsuDifficultyHitObject.NORMALISED_RADIUS);
+            return strain * DifficultyCalculationUtils.Smootherstep(osuCurrObj.LazyJumpDistance, 0, OsuDifficultyHitObject.NORMALISED_RADIUS);
+        }
+
+        private static double getStrain(OsuDifficultyHitObject osuCurrObj, OsuDifficultyHitObject? osuPrevObj)
+        {
+            double travelDistance = osuPrevObj?.LazyTravelDistance ?? 0;
+            double distance = travelDistance + osuCurrObj.LazyJumpDistance;
+            double distanceScaled = Math.Min(distance, distance_cap) / distance_cap;
+            return distanceScaled * 1000 / osuCurrObj.AdjustedDeltaTime;
         }
 
         private static double highBpmBonus(double ms) => 1 / (1 - Math.Pow(0.2, ms / 1000));
