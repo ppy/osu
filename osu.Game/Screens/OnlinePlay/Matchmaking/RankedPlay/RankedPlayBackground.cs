e69f546469baef60cf75b402ddbaca1f619bbd6d
Dean Herbert <87902114+bubb13gummy@users.noreply.github.com>
2026-04-07 15:33:29 +0900

    Ranked play general UI/UX fixes (#37220)

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/RankedPlayBackground.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/RankedPlayBackground.cs
@@ -1,213 +1,81 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Rendering;
-using osu.Framework.Graphics.Shaders;
-using osu.Framework.Graphics.Shaders.Types;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.Textures;
-using osu.Framework.Utils;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.Backgrounds;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
 {
-    public partial class RankedPlayBackground : CompositeDrawable, IBufferedDrawable
+    public partial class RankedPlayBackground : CompositeDrawable
     {
-        public float GridSize = 10;
+        public Color4 GradientBottom = Color4Extensions.FromHex("#15061e");
+        public Color4 GradientTop = Color4Extensions.FromHex("#240d36");
 
-        public IShader? TextureShader { get; private set; }
-        public Color4 BackgroundColour => Color4.Black;
-        public DrawColourInfo? FrameBufferDrawColour => null;
-        public Vector2 FrameBufferScale => new Vector2(0.1f);
+        private BufferedContainer triangles1Buffered = null!;
+        private Box bgBox = null!;
+        private TrianglesV2 triangles2 = null!;
 
-        public Color4 GradientOutside = Color4Extensions.FromHex("AC6D97");
-        public Color4 GradientInside = Color4Extensions.FromHex("544483");
-        public Color4 DotsColour = Color4Extensions.FromHex("6b2980");
-
-        public RankedPlayBackground()
+        [BackgroundDependencyLoader]
+        private void load()
         {
             InternalChildren =
             [
-                new Triangles
+                bgBox = new Box
                 {
+                    RelativeSizeAxes = Axes.Both,
+                },
+                triangles1Buffered = new BufferedContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        new Triangles
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            Size = new Vector2(0.5f, 1),
+                            SpawnRatio = 1.4f,
+                            ClampAxes = Axes.Y,
+                            Velocity = 0.5f,
+                            TriangleScale = 4,
+                            ColourLight = Color4.White,
+                            ColourDark = Color4.White,
+                            RelativeSizeAxes = Axes.Both,
+                        },
+                    }
+                },
+                triangles2 = new TrianglesV2
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(0.4f, 1),
+                    SpawnRatio = 2,
+                    ClampAxes = Axes.Y,
                     RelativeSizeAxes = Axes.Both,
                 },
             ];
         }
 
-        [BackgroundDependencyLoader]
-        private void load(ShaderManager shaders)
+        public void FadeColours(Color4 top, Color4 bottom)
         {
-            TextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, @"RankedPlayBackground");
+            this.TransformTo(nameof(GradientTop), top, 1500, Easing.OutQuint);
+            this.TransformTo(nameof(GradientBottom), bottom, 1500, Easing.OutQuint);
         }
-
-        private readonly BufferedDrawNodeSharedData sharedData = new BufferedDrawNodeSharedData();
 
         protected override void Update()
         {
             base.Update();
 
-            Invalidate(Invalidation.DrawNode);
-        }
-
-        protected override DrawNode CreateDrawNode() => new RankedPlayBackgroundDrawNode(this, sharedData);
-
-        protected override void Dispose(bool isDisposing)
-        {
-            sharedData.Dispose();
-
-            base.Dispose(isDisposing);
-        }
-
-        private class RankedPlayBackgroundDrawNode : BufferedDrawNode, ICompositeDrawNode
-        {
-            protected new RankedPlayBackground Source => (RankedPlayBackground)base.Source;
-
-            protected new CompositeDrawableDrawNode Child => (CompositeDrawableDrawNode)base.Child;
-
-            public RankedPlayBackgroundDrawNode(RankedPlayBackground source, BufferedDrawNodeSharedData sharedData)
-                : base(source, new CompositeDrawableDrawNode(source), sharedData)
-            {
-            }
-
-            private Vector2 drawSize;
-            private float time;
-            private float gridSize;
-            private Color4 gradientOutside;
-            private Color4 gradientInside;
-            private Color4 dotsColour;
-
-            private IUniformBuffer<RankedPlayBackgroundParameters>? shaderParameterBuffer;
-
-            public override void ApplyState()
-            {
-                base.ApplyState();
-
-                time = (float)(Source.Time.Current / 1000);
-                drawSize = Source.DrawSize;
-                gridSize = Source.GridSize;
-                gradientOutside = Source.GradientOutside;
-                gradientInside = Source.GradientInside;
-                dotsColour = Source.DotsColour;
-            }
-
-            protected override void BindUniformResources(IShader shader, IRenderer renderer)
-            {
-                shaderParameterBuffer ??= renderer.CreateUniformBuffer<RankedPlayBackgroundParameters>();
-
-                shaderParameterBuffer.Data = new RankedPlayBackgroundParameters
-                {
-                    DrawSize = drawSize,
-                    Time = time,
-                    GridSize = gridSize,
-                    GradientOutside = new Vector4(gradientOutside.R, gradientOutside.G, gradientOutside.B, gradientOutside.A),
-                    GradientInside = new Vector4(gradientInside.R, gradientInside.G, gradientInside.B, gradientInside.A),
-                    DotsColour = new Vector4(dotsColour.R, dotsColour.G, dotsColour.B, dotsColour.A),
-                };
-
-                shader.BindUniformBlock("m_RankedPlayBackgroundParameters", shaderParameterBuffer);
-            }
-
-            public List<DrawNode>? Children
-            {
-                get => Child.Children;
-                set => Child.Children = value;
-            }
-
-            public bool AddChildDrawNodes => RequiresRedraw;
-
-            [StructLayout(LayoutKind.Sequential, Pack = 1)]
-            private record struct RankedPlayBackgroundParameters
-            {
-                public UniformVector2 DrawSize;
-                public UniformFloat Time;
-                public UniformFloat GridSize;
-                public UniformVector4 GradientOutside;
-                public UniformVector4 GradientInside;
-                public UniformVector4 DotsColour;
-            }
-        }
-
-        public partial class Triangles : CompositeDrawable
-        {
-            private Texture triangleTexture = null!;
-
-            [BackgroundDependencyLoader]
-            private void load(TextureStore textures)
-            {
-                triangleTexture = textures.Get("Online/RankedPlay/triangle");
-            }
-
-            protected override void LoadComplete()
-            {
-                base.LoadComplete();
-
-                for (int i = 0; i < 20; i++)
-                {
-                    AddInternal(new Triangle
-                    {
-                        Texture = triangleTexture,
-                        RelativePositionAxes = Axes.Both,
-                        X = RNG.NextSingle(),
-                        Y = -0.2f + RNG.NextSingle() * 1.4f,
-                        Origin = Anchor.Centre,
-                        Rotation = RNG.NextSingle() * 360,
-                        AngularVelocity = RNG.NextSingle() - 0.75f,
-                        Size = new Vector2(100 + RNG.NextSingle() * 1000),
-                        MovementSpeed = 0.25f + RNG.NextSingle() * 0.75f,
-                        Alpha = 0.5f + RNG.NextSingle() * 0.5f,
-                    });
-                }
-            }
-
-            public float ParticleVelocity = 1;
-
-            protected override void Update()
-            {
-                base.Update();
-
-                if (DrawHeight <= 0)
-                    return;
-
-                float baseVelocity = 0.03f * ParticleVelocity / DrawHeight;
-                float elapsed = (float)Time.Elapsed;
-
-                foreach (var c in InternalChildren)
-                {
-                    var triangle = (Triangle)c;
-
-                    triangle.Y -= baseVelocity * elapsed * triangle.MovementSpeed;
-
-                    triangle.Rotation += triangle.AngularVelocity * elapsed * 0.02f;
-
-                    // wrap vertically
-                    if (triangle.Y < -0.2f)
-                    {
-                        triangle.X = RNG.NextSingle();
-                        triangle.Y = 1.2f;
-                        triangle.Alpha = 0.5f + RNG.NextSingle() * 0.5f;
-                    }
-                    else if (triangle.Y > 1.2f)
-                    {
-                        triangle.X = RNG.NextSingle();
-                        triangle.Y = -0.2f;
-                        triangle.Alpha = 0.5f + RNG.NextSingle() * 0.5f;
-                    }
-                }
-            }
-
-            private partial class Triangle : Sprite
-            {
-                public float MovementSpeed = 1;
-                public float AngularVelocity;
-            }
+            bgBox.Colour = ColourInfo.GradientVertical(GradientTop, GradientBottom);
+            triangles1Buffered.Colour = ColourInfo.GradientVertical(GradientTop.Lighten(0.2f), GradientBottom.Lighten(0.2f));
+            triangles2.Colour = GradientTop.Lighten(0.5f);
         }
     }
 }
