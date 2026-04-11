4a501692cddb1c4da9dc95d529af08609f3f7c8b
Byrbyrka <stepandavydov556@mail.ru>
2026-04-11 16:40:46 +0300

    Make OsuModSpunOut incompatible with OsuModPerfect

--- osu.Game.Rulesets.Osu/Mods/OsuModSpunOut.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModSpunOut.cs
@@ -21,7 +21,7 @@ namespace osu.Game.Rulesets.Osu.Mods
         public override ModType Type => ModType.Automation;
         public override LocalisableString Description => @"Spinners will be automatically completed.";
         public override double ScoreMultiplier => 0.9;
-        public override Type[] IncompatibleMods => new[] { typeof(ModAutoplay), typeof(OsuModAutopilot), typeof(OsuModTargetPractice) };
+        public override Type[] IncompatibleMods => new[] { typeof(ModAutoplay), typeof(OsuModAutopilot), typeof(OsuModTargetPractice), typeof(OsuModPerfect) };
         public override bool Ranked => UsesDefaultConfiguration;
 
         public void ApplyToDrawableHitObject(DrawableHitObject hitObject)
