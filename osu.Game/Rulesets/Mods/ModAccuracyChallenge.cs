ppy/osu

commit 22b8a8e57db181f783e3eae78a55190f68a0c804

Increase precision for the Accuracy Challenge mod (#37048)
1% is not precise enough to push accuracy to high enough levels.
Increasing the precision of the mod will make it more useful for a
larger amount of players who want to push their accuracy to their
absolute limits. This does come with the caveat that it's impossible to
achieve over 99.9% accuracy on many short maps, but I don't think it
really matters if high enough settings act like the Perfect mod on short
enough maps.

Co-authored-by: evill <jlkdsf;ajfklsjd@123.n>

osu.Game/Rulesets/Mods/ModAccuracyChallenge.cs
@@ -57,8 +57,8 @@ namespace osu.Game.Rulesets.Mods
         public BindableNumber<double> MinimumAccuracy { get; } = new BindableDouble
         {
             MinValue = 0.60,
-            MaxValue = 0.99,
-            Precision = 0.01,
+            MaxValue = 0.999,
+            Precision = 0.001,
             Default = 0.9,
             Value = 0.9,
         };

commit 16882c64c5721aee53bca9ed4a917f9ac8efafca

Fix `KeyboardStep` precision in Accuracy Challenge (#37299)
- closes https://github.com/ppy/osu/issues/37179
- regressed in https://github.com/ppy/osu/pull/37048

osu.Game/Rulesets/Mods/ModAccuracyChallenge.cs
@@ -108,7 +108,7 @@ namespace osu.Game.Rulesets.Mods
     {
         public MinimumAccuracySlider()
         {
-            KeyboardStep = 0.01f;
+            KeyboardStep = 0.001f;
         }
     }
 }