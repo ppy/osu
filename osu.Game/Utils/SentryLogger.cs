ppy/osu

commit 9868106f7f4b539fa9b81a92a0e34a81272460aa

Remove sentry DSN from repo
People doing stupid shit submitting stupid errors we don't care about.

osu.Game/Utils/SentryLogger.cs
@@ -54,7 +54,7 @@ namespace osu.Game.Utils
 
             sentrySession = SentrySdk.Init(options =>
             {
-                options.Dsn = "https://ad9f78529cef40ac874afb95a9aca04e@sentry.ppy.sh/2";
+                options.Dsn = "https://localhost";
                 options.AutoSessionTracking = true;
                 options.IsEnvironmentUser = false;
                 options.IsGlobalModeEnabled = true;

commit 355724aa4b5ab5ee423a161c8e63939c60950eba

Disable sentry during test runs
Exposed to be running by 9868106f7f4b539fa9b81a92a0e34a81272460aa which
caused test failures.

You'd think that the `IsDeployedBuild` check would catch it but it
doesn't. `IsDeployedBuild` is `AssemblyVersion.Major > 0`, and the
assembly version is taken from the entry assembly. In tests the entry
assembly is either resharper or nunit.

osu.Game/Utils/SentryLogger.cs
@@ -14,6 +14,7 @@ using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
+using osu.Framework.Development;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
 using osu.Framework.Statistics;
@@ -47,6 +48,9 @@ namespace osu.Game.Utils
             this.game = game;
 
             if (Environment.GetEnvironmentVariable("OSU_DISABLE_ERROR_REPORTING") == "1")
+                return;
+
+            if (DebugUtils.IsNUnitRunning)
                 return;
 
             if (!game.IsDeployedBuild || !game.CreateEndpoints().WebsiteUrl.EndsWith(@".ppy.sh", StringComparison.Ordinal))