34ab20fc640b06cdcb44489afd6d10e786b0ba70
failaip12 <86018517+failaip12@users.noreply.github.com>
2026-04-21 03:56:27 +0200

    Fix formatting

--- osu.Game/Localisation/GeneralSettingsStrings.cs
+++ osu.Game/Localisation/GeneralSettingsStrings.cs
@@ -109,7 +109,6 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString ChangeReleaseStreamConfirmation => new TranslatableString(getKey(@"change_release_stream_confirmation"), @"Are you sure you want to run a potentially unstable version of the game?");
 
-
         /// <summary>
         /// "Speed alterations affect main menu and song select music"
         /// </summary>
