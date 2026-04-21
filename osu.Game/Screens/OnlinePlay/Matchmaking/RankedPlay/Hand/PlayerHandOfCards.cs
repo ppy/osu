f25c832d5ce6d3181d6eb64c319d860a79c14e5e
Dan Balasescu <smoogipoo@smgi.me>
2026-04-21 16:52:49 +0900

    Add more modifiers
    Co-authored-by: Bartłomiej Dach <dach.bartlomiej@gmail.com>

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Hand/PlayerHandOfCards.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Hand/PlayerHandOfCards.cs
@@ -155,7 +155,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Hand
             if (e.Repeat || Contracted || Cards.Any(static c => c.CardDragged))
                 return false;
 
-            if (e.ShiftPressed || e.ControlPressed)
+            if (e.ShiftPressed || e.ControlPressed || e.AltPressed || e.SuperPressed)
                 return false;
 
             switch (e.Key)
