ppy/osu

commit 71b3d51ed5847a6b5c9301c2f3f3d8d28ce6ccc7

Rewrite ranked play card song preview playback logic to hopefully work around framework breakage (#37453)
Similar idea to
https://github.com/ppy/osu/commit/622216d8911832c39fa4e126b2810e4e0f46cbf7
(which was included in https://github.com/ppy/osu/pull/37218).

Probably closes https://github.com/ppy/osu/issues/37420.

I wouldn't be myself if I didn't remark that the WTF/sec on ranked play
code continues to be quite high as I fix these issues. Like, look at the
old code: why does `Enabled` becoming false stop the preview track, but
`CardHovered` becoming false *doesn't*? Even though `shouldBePlaying`
was explicitly defined as derived from *both flags*? Maybe me changing
this to be actually consistent incurs a behaviour change, but like... I
can't tell if it is a bug or not.

Not to mention this nugget:


https://github.com/ppy/osu/blob/0e9664bfdfa69b4b26ff9cf84615c4b83a195a0e/osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Hand/HandOfCards.HandCard.cs#L107

https://github.com/ppy/osu/blob/0e9664bfdfa69b4b26ff9cf84615c4b83a195a0e/osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Card/RankedPlayCard.cs#L47-L50

What is this naming even?

osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Card/RankedPlayCard.SongPreview.cs
@@ -45,8 +45,6 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
 
             private readonly Container overlayLayer;
 
-            private bool shouldBePlaying => Enabled.Value && CardHovered.Value;
-
             [Resolved]
             private PreviewTrackManager previewTrackManager { get; set; } = null!;
 
@@ -77,33 +75,6 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
                 ];
             }
 
-            protected override void LoadComplete()
-            {
-                base.LoadComplete();
-
-                Enabled.BindValueChanged(enabled =>
-                {
-                    if (!enabled.NewValue)
-                    {
-                        previewTrack?.Stop();
-                        return;
-                    }
-
-                    if (shouldBePlaying)
-                    {
-                        startPreviewIfAvailable();
-                    }
-                });
-
-                CardHovered.BindValueChanged(selected =>
-                {
-                    if (selected.NewValue && shouldBePlaying)
-                    {
-                        startPreviewIfAvailable();
-                    }
-                });
-            }
-
             private PreviewTrack? previewTrack;
 
             public void LoadPreview(APIBeatmap beatmap)
@@ -126,13 +97,31 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
                     {
                         TrackRunning = { BindTarget = trackRunning }
                     });
-
-                    if (shouldBePlaying)
-                        startPreviewIfAvailable();
                 });
             }
 
-            private void startPreviewIfAvailable() => previewTrack?.Start();
+            protected override void Update()
+            {
+                base.Update();
+
+                updatePlayingState();
+            }
+
+            private void updatePlayingState()
+            {
+                if (previewTrack?.IsLoaded != true)
+                    return;
+
+                bool shouldBePlaying = Enabled.Value && CardHovered.Value;
+
+                if (shouldBePlaying == previewTrack.IsRunning)
+                    return;
+
+                if (shouldBePlaying)
+                    previewTrack.Start();
+                else
+                    previewTrack.Stop();
+            }
 
             #region IBeatSyncProvider implementation
 

commit 8e12382f072fce6cd88b12d9d29784f41e4b8c68

Fix ranked play preview tracks not stopping when entering gameplay
Closes https://github.com/ppy/osu/issues/37471 probably.

This is a dogshit change but I have no better.

This regressed in https://github.com/ppy/osu/pull/37453 because of
trying to work around the framework bug which makes attempting to start
or stop the track in certain threading scenarios kill the game.

Changing the playback logic to be `Update()`-based broke
`.StopAnyPlaying()` calls. In particular this one.

https://github.com/ppy/osu/blob/71356a9455e0738b942371e3f78134e8b14ec7ab/osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/RankedPlayScreen.cs#L361

So to fix attempt to manually check whether the ranked play screen is
still current in the update loop jank so that it doesn't restart the
track if it was stopped by the screen suspending.

How confident am I that this won't break in some eldritch threading
scenario? Not very! I can sprinkle a `StopAnyPlaying()` in
`MultiplayerPlayerLoader` if it helps. That one *should* theoretically
be safer. But also double the dogshit.

Note that I previously tried to fix the framework bug that causes all
this in https://github.com/ppy/osu-framework/pull/6727. That PR was
briefly reverted in https://github.com/ppy/osu-framework/pull/6728
because it started failing CI game-side.

Anyone thinking they can do better is cordially invited to try.

The other choice is just reverting all of my attempts and just accepting
that players on single thread shall be indiscriminately and randomly
smited for trying to play on single thread.

osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Card/RankedPlayCard.SongPreview.cs
@@ -13,6 +13,7 @@ using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Transforms;
+using osu.Framework.Screens;
 using osu.Framework.Timing;
 using osu.Game.Audio;
 using osu.Game.Beatmaps;
@@ -46,6 +47,9 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
 
             [Resolved]
             private OsuColour colours { get; set; } = null!;
+
+            [Resolved]
+            private RankedPlayScreen rankedPlayScreen { get; set; } = null!;
 
             public readonly IBindable<SongPreviewContainer?> CurrentPlayingPreview = new Bindable<SongPreviewContainer?>();
 
@@ -108,6 +112,17 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Card
             private void updatePlayingState()
             {
                 if (previewTrack?.IsLoaded != true)
+                    return;
+
+                // TODO: the ranked play screen current check should not be required. `IPreviewTrackOwner` should be handling all of this.
+                // the reason why all of this logic is so wonky and `Update()`-based is because of framework breakage that causes hard crashes
+                // in inopportune threading arrangements.
+                // read:
+                // - https://github.com/ppy/osu-framework/pull/6727
+                // - https://github.com/ppy/osu-framework/pull/6728
+                // - https://github.com/ppy/osu/pull/37218
+                // - https://github.com/ppy/osu/pull/37453
+                if (!rankedPlayScreen.IsCurrentScreen())
                     return;
 
                 bool shouldBePlaying = CurrentPlayingPreview.Value == this;