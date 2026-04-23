553251656cf3d833f561dbc88550388f6353b269
Susko3 <Susko3@protonmail.com>
2026-04-23 23:21:39 +0200

    Add pen settings

---
+++ osu.Game/Localisation/PenSettingsStrings.cs
@@ -0,0 +1,24 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Localisation
+{
+    public static class PenSettingsStrings
+    {
+        private const string prefix = @"osu.Game.Resources.Localisation.PenSettings";
+
+        /// <summary>
+        /// "Pen"
+        /// </summary>
+        public static LocalisableString Pen => new TranslatableString(getKey(@"pen"), @"Pen");
+
+        /// <summary>
+        /// "Cursor sensitivity"
+        /// </summary>
+        public static LocalisableString CursorSensitivity => new TranslatableString(getKey(@"cursor_sensitivity"), @"Cursor sensitivity");
+
+        private static string getKey(string key) => $@"{prefix}:{key}";
+    }
+}
