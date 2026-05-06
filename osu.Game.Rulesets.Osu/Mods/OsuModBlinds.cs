36d45159b1113800a0f9a96c3d14d81e24d094db
Robin Brams <robin17.brams@outlook.com>
2026-05-06 16:57:32 +0200

    Fix OsuModBlinds.cs

--- osu.Game.Rulesets.Osu/Mods/OsuModBlinds.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModBlinds.cs
@@ -13,7 +13,6 @@ using osu.Framework.Utils;
 using osu.Game.Beatmaps;
 using osu.Game.Graphics;
 using osu.Game.Localisation;
-using osu.Game.Overlays.Mods;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Scoring;
