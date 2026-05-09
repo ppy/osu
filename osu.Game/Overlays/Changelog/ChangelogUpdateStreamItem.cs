7ab7c5b6a32570909fed18a29d2245e30fae489f
Denis Titovets <den232titovets@yandex.ru>
2026-05-09 23:49:24 +0300

    Add localisation support to "users online"

--- osu.Game/Overlays/Changelog/ChangelogUpdateStreamItem.cs
+++ osu.Game/Overlays/Changelog/ChangelogUpdateStreamItem.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using Humanizer;
+using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Resources.Localisation.Web;
 using osuTK.Graphics;
 
 namespace osu.Game.Overlays.Changelog
@@ -19,7 +20,9 @@ namespace osu.Game.Overlays.Changelog
 
             MainText = Value.DisplayName;
             AdditionalText = Value.LatestBuild.DisplayVersion;
-            InfoText = Value.UserCount > 0 ? $"{"user".ToQuantity(Value.UserCount, "N0")} online" : default(LocalisableString);
+            InfoText = Value.UserCount > 0
+                ? ChangelogStrings.BuildsUsersOnline(Value.UserCount.ToLocalisableString()).ToQuantity(Value.UserCount)
+                : default(LocalisableString);
         }
 
         protected override Color4 GetBarColour(OsuColour colours) => Value.Colour;
