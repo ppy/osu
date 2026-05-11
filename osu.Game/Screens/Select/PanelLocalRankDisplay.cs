ppy/osu

commit 08c02e29b9e1b31e273ad428a80bf37b93596fcf

Fix song select scrolling performance when user has many beatmaps loaded (#37666)
Also fixes wrong rank showing briefly in some scenarios.

---

I'm quite confused why the overhead is in the post-async-filter
collection access, but it is. It occurs when using `MaxBy` (realm
snapshot creation), but also when calling `.Count` on the collection, or
even just accessing `sender[0]`. I tried everything, and ended up
settling on simplifying the realm part enough that we can do
post-filtering without much sweat or mess.

Before:


https://github.com/user-attachments/assets/59aed895-03ed-4923-9515-9a5426156f7e

After:


https://github.com/user-attachments/assets/9a37f34a-c955-45bf-877f-89f248d8ea72

Tested using [this realm](https://screvillshot.s-ul.eu/YJUJ4SR1).

- Closes https://github.com/ppy/osu/issues/37574.
- Closes https://github.com/ppy/osu/issues/37661.

osu.Game/Screens/Select/PanelLocalRankDisplay.cs
@@ -72,15 +72,12 @@ namespace osu.Game.Screens.Select
         private void updateSubscription()
         {
             scoreSubscription?.Dispose();
+            setRankFromScore(null);
 
             if (beatmap == null)
                 return;
 
-            scoreSubscription = realm.RegisterForNotifications(r =>
-                    r.GetAllLocalScoresForUser(api.LocalUser.Value.Id)
-                     .Filter($@"{nameof(ScoreInfo.BeatmapInfo)}.{nameof(BeatmapInfo.ID)} == $0"
-                             + $" && {nameof(ScoreInfo.Ruleset)}.{nameof(RulesetInfo.ShortName)} == $1", beatmap.ID, ruleset.Value.ShortName),
-                localScoresChanged);
+            scoreSubscription = realm.RegisterForNotifications(r => r.All<ScoreInfo>().Where(s => s.BeatmapHash == beatmap.Hash && !s.DeletePending), localScoresChanged);
         }
 
         private void localScoresChanged(IRealmCollection<ScoreInfo> sender, ChangeSet? changes)
@@ -90,7 +87,17 @@ namespace osu.Game.Screens.Select
             if (changes?.HasCollectionChanges() == false)
                 return;
 
-            ScoreInfo? topScore = sender.MaxBy(info => (info.TotalScore, -info.Date.UtcDateTime.Ticks));
+            ScoreInfo? topScore = sender
+                                  // doing these post realm filter is most efficient.
+                                  .Where(s => s.UserID == api.LocalUser.Value.Id || s.UserID <= 1)
+                                  .Where(s => s.Ruleset.ShortName == ruleset.Value.ShortName)
+                                  .MaxBy(info => (info.TotalScore, -info.Date.UtcDateTime.Ticks));
+
+            setRankFromScore(topScore);
+        }
+
+        private void setRankFromScore(ScoreInfo? topScore)
+        {
             updateable.Rank = topScore?.Rank;
             updateable.Alpha = topScore != null ? 1 : 0;
         }

osu.Game.Tests/Visual/SongSelect/TestScenePanelBeatmap.cs
@@ -76,6 +76,12 @@ namespace osu.Game.Tests.Visual.SongSelect
         [Test]
         public void TestLocalRank()
         {
+            AddStep("set null rank", () => this.ChildrenOfType<UpdateableRank>().ForEach(p =>
+            {
+                p.Hide();
+                p.Rank = null;
+            }));
+
             foreach (var rank in Enum.GetValues<ScoreRank>())
             {
                 AddStep($"set {rank.GetDescription()} rank", () => this.ChildrenOfType<UpdateableRank>().ForEach(p =>

osu.Game/Scoring/ScoreInfo.cs
@@ -55,6 +55,7 @@ namespace osu.Game.Scoring
         /// <summary>
         /// The <see cref="osu.Game.Beatmaps.BeatmapInfo.Hash"/> at the point in time when the score was set.
         /// </summary>
+        [Indexed]
         public string BeatmapHash { get; set; } = string.Empty;
 
         public RulesetInfo Ruleset { get; set; } = null!;

commit 6025f9379d9b03322cf5a59d199b74ff7fcb8c0f

Fix song select local ranks disappearing on beatmap downloads
Rather than a complete regression from
https://github.com/ppy/osu/pull/37666, the change I made to hide stale
ranks revealed that this subscription was being reinitialised *way* too
often.

osu.Game/Screens/Select/PanelLocalRankDisplay.cs
@@ -27,6 +27,9 @@ namespace osu.Game.Screens.Select
             get => beatmap;
             set
             {
+                if (beatmap?.Equals(value) == true)
+                    return;
+
                 beatmap = value;
 
                 if (IsLoaded)