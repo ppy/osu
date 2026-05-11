15aac9f1e90a6f2158998c1b2e9c3743e33ce2ee
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-05-11 14:06:26 +0200

    Add test coverage for disabling hyperdashes

--- osu.Game.Rulesets.Catch.Tests/Mods/TestSceneCatchModRottenFruits.cs
+++ osu.Game.Rulesets.Catch.Tests/Mods/TestSceneCatchModRottenFruits.cs
@@ -2,10 +2,13 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using osu.Framework.Testing;
 using osu.Game.Rulesets.Catch.Beatmaps;
 using osu.Game.Rulesets.Catch.Mods;
 using osu.Game.Rulesets.Catch.Objects;
+using osu.Game.Rulesets.Catch.Objects.Drawables;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Tests.Visual;
@@ -76,5 +79,62 @@ namespace osu.Game.Rulesets.Catch.Tests.Mods
             ReplayFrames = [],
             PassCondition = () => Player.ScoreProcessor.Statistics.GetValueOrDefault(HitResult.Miss) == 1,
         });
+
+        [Test]
+        public void TestHyperDashesDisabled()
+        {
+            var nonHyperDashObjects = new HashSet<HitObject>();
+
+            CreateModTest(new ModTestData
+            {
+                Mod = new CatchModRottenFruits(),
+                CreateBeatmap = () => new CatchBeatmap
+                {
+                    HitObjects =
+                    [
+                        new Fruit { StartTime = 0, X = 0 },
+                        new Fruit { StartTime = 100, X = 512 },
+                        new JuiceStream
+                        {
+                            StartTime = 200,
+                            Path = new SliderPath
+                            {
+                                ControlPoints =
+                                {
+                                    new PathControlPoint(),
+                                    new PathControlPoint(new Vector2(512, 50)),
+                                    new PathControlPoint(new Vector2(0, 100))
+                                }
+                            },
+                            SliderVelocityMultiplier = 10
+                        },
+                        new JuiceStream
+                        {
+                            StartTime = 1000,
+                            Path = new SliderPath
+                            {
+                                ControlPoints =
+                                {
+                                    new PathControlPoint(new Vector2(512, 0)),
+                                    new PathControlPoint(new Vector2(0, 50)),
+                                    new PathControlPoint(new Vector2(512, 100))
+                                }
+                            },
+                            SliderVelocityMultiplier = 10
+                        },
+                    ]
+                },
+                Autoplay = false,
+                ReplayFrames = [],
+                PassCondition = () =>
+                {
+                    nonHyperDashObjects.UnionWith(Player.DrawableRuleset
+                                                        .ChildrenOfType<DrawablePalpableCatchHitObject>()
+                                                        .Where(dho => !dho.HyperDash.Value)
+                                                        .Select(dho => dho.HitObject));
+                    return nonHyperDashObjects.Count == 20;
+                },
+            });
+        }
     }
 }
