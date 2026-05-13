61f656ddf62c48e33441fdc6274c1253a28aa9a8
Dan Balasescu <smoogipoo@smgi.me>
2026-05-13 15:32:10 +0900

    Add damage breakdown and individual multiplier

--- osu.Game/Online/Multiplayer/MatchTypes/RankedPlay/RankedPlayDamageInfo.cs
+++ osu.Game/Online/Multiplayer/MatchTypes/RankedPlay/RankedPlayDamageInfo.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 using MessagePack;
 
 namespace osu.Game.Online.Multiplayer.MatchTypes.RankedPlay
@@ -14,43 +15,99 @@ namespace osu.Game.Online.Multiplayer.MatchTypes.RankedPlay
         /// Total amount of damage dealt.
         /// </summary>
         [Key(0)]
-        public required int Damage { get; init; }
+        public int Damage { get; set; }
 
         /// <summary>
         /// Damage dealt before multipliers are applied.
         /// </summary>
         [Key(1)]
-        public required int RawDamage { get; init; }
+        public int RawDamage { get; set; }
 
         /// <summary>
         /// Life before damage was applied.
         /// </summary>
         [Key(2)]
-        public required int OldLife { get; init; }
+        public int OldLife { get; set; }
 
         /// <summary>
         /// Life after damage was applied.
         /// </summary>
         [Key(3)]
-        public required int NewLife { get; init; }
+        public int NewLife { get; set; }
+
+        /// <summary>
+        /// Describes each source of damage.
+        /// </summary>
+        [Key(4)]
+        public List<RankedPlayDamageSource> Sources { get; set; } = [];
 
         public bool Equals(RankedPlayDamageInfo? other)
         {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
+            if (other == null)
+                return false;
 
-            return Damage == other.Damage && RawDamage == other.RawDamage && OldLife == other.OldLife && NewLife == other.NewLife;
+            if (Damage != other.Damage || RawDamage != other.RawDamage || OldLife != other.OldLife || NewLife != other.NewLife)
+                return false;
+
+            if (Sources.Count != other.Sources.Count)
+                return false;
+
+            for (int i = 0; i < Sources.Count; i++)
+            {
+                if (!Sources[i].Equals(other.Sources[i]))
+                    return false;
+            }
+
+            return true;
         }
+    }
 
-        public override bool Equals(object? obj)
+    [Serializable]
+    [MessagePackObject]
+    public class RankedPlayDamageSource : IEquatable<RankedPlayDamageSource>
+    {
+        /// <summary>
+        /// The damage source.
+        /// </summary>
+        [Key(0)]
+        public RankedPlayDamageType Type { get; set; }
+
+        /// <summary>
+        /// Raw value of this damage source (e.g. multiplier, score, etc).
+        /// </summary>
+        [Key(1)]
+        public double RawValue { get; set; }
+
+        /// <summary>
+        /// Total amount of damage dealt from this source.
+        /// </summary>
+        [Key(2)]
+        public int Damage { get; set; }
+
+        public bool Equals(RankedPlayDamageSource? other)
         {
-            if (obj is null) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+            if (other == null)
+                return false;
 
-            return Equals((RankedPlayDamageInfo)obj);
+            return Type == other.Type && RawValue == other.RawValue && Damage == other.Damage;
         }
+    }
 
-        public override int GetHashCode() => HashCode.Combine(Damage, RawDamage, OldLife, NewLife);
+    public enum RankedPlayDamageType
+    {
+        /// <summary>
+        /// Score damage dealt directly by the opposing player.
+        /// </summary>
+        Attack,
+
+        /// <summary>
+        /// Damage inflicted through the damage multiplier.
+        /// </summary>
+        Multiplier,
+
+        /// <summary>
+        /// Base damage dealt for winning a round.
+        /// </summary>
+        Bonus
     }
 }
