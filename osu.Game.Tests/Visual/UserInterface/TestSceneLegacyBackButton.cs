a0e609ea4387f26985ed3d2c63c4aaa6518a2062
Salman Alshamrani <frenzibyte@gmail.com>
2026-05-14 01:31:38 +0300

    Expand back button test scene to cover beat sync

--- osu.Game.Tests/Visual/UserInterface/TestSceneLegacyBackButton.cs
+++ osu.Game.Tests/Visual/UserInterface/TestSceneLegacyBackButton.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using osu.Game.Beatmaps;
+using osu.Game.Overlays;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Skinning;
@@ -18,32 +21,73 @@ namespace osu.Game.Tests.Visual.UserInterface
         [Resolved]
         private SkinManager skins { get; set; } = null!;
 
+        [Resolved]
+        private BeatmapManager beatmaps { get; set; } = null!;
+
+        private NowPlayingOverlay overlay = null!;
+
         protected override Ruleset CreateRulesetForSkinProvider() => new OsuRuleset();
 
-        [SetUp]
-        public void SetUp() => Schedule(() =>
+        protected override void LoadComplete()
         {
-            SetContents(s =>
-            {
-                if (s is not LegacySkin)
-                    return Empty();
+            base.LoadComplete();
 
-                return new Container
-                {
-                    Size = new Vector2(300),
-                    Child = new SkinProvidingContainer(skins.DefaultClassicSkin)
-                    {
-                        Child = new SkinProvidingContainer(s)
-                        {
-                            Child = new LegacyBackButton
-                            {
-                                Anchor = Anchor.BottomLeft,
-                                Origin = Anchor.BottomLeft,
-                            }
-                        },
-                    }
-                };
+            Add(overlay = new NowPlayingOverlay
+            {
+                Anchor = Anchor.TopRight,
+                Origin = Anchor.TopRight,
             });
-        });
+        }
+
+        [SetUpSteps]
+        public void SetUpSteps()
+        {
+            AddStep("load buttons", () =>
+            {
+                SetContents(s =>
+                {
+                    if (s is not LegacySkin)
+                        return Empty();
+
+                    return new Container
+                    {
+                        Size = new Vector2(300),
+                        Child = new SkinProvidingContainer(skins.DefaultClassicSkin)
+                        {
+                            Child = new SkinProvidingContainer(s)
+                            {
+                                Child = new LegacyBackButton
+                                {
+                                    Anchor = Anchor.BottomLeft,
+                                    Origin = Anchor.BottomLeft,
+                                }
+                            },
+                        }
+                    };
+                });
+            });
+        }
+
+        [Test]
+        public void TestDisplay()
+        {
+            AddStep("do nothing", () =>
+            {
+                // for some reason TestBeat automatically runs.
+                // we don't want that.
+            });
+        }
+
+        [Test]
+        public void TestBeat()
+        {
+            AddStep("show now playing overlay", () => overlay.Show());
+            AddStep("play my love", () =>
+            {
+                Beatmap.Value = beatmaps.GetWorkingBeatmap(beatmaps.QueryOnlineBeatmapId(2868389));
+                Beatmap.Value.BeginAsyncLoad(); // need beatmap loaded for back button to access timing info
+                MusicController.EnsurePlayingSomething();
+            });
+        }
     }
 }
