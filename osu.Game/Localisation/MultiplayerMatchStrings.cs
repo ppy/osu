a49c6fbc0c49e7001c7c26ac86d6443ae53290ec
pacowoc <pacowoc@gmail.com>
2026-05-23 01:10:23 +0800

    some grammar changes

--- osu.Game/Localisation/MultiplayerMatchStrings.cs
+++ osu.Game/Localisation/MultiplayerMatchStrings.cs
@@ -50,9 +50,9 @@ namespace osu.Game.Localisation
         public static LocalisableString MatchIsReady => new TranslatableString(getKey(@"match_is_ready"), @"Your match is ready! Click to join.");
 
         /// <summary>
-        /// "Your match has been invalidated, please queue again. (Most common reason: Match timed out)"
+        /// "Your match has been invalidated by the server. Please queue again. (Most common reason: Match timed out)"
         /// </summary>
-        public static LocalisableString MatchInvalid => new TranslatableString(getKey(@"match_invalid"), @"Your match has been invalidated, please queue again. (Most common reason: Match timed out)");
+        public static LocalisableString MatchInvalid => new TranslatableString(getKey(@"match_invalid"), @"Your match has been invalidated by the server. Please queue again. (Most common reason: Match timed out)");
 
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
