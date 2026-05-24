abe80fa566dde496d9ec1389ce1f4e35fafc3171
TomyDoesThings <119363392+TomyDoesThings@users.noreply.github.com>
2026-05-24 01:08:30 -0400

    Update CatchModFirstPerson.cs: Huge overhaul and progress of code
    Drawables handle their own X position updating now and the mod now accounts for the show storyboard setting. Instead of setting X with =, now using MoveToX. Overall, should see more stability and smoothness all around.
    
    Background returning to default X after gameplay, knowing where 1.6f comes from, and more careful reviewing remains.

--- osu.Game.Rulesets.Catch/Mods/CatchModFirstPerson.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModFirstPerson.cs
@@ -3,21 +3,27 @@
 
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions.IEnumerableExtensions;
+using osu.Framework.Extensions.ObjectExtensions;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
+using osu.Framework.Screens;
 using osu.Game.Configuration;
 using osu.Game.Graphics;
+using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.UI;
+using osu.Game.Screens.Backgrounds;
 using osu.Game.Screens.Play;
+using osu.Game.Storyboards;
 using osu.Game.Storyboards.Drawables;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
-    public class CatchModFirstPerson : Mod, IApplicableToPlayer, IUpdatableByPlayfield
+    public class CatchModFirstPerson : Mod, IReadFromConfig, IApplicableToDrawableRuleset<CatchHitObject>, IApplicableToPlayer
     {
         public override string Name => "First Person";
         public override string Acronym => "FP";
@@ -27,34 +33,90 @@ namespace osu.Game.Rulesets.Catch.Mods
         public override IconUsage? Icon => OsuIcon.ModMovingFast;
         public override Type[] IncompatibleMods => new[] { typeof(ModCinema), typeof(ModRelax), typeof(ModNoScope) };
 
-        [SettingSource("Centred background", "Have the background follow.")] // From the perspective of the player
+        [SettingSource("Centred background", "Have the background follow.")] // From the perspective of the player w.r.t. catcher. Less ambiguous than e.g. "(un)adjusted"
         public BindableBool CentredBackground { get; } = new BindableBool();
 
-        [SettingSource("Centred storyboard / video", "Have the storyboard / video follow.")] // From the perspective of the player
+        [SettingSource("Centred storyboard / video", "Have the storyboard / video follow.")] // From the perspective of the player w.r.t. catcher. Less ambiguous than e.g. "(un)adjusted"
         public BindableBool CentredStoryboard { get; } = new BindableBool();
 
-        private Action<float> setBackgroundX = null!;
-        private Action<float> setStoryboardX = null!;
+        private readonly Bindable<bool> showStoryboard = new Bindable<bool>();
 
-        private const float shift_x_factor = 1.6f; // Bruteforced, is magical, todo: may need more intricate calculation
+        public void ReadFromConfig(OsuConfigManager config)
+        {
+            config.BindWith(OsuSetting.ShowStoryboard, showStoryboard);
+        }
+
+        private CatchPlayfield catchPlayfield = null!;
+
+        private float miscellaneousXOffset => shift_x_factor * catchPlayfield.X;
+        private const float shift_x_factor = 1.6f; // Brute-forced, is magical, todo: may need more intricate calculation
+
+        public void ApplyToDrawableRuleset(DrawableRuleset<CatchHitObject> drawableRuleset)
+        {
+            catchPlayfield = (CatchPlayfield)drawableRuleset.Playfield;
+
+            catchPlayfield.OnUpdate += _ => catchPlayfield.MoveToX(CatchPlayfield.CENTER_X - catchPlayfield.Catcher.X);
+        }
 
         public void ApplyToPlayer(Player player)
         {
-            setBackgroundX = x => player.ApplyToBackground(b => b.X = x);
-            setStoryboardX = x => player.DimmableStoryboard.Children.Where(c => c is DrawableStoryboard).ForEach(ds => ds.X = x);
+            Storyboard storyboard = player.GameplayState.Storyboard;
+
+            player.ApplyToBackground(bsb => bsb.OnUpdate += _ =>
+            {
+                // TODO: Make background return to default X when leaving gameplay
+
+                bool storyboardReplacesBackground = storyboard.ReplacesBackground && storyboard.HasDrawable; // Based on Player's
+                if (!storyboardReplacesBackground)
+                    updateBackgroundX(bsb);
+            });
+
+            if (!storyboard.HasDrawable)
+                return;
+
+            Drawable? drawableStoryboard = getDrawableStoryboard(player);
+
+            if (drawableStoryboard.IsNull())
+            {
+                showStoryboard.BindValueChanged(ss => Task.Run(async () => // Task.Run to not have 'async' lambda with delegate returning 'void'
+                {
+                    if (drawableStoryboard.IsNotNull() || !ss.NewValue) // Using De Morgan's for ease of understanding: if drawable storyboard still is null and show storyboard is enabled, proceed
+                        return;
+
+                    showStoryboard.UnbindBindings(); // Show storyboard being enabled even briefly during gameplay means the drawable storyboard will load
+
+                    drawableStoryboard = getDrawableStoryboard(player);
+
+                    while (drawableStoryboard.IsNull())
+                    {
+                        await Task.Delay(200).ConfigureAwait(false);
+                        drawableStoryboard = getDrawableStoryboard(player);
+
+                        if (!player.IsAlive || !player.IsCurrentScreen())
+                            return;
+                    }
+
+                    drawableStoryboard.OnUpdate += _ => updateStoryboardX(drawableStoryboard);
+                }), true);
+
+                return;
+            }
+
+            drawableStoryboard.OnUpdate += _ => updateStoryboardX(drawableStoryboard);
         }
 
-        public void Update(Playfield playfield)
+        private Drawable? getDrawableStoryboard(Player player) => player.DimmableStoryboard.Children.FirstOrDefault(c => c is DrawableStoryboard);
+
+        private void updateBackgroundX(BackgroundScreenBeatmap backgroundScreenBeatmap)
         {
-            var catchPlayfield = (CatchPlayfield)playfield;
-
-            catchPlayfield.X = CatchPlayfield.CENTER_X - catchPlayfield.Catcher.X;
-            float miscellaneousXOffset = shift_x_factor * catchPlayfield.X;
-
             if (!CentredBackground.Value)
-                setBackgroundX(miscellaneousXOffset); // TODO: Fix crashing when exiting replay (from Auto) and the leaving background at the same X when exiting until there is a background refresh
+                backgroundScreenBeatmap.MoveToX(miscellaneousXOffset);
+        }
+
+        private void updateStoryboardX(Drawable drawableStoryboard)
+        {
             if (!CentredStoryboard.Value)
-                setStoryboardX(miscellaneousXOffset);
+                drawableStoryboard.MoveToX(miscellaneousXOffset);
         }
     }
 }
