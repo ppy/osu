207d71ce0a1e24d841a66b1fbf2498e6593cc62b
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-05-27 07:55:40 +0200

    Clean up unused `RateAdjustModHelper` in `ModNightcore`
    Co-authored-by: Dean Herbert <pe@ppy.sh>

--- osu.Game/Rulesets/Mods/ModNightcore.cs
+++ osu.Game/Rulesets/Mods/ModNightcore.cs
@@ -41,13 +41,9 @@ namespace osu.Game.Rulesets.Mods
         private readonly BindableNumber<double> tempoAdjust = new BindableDouble(1);
         private readonly BindableNumber<double> freqAdjust = new BindableDouble(1);
 
-        private readonly RateAdjustModHelper rateAdjustHelper;
-
         protected ModNightcore()
         {
-            rateAdjustHelper = new RateAdjustModHelper(SpeedChange);
-
-            // intentionally not deferring the speed change handling to `RateAdjustModHelper`
+            // intentionally not using `RateAdjustModHelper`
             // as the expected result of operation is not the same (nightcore should preserve constant pitch).
             SpeedChange.BindValueChanged(val =>
             {
