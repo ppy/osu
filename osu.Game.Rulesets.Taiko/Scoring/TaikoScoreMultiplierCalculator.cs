c83737bec5de63d14754a7ee194dec9ef4fd4058
James Wilson <tsunyoku@gmail.com>
2026-06-05 06:57:23 +0100

    Implement new score multipliers (#37967)
    - Part of https://github.com/ppy/osu/issues/37818
    
    Updates score multipliers to latest proposals, and bumps replay version
    ready for recalculation.
    
    ---
    
    For anyone following along, **please wait for the incoming news post
    before sharing this publicly**. This is intended for internal
    development use at this point. There's still some time before these go
    live.
    
    ---------
    
    Co-authored-by: Bartłomiej Dach <dach.bartlomiej@gmail.com>

--- osu.Game.Rulesets.Taiko/Scoring/TaikoScoreMultiplierCalculator.cs
+++ osu.Game.Rulesets.Taiko/Scoring/TaikoScoreMultiplierCalculator.cs
@@ -4,6 +4,7 @@
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Rulesets.Taiko.Mods;
+using osu.Game.Scoring;
 
 namespace osu.Game.Rulesets.Taiko.Scoring
 {
@@ -39,7 +40,7 @@ namespace osu.Game.Rulesets.Taiko.Scoring
 
             // Random
             Single<TaikoModDifficultyAdjust>(hasMultiplier: 0.5);
-            Single<TaikoModClassic>(hasMultiplier: 0.96);
+            Single<TaikoModClassic>(hasMultiplier: _ => classicMultiplier(Context.Score));
             // Swap
             // Single Tap
             Single<TaikoModConstantSpeed>(hasMultiplier: 0.9);
@@ -82,6 +83,14 @@ namespace osu.Game.Rulesets.Taiko.Scoring
                 return 1 + value / 5;
             else
                 return 0.6 + value;
+        }
+
+        private static double classicMultiplier(ScoreInfo? score)
+        {
+            if (score != null && score.TotalScoreVersion < 30000017)
+                return 0.96;
+
+            return 1;
         }
     }
 }
