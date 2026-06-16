83e3d54995d95371a848ecad31771a81160456e1
tsunyoku <tsunyoku@gmail.com>
2026-06-16 17:42:20 +0100

    Add `Serializable` property

--- osu.Desktop/IPC/Messages/OsuWebSocketMessage.cs
+++ osu.Desktop/IPC/Messages/OsuWebSocketMessage.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Text.Json.Serialization;
 using osu.Framework.Extensions.TypeExtensions;
 
 namespace osu.Desktop.IPC.Messages
 {
+    [Serializable]
     public abstract class OsuWebSocketMessage
     {
         [JsonPropertyName("type")]
