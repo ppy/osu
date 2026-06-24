9e9ac51ab806c5ebfdeccd3e8ae679f98c62fcdf
Salman Alshamrani <frenzibyte@gmail.com>
2026-06-24 06:11:42 -0400

    Rewrite back button position and size specifications one more time

--- osu.Game/Skinning/Select/LegacyNewBackButtonPiece.cs
+++ osu.Game/Skinning/Select/LegacyNewBackButtonPiece.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
@@ -16,6 +17,10 @@ using osuTK.Graphics;
 
 namespace osu.Game.Skinning.Select
 {
+    /// <summary>
+    /// Represents a port of the default osu!stable back button.
+    /// </summary>
+    /// <seealso href="https://github.com/peppy/osu-stable-reference/blob/c34a74fb61c17c5667486a12548485d1f03baa2e/osu!/Graphics/UserInterface/BackButton.cs">osu!stable reference source</seealso>
     public partial class LegacyNewBackButtonPiece : BeatSyncedContainer
     {
         private static readonly Color4 idle_colour = new Color4(238, 51, 153, 255);
@@ -29,7 +34,12 @@ namespace osu.Game.Skinning.Select
         private OsuSpriteText text = null!;
         private Sprite rightLayer = null!;
 
-        private const float vertical_offset = 22;
+        private const float back_text_size = 15 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR;
+        private const float vertical_offset = 22 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR;
+
+        // 0.775x is an arbitrary adjustment to better match visually.
+        // on testing with stable this factor is only relevant to fontawesome icon sizes.
+        private const float back_icon_size = 18 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR * 0.775f;
 
         [BackgroundDependencyLoader]
         private void load(SkinManager skins)
@@ -50,8 +60,8 @@ namespace osu.Game.Skinning.Select
                     Anchor = Anchor.BottomLeft,
                     Origin = Anchor.Centre,
                     Text = @"back",
-                    Font = OsuFont.Default.With(size: 15 * 1.6f),
-                    Y = -vertical_offset * 1.6f,
+                    Font = OsuFont.Default.With(size: back_text_size),
+                    Y = -vertical_offset,
                     UseFullGlyphHeight = false,
                 },
                 leftLayer = new Sprite
@@ -66,8 +76,8 @@ namespace osu.Game.Skinning.Select
                     Anchor = Anchor.BottomLeft,
                     Origin = Anchor.Centre,
                     Icon = FontAwesome.Solid.ChevronCircleLeft,
-                    Size = new Vector2(22),
-                    Y = -vertical_offset * 1.6f,
+                    Size = new Vector2(back_icon_size),
+                    Y = -vertical_offset,
                     Shadow = true,
                 },
             };
@@ -111,34 +121,34 @@ namespace osu.Game.Skinning.Select
                 contract(600, Easing.OutElastic);
         }
 
-        // stable reference: https://github.com/peppy/osu-stable-reference/blob/c34a74fb61c17c5667486a12548485d1f03baa2e/osu!/Graphics/UserInterface/BackButton.cs#L18-L28
-        // some numbers are manually adjusted to better match stable.
-
         private const float centre_offset = 2.4f;
-        private const float text_allowance = 6 * 1.6f;
+
+        private const float left_layer_idle = 22 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR + 2; // +2 to better match stable
+        private const float left_layer_active = 40 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR + 3; // +3 to better match stable
+
+        private const float right_layer_idle = 60 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR + 5; // +5 to better match stable
+        private const float right_layer_active = 86 * LegacyManiaSkinConfiguration.POSITION_SCALE_FACTOR + 7; // +7 to better match stable
+
+        private const float back_text_width = 45; // determined via draw visualizer
+
+        private static readonly float back_text_allowance = Math.Max(0, back_text_width - (right_layer_idle - left_layer_idle));
 
         private void expand(double duration, Easing easing)
         {
-            const float left_layer_active = 40 * 1.6f;
-            const float right_layer_active = 86 * 1.6f;
-
             leftLayer.MoveToX(left_layer_active, duration, easing);
             leftLayer.FadeColour(active_colour, duration, easing);
             icon.MoveToX(left_layer_active / centre_offset, duration, easing);
-            rightLayer.MoveToX(right_layer_active + text_allowance, duration, easing);
-            text.MoveToX((right_layer_active - left_layer_active + text_allowance) / centre_offset + left_layer_active, duration, easing);
+            rightLayer.MoveToX(right_layer_active + back_text_allowance, duration, easing);
+            text.MoveToX((right_layer_active - left_layer_active + back_text_allowance) / centre_offset + left_layer_active, duration, easing);
         }
 
         private void contract(double duration, Easing easing)
         {
-            const float left_layer_idle = 25 * 1.6f;
-            const float right_layer_idle = 60 * 1.6f;
-
             leftLayer.MoveToX(left_layer_idle, duration, easing);
             leftLayer.FadeColour(idle_colour, duration, easing);
             icon.MoveToX(left_layer_idle / centre_offset, duration, easing);
-            rightLayer.MoveToX(right_layer_idle + text_allowance, duration, easing);
-            text.MoveToX((right_layer_idle - left_layer_idle + text_allowance) / centre_offset + left_layer_idle, duration, easing);
+            rightLayer.MoveToX(right_layer_idle + back_text_allowance, duration, easing);
+            text.MoveToX((right_layer_idle - left_layer_idle + back_text_allowance) / centre_offset + left_layer_idle, duration, easing);
         }
 
         protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
