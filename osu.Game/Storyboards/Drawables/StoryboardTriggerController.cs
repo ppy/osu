3d72487a07d9426a62d723882161398c8b6957c7
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-07-24 10:04:54 +0200

    First draft of hit sound trigger parsing & matching logic

--- osu.Game/Storyboards/Drawables/StoryboardTriggerController.cs
+++ osu.Game/Storyboards/Drawables/StoryboardTriggerController.cs
@@ -1,11 +1,15 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text.RegularExpressions;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
+using osu.Game.Audio;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Screens.Play;
@@ -71,6 +75,76 @@ namespace osu.Game.Storyboards.Drawables
                     playTrigger(drawable, triggerGroup);
             });
         }
+
+        #region Hit sample triggers
+
+        private struct HitSampleTriggerDefinition
+        {
+            public string? NormalBank { get; set; }
+            public string? AdditionBank { get; set; }
+            public string? Name { get; set; }
+            public string? Suffix { get; set; }
+
+            public string CanonicalName => $@"HitSound{NormalBank}{AdditionBank}{Name}{Suffix}";
+
+            private static readonly Regex parse_regex = new Regex(
+                @"(?i)^HitSound(?<bank1>(All|Normal|Soft|Drum))?(?<bank2>(All|Normal|Soft|Drum))?(?<name>(Whistle|Clap|Finish))?(?<suffix>\d+)?$",
+                RegexOptions.Compiled);
+
+            public bool TryParse(string triggerName, [NotNullWhen(true)] out HitSampleTriggerDefinition? result)
+            {
+                var match = parse_regex.Match(triggerName);
+
+                if (!match.Success)
+                {
+                    result = null;
+                    return false;
+                }
+
+                string? name = match.Groups[@"name"].Success ? match.Groups[@"name"].Value : null;
+                string? bank1 = match.Groups[@"bank1"].Success ? match.Groups[@"bank1"].Value : null;
+                string? bank2 = match.Groups[@"bank2"].Success ? match.Groups[@"bank2"].Value : null;
+                string? suffix = match.Groups[@"suffix"].Success ? match.Groups[@"suffix"].Value : null;
+
+                bool bank1IsAddition = bank1 != null && bank2 == null && name != null;
+
+                var trigger = new HitSampleTriggerDefinition
+                {
+                    Name = name,
+                    NormalBank = bank1IsAddition ? bank2 : bank1,
+                    AdditionBank = bank1IsAddition ? bank1 : bank2,
+                    Suffix = suffix,
+                };
+
+                if (!string.Equals(triggerName, trigger.CanonicalName, StringComparison.OrdinalIgnoreCase))
+                {
+                    result = null;
+                    return false;
+                }
+
+                result = trigger;
+                return true;
+            }
+
+            public bool Matches(HitSampleInfo hitSampleInfo)
+            {
+                if (Name != null && !string.Equals(Name, hitSampleInfo.Name, StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                if (NormalBank != null && (hitSampleInfo.Name != HitSampleInfo.HIT_NORMAL || !string.Equals(hitSampleInfo.Bank, NormalBank, StringComparison.OrdinalIgnoreCase)))
+                    return false;
+
+                if (AdditionBank != null && (hitSampleInfo.Name == HitSampleInfo.HIT_NORMAL || !string.Equals(hitSampleInfo.Bank, AdditionBank, StringComparison.OrdinalIgnoreCase)))
+                    return false;
+
+                if (Suffix != null && !string.Equals(Name, hitSampleInfo.Name, StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                return true;
+            }
+        }
+
+        #endregion
 
         private static void playTrigger<TDrawable>(TDrawable drawable, StoryboardTriggerGroup triggerGroup)
             where TDrawable : Drawable, IFlippable, IVectorScalable
