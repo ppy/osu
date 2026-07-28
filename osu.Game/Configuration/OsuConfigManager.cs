356618b7cbf3fc7c962391be5010cbbd88808f18
andylovescode <andytk2010@gmail.com>
2026-07-28 10:34:52 -0700

    Settings: Reduce minimum parallax to 0

--- osu.Game/Configuration/OsuConfigManager.cs
+++ osu.Game/Configuration/OsuConfigManager.cs
@@ -135,7 +135,7 @@ namespace osu.Game.Configuration
 #pragma warning disable CS0612 // Type or member is obsolete (setting default value to avoid risk of any future crashes)
             SetDefault(OsuSetting.MenuParallax, true);
 #pragma warning restore CS0612 // Type or member is obsolete
-            SetDefault(OsuSetting.MenuParallaxScale, 1.0f, 0.1f, 4f, 0.01f);
+            SetDefault(OsuSetting.MenuParallaxScale, 1.0f, 0.0f, 4f, 0.01f);
 
             // See https://stackoverflow.com/a/63307411 for default sourcing.
             SetDefault(OsuSetting.Prefer24HourTime, !CultureInfoHelper.SystemCulture.DateTimeFormat.ShortTimePattern.Contains(@"tt"));
