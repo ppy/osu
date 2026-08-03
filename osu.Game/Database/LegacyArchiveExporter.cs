d7cdac2bfe70ca6d92f34405b10389602926680a
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-08-03 14:55:44 +0200

    Throw when attempting to create ZIP archive with duplicated filenames in entries (#38507)
    This came up [in a `osu-server-beatmap-submission` sentry
    alert](https://sentry.ppy.sh/organizations/ppy/issues/146593).
    
    In this case the server is correct to reject the submission, and there's
    not really anything the server should be doing, but that begs the
    question how this happened in the first place. I have no answer to that,
    as `RealmFileStore.AddFile()` appears to be correctly compensating for
    the same with the same name already existing.
    
    So for now this is intended to stop the chain of failure one level
    earlier, which at least probably provides slightly better messaging, and
    maybe someone will bother to report this better with reproduction steps,
    at which point maybe the actual primary bug can get fixed too.

--- osu.Game/Database/LegacyArchiveExporter.cs
+++ osu.Game/Database/LegacyArchiveExporter.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -46,10 +48,14 @@ namespace osu.Game.Database
                 int i = 0;
                 int fileCount = model.Files.Count();
                 bool anyFileMissing = false;
+                HashSet<string> filesWritten = [];
 
                 foreach (var file in model.Files)
                 {
                     cancellationToken.ThrowIfCancellationRequested();
+
+                    if (filesWritten.Contains(file.Filename))
+                        throw new InvalidOperationException($"Error when exporting {model.GetDisplayString()}: Multiple files specify filename of {file.Filename}");
 
                     using (var stream = GetFileContents(model, file))
                     {
@@ -61,6 +67,7 @@ namespace osu.Game.Database
                         }
 
                         writer.Write(file.Filename, stream);
+                        filesWritten.Add(file.Filename);
                     }
 
                     i++;
