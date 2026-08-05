aab89e65e87f24b259eacb8ab7a0c867158bc159
piiid <piiidosu24@gmail.com>
2026-08-06 08:15:45 +1200

    remove unnecessary Using directives

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Reading.cs
@@ -2,9 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Utils;
 using osu.Game.Rulesets.Difficulty.Aggregation;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
