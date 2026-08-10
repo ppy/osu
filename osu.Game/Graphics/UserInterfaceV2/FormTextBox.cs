60c4adb6c7a2fdd796648c55a9cf4ce8407ca300
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-08-10 08:33:20 +0200

    Fix text clipping on form controls
    Closes https://github.com/ppy/osu/issues/38558.

--- osu.Game/Graphics/UserInterfaceV2/FormTextBox.cs
+++ osu.Game/Graphics/UserInterfaceV2/FormTextBox.cs
@@ -219,6 +219,7 @@ namespace osu.Game.Graphics.UserInterfaceV2
                 Height = 16;
                 TextContainer.Height = 1;
                 BackgroundUnfocused = BackgroundFocused = BackgroundCommit = Colour4.Transparent;
+                Masking = false;
             }
 
             protected override SpriteText CreatePlaceholder() => base.CreatePlaceholder().With(t => t.Margin = default);
