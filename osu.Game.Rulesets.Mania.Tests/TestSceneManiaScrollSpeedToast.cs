cba1b1c52a2532f54a37cbbc837afbddf99024d5
Adrian <adrian@edelen.haus>
2026-08-10 18:12:06 -0400

    updated visual test to check for actual increase/decrease actions rather than just any value

--- osu.Game.Rulesets.Mania.Tests/TestSceneManiaScrollSpeedToast.cs
+++ osu.Game.Rulesets.Mania.Tests/TestSceneManiaScrollSpeedToast.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Testing;
+using osu.Game.Input.Bindings;
 using osu.Game.Overlays;
 using osu.Game.Overlays.OSD;
 using osu.Game.Rulesets.Mania.Configuration;
@@ -34,9 +36,20 @@ namespace osu.Game.Rulesets.Mania.Tests
         [Test]
         public void TestScrollSpeedToastShowsShortcut()
         {
+            var lookedUpActions = new List<GlobalAction>();
+
+            AddStep("set lookup spy", () =>
+                config.LookupKeyBindings = action =>
+                {
+                    lookedUpActions.Add(action);
+                    return action.ToString();
+                });
+
             AddStep("increase scroll speed", () => config.SetValue(ManiaRulesetSetting.ScrollSpeed, config.Get<double>(ManiaRulesetSetting.ScrollSpeed) + 1));
             AddUntilStep("wait for toast", () => osd.ChildrenOfType<TrackedSettingToast>().Any());
-            AddAssert("toast shortcut is not empty", () => osd.ChildrenOfType<TrackedSettingToast>().Single().ExtraText != default);
+            AddAssert("shortcut uses increase and decrease actions", () =>
+                lookedUpActions.Contains(GlobalAction.IncreaseScrollSpeed) &&
+                lookedUpActions.Contains(GlobalAction.DecreaseScrollSpeed));
         }
 
         private partial class TestOnScreenDisplay : OnScreenDisplay
