1b75a44f63dfa123ae67dee6e5d1c09ec8b7f861
StanR <hi@stanr.info>
2026-08-17 14:08:39 +0500

    Rework osu! Speed skill to consider stamina better

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -5,13 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Difficulty.Aggregation;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Difficulty.Utils;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators.Speed;
-using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Mods;
-using osu.Game.Rulesets.Osu.Objects;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
@@ -22,7 +22,11 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     {
         private readonly List<double> sliderStrains = new List<double>();
 
-        private double currentStrain;
+        private double currentBurstStrain;
+        private double currentStreamStrain;
+        private double currentStaminaStrain;
+        private double currentRhythm;
+
         private double harmonicWeightSum;
 
         public Speed(Mod[] mods)
@@ -30,34 +34,54 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         {
         }
 
-        private double strainDecay(double ms) => DiffUtils.Pow(0.3, ms / 1000);
+        private double strainDecayBurst(double ms) => DiffUtils.Pow(0.1, ms / 1000);
+        private double strainDecayStream(double ms) => DiffUtils.Pow(0.01, DiffUtils.Pow(ms / 1000, 1.6));
+
+        private double strainDecayStamina(double ms, double staminaValue)
+        {
+            double changeFactor = currentStaminaStrain > 0 ? 1 + DiffUtils.Pow(currentStaminaStrain / (staminaValue + currentStaminaStrain), 25.0) : 1.0;
+            return DiffUtils.Pow(0.05, DiffUtils.Pow(ms * changeFactor / 1000, 3.5));
+        }
 
         protected override double ProcessInternal(DifficultyHitObject current)
         {
-            const double skill_multiplier = 1.16;
+            const double total_multiplier = 0.81;
+            const double burst_multiplier = 2.31;
+            const double stream_multiplier = 0.16;
+            const double stamina_multiplier = 0.028;
+            const double mean_exponent = 1.25;
 
             if (Mods.Any(m => m is OsuModRelax))
                 return 0;
 
-            double decay = strainDecay(((OsuDifficultyHitObject)current).AdjustedDeltaTime);
+            double burstDifficulty = adjustDifficulty(SpeedEvaluator.EvaluateDifficultyOf(current));
+            double staminaDifficulty = adjustDifficulty(StaminaEvaluator.EvaluateDifficultyOf(current));
+            double rhythmDifficulty = RhythmEvaluator.EvaluateDifficultyOf(current);
 
-            currentStrain *= decay;
-            currentStrain += calculateAdjustedDifficulty(current) * (1 - decay) * skill_multiplier;
+            currentRhythm = rhythmDifficulty;
 
-            double currentRhythm = RhythmEvaluator.EvaluateDifficultyOf(current);
+            currentBurstStrain *= strainDecayBurst(((OsuDifficultyHitObject)current).AdjustedDeltaTime);
+            currentBurstStrain += burstDifficulty * burst_multiplier;
 
-            double totalStrain = currentStrain * currentRhythm;
+            currentStreamStrain *= strainDecayStream(((OsuDifficultyHitObject)current).AdjustedDeltaTime);
+            currentStreamStrain += staminaDifficulty * stream_multiplier;
+
+            currentStaminaStrain *= strainDecayStamina(((OsuDifficultyHitObject)current).AdjustedDeltaTime, staminaDifficulty * stamina_multiplier);
+            currentStaminaStrain += staminaDifficulty * stamina_multiplier;
+
+            double totalValue = DiffUtils.Norm(mean_exponent,
+                currentBurstStrain * currentRhythm,
+                currentStreamStrain,
+                currentStaminaStrain) * total_multiplier;
 
             if (current.BaseObject is Slider)
-                sliderStrains.Add(totalStrain);
+                sliderStrains.Add(totalValue);
 
-            return totalStrain;
+            return totalValue;
         }
 
-        private double calculateAdjustedDifficulty(DifficultyHitObject current)
+        private double adjustDifficulty(double difficulty)
         {
-            double difficulty = SpeedEvaluator.EvaluateDifficultyOf(current);
-
             if (Mods.Any(m => m is OsuModAutopilot))
                 difficulty *= 0.5;
 
@@ -69,7 +93,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             if (ObjectDifficulties.Count == 0)
                 return 0;
 
-            (double difficulty, harmonicWeightSum) = HarmonicSeries.Aggregate(ObjectDifficulties, harmonicScale: 20);
+            (double difficulty, harmonicWeightSum) = HarmonicSeries.Aggregate(ObjectDifficulties, harmonicScale: 15);
 
             return difficulty;
         }
