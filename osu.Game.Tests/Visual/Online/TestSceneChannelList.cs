fbee3df7432b7070b2760264418b19d74c45b6b1
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-09-11 08:59:59 +0200

    Remove unused using

--- osu.Game.Tests/Visual/Online/TestSceneChannelList.cs
+++ osu.Game.Tests/Visual/Online/TestSceneChannelList.cs
@@ -8,7 +8,6 @@ using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Testing;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Chat;
