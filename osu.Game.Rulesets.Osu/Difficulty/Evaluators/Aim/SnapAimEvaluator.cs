e666f7c47e5b030201ed2fc0617cd2d12dc94832
kwotaq <PJXORISJ@gmail.com>
2026-09-12 22:25:09 +0300

    rebalancge

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
@@ -91,7 +91,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
         private static double calculateWideAngleBonus(OsuDifficultyHitObject osuCurrObj, OsuDifficultyHitObject osuLastObj,
                                                       double currDistance, double prevDistance, bool withSliderTravelDistance)
         {
-            const double wide_angle_multiplier = 9.0;
+            const double wide_angle_multiplier = 8.5;
 
             if (osuCurrObj.Angle == null || osuLastObj.Angle == null)
                 return 0;
