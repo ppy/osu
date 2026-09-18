ppy/osu

commit 693a0ba736801eff765d5625eaa66f9c8be842e1

Allow reporting messages in ranked play chat (#38841)
Supersedes #37617.

This PR includes an assortment of cleanups which have been split out of
the main commit for clarity:

# [Localise chat report confirmation
strings](https://github.com/ppy/osu/commit/9eb124b19bd13ea9ab19e14406652c0ad4e4b003)

Self explanatory.

# [Make MessageBubble take in the whole message
object](https://github.com/ppy/osu/commit/3a553bacaa7e4b44578c71211cd215694f0f87bb)

Needed to have access to the entire `Message` object in order to create
a `ReportChatDialog`.

# [Move handling new messages to
BubbleChatHistory](https://github.com/ppy/osu/commit/2eb465dd83b2285be7318688a0fe50069c012bee)

This was done in order to prevent the same bug as in
https://github.com/ppy/osu/pull/38833, where the report confirmation
message would be sent to the wrong channel. I needed to cache the
channel object so I could later [use it inside of
`MessageBubble`](https://github.com/ppy/osu/commit/0332000c3be00a06c3d782ab9d06242cfb01dd0c#diff-64ea00a0037698f1234902a3535721ff4a170129e50d8fe04ba0bebccb88b33bR495),
however the instance of it in `RankedPlayChatDisplay` is nullable as the
channel needs to be joined first. Hence moving the responsibility for
the channel to `BubbleChatHistory` where it can be non-nullable and
properly thrown into DI.

Also I think it makes more sense as it matches the responsibility split
between `StandAloneChatDisplay` and `DrawableChannel` better.

---

This PR adds a context menu to the chat messages in ranked play,
allowing the specific messages to be reported:


[Screencast_20260916_231713.webm](https://github.com/user-attachments/assets/8ded551c-1157-424c-bb4e-50ced8fc8099)

I used the [purple colour which the background takes before it's been
any of the user's
turn](https://github.com/ppy/osu/blob/master/osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/RankedPlayBackground.cs#L19)
as the colour of the info messages, as it seems like a good 'neutral'

osu.Game.Tests/Visual/RankedPlay/TestSceneRankedPlayChat.cs
@@ -6,13 +6,20 @@ using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Testing;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Online.API;
+using osu.Game.Online.API.Requests;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Chat;
 using osu.Game.Online.Multiplayer.MatchTypes.RankedPlay;
 using osu.Game.Online.Rooms;
+using osu.Game.Overlays.Chat;
 using osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay;
 using osu.Game.Tests.Visual.Multiplayer;
+using osuTK.Input;
 
 namespace osu.Game.Tests.Visual.RankedPlay
 {
@@ -20,7 +27,10 @@ namespace osu.Game.Tests.Visual.RankedPlay
     {
         private ChannelManager channelManager = null!;
         private Channel testChannel = null!;
+        private int channelIdSequence;
         private int messageIdSequence;
+
+        private DummyAPIAccess dummyAPI => (DummyAPIAccess)API;
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
         {
@@ -37,7 +47,7 @@ namespace osu.Game.Tests.Visual.RankedPlay
         public void SetUp() => Schedule(() =>
         {
             messageIdSequence = 0;
-            testChannel = channelManager.JoinChannel(new Channel { Id = 1, Type = ChannelType.Multiplayer });
+            testChannel = channelManager.JoinChannel(new Channel { Id = ++channelIdSequence, Type = ChannelType.Multiplayer });
         });
 
         public override void SetUpSteps()
@@ -47,7 +57,7 @@ namespace osu.Game.Tests.Visual.RankedPlay
             AddStep("join room", () =>
             {
                 var room = CreateDefaultRoom(MatchType.RankedPlay);
-                room.ChannelId = 1;
+                room.ChannelId = channelIdSequence;
                 JoinRoom(room);
             });
 
@@ -100,6 +110,52 @@ namespace osu.Game.Tests.Visual.RankedPlay
                     }
                 }
             }).WaitSafely());
+        }
+
+        [Test]
+        public void TestReport()
+        {
+            ReportChatDialog dialog = null!;
+
+            AddStep("setup request handling", () =>
+            {
+                dummyAPI.HandleRequest += request =>
+                {
+                    if (request is ChatReportRequest chatReportRequest)
+                    {
+                        chatReportRequest.TriggerSuccess();
+                        return true;
+                    }
+
+                    return false;
+                };
+            });
+
+            AddStep("set pick state", () => MultiplayerClient.RankedPlayChangeStage(RankedPlayStage.CardPlay, state => state.ActiveUserId = API.LocalUser.Value.OnlineID).WaitSafely());
+            postOpponentMessage("wangs");
+
+            AddStep("show chat history", () =>
+            {
+                InputManager.MoveMouseTo(this.ChildrenOfType<StandAloneChatDisplay.ChatTextBox>().Single());
+                InputManager.Click(MouseButton.Left);
+            });
+
+            AddStep("right click message", () =>
+            {
+                InputManager.MoveMouseTo(this.ChildrenOfType<OsuSpriteText>().First(t => t.Text == "wangs"));
+                InputManager.Click(MouseButton.Right);
+            });
+            AddStep("Select report option", () =>
+            {
+                InputManager.MoveMouseTo(this.ChildrenOfType<Menu.DrawableMenuItem>().First(m => m.Item.Text.ToString() == "Report"));
+                InputManager.Click(MouseButton.Left);
+            });
+            AddAssert("report dialog is present", () => (dialog = this.ChildrenOfType<ReportChatDialog>().Single()).IsPresent, () => Is.True);
+
+            AddStep("input reason", () => dialog.ChildrenOfType<OsuTextBox>().First().Text = "reason");
+            AddStep("send report", () => DialogOverlay.CurrentDialog!.PerformAction<ReportChatDialog.SubmitButton>());
+
+            AddUntilStep("Info message displayed", () => testChannel.Messages.Last(), Is.InstanceOf<InfoMessage>);
         }
 
         private void postLocalUserMessage(string content)

osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Components/RankedPlayChatDisplay.cs
@@ -10,31 +10,40 @@ using osu.Framework.Audio.Sample;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
 using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Events;
 using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Cursor;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Input;
 using osu.Game.Input.Bindings;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Online.Chat;
 using osu.Game.Online.Multiplayer;
+using osu.Game.Overlays;
+using osu.Game.Overlays.Chat;
 using osu.Game.Resources.Localisation.Web;
 using osu.Game.Users.Drawables;
 using osuTK;
 using osuTK.Graphics;
+using ClientChatStrings = osu.Game.Localisation.ChatStrings;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
 {
-    public partial class RankedPlayChatDisplay : VisibilityContainer, IKeyBindingHandler<GlobalAction>
+    public partial class RankedPlayChatDisplay : VisibilityContainer, IKeyBindingHandler<GlobalAction>, IFocusManager
     {
         [Resolved]
         private ChannelManager? channelManager { get; set; }
 
         [Resolved]
         private RealmKeyBindingStore keyBindingStore { get; set; } = null!;
+
+        private IFocusManager parentFocusManager = null!;
 
         private readonly MultiplayerRoom room;
 
@@ -43,11 +52,12 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
 
         private Channel? channel;
 
-        private const float width = 320;
+        private const float chatbox_width = 320;
 
         public RankedPlayChatDisplay(MultiplayerRoom room)
         {
-            Size = new Vector2(width, 160);
+            RelativeSizeAxes = Axes.Y;
+            Width = chatbox_width * 1.5f;
             this.room = room;
         }
 
@@ -60,7 +70,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 {
                     Anchor = Anchor.BottomRight,
                     Origin = Anchor.BottomRight,
-                    RelativeSizeAxes = Axes.X,
+                    Width = chatbox_width,
                     Height = 30,
                     CornerRadius = 10,
                     ReleaseFocusOnCommit = true,
@@ -68,18 +78,6 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                     Focus = onFocusGained,
                     FocusLost = onFocusLost
                 },
-                new Container
-                {
-                    Anchor = Anchor.BottomCentre,
-                    Origin = Anchor.BottomCentre,
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Padding = new MarginPadding { Bottom = 35 },
-                    Child = chatHistory = new BubbleChatHistory
-                    {
-                        RelativeSizeAxes = Axes.X
-                    }
-                }
             };
         }
 
@@ -87,17 +85,30 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
         {
             base.LoadComplete();
 
+            parentFocusManager = GetContainingFocusManager()!;
+
             resetPlaceholderText();
             textbox.OnCommit += onCommit;
 
             channel = channelManager?.JoinChannel(new Channel { Id = room.ChannelID, Type = ChannelType.Multiplayer, Name = $"#lazermp_{room.RoomID}" });
 
-            if (channel != null)
-            {
-                channel.NewMessagesArrived += onNewMessagesArrived;
+            if (channel == null) return;
 
-                textbox.Current.BindTo(channel.TextBoxMessage);
-            }
+            textbox.Current.BindTo(channel.TextBoxMessage);
+
+            AddInternal(new ChatContextMenuContainer
+            {
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                RelativeSizeAxes = Axes.Both,
+                Padding = new MarginPadding { Bottom = 35 },
+                Child = chatHistory = new BubbleChatHistory(channel)
+                {
+                    Anchor = Anchor.BottomRight,
+                    Origin = Anchor.BottomRight,
+                    RelativeSizeAxes = Axes.X
+                },
+            });
         }
 
         private void onCommit(TextBox sender, bool newText)
@@ -113,12 +124,6 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 channelManager?.PostMessage(text, target: channel);
 
             textbox.Text = string.Empty;
-        }
-
-        private void onNewMessagesArrived(IEnumerable<Message> bundle)
-        {
-            foreach (var message in bundle)
-                chatHistory.PostMessage(message.Sender, message.Content);
         }
 
         private void onFocusGained()
@@ -168,6 +173,20 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
         {
         }
 
+        public void TriggerFocusContention(Drawable? triggerSource)
+        {
+            if (triggerSource == null || triggerSource.IsRootedAt(chatHistory))
+                parentFocusManager.TriggerFocusContention(triggerSource);
+        }
+
+        public bool ChangeFocus(Drawable? potentialFocusTarget)
+        {
+            if (potentialFocusTarget == null || potentialFocusTarget.IsRootedAt(chatHistory))
+                return parentFocusManager.ChangeFocus(potentialFocusTarget);
+
+            return false;
+        }
+
         protected override void PopIn()
         {
             FinishTransforms();
@@ -191,10 +210,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
             base.Dispose(isDisposing);
 
             if (channel != null)
-            {
-                channel.NewMessagesArrived -= onNewMessagesArrived;
                 textbox.Current.UnbindFrom(channel.TextBoxMessage);
-            }
         }
 
         private partial class ChatTextBox : StandAloneChatDisplay.ChatTextBox
@@ -206,6 +222,15 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 BackgroundFocused = Colour4.FromHex("222228");
                 BackgroundUnfocused = BackgroundFocused.Opacity(0.7f);
                 Placeholder.Colour = Color4.White;
+            }
+        }
+
+        private partial class ChatContextMenuContainer : OsuContextMenuContainer
+        {
+            public ChatContextMenuContainer()
+            {
+                Content.Anchor = Anchor.BottomRight;
+                Content.Origin = Anchor.BottomRight;
             }
         }
 
@@ -226,6 +251,9 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
             /// </summary>
             private const float time_before_disappear = 5000;
 
+            [Cached]
+            private readonly Channel channel;
+
             private readonly Container<MessageBubble> messageContainer;
 
             private bool expanded;
@@ -233,9 +261,11 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
             private Sample messageReceivedSample = null!;
             private double? lastSamplePlayback;
 
-            public BubbleChatHistory()
+            public BubbleChatHistory(Channel channel)
             {
                 AutoSizeAxes = Axes.Y;
+
+                this.channel = channel;
 
                 InternalChild = messageContainer = new Container<MessageBubble>
                 {
@@ -248,6 +278,8 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
             private void load(AudioManager audio)
             {
                 messageReceivedSample = audio.Samples.Get(@"Multiplayer/Matchmaking/Ranked/message");
+
+                channel.NewMessagesArrived += onNewMessagesArrived;
             }
 
             /// <summary>
@@ -279,53 +311,51 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                     child.Show();
             }
 
-            /// <summary>
-            /// Posts a message.
-            /// </summary>
-            /// <param name="user">The user that posted the message.</param>
-            /// <param name="content">The message content.</param>
-            public void PostMessage(APIUser user, string content)
+            private void onNewMessagesArrived(IEnumerable<Message> bundle)
             {
-                var newMessage = new MessageBubble(user, content)
+                foreach (var message in bundle)
                 {
-                    Anchor = Anchor.BottomRight,
-                    Origin = Anchor.BottomRight,
-                    PostTime = Time.Current
-                };
-
-                messageContainer.Add(newMessage);
-
-                float offset = 0;
-
-                ScheduleAfterChildren(() =>
-                {
-                    // Layout bubbles, pushing all others upwards to make room for the new one.
-                    foreach (var child in messageContainer.Reverse())
+                    var newMessage = new MessageBubble(message)
                     {
-                        child.MoveToY(-offset, 400, Easing.OutPow10);
-                        offset += child.DrawHeight + message_spacing;
+                        Anchor = Anchor.BottomRight,
+                        Origin = Anchor.BottomRight,
+                        PostTime = Time.Current
+                    };
+
+                    messageContainer.Add(newMessage);
+
+                    float offset = 0;
+
+                    ScheduleAfterChildren(() =>
+                    {
+                        // Layout bubbles, pushing all others upwards to make room for the new one.
+                        foreach (var child in messageContainer.Reverse())
+                        {
+                            child.MoveToY(-offset, 400, Easing.OutPow10);
+                            offset += child.DrawHeight + message_spacing;
+                        }
+                    });
+
+                    // Hide any overflowing message.
+                    // Only need to handle the most-recently-overflowing one, because others would be handled in prior calls to this method.
+                    if (messageContainer.Count > max_length)
+                    {
+                        var lastBubble = messageContainer[messageContainer.Count - max_length - 1];
+
+                        lastBubble.Hide();
+                        lastBubble.Expire();
                     }
-                });
 
-                // Hide any overflowing message.
-                // Only need to handle the most-recently-overflowing one, because others would be handled in prior calls to this method.
-                if (messageContainer.Count > max_length)
-                {
-                    var lastBubble = messageContainer[messageContainer.Count - max_length - 1];
+                    newMessage.Show();
 
-                    lastBubble.Hide();
-                    lastBubble.Expire();
-                }
+                    playSample();
 
-                newMessage.Show();
-
-                playSample();
-
-                // If not in the expanded state, hide the new message after a short while.
-                if (!expanded)
-                {
-                    using (BeginDelayedSequence(time_before_disappear))
-                        newMessage.Hide();
+                    // If not in the expanded state, hide the new message after a short while.
+                    if (!expanded)
+                    {
+                        using (BeginDelayedSequence(time_before_disappear))
+                            newMessage.Hide();
+                    }
                 }
             }
 
@@ -338,19 +368,24 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 lastSamplePlayback = Time.Current;
             }
 
-            private partial class MessageBubble : CompositeDrawable
+            protected override void Dispose(bool isDisposing)
             {
-                private readonly APIUser user;
-                private readonly string message;
+                base.Dispose(isDisposing);
+
+                channel.NewMessagesArrived -= onNewMessagesArrived;
+            }
+
+            private partial class MessageBubble : CompositeDrawable, IHasContextMenu
+            {
+                private readonly Message message;
 
                 /// <summary>
                 /// The time at which this message was posted.
                 /// </summary>
                 public required double PostTime { get; init; }
 
-                public MessageBubble(APIUser user, string message)
+                public MessageBubble(Message message)
                 {
-                    this.user = user;
                     this.message = message;
                     AutoSizeAxes = Axes.Both;
 
@@ -360,6 +395,15 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
 
                 [Resolved]
                 private IAPIProvider api { get; set; } = null!;
+
+                [Resolved]
+                private Channel channel { get; set; } = null!;
+
+                [Resolved]
+                private IDialogOverlay? dialogOverlay { get; set; }
+
+                private const int padding = 8;
+                private const int avatar_offset = 20;
 
                 [BackgroundDependencyLoader]
                 private void load()
@@ -376,14 +420,12 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                                 new Box
                                 {
                                     RelativeSizeAxes = Axes.Both,
-                                    Colour = api.LocalUser.Value.Id == user.Id
-                                        ? RankedPlayColourScheme.BLUE.PrimaryDarkest
-                                        : RankedPlayColourScheme.RED.PrimaryDarkest,
+                                    Colour = getColour(),
                                 },
                                 new Container
                                 {
                                     AutoSizeAxes = Axes.Both,
-                                    Padding = new MarginPadding(8),
+                                    Padding = new MarginPadding(padding),
                                     Children = new Drawable[]
                                     {
                                         new CircularContainer
@@ -392,7 +434,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                                             Origin = Anchor.CentreLeft,
                                             Size = new Vector2(16),
                                             Masking = true,
-                                            Child = new UpdateableAvatar(user)
+                                            Child = new UpdateableAvatar(message.Sender)
                                             {
                                                 DelayedLoad = false,
                                                 RelativeSizeAxes = Axes.Both
@@ -400,12 +442,12 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                                         },
                                         new OsuTextFlowContainer
                                         {
-                                            X = 20,
-                                            MaximumSize = new Vector2(width * 1.5f, 0),
+                                            X = avatar_offset,
+                                            MaximumSize = new Vector2(chatbox_width * 1.5f - avatar_offset - padding * 2, 0),
                                             Anchor = Anchor.CentreLeft,
                                             Origin = Anchor.CentreLeft,
                                             AutoSizeAxes = Axes.Both,
-                                            Text = message,
+                                            Text = message.Content,
                                         }
                                     }
                                 }
@@ -423,6 +465,37 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 public override void Hide()
                 {
                     this.FadeOut(200, Easing.OutQuint);
+                }
+
+                private Colour4 getColour()
+                {
+                    if (message is InfoMessage)
+                        return Color4Extensions.FromHex("#240d36");
+
+                    if (message.Sender.Id == api.LocalUser.Value.Id)
+                        return RankedPlayColourScheme.BLUE.PrimaryDarkest;
+
+                    return RankedPlayColourScheme.RED.PrimaryDarkest;
+                }
+
+                public MenuItem[]? ContextMenuItems
+                {
+                    get
+                    {
+                        if (message.Sender.Equals(APIUser.SYSTEM_USER))
+                            return null;
+
+                        if (message.Sender.Equals(api.LocalUser.Value))
+                            return null;
+
+                        return
+                        [
+                            new OsuMenuItem(UsersStrings.ReportButtonText, MenuItemType.Destructive, () => dialogOverlay?.Push(new ReportChatDialog(message)
+                            {
+                                Success = () => channel.AddNewMessages(new InfoMessage(ClientChatStrings.ReportConfirmation)),
+                            }))
+                        ];
+                    }
                 }
             }
         }

osu.Game.Tests/Visual/RankedPlay/TestSceneBubbleChatHistory.cs
@@ -4,6 +4,7 @@
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Game.Online.API.Requests.Responses;
+using osu.Game.Online.Chat;
 using osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components;
 
 namespace osu.Game.Tests.Visual.RankedPlay
@@ -11,11 +12,12 @@ namespace osu.Game.Tests.Visual.RankedPlay
     public partial class TestSceneBubbleChatHistory : OsuTestScene
     {
         private RankedPlayChatDisplay.BubbleChatHistory history = null!;
+        private Channel testChannel = null!;
 
         [SetUp]
         public void Setup() => Schedule(() =>
         {
-            Child = history = new RankedPlayChatDisplay.BubbleChatHistory
+            Child = history = new RankedPlayChatDisplay.BubbleChatHistory(testChannel = new Channel())
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.BottomCentre,
@@ -27,7 +29,11 @@ namespace osu.Game.Tests.Visual.RankedPlay
         public void TestPostMessages()
         {
             int messageId = 1;
-            AddRepeatStep("post message", () => history.PostMessage(new APIUser { Id = 2 }, $"message {messageId++}"), 20);
+            AddRepeatStep("post message", () => testChannel.AddNewMessages(new Message
+            {
+                Sender = new APIUser { Id = 2 },
+                Content = $"message {messageId++}",
+            }), 20);
         }
 
         [Test]
@@ -38,7 +44,13 @@ namespace osu.Game.Tests.Visual.RankedPlay
             AddStep("post some messages", () =>
             {
                 for (int i = 0; i < 10; i++)
-                    history.PostMessage(new APIUser { Id = 2 }, $"message {i}");
+                {
+                    testChannel.AddNewMessages(new Message
+                    {
+                        Sender = new APIUser { Id = 2 },
+                        Content = $"message {i}",
+                    });
+                }
             });
 
             AddWaitStep("wait a bit", 10);

commit b02fb46afba7d8cb8c837f13aaf5ed6d5e1e47f4

Fix ranked play chat box not getting focus on keyboard input
Regressed with https://github.com/ppy/osu/pull/38841.

osu.Game.Tests/Visual/RankedPlay/TestSceneRankedPlayChat.cs
@@ -10,6 +10,7 @@ using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Testing;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
+using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests;
 using osu.Game.Online.API.Requests.Responses;
@@ -69,12 +70,25 @@ namespace osu.Game.Tests.Visual.RankedPlay
         }
 
         [Test]
+        public void TestFocusChat()
+        {
+            AddStep("set discard phase", () => MultiplayerClient.RankedPlayChangeStage(RankedPlayStage.CardDiscard).WaitSafely());
+
+            AddUntilStep("chat not focused", () => this.ChildrenOfType<StandAloneChatDisplay.ChatTextBox>().SingleOrDefault()?.HasFocus, () => Is.False);
+
+            AddStep("press enter", () => InputManager.Key(Key.Enter));
+
+            AddUntilStep("chat is focused", () => this.ChildrenOfType<StandAloneChatDisplay.ChatTextBox>().Single().HasFocus, () => Is.True);
+        }
+
+        [Test]
         public void TestDiscardCardStage()
         {
             AddStep("set discard phase", () => MultiplayerClient.RankedPlayChangeStage(RankedPlayStage.CardDiscard).WaitSafely());
 
             postLocalUserMessage("this is a message from the local user");
-            postOpponentMessage("this is a message from the opponent. your opponent has a lot to say about you. nice stuff, of course. they see your potential in this game and want to shower you with compliments.");
+            postOpponentMessage(
+                "this is a message from the opponent. your opponent has a lot to say about you. nice stuff, of course. they see your potential in this game and want to shower you with compliments.");
         }
 
         [Test]
@@ -153,7 +167,7 @@ namespace osu.Game.Tests.Visual.RankedPlay
             AddAssert("report dialog is present", () => (dialog = this.ChildrenOfType<ReportChatDialog>().Single()).IsPresent, () => Is.True);
 
             AddStep("input reason", () => dialog.ChildrenOfType<OsuTextBox>().First().Text = "reason");
-            AddStep("send report", () => DialogOverlay.CurrentDialog!.PerformAction<ReportChatDialog.SubmitButton>());
+            AddStep("send report", () => DialogOverlay.CurrentDialog!.PerformAction<ReportDialog<ChatReportReason>.SubmitButton>());
 
             AddUntilStep("Info message displayed", () => testChannel.Messages.Last(), Is.InstanceOf<InfoMessage>);
         }

osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Components/RankedPlayChatDisplay.cs
@@ -159,7 +159,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Components
                 case GlobalAction.ToggleChatFocus:
                     if (!textbox.HasFocus)
                     {
-                        Schedule(() => textbox.TakeFocus());
+                        Schedule(() => parentFocusManager.ChangeFocus(textbox));
                         return true;
                     }
 