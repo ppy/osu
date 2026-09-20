2bd319c7688f439f2159f8a179d107018b7b6fc3
StanR <hi@stanr.info>
2026-09-20 15:53:51 +0500

    Calculate delta time of stacked objects together in agility

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AgilityEvaluator.cs
@@ -15,12 +15,19 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
         /// </summary>
         public static double EvaluateDifficultyOf(DifficultyHitObject current)
         {
+            const double previous_delta_influence = 0.75;
+
             if (current.BaseObject is Spinner)
                 return 0;
 
             var osuCurrObj = (OsuDifficultyHitObject)current;
+            var osuPrevObj = (OsuDifficultyHitObject?)current.Previous();
 
-            double agilityDifficulty = DiffUtils.Pow(1000 / osuCurrObj.AdjustedDeltaTime, 2);
+            // For objects that are stacked we want to reduce the agility difficulty slightly by combining delta times of both objects since we can assume that they would be done in one movement.
+            double previousDelta = osuPrevObj?.AdjustedDeltaTime * DiffUtils.ReverseLerp(osuPrevObj?.LazyJumpDistance ?? 0, OsuDifficultyHitObject.NORMALISED_RADIUS, 0) ?? 0;
+            double combinedDelta = osuCurrObj.AdjustedDeltaTime + previousDelta * previous_delta_influence;
+
+            double agilityDifficulty = DiffUtils.Pow(1000 / combinedDelta, 2);
 
             agilityDifficulty *= DiffUtils.Pow(osuCurrObj.SmallCircleBonus, 1.5);
 
