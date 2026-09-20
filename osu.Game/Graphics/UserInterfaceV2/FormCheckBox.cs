072178bc7271e8c6e4c9e57a821c575d3eef5e03
Joseph Madamba <madamba.joehu@outlook.com>
2026-09-20 14:52:52 -0700

    Fix xmldoc copy paste error

--- osu.Game/Graphics/UserInterfaceV2/FormCheckBox.cs
+++ osu.Game/Graphics/UserInterfaceV2/FormCheckBox.cs
@@ -30,12 +30,12 @@ namespace osu.Game.Graphics.UserInterfaceV2
         private readonly BindableWithCurrent<bool> current = new BindableWithCurrent<bool>();
 
         /// <summary>
-        /// Caption describing this slider bar, displayed on top of the controls.
+        /// Caption describing this control, displayed on top of the controls.
         /// </summary>
         public LocalisableString Caption { get; init; }
 
         /// <summary>
-        /// Hint text containing an extended description of this slider bar, displayed in a tooltip when hovering the caption.
+        /// Hint text containing an extended description of this control, displayed in a tooltip when hovering the caption.
         /// </summary>
         public LocalisableString HintText { get; init; }
 
--- osu.Game/Graphics/UserInterfaceV2/FormTextBox.cs
+++ osu.Game/Graphics/UserInterfaceV2/FormTextBox.cs
@@ -59,12 +59,12 @@ namespace osu.Game.Graphics.UserInterfaceV2
         private readonly BindableWithCurrent<string> current = new BindableWithCurrent<string>();
 
         /// <summary>
-        /// Caption describing this slider bar, displayed on top of the controls.
+        /// Caption describing this control, displayed on top of the controls.
         /// </summary>
         public LocalisableString Caption { get; init; }
 
         /// <summary>
-        /// Hint text containing an extended description of this slider bar, displayed in a tooltip when hovering the caption.
+        /// Hint text containing an extended description of this control, displayed in a tooltip when hovering the caption.
         /// </summary>
         public LocalisableString HintText { get; init; }
 
