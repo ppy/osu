0e17229087665cf30e2dc7eff4307b3296e315e9
Wakka <rw24013y@st.omu.ac.jp>
2026-09-21 17:24:56 +0900

    fix InspectCode scripts after nvika removal (#38857)
    Fix `InspectCode.sh` and `InspectCode.ps1` failing because `nvika` is no
    longer available.
    
    `nvika` was removed from `.config/dotnet-tools.json` in #37031, but both
    scripts still invoke it./
    
    ---------
    
    Co-authored-by: Bartłomiej Dach <dach.bartlomiej@gmail.com>

--- InspectCode.sh
+++ InspectCode.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 
 dotnet tool restore
+# clean is required to ensure all code style errors are (re-)raised by compiler
+dotnet clean ./osu.Desktop.slnf --verbosity=q
+dotnet build -c Debug -warnaserror ./osu.Desktop.slnf -p:EnforceCodeStyleInBuild=true
 dotnet CodeFileSanity
-dotnet jb inspectcode "osu.Desktop.slnf" --no-build --output="inspectcodereport.xml" --caches-home="inspectcode" --verbosity=WARN
-dotnet nvika parsereport "inspectcodereport.xml" --treatwarningsaserrors
+dotnet jb inspectcode "osu.Desktop.slnf" --no-build --format=Text --stdout --caches-home="inspectcode" --verbosity=WARN
