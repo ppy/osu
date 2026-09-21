ppy/osu

commit 1b488949e13568c23faa0d88b18c8036f4a7dbc8

Localise some more `PopupDialog`s (#36890)
changes can be reviewed commit by commit

---------

Co-authored-by: Dean Herbert <pe@ppy.sh>

osu.Game/Localisation/DialogStrings.cs
@@ -49,6 +49,102 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString StableDirectoryLocationCancelButton => new TranslatableString(getKey(@"stable_directory_location_cancel_button"), @"Actually I don't have osu!stable installed.");
 
+        /// <summary>
+        /// "All local scores on {0}"
+        /// </summary>
+        public static LocalisableString BeatmapClearScoresBodyText(string difficulty) => new TranslatableString(getKey(@"beatmap_clear_scores_body_text"), @"All local scores on {0}", difficulty);
+
+        /// <summary>
+        /// "Are you sure you want to close the following playlist:"
+        /// </summary>
+        public static LocalisableString ClosePlaylistHeaderText => new TranslatableString(getKey(@"close_playlist_header_text"), @"Are you sure you want to close the following playlist:");
+
+        /// <summary>
+        /// "Are you sure you want to abort the match?"
+        /// </summary>
+        public static LocalisableString ConfirmAbortMatchHeaderText => new TranslatableString(getKey(@"confirm_abort_match_header_text"), @"Are you sure you want to abort the match?");
+
+        /// <summary>
+        /// "Are you sure you want to exit osu!?"
+        /// </summary>
+        public static LocalisableString ConfirmExitHeaderText => new TranslatableString(getKey(@"confirm_exit_header_text"), @"Are you sure you want to exit osu!?");
+
+        /// <summary>
+        /// "Last chance to turn back"
+        /// </summary>
+        public static LocalisableString ConfirmDialogBodyText => new TranslatableString(getKey(@"confirm_exit_body_text"), @"Last chance to turn back");
+
+        /// <summary>
+        /// "There are currently some background operations which will be aborted if you continue:
+        ///
+        /// {0}"
+        /// </summary>
+        public static LocalisableString ConfirmExitBodyTextOngoingOperations(string ongoingOperationsText) => new TranslatableString(getKey(@"confirm_exit_body_text_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
+
+{0}", ongoingOperationsText);
+
+        /// <summary>
+        /// "There are currently some background operations which will be aborted if you continue:
+        ///
+        /// {0}
+        ///
+        /// and {1} other operation(s)."
+        /// </summary>
+        public static LocalisableString ConfirmExitBodyTextOtherOngoingOperations(string ongoingOperationsText, int count) => new TranslatableString(getKey(@"confirm_exit_body_text_other_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
+
+{0}
+
+and {1} other operation(s).", ongoingOperationsText, count);
+
+        /// <summary>
+        /// "Let me out!"
+        /// </summary>
+        public static LocalisableString ConfirmExitOkButton => new TranslatableString(getKey(@"confirm_exit_ok_button"), @"Let me out!");
+
+        /// <summary>
+        /// "Just a little more..."
+        /// </summary>
+        public static LocalisableString ConfirmExitCancelButton => new TranslatableString(getKey(@"confirm_exit_cancel_button"), @"Just a little more...");
+
+        /// <summary>
+        /// "Are you sure you want to go back?"
+        /// </summary>
+        public static LocalisableString ConfirmDiscardChangesHeaderText => new TranslatableString(getKey(@"confirm_discard_changes_header_text"), @"Are you sure you want to go back?");
+
+        /// <summary>
+        /// "This will discard any unsaved changes"
+        /// </summary>
+        public static LocalisableString ConfirmDiscardChangesBodyText => new TranslatableString(getKey(@"confirm_discard_changes_body_text"), @"This will discard any unsaved changes");
+
+        /// <summary>
+        /// "No I didn't mean to"
+        /// </summary>
+        public static LocalisableString ConfirmDiscardChangesCancelButton => new TranslatableString(getKey(@"confirm_discard_changes_cancel_button"), @"No I didn't mean to");
+
+        /// <summary>
+        /// "Are you sure you want to open the following link in a web browser?
+        ///
+        /// {0}"
+        /// </summary>
+        public static LocalisableString ExternalLinkBodyText(string url) => new TranslatableString(getKey(@"external_link_body_text"), @"Are you sure you want to open the following link in a web browser?
+
+{0}", url);
+
+        /// <summary>
+        /// "Open in browser"
+        /// </summary>
+        public static LocalisableString ExternalLinkOkButton => new TranslatableString(getKey(@"external_link_ok_button"), @"Open in browser");
+
+        /// <summary>
+        /// "Do you really want to delete your comment?"
+        /// </summary>
+        public static LocalisableString DeleteCommentBodyText => new TranslatableString(getKey(@"delete_comment_body_text"), @"Do you really want to delete your comment?");
+
+        /// <summary>
+        /// "Are you sure you want to leave this multiplayer match?"
+        /// </summary>
+        public static LocalisableString ConfirmExitMultiplayerMatchBodyText => new TranslatableString(getKey(@"confirm_exit_multiplayer_match_body_text"), @"Are you sure you want to leave this multiplayer match?");
+
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }

osu.Game/Screens/Menu/ConfirmExitDialog.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Localisation;
 using osu.Game.Localisation;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Dialog;
@@ -30,31 +31,29 @@ namespace osu.Game.Screens.Menu
         [BackgroundDependencyLoader]
         private void load(INotificationOverlay notifications)
         {
-            HeaderText = "Are you sure you want to exit osu!?";
+            HeaderText = DialogStrings.ConfirmExitHeaderText;
 
             Icon = FontAwesome.Solid.ExclamationTriangle;
 
             if (notifications.HasOngoingOperations)
             {
-                string text = "There are currently some background operations which will be aborted if you continue:\n\n";
-
                 var ongoingOperations = notifications.OngoingOperations.ToArray();
+                string ongoingOperationsText = ongoingOperations.Take(10).Aggregate(string.Empty, (current, n) => current + $"{n.Text} ({n.Progress:0%})\n");
 
-                foreach (var n in ongoingOperations.Take(10))
-                    text += $"{n.Text} ({n.Progress:0%})\n";
+                LocalisableString ongoingOperationsLocalisableString;
 
                 if (ongoingOperations.Length > 10)
-                    text += $"\nand {ongoingOperations.Length - 10} other operation(s).\n";
+                    ongoingOperationsLocalisableString = DialogStrings.ConfirmExitBodyTextOtherOngoingOperations(ongoingOperationsText, ongoingOperations.Length - 10);
+                else
+                    ongoingOperationsLocalisableString = DialogStrings.ConfirmExitBodyTextOngoingOperations(ongoingOperationsText);
 
-                text += "\nLast chance to turn back";
-
-                BodyText = text;
+                BodyText = LocalisableString.Interpolate($"{ongoingOperationsLocalisableString}\n\n{DialogStrings.ConfirmDialogBodyText}");
 
                 Buttons = new PopupDialogButton[]
                 {
                     new PopupDialogDangerousButton
                     {
-                        Text = @"Let me out!",
+                        Text = DialogStrings.ConfirmExitOkButton,
                         Action = onConfirm
                     },
                     new PopupDialogCancelButton
@@ -66,18 +65,18 @@ namespace osu.Game.Screens.Menu
             }
             else
             {
-                BodyText = "Last chance to turn back";
+                BodyText = DialogStrings.ConfirmDialogBodyText;
 
                 Buttons = new PopupDialogButton[]
                 {
                     new PopupDialogOkButton
                     {
-                        Text = @"Let me out!",
+                        Text = DialogStrings.ConfirmExitOkButton,
                         Action = onConfirm
                     },
                     new PopupDialogCancelButton
                     {
-                        Text = @"Just a little more...",
+                        Text = DialogStrings.ConfirmExitCancelButton,
                         Action = onCancel
                     },
                 };

osu.Game/Localisation/ButtonSystemStrings.cs
@@ -93,6 +93,11 @@ Your experience will not be perfect, and may even feel subpar compared to games 
 
 Please bear with us as we continue to improve the game for you!");
 
+        /// <summary>
+        /// "Understood"
+        /// </summary>
+        public static LocalisableString MobileDisclaimerOkButton => new TranslatableString(getKey(@"mobile_disclaimer_ok_button"), @"Understood");
+
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }

osu.Game/Localisation/EditorDialogsStrings.cs
@@ -59,6 +59,26 @@ namespace osu.Game.Localisation
         /// </summary>
         public static LocalisableString DiscardUnsavedChangesDialogHeader => new TranslatableString(getKey(@"discard_unsaved_changes_dialog_header"), @"Discard all unsaved changes? This cannot be undone.");
 
+        /// <summary>
+        /// "The beatmap will be saved to continue with this operation."
+        /// </summary>
+        public static LocalisableString SaveRequiredDialogHeader => new TranslatableString(getKey(@"save_required_dialog_header"), @"The beatmap will be saved to continue with this operation.");
+
+        /// <summary>
+        /// "Sounds good, let&#39;s go!"
+        /// </summary>
+        public static LocalisableString SoundsGood => new TranslatableString(getKey(@"sounds_good"), @"Sounds good, let's go!");
+
+        /// <summary>
+        /// "Difficulty &quot;{0}&quot; with {1} objects"
+        /// </summary>
+        public static LocalisableString DeleteDifficultyDetails(string difficultyName, int objectCount) => new TranslatableString(getKey(@"delete_difficulty_details"), @"Difficulty ""{0}"" with {1} objects", difficultyName, objectCount);
+
+        /// <summary>
+        /// "All Bookmarks"
+        /// </summary>
+        public static LocalisableString AllBookmarks => new TranslatableString(getKey(@"all_bookmarks"), @"All Bookmarks");
+
         private static string getKey(string key) => $@"{prefix}:{key}";
     }
 }

osu.Game/Online/Chat/ExternalLinkOpener.cs
@@ -98,7 +98,7 @@ namespace osu.Game.Online.Chat
             public ExternalLinkDialog(string url, Action openExternalLinkAction, Action copyExternalLinkAction)
             {
                 HeaderText = DialogStrings.CautionHeaderText;
-                BodyText = $"Are you sure you want to open the following link in a web browser?\n\n{url}";
+                BodyText = DialogStrings.ExternalLinkBodyText(url);
 
                 Icon = FontAwesome.Solid.ExclamationTriangle;
 
@@ -106,7 +106,7 @@ namespace osu.Game.Online.Chat
                 {
                     new PopupDialogOkButton
                     {
-                        Text = @"Open in browser",
+                        Text = DialogStrings.ExternalLinkOkButton,
                         Action = openExternalLinkAction
                     },
                     new PopupDialogCancelButton

osu.Game/Overlays/Comments/DrawableComment.cs
@@ -21,11 +21,12 @@ using System.Diagnostics;
 using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Logging;
 using osu.Game.Graphics.UserInterface;
+using osu.Game.Localisation;
 using osu.Game.Online.API;
 using osu.Game.Online.API.Requests;
 using osu.Game.Overlays.Comments.Buttons;
 using osu.Game.Overlays.Dialog;
-using osu.Game.Resources.Localisation.Web;
+using WebCommonStrings = osu.Game.Resources.Localisation.Web.CommonStrings;
 
 namespace osu.Game.Overlays.Comments
 {
@@ -324,13 +325,13 @@ namespace osu.Game.Overlays.Comments
             if (WasDeleted)
                 makeDeleted();
 
-            actionsContainer.AddLink(CommonStrings.ButtonsPermalink, () => game?.CopyToClipboard($@"{api.Endpoints.APIUrl}/comments/{Comment.Id}"));
+            actionsContainer.AddLink(WebCommonStrings.ButtonsPermalink, () => game?.CopyToClipboard($@"{api.Endpoints.APIUrl}/comments/{Comment.Id}"));
             actionsContainer.AddArbitraryDrawable(Empty().With(d => d.Width = 10));
-            actionsContainer.AddLink(CommonStrings.ButtonsReply.ToLower(), toggleReply);
+            actionsContainer.AddLink(WebCommonStrings.ButtonsReply.ToLower(), toggleReply);
             actionsContainer.AddArbitraryDrawable(Empty().With(d => d.Width = 10));
 
             if (Comment.UserId.HasValue && Comment.UserId.Value == api.LocalUser.Value.Id)
-                actionsContainer.AddLink(CommonStrings.ButtonsDelete.ToLower(), deleteComment);
+                actionsContainer.AddLink(WebCommonStrings.ButtonsDelete.ToLower(), deleteComment);
             else
                 actionsContainer.AddArbitraryDrawable(new CommentReportButton(Comment));
 
@@ -384,7 +385,7 @@ namespace osu.Game.Overlays.Comments
             if (dialogOverlay == null)
                 deleteCommentRequest();
             else
-                dialogOverlay.Push(new ConfirmDialog("Do you really want to delete your comment?", deleteCommentRequest));
+                dialogOverlay.Push(new ConfirmDialog(DialogStrings.DeleteCommentBodyText, deleteCommentRequest));
         }
 
         /// <summary>

osu.Game/Overlays/Dialog/ConfirmDialog.cs
@@ -6,7 +6,8 @@
 using System;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
-using osu.Game.Resources.Localisation.Web;
+using osu.Game.Localisation;
+using WebCommonStrings = osu.Game.Resources.Localisation.Web.CommonStrings;
 
 namespace osu.Game.Overlays.Dialog
 {
@@ -24,7 +25,7 @@ namespace osu.Game.Overlays.Dialog
         public ConfirmDialog(LocalisableString message, Action onConfirm, Action onCancel = null)
         {
             HeaderText = message;
-            BodyText = "Last chance to turn back";
+            BodyText = DialogStrings.ConfirmDialogBodyText;
 
             Icon = FontAwesome.Solid.ExclamationTriangle;
 
@@ -32,12 +33,12 @@ namespace osu.Game.Overlays.Dialog
             {
                 new PopupDialogOkButton
                 {
-                    Text = @"Yes",
+                    Text = DialogStrings.Confirm,
                     Action = onConfirm
                 },
                 new PopupDialogCancelButton
                 {
-                    Text = CommonStrings.ButtonsCancel,
+                    Text = WebCommonStrings.ButtonsCancel,
                     Action = onCancel
                 },
             };

osu.Game/Screens/Edit/BookmarkResetDialog.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Game.Localisation;
 using osu.Game.Overlays.Dialog;
 
 namespace osu.Game.Screens.Edit
@@ -13,7 +14,7 @@ namespace osu.Game.Screens.Edit
         public BookmarkResetDialog(EditorBeatmap editorBeatmap)
         {
             editor = editorBeatmap;
-            BodyText = "All Bookmarks";
+            BodyText = EditorDialogsStrings.AllBookmarks;
         }
 
         [BackgroundDependencyLoader]
@@ -23,4 +24,3 @@ namespace osu.Game.Screens.Edit
         }
     }
 }
-

commit 0f7d7a01439a1011732a7250a291dd33d81cb728

Fix `ConfirmExitDialog` localisation (#38858)
- regressed in https://github.com/ppy/osu/pull/36890

not sure what i was thinking before:
```cs
string ongoingOperationsText = ongoingOperations.Take(10).Aggregate(string.Empty, (current, n) => current + $"{n.Text} ({n.Progress:0%})\n");
```

| master | PR |
|-|-|
| <img width="1920" height="1080" alt="osu_2026-09-18_17-37-01"
src="https://github.com/user-attachments/assets/d251452d-92a8-4e94-bb84-9b2a15672a97"
/> | <img width="1366" height="768" alt="osu_2026-09-18_17-36-13"
src="https://github.com/user-attachments/assets/579b659b-4e2d-463b-a29e-2c68c81aed77"
/> |

osu.Game/Localisation/DialogStrings.cs
@@ -79,7 +79,7 @@ namespace osu.Game.Localisation
         ///
         /// {0}"
         /// </summary>
-        public static LocalisableString ConfirmExitBodyTextOngoingOperations(string ongoingOperationsText) => new TranslatableString(getKey(@"confirm_exit_body_text_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
+        public static LocalisableString ConfirmExitBodyTextOngoingOperations(LocalisableString ongoingOperationsText) => new TranslatableString(getKey(@"confirm_exit_body_text_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
 
 {0}", ongoingOperationsText);
 
@@ -90,7 +90,7 @@ namespace osu.Game.Localisation
         ///
         /// and {1} other operation(s)."
         /// </summary>
-        public static LocalisableString ConfirmExitBodyTextOtherOngoingOperations(string ongoingOperationsText, int count) => new TranslatableString(getKey(@"confirm_exit_body_text_other_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
+        public static LocalisableString ConfirmExitBodyTextOtherOngoingOperations(LocalisableString ongoingOperationsText, int count) => new TranslatableString(getKey(@"confirm_exit_body_text_other_ongoing_operations"), @"There are currently some background operations which will be aborted if you continue:
 
 {0}
 

osu.Game/Screens/Menu/ConfirmExitDialog.cs
@@ -9,6 +9,7 @@ using osu.Framework.Localisation;
 using osu.Game.Localisation;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Dialog;
+using osu.Game.Overlays.Notifications;
 
 namespace osu.Game.Screens.Menu
 {
@@ -31,30 +32,25 @@ namespace osu.Game.Screens.Menu
         [BackgroundDependencyLoader]
         private void load(INotificationOverlay notifications)
         {
-            HeaderText = DialogStrings.ConfirmExitHeaderText;
-
             Icon = FontAwesome.Solid.ExclamationTriangle;
+            HeaderText = DialogStrings.ConfirmExitHeaderText;
 
             if (notifications.HasOngoingOperations)
             {
                 var ongoingOperations = notifications.OngoingOperations.ToArray();
-                string ongoingOperationsText = ongoingOperations.Take(10).Aggregate(string.Empty, (current, n) =>
+                LocalisableString ongoingOperationsText = ongoingOperations.Take(10).Aggregate<ProgressNotification, LocalisableString>(string.Empty, (current, n) =>
                 {
                     if (n.Progress > 0)
-                        return current + $"{n.Text} ({n.Progress:0%})\n";
+                        return LocalisableString.Interpolate($"{current}\n{n.Text} ({n.Progress:0%})");
 
-                    return current + $"{n.Text}\n";
+                    return LocalisableString.Interpolate($"{current}\n{n.Text}");
                 });
 
-                LocalisableString ongoingOperationsLocalisableString;
+                var dialogText = ongoingOperations.Length > 10
+                    ? DialogStrings.ConfirmExitBodyTextOtherOngoingOperations(ongoingOperationsText, ongoingOperations.Length - 10)
+                    : DialogStrings.ConfirmExitBodyTextOngoingOperations(ongoingOperationsText);
 
-                if (ongoingOperations.Length > 10)
-                    ongoingOperationsLocalisableString = DialogStrings.ConfirmExitBodyTextOtherOngoingOperations(ongoingOperationsText, ongoingOperations.Length - 10);
-                else
-                    ongoingOperationsLocalisableString = DialogStrings.ConfirmExitBodyTextOngoingOperations(ongoingOperationsText);
-
-                BodyText = LocalisableString.Interpolate($"{ongoingOperationsLocalisableString}\n\n{DialogStrings.ConfirmDialogBodyText}");
-
+                BodyText = LocalisableString.Interpolate($"{dialogText}\n\n{DialogStrings.ConfirmDialogBodyText}");
                 Buttons = new PopupDialogButton[]
                 {
                     new PopupDialogDangerousButton
@@ -72,7 +68,6 @@ namespace osu.Game.Screens.Menu
             else
             {
                 BodyText = DialogStrings.ConfirmDialogBodyText;
-
                 Buttons = new PopupDialogButton[]
                 {
                     new PopupDialogOkButton